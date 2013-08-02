b75aea78c56acde25ad28670d43674f53e4e457f
Jon Phenow <j.phenow@gmail.com>
2013-08-02 10:48:35 -0500

    stupid SAML requires some have full closing XML tag

--- lib/saml_idp/signed_info_builder.rb
+++ lib/saml_idp/signed_info_builder.rb
@@ -31,15 +31,17 @@ module SamlIdp
     private :encoded
 
     def build
-      @build ||= builder.tag! "ds:SignedInfo", "xmlns:ds" => "http://www.w3.org/2000/09/xmldsig#" do |signed_info|
-        signed_info.tag! "ds:CanonicalizationMethod", Algorithm: "http://www.w3.org/2001/10/xml-exc-c14n#"
-        signed_info.tag! "ds:SignatureMethod", Algorithm: "http://www.w3.org/2000/09/xmldsig#rsa-#{algorithm_name}"
+      builder = Builder::XmlMarkup.new
+      builder.tag! "ds:SignedInfo", "xmlns:ds" => "http://www.w3.org/2000/09/xmldsig#" do |signed_info|
+        signed_info.tag!("ds:CanonicalizationMethod", Algorithm: "http://www.w3.org/2001/10/xml-exc-c14n#") {}
+        signed_info.tag!("ds:SignatureMethod", Algorithm: "http://www.w3.org/2000/09/xmldsig#rsa-#{algorithm_name}") {}
         signed_info.tag! "ds:Reference", URI: reference_string do |reference|
           reference.tag! "ds:Transforms" do |transforms|
-            transforms.tag! "ds:Transform", Algorithm: "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
-            transforms.tag! "ds:Transform", Algorithm: "http://www.w3.org/2001/10/xml-exc-c14n#"
+            transforms.tag!("ds:Transform", Algorithm: "http://www.w3.org/2000/09/xmldsig#enveloped-signature") {}
+            transforms.tag!("ds:Transform", Algorithm: "http://www.w3.org/2001/10/xml-exc-c14n#") {}
           end
-          reference.tag! "ds:DigestMethod", Algorithm: "http://www.w3.org/2000/09/xmldsig##{algorithm_name}"
+          reference.tag! "ds:DigestMethod", Algorithm: "http://www.w3.org/2000/09/xmldsig##{algorithm_name}" do
+          end
           reference.tag! "ds:DigestValue", digest_value
         end
       end
