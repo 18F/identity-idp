50676d722563656afe779c1ee6b82417e0a10690
Jon Phenow <j.phenow@gmail.com>
2013-08-05 09:53:33 -0500

    separate concerns of various controller methods

--- lib/saml_idp/controller.rb
+++ lib/saml_idp/controller.rb
@@ -7,7 +7,7 @@ module SamlIdp
     require 'uuid'
 
     attr_accessor :algorithm
-    attr_accessor :saml_acs_url
+    attr_accessor :saml_acs_url, :saml_request_id, :saml_request
 
     protected
 
@@ -16,12 +16,48 @@ module SamlIdp
     end
 
     def decode_SAMLRequest(saml_request)
+      self.saml_request = inflate_request saml_request
+      self.saml_request_id = authn_request_hash["ID"]
+      self.saml_acs_url = authn_request_hash["AssertionConsumerServiceURL"]
+    end
+
+    def encode_SAMLResponse(name_id, opts = {})
+      response_id, reference_id = get_saml_response_id, get_saml_reference_id
+      audience_uri = opts[:audience_uri] || saml_acs_url[/^(.*?\/\/.*?\/)/, 1]
+      issuer_uri = opts[:issuer_uri] || (defined?(request) && request.url) || "http://example.com"
+
+      SamlResponse.new(
+        reference_id,
+        response_id,
+        issuer_uri,
+        name_id,
+        audience_uri,
+        saml_request_id,
+        saml_acs_url,
+        algorithm
+      ).build
+    end
+
+    def inflate_request(raw_request)
       zstream  = Zlib::Inflate.new(-Zlib::MAX_WBITS)
-      @saml_request = zstream.inflate(Base64.decode64(saml_request))
-      zstream.finish
-      zstream.close
-      @saml_request_id = @saml_request[/ID=['"](.+?)['"]/, 1]
-      @saml_acs_url = @saml_request[/AssertionConsumerServiceURL=['"](.+?)['"]/, 1]
+      zstream.inflate(Base64.decode64(raw_request)).tap do
+        zstream.finish
+        zstream.close
+      end
+    end
+
+    def authn_request_hash
+      saml_request_hash.fetch("AuthnRequest") { {} }
+    end
+
+    def saml_request_hash
+      if saml_request
+        @saml_request_hash ||= Hash.from_xml(saml_request)
+      else
+        {}
+      end
+    rescue REXML::ParseException
+      nil
     end
 
     def get_saml_response_id
@@ -30,23 +66,6 @@ module SamlIdp
 
     def get_saml_reference_id
       UUID.generate
-    end
-
-    def encode_SAMLResponse(name_id, opts = {})
-      response_id, reference_id = get_saml_response_id, get_saml_reference_id
-      audience_uri = opts[:audience_uri] || saml_acs_url[/^(.*?\/\/.*?\/)/, 1]
-      issuer_uri = opts[:issuer_uri] || (defined?(request) && request.url) || "http://example.com"
-
-      response = SamlResponse.new reference_id,
-        response_id,
-        issuer_uri,
-        name_id,
-        audience_uri,
-        @saml_request_id,
-        @saml_acs_url,
-        algorithm
-
-      response.build
     end
   end
 end
