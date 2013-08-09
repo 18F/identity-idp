cd435a3aa47830d7bd2cb6e534332d74cd05c51d
Jon Phenow <j.phenow@gmail.com>
2013-08-09 16:25:52 -0500

    attributer module

--- lib/saml_idp/service_provider.rb
+++ lib/saml_idp/service_provider.rb
@@ -1,17 +1,13 @@
 require 'httparty'
+require 'saml_idp/attributeable'
+require 'saml_idp/incoming_metadata'
 module SamlIdp
   class ServiceProvider
-    def self.expose_attribute(att)
-      define_method(att) { attributes[att] }
-    end
+    include Attributeable
+    attribute :fingerprint
+    attribute :metadata_url
 
-    attr_accessor :attributes
-    expose_attribute :fingerprint
-    expose_attribute :metadata_url
-
-    def initialize(attributes = {})
-      self.attributes = (attributes || {}).with_indifferent_access
-    end
+    delegate :config, to: :SamlIdp
 
     def valid?
       attributes.present?
@@ -39,5 +35,6 @@ module SamlIdp
     def request_metadata
       HTTParty.get(metadata_url).body
     end
+    private :request_metadata
   end
 end
