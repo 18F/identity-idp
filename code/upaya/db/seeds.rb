a1cd5090a6136dc6c983716b8f00fb28376647fb
Justin Grevich <justin@grevi.ch>
2016-04-05 19:27:57 -0700

    Initial Rails app based on save-ferris
    New rails 4 app using save-ferris gemset, db schema, and locales.

---
+++ code/upaya/db/seeds.rb
@@ -0,0 +1,12 @@
+require "#{Rails.root}/db/data/faq_importer"
+require "#{Rails.root}/lib/security_question_populator"
+include SecurityQuestionPopulator
+
+# create second factors (email and sms)
+%w(Email Mobile).collect { |factor| SecondFactor.find_or_create_by!(name: factor) }
+
+AppSetting.find_or_create_by!(name: 'RegistrationsEnabled') do |setting|
+  setting.value = '1'
+end
+
+# populate_security_questions unless Rails.env.test?
