23438b4c67acad9e659a0a3869bf8a8507ee97e0
Brendan Sudol <brendan.sudol@gsa.gov>
2016-04-12 17:28:20 -0400

    remove babel stage-0 for now

--- code/upaya/webpack.config.js
+++ code/upaya/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'stage-0', 'react'],
+          presets: ['es2015', 'react'],
         }
       },
     ]
