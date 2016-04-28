9f15fec15efc89e0068db1ed6caa38f882c6d0ea
Brendan Sudol <brendan.sudol@gsa.gov>
2016-04-28 11:39:00 -0400

    implement basic design concepts
    **why**
    * to provide a nice user experience

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,8 +1,12 @@
 SimpleForm.setup do |config|
   config.button_class = 'btn btn-primary'
   config.boolean_label_class = nil
-  config.input_class = 'block col-12 field'
   config.error_notification_tag = :div
+
+  config.wrappers :base do |b|
+    b.use :html5
+    b.use :input, class: 'field'
+  end
 
   config.wrappers :vertical_form, tag: 'div', class: 'mb2', error_class: 'has-error' do |b|
     b.use :html5
@@ -12,7 +16,7 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
     b.use :label
-    b.use :input
+    b.use :input, class: 'block col-12 field'
     b.use :error, wrap_with: { tag: 'div', class: 'error-description' }
   end
 
