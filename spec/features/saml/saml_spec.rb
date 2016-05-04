20faad22052fc0538ac20303606b249e980e58fa
amoose <amos@amosarts.com>
2016-05-04 11:33:19 -0700

    Adds SAML logout specs

--- spec/features/saml/saml_spec.rb
+++ spec/features/saml/saml_spec.rb
@@ -101,30 +101,117 @@ feature 'saml api', devise: true, sms: true do
 
   context 'visiting /test/saml' do
     scenario 'it requires 2FA' do
-      sign_in_user
       visit '/test/saml'
-
+      sign_in_user
       expect(current_path).to eq(users_otp_path)
       expect(page).to have_content(I18n.t('devise.two_factor_authentication.otp_setup'))
     end
+  end
 
-    scenario 'it requires security questions' do
-      user = create(:user)
-      confirm_last_user
-      sign_in_user(user)
+  context 'visiting /api/saml/logout' do
+    context 'when logged in to single SP with IdP-initiated logout' do
+      let(:user) { create(:user, :signed_up) }
+      let(:xmldoc) { XmlDoc.new('feature', 'request_assertion') }
+      let(:response_xmldoc) { XmlDoc.new('feature', 'response_assertion') }
 
-      # choose OTP delivery
-      check 'Email'
-      uncheck 'Mobile'
-      click_button 'Submit'
-      # confirm OTP receipt
-      fill_in 'code', with: user.otp_code
-      click_button 'Submit'
+      before do
+        visit sp1_authnrequest
+        authenticate_user(user)
 
-      visit '/test/saml'
+        @asserted_session_index = response_xmldoc.assertion_statement_node['SessionIndex']
+        visit destroy_user_session_url
+      end
 
-      expect(current_path).to eq(users_questions_path)
-      expect(page).to have_content('You must setup your security questions to continue')
+      it 'successfully logs the user out' do
+        click_button 'Submit'
+        expect(page).to have_content t('devise.sessions.signed_out')
+      end
+
+      it 'generates a signed LogoutRequest' do
+        expect(xmldoc.request_assertion).to_not be_nil
+      end
+
+      it 'references the correct SessionIndex' do
+        expect(xmldoc.logout_asserted_session_index).to eq(@asserted_session_index)
+      end
+
+      it 'generates logout request with Issuer' do
+        expect(xmldoc.issuer_nodeset.length).to eq(1)
+        expect(xmldoc.issuer_nodeset[0].content).to eq "https://#{Figaro.env.domain_name}/api/saml"
+      end
+    end
+
+    context 'when logged in to a single SP with SP-initiated logout' do
+      let(:user) { create(:user, :signed_up) }
+      let(:xmldoc) { XmlDoc.new('feature', 'logout_assertion') }
+
+      before do
+        visit sp1_authnrequest
+        authenticate_user(user)
+
+        request = OneLogin::RubySaml::Logoutrequest.new
+        settings = sp1_saml_settings
+        settings.name_identifier_value = user.uuid
+
+        visit request.create(settings)
+      end
+
+      it 'signs out the user from IdP' do
+        expect(user.current_sign_in_at).to be_nil
+
+        visit edit_user_registration_path
+
+        expect(page).to have_content I18n.t 'devise.failure.unauthenticated'
+      end
+
+      it 'contains an issuer nodeset' do
+        expect(xmldoc.issuer_nodeset.length).to eq(1)
+        expect(xmldoc.issuer_nodeset[0].content).to eq "https://#{Figaro.env.domain_name}/api/saml"
+      end
+
+      it 'contains a signature nodeset' do
+        expect(xmldoc.signature_nodeset.length).to eq(1)
+      end
+
+      it 'contains a signature method nodeset with SHA256 algorithm' do
+        expect(xmldoc.signature_method_nodeset.length).to eq(1)
+        expect(xmldoc.signature_method_nodeset[0].attr('Algorithm')).
+          to eq('http://www.w3.org/2001/04/xmldsig-more#rsa-sha256')
+      end
+
+      it 'contains a digest method nodeset with SHA256 algorithm' do
+        expect(xmldoc.digest_method_nodeset.length).to eq(1)
+        expect(xmldoc.digest_method_nodeset[0].attr('Algorithm')).
+          to eq('http://www.w3.org/2001/04/xmlenc#sha256')
+      end
+
+      it 'returns a SAMLResponse' do
+        expect(page.find('#SAMLResponse', visible: false)).to be_truthy
+      end
+
+      it 'disables cache' do
+        expect(page.response_headers['Pragma']).to eq 'no-cache'
+      end
+
+      it 'deactivates the identity' do
+        expect(user.active_identities.size).to eq(0)
+      end
+    end
+
+    context 'with authentication at a SP and session times out' do
+      let(:logout_user) { create(:user, :signed_up) }
+
+      before do
+        visit sp1_authnrequest
+        authenticate_user(logout_user)
+      end
+
+      it 'redirects to root' do
+        Timecop.travel(Devise.timeout_in + 1.second)
+        visit destroy_user_session_url
+        expect(page.current_path).to eq('/')
+        Timecop.return
+      end
     end
   end
 end
