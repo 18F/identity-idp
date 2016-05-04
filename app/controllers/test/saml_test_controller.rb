0cd3fd463666efc5c5fc158bf73c00506bdab2bd
amoose <amos@amosarts.com>
2016-05-04 10:07:51 -0700

    Adds saml auth helper to saml test controller
    Normalizes test auth flow with Helper

--- app/controllers/test/saml_test_controller.rb
+++ app/controllers/test/saml_test_controller.rb
@@ -1,14 +1,16 @@
 require 'saml_idp_constants'
 require 'saml_idp/logout_request_builder'
+require './spec/support/saml_auth_helper'
 
-# rubocop:disable ClassLength
 module Test
   class SamlTestController < ApplicationController
+    include SamlAuthHelper
+
     skip_before_action :verify_authenticity_token, only: [:decode_response, :decode_slo_request]
 
     def start
       request = OneLogin::RubySaml::Authrequest.new
-      redirect_to(request.create(saml_settings,
+      redirect_to(request.create(test_saml_settings,
                                  {},
                                  key: Rails.application.secrets.saml_client_private_key,
                                  algorithm: :sha256))
@@ -26,8 +28,8 @@ module Test
 
       logout_request_builder = SamlIdp::LogoutRequestBuilder.new(
         "_#{UUID.generate}", # response_id
-        saml_settings.issuer,
-        saml_settings.idp_slo_target_url,
+        test_saml_settings.issuer,
+        test_saml_settings.idp_slo_target_url,
         UUID.generate,       # name_id
         'bogus_fuzzy_lambs', # name_qualifier
         UUID.generate,       # session_index
@@ -46,12 +48,12 @@ module Test
     def decode_response
       response = OneLogin::RubySaml::Response.new(
         Base64.decode64(params[:SAMLResponse]),
-        private_key: saml_settings.private_key,
+        private_key: test_saml_settings.private_key,
         private_key_password: ''
       )
-      response.settings = saml_settings
+      response.settings = test_saml_settings
 
-      # Ruby-saml only understands validation of Assertions. If it's
+      # TODO(awong): Ruby-saml only understands validation of Assertions. If it's
       # a LogoutRequest, just validate the signature.
       doc = Saml::XML::Document.parse(response.document.to_s)
       if doc.at_xpath('/samlp:Response', samlp: Saml::XML::Namespaces::PROTOCOL)
@@ -84,7 +86,7 @@ module Test
           logout_request_id = logout_request.id
           logout_response = OneLogin::RubySaml::SloLogoutresponse.new
           saml_response = logout_response.create(
-            saml_settings,
+            test_saml_settings,
             logout_request_id,
             nil,
             RelayState: params[:RelayState]
@@ -103,45 +105,11 @@ module Test
 
     private
 
-    # rubocop:disable AbcSize, MethodLength
-    def saml_settings
-      settings = OneLogin::RubySaml::Settings.new
-
-      # SP settings
-      settings.assertion_consumer_service_url = 'http://localhost:3000/test/saml/decode_assertion'
-      settings.assertion_consumer_logout_service_url = 'http://localhost:3000/test/saml/decode_slo_request'
-      settings.certificate = Rails.application.secrets.saml_cert
-      settings.private_key = private_key
-      settings.authn_context = Saml::Idp::Constants::LOA1_AUTHNCONTEXT_CLASSREF
-
-      # SP + IdP Settings
-      settings.issuer = 'https://upaya-dev.ngrok.io'
-      settings.security[:logout_requests_signed] = true
-      settings.security[:embed_sign] = true
-      settings.security[:digest_method] = 'http://www.w3.org/2001/04/xmlenc#sha256'
-      settings.security[:signature_method] = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'
-      settings.name_identifier_format = 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent'
-      settings.double_quote_xml_attribute_values = true
-      # IdP setting
-      settings.idp_sso_target_url = 'https://upaya-dev.ngrok.io/api/saml/auth'
-      settings.idp_slo_target_url = 'https://upaya-dev.ngrok.io/api/saml/logout'
-      settings.idp_cert_fingerprint = fingerprint
-
+    def test_saml_settings
+      settings = sp1_saml_settings
+      settings.idp_sso_target_url = "http://#{Figaro.env.domain_name}/api/saml/auth"
+      settings.idp_slo_target_url = "http://#{Figaro.env.domain_name}/api/saml/logout"
       settings
-    end
-    # rubocop:enable AbcSize, MethodLength
-
-    def private_key
-      OpenSSL::PKey::RSA.new(
-        File.read(Rails.root + 'config/saml.key.enc'),
-        Rails.application.secrets.saml_passphrase
-      ).to_pem
-    end
-
-    def fingerprint
-      'F9:A3:9B:2F:8F:1C:E2:79:27:69:EB:32:ED:2A:D5:' \
-      'A2:A7:58:5F:C0:74:8A:4A:03:D9:0F:77:A5:89:7F:F9:68'
     end
   end
 end
-# rubocop:enable ClassLength
