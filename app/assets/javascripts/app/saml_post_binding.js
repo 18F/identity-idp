aa7ba4b815c8903d66d067084a768a11e7cee977
Brendan Sudol <brendan.sudol@gsa.gov>
2016-05-16 17:35:01 -0400

    Improve saml form auto-submit js

--- app/assets/javascripts/app/saml_post_binding.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var samlForm = document.getElementById('saml-post-binding');
-  if (samlForm) {
-    document.body.className += ' hidden';
-    samlForm.submit();
-  }
-});
