97e0ac75680f7b3bffe4959a7698f579fc426dba
Brendan Sudol <brendan.sudol@gsa.gov>
2016-06-14 14:51:52 -0400

    Add signup/login navigation
    **Why**: to make it easy to see where you are
    and navigate between sign up & log in pages

--- spec/views/devise/registrations/new.html.slim_spec.rb
+++ spec/views/devise/registrations/new.html.slim_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'devise/registrations/new.html.slim' do
   before do
     @register_user_email_form = RegisterUserEmailForm.new
+    allow(view).to receive(:controller_name).and_return('registrations')
   end
 
   it 'has a localized title' do
@@ -15,6 +16,20 @@ describe 'devise/registrations/new.html.slim' do
     render
 
     expect(rendered).to have_selector('h2', text: t('upaya.headings.registrations.new'))
+  end
+
+  it 'has proper css classes for log in / sign up nav' do
+    render
+
+    base_class = 'btn btn-primary border-box col-12 center'
+
+    sign_up_class = "#{base_class} bg-navy"
+    expect(rendered).
+      to have_xpath("//a[@class='#{sign_up_class}' and @href='#{new_user_registration_path}']")
+
+    log_in_class = "#{base_class} bg-gray"
+    expect(rendered).
+      to have_xpath("//a[@class='#{log_in_class}' and @href='#{new_user_session_path}']")
   end
 
   it 'sets form autocomplete to off' do
--- spec/views/devise/sessions/new.html.slim_spec.rb
+++ spec/views/devise/sessions/new.html.slim_spec.rb
@@ -5,6 +5,7 @@ describe 'devise/sessions/new.html.slim' do
     allow(view).to receive(:resource).and_return(build_stubbed(:user))
     allow(view).to receive(:resource_name).and_return(:user)
     allow(view).to receive(:devise_mapping).and_return(Devise.mappings[:user])
+    allow(view).to receive(:controller_name).and_return('sessions')
   end
 
   it 'has a localized title' do
@@ -19,12 +20,26 @@ describe 'devise/sessions/new.html.slim' do
     expect(rendered).to have_selector('h2', text: t('upaya.headings.log_in'))
   end
 
+  it 'has proper css classes for log in / sign up nav' do
+    render
+
+    base_class = 'btn btn-primary border-box col-12 center'
+
+    sign_up_class = "#{base_class} bg-gray"
+    expect(rendered).
+      to have_xpath("//a[@class='#{sign_up_class}' and @href='#{new_user_registration_path}']")
+
+    log_in_class = "#{base_class} bg-navy"
+    expect(rendered).
+      to have_xpath("//a[@class='#{log_in_class}' and @href='#{new_user_session_path}']")
+  end
+
   it 'includes a link to create a new account' do
     render
 
     expect(rendered).
       to have_link(
-        t('upaya.links.create_new_account'), href: new_user_registration_path
+        t('upaya.links.sign_up'), href: new_user_registration_path
       )
   end
 end
