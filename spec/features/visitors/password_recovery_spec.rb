c5d89dc8925fe6048c2f3ef36d494d2b7f577f7c
Moncef Belyamani <moncef.belyamani@gsa.gov>
2016-06-28 12:31:24 -0400

    Update tests to work with new mailer layout
    **Why**: So they can pass again.

--- spec/features/visitors/password_recovery_spec.rb
+++ spec/features/visitors/password_recovery_spec.rb
@@ -44,7 +44,7 @@ feature 'Password Recovery' do
 
     it 'includes a link to reset the password in the email' do
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/reset_password_token/)
 
       expect(current_path).to eq edit_user_password_path
     end
@@ -58,20 +58,21 @@ feature 'Password Recovery' do
   # Scenario: User that has only confirmed their email can reset their password
   #   Given I have not created my password yet
   #   And I click the Forgot password? link and enter my email
-  #   When I click the link in the password reset email
-  #   Then I can set a new password
+  #   Then I receive the confirmation email again
+  #   And when I click the link in the confirmation email
+  #   Then I can set my password
   context 'user with only email confirmation resets password', email: true do
     before do
       sign_up_with('email@example.com')
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/confirmation_token/)
       reset_email
       visit root_path
       click_link t('upaya.headings.passwords.forgot')
       fill_in 'Email', with: 'email@example.com'
       click_button t('upaya.forms.buttons.reset_password')
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/confirmation_token/)
     end
 
     it 'shows the password form' do
@@ -88,13 +89,13 @@ feature 'Password Recovery' do
     before do
       sign_up_with('email@example.com')
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/confirmation_token/)
       reset_email
       visit new_user_confirmation_path
       fill_in 'Email', with: 'email@example.com'
       click_button 'Resend confirmation instructions'
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/confirmation_token/)
     end
 
     it 'shows the password form' do
@@ -116,7 +117,7 @@ feature 'Password Recovery' do
       fill_in 'Email', with: 'email@example.com'
       click_button t('upaya.forms.buttons.reset_password')
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/reset_password_token/)
     end
 
     it 'shows the password form' do
@@ -174,7 +175,7 @@ feature 'Password Recovery' do
       fill_in 'Email', with: 'email@example.com'
       click_button t('upaya.forms.buttons.reset_password')
       open_last_email
-      click_first_link_in_email
+      click_email_link_matching(/reset_password_token/)
     end
 
     it 'shows the password form' do
--- spec/requests/edit_user_spec.rb
+++ spec/requests/edit_user_spec.rb
@@ -45,7 +45,7 @@ describe 'user edits their account', email: true do
 
   shared_examples 'changing_email' do
     it 'displays a notice informing the user their email has been confirmed when user confirms' do
-      get_via_redirect links_in_email(last_email).first
+      get_via_redirect parse_email_for_link(last_email, /confirmation_token/)
 
       expect(flash[:notice]).to eq t('devise.confirmations.confirmed')
       expect(response).to render_template('user_mailer/email_changed')
@@ -57,12 +57,12 @@ describe 'user edits their account', email: true do
       expect(EmailNotifier).to receive(:new).with(user).and_return(notifier)
       expect(notifier).to receive(:send_email_changed_email)
 
-      get_via_redirect links_in_email(last_email).first
+      get_via_redirect parse_email_for_link(last_email, /confirmation_token/)
     end
 
     it 'confirms email when user clicks link in email while signed out' do
       delete_via_redirect destroy_user_session_path
-      get_via_redirect links_in_email(last_email).first
+      get_via_redirect parse_email_for_link(last_email, /confirmation_token/)
 
       expect(flash[:notice]).to eq t('devise.confirmations.confirmed')
     end
