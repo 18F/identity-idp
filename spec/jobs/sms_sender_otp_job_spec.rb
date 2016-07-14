90a91ef1fb1cca4cb074497a2c15d391808ddf8f
Brendan Sudol <brendan.sudol@gsa.gov>
2016-07-14 13:50:31 -0400

    Shorten SMS message
    **Why**: so code is visible via phone notifications

--- spec/jobs/sms_sender_otp_job_spec.rb
+++ spec/jobs/sms_sender_otp_job_spec.rb
@@ -8,7 +8,7 @@ describe SmsSenderOtpJob, sms: true do
       expect(messages.size).to eq(1)
       msg = messages.first
       expect(msg.number).to eq('555-5555')
-      expect(msg.body).to include('secure one-time password')
+      expect(msg.body).to include('one-time password')
       expect(msg.body).to include('1234')
     end
   end
