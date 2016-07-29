0301d233fc0e7299e1c1bdae215ecbe9bb9c46de
Justin Grevich <justin@grevi.ch>
2016-07-29 11:31:42 -0700

    Add config for custom sidekiq server url (#313)
    __Why__
    This is needed to support Elasticache and other remote services.
    
    __How__
    Add a block to the initializer and set a default config.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,9 @@
 Sidekiq::Logging.logger.level = Logger::WARN
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: Figaro.env.redis_url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: Figaro.env.redis_url }
+end
