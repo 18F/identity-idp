86f7d6368b8aaefb5d257e7498b2997be4c952f0
Brendan Sudol <brendan.sudol@gsa.gov>
2016-08-01 10:19:45 -0400

    Fix link on verify email page
    **Why**: confirm email resend link was off

---
+++ spec/views/devise/registrations/verify_email.html.slim_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'devise/registrations/verify_email.html.slim' do
+  before do
+    allow(view).to receive(:email).and_return('foo@bar.com')
+  end
+
+  it 'has a localized title' do
+    expect(view).to receive(:title).with(t('titles.registrations.verify_email'))
+
+    render
+  end
+
+  it 'has a localized header' do
+    render
+
+    expect(rendered).to have_selector('h1', text: t('headings.registrations.verify_email'))
+  end
+
+  it 'contains link to resend confirmation page' do
+    render
+
+    expect(rendered).to have_link(t('links.resend'), href: new_user_confirmation_path)
+  end
+end
