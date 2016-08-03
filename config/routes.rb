46c90d92a4e799bdd65539f49b47d7f7f89d8a7f
Brendan Sudol <brendan.sudol@gsa.gov>
2016-08-03 16:39:05 -0400

    Add edit password functionality
    **Why**: this is a feature we want to provide

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,9 @@ Rails.application.routes.draw do
 
     patch '/confirm' => 'users/confirmations#confirm'
 
-    match '/edit/email' => 'users/edit_info#email', via: [:get, :put], as: :edit_email
-    match '/edit/mobile' => 'users/edit_info#mobile', via: [:get, :put], as: :edit_mobile
+    match '/edit/email' => 'users/edit_info#email', via: [:get, :put]
+    match '/edit/mobile' => 'users/edit_info#mobile', via: [:get, :put]
+    match '/edit/password' => 'users/edit_info#password', via: [:get, :put], as: :change_password
 
     get '/phone_setup' => 'devise/two_factor_authentication_setup#index'
     patch '/phone_setup' => 'devise/two_factor_authentication_setup#set'
