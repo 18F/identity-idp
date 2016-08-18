140a6c9c58fb620fff9534c3b6e4e4e3e85239c1
Brian Hurst <brian.hurst@gsa.gov>
2016-08-18 15:37:15 -0400

    Update form field formatter
    **Why**: There were some issues with the previous library we were using, this instead uses Field-Kit which is more flexible, and also being implemented by the USWDS

--- app/assets/javascripts/app/form-validation.js
+++ app/assets/javascripts/app/form-validation.js
@@ -1,13 +1,9 @@
-import 'classlist.js';
 import h5f from 'h5f';
+
+import validateField from './validate-field';
 
 
 const validate = {
-  msgs: {
-    missing: 'Please fill in this field.',
-    mismatch: 'Please match the requested format.',
-  },
-
   init() {
     this.form = document.querySelector('form');
     if (!this.form) return;
@@ -25,7 +21,7 @@ const validate = {
   },
 
   addEvents() {
-    this.form.addEventListener('change', e => this.validateField(e.target));
+    this.form.addEventListener('change', e => validateField(e.target));
     this.form.addEventListener('submit', e => this.validateForm(e));
   },
 
@@ -34,47 +30,12 @@ const validate = {
 
     const fields = this.form.querySelectorAll('.field');
     for (let i = 0; i < fields.length; i++) {
-      this.validateField(fields[i]);
+      validateField(fields[i]);
     }
 
     // add focus to first invalid input
     const invalidField = this.form.querySelector(':invalid');
     if (invalidField) invalidField.focus();
-  },
-
-  validateField(f) {
-    f.classList.add('interacted');
-
-    const parent = f.parentNode;
-    const errorMsg = parent.querySelector('.error-message');
-
-    if (errorMsg !== null) parent.removeChild(errorMsg);
-
-    if (!f.validity.valid) this.addInvalidMarkup(f);
-    else this.removeInvalidMarkup(f);
-  },
-
-  addInvalidMarkup(f) {
-    f.setAttribute('aria-invalid', 'true');
-    f.setAttribute('aria-describedby', `alert_${f.id}`);
-
-    if (f.validity.valueMissing) f.setCustomValidity(this.msgs.missing);
-    else if (f.validity.patternMismatch
-      || f.validity.typeMismatch) f.setCustomValidity(this.msgs.mismatch);
-    else f.setCustomValidity('');
-
-    f.insertAdjacentHTML(
-      'afterend',
-      `<div role='alert' class='error-message red h5' id='alert_${f.id}'>
-        ${f.validationMessage}
-      </div>`
-    );
-  },
-
-  removeInvalidMarkup(f) {
-    f.parentNode.classList.remove('has-error');
-    f.removeAttribute('aria-invalid');
-    f.removeAttribute('aria-describedby');
   },
 };
 
---
+++ app/assets/javascripts/app/modules/zip-code-formatter.js
@@ -0,0 +1,43 @@
+import { DelimitedTextFormatter } from 'field-kit';
+
+/**
+ * @const
+ * @private
+ */
+const DIGITS_PATTERN = /^\d*$/;
+
+/**
+ * @extends DelimitedTextFormatter
+ */
+class ZipCodeFormatter extends DelimitedTextFormatter {
+  constructor() {
+    super('-', true);
+    this.maximumLength = 9 + 1;
+  }
+
+  /**
+   * @param {number} index
+   * @returns {boolean}
+   */
+  hasDelimiterAtIndex(index) {
+    return index === 5;
+  }
+
+  /**
+   * Determines whether the given change should be allowed and, if so, whether
+   * it should be altered.
+   *
+   * @param {TextFieldStateChange} change
+   * @param {function(string)} error
+   * @returns {boolean}
+   */
+  isChangeValid(change, error) {
+    if (DIGITS_PATTERN.test(change.inserted.text)) {
+      return super.isChangeValid(change, error);
+    }
+
+    return false;
+  }
+}
+
+export default ZipCodeFormatter;
