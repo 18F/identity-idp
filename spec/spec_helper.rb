6802d40e686225ece00c669ebcaba6765394c1c2
amoose <amos@amosarts.com>
2016-08-22 16:21:06 -0700

    Enables color in rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,10 @@ ENV['RAILS_ENV'] ||= 'test'
 
 RSpec.configure do |config|
   # see more settings at spec/rails_helper.rb
-
   config.raise_errors_for_deprecations!
   config.order = :random
+  config.color = true
+
   # allows you to run only the failures from the previous run:
   # rspec --only-failures
   config.example_status_persistence_file_path = './tmp/rspec-examples.txt'
