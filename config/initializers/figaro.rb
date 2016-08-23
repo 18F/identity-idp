60834e33031b8da154b77ec365f061fd632227ca
Moncef Belyamani <monfresh@users.noreply.github.com>
2016-08-23 11:38:22 -0400

    Add throttle to limit new OTP requests (#395)
    **Why**: Every time we send an OTP via SMS or voice, it costs us money.
    To prevent someone maliciously trying to empty our pockets, I've
    added a blocklist with Allow2Ban so that the user will be blocked from
    making any more requests for 10 minutes after requesting 5 OTP codes
    in the span of 5 minutes. Those three numbers are configurable via
    `application.yml`.
    
    Note that the throttle discriminator is the phone number as opposed to
    the user since multiple users can have the same phone number.
    
    The Allow2Ban filter requires an ActiveSupport-compliant cache store
    that accepts an options hash as a second argument to the `read` method,
    hence the need for the readthis gem.

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,6 +1,7 @@
 Figaro.require_keys(
   'allow_third_party_auth', 'domain_name', 'idp_sso_target_url',
-  'logins_per_ip_limit', 'logins_per_ip_period', 'requests_per_ip_limit',
-  'requests_per_ip_period', 'saml_passphrase', 'secret_key_base',
-  'twilio_accounts'
+  'logins_per_ip_limit', 'logins_per_ip_period', 'otp_delivery_blocklist_bantime',
+  'otp_delivery_blocklist_findtime', 'otp_delivery_blocklist_maxretry',
+  'requests_per_ip_limit', 'requests_per_ip_period', 'saml_passphrase',
+  'secret_key_base', 'twilio_accounts'
 )
