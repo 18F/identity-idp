3f198de89ef12f195dba7ece9e4de8e3c477d7f1
Justin Grevich <justin@grevi.ch>
2016-09-01 15:08:34 -0700

    Override the assets_roles var to include the worker host
    __Why__
    Although it's a worker host it still needs to compile assets in order to send
    html formatted email.
    
    __How__
    Override the default values for asset_roles used by capistrano-rails to include
    the worker host.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 # GLOBAL CONFIG
 #################
 set :application, 'idp'
+set :assets_roles, [:web, :worker]
 # set branch based on env var or ask with the default set to the current local branch
 set :branch, ENV['branch'] || ENV['BRANCH'] || ask(:branch, `git branch`.match(/\* (\S+)\s/m)[1])
 set :bundle_without, 'deploy development doc test'
@@ -16,7 +17,7 @@ set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :rails_env, :production
 set :repo_url, 'https://github.com/18F/identity-idp.git'
 set :sidekiq_options, ''
-set :sidekiq_queue, [:mailers, :sms, :voice, :analytics]
+set :sidekiq_queue, [:analytics, :mailers, :sms, :voice]
 set :sidekiq_monit_use_sudo, true
 set :sidekiq_user, 'ubuntu'
 set :ssh_options, forward_agent: false, user: 'ubuntu'
