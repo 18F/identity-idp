991e1b5f9d2c079ea2e36662a18150cfe44f5694
Peter Karman <pkarman@users.noreply.github.com>
2016-09-07 12:13:18 -0500

    Decrease idle session timeout to 8 minutes (#454)
    **Why**: Mitigate session vulnerability.
    
    NOTE this also alphabetizes the application.yml settings
    and removes a couple of duplicates.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -152,7 +152,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 15.minutes
+  config.timeout_in = Figaro.env.session_timeout_in.to_i.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
