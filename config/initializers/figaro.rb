e33c6ba41658d2d22fe0620269880679d9b2fb54
Moncef Belyamani <moncef.belyamani@gsa.gov>
2016-09-14 10:49:38 -0400

    Add new `enable_test_routes` Figaro key
    **Why**: To make it easier to enable or disable the test routes on any
    particular server, rather than using the domain name as the
    differentiator. This conforms to the Twelve Factor App principles, and
    keeps our configuration consistent in a single file.

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,5 +1,5 @@
 Figaro.require_keys(
-  'allow_third_party_auth', 'domain_name', 'idp_sso_target_url',
+  'allow_third_party_auth', 'domain_name', 'enable_test_routes', 'idp_sso_target_url',
   'logins_per_ip_limit', 'logins_per_ip_period', 'otp_delivery_blocklist_bantime',
   'otp_delivery_blocklist_findtime', 'otp_delivery_blocklist_maxretry',
   'requests_per_ip_limit', 'requests_per_ip_period', 'saml_passphrase',
