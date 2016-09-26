a92fd1f10c8e45f4bf5500c8b222381ba50d1bf9
Peter Karman <pkarman@users.noreply.github.com>
2016-09-26 09:37:59 -0500

    Add test coverage for Idv::Session (#501)
    **Why**: 100% test coverage is a virtue

--- spec/services/idv/session_spec.rb
+++ spec/services/idv/session_spec.rb
@@ -12,9 +12,25 @@ describe Idv::Session do
     end
 
     it 'allows supported attributes' do
-      subject.vendor = 'bar'
+      Idv::Session::VALID_SESSION_ATTRIBUTES.each do |attr|
+        subject.send attr, 'foo'
+        expect(subject.send(attr)).to eq 'foo'
+        subject.send "#{attr}=".to_sym, 'foo'
+        expect(subject.send(attr)).to eq 'foo'
+      end
+    end
+  end
 
-      expect(subject.vendor).to eq 'bar'
+  describe '#respond_to_missing?' do
+    it 'disallows un-supported attributes' do
+      expect(subject.respond_to?(:foo=, false)).to eq false
+    end
+
+    it 'allows supported attributes' do
+      Idv::Session::VALID_SESSION_ATTRIBUTES.each do |attr|
+        expect(subject.respond_to?(attr, false)).to eq true
+        expect(subject.respond_to?("#{attr}=".to_sym, false)).to eq true
+      end
     end
   end
 end
