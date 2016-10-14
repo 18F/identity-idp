9a61ecbad57e78b1b19267260ef7cccbf06c4b56
Brendan Sudol <brendan.sudol@gsa.gov>
2016-10-14 13:08:21 -0400

    Tweak non babelified js
    **Why**: scripts in misc not being transpiled, thus cant use things like "let"

--- app/assets/javascripts/misc/i18n-mode.js
+++ app/assets/javascripts/misc/i18n-mode.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-var, vars-on-top */
+
 document.addEventListener('DOMContentLoaded', function() {
   const lookupTxt = '<small class="i18n-anchor">';
   const inputs = document.querySelectorAll('input[type="submit"]');
 
   if (inputs) {
-    for (let i = 0; i < inputs.length; i++) {
+    for (var i = 0; i < inputs.length; i++) {
       const input = inputs[i];
       const val = input.value;
       const i18nStart = val.indexOf(lookupTxt);
