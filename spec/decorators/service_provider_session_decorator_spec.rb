884ccba41fb9407611aaf31e8ddd04a7dd0a5c8e
Zach Margolis <Zachary_B_Margolis@omb.eop.gov>
2016-10-31 12:46:50 -0400

    Add specs to make sure *SessionDecorator classes don't diverge
    **Why**:
    Because Ruby does not have static interfaces, we may need some help
    making sure that classes are interchangeable

---
+++ spec/decorators/service_provider_session_decorator_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ServiceProviderSessionDecorator do
+  it 'has the same public API as SessionDecorator' do
+    SessionDecorator.public_instance_methods.each do |method|
+      expect(described_class.public_method_defined?(method)).to be(true),
+        "expected #{described_class} to have ##{method}"
+    end
+  end
+end
