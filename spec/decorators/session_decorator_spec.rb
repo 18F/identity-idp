e23e1cf014c9a7723d69b395e5844de0b1cd2723
Jessie A. Young <jessieay@users.noreply.github.com>
2016-11-01 13:13:47 -0700

    Move more sp-specific view logic into decorators (#651)
    **Why**:
    * Moving logic out of views is a good thing
    * This also PR introduced usage of null view template, which we can take
      advantage of in the future when we want to render a template for one
      use case and "nothing" for another.
    * Add specs to make sure *SessionDecorator classes don't diverge

---
+++ spec/decorators/session_decorator_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe SessionDecorator do
+  it 'has the same public API as ServiceProviderSessionDecorator' do
+    ServiceProviderSessionDecorator.public_instance_methods.each do |method|
+      expect(
+        described_class.public_method_defined?(method)
+      ).to be(true), "expected #{described_class} to have ##{method}"
+    end
+  end
+end
