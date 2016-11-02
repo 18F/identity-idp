a895268d34082f49777dbb34100002a242cf5f5d
Jessie A. Young <jessie.a.young@gmail.com>
2016-11-02 10:46:47 -0700

    Remove pundit
    **Why**:
    * It is unused

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
-  include Pundit
   include BrandedExperience
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from Pundit::NotAuthorizedError, with: :render_401
   rescue_from ActionController::InvalidAuthenticityToken,
               with: :invalid_auth_token
 
