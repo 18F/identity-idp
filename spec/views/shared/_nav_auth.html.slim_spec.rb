9ba83893be83a1c75f4d97d5c164e4a426b28930
Peter Karman <pkarman@users.noreply.github.com>
2016-11-02 12:48:52 -0500

    Do not show My Account link within 2FA process (#664)
    **Why**: The link to My Account should only show
    when the user has completed the full 2FA process.

--- spec/views/shared/_nav_auth.html.slim_spec.rb
+++ spec/views/shared/_nav_auth.html.slim_spec.rb
@@ -5,7 +5,7 @@ describe 'shared/_nav_auth.html.slim' do
     before do
       @user = build_stubbed(:user, :signed_up)
       allow(view).to receive(:current_user).and_return(@user)
-      allow(view).to receive(:signed_in?).and_return(true)
+      allow(view).to receive(:user_fully_authenticated?).and_return(true)
     end
 
     it 'contains welcome message' do
@@ -14,10 +14,30 @@ describe 'shared/_nav_auth.html.slim' do
       expect(rendered).to have_content "Welcome #{@user.email}"
     end
 
+    it 'contains link to my account' do
+      render
+
+      expect(rendered).to have_link(t('shared.nav_auth.my_account'), href: profile_path)
+    end
+
     it 'contains sign out link' do
       render
 
       expect(rendered).to have_link(t('links.sign_out'), href: destroy_user_session_path)
+    end
+  end
+
+  context 'user has entered password but not complete 2fa' do
+    before do
+      @user = build_stubbed(:user, :signed_up)
+      allow(view).to receive(:current_user).and_return(@user)
+      allow(view).to receive(:user_fully_authenticated?).and_return(false)
+    end
+
+    it 'does not contain link to my account' do
+      render
+
+      expect(rendered).to_not have_link(t('shared.nav_auth.my_account'), href: profile_path)
     end
   end
 end
