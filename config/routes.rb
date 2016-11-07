b6869b33a8cae113e2f048265af194aac14076b2
Zach Margolis <zachmargolis@users.noreply.github.com>
2016-11-07 17:28:59 -0500

    Add current working copy for privacy policy (#688)
    **Why**:
    Lorem ipsum is distracting

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ Rails.application.routes.draw do
         as: :destroy_user_session
   match '/api/saml/auth' => 'saml_idp#auth', via: [:get, :post]
 
-  get '/contact' => 'contact#new'
+  get '/contact' => 'contact#new', as: :contact
   post '/contact' => 'contact#create'
 
   get '/edit/email' => 'users/edit_email#edit'
