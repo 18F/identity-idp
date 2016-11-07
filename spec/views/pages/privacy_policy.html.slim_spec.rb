9b8c2c0187774a7aa838604ee2ab41b58d14e8ca
Zach Margolis <zachmargolis@users.noreply.github.com>
2016-11-07 17:28:59 -0500

    Add current working copy for privacy policy (#688)
    **Why**:
    Lorem ipsum is distracting

--- spec/views/pages/privacy_policy.html.slim_spec.rb
+++ spec/views/pages/privacy_policy.html.slim_spec.rb
@@ -7,10 +7,20 @@ describe 'pages/privacy_policy.html.slim' do
     render
   end
 
-  it 'includes a link to create a new account' do
+  it 'links to the system of record notices' do
+    pending 'having the correct link'
+
+    render
+
+    doc = Nokogiri::HTML(rendered)
+    link_title = t('pages.privacy_policy.systems_of_record_notices')
+    expect(doc.xpath("//a[text()='#{link_title}']").first[:href]).to_not eq('#')
+  end
+
+  it 'links to the contact form' do
     render
 
     expect(rendered).
-      to have_link(t('links.create_account'), href: new_user_registration_path)
+      to have_link(t('pages.privacy_policy.contact_form_link'), href: contact_path)
   end
 end
