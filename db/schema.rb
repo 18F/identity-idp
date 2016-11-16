15f81912a6dd3cefe2aeddb5ccf0fb4e5f323b90
Peter Karman <peter.karman@gsa.gov>
2016-11-16 13:35:00 -0600

    Set users.password_salt if it is not set
    **Why**: One-time clean up to new encryption in #650

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114195108) do
+ActiveRecord::Schema.define(version: 20161116193316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
