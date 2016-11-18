41de20e60ad7be72d747780b364ec3b2b3b19579
Peter Karman <pkarman@users.noreply.github.com>
2016-11-18 10:45:22 -0600

    Add User.password_cost and User.recovery_cost (#745)
    **Why**: We will eventually change default SCrypt calibration string
    as computers get faster.
    
    **How**: Rather than relying on hardcoded SCrypt calibration string,
    store it in `*_cost` column and prefer it when building UserAccessKey.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118150203) do
+ActiveRecord::Schema.define(version: 20161118150204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,8 @@ ActiveRecord::Schema.define(version: 20161118150203) do
     t.string   "password_salt"
     t.string   "encryption_key"
     t.string   "recovery_salt"
+    t.string   "password_cost"
+    t.string   "recovery_cost"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
