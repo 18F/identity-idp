b144c445bf3c58c9205bed39c38087ca721886fd
Jessie A. Young <jessieay@users.noreply.github.com>
2016-11-18 08:01:18 -0800

    Remove comments from config files (#741)
    **Why**:
    * This makes the config files easier to read
    * Next step is to alpha order so we know that we don't config something
      twice in a single file
    * Also may find that there is repetitive setup that we can add in
      `config/application.rb` rather than in each environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,74 +1,17 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like
-  # NGINX, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
-  # `config.assets.precompile` and `config.assets.version` have moved to
-  # config/initializers/assets.rb
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # Note: We are not enabling this here since these three policies are enforces by other means:
-  # Strict-Transport-Security: enforced by secure_headers gem
-  # secure cookes: enforced by secure_headers gem
-  # SSL everywhre: enforced by the webserver (nginx) that terminates user SSL connections
-  # config.force_ssl = true
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Keys must be symbolized per ActionMailer documentation
   config.action_mailer.smtp_settings = JSON.parse(Figaro.env.smtp_settings).symbolize_keys
 
-  # ActionMailer Config
   config.action_mailer.default_url_options = {
     host: Figaro.env.domain_name,
     protocol: 'https'
@@ -81,20 +24,11 @@ Rails.application.configure do
   # creates false positive results.
   config.action_dispatch.ip_spoofing_check = false
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
   config.log_level = :info
-
-  # Enable Lograge in production for succinct logging
   config.lograge.enabled = true
   config.lograge.custom_options = ->(event) { event.payload.except(:params) }
   config.lograge.ignore_actions = ['Users::SessionsController#active']
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.logstash.type = :multi_delegator
-  config.logstash.outputs = [
-    {
-      type: :file,
-      path: 'log/production.log'
-    }
-  ]
+  config.logstash.outputs = [{ type: :file, path: 'log/production.log' }]
 end
