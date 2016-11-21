c520edafd4687fc7c18da2b9b9462f472d9987d0
Peter Karman <peter.karman@gsa.gov>
2016-11-21 12:47:11 -0600

    Fix migrations for User email encryption
    **Why**: Devise method conflicts because columns change
    from email -> email_fingerprint.

--- db/migrate/20161118150205_add_user_email_fingerprint.rb
+++ db/migrate/20161118150205_add_user_email_fingerprint.rb
@@ -8,7 +8,6 @@ class AddUserEmailFingerprint < ActiveRecord::Migration
     encrypt_user_emails
     change_column_null :users, :email_fingerprint, false
     change_column_null :users, :encrypted_email, false
-    add_index :users, :email_fingerprint, unique: true
     remove_column :users, :email
   end
 
@@ -24,10 +23,8 @@ class AddUserEmailFingerprint < ActiveRecord::Migration
   def encrypt_user_emails
     User.where(encrypted_email: '').each do |user|
       ee = EncryptedEmail.new_from_email(user.email)
-      user.update!(
-        encrypted_email: ee.encrypted,
-        email_fingerprint: ee.fingerprint
-      )
+      # must use raw SQL here to change data during the migration transaction.
+      execute "UPDATE users SET encrypted_email='#{ee.encrypted}', email_fingerprint='#{ee.fingerprint}' WHERE id=#{user.id}"
     end
   end
 
