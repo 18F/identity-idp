72e80fb62ff6635dc31d47889a250b27a4cc3661
Peter Karman <peter.karman@gsa.gov>
2016-11-22 11:07:48 -0600

    Rename users.email instead of dropping
    **Why**: To help with migrations, rename the email column instead
    of dropping. The actual drop will happen in a future PR.

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20161122031010) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
+    t.string   "email_plain",                   limit: 255, default: ""
     t.string   "encrypted_password",            limit: 255, default: ""
     t.string   "reset_password_token",          limit: 255
     t.datetime "reset_password_sent_at"
