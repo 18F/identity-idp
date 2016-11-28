ddae866ecf3d3ff395b81981f2eac842926ae329
Brendan Sudol <brendansudol@gmail.com>
2016-11-28 16:01:10 -0500

    Add progress bar (LOA3 user flow) (#699)
    * rough first stab at progress step implementation
    
    **Why**: UI we want to add
    
    * Fix rubocop warning
    
    **Why**: Resolve warning
    
    * tweaks, tests

--- spec/views/devise/two_factor_authentication_setup/index.html.slim_spec.rb
+++ spec/views/devise/two_factor_authentication_setup/index.html.slim_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 describe 'devise/two_factor_authentication_setup/index.html.slim' do
+  before do
+    user = build_stubbed(:user, :signed_up)
+    @two_factor_setup_form = TwoFactorSetupForm.new(user)
+  end
+
   it 'sets form autocomplete to off' do
-    @two_factor_setup_form = TwoFactorSetupForm.new(build_stubbed(:user))
     render
 
     expect(rendered).to have_xpath("//form[@autocomplete='off']")
+  end
+
+  it 'displays the correct progress step' do
+    render
+
+    expect(rendered).to have_css('.step-3.active')
   end
 end
