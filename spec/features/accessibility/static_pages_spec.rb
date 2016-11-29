313b6085d9d5fcd63affd6a02761d7754c796bf2
Jessie A. Young <jessieay@users.noreply.github.com>
2016-11-29 15:23:09 -0800

    Add `alt` tag to all login.gov logos (#788)
    **Why**: Accessibility requires it
    * Minor: Also add required `en` attr to HTML element

--- spec/features/accessibility/static_pages_spec.rb
+++ spec/features/accessibility/static_pages_spec.rb
@@ -14,8 +14,32 @@ feature 'Accessibility on static pages', :js do
     expect(page).to be_accessible
   end
 
-  pending 'not found page' do
+  scenario 'not found page' do
     visit '/non_existent_page'
+
+    expect(page).to be_accessible
+  end
+
+  scenario '401 page' do
+    visit '/401'
+
+    expect(page).to be_accessible
+  end
+
+  scenario '422 page' do
+    visit '/422'
+
+    expect(page).to be_accessible
+  end
+
+  scenario '429 page' do
+    visit '/429'
+
+    expect(page).to be_accessible
+  end
+
+  scenario '500 page' do
+    visit '/500'
 
     expect(page).to be_accessible
   end
