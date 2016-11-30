022d97e0506966286f935ce0125d5862422c5269
Adam B <el-mapache@users.noreply.github.com>
2016-11-30 15:51:42 -0500

    Fixes error persistence on email confirmation page (#794)
    **Why**:
    It's confusing to have both success and confirmation message show to the
    user at once
    
    **How**:
    Use `flash.now` instead of `flash` in the confirmations_controller, as
    the former does not persist through a redirect

---
+++ spec/features/users/sign_up_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'Sign Up' do
+  context 'confirmation token error message does not persist on success' do
+    scenario 'with no or invalid token' do
+      visit '/users/confirmation?confirmation_token='
+      expect(page).to have_content t('errors.messages.confirmation_invalid_token')
+
+      sign_up
+
+      expect(page).not_to have_content t('errors.messages.confirmation_invalid_token')
+    end
+  end
+end
