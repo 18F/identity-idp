217bdbb69d6ef5a428cc8a1b62e7e6abe60b134d
Zach Margolis <Zachary_B_Margolis@omb.eop.gov>
2016-12-08 13:40:23 -0800

    Remove second autoLogout() call
    **Why**:
    We were being logged out twice and clearing the flash, so we
    were not able to show the "you've been logged out" banner

--- app/assets/javascripts/app/utils/countdown-timer.js
+++ app/assets/javascripts/app/utils/countdown-timer.js
@@ -1,5 +1,4 @@
 import msFormatter from './ms-formatter';
-import autoLogout from './auto-logout';
 
 export default (targetSelector, timeLeft = 0, interval = 1000) => {
   const countdownTarget = document.querySelector(targetSelector);
@@ -11,7 +10,6 @@ export default (targetSelector, timeLeft = 0, interval = 1000) => {
     countdownTarget.innerHTML = msFormatter(remaining);
 
     if (remaining <= 0) {
-      autoLogout();
       return;
     }
 
