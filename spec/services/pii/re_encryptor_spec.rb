54d56b4185e84ed816e9ebc751390f489bce72f3
Jessie A. Young <jessie.a.young@gmail.com>
2016-12-13 09:57:22 -0800

    Stub more things in unit test

--- spec/services/pii/re_encryptor_spec.rb
+++ spec/services/pii/re_encryptor_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 describe Pii::ReEncryptor do
   describe '#perform' do
     it 're-encrypts PII using new code' do
-      profile = create(:profile, :active, :verified, pii: { ssn: '1234' })
-      user = profile.user
-      generator = RecoveryCodeGenerator.new(user)
-      allow(RecoveryCodeGenerator).to receive(:new).and_return(generator)
+      profile = build_stubbed(:profile, :active, :verified, pii: { ssn: '1234' })
+      user = build_stubbed(:user, profiles: [profile])
+      user_session = {
+        decrypted_pii: { ssn: '1234' }.to_json
+      }
 
-      user.unlock_user_access_key(user.password)
-      user_session = {}
-      cacher = Pii::Cacher.new(user, user_session)
-      cacher.save(user.user_access_key, profile)
-      allow(Pii::Cacher).to receive(:new).and_return(cacher)
+      pii_attributes = Pii::Attributes.new
+      pii_attributes[:ssn] = '1234'
 
+      expect(user.active_profile).to receive(:encrypt_recovery_pii).
+        with(pii_attributes).and_call_original
       expect(user.active_profile).to receive(:save!).and_call_original
 
       Pii::ReEncryptor.new(user, user_session).perform
