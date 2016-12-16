7aba8763642d07889aba83c49401e10b67c6bffc
Moncef Belyamani <monfresh@users.noreply.github.com>
2016-12-16 15:08:28 -0500

    Sign user out when reached max password attempts (#857)
    * Sign user out when reached max password attempts
    
    **Why**:
    If a user is already signed in, and is trying to change their password,
    email, or phone, they will be prompted to enter their current password
    before they can make the changes. If an incorrect password is being
    entered repeatedly, it is most likely due to a malicious person taking
    over the user's machine that they left unattended. In that case, for the
    user's security, we sign the user out.
    
    The max attempts count is configurable, and has been set to 3 for now,
    to match the OTP max attempts.

--- app/controllers/mfa_confirmation_controller.rb
+++ app/controllers/mfa_confirmation_controller.rb
@@ -1,12 +1,15 @@
 class MfaConfirmationController < ApplicationController
   before_action :confirm_two_factor_authenticated
 
+  def new
+    session[:password_attempts] ||= 0
+  end
+
   def create
     if current_user.valid_password?(password)
-      redirect_to user_two_factor_authentication_path(reauthn: true)
+      handle_valid_password
     else
-      flash[:error] = t('errors.confirm_password_incorrect')
-      redirect_to user_password_confirm_path
+      handle_invalid_password
     end
   end
 
@@ -14,5 +17,27 @@ class MfaConfirmationController < ApplicationController
 
   def password
     params.require(:user)[:password]
+  end
+
+  def handle_valid_password
+    redirect_to user_two_factor_authentication_path(reauthn: true)
+    session[:password_attempts] = 0
+  end
+
+  def handle_invalid_password
+    session[:password_attempts] += 1
+
+    if session[:password_attempts] < Figaro.env.password_max_attempts.to_i
+      flash[:error] = t('errors.confirm_password_incorrect')
+      redirect_to user_password_confirm_path
+    else
+      handle_max_password_attempts_reached
+    end
+  end
+
+  def handle_max_password_attempts_reached
+    analytics.track_event(Analytics::PASSWORD_MAX_ATTEMPTS)
+    sign_out
+    redirect_to root_url, alert: t('errors.max_password_attempts_reached')
   end
 end
--- spec/controllers/mfa_confirmation_controller_spec.rb
+++ spec/controllers/mfa_confirmation_controller_spec.rb
@@ -1,46 +1,100 @@
 require 'rails_helper'
 
 describe MfaConfirmationController do
-  describe 'GET /reauthn' do
+  describe '#new' do
     it 'presents the password confirmation form' do
       stub_sign_in
 
       get :new
 
       expect(response.status).to eq 200
+      expect(session[:password_attempts]).to eq 0
+    end
+
+    it 'does not reset password attempts if already set' do
+      stub_sign_in
+      session[:password_attempts] = 1
+
+      get :new
+
+      expect(session[:password_attempts]).to eq 1
     end
   end
 
-  describe 'PUT /reauthn' do
+  describe '#create' do
     let(:user) { build(:user, password: 'password') }
 
     before do
       stub_sign_in(user)
+      session[:password_attempts] = 1
     end
 
     context 'password is empty' do
-      it 'redirects with error message' do
+      it 'redirects with error message and increments password attempts' do
         post :create, user: { password: '' }
 
         expect(response).to redirect_to(user_password_confirm_path)
         expect(flash[:error]).to eq t('errors.confirm_password_incorrect')
+        expect(session[:password_attempts]).to eq 2
       end
     end
 
     context 'password is wrong' do
-      it 'redirects with error message' do
+      it 'redirects with error message and increments password attempts' do
         post :create, user: { password: 'wrong' }
 
         expect(response).to redirect_to(user_password_confirm_path)
         expect(flash[:error]).to eq t('errors.confirm_password_incorrect')
+        expect(session[:password_attempts]).to eq 2
       end
     end
 
     context 'password is correct' do
-      it 'redirects to 2FA' do
+      it 'redirects to 2FA and resets password attempts' do
         post :create, user: { password: 'password' }
 
         expect(response).to redirect_to(user_two_factor_authentication_path(reauthn: true))
+        expect(session[:password_attempts]).to eq 0
+      end
+    end
+  end
+
+  describe 'password attempts counter' do
+    context 'max password attempts reached' do
+      it 'signs the user out' do
+        user = build(:user, :signed_up)
+        sign_in user
+        session[:password_attempts] = 0
+        stub_analytics
+        allow(@analytics).to receive(:track_event)
+
+        max_allowed_attempts = Figaro.env.password_max_attempts.to_i
+        max_allowed_attempts.times do
+          post :create, user: { password: 'wrong' }
+        end
+
+        expect(response).to redirect_to(root_path)
+        expect(controller.current_user).to be_nil
+        expect(flash[:alert]).to eq t('errors.max_password_attempts_reached')
+        expect(@analytics).to have_received(:track_event).
+          with(Analytics::PASSWORD_MAX_ATTEMPTS)
+      end
+    end
+
+    context 'last password attempt is correct' do
+      it 'does not sign the user out' do
+        user = build_stubbed(:user, password: 'password')
+        stub_sign_in user
+        session[:password_attempts] = 0
+
+        max_allowed_attempts = Figaro.env.password_max_attempts.to_i
+        (max_allowed_attempts - 1).times do
+          post :create, user: { password: 'wrong' }
+        end
+
+        post :create, user: { password: 'password' }
+
+        expect(response).to redirect_to user_two_factor_authentication_path(reauthn: true)
       end
     end
   end
