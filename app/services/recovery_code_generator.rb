4a3b75df55bab46ff0376b3c051b7acb53be4684
Peter Karman <pkarman@users.noreply.github.com>
2016-12-22 13:01:01 -0600

    Recovery code is now a random phrase (#888)
    **Why**: Easier to write down.
    
    **How**: Generate random array of words from US Constitution lexicon.

--- app/services/recovery_code_generator.rb
+++ app/services/recovery_code_generator.rb
@@ -1,7 +1,7 @@
 class RecoveryCodeGenerator
   attr_reader :user_access_key
 
-  def initialize(user, length: 16)
+  def initialize(user, length: 5)
     @user = user
     @length = length
     @key_maker = EncryptedKeyMaker.new
@@ -44,7 +44,7 @@ class RecoveryCodeGenerator
   end
 
   def raw_recovery_code
-    @raw_recovery_code ||= SecureRandom.hex(recovery_code_length / 2)
+    @raw_recovery_code ||= RandomPhrase.new(recovery_code_length).to_s
   end
 
   def recovery_code_length
