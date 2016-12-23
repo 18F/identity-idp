457e477cb17a81a5e80b7d323e5de146d6c37a56
Adam B <el-mapache@users.noreply.github.com>
2016-12-23 15:11:24 -0500

    Removes white nav bar unless user fully auth'd (#861)
    **Why**: It's the intended design

--- spec/views/shared/_nav_auth.html.slim_spec.rb
+++ spec/views/shared/_nav_auth.html.slim_spec.rb
@@ -5,7 +5,6 @@ describe 'shared/_nav_auth.html.slim' do
     before do
       @user = build_stubbed(:user, :signed_up)
       allow(view).to receive(:current_user).and_return(@user)
-      allow(view).to receive(:user_fully_authenticated?).and_return(true)
       render
     end
 
@@ -23,31 +22,6 @@ describe 'shared/_nav_auth.html.slim' do
 
     it 'contains sign out link' do
       expect(rendered).to have_link(t('links.sign_out'), href: destroy_user_session_path)
-    end
-  end
-
-  context 'user has entered password but not complete 2fa' do
-    before do
-      @user = build_stubbed(:user, :signed_up)
-      allow(view).to receive(:current_user).and_return(@user)
-      allow(view).to receive(:user_fully_authenticated?).and_return(false)
-      render
-    end
-
-    it 'does not contain link to my account' do
-      expect(rendered).to_not have_link(t('shared.nav_auth.my_account'), href: profile_path)
-    end
-
-    it 'does not contain welcome' do
-      expect(rendered).to_not have_content(t('shared.nav_auth.welcome'))
-    end
-
-    it 'does not contain sign out link' do
-      expect(rendered).to_not have_link(t('links.sign_out'), href: destroy_user_session_path)
-    end
-
-    it 'does contain a link to cancel' do
-      expect(rendered).to have_link(t('links.cancel'), href: destroy_user_session_path)
     end
   end
 end
