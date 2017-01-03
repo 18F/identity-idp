acc66b27221e6b2ead95f1ead18a062410948792
Adam B <el-mapache@users.noreply.github.com>
2017-01-03 12:10:14 -0500

    Clicking site logo during 2fa signs user out (#897)
    **Why**: Currently, clicking the logo during the 2fa process redirects
    the user back to the same page, a behavior that's confusing. 
    
    **How**: If the user has not yet completed 2FA, clicking the logo will
    sign them out and take them to the sign in page. This doesn't change the 
    behavior of clicking the logo once the user has fully authenticated.

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -271,12 +271,41 @@ describe Users::SessionsController, devise: true do
   end
 
   describe '#new' do
-    it 'tracks page visit' do
-      stub_analytics
+    context 'with fully authenticated user' do
+      it 'redirects to the profile page' do
+        stub_sign_in
+        subject.session[:logged_in] = true
+        get :new
 
-      expect(@analytics).to receive(:track_event).with(Analytics::SIGN_IN_PAGE_VISIT)
+        expect(response).to redirect_to profile_path
+        expect(subject.session[:logged_in]).to be true
+      end
+    end
 
-      get :new
+    context 'with current user' do
+      it 'logs the user out' do
+        stub_sign_in_before_2fa
+        subject.session[:logged_in] = true
+        get :new
+
+        expect(request.path).to eq root_path
+        expect(subject.session[:logged_in]).to be_nil
+      end
+    end
+
+    context 'with a new user' do
+      it 'renders the new template' do
+        get :new
+        expect(response).to render_template(:new)
+      end
+
+      it 'tracks page visit' do
+        stub_analytics
+
+        expect(@analytics).to receive(:track_event).with(Analytics::SIGN_IN_PAGE_VISIT)
+
+        get :new
+      end
     end
   end
 end
