1b88f1ac6fe1474a3618d4ccf39bf50534c32d0e
Peter Karman <peter.karman@gsa.gov>
2017-01-24 09:51:13 -0600

    Add /api/service_provider endpoint
    **Why**: Allow HTTP request to trigger re-read from dashboard.

--- spec/services/service_provider_updater_spec.rb
+++ spec/services/service_provider_updater_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe ServiceProviderUpdater do
   include SamlAuthHelper
 
+  let(:fake_dashboard_url) { 'http://dashboard.example.org' }
   let(:dashboard_sp_issuer) { 'some-dashboard-service-provider' }
-  let(:array_of_sps) do
+  let(:dashboard_service_providers) do
     [
       {
         issuer: dashboard_sp_issuer,
@@ -19,23 +20,54 @@ describe ServiceProviderUpdater do
     ]
   end
 
-  before do
-    allow(subject).to receive(:dashboard_service_providers).and_return(array_of_sps)
-  end
-
   describe '#run' do
-    it 'updates global var registry of Service Providers' do
-      expect(SERVICE_PROVIDERS[dashboard_sp_issuer]).to eq nil
-      expect(VALID_SERVICE_PROVIDERS).to_not include dashboard_sp_issuer
+    before do
+      allow(Figaro.env).to receive(:dashboard_url).and_return(fake_dashboard_url)
+    end
 
-      subject.run
+    context 'dashboard is available' do
+      before do
+        stub_request(:get, fake_dashboard_url).to_return(
+          status: 200,
+          body: dashboard_service_providers.to_json
+        )
+        SERVICE_PROVIDERS.delete dashboard_sp_issuer
+        VALID_SERVICE_PROVIDERS.delete dashboard_sp_issuer
+      end
 
-      sp = ServiceProvider.new(dashboard_sp_issuer)
+      after do
+        SERVICE_PROVIDERS.delete dashboard_sp_issuer
+        VALID_SERVICE_PROVIDERS.delete dashboard_sp_issuer
+      end
 
-      expect(sp.metadata[:agency]).to eq array_of_sps.first[:agency]
-      expect(sp.ssl_cert).to be_a OpenSSL::X509::Certificate
-      expect(sp.valid?).to eq true
-      expect(VALID_SERVICE_PROVIDERS).to include dashboard_sp_issuer
+      it 'updates global var registry of Service Providers' do
+        expect(SERVICE_PROVIDERS[dashboard_sp_issuer]).to eq nil
+        expect(VALID_SERVICE_PROVIDERS).to_not include dashboard_sp_issuer
+
+        subject.run
+
+        sp = ServiceProvider.new(dashboard_sp_issuer)
+
+        expect(sp.metadata[:agency]).to eq dashboard_service_providers.first[:agency]
+        expect(sp.ssl_cert).to be_a OpenSSL::X509::Certificate
+        expect(sp.valid?).to eq true
+        expect(VALID_SERVICE_PROVIDERS).to include dashboard_sp_issuer
+      end
+    end
+
+    context 'dashboard is not available' do
+      it 'logs error and does not affect registry' do
+        allow(subject).to receive(:log_error)
+
+        valid_service_providers = VALID_SERVICE_PROVIDERS.dup
+
+        stub_request(:get, fake_dashboard_url).to_return(status: 500)
+
+        subject.run
+
+        expect(subject).to have_received(:log_error)
+        expect(valid_service_providers).to eq VALID_SERVICE_PROVIDERS
+      end
     end
   end
 end
