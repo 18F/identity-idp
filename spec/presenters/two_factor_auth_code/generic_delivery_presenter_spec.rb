ff2eb1be862a430d59a4dd1b46847ddbed4ef48a
Adam B <el-mapache@users.noreply.github.com>
2017-01-28 19:59:31 -0500

    Adds authenticator app tooltip (#904)
    * Adds authenticator app tooltip
    
    **Why**: To ensure users understand what the app is, and how they can
    use it
    
    * Pass optional explicit view context to presenter
    
    **Why**: It is the easiest way to allow the presenter to have access to
    mehtods inside view helpers

--- spec/presenters/two_factor_auth_code/generic_delivery_presenter_spec.rb
+++ spec/presenters/two_factor_auth_code/generic_delivery_presenter_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-def presenter_with(arguments = {})
-  TwoFactorAuthCode::GenericDeliveryPresenter.new(arguments)
+def presenter_with(arguments = {}, view = nil)
+  TwoFactorAuthCode::GenericDeliveryPresenter.new(arguments, view)
 end
 
 describe TwoFactorAuthCode::GenericDeliveryPresenter do
@@ -12,6 +12,14 @@ describe TwoFactorAuthCode::GenericDeliveryPresenter do
       expect do
         presenter.send(m.to_sym)
       end.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe 'view context' do
+    it 'makes the view context available' do
+      view = ActionController::Base.new.view_context
+      presenter = presenter_with({}, view)
+      expect(presenter.view).to equal(view)
     end
   end
 
