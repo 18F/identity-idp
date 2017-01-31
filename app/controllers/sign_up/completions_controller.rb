09b5c8f34b69eaf3bc83108a90fc007bc94e5a20
Jessie A. Young <jessie.a.young@gmail.com>
2017-01-30 17:00:07 -0800

    Track analytics for agency handoff page

--- app/controllers/sign_up/completions_controller.rb
+++ app/controllers/sign_up/completions_controller.rb
@@ -1,6 +1,11 @@
 module SignUp
   class CompletionsController < ApplicationController
-    def show; end
+    def show
+      analytics.track_event(
+        Analytics::USER_REGISTRATION_AGENCY_HANDOFF_PAGE_VISIT,
+        { loa3: session[:sp].try(:loa3), service_provider_name: @sp_name }
+      )
+    end
 
     def update
       redirect_to session[:saml_request_url]
