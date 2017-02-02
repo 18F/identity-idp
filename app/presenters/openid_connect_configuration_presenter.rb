78ebefd6e13d6e34760cc64a2e5569325d13ca10
Zach Margolis <zachary.margolis@gsa.gov>
2017-02-02 10:04:31 -0500

    Move OpenID Connect configuration into a presenter
    **Why**: to simplify the controller

---
+++ app/presenters/openid_connect_configuration_presenter.rb
@@ -0,0 +1,39 @@
+class OpenidConnectConfigurationPresenter
+  include Rails.application.routes.url_helpers
+
+  def configuration
+    {
+      scopes_supported: OpenidConnectAttributeScoper::VALID_SCOPES,
+      response_types_supported: %w(code),
+      grant_types_supported: %w(authorization_code),
+      acr_values_supported: Saml::Idp::Constants::VALID_AUTHN_CONTEXTS,
+      subject_types_supported: %w(pairwise),
+      claims_supported: claims_supported
+    }.merge(url_configuration).merge(crypto_configuration)
+  end
+
+  private
+
+  def url_configuration
+    {
+      issuer: root_url,
+      authorization_endpoint: openid_connect_authorize_url,
+      token_endpoint: openid_connect_token_url,
+      userinfo_endpoint: openid_connect_userinfo_url,
+      service_documentation: '', # TODO
+      jwks_uri: '' # TODO
+    }
+  end
+
+  def crypto_configuration
+    {
+      id_token_signing_alg_values_supported: %w(RS256),
+      token_endpoint_auth_methods_supported: %w(private_key_jwt),
+      token_endpoint_auth_signing_alg_values_supported: %w(RS256)
+    }
+  end
+
+  def claims_supported
+    %w(iss sub) + OpenidConnectAttributeScoper::CLAIMS
+  end
+end
--- spec/controllers/openid_connect/configuration_controller_spec.rb
+++ spec/controllers/openid_connect/configuration_controller_spec.rb
@@ -7,34 +7,7 @@ RSpec.describe OpenidConnect::ConfigurationController do
     it 'renders information about the OpenID Connect integration' do
       get :index
 
-      aggregate_failures do
-        expect(json_response[:issuer]).to eq(root_url)
-        expect(json_response[:authorization_endpoint]).to eq(openid_connect_authorize_url)
-        expect(json_response[:token_endpoint]).to eq(openid_connect_token_url)
-        expect(json_response[:userinfo_endpoint]).to eq(openid_connect_userinfo_url)
-        expect(json_response[:response_types_supported]).to eq(%w(code))
-        expect(json_response[:grant_types_supported]).to eq(%w(authorization_code))
-        expect(json_response[:acr_values_supported]).
-          to match_array(Saml::Idp::Constants::VALID_AUTHN_CONTEXTS)
-        expect(json_response[:subject_types_supported]).to eq(%w(pairwise))
-        expect(json_response[:id_token_signing_alg_values_supported]).to eq(%w(RS256))
-        expect(json_response[:token_endpoint_auth_methods_supported]).to eq(%w(private_key_jwt))
-        expect(json_response[:token_endpoint_auth_signing_alg_values_supported]).to eq(%w(RS256))
-
-        claims = %w(iss sub) + OpenidConnectAttributeScoper::CLAIMS
-        expect(json_response[:claims_supported]).to match_array(claims)
-      end
-    end
-
-    it 'renders all keys' do
-      get :index
-
-      pending 'additional details'
-
-      aggregate_failures do
-        expect(json_response[:jwks_uri]).to be_present
-        expect(json_response[:service_documentation]).to be_present
-      end
+      expect(json_response).to eq(OpenidConnectConfigurationPresenter.new.configuration.as_json)
     end
   end
 end
