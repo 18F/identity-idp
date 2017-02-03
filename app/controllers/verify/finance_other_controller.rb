92daec5f5bc626b898a3cf2bf58bf26ea78a8da5
Peter Karman <peter.karman@gsa.gov>
2017-02-03 11:45:19 -0600

    Convert StepController to IdvStepConcern
    **Why**: Concerns are better than base class controllers.

--- app/controllers/verify/finance_other_controller.rb
+++ app/controllers/verify/finance_other_controller.rb
@@ -1,6 +1,9 @@
 module Verify
-  class FinanceOtherController < StepController
+  class FinanceOtherController < ApplicationController
+    include IdvStepConcern
+
     before_action :confirm_step_needed
+    before_action :confirm_step_allowed
 
     helper_method :idv_finance_form
 
@@ -9,6 +12,10 @@ module Verify
     end
 
     private
+
+    def step_name
+      :financials
+    end
 
     def confirm_step_needed
       redirect_to verify_phone_path if idv_session.financials_confirmation.try(:success?)
