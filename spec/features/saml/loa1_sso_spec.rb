6b203ac461658069144a2b8292d0d688a0fce654
Moncef Belyamani <monfresh@users.noreply.github.com>
2017-02-06 19:01:19 -0500

    Remove JS from loa1_sso_spec (#1052)
    **Why**:
    - It's causing the test to fail for some unknown reason
    - We already have a JS test for the recovery code modal
    - It takes 30+ seconds to run this one file
    
    **How**:
    - Stub as much as possible since we already have another feature
    spec (loa3_sso_spec) that tests a user manually creating an account
    after a SAML request
    
    This speeds up the test by 10x, from 30+ seconds to about 3 seconds.

--- spec/features/saml/loa1_sso_spec.rb
+++ spec/features/saml/loa1_sso_spec.rb
@@ -4,39 +4,34 @@ feature 'LOA1 Single Sign On' do
   include SamlAuthHelper
 
   context 'First time registration' do
-    before do
+    it 'takes user to agency handoff page when sign up flow complete' do
       allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
       @saml_authn_request = auth_request.create(saml_settings)
 
-      visit @saml_authn_request
-      sign_up_and_set_password
-      fill_in 'Phone', with: '202-555-1212'
-      select_sms_delivery
-      enter_2fa_code
-    end
+      user = create(:user, :with_phone)
+      sign_in_and_require_viewing_recovery_code(user)
 
-    scenario 'taken to agency handoff page when sign up flow complete' do
       click_acknowledge_recovery_code
 
       expect(page).to have_content t('titles.loa3_verified.false', app: APP_NAME)
-      click_on I18n.t('forms.buttons.continue_to', sp: @sp_name)
+      click_on I18n.t('forms.buttons.continue_to', sp: 'Your friendly Government Agency')
 
       expect(current_url).to eq @saml_authn_request
     end
+  end
 
-    it 'user is prompted to confirm recovery code before being redirected', js: true do
-      Warden.on_next_request do |proxy|
-        session = proxy.env['rack.session']
-        session[:saml_request_url] = @saml_authn_request
-        session[:sp] = { loa3: false }
-      end
-
-      acknowledge_and_confirm_recovery_code
-
-      expect(page).to have_content t('titles.loa3_verified.false', app: APP_NAME)
-      click_on I18n.t('forms.buttons.continue_to', sp: @sp_name)
-
-      expect(current_url).to eq @saml_authn_request
+  def sign_in_and_require_viewing_recovery_code(user)
+    login_as(user, scope: :user, run_callbacks: false)
+    Warden.on_next_request do |proxy|
+      session = proxy.env['rack.session']
+      session['warden.user.user.session'] = {
+        'need_two_factor_authentication' => true,
+        first_time_recovery_code_view: true
+      }
+      session[:saml_request_url] = @saml_authn_request
+      session[:sp] = { loa3: false, name: 'Your friendly Government Agency' }
     end
+    visit profile_path
+    click_submit_default
   end
 end
