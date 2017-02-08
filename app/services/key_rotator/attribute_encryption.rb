087691c9a543b9a1f3b6a941ea025108dba24f9c
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-02-07 19:06:42 -0500

    Add missing Pii:Cacher spec

--- app/services/key_rotator/attribute_encryption.rb
+++ app/services/key_rotator/attribute_encryption.rb
@@ -1,33 +1,45 @@
 module KeyRotator
   class AttributeEncryption
-    def initialize
+    ATTRIBUTES = [:phone, :otp_secret_key].freeze
+
+    def initialize(user)
+      @user = user
       self.new_cost = Figaro.env.attribute_cost
       self.encryptor = Pii::PasswordEncryptor.new
     end
 
-    def rotate(user)
-      user.update_columns(build_rotated_columns(user))
+    def rotate
+      user.update_columns(encrypted_attributes)
     end
 
     private
 
     attr_accessor :encryptor, :new_cost
+    attr_reader :user
 
     def uak
       @_uak ||= EncryptedAttribute.new_user_access_key(cost: new_cost)
     end
 
-    def build_rotated_columns(user)
+    def encrypted_attributes
+      encrypted_email_attributes.merge!(other_encrypted_attributes)
+    end
+
+    def encrypted_email_attributes
       email = EncryptedAttribute.new_from_decrypted(user.email, uak)
-      to_update = { encrypted_email: email.encrypted, attribute_cost: new_cost }
-      plain_phone = user.phone
-      if plain_phone.present?
-        to_update[:encrypted_phone] = EncryptedAttribute.new_from_decrypted(
-          plain_phone,
+      { encrypted_email: email.encrypted, attribute_cost: new_cost }
+    end
+
+    def other_encrypted_attributes
+      ATTRIBUTES.each_with_object({}) do |attribute, result|
+        plain_attribute = user.public_send(attribute)
+        next unless plain_attribute
+
+        result[:"encrypted_#{attribute}"] = EncryptedAttribute.new_from_decrypted(
+          plain_attribute,
           uak
         ).encrypted
       end
-      to_update
     end
   end
 end
