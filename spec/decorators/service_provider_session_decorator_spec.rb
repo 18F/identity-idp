034af918dbb6363f12ece83430adabbdce7b844b
Jessie A. Young <jessie.a.young@gmail.com>
2017-02-07 16:42:37 -0800

    Backfill test for session decorators
    **Why**: Tests == good

--- spec/decorators/service_provider_session_decorator_spec.rb
+++ spec/decorators/service_provider_session_decorator_spec.rb
@@ -9,6 +9,48 @@ RSpec.describe ServiceProviderSessionDecorator do
     end
   end
 
+  describe '#return_to_service_provider_partial' do
+    it 'returns the correct partial' do
+      expect(decorator.return_to_service_provider_partial).to eq(
+        'devise/sessions/return_to_service_provider'
+      )
+    end
+  end
+
+  describe '#nav_partial' do
+    it 'returns the correct partial' do
+      expect(decorator.nav_partial).to eq 'shared/nav_branded'
+    end
+  end
+
+  describe '#new_session_heading' do
+    it 'returns the correct string' do
+      expect(decorator.new_session_heading).to eq I18n.t('headings.sign_in_with_sp', sp: sp_name)
+    end
+  end
+
+  describe '#registration_heading' do
+    it 'returns the correct string' do
+      expect(decorator.registration_heading).to eq(
+        I18n.t('headings.create_account_with_sp', sp: sp_name)
+      )
+    end
+  end
+
+  describe '#registration_bullet_1' do
+    it 'returns the correct string' do
+      expect(decorator.registration_bullet_1).to eq(
+        I18n.t('devise.registrations.start.bullet_1_with_sp', sp: sp_name)
+      )
+    end
+  end
+
+  describe '#idv_hardfail4_partial' do
+    it 'returns the correct partial' do
+      expect(decorator.idv_hardfail4_partial).to eq 'verify/hardfail4'
+    end
+  end
+
   describe '#logo_partial' do
     context 'logo present' do
       it 'returns branded logo partial' do
@@ -25,5 +67,13 @@ RSpec.describe ServiceProviderSessionDecorator do
         expect(decorator.logo_partial).to eq 'shared/null'
       end
     end
+  end
+
+  def decorator
+    ServiceProviderSessionDecorator.new(sp_name: sp_name, sp_logo: nil)
+  end
+
+  def sp_name
+    'Best SP ever!'
   end
 end
--- spec/decorators/session_decorator_spec.rb
+++ spec/decorators/session_decorator_spec.rb
@@ -8,4 +8,46 @@ RSpec.describe SessionDecorator do
       ).to be(true), "expected #{described_class} to have ##{method}"
     end
   end
+
+  describe '#return_to_service_provider_partial' do
+    it 'returns the correct partial' do
+      expect(decorator.return_to_service_provider_partial).to eq 'shared/null'
+    end
+  end
+
+  describe '#nav_partial' do
+    it 'returns the correct partial' do
+      expect(decorator.nav_partial).to eq 'shared/nav_lite'
+    end
+  end
+
+  describe '#new_session_heading' do
+    it 'returns the correct string' do
+      expect(decorator.new_session_heading).to eq I18n.t('headings.sign_in_without_sp')
+    end
+  end
+
+  describe '#registration_heading' do
+    it 'returns the correct string' do
+      expect(decorator.registration_heading).to eq I18n.t('headings.create_account_without_sp')
+    end
+  end
+
+  describe '#registration_bullet_1' do
+    it 'returns the correct string' do
+      expect(decorator.registration_bullet_1).to eq(
+        I18n.t('devise.registrations.start.bullet_1_without_sp')
+      )
+    end
+  end
+
+  describe '#idv_hardfail4_partial' do
+    it 'returns the correct partial' do
+      expect(decorator.idv_hardfail4_partial).to eq 'shared/null'
+    end
+  end
+
+  def decorator
+    SessionDecorator.new
+  end
 end
