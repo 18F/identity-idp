4b43aaf11044443bd8d617eeecfd7da4ee6b03bb
Jessie A. Young <jessieay@users.noreply.github.com>
2017-02-08 12:00:12 -0800

    Backfill test for session decorators (#1068)
    **Why**: Tests == good

--- spec/decorators/service_provider_session_decorator_spec.rb
+++ spec/decorators/service_provider_session_decorator_spec.rb
@@ -1,11 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe ServiceProviderSessionDecorator do
+  subject { ServiceProviderSessionDecorator.new(sp_name: sp_name, sp_logo: nil) }
+  let(:sp_name) { 'Best SP ever!' }
+
   it 'has the same public API as SessionDecorator' do
     SessionDecorator.public_instance_methods.each do |method|
       expect(
         described_class.public_method_defined?(method)
       ).to be(true), "expected #{described_class} to have ##{method}"
+    end
+  end
+
+  describe '#return_to_service_provider_partial' do
+    it 'returns the correct partial' do
+      expect(subject.return_to_service_provider_partial).to eq(
+        'devise/sessions/return_to_service_provider'
+      )
+    end
+  end
+
+  describe '#nav_partial' do
+    it 'returns the correct partial' do
+      expect(subject.nav_partial).to eq 'shared/nav_branded'
+    end
+  end
+
+  describe '#new_session_heading' do
+    it 'returns the correct string' do
+      expect(subject.new_session_heading).to eq I18n.t('headings.sign_in_with_sp', sp: sp_name)
+    end
+  end
+
+  describe '#registration_heading' do
+    it 'returns the correct string' do
+      expect(subject.registration_heading).to eq(
+        I18n.t('headings.create_account_with_sp', sp: sp_name)
+      )
+    end
+  end
+
+  describe '#registration_bullet_1' do
+    it 'returns the correct string' do
+      expect(subject.registration_bullet_1).to eq(
+        I18n.t('devise.registrations.start.bullet_1_with_sp', sp: sp_name)
+      )
+    end
+  end
+
+  describe '#idv_hardfail4_partial' do
+    it 'returns the correct partial' do
+      expect(subject.idv_hardfail4_partial).to eq 'verify/hardfail4'
     end
   end
 
--- spec/decorators/session_decorator_spec.rb
+++ spec/decorators/session_decorator_spec.rb
@@ -1,11 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe SessionDecorator do
+  subject { SessionDecorator.new }
+
   it 'has the same public API as ServiceProviderSessionDecorator' do
     ServiceProviderSessionDecorator.public_instance_methods.each do |method|
       expect(
         described_class.public_method_defined?(method)
       ).to be(true), "expected #{described_class} to have ##{method}"
+    end
+  end
+
+  describe '#return_to_service_provider_partial' do
+    it 'returns the correct partial' do
+      expect(subject.return_to_service_provider_partial).to eq 'shared/null'
+    end
+  end
+
+  describe '#nav_partial' do
+    it 'returns the correct partial' do
+      expect(subject.nav_partial).to eq 'shared/nav_lite'
+    end
+  end
+
+  describe '#new_session_heading' do
+    it 'returns the correct string' do
+      expect(subject.new_session_heading).to eq I18n.t('headings.sign_in_without_sp')
+    end
+  end
+
+  describe '#registration_heading' do
+    it 'returns the correct string' do
+      expect(subject.registration_heading).to eq I18n.t('headings.create_account_without_sp')
+    end
+  end
+
+  describe '#registration_bullet_1' do
+    it 'returns the correct string' do
+      expect(subject.registration_bullet_1).to eq(
+        I18n.t('devise.registrations.start.bullet_1_without_sp')
+      )
+    end
+  end
+
+  describe '#idv_hardfail4_partial' do
+    it 'returns the correct partial' do
+      expect(subject.idv_hardfail4_partial).to eq 'shared/null'
     end
   end
 end
