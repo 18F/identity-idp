c8b01bb856304f75d51ffdf61e0213cde03070db
Zach Margolis <zachary.margolis@gsa.gov>
2017-02-09 10:44:23 -0500

    Fix CSP to work with OpenID Connect
    **Why**: CSP was blocking redirects to other domains

--- app/controllers/openid_connect/authorization_controller.rb
+++ app/controllers/openid_connect/authorization_controller.rb
@@ -2,9 +2,12 @@ module OpenidConnect
   class AuthorizationController < ApplicationController
     before_action :confirm_two_factor_authenticated
 
-    def index
-      @authorize_form = build_authorize_form
+    before_action :build_authorize_form_from_params, only: [:index]
+    before_action :load_authorize_form_from_session, only: [:create, :destroy]
 
+    before_action :apply_secure_headers_override, only: [:index]
+
+    def index
       return redirect_to verify_url if identity_needs_verification?
 
       success = @authorize_form.valid?
@@ -17,8 +20,6 @@ module OpenidConnect
     end
 
     def create
-      @authorize_form = OpenidConnectAuthorizeForm.new(session_params)
-
       result = @authorize_form.submit(current_user, session.id)
       analytics.track_event(Analytics::OPENID_CONNECT_ALLOW, result.except(:redirect_uri))
 
@@ -30,7 +31,6 @@ module OpenidConnect
     end
 
     def destroy
-      @authorize_form = OpenidConnectAuthorizeForm.new(session_params)
       analytics.track_event(Analytics::OPENID_CONNECT_DECLINE, client_id: @authorize_form.client_id)
 
       render nothing: true # TODO: should we try to redirect back with an error?
@@ -38,18 +38,28 @@ module OpenidConnect
 
     private
 
+    def apply_secure_headers_override
+      override_content_security_policy_directives(
+        form_action: ["'self'", @authorize_form.allowed_form_action].compact
+      )
+    end
+
     def identity_needs_verification?
       @authorize_form.loa3_requested? && decorated_user.identity_not_verified?
     end
 
-    def build_authorize_form
+    def build_authorize_form_from_params
       user_session[:openid_auth_request] = authorization_params
 
-      OpenidConnectAuthorizeForm.new(authorization_params)
+      @authorize_form = OpenidConnectAuthorizeForm.new(authorization_params)
     end
 
     def authorization_params
       params.permit(OpenidConnectAuthorizeForm::ATTRS)
+    end
+
+    def load_authorize_form_from_session
+      @authorize_form = OpenidConnectAuthorizeForm.new(session_params)
     end
 
     def session_params
