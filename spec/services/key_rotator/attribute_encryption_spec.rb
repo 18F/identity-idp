88f426e0d6e71134dd5a441edf4e81c29b728a33
Jessie A. Young <jessieay@users.noreply.github.com>
2017-02-10 09:28:00 -0800

    Add specs for use of `update_columns` (#1086)
    **Why**: We are using `update_columns` because it is faster and does not
    change the `updated_at` timestamp on records. In this case, there is not
    business rule for why we are doing this, but we want to preserve the
    behvaior. So these tests are serving as documentation that using
    `update_columns` was an intentional choice in these classes.

--- spec/services/key_rotator/attribute_encryption_spec.rb
+++ spec/services/key_rotator/attribute_encryption_spec.rb
@@ -9,11 +9,21 @@ describe KeyRotator::AttributeEncryption do
       old_encrypted_phone = user.encrypted_phone
 
       rotate_attribute_encryption_key
-
       rotator.rotate
 
       expect(user.encrypted_email).to_not eq old_encrypted_email
       expect(user.encrypted_phone).to_not eq old_encrypted_phone
+    end
+
+    it 'does not change the `updated_at` timestamp' do
+      user = create(:user)
+      old_updated_timestamp = user.updated_at
+
+      rotate_attribute_encryption_key
+      rotator = described_class.new(user)
+      rotator.rotate
+
+      expect(user.updated_at).to eq old_updated_timestamp
     end
   end
 end
