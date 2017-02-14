70ebeec43553c97ada6ac44f4d85a7417f6c7f61
Peter Karman <peter.karman@gsa.gov>
2017-02-14 15:30:13 -0600

    Add vendor-normalized values to Pii::Attributes
    **Why**: Preserving what vendor tells us for later use.

--- spec/services/idv/profile_from_applicant_spec.rb
+++ spec/services/idv/profile_from_applicant_spec.rb
@@ -4,9 +4,14 @@ describe Idv::ProfileFromApplicant do
   describe '#create' do
     it 'creates Profile with encrypted PII' do
       applicant = Proofer::Applicant.new first_name: 'Some', last_name: 'One'
+      normalized_applicant = Proofer::Applicant.new first_name: 'Somebody', last_name: 'Oneatatime'
       user = create(:user, :signed_up)
       user.unlock_user_access_key(user.password)
-      profile = described_class.create(applicant, user)
+      profile = described_class.create(
+        applicant: applicant,
+        user: user,
+        normalized_applicant: normalized_applicant
+      )
 
       expect(profile.id).to_not be_nil
       expect(profile.encrypted_pii).to_not be_nil
