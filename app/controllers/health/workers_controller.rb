7c88129c99faa2996cf052f391817a1f2dceb594
Moncef Belyamani <monfresh@users.noreply.github.com>
2017-03-01 18:19:47 -0500

    Improve WorkersController test coverage (#1158)
    **Why**: The `worker_health_checker` method was not hit due to the
    stubbing.
    
    **How**:
    - Remove the method since it's not really needed
    - stub `WorkerHealthChecker` directly

--- app/controllers/health/workers_controller.rb
+++ app/controllers/health/workers_controller.rb
@@ -1,17 +1,11 @@
 module Health
   class WorkersController < ApplicationController
     def index
-      summary = worker_health_checker.summary
+      summary = WorkerHealthChecker.summary
 
       status = summary.all_healthy? ? :ok : :internal_error
 
       render json: summary, status: status
-    end
-
-    protected
-
-    def worker_health_checker
-      @_worker_health_checker ||= WorkerHealthChecker
     end
   end
 end
