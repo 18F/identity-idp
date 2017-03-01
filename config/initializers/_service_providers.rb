61d4538a142ed23f8ca23f86df64ebde6be84c11
Moncef Belyamani <monfresh@users.noreply.github.com>
2017-03-01 17:16:00 -0500

    Add missing specs for ServiceProviderUpdater (#1157)
    **Why**: Moar test coverage!

--- config/initializers/_service_providers.rb
+++
@@ -1,8 +0,0 @@
-SERVICE_PROVIDERS = YAML.load_file("#{Rails.root}/config/service_providers.yml").
-                    fetch(Rails.env, {})
-
-# merge from dashboard
-require 'feature_management'
-if FeatureManagement.use_dashboard_service_providers?
-  Thread.new { ServiceProviderUpdater.new.run }
-end
