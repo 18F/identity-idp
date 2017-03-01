2425f62077ce063af0eb1e6a6b0321f379dade60
Moncef Belyamani <monfresh@users.noreply.github.com>
2017-03-01 14:03:50 -0500

    Refactor session timeout modal presenters (#1156)
    **Why**:
    - To improve test coverage
    - To eliminate "raise NotImplementedError"
    
    **How**:
    - Eliminate unnecessary inheritance
    - Test the return value of methods as opposed to whether or not
    the Class defines certain methods.

--- app/presenters/partially_signed_in_modal_presenter.rb
+++ app/presenters/partially_signed_in_modal_presenter.rb
@@ -1,4 +1,11 @@
-class PartiallySignedInModalPresenter < SessionTimeoutWarningModalPresenter
+class PartiallySignedInModalPresenter
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::TranslationHelper
+
+  def initialize(time_left_in_session)
+    @time_left_in_session = time_left_in_session
+  end
+
   def message
     t('notices.timeout_warning.partially_signed_in.message_html',
       time_left_in_session: content_tag(:span, time_left_in_session, id: 'countdown'))
@@ -11,4 +18,8 @@ class PartiallySignedInModalPresenter < SessionTimeoutWarningModalPresenter
   def sign_out
     t('notices.timeout_warning.partially_signed_in.sign_out')
   end
+
+  private
+
+  attr_reader :time_left_in_session
 end
