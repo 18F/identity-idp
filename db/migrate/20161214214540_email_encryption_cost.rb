ae9ba4d4de25328b93066fdea9409d8310f14de0
amos <amos@amosarts.com>
2017-03-03 14:00:54 -0800

    Squashes migrations prior to 2017
    __Why__
    The flux of data encryption has resulted in migrations which
    manipulate data far more than we'd like
    
    __How__
    Squashes migrations prior to 2017 so that freshly provisioned databases
    are migrated with minimal data manipulation. Had these migrations not
    been squashed, the damage to user data or saved hours of 18F personnel
    could have been minimized.

--- db/migrate/20161214214540_email_encryption_cost.rb
+++
@@ -1,9 +0,0 @@
-class EmailEncryptionCost < ActiveRecord::Migration
-  def up
-    add_column :users, :email_encryption_cost, :string
-  end
-
-  def down
-    remove_column :users, :email_encryption_cost
-  end
-end
