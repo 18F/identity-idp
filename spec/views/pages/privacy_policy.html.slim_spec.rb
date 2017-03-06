eb3706d6a433871ab8c67c9c696a8f9421b22013
Zach Margolis <zachary.margolis@gsa.gov>
2017-03-06 17:50:45 -0500

    Make sure external links open in new windows
    **Why**: For a consistent experience

--- spec/views/pages/privacy_policy.html.slim_spec.rb
+++ spec/views/pages/privacy_policy.html.slim_spec.rb
@@ -7,7 +7,9 @@ describe 'pages/privacy_policy.html.slim' do
     render
   end
 
-  it 'links to the system of record notices' do
+  let(:doc) { Nokogiri::HTML(rendered) }
+
+  it 'links to the system of record notices in a new window' do
     sorn_url = <<-EOS.gsub(/\s/, '')
       https://www.federalregister.gov/documents/2017/01/19/2017-01174
       /privacy-act-of-1974-notice-of-a-new-system-of-records
@@ -15,8 +17,21 @@ describe 'pages/privacy_policy.html.slim' do
 
     render
 
-    expect(rendered).
-      to have_link(t('pages.privacy_policy.systems_of_record_notices'), href: sorn_url)
+    link = doc.css("a[href='#{sorn_url}']").first
+    expect(link).to be_present
+    expect(link.text).to eq(t('pages.privacy_policy.systems_of_record_notices'))
+    expect(link[:target]).to eq('_blank')
+  end
+
+  it 'links to the e-Government act in a new window' do
+    egov_url = 'https://www.gpo.gov/fdsys/pkg/PLAW-107publ347/html/PLAW-107publ347.htm'
+
+    render
+
+    link = doc.css("a[href='#{egov_url}']").first
+    expect(link).to be_present
+    expect(link.text).to eq(t('pages.privacy_policy.e_government_link'))
+    expect(link[:target]).to eq('_blank')
   end
 
   it 'links to the contact form' do
