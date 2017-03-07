da35ed94be8647c342cb20597bee505c075813b4
Peter Karman <peter.karman@gsa.gov>
2017-03-07 11:24:11 -0600

    Add usps_confirmations table
    **Why**: USPS confirmation requires separate storage than
    Profile since the related PII must be symmetrically encrypted
    w/o the user's password.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222182714) do
+ActiveRecord::Schema.define(version: 20170306214524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,12 @@ ActiveRecord::Schema.define(version: 20170222182714) do
   add_index "users", ["unconfirmed_email"], name: "index_users_on_unconfirmed_email", using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", using: :btree
   add_index "users", ["uuid"], name: "index_users_on_uuid", unique: true, using: :btree
+
+  create_table "usps_confirmations", force: :cascade do |t|
+    t.text     "entry",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "events", "users"
 end
