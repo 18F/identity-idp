3ea9b7ac69b3a4438b40b2d4b7922b72bd0e74fa
Moncef Belyamani <monfresh@users.noreply.github.com>
2017-03-10 15:45:58 -0500

    Return FormResponse in TotpVerificationForm (#1209)
    * Return FormResponse in TotpVerificationForm
    
    **Why**: For consistency with other Form Objects

--- app/controllers/two_factor_authentication/totp_verification_controller.rb
+++ app/controllers/two_factor_authentication/totp_verification_controller.rb
@@ -11,9 +11,9 @@ module TwoFactorAuthentication
     def create
       result = TotpVerificationForm.new(current_user, params[:code].strip).submit
 
-      analytics.track_event(Analytics::MULTI_FACTOR_AUTH, result.merge(method: 'totp'))
+      analytics.track_event(Analytics::MULTI_FACTOR_AUTH, result.to_h)
 
-      if result[:success]
+      if result.success?
         handle_valid_otp
       else
         handle_invalid_otp
