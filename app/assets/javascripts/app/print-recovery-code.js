46713451e7395eb22f24b1a32e5789e167b3e47c
Adam Biagianti <adam.biagianti@gsa.gov>
2017-03-17 15:59:54 -0400

    Remove mobile user agent check and show print btn
    **Why**: Users who want to print from a phone will probably have a way
    set up to do so

--- app/assets/javascripts/app/print-recovery-code.js
+++ app/assets/javascripts/app/print-recovery-code.js
@@ -1,5 +1,3 @@
-import isMobileUserAgent from './utils/mobile-user-agent';
-
 const openSystemPrintDialog = (event) => {
   event.preventDefault();
   window.print();
@@ -9,13 +7,6 @@ const enableRecoveryCodePrintButton = () => {
   const buttonNodes = document.querySelectorAll('[data-print]');
   const buttons = [].slice.call(buttonNodes);
   const { userAgent } = navigator;
-
-  if (isMobileUserAgent(userAgent)) {
-    buttons.forEach(button => {
-      /* eslint-disable no-param-reassign */
-      button.style.display = 'none';
-    });
-  }
 
   buttons.forEach((button) => {
     button.addEventListener('click', openSystemPrintDialog);
