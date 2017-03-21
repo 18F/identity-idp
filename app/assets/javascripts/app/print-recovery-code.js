1f94f2ba488a72b89f10be779f3fd7557512671b
Adam B <el-mapache@users.noreply.github.com>
2017-03-21 09:43:53 -0400

    Remove mobile user agent check and show print btn (#1244)
    * Remove mobile user agent check and show print btn
    
    **Why**: Users who want to print from a phone will probably have a way
    set up to do so
    
    * Remove UA sniffing JS and spec
    
    **Why**: Not used anywhere in the code base
    
    * Removes unused variable
    
    **Why**: Codeclimate doesn't like it, and it might be confusing to other
    devs reading the code

--- app/assets/javascripts/app/print-recovery-code.js
+++ app/assets/javascripts/app/print-recovery-code.js
@@ -1,5 +1,3 @@
-import isMobileUserAgent from './utils/mobile-user-agent';
-
 const openSystemPrintDialog = (event) => {
   event.preventDefault();
   window.print();
@@ -8,14 +6,6 @@ const openSystemPrintDialog = (event) => {
 const enableRecoveryCodePrintButton = () => {
   const buttonNodes = document.querySelectorAll('[data-print]');
   const buttons = [].slice.call(buttonNodes);
-  const { userAgent } = navigator;
-
-  if (isMobileUserAgent(userAgent)) {
-    buttons.forEach(button => {
-      /* eslint-disable no-param-reassign */
-      button.style.display = 'none';
-    });
-  }
 
   buttons.forEach((button) => {
     button.addEventListener('click', openSystemPrintDialog);
