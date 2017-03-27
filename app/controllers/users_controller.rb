31281cd3f37665e7d862f05553e3cbc909a7b27f
Adam Biagianti <adam.biagianti@gsa.gov>
2017-03-27 12:38:45 -0400

    Call sign_out without supplying user as parameter
    **Why**: Its the devise way, and its easier than manually deleting keys

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def destroy_user
     user = current_user || User.find_by(confirmation_token: session[:user_confirmation_token])
-    session.delete(:user_confirmation_token)
     user && user.destroy!
+    sign_out if user
   end
 end
