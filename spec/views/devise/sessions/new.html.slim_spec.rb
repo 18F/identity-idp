67a952c4ee5d20801357f697257b70831f945dd7
Jessie A. Young <jessieay@users.noreply.github.com>
2017-04-04 13:58:50 -0700

    Small copy update for links to policy page (#1312)
    **WHY**: Per our privacy lawyer, we need to both change the link text
    AND remove the "By continuing, you agree to/By signing in, you agree to"
    language.

--- spec/views/devise/sessions/new.html.slim_spec.rb
+++ spec/views/devise/sessions/new.html.slim_spec.rb
@@ -34,7 +34,7 @@ describe 'devise/sessions/new.html.slim' do
     render
 
     expect(rendered).
-      to have_link(t('notices.sign_in_consent.link'), href: MarketingSite.privacy_url)
+      to have_link(t('notices.terms_of_service.link'), href: MarketingSite.privacy_url)
 
     expect(rendered).to have_selector("a[href='#{MarketingSite.privacy_url}'][target='_blank']")
   end
