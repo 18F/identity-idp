d3b303342e563c12c0c6ab6f223d8d89ae752c16
Zach Margolis <zachary.margolis@gsa.gov>
2017-04-05 12:20:03 -0400

    Update marketing site URL
    **Why**: We're making www. the canonical domain for marketing

--- spec/services/marketing_site_spec.rb
+++ spec/services/marketing_site_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe MarketingSite do
   describe '.base_url' do
     it 'points to the base URL' do
-      expect(MarketingSite.base_url).to eq('https://login.gov')
+      expect(MarketingSite.base_url).to eq('https://www.login.gov')
     end
   end
 
   describe '.privacy_url' do
     it 'points to the privacy page' do
-      expect(MarketingSite.privacy_url).to eq('https://login.gov/policy')
+      expect(MarketingSite.privacy_url).to eq('https://www.login.gov/policy')
     end
   end
 
   describe '.contact_url' do
     it 'points to the contact page' do
-      expect(MarketingSite.contact_url).to eq('https://login.gov/contact')
+      expect(MarketingSite.contact_url).to eq('https://www.login.gov/contact')
     end
   end
 
   describe '.help_url' do
     it 'points to the help page' do
-      expect(MarketingSite.help_url).to eq('https://login.gov/help')
+      expect(MarketingSite.help_url).to eq('https://www.login.gov/help')
     end
   end
 end
