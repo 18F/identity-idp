140cf7e4e9272aa5989f43a77cb5da9e99dcee23
Adam Biagianti <adam.biagianti@gsa.gov>
2017-04-05 13:55:15 -0400

    Replace view logic w/ view_model partial methods
    **Why**: Reduces the size of the main view file, moves logic out of view
    and into code

--- app/view_models/user_profile/profile_index.rb
+++ app/view_models/user_profile/profile_index.rb
@@ -7,5 +7,29 @@ module UserProfile
       @recovery_code = recovery_code
       @has_password_reset_profile = has_password_reset_profile
     end
+
+    def reactivation_instructions_partial
+      if has_password_reset_profile
+        'profile/reactivation_instructions'
+      else
+        'shared/null'
+      end
+    end
+
+    def pii_partial
+      if decrypted_pii
+        'profile/verified_profile_information'
+      else
+        'shared/null'
+      end
+    end
+
+    def recovery_code_warning_partial
+      if recovery_code
+        'profile/recovery_code_warning'
+      else
+        'shared/null'
+      end
+    end
   end
 end
