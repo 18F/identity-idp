02cefbfc5dbd7250915acc32a8655d1d41503af7
Adam Biagianti <adam.biagianti@gsa.gov>
2017-04-05 16:00:28 -0400

    Add verified profile information
    **Why**: Use new styles to display profile information

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,13 +3,12 @@ class ProfileController < ApplicationController
   layout 'card_wide'
 
   def index
-    p 'it me!'
     cacher = Pii::Cacher.new(current_user, user_session)
 
     @view_model = UserProfile::ProfileIndex.new(
       decrypted_pii: cacher.fetch,
       recovery_code: flash[:recovery_code],
-      has_password_reset_profile: current_user.password_reset_profile.present?
+      current_user: current_user
     )
 
     flash.delete(:recovery_code)
--- app/view_models/user_profile/profile_index.rb
+++ app/view_models/user_profile/profile_index.rb
@@ -2,10 +2,10 @@ module UserProfile
   class ProfileIndex
     attr_reader :decrypted_pii, :recovery_code, :has_password_reset_profile
 
-    def initialize(decrypted_pii:, recovery_code:, has_password_reset_profile:)
+    def initialize(decrypted_pii:, recovery_code:, current_user:)
       @decrypted_pii = decrypted_pii
       @recovery_code = recovery_code
-      @has_password_reset_profile = has_password_reset_profile
+      @has_password_reset_profile = current_user.password_reset_profile.present?
     end
 
     def reactivation_instructions_partial
