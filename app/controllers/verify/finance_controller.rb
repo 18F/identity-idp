e38d845869bf8520d00d42d75b2c2ac0c5421db3
Jessie A. Young <jessieay@users.noreply.github.com>
2017-04-07 11:43:39 -0700

    Use methods on views models instead of `helper_method` (#1338)
    **WHY**: Keeps logic in one place.

--- app/controllers/verify/finance_controller.rb
+++ app/controllers/verify/finance_controller.rb
@@ -6,10 +6,8 @@ module Verify
     before_action :confirm_step_needed
     before_action :confirm_step_allowed
 
-    helper_method :idv_finance_form
-
     def new
-      @view_model = Verify::FinancialsNew.new(remaining_attempts: remaining_step_attempts)
+      @view_model = view_model
       analytics.track_event(Analytics::IDV_FINANCE_CCN_VISIT)
     end
 
@@ -28,13 +26,29 @@ module Verify
 
     private
 
+    def step_name
+      :financials
+    end
+
+    def confirm_step_needed
+      redirect_to verify_address_path if idv_session.financials_confirmation == true
+    end
+
+    def view_model(error: nil)
+      Verify::FinancialsNew.new(
+        error: error,
+        remaining_attempts: remaining_step_attempts,
+        idv_finance_form: idv_finance_form
+      )
+    end
+
+    def idv_finance_form
+      @_idv_finance_form ||= Idv::FinanceForm.new(idv_session.params)
+    end
+
     def handle_success
       flash[:success] = t('idv.messages.personal_details_verified')
       redirect_to verify_address_url
-    end
-
-    def step_name
-      :financials
     end
 
     def step
@@ -45,16 +59,8 @@ module Verify
       )
     end
 
-    def view_model(error: nil)
-      Verify::FinancialsNew.new(error: error, remaining_attempts: remaining_step_attempts)
-    end
-
     def step_params
       params.require(:idv_finance_form).permit(:finance_type, *Idv::FinanceForm::FINANCE_TYPES)
-    end
-
-    def confirm_step_needed
-      redirect_to verify_address_path if idv_session.financials_confirmation == true
     end
 
     def render_form
@@ -63,10 +69,6 @@ module Verify
       else
         render 'verify/finance_other/new'
       end
-    end
-
-    def idv_finance_form
-      @_idv_finance_form ||= Idv::FinanceForm.new(idv_session.params)
     end
   end
 end
