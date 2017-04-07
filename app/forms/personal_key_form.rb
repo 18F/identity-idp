768c8250fa86931899a9124139e86e686dab6884
Brian Hurst <brian.hurst@gsa.gov>
2017-04-07 14:14:23 -0400

    Rubocop fix

--- app/forms/personal_key_form.rb
+++ app/forms/personal_key_form.rb
@@ -21,13 +21,23 @@ class PersonalKeyForm
   attr_reader :user, :success
 
   def valid_personal_key?
+    return false unless code =~ personal_key_regexp
+    personal_key_generator = PersonalKeyGenerator.new(user)
+    personal_key_generator.verify(code)
+  end
+
+  def personal_key_regexp
     char_count = RandomPhrase::WORD_LENGTH
     word_count = Figaro.env.recovery_code_length.to_i
     valid_char = '[a-zA-Z0-9]'
-    return false unless code =~
-      /\A(?:#{valid_char}{#{char_count}}([\s-])?){#{word_count - 1}}#{valid_char}{#{char_count}}\Z/
-    personal_key_generator = PersonalKeyGenerator.new(user)
-    personal_key_generator.verify(code)
+    regexp = /
+      \A
+      (?:#{valid_char}{#{char_count}}([\s-])?){#{word_count - 1}}
+      #{valid_char}{#{char_count}}
+      \Z
+    /x
+
+    regexp
   end
 
   def extra_analytics_attributes
