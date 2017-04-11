ed6210b99c8317c7c924fd1f191a537461776975
Jessie A. Young <jessie.a.young@gmail.com>
2017-04-11 15:09:34 -0700

    Call `render` before OTP partials
    **WHY**: Otherwise, was rendering string value instead of view partial.

--- spec/views/two_factor_authentication/totp_verification/show.html.slim_spec.rb
+++ spec/views/two_factor_authentication/totp_verification/show.html.slim_spec.rb
@@ -65,5 +65,11 @@ describe 'two_factor_authentication/totp_verification/show.html.slim' do
         href: profile_path
       )
     end
+
+    it 'renders the reauthn partial' do
+      expect(view).to render_template(
+        partial: 'two_factor_authentication/totp_verification/_reauthn'
+      )
+    end
   end
 end
