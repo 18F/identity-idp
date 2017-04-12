89efa592bc8357812eb04ab3f32e108355047e4b
Jessie A. Young <jessieay@users.noreply.github.com>
2017-04-12 13:24:44 -0700

    Return timestamp in Profile events list (#1360)
    **WHY**: Users should be able to see the time AND date for events in their account history.

--- spec/decorators/event_decorator_spec.rb
+++ spec/decorators/event_decorator_spec.rb
@@ -9,13 +9,4 @@ describe EventDecorator do
       expect(decorator.pretty_event_type).to eq t('event_types.email_changed')
     end
   end
-
-  describe '#happened_at' do
-    it 'returns created_at' do
-      event = build_stubbed(:event, event_type: :email_changed)
-      decorator = EventDecorator.new(event)
-
-      expect(decorator.happened_at).to eq event.created_at
-    end
-  end
 end
