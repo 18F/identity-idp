e28bf207d5559d97efdbec18567f6c84a2c6dfa1
Jessie A. Young <jessie.a.young@gmail.com>
2017-04-12 10:29:18 -0700

    Put flash keys into a constant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include UserSessionContext
 
+  FLASH_KEYS = ['alert', 'error', 'notice', 'success', 'warning'].freeze
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
