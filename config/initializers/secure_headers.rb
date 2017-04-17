278a36358c5e82ad0aeb58f5f82be75d39f15bf1
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-04-17 11:32:07 -0400

    Set SameSite back to Strict
    **Why**: We fixed the bug that caused an issue in Chrome where visiting
    the IdP via an OIDC SP (while SameSite=Strict) bypassed the landing
    page. See https://github.com/18F/identity-idp/pull/1374 for the fix.
    We verified that with this fix, the landing page appears when
    SameSite=Strict.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -45,7 +45,7 @@ SecureHeaders::Configuration.default do |config|
     secure: true, # mark all cookies as "Secure"
     httponly: true, # mark all cookies as "HttpOnly"
     samesite: {
-      lax: true # mark all cookies as SameSite=Strict.
+      strict: true # mark all cookies as SameSite=Strict.
     },
   }
 
