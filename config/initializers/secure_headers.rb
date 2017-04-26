51c570c3f76465d0db4f641a858e265d90edaeef
Zach Margolis <zachmargolis@users.noreply.github.com>
2017-04-26 15:52:15 -0700

    Merge pull request #1403 from 18F/margolis-1915-lax-cookie
    Set SameSite=Lax to fix OIDC CSP issue

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -45,7 +45,7 @@ SecureHeaders::Configuration.default do |config|
     secure: true, # mark all cookies as "Secure"
     httponly: true, # mark all cookies as "HttpOnly"
     samesite: {
-      strict: true # mark all cookies as SameSite=Strict.
+      lax: true # SameSite setting.
     },
   }
 
