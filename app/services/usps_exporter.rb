b4ccd23296d9ee51eb24225e73e3be4c697d4be4
Zach Margolis <zachary.margolis@gsa.gov>
2017-05-01 14:45:05 -0700

    Update PSV generation to match spec
    **Why**:
    Vendor asked for a new field and a header row.
    The field indidates the type of row, "01" = header, "02" = content
    The header row contains the number of content rows.

--- app/services/usps_exporter.rb
+++ app/services/usps_exporter.rb
@@ -1,22 +1,26 @@
 class UspsExporter
-  OTP_MAX_VALID_DAYS = 30
+  OTP_MAX_VALID_DAYS = Figaro.env.usps_confirmation_max_days.to_i
+  HEADER_ROW_ID = '01'.freeze
+  CONTENT_ROW_ID = '02'.freeze
 
-  def initialize(csv_file_path)
-    @csv_file_path = csv_file_path
+  def initialize(psv_file_path)
+    @psv_file_path = psv_file_path
   end
 
   def run
-    CSV.open(csv_file_path, 'wb', col_sep: '|') do |csv|
-      make_csv(csv)
+    psv_buffer = CSV.generate(col_sep: '|', row_sep: "\r\n") do |csv|
+      make_psv(csv)
     end
+    File.open(psv_file_path, 'wb') { |file| file.write psv_buffer }
     clear_entries
   end
 
   private
 
-  attr_reader :csv_file_path
+  attr_reader :psv_file_path
 
-  def make_csv(csv)
+  def make_psv(csv)
+    csv << make_header_row(entries.size)
     entries.map(&:decrypted_entry).each do |entry|
       csv << make_entry_row(entry)
     end
@@ -30,11 +34,16 @@ class UspsExporter
     UspsConfirmation.where(id: entries.map(&:id)).destroy_all
   end
 
+  def make_header_row(num_entries)
+    [HEADER_ROW_ID, num_entries]
+  end
+
   # rubocop:disable MethodLength, AbcSize
   def make_entry_row(entry)
     now = Time.zone.now
     due = now + OTP_MAX_VALID_DAYS.days
     [
+      CONTENT_ROW_ID,
       "#{entry.first_name} #{entry.last_name}",
       entry.address1,
       entry.address2,
@@ -42,8 +51,8 @@ class UspsExporter
       entry.state,
       entry.zipcode,
       entry.otp,
-      "#{now.strftime('%B %e')},#{now.year}",
-      "#{due.strftime('%B %e')},#{due.year}",
+      "#{now.strftime('%-B %-e')}, #{now.year}",
+      "#{due.strftime('%-B %-e')}, #{due.year}",
     ]
   end
   # rubocop:enable MethodLength, AbcSize
--- spec/services/usps_exporter_spec.rb
+++ spec/services/usps_exporter_spec.rb
@@ -7,18 +7,20 @@ describe UspsExporter do
       first_name: 'Some',
       last_name: 'One',
       address1: '123 Any St',
+      address2: 'Ste 123',
       city: 'Somewhere',
       state: 'KS',
       zipcode: '66666-1234',
       otp: 123
     )
   end
-  let(:psv_file_contents) do
+  let(:psv_row_contents) do
     now = Time.zone.now
     due = now + UspsExporter::OTP_MAX_VALID_DAYS.days
-    current_date = now.strftime('%B %e')
-    due_date = due.strftime('%B %e')
+    current_date = now.strftime('%-B %-e')
+    due_date = due.strftime('%-B %-e')
     values = [
+      UspsExporter::CONTENT_ROW_ID,
       usps_entry.first_name + ' ' + usps_entry.last_name,
       usps_entry.address1,
       usps_entry.address2,
@@ -26,25 +28,30 @@ describe UspsExporter do
       usps_entry.state,
       usps_entry.zipcode,
       usps_entry.otp,
-      "#{current_date},#{now.year}",
-      "#{due_date},#{due.year}",
+      "#{current_date}, #{now.year}",
+      "#{due_date}, #{due.year}",
     ]
     values.join('|')
   end
 
-  subject { described_class.new(export_file) }
+  subject { described_class.new(export_file.path) }
+
+  after do
+    export_file.close
+    export_file.unlink
+  end
 
   describe '#run' do
     before do
       UspsConfirmation.create(entry: usps_entry.encrypted)
     end
 
-    it 'creates file' do
+    it 'creates the file' do
       subject.run
 
-      psv_contents = File.read(export_file)
+      psv_contents = export_file.read
 
-      expect(psv_contents).to eq("#{psv_file_contents}\n")
+      expect(psv_contents).to eq("01|1\r\n#{psv_row_contents}\r\n")
     end
 
     it 'clears entries after creating file' do
