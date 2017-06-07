8c3501b5d0a478cda423673cc8effc136f3064e0
Zach Margolis <zachary.margolis@gsa.gov>
2017-06-07 17:00:32 -0400

    Add scripts to help translate markdown files
    **Why**: Useful for the static site

--- spec/lib/i18n_converter_spec.rb
+++ spec/lib/i18n_converter_spec.rb
@@ -31,6 +31,30 @@ RSpec.describe I18nConverter do
     XML
   end
 
+  let(:markdown_md) do
+    <<~MD
+      ---
+      title: Title
+      description: Description
+      ---
+      Some **fancy** markdown
+    MD
+  end
+
+  let(:markdown_xml) do
+    <<~XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <hash>
+        <front-matter>
+          <title>Title</title>
+          <description>Description</description>
+        </front-matter>
+        <content>Some **fancy** markdown
+      </content>
+      </hash>
+    XML
+  end
+
   subject(:converter) { I18nConverter.new(stdin: stdin, stdout: stdout) }
 
   describe '.yml_to_xml' do
@@ -76,6 +100,52 @@ RSpec.describe I18nConverter do
       converter.xml_to_yml
 
       expect(stdout.string).to eq(translation_yml)
+    end
+  end
+
+  describe '.md_to_xml' do
+    let(:stdout) { StringIO.new }
+    let(:stdin) { StringIO.new(markdown_md) }
+
+    context 'with a TTY on STDIN' do
+      let(:stdin) { instance_double('IO', tty?: true) }
+
+      it 'prints an error and exits' do
+        expect(converter).to receive(:exit).with(1)
+
+        converter.md_to_xml
+
+        expect(stdout.string.chomp).to eq("Usage: cat en.md | #{$PROGRAM_NAME} > output.xml")
+      end
+    end
+
+    it 'outputs XML' do
+      converter.md_to_xml
+
+      expect(stdout.string).to eq(markdown_xml)
+    end
+  end
+
+  describe '.xml_to_md' do
+    let(:stdout) { StringIO.new }
+    let(:stdin) { StringIO.new(markdown_xml) }
+
+    context 'with a TTY on STDIN' do
+      let(:stdin) { instance_double('IO', tty?: true) }
+
+      it 'prints an error and exits' do
+        expect(converter).to receive(:exit).with(1)
+
+        converter.xml_to_md
+
+        expect(stdout.string.chomp).to eq("Usage: cat en.xml | #{$PROGRAM_NAME} > output.md")
+      end
+    end
+
+    it 'outputs XML' do
+      converter.xml_to_md
+
+      expect(stdout.string).to eq(markdown_md)
     end
   end
 end
