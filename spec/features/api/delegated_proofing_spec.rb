f46df5a64f2ce8f8a7c0f700028b3047603c8ef6
Zach Margolis <zachary.margolis@gsa.gov>
2017-06-14 13:49:41 -0400

    wip

---
+++ spec/features/api/delegated_proofing_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Delegated Proofing' do
+  it 'allows accounts to be verified by a participating agency out-of-band' do
+    # LOA3 auth request
+
+    # sign up
+
+    # payload -> agency
+
+    # agency -> API
+
+    # user logs in, sees LOA3 verified
+  end
+end
