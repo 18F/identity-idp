913ac58c3ecb5c8b31caaa4d97f3976804cd350d
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-08-15 13:23:10 -0500

    Add validation error when mail OTP is expired
    **Why**: So that users can't use an expired mail OTP to verify an
    identity.

--- spec/support/idv_examples/usps_verification.rb
+++ spec/support/idv_examples/usps_verification.rb
@@ -1,21 +1,17 @@
 shared_examples 'signing in with pending USPS verification' do |sp|
-  it 'prompts for confirmation code at sign in' do
-    otp = 'abc123'
-    profile = create(
+  let(:otp) { 'abc123' }
+  let(:profile) do
+    create(
       :profile,
       deactivation_reason: :verification_pending,
       phone_confirmed: false,
       pii: { otp: otp, ssn: '123-45-6789', dob: '1970-01-01' }
     )
-    user = profile.user
+  end
+  let(:user) { profile.user }
 
-    visit_idp_from_sp_with_loa3(sp)
-
-    if %i[saml oidc].include?(sp)
-      sign_in_via_branded_page(user)
-    else
-      sign_in_live_with_2fa(user)
-    end
+  it 'prompts for confirmation code at sign in' do
+    sign_in_from_sp(sp)
 
     expect(current_path).to eq verify_account_path
     expect(page).to have_content t('idv.messages.usps.resend')
@@ -40,6 +36,33 @@ shared_examples 'signing in with pending USPS verification' do |sp|
       end
     else
       expect(current_path).to eq account_path
+    end
+  end
+
+  it 'renders an error for an expired USPS OTP' do
+    sign_in_from_sp(sp)
+
+    Event.create(event_type: :usps_mail_sent, user: user, updated_at: 11.days.ago)
+
+    fill_in t('forms.verify_profile.name'), with: otp
+    click_button t('forms.verify_profile.submit')
+
+    expect(current_path).to eq verify_account_path
+    expect(page).to have_content t('errors.messages.usps_otp_expired')
+
+    user.reload
+
+    expect(user.events.account_verified.size).to eq 0
+    expect(user.active_profile).to be_nil
+  end
+
+  def sign_in_from_sp(sp)
+    visit_idp_from_sp_with_loa3(sp)
+
+    if %i[saml oidc].include?(sp)
+      sign_in_via_branded_page(user)
+    else
+      sign_in_live_with_2fa(user)
     end
   end
 end
