1ad3e955849f58b692c11d878a202e8ebe2d22cc
Brian Hurst <brian.hurst@gsa.gov>
2017-08-23 14:49:23 -0400

    Translate HTML5 required input message

--- app/assets/javascripts/app/form-validation.js
+++ app/assets/javascripts/app/form-validation.js
@@ -1,19 +1,35 @@
+import 'classlist.js';
+
 const I18n = window.LoginGov.I18n;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  if (form) {
-    const fields = ['dob', 'personal-key', 'ssn', 'zipcode'];
+  const forms = document.querySelectorAll('form');
 
-    fields.forEach(function(f) {
-      const input = document.querySelector(`.${f}`);
-      if (input) {
-        input.addEventListener('input', () => {
-          if (input.validity.patternMismatch) {
-            input.setCustomValidity(I18n.t(`idv.errors.pattern_mismatch.${I18n.key(f)}`));
-          } else {
-            input.setCustomValidity('');
-          }
+  function addListenerMulti(el, events, fn) {
+    events.split(' ').forEach(e => el.addEventListener(e, fn, false));
+  }
+
+  if (forms.length !== 0) {
+    [].forEach.call(forms, function(form) {
+      const inputs = form.querySelectorAll('.field');
+
+      if (inputs.length !== 0) {
+        [].forEach.call(inputs, function(input) {
+          const types = ['dob', 'personal-key', 'ssn', 'zipcode'];
+
+          addListenerMulti(input, 'input invalid', (e) => {
+            e.target.setCustomValidity('');
+
+            if (e.target.validity.valueMissing) {
+              e.target.setCustomValidity(I18n.t('simple_form.required.text'));
+            } else if (e.target.validity.patternMismatch) {
+              types.forEach(function(type) {
+                if (e.target.classList.contains(type)) {
+                  e.target.setCustomValidity(I18n.t(`idv.errors.pattern_mismatch.${I18n.key(type)}`));
+                }
+              });
+            }
+          });
         });
       }
     });
