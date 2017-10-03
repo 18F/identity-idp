2a7a3747ea9dbccac490bb21d730124565f92bb2
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-10-02 22:14:20 -0500

    Unlock access key in session encryptor
    **Why**: The session encryptor was not unlocking the user access keys
    when it was initializing. This meant that if an old session that was
    created with the old non-deterministic access key was decrypted, that
    key would be used to unlock the session encryptor's user access key,
    setting the random_r to the value of the random_r for the
    non-deterministic key.

--- spec/services/session_encryptor_spec.rb
+++ spec/services/session_encryptor_spec.rb
@@ -21,6 +21,18 @@ describe SessionEncryptor do
     expect(encryptor2.load(encrypted_text)).to eq(payload)
   end
 
+  it 'does not modify the cek when failing to decrypt a payload encrypted with an old key' do
+    old_encryptor = SessionEncryptor.new
+    old_encryptor.user_access_key.unlock(Pii::Cipher.random_key)
+    old_payload = old_encryptor.dump('asdf' => '1234')
+
+    new_encryptor = SessionEncryptor.new
+    original_cek = new_encryptor.user_access_key.cek
+
+    expect { new_encryptor.load(old_payload) }.to raise_error(Pii::EncryptionError)
+    expect(new_encryptor.user_access_key.cek).to eq(original_cek)
+  end
+
   describe '#dump' do
     it 'encrypts session' do
       session = SessionEncryptor.new.dump(foo: 'bar')
