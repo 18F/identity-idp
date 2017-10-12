c10b78e70d014ea05feb04fae57571f4ae924153
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-10-12 12:18:13 -0500

    Add link to service provider to account history
    **Why**: So that user's can navigate to an SP from their account history
    page. Note that this only applies to SP's that have a
    `return_to_sp_url`. SP's without do not have a link.

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,5 +1,9 @@
 EventDecorator = Struct.new(:event) do
-  def pretty_event_type
+  def event_partial
+    'accounts/event_item'
+  end
+
+  def event_type
     I18n.t("event_types.#{event.event_type}")
   end
 
