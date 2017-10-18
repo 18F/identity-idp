bedcfe1309eb942b2f367ff73a475372e64067d8
Zach Margolis <zachary.margolis@gsa.gov>
2017-10-18 09:51:04 -0400

    Fix service provider seeding
    **Why**:
    - Update existing SPs with data in service_providers.yml
    - Stop relying on Figaro.env.chef_env, use LoginGov::Hostdata.env
    - Add ability to restrict SP to arbitrary envs for lower envs

--- app/services/service_provider_seeder.rb
+++ app/services/service_provider_seeder.rb
@@ -6,21 +6,33 @@ class ServiceProviderSeeder
   end
 
   def run
-    content = ERB.new(Rails.root.join('config', 'service_providers.yml').read).result
-    service_providers = YAML.load(content).fetch(rails_env, {})
-
     service_providers.each do |issuer, config|
-      next if Figaro.env.chef_env == 'prod' && config['allow_on_prod_chef_env'] != 'true'
+      next unless write_service_provider?(config)
+
       ServiceProvider.find_or_create_by!(issuer: issuer) do |sp|
         sp.approved = true
         sp.active = true
         sp.native = true
-        sp.attributes = config.except('allow_on_prod_chef_env')
-      end
+      end.update(config.except('restrict_to_deploy_env'))
     end
   end
 
   private
 
   attr_reader :rails_env, :deploy_env
+
+  def service_providers
+    content = ERB.new(Rails.root.join('config', 'service_providers.yml').read).result
+    YAML.safe_load(content).fetch(rails_env, {})
+  end
+
+  def write_service_provider?(config)
+    return true if rails_env != 'production'
+
+    restrict_env = config['restrict_to_deploy_env']
+
+    is_production_or_has_a_restriction = (deploy_env == 'prod' || restrict_env.present?)
+
+    !is_production_or_has_a_restriction || (restrict_env == deploy_env)
+  end
 end
--- spec/services/service_provider_seeder_spec.rb
+++ spec/services/service_provider_seeder_spec.rb
@@ -14,6 +14,28 @@ RSpec.describe ServiceProviderSeeder do
       expect { run }.to change { ServiceProvider.count }
     end
 
+    context 'with other existing service providers in the database' do
+      let!(:existing_provider) { create(:service_provider) }
+
+      it 'sets approved, active and native on service providers from the yaml' do
+        run
+
+        config_sp = ServiceProvider.from_issuer('http://test.host')
+        expect(config_sp.approved).to eq(true)
+        expect(config_sp.active).to eq(true)
+        expect(config_sp.native).to eq(true)
+      end
+
+      it 'does not change approve, active and native on the other existing service providers' do
+        run
+
+        existing_provider.reload
+        expect(existing_provider.approved).to_not eq(true)
+        expect(existing_provider.active).to_not eq(true)
+        expect(existing_provider.native).to_not eq(true)
+      end
+    end
+
     context 'when a service provider already exists in the database' do
       before do
         create(
@@ -33,14 +55,44 @@ RSpec.describe ServiceProviderSeeder do
     context 'when running in a production environment' do
       let(:rails_env) { 'production' }
 
-      it 'only adds/updates configs for that environment' do
-        run
+      context 'in prod' do
+        let(:deploy_env) { 'prod' }
 
-        expect(ServiceProvider.find_by(issuer: 'urn:gov:dhs.cbp.jobs:openidconnect:aws-cbp-ttp')).
-          to be
+        it 'only writes configs with restrict_to_deploy_env for prod' do
+          run
 
-        expect(ServiceProvider.find_by(issuer: 'urn:gov:dhs.cbp.jobs:openidconnect:cert')).
-          to eq(nil)
+          # restrict_to_deploy_env: prod
+          expect(ServiceProvider.find_by(issuer: 'urn:gov:dhs.cbp.jobs:openidconnect:aws-cbp-ttp')).
+            to be_present
+
+          # restrict_to_deploy_env: staging
+          expect(ServiceProvider.find_by(issuer: 'urn:gov:dhs.cbp.jobs:openidconnect:cert')).
+            to eq(nil)
+
+          # restrict_to_deploy_env: nil
+          expect(ServiceProvider.find_by(issuer: 'urn:gov:gsa:openidconnect:sp:sinatra')).
+            to eq(nil)
+        end
+      end
+
+      context 'in another environment' do
+        let(:deploy_env) { 'staging' }
+
+        it 'only writes configs with restrict_to_deploy_env for that env, or no restrictions' do
+          run
+
+          # restrict_to_deploy_env: prod
+          expect(ServiceProvider.find_by(issuer: 'urn:gov:dhs.cbp.jobs:openidconnect:aws-cbp-ttp')).
+            to eq(nil)
+
+          # restrict_to_deploy_env: staging
+          expect(ServiceProvider.find_by(issuer: 'urn:gov:dhs.cbp.jobs:openidconnect:cert')).
+            to be_present
+
+          # restrict_to_deploy_env: nil
+          expect(ServiceProvider.find_by(issuer: 'urn:gov:gsa:openidconnect:sp:sinatra')).
+            to be_present
+        end
       end
     end
   end
