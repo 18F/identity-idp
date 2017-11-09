4f48fdc799a28745f3b4b810e741c73ece9c099f
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-11-08 22:02:47 -0500

    Don't retry SMS and Voice background jobs
    **Why**:
    By default, Sidekiq automatically retries any failed job, but sometimes,
    the job might fail after the OTP was already sent, and retrying the job
    will send the OTP again, costing us money, and potentially the user
    too, as well as resulting in a confusing and annoying user experience.
    
    Moreover, OTPs are only valid for 5 minutes. It doesn't make sense to
    keep sending the same OTP over and over once it is no longer valid.

--- lib/no_retry_jobs.rb
+++ lib/no_retry_jobs.rb
@@ -2,7 +2,7 @@ class NoRetryJobs
   def call(_worker, msg, queue)
     yield
   rescue StandardError => _e
-    msg['retry'] = false if queue == 'idv'
+    msg['retry'] = false if %w[idv sms voice].include?(queue)
     raise
   end
 end
