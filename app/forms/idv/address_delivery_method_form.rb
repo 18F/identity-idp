72e6f3f18289ab2f44d4edcd38ef52f6505ed14b
TC Baxter <timothy.baxter@gsa.gov>
2017-11-15 13:33:21 -0600

    Delinting

--- app/forms/idv/address_delivery_method_form.rb
+++ app/forms/idv/address_delivery_method_form.rb
@@ -1,7 +1,9 @@
 module Idv
   class AddressDeliveryMethodForm
+    attr_accessor :address_delivery_method
+
     def submit(address_delivery_method: '')
-      @address_delivery_method = address_delivery_method
+      self.address_delivery_method = address_delivery_method
 
       FormResponse.new(success: valid_address_delivery_method?, errors: {})
     end
@@ -9,7 +11,7 @@ module Idv
     private
 
     def valid_address_delivery_method?
-      %w[phone usps].include? @address_delivery_method
+      %w[phone usps].include? self.address_delivery_method
     end
   end
 end
