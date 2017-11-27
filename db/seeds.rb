456fe3c0cb2484b2cc95d134f7274447b4cd09ac
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-11-27 11:13:32 -0500

    Remove code related to AppSetting
    **Why**: We are not using it, and it is incurring an unncessary DB
    lookup on every visit to the email registration page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# enable registrations by default
-AppSetting.find_or_create_by!(name: 'RegistrationsEnabled') do |setting|
-  setting.value = '1'
-end
-
 # add config/service_providers.yml
 ServiceProviderSeeder.new.run
