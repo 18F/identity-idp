37760c0663612b0ef773f92404c688e40fffd786
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-11-27 15:00:58 -0600

    Use nil for empty user/pass in basic auth
    **Why**: Building a URI with an empty string for the username or
    password raises a URI::InvalidURIError

--- spec/services/basic_auth_url_spec.rb
+++ spec/services/basic_auth_url_spec.rb
@@ -2,11 +2,22 @@ require 'rails_helper'
 
 RSpec.describe BasicAuthUrl do
   describe '.build' do
-    it 'is the URL as-is with no username or password' do
-      url = 'https://foo.example.com/bar'
-      external_url = BasicAuthUrl.build(url, user: nil, password: nil)
+    context 'with nil username and password' do
+      it 'is the URL as-is with no username or password' do
+        url = 'https://foo.example.com/bar'
+        external_url = BasicAuthUrl.build(url, user: nil, password: nil)
 
-      expect(external_url).to eq(url)
+        expect(external_url).to eq(url)
+      end
+    end
+
+    context 'with empty string for username and password' do
+      it 'is the URL as-is with no username or password' do
+        url = 'https://foo.example.com/bar'
+        external_url = BasicAuthUrl.build(url, user: '', password: '')
+
+        expect(external_url).to eq(url)
+      end
     end
 
     context 'with basic auth username and pass set in the config' do
