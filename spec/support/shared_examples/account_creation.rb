0457edf4785665f1f95eb929ab5a392901654ec4
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-12-15 22:49:23 -0500

    Redirect back to SP regardless of locale
    **Why**: Andrew found a bug where selecting Spanish during account
    creation or authentication would not redirect you back to the SP.
    
    The bug was in `SecureHeadersConcern`, which only allowed the redirect
    to happen if the redirect URI started with the OIDC endpoint. The
    problem is the URL of the OIDC endpoint is dynamic depending on the
    language. When the language is not English, the SP's redirect URI no
    longer starts with the same set of characters as the OIDC endpoint,
    therefore the condition will always be false, and the redirect will
    not be allowed.
    
    **How**: Instead of determining whether or not to return early based on
    whether or not the SP request URL is an OIDC URL, base it only on the
    presence of the request URL.
    
    I believe the original intent of the conditional in the first return
    statement was for performance reasons, since SAML URLs do not need to
    be added to CSP headers in the CompletionsController. The reason is
    that CSP headers only matter on the last POST request before a redirect
    to an external URL. In the case of OIDC, clicking Continue on the
    Completions page, or after 2FA authentication, is the last form that
    is submitted before the redirect back to the SP. However, for SAML,
    there is an additional hidden form that is rendered and automatically
    submitted via JS. This happens in SamlIdpController, which is why it
    has its own CSP override.
    
    Thanks to our use of the Null Object pattern, passing in a SAML URL
    in `SecureHeadersConcern` allows the app to work as expected because
    `authorize_form.valid?` will return `false`.

--- spec/support/shared_examples/account_creation.rb
+++ spec/support/shared_examples/account_creation.rb
@@ -23,3 +23,30 @@ shared_examples 'csrf error when acknowledging personal key' do |sp|
     expect(page).to have_content t('errors.invalid_authenticity_token')
   end
 end
+
+shared_examples 'creating an account with the site in Spanish' do |sp|
+  it 'redirects to the SP', email: true do
+    Capybara.current_session.driver.header('Accept-Language', 'es')
+    visit_idp_from_sp_with_loa1(sp)
+    register_user
+    click_acknowledge_personal_key
+
+    if sp == :oidc
+      expect(page.response_headers['Content-Security-Policy']).
+        to(include('form-action \'self\' http://localhost:7654'))
+    end
+
+    click_on t('forms.buttons.continue')
+
+    if sp == :saml
+      expect(current_url).to eq @saml_authn_request
+    end
+
+    if sp == :oidc
+      redirect_uri = URI(current_url)
+      redirect_params = Rack::Utils.parse_query(redirect_uri.query).with_indifferent_access
+
+      expect(redirect_uri.to_s).to start_with('http://localhost:7654/auth/result')
+    end
+  end
+end
