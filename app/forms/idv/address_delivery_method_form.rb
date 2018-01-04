f0b7b6d3e7de942770aedb1aa71e6e704b511875
Steve Urciuoli <gsa.steve.urciuoli@gmail.com>
2018-01-04 12:43:14 -0500

    Track address delivery method
    **Why**: It would be good to capture the address delivery method to see how many people choose phone vs USPS.
    
    **How**: Added the ADDRESS_DELIVERY_METHOD event tracking to the create method of the address controller

--- app/forms/idv/address_delivery_method_form.rb
+++ app/forms/idv/address_delivery_method_form.rb
@@ -2,13 +2,20 @@ module Idv
   class AddressDeliveryMethodForm
     attr_accessor :address_delivery_method
 
-    def submit(address_delivery_method: '')
-      self.address_delivery_method = address_delivery_method
+    def submit(params)
+      self.address_delivery_method = params[:address_delivery_method]
 
-      FormResponse.new(success: valid_address_delivery_method?, errors: {})
+      FormResponse.new(success: valid_address_delivery_method?, errors: {},
+                       extra: extra_analytics_attributes)
     end
 
     private
+
+    def extra_analytics_attributes
+      {
+        address_delivery_method: address_delivery_method,
+      }
+    end
 
     def valid_address_delivery_method?
       %w[phone usps].include? address_delivery_method
