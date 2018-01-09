e37a6d1fa7d1d6b502c577174a0cc6644c09b205
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-01-09 00:51:47 -0500

    Update rate limit settings
    **Why**: We want to be able to rate limit email+IP combinations
    separately from generic logins per IP. We also want to set the req/ip
    and logins/ip rate limits to track only mode for now until we get
    enough data from USAJOBS customers to figure out the optimal limit and
    period settings. Finally, we want to update the 429 page with a more
    helpful message.
    
    **How**:
    - Add Figaro config settings to allow us to easily toggle between track
    and throttle mode for the req/ip and logins/ip rate limits.
    - Add a blocklist with Allow2Ban to limit logins per email+IP. We could
    have used a throttle, but a blocklist provides more control over the
    bantime, which can be separate from the findtime. It also makes the
    bantime more accurate. In a throttle, the cache key gets cleared at
    the beginning of the minute. For example, if the throttle was set to
    5 requests per 60 seconds, if you made 6 requests between 11:00:50 and
    11:00:55, you be rate limited only until 11:01:00, at which point you
    have another 5 tries until 11:02:00, giving you 10 tries over 70
    seconds.
    - Log rate limit triggers via Analytics as opposed to Rails.logger
    so that we benefit from the request and user_agent parsing. Perhaps more
    importantly, adding the request host to the hash we were passing to
    Rails.logger resulted in a failing spec in `headers_spec` because the
    `to_json` call was not able to convert the ASCII-8BIT encoded host.
    - Update the text on the 429 page to make it more helpful to the user.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,6 +10,12 @@ module Rack
       def remote_ip
         @remote_ip ||= (env['action_dispatch.remote_ip'] || ip).to_s
       end
+
+      # This is needed because Ahoy (the gem our Analytics class calls) expects
+      # the request to respond to `headers`, which Rack::Attack does not define.
+      def headers
+        {}
+      end
     end
 
     ### Configure Cache ###
@@ -48,12 +54,22 @@ module Rack
     # Throttle all requests by IP
     #
     # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.remote_ip}"
-    throttle(
-      'req/ip',
-      limit: Figaro.env.requests_per_ip_limit.to_i,
-      period: Figaro.env.requests_per_ip_period.to_i.seconds
-    ) do |req|
-      req.remote_ip unless req.path.starts_with?('/assets')
+    if Figaro.env.requests_per_ip_track_only_mode == 'true'
+      track(
+        'req/ip',
+        limit: Figaro.env.requests_per_ip_limit.to_i,
+        period: Figaro.env.requests_per_ip_period.to_i
+      ) do |req|
+        req.remote_ip unless req.path.starts_with?('/assets')
+      end
+    else
+      throttle(
+        'req/ip',
+        limit: Figaro.env.requests_per_ip_limit.to_i,
+        period: Figaro.env.requests_per_ip_period.to_i
+      ) do |req|
+        req.remote_ip unless req.path.starts_with?('/assets')
+      end
     end
 
     ### Prevent Brute-Force Login Attacks ###
@@ -68,12 +84,45 @@ module Rack
     # Throttle sign in attempts by IP address
     #
     # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.remote_ip}"
-    throttle(
-      'logins/ip',
-      limit: Figaro.env.logins_per_ip_limit.to_i,
-      period: Figaro.env.logins_per_ip_period.to_i.seconds
-    ) do |req|
-      req.remote_ip if req.path == '/' && req.post?
+    if Figaro.env.logins_per_ip_track_only_mode == 'true'
+      track(
+        'logins/ip',
+        limit: Figaro.env.logins_per_ip_limit.to_i,
+        period: Figaro.env.logins_per_ip_period.to_i
+      ) do |req|
+        req.remote_ip if req.path == '/' && req.post?
+      end
+    else
+      throttle(
+        'logins/ip',
+        limit: Figaro.env.logins_per_ip_limit.to_i,
+        period: Figaro.env.logins_per_ip_period.to_i
+      ) do |req|
+        req.remote_ip if req.path == '/' && req.post?
+      end
+    end
+
+    # Lockout IP addresses that are attempting to sign in with the same username
+    # over and over.
+    # After maxretry requests in findtime minutes, block all requests from that IP for bantime.
+    blocklist('logins/email+ip') do |req|
+      if req.path == '/' && req.post?
+        # `filter` returns false if POST request is for the login page (but still
+        # increments the count), so requests below the limit are not blocked until
+        # they hit the limit. At that point, `filter` will return true and block.
+        user = req.params.fetch('user', {})
+        email = user['email'].to_s
+        email_fingerprint = Pii::Fingerprinter.fingerprint(email) if email.present?
+        email_and_ip = "#{email_fingerprint}-#{req.remote_ip}"
+        maxretry = Figaro.env.logins_per_email_and_ip_limit.to_i
+        findtime = Figaro.env.logins_per_email_and_ip_period.to_i
+        bantime = Figaro.env.logins_per_email_and_ip_bantime.to_i
+
+        Allow2Ban.filter(email_and_ip, maxretry: maxretry, findtime: findtime, bantime: bantime) do
+          # The count for the email and IP combination is incremented if the return value is truthy.
+          req.path == '/' && req.post?
+        end
+      end
     end
 
     ### Custom Throttle Response ###
@@ -97,11 +146,7 @@ module Rack
 end
 
 ActiveSupport::Notifications.subscribe('rack.attack') do |_name, _start, _finish, _request_id, req|
-  Rails.logger.warn({
-    event: 'throttle',
-    type: req.env['rack.attack.matched'],
-    user_ip: req.remote_ip,
-    user_uuid: req.env['warden'].user&.uuid,
-    visitor_id: req.cookies['ahoy_visitor'],
-  }.to_json)
+  user = req.env['warden'].user || AnonymousUser.new
+  analytics = Analytics.new(user: user, request: req, sp: nil)
+  analytics.track_event(Analytics::RATE_LIMIT_TRIGGERED, type: req.env['rack.attack.matched'])
 end
