64dffcd6ccb2379a1335a81d38e621cf24366888
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-04-05 22:55:12 -0500

    rubocop fixes

--- app/services/remember_device_cookie.rb
+++ app/services/remember_device_cookie.rb
@@ -34,8 +34,18 @@ class RememberDeviceCookie
 
   def valid_for_user?(user)
     return false if user.id != user_id
-    return false if user.phone_confirmed_at.to_i > created_at.to_i
-    return false if created_at < Figaro.env.remember_device_expiration_days.to_i.days.ago
+    return false if user_has_changed_phone?(phone)
+    return false if expired?
     true
+  end
+
+  private
+
+  def expired?
+    created_at < Figaro.env.remember_device_expiration_days.to_i.days.ago
+  end
+
+  def user_has_changed_phone?(user)
+    user.phone_confirmed_at.to_i > created_at.to_i
   end
 end
