95def178e93ccec8918ea71e244ea85a2c3792c6
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-04-13 14:31:28 -0500

    Create a phone step spec
    **Why**: So we can test all of the phone step features in one place

--- spec/features/idv/failed_job_spec.rb
+++ spec/features/idv/failed_job_spec.rb
@@ -4,12 +4,6 @@ feature 'IdV session', :idv_job do
   include IdvStepHelper
 
   context 'profile job' do
-    let(:idv_job_class) { Idv::ProfileJob }
     it_behaves_like 'failed idv job', :profile
-  end
-
-  context 'phone job' do
-    let(:idv_job_class) { Idv::PhoneJob }
-    it_behaves_like 'failed idv job', :phone
   end
 end
--- spec/features/idv/max_attempts_spec.rb
+++ spec/features/idv/max_attempts_spec.rb
@@ -9,10 +9,4 @@ feature 'IdV max attempts', :idv_job, :email do
     it_behaves_like 'verification step max attempts', :profile, :oidc
     it_behaves_like 'verification step max attempts', :profile, :saml
   end
-
-  context 'phone step' do
-    it_behaves_like 'verification step max attempts', :phone
-    it_behaves_like 'verification step max attempts', :phone, :oidc
-    it_behaves_like 'verification step max attempts', :phone, :saml
-  end
 end
