6c913c1f13044803859c38b840898af731f1c20b
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-04-13 13:21:23 -0500

    use idv step helpers from master

--- spec/features/idv/failed_job_spec.rb
+++ spec/features/idv/failed_job_spec.rb
@@ -5,17 +5,11 @@ feature 'IdV session', :idv_job do
 
   context 'profile job' do
     let(:idv_job_class) { Idv::ProfileJob }
-
-    alias complete_previous_idv_steps start_idv_at_profile_step
-
-    it_behaves_like 'failed idv job', :sessions
+    it_behaves_like 'failed idv job', :profile
   end
 
   context 'phone job' do
     let(:idv_job_class) { Idv::PhoneJob }
-
-    alias complete_previous_idv_steps complete_idv_steps_before_phone_step
-
     it_behaves_like 'failed idv job', :phone
   end
 end
--- spec/support/idv_examples/failed_idv_job.rb
+++ spec/support/idv_examples/failed_idv_job.rb
@@ -1,36 +1,41 @@
 shared_examples 'failed idv job' do |step|
+  let(:step_locale_key) do
+    return :sessions if step == :profile
+    step
+  end
+
   before do
     visit_idp_from_sp_with_loa3(:oidc)
     click_link t('links.sign_in')
-    complete_previous_idv_steps
+    complete_idv_steps_before_step(step)
   end
 
   context 'the job raises an error' do
     before do
       stub_idv_job_to_raise_error_in_background(idv_job_class)
 
-      fill_out_idv_form_ok if step == :sessions
+      fill_out_idv_form_ok if step == :profile
       fill_out_phone_form_ok if step == :phone
       click_idv_continue
     end
 
     context 'without js' do
       it 'shows a warning' do
-        expect(page).to have_content t("idv.modal.#{step}.heading")
-        expect(page).to have_content t("idv.modal.#{step}.jobfail")
-        expect(page).to have_current_path(verify_session_result_path) if step == :sessions
+        expect(page).to have_content t("idv.modal.#{step_locale_key}.heading")
+        expect(page).to have_content t("idv.modal.#{step_locale_key}.jobfail")
+        expect(page).to have_current_path(verify_session_result_path) if step == :profile
         expect(page).to have_current_path(verify_phone_result_path) if step == :phone
       end
     end
 
     context 'with js', :js do
       it 'shows a modal' do
-        expect(page).to have_css('.modal-warning', text: t("idv.modal.#{step}.heading"))
+        expect(page).to have_css('.modal-warning', text: t("idv.modal.#{step_locale_key}.heading"))
         expect(page).to have_css(
           '.modal-warning',
-          text: ActionController::Base.helpers.strip_tags(t("idv.modal.#{step}.jobfail"))
+          text: ActionController::Base.helpers.strip_tags(t("idv.modal.#{step_locale_key}.jobfail"))
         )
-        expect(page).to have_current_path(verify_session_result_path) if step == :sessions
+        expect(page).to have_current_path(verify_session_result_path) if step == :profile
         expect(page).to have_current_path(verify_phone_result_path) if step == :phone
       end
     end
@@ -40,7 +45,7 @@ shared_examples 'failed idv job' do |step|
     before do
       stub_idv_job_to_timeout_in_background(idv_job_class)
 
-      fill_out_idv_form_ok if step == :sessions
+      fill_out_idv_form_ok if step == :profile
       fill_out_phone_form_ok('5202691958') if step == :phone
       click_idv_continue
 
@@ -55,21 +60,21 @@ shared_examples 'failed idv job' do |step|
 
     context 'without js' do
       it 'shows a warning' do
-        expect(page).to have_content t("idv.modal.#{step}.heading")
-        expect(page).to have_content t("idv.modal.#{step}.timeout")
-        expect(page).to have_current_path(verify_session_result_path) if step == :sessions
+        expect(page).to have_content t("idv.modal.#{step_locale_key}.heading")
+        expect(page).to have_content t("idv.modal.#{step_locale_key}.timeout")
+        expect(page).to have_current_path(verify_session_result_path) if step == :profile
         expect(page).to have_current_path(verify_phone_result_path) if step == :phone
       end
     end
 
     context 'with js' do
       it 'shows a modal' do
-        expect(page).to have_css('.modal-warning', text: t("idv.modal.#{step}.heading"))
+        expect(page).to have_css('.modal-warning', text: t("idv.modal.#{step_locale_key}.heading"))
         expect(page).to have_css(
           '.modal-warning',
-          text: ActionController::Base.helpers.strip_tags(t("idv.modal.#{step}.timeout"))
+          text: ActionController::Base.helpers.strip_tags(t("idv.modal.#{step_locale_key}.timeout"))
         )
-        expect(page).to have_current_path(verify_session_result_path) if step == :sessions
+        expect(page).to have_current_path(verify_session_result_path) if step == :profile
         expect(page).to have_current_path(verify_phone_result_path) if step == :phone
       end
     end
