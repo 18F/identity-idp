5e5c2ec1bd0249dde2e83068286a0ee4fbef733b
David Corwin <david.corwin@gsa.gov>
2018-04-24 15:56:35 -0700

    Fix rubocop offenses

--- app/services/idv/agent.rb
+++ app/services/idv/agent.rb
@@ -11,18 +11,11 @@ module Idv
     end
 
     def proof(*stages)
-      results = {
-        errors: {},
-        normalized_applicant: {},
-        reasons: [],
-        success: false,
-      }
+      results = { errors: {}, normalized_applicant: {}, reasons: [], success: false }
 
       stages.each do |stage|
         proofer_result = proof_one(stage)
-
         results = merge_results(results, proofer_result)
-
         break unless proofer_result.success?
       end
 
@@ -30,35 +23,35 @@ module Idv
     end
 
     def proof_one(stage)
+      applicant_hash = @applicant.to_hash.with_indifferent_access
+
       case stage
 
       when :phone
-        get_agent(:phone_proofing_vendor).
-          submit_phone(@applicant.phone)
+        get_agent(:phone_proofing_vendor).submit_phone(@applicant.phone)
 
       when :profile
-        get_agent(:profile_proofing_vendor).
-          start(@applicant.to_hash.with_indifferent_access)
+        get_agent(:profile_proofing_vendor).start(applicant_hash)
 
       when :state_id
         get_agent(:state_id_proofing_vendor).
-          submit_state_id(@applicant.to_hash.with_indifferent_access.merge(state_id_jurisdiction: @applicant.state))
+          submit_state_id(applicant_hash.merge(state_id_jurisdiction: @applicant.state))
       end
     end
 
     private
 
     def merge_results(results, proofer_result)
-        vr = proofer_result.vendor_resp
+      vr = proofer_result.vendor_resp
 
-        normalized_applicant = vr.respond_to?(:normalized_applicant) ? vr.normalized_applicant : {}
+      normalized_applicant = vr.try(:normalized_applicant) || {}
 
-        {
-          errors: results[:errors].merge(proofer_result.errors),
-          normalized_applicant: results[:normalized_applicant].merge(normalized_applicant),
-          reasons: results[:reasons] + vr.reasons,
-          success: proofer_result.success?,
-        }
+      {
+        errors: results[:errors].merge(proofer_result.errors),
+        normalized_applicant: results[:normalized_applicant].merge(normalized_applicant),
+        reasons: results[:reasons] + vr.reasons,
+        success: proofer_result.success?,
+      }
     end
 
     def get_agent(vendor)
