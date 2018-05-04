1b314118def85f17b13ba6107cad062309b08468
David Corwin <david.corwin@gsa.gov>
2018-05-04 10:47:53 -0700

    Only configure proofers if identity verification is enabled

--- config/initializers/proofer.rb
+++ config/initializers/proofer.rb
@@ -1,7 +1,9 @@
-Idv::Proofer.configure do |config|
-  config.mock_fallback = Figaro.env.proofer_mock_fallback == 'true'
-  config.raise_on_missing_proofers = Figaro.env.proofer_raise_on_missing_proofers == 'false'
-  config.vendors = JSON.parse(Figaro.env.proofer_vendors || '[]')
-end
+if FeatureManagament.enable_identity_verification?
+  Idv::Proofer.configure do |config|
+    config.mock_fallback = Figaro.env.proofer_mock_fallback == 'true'
+    config.raise_on_missing_proofers = Figaro.env.proofer_raise_on_missing_proofers == 'false'
+    config.vendors = JSON.parse(Figaro.env.proofer_vendors || '[]')
+  end
 
-Idv::Proofer.init
+  Idv::Proofer.init
+end
