ce1e97da20e31f8f94003f43d06111a1028597fe
David Corwin <david.corwin@gsa.gov>
2018-05-04 10:56:12 -0700

    Fix spelling error

--- config/initializers/proofer.rb
+++ config/initializers/proofer.rb
@@ -1,4 +1,4 @@
-if FeatureManagament.enable_identity_verification?
+if FeatureManagement.enable_identity_verification?
   Idv::Proofer.configure do |config|
     config.mock_fallback = Figaro.env.proofer_mock_fallback == 'true'
     config.raise_on_missing_proofers = Figaro.env.proofer_raise_on_missing_proofers == 'false'
