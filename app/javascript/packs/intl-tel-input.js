b8b0495dd65226d2aabc05713ac11b5f27e40990
Scott Weber <stephen.weber@gsa.gov>
2018-05-09 15:14:26 -0400

    Fix code climate JS issues

--- app/javascript/packs/intl-tel-input.js
+++ app/javascript/packs/intl-tel-input.js
@@ -1,6 +1,5 @@
 // Setting hidden input for intl code
 
-const countryData = $.fn.intlTelInput.getCountryData();
 const telInput = $('#user_phone_form_phone');
 const intlCode = $('#user_phone_form_international_code');
 
@@ -9,11 +8,12 @@ telInput.intlTelInput({ preferredCountries: ['us', 'ca'] });
 
 // set it's initial value
 const initialCountry = telInput.intlTelInput('getSelectedCountryData').dialCode;
-telInput.val('+' + initialCountry);
+telInput.val(`+initialCountry`);
 
 // inserting country code on dropdown change
 telInput.on('countrychange', function(e, countryData) {
-  telInput.val('+' + countryData.dialCode);
+
+  telInput.val(`+countryData.dialCode`);
   intlCode.val(countryData.iso2.toUpperCase());
 
 });
