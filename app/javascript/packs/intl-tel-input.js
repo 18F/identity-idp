a6d18b45206af80125b9088b33c10f6293dd7c0e
Scott Weber <stephen.weber@gsa.gov>
2018-05-09 16:22:05 -0400

    Fix code climate unexpected string concatenation

--- app/javascript/packs/intl-tel-input.js
+++ app/javascript/packs/intl-tel-input.js
@@ -4,11 +4,14 @@ const telInput = $('#user_phone_form_phone');
 const intlCode = $('#user_phone_form_international_code');
 
 // initialise plugin
-telInput.intlTelInput({ preferredCountries: ['us', 'ca'] });
+telInput.intlTelInput({
+  preferredCountries: ['us', 'ca'],
+  excludeCountries: []
+});
 
 // set it's initial value
 const initialCountry = telInput.intlTelInput('getSelectedCountryData').dialCode;
-telInput.val('+' + initialCountry);
+telInput.val(`+${initialCountry}`);
 
 // inserting country code on dropdown change
 telInput.on('countrychange', function(e, countryData) {
