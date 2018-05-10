1110e1a64300ebf603de88bf19b4b453c4b35de5
Scott Weber <stephen.weber@gsa.gov>
2018-05-10 12:54:18 -0400

    Fix tests and refactor `county_select` method

--- spec/features/two_factor_authentication/sign_in_spec.rb
+++ spec/features/two_factor_authentication/sign_in_spec.rb
@@ -106,9 +106,7 @@ feature 'Two Factor Authentication' do
 
       scenario 'disables the phone option and displays a warning with js', :js do
         sign_in_before_2fa
-        select_country('tr')
-        input = find('#user_phone_form_phone')
-        input.send_keys ('3122132965')
+        select_country_and_type_phone_number(country: 'tr', number: '3122132965')
 
         phone_radio_button = page.find(
           '#user_phone_form_otp_delivery_preference_voice',
@@ -121,9 +119,7 @@ feature 'Two Factor Authentication' do
         )
         expect(phone_radio_button).to be_disabled
 
-        select_country('ca')
-        input = find('#user_phone_form_phone')
-        input.send_keys ('3122132965')
+        select_country_and_type_phone_number(country: 'ca', number: '3122132965')
 
         expect(page).not_to have_content t(
           'devise.two_factor_authentication.otp_delivery_preference.phone_unsupported',
@@ -134,31 +130,30 @@ feature 'Two Factor Authentication' do
 
       scenario 'does not allow the user to remove the international code after entering it', :js do
         sign_in_before_2fa
-        select_country('jp')
-        input = find('#user_phone_form_phone')
-        input.send_keys ('543543643')
-
+        select_country_and_type_phone_number(country: 'jp', number: '543543643')
         input = find('#user_phone_form_phone')
         input.send_keys(*([:backspace] * input.value.length))
 
-        expect(input.value).to eq('+81 ')
+        expect(phone_field.value).to eq('+81 ')
       end
 
       scenario 'allows a user to continue typing even if a number is invalid', :js do
         sign_in_before_2fa
-        select_country('us')
+        select_country_and_type_phone_number(country: 'us', number: '12345678901234567890')
 
-        input = find('#user_phone_form_phone')
-        input.send_keys('12345678901234567890')
-
-        expect(input.value).to eq('+1 12345678901234567890')
+        expect(phone_field.value).to eq('+1 12345678901234567890')
       end
     end
   end
 
-  def select_country(country)
+  def phone_field
+    find('#user_phone_form_phone')
+  end
+
+  def select_country_and_type_phone_number(country:, number:)
     find(".selected-flag").click
     find(".country[data-country-code='#{country}']:not(.preferred)").click
+    phone_field.send_keys(number)
   end
 
   def attempt_to_bypass_2fa_setup
