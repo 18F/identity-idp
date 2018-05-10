f40bd3ba408ec3d65fc3692cb583b274e10c5edd
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-05-10 16:25:02 -0500

    Fix scope of present_with method
    **Why**: Defining the method at the global scope means that when another
    presenter spec redefines it overrides the original implementation
    causing the tests to fail.

--- spec/presenters/two_factor_auth_code/generic_delivery_presenter_spec.rb
+++ spec/presenters/two_factor_auth_code/generic_delivery_presenter_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
-def presenter_with(arguments = {}, view = ActionController::Base.new.view_context)
-  TwoFactorAuthCode::GenericDeliveryPresenter.new(data: arguments, view: view)
-end
-
 describe TwoFactorAuthCode::GenericDeliveryPresenter do
   it 'is an abstract presenter with methods that should be implemented' do
     presenter = presenter_with
@@ -15,7 +11,7 @@ describe TwoFactorAuthCode::GenericDeliveryPresenter do
 
   describe '#personal_key_link' do
     context 'with unconfirmed user' do
-      presenter = presenter_with(personal_key_unavailable: true)
+      let(:presenter) { presenter_with(personal_key_unavailable: true) }
 
       it 'returns without providing the option to use a personal key' do
         expect(presenter.personal_key_link).to be_nil
@@ -23,7 +19,7 @@ describe TwoFactorAuthCode::GenericDeliveryPresenter do
     end
 
     context 'with confirmed user' do
-      presenter = presenter_with(personal_key_unavailable: false)
+      let(:presenter) { presenter_with(personal_key_unavailable: false) }
 
       it 'returns a personal key link' do
         expect(presenter.personal_key_link).not_to be_nil
@@ -50,5 +46,9 @@ describe TwoFactorAuthCode::GenericDeliveryPresenter do
         )
       end
     end
+  end
+
+  def presenter_with(arguments = {}, view = ActionController::Base.new.view_context)
+    TwoFactorAuthCode::GenericDeliveryPresenter.new(data: arguments, view: view)
   end
 end
