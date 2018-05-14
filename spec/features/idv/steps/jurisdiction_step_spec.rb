acd373fe5e1dbae7c4e1aa3fcea89b4e8e75d49e
David Corwin <david.corwin@gsa.gov>
2018-05-14 13:18:08 -0700

    Rename specs from verify to jurisdiction

--- spec/features/idv/steps/jurisdiction_step_spec.rb
+++ spec/features/idv/steps/jurisdiction_step_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-feature 'idv verify step' do
+feature 'idv jurisdiction step' do
   include IdvStepHelper
 
   it 'allows the user to continue to the profile step' do
     start_idv_from_sp
-    complete_idv_steps_before_verify_step
+    complete_idv_steps_before_jurisdiction_step
 
     expect(page).to have_current_path(idv_jurisdiction_path)
     expect(page).to have_content(t('idv.messages.jurisdiction.why'))
@@ -18,8 +18,8 @@ feature 'idv verify step' do
   end
 
   context 'cancelling idv' do
-    it_behaves_like 'cancel at idv step', :verify
-    it_behaves_like 'cancel at idv step', :verify, :oidc
-    it_behaves_like 'cancel at idv step', :verify, :saml
+    it_behaves_like 'cancel at idv step', :jurisdiction
+    it_behaves_like 'cancel at idv step', :jurisdiction, :oidc
+    it_behaves_like 'cancel at idv step', :jurisdiction, :saml
   end
 end
