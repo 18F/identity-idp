acfe427b2ea62dbad14f74a7fcddc3732d494e61
Jonathan Hooper <jon9820@gmail.com>
2018-05-15 09:25:49 -0500

    Add session encryptor (#2164)
    **Why**: To modularize the encryption code and make use of the
    conventions setup by the attribute and PII encryptors

--- spec/services/session_encryptor_spec.rb
+++ spec/services/session_encryptor_spec.rb
@@ -9,6 +9,15 @@ describe SessionEncryptor do
     end
   end
 
+  describe '#dump' do
+    it 'encrypts session' do
+      session = SessionEncryptor.new.dump(foo: 'bar')
+
+      expect(session).to_not match 'foo'
+      expect(session).to_not match 'bar'
+    end
+  end
+
   it 'makes a roundtrip okay across separate instances' do
     encryptor1 = SessionEncryptor.new
     encryptor2 = SessionEncryptor.new
@@ -19,28 +28,5 @@ describe SessionEncryptor do
     payload = { 'hello' => 'world' }
     encrypted_text = encryptor1.dump(payload)
     expect(encryptor2.load(encrypted_text)).to eq(payload)
-  end
-
-  it 'does not modify the user access key when decrypting a payload encrypted with an old key' do
-    old_encryptor = SessionEncryptor.new
-    old_payload = old_encryptor.dump('asdf' => '1234')
-
-    new_encryptor = SessionEncryptor.new
-    new_payload = new_encryptor.dump('1234' => 'asdf')
-    original_cek = new_encryptor.duped_user_access_key.cek
-
-    expect(new_encryptor.load(old_payload)).to eq('asdf' => '1234')
-    expect(new_encryptor.duped_user_access_key.cek).to eq(original_cek)
-
-    expect(new_encryptor.load(new_payload)).to eq('1234' => 'asdf')
-  end
-
-  describe '#dump' do
-    it 'encrypts session' do
-      session = SessionEncryptor.new.dump(foo: 'bar')
-
-      expect(session).to_not match 'foo'
-      expect(session).to_not match 'bar'
-    end
   end
 end
