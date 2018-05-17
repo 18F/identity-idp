deb3089a69989265e77d746e402006f02de311bf
Scott Weber <stephen.weber@gsa.gov>
2018-05-16 20:54:48 -0400

    Update and clean up JS remove jQuery from phone-internationalization

--- app/javascript/packs/intl-tel-input.js
+++ app/javascript/packs/intl-tel-input.js
@@ -2,21 +2,26 @@
 import $ from 'jquery';
 import 'intl-tel-input';
 
-const telInput = $('#user_phone_form_phone');
-const intlCode = $('#user_phone_form_international_code');
+// Setting variables that jQuery is using with a $ at the start of the const name
+const $telInput = $('#user_phone_form_phone');
+const telInput = document.querySelector('#user_phone_form_phone');
+const $intlCode = $('#user_phone_form_international_code');
 
 // initialise plugin
-telInput.intlTelInput({
+$telInput.intlTelInput({
   preferredCountries: ['us', 'ca'],
   excludeCountries: ['io', 'ki', 'nf', 'nr', 'nu', 'sh', 'sx', 'tk', 'wf'],
 });
 
-// set its initial value
-const initialCountry = telInput.intlTelInput('getSelectedCountryData').dialCode;
-telInput.val(`+${initialCountry}`);
+// Setting initial country dial code for phone input
+const $initialCountry = $telInput.intlTelInput('getSelectedCountryData').dialCode;
+$telInput.val(`+${$initialCountry}`);
 
-// inserting country code on dropdown change
-telInput.on('countrychange', function(e, countryData) {
-  telInput.val(`+${countryData.dialCode}`);
-  intlCode.val(countryData.iso2.toUpperCase());
+$telInput.on('countrychange', function(e, countryData) {
+// Adding country dial code to phone input on JS dropdown change
+  $telInput.val(`+${countryData.dialCode}`);
+// Changing hidden dropdown country code on JS dropdown change
+  $intlCode.val(countryData.iso2.toUpperCase());
+// Using plain JS to dispatch the country change event to phone-internationalization.js
+  telInput.dispatchEvent(new Event('countryChange'));
 });
