772e2f8aed2ba5c97a44e4e6f79e4cc249ef1477
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-06-11 10:55:01 -0500

    Add tests for the password verifier

--- spec/services/encryption/password_verifier_spec.rb
+++ spec/services/encryption/password_verifier_spec.rb
@@ -9,7 +9,7 @@ describe Encryption::PasswordVerifier do
       digest = described_class.digest('saltypickles')
 
       parsed_digest = JSON.parse(digest)
-      uak = UserAccessKey.new(password: 'saltypickles', salt: salt)
+      uak = Encryption::UserAccessKey.new(password: 'saltypickles', salt: salt)
       uak.unlock(parsed_digest['encryption_key'])
 
       expect(parsed_digest['encrypted_password']).to eq(uak.encrypted_password)
@@ -21,11 +21,21 @@ describe Encryption::PasswordVerifier do
 
   describe '.verify' do
     it 'returns true if the password matches' do
+      password = 'saltypickles'
 
+      digest = described_class.digest(password)
+      result = described_class.verify(password: password, digest: digest)
+
+      expect(result).to eq(true)
     end
 
     it 'returns false if the password does not match' do
+      password = 'saltypickles'
 
+      digest = described_class.digest('saltypickles')
+      result = described_class.verify(password: 'pepperpickles', digest: digest)
+
+      expect(result).to eq(false)
     end
   end
 end
