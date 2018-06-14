f334a8745f644eb9a4a42f3b4cc11882c69378e2
James Smith <James_G_Smith@omb.eop.gov>
2018-06-14 15:01:21 -0400

    [LG-267] Auth requests to decode pivcac tokens
    **Why**:
    We don't want everyone being able to take a token and
    inspect it. We only want the IdP able to do this.
    
    **How**:
    We have several options, including client/server TLS
    certificates. But adding an HMAC with a shared secret
    is the lightest weight way to lock down the decoding
    endpoint.

--- app/services/piv_cac_service.rb
+++ app/services/piv_cac_service.rb
@@ -1,3 +1,4 @@
+require 'base64'
 require 'cgi'
 require 'net/https'
 
@@ -61,14 +62,33 @@ module PivCacService
       return { 'error' => 'service.disabled' } if FeatureManagement.identity_pki_disabled?
 
       uri = URI(piv_cac_verify_token_link)
-      res = Net::HTTP.post_form(uri, token: token)
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.request(decode_request(uri, token))
+      end
       decode_token_response(res)
+    end
+
+    def decode_request(uri, token)
+      req = Net::HTTP::Post.new(uri, 'Authentication' => authenticate(token))
+      req.form_data = { token: token }
+      req
+    end
+
+    def authenticate(token)
+      # TODO: make this secret required once we have everything deployed and configured
+      # The piv/cac service side is pending, so this is not critical yet.
+      secret = Figaro.env.piv_cac_verify_token_secret
+      return '' if secret.blank?
+      nonce = SecureRandom.hex(10)
+      hmac = Base64.urlsafe_encode64(
+        OpenSSL::HMAC.digest('SHA256', secret, [token, nonce].join('+'))
+      )
+      "hmac :#{nonce}:#{hmac}"
     end
 
     def decode_token_response(res)
       return { 'error' => 'token.bad' } unless res.code.to_i == 200
-      result = res.body
-      JSON.parse(result)
+      JSON.parse(res.body)
     rescue JSON::JSONError
       { 'error' => 'token.bad' }
     end
