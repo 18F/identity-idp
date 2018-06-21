1057c7762ba8fd2b08f8b3d1ab791f1ad08753ee
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-06-21 14:02:41 -0500

    some sql sanitizing

--- app/models/password_metric.rb
+++ app/models/password_metric.rb
@@ -3,24 +3,24 @@ class PasswordMetric < ApplicationRecord
 
   def self.increment(metric, value)
     create_row_for_metric_category(metric, value)
-    sql = <<-SQL
-      UPDATE password_metrics
-      SET count = count + 1
-      WHERE metric = #{metrics[metric.to_s]} AND value = #{value}
-    SQL
-    connection.execute(sql)
+    query = sanitize_sql_array [
+      'UPDATE password_metrics',
+      'SET count = count + 1',
+      "WHERE metric = #{metrics[metric.to_s]} AND value = #{value}",
+    ]
+    connection.execute(query)
   end
 
   private_class_method def self.create_row_for_metric_category(metric, value)
     metric_key = metrics[metric.to_s]
     # Insert a row with the count equal to 0 if a row does not already exist
-    sql = <<-SQL
-      INSERT INTO password_metrics (metric, value, count)
-      SELECT #{metric_key}, #{value}, 0
-      WHERE NOT EXISTS (
-        SELECT id FROM password_metrics WHERE metric = #{metric_key} AND value = #{value}
-      )
-    SQL
-    connection.execute(sql)
+    query = sanitize_sql_array [
+      'INSERT INTO password_metrics (metric, value, count)',
+      "SELECT #{metric_key}, #{value}, 0",
+      'WHERE NOT EXISTS (',
+      "SELECT id FROM password_metrics WHERE metric = #{metric_key} AND value = #{value}",
+      ')',
+    ]
+    connection.execute(query)
   end
 end
