7dcf0e558df5feb345495dbef15ea3ddc3cc0d0b
David Corwin <david.corwin@gsa.gov>
2018-06-26 09:38:53 -0700

    Add authentication to usps_upload endpoint

--- app/controllers/usps_upload_controller.rb
+++ app/controllers/usps_upload_controller.rb
@@ -1,10 +1,25 @@
 class UspsUploadController < ApplicationController
   def create
-    UspsUploader.new.run unless HolidayService.observed_holiday?(today)
-    render plain: 'ok'
+    authorize do
+      UspsUploader.new.run unless HolidayService.observed_holiday?(today)
+      render plain: 'ok'
+    end
   end
 
   private
+
+  def authorize
+    # Check for empty to make sure that the token is configured
+    if authorization_token && authorization_token == Figaro.env.usps_upload_token
+      yield
+    else
+      head :unauthorized
+    end
+  end
+
+  def authorization_token
+    request.headers['X-USPS-UPLOAD-TOKEN']
+  end
 
   def today
     Time.zone.today
--- spec/controllers/usps_upload_controller_spec.rb
+++ spec/controllers/usps_upload_controller_spec.rb
@@ -2,24 +2,58 @@ require 'rails_helper'
 
 describe UspsUploadController do
   describe '#create' do
-    it 'runs the uploader' do
-      usps_uploader = instance_double(UspsUploader)
-      expect(usps_uploader).to receive(:run)
-      expect(UspsUploader).to receive(:new).and_return(usps_uploader)
+    context 'with no token' do
+      it 'returns unauthorized' do
+        post :create
 
-      post :create
-
-      expect(response).to have_http_status(:ok)
+        expect(response.status).to eq 401
+      end
     end
 
-    it 'does not run the upload on a federal holiday' do
-      expect(controller).to receive(:today).and_return(Date.new(2019, 1, 1))
+    context 'with an invalid token' do
+      before do
+        headers('foobar')
+      end
 
-      expect(UspsUploader).not_to receive(:new)
+      it 'returns unauthorized' do
+        post :create
 
-      post :create
-
-      expect(response).to have_http_status(:ok)
+        expect(response.status).to eq 401
+      end
     end
+
+    context 'with a valid token' do
+      before do
+        headers(Figaro.env.usps_upload_token)
+      end
+
+      context 'on a federal workday' do
+        it 'runs the uploader' do
+          usps_uploader = instance_double(UspsUploader)
+          expect(usps_uploader).to receive(:run)
+          expect(UspsUploader).to receive(:new).and_return(usps_uploader)
+
+          post :create
+
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'on a federal holiday' do
+        it 'does not run the uploader' do
+          expect(controller).to receive(:today).and_return(Date.new(2019, 1, 1))
+
+          expect(UspsUploader).not_to receive(:new)
+
+          post :create
+
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+  end
+
+  def headers(token)
+    request.headers['X-USPS-UPLOAD-TOKEN'] = token
   end
 end
