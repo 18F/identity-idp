2c52408949fa4e1cf68c0ec716b9e3fc9f36d739
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-07-03 23:15:50 -0400

    Remove TODOs from codebase
    **Why**: They don't belong in the code. History has shown that once they
    are added, they are rarely removed or acted upon. If something is
    important enough, an issue should be opened so it can be tracked.

--- app/controllers/concerns/user_session_context.rb
+++ app/controllers/concerns/user_session_context.rb
@@ -5,7 +5,6 @@ module UserSessionContext
     user_session[:context] || DEFAULT_CONTEXT
   end
 
-  # TODO: Figure out better names for this and the method below
   def initial_authentication_context?
     context == DEFAULT_CONTEXT
   end
