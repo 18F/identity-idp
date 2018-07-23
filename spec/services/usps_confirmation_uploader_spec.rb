744c0227851296ed2b3a7c570680e373f71f6031
David Corwin <david.corwin@gsa.gov>
2018-07-23 14:51:16 -0700

    Finish up specs;

--- spec/services/usps_confirmation_uploader_spec.rb
+++ spec/services/usps_confirmation_uploader_spec.rb
@@ -1,100 +1,107 @@
 require 'rails_helper'
 
 RSpec.describe UspsConfirmationUploader do
-  subject(:uploader) { described_class.new }
+  let(:uploader) { described_class.new }
 
-  describe '#run' do
-    subject(:run) { uploader.run }
+  let(:export) { 'foo|bar' }
+  let(:confirmations) do
+    [
+      UspsConfirmation.create!(
+        entry: {
+          first_name: 'John',
+          last_name: 'Johnson',
+          address1: '123 Sesame St',
+          address2: '',
+          city: 'Anytown',
+          state: 'WA',
+          zipcode: '98021',
+          otp: 'ZYX987',
+          issuer: '',
+        }
+      ),
+    ]
+  end
+
+  describe '#generate_export' do
+    subject { uploader.send(:generate_export, confirmations) }
+
+    it 'generates an export using the UspsConfirmationExporter and all current UspsConfirmations' do
+      expect(subject).to eq(UspsConfirmationExporter.new(confirmations).run)
+    end
+  end
+
+  describe '#clear_confirmations' do
+    subject { uploader.send(:clear_confirmations, confirmations) }
+
+    it 'deletes the provided confirmations' do
+      exist = ->(confirmation) { UspsConfirmation.exists?(confirmation.id) }
+
+      expect(confirmations.all?(&exist)).to eq(true)
+
+      subject
+
+      expect(confirmations.none?(&exist)).to eq(true)
+    end
+  end
+
+  describe '#upload_export' do
+    subject { uploader.send(:upload_export, export) }
 
     let(:sftp_connection) { instance_double('Net::SFTP::Session') }
+    let(:File) { class_double('File') }
+    let(:file) { instance_double('File') }
 
-    before do
-      sftp_options = [
-        Figaro.env.usps_upload_sftp_host,
-        Figaro.env.usps_upload_sftp_username,
-        { password: Figaro.env.usps_upload_sftp_password },
-      ]
-      expect(Net::SFTP).to receive(:start).
-        with(*sftp_options).and_yield(sftp_connection)
+    it 'uploads the export via sftp' do
+      expect(Net::SFTP).to receive(:start).with(*sftp_options).and_yield(sftp_connection)
+      expect(sftp_connection).to receive(:file).and_return(File)
+      expect(File).to receive(:open).with(upload_folder, write_permission).and_yield(file)
+      expect(file).to receive(:write).with(export)
+
+      subject
+    end
+  end
+
+  describe '#run' do
+    subject { uploader.run }
+
+    context 'when successful' do
+      it 'uploads the psv created by creates a file, uploads it via SFTP, and deletes it after' do
+        expect(uploader).to receive(:generate_export).with(confirmations).and_return(export)
+        expect(uploader).to receive(:upload_export).with(export)
+        expect(uploader).to receive(:clear_confirmations).with(confirmations)
+
+        subject
+      end
     end
 
-    it 'creates a file, uploads it via SFTP, and deletes it after' do
-      expect(sftp_connection).to receive(:upload!).
-        with(uploader.local_path.to_s, upload_folder)
+    context 'when there is an error' do
+      it 'notifies NewRelic and does not clear confirmations if SFTP fails' do
+        expect(uploader).to receive(:generate_export).with(confirmations).and_return(export)
+        expect(uploader).to receive(:upload_export).with(export).and_raise(StandardError)
+        expect(uploader).not_to receive(:clear_confirmations)
 
-      run
+        expect(NewRelic::Agent).to receive(:notice_error)
 
-      expect(File.exist?(uploader.local_path)).to eq(false)
+        expect { subject }.to_not raise_error
+
+        expect(UspsConfirmation.count).to eq 1
+      end
     end
+  end
 
-    it 'notifies NewRelic and does not delete the file if SFTP fails' do
-      expect(sftp_connection).to receive(:upload!).and_raise(StandardError)
-      expect(NewRelic::Agent).to receive(:notice_error)
-
-      expect { run }.to_not raise_error
-
-      expect(File.exist?(uploader.local_path)).to eq(true)
-    end
-
-    it 'clears entries after uploading file' do
-      expect(UspsConfirmation.count).to eq 1
-
-      subject.run
-
-      expect(UspsConfirmation.count).to eq 0
-    end
+  def sftp_options
+    [
+      Figaro.env.usps_upload_sftp_host,
+      Figaro.env.usps_upload_sftp_username,
+      { password: Figaro.env.usps_upload_sftp_password },
+    ]
   end
 
   def upload_folder
     File.join(Figaro.env.usps_upload_sftp_directory, 'batch.psv')
   end
+
+  def write_permission
+    'w'
+  end
 end
-
-
-  # let(:otp) { 'ABC123' }
-  # let(:pii_attributes) do
-  #   Pii::Attributes.new_from_hash(
-  #     first_name: 'Söme',
-  #     last_name: 'Öne',
-  #     address1: '123 Añy St',
-  #     address2: 'Sté 123',
-  #     city: 'Sömewhere',
-  #     state: 'KS',
-  #     zipcode: '66666-1234'
-  #   )
-  # end
-
-  # let(:psv_rows) do
-  #   now = Time.zone.now
-  #   due = now + described_class::OTP_MAX_VALID_DAYS.days
-  #   current_date = now.strftime('%-B %-e')
-  #   due_date = due.strftime('%-B %-e')
-  #   values = [
-  #     described_class::CONTENT_ROW_ID,
-  #     pii_attributes.first_name + ' ' + pii_attributes.last_name,
-  #     pii_attributes.address1,
-  #     pii_attributes.address2,
-  #     pii_attributes.city,
-  #     pii_attributes.state,
-  #     pii_attributes.zipcode,
-  #     otp,
-  #     "#{current_date}, #{now.year}",
-  #     "#{due_date}, #{due.year}",
-  #     service_provider.friendly_name,
-  #     "https://#{Figaro.env.domain_name}",
-  #   ]
-  #   values.join(DELIMITER)
-  # end
-
-
-    # before do
-    #   confirmation_maker = UspsConfirmationMaker.new(
-    #     pii: pii_attributes,
-    #     issuer: service_provider.issuer,
-    #     profile: build(:profile)
-    #   )
-
-    #   allow(confirmation_maker).to receive(:otp).and_return(otp)
-
-    #   confirmation_maker.perform
-    # end
