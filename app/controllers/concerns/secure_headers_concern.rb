84a9f2db08343880a7d78b380adaf224e829e876
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-08-02 09:40:56 -0400

    Revert "Merge pull request #2351 from 18F/mb-refactor-redirect-uri-validation"
    This reverts commit 1087dd8416dc37198250280391e6e2fc982f97ac, reversing
    changes made to bd698faac2c3b17f78a214d682ea368c42795d73.

--- app/controllers/concerns/secure_headers_concern.rb
+++ app/controllers/concerns/secure_headers_concern.rb
@@ -5,19 +5,16 @@ module SecureHeadersConcern
     return if stored_url_for_user.blank?
 
     authorize_params = URIService.params(stored_url_for_user)
+
     authorize_form = OpenidConnectAuthorizeForm.new(authorize_params)
 
     return unless authorize_form.valid?
 
-    redirect_uri = authorize_params[:redirect_uri]
-
     override_content_security_policy_directives(
-      form_action: ["'self'", redirect_uri].compact,
+      form_action: ["'self'", authorize_form.sp_redirect_uri].compact,
       preserve_schemes: true
     )
   end
-
-  private
 
   def stored_url_for_user
     sp_session[:request_url]
