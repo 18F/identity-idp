bc63c291463d5f90c77d624a5196c3ab648b46ca
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-08-21 00:18:00 -0400

    Refactor and fix account reset requests
    **Why**:
    - We value lean controllers with minimal business logic
    - To fix a bug where users without a phone would see a message that an
    SMS was sent to their phone
    - To fix a bug where users who had not yet set up 2FA were redirected to
    the phone setup page instead of the 2FA setup page
    - To fix the French translation for the SMS message
    - To capture analytics about what kind of 2FA options the user has
    configured, to see if phone users request account deletion more than
    TOTP users, for example.
    - To capture analytics for visiting the account deletion page

---
+++ app/services/account_reset/create_request.rb
@@ -0,0 +1,41 @@
+module AccountReset
+  class CreateRequest
+    def initialize(user)
+      @user = user
+    end
+
+    def call
+      create_request
+      notify_user_by_email
+      notify_user_by_sms_if_applicable
+    end
+
+    private
+
+    attr_reader :user
+
+    def create_request
+      request = AccountResetRequest.find_or_create_by(user: user)
+      request.update!(
+        request_token: SecureRandom.uuid,
+        requested_at: Time.zone.now,
+        cancelled_at: nil,
+        granted_at: nil,
+        granted_token: nil
+      )
+    end
+
+    def notify_user_by_email
+      UserMailer.account_reset_request(user).deliver_later
+    end
+
+    def notify_user_by_sms_if_applicable
+      phone = user.phone
+      return unless phone
+      SmsAccountResetNotifierJob.perform_now(
+        phone: phone,
+        token: user.account_reset_request.request_token
+      )
+    end
+  end
+end
