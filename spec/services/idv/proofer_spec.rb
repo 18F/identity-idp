ac1edabce5a4d4776bf5b3255200d1fd3bdd39d1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-08-21 15:42:01 -0500

    Remove proofer initializer
    **Why**: Autoloader was reloading the `Idv::Proofer` class when there
    were changes to the app. As a result, the configurations set in the
    initializer were being wiped out since the initializer was not re-run
    when the class was autoloaded.
    
    Since the configurations in the proofer class that were being set are
    all around fetching vendors, they could be replaced with code that
    lazily instantiates the list of vendors. That is what this PR does.
    
    This PR also has some code to do some refactoring and simplify some of
    the logic.

--- spec/services/idv/proofer_spec.rb
+++ spec/services/idv/proofer_spec.rb
@@ -1,20 +1,54 @@
 require 'rails_helper'
 
 describe Idv::Proofer do
-  describe '.attribute?' do
-    subject { described_class.attribute?(attribute) }
+  let(:resolution_dummy) do
+    class_double('Proofer::Base', vendor_name: 'dummy:resolution', stage: :resolution)
+  end
+  let(:state_id_dummy) do
+    class_double('Proofer::Base', vendor_name: 'dummy:state_id', stage: :state_id)
+  end
+  let(:address_dummy) do
+    class_double('Proofer::Base', vendor_name: 'dummy:address', stage: :address)
+  end
+  let(:dummy_vendors) { [resolution_dummy, state_id_dummy, address_dummy] }
 
+  let(:proofer_vendors) { '["dummy:resolution", "dummy:state_id", "dummy:address"]' }
+  let(:proofer_mock_fallback) { 'false' }
+
+  before do
+    allow(Figaro.env).to receive(:proofer_vendors).
+      and_return(proofer_vendors)
+    allow(Figaro.env).to receive(:proofer_mock_fallback).
+      and_return(proofer_mock_fallback)
+
+    original_descendants = Proofer::Base.descendants
+    allow(Proofer::Base).to receive(:descendants).and_return(
+      original_descendants + dummy_vendors
+    )
+
+    subject.instance_variable_set(:@vendors, nil)
+  end
+
+  after do
+    # This is necessary to prevent mocks created in these examples from leaking
+    # out with the memoized vendors value
+    subject.instance_variable_set(:@vendors, nil)
+  end
+
+  subject { described_class }
+
+  describe '.attribute?' do
     context 'when the attribute exists' do
       context 'and is passed as a string' do
         let(:attribute) { 'last_name' }
 
-        it { is_expected.to eq(true) }
+        it { expect(subject.attribute?(attribute)).to eq(true) }
       end
 
       context 'and is passed as a symbol' do
         let(:attribute) { :last_name }
 
-        it { is_expected.to eq(true) }
+        it { expect(subject.attribute?(attribute)).to eq(true) }
       end
     end
 
@@ -22,241 +56,108 @@ describe Idv::Proofer do
       context 'and is passed as a string' do
         let(:attribute) { 'fooobar' }
 
-        it { is_expected.to eq(false) }
+        it { expect(subject.attribute?(attribute)).to eq(false) }
       end
 
       context 'and is passed as a symbol' do
         let(:attribute) { :fooobar }
 
-        it { is_expected.to eq(false) }
+        it { expect(subject.attribute?(attribute)).to eq(false) }
       end
     end
   end
 
-  describe '.loaded_vendors' do
-    subject { described_class.send(:loaded_vendors) }
+  describe '.get_vendor' do
+    context 'with mock proofers enabled' do
+      let(:proofer_mock_fallback) { 'true' }
 
-    it 'returns all of the subclasses of Proofer::Base' do
-      subclasses = ['foo']
-      expect(::Proofer::Base).to receive(:descendants).and_return(subclasses)
-      expect(subject).to eq(subclasses)
-    end
-  end
-
-  describe '.available_vendors' do
-    subject { described_class.send(:available_vendors, configured_vendors, vendors) }
-
-    let(:vendors) do
-      [
-        class_double('Proofer::Base', vendor_name: 'foo'),
-        class_double('Proofer::Base', vendor_name: 'baz'),
-      ]
-    end
-
-    let(:configured_vendors) { %w[foo bar] }
-
-    it 'selects only the vendors that have been configured' do
-      available_vendors = [vendors.first]
-      expect(subject).to eq(available_vendors)
-    end
-  end
-
-  describe '.require_mock_vendors' do
-    subject { described_class.send(:require_mock_vendors) }
-
-    it 'requires all of the mock vendors' do
-      Dir[Rails.root.join('lib', 'proofer_mocks', '*')].each do |file|
-        expect_any_instance_of(Object).to receive(:require).with(file)
-      end
-
-      subject
-    end
-  end
-
-  describe '.assign_vendors' do
-    subject { described_class.send(:assign_vendors, stages, external_vendors, mock_vendors) }
-
-    let(:stages) { %i[resolution state_id address] }
-
-    let(:external_vendors) do
-      [
-        class_double('Proofer::Base', stage: :resolution),
-        class_double('Proofer::Base', stage: :foo),
-      ]
-    end
-
-    let(:mock_vendors) do
-      [
-        class_double('Proofer::Base', stage: :resolution),
-        class_double('Proofer::Base', stage: 'state_id'),
-        class_double('Proofer::Base', stage: :baz),
-      ]
-    end
-
-    it 'maps stages to vendors, falling back to mock vendors' do
-      assigned_vendors = {
-        resolution: external_vendors.first,
-        state_id: mock_vendors.second,
-      }
-      expect(subject).to eq(assigned_vendors)
-    end
-  end
-
-  describe '.stage_vendor' do
-    subject { described_class.send(:stage_vendor, stage, vendors) }
-
-    let(:stage) { :foo }
-
-    context 'when stage is a string' do
-      let(:vendors) do
-        [
-          class_double('Proofer::Base', stage: :resolution),
-          class_double('Proofer::Base', stage: 'foo'),
-        ]
-      end
-
-      it 'selects the vendor for the stage' do
-        expect(subject).to eq(vendors.second)
-      end
-    end
-
-    context 'when stage is a symbol' do
-      let(:vendors) do
-        [
-          class_double('Proofer::Base', stage: :resolution),
-          class_double('Proofer::Base', stage: :foo),
-        ]
-      end
-
-      it 'selects the vendor for the stage' do
-        expect(subject).to eq(vendors.second)
-      end
-    end
-
-    context 'when no vendor exists' do
-      let(:vendors) do
-        [
-          class_double('Proofer::Base', stage: :resolution),
-        ]
-      end
-
-      it 'is nil' do
-        expect(subject).to be_nil
-      end
-    end
-  end
-
-  describe '.validate_vendors' do
-    subject { described_class.send(:validate_vendors, stages, vendors) }
-
-    let(:stages) { %i[foo] }
-
-    context 'when there are vendors for all stages' do
-      let(:vendors) { { foo: class_double('Proofer::Base') } }
-
-      it 'does not raise an error' do
-        expect { subject }.not_to raise_error
-      end
-    end
-
-    context 'when there are stages without vendors' do
-      let(:vendors) { { bar: class_double('Proofer::Base') } }
-
-      it 'does raises an error' do
-        expect { subject }.to raise_error('No proofer vendor configured for stage(s): foo')
-      end
-    end
-  end
-
-  describe '.configure_vendors' do
-    subject { described_class.configure_vendors(stages, config) }
-
-    let(:stages) { %i[foo] }
-
-    let(:config) { double }
-
-    let(:configured_vendors) { %w[vendor1 vendor2] }
-
-    let(:loaded_vendors) do
-      [
-        class_double('Proofer::Base', stage: :foo, vendor_name: 'vendor3'),
-        class_double('Proofer::Base', stage: :foo, vendor_name: 'vendor1'),
-        class_double('Proofer::Base', stage: :bar, vendor_name: 'vendor2'),
-      ]
-    end
-
-    let(:mock_vendors) do
-      [
-        class_double('Proofer::Base', stage: :foo),
-        class_double('Proofer::Base', stage: :baz),
-      ]
-    end
-
-    before do
-      expect(config).to receive(:vendors).and_return(configured_vendors)
-    end
-
-    context 'default configuration' do
-      before do
-        expect(config).to receive(:mock_fallback).and_return(false)
-        expect(config).to receive(:raise_on_missing_proofers).and_return(true)
-        expect(described_class).
-          to receive(:loaded_vendors).and_return(loaded_vendors, loaded_vendors)
-      end
-
-      context 'when a stage is missing an external vendor' do
-        let(:stages) { %i[foo baz] }
-
-        it 'raises' do
-          expect { subject }.to raise_error('No proofer vendor configured for stage(s): baz')
+      context 'with a vendor configured for the state' do
+        it 'returns the vendor' do
+          expect(subject.get_vendor(:resolution)).to eq(resolution_dummy)
+          expect(subject.get_vendor(:state_id)).to eq(state_id_dummy)
+          expect(subject.get_vendor(:address)).to eq(address_dummy)
         end
       end
 
-      context 'when all stages have vendors' do
-        it 'maps the vendors, ignoring non-configured ones' do
-          expect(subject).to eq(foo: loaded_vendors.second)
+      context 'without a vendor configured for the state' do
+        let(:proofer_vendors) { '["dummy:state_id"]' }
+
+        it 'returns a mock vendor' do
+          expect(subject.get_vendor(:resolution)).to eq(ResolutionMock)
+          expect(subject.get_vendor(:state_id)).to eq(state_id_dummy)
+          expect(subject.get_vendor(:address)).to eq(AddressMock)
+        end
+      end
+
+      context 'without a proofer vendor configuration' do
+        let(:proofer_vendors) { nil }
+
+        it 'returns all mock proofers' do
+          expect(subject.get_vendor(:resolution)).to eq(ResolutionMock)
+          expect(subject.get_vendor(:state_id)).to eq(StateIdMock)
+          expect(subject.get_vendor(:address)).to eq(AddressMock)
         end
       end
     end
 
-    context 'when mock_fallback is enabled' do
-      before do
-        expect(config).to receive(:mock_fallback).and_return(true)
-        expect(config).to receive(:raise_on_missing_proofers).and_return(true)
-        expect(described_class).to receive(:loaded_vendors).and_return(loaded_vendors, mock_vendors)
+    context 'without mock proofers enabled' do
+      let(:proofer_mock_fallback) { 'false' }
+
+      context 'with a vendor configured for the state' do
+        it 'returns the vendor' do
+          expect(subject.get_vendor(:resolution)).to eq(resolution_dummy)
+          expect(subject.get_vendor(:state_id)).to eq(state_id_dummy)
+          expect(subject.get_vendor(:address)).to eq(address_dummy)
+        end
       end
 
-      context 'when a stage is missing an external vendor' do
-        let(:stages) { %i[foo baz] }
+      context 'without a vendor configured for the state' do
+        let(:proofer_vendors) { '["dummy:state_id"]' }
 
-        it 'does not raise' do
-          expect { subject }.not_to raise_error
+        it 'returns nil' do
+          expect(subject.get_vendor(:resolution)).to eq(nil)
+          expect(subject.get_vendor(:state_id)).to eq(state_id_dummy)
+          expect(subject.get_vendor(:address)).to eq(nil)
         end
+      end
 
-        it 'returns the mapped vendors with the mock fallback' do
-          expect(subject).to eq(foo: loaded_vendors.second, baz: mock_vendors.second)
+      context 'without a proofer vendor configuration' do
+        let(:proofer_vendors) { nil }
+
+        it 'returns nil' do
+          expect(subject.get_vendor(:resolution)).to eq(nil)
+          expect(subject.get_vendor(:state_id)).to eq(nil)
+          expect(subject.get_vendor(:address)).to eq(nil)
         end
       end
     end
+  end
 
-    context 'when raise_on_missing_proofers is disabled' do
-      before do
-        expect(config).to receive(:mock_fallback).and_return(false)
-        expect(config).to receive(:raise_on_missing_proofers).and_return(false)
-        expect(described_class).
-          to receive(:loaded_vendors).and_return(loaded_vendors, loaded_vendors)
+  describe '.validate_vendors!' do
+    let(:proofer_mock_fallback) { 'false' }
+
+    context 'with vendors configured for each stage' do
+      it 'does not raise' do
+        expect { described_class.validate_vendors! }.to_not raise_error
       end
+    end
 
-      context 'when a stage is missing an external vendor' do
-        let(:stages) { %i[foo baz] }
+    context 'without vendors configured for each stage' do
+      let(:proofer_vendors) { '["dummy:state_id"]' }
 
-        it 'does not raise' do
-          expect { subject }.not_to raise_error
-        end
+      it 'does raise' do
+        expect { described_class.validate_vendors! }.to raise_error(
+          RuntimeError, 'No proofer vendor configured for stage(s): resolution, address'
+        )
+      end
+    end
 
-        it 'returns the mapped vendors missing the stage' do
-          expect(subject).to eq(foo: loaded_vendors.second)
-        end
+    context 'without vendors configured but with mock vendors enabled' do
+      let(:proofer_vendors) { '["dummy:state_id"]' }
+      let(:proofer_mock_fallback) { 'true' }
+
+      it 'does not raise' do
+        expect { described_class.validate_vendors! }.to_not raise_error
       end
     end
   end
