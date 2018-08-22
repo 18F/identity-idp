949a5d8a9c809a8eed6fdb41468f557ed5607aef
James Smith <jamesg.smith@gsa.gov>
2018-08-22 12:11:55 -0400

    add stats output and sleep

--- app/services/populate_phone_configurations_table.rb
+++ app/services/populate_phone_configurations_table.rb
@@ -1,9 +1,19 @@
 class PopulatePhoneConfigurationsTable
+  def initialize
+    @count = 0
+    @total = 0
+  end
+
   def call
     # we don't have a uniqueness constraint in the database to let us blindly insert
     # everything in a single SQL statement. So we have to load by batches and copy
     # over. Much slower, but doesn't duplicate information.
-    User.in_batches(of: 1000) { |relation| process_batch(relation) }
+    User.in_batches(of: 1000) do |relation| 
+      sleep(1)
+      process_batch(relation)
+      puts "#{@count} / #{@total}"
+    end
+    puts "Processed #{@count} user phone configurations"
   end
 
   private
@@ -12,8 +22,10 @@ class PopulatePhoneConfigurationsTable
   def process_batch(relation)
     User.transaction do
       relation.each do |user|
+        @total += 1
         next if user.phone_configuration.present? || user.encrypted_phone.blank?
         user.create_phone_configuration(phone_info_for_user(user))
+        @count += 1
       end
     end
   end
