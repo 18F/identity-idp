b96f312ebfbb5857de79c9445ce2520a39e42fa0
Gregory John Casamento <gregory.casamento@gsa.gov>
2018-08-29 17:00:42 -0400

    LG-532: Add new column and index. **Why**: Added index and column to improve performance of lookup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180824155031) do
+ActiveRecord::Schema.define(version: 20180829205032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,8 @@ ActiveRecord::Schema.define(version: 20180824155031) do
     t.integer "used", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "when_user"
+    t.index ["user_id"], name: "index_recovery_codes_on_user_id"
   end
 
   create_table "remote_settings", force: :cascade do |t|
