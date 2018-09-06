c18c1db5c410ef83a7c1914d557836b19e94cc47
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-09-06 13:03:22 -0400

    Removed algorithm

--- app/javascript/packs/webauthn-setup.js
+++ app/javascript/packs/webauthn-setup.js
@@ -34,10 +34,6 @@ function webauthn() {
           type: 'public-key',
           alg: -7,
         },
-        {
-          type: 'public-key',
-          alg: -257,
-        },
       ],
       timeout: 800000,
       attestation: 'direct',
