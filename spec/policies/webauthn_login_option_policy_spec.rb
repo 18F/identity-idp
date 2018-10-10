c0955c12de1c3b128d1a4fd09181d21c9798162d
Steve Urciuoli <gsa.steve.urciuoli@gmail.com>
2018-10-10 13:53:53 -0400

    LG-709 LG-710 LG-731 Enable webauthn for all users (#2585)
    **Why**: To support authentication with security keys
    
    **How**: Revert code that limited webauthn to no SP

--- spec/policies/webauthn_login_option_policy_spec.rb
+++ spec/policies/webauthn_login_option_policy_spec.rb
@@ -3,46 +3,23 @@ require 'rails_helper'
 describe TwoFactorAuthentication::WebauthnPolicy do
   include WebauthnVerificationHelper
 
+  let(:subject) { described_class.new(user) }
+
   describe '#configured?' do
-    context 'with no sp' do
-      let(:subject) { described_class.new(user, nil) }
+    context 'without a webauthn configured' do
+      let(:user) { build(:user) }
 
-      context 'without a webauthn configured' do
-        let(:user) { build(:user) }
-
-        it { expect(subject.configured?).to be_falsey }
-      end
-
-      context 'with a webauthn configured' do
-        let(:user) { create(:user) }
-        before do
-          create_webauthn_configuration(user)
-        end
-
-        it 'returns a truthy value' do
-          expect(subject.configured?).to be_truthy
-        end
-      end
+      it { expect(subject.configured?).to be_falsey }
     end
 
-    context 'with an sp' do
-      let(:subject) { described_class.new(user, 'foo') }
-
-      context 'without a webauthn configured' do
-        let(:user) { build(:user) }
-
-        it { expect(subject.configured?).to be_falsey }
+    context 'with a webauthn configured' do
+      let(:user) { create(:user) }
+      before do
+        create_webauthn_configuration(user)
       end
 
-      context 'with a webauthn configured' do
-        let(:user) { create(:user) }
-        before do
-          create_webauthn_configuration(user)
-        end
-
-        it 'returns a truthy value' do
-          expect(subject.configured?).to be_falsey
-        end
+      it 'returns a truthy value' do
+        expect(subject.configured?).to be_truthy
       end
     end
   end
