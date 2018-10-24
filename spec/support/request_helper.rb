ed72ba738652e55f67f5d9cb6d409b9344d831a9
Jonathan Hooper <jon9820@gmail.com>
2018-10-24 12:11:29 -0500

    Namespace platform authenticator params in analytics controller (#2622)
    **Why**: The analytics controller is meant to function as an general
    controller for capture anayltics for the frontend. Prior to this commit
    it was primarily handling the platform authenticator analytics event.
    
    **How**: This commit makes the controller more general so that it communicates
    that it should be built out to handle more events and moves the platform
    auth event into a namespace so that the API can be extended to include
    more events. Additionally, care was given to make sure that the current
    implementation is supported so that we don't drop events while running
    instances running this code alongside instances running the new code.

---
+++ spec/support/request_helper.rb
@@ -0,0 +1,20 @@
+module RequestHelper
+  VALID_PASSWORD = 'Val!d Pass w0rd'.freeze
+
+  def user_with_2fa
+    create(:user, :signed_up, with: { phone: '+1 202-555-1212' }, password: VALID_PASSWORD)
+  end
+
+  def sign_in_user(user = user_with_2fa)
+    post new_user_session_path, params: { user: { email: user.email, password: user.password } }
+    get otp_send_path, params: { otp_delivery_selection_form: { otp_delivery_preference: 'sms' } }
+    follow_redirect!
+    post login_two_factor_path, params: {
+      otp_delivery_preference: 'sms', code: user.reload.direct_otp
+    }
+  end
+end
+
+RSpec.configure do |config|
+  config.include RequestHelper, type: :request
+end
