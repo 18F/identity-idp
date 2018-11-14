3425d752d5a6d27a4c5d7fe713c7bff2fec0e417
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-11-13 21:48:27 -0500

    Store GeoLite DB in S3 and pull it down on deploy

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
 Geocoder.configure(
   ip_lookup: :geoip2,
   geoip2: {
-    file: Rails.root.join('data', 'GeoLite2-City_20181106', 'GeoLite2-City.mmdb'),
+    file: Rails.root.join('data', 'GeoLite2-City.mmdb'),
   }
 )
