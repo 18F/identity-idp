7e1d39b40a401c7683522e229cc3adf168894bc8
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-11-25 19:38:44 -0500

    Fix spec 3

--- app/presenters/saml_request_presenter.rb
+++ app/presenters/saml_request_presenter.rb
@@ -37,8 +37,10 @@ class SamlRequestPresenter
   end
 
   def bundle
+    attribute_bundle = service_provider.attribute_bundle
     @_bundle ||= (
-      (authn_request_bundle & service_provider.attribute_bundle) || []
+      (authn_request_bundle & attribute_bundle) ||
+      attribute_bundle || []
     ).map(&:to_sym)
   end
 
