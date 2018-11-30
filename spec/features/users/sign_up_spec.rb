ccadc628bc6080dd27726dc74d53aedce06931f5
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-11-30 07:15:05 -0500

    Fix specs 3

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -64,39 +64,6 @@ feature 'Sign Up' do
   end
 
   context 'with js', js: true do
-    context 'sp loa1' do
-      it 'allows the user to toggle the modal' do
-        begin_sign_up_with_sp_and_loa(loa3: false)
-        expect(page).not_to have_xpath("//div[@id='cancel-action-modal']")
-
-        click_on t('links.cancel')
-        expect(page).to have_xpath("//div[@id='cancel-action-modal']")
-
-        click_on t('sign_up.buttons.continue')
-        expect(page).not_to have_xpath("//div[@id='cancel-action-modal']")
-      end
-
-      it 'allows the user to delete their account and returns them to the branded start page' do
-        user = begin_sign_up_with_sp_and_loa(loa3: false)
-
-        click_on t('links.cancel')
-        click_on t('sign_up.buttons.cancel')
-
-        expect(page).to have_current_path(sign_up_start_path(request_id: '123'))
-        expect { User.find(user.id) }.to raise_error ActiveRecord::RecordNotFound
-      end
-    end
-
-    context 'sp loa3' do
-      it 'behaves like loa1 when user has not finished sign up' do
-        begin_sign_up_with_sp_and_loa(loa3: true)
-
-        click_on t('links.cancel')
-
-        expect(page).to have_xpath("//input[@value=\"#{t('sign_up.buttons.cancel')}\"]")
-      end
-    end
-
     context 'user enters their email as their password', email: true do
       it 'treats it as a weak password' do
         email = 'test@test.com'
