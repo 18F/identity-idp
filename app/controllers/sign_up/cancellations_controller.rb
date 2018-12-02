14f989d48c116fa1589193f3c9d32565cd5c6bec
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-01 23:13:23 -0500

    Ensure setup

--- app/controllers/sign_up/cancellations_controller.rb
+++ app/controllers/sign_up/cancellations_controller.rb
@@ -1,6 +1,6 @@
 module SignUp
   class CancellationsController < ApplicationController
-    before_action :confirm_sign_up
+    before_action :ensure_in_setup
 
     def new
       properties = ParseControllerFromReferer.new(request.referer).call
@@ -10,8 +10,12 @@ module SignUp
 
     private
 
-    def confirm_sign_up
-      redirect_to root_url unless session[:user_confirmation_token] || current_user
+    def ensure_in_setup
+      redirect_to root_url if !session[:user_confirmation_token] && two_factor_enabled
+    end
+
+    def two_factor_enabled
+      current_user && MfaPolicy.new(current_user).two_factor_enabled?
     end
   end
 end
