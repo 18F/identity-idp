8ccd009c8f6e0e24b028aa6056c05c9bd115236a
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-02 00:48:27 -0500

    Simulate setup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,6 +11,7 @@ describe UsersController do
 
     it 'destroys the current user and redirects to sign in page, with a helpful flash message' do
       sign_in_as_user
+      subject.session[:user_confirmation_token] = '1'
 
       expect { delete :destroy }.to change(User, :count).by(-1)
       expect(response).to redirect_to(root_url)
