f9e1df19606e47c63a830ad8e6174e8ac0c95478
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-06 12:35:46 -0500

    Added more coverage

--- spec/services/undeliverable_address_notifier_spec.rb
+++ spec/services/undeliverable_address_notifier_spec.rb
@@ -39,6 +39,21 @@ RSpec.describe UndeliverableAddressNotifier do
     expect(notifications_sent).to eq(0)
   end
 
+  describe '#download_file' do
+    let(:sftp_connection) { instance_double('Net::SFTP::Session') }
+    before do
+      allow(Net::SFTP).to receive(:start).and_yield(sftp_connection)
+      allow(sftp_connection).to receive(:download!)
+    end
+
+    it 'downloads the file via sftp' do
+      expect(Net::SFTP).to receive(:start).with(*sftp_options).and_yield(sftp_connection)
+      expect(sftp_connection).to receive(:download!)
+
+      subject.send(:download_file)
+    end
+  end
+
   def create_test_data
     process_file_and_send_notifications
   end
@@ -52,5 +67,22 @@ RSpec.describe UndeliverableAddressNotifier do
     end
     allow_any_instance_of(UndeliverableAddressNotifier).to receive(:download_file).
       and_return(temp_file)
+  end
+
+  def download_folder
+    File.join(Figaro.env.usps_download_sftp_directory, 'batch.psv')
+  end
+
+  def sftp_options
+    [
+      env.usps_download_sftp_host,
+      env.usps_download_sftp_username,
+      password: env.usps_download_sftp_password,
+      timeout: env.usps_download_sftp_timeout.to_i,
+    ]
+  end
+
+  def env
+    Figaro.env
   end
 end
