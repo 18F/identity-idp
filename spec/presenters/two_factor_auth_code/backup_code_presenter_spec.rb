31f0ab4e765f37c0f6b3c0c785563f6a848896fc
Gregory John Casamento <gregory.casamento@gsa.gov>
2018-12-06 13:00:54 -0500

    Fix tests

---
+++ spec/presenters/two_factor_auth_code/backup_code_presenter_spec.rb
@@ -0,0 +1,47 @@
+require 'rspec'
+
+describe 'BackupCodePresenter' do
+  let(:presenter) do
+    BackupCodePresenter.new
+  end
+
+  describe '#help_text'
+  it 'should return help text' do
+    expect(presenter.help_text).to eq ''
+  end
+
+  describe '#fallback_question' do
+    it 'returns the fallback question' do
+      expect(presenter.fallback_question).to eq \
+        t('two_factor_authentication.backup_code_fallback.question')
+    end
+  end
+end
+
+
+#module TwoFactorAuthCode
+#  class BackupCodePresenter < TwoFactorAuthCode::GenericDeliveryPresenter
+#    include Rails.application.routes.url_helpers
+#    include ActionView::Helpers::TranslationHelper
+#
+#    attr_reader :credential_ids
+#
+#    def help_text
+#      ''
+#    end
+#
+#    def cancel_link
+#      locale = LinkLocaleResolver.locale
+#      if reauthn
+#        account_path(locale: locale)
+#      else
+#        sign_out_path(locale: locale)
+#      end
+#    end
+#
+#    def fallback_question
+#      t('two_factor_authentication.backup_code_fallback.question')
+#    end
+#  end
+#end
+
