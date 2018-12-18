40f1a20a099ed01ee02e1121bcd44141f84ddab0
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-18 16:25:44 -0500

    Added specs for backup_code_policy configured?

--- spec/policies/backup_code_policy_spec.rb
+++ spec/policies/backup_code_policy_spec.rb
@@ -5,8 +5,22 @@ describe TwoFactorAuthentication::BackupCodePolicy do
   let(:policy) { described_class.new(user) }
 
   describe '#configured?' do
-    it 'is set to false' do
+    it 'returns false if there are no codes' do
       expect(policy.configured?).to eq false
+    end
+
+    it 'returns false if all the backup codes are used' do
+      user.save
+      user.backup_code_configurations.create!(code: 'foo', used: true)
+
+      expect(policy.configured?).to eq false
+    end
+
+    it 'returns true if there are usable codes' do
+      user.save
+      user.backup_code_configurations.create!(code: 'foo')
+
+      expect(policy.configured?).to eq true
     end
   end
 
