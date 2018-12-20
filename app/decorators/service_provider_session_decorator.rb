7bcfff53c54635c569bd6350a3dde3ccbd70b9c4
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-20 13:17:51 -0500

    Split flg app into staging and prod

--- app/decorators/service_provider_session_decorator.rb
+++ app/decorators/service_provider_session_decorator.rb
@@ -4,7 +4,7 @@ class ServiceProviderSessionDecorator
   DEFAULT_LOGO = 'generic.svg'.freeze
   CUSTOM_ALERT_SP_NAMES = ['CBP Trusted Traveler Programs'].freeze
   DEFAULT_ALERT_SP_NAMES = ['USAJOBS', 'SAM', 'HOMES.mil', 'HOMES.mil - test', 'Rule 19d-1',
-                            'Foreign Labor Gateway'].freeze
+                            'Foreign Labor Gateway', 'Foreign Labor Gateway - Staging'].freeze
 
   # These are SPs that are migrating users and require special help messages
   CUSTOM_SP_ALERTS = {
