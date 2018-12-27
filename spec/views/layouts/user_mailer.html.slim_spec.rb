d7191d584cd7056aead2a7c1bc6d06572d2ac981
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-27 10:23:33 -0500

    Fix spec

--- spec/views/layouts/user_mailer.html.slim_spec.rb
+++ spec/views/layouts/user_mailer.html.slim_spec.rb
@@ -28,9 +28,9 @@ describe 'layouts/user_mailer.html.slim' do
   it 'includes the support text and link' do
     expect(rendered).to have_content(t('mailer.no_reply'))
     expect(rendered).to have_content(
-      t('mailer.help', app: APP_NAME, link: MarketingSite.help_url)
+      t('mailer.help', app: APP_NAME, link: MarketingSite.nice_help_url)
     )
-    expect(rendered).to have_link(MarketingSite.help_url, href: MarketingSite.help_url)
+    expect(rendered).to have_link(MarketingSite.nice_help_url, href: MarketingSite.help_url)
   end
 
   it 'includes link to About login.gov' do
