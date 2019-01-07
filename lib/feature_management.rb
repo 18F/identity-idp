e0e9ebff0919027668a04711f3c049c7bef602ba
James G Smith <james_g_smith@omb.eop.gov>
2019-01-07 09:24:39 -0500

    [LG-860] Allow piv/cac based on email (#2710)
    **Why**: We are confident that for folk registering with an
    email in a set of known domains, we can support their PIV/CAC.
    
    **How**: Rather than check for SP affiliation, just check the
    domain of their registered email address.

--- lib/feature_management.rb
+++ lib/feature_management.rb
@@ -20,6 +20,10 @@ class FeatureManagement
       !env.piv_cac_verify_token_url
   end
 
+  def self.allow_piv_cac_by_email_only?
+    Figaro.env.allow_piv_cac_by_email_only == 'true'
+  end
+
   def self.development_and_identity_pki_disabled?
     # This controls if we try to hop over to identity-pki or just throw up
     # a screen asking for a Subject or one of a list of error conditions.
