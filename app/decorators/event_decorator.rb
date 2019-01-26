f7f4b13f07e49a12d2951754b15b666e3e3cc900
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-01-26 09:54:00 -0500

    Linting

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -16,12 +16,12 @@ EventDecorator = Struct.new(:event) do
   end
 
   def last_sign_in_location_and_ip
-    return '' unless event&.ip
+    return '' unless event&.respond_to?(:ip)
     I18n.t('account.index.sign_in_location_and_ip', location: last_location, ip: event.ip)
   end
 
   def last_location
-    return '' unless event&.ip
+    return '' unless event&.respond_to?(:ip)
     IpGeocoder.new(event.ip).location
   end
 end
