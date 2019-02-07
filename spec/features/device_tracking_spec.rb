e0dc9cdd7ca59edb98311c0ae7b67b00ad549d06
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-02-07 12:58:13 -0500

    Use factories in device tracking tests
    **Why**: So that we can consolidate factory logic in the factories. This
    also served as an opportunity to clean up those factories in light of
    changes related to device tracking.

--- spec/features/device_tracking_spec.rb
+++ spec/features/device_tracking_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Device tracking' do
   let(:user) { create(:user, :signed_up) }
   let(:now) { Time.zone.now }
-  let(:device) { create(:device, user_id: user.id, last_ip: '4.3.2.1', last_used_at: now) }
+  let(:device) { create(:device, user: user, last_ip: '4.3.2.1', last_used_at: now) }
 
   before do
-    create(:event, device_id: device.id, ip: '4.3.2.1', user: user, created_at: now)
+    create(:event, device: device, ip: '4.3.2.1', user: user, created_at: now)
     sign_in_and_2fa_user(user)
   end
 
