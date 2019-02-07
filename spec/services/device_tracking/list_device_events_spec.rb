b454c9285a19bbd8954707f621ebeb82ddc6e504
Jonathan Hooper <jon9820@gmail.com>
2019-02-07 16:18:10 -0500

    Use factories in device tracking tests (#2738)
    **Why**: So that we can consolidate factory logic in the factories. This
    also served as an opportunity to clean up those factories in light of
    changes related to device tracking.

--- spec/services/device_tracking/list_device_events_spec.rb
+++ spec/services/device_tracking/list_device_events_spec.rb
@@ -2,53 +2,54 @@ require 'rails_helper'
 
 describe DeviceTracking::ListDeviceEvents do
   subject { described_class }
-  let(:now) { Time.zone.now }
-  let(:user) { create(:user, id: 1) }
-  let(:bad_user_id) { 0 }
-  let(:good_user_id) { 1 }
-  let(:device) { create(:device) }
-  let(:bad_device_id) { 0 }
-  let(:good_device_id) { 1 }
-  let(:event1) { create_event(1, 2) }
-  let(:event2) { create_event(2, 3) }
-  let(:event3) { create_event(3, 1) }
+
+  let(:current_user) { create(:user) }
+  let(:other_user) { create(:user) }
+
+  let(:current_device) { create(:device, user: current_user) }
+  let(:other_device) { create(:device) }
+
+  let(:events) do
+    [
+      create(:event, user: current_user, device: current_device, created_at: 2.hours.ago),
+      create(:event, user: current_user, device: current_device, created_at: 3.hours.ago),
+      create(:event, user: current_user, device: current_device, created_at: 1.hour.ago),
+    ]
+  end
 
   before do
-    user
-    device
-    event1
-    event2
-    event3
+    # Memoize records to run creates for specs that query the table
+    current_user
+    other_user
+    current_device
+    other_device
+    events
   end
 
   it 'returns an empty list if the device is not found' do
-    result = subject.call(good_user_id, bad_device_id, 0, 1)
+    result = subject.call(current_user.id, other_device.id, 0, 1)
 
     expect(result).to eq([])
   end
 
   it 'returns an empty list if the device is found but the user is not' do
-    result = subject.call(bad_user_id, good_device_id, 0, 1)
+    result = subject.call(other_user.id, current_device.id, 0, 1)
 
     expect(result).to eq([])
   end
 
   it 'returns a list containing a single most recent event' do
-    result = subject.call(good_user_id, good_device_id, 0, 1)
+    result = subject.call(current_user.id, current_device.id, 0, 1)
 
     expect(result.size).to eq(1)
-    expect(result[0].id).to eq(3)
+    expect(result[0].id).to eq(events[2].id)
   end
 
   it 'returns a list containing multiple most recent events in order and using an offset' do
-    result = subject.call(good_user_id, good_device_id, 1, 2)
+    result = subject.call(current_user.id, current_device.id, 1, 2)
 
     expect(result.size).to eq(2)
-    expect(result[0].id).to eq(1)
-    expect(result[1].id).to eq(2)
-  end
-
-  def create_event(id, ago)
-    create(:event, id: id, user_id: 1, device_id: 1, ip: '4.3.2.1', created_at: now - ago.hour)
+    expect(result[0].id).to eq(events[0].id)
+    expect(result[1].id).to eq(events[1].id)
   end
 end
--- spec/services/device_tracking/lookup_device_for_user_spec.rb
+++ spec/services/device_tracking/lookup_device_for_user_spec.rb
@@ -2,35 +2,38 @@ require 'rails_helper'
 
 describe DeviceTracking::LookupDeviceForUser do
   subject { described_class }
-  let(:user) { create(:user) }
-  let(:bad_user_id) { 0 }
-  let(:device) { create(:device) }
-  let(:good_uuid) { 'foo' }
-  let(:good_user_id) { 1 }
-  let(:bad_uuid) { 'bar' }
-  let(:bad_user_id) { 2 }
+  let(:current_user) { create(:user) }
+  let(:other_user) { create(:user) }
+
+  let(:device) { create(:device, user: current_user, cookie_uuid: device_uuid) }
+
+  let(:device_uuid) { 'foo' }
+  let(:other_uuid) { 'bar' }
 
   before do
-    user
+    current_user
+    other_user
     device
   end
 
   it 'returns nil if the user is found but the cookie uuid is not' do
-    expect(Device.find_by(user_id: good_user_id)).to be_present
-    result = subject.call(good_user_id, bad_uuid)
+    expect(Device.find_by(user_id: current_user.id)).to be_present
+
+    result = subject.call(current_user.id, other_uuid)
 
     expect(result).to be_nil
   end
 
   it 'returns nil if the user is not found and the cookie uuid is found' do
-    expect(Device.find_by(cookie_uuid: good_uuid)).to be_present
-    result = subject.call(bad_user_id, good_uuid)
+    expect(Device.find_by(cookie_uuid: device_uuid)).to be_present
+
+    result = subject.call(other_user.id, device_uuid)
 
     expect(result).to be_nil
   end
 
   it 'returns the device' do
-    result = subject.call(good_user_id, good_uuid)
+    result = subject.call(current_user.id, device_uuid)
 
     expect(result).to be_present
   end
