be8f73cf42124ed66add7aab94229ec5353d7544
Clara Bridges <clarapbridges@gmail.com>
2019-02-25 16:45:22 -0500

    Removed some empty spaces, added some empty spaces

--- app/services/device_tracking/manage_device.rb
+++ app/services/device_tracking/manage_device.rb
@@ -1,15 +1,19 @@
 module DeviceTracking
   class ManageDevice
-
-    # primary method: updates the usage time if it is not a new device, and calls create if the device used to log in
-    # has not been used before and alerts the user via email and sms about the new device in use
+    # primary method: updates the usage time if it is not a new device, and calls create
+    # if the device used to log in has not been used before and alerts the user via email
+    # and sms about the new device in use
     def self.call(user, device_hash, remote_ip, user_agent)
-      device = DeviceTracking::LookupDeviceForUser.call(user.id, device_hash)
+      user_id = user.id
+
+      device = DeviceTracking::LookupDeviceForUser.call(user_id, device_hash)
       alert_user = device.nil? && UserDecorator.new(user).devices?
 
-      device = device ?
-                   DeviceTracking::UpdateDevice.call(device, remote_ip) :
-                   DeviceTracking::CreateDevice.call(user.id, remote_ip, user_agent, device_hash)
+      device = if device
+                   DeviceTracking::UpdateDevice.call(device, remote_ip)
+               else
+                  DeviceTracking::CreateDevice.call(user_id, remote_ip, user_agent, device_hash)
+               end
 
       alert_user_of_new_device(user, device) if alert_user
 
@@ -18,13 +22,13 @@ module DeviceTracking
 
     # private
     def self.alert_user_of_new_device(user, device)
+      login_location = DeviceDecorator.new(device).last_sign_in_location_and_ip
       UserMailer.new_device_sign_in(user.email,
                                     Time.zone.now.strftime('%B %-d, %Y %H:%M'),
-                                    DeviceDecorator.new(device).last_sign_in_location_and_ip).deliver_now
+                                    login_location).deliver_now
 
       SmsNewDeviceSignInNotifierJob.perform_now(phone: MfaContext.new(user).phone_configurations.first&.phone)
     end
     private_class_method :alert_user_of_new_device
-
   end
 end
