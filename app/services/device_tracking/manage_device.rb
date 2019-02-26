72eba09c22974cc8fba39be9bf6fad70ef1617f1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-02-26 11:16:46 -0500

    fix new device issue

--- app/services/device_tracking/manage_device.rb
+++ app/services/device_tracking/manage_device.rb
@@ -4,25 +4,27 @@ module DeviceTracking
     # if the device used to log in has not been used before and alerts the user via email
     # and sms about the new device in use
     # returns the existing, updated device or the new one just created
+    # :reek:DuplicateMethodCall
     def self.call(user, hash, remote_ip, user_agent)
-      uid = user.id
-      device = DeviceTracking::LookupDeviceForUser.call(uid, hash)
+      user_has_multiple_devise = UserDecorator.new(user).devices?
+      device = DeviceTracking::LookupDeviceForUser.call(user.id, hash)
 
       if device
         DeviceTracking::UpdateDevice.call(device, remote_ip)
       else
-        new_device = DeviceTracking::CreateDevice.call(uid, remote_ip, user_agent, hash)
-        alert_user_of_new_device(user, new_device) if UserDecorator.new(user).devices?
+        new_device = DeviceTracking::CreateDevice.call(user.id, remote_ip, user_agent, hash)
+        alert_user_of_new_device(user, new_device) if user_has_multiple_devise
         new_device
       end
     end
 
-    # private
     def self.alert_user_of_new_device(user, device)
       login_location = DeviceDecorator.new(device).last_sign_in_location_and_ip
       UserMailer.new_device_sign_in(user.email,
                                     Time.zone.now.strftime('%B %-d, %Y %H:%M'),
                                     login_location).deliver_now
+
+      return unless FeatureManagement.send_new_device_sms?
 
       SmsNewDeviceSignInNotifierJob.
         perform_now(phone: MfaContext.new(user).phone_configurations.first&.phone)
