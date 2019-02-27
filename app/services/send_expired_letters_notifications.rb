b708980c841de9ae30b138f757d7deb5902655c8
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-02-27 10:34:11 -0500

    Sql

--- app/services/send_expired_letters_notifications.rb
+++ app/services/send_expired_letters_notifications.rb
@@ -2,9 +2,8 @@ class SendExpiredLetterNotifications
   def call
     notifications_sent = 0
     UspsConfirmationCode.where(
-      sql_query_for_expired_letters,
-      tvalue: Time.zone.now - Figaro.env.usps_confirmation_max_days.to_i.days,
-    ).order('created_at ASC').each do |ucc|
+      'created_at < ?', Time.zone.now - Figaro.env.usps_confirmation_max_days.to_i.days
+    ).where(bounced_at: nil, letter_expired_sent_at: nil).order(created_at: :asc).each do |ucc|
       mark_sent_and_send_email(ucc)
       notifications_sent += 1
     end
@@ -12,14 +11,6 @@ class SendExpiredLetterNotifications
   end
 
   private
-
-  def sql_query_for_expired_letters
-    <<~SQL
-      bounced_at IS NULL AND
-      letter_expired_sent_at IS NULL AND
-      created_at < :tvalue
-    SQL
-  end
 
   def mark_sent_and_send_email(ucc)
     user = ucc.profile.user
