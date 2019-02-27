569428424c7f68e5e9b9b861edafd8ab461e37cf
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-02-27 09:23:35 -0500

    Tear down scaffolding for migrating to sessions w/ KMS contexts
    **Why**: After #2787 is deployed, we won't be using contextless KMS for
    session encryption anymore.
    
    This should not be merged until after #2787 is deployed to production
    and the feature flags have been adjusted to have the new instances write
    sessions encrypted with KMS contexts.

--- lib/feature_management.rb
+++ lib/feature_management.rb
@@ -109,8 +109,4 @@ class FeatureManagement
   def self.write_2lkms_passwords?
     Figaro.env.write_2lkms_passwords == 'true'
   end
-
-  def self.use_kms_context_for_sessions?
-    Figaro.env.use_kms_context_for_sessions == 'true'
-  end
 end
