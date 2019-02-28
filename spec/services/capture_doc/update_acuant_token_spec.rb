7609f04acc1b3fa50780e54450ccda3366dbd4da
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-02-28 16:33:00 -0500

    Even more coverage

--- spec/services/capture_doc/update_acuant_token_spec.rb
+++ spec/services/capture_doc/update_acuant_token_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe CaptureDoc::UpdateAcuantToken do
   let(:subject) { described_class }
   let(:user_id) { 1 }
-  let(:old_timestamp) { Time.zone.now - 1.year }
   let(:token) { 'foo' }
 
   it 'updates the token if the entry exists' do
