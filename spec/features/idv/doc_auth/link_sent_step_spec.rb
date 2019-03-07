f1c328746683ca2be7c28c580a4d2182a5be7db6
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-03-07 15:28:46 -0500

    Fix 500 errors on doc auth when no phone used for 2fa

--- spec/features/idv/doc_auth/link_sent_step_spec.rb
+++ spec/features/idv/doc_auth/link_sent_step_spec.rb
@@ -28,6 +28,13 @@ shared_examples 'link sent step' do |simulate|
       expect(page).to have_current_path(idv_doc_auth_ssn_step)
     end
 
+    it 'proceeds to the next page if the user does not have a phone' do
+      complete_doc_auth_steps_before_link_sent_step(create(:user, :with_piv_or_cac))
+      click_idv_continue
+
+      expect(page).to have_current_path(idv_doc_auth_ssn_step)
+    end
+
     it 'does not proceed to the next page with invalid info' do
       allow_any_instance_of(Idv::Acuant::AssureId).to receive(:post_back_image).
         and_return([false, ''])
