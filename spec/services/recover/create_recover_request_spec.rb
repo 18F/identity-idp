a10c313b9e7d50936bd1a5ba5775064a6f7892e2
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-03-18 00:51:19 -0400

    More coverage

--- spec/services/recover/create_recover_request_spec.rb
+++ spec/services/recover/create_recover_request_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
-describe CaptureDoc::CreateRequest do
+describe Recover::CreateRecoverRequest do
   let(:subject) { described_class }
   let(:user_id) { 1 }
   let(:old_timestamp) { Time.zone.now - 1.year }
 
   it 'creates a new request if one does not exist' do
     result = subject.call(user_id)
-    expect(result).to be_kind_of(DocCapture)
+    expect(result).to be_kind_of(AccountRecoveryRequest)
 
-    doc_capture = DocCapture.find_by(user_id: user_id)
+    account_recovery_request = AccountRecoveryRequest.find_by(user_id: user_id)
 
-    expect(doc_capture.request_token).to be_present
-    expect(doc_capture.requested_at).to be_present
-    expect(doc_capture.acuant_token).to be_nil
+    expect(account_recovery_request.request_token).to be_present
+    expect(account_recovery_request.requested_at).to be_present
   end
 
   it 'update a request if one already exists' do
-    DocCapture.create(user_id: user_id, request_token: 'foo', requested_at: old_timestamp)
+    AccountRecoveryRequest.create(user_id: user_id,
+                                  request_token: 'foo',
+                                  requested_at: old_timestamp)
 
     result = subject.call(user_id)
-    expect(result).to be_kind_of(DocCapture)
+    expect(result).to be_kind_of(AccountRecoveryRequest)
 
-    doc_capture = DocCapture.find_by(user_id: user_id)
-    expect(doc_capture.request_token).to be_present
-    expect(doc_capture.request_token).to_not eq('foo')
-    expect(doc_capture.request_token).to be_present
-    expect(doc_capture.requested_at).to_not eq(old_timestamp)
-    expect(doc_capture.acuant_token).to be_nil
+    account_recovery_request = AccountRecoveryRequest.find_by(user_id: user_id)
+    expect(account_recovery_request.request_token).to be_present
+    expect(account_recovery_request.request_token).to_not eq('foo')
+    expect(account_recovery_request.request_token).to be_present
+    expect(account_recovery_request.requested_at).to_not eq(old_timestamp)
   end
 end
--- spec/support/features/recovery_helper.rb
+++ spec/support/features/recovery_helper.rb
@@ -5,11 +5,49 @@ module RecoveryHelper
     dc.request_token
   end
 
+  def complete_recovery_steps_before_welcome_step(user = user_with_2fa)
+    complete_recovery_steps_before_recover_step(user)
+    click_idv_continue
+  end
+
+  def complete_recovery_steps_before_upload_step(user = user_with_2fa)
+    complete_recovery_steps_before_welcome_step(user)
+    click_on t('doc_auth.buttons.get_started')
+  end
+
+  def complete_recovery_steps_before_front_image_step(user = user_with_2fa)
+    complete_recovery_steps_before_upload_step(user)
+    click_on t('doc_auth.buttons.use_computer')
+  end
+
+  def complete_recovery_steps_before_back_image_step(user = user_with_2fa)
+    complete_recovery_steps_before_front_image_step(user)
+    mock_assure_id_ok
+    attach_image
+    click_idv_continue
+  end
+
   def idv_recovery_recover_step(token)
     idv_recovery_step_path(step: :recover, token: token)
   end
 
   def idv_recovery_welcome_step
     idv_recovery_step_path(step: :welcome)
+  end
+
+  def idv_recovery_upload_step
+    idv_recovery_step_path(step: :upload)
+  end
+
+  def idv_recovery_front_image_step
+    idv_recovery_step_path(step: :front_image)
+  end
+
+  def idv_recovery_back_image_step
+    idv_recovery_step_path(step: :back_image)
+  end
+
+  def idv_recovery_ssn_step
+    idv_recovery_step_path(step: :ssn)
   end
 end
