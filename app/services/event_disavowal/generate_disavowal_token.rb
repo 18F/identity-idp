b546e3b6f6e526f2a4c6eb7bb48a5ccbe93fec33
Jonathan Hooper <jon9820@gmail.com>
2019-03-20 15:12:41 -0400

    Make create_user_event_with_disavowal return an event (#2831)
    **Why**: To have a return type that is consistent with `create_user_event`.

--- app/services/event_disavowal/generate_disavowal_token.rb
+++ app/services/event_disavowal/generate_disavowal_token.rb
@@ -9,18 +9,18 @@ module EventDisavowal
     def call
       generate_disavowal_token
       fingerprint_and_save_disavowal_token
-      disavowal_token
+      event.disavowal_token
     end
 
     private
 
     def generate_disavowal_token
-      @disavowal_token = SecureRandom.urlsafe_base64(32)
+      event.disavowal_token = SecureRandom.urlsafe_base64(32)
     end
 
     def fingerprint_and_save_disavowal_token
       event.update!(
-        disavowal_token_fingerprint: Pii::Fingerprinter.fingerprint(disavowal_token),
+        disavowal_token_fingerprint: Pii::Fingerprinter.fingerprint(event.disavowal_token),
       )
     end
   end
--- spec/services/event_disavowal/generate_disavowal_token_spec.rb
+++ spec/services/event_disavowal/generate_disavowal_token_spec.rb
@@ -9,7 +9,9 @@ describe EventDisavowal::GenerateDisavowalToken do
     it 'adds a disavowal token fingerprint to an event and returns the token' do
       token = subject.call
 
+      expect(event.disavowal_token).to eq(token)
       expect(Pii::Fingerprinter.fingerprint(token)).to eq(event.reload.disavowal_token_fingerprint)
+      expect(Event.find(event.id).disavowal_token).to eq(nil)
     end
   end
 end
