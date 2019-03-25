16e2c0b13680fe6c6a4b6e65784bd5d1c68aa735
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-03-25 13:45:51 -0400

    Dont mock session in doc success step

--- spec/features/idv/recovery/recover_doc_success_step_spec.rb
+++ spec/features/idv/recovery/recover_doc_success_step_spec.rb
@@ -7,15 +7,16 @@ feature 'recovery doc success step' do
 
   let(:user) { create(:user, :with_phone) }
   let(:good_ssn) { '666-66-1234' }
-  let(:profile) { build(:profile, :active, :verified, user: user, pii: { ssn: good_ssn }) }
+  let(:profile) { create(:profile, :active, :verified, user: user, pii: saved_pii) }
   let(:saved_pii) { DocAuthHelper::ACUANT_RESULTS_TO_PII.merge(ssn: good_ssn) }
   let(:bad_pii) { DocAuthHelper::ACUANT_RESULTS_TO_PII.merge(ssn: '123') }
 
   before do
-    sign_in_before_2fa(user)
+    profile # Create the profile so the record is in the db
+    sign_in_user(user)
     enable_doc_auth
     mock_assure_id_ok
-    complete_recovery_steps_before_doc_success_step
+    complete_recovery_steps_before_doc_success_step(user)
   end
 
   it 'is on the correct page' do
@@ -23,27 +24,21 @@ feature 'recovery doc success step' do
     expect(page).to have_content(t('doc_auth.forms.doc_success'))
   end
 
-  it 'proceeds to the account page if the document pii matches the saved pii' do
-    mock_decrypted_pii_data_in_session(saved_pii)
-    click_idv_continue
+  context 'document pii matches saved pii' do
+    it 'proceeds to the account page if the document pii matches the saved pii' do
+      click_idv_continue
 
-    expect(page).to have_current_path(account_path)
+      expect(page).to have_current_path(account_path)
+    end
   end
 
-  it 'fails to re-verify if the document pii does not match the saved pii' do
-    mock_decrypted_pii_data_in_session(bad_pii)
+  context 'document pii does not match the saved pii' do
+    let(:profile) { create(:profile, :active, :verified, user: user, pii: bad_pii) }
 
-    click_idv_continue
+    it 'fails to re-verify if the document pii does not match the saved pii' do
+      click_idv_continue
 
-    expect(page).to have_current_path(idv_recovery_fail_step)
-  end
-
-  def mock_decrypted_pii_data_in_session(decrypted_pii)
-    allow_any_instance_of(Idv::Flows::RecoveryFlow).to receive(:session).
-      and_wrap_original do |m, *args|
-      hash = m.call(*args)
-      hash['decrypted_pii'] = decrypted_pii.stringify_keys.to_json
-      hash
+      expect(page).to have_current_path(idv_recovery_fail_step)
     end
   end
 end
