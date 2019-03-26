be277d2e4064b230c5d018e0f6289193222dd226
Jonathan Hooper <jon9820@gmail.com>
2019-03-26 12:54:00 -0400

    Parallelize test suite when running with make test (#2844)
    **Why**: So the tests run quicker when run locally
    
    This necesitated a change to the KMS logger spec which was looking at a file that is written to by other processes running the tests.

--- spec/services/encryption/kms_logger_spec.rb
+++ spec/services/encryption/kms_logger_spec.rb
@@ -4,38 +4,39 @@ describe Encryption::KmsLogger do
   describe '.log' do
     context 'with a context' do
       it 'logs the context' do
+        log = {
+          kms: {
+            action: 'encrypt',
+            encryption_context: { context: 'pii-encryption', user_uuid: '1234-abc' },
+          },
+        }.to_json
+
+        expect(described_class.logger).to receive(:info).with(log)
+
         described_class.log(:encrypt, context: 'pii-encryption', user_uuid: '1234-abc')
-
-        log_contents = File.read('log/kms.log').split("\n")
-        log = log_contents.last
-
-        expect(log).to include(
-          {
-            kms: {
-              action: 'encrypt',
-              encryption_context: { context: 'pii-encryption', user_uuid: '1234-abc' },
-            },
-          }.to_json,
-        )
       end
     end
 
     context 'without a context' do
       it 'logs that an encryption happened without a context' do
+        log = {
+          kms: {
+            action: 'decrypt',
+            encryption_context: nil,
+          },
+        }.to_json
+
+        expect(described_class.logger).to receive(:info).with(log)
+
         described_class.log(:decrypt)
-
-        log_contents = File.read('log/kms.log').split("\n")
-        log = log_contents.last
-
-        expect(log).to include(
-          {
-            kms: {
-              action: 'decrypt',
-              encryption_context: nil,
-            },
-          }.to_json,
-        )
       end
+    end
+  end
+
+  describe '.logger' do
+    it 'is a logger that write to log/kms.log' do
+      expect(described_class.logger).to be_a(Logger)
+      expect(described_class.logger.instance_variable_get(:@logdev).filename).to eq('log/kms.log')
     end
   end
 end
