4174dc83723bccf8b85f7edb1f81e2a95925d580
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-03-27 00:57:55 -0400

    Lint

--- app/services/idv/steps/recover_doc_success_step.rb
+++ app/services/idv/steps/recover_doc_success_step.rb
@@ -6,13 +6,17 @@ module Idv
         decrypted_pii = JSON.parse(saved_pii)
         return unless pii_matches_data_on_file?(pii_from_doc, decrypted_pii)
 
+        recovery_success
+      end
+
+      private
+
+      def recovery_success
         mark_step_complete(:recover_fail)
         flash[:success] = I18n.t('recover.reverify.success')
         redirect_to account_url
         session['need_two_factor_authentication'] = false
       end
-
-      private
 
       def saved_pii
         session['decrypted_pii']
