6bad9e3737052e788ca45a394c7d4fbae441bee9
Clara Bridges <clara.bridges@gsa.gov>
2019-04-11 14:18:30 -0400

    LG-1174 & LG-1187 Make personal keys count as an MFA

--- spec/decorators/mfa_context_spec.rb
+++ spec/decorators/mfa_context_spec.rb
@@ -270,6 +270,15 @@ describe MfaContext do
       end
     end
 
+    context 'with a phone and a personal key' do
+      it 'returns 2' do
+        user = create(:user, :signed_up, :with_personal_key)
+        subject = described_class.new(user.reload)
+
+        expect(subject.enabled_mfa_methods_count).to eq(2)
+      end
+    end
+
     context 'with a phone and an auth app' do
       it 'returns 2' do
         user = create(:user, :signed_up, :with_authentication_app)
