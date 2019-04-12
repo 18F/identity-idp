199dc1fb68e0e027f829e452380aa4efe73de412
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-04-12 14:27:35 -0500

    cleanup untouched code

--- app/controllers/concerns/user_navigation_concern.rb
+++ app/controllers/concerns/user_navigation_concern.rb
@@ -17,19 +17,7 @@ module UserNavigationConcern
     elsif decorated_user.password_reset_profile.present?
       reactivate_account_url
     else
-      return successful_path
+      return after_sign_in_path_for(current_user)
     end
-  end
-
-  def successful_path
-    if !MfaPolicy.new(current_user).two_factor_enabled?
-      two_factor_options_url
-    else
-      after_sign_in_path_for(current_user)
-    end
-  end
-
-  def user_already_has_a_personal_key?
-    TwoFactorAuthentication::PersonalKeyPolicy.new(current_user).configured?
   end
 end
