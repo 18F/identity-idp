6c983db4676bcd07d82c006d75500bdfcc9d8a47
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-04-13 18:47:13 -0400

    Fix spec

--- spec/controllers/account_reset/request_controller_spec.rb
+++ spec/controllers/account_reset/request_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe AccountReset::RequestController do
-  let(:user) { create(:user, :with_authentication_app, :with_email) }
+  let(:user) { create(:user, :with_authentication_app, :with_backup_code, :with_email) }
   describe '#show' do
     it 'renders the page' do
       stub_sign_in_before_2fa(user)
@@ -72,7 +72,7 @@ describe AccountReset::RequestController do
     end
 
     it 'logs PIV/CAC user in the analytics' do
-      user = create(:user, :with_piv_or_cac, :with_email)
+      user = create(:user, :with_piv_or_cac, :with_backup_code, :with_email)
       stub_sign_in_before_2fa(user)
 
       stub_analytics
