945a247ab532687e187046aa0e5a54c683c8ad0c
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-04-17 01:54:38 -0400

    Fix specs

--- app/controllers/two_factor_authentication/personal_key_verification_controller.rb
+++ app/controllers/two_factor_authentication/personal_key_verification_controller.rb
@@ -70,7 +70,11 @@ module TwoFactorAuthentication
 
     def handle_valid_otp
       handle_valid_otp_for_authentication_context
-      redirect_to manage_personal_key_url
+      if current_user.decorate.identity_verified?
+        redirect_to manage_personal_key_url
+      else
+        redirect_to after_sign_in_path_for(current_user)
+      end
       reset_otp_session_data
       user_session.delete(:mfa_device_remembered)
     end
