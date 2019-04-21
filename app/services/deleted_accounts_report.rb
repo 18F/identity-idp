b6d326be3de5e762721f77732a9bc8246865122d
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-04-21 14:37:17 -0400

    Speed improvements

--- app/services/deleted_accounts_report.rb
+++ app/services/deleted_accounts_report.rb
@@ -1,11 +1,12 @@
 class DeletedAccountsReport
   def self.call(service_provider, days_ago)
     report_sql = <<~SQL
-      SELECT uuid,last_authenticated_at
-      FROM identities
-      WHERE user_id not in (SELECT id FROM users)
-      AND service_provider='%s'
-      AND last_authenticated_at > '%s'
+      SELECT last_authenticated_at, identity_uuid FROM
+      (SELECT ids.last_authenticated_at AS last_authenticated_at,
+              ids.uuid AS identity_uuid, us.id AS users_id
+      FROM identities AS ids LEFT JOIN users AS us ON ids.user_id=us.id
+      WHERE service_provider='%s' AND last_authenticated_at > '%s') AS tbl
+      WHERE users_id IS NULL ORDER BY last_authenticated_at ASC
     SQL
     sql = format(report_sql, service_provider, days_ago.to_i.days.ago)
     ActiveRecord::Base.connection.execute(sql)
--- lib/tasks/deleted_accounts.rake
+++ lib/tasks/deleted_accounts.rake
@@ -1,9 +1,12 @@
 namespace :deleted_accounts do
   task :report, %i[service_provider days_ago] => [:environment] do |_task, args|
-    puts 'uuid, last_authenticated_at'
-    DeletedAccountsReport.call(args[:service_provider], args[:days_ago]).each do |row|
-      puts "#{row['last_authenticated_at']}, #{row['uuid']}"
+    puts 'last_authenticated_at, identity_uuid'
+    days_ago = args[:days_ago].to_i
+    rows = DeletedAccountsReport.call(args[:service_provider], args[:days_ago])
+    rows.each do |row|
+      puts "#{row['last_authenticated_at']}, #{row['identity_uuid']}"
     end
+    puts "Total records=#{rows.count} Date range=#{days_ago.days.ago} - #{Time.zone.now}"
   end
 end
 # rake "deleted_accounts:report[urn:gov:gsa:openidconnect:sp:sinatra,30]"
