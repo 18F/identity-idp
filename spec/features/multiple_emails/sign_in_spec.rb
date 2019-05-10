c1f2f202c746c9683ff80d6d9086c7a97cc09351
Paolo Valladolid <47984506+paolov18F@users.noreply.github.com>
2019-05-10 16:23:10 -0400

    LG-1276 Add test for sign-in with multiple email addresses (#2935)
    **Why**: Test sign-in from more than one email address for the same user, to make sure that feature continues to work.

---
+++ spec/features/multiple_emails/sign_in_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'sign in with any email address' do
+  scenario 'signing in any email address' do
+    allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
+
+    user = create(:user, :signed_up)
+    create(:email_address, user: user)
+
+    email1, email2 = user.reload.email_addresses.map(&:email)
+
+    signin(email1, user.password)
+    click_submit_default
+
+    expect(page).to have_current_path(account_path)
+
+    first(:link, t('links.sign_out')).click
+
+    signin(email2, user.password)
+    click_submit_default
+
+    expect(page).to have_current_path(account_path)
+  end
+end
