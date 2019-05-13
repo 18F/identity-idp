a0e02357c663bd7861c00c506e7a97ad8dd90cb0
Clara Bridges <clara.bridges@gsa.gov>
2019-05-13 10:20:38 -0400

    removed spec no longer applicable

--- spec/views/accounts/show.html.slim_spec.rb
+++ spec/views/accounts/show.html.slim_spec.rb
@@ -202,15 +202,7 @@ describe 'accounts/show.html.slim' do
 
       expect(view).to render_template(partial: '_emails')
     end
-
-    it 'shows the add email link' do
-      render
-
-      expect(rendered).to have_link(
-        t('account.index.email_add'), href: add_email_path
-      )
-    end
-
+    
     it 'shows one email if the user has only one email' do
       expect(user.email_addresses.size).to eq(1)
     end
