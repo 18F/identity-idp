1aa9eea4eb816f56404c499ad21886a4caf5d8de
Aaron Chapman <achapm@gmail.com>
2019-05-13 11:29:14 -0400

    Fixed phone configuration spec

--- spec/features/phone_configuration/phone_configuration_spec.rb
+++ spec/features/phone_configuration/phone_configuration_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
-describe 'Set default 2FA phone number' do
+describe 'default 2FA phone number' do
+
+  before do
+    TwilioService::Utils.telephony_service = FakeSms
+  end
+
   context 'when the user has not set a default phone number' do
     it 'uses the first phone created as the default' do
       user = create(:user, :with_phone)
@@ -28,37 +33,30 @@ describe 'Set default 2FA phone number' do
 
       check 'user_phone_form_otp_make_default_number'
       click_button t('forms.buttons.submit.confirm_change')
+
+      expect(current_path).to eq login_two_factor_path(otp_delivery_preference: 'sms')
+      submit_prefilled_otp_code(user)
+
+      expect(current_path).to eq sign_up_personal_key_path
+      click_acknowledge_personal_key
+
+      expect(page).to have_current_path(account_path)
       expect(page).to have_content t('account.index.default')
 
-      first(:link, t('links.sign_out')).click
-      sign_in_before_2fa(user)
-      t('instructions.mfa.sms.number_message',
-        number: '***-***-2323',
-        expiration: Figaro.env.otp_valid_for)
+      sign_out_sign_in(user)
+      expect(page).to have_content t('instructions.mfa.sms.number_message',
+                                     number: '***-***-2323',
+                                     expiration: Figaro.env.otp_valid_for)
     end
   end
 
-  context 'when the user sets a default phone number with voice delivery' do
-    it 'displays the new default number for 2FA with voice message' do
-      user = create(:user, :with_phone)
-      phone_config2 = create(:phone_configuration, user: user,
-                                                   phone: '+1 202-555-2323',
-                                                   created_at: Time.zone.now + 1.hour,
-                                                   delivery_preference: 1)
+  def sign_out_sign_in(user)
+    first(:link, t('links.sign_out')).click
+    sign_in_before_2fa(user)
+  end
 
-      sign_in_and_2fa_user(user)
-      visit manage_phone_path(id: phone_config2.id)
-      expect(page).to have_content t('two_factor_authentication.otp_make_default_number.label')
-
-      check 'user_phone_form_otp_make_default_number'
-      click_button t('forms.buttons.submit.confirm_change')
-      expect(page).to have_content t('account.index.default')
-
-      first(:link, t('links.sign_out')).click
-      sign_in_before_2fa(user)
-      t('instructions.mfa.voice.number_message',
-        number: '***-***-2323',
-        expiration: Figaro.env.otp_valid_for)
-    end
+  def submit_prefilled_otp_code(user)
+    fill_in('code', with: user.reload.direct_otp)
+    click_button t('forms.buttons.submit.default')
   end
 end
