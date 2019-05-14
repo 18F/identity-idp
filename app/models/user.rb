b620159691c898c84ec5426dedbc51bafece24f8
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-05-14 15:23:00 -0400

    cleanup old code

--- app/models/user.rb
+++ app/models/user.rb
@@ -135,13 +135,4 @@ class User < ApplicationRecord
   def send_confirmation_instructions
     # no-op
   end
-
-  # def send_custom_confirmation_instructions(id = nil, instructions = nil)
-  #   generate_confirmation_token! unless @raw_confirmation_token
-
-  #   opts = pending_reconfirmation? ? { to: unconfirmed_email, request_id: id } : { request_id: id }
-  #   opts[:first_sentence] = instructions if instructions
-  #   send_devise_notification(:confirmation_instructions,
-  #                            @raw_confirmation_token, opts)
-  # end
 end
