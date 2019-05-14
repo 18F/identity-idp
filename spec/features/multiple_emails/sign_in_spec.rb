a62816a4464c322d7c12070d6f804d96eca8241a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-05-14 10:24:00 -0400

    LG-1285 Prevent sign in with an unconfirmed email address (#2945)
    **Why**: So that users can't add an email address and use it to sign in without confirming it.

--- spec/features/multiple_emails/sign_in_spec.rb
+++ spec/features/multiple_emails/sign_in_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 feature 'sign in with any email address' do
-  scenario 'signing in any email address' do
+  scenario 'signing in with any email address' do
     allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
 
-    user = create(:user, :signed_up)
-    create(:email_address, user: user)
+    user = create(:user, :signed_up, :with_multiple_emails)
 
     email1, email2 = user.reload.email_addresses.map(&:email)
 
@@ -20,5 +19,19 @@ feature 'sign in with any email address' do
     click_submit_default
 
     expect(page).to have_current_path(account_path)
+  end
+
+  scenario 'signing in with an unconfirmed email results in an error' do
+    user = create(:user, :signed_up, :with_multiple_emails)
+
+    email_address = user.email_addresses.first
+    email_address.update!(confirmed_at: nil)
+
+    signin(email_address.email, user.password)
+
+    error_message = t('devise.failure.invalid_html', link: t('devise.failure.invalid_link_text'))
+
+    expect(page).to have_content(error_message)
+    expect(page).to have_current_path(new_user_session_path)
   end
 end
