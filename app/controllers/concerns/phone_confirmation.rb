50992c829b789b97b6f2da862ad6537b0599c2f0
Clara Bridges <clara.bridges@gsa.gov>
2019-05-14 15:32:26 -0400

    lint fix

--- app/controllers/concerns/phone_confirmation.rb
+++ app/controllers/concerns/phone_confirmation.rb
@@ -12,9 +12,9 @@ module PhoneConfirmation
 
   private
 
-  def otp_delivery_method(id, phone, selected_delivery_method)
+  def otp_delivery_method(_id, phone, selected_delivery_method)
     return :sms if PhoneNumberCapabilities.new(phone).sms_only?
     return selected_delivery_method if selected_delivery_method.present?
-      current_user.otp_delivery_preference
+    current_user.otp_delivery_preference
   end
 end
