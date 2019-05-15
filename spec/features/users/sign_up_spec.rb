008f4f085937da5ba3d04956f9c987396f6493a4
Clara Bridges <clara.bridges@gsa.gov>
2019-05-15 13:45:07 -0400

    moved the feature tests to their own file

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -14,52 +14,6 @@ feature 'Sign Up' do
     end
   end
 
-  context 'and set up voice as 2FA' do
-    before do
-      sign_in_user
-      allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
-      select_2fa_option('voice')
-      fill_in 'user_phone_form[phone]', with: '202-555-1212'
-      click_send_security_code
-      click_submit_default
-    end
-
-    it 'then set up SMS as backup MFA' do
-      allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
-      expect(page).to have_current_path(two_factor_options_path)
-      select_2fa_option('sms')
-      expect(page).to have_content t('titles.phone_setup.sms')
-      fill_in 'user_phone_form[phone]', with: '202-555-1212'
-      click_send_security_code
-      expect(page).to have_content(t('instructions.mfa.sms.number_message',
-                                     number: '+1 202-555-1212',
-                                     expiration: Figaro.env.otp_valid_for))
-    end
-  end
-
-  context 'and set up SMS as 2FA' do
-    before do
-      sign_in_user
-      allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
-      select_2fa_option('sms')
-      fill_in 'user_phone_form[phone]', with: '202-555-1212'
-      click_send_security_code
-      click_submit_default
-    end
-
-    it 'then set up voice as backup MFA' do
-      allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
-      expect(page).to have_current_path(two_factor_options_path)
-      select_2fa_option('voice')
-      expect(page).to have_content t('titles.phone_setup.voice')
-      fill_in 'user_phone_form[phone]', with: '202-555-1212'
-      click_send_security_code
-      expect(page).to have_content(t('instructions.mfa.voice.number_message',
-                                     number: '+1 202-555-1212',
-                                     expiration: Figaro.env.otp_valid_for))
-    end
-  end
-
   context 'user cancels sign up on email screen' do
     before do
       visit sign_up_email_path
