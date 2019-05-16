a52742feccdf101f4fd8f0c744c04146af21d8d4
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-05-15 22:10:52 -0400

    Fix specs

--- spec/features/remember_device/totp_spec.rb
+++ spec/features/remember_device/totp_spec.rb
@@ -26,6 +26,8 @@ describe 'Remembering a TOTP device' do
     def remember_device_and_sign_out_user
       user = sign_up_and_set_password
       user.password = Features::SessionHelper::VALID_PASSWORD
+      select_2fa_option('backup_code')
+      click_continue
       select_2fa_option('auth_app')
       fill_in :code, with: totp_secret_from_page
       check :remember_device
