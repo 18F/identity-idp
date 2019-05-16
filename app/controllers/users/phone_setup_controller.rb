86cce3f3c2794fe051dd9237ad0c540816a8d95a
Clara Bridges <46763138+cpbgsa@users.noreply.github.com>
2019-05-16 11:33:31 -0400

    Fixed LG-1288 bug which defaults the delivery preference to the selection made for the previous phone (#2949)
    removed assigning the delivery method to whatever the first registered phone selection was

--- app/controllers/users/phone_setup_controller.rb
+++ app/controllers/users/phone_setup_controller.rb
@@ -31,8 +31,7 @@ module Users
     private
 
     def delivery_preference
-      MfaContext.new(current_user).phone_configurations.take&.delivery_preference ||
-        current_user.otp_delivery_preference
+      current_user.otp_delivery_preference
     end
 
     def user_phone_form_params
