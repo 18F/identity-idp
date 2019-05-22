674d4401b5d0e8d4fd5df7d8c07668e8c6442fe7
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-05-22 13:24:56 -0400

    update interval

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -1,8 +1,8 @@
 # rubocop:disable Metrics/LineLength
 JobRunner::Runner.configurations << JobRunner::JobConfiguration.new(
   name: 'Send GPO letter',
-  interval: 300,
-  timeout: 30,
+  interval: 24 * 60 * 60,
+  timeout: 300,
   callback: -> { UspsConfirmationUploader.new.run unless HolidayService.observed_holiday?(Time.zone.today) },
 )
 # rubocop:enable Metrics/LineLength
