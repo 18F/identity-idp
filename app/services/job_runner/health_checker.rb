b5d5de64689e56ff8b38bebd15ab8f8e3b85d572
Paolo Valladolid <47984506+paolov18F@users.noreply.github.com>
2019-05-23 10:39:39 -0400

    LG-1113 Implement GPO mail job as rake task using RDS (#2919)
    **Why**: So that we have a mechanism for running background jobs and use that mechanism to send letters via GPO.

---
+++ app/services/job_runner/health_checker.rb
@@ -0,0 +1,30 @@
+module JobRunner
+  class HealthChecker
+    Summary = Struct.new(:healthy, :result) do
+      def as_json(*args)
+        to_h.as_json(*args)
+      end
+
+      alias_method :healthy?, :healthy
+    end
+
+    class << self
+      def check
+        result = Runner.configurations.map do |job_configuration|
+          [job_configuration.name, successful_job_run_within_2_intervals?(job_configuration)]
+        end.to_h
+        healthy = !result.value?(false)
+        Summary.new(healthy, result)
+      end
+
+      private
+
+      def successful_job_run_within_2_intervals?(job_configuration)
+        interval_window = (job_configuration.interval * 2).seconds.ago
+        JobRun.where(job_name: job_configuration.name, error: nil).
+          where('created_at > ?', interval_window).
+          any?
+      end
+    end
+  end
+end
---
+++ spec/factories/job_runs.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :job_run do
+    job_name { 'Send GPO letter' }
+  end
+end
