131ccd6263d96ede4a3ed72c389661c628b6b469
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-05-31 02:02:34 -0400

    Add time component

--- app/services/auths_per_sp_report.rb
+++ app/services/auths_per_sp_report.rb
@@ -1,14 +1,13 @@
 class AuthsPerSpReport
   def self.call(days_ago)
     report_sql = <<~SQL
-      SELECT agency, friendly_name, service_provider, cnt
-      FROM service_providers,
-      (SELECT service_provider, COUNT(*) as cnt
-      FROM identities
+      SELECT agency, friendly_name, service_provider, cnt FROM service_providers,
+      (SELECT service_provider, COUNT(*) as cnt FROM identities WHERE last_authenticated_at > '%s'
       GROUP BY service_provider) as sps_n_counts
       WHERE service_providers.issuer = sps_n_counts.service_provider
       ORDER BY cnt DESC
     SQL
-    ActiveRecord::Base.connection.execute(format(report_sql, days_ago.days.ago))
+    sql = format(report_sql, days_ago.days.ago)
+    ActiveRecord::Base.connection.execute(sql)
   end
 end
