f4a2750d84615467f1775955f953618092e3fb5d
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-05-31 12:01:26 -0400

    Make the PR age script take draft PRs into account
    **Why**: Because we want to measure how long PRs are open for review, and not how long they are open overall.

--- lib/github_metrics/pr_age_reporter.rb
+++ lib/github_metrics/pr_age_reporter.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Rails/Date, Rails/TimeZone, Rails/Output
 require 'octokit'
 require 'date'
 
@@ -6,53 +5,26 @@ module GithubMetrics
   class PrAgeReporter
     attr_reader :tot
 
-    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Rails/Output
     def call
       numerator = 0
       sprint_pull_requests.each do |pr|
-        pr_open_seconds = (pr.closed_at || pr.merged_at || Time.now).to_i - pr.created_at.to_i
+        pr_open_seconds = pr.done_at_or_current_date - pr.ready_for_review_at
         numerator += pr_open_seconds
-        puts "'#{pr.title}' open for #{seconds_to_hours(pr_open_seconds)} hours"
+        puts "'#{pr.title}' under review for #{seconds_to_hours(pr_open_seconds)} hours"
       end
       puts "Average: #{seconds_to_hours(numerator / sprint_pull_requests.count.to_f)} hours"
     end
-    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Rails/Output
 
     private
 
-    def sprint_start
-      @sprint_start ||= begin
-        reference_sprint_start = Date.new(2019, 4, 22)
-        days_since_reference = Date.today - reference_sprint_start
-        sprints_since_reference = (days_since_reference / 14).floor
-        (reference_sprint_start + 14 * sprints_since_reference).to_time
-      end
-    end
-
-    def sprint_end
-      (sprint_start + 14).to_time
-    end
-
-    # :reek:FeatureEnvy
-    def pr_open_in_current_sprint?(pr)
-      closed_at_or_current_time = pr.closed_at || pr.merged_at || Time.now
-      return true if pr.created_at > sprint_start || closed_at_or_current_time > sprint_start
-      false
-    end
-
     def sprint_pull_requests
-      github_client.pull_requests('18f/identity-idp', per_page: 100, state: :all).select do |pr|
-        pr_open_in_current_sprint?(pr)
-      end
+      @sprint_pull_requests ||= PullRequest.fetch_pull_requests_for_sprint(Sprint.new)
     end
 
-    def github_client
-      @github_client = Octokit::Client.new
-    end
-
-    def seconds_to_hours(value)
-      (value / 3600.0).round
+    def seconds_to_hours(seconds)
+      (seconds / 3600).round
     end
   end
 end
-# rubocop:enable Rails/Date, Rails/TimeZone, Rails/Output
