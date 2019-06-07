7ef88489c4a0d4adef86ad3ed5cec8e652fb62be
Alison Dowski <alison.dowski@gsa.gov>
2019-06-07 10:23:15 -0400

    single quotes

--- app/javascript/packs/webauthn-unhide-signup.js
+++ app/javascript/packs/webauthn-unhide-signup.js
@@ -2,7 +2,7 @@ const WebAuthn = require('../app/webauthn');
 
 function unhideWebauthn() {
   if (WebAuthn.isWebAuthnEnabled()) {
-    const elem = document.querySelector("label[for=two_factor_options_form_selection_webauthn]");
+    const elem = document.querySelector('label[for=two_factor_options_form_selection_webauthn]');
     if (elem) {
       elem.classList.remove('hide');
     }
