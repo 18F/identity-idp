321f81dac89cc513808ef49d8f54a56f47fd7d4d
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-06-07 15:07:10 -0400

    LG-1325 User should not change one phone to their other phone (#2992)
    **Why**:  To prevent users from having a single MFA and duplicating phones in the phone_configurations table
    
    **How**: The phone table can't have a unique index on a fingerprint to prevent looking up PII so we have to prevent duplicates in the application.  Simply check to see if the user is already using that phone number when trying to change a phone number

--- spec/features/backup_mfa/no_duplicate_phone_numbers_spec.rb
+++ spec/features/backup_mfa/no_duplicate_phone_numbers_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 feature 'OTP delivery selection' do
   context 'set up a number as 2FA for voice' do
+    let(:user) { create(:user) }
+
     before do
-      sign_in_user
+      sign_in_user(user)
       allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
       select_2fa_option('voice')
       fill_in 'user_phone_form[phone]', with: '202-555-1212'
@@ -22,6 +24,25 @@ feature 'OTP delivery selection' do
     end
 
     it 'should success if using a new number as backup MFA voice' do
+      choose_phone_as_backup_mfa
+    end
+
+    it 'should prevent changing one phone to the other number' do
+      choose_phone_as_backup_mfa
+      click_submit_default
+
+      expect(current_path).to eq(account_path)
+
+      first_phone = user.phone_configurations.first
+      second_phone = user.phone_configurations.last
+      visit manage_phone_path(id: first_phone.id)
+      fill_in 'user_phone_form[phone]', with: second_phone.phone
+      click_button t('forms.buttons.submit.confirm_change')
+
+      expect(page).to have_content(t('errors.messages.phone_duplicate'))
+    end
+
+    def choose_phone_as_backup_mfa
       allow(FeatureManagement).to receive(:prefill_otp_codes?).and_return(true)
       expect(page).to have_current_path(two_factor_options_path)
       select_2fa_option('voice')
