46f10fc5020f42ac1f564e913c799a89a5e2fd21
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-06-08 00:42:58 -0400

    LG-1408 Sign in with last backup code should regenerate backup codes

--- spec/features/two_factor_authentication/backup_code_sign_up_spec.rb
+++ spec/features/two_factor_authentication/backup_code_sign_up_spec.rb
@@ -28,7 +28,6 @@ feature 'sign up with backup code' do
 
   it 'works for each code and refreshes the codes on the last one' do
     user = create(:user, :signed_up, :with_authentication_app, :with_backup_code)
-    old_codes = user.backup_code_configurations.map(&:code)
     BackupCodeGenerator::NUMBER_OF_CODES.times do |index|
       signin(user.email, user.password)
       code = user.backup_code_configurations[index].code
@@ -36,10 +35,13 @@ feature 'sign up with backup code' do
       fill_in :backup_code_verification_form_backup_code, with: code
       click_on 'Submit'
       if index == BackupCodeGenerator::NUMBER_OF_CODES - 1
-        visit login_two_factor_backup_code_path
-        user.reload
-        new_codes = user.backup_code_configurations.map(&:code)
-        expect(new_codes & old_codes).to eq([])
+        expect(current_path).to eq backup_code_setup_path
+        expect(user.backup_code_configurations.count).to eq(0)
+
+        click_on 'Continue'
+
+        expect(current_path).to eq account_path
+        expect(user.backup_code_configurations.count).to eq(10)
       else
         expect(current_path).to eq account_path
         sign_out_user
