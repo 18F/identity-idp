fb94349e18bb512329212bfea439cceae39acfb9
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-06-19 12:03:59 -0400

    Remove code and tests around changing email address (#3024)
    **Why**: Because users can no longer edit email addresses, so this code no longer runs.

--- app/controllers/users/emails_controller.rb
+++ app/controllers/users/emails_controller.rb
@@ -1,6 +1,5 @@
 # :reek:RepeatedConditional
 module Users
-  # rubocop:disable Metrics/ClassLength
   class EmailsController < ReauthnRequiredController
     before_action :confirm_two_factor_authenticated
     before_action :authorize_user_to_edit_email, except: %i[add show verify resend]
@@ -28,25 +27,6 @@ module Users
       SendAddEmailConfirmation.new(current_user).call(email_address)
       flash[:success] = t('notices.resend_confirmation_email.success')
       redirect_to add_email_verify_email_url
-    end
-
-    def edit
-      @update_user_email_form = UpdateUserEmailForm.new(current_user, email_address)
-    end
-
-    def update
-      @update_user_email_form = UpdateUserEmailForm.new(current_user, email_address)
-
-      result = @update_user_email_form.submit(user_params)
-
-      analytics.track_event(Analytics::EMAIL_CHANGE_REQUEST, result.to_h)
-
-      if result.success?
-        process_updates
-        bypass_sign_in current_user
-      else
-        render :edit
-      end
     end
 
     def confirm_delete
@@ -89,22 +69,10 @@ module Users
       EmailAddress.find(params[:id])
     end
 
-    def user_params
-      params.require(:update_user_email_form).permit(:email)
-    end
-
     def handle_successful_delete
       send_delete_email_notification
       flash[:success] = t('email_addresses.delete.success')
       create_user_event(:email_deleted)
-    end
-
-    def process_updates
-      if @update_user_email_form.email_changed?
-        flash[:notice] = t('devise.registrations.email_update_needs_confirmation')
-      end
-
-      redirect_to account_url
     end
 
     def process_successful_creation
@@ -138,5 +106,4 @@ module Users
       end
     end
   end
-  # rubocop:enable Metrics/ClassLength
 end
