ce38d28db274764f4e2bc0efa8eb0c1a1fcfd633
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-06-21 09:09:54 -0400

    add uniqueness constraint

--- db/migrate/20190620204206_add_confirmation_token_index_to_email_address.rb
+++ db/migrate/20190620204206_add_confirmation_token_index_to_email_address.rb
@@ -2,6 +2,6 @@ class AddConfirmationTokenIndexToEmailAddress < ActiveRecord::Migration[5.1]
   disable_ddl_transaction!
 
   def change
-    add_index :email_addresses, :confirmation_token, algorithm: :concurrently
+    add_index :email_addresses, :confirmation_token, algorithm: :concurrently, unique: true
   end
 end
