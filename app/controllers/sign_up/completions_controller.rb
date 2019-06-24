ba2c4c6ece67246df7356a3f2e9c3dd24c213b94
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-06-24 00:10:42 -0400

    LG-1458 Fix logic to show sign up completed page
    **Why**: Users only need to consent to an sp once in IAL1.  This also fixes the analytics.

--- app/controllers/sign_up/completions_controller.rb
+++ app/controllers/sign_up/completions_controller.rb
@@ -6,17 +6,15 @@ module SignUp
     before_action :confirm_two_factor_authenticated
     before_action :verify_confirmed, if: :loa3?
     before_action :apply_secure_headers_override, only: :show
+    before_action :redirect_to_account_if_no_issuer
+    before_action :redirect_to_sp_if_user_has_identity_for_issuer
 
     def show
       @view_model = view_model
-      if show_completions_page?
-        analytics.track_event(
-          Analytics::USER_REGISTRATION_AGENCY_HANDOFF_PAGE_VISIT,
-          analytics_attributes(''),
+      analytics.track_event(
+        Analytics::USER_REGISTRATION_AGENCY_HANDOFF_PAGE_VISIT,
+        analytics_attributes(''),
         )
-      else
-        return_to_account
-      end
     end
 
     def update
@@ -31,14 +29,20 @@ module SignUp
 
     private
 
-    def handle_verified_attributes
-      update_verified_attributes
-      clear_verify_attributes_sessions
+    def redirect_to_account_if_no_issuer
+      redirect_to account_url if sp_session[:issuer].blank?
     end
 
-    def show_completions_page?
-      service_providers = sp_session[:issuer].present? || @view_model.user_has_identities?
-      user_fully_authenticated? && service_providers
+    def redirect_to_sp_if_user_has_identity_for_issuer
+      redirect_to after_sign_in_url if user_has_identity_for_issuer?(sp_session[:issuer])
+    end
+
+    def user_has_identity_for_issuer?(issuer)
+      current_user.identities.where(service_provider: issuer).any?
+    end
+
+    def after_sign_in_url
+      after_sign_in_path_for(current_user)
     end
 
     def view_model
