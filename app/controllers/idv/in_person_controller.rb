fc8bc2ebbd8571799a4325a767193d8754dde07d
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-06-24 12:52:05 -0400

    Updates per Michelle

--- app/controllers/idv/in_person_controller.rb
+++ app/controllers/idv/in_person_controller.rb
@@ -8,7 +8,7 @@ module Idv
       step_url: :idv_in_person_step_url,
       final_url: :account_url,
       flow: Idv::Flows::InPersonFlow,
-      analytics_id: Analytics::DOC_AUTH,
+      analytics_id: Analytics::IN_PERSON_PROOFING,
     }.freeze
   end
 end
