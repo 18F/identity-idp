d712ec90940582a386b7ffb6af41455ee8723e88
Alison Dowski <43366398+adowski@users.noreply.github.com>
2019-06-28 12:01:24 -0400

    LG-1478 Bug fix - Webauthn not visible on sign in (#3055)
    fixed bug where webauthn option was being hidden on sign in
    
    **Why**: so that users with webauthn enabled as an authentication
    option are able to use the option upon sign in

--- app/javascript/packs/webauthn-unhide-signin.js
+++ app/javascript/packs/webauthn-unhide-signin.js
@@ -4,12 +4,12 @@ function unhideWebauthn() {
   if (WebAuthn.isWebAuthnEnabled()) {
     const elem = document.getElementById('select_webauthn');
     if (elem) {
-      elem.classList.remove('hidden');
+      elem.classList.remove('hide');
     }
   } else {
     const checkboxes = document.querySelectorAll('input[name="two_factor_options_form[selection]"]');
     for (let i = 0, len = checkboxes.length; i < len; i += 1) {
-      if (!checkboxes[i].classList.contains('hidden')) {
+      if (!checkboxes[i].classList.contains('hide')) {
         checkboxes[i].checked = true;
         break;
       }
