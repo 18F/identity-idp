cadc61863824aba6d1a82cadbf536106debad8a2
Aaron Chapman <aaron.chapman@gsa.gov>
2019-07-01 11:39:56 -0400

    add missing copy to locals

--- app/presenters/backup_code_setup_intro_presenter.rb
+++ app/presenters/backup_code_setup_intro_presenter.rb
@@ -9,6 +9,7 @@ class BackupCodeSetupIntroPresenter
       description: I18n.t('forms.backup_code.are_you_sure_desc'),
       other_option_display: true,
       other_option_title: I18n.t('forms.backup_code.generate_codes_other_option'),
+      continue_bttn_prologue:  I18n.t('forms.backup_code.are_you_sure_continue_prologue'),
       continue_bttn_title: I18n.t('forms.backup_code.are_you_sure_continue'),
       continue_bttn_class: 'btn btn-link',
     },
@@ -17,6 +18,7 @@ class BackupCodeSetupIntroPresenter
       description: I18n.t('forms.backup_code.generate_codes_desc'),
       other_option_display: false,
       other_option_title: '',
+      continue_bttn_prologue:  '',
       continue_bttn_title: I18n.t('forms.buttons.continue'),
       continue_bttn_class: 'btn btn-primary btn-wide',
     },
@@ -48,6 +50,10 @@ class BackupCodeSetupIntroPresenter
 
   def continue_title
     STATE_CONFIG.dig(state, :continue_title)
+  end
+
+  def continue_bttn_prologue
+    STATE_CONFIG.dig(state, :continue_bttn_prologue)
   end
 
   def continue_bttn_title
