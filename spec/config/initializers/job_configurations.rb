1e761b905ff3dcfdfe61f4a9d4f287b26f6b8fcb
Andy Brody <brodygov@users.noreply.github.com>
2019-07-02 16:59:09 -0400

    [LG-1488] Move account reset notice into job_configurations. (#3062)
    * Move account reset notice into job_configurations.
    
    As part of LG-1113, move the account reset email notification job out of
    the recurring job controllers and into the built-in app job
    configurations.
    
    **Why**: We are moving away from triggering recurring jobs via lambdas
    to instead run long-running background jobs using the
    idp-jobs/job_configurations.rb framework, which allows each idp server
    to pull jobs and run them as needed, without any external moving parts.
    
    This makes adding a new recurring job entirely self-contained within the
    identity-idp repo, so no interaction with AWS or lambdas or terraform or
    any other repos is needed.
    
    **How**:
    - Remove lib/lambdas/account_reset_lambda.rb.
    - Add a new job configuration for the account reset email job to
      `config/initializers/job_configurations.rb`, running every 5 minutes.
    - Change the deprecated account reset controller to return HTTP 410, to
      indicate that the endpoint should not be used.
    - Add tests of all the job_configurations.rb and fix tests for the
      changes.
    
    * Add timestamps to the default rails log.
    
    **Why**: We currently output logs to a file. We have no idea when these
    events happen unless we add a timestamp.
    
    **How**: Create a custom log formatter class that overrides the default
    activesupport formatter. Check if the input appears to be JSON based on
    leading/trailing `{}`, and output without any timestamp if so. That
    preserves log parsing compatibility with the existing analytics ETL
    pipelines.
    
    * Add a few log formatter tests.

---
+++ spec/config/initializers/job_configurations.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe JobRunner::Runner do
+  describe '.configurations' do
+    it 'has the GPO letter job' do
+      job = JobRunner::Runner.configurations.find { |c| c.name == 'Send GPO letter' }
+      expect(job).to be_instance_of(JobRunner::JobConfiguration)
+      expect(job.interval).to eq 24 * 60 * 60
+
+      stub = instance_double(UspsConfirmationUploader)
+      expect(UspsConfirmationUploader).to receive(:new).and_return(stub)
+      expect(stub).to receive(:run).and_return('the GPO test worked')
+
+      expect(job.callback.call).to eq 'the GPO test worked'
+    end
+
+    it 'runs the account reset job' do
+      job = JobRunner::Runner.configurations.find { |c| c.name == 'Account reset notice' }
+      expect(job).to be_instance_of(JobRunner::JobConfiguration)
+      expect(job.interval).to eq 300
+
+      service = instance_double(AccountReset::GrantRequestsAndSendEmails)
+      expect(AccountReset::GrantRequestsAndSendEmails).to receive(:new).and_return(service)
+      expect(service).to receive(:call).and_return('the reset test worked')
+
+      expect(job.callback.call).to eq 'the reset test worked'
+    end
+  end
+end
