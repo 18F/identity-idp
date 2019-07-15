ca2806897ec8960ef56a56dc36c63a4242ae9e71
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-07-15 12:18:32 -0400

    Fix specs and code

--- spec/views/account_reset/request/show.html.slim_spec.rb
+++ spec/views/account_reset/request/show.html.slim_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe 'account_reset/request/show.html.slim' do
+  before do
+    user = create(:user, :signed_up)
+    allow(view).to receive(:current_user).and_return(user)
+  end
+
   it 'has a localized title' do
     expect(view).to receive(:title).with(t('account_reset.request.title'))
 
