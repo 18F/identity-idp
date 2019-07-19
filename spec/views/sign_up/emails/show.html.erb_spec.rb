231af25bf20478b90f8595f375d9130534710d0a
Alison Dowski <alison.dowski@gsa.gov>
2019-07-19 16:34:38 -0400

    erb

--- spec/views/sign_up/emails/show.html.erb_spec.rb
+++ spec/views/sign_up/emails/show.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'sign_up/emails/show.html.slim' do
+describe 'sign_up/emails/show.html.erb' do
   before do
     allow(view).to receive(:email).and_return('foo@bar.com')
     @register_user_email_form = RegisterUserEmailForm.new
