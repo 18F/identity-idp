fa9f0226afe1d0d732fd8a2c3e512d8f9db1fd98
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-07-22 22:20:01 -0400

    more coverage

--- app/services/funnel/registration/range_submitted_count.rb
+++ app/services/funnel/registration/range_submitted_count.rb
@@ -2,7 +2,7 @@ module Funnel
   module Registration
     class RangeSubmittedCount
       def self.call(start, finish)
-        RegistrationFunnel.where('? < submitted_at AND submitted_at > ?', start, finish).count
+        RegistrationFunnel.where('? < submitted_at AND submitted_at < ?', start, finish).count
       end
     end
   end
