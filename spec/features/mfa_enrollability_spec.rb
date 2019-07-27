eed5efcedbb9603891b7daf41149bbdf4c682f5d
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-07-26 23:38:37 -0400

    Fix spec

--- spec/features/mfa_enrollability_spec.rb
+++ spec/features/mfa_enrollability_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 describe 'MFA enrollability' do
   let(:user) { create(:user, :signed_up, :with_authentication_app) }
 
-  it 'backup codes are not available for backup mfa after being chosen as primary' do
-    sign_up_and_set_password
-
-    select_2fa_option('backup_code')
-
-    click_on 'Continue'
-    click_continue
-
-    expect(page).to have_selector('#two_factor_options_form_selection_backup_code_only', count: 1)
-    expect(page).to have_current_path(two_factor_options_path)
-  end
-
   it 'does not allow choosing totp as backup auth method after it is used as primary' do
     sign_up_and_set_password
 
