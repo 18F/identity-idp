4106ed6fc170a5c7dec2da3c47dea0fbd396efa4
Andy Brody <andrew.brody@gsa.gov>
2019-08-02 18:35:35 -0400

    Add a way to disable recurring jobs using config.
    **Why**: We may encounter situations where a recurring job is failing
    and we need to rapidly disable it, or where a job is failing and we
    don't care so we want health checks to stop failing.
    
    **How**: Add a new Figaro key, `recurring_jobs_disabled_names`. This key
    is expected to contain JSON representing an array of string job names.
    Any job names present in this list will be ignored when
    JobRunner::Runner.add_config is called to add a job with a matching
    name. This required refactoring the process to add job configs so that
    we have an easy place to add such validation.

--- app/services/job_runner/runner.rb
+++ app/services/job_runner/runner.rb
@@ -1,7 +1,53 @@
 module JobRunner
+  # The Runner class manages the list of known configurations and is
+  # responsible for running all of them in turn when {#run} is called.
   class Runner
+    # The list of known job configurations. Calling {#run} will execute these
+    # jobs according to their schedules. Add new configurations to this list by
+    # calling {.add_config} aka {.add_configuration}.
+    #
+    # @return [Array<JobRunner::JobConfiguration>]
+    #
     def self.configurations
       @configurations ||= []
+      # dup and freeze so that callers have to use add_config to add jobs
+      @configurations.dup.freeze
+    end
+
+    # Empty the job configurations list.
+    def self.clear_configurations
+      @configurations = []
+    end
+
+    # Load the disabled job list from config. Provides a way to disable jobs
+    # without changing code.
+    def self.disabled_jobs
+      @disabled_jobs ||= JSON.parse(Figaro.env.recurring_jobs_disabled_names!)
+    end
+
+    # Add a new job configuration to the list of jobs to run. This is the
+    # primary entrypoint for users to add new jobs.
+    #
+    # @param [JobRunner::JobConfiguration] job_config
+    #
+    # @see .configurations
+    #
+    def self.add_configuration(job_config)
+      unless job_config.is_a?(JobRunner::JobConfiguration)
+        raise ArgumentError, 'job_config must be a JobRunner::JobConfiguration'
+      end
+
+      if disabled_jobs.include?(job_config.name)
+        Rails.logger.warn("JobRunner: skipping disabled job: #{job_config.name.inspect}")
+        return false
+      end
+
+      @configurations ||= []
+      @configurations << job_config
+    end
+
+    class << self
+      alias add_config add_configuration
     end
 
     def run
