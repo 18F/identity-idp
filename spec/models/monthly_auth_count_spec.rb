28a1f4eb866131f86a2248f3dff7141a68c77611
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-08-06 00:43:27 -0400

    LG-1717 Track total and unique monthly authentications for SPs

---
+++ spec/models/monthly_auth_count_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe MonthlyAuthCount do
+  let(:user_id) { 1 }
+  let(:issuer) { 'foo' }
+
+  describe '.increment' do
+    it 'sets the monthly count to 1' do
+      year_month = current_year_month
+      MonthlyAuthCount.increment(user_id, issuer)
+
+      monthly_auth_count = MonthlyAuthCount.first
+      expect(monthly_auth_count.user_id).to eq(user_id)
+      expect(monthly_auth_count.issuer).to eq(issuer)
+      expect(monthly_auth_count.year_month).to eq(year_month)
+      expect(monthly_auth_count.auth_count).to eq(1)
+    end
+
+    it 'updates the monthly count' do
+      year_month = current_year_month
+      MonthlyAuthCount.increment(user_id, issuer)
+      MonthlyAuthCount.increment(user_id, issuer)
+
+      monthly_auth_count = MonthlyAuthCount.first
+      expect(monthly_auth_count.user_id).to eq(user_id)
+      expect(monthly_auth_count.issuer).to eq(issuer)
+      expect(monthly_auth_count.year_month).to eq(year_month)
+      expect(monthly_auth_count.auth_count).to eq(2)
+    end
+  end
+
+  def current_year_month
+    Time.zone.today.strftime('%Y%m')
+  end
+end
