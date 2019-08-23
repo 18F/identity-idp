7b30217a156277cc920df2646e686071145b73e3
Clara Bridges <clara.bridges@gsa.gov>
2019-08-23 15:53:12 -0400

    fixed phone number being validate on edit

--- app/validators/form_phone_validator.rb
+++ app/validators/form_phone_validator.rb
@@ -7,9 +7,10 @@ module FormPhoneValidator
               phone: {
                 message: :improbable_phone,
                 country_specifier: ->(form) { form.international_code },
-              }
+              },
+              on: :create
     validates :international_code, inclusion: {
       in: PhoneNumberCapabilities::INTERNATIONAL_CODES.keys,
-    }
+    }, on: :create
   end
 end
