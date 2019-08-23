e5f859d1cc0c0fe394cef304848b6ca097cf4644
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-08-23 01:42:49 -0400

    LG-1736 Support entry point for proofing with either State ID or CAC

---
+++ spec/features/idv/cac/use_cac_step_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'use cac step' do
+  include CacProofingHelper
+  include DocAuthHelper
+
+  let(:use_cac_content) do
+    strip_tags(t('doc_auth.info.use_cac_html', link: t('doc_auth.info.use_cac_link')))
+  end
+
+  it 'cac proofing option is available if cac proofing is enabled' do
+    enable_cac_proofing
+    sign_in_and_2fa_user
+    complete_doc_auth_steps_before_upload_step
+
+    expect(page).to have_content use_cac_content
+  end
+
+  it 'cac proofing option is not available if cac proofing is disabled' do
+    sign_in_and_2fa_user
+    complete_doc_auth_steps_before_upload_step
+
+    expect(page).to_not have_content use_cac_content
+  end
+end
