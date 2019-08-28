7064084075b237d8678efb36b4f7f23480902bde
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-08-28 12:02:10 -0400

    Add pinpoint configs to telephony initializers (#3242)
    **Why**: So we can configure pinpoint with the application.yml

--- config/initializers/telephony.rb
+++ config/initializers/telephony.rb
@@ -9,4 +9,8 @@ Telephony.config do |c|
   c.twilio_voice_callback_base_url = "https://#{Figaro.env.domain_name}/api/twilio/voice"
   c.twilio_timeout = Figaro.env.twilio_timeout.to_i unless Figaro.env.twilio_timeout.nil?
   c.twilio_record_voice = Figaro.env.twilio_record_voice == 'true'
+  c.pinpoint_region = Figaro.env.aws_region
+  c.pinpoint_application_id = Figaro.env.pinpoint_application_id
+  c.pinpoint_shortcode = Figaro.env.pinpoint_shortcode
+  c.pinpoint_longcode_pool = JSON.parse(Figaro.env.pinpoint_longcode_pool || '[]')
 end
