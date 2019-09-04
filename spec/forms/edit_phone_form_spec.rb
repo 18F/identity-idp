df256068f8d3c49a7cf24fdccd2b58a616e18d6c
Clara Bridges <clara.bridges@gsa.gov>
2019-09-04 09:49:26 -0400

    added more relevant tests

--- spec/forms/edit_phone_form_spec.rb
+++ spec/forms/edit_phone_form_spec.rb
@@ -6,109 +6,16 @@ describe EditPhoneForm do
   let(:user) { build(:user, :signed_up) }
   let(:params) do
     {
-      phone: '703-555-5000',
-      international_code: 'US',
       otp_delivery_preference: 'sms',
     }
   end
   subject { EditPhoneForm.new(user, MfaContext.new(user).phone_configurations.first) }
 
-  describe 'phone validation' do
-    it do
-      if subject.phone_configuration.blank?
-        should validate_inclusion_of(:international_code).
-          in_array(PhoneNumberCapabilities::INTERNATIONAL_CODES.keys)
-      end
-    end
-
-    it 'validates that the number matches the requested international code' do
-      params[:phone] = '123 123 1234'
-      params[:international_code] = 'MA'
-      result = subject.submit(params)
-
-      expect(result).to be_kind_of(FormResponse)
-      if subject.phone_configuration.blank?
-        expect(result.success?).to eq(false)
-        expect(result.errors).to include(:phone)
-      end
-    end
-  end
-
   describe '#submit' do
-    context 'when phone is valid' do
-      it 'is valid' do
-        result = subject.submit(params)
-
-        expect(result).to be_kind_of(FormResponse)
-        expect(result.success?).to eq(true)
-        expect(result.errors).to be_empty
-      end
-
-      it 'includes otp preference in the form response extra' do
-        result = subject.submit(params)
-
-        expect(result.extra).to eq(
-          otp_delivery_preference: params[:otp_delivery_preference],
-        )
-      end
-
-      it 'does not update the user phone attribute' do
-        user = create(:user)
-        subject = EditPhoneForm.new(user, MfaContext.new(user).phone_configurations.first)
-        params[:phone] = '+1 504 444 1643'
-
-        subject.submit(params)
-
-        user.reload
-        expect(MfaContext.new(user).phone_configurations).to be_empty
-      end
-
-      it 'preserves the format of the submitted phone number if phone is invalid' do
-        if subject.phone_configuration.blank?
-          params[:phone] = '555-555-5000'
-          params[:international_code] = 'MA'
-
-          result = subject.submit(params)
-
-          expect(result.success?).to eq(false)
-          expect(subject.phone).to eq('555-555-5000')
-        end
-      end
-    end
-
-    context 'when otp_delivery_preference is voice and phone number does not support voice' do
-      let(:unsupported_phone) { '242-327-0143' }
-      let(:params) do
-        {
-          phone: unsupported_phone,
-          international_code: 'US',
-          otp_delivery_preference: 'voice',
-        }
-      end
-
-      it 'is invalid' do
-        result = subject.submit(params)
-
-        expect(result.success?).to eq(false)
-      end
-    end
-
     context 'when otp_delivery_preference is not voice or sms' do
       let(:params) do
         {
-          phone: '703-555-1212',
-          international_code: 'US',
           otp_delivery_preference: 'foo',
-        }
-      end
-    end
-
-    context 'when otp_delivery_preference is empty' do
-      let(:params) do
-        {
-          phone: '703-555-1212',
-          international_code: 'US',
-          otp_delivery_preference: '',
         }
       end
 
@@ -121,30 +28,30 @@ describe EditPhoneForm do
       end
     end
 
-    context 'when otp_delivery_preference param is not present' do
-      let(:params) do
-        {
-          phone: '703-555-1212',
-          international_code: 'US',
-        }
-      end
-
-      it 'is valid' do
+    context 'when phone is valid' do
+      it 'includes otp preference in the form response extra' do
         result = subject.submit(params)
 
-        expect(result.success?).to eq(true)
+        expect(result.extra).to eq(
+                                  otp_delivery_preference: params[:otp_delivery_preference],
+                                  )
       end
     end
 
-    it 'does not raise inclusion errors for Norwegian phone numbers' do
-      # ref: https://github.com/18F/identity-private/issues/2392
-      params[:phone] = '21 11 11 11'
-      params[:international_code] = 'NO'
-      result = subject.submit(params)
+    context 'when otp_delivery_preference is empty' do
+      let(:params) do
+        {
+          otp_delivery_preference: '',
+        }
+      end
 
-      expect(result).to be_kind_of(FormResponse)
-      expect(result.success?).to eq(true)
-      expect(result.errors).to be_empty
+      it 'is invalid' do
+        result = subject.submit(params)
+
+        expect(result.success?).to eq(false)
+        expect(result.errors[:otp_delivery_preference].first).
+          to eq 'is not included in the list'
+      end
     end
 
     it 'revokes the users rememder device sessions' do
