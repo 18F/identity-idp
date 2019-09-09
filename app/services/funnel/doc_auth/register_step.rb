157118660c9eaae5dd1ae6a57c177e6da910040a
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-09 01:35:16 -0400

    Fix spec

--- app/services/funnel/doc_auth/register_step.rb
+++ app/services/funnel/doc_auth/register_step.rb
@@ -22,6 +22,7 @@ module Funnel
       ].freeze
 
       def self.call(user_id, token, step_type, success)
+        return unless user_id
         doc_auth_log = DocAuthLog.find_or_create_by(user_id: user_id)
         return unless TOKEN_WHITELIST.index(token.to_sym)
         klass = step_type == :update ?  RegisterSubmitStep : RegisterViewStep
