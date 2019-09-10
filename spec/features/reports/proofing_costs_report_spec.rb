ee99e6ce88b49877097342bbbedf6af39c3021c2
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-10 00:39:09 -0400

    Coverage

--- spec/features/reports/proofing_costs_report_spec.rb
+++ spec/features/reports/proofing_costs_report_spec.rb
@@ -4,7 +4,7 @@ feature 'Proofing Costs report' do
   include IdvStepHelper
   include DocAuthHelper
 
-  let(:subject) { Db::ProofingCost::ProofingCostsSummary }
+  let(:subject) { Reports::ProofingCostsReport }
   let(:user) { create(:user, :signed_up) }
   let(:user2) { create(:user, :signed_up) }
   let(:summary1) do
@@ -29,19 +29,19 @@ feature 'Proofing Costs report' do
   end
 
   it 'works for no records' do
-    expect(subject.new.call).to eq({})
+    expect(JSON.parse(subject.new.call)).to eq({})
   end
 
   it 'works for one flow' do
     complete_doc_auth_steps_before_doc_success_step(user)
 
-    expect(subject.new.call).to eq(doc_success_funnel.merge(summary1))
+    expect(JSON.parse(subject.new.call)).to eq(doc_success_funnel.merge(summary1))
   end
 
   it 'works for two flows' do
     complete_doc_auth_steps_before_doc_success_step(user)
     complete_doc_auth_steps_before_doc_success_step(user2)
 
-    expect(subject.new.call).to eq(doc_success_funnel.merge(summary2))
+    expect(JSON.parse(subject.new.call)).to eq(doc_success_funnel.merge(summary2))
   end
 end
