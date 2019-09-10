710645c61891d28f21dc8032fe50ff2a8f84aebc
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-10 07:17:50 -0400

    Lint

--- app/services/funnel/doc_auth/reset_steps.rb
+++ app/services/funnel/doc_auth/reset_steps.rb
@@ -5,12 +5,8 @@ module Funnel
         doc_auth_log = DocAuthLog.find_or_create_by(user_id: user_id)
         DocAuthLog.new.attributes.keys.each do |method|
           assignment = "#{method}=".to_sym
-          case method
-          when /at$/
-            doc_auth_log.send(assignment, nil)
-          when /count$/
-            doc_auth_log.send(assignment, 0)
-          end
+          doc_auth_log.send(assignment, nil) if method =~ /at$/
+          doc_auth_log.send(assignment, 0) if method =~ /count$/
         end
         doc_auth_log.save
       end
