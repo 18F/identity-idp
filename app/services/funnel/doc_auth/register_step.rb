22de09d716b02f61e9d961cf131107aff91e5a20
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-10 07:42:14 -0400

    Lint

--- app/services/funnel/doc_auth/register_step.rb
+++ app/services/funnel/doc_auth/register_step.rb
@@ -20,12 +20,16 @@ module Funnel
         usps_address
         personal_key
       ].freeze
+      STEP_TYPE_TO_CLASS = {
+        update:  RegisterSubmitStep,
+        view: RegisterViewStep
+      }.freeze
 
       def self.call(user_id, token, step_type, success)
         return unless user_id
         doc_auth_log = DocAuthLog.find_or_create_by(user_id: user_id)
         return unless TOKEN_WHITELIST.index(token.to_sym)
-        klass = step_type == :update ?  RegisterSubmitStep : RegisterViewStep
+        klass = STEP_TYPE_TO_CLASS[step_type]
         klass.call(doc_auth_log, token, success)
       end
     end
