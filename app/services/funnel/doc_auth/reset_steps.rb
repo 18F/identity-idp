84167e1bc30b8b3b1b6a97fa51756a6fabc75d78
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-12 06:46:07 -0400

    Fix

--- app/services/funnel/doc_auth/reset_steps.rb
+++ app/services/funnel/doc_auth/reset_steps.rb
@@ -1,9 +1,12 @@
 module Funnel
   module DocAuth
     class ResetSteps
+      SKIP_FIELDS = %w[id user_id created_at updated_at].freeze
+
       def self.call(user_id)
         doc_auth_log = DocAuthLog.find_or_create_by(user_id: user_id)
         DocAuthLog.new.attributes.keys.each do |attribute|
+          next if SKIP_FIELDS.index(attribute)
           doc_auth_log[attribute] = nil if attribute =~ /at$/
           doc_auth_log[attribute] = 0 if attribute =~ /count$/
         end
