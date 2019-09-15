0ed9bc906212d68f0ec66ac780dcb8d085415291
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-15 17:18:04 -0400

    More coverage

--- spec/features/reports/sp_success_rate_report_spec.rb
+++ spec/features/reports/sp_success_rate_report_spec.rb
@@ -7,10 +7,26 @@ feature 'scheduler runs report' do
     expect(Reports::SpSuccessRateReport.new.call).to eq([].to_json)
   end
 
-  it 'works for users that have landed but not signed in' do
+  it 'works for users that have landed from sp and left' do
     visit_idp_from_sp_with_loa1(:oidc)
 
-    results = [{issuer: 'urn:gov:gsa:openidconnect:sp:server', return_rate: 0.0}]
+    results = [{ issuer: 'urn:gov:gsa:openidconnect:sp:server', return_rate: 0.0 }]
     expect(Reports::SpSuccessRateReport.new.call).to eq(results.to_json)
+  end
+
+  it 'works for users that have landed from sp signed in and returned to sp' do
+    visit_idp_from_sp_and_back_again
+
+    results = [{ issuer: 'urn:gov:gsa:openidconnect:sp:server', return_rate: 1.0 }]
+    expect(Reports::SpSuccessRateReport.new.call).to eq(results.to_json)
+  end
+
+  def visit_idp_from_sp_and_back_again
+    user = create(:user, :signed_up)
+    visit_idp_from_sp_with_loa1(:oidc)
+    fill_in_credentials_and_submit(user.email, user.password)
+    fill_in_code_with_last_phone_otp
+    click_submit_default
+    click_continue
   end
 end
