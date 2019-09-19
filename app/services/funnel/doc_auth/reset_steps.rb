9f89563acc4251002ceac75ff948be0e85246935
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-19 13:08:25 -0400

    Remove extra doc auth funnel creates

--- app/services/funnel/doc_auth/reset_steps.rb
+++ app/services/funnel/doc_auth/reset_steps.rb
@@ -4,7 +4,8 @@ module Funnel
       SKIP_FIELDS = %w[id user_id created_at updated_at].freeze
 
       def self.call(user_id)
-        doc_auth_log = DocAuthLog.find_or_create_by(user_id: user_id)
+        doc_auth_log = DocAuthLog.find_by(user_id: user_id)
+        return unless doc_auth_log
         DocAuthLog.new.attributes.keys.each do |attribute|
           next if SKIP_FIELDS.index(attribute)
           doc_auth_log[attribute] = nil if attribute =~ /at$/
