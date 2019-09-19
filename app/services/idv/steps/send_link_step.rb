6df071b65d40e6e2cdcea884f2716a61e94ba82c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-09-19 17:25:22 -0400

    LG-1954 Format the hybrid flow phone number
    **Why**: It needs to be formatted or Pinpoint won't send the message

--- app/services/idv/steps/send_link_step.rb
+++ app/services/idv/steps/send_link_step.rb
@@ -5,7 +5,7 @@ module Idv
         capture_doc = CaptureDoc::CreateRequest.call(current_user.id)
         begin
           Telephony.send_doc_auth_link(
-            to: permit(:phone)[:phone],
+            to: formatted_destination_phone,
             link: link(capture_doc.request_token),
           )
         rescue Telephony::TelephonyError => err
@@ -17,6 +17,11 @@ module Idv
 
       def form_submit
         Idv::PhoneForm.new(previous_params: {}, user: current_user).submit(permit(:phone))
+      end
+
+      def formatted_destination_phone
+        raw_phone = permit(:phone)[:phone]
+        PhoneFormatter.format(raw_phone, country_code: 'US')
       end
 
       def link(token)
