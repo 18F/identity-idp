c2c31371afa72405dbfe863c2263134bf0c21322
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-09-25 16:38:57 -0400

    Lint

--- app/services/idv/phone_step.rb
+++ app/services/idv/phone_step.rb
@@ -17,7 +17,7 @@ module Idv
     end
 
     def failure_reason
-      return :fail if throttled?
+      return :fail if Throttler::IsThrottled.call(idv_session.current_user.id, :idv_resolution)
       return :timeout if idv_result[:timed_out]
       return :jobfail if idv_result[:exception].present?
       return :warning if idv_result[:success] != true
@@ -26,10 +26,6 @@ module Idv
     private
 
     attr_accessor :idv_session, :step_params, :idv_result
-
-    def throttled?
-      Throttler::IsThrottled.call(idv_session.current_user.id, :idv_resolution)
-    end
 
     def proof_address
       self.idv_result = Idv::Agent.new(applicant).proof(:address)
