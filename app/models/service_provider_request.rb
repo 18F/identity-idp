ec824ee0163d490ecfda8fc639734c4b5ec630e6
Clara Bridges <clara.bridges@gsa.gov>
2019-09-25 18:52:49 -0400

    added override for create to ensure loa is being written to as well as ial

--- app/models/service_provider_request.rb
+++ app/models/service_provider_request.rb
@@ -1,4 +1,9 @@
 class ServiceProviderRequest < ApplicationRecord
+  def self.create(attributes)
+    attributes[:loa] = (attributes[:ial] == 2 ? 3 : 1)
+    super
+  end
+
   def self.from_uuid(uuid)
     find_by(uuid: uuid) || NullServiceProviderRequest.new
   rescue ArgumentError # a null byte in the uuid will raise this
