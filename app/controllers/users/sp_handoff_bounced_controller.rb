a3de5230a529f573de2d73bd62e2fb3533e25b11
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-10-02 14:46:02 -0400

    Oops

--- app/controllers/users/sp_handoff_bounced_controller.rb
+++ app/controllers/users/sp_handoff_bounced_controller.rb
@@ -5,8 +5,8 @@ module Users
       @sp_name = I18n.t('instructions.sp_handoff_bounced_with_no_sp')
       return if issuer.blank?
       service_provider = ServiceProvider.from_issuer(issuer)
-      @sp_name = 'USAJobs' #service_provider.friendly_name
-      @sp_link = "http://www.google.com" #service_provider.return_to_sp_url
+      @sp_name = service_provider.friendly_name
+      @sp_link = service_provider.return_to_sp_url
     end
   end
 end
