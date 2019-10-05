e9281f0d23548f74dd3a466821a09fa6f16947e4
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-10-05 11:20:24 -0400

    Fix spec

--- app/controllers/idv/review_controller.rb
+++ app/controllers/idv/review_controller.rb
@@ -1,13 +1,13 @@
 module Idv
   class ReviewController < ApplicationController
+    before_action :personal_key_confirmed
+
     include IdvStepConcern
     include PhoneConfirmation
 
     before_action :confirm_idv_steps_complete
     before_action :confirm_idv_phone_confirmed
     before_action :confirm_current_password, only: [:create]
-
-    before_action :personal_key_confirmed
 
     def confirm_idv_steps_complete
       return redirect_to(idv_session_url) unless idv_profile_complete?
@@ -87,6 +87,7 @@ module Idv
     end
 
     def personal_key_confirmed
+      return unless current_user
       return unless current_user.active_profile.present? && need_personal_key_confirmation?
       redirect_to idv_confirmations_url
     end
