7ce15f5a3b4ec55df5aee6acf85f9944c7d3c87c
Aaron Chapman <aaron.chapman@gsa.gov>
2019-10-08 08:57:55 -0400

    Hide docauth error and info after new image selected (#3335)
    **Why**: So the user does not get confused and think there is an issue with the image they just uploaded

--- app/javascript/packs/image-preview.js
+++ app/javascript/packs/image-preview.js
@@ -5,13 +5,15 @@ function imagePreview() {
     document.getElementById('doc_auth_image').click();
   });
   $('#doc_auth_image').on('change', function(event) {
+    $('.simple_form .alert-error').hide();
+    $('.simple_form .alert-notice').hide();
     const files = event.target.files;
     const image = files[0];
     const reader = new FileReader();
     reader.onload = function(file) {
       const img = new Image();
       img.onload = function () {
-        const displayWidth = '500';
+        const displayWidth = '460';
         const ratio = (this.height / this.width);
         img.width = displayWidth;
         img.height = (displayWidth * ratio);
