62fc1defdd637c5067b706161d826a44e766c3d7
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-10-23 14:41:56 -0400

    fix another test

--- spec/services/reports/agency_user_counts_report_spec.rb
+++ spec/services/reports/agency_user_counts_report_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe Reports::AgencyUserCountsReport do
   subject { described_class.new }
 
-  let(:agency) { 'USDS' }
+  let(:agency) { 'EOP' }
 
   it 'is empty' do
     expect(subject.call).to eq('[]')
   end
 
   it 'returns the total user counts per agency' do
-    Agency.create(id: 3, name: 'USDS') unless Agency.find_by(id: 3)
+    Agency.create(id: 3, name: 'EOP') unless Agency.find_by(id: 3)
     AgencyIdentity.create(user_id: 1, agency_id: 3, uuid: 'foo1')
     AgencyIdentity.create(user_id: 2, agency_id: 3, uuid: 'foo2')
     result = [{ agency: agency, total: 2 }].to_json
