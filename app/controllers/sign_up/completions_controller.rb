f23b1d6255efcd8dd87ea0ddf85c86b02ffb4c34
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-10-26 13:41:12 -0400

    LG-1832 Display attributes on consent screen

--- app/controllers/sign_up/completions_controller.rb
+++ app/controllers/sign_up/completions_controller.rb
@@ -1,3 +1,4 @@
+# :reek:TooManyMethods
 module SignUp
   class CompletionsController < ApplicationController
     include SecureHeadersConcern
@@ -8,6 +9,7 @@ module SignUp
 
     def show
       @view_model = view_model
+      @pii = pii_to_displayable_attributes
       if needs_completions_screen?
         analytics.track_event(
           Analytics::USER_REGISTRATION_AGENCY_HANDOFF_PAGE_VISIT,
@@ -58,9 +60,7 @@ module SignUp
     end
 
     def decider
-      CompletionsDecider.new(
-        user_agent: request.user_agent, request_url: sp_session[:request_url],
-      )
+      CompletionsDecider.new(user_agent: request.user_agent, request_url: sp_session[:request_url])
     end
 
     def sign_user_out_and_instruct_to_go_back_to_mobile_app
@@ -79,16 +79,43 @@ module SignUp
     end
 
     def track_completion_event(last_page)
-      analytics.track_event(
-        Analytics::USER_REGISTRATION_COMPLETE,
-        analytics_attributes(last_page),
-      )
+      analytics.track_event(Analytics::USER_REGISTRATION_COMPLETE, analytics_attributes(last_page))
       GoogleAnalyticsMeasurement.new(
         category: 'registration',
         event_action: 'completion',
         method: last_page,
         client_id: ga_cookie_client_id,
       ).send_event
+    end
+
+    def pii
+      @pii ||= user_session['idv']['applicant'].symbolize_keys
+    end
+
+    def address
+      addr = pii[:address2]
+      addr = addr ? "#{addr} " : ''
+      "#{pii[:address1]} #{addr}#{pii[:city]}, #{pii[:state]} #{pii[:zipcode]}"
+    end
+
+    def full_name
+      "#{pii[:first_name]} #{pii[:last_name]}"
+    end
+
+    def email
+      EmailContext.new(current_user).last_sign_in_email_address.email
+    end
+
+    def pii_to_displayable_attributes
+      {
+        full_name: full_name,
+        social_security_number: pii[:ssn],
+        address: address,
+        birthdate: pii[:dob].to_date.to_formatted_s(:long),
+        phone: pii[:phone].to_s,
+        email: email,
+        x509_subject: current_user.x509_dn_uuid,
+      }
     end
   end
 end
