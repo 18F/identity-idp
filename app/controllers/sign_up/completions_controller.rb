de5f9f7d7d63945b3ccca53d4475422d3ced333e
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-10-29 10:24:40 -0400

    LG-1832 Display attributes on consent screen (#3366)

--- app/controllers/sign_up/completions_controller.rb
+++ app/controllers/sign_up/completions_controller.rb
@@ -1,3 +1,5 @@
+# :reek:TooManyMethods
+# rubocop:disable Metrics/ClassLength
 module SignUp
   class CompletionsController < ApplicationController
     include SecureHeadersConcern
@@ -9,6 +11,7 @@ module SignUp
     def show
       @view_model = view_model
       if needs_completions_screen?
+        @pii = displayable_attributes
         analytics.track_event(
           Analytics::USER_REGISTRATION_AGENCY_HANDOFF_PAGE_VISIT,
           analytics_attributes(''),
@@ -58,9 +61,7 @@ module SignUp
     end
 
     def decider
-      CompletionsDecider.new(
-        user_agent: request.user_agent, request_url: sp_session[:request_url],
-      )
+      CompletionsDecider.new(user_agent: request.user_agent, request_url: sp_session[:request_url])
     end
 
     def sign_user_out_and_instruct_to_go_back_to_mobile_app
@@ -79,10 +80,7 @@ module SignUp
     end
 
     def track_completion_event(last_page)
-      analytics.track_event(
-        Analytics::USER_REGISTRATION_COMPLETE,
-        analytics_attributes(last_page),
-      )
+      analytics.track_event(Analytics::USER_REGISTRATION_COMPLETE, analytics_attributes(last_page))
       GoogleAnalyticsMeasurement.new(
         category: 'registration',
         event_action: 'completion',
@@ -90,5 +88,49 @@ module SignUp
         client_id: ga_cookie_client_id,
       ).send_event
     end
+
+    def pii
+      @pii ||= JSON.parse(user_session['decrypted_pii']).symbolize_keys
+    end
+
+    def address
+      addr = pii[:address2]
+      addr = addr ? "#{addr} " : ''
+      "#{pii[:address1]} #{addr}#{pii[:city]}, #{pii[:state]} #{pii[:zipcode]}"
+    end
+
+    def full_name
+      "#{pii[:first_name]} #{pii[:last_name]}"
+    end
+
+    def email
+      EmailContext.new(current_user).last_sign_in_email_address.email
+    end
+
+    def displayable_attributes
+      return pii_to_displayable_attributes if user_session['decrypted_pii'].present?
+      {
+        email: email,
+        x509_subject: current_user.x509_dn_uuid,
+      }
+    end
+
+    def dob
+      pii_dob = pii[:dob]
+      pii_dob ? pii_dob.to_date.to_formatted_s(:long) : ''
+    end
+
+    def pii_to_displayable_attributes
+      {
+        full_name: full_name,
+        social_security_number: pii[:ssn],
+        address: address,
+        birthdate: dob,
+        phone: pii[:phone].to_s,
+        email: email,
+        x509_subject: current_user.x509_dn_uuid,
+      }
+    end
   end
 end
+# rubocop:enable Metrics/ClassLength
--- spec/support/idv_examples/sp_requested_attributes.rb
+++ spec/support/idv_examples/sp_requested_attributes.rb
@@ -3,6 +3,9 @@ shared_examples 'sp requesting attributes' do |sp|
   include IdvHelper
 
   let(:user) { user_with_2fa }
+  let(:good_ssn) { '666-66-1234' }
+  let(:profile) { create(:profile, :active, :verified, user: user, pii: saved_pii) }
+  let(:saved_pii) { DocAuthHelper::ACUANT_RESULTS_TO_PII.merge(ssn: good_ssn) }
 
   context 'visiting an SP for the first time' do
     it 'requires the user to verify the attributes submitted to the SP' do
@@ -22,11 +25,15 @@ shared_examples 'sp requesting attributes' do |sp|
 
       within('.requested-attributes') do
         expect(page).to have_content t('help_text.requested_attributes.email')
+        expect(page).to have_content user.email
         expect(page).to_not have_content t('help_text.requested_attributes.address')
         expect(page).to_not have_content t('help_text.requested_attributes.birthdate')
         expect(page).to have_content t('help_text.requested_attributes.full_name')
+        expect(page).to have_content 'José One'
         expect(page).to have_content t('help_text.requested_attributes.phone')
+        expect(page).to have_content '2025551212'
         expect(page).to have_content t('help_text.requested_attributes.social_security_number')
+        expect(page).to have_content '666-66-1234'
       end
     end
   end
@@ -56,6 +63,31 @@ shared_examples 'sp requesting attributes' do |sp|
         expect(current_url).to include('http://localhost:7654/auth/result')
       elsif sp == :saml
         expect(current_url).to include(api_saml_auth2019_url)
+      end
+    end
+  end
+
+  context 'siging in from an SP after creating a verified account directly' do
+    it 'displays the correct values' do
+      create(:profile, :active, :verified, user: user, pii: saved_pii)
+      visit_idp_from_sp_with_ial2(sp)
+      sign_in_user(user)
+      fill_in_code_with_last_phone_otp
+      click_submit_default
+
+      expect(current_path).to eq(sign_up_completed_path)
+
+      within('.requested-attributes') do
+        expect(page).to have_content t('help_text.requested_attributes.email')
+        expect(page).to have_content user.email
+        expect(page).to_not have_content t('help_text.requested_attributes.address')
+        expect(page).to_not have_content t('help_text.requested_attributes.birthdate')
+        expect(page).to have_content t('help_text.requested_attributes.full_name')
+        expect(page).to have_content 'Jane Doe'
+        expect(page).to have_content t('help_text.requested_attributes.phone')
+        expect(page).to have_content '456'
+        expect(page).to have_content t('help_text.requested_attributes.social_security_number')
+        expect(page).to have_content '666-66-1234'
       end
     end
   end
