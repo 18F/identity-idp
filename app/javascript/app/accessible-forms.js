f9f6b1e92201fef80256771b505bb0f2822799f5
Aaron Chapman <aaron.chapman@gsa.gov>
2019-10-31 10:46:40 -0400

    added read-after-submit class filter for excluding forms

--- app/javascript/app/accessible-forms.js
+++ app/javascript/app/accessible-forms.js
@@ -8,7 +8,9 @@ function preventScreenRead() {
 
 // attaches a submit listener to every form
 function accessibleForms() {
-  const forms = document.querySelectorAll('form');
+  // if you do not want aria-hidden added to the body after submit,
+  // then add the read-after-submit class to the form
+  const forms = document.querySelectorAll('form:not(.read-after-submit)');
   if (forms) {
     [].slice.call(forms).forEach((element) => {
       element.addEventListener('submit', preventScreenRead);
