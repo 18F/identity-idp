a1a9e5666c7741ae1c1e8feada54f9f5250fbcad
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-11-01 13:59:42 -0400

    recovery flow bugfix

--- app/controllers/idv/session_errors_controller.rb
+++ app/controllers/idv/session_errors_controller.rb
@@ -2,6 +2,7 @@ module Idv
   class SessionErrorsController < ApplicationController
     include IdvSession
 
+    before_action :confirm_two_factor_authenticated_or_recovery
     before_action :confirm_idv_session_step_needed
 
     def warning
@@ -20,12 +21,25 @@ module Idv
 
     private
 
+    def confirm_two_factor_authenticated_or_recovery
+      user_signed_in? || session[:ial2_recovery_user_id].present?
+    end
+
     def confirm_idv_session_step_needed
+      return unless user_signed_in?
       redirect_to idv_phone_url if idv_session.profile_confirmation == true
     end
 
     def remaining_step_attempts
-      Throttler::RemainingCount.call(current_user.id, :idv_resolution)
+      Throttler::RemainingCount.call(user_id, :idv_resolution)
+    end
+
+    def user_id
+      if user_signed_in?
+        current_user.id
+      else
+        session[:ial2_recovery_user_id]
+      end
     end
   end
 end
