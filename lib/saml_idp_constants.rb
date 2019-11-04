3cec573b217a48bcae6f1d625852dded3d43f9b9
Jenny Wagner <jennywagg@gmail.com>
2019-11-04 14:55:44 -0500

    LG-1896 accept oidc and saml requests with ial in request

--- lib/saml_idp_constants.rb
+++ lib/saml_idp_constants.rb
@@ -5,10 +5,9 @@ module Saml
     module Constants
       LOA1_AUTHN_CONTEXT_CLASSREF = 'http://idmanagement.gov/ns/assurance/loa/1'.freeze
       LOA3_AUTHN_CONTEXT_CLASSREF = 'http://idmanagement.gov/ns/assurance/loa/3'.freeze
+      IAL1_AUTHN_CONTEXT_CLASSREF = 'http://idmanagement.gov/ns/assurance/ial/1'.freeze
+      IAL2_AUTHN_CONTEXT_CLASSREF = 'http://idmanagement.gov/ns/assurance/ial/2'.freeze
 
-      # For now the acr values returned are still LOA
-      IAL1_AUTHN_CONTEXT_CLASSREF = LOA1_AUTHN_CONTEXT_CLASSREF
-      IAL2_AUTHN_CONTEXT_CLASSREF = LOA3_AUTHN_CONTEXT_CLASSREF
 
       REQUESTED_ATTRIBUTES_CLASSREF = 'http://idmanagement.gov/ns/requested_attributes?ReqAttr='.freeze
 
