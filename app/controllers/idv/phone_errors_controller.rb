5e094e63e973b61759ec43a3f1adc040caae9792
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-11-04 19:40:04 -0500

    add test coverage

--- app/controllers/idv/phone_errors_controller.rb
+++ app/controllers/idv/phone_errors_controller.rb
@@ -2,10 +2,8 @@ module Idv
   class PhoneErrorsController < ApplicationController
     include IdvSession
 
-    before_action :confirm_two_factor_authenticated
-    before_action :confirm_idv_needed
-    before_action :confirm_idv_session_started
-    before_action :confirmat_phone_confirmation_needed
+    before_action :confirm_two_factor_authenticated_or_recovery
+    before_action :confirm_idv_phone_step_needed
 
     def warning
       @remaining_step_attempts = remaining_step_attempts
@@ -23,26 +21,26 @@ module Idv
 
     private
 
-    def confirmat_phone_confirmation_needed
-      redirect_to_next_step if idv_session.user_phone_confirmation == true
-    end
-
-    def redirect_to_next_step
-      if phone_confirmation_required?
-        redirect_to idv_otp_delivery_method_url
-      else
-        redirect_to idv_review_url
-      end
-    end
-
-    def phone_confirmation_required?
-      idv_session.user_phone_confirmation != true
-    end
-
     def remaining_step_attempts
       max_attempts = Throttle::THROTTLE_CONFIG[:idv_resolution][:max_attempts]
       attempt_count = idv_session.step_attempts[:phone]
       max_attempts - attempt_count
+    end
+
+    def confirm_two_factor_authenticated_or_recovery
+      return if session[:ial2_recovery_user_id].present?
+      confirm_two_factor_authenticated
+    end
+
+    def confirm_idv_phone_step_needed
+      return unless user_fully_authenticated?
+      redirect_to idv_review_url if idv_session.user_phone_confirmation == true
+    end
+
+    def user_id
+      ial2_recovery_user_id = session[:ial2_recovery_user_id]
+      return ial2_recovery_user_id if ial2_recovery_user_id.present?
+      current_user.id
     end
   end
 end
--- app/controllers/idv/session_errors_controller.rb
+++ app/controllers/idv/session_errors_controller.rb
@@ -21,25 +21,24 @@ module Idv
 
     private
 
-    def confirm_two_factor_authenticated_or_recovery
-      user_signed_in? || session[:ial2_recovery_user_id].present?
-    end
-
-    def confirm_idv_session_step_needed
-      return unless user_signed_in?
-      redirect_to idv_phone_url if idv_session.profile_confirmation == true
-    end
-
     def remaining_step_attempts
       Throttler::RemainingCount.call(user_id, :idv_resolution)
     end
 
+    def confirm_two_factor_authenticated_or_recovery
+      return if session[:ial2_recovery_user_id].present?
+      confirm_two_factor_authenticated
+    end
+
+    def confirm_idv_session_step_needed
+      return unless user_fully_authenticated?
+      redirect_to idv_phone_url if idv_session.profile_confirmation == true
+    end
+
     def user_id
-      if user_signed_in?
-        current_user.id
-      else
-        session[:ial2_recovery_user_id]
-      end
+      ial2_recovery_user_id = session[:ial2_recovery_user_id]
+      return ial2_recovery_user_id if ial2_recovery_user_id.present?
+      current_user.id
     end
   end
 end
