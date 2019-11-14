5ad420af921ccf5bb21e52b2bc4c5ae510f0268d
Aaron Chapman <aaron.chapman@gsa.gov>
2019-11-14 18:12:30 -0500

    LG-1996 Email address form field does not provide error summary (#3408)
    **Why**: As SAM.gov I want the error message for incorrectly formatted emails to be consistent between IE and Chrome (see attached screenshots) and WCAG standards.
    
    See more details here:
    https://cm-jira.usa.gov/browse/LG-1996

---
+++ app/javascript/packs/email-validation.js
@@ -0,0 +1,44 @@
+// Allows HTML5 email validation with consistent errors across all browsers
+function emailValidation() {
+  const alert = document.querySelector('.email-invalid-alert');
+  const alertInline = document.querySelector('.email-invalid-alert-inline');
+  const email = document.querySelector('input[type="email"]');
+  let blurTimer;
+
+  // remove focus from the email input after error is displayed
+  function blurEmailInput(input) {
+    blurTimer = setTimeout(function () {
+      input.blur();
+    }, 0);
+  }
+
+  function resetEmailInvalid(input) {
+    input.classList.remove('usa-input--error');
+    alert.classList.add('hide');
+    alertInline.classList.add('hide');
+    clearTimeout(blurTimer);
+  }
+
+  function displayEmailInvalid(input) {
+    input.classList.add('usa-input--error');
+    alert.classList.remove('hide');
+    alertInline.classList.remove('hide');
+    blurEmailInput(input);
+  }
+
+  if (email) {
+    email.classList.remove('field');
+    email.classList.add('usa-input'); // use usds 2.0 styles
+    email.addEventListener('invalid', (e) => {
+      resetEmailInvalid(e.target);
+      if (!e.target.validity.valid) {
+        displayEmailInvalid(e.target);
+      }
+    });
+    email.addEventListener('input', (e) => {
+      resetEmailInvalid(e.target);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', emailValidation);
