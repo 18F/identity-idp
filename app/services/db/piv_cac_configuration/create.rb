798bfe4bdc3051d4fccbab7791f65da86cccaeb5
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-11-25 16:50:45 -0500

    Fix name

--- app/services/db/piv_cac_configuration/create.rb
+++ app/services/db/piv_cac_configuration/create.rb
@@ -1,7 +1,7 @@
 module Db
   module PivCacConfiguration
     class Create
-      def self.call(user_id, x509_dn_uuid, name = '')
+      def self.call(user_id, x509_dn_uuid, name = x509_dn_uuid)
         user = User.find_by(id: user_id)
         return unless user
         user.x509_dn_uuid = x509_dn_uuid
