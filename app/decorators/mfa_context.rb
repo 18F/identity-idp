fcf23c71e95f1fea66caeb2f97ce135e0a74f582
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-11-26 09:43:59 -0500

    Fix again

--- app/decorators/mfa_context.rb
+++ app/decorators/mfa_context.rb
@@ -47,8 +47,9 @@ class MfaContext
 
   def piv_cac_configurations
     if user.present?
+      user_id = user.class == AnonymousUser ? nil : user.id
       [user.piv_cac_configurations.first ||
-        PivCacConfiguration.new(user_id: user.id, x509_dn_uuid: user&.x509_dn_uuid, name: '')]
+        PivCacConfiguration.new(user_id: user_id, x509_dn_uuid: user&.x509_dn_uuid, name: '')]
     else
       PivCacConfiguration.none
     end
