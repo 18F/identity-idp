7d5beae10e2b2ad6d663924f5b0845c38e27b285
Aaron Chapman <aaron.chapman@gsa.gov>
2019-12-02 15:31:51 -0500

    codeclimate and friendly message content

--- app/services/friendly_error/find_key.rb
+++ app/services/friendly_error/find_key.rb
@@ -1,12 +1,7 @@
 module FriendlyError
   class FindKey
     def self.call(message, path)
-      return message_key(message, path) || 'general_error'
+      FRIENDLY_ERROR_CONFIG[path].find { |_key, value| value == message }&.first
     end
-
-    def self.message_key(message, path)
-      FRIENDLY_ERROR_CONFIG[path].find{ |key, value| value == message }&.first
-    end
-    private_class_method :message_key
   end
 end
--- app/services/friendly_error/message.rb
+++ app/services/friendly_error/message.rb
@@ -2,6 +2,7 @@ module FriendlyError
   class Message
     def self.call(message, path)
       error_key = FriendlyError::FindKey.call(message, path)
+      return message if error_key.blank?
       I18n.t('friendly_errors.' + path + '.' + error_key.to_s, default: message)
     end
   end
