dac511b8152cba39bf9d0a6ef0e1a8060ad0316c
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-12-16 11:34:16 -0500

    Remove delete_all

--- app/services/db/piv_cac_configuration/delete_all.rb
+++
@@ -1,9 +0,0 @@
-module Db
-  module PivCacConfiguration
-    class DeleteAll
-      def self.call(user_id)
-        ::PivCacConfiguration.where(user_id: user_id).delete_all
-      end
-    end
-  end
-end
