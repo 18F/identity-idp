71ccc04e991b244b130943395c0e2d37f1c68843
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-12-26 09:59:27 -0600

    Add front image

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -2,10 +2,15 @@
 module Rack
   class Timeout
     @excludes = [
+      '/verify/doc_auth/front_image',
       '/verify/doc_auth/back_image',
+      '/verify/doc_auth/mobile_front_image',
       '/verify/doc_auth/mobile_back_image',
+      '/verify/capture_doc/mobile_front_image',
       '/verify/capture_doc/capture_mobile_back_image',
+      '/verify/recovery/front_image',
       '/verify/recovery/back_image',
+      '/verify/recovery/mobile_front_image',
       '/verify/recovery/mobile_back_image',
     ]
 
