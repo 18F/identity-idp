e4313d9e652030b147f7210dc8b282fe8d971dc0
Steve Urciuoli <steve.urciuoli@gsa.gov>
2019-12-26 16:32:48 -0600

    LG-2484 Increase timeouts for Acuant results API call (#3498)
    **Why** : So users don't timeout with the rack timeout before the proofing call completes.
    **How**: Monkey patch rack timeout to exclude the acuant proofing endpoints.  Cap the response time of API calls using a standard timeout

---
+++ config/initializers/rack_timeout.rb
@@ -0,0 +1,32 @@
+# :reek:InstanceVariableAssumption
+module Rack
+  class Timeout
+    @excludes = [
+      '/verify/doc_auth/front_image',
+      '/verify/doc_auth/back_image',
+      '/verify/doc_auth/mobile_front_image',
+      '/verify/doc_auth/mobile_back_image',
+      '/verify/capture_doc/mobile_front_image',
+      '/verify/capture_doc/capture_mobile_back_image',
+      '/verify/recovery/front_image',
+      '/verify/recovery/back_image',
+      '/verify/recovery/mobile_front_image',
+      '/verify/recovery/mobile_back_image',
+    ]
+
+    class << self
+      attr_accessor :excludes
+    end
+
+    def call_with_excludes(env)
+      if self.class.excludes.any? { |exclude_uri| /\A#{exclude_uri}/ =~ env['REQUEST_URI'] }
+        @app.call(env)
+      else
+        call_without_excludes(env)
+      end
+    end
+
+    alias call_without_excludes call
+    alias call call_with_excludes
+  end
+end
