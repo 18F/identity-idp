289882842057df051824ec42d9646b4f7589d318
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-01 12:45:44 -0600

    Fix

--- app/services/db/auth_app_configuration/create.rb
+++ app/services/db/auth_app_configuration/create.rb
@@ -2,6 +2,7 @@ module Db
   module AuthAppConfiguration
     class Create
       def self.call(user, otp_secret_key, name = Time.zone.now.to_s)
+        user.save
         user.auth_app_configurations.create(otp_secret_key: otp_secret_key, name: name)
       end
     end
