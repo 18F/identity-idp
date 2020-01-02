7d59cd6f0734e73d3f1653fdbe9b0eb72ddb3163
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-01 23:53:22 -0600

    Oops

--- app/services/db/auth_app_configuration/authenticate.rb
+++ app/services/db/auth_app_configuration/authenticate.rb
@@ -7,13 +7,13 @@ module Db
           new_timestamp = totp.verify_with_drift_and_prior(code,
                                                            Devise.allowed_otp_drift_seconds,
                                                            cfg.totp_timestamp)
-          next unless new_timestamp
-          update_timestamp(cfg, new_timetstamp)
-          return true
+          return true if update_timestamp(cfg, new_timestamp)
         end
+        false
       end
 
       def self.update_timestamp(cfg, new_timestamp)
+        return unless new_timestamp
         cfg.totp_timestamp = new_timestamp
         cfg.save
       end
