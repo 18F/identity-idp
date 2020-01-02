d4bb7a9de92e9de991c76a2129bf3e1e8f10286a
Aaron Chapman <aaron.chapman@gsa.gov>
2020-01-02 15:35:34 -0500

    fixed strong_migrations error on remove_column

--- db/migrate/20200102010300_drop_x509_dn_uuid_column_from_user.rb
+++ db/migrate/20200102010300_drop_x509_dn_uuid_column_from_user.rb
@@ -1,5 +1,7 @@
 class DropX509DnUuidColumnFromUser < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :x509_dn_uuid
+    safety_assured do
+      remove_column :users, :x509_dn_uuid
+    end
   end
 end
