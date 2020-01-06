ba60719249e03d99985e8ba039a874c41a322dda
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-01-06 15:19:50 -0500

    Remove Geolite2 setup from the setup script
    **Why**: MaxMind has removed public links to the Geolite2 database to comply with CPAA. This broke out setup script which expected the file to be public.
    
    This commit fixes the issue by using the mock in local dev unless a file is present. It also adds instructions to the README on how to download the file to enable geolocation locally.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,33 @@
-Geocoder.configure(
-  ip_lookup: :geoip2,
-  geoip2: {
-    file: Rails.root.join('geo_data', 'GeoLite2-City.mmdb'),
-  },
-)
+# For some reason the test result class does not impelement the `language=`
+# method. This patches an empty method onto it to prevent NoMethodErrors in
+# the tests
+module Geocoder
+  module Result
+    class Test
+      def language=(_locale); end
+    end
+  end
+end
+
+GEO_DATA_FILEPATH = Rails.root.join('geo_data', 'GeoLite2-City.mmdb').freeze
+
+if !Rails.env.production? && !File.exist?(GEO_DATA_FILEPATH)
+  Geocoder.configure(ip_lookup: :test)
+  Geocoder::Lookup::Test.add_stub(
+    '127.0.0.1', [
+      { 'city' => '', 'country' => 'United States', 'state_code' => '' },
+    ]
+  )
+  Geocoder::Lookup::Test.add_stub(
+    '::1', [
+      { 'city' => '', 'country' => 'United States', 'state_code' => '' },
+    ]
+  )
+else
+  Geocoder.configure(
+    ip_lookup: :geoip2,
+    geoip2: {
+      file: GEO_DATA_FILEPATH,
+    },
+  )
+end
