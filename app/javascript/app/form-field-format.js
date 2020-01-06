46d85095594bfb8e0a9c6780892bafc728daba6c
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-06 13:22:03 -0600

    Remove send link formatting and unused javascript

--- app/javascript/app/form-field-format.js
+++ app/javascript/app/form-field-format.js
@@ -2,7 +2,6 @@ import { SocialSecurityNumberFormatter, TextField } from 'field-kit';
 import DateFormatter from './modules/date-formatter';
 import NumericFormatter from './modules/numeric-formatter';
 import PersonalKeyFormatter from './modules/personal-key-formatter';
-import USPhoneFormatter from './modules/us-phone-formatter';
 import ZipCodeFormatter from './modules/zip-code-formatter';
 
 
@@ -10,7 +9,6 @@ function formatForm() {
   const formats = [
     ['.dob', new DateFormatter()],
     ['.mfa', new NumericFormatter()],
-    ['.us-phone', new USPhoneFormatter()],
     ['.personal-key', new PersonalKeyFormatter()],
     ['.ssn', new SocialSecurityNumberFormatter()],
     ['.zipcode', new ZipCodeFormatter()],
