e35117d872b4d89e220ecb8f4353687a447a52d9
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-06 18:35:38 -0600

    Fix

--- app/services/db/piv_cac_configuration/create.rb
+++ app/services/db/piv_cac_configuration/create.rb
@@ -2,7 +2,7 @@ module Db
   module PivCacConfiguration
     class Create
       def self.call(user, x509_dn_uuid, name = x509_dn_uuid)
-       user.piv_cac_configurations.create!(x509_dn_uuid: x509_dn_uuid, name: name)
+        user.piv_cac_configurations.create!(x509_dn_uuid: x509_dn_uuid, name: name)
       end
     end
   end
