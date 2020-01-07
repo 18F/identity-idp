54dc7d02d311a85ed5551f254d7a2dbfd802e69b
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-06 18:30:01 -0600

    Fixes

--- spec/features/mfa_enrollability_spec.rb
+++ spec/features/mfa_enrollability_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'MFA enrollability' do
   let(:user) { create(:user, :signed_up, :with_authentication_app) }
 
-  it 'does not allow choosing totp as backup auth method after it is used as primary' do
+  it 'does allow choosing totp as backup auth method after it is used as primary' do
     sign_up_and_set_password
 
     select_2fa_option('auth_app')
@@ -13,7 +13,7 @@ describe 'MFA enrollability' do
 
     click_continue
 
-    expect(page).to have_selector('#two_factor_options_form_selection_auth_app', count: 0)
+    expect(page).to have_selector('#two_factor_options_form_selection_auth_app', count: 1)
     expect(page).to have_selector('#two_factor_options_form_selection_phone', count: 1)
   end
 end
