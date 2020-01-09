e7cd20044461839905d72e0d71596c125faca507
Jenny Wagner <jennywagg@gmail.com>
2020-01-09 13:16:19 -0500

    LG-2308 do not allow prompt = nil

--- app/forms/openid_connect_authorize_form.rb
+++ app/forms/openid_connect_authorize_form.rb
@@ -29,7 +29,7 @@ class OpenidConnectAuthorizeForm
   validates :nonce, presence: true, length: { minimum: RANDOM_VALUE_MINIMUM_LENGTH }
 
   validates :response_type, inclusion: { in: %w[code] }
-  validates :prompt, presence: true, allow_nil: true, inclusion: { in: %w[login select_account] }
+  validates :prompt, presence: true, inclusion: { in: %w[login select_account] }
   validates :code_challenge_method, inclusion: { in: %w[S256] }, if: :code_challenge
 
   validate :validate_acr_values
