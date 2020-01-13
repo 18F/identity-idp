c4fbd3e3b2be93076a9d32f848a851f484a13063
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-13 15:55:11 -0500

    Direct

--- spec/services/db/sp_cost/add_sp_cost_spec.rb
+++ spec/services/db/sp_cost/add_sp_cost_spec.rb
@@ -16,25 +16,26 @@ feature 'SP Costing' do
   let(:email2) { 'test2@test.com' }
   let(:email3) { 'test3@test.com' }
   let(:email4) { 'test4@test.com' }
+  let(:email5) { 'test5@test.com' }
   let(:password) { Features::SessionHelper::VALID_PASSWORD }
 
   it 'logs the correct costs for an ial1 user creation from sp with oidc' do
     create_ial1_user_from_sp(email1)
 
-    expect_cost_type(0, 'sms')
-    expect_cost_type(1, 'ial1_user_added')
-    expect_cost_type(2, 'authentication')
+    expect_sp_cost_type(0, 'sms')
+    expect_sp_cost_type(1, 'ial1_user_added')
+    expect_sp_cost_type(2, 'authentication')
   end
 
   it 'logs the correct costs for an ial2 user creation from sp with oidc' do
     create_ial2_user_from_sp(email2)
 
-    expect_cost_type(0, 'sms')
-    expect_cost_type(1, 'acuant_front_image')
-    expect_cost_type(2, 'acuant_back_image')
-    expect_cost_type(3, 'lexis_nexis_resolution')
-    expect_cost_type(4, 'ial2_user_added')
-    expect_cost_type(5, 'authentication')
+    expect_sp_cost_type(0, 'sms')
+    expect_sp_cost_type(1, 'acuant_front_image')
+    expect_sp_cost_type(2, 'acuant_back_image')
+    expect_sp_cost_type(3, 'lexis_nexis_resolution')
+    expect_sp_cost_type(4, 'ial2_user_added')
+    expect_sp_cost_type(5, 'authentication')
   end
 
   it 'logs the correct costs for an ial1 authentication' do
@@ -47,8 +48,8 @@ feature 'SP Costing' do
     fill_in_code_with_last_phone_otp
     click_submit_default
 
-    expect_cost_type(0, 'digest')
-    expect_cost_type(1, 'authentication')
+    expect_sp_cost_type(0, 'digest')
+    expect_sp_cost_type(1, 'authentication')
   end
 
   it 'logs the correct costs for an ial2 authentication' do
@@ -61,14 +62,33 @@ feature 'SP Costing' do
     fill_in_code_with_last_phone_otp
     click_submit_default
 
-    expect_cost_type(0, 'digest')
-    expect_cost_type(1, 'authentication')
+    expect_sp_cost_type(0, 'digest')
+    expect_sp_cost_type(1, 'authentication')
+  end
+
+  it 'logs the correct costs for a direct authentication' do
+    visit root_path
+    create_ial1_user_directly(email5)
+    SpCost.delete_all
+    visit sign_out_url
+
+    visit root_path
+    fill_in_credentials_and_submit(email5, password)
+    fill_in_code_with_last_phone_otp
+    click_submit_default
+
+    expect_direct_cost_type(0, 'digest')
   end
 
   def create_ial1_user_from_sp(email)
     visit_idp_from_sp_with_ial1(:oidc)
     register_user(email)
     click_on t('forms.buttons.continue')
+  end
+
+  def create_ial1_user_directly(email)
+    visit root_path
+    register_user(email)
   end
 
   def create_ial2_user_from_sp(email)
@@ -82,10 +102,17 @@ feature 'SP Costing' do
     click_continue
   end
 
-  def expect_cost_type(sp_cost_index, token)
+  def expect_sp_cost_type(sp_cost_index, token)
     sp_cost = sp_costs(sp_cost_index)
     expect(sp_cost.issuer).to eq(issuer)
     expect(sp_cost.agency_id).to eq(agency_id)
+    expect(sp_cost.cost_type).to eq(token)
+  end
+
+  def expect_direct_cost_type(sp_cost_index, token)
+    sp_cost = sp_costs(sp_cost_index)
+    expect(sp_cost.issuer).to eq('')
+    expect(sp_cost.agency_id).to eq(0)
     expect(sp_cost.cost_type).to eq(token)
   end
 
