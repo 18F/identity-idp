c3ad47a2900be4ce3e443b42a09bd3674f99eda1
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-17 07:54:52 -0500

    Oops

--- app/services/db/establish_connection/read_replica.rb
+++ app/services/db/establish_connection/read_replica.rb
@@ -6,7 +6,7 @@ module Db
         env = Figaro.env
         ActiveRecord::Base.establish_connection(
           adapter: 'postgresql',
-          database: 'upaya_test',
+          database: "upaya_#{Rails.env}",
           host: env.database_host,
           username: env.database_username,
           password: env.database_password,
