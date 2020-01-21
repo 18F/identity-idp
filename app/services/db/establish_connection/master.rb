d219683a62611977fd1ae61db68b7a5111949ddb
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-01-20 19:00:44 -0500

    Last fix

--- app/services/db/establish_connection/master.rb
+++ app/services/db/establish_connection/master.rb
@@ -3,9 +3,10 @@ module Db
     class Master
       def self.call
         env = Figaro.env
+        rails_env = Rails.env
         ActiveRecord::Base.establish_connection(
           adapter: 'postgresql',
-          database: env.production? ? env.database_name : "upaya_#{Rails.env}",
+          database: rails_env.production? ? env.database_name : "upaya_#{rails_env}",
           host: env.database_host,
           username: env.database_username,
           password: env.database_password,
