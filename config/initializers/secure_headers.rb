29f7784a792b343efaf2c7b323a95b75ed65ba88
Aaron Chapman <aaron.chapman@gsa.gov>
2020-01-22 14:00:01 -0500

    Added piv_cac domains to CSP (#3532)
    * Added piv_cac domains to CSP
    
    * fixed formatting issue
    
    * codeclimate
    
    * added wildcard for all environments

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,6 +6,14 @@ SecureHeaders::Configuration.default do |config| # rubocop:disable Metrics/Block
   config.x_download_options = 'noopen'
   config.x_permitted_cross_domain_policies = 'none'
 
+  # On the development environment only we allow 'https://*' to hop over to identity-pki
+  form_action = if Rails.env.development?
+                  ["'self'", 'https://*']
+                else
+                  ["'self'",
+                   'https://*.pivcac.*.identitysandbox.gov',
+                   'https://*.pivcac.*.login.gov']
+                end
   default_csp_config = {
     default_src: ["'self'"],
     child_src: ["'self'", 'www.google.com'], # CSP 2.0 only; replaces frame_src
