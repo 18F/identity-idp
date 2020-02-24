bf70a9daf0a551b65121affb18f2e7007c9375f6
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-02-24 17:00:04 -0500

    Lint

--- spec/services/db/identity/sp_user_counts_spec.rb
+++ spec/services/db/identity/sp_user_counts_spec.rb
@@ -12,7 +12,7 @@ describe Db::Identity::SpUserCounts do
   it 'returns the total user counts per sp' do
     Identity.create(user_id: 1, service_provider: issuer, uuid: 'foo1')
     Identity.create(user_id: 2, service_provider: issuer, uuid: 'foo2')
-    Identity.create(user_id: 3, service_provider: issuer, uuid: 'foo3', verified_at: Time.now)
+    Identity.create(user_id: 3, service_provider: issuer, uuid: 'foo3', verified_at: Time.zone.now)
     result = { issuer: issuer, total: 3, ial1_total: 2, ial2_total: 1 }.to_json
 
     expect(subject.call.ntuples).to eq(1)
