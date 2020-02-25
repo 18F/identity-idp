cad0b1709f26efff2eb8144b6dc2fe77ebedd67a
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-02-24 23:46:02 -0500

    Quota

--- app/services/db/identity/sp_user_counts.rb
+++ app/services/db/identity/sp_user_counts.rb
@@ -3,9 +3,28 @@ module Db
     class SpUserCounts
       def self.call
         sql = <<~SQL
-          SELECT service_provider as issuer,count(user_id) AS total, count(user_id)-count(verified_at) AS ial1_total, count(verified_at) AS ial2_total
+          SELECT
+            service_providers.issuer,
+            total,
+            ial1_total,
+            ial2_total,
+            CAST(
+              (CASE WHEN ial2_quota IS NULL
+              THEN 0
+              ELSE ROUND(ial2_total * 100.0 / ial2_quota)
+              END)
+              AS INTEGER
+            ) AS percent_ial2_quota
+          FROM service_providers, 
+          (SELECT
+            service_provider AS issuer,
+            count(user_id) AS total,
+            count(user_id)-count(verified_at) AS ial1_total,
+            count(verified_at) AS ial2_total
           FROM identities
-          GROUP BY issuer ORDER BY issuer
+          GROUP BY issuer ORDER BY issuer)
+          AS TBL
+          WHERE TBL.issuer = service_providers.issuer
         SQL
         ActiveRecord::Base.connection.execute(sql)
       end
--- spec/services/db/identity/sp_user_counts_spec.rb
+++ spec/services/db/identity/sp_user_counts_spec.rb
@@ -10,10 +10,11 @@ describe Db::Identity::SpUserCounts do
   end
 
   it 'returns the total user counts per sp' do
+    ServiceProvider.create(issuer: issuer, friendly_name: issuer)
     Identity.create(user_id: 1, service_provider: issuer, uuid: 'foo1')
     Identity.create(user_id: 2, service_provider: issuer, uuid: 'foo2')
     Identity.create(user_id: 3, service_provider: issuer, uuid: 'foo3', verified_at: Time.zone.now)
-    result = { issuer: issuer, total: 3, ial1_total: 2, ial2_total: 1 }.to_json
+    result = { issuer: issuer, total: 3, ial1_total: 2, ial2_total: 1, percent_ial2_quota: 0 }.to_json
 
     expect(subject.call.ntuples).to eq(1)
     expect(subject.call[0].to_json).to eq(result)
