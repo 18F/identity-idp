b419d69abb9934478fc7d47998f9432f7e2584c6
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-02-26 21:21:22 -0500

    Fix

--- spec/services/reports/sp_user_counts_report_spec.rb
+++ spec/services/reports/sp_user_counts_report_spec.rb
@@ -9,15 +9,14 @@ describe Reports::SpUserCountsReport do
     expect(subject.call).to eq('[]')
   end
 
-  it 'returns the total user counts per sp broken down by ial1 and ial2 with percent ial2 quota' do
+  it 'returns the total user counts per sp broken down by ial1 and ial2' do
     ServiceProvider.create(issuer: issuer, friendly_name: issuer)
     Identity.create(user_id: 1, service_provider: issuer, uuid: 'foo1')
     Identity.create(user_id: 2, service_provider: issuer, uuid: 'foo2')
     Identity.create(user_id: 3, service_provider: issuer, uuid: 'foo3', verified_at: Time.zone.now)
     puts ServiceProvider.count
     puts Identity.count
-    result = [{ issuer: issuer, total: 3, ial1_total: 2, ial2_total: 1,
-                percent_ial2_quota: 0 }].to_json
+    result = [{ issuer: issuer, total: 3, ial1_total: 2, ial2_total: 1 }].to_json
 
     expect(subject.call).to eq(result)
   end
