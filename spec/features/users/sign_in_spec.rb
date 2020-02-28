db46a59c0564153c562273f0ca0bea35efcb930c
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-02-28 11:48:55 -0500

    Spec

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -806,6 +806,22 @@ feature 'Sign in' do
 
       expect(current_url).to start_with('http://localhost:7654/auth/result')
     end
+
+    it 'uses ial2 flow with an ial2 param' do
+      user = create(:profile, :active, :verified,
+                    pii: { first_name: 'John', ssn: '111223333' }).user
+      visit_idp_from_oidc_sp_with_ialmax
+      fill_in_credentials_and_submit(user.email, user.password)
+      fill_in_code_with_last_phone_otp
+      click_submit_default
+
+      expect(current_path).to eq sign_up_completed_path
+      expect(page).to have_content('111223333')
+
+      click_continue
+
+      expect(current_url).to start_with('http://localhost:7654/auth/result')
+    end
   end
 
   context 'saml sp requests ialmax' do
@@ -823,21 +839,18 @@ feature 'Sign in' do
 
       expect(current_url).to eq @saml_authn_request
     end
+  end
 
-    it 'returns ial2 info for a verified user' do
-      user = create(:profile, :active, :verified,
-                    pii: { first_name: 'John', ssn: '111223333' }).user
-      visit_idp_from_saml_sp_with_ialmax
+  context 'ial2 param on sign up screen' do
+    it 'invokes ial2 flow' do
+      enable_doc_auth
+      user = create(:user, :signed_up)
+      visit root_path(ial: 2)
       fill_in_credentials_and_submit(user.email, user.password)
       fill_in_code_with_last_phone_otp
       click_submit_default
 
-      expect(current_path).to eq sign_up_completed_path
-      expect(page).to have_content('111223333')
-
-      click_continue
-
-      expect(current_url).to eq @saml_authn_request
+      expect(current_path).to eq(idv_doc_auth_welcome_step)
     end
   end
 
