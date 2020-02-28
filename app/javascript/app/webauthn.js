dceb78d019cc90a1be9c4be176cbdd2070a3d703
Clint Troxel <clinton.troxel@gsa.gov>
2020-02-28 13:18:54 -0700

    Better webauthn Windows support
    **Why**: I wanted to use a Yubikey to log in

--- app/javascript/app/webauthn.js
+++ app/javascript/app/webauthn.js
@@ -15,10 +15,12 @@ const longToByteArray = long => new Uint8Array(8).map(() => {
   return byte;
 });
 
-const extractCredentials = credentials => credentials.split(',').map(credential => ({
-  type: 'public-key',
-  id: base64ToArrayBuffer(credential),
-}));
+const extractCredentials = credentials => {
+  return credentials.split(',').filter(element => element !== "").map(credential => ({
+    type: 'public-key',
+    id: base64ToArrayBuffer(credential),
+  }));
+};
 
 const isWebAuthnEnabled = () => {
   if (navigator && navigator.credentials && navigator.credentials.create) {
@@ -70,6 +72,12 @@ const enrollWebauthnDevice = ({ userId, userEmail, userChallenge, excludeCredent
       timeout: 800000,
       attestation: 'none',
       excludeList: [],
+      authenticatorSelection: {
+        // Prevents user from needing to use PIN with Security Key
+        userVerification: "discouraged",
+        // Defaults to "Security Key" instead of things like "Windows Hello"
+        authenticatorAttachment: "cross-platform"
+      },
       excludeCredentials: extractCredentials(excludeCredentials),
     },
   };
