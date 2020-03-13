e4a7236af0445c6df8a9144654eb1c3783a5281e
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-03-12 21:33:43 -0400

    Lint

--- spec/services/reports/total_monthly_auths_report_spec.rb
+++ spec/services/reports/total_monthly_auths_report_spec.rb
@@ -11,8 +11,10 @@ describe Reports::TotalMonthlyAuthsReport do
   end
 
   it 'returns the total monthly auths' do
-    MonthlySpAuthCount.create(issuer: 'foo', ial: 1, year_month: '201901', user_id: 2, auth_count: 7)
-    MonthlySpAuthCount.create(issuer: 'foo', ial: 1, year_month: '201901', user_id: 3, auth_count: 3)
+    MonthlySpAuthCount.create(issuer: 'foo', ial: 1, year_month: '201901', user_id: 2,
+                              auth_count: 7)
+    MonthlySpAuthCount.create(issuer: 'foo', ial: 1, year_month: '201901', user_id: 3,
+                              auth_count: 3)
     result = [{ issuer: 'foo', ial: 1, year_month: '201901', total: 10 }].to_json
 
     expect(subject.call).to eq(result)
