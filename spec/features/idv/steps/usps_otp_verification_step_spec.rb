b6e3d1bfc90bb988b508e77b43554fecb256a466
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-03-17 10:43:54 -0400

    LG-2801 Allow USPS proofing to be disabled without errors
    **Why**: It looks like we may need to temporarily disable USPS proofing while our mailing vendor is temporarily suspending operations.
    
    This commit has 2 changes to enable that:
    
    - Fix a link that was causing an error because of a missing route on the phone steps
    - Allow a user who has already received a letter to enter their OTP and verify while not allowing new letters to be sent

--- spec/features/idv/steps/usps_otp_verification_step_spec.rb
+++ spec/features/idv/steps/usps_otp_verification_step_spec.rb
@@ -6,4 +6,10 @@ feature 'idv usps otp verification step' do
   it_behaves_like 'usps otp verfication step'
   it_behaves_like 'usps otp verfication step', :oidc
   it_behaves_like 'usps otp verfication step', :saml
+
+  context 'with USPS proofing disabled it still lets users with a letter verify' do
+    it_behaves_like 'usps otp verfication step'
+    it_behaves_like 'usps otp verfication step', :oidc
+    it_behaves_like 'usps otp verfication step', :saml
+  end
 end
