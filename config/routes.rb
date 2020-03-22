c3e6381c073222b4e137173a78ac9234584e3738
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-03-22 10:34:57 -0400

    LG-2822 Add polling to hybrid flow continue step (#3646)
    This commit adds polling to the doc auth hybrid link sent spec. This results in the continue button being hidden. The page automatically advances to the next step when the user is finished on their phone.

--- config/routes.rb
+++ config/routes.rb
@@ -293,6 +293,7 @@ Rails.application.routes.draw do
         get '/doc_auth' => 'doc_auth#index'
         get '/doc_auth/:step' => 'doc_auth#show', as: :doc_auth_step
         put '/doc_auth/:step' => 'doc_auth#update'
+        get '/doc_auth/link_sent/poll' => 'doc_auth#doc_capture_poll'
         get '/capture_doc' => 'capture_doc#index'
         get '/capture_doc/:step' => 'capture_doc#show', as: :capture_doc_step
         put '/capture_doc/:step' => 'capture_doc#update'
