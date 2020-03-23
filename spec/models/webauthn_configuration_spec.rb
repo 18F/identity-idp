c4167fd3b87b67cbdf782dc7295407642c7b4942
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-03-23 09:41:01 -0400

    some cleanup

--- spec/models/webauthn_configuration_spec.rb
+++ spec/models/webauthn_configuration_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe WebauthnConfiguration do
   describe 'Associations' do
     it { is_expected.to belong_to(:user) }
-    it { is_expected.to validate_presence_of(:user_id) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:credential_id) }
     it { is_expected.to validate_presence_of(:credential_public_key) }
