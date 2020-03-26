07d1ce703e3faa3ca48da6105e7134417da1bf71
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-03-26 15:57:57 -0400

    Don't let certain SPs use AAMVA for proofing (#3668)
    **Why**: AAMVA is not able to handle the scale that larger SPs will need at IAL2.
    
    During this work I discovered that there weren't any tests for the logic around whether a user's info should be verified by AAMVA. I added that.

--- spec/support/features/doc_auth_helper.rb
+++ spec/support/features/doc_auth_helper.rb
@@ -9,6 +9,8 @@ module DocAuthHelper
       { 'Name' => 'Address City', 'Value' => 'New York' },
       { 'Name' => 'Address State', 'Value' => 'NY' },
       { 'Name' => 'Address Postal Code', 'Value' => '11364' },
+      { 'Name' => 'Issuing State Code', 'Value' => 'NY' },
+      { 'Name' => 'Document Number', 'Value' => '123ABC' },
       { 'Name' => 'Birth Date', 'Value' => '/Date(' +
         (Date.strptime('10-05-1938', '%m-%d-%Y').strftime('%Q').to_i + 43_200_000).to_s + ')/' },
     ],
@@ -26,8 +28,8 @@ module DocAuthHelper
       dob: '10/05/1938',
       ssn: '123',
       phone: '456',
-      state_id_jurisdiction: nil,
-      state_id_number: nil,
+      state_id_jurisdiction: 'NY',
+      state_id_number: '123ABC',
       state_id_type: 'drivers_license',
     }.freeze
 
