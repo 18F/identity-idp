623573f36c7b3ad1f7efc084b9f6f4d5836d4be6
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-03-27 15:32:27 -0500

    Lint

--- app/controllers/mobile_capture_controller.rb
+++ app/controllers/mobile_capture_controller.rb
@@ -1,6 +1,7 @@
 class MobileCaptureController < ApplicationController
   def new
-    SecureHeaders.append_content_security_policy_directives(request, script_src: ['\'unsafe-eval\''])
+    SecureHeaders.append_content_security_policy_directives(request,
+                                                            script_src: ['\'unsafe-eval\''])
     render layout: false
   end
 end
