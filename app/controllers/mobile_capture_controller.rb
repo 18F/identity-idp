62e06cbb83b478de899f1de57420b589501ed145
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-03-27 15:25:09 -0500

    Allow wasm on mobile capture

--- app/controllers/mobile_capture_controller.rb
+++ app/controllers/mobile_capture_controller.rb
@@ -1,5 +1,6 @@
 class MobileCaptureController < ApplicationController
   def new
+    SecureHeaders.append_content_security_policy_directives(request, script_src: ['\'unsafe-eval\''])
     render layout: false
   end
 end
