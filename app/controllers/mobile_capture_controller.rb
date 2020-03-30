ef33574619b028494be8f48380705165ccc81eff
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-03-30 12:32:59 -0500

    Revert code

--- app/controllers/mobile_capture_controller.rb
+++ app/controllers/mobile_capture_controller.rb
@@ -1,7 +1,8 @@
 class MobileCaptureController < ApplicationController
   def new
     # required to run wasm until wasm-eval is available
-    SecureHeaders.append_content_security_policy_directives(request, script_src: %w['unsafe-eval'])
+    SecureHeaders.append_content_security_policy_directives(request,
+                                                            script_src: ['\'unsafe-eval\''])
     render layout: false
   end
 end
