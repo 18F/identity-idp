dfe7537ec584e337edee77efeb4e30c8562806af
Zach Margolis <zachary.margolis@gsa.gov>
2020-03-31 16:05:30 -0700

    Switch reports to use TimeWithZone objects
    **Why**: It's March 31 locally, but April 1 UTC, so objects
    created "now" were falling into the bucket for the next month.
    This makes the tests more stable across timezones, because the
    objects are created in UTC time and are now queried against in
    UTC time

--- app/services/db/sp_cost/total_sp_cost_summary.rb
+++ app/services/db/sp_cost/total_sp_cost_summary.rb
@@ -2,10 +2,15 @@ module Db
   module SpCost
     class TotalSpCostSummary
       def self.call(start, finish)
-        sql = <<~SQL
+        params = {
+          start: ActiveRecord::Base.connection.quote(start),
+          finish: ActiveRecord::Base.connection.quote(finish),
+        }
+
+        sql = format(<<~SQL, params)
           SELECT cost_type,COUNT(*)
           FROM sp_costs
-          WHERE '#{start}' <= created_at and created_at <= '#{finish}'
+          WHERE %{start} <= created_at and created_at <= %{finish}
           GROUP BY cost_type ORDER BY cost_type
         SQL
         ActiveRecord::Base.connection.execute(sql)
