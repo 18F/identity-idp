261d85080cdb8206c79c412939da4ada2fb228b2
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-04-01 11:18:23 -0400

    fix phone remember device tests

--- spec/features/remember_device/phone_spec.rb
+++ spec/features/remember_device/phone_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'Remembering a phone' do
-  include IdvHelper
+  include IdvStepHelper
 
   before do
     allow(Figaro.env).to receive(:otp_delivery_blocklist_maxretry).and_return('1000')
@@ -50,10 +50,8 @@ feature 'Remembering a phone' do
       check :remember_device
       fill_in_code_with_last_phone_otp
       click_submit_default
-      visit idv_session_path
-      fill_out_idv_form_ok
-      click_idv_continue
-      click_idv_continue
+      visit idv_path
+      complete_all_doc_auth_steps
       fill_out_phone_form_ok('2022603829')
       click_idv_continue
       choose_idv_otp_delivery_method_sms
