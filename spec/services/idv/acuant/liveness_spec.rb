d011f85d8b99907b54987e0b11c20f58481f2738
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-04-03 11:14:31 -0500

    Fix more specs

--- spec/services/idv/acuant/liveness_spec.rb
+++ spec/services/idv/acuant/liveness_spec.rb
@@ -16,20 +16,4 @@ describe Idv::Acuant::Liveness do
       expect(result).to eq([true, body])
     end
   end
-
-  describe '#facematch' do
-    let(:acuant_base_url) { 'https://example.com' }
-    let(:path) { '/api/v1/facematch' }
-    let(:body) { 'body' }
-
-    it 'returns a good status' do
-      stub_request(:post, acuant_base_url + path).
-        with(headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }).
-        to_return(status: 200, body: body)
-
-      result = subject.facematch(body)
-
-      expect(result).to eq([true, body])
-    end
-  end
 end
