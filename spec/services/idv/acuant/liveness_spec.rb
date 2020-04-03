aac0133f3cb1a5b3e9f10b88a0cbee27ee61b8a6
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-04-03 11:19:28 -0500

    Last specs

--- spec/services/idv/acuant/liveness_spec.rb
+++ spec/services/idv/acuant/liveness_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe Idv::Acuant::Liveness do
   describe '#liveness' do
-    let(:acuant_base_url) { 'https://example.com' }
+    let(:acuant_passlive_url) { 'https://example.com' }
     let(:path) { '/api/v1/liveness' }
     let(:body) { 'body' }
 
     it 'returns a good status' do
-      stub_request(:post, acuant_base_url + path).
+      stub_request(:post, acuant_passlive_url + path).
         with(headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }).
         to_return(status: 200, body: body)
 
