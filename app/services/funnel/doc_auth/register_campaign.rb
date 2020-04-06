f43a868d59a709941a368a8aa91401e7f1d4f30a
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-04-06 17:16:24 -0500

    Fix

--- app/services/funnel/doc_auth/register_campaign.rb
+++ app/services/funnel/doc_auth/register_campaign.rb
@@ -3,7 +3,7 @@ module Funnel
     class RegisterCampaign
       def self.call(user_id, campaign)
         doc_auth_log = DocAuthLog.find_by(user_id: user_id)
-        return if doc_auth_log&.no_sp_campaign
+        return if doc_auth_log.nil? || doc_auth_log.no_sp_campaign
         doc_auth_log.no_sp_campaign = campaign.to_s
         doc_auth_log.no_sp_at = Time.zone.now
         doc_auth_log.save
