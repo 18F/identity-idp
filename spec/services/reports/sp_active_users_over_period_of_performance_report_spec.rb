c7f1d159eacbc34a10639039f20b91f8e2f51777
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-04-06 14:09:02 -0500

    LG-2861 Unique actives over period of performance report (#3689)

---
+++ spec/services/reports/sp_active_users_over_period_of_performance_report_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Reports::SpActiveUsersOverPeriodOfPerformanceReport do
+  subject { described_class.new }
+
+  let(:issuer) { 'foo' }
+  let(:issuer2) { 'foo2' }
+
+  it 'is empty' do
+    expect(subject.call).to eq('[]')
+  end
+
+  it 'returns total active user counts per sp broken down by ial1 and ial2' do
+    now = Time.zone.now
+    ServiceProvider.create(issuer: issuer, friendly_name: issuer)
+    Identity.create(user_id: 1, service_provider: issuer, uuid: 'foo1',
+                    last_ial1_authenticated_at: now, last_ial2_authenticated_at: now)
+    Identity.create(user_id: 2, service_provider: issuer, uuid: 'foo2',
+                    last_ial1_authenticated_at: now)
+    Identity.create(user_id: 3, service_provider: issuer, uuid: 'foo3',
+                    last_ial2_authenticated_at: now)
+    Identity.create(user_id: 4, service_provider: issuer, uuid: 'foo4',
+                    last_ial2_authenticated_at: now)
+    result = [{ issuer: issuer, total_ial1_active: 2, total_ial2_active: 3 }].to_json
+
+    expect(subject.call).to eq(result)
+  end
+end
