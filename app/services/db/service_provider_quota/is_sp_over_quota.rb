0a9efb08b76c3e4008e3cb593b51f63efcf20226
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-04-21 11:14:15 -0500

    Fixes

--- app/services/db/service_provider_quota/is_sp_over_quota.rb
+++ app/services/db/service_provider_quota/is_sp_over_quota.rb
@@ -2,7 +2,7 @@ module Db
   module ServiceProviderQuota
     class IsSpOverQuota
       def self.call(issuer)
-        ServiceProviderQuota.find_by(issuer)&.percentage_full.to_i > 100
+        ::ServiceProviderQuota.find_by(issuer)&.percentage_full.to_i > 100
       end
     end
   end
