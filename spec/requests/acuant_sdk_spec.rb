81a41941c4b5be25c609938140a60aefbfdaf413
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-04-22 16:32:05 -0400

    Add unsafe-eval to the CSP for the Acuant SDK JS and WASM (#3728)
    **Why**: Because apparently that is where Safari and a few other mobile browsers look for it

--- spec/requests/acuant_sdk_spec.rb
+++ spec/requests/acuant_sdk_spec.rb
@@ -17,6 +17,12 @@ describe 'requesting acuant SDK assets' do
       expect(response.headers['Content-Type']).to eq('application/wasm')
       expect(response.body.length).to eq(File.size('public/AcuantImageProcessingService.wasm'))
     end
+
+    it 'adds unsafe-eval to the CSP' do
+      get '/verify/doc_auth/AcuantJavascriptWebSdk.min.js'
+
+      expect(response.headers['Content-Security-Policy']).to match(/script-src[^;]*'unsafe-eval'/)
+    end
   end
 
   context 'with something that is not a valid Acuant SDK asset' do
