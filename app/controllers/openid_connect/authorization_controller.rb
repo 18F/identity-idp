dba59467680d71c1851a1e1a98af4617d0eaa83f
Zach Margolis <zachary.margolis@gsa.gov>
2020-04-24 13:27:48 -0700

    wip

--- app/controllers/openid_connect/authorization_controller.rb
+++ app/controllers/openid_connect/authorization_controller.rb
@@ -71,11 +71,8 @@ module OpenidConnect
 
     def identity_needs_verification?
       @authorize_form.ial2_requested? &&
-        (current_user.decorate.identity_not_verified? || sp_requested_more_recent_verification?)
-    end
-
-    def sp_requested_more_recent_verification?
-      decorated_session.requested_more_recent_verification?
+        (current_user.decorate.identity_not_verified? ||
+          decorated_session.requested_more_recent_verification?)
     end
 
     def build_authorize_form_from_params
