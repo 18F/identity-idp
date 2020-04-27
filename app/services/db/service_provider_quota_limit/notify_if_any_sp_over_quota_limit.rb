81d9df43bc591a88ea361fdf87495cb1030b6f52
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-04-26 23:04:39 -0500

    Fix

--- app/services/db/service_provider_quota_limit/notify_if_any_sp_over_quota_limit.rb
+++ app/services/db/service_provider_quota_limit/notify_if_any_sp_over_quota_limit.rb
@@ -2,6 +2,7 @@ module Db
   module ServiceProviderQuotaLimit
     class NotifyIfAnySpOverQuotaLimit
       def self.call
+        return unless Db::ServiceProviderQuotaLimit::AnySpOverQuotaLimit.call
         email_list = JSON.parse(Figaro.env.sps_over_quota_limit_notify_email_list || '[]')
         email_list.each do |email|
           UserMailer.sps_over_quota_limit(email)
