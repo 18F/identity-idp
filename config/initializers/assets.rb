68ff316a963cc84c3cb6ec7e35d24f6f872e397f
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-04-29 15:42:11 -0400

    Move assets from the precompilation array to manifest.js
    **Why**: Sprockets started using the manifest instead of the precompilation array in v4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,13 +5,3 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[
-  i18n-strings.js email.css es5-shim.min.js html5shiv.js respond.min.js
-  intl-tel-input/build/css/intlTelInput.css intl-tel-input/build/img/flags.png
-  intl-tel-input/build/img/flags@2x.png
-]
-
-Rails.application.config.assets.precompile += %w[spec_helper.js] if Rails.env.test?
