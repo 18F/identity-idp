17e499aa661d1e0b9653340ba82fe67c3493e7ce
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-05-11 22:18:53 -0500

    Line breaks

--- app/services/idv/acuant/fake_liveness.rb
+++ app/services/idv/acuant/fake_liveness.rb
@@ -1,11 +1,14 @@
 module Idv
   module Acuant
     class FakeLiveness
-      # rubocop:disable all
-      GOOD_LIVENESS_DATA = {'LivenessResult':{'Score':99,'LivenessAssessment':'Live'},'Error':nil,'ErrorCode':nil,'TransactionId':'4a11ceed-7a54-45fa-9528-3945b51a1e23'}
-      # rubocop:enable
+      GOOD_LIVENESS_DATA = {
+        'LivenessResult': { 'Score': 99, 'LivenessAssessment': 'Live' },
+        'Error': nil,
+        'ErrorCode': nil,
+        'TransactionId': '4a11ceed-7a54-45fa-9528-3945b51a1e23',
+      }.freeze
 
-      def liveness(body)
+      def liveness(_body)
         [true, GOOD_LIVENESS_DATA.to_json]
       end
     end
