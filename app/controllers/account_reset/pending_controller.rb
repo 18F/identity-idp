da6b5f02e97a7578b6333beae21b1db6b4ed7c7a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-05-22 12:44:57 -0400

    Add a service for finding pending account reset requets

--- app/controllers/account_reset/pending_controller.rb
+++ app/controllers/account_reset/pending_controller.rb
@@ -2,29 +2,26 @@ module AccountReset
   class PendingController < ApplicationController
     include PendingAccountResetRequestConcern
 
-    before_action :render_404_if_request_missing
+    before_action :confirm_account_reset_request_exists
 
     def show
       analytics.track_event event: 'account reset is pending', user_id: current_user.uuid
-
       @pending_presenter = AccountReset::PendingPresenter.new(account_reset_request)
     end
 
     def cancel
-      account_reset_request.update(cancelled_at: Time.zone.now)
-      current_user.confirmed_email_addresses.each do |email_address|
-        UserMailer.account_reset_cancel(email_address).deliver_now
-      end
+      AccountReset::CancelRequestForUser.new(user).call
+      # account_reset_request.update(cancelled_at: Time.zone.now)
+      # current_user.confirmed_email_addresses.each do |email_address|
+      #   UserMailer.account_reset_cancel(email_address).deliver_now
+      # end
       redirect_to user_two_factor_authentication_url
-    rescue StandardError
-      flash[:error] = t('account_reset.pending.cancel_error')
-      redirect_to account_reset_pending_url
     end
 
     private
 
-    def render_404_if_request_missing
-      render_not_found unless account_reset_request
+    def confirm_account_reset_request_exists
+      render_not_found if account_reset_request.blank?
     end
 
     def account_reset_request
--- app/controllers/concerns/pending_account_reset_request_concern.rb
+++ app/controllers/concerns/pending_account_reset_request_concern.rb
@@ -1,11 +1,5 @@
 module PendingAccountResetRequestConcern
   extend ActiveSupport::Concern
 
-  def pending_account_reset_request(user)
-    AccountResetRequest.where(
-      user_id: user.id,
-      granted_at: nil,
-      cancelled_at: nil,
-    ).order(requested_at: :asc).first
-  end
+  def pending_account_reset_request(user); end
 end
