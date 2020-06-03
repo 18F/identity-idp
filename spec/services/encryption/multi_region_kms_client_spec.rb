5b528a2059e30a6fc6e108be334b2057be2901a3
Will Beddow <william.beddow@gsa.gov>
2020-06-03 14:46:19 -0400

    Expand region specific testing
    Add tests to assert that region prioritization for decryption is implemented successfully, and that region failover works

--- spec/services/encryption/multi_region_kms_client_spec.rb
+++ spec/services/encryption/multi_region_kms_client_spec.rb
@@ -4,12 +4,14 @@ describe Encryption::MultiRegionKMSClient do
   before do
     stub_mapped_aws_kms_client(
       'a' * 3000 => 'kms1',
+      'b' * 3000 => 'kms2',
     )
 
     allow(FeatureManagement).to receive(:use_kms?).and_return(kms_enabled)
   end
 
-  let(:plaintext) { 'a' * 3000 }
+  let(:first_plaintext) { 'a' * 3000 }
+  let(:second_plaintext) { 'b' * 3000 }
   let(:encryption_context) { { 'context' => 'attribute-bundle', 'user_id' => '123-abc-456-def' } }
 
   kms_regions = JSON.parse(Figaro.env.aws_kms_regions)
@@ -33,45 +35,71 @@ describe Encryption::MultiRegionKMSClient do
   describe '#encrypt' do
     context 'with multi region enabled' do
       it 'encrypts with KMS' do
-        result = subject.encrypt(aws_key_id, plaintext, encryption_context)
+        result = subject.encrypt(aws_key_id, first_plaintext, encryption_context)
         expect(result).to eq(regionalized_kms_ciphertext)
       end
     end
 
-    it 'decrypts successfully if the default region is not present' do
-      ciphertext = {
-        regions: { 'us-east-1' => 'kms1' },
-      }.to_json
-      result = subject.decrypt(ciphertext, encryption_context)
-      expect(result).to eq(plaintext)
-    end
 
-    it 'decrypts unsuccessfully if none of the encryption regions are present' do
-      ciphertext = {
-        regions: {
-          foo: 'kms1',
-          bar: 'kms2',
-        },
-      }.to_json
-      expect {
-        subject.decrypt(ciphertext, encryption_context)
-      }.to raise_error(Encryption::EncryptionError)
-    end
   end
 
   describe '#decrypt' do
     context 'with a multi region ciphertext' do
       it 'decrypts the ciphertext with KMS' do
         result = subject.decrypt(regionalized_kms_ciphertext, encryption_context)
-        expect(result).to eq(plaintext)
+        expect(result).to eq(first_plaintext)
       end
     end
 
     context 'with a legacy ciphertext' do
       it 'decrypts the ciphertext with KMS' do
         result = subject.decrypt(legacy_kms_ciphertext, encryption_context)
-        expect(result).to eq(plaintext)
+        expect(result).to eq(first_plaintext)
       end
+    end
+
+    it 'decrypts successfully if the default region is not present' do
+      non_default_ciphertext = {
+        regions: { 'us-east-1' => 'kms1' },
+      }.to_json
+      result = subject.decrypt(non_default_ciphertext, encryption_context)
+      expect(result).to eq(first_plaintext)
+    end
+
+    it 'decrypts unsuccessfully if none of the encryption regions are present' do
+      bad_region_ciphertext = {
+        regions: {
+          foo: 'kms1',
+          bar: 'kms2',
+        },
+      }.to_json
+      expect do
+        subject.decrypt(bad_region_ciphertext, encryption_context)
+      end.to raise_error(Encryption::EncryptionError)
+    end
+
+    it 'decrypts successfully if only one of the encryption regions is valid' do
+      partially_valid_ciphertext = {
+        regions: {
+          foo: 'kms1',
+          'us-west-2': 'kms2',
+        },
+      }.to_json
+      result = subject.decrypt(partially_valid_ciphertext, encryption_context)
+      expect(result).to eq(second_plaintext)
+    end
+
+    it 'decrypts in default region where multiple regions present' do
+      # At the moment, 'us-east-1' is the default AWS region for testing in application.yml.
+      # This test will fail if that changes, make sure to update accordingly
+      multi_region_ciphertext = {
+        regions: {
+          'us-east-1': 'kms1',
+          'us-west-2': 'kms2',
+        },
+      }.to_json
+      result = subject.decrypt(multi_region_ciphertext, encryption_context)
+      expect(result).to eq(first_plaintext)
     end
   end
 end
