5e89f02d6b00b0a8047509feae6aa69a3126121d
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-06-03 21:55:19 -0500

    Add ial3 request

--- spec/features/idv/liveness/upgrade_to_strong_ial2_spec.rb
+++ spec/features/idv/liveness/upgrade_to_strong_ial2_spec.rb
@@ -5,7 +5,7 @@ describe 'Strong IAL2' do
   include OidcAuthHelper
   include DocAuthHelper
 
-  context 'with a liveness required SP and a current verified profile with no liveness' do
+  context 'with an sp that requires liveness and a current verified profile with no liveness' do
     before do
       ServiceProvider.from_issuer('urn:gov:gsa:openidconnect:sp:server').update!(
         liveness_checking_required: true,
@@ -30,6 +30,31 @@ describe 'Strong IAL2' do
       allow(Figaro.env).to receive(:liveness_checking_enabled).and_return('false')
 
       visit_idp_from_sp_with_ial2(:oidc)
+
+      expect(current_url).to start_with('http://localhost:7654/auth/result?error=invalid_request'\
+'&error_description=Acr+values+Liveness+checking+is+disabled')
+    end
+  end
+
+  context 'with an sp that sends an IAL3 request and a current verified profile with no liveness' do
+    it 'upgrades user to IAL3 if liveness checking is enabled' do
+      allow(Figaro.env).to receive(:liveness_checking_enabled).and_return('true')
+
+      user ||= create(:profile, :active, :verified,
+                      pii: { first_name: 'John', ssn: '111223333' }).user
+      visit_idp_from_oidc_sp_with_ial3
+      sign_in_user(user)
+      fill_in_code_with_last_phone_otp
+      click_submit_default
+      click_agree_and_continue_optional
+
+      expect(page.current_path).to eq(idv_doc_auth_welcome_step)
+    end
+
+    it 'returns an error if liveness checking is disabled' do
+      allow(Figaro.env).to receive(:liveness_checking_enabled).and_return('false')
+
+      visit_idp_from_oidc_sp_with_ial3
 
       expect(current_url).to start_with('http://localhost:7654/auth/result?error=invalid_request'\
 '&error_description=Acr+values+Liveness+checking+is+disabled')
