644ecff42eee156a6e9d635544814ad4f55f1597
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-06-04 14:21:24 -0500

    Revert

--- app/services/acuant/acuant_base.rb
+++ app/services/acuant/acuant_base.rb
@@ -23,7 +23,7 @@ module Acuant
     end
 
     def new_assure_id
-      (Rails.env.test? ? Idv::Acuant::FakeAssureId : Idv::Acuant::AssureId).new
+      (Figaro.env.acuant_simulator == 'true' ? Idv::Acuant::FakeAssureId : Idv::Acuant::AssureId).new
     end
   end
 end
