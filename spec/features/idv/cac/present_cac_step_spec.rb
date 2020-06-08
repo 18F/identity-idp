e5f6fab551a514c90d77fa7d1c972e698915f541
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-06-08 16:15:05 -0500

    More coverage

--- spec/features/idv/cac/present_cac_step_spec.rb
+++ spec/features/idv/cac/present_cac_step_spec.rb
@@ -11,19 +11,16 @@ feature 'cac proofing present cac step' do
   end
 
   before do
+    simulate_piv_cac_token(decoded_token)
     sign_in_and_2fa_user
     complete_cac_proofing_steps_before_present_cac_step
-    # temporary until nonce specs fixed
-    allow_any_instance_of(PivCacProofingForm).to receive(:token_has_correct_nonce).and_return(true)
   end
 
   it 'is on the correct page' do
     expect(page).to have_current_path(idv_cac_proofing_present_cac_step)
   end
 
-  it 'proceeds to the next page with a CAC and does not ask for full name' do
-    allow(PivCacService).to receive(:decode_token).and_return(decoded_token)
-
+  it 'proceeds to the next page with a CAC' do
     click_link t('forms.buttons.cac')
 
     expect(page.current_url.include?("/\?nonce=")).to eq(true)
@@ -31,14 +28,14 @@ feature 'cac proofing present cac step' do
     visit idv_cac_step_path(step: :present_cac, token: 'foo')
 
     expect(page.current_path).to eq(idv_cac_proofing_enter_info_step)
-    # expect(page.have_content).to_not have(t('in_person_proofing.forms.first_name'))
-    # expect(page.have_content).to_not have(t('in_person_proofing.forms.last_name'))
 
     expect(DocAuthLog.first.present_cac_submit_count).to eq(1)
     expect(DocAuthLog.first.present_cac_error_count).to eq(0)
   end
 
   it 'does not proceed to the next page with a bad CAC and allows doc auth' do
+    simulate_piv_cac_token({})
+
     click_link t('forms.buttons.cac')
 
     expect(page.current_url.include?("/\?nonce=")).to eq(true)
@@ -52,12 +49,12 @@ feature 'cac proofing present cac step' do
     expect(DocAuthLog.first.present_cac_error_count).to eq(1)
   end
 
-  it 'does proceed to the next page if card_type is PIV and asks for full name' do
+  it 'does proceed to the next page if card_type is PIV' do
     decoded_token_piv = {
       'subject' => 'C=US, O=U.S. Government, OU=DoD, OU=PKI, CN=DOE.JOHN.1234',
       'card_type' => 'piv',
     }
-    allow(PivCacService).to receive(:decode_token).and_return(decoded_token_piv)
+    simulate_piv_cac_token(decoded_token_piv)
 
     click_link t('forms.buttons.cac')
 
@@ -66,7 +63,5 @@ feature 'cac proofing present cac step' do
     visit idv_cac_step_path(step: :present_cac, token: 'foo')
 
     expect(page.current_path).to eq(idv_cac_proofing_enter_info_step)
-    # expect(page.have_content).to have(t('in_person_proofing.forms.first_name'))
-    # expect(page.have_content).to have(t('in_person_proofing.forms.last_name'))
   end
 end
