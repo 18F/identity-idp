84133f21b5a5068641002c19aea90b40226a8010
Zach Margolis <zachary.margolis@gsa.gov>
2020-06-10 14:58:55 -0700

    Put this back the way it was

--- app/controllers/openid_connect/authorization_controller.rb
+++ app/controllers/openid_connect/authorization_controller.rb
@@ -20,7 +20,7 @@ module OpenidConnect
       return redirect_to_account_or_verify_profile_url if profile_or_identity_needs_verification?
       return redirect_to(sign_up_completed_url) if needs_sp_attribute_verification?
       link_identity_to_service_provider
-      return redirect_to(user_authorization_confirmation_url) if auth_count > 1
+      return redirect_to(user_authorization_confirmation_url) if auth_count == 1
       handle_successful_handoff
     end
 
