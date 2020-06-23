4a38b02692027c99556d1d65c932972391cd5528
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-06-22 22:22:20 -0500

    Update specs

--- spec/services/reports/sp_active_users_over_period_of_performance_report_spec.rb
+++ spec/services/reports/sp_active_users_over_period_of_performance_report_spec.rb
@@ -5,6 +5,7 @@ describe Reports::SpActiveUsersOverPeriodOfPerformanceReport do
 
   let(:issuer) { 'foo' }
   let(:issuer2) { 'foo2' }
+  let(:app_id) { 'app' }
 
   it 'is empty' do
     expect(subject.call).to eq('[]')
@@ -12,7 +13,7 @@ describe Reports::SpActiveUsersOverPeriodOfPerformanceReport do
 
   it 'returns total active user counts per sp broken down by ial1 and ial2' do
     now = Time.zone.now
-    ServiceProvider.create(issuer: issuer, friendly_name: issuer)
+    ServiceProvider.create(issuer: issuer, friendly_name: issuer, app_id: app_id)
     Identity.create(user_id: 1, service_provider: issuer, uuid: 'foo1',
                     last_ial1_authenticated_at: now, last_ial2_authenticated_at: now)
     Identity.create(user_id: 2, service_provider: issuer, uuid: 'foo2',
@@ -21,7 +22,8 @@ describe Reports::SpActiveUsersOverPeriodOfPerformanceReport do
                     last_ial2_authenticated_at: now)
     Identity.create(user_id: 4, service_provider: issuer, uuid: 'foo4',
                     last_ial2_authenticated_at: now)
-    result = [{ issuer: issuer, total_ial1_active: 2, total_ial2_active: 3 }].to_json
+    result = [{ issuer: issuer, app_id: app_id, total_ial1_active: 2,
+                total_ial2_active: 3 }].to_json
 
     expect(subject.call).to eq(result)
   end
