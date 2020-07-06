edd6c15e5cb0deced95569a6b23c9792c17e21b5
Doug Price <dprice@fearless.tech>
2020-07-06 16:40:47 -0400

    rubocop/pending specs

--- spec/features/saml/aal3_required_spec.rb
+++ spec/features/saml/aal3_required_spec.rb
@@ -3,33 +3,19 @@ require 'rails_helper'
 describe 'AAL3 authentication required in an SAML context' do
   include SamlAuthHelper
 
-  describe 'SAML requesting AAL3 authentication' do
-    xcontext 'user does not have aal3 auth configured' do
-      it 'sends user to set up AAL3 auth' do
-        user = user_with_2fa
+  describe 'SAML ServiceProvider requesting AAL3 authentication' do
+    pending 'Pending actual support of multiple auth contexts in SAML'
 
-        visit_idp_from_ial1_oidc_sp_requesting_aal3(prompt: 'select_account')
-        sign_in_live_with_2fa(user)
-
-        expect(current_url).to eq(two_factor_options_url)
-        expect(page).to have_content(t('two_factor_authentication.two_factor_aal3_choice'))
-        expect(page).to have_xpath("//img[@alt='important alert icon']")
-      end
+    context 'user does not have aal3 auth configured' do
+      it 'sends user to set up AAL3 auth'
     end
 
-    xcontext 'user has aal3 auth configured' do
-      it 'sends user to authenticate with AAL3 auth' do
-        user = user_with_aal3_2fa
-
-        visit_idp_from_ial1_oidc_sp_requesting_aal3(prompt: 'select_account')
-        sign_in_live_with_aal2_2fa_only(user)
-
-        expect(current_url).to eq(login_two_factor_webauthn_url)
-      end
+    context 'user has aal3 auth configured' do
+      it 'sends user to authenticate with AAL3 auth'
     end
   end
 
-  describe 'ServiceProvider configured to require AAL3 authentication' do
+  describe 'SAML ServiceProvider configured to require AAL3 authentication' do
     context 'user does not have aal3 auth configured' do
       it 'sends user to set up AAL3 auth' do
         user = user_with_2fa
