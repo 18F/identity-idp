4ed05a0cf4520d7f005cab2adb8e59cd82ef7017
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-10 11:58:25 -0400

    Create new JavaScript pack document-capture

--- app/javascript/packs/acuant_document_capture.js
+++ app/javascript/packs/acuant_document_capture.js
@@ -1,14 +1,7 @@
 import { loadAndInitializeAcuantSdk } from '../app/acuant/document_capture';
 import { setDocumentCaptureFallbackTimeout } from '../app/acuant/document_capture_fallback';
-import { createElement } from 'react';
-import ReactDOM from 'react-dom';
-import Test from '../app/acuant/document_capture_new';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const reactContainer = document.getElementById('document-capture-form');
-  if (reactContainer) {
-    ReactDOM.render(createElement(Test), reactContainer);
-  }
   loadAndInitializeAcuantSdk();
   setDocumentCaptureFallbackTimeout();
 });
