5bd4dd695068872b4ea6828fa260344323a5a1b4
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-13 11:27:06 -0400

    Fix typo capture-flow -> document-capture
    Leftover from earlier iterations

--- spec/javascripts/app/document-capture/components/document-capture-spec.jsx
+++ spec/javascripts/app/document-capture/components/document-capture-spec.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import DocumentCapture from '../../../../../app/javascript/app/document-capture/components/document-capture';
 import { useDOM } from '../../../support/dom';
 
-describe('document-capture/components/capture-flow', () => {
+describe('document-capture/components/document-capture', () => {
   useDOM();
 
   it('renders', () => {
