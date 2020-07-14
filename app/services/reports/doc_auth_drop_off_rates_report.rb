7d4a3f248c9d3c4214bb5924b62d665f64ffd355
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-07-14 00:22:47 -0500

    Lint

--- app/services/reports/doc_auth_drop_off_rates_report.rb
+++ app/services/reports/doc_auth_drop_off_rates_report.rb
@@ -13,22 +13,26 @@ module Reports
 
     def generate_report
       ret = []
-      all_sps_report(ret)
+      generate_per_sp_reports(ret)
       per_sp_report(ret)
       ret
     end
 
-    def per_sp_report(ret)
+    def generate_per_sp_reports(ret)
       ServiceProvider.where(ial: 2).each do |sp|
         transaction_with_timeout do
-          ret << Db::DocAuthLog::BlanketDropOffRatesPerSpAllTime.new.
-                 call('Drop off rates per SP all time', sp.issuer)
-          ret << Db::DocAuthLog::BlanketDropOffRatesPerSpInRange.new.
-                 call('Drop off rates last 24 hours', sp.issuer, Date.yesterday, today)
-          ret << Db::DocAuthLog::BlanketDropOffRatesPerSpInRange.new.
-                 call('Drop off rates last 30 days', sp.issuer, today - 30.days, today)
+          generate_per_sp_report(sp, ret)
         end
       end
+    end
+
+    def generate_per_sp_report(sp, ret)
+      ret << Db::DocAuthLog::BlanketDropOffRatesPerSpAllTime.new.
+             call('Drop off rates per SP all time', sp.issuer)
+      ret << Db::DocAuthLog::BlanketDropOffRatesPerSpInRange.new.
+             call('Drop off rates last 24 hours', sp.issuer, Date.yesterday, today)
+      ret << Db::DocAuthLog::BlanketDropOffRatesPerSpInRange.new.
+             call('Drop off rates last 30 days', sp.issuer, today - 30.days, today)
     end
 
     def all_sps_report(ret)
--- spec/features/reports/doc_auth_drop_off_rates_per_sprint_report_spec.rb
+++ spec/features/reports/doc_auth_drop_off_rates_per_sprint_report_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 feature 'Doc auth drop offs per sprint report' do
   it 'does not throw an error' do
-    expect(Reports::DocAuthDropOffRatesReport.new.call).to be_present
+    expect(Reports::DocAuthDropOffRatesPerSprintReport.new.call).to be_present
   end
 end
