7e31dd77336816f55649b92a024d2a7dae628c79
amathews <amathews@fearsol.com>
2020-07-14 17:09:33 -0400

    LG-3076 Fixing code climate.

--- app/services/account_reset/create_request.rb
+++ app/services/account_reset/create_request.rb
@@ -41,9 +41,6 @@ module AccountReset
     def notify_user_by_sms_if_applicable
       phone = MfaContext.new(user).phone_configurations.take&.phone
       return unless phone
-      cancel_link = Rails.application.routes.url_helpers.account_reset_cancel_url(
-        token: user.account_reset_request.request_token,
-      )
       @telephony_response = Telephony.send_account_reset_notice(to: phone)
     end
 
