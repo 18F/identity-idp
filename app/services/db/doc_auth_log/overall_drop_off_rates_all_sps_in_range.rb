9fc8a43d67bb618da164eb2f9e7cd2803cf07c2e
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-07-13 23:55:07 -0500

    More lint

--- app/services/db/doc_auth_log/overall_drop_off_rates_all_sps_in_range.rb
+++ app/services/db/doc_auth_log/overall_drop_off_rates_all_sps_in_range.rb
@@ -12,7 +12,7 @@ module Db
       def verified_user_counts_query
         <<~SQL
           #{select_count_from_profiles_where_verified_and_active}
-          and user_id in (select user_id from doc_auth_logs where '#{start}' <= welcome_view_at and welcome_view_at < '#{finish}' and (front_image_submit_count>0 or back_image_submit_count>0 or mobile_front_image_submit_count>0 or  mobile_back_image_submit_count>0 or  capture_mobile_back_image_submit_count>0))
+          and user_id in (select user_id from doc_auth_logs where '#{start}' <= welcome_view_at and welcome_view_at < '#{finish}' and #{at_least_one_image_submitted})
         SQL
       end
 
