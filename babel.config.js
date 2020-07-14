9481b936137e1a6f272ab5c84204151ff75b5726
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-14 10:40:58 -0400

    Fix lint errors in base babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -7,9 +7,9 @@ module.exports = function(api) {
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
-      `${'Please specify a valid `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: '}${
+      `${'Please specify a valid `NODE_ENV` or '
+        + '`BABEL_ENV` environment variables. Valid values are "development", '
+        + '"test", and "production". Instead, received: '}${
         JSON.stringify(currentEnv)
       }.`,
     );
