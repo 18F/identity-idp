3defee03f0ebdd6e739b5f10b6f9fe090baedd53
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-16 13:05:12 -0400

    Add Accordion React component
    **Why**: To recreate the existing document capture flow, we will need to be able to represent collapsible accordion content in React

---
+++ spec/javascripts/app/document-capture/components/accordion-spec.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Accordion from '../../../../../app/javascript/app/document-capture/components/accordion';
+
+describe('document-capture/components/accordion', () => {
+  it('renders with a unique ID', () => {
+    const { container } = render(
+      <>
+        <Accordion title="Title">Content</Accordion>
+        <Accordion title="Title">Content</Accordion>
+      </>,
+    );
+
+    const contents = container.querySelectorAll('[id^="accordion-content-"]');
+
+    expect(contents).to.have.lengthOf(2);
+    expect(contents[0].id).to.be.ok();
+    expect(contents[1].id).to.be.ok();
+    expect(contents[0].id).not.to.equal(contents[1].id);
+  });
+});
