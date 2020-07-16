4da514cc4bcb975ece9dc1b5a9f5fe2de38cd95f
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-16 13:06:28 -0400

    Add document tips accordion to document capture
    **Why**: Recreating the existing document capture flow requires integrating existing tips content

--- app/javascript/app/document-capture/components/document-capture.jsx
+++ app/javascript/app/document-capture/components/document-capture.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
+import DocumentTips from './document-tips';
+import Image from './image';
 import useI18n from '../hooks/use-i18n';
 
 function DocumentCapture() {
   const t = useI18n();
 
-  return t('doc_auth.headings.welcome');
+  const sample = (
+    <Image
+      assetPath="state-id-sample-front.jpg"
+      alt="Sample front of state issued ID"
+      width={450}
+      height={338}
+    />
+  );
+
+  return (
+    <>
+      <DocumentTips sample={sample} />
+      {t('doc_auth.headings.welcome')}
+    </>
+  );
 }
 
 export default DocumentCapture;
