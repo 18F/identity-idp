deb624c6685c17004c28699875e21cedbbf98ed7
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-16 10:17:03 -0400

    Stub self global in test environment
    **Why**: If classList.js is imported (directly or indirectly) through any tested files, an error will occur.
    
    See inline comment for additional details.

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -3,3 +3,12 @@ const dirtyChai = require('dirty-chai');
 
 chai.use(dirtyChai);
 global.expect = chai.expect;
+
+// classList.js will throw an error when loaded into the test environment, since
+// it assumes the presence of a `self` global. This shim is enough only to skip
+// the polyfill. It's expected where a DOM is used that JSDOM will provide the
+// Element#classList implementation.
+//
+// See: https://github.com/eligrey/classList.js/issues/48
+// See: https://github.com/eligrey/classList.js/blob/ecb3305/classList.js#L14
+global.self = global;
