93df341edaf7f245ed7eeb8ae9cb9ae7dc830568
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-16 10:05:12 -0400

    Create application asset context
    **Why**: Asset paths include a fingerprint which can't be known to the client unless provided by the server.
    
    Co-authored-by: Will Beddow <william.beddow@gsa.gov>

--- app/javascript/packs/document-capture.jsx
+++ app/javascript/packs/document-capture.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import DocumentCapture from '../app/document-capture/components/document-capture';
+import AssetContext from '../app/document-capture/context/asset';
 import I18nContext from '../app/document-capture/context/i18n';
 
-const { I18n: i18n } = window.LoginGov;
+const { I18n: i18n, assets } = window.LoginGov;
 
 const appRoot = document.getElementById('document-capture-form');
 appRoot.innerHTML = '';
 render(
-  <I18nContext.Provider value={i18n.strings[i18n.currentLocale()]}>
-    <DocumentCapture />
-  </I18nContext.Provider>,
+  <AssetContext.Provider value={assets}>
+    <I18nContext.Provider value={i18n.strings[i18n.currentLocale()]}>
+      <DocumentCapture />
+    </I18nContext.Provider>
+  </AssetContext.Provider>,
   appRoot,
 );
