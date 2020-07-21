98fa9fd58875f5158e0b731bee74f0731b459ee9
Andrew Duthie <andrew.duthie@gsa.gov>
2020-07-21 11:55:42 -0400

    Add React-based tips accordion to single-page document capture form (#3931)
    * Create application asset context
    
    **Why**: Asset paths include a fingerprint which can't be known to the client unless provided by the server.
    
    Co-authored-by: Will Beddow <william.beddow@gsa.gov>
    
    * Update accordion to check for previous initialization
    
    **Why**: Current Login.gov pages will check for and initialize any accordions present at page load. Since React components can be mounted after page load, and also need to initialize, there should be internal checks to protect against double initialization, since otherwise event handlers may be triggered multiple times.
    
    * Add Image component
    
    **Why**: As an abstraction to simply render an image at the known asset path, encapsulating behavior to perform asset lookup by asset context
    
    * Add Accordion React component
    
    **Why**: To recreate the existing document capture flow, we will need to be able to represent collapsible accordion content in React
    
    * Add document tips accordion to document capture
    
    **Why**: Recreating the existing document capture flow requires integrating existing tips content
    
    Co-authored-by: Will Beddow <william.beddow@gsa.gov>

--- app/javascript/app/document-capture/components/document-capture.jsx
+++ app/javascript/app/document-capture/components/document-capture.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import AcuantCapture from './acuant-capture';
+import DocumentTips from './document-tips';
+import Image from './image';
 import useI18n from '../hooks/use-i18n';
 
 function DocumentCapture() {
   const t = useI18n();
 
+  const sample = (
+    <Image
+      assetPath="state-id-sample-front.jpg"
+      alt="Sample front of state issued ID"
+      width={450}
+      height={338}
+    />
+  );
+
   return (
     <>
       <h2>{t('doc_auth.headings.welcome')}</h2>
+      <DocumentTips sample={sample} />
       <AcuantCapture />
     </>
   );
