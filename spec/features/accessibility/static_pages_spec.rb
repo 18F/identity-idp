7dd0ed5a440f2b9eacce54a22bcda67070f61dd7
Mitchell Henke <mitchell.henke@gsa.gov>
2020-07-23 14:10:37 -0500

    Check axe best practices and section 508 (LG-3221)
    **Why**:
    By default, axe only checks "best practices", which may not
    overlap with Section 508 checks.

--- spec/features/accessibility/static_pages_spec.rb
+++ spec/features/accessibility/static_pages_spec.rb
@@ -5,30 +5,30 @@ feature 'Accessibility on static pages', :js do
   scenario 'not found page' do
     visit '/non_existent_page'
 
-    expect(page).to be_accessible
+    expect(page).to be_accessible.according_to :section508, :"best-practice"
   end
 
   scenario '401 page' do
     visit '/401'
 
-    expect(page).to be_accessible
+    expect(page).to be_accessible.according_to :section508, :"best-practice"
   end
 
   scenario '422 page' do
     visit '/422'
 
-    expect(page).to be_accessible
+    expect(page).to be_accessible.according_to :section508, :"best-practice"
   end
 
   scenario '429 page' do
     visit '/429'
 
-    expect(page).to be_accessible
+    expect(page).to be_accessible.according_to :section508, :"best-practice"
   end
 
   scenario '500 page' do
     visit '/500'
 
-    expect(page).to be_accessible
+    expect(page).to be_accessible.according_to :section508, :"best-practice"
   end
 end
