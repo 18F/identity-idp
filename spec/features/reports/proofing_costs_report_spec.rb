77ce5ad13fbe402a49fe98a49d1a5fe03202f61e
Zach Margolis <zachary.margolis@gsa.gov>
2020-08-03 11:34:36 -0700

    add some specific tests for resultcode, fix proofing cost spec

--- spec/features/reports/proofing_costs_report_spec.rb
+++ spec/features/reports/proofing_costs_report_spec.rb
@@ -21,7 +21,7 @@ feature 'Proofing Costs report' do
     {
       'acuant_front_image_count_average' => 1.0,
       'acuant_back_image_count_average' => 1.0,
-      'acuant_result_count_average' => 0.0,
+      'acuant_result_count_average' => 1.0,
       'aamva_count_average' => 1.0,
       'lexis_nexis_resolution_count_average' => 1.0,
       'gpo_letter_count_average' => 0.0,
@@ -39,19 +39,6 @@ feature 'Proofing Costs report' do
     complete_doc_auth_steps_before_doc_success_step
 
     expect(report).to eq(doc_success_funnel.merge(summary1))
-  end
-
-  it 'works for one flow that does the selfie check' do
-    allow(Figaro.env).to receive(:liveness_checking_enabled).and_return('true')
-    allow(Figaro.env).to receive(:acuant_sdk_document_capture_enabled).and_return('true')
-    allow(Figaro.env).to receive(:document_capture_step_enabled).and_return('true')
-
-    sign_in_and_2fa_user(user)
-    complete_doc_auth_steps_before_document_capture_step
-    attach_images
-    click_idv_continue
-
-    expect(report).to include('acuant_result_count_average' => 1.0)
   end
 
   it 'works for two flows' do
