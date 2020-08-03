97eb1ad1f35db554e8e74bd0f9c9231e4272d443
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-03 09:13:43 -0400

    Use animated ID card for doc auth loading interstitial
    **Why**: The Clock icon was used as a temporary asset, and is not intended for use in the final design for this screen. An animation effect was added to the card to give the user the impression there is activity happening in the background, so that they aren't confused into thinking the process has stalled.

--- app/javascript/app/document-capture/components/submission-pending.jsx
+++ app/javascript/app/document-capture/components/submission-pending.jsx
@@ -4,7 +4,7 @@ import Image from './image';
 function SubmissionPending() {
   return (
     <div>
-      <Image assetPath="clock.svg" alt="" width="50" height="50" />
+      <Image assetPath="id-card.svg" alt="" width="216" height="116" />
       <h2>We are processing your images…</h2>
       <p>This might take up to a minute. We’ll load the next step automatically when it’s done.</p>
       <p>Thanks for your patience!</p>
