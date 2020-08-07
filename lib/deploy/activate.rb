0ba0b3148b570d6ceee18d11dfe0cd1b36121f0b
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-08-07 14:37:03 -0700

    Fix test flakiness: Update Deploy::Activate specs to use a tmpdir (#4033)

--- lib/deploy/activate.rb
+++ lib/deploy/activate.rb
@@ -8,14 +8,18 @@ require 'yaml'
 
 module Deploy
   class Activate
-    attr_reader :logger, :s3_client, :env_yaml_path, :result_yaml_path
+    attr_reader :logger, :s3_client
 
-    def initialize(logger: default_logger, s3_client: nil,
-                   result_yaml_path: nil, env_yaml_path: nil)
+    def initialize(
+      logger: default_logger,
+      s3_client: nil,
+      root: nil,
+      example_application_yaml_path: nil
+    )
       @logger = logger
       @s3_client = s3_client
-      @env_yaml_path = env_yaml_path || File.join(root, 'config/application_s3_env.yml')
-      @result_yaml_path = result_yaml_path || File.join(root, 'config/application.yml')
+      @root = root
+      @example_application_yaml_path = example_application_yaml_path
     end
 
     def run
@@ -134,7 +138,7 @@ module Deploy
     end
 
     def root
-      File.expand_path('../../../', __FILE__)
+      @root || File.expand_path('../../../', __FILE__)
     end
 
     def application_config
@@ -142,7 +146,15 @@ module Deploy
     end
 
     def example_application_yaml_path
-      File.join(root, 'config/application.yml.default')
+      @example_application_yaml_path || File.join(root, 'config/application.yml.default')
+    end
+
+    def env_yaml_path
+      File.join(root, 'config/application_s3_env.yml')
+    end
+
+    def result_yaml_path
+      File.join(root, 'config/application.yml')
     end
 
     def geolocation_db_path
