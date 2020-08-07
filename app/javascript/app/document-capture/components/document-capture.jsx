e1f76c1706e9551859ee7f1bc3f20c222fe50ccd
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-07 16:51:41 -0400

    Add document capture mobile introduction step

--- app/javascript/app/document-capture/components/document-capture.jsx
+++ app/javascript/app/document-capture/components/document-capture.jsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import FormSteps from './form-steps';
 import DocumentsStep, { isValid as isDocumentsStepValid } from './documents-step';
 import SelfieStep, { isValid as isSelfieStepValid } from './selfie-step';
+import MobileIntroStep from './mobile-intro-step';
+import DeviceContext from '../context/device';
 import Submission from './submission';
 
 function DocumentCapture() {
   const [formValues, setFormValues] = useState(null);
+  const { isMobile } = useContext(DeviceContext);
+
+  const steps = [
+    {
+      name: 'documents',
+      component: DocumentsStep,
+      isValid: isDocumentsStepValid,
+    },
+    {
+      name: 'selfie',
+      component: SelfieStep,
+      isValid: isSelfieStepValid,
+    },
+  ];
+
+  if (isMobile) {
+    steps.unshift({
+      name: 'intro',
+      component: MobileIntroStep,
+    });
+  }
 
   return formValues ? (
     <Submission payload={formValues} />
   ) : (
-    <FormSteps
-      steps={[
-        {
-          name: 'documents',
-          component: DocumentsStep,
-          isValid: isDocumentsStepValid,
-        },
-        {
-          name: 'selfie',
-          component: SelfieStep,
-          isValid: isSelfieStepValid,
-        },
-      ]}
-      onComplete={setFormValues}
-    />
+    <FormSteps steps={steps} onComplete={setFormValues} />
   );
 }
 
