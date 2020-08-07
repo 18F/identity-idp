b2192b8909453768137f063c21c8e5a490f901ed
Mitchell Henke <mitchell.henke@gsa.gov>
2020-08-07 10:38:12 -0500

    Allow disabling bootsnap via ENV

--- config/boot.rb
+++ config/boot.rb
@@ -5,6 +5,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 begin
-  require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'production'
+  require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'production' && ENV['ENABLE_BOOTSNAP'] != 'false'
 rescue LoadError
 end
