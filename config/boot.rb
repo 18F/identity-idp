46e3635563158a14aee1dee0ad19a9782c924ee8
Mitchell Henke <mitchell.henke@gsa.gov>
2020-08-07 12:09:15 -0500

    rubocop

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,7 @@ ENV['RAILS_ENV'] ||= 'production'.freeze if File.exist?('/etc/login.gov/info/dom
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+# rubocop:disable Lint/HandleExceptions
 begin
   require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'production' && ENV['ENABLE_BOOTSNAP'] != 'false'
 rescue LoadError
