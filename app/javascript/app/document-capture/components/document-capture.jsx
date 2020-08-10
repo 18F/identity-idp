804ea7436640af233e96f2cfc2371b9f9c5d4c7b
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-10 08:50:10 -0400

    Add document capture mobile introduction step (#4030)
    * Add document capture mobile introduction step
    
    * Construct steps array as compacted array
    
    See: https://github.com/18F/identity-idp/pull/4030#discussion_r467276832

--- app/javascript/app/document-capture/components/document-capture.jsx
+++ app/javascript/app/document-capture/components/document-capture.jsx
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import FormSteps from './form-steps';
 import DocumentsStep, { isValid as isDocumentsStepValid } from './documents-step';
 import SelfieStep, { isValid as isSelfieStepValid } from './selfie-step';
+import MobileIntroStep from './mobile-intro-step';
+import DeviceContext from '../context/device';
 import Submission from './submission';
 
 function DocumentCapture() {
   const [formValues, setFormValues] = useState(null);
+  const { isMobile } = useContext(DeviceContext);
+
+  const steps = [
+    isMobile && {
+      name: 'intro',
+      component: MobileIntroStep,
+    },
+    {
+      name: 'documents',
+      component: DocumentsStep,
+      isValid: isDocumentsStepValid,
+    },
+    {
+      name: 'selfie',
+      component: SelfieStep,
+      isValid: isSelfieStepValid,
+    },
+  ].filter(Boolean);
 
   return formValues ? (
     <Submission payload={formValues} />
   ) : (
-    <FormSteps
-      steps={[
-        {
-          name: 'documents',
-          component: DocumentsStep,
-          isValid: isDocumentsStepValid,
-        },
-        {
-          name: 'selfie',
-          component: SelfieStep,
-          isValid: isSelfieStepValid,
-        },
-      ]}
-      onComplete={setFormValues}
-    />
+    <FormSteps steps={steps} onComplete={setFormValues} />
   );
 }
 
