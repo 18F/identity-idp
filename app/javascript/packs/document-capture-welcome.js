8a9d4a1e72aca91d041238b66c609fe8eabf3997
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-12 10:29:11 -0400

    Use mobile test to determine whether to skip
    **Why**:
    
    1. Desktop users would otherwise not see links to "Don't have a state-issued ID?" and "Use your CAC card"
    2. It's still recommended to use a phone on camera-capable desktop computers, since it is easier to take photos of documents from mobile

--- app/javascript/packs/document-capture-welcome.js
+++ app/javascript/packs/document-capture-welcome.js
@@ -1,22 +1,14 @@
 const form = document.querySelector('form[action="/verify/doc_auth/welcome"]');
 
 // TODO:
-//  Change to: import { hasCamera } from '@18f/identity-device';
+//  Change to: import { isMobile } from '@18f/identity-device';
 //  ...after:  https://github.com/18F/identity-idp/pull/4048
-export async function hasCamera() {
-  if (!('mediaDevices' in navigator)) {
-    return false;
-  }
+const isMobile =
+  'mediaDevices' in window.navigator && /ip(hone|ad|od)|android/i.test(window.navigator.userAgent);
 
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  return devices.some((device) => device.kind === 'videoinput');
+if (form && isMobile) {
+  const input = document.createElement('input');
+  input.type = 'hidden';
+  input.name = 'skip_upload';
+  form.appendChild(input);
 }
-
-(async () => {
-  if (form && (await hasCamera())) {
-    const input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = 'skip_upload';
-    form.appendChild(input);
-  }
-})();
