4f2daf2054e457ae529c398ff1de6b88a009f454
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-08-13 11:17:29 -0500

    Add auth counts

--- spec/services/reports/iaa_billing_report_spec.rb
+++ spec/services/reports/iaa_billing_report_spec.rb
@@ -10,6 +10,18 @@ describe Reports::IaaBillingReport do
         'iaa_start_date': '2020-01-01',
         'iaa_end_date': '2020-12-31',
         'ial2_active_count': 0,
+        'auth_counts': [
+          {
+            'issuer': 'http://test.host',
+            'ial': 1,
+            'count': 0,
+          },
+          {
+            'issuer': 'http://test.host',
+            'ial': 2,
+            'count': 0,
+          },
+        ],
       },
     ]
   end
