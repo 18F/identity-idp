793da62e264e4e9fa0ce5f5dd939f8cf265b60de
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-13 16:14:50 -0400

    Remove unused dependency

--- spec/javascripts/packages/document-capture/components/acuant-capture-spec.jsx
+++ spec/javascripts/packages/document-capture/components/acuant-capture-spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, cleanup } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import sinon from 'sinon';
 import AcuantCapture, {
