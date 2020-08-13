295e3325ece386b811fb9fccad5af125fdef2901
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-13 15:42:57 -0400

    Await Acuant initialization before starting capture
    **Why**: If a user is fast enough that they proceed through steps and attempt to start capture before Acuant is ready, an error would occur.
    
    These changes allow the Acuant canvas to be rendered, but for the start of capture to be delayed until Acuant is ready.

---
+++ spec/javascripts/support/acuant.js
@@ -0,0 +1,26 @@
+import sinon from 'sinon';
+import { cleanup } from '@testing-library/react';
+
+export function useAcuant() {
+  afterEach(() => {
+    // While RTL will perform this automatically, it must to occur prior to
+    // resetting the global variables, since otherwise the component's effect
+    // unsubscribe will attempt to reference globals that no longer exist.
+    cleanup();
+    delete window.AcuantJavascriptWebSdk;
+    delete window.AcuantCamera;
+    delete window.AcuantCameraUI;
+  });
+
+  return {
+    initialize({ isSuccess = true, isCameraSupported = false } = {}) {
+      window.AcuantJavascriptWebSdk = {
+        initialize: (_credentials, _endpoint, { onSuccess, onFail }) =>
+          isSuccess ? onSuccess() : onFail(),
+      };
+      window.AcuantCamera = { isCameraSupported };
+      window.AcuantCameraUI = { start: sinon.stub(), end: sinon.stub() };
+      window.onAcuantSdkLoaded();
+    },
+  };
+}
