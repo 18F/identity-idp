102fb7427ded0121b700c8f6ec84b734a90bbe97
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-14 16:58:31 -0400

    Use device package to detect mobile from document capture welcome
    **Why**: To have consistent behavior with the document capture form

--- app/javascript/packs/document-capture-welcome.js
+++ app/javascript/packs/document-capture-welcome.js
@@ -1,12 +1,8 @@
+import { isCameraCapableMobile } from '@18f/identity-device';
+
 const form = document.querySelector('form[action="/verify/doc_auth/welcome"]');
 
-// TODO:
-//  Change to: import { isMobile } from '@18f/identity-device';
-//  ...after:  https://github.com/18F/identity-idp/pull/4048
-const isMobile =
-  'mediaDevices' in window.navigator && /ip(hone|ad|od)|android/i.test(window.navigator.userAgent);
-
-if (form && isMobile) {
+if (form && isCameraCapableMobile()) {
   const input = document.createElement('input');
   input.type = 'hidden';
   input.name = 'skip_upload';
