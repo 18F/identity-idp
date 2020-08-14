cbf9b7045539b823f2a2a0563fb14c36b22d57a0
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-08-14 10:18:47 -0500

    Spec updates

--- spec/services/reports/iaa_billing_report_spec.rb
+++ spec/services/reports/iaa_billing_report_spec.rb
@@ -3,30 +3,65 @@ require 'rails_helper'
 describe Reports::IaaBillingReport do
   subject { described_class.new }
 
-  let(:results) do
+  let(:issuer) { 'foo' }
+  let(:issuer2) { 'foo2' }
+  let(:issuer3) { 'foo3' }
+  let(:iaa) { 'iaa' }
+  let(:results_for_1_iaa) do
     [
       {
-        'iaa': 'ABC123-2020',
-        'iaa_start_date': '2020-01-01',
-        'iaa_end_date': '2020-12-31',
+        'iaa': 'iaa',
+        'iaa_start_date': '2019-12-15',
+        'iaa_end_date': '2020-07-15',
         'ial2_active_count': 0,
-        'auth_counts': [
-          {
-            'issuer': 'http://test.host',
-            'ial': 1,
-            'count': 0,
-          },
-          {
-            'issuer': 'http://test.host',
-            'ial': 2,
-            'count': 0,
-          },
-        ],
+        'auth_counts':
+          [
+            {
+              'issuer': 'foo',
+              'ial': 1,
+              'count': 0,
+            },
+            {
+              'issuer': 'foo',
+              'ial': 2,
+              'count': 0,
+            },
+            {
+              'issuer': 'foo2',
+              'ial': 1,
+              'count': 0,
+            },
+            {
+              'issuer': 'foo2',
+              'ial': 2,
+              'count': 0,
+            },
+          ],
       },
     ]
   end
+  let(:now) { Time.zone.parse('2020-06-15') }
 
-  it 'works' do
-    expect(subject.call).to eq(results.to_json)
+  before do
+    ServiceProvider.delete_all
+  end
+
+  it 'works with no SPs' do
+    expect(subject.call).to eq([].to_json)
+  end
+
+  it 'ignores sps without an IAA' do
+    ServiceProvider.create(issuer: issuer, friendly_name: issuer, ial: 1)
+
+    expect(subject.call).to eq([].to_json)
+  end
+
+  it 'rolls up 2 issuers in a single IAA' do
+    ServiceProvider.create(issuer: issuer, friendly_name: issuer, ial: 1, iaa: iaa,
+                           iaa_start_date: now - 6.months, iaa_end_date: now + 1.month)
+    ServiceProvider.create(issuer: issuer2, friendly_name: issuer2, ial: 2, iaa: iaa,
+                           iaa_start_date: now - 6.months, iaa_end_date: now + 1.month)
+
+    expect(subject.call).to eq(results_for_1_iaa.to_json)
   end
 end
