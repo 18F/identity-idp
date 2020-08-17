b745250b7cc68bee8099d01ca9c78b6d8b4526e4
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-17 13:56:23 -0400

    Remove redundant step skips
    **Why:** Already skipped via `mark_document_capture_or_image_upload_steps_complete`

--- app/services/idv/steps/welcome_step.rb
+++ app/services/idv/steps/welcome_step.rb
@@ -18,12 +18,14 @@ module Idv
       private
 
       def skip_to_capture
+        # Skips to `document_capture` step. Assumes that base step will have already flagged old
+        # flow steps if `FeatureManagement.document_capture_step_enabled?`.
+        #
+        # See: `mark_document_capture_or_image_upload_steps_complete`
         mark_step_complete(:upload)
         mark_step_complete(:send_link)
         mark_step_complete(:link_sent)
         mark_step_complete(:email_sent)
-        mark_step_complete(:mobile_front_image)
-        mark_step_complete(:mobile_back_image)
         mark_step_complete(:mobile_document_capture)
       end
     end
