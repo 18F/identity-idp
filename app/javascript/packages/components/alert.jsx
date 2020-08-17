72252a614e7b4b5ba99937822f608ccaf00af751
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-17 09:01:15 -0400

    Update Alert component to accept optional classnames
    **Why:** May need to be used to offset alert by margin

--- app/javascript/packages/components/alert.jsx
+++ app/javascript/packages/components/alert.jsx
@@ -9,16 +9,19 @@ import React from 'react';
 /**
  * @typedef AlertProps
  *
- * @prop {AlertType=} type     Alert type. Defaults to "other".
- * @prop {ReactNode}  children Child elements.
+ * @prop {AlertType=} type Alert type. Defaults to "other".
+ * @prop {string=} className Optional additional class names to add to element.
+ * @prop {ReactNode} children Child elements.
  */
 
 /**
  * @param {AlertProps} props Props object.
  */
-function Alert({ type = 'other', children }) {
+function Alert({ type = 'other', className, children }) {
+  const classes = [`usa-alert usa-alert--${type}`, className].filter(Boolean).join(' ');
+
   return (
-    <div className={`usa-alert usa-alert--${type}`} role="alert">
+    <div className={classes} role="alert">
       <div className="usa-alert__body">
         <p className="usa-alert__text">{children}</p>
       </div>
--- spec/javascripts/packages/components/alert-spec.jsx
+++ spec/javascripts/packages/components/alert-spec.jsx
@@ -10,4 +10,16 @@ describe('identity-components/alert', () => {
 
     expect(alert).to.be.ok();
   });
+
+  it('accepts additional class names', () => {
+    const { getByRole } = render(
+      <Alert type="warning" className="my-class">
+        Uh oh!
+      </Alert>,
+    );
+
+    const alert = getByRole('alert');
+
+    expect(alert.classList.contains('my-class')).to.be.true();
+  });
 });
