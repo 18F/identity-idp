878ea3a112820eda2b61eec138663739649eb635
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-19 10:07:53 -0400

    Add consent blocked messaging

--- app/javascript/packages/document-capture/components/selfie-capture.jsx
+++ app/javascript/packages/document-capture/components/selfie-capture.jsx
@@ -14,6 +14,8 @@ import useI18n from '../hooks/use-i18n';
  * @param {SelfieCaptureProps} props Props object.
  */
 function SelfieCapture({ value, onChange }) {
+  const { t } = useI18n();
+
   const videoRef = useRef(/** @type {HTMLVideoElement?} */ (null));
   const setVideoRef = useCallback((ref) => {
     // React will call an assigned `ref` callback with `null` at the time the element is being
@@ -25,6 +27,7 @@ function SelfieCapture({ value, onChange }) {
     videoRef.current = ref;
   }, []);
 
+  const [isAccessRejected, setIsAccessRejected] = useState(false);
   const [isCapturing, setIsCapturing] = useState(false);
   // Sync capturing state with the availability of a value. If a value is assigned while capture is
   // in progress, reset state. Most often, this is a direct result of calling `onChange` with the
@@ -32,24 +35,35 @@ function SelfieCapture({ value, onChange }) {
   useMemo(() => setIsCapturing(isCapturing && !value), [value]);
 
   const ifStillMounted = useIfStillMounted();
-  const { t } = useI18n();
+
   useEffect(() => {
     // Start capturing only if not already capturing, and if value has yet to be assigned.
     if (value || isCapturing) {
       return;
     }
 
-    navigator.mediaDevices.getUserMedia({ video: true }).then(
-      ifStillMounted((/** @type {MediaStream} */ stream) => {
-        if (!videoRef.current) {
-          return;
-        }
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(
+        ifStillMounted((/** @type {MediaStream} */ stream) => {
+          if (!videoRef.current) {
+            return;
+          }
 
-        videoRef.current.srcObject = stream;
-        videoRef.current.play();
-        setIsCapturing(true);
-      }),
-    );
+          videoRef.current.srcObject = stream;
+          videoRef.current.play();
+          setIsCapturing(true);
+        }),
+      )
+      .catch(
+        ifStillMounted((error) => {
+          if (error.name !== 'NotAllowedError') {
+            throw error;
+          }
+
+          setIsAccessRejected(true);
+        }),
+      );
   }, [value]);
 
   function onCapture() {
@@ -81,68 +95,75 @@ function SelfieCapture({ value, onChange }) {
     .join(' ');
 
   return (
-    <div className={classes}>
-      {value ? (
-        <>
-          <div className="selfie-capture__preview-heading usa-file-input__preview-heading">
-            <span />
-            <button
-              type="button"
-              onClick={() => onChange(null)}
-              className="usa-file-input__choose usa-button--unstyled"
-            >
-              {t('doc_auth.buttons.take_picture_retry')}
-            </button>
-          </div>
-          <FileImage file={value} alt="" className="selfie-capture__preview-image" />
-        </>
-      ) : (
-        <>
-          {/* Disable reason: Video is used only for direct capture */}
-          {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-          <video ref={setVideoRef} className="selfie-capture__video" />
-          {isCapturing ? (
-            <>
-              <div className="selfie-capture__frame">
-                <div className="selfie-capture__frame-corner" />
-                <div className="selfie-capture__frame-corner" />
-                <div className="selfie-capture__frame-corner" />
-                <div className="selfie-capture__frame-corner" />
-              </div>
+    <>
+      {isAccessRejected && (
+        <span className="usa-error-message" role="alert">
+          {t('doc_auth.instructions.document_capture_selfie_consent_blocked')}
+        </span>
+      )}
+      <div className={classes}>
+        {value ? (
+          <>
+            <div className="selfie-capture__preview-heading usa-file-input__preview-heading">
+              <span />
               <button
                 type="button"
-                className="usa-button selfie-capture__capture"
-                aria-label={t('doc_auth.buttons.take_picture')}
-                onClick={onCapture}
+                onClick={() => onChange(null)}
+                className="usa-file-input__choose usa-button--unstyled"
               >
-                <svg
-                  aria-hidden
-                  focusable={false}
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                  className="selfie-capture__capture-icon"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"
-                  />
-                </svg>
+                {t('doc_auth.buttons.take_picture_retry')}
               </button>
-            </>
-          ) : (
-            <div className="selfie-capture__consent-prompt">
-              <strong className="selfie-capture__consent-prompt-banner usa-file-input__banner-text">
-                {t('doc_auth.instructions.document_capture_selfie_consent_banner')}
-              </strong>
-              <span className="usa-file-input__drag-text">
-                {t('doc_auth.instructions.document_capture_selfie_consent_reason')}
-              </span>
             </div>
-          )}
-        </>
-      )}
-    </div>
+            <FileImage file={value} alt="" className="selfie-capture__preview-image" />
+          </>
+        ) : (
+          <>
+            {/* Disable reason: Video is used only for direct capture */}
+            {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+            <video ref={setVideoRef} className="selfie-capture__video" />
+            {isCapturing ? (
+              <>
+                <div className="selfie-capture__frame">
+                  <div className="selfie-capture__frame-corner" />
+                  <div className="selfie-capture__frame-corner" />
+                  <div className="selfie-capture__frame-corner" />
+                  <div className="selfie-capture__frame-corner" />
+                </div>
+                <button
+                  type="button"
+                  className="usa-button selfie-capture__capture"
+                  aria-label={t('doc_auth.buttons.take_picture')}
+                  onClick={onCapture}
+                >
+                  <svg
+                    aria-hidden
+                    focusable={false}
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    className="selfie-capture__capture-icon"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"
+                    />
+                  </svg>
+                </button>
+              </>
+            ) : (
+              <div className="selfie-capture__consent-prompt">
+                <strong className="selfie-capture__consent-prompt-banner usa-file-input__banner-text">
+                  {t('doc_auth.instructions.document_capture_selfie_consent_banner')}
+                </strong>
+                <span className="usa-file-input__drag-text">
+                  {t('doc_auth.instructions.document_capture_selfie_consent_reason')}
+                </span>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
