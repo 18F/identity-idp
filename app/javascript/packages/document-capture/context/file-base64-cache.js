cba58857636f704b1aaa290f9fbd15841677f903
Andrew Duthie <andrew.duthie@gsa.gov>
2020-08-21 17:04:36 -0400

    Suffix Context

--- app/javascript/packages/document-capture/context/file-base64-cache.js
+++ app/javascript/packages/document-capture/context/file-base64-cache.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const FileBase64Cache = createContext(/** @type {WeakMap<Blob,string>} */ (new WeakMap()));
+const FileBase64CacheContext = createContext(/** @type {WeakMap<Blob,string>} */ (new WeakMap()));
 
-export default FileBase64Cache;
+export default FileBase64CacheContext;
