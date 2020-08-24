79e2d618b01494f1479188f19aa22b4b53970cac
Zach Margolis <zachary.margolis@gsa.gov>
2020-08-24 12:15:38 -0700

    Update asset checking script, add to CircleCI config

--- lib/asset_checker.rb
+++ lib/asset_checker.rb
@@ -1,13 +1,22 @@
+require 'yaml'
+
 class AssetChecker
-  def self.check_files(argv)
-    assets_file = 'app/assets/javascripts/assets.js.erb'
-    translations_file = 'app/assets/javascripts/i18n-strings.js.erb'
-    @asset_strings = load_included_strings(assets_file)
-    @translation_strings = load_included_strings(translations_file)
-    argv.any? { |f| file_has_missing?(f) }
+  attr_reader :files
+
+  def initialize(files)
+    @files = files
   end
 
-  def self.file_has_missing?(file)
+  # @return [Boolean] true if any files are missing
+  def check_files
+    assets_file = 'app/assets/javascripts/assets.js.erb'
+    translations_file = 'config/js_locale_strings.yml'
+    @asset_strings = load_included_strings(assets_file)
+    @translation_strings = YAML.load_file(translations_file)
+    files.any? { |f| file_has_missing?(f) }
+  end
+
+  def file_has_missing?(file)
     data = File.open(file).read
     missing_translations = find_missing(data, /\Wt\s?\(['"]([^'"]*?)['"]\)/, @translation_strings)
     missing_assets = find_missing(data, /\WassetPath=["'](.*?)['"]/, @asset_strings)
@@ -24,14 +33,14 @@ class AssetChecker
     has_missing
   end
 
-  def self.find_missing(file_data, pattern, source)
+  def find_missing(file_data, pattern, source)
     strings = (file_data.scan pattern).flatten
     strings.reject { |s| source.include? s }
   end
 
-  def self.load_included_strings(file)
+  def load_included_strings(file)
     data = File.open(file).read
-    key_data = data.split('<% keys = [')[1].split('] %>')[0]
+    key_data = data.split('<% keys = [').last.split('] %>').first
     key_data.scan(/['"](.*)['"]/).flatten
   end
 end
