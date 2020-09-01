324323817df92ada05356e5119ab35675f9aee88
Zach Margolis <zachary.margolis@gsa.gov>
2020-09-01 12:13:37 -0700

    erorrs

--- app/services/idv/steps/front_image_step.rb
+++ app/services/idv/steps/front_image_step.rb
@@ -8,7 +8,10 @@ module Idv
           flow_session[:instance_id] = create_document_response.instance_id
           upload_front_image
         else
-          failure(create_document_response.errors.values.flatten.join(' '), create_document_response.to_h)
+          failure(
+            create_document_response.errors.values.flatten.join(' '),
+            create_document_response.to_h,
+          )
         end
       end
 
