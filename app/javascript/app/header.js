3b0199cea7d4dbdf20b6e293d1bf84a26b7cf01c
Shade L. Jenifer <shade.jenifer@gsa.gov>
2020-09-09 15:15:16 -0400

    Center banner text
    **Why**: LG-3104
    **How**: CSS tweaks
    **How**: Redo banner close button (mobile) w/html + js

---
+++ app/javascript/app/header.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+document.addEventListener('DOMContentLoaded', function() {
+  $('.close-banner-how-you-know').click(function() {
+    $('.usa-proof').click();
+  });
+});
+
