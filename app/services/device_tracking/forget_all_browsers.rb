9f8b8576b4b4da285ff4140db9d6ce93a25f5e8f
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-09-14 09:23:45 -0700

    Forget remembered browsers after RISC password reset (LG-3120) (#4195)

--- app/services/device_tracking/forget_all_browsers.rb
+++ app/services/device_tracking/forget_all_browsers.rb
@@ -2,9 +2,9 @@ module DeviceTracking
   class ForgetAllBrowsers
     attr_reader :user, :remember_device_revoked_at
 
-    def initialize(user, remember_device_revoked_at: Time.zone.now)
+    def initialize(user, remember_device_revoked_at: nil)
       @user = user
-      @remember_device_revoked_at = remember_device_revoked_at
+      @remember_device_revoked_at = remember_device_revoked_at || Time.zone.now
     end
 
     def call
