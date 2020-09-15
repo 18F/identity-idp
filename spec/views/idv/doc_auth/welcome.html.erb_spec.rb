85ee0e2aafe2c83fbe4c77a249edcff193f62642
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-09-15 14:26:41 -0700

    Add maintenance window notification for Acuant (LG-3451) (#4202)
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- spec/views/idv/doc_auth/welcome.html.erb_spec.rb
+++ spec/views/idv/doc_auth/welcome.html.erb_spec.rb
@@ -52,4 +52,26 @@ describe 'idv/doc_auth/welcome.html.erb' do
       expect(rendered).to_not have_text(t('doc_auth.instructions.bullet1a'))
     end
   end
+
+  context 'during the acuant maintenance window' do
+    let(:start) { Time.zone.parse('2020-01-01T00:00:00Z') }
+    let(:now) { Time.zone.parse('2020-01-01T12:00:00Z') }
+    let(:finish) { Time.zone.parse('2020-01-01T23:59:59Z') }
+
+    before do
+      allow(Figaro.env).to receive(:acuant_maintenance_window_start).and_return(start.iso8601)
+      allow(Figaro.env).to receive(:acuant_maintenance_window_finish).and_return(finish.iso8601)
+    end
+
+    around do |ex|
+      Timecop.travel(now) { ex.run }
+    end
+
+    it 'renders the warning banner but no other content' do
+      render template: 'idv/doc_auth/welcome.html.erb'
+
+      expect(rendered).to have_content('We are currently under maintenance')
+      expect(rendered).to_not have_content(t('doc_auth.headings.welcome'))
+    end
+  end
 end
