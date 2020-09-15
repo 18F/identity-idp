ea4836e1972dc313507eb6a9a7f76a7e8f2615e6
Zach Margolis <zachary.margolis@gsa.gov>
2020-09-15 13:59:49 -0700

    rubocop: trailing whitespace

--- app/services/maintenance_window.rb
+++ app/services/maintenance_window.rb
@@ -12,7 +12,7 @@ class MaintenanceWindow
   end
 
   private
-  
+
   def parse(time_str, display_time_zone:)
     Time.zone.parse(time_str).in_time_zone(display_time_zone) if time_str.present?
   end
