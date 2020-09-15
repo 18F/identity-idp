394407d1ff74df67e1939fb2f498690cd95a20c7
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-09-15 09:51:32 -0500

    Unused

--- app/javascript/packs/acuant_selfie_capture.js
+++ app/javascript/packs/acuant_selfie_capture.js
@@ -1,5 +1,4 @@
 import { loadAndInitializeAcuantSdk } from '../app/acuant/selfie_capture';
-import { setDocumentCaptureFallbackTimeout } from '../app/acuant/document_capture_fallback';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadAndInitializeAcuantSdk();
