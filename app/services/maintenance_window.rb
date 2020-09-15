174031047c8449589c244abe4efe6cd4cd9ee982
Zach Margolis <zachary.margolis@gsa.gov>
2020-09-15 12:44:33 -0700

    Add maintenance window notification for Acuant (LG-3451)

---
+++ app/services/maintenance_window.rb
@@ -0,0 +1,18 @@
+class MaintenanceWindow
+  attr_reader :start, :finish, :now
+
+  def initialize(start:, finish:, now: nil, display_time_zone: 'America/New_York')
+    @start = parse(start, display_time_zone: display_time_zone)
+    @finish = parse(finish, display_time_zone: display_time_zone)
+    @now = now || Time.zone.now
+  end
+
+  def active?
+    (start...finish).cover?(now) if start && finish
+  end
+
+  # @api private
+  def parse(time_str, display_time_zone:)
+    Time.zone.parse(time_str).in_time_zone(display_time_zone) if time_str.present?
+  end
+end
---
+++ spec/views/shared/_maintenance_window_alert.html.erb_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'shared/_maintenance_window_alert.html.erb' do
+  let(:start) { Time.zone.parse('2020-01-01T00:00:00Z') }
+  let(:finish) { Time.zone.parse('2020-01-01T23:59:59Z') }
+
+  before do
+    allow(Figaro.env).to receive(:acuant_maintenance_window_start).and_return(start.iso8601)
+    allow(Figaro.env).to receive(:acuant_maintenance_window_finish).and_return(finish.iso8601)
+  end
+
+  subject(:render_partial) do
+    render(
+      'shared/maintenance_window_alert',
+      now: now,
+    ) { 'contents of block' }
+  end
+
+  context 'during the maintenance window' do
+    let(:now) { Time.zone.parse('2020-01-01T12:00:00Z') }
+
+    it 'renders a warning and not the contents of the block' do
+      render_partial
+
+      expect(rendered).to have_content('We are currently under maintence')
+
+      formatted_finish = l(
+        finish.in_time_zone('America/New_York'),
+        format: t('time.formats.event_timestamp_with_zone'),
+      )
+      expect(rendered).to have_content(formatted_finish)
+
+      expect(rendered).to_not have_content('contents of block')
+    end
+  end
+
+  context 'outside the maintenance window' do
+    let(:now) { Time.zone.parse('2020-01-03T00:00:00Z') }
+
+    it 'renders the contents of the block but no warning' do
+      render_partial
+
+      expect(rendered).to have_content('contents of block')
+
+      expect(rendered).to_not have_content('We are currently under maintence')
+    end
+  end
+end
