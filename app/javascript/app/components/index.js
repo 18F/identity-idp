2fee5a7d1101f7f19247ee4f6d34be19abf044f8
Andrew Duthie <andrew.duthie@gsa.gov>
2020-09-16 16:29:52 -0400

    Simplify components index file
    **Why**:
    
    - Exports unused
    - TrapModal variable as pass-through is unnecessary, since direct access to Modal import is available

--- app/javascript/app/components/index.js
+++ app/javascript/app/components/index.js
@@ -1,9 +1,4 @@
 import Modal from './modal';
 
 window.LoginGov = window.LoginGov || {};
-const { LoginGov } = window;
-const TrapModal = Modal;
-
-LoginGov.Modal = TrapModal;
-
-export { TrapModal as Modal };
+window.LoginGov.Modal = Modal;
