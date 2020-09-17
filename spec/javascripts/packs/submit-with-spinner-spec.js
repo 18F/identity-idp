ad1c088c181c90756fc91afc26ccea0be626fdef
Andrew Duthie <andrew.duthie@gsa.gov>
2020-09-17 17:06:00 -0400

    Extract submit-with-spinner pack from form-validation.js

---
+++ spec/javascripts/packs/submit-with-spinner-spec.js
@@ -0,0 +1,42 @@
+import { screen } from '@testing-library/dom';
+import { useCleanDOM } from '../support/dom';
+
+describe('submit-with-spinner', () => {
+  useCleanDOM();
+
+  async function initialize({ withForm = true } = {}) {
+    const parent = withForm
+      ? document.body.appendChild(document.createElement('form'))
+      : document.body;
+
+    parent.innerHTML = `
+      <button type='submit' class='btn btn-primary btn-wide sm-col sm-col-6'>
+        Continue
+      </button>
+      <div class='pl1 sm-col sm-col-3'>
+      <div class='spinner hidden' id='submit-spinner'>
+        <img height="50" width="50" alt="Loading spinner" src="/assets/wait.gif">
+      </div>
+      <div class='clearfix'></div>
+    `;
+
+    delete require.cache[require.resolve('../../../app/javascript/packs/submit-with-spinner')];
+    await import('../../../app/javascript/packs/submit-with-spinner');
+  }
+
+  it('gracefully handles absence of form', async () => {
+    await initialize({ withForm: false });
+  });
+
+  it('should show spinner on form submit', async () => {
+    await initialize();
+
+    const form = document.querySelector('form');
+    const event = new window.Event('submit', { target: form });
+    form.dispatchEvent(event);
+
+    const spinner = screen.getByAltText('Loading spinner');
+
+    expect(spinner.parentNode.classList.contains('hidden')).to.be.false();
+  });
+});
