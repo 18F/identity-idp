0fb6604a42557c33f4099b5de5b8c22dc4770938
Andrew Duthie <andrew.duthie@gsa.gov>
2020-09-18 16:52:14 -0400

    Print javascript_pack_tag using variadic arguments

--- app/helpers/script_helper.rb
+++ app/helpers/script_helper.rb
@@ -9,11 +9,8 @@ module ScriptHelper
     @scripts.add(name)
   end
 
-  # rubocop:disable Rails/OutputSafety
   def print_javascript_pack_once_tags
-    return unless @scripts
-    @scripts.map { |name| javascript_pack_tag(name) }.join('').html_safe
+    javascript_pack_tag(*@scripts) if @scripts
   end
-  # rubocop:enable Rails/OutputSafety
 end
 # rubocop:enable Rails/HelperInstanceVariable
