9e12beece261961db505d9e96929f4da8990faf4
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-09-22 11:34:48 -0400

    Some tests

--- spec/services/capture_doc/create_request_spec.rb
+++ spec/services/capture_doc/create_request_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe CaptureDoc::CreateRequest do
   let(:subject) { described_class }
   let(:user_id) { 1 }
+  let(:sp_session) do
+    {
+      ial2_strict: true,
+      issuer: 'fake-sp-issuer',
+    }
+  end
   let(:old_timestamp) { Time.zone.now - 1.year }
 
   it 'creates a new request if one does not exist' do
@@ -14,6 +20,8 @@ describe CaptureDoc::CreateRequest do
     expect(doc_capture.request_token).to be_present
     expect(doc_capture.requested_at).to be_present
     expect(doc_capture.acuant_token).to be_nil
+    expect(doc_capture.ial2_strict).to eq(true)
+    expect(doc_capture.issuer).to eq 'fake-sp-issuer'
   end
 
   it 'update a request if one already exists' do
@@ -33,5 +41,7 @@ describe CaptureDoc::CreateRequest do
     expect(doc_capture.request_token).to be_present
     expect(doc_capture.requested_at).to_not eq(old_timestamp)
     expect(doc_capture.acuant_token).to be_nil
+    expect(doc_capture.ial2_strict).to eq(true)
+    expect(doc_capture.issuer).to eq 'fake-sp-issuer'
   end
 end
