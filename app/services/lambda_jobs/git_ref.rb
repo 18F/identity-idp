13017784de04b7d102ea4f2391974ba1202d2bbf
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-09-30 14:36:03 -0600

    Structure for calling lambdas (LG-3578) (#4251)

---
+++ app/services/lambda_jobs/git_ref.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LambdaJobs
+  GIT_REF = '97a07927e96d17e5ec204e5476de25ca52e5bf01'
+end
---
+++ app/services/lambda_jobs/runner.rb
@@ -0,0 +1,36 @@
+module LambdaJobs
+  class Runner
+    attr_reader :job_name, :job_class, :args
+
+    def initialize(job_name:, job_class:, args:)
+      @job_name = job_name
+      @job_class = job_class
+      @args = args
+    end
+
+    def run
+      if LoginGov::Hostdata.in_datacenter?
+        aws_lambda_client.invoke(
+          function_name: function_name,
+          invocation_type: 'Event',
+          log_type: 'None',
+          payload: args.to_json,
+        )
+      else
+        job_class.handle(
+          event: { body: args.to_json },
+          context: nil,
+        )
+      end
+    end
+
+    def aws_lambda_client
+      Aws::Lambda::Client.new(region: Figaro.env.aws_region)
+    end
+
+    # Due to length limits, we can only use the first 10 characters of a git SHA
+    def function_name
+      "#{job_name}:#{LambdaJobs::GIT_REF[0...10]}"
+    end
+  end
+end
