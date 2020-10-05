8b672259ba7dbe05f5dd95ad9725b4179c0476cd
Doug Price <dprice@fearless.tech>
2020-10-05 15:25:48 -0400

    don't use instance variables for local variables

--- app/services/flow/flow_state_machine.rb
+++ app/services/flow/flow_state_machine.rb
@@ -95,9 +95,9 @@ module Flow
       @params = params
       @request = request
       return if call_optional_show_step(step)
-      @step_params = flow.pre_render(step)
-      @local_params = @step_params.merge(flow_session: flow_session)
-      render template: "#{@view || @name}/#{step}", locals: @local_params
+      step_params = flow.pre_render(step)
+      local_params = step_params.merge(flow_session: flow_session)
+      render template: "#{@view || @name}/#{step}", locals: local_params
     end
 
     def call_optional_show_step(step)
