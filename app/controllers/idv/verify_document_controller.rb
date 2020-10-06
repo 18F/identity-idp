aa50d2190e57e6a7b9bbe3b7acc582d0d6a52886
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-10-06 10:10:58 -0500

    Unused

--- app/controllers/idv/verify_document_controller.rb
+++ app/controllers/idv/verify_document_controller.rb
@@ -36,7 +36,6 @@ module Idv
           client_response.to_h,
         )
 
-        store_pii(client_response) if client_response.success?
         status = :bad_request unless client_response.success?
       else
         status = verify_document_form.status
@@ -62,65 +61,8 @@ module Idv
         new(params, liveness_checking_enabled: liveness_checking_enabled?)
     end
 
-    def store_pii(doc_response)
-      verify_document_form.document_capture_session.store_result_from_response(doc_response)
-    end
-
     def doc_auth_client
       @doc_auth_client ||= DocAuth::Client.client
-    end
-
-    def call
-      if request_should_use_stored_result?
-        handle_stored_result
-      else
-        post_images_and_handle_result
-      end
-    end
-
-    def post_images_and_handle_result
-      response = post_images
-      if response.success?
-        save_proofing_components
-        document_capture_session.store_result_from_response(response)
-        extract_pii_from_doc(response)
-        response
-      else
-        handle_document_verification_failure(response)
-      end
-    end
-
-    def handle_document_verification_failure(response)
-      mark_step_incomplete(:document_capture)
-      notice = if liveness_checking_enabled?
-                 { notice: I18n.t('errors.doc_auth.document_capture_info_with_selfie_html') }
-               else
-                 { notice: I18n.t('errors.doc_auth.document_capture_info_html') }
-               end
-      log_document_error(response)
-      extra = response.to_h.merge(notice)
-      failure(response.first_error_message, extra)
-    end
-
-    def handle_stored_result
-      if stored_result.success?
-        extract_pii_from_doc(stored_result)
-      else
-        extra = { stored_result_present: stored_result.present? }
-        failure(I18n.t('errors.doc_auth.acuant_network_error'), extra)
-      end
-    end
-
-    def stored_result
-      @stored_document_capture_session_result ||= document_capture_session&.load_result
-    end
-
-    def request_should_use_stored_result?
-      return false if stored_result.blank?
-      IMAGE_UPLOAD_PARAM_NAMES.each do |param_name|
-        return false if flow_params[param_name].present?
-      end
-      true
     end
 
     def ec2_data
