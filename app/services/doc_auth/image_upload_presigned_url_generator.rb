e89eede22a36b7e208135cdfec37328e476a42f6
Doug Price <douglas.price@gsa.gov>
2020-10-06 11:08:48 -0400

    only generate presigned s3 urls if feature flagged in (#4272)

--- app/services/doc_auth/image_upload_presigned_url_generator.rb
+++ app/services/doc_auth/image_upload_presigned_url_generator.rb
@@ -3,6 +3,8 @@ module DocAuth
     include AwsS3Helper
 
     def presigned_image_upload_url(image_type:, transaction_id:)
+      return nil unless Figaro.env.doc_auth_enable_presigned_s3_urls == 'true'
+
       s3_presigned_url(
         bucket_prefix: bucket_prefix,
         keyname: "#{transaction_id}-#{image_type}",
