9c2dd3312de6b2900908a259b0047f9387bcc0ea
Mitchell Henke <mitchell.henke@gsa.gov>
2020-10-08 12:54:27 -0500

    track exception on lambda callback

--- app/controllers/lambda_callback/resolution_proof_result_controller.rb
+++ app/controllers/lambda_callback/resolution_proof_result_controller.rb
@@ -4,6 +4,8 @@ module LambdaCallback
       dcs = DocumentCaptureSession.new
       dcs.result_id = result_id_parameter
       dcs.store_proofing_result(resolution_result_parameter)
+
+      track_exception_in_result(resolution_result_parameter)
     end
 
     private
@@ -14,6 +16,14 @@ module LambdaCallback
 
     def resolution_result_parameter
       params.require(:resolution_result)
+    end
+
+    def track_exception_in_result(result)
+      exception = resolution_result_parameter[:exception]
+      return if exception.nil?
+
+      NewRelic::Agent.notice_error(exception)
+      ExceptionNotifier.notify_exception(exception)
     end
 
     def config_auth_token
