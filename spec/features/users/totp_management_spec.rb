326a9fe06f6f570707a9dd22f8636ee5ecd7209f
Zach Margolis <zachary.margolis@gsa.gov>
2020-10-08 12:15:00 -0700

    rubocop

--- spec/features/users/totp_management_spec.rb
+++ spec/features/users/totp_management_spec.rb
@@ -93,7 +93,8 @@ describe 'totp management' do
 
         expect(page).to have_current_path(account_two_factor_authentication_path)
         expect(user.auth_app_configurations.count).to eq(2)
-        expect(page).to_not have_link("+ #{t('forms.buttons.enable')}", href: authenticator_setup_url)
+        expect(page).
+          to_not have_link("+ #{t('forms.buttons.enable')}", href: authenticator_setup_url)
       end
     end
   end
