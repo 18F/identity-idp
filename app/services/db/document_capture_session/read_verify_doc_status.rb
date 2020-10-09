4043a4e1af4759cccc9db159a3fcd3db2de788fe
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-10-09 13:25:06 -0500

    in_progress symbol to string

--- app/services/db/document_capture_session/read_verify_doc_status.rb
+++ app/services/db/document_capture_session/read_verify_doc_status.rb
@@ -1,10 +1,12 @@
 module Db
   module DocumentCaptureSession
     class ReadVerifyDocStatus
+      IN_PROGRESS = 'in_progress'.freeze
+
       def self.call(uuid)
         doc_capture_session = ::DocumentCaptureSession.find_by(uuid: uuid)
         return unless doc_capture_session && doc_capture_session.verify_doc_submitted_at.present?
-        doc_capture_session.verify_doc_status || :in_progress
+        doc_capture_session.verify_doc_status || IN_PROGRESS
       end
     end
   end
