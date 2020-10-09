dab5f5573fc763c4072b8db397054f5251c13ba3
Andrew Duthie <andrew.duthie@gsa.gov>
2020-10-09 11:32:10 -0400

    Remove unnecessary eslint-disable
    Configured in eslintrc

--- app/javascript/packages/es5-safe/cli.js
+++ app/javascript/packages/es5-safe/cli.js
@@ -5,7 +5,6 @@ const patterns = process.argv.slice(2);
 if (patterns.length) {
   isAllSafe(patterns).then((isSafe) => process.exit(isSafe ? 0 : 1));
 } else {
-  // eslint-disable-next-line no-console
   console.log('Usage: is-es5-safe <pattern>');
   process.exit(1);
 }
