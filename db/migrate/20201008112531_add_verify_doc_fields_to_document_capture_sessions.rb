2677392d0a0244782642f704b22cecdc31ef620b
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-10-13 16:06:53 -0500

    Remove storing in db

--- db/migrate/20201008112531_add_verify_doc_fields_to_document_capture_sessions.rb
+++
@@ -1,8 +0,0 @@
-class AddVerifyDocFieldsToDocumentCaptureSessions < ActiveRecord::Migration[5.2]
-  def change
-    add_column :document_capture_sessions, :verify_doc_submitted_at, :timestamp
-    add_column :document_capture_sessions, :verify_doc_results_at, :timestamp
-    add_column :document_capture_sessions, :verify_doc_results, :jsonb
-    add_column :document_capture_sessions, :verify_doc_status, :string
-  end
-end
