9b6daefd320652833460ea42894aa221e5261b73
Zach Margolis <zachary.margolis@gsa.gov>
2020-10-14 15:21:19 -0700

    Add aria-invalid attribute to all required fields (LG-3531)

---
+++ spec/support/matchers/accessibility.rb
@@ -0,0 +1,25 @@
+RSpec::Matchers.define :label_required_fields do
+  elements = []
+
+  match do |page|
+    page.all(:css, 'input[required]').each do |input|
+      elements << input unless input['aria-invalid'].present?
+    end
+    elements.empty?
+  end
+
+  failure_message do |page|
+    elem_summaries = elements.map do |elem|
+      [
+        elem.tag_name,
+        elem[:name] ? " name=#{elem[:name]} " : nil,
+        elem[:class].split(' ').map { |c| ".#{c}" },
+      ].join('')
+    end.join(', ')
+
+    <<-STR.squish
+      On #{page.current_path} found #{elements.count} elements with
+      required=true but missing aria-invalid (#{elem_summaries})
+    STR
+  end
+end
