1e17c4cbf46e5b982318426460b18722ea1e27f7
Andrew Duthie <andrew.duthie@gsa.gov>
2020-10-14 10:07:39 -0400

    Use default USWDS pseudo-class for banner collapse button (#4277)
    **Why**: Consistency with USWDS, eliminate redundant (duplicate) button controls, reduce localization string maintenance overhead, eliminate jQuery dependency from application JavaScript pack

--- app/javascript/app/header.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-document.addEventListener('DOMContentLoaded', function () {
-  $('.close-banner-how-you-know').click(function () {
-    $('.usa-proof').click();
-  });
-});
