4ec30966d834fc018b7df93600d7a63dea1e5412
Mitchell Henke <mitchell.henke@gsa.gov>
2020-10-19 12:38:39 -0500

    Update styles to not conflict
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,19 +1,12 @@
 module LinkHelper
-  EXTERNAL_LINK_CLASSES = ['usa-link', 'usa-link--external'].freeze
-  ALT_EXTERNAL_LINK_CLASSES = ['usa-link--alt', 'usa-link--external'].freeze
+  EXTERNAL_LINK_CLASS = 'usa-link--external'.freeze
 
   def new_window_link_to(name = nil, url = nil, html_options = nil)
     html_options ||= {}
     html_options[:class] ||= html_options[:class].to_s
     html_options[:target] = '_blank'
 
-    classes = html_options[:class].split(' ')
-
-    if classes.include?('usa-link--alt')
-      classes.concat(ALT_EXTERNAL_LINK_CLASSES)
-    else
-      classes.concat(EXTERNAL_LINK_CLASSES)
-    end
+    classes = html_options[:class].split(' ').append(EXTERNAL_LINK_CLASS)
 
     html_options[:class] = classes.uniq.join(' ')
 
