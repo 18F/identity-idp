9812580ed0d66f98893c10e19544dbd4ab13a03c
Mitchell Henke <mitchell.henke@gsa.gov>
2020-10-19 15:48:08 -0500

    support block form in new_window_link_to

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,18 +1,24 @@
 module LinkHelper
   EXTERNAL_LINK_CLASS = 'usa-link--external'.freeze
 
-  def new_window_link_to(name = nil, url = nil, html_options = nil)
+  def new_window_link_to(name = nil, url = nil, html_options = nil, &block)
     html_options ||= {}
     html_options[:class] ||= html_options[:class].to_s
     html_options[:target] = '_blank'
 
     classes = html_options[:class].split(' ').append(EXTERNAL_LINK_CLASS)
 
+
     html_options[:class] = classes.uniq.join(' ')
 
     link_to(url, html_options) do
-      content_tag('span', name) +
-        content_tag('span', t('links.new_window'), class: 'usa-sr-only')
+      if block_given?
+        yield(block)
+        concat content_tag('span', t('links.new_window'), class: 'usa-sr-only')
+      else
+        content_tag('span', name) +
+          content_tag('span', t('links.new_window'), class: 'usa-sr-only')
+      end
     end
   end
 end
