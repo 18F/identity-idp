db9cb56804ba39b9b386549e6fdf1f4e1b05e6ab
Mitchell Henke <mitchell.henke@gsa.gov>
2020-10-20 10:25:18 -0500

    update helper to behave more like link_to

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,20 +1,32 @@
 module LinkHelper
   EXTERNAL_LINK_CLASS = 'usa-link--external'.freeze
 
-  def new_window_link_to(name = nil, url = nil, html_options = nil, &block)
-    html_options ||= {}
-    html_options[:class] ||= html_options[:class].to_s
-    html_options[:target] = '_blank'
+  def new_window_link_to(name = nil, options = nil, html_options = nil, &block)
+    if block_given?
+      html_options = options
+      options = name
+      name = block
+    end
+    options ||= {}
 
-    classes = html_options[:class].split(' ').append(EXTERNAL_LINK_CLASS)
+    html_options = convert_options_to_data_attributes(options, html_options)
 
-    html_options[:class] = classes.uniq.join(' ')
+    url = url_for(options)
+    html_options['href'] ||= url
+    html_options['class'] ||= html_options['class'].to_s
+    html_options['target'] = '_blank'
 
-    link_to(url, html_options) do
-      if block_given?
+    classes = html_options['class'].split(' ').append(EXTERNAL_LINK_CLASS)
+
+    html_options['class'] = classes.uniq.join(' ')
+
+    if block_given?
+      link_to(url, html_options) do
         yield(block)
         concat content_tag('span', t('links.new_window'), class: 'usa-sr-only')
-      else
+      end
+    else
+      link_to(url, html_options) do
         content_tag('span', name) +
           content_tag('span', t('links.new_window'), class: 'usa-sr-only')
       end
