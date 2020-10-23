216bd0adc7fee43b06ec5064430d3683215177d3
Mitchell Henke <mitchell.henke@gsa.gov>
2020-10-23 14:54:58 -0500

    SSN field show/hide toggle

---
+++ app/javascript/app/ssn-field.js
@@ -0,0 +1,62 @@
+import Cleave from 'cleave.js';
+const { I18n } = window.LoginGov;
+
+/* eslint-disable no-new */
+function formatSSNField() {
+  const inputs = document.querySelectorAll('input.ssn-toggle[type="password"]');
+
+  if (inputs) {
+    [].slice.call(inputs).forEach((input, i) => {
+      const el = `
+        <div class="mt1 right">
+          <label class="btn-border" for="ssn-toggle-${i}">
+            <div class="checkbox">
+              <input id="ssn-toggle-${i}" type="checkbox">
+              <span class="indicator"></span>
+              ${I18n.t('forms.ssn.show')}
+            </div>
+          </label>
+        </div>`;
+      input.insertAdjacentHTML('afterend', el);
+
+      let ssnCleave = new Cleave(input, {
+        numericOnly: true,
+        blocks: [3, 2, 4],
+        delimiter: '',
+      });
+
+      const toggle = document.getElementById(`ssn-toggle-${i}`);
+      toggle.addEventListener('change', function () {
+
+        input.type = toggle.checked ? 'text' : 'password';
+
+        if(!toggle.checked) {
+          if (ssnCleave) {
+            ssnCleave.destroy();
+
+            ssnCleave = new Cleave(input, {
+              numericOnly: true,
+              blocks: [3, 2, 4],
+              delimiter: '',
+            });
+          }
+
+          input.value = input.value.replace(/-/g, '');
+
+          input.maxLength = 9;
+        } else {
+          input.maxLength = 11;
+
+          ssnCleave.destroy();
+          ssnCleave = new Cleave(input, {
+            numericOnly: true,
+            blocks: [3, 2, 4],
+            delimiter: '-',
+          });
+        }
+      });
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', formatSSNField);
