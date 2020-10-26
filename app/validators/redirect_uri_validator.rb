60304853bba6c7b3c735037c2f8d877e0fc81ed3
Zach Margolis <zachary.margolis@gsa.gov>
2020-10-26 10:53:57 -0700

    Revert "wip"
    This reverts commit 2fc753ab789dbbc8c30190e83cd4ccc30107f5ae.

--- app/validators/redirect_uri_validator.rb
+++ app/validators/redirect_uri_validator.rb
@@ -19,8 +19,7 @@ module RedirectUriValidator
     service_provider.redirect_uris.any? do |sp_redirect_uri|
       parsed_sp_redirect_uri = URI.parse(sp_redirect_uri)
 
-      parsed_sp_redirect_uri == parsed_redirect_uri ||
-        matches_localhost_in_test?(parsed_sp_redirect_uri)
+      parsed_sp_redirect_uri == parsed_redirect_uri
     end
   rescue ArgumentError, URI::InvalidURIError
     errors.add(:redirect_uri, t('openid_connect.authorization.errors.redirect_uri_invalid'))
@@ -29,11 +28,5 @@ module RedirectUriValidator
   def parsed_redirect_uri
     requested_uri = post_logout_redirect_uri || redirect_uri
     URI.parse(requested_uri)
-  end
-
-  def matches_localhost_in_test?(parsed_sp_redirect_uri)
-    return false unless Rails.env.test?
-    parsed_redirect_uri.host == '127.0.0.1' &&
-      parsed_redirect_uri.path == parsed_sp_redirect_uri.path
   end
 end
