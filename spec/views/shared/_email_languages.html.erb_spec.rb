681d1bdc233fd75d785c17cf8a160c3c4ed5c7e9
Zach Margolis <zachary.margolis@gsa.gov>
2020-10-30 16:06:17 -0700

    Fix radio button accessibility

--- spec/views/shared/_email_languages.html.erb_spec.rb
+++ spec/views/shared/_email_languages.html.erb_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe 'shared/_email_languages.html.erb' do
 
   subject(:render_partial) do
     simple_form_for(:user, url: '/') do |f|
-      render partial: 'shared/email_languages', locals: { f: f, selection: selection }
+      render partial: 'shared/email_languages',
+             locals: { f: f, selection: selection, labelledby: labelledby }
     end
   end
 
   let(:selection) { 'fr' }
+  let(:labelledby) { 'outer-label-id' }
 
   it 'renders a checkbox per available locale' do
     render_partial
@@ -18,6 +20,32 @@ RSpec.describe 'shared/_email_languages.html.erb' do
     radios = doc.css('input[type=radio]')
 
     expect(radios.map { |r| r['value'] }).to match_array(I18n.available_locales.map(&:to_s))
+  end
+
+  context 'accessibility' do
+    it 'marks all radio boxes as labelled by the outer label' do
+      render_partial
+
+      doc = Nokogiri::HTML(rendered)
+      radios = doc.css('input[type=radio]')
+
+      radios.each do |radio|
+        expect(radio['aria-labelledby']).to include(labelledby)
+      end
+    end
+
+    it 'marks each radio box as labelled by its unique label' do
+      render_partial
+
+      doc = Nokogiri::HTML(rendered)
+      radios = doc.css('input[type=radio]')
+
+      radios.each do |radio|
+        aria = radio['aria-labelledby']
+        unique_label = (aria.split(' ') - [labelledby]).first
+        expect(doc.css("label##{unique_label}").size).to eq(1)
+      end
+    end
   end
 
   context 'with a nil selection' do
