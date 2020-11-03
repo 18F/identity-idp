14d4f84d2a3e5011fc934ad95d46695117f9340a
Andrew Duthie <andrew.duthie@gsa.gov>
2020-11-03 17:09:55 -0500

    Use i18n-tasks to audit used JS locale strings
    **Why**:
    
    As a developer, I want to be warned when the locale strings in use do not match those provided by the server, so I can have confidence that the texts will be presented correctly to the user.
    
    As a developer, I want to have a well-known inventory of strings in use, so that I can have confidence in future refactoring if and how strings are being used.
    
    As a developer, I want to leverage existing tooling, so that I can feel confidence in knowing that string usage is treated consistently, and so that I don't need to deal with the overhead of maintaining a custom translation string checking utility.
    
    As a user, I want only the necessary translations to be included in the JavaScript assets, so that my pages load faster.

--- spec/lib/asset_checker_spec.rb
+++ spec/lib/asset_checker_spec.rb
@@ -1,8 +1,9 @@
 require 'asset_checker'
+require 'tempfile'
 
 RSpec.describe AssetChecker do
   subject(:asset_checker) do
-    AssetChecker.new(files, assets_file: assets_file, translations_file: translations_file)
+    AssetChecker.new(files, assets_file: assets_file)
   end
 
   describe '::ASSETS_FILE' do
@@ -11,15 +12,8 @@ RSpec.describe AssetChecker do
     end
   end
 
-  describe '::TRANSLATIONS_FILE' do
-    it 'exists' do
-      expect(File.exist?(AssetChecker::TRANSLATIONS_FILE)).to eq(true)
-    end
-  end
-
   describe '#check_files' do
     let(:assets_file) { Tempfile.new }
-    let(:translations_file) { Tempfile.new }
 
     before do
       File.open(assets_file.path, 'w') do |f|
@@ -29,18 +23,12 @@ RSpec.describe AssetChecker do
           ] %>
         STR
       end
-
-      File.open(translations_file.path, 'w') do |f|
-        f.puts YAML.dump(translation_strings)
-      end
     end
 
     after do
       assets_file.unlink
-      translations_file.unlink
     end
 
-    let(:translation_strings) { %w[first_translation second_translation] }
     let(:asset_strings) { %w[first_asset.png second_asset.gif] }
 
     context 'with matching assets' do
@@ -49,7 +37,7 @@ RSpec.describe AssetChecker do
 
       before do
         File.open(tempfile.path, 'w') do |f|
-          f.puts(build_js_with_strings('first_asset.png', 'first_translation'))
+          f.puts(build_js_with_strings('first_asset.png'))
         end
       end
 
@@ -66,7 +54,7 @@ RSpec.describe AssetChecker do
 
       before do
         File.open(tempfile.path, 'w') do |f|
-          f.puts(build_js_with_strings('wont_find.svg', 'not-found'))
+          f.puts(build_js_with_strings('wont_find.svg'))
         end
       end
 
@@ -74,41 +62,27 @@ RSpec.describe AssetChecker do
 
       it 'identifies issues' do
         expect(asset_checker).to receive(:warn).with(tempfile.path)
-        expect(asset_checker).to receive(:warn).with('Missing translation, not-found')
-        expect(asset_checker).to receive(:warn).twice.with('Missing asset, wont_find.svg')
+        expect(asset_checker).to receive(:warn).once.with('Missing asset, wont_find.svg')
         expect(asset_checker.check_files).to eq(true)
       end
     end
   end
 
-  def build_js_with_strings(asset, translation)
+  def build_js_with_strings(asset)
     <<-STR
       import React from 'react';
-      import AcuantCapture from './acuant-capture';
-      import DocumentTips from './document-tips';
-      import useI18n from '../hooks/use-i18n';
       import useAsset from '../hooks/use-asset';
 
       function DocumentCapture() {
-        const { t } = useI18n();
         const { getAssetPath } = useAsset();
 
-        const sample = (
+        return (
           <img
             src={getAssetPath('#{asset}')}
             alt="Sample front of state issued ID"
             width={450}
             height={338}
           />
-        );
-
-        return (
-          <>
-            <h2>{t('#{translation}')}</h2>
-            <DocumentTips sample={sample} />
-            <AcuantCapture />
-            <img src={getAssetPath('#{asset}')} alt="" />
-          </>
         );
       }
 
