ac5c7f4b9d84651f1d0d26301f02bb233412f047
Mitchell Henke <mitchell.henke@gsa.gov>
2020-11-16 14:35:54 -0600

    add specs

---
+++ spec/lib/figaro_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe Figaro::Environment do
+  let(:config) do
+    {
+      'test_key' => 'value',
+      'production' => {
+        'test_key' => 'overridden value',
+      },
+    }
+  end
+
+  let(:config_environment) { 'test' }
+
+  describe '#initialize' do
+    it 'warns and uses ENV when key is set in ENV and file config' do
+      allow(ENV).to receive(:[]).with('test_key').and_return('overridden value')
+
+      expect do
+        environment = described_class.new(config, config_environment)
+        expect(environment.test_key).to eq 'overridden value'
+      end.to output(
+        /test_key is being loaded from ENV/,
+      ).to_stderr
+    end
+
+    it 'overrides from specified environment key' do
+      environment = described_class.new(config, 'production')
+
+      expect(environment.test_key).to eq 'overridden value'
+    end
+  end
+
+  describe '#method_missing' do
+    it 'reads from configuration' do
+      environment = described_class.new(config, config_environment)
+
+      expect(environment.test_key).to eq config['test_key']
+    end
+  end
+
+  describe '#require_keys' do
+    it 'does not raise an error if required keys are set' do
+      environment = described_class.new(config, config_environment)
+
+      expect(environment.require_keys(['test_key'])).to eq true
+    end
+
+    it 'raises an error if required key is not set' do
+      environment = described_class.new(config, config_environment)
+
+      expect { environment.require_keys(['unset_key']) }.to raise_error(
+        RuntimeError,
+        'unset_key is missing',
+      )
+    end
+  end
+end
