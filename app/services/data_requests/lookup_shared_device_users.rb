75249841c69c1c4f079432a04e0b9788cd0efa11
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-11-17 14:58:28 -0500

    fix

--- app/services/data_requests/lookup_shared_device_users.rb
+++ app/services/data_requests/lookup_shared_device_users.rb
@@ -27,7 +27,7 @@ module DataRequests
       return if user_uuids_by_device.keys.include?(cookie_uuid)
 
       warn "Searching for new devices matching #{cookie_uuid}"
-      user_uuids = matching_device_records.map(&:user).compact.map(&:uuid)
+      user_uuids = Device.joins(:user).where(cookie_uuid: cookie_uuid).pluck(:uuid)
       user_uuids_by_device[cookie_uuid] = user_uuids
     end
 
