6c3157d3bc238070aea7fc2c1f9e0db6225c3480
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-11-16 20:07:14 -0600

    LG-3163 Email deleted user accounts reports (#4322)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -204,6 +204,14 @@ class UserMailer < ActionMailer::Base
     mail(to: email, subject: t('user_mailer.sps_over_quota_limit.subject'))
   end
 
+  def deleted_user_accounts_report(email:, name:, issuers:, data:)
+    @name = name
+    @issuers = issuers
+    @data = data
+    attachments['deleted_user_accounts.csv'] = data
+    mail(to: email, subject: t('user_mailer.deleted_accounts_report.subject'))
+  end
+
   private
 
   def email_should_receive_nonessential_notifications?(email)
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -501,6 +501,33 @@ describe UserMailer, type: :mailer do
     end
   end
 
+  describe 'deleted_user_accounts_report' do
+    let(:mail) do
+      UserMailer.deleted_user_accounts_report(
+        email: email_address.email,
+        name: 'my name',
+        issuers: %w[issuer1 issuer2],
+        data: 'data',
+      )
+    end
+
+    it_behaves_like 'a system email'
+
+    it 'sends to the current email' do
+      expect(mail.to).to eq [email_address.email]
+    end
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq t('user_mailer.deleted_accounts_report.subject')
+    end
+
+    it 'renders the body' do
+      expect(mail.html_part.body).to have_content('my name')
+      expect(mail.html_part.body).to have_content('issuer1')
+      expect(mail.html_part.body).to have_content('issuer2')
+    end
+  end
+
   def strip_tags(str)
     ActionController::Base.helpers.strip_tags(str)
   end
