c6208d8ce6c0418b4d21b1f3fd600e693349f604
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-11-17 23:16:48 -0600

    Nix front image

--- app/services/db/doc_auth_log/drop_off_rates_helper.rb
+++ app/services/db/doc_auth_log/drop_off_rates_helper.rb
@@ -1,7 +1,7 @@
 module Db
   module DocAuthLog
     module DropOffRatesHelper
-      STEPS = %w[welcome upload_option front_image back_image ssn verify_info phone
+      STEPS = %w[welcome upload_option back_image ssn verify_info phone
                  encrypt personal_key verified].freeze
 
       private
@@ -27,7 +27,6 @@ module Db
       def select_counts_from_doc_auth_logs
         <<~SQL
           select count(welcome_view_at) as welcome, count(upload_view_at) as upload_option,
-          count(COALESCE(front_image_view_at,mobile_front_image_view_at)) as front_image,
           count(COALESCE(back_image_view_at,mobile_back_image_view_at,capture_mobile_back_image_view_at,present_cac_view_at)) as back_image,
           count(COALESCE(ssn_view_at,enter_info_view_at)) as ssn,
           count(verify_view_at) as verify_info,
