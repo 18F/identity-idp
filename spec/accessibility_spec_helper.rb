9c77af0629e3329acd885171125d3bef0a9ad18f
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-11-20 10:35:05 -0500

    WIP/POC: Run accessibility checks on every single page by monkey patching Capybara
    To run: `rspec --require accessibility_spec_helper.rb spec/features`
    
    This may be something to consider running within a nightly build.

---
+++ spec/accessibility_spec_helper.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+require 'axe/rspec'
+
+Capybara.default_driver = Capybara.javascript_driver
+
+module PageIsAccessible
+  include RSpec::Matchers
+
+  def visit(*args)
+    super(*args)
+
+    expect(page).to be_accessible.according_to :section508, :"best-practice"
+    expect(page).to label_required_fields
+
+    # TODO: Replace with a matcher
+    # Visiting a page with the default app name title is considered a failure, and should be resolved by
+    # providing a distinct description for the page using the `:title` content block.
+    raise "Visited page '#{current_path}' without distinct title" if title&.strip == APP_NAME
+  end
+end
+
+module Capybara
+  module DSL
+    prepend PageIsAccessible
+  end
+end
