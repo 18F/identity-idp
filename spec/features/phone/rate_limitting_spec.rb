bcc91647f6af5c0f2c17098ee253268bbaf7e764
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-11-21 09:40:56 -0500

    lint

--- spec/features/phone/rate_limitting_spec.rb
+++ spec/features/phone/rate_limitting_spec.rb
@@ -10,7 +10,7 @@ describe 'phone rate limitting' do
     # Restore this line when voice confirmation is re-enabled
     # it_behaves_like 'phone rate limitting', :voice
 
-    def visit_otp_confirmation(delivery_method)
+    def visit_otp_confirmation(_delivery_method)
       select_2fa_option(:phone)
       # Restore this line when voice confirmation is re-enabled
       # select_phone_delivery_option(delivery_method)
@@ -26,7 +26,7 @@ describe 'phone rate limitting' do
     # Restore this line when voice confirmation is re-enabled
     # it_behaves_like 'phone rate limitting', :voice
 
-    def visit_otp_confirmation(delivery_method)
+    def visit_otp_confirmation(_delivery_method)
       sign_in_live_with_2fa(user)
       click_on "+ #{t('account.index.phone_add')}"
       fill_in :new_phone_form_phone, with: phone
