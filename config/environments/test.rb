cdbef7c499ccc9705eea445dd11775a9581b793c
Mitchell Henke <mitchell.henke@gsa.gov>
2020-11-23 16:24:49 -0600

    update config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,11 +1,13 @@
 Rails.application.configure do
   config.active_job.queue_adapter = :test
-  config.cache_classes = true
+  config.cache_classes = false
+  config.action_view.cache_template_loading = true
   config.eager_load = false
   config.public_file_server.enabled = true
   config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
+  config.cache_store = :null_store
   config.action_dispatch.show_exceptions = false
   config.action_controller.allow_forgery_protection = false
   config.active_support.test_order = :random
