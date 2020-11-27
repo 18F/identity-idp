4d3bd52065d7cedacdb8cd2a6183b7f3eb57c964
Andrew Duthie <andrew.duthie@gsa.gov>
2020-11-27 11:54:49 -0500

    Avoid class_eval for SimpleForm monkey-patch
    **Why**: Autoload interop

--- lib/simple_form/error_notification.rb
+++ lib/simple_form/error_notification.rb
@@ -1,10 +1,11 @@
-SimpleForm::ErrorNotification.class_eval do
-  alias_method :old_render, :render
-  def render
-    return unless has_errors?
-    template.render 'shared/alert',
-                    type: 'error',
-                    message: error_message,
-                    class: 'margin-bottom-8'
+module SimpleForm
+  class ErrorNotification
+    def render
+      return unless has_errors?
+      template.render 'shared/alert',
+                      type: 'error',
+                      message: error_message,
+                      class: 'margin-bottom-8'
+    end
   end
 end
