36c6e40fb9501685abd46f97843510ee0927565b
Mitchell Henke <mitchell.henke@gsa.gov>
2020-11-30 13:25:15 -0600

    add 406 tests

--- spec/controllers/sign_up/registrations_controller_spec.rb
+++ spec/controllers/sign_up/registrations_controller_spec.rb
@@ -21,11 +21,11 @@ describe SignUp::RegistrationsController, devise: true do
       expect(response).to redirect_to account_path
     end
 
-    xit 'gracefully handles invalid formats' do
+    it 'gracefully handles invalid formats' do
       @request.env['HTTP_ACCEPT'] = "nessus=bad_bad_value'"
-      get :new
 
-      expect(response.status).to eq(200)
+      expect { get :new }.
+        to raise_error(Mime::Type::InvalidMimeType)
     end
   end
 
