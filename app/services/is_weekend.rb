13800b8f10d1b900b1d2f1a98acf3da75d5a4e33
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-12-04 12:26:25 -0600

    Unused

--- app/services/is_weekend.rb
+++
@@ -1,5 +0,0 @@
-class IsWeekend
-  def self.call(date)
-    date.saturday? || date.sunday?
-  end
-end
--- app/services/is_weekend_or_holiday.rb
+++
@@ -1,5 +0,0 @@
-class IsWeekendOrHoliday
-  def self.call(date)
-    IsWeekend.call(date) || CalendarService.observed_holiday?(date)
-  end
-end
