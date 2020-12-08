1a7aa1792fd3e80b5c6bcea35c46f67e361e9882
Andrew Duthie <andrew.duthie@gsa.gov>
2020-12-08 16:00:15 -0500

    Add view spec for password strength partial
    See: https://github.com/18F/identity-idp/pull/4497/files#r538583494

---
+++ spec/views/devise/shared/_password_strength.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'devise/shared/_password_strength.html.erb' do
+  let(:forbidden_passwords) { nil }
+
+  before(:each) do
+    assign(:forbidden_passwords, forbidden_passwords)
+  end
+
+  describe 'forbidden attributes' do
+    context 'when local is unassigned' do
+      let(:forbidden_passwords) { nil }
+
+      it 'omits data-forbidden attribute from strength text tag' do
+        render
+
+        expect(rendered).to have_selector('#pw-strength-txt:not([data-forbidden])')
+      end
+    end
+
+    context 'when local is assigned' do
+      let(:forbidden_passwords) { ['a', 'b', 'c'] }
+
+      it 'adds JSON-encoded data-forbidden to strength text tag' do
+        render
+
+        expect(rendered).to have_selector('#pw-strength-txt[data-forbidden="[\"a\",\"b\",\"c\"]"]')
+      end
+    end
+  end
+end
