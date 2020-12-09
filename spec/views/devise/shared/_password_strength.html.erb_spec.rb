dc240be5cbb61cbe04c7e15ff11844e363ee33a3
Andrew Duthie <andrew.duthie@gsa.gov>
2020-12-09 14:57:57 -0500

    Pass locals as argument in password_strength spec

--- spec/views/devise/shared/_password_strength.html.erb_spec.rb
+++ spec/views/devise/shared/_password_strength.html.erb_spec.rb
@@ -1,29 +1,33 @@
 require 'rails_helper'
 
 describe 'devise/shared/_password_strength.html.erb' do
-  let(:forbidden_passwords) { nil }
-
-  before(:each) do
-    allow(view).to receive(:forbidden_passwords).and_return(forbidden_passwords)
-  end
-
   describe 'forbidden attributes' do
     context 'when local is unassigned' do
-      let(:forbidden_passwords) { nil }
+      before do
+        render
+      end
 
       it 'omits data-forbidden attribute from strength text tag' do
-        render
+        expect(rendered).to have_selector('#pw-strength-txt:not([data-forbidden])')
+      end
+    end
 
+    context 'when local is nil' do
+      before do
+        render 'devise/shared/password_strength', forbidden_passwords: nil
+      end
+
+      it 'omits data-forbidden attribute from strength text tag' do
         expect(rendered).to have_selector('#pw-strength-txt:not([data-forbidden])')
       end
     end
 
     context 'when local is assigned' do
-      let(:forbidden_passwords) { ['a', 'b', 'c'] }
+      before do
+        render 'devise/shared/password_strength', forbidden_passwords: ['a', 'b', 'c']
+      end
 
       it 'adds JSON-encoded data-forbidden to strength text tag' do
-        render
-
         expect(rendered).to have_selector('#pw-strength-txt[data-forbidden="[\"a\",\"b\",\"c\"]"]')
       end
     end
