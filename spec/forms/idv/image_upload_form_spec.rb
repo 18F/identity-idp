efd6454580ab5271d3c1b7597dbc66fc7b9c21a0
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-12-17 12:43:22 -0500

    fix more tests

--- spec/forms/idv/image_upload_form_spec.rb
+++ spec/forms/idv/image_upload_form_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Idv::ImageUploadForm do
   let(:subject) { Idv::ImageUploadForm.new }
   let(:image_data) { 'abc' }
-  let(:image_data_url) { 'data:image/jpeg;base64,abc' }
 
   describe '#submit' do
     context 'when the form has an image' do
@@ -17,17 +16,6 @@ describe Idv::ImageUploadForm do
       end
     end
 
-    context 'when the form has an image_data_url' do
-      it 'returns a successful form response' do
-        result = subject.submit(image_data_url: image_data_url)
-
-        expect(result).to be_kind_of(FormResponse)
-        expect(result.success?).to eq(true)
-        expect(result.errors).to be_empty
-        expect(result.extra).to eq(is_fallback_link: false)
-      end
-    end
-
     context 'when the form has invalid attributes' do
       it 'raises an error' do
         expect { subject.submit(image: image_data, foo: 1) }.
@@ -37,7 +25,7 @@ describe Idv::ImageUploadForm do
   end
 
   describe 'presence validations' do
-    it 'is invalid when image and image_data_url attribute is not present' do
+    it 'is invalid when image  attribute is not present' do
       result = subject.submit({})
 
       expect(subject).to_not be_valid
