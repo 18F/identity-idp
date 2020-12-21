685cdcd7126044099c16dd68c298bfcc9720035f
Andrew Duthie <andrew.duthie@gsa.gov>
2020-12-21 16:09:45 -0500

    Fix fake timer test lifecycle leakage

--- spec/javascripts/support/sinon.js
+++ spec/javascripts/support/sinon.js
@@ -7,10 +7,23 @@ import sinon from 'sinon';
  * @param {sinon.SinonSandboxConfig=} config
  */
 export function useSandbox(config) {
-  const sandbox = sinon.createSandbox(config);
+  const { useFakeTimers = false, ...remainingConfig } = config ?? {};
+  const sandbox = sinon.createSandbox(remainingConfig);
+
+  beforeEach(() => {
+    // useFakeTimers overrides global timer functions as soon as sandbox is created, thus leaking
+    // across tests. Instead, wait until tests start to initialize.
+    if (useFakeTimers) {
+      sandbox.useFakeTimers();
+    }
+  });
 
   afterEach(() => {
     sandbox.restore();
+
+    if (useFakeTimers) {
+      sandbox.clock.restore();
+    }
   });
 
   return sandbox;
