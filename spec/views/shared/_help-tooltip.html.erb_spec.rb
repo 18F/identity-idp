b17c57910a09756739c0f4cc511d128f3805d04e
Andrew Duthie <andrew.duthie@gsa.gov>
2020-12-23 15:57:06 -0500

    Enhance test case to validate escaping
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>

--- spec/views/shared/_help-tooltip.html.erb_spec.rb
+++ spec/views/shared/_help-tooltip.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'shared/_help-tooltip.html.erb' do
   it 'renders with block as USWDS tooltip' do
-    tooltip_text = 'foobar'
+    tooltip_text = 'foo "bar"'
     render('shared/help-tooltip') { tooltip_text }
 
     expect(rendered).to have_css(".help-tooltip__button[title='#{tooltip_text}']")
