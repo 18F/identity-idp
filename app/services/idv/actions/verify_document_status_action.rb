7e92e82242889a7f09af0b572e0dd3f2284b5bf2
Zach Margolis <zachary.margolis@gsa.gov>
2020-12-28 11:45:50 -0800

    rubocop: missing comma

--- app/services/idv/actions/verify_document_status_action.rb
+++ app/services/idv/actions/verify_document_status_action.rb
@@ -61,7 +61,7 @@ module Idv
       def verify_document_capture_session
         return @verify_document_capture_session if defined?(@verify_document_capture_session)
         @verify_document_capture_session = DocumentCaptureSession.find_by(
-          uuid: flow_session[verify_document_capture_session_uuid_key]
+          uuid: flow_session[verify_document_capture_session_uuid_key],
         )
       end
 
