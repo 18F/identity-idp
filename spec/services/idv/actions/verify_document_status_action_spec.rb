300778d181d91841ff3594db81ff318b1e594e24
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-12-28 12:39:39 -0800

    Rename document_capture_session in verify actions (#4539)
    **Why**: Disambiguate from DocAuthBaseStep#document_capture_session

--- spec/services/idv/actions/verify_document_status_action_spec.rb
+++ spec/services/idv/actions/verify_document_status_action_spec.rb
@@ -18,24 +18,27 @@ describe Idv::Actions::VerifyDocumentStatusAction do
 
       expect(fake_analytics).to have_logged_event(
         Analytics::DOC_AUTH_ASYNC,
-        error: 'failed to load document_capture_session',
+        error: 'failed to load verify_document_capture_session',
         uuid: nil,
       )
     end
 
     it 'calls analytics if timed out from no result in document capture session' do
-      document_capture_session = DocumentCaptureSession.new(uuid: 'uuid', result_id: 'result_id')
+      verify_document_capture_session = DocumentCaptureSession.new(
+        uuid: 'uuid',
+        result_id: 'result_id',
+      )
 
-      expect(subject).to receive(:document_capture_session).and_return(document_capture_session).
-        at_least(:once)
+      expect(subject).to receive(:verify_document_capture_session).
+        and_return(verify_document_capture_session).at_least(:once)
       expect(controller).to receive(:analytics).and_return(fake_analytics).twice
       response = subject.call
 
       expect(fake_analytics).to have_logged_event(
         Analytics::DOC_AUTH_ASYNC,
         error: 'failed to load async result',
-        uuid: document_capture_session.uuid,
-        result_id: document_capture_session.result_id,
+        uuid: verify_document_capture_session.uuid,
+        result_id: verify_document_capture_session.result_id,
       )
     end
   end
