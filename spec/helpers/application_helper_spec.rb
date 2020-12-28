a04687a65d1a743b5437a428cd8dea24a8aa2b6c
Andrew Duthie <andrew.duthie@gsa.gov>
2020-12-28 11:19:10 -0500

    Remove hint.css TOTP informational tooltip
    **Why**:
    
    - It is currently broken, displaying as non-visible except by cursor hover or keyboard focus.
    - It's the only instance of a tooltip in the entire application.
    - Tooltips are not ideal for longer-form content like the informational text we show in this tooltip
      - See: https://designsystem.digital.gov/components/tooltip/
    - We are not space-constrained on this page and could show informational text as part of the primary content
    - Fewer dependencies for us to manage
      - If we were to want tooltips, we should ideally be using USWDS tooltips (see #4529) to consolidate dependencies
    - Less styles for the user to download (faster load time)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,18 +9,6 @@ describe ApplicationHelper do
     end
   end
 
-  describe '#tooltip' do
-    it 'creates a span containing aria label with text and image' do
-      tooltip_text = 'foobar'
-
-      html = helper.tooltip(tooltip_text)
-
-      expect(html).to have_css('.hint--top')
-      expect(html).to have_selector('img')
-      expect(html).to have_xpath("//span[@aria-label='#{tooltip_text}']")
-    end
-  end
-
   describe '#session_with_trust?' do
     context 'no user present and page is not one with trust' do
       before do
