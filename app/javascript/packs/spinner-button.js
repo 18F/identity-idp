a523412cd64e85a70dc070632c1bad01e75591fd
Andrew Duthie <andrew.duthie@gsa.gov>
2021-01-06 13:00:24 -0500

    Revert spinner button for IAL2 async step (#4563)
    * Revert "Add spinner button to phone step (#4546)"
    
    This reverts commit c5d3a2178d3d97252ac8628dacda705e2d48d112.
    
    * Revert "LG-3787: Create waiting experience for address checking, ID resolution and DocAuth (#4517)"
    
    This reverts commit 9d5f67863febe51c4f9a4edcd76f0c5eb9d7df5c.

--- app/javascript/packs/spinner-button.js
+++ app/javascript/packs/spinner-button.js
@@ -2,60 +2,30 @@
  * @typedef SpinnerButtonElements
  *
  * @prop {HTMLDivElement} wrapper
+ * @prop {HTMLImageElement} spinner
  * @prop {HTMLButtonElement|HTMLInputElement|HTMLLinkElement} button
- * @prop {HTMLDivElement?} actionMessage
  */
-
-/**
- * @typedef SpinnerButtonOptions
- *
- * @prop {number} longWaitDurationMs
- */
-
-/** @type {SpinnerButtonOptions} */
-const DEFAULT_OPTIONS = {
-  longWaitDurationMs: 15000,
-};
 
 export class SpinnerButton {
   constructor(wrapper) {
     /** @type {SpinnerButtonElements} */
     this.elements = {
       wrapper,
-      button: wrapper.querySelector('a,button:not([type]),[type="submit"],[type="button"]'),
-      actionMessage: wrapper.querySelector('.spinner-button__action-message'),
+      spinner: wrapper.querySelector('.spinner-button__spinner'),
+      button: wrapper.querySelector('a,button:not([type]),[type="submit"]'),
     };
 
-    this.options = {
-      ...DEFAULT_OPTIONS,
-      ...this.elements.wrapper.dataset,
-    };
-
-    this.options.longWaitDurationMs = Number(this.options.longWaitDurationMs);
+    this.bindEvents();
   }
 
-  bind() {
+  bindEvents() {
     this.elements.button.addEventListener('click', () => this.showSpinner());
   }
 
   showSpinner() {
-    const { wrapper, button, actionMessage } = this.elements;
-    wrapper.classList.add('spinner-button--spinner-active');
-
-    // Avoid setting disabled immediately to allow click event to propagate for form submission.
-    setTimeout(() => button.setAttribute('disabled', ''), 0);
-
-    if (actionMessage) {
-      actionMessage.textContent = /** @type {string} */ (actionMessage.dataset.message);
-    }
-
-    setTimeout(() => this.handleLongWait(), this.options.longWaitDurationMs);
-  }
-
-  handleLongWait() {
-    this.elements.actionMessage?.classList.remove('usa-sr-only');
+    this.elements.wrapper.classList.add('spinner-button--spinner-active');
+    this.elements.spinner.classList.remove('usa-sr-only');
   }
 }
 
-const wrappers = [...document.querySelectorAll('.spinner-button')];
-wrappers.forEach((wrapper) => new SpinnerButton(wrapper).bind());
+[...document.querySelectorAll('.spinner-button')].forEach((wrapper) => new SpinnerButton(wrapper));
--- spec/javascripts/packs/spinner-button-spec.js
+++ spec/javascripts/packs/spinner-button-spec.js
@@ -1,105 +1,34 @@
-import sinon from 'sinon';
 import userEvent from '@testing-library/user-event';
-import { getByRole } from '@testing-library/dom';
 import { SpinnerButton } from '../../../app/javascript/packs/spinner-button';
 
 describe('SpinnerButton', () => {
-  let clock;
-
-  const longWaitDurationMs = 1000;
-
-  function createWrapper({ actionMessage, tagName = 'a' } = {}) {
-    document.body.innerHTML = `
-      <div class="spinner-button" data-long-wait-duration-ms="${longWaitDurationMs}">
-        <div class="spinner-button__content">
-          ${tagName === 'a' ? '<a href="#">Click Me</a>' : '<input type="submit" value="Click Me">'}
-          <span class="spinner-button__spinner" aria-hidden="true">
-            <span class="spinner-button__spinner-dot"></span>
-            <span class="spinner-button__spinner-dot"></span>
-            <span class="spinner-button__spinner-dot"></span>
-          </span>
-        </div>
-        ${
-          actionMessage &&
-          `<div
-            role="status"
-            data-message="${actionMessage}"
-            class="spinner-button__action-message usa-sr-only"></div>`
-        }
-      </div>`;
-
-    return document.body.firstElementChild;
-  }
-
+  let wrapper;
   beforeEach(() => {
-    clock = sinon.useFakeTimers();
-  });
-
-  afterEach(() => {
-    clock.restore();
+    wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+      <div class="spinner-button">
+        <div class="spinner-button__content">
+          <a href="#">Click Me</a>
+        </div>
+        <img
+          srcset="/assets/spinner@2x.gif 2x"
+          height="144"
+          width="144"
+          alt=""
+          aria-hidden="true"
+          class="spinner-button__spinner usa-sr-only"
+          src="/assets/spinner.gif">
+      </div>
+    `;
+    wrapper = wrapper.firstElementChild;
   });
 
   it('shows spinner on click', () => {
-    const wrapper = createWrapper();
-    const spinnerButton = new SpinnerButton(wrapper);
-    spinnerButton.bind();
-    const { button } = spinnerButton.elements;
+    const { button, spinner } = new SpinnerButton(wrapper).elements;
 
     userEvent.click(button);
 
     expect(wrapper.classList.contains('spinner-button--spinner-active')).to.be.true();
-  });
-
-  it('disables button without preventing form handlers', () => {
-    const wrapper = createWrapper({ tagName: 'button' });
-    let submitted = false;
-    const form = document.createElement('form');
-    form.action = '#';
-    form.addEventListener('submit', (event) => {
-      submitted = true;
-      event.preventDefault();
-    });
-    document.body.appendChild(form);
-    form.appendChild(wrapper);
-    const spinnerButton = new SpinnerButton(wrapper);
-    spinnerButton.bind();
-    const { button } = spinnerButton.elements;
-
-    userEvent.type(button, '{enter}');
-    clock.tick(0);
-
-    expect(submitted).to.be.true();
-    expect(button.hasAttribute('disabled')).to.be.true();
-  });
-
-  it('announces action message', () => {
-    const wrapper = createWrapper({ actionMessage: 'Verifying...' });
-    const status = getByRole(wrapper, 'status');
-    const spinnerButton = new SpinnerButton(wrapper);
-    spinnerButton.bind();
-    const { button } = spinnerButton.elements;
-
-    expect(status.textContent).to.be.empty();
-
-    userEvent.click(button);
-
-    expect(status.textContent).to.equal('Verifying...');
-    expect(status.classList.contains('usa-sr-only')).to.be.true();
-  });
-
-  it('shows action message visually after long delay', () => {
-    const wrapper = createWrapper({ actionMessage: 'Verifying...' });
-    const status = getByRole(wrapper, 'status');
-    const spinnerButton = new SpinnerButton(wrapper);
-    spinnerButton.bind();
-    const { button } = spinnerButton.elements;
-
-    expect(status.textContent).to.be.empty();
-
-    userEvent.click(button);
-    clock.tick(longWaitDurationMs - 1);
-    expect(status.classList.contains('usa-sr-only')).to.be.true();
-    clock.tick(1);
-    expect(status.classList.contains('usa-sr-only')).to.be.false();
+    expect(spinner.classList.contains('usa-sr-only')).to.be.false();
   });
 });
