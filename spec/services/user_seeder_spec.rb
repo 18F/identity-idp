d4248710c7d1910c21e09b44667c5163e60a06c0
Oren Kanner <oren.kanner@gsa.gov>
2021-01-12 23:59:37 -0500

    Add rake task to seed users from a CSV file
    Adds the rake task `partners:seed_users` which requires two environment
    variables to be set - CSV_FILE and EMAIL_DOMAIN. These are passed to a
    `UserSeeder` service object that validates the inputs, ensures that this
    task does not run in the upper environments, and proceeds unless it
    would cause an existing user to be overwritten. It also then saves a
    modified CSV file that includes the backup codes and personal key for
    each created user.

---
+++ spec/services/user_seeder_spec.rb
@@ -0,0 +1,111 @@
+require 'rails_helper'
+
+RSpec.describe UserSeeder do
+  let(:valid_fixture) { 'spec/fixtures/valid_user_csv.csv' }
+
+  describe '.new' do
+    it 'raises the appropriate error with missing CSV file' do
+      opts = { csv_file: 'does_not_exist.csv', email_domain: 'foo.com' }
+
+      expect { described_class.new(**opts) }.to \
+        raise_error(ArgumentError, /does not exist/)
+    end
+
+    it 'raises the appropriate error with invalid CSV file' do
+      opts = {
+        csv_file: 'spec/fixtures/invalid_user_csv.csv',
+        email_domain: 'foo.com',
+      }
+
+      expect { described_class.new(**opts) }.to \
+        raise_error(ArgumentError, /must be a CSV file with headers/)
+    end
+
+    it 'raises the appropriate error with invalid email domain' do
+      opts = { csv_file: valid_fixture, email_domain: 'foo_com' }
+
+      expect { described_class.new(**opts) }.to \
+        raise_error(ArgumentError, /is not a valid hostname/)
+    end
+  end
+
+  describe '.run' do
+    it 'raises the appropriate error when running in prod' do
+      opts = { csv_file: valid_fixture, email_domain: 'foo.com', deploy_env: 'prod' }
+
+      expect { described_class.run(**opts) }.to \
+        raise_error(StandardError, /This cannot be run in staging or production/)
+    end
+
+    it 'raises the appropriate error when running in staging' do
+      opts = { csv_file: valid_fixture, email_domain: 'foo.com', deploy_env: 'staging' }
+
+      expect { described_class.run(**opts) }.to \
+        raise_error(StandardError, /This cannot be run in staging or production/)
+    end
+
+    it 'defaults to the value in LoginGov::Hostdata.env' do
+      opts = { csv_file: valid_fixture, email_domain: 'foo.com' }
+      allow(LoginGov::Hostdata).to receive(:env).and_return('prod')
+
+      expect { described_class.run(**opts) }.to \
+        raise_error(StandardError, /This cannot be run in staging or production/)
+    end
+
+    context 'when an email address is already taken' do
+      let(:taken_email) { 'user1@foo.com' }
+      let(:opts) { { csv_file: valid_fixture, email_domain: 'foo.com' } }
+
+      before { create_user_with_email(taken_email) }
+
+      it 'raises the appropriate error' do
+        expect { described_class.run(**opts) }.to \
+          raise_error(ArgumentError, /invalid - would overwrite existing users/)
+      end
+
+      it 'does not persist any users' do
+        described_class.run(**opts)
+      rescue ArgumentError
+        expect(User.count).to eq(1)
+      end
+
+      def create_user_with_email(email)
+        ee = EncryptedAttribute.new_from_decrypted(email)
+        user = User.create!(email_fingerprint: ee.fingerprint)
+        user.encrypted_email = ee.encrypted
+        user.reset_password('S00per Seekret', 'S00per Seekret')
+        # rubocop:disable Rails/SkipsModelValidations
+        user.email_addresses.update_all(confirmed_at: Time.zone.now)
+        # rubocop:enable Rails/SkipsModelValidations
+      end
+    end
+
+    context 'with valid inputs' do
+      let(:opts) { { csv_file: valid_fixture, email_domain: 'foo.com' } }
+      let(:output_file) { valid_fixture.gsub('.csv', '-updated.csv') }
+
+      after { File.delete(output_file) }
+
+      it 'creates the right number of users' do
+        expect { described_class.run(**opts) }.to change { User.count }.by(2)
+      end
+
+      it 'returns the number of users created' do
+        expect(described_class.run(**opts)).to eq(2)
+      end
+
+      it 'creates verified users' do
+        described_class.run(**opts)
+
+        expect(User.all.all? { |u| u.active_profile.present? }).to be_truthy
+      end
+
+      it 'saves the credentials to a CSV' do
+        described_class.run(**opts)
+
+        expect(File.read(output_file)).to \
+          match(/email_address,password,codes,personal_key\n/)
+      end
+    end
+  end
+end
