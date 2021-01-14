49ae49a3f07496144432dc8da6833fdd1b9ff57d
Andrew Duthie <andrew.duthie@gsa.gov>
2021-01-14 08:55:35 -0500

    Revert "Revert spinner button for IAL2 async step (#4563)" (#4565)
    This reverts commit a523412cd64e85a70dc070632c1bad01e75591fd.

--- spec/javascripts/support/sinon.js
+++ spec/javascripts/support/sinon.js
@@ -3,12 +3,27 @@ import sinon from 'sinon';
 /**
  * Returns an instance of a Sinon sandbox, and automatically restores all stubbed methods after each
  * test case.
+ *
+ * @param {sinon.SinonSandboxConfig=} config
  */
-export function useSandbox() {
-  const sandbox = sinon.createSandbox();
+export function useSandbox(config) {
+  const { useFakeTimers = false, ...remainingConfig } = config ?? {};
+  const sandbox = sinon.createSandbox(remainingConfig);
+
+  beforeEach(() => {
+    // useFakeTimers overrides global timer functions as soon as sandbox is created, thus leaking
+    // across tests. Instead, wait until tests start to initialize.
+    if (useFakeTimers) {
+      sandbox.useFakeTimers();
+    }
+  });
 
   afterEach(() => {
     sandbox.restore();
+
+    if (useFakeTimers) {
+      sandbox.clock.restore();
+    }
   });
 
   return sandbox;
