31d9f660ac509d73f72ddf3f0f4ca2b9f87d2b29
Mitchell Henke <mitchell.henke@gsa.gov>
2021-01-14 12:12:47 -0600

    fix inconsistent webpack hashing

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,12 @@
 const environment = require('./environment')
+const { merge } = require('webpack-merge')
+
+const sassLoader = environment.loaders.get('sass');
+
+sassLoader.use.map(function(loader) {
+  if (loader.loader === "css-loader") {
+    loader.options = merge(loader.options, { sourceMap: false });
+  }
+});
 
 module.exports = environment.toWebpackConfig()
