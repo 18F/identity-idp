c8a4bec4b17e8bc7b67eaa517df94919aced407f
Zach Margolis <zachary.margolis@gsa.gov>
2021-01-14 10:18:31 -0800

    Expand test coverage of more paths

--- spec/requests/acuant_sdk_spec.rb
+++ spec/requests/acuant_sdk_spec.rb
@@ -4,35 +4,42 @@ describe 'requesting acuant SDK assets' do
   context 'with a valid Acuant SDK asset' do
     [nil, *I18n.available_locales].each do |locale|
       context "with locale prefix: #{locale || '(none)'}" do
-        it 'renders a JS asset' do
-          get "/#{locale}/verify/doc_auth/AcuantImageProcessingWorker.min.js"
+        %w[doc_auth capture-doc capture_doc].each do |verify_path|
+          context "with verify path: #{verify_path}" do
+            it 'renders a JS asset' do
+              # example URLs:
+              # - /verify/doc_auth/AcuantImageProcessingWorker.min.js
+              # - /en/verify/capture_doc/AcuantImageProcessingWorker.min.js
+              get "/#{locale}/verify/#{verify_path}/AcuantImageProcessingWorker.min.js"
 
-          expect(response.status).to eq(200)
-          expect(response.headers['Content-Type']).to eq('application/javascript')
-          expect(response.body).to eq(
-            File.read('public/acuant/11.4.1/AcuantImageProcessingWorker.min.js'),
-          )
-        end
+              expect(response.status).to eq(200)
+              expect(response.headers['Content-Type']).to eq('application/javascript')
+              expect(response.body).to eq(
+                File.read('public/acuant/11.4.1/AcuantImageProcessingWorker.min.js'),
+              )
+            end
 
-        it 'renders a WASM asset' do
-          get "/#{locale}/verify/doc_auth/AcuantImageProcessingWorker.wasm"
+            it 'renders a WASM asset' do
+              get "/#{locale}/verify/#{verify_path}/AcuantImageProcessingWorker.wasm"
 
-          expect(response.status).to eq(200)
-          expect(response.headers['Content-Type']).to eq('application/wasm')
-          expect(response.body.length).to eq(
-            File.size('public/acuant/11.4.1/AcuantImageProcessingWorker.wasm'),
-          )
-        end
+              expect(response.status).to eq(200)
+              expect(response.headers['Content-Type']).to eq('application/wasm')
+              expect(response.body.length).to eq(
+                File.size('public/acuant/11.4.1/AcuantImageProcessingWorker.wasm'),
+              )
+            end
 
-        it 'does not add CSP headers' do
-          get "/#{locale}/verify/doc_auth/AcuantImageProcessingWorker.min.js"
+            it 'does not add CSP headers' do
+              get "/#{locale}/verify/#{verify_path}/AcuantImageProcessingWorker.min.js"
 
-          expect(response.headers['Content-Security-Policy']).to be_nil
-        end
+              expect(response.headers['Content-Security-Policy']).to be_nil
+            end
 
-        it 'does not include session or cookies' do
-          get "/#{locale}/verify/doc_auth/AcuantImageProcessingWorker.wasm"
-          expect(response.cookies.keys).to_not include('_upaya_session')
+            it 'does not include session or cookies' do
+              get "/#{locale}/verify/#{verify_path}/AcuantImageProcessingWorker.wasm"
+              expect(response.cookies.keys).to_not include('_upaya_session')
+            end
+          end
         end
       end
     end
