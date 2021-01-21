15fe73cc6bd9e8751ce3d631f6e96b9352da2a91
amathews <amathews@fearsol.com>
2021-01-21 14:12:53 -0500

    LG-3951 Minor lint fix.

--- app/controllers/users/service_provider_inactive_controller.rb
+++ app/controllers/users/service_provider_inactive_controller.rb
@@ -6,7 +6,7 @@ module Users
       analytics.track_event(Analytics::SP_INACTIVE_VISIT)
 
       @sp_name = sp_from_sp_session&.friendly_name ||
-        I18n.t('service_providers.errors.generic_sp_name')
+                 I18n.t('service_providers.errors.generic_sp_name')
 
       delete_branded_experience
       session[:sp] = {}
