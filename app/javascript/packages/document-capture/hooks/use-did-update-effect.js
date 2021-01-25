6cd645fffd772cd0aa0bc7cb991edb3efc3d182e
Andrew Duthie <andrew.duthie@gsa.gov>
2021-01-25 16:06:13 -0500

    Add documented behavior for useDidUpdateEffect

--- app/javascript/packages/document-capture/hooks/use-did-update-effect.js
+++ app/javascript/packages/document-capture/hooks/use-did-update-effect.js
@@ -1,6 +1,9 @@
 import { useRef, useEffect } from 'react';
 
 /**
+ * A hook behaving the same as useEffect in invoking the given callback when dependencies change,
+ * but does not call the callback during initial mount.
+ *
  * @type {typeof useEffect}
  */
 function useDidUpdateEffect(callback, deps) {
