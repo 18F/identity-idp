8dfbe6e08f52c10858a7a9cd463567b0c77bcb1a
Andrew Duthie <andrew.duthie@gsa.gov>
2021-01-25 16:13:05 -0500

    More detail about comparison to componentDidUpdate, not unmounting callback

--- app/javascript/packages/document-capture/hooks/use-did-update-effect.js
+++ app/javascript/packages/document-capture/hooks/use-did-update-effect.js
@@ -2,7 +2,8 @@ import { useRef, useEffect } from 'react';
 
 /**
  * A hook behaving the same as useEffect in invoking the given callback when dependencies change,
- * but does not call the callback during initial mount.
+ * but does not call the callback during initial mount or when unmounting. It can be considered as
+ * similar to ReactComponent#componentDidUpdate.
  *
  * @type {typeof useEffect}
  */
