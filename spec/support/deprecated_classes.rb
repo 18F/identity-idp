55c9fcd376482ff1d381193a40c0cc5377bc63d6
Andrew Duthie <andrew.duthie@gsa.gov>
2021-01-25 13:58:44 -0500

    Monkey-patch TagBuilder#tag_option for deprecated class detection
    **Why**: Better error messaging, better reconciliation of class defined in alternative formats (array, hash)
    
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>

--- spec/support/deprecated_classes.rb
+++ spec/support/deprecated_classes.rb
@@ -1,14 +1,23 @@
-RSpec.configure do |config|
-  deprecated = YAML.safe_load(File.read(File.expand_path('../../../.erb-lint.yml', __FILE__))).
-    dig('linters', 'DeprecatedClasses', 'rule_set').
-    flat_map { |rule| rule['deprecated'] }
-
-  pattern = Regexp.new "(^|\\b)(#{deprecated.join('|')})(\\b|$)"
-
-  config.before(:each) do
-    allow_any_instance_of(ActionView::Helpers::TagHelper::TagBuilder).
-      to receive(:tag_options).
-      with(hash_excluding(class: pattern), anything).
-      and_call_original
+class ActionView::Helpers::TagHelper::TagBuilder
+  def self.deprecated_classes
+    @deprecated_classes ||= begin
+      YAML.safe_load(File.read(File.expand_path('../../../.erb-lint.yml', __FILE__))).
+        dig('linters', 'DeprecatedClasses', 'rule_set').
+        flat_map { |rule| rule['deprecated'] }.
+        map { |regex_str| Regexp.new "^#{regex_str}$" }
+    end
   end
+
+  def modified_tag_option(key, value, *rest)
+    original_result = original_tag_option(key, value, *rest)
+    return original_result unless key == :class
+    attribute, classes = original_result.split('=')
+    classes = classes.tr('"', '').split(/ +/)
+    regex = self.class.deprecated_classes.find { |r| classes.any? { |c| r =~ c } }
+    raise "CSS class '#{value}' matched regex for deprecated classes #{regex}" if regex
+    original_result
+  end
+
+  alias_method :original_tag_option, :tag_option
+  alias_method :tag_option, :modified_tag_option
 end
