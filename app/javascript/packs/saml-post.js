4500c38818c7ad1407a4ca78327a459bf08a9db9
Andrew Duthie <andrew.duthie@gsa.gov>
2021-02-03 09:03:46 -0500

    Revert to usa-sr-only for screen-reader accessible text
    See: https://github.com/18F/identity-idp/pull/4638#discussion_r568696179
    See: https://github.com/18F/identity-idp/pull/4638/files#r569419688
    See: https://github.com/18F/identity-idp/pull/4638/files#r569423756

--- app/javascript/packs/saml-post.js
+++ app/javascript/packs/saml-post.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.body.className += ' display-none';
+  document.body.className += ' usa-sr-only';
   document.getElementById('saml-post-binding').submit();
 });
