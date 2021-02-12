cc963997546ac2710ed1ed9da807b8934388b16d
Aaron Chapman <aaron.chapman@gsa.gov>
2021-02-12 08:11:38 -0500

    LG-4179 count authentications in ial1 to ial2 step up flow (#4661)
    * count auths ial1 to ial2 step up flow
    
    * added tests
    
    * removed comments
    
    * removed trailing space
    
    * use ial_context.ial instead
    
    * add sp_session_ial_1_or_2 for billing
    
    * missed in last commit
    
    * fix piv_cac failures
    
    * spec cleanup

--- app/controllers/openid_connect/authorization_controller.rb
+++ app/controllers/openid_connect/authorization_controller.rb
@@ -137,8 +137,7 @@ module OpenidConnect
     end
 
     def track_events
-      ial = sp_session[:ial2] ? 2 : 1
-      analytics.track_event(Analytics::SP_REDIRECT_INITIATED, ial: ial)
+      analytics.track_event(Analytics::SP_REDIRECT_INITIATED, ial: sp_session_ial)
       Db::SpReturnLog::AddReturn.call(request_id, current_user.id)
       increment_monthly_auth_count
       add_sp_cost(:authentication)
--- spec/controllers/sign_up/completions_controller_spec.rb
+++ spec/controllers/sign_up/completions_controller_spec.rb
@@ -126,7 +126,7 @@ describe SignUp::CompletionsController do
       it 'updates verified attributes' do
         stub_sign_in
         subject.session[:sp] = {
-          ial2: false,
+          ial: 1,
           request_url: 'http://example.com',
           requested_attributes: ['email'],
         }
@@ -166,7 +166,7 @@ describe SignUp::CompletionsController do
         user = create(:user, profiles: [create(:profile, :verified, :active)])
         stub_sign_in(user)
         subject.session[:sp] = {
-          ial2: true,
+          ial: 2,
           request_url: 'http://example.com',
           requested_attributes: %w[email first_name verified_at],
         }
