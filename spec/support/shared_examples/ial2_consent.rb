4b07dd5d62743fde8fd7e725d94082b2fe5f6be2
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-02-12 13:41:54 -0600

    Add test that confirms notice shown if they try to click Continue without giving consent

--- spec/support/shared_examples/ial2_consent.rb
+++ spec/support/shared_examples/ial2_consent.rb
@@ -1,4 +1,12 @@
 shared_examples 'ial2 consent with js' do
+  it 'shows the notice if the user clicks continue without giving consent' do
+    expect(page).to have_button('Continue')
+    click_continue
+
+    expect(page).to have_current_path(idv_doc_auth_welcome_step)
+    expect(page).to have_content(t('errors.doc_auth.consent_form'))
+  end
+
   it 'allows the user to continue after checking the checkbox' do
     find('span[class="indicator"]').set(true)
     expect(page).to have_button('Continue')
