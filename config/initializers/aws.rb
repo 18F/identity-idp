a2095e7502a2393f5497b717914c59d5cda9c25f
Mitchell Henke <mitchell.henke@gsa.gov>
2021-03-02 16:51:14 -0600

    configure AWS to avoid rack timeouts

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,7 @@
 Aws.config.update(
   region: AppConfig.env.aws_region,
-  http_open_timeout: AppConfig.env.aws_http_timeout.to_i,
-  http_read_timeout: AppConfig.env.aws_http_timeout.to_i,
+  http_open_timeout: AppConfig.env.aws_http_timeout.to_f,
+  http_read_timeout: AppConfig.env.aws_http_timeout.to_f,
+  retry_limit: AppConfig.env.aws_http_retry_limit.to_i,
+  retry_max_delay: AppConfig.env.aws_http_retry_max_delay.to_i,
 )
