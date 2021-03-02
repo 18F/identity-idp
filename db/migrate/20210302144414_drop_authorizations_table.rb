efb0f5ae5f33ede9ea63dfd5aa4dfbc42c36afe7
Mitchell Henke <mitchell.henke@gsa.gov>
2021-03-02 10:26:09 -0600

    Update db/migrate/20210302144414_drop_authorizations_table.rb
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- db/migrate/20210302144414_drop_authorizations_table.rb
+++ db/migrate/20210302144414_drop_authorizations_table.rb
@@ -1,9 +1,9 @@
 class DropAuthorizationsTable < ActiveRecord::Migration[6.1]
   def change
     drop_table :authorizations do |t|
-      t.string   :provider
-      t.string   :uid
-      t.bigint  :user_id
+      t.string :provider
+      t.string :uid
+      t.bigint :user_id
       t.datetime :authorized_at
       t.timestamps
 
