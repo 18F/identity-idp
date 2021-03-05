5b21c9ebb5f822dd8071b462b0f0d20177ec6ab2
amathews <amathews@fearsol.com>
2021-03-05 15:46:44 -0500

    LG-4323 Update the CaptureDocController analytic_id base string to match the rest of Doc Auth and consolidate CloudWatch events.

--- spec/controllers/idv/capture_doc_controller_spec.rb
+++ spec/controllers/idv/capture_doc_controller_spec.rb
@@ -104,7 +104,7 @@ describe Idv::CaptureDocController do
         get :show, params: { step: 'capture_complete' }
 
         expect(@analytics).to have_received(:track_event).with(
-          Analytics::CAPTURE_DOC + ' visited', result
+          Analytics::DOC_AUTH + ' visited', result
         )
       end
 
@@ -115,11 +115,11 @@ describe Idv::CaptureDocController do
         get :show, params: { step: 'capture_complete' }
 
         expect(@analytics).to have_received(:track_event).ordered.with(
-          Analytics::CAPTURE_DOC + ' visited',
+          Analytics::DOC_AUTH + ' visited',
           hash_including(step: 'capture_complete', step_count: 1),
         )
         expect(@analytics).to have_received(:track_event).ordered.with(
-          Analytics::CAPTURE_DOC + ' visited',
+          Analytics::DOC_AUTH + ' visited',
           hash_including(step: 'capture_complete', step_count: 2),
         )
       end
