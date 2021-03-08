64aa2127e1617dc98945448f6961381f9c989712
Zach Margolis <zachary.margolis@gsa.gov>
2021-03-08 14:24:56 -0800

    fix requires

--- app/services/idv/proofer.rb
+++ app/services/idv/proofer.rb
@@ -7,24 +7,30 @@ module Idv
 
       def resolution_job_class
         if mock_fallback_enabled?
+          require 'identity-idp-functions/proof_resolution_mock'
           IdentityIdpFunctions::ProofResolutionMock
         else
+          require 'identity-idp-functions/proof_resolution'
           IdentityIdpFunctions::ProofResolution
         end
       end
 
       def address_job_class
         if mock_fallback_enabled?
+          require 'identity-idp-functions/proof_address_mock'
           IdentityIdpFunctions::ProofAddressMock
         else
+          require 'identity-idp-functions/proof_address'
           IdentityIdpFunctions::ProofAddress
         end
       end
 
       def document_job_class
         if mock_fallback_enabled?
+          require 'identity-idp-functions/proof_document_mock'
           IdentityIdpFunctions::ProofDocumentMock
         else
+          require 'identity-idp-functions/proof_document'
           IdentityIdpFunctions::ProofDocument
         end
       end
