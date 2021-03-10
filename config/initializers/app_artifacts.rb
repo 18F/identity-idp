b6055b61774143ac8ada9001001fc6988f3b8c7a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-03-10 13:22:22 -0500

    :sigh:

--- config/initializers/app_artifacts.rb
+++ config/initializers/app_artifacts.rb
@@ -6,7 +6,7 @@ AppArtifacts.setup do |store|
   store.add_artifact(:saml_2020_cert, '/%<env>s/saml2020.crt')
   store.add_artifact(:saml_2020_key, '/%<env>s/saml2020.key.enc')
   store.add_artifact(:saml_2021_cert, '/%<env>s/saml2021.crt')
-  store.add_artifact(:saml_2022_key, '/%<env>s/saml2021.key.enc')
+  store.add_artifact(:saml_2021_key, '/%<env>s/saml2021.key.enc')
 
   store.add_artifact(:oidc_private_key, '/%<env>s/oidc.key') { |k| OpenSSL::PKey::RSA.new(k) }
   store.add_artifact(:oidc_public_key, '/%<env>s/oidc.pub') { |k| OpenSSL::PKey::RSA.new(k) }
