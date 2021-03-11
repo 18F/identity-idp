253be64312d17914473b6b871f28040ef8225baf
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-03-10 23:19:22 -0600

    LG-4294 Add age requirement to proofing

--- app/forms/idv/doc_pii_form.rb
+++ app/forms/idv/doc_pii_form.rb
@@ -27,8 +27,10 @@ module Idv
         errors.add(:pii, generic_error)
       elsif !name_valid?
         errors.add(:pii, name_error)
-      elsif !dob_valid?
+      elsif !dob.present?
         errors.add(:pii, dob_error)
+      elsif !dob_meets_min_age?
+        errors.add(:pii, dob_min_age_error)
       elsif !state_valid?
         errors.add(:pii, generic_error)
       end
@@ -39,7 +41,11 @@ module Idv
     end
 
     def dob_valid?
-      dob.present?
+      dob.present? && dob_meets_min_age?
+    end
+
+    def dob_meets_min_age?
+      age(dob) >= AppConfig.env.idv_min_age.to_i
     end
 
     def state_valid?
@@ -60,6 +66,17 @@ module Idv
 
     def dob_error
       I18n.t('doc_auth.errors.lexis_nexis.birth_date_checks')
+    end
+
+    def dob_min_age_error
+      I18n.t('doc_auth.errors.lexis_nexis.birth_date_min_age')
+    end
+
+    def age(dob_str)
+      dob = Date.parse(dob_str)
+      now = Date.today
+      now.year - dob.year -
+        ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
     end
   end
 end
