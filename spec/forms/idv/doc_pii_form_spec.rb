2734c1ecc1e09e227f5c9f67933e211526093da7
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-03-11 11:41:31 -0600

    LG-4294 Add age requirement to proofing (#4788)

--- spec/forms/idv/doc_pii_form_spec.rb
+++ spec/forms/idv/doc_pii_form_spec.rb
@@ -3,21 +3,31 @@ require 'rails_helper'
 describe Idv::DocPiiForm do
   let(:user) { create(:user) }
   let(:subject) { Idv::DocPiiForm }
+  let(:valid_dob) { (Time.zone.today - (AppConfig.env.idv_min_age_years.to_i + 1).years).to_s }
+  let(:too_young_dob) { (Time.zone.today - (AppConfig.env.idv_min_age_years.to_i - 1).years).to_s }
   let(:good_pii) do
     {
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
-      dob: Time.zone.today.to_s,
+      dob: valid_dob,
       state: Faker::Address.state_abbr,
     }
   end
   let(:name_errors_pii) do
-    { first_name: nil, last_name: nil, dob: Time.zone.today.to_s,
+    { first_name: nil, last_name: nil, dob: valid_dob,
       state: Faker::Address.state_abbr }
   end
   let(:name_and_dob_errors_pii) do
     { first_name: nil, last_name: nil, dob: nil,
       state: Faker::Address.state_abbr }
+  end
+  let(:dob_min_age_error_pii) do
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      dob: too_young_dob,
+      state: Faker::Address.state_abbr,
+    }
   end
 
   describe '#submit' do
@@ -49,6 +59,18 @@ describe Idv::DocPiiForm do
         expect(result.success?).to eq(false)
         expect(result.errors[:pii]).to eq [
           t('doc_auth.errors.lexis_nexis.general_error_no_liveness'),
+        ]
+      end
+    end
+
+    context 'when there is an error with dob minimum age' do
+      it 'returns a single min age error' do
+        result = subject.new(dob_min_age_error_pii).submit
+
+        expect(result).to be_kind_of(FormResponse)
+        expect(result.success?).to eq(false)
+        expect(result.errors[:pii]).to eq [
+          t('doc_auth.errors.lexis_nexis.birth_date_min_age'),
         ]
       end
     end
