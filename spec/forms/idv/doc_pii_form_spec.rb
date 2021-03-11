0dd696d05172d21b15dc8345af0388de61b536be
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-03-10 23:41:28 -0600

    More spec

--- spec/forms/idv/doc_pii_form_spec.rb
+++ spec/forms/idv/doc_pii_form_spec.rb
@@ -4,6 +4,7 @@ describe Idv::DocPiiForm do
   let(:user) { create(:user) }
   let(:subject) { Idv::DocPiiForm }
   let(:valid_dob) { (Time.zone.today - (AppConfig.env.idv_min_age.to_i + 1).years).to_s }
+  let(:too_young_dob) { (Time.zone.today - (AppConfig.env.idv_min_age.to_i - 1).years).to_s }
   let(:good_pii) do
     {
       first_name: Faker::Name.first_name,
@@ -19,6 +20,14 @@ describe Idv::DocPiiForm do
   let(:name_and_dob_errors_pii) do
     { first_name: nil, last_name: nil, dob: nil,
       state: Faker::Address.state_abbr }
+  end
+  let(:dob_min_age_error_pii) do
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      dob: too_young_dob,
+      state: Faker::Address.state_abbr,
+    }
   end
 
   describe '#submit' do
@@ -50,6 +59,18 @@ describe Idv::DocPiiForm do
         expect(result.success?).to eq(false)
         expect(result.errors[:pii]).to eq [
           t('doc_auth.errors.lexis_nexis.general_error_no_liveness'),
+        ]
+      end
+    end
+
+    context 'when there is an error with dob minimum age' do
+      it 'returns a single min age error' do
+        result = subject.new(dob_min_age_error_pii).submit
+
+        expect(result).to be_kind_of(FormResponse)
+        expect(result.success?).to eq(false)
+        expect(result.errors[:pii]).to eq [
+          t('doc_auth.errors.lexis_nexis.birth_date_min_age'),
         ]
       end
     end
