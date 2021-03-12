699452bf64d507ea7d1f080db7b69ae10225593e
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-03-12 13:57:37 -0500

    Use configurable paths for geolite db and pwned passwords
    **Why**: So we don't have to depend on chef to put them on the right magical path
    
    This commit also includes a bonus fix to prevent us from download the files if they already exist.

--- app/services/pwned_passwords/lookup_password.rb
+++ app/services/pwned_passwords/lookup_password.rb
@@ -1,6 +1,6 @@
 module PwnedPasswords
   class LookupPassword
-    PWNED_PASSWORD_FILE = Rails.root.join('pwned_passwords', 'pwned_passwords.txt')
+    PWNED_PASSWORD_FILE = Rails.root.join(AppConfig.env.pwned_passwords_file_path).freeze
 
     def self.call(password)
       BinarySearchSortedHashFile.new(PWNED_PASSWORD_FILE).call(password)
