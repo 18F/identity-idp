3aa80f1d59ae1a797b2845c55452fea043fa4ba1
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-03-19 12:05:19 -0500

    Unused

--- app/services/idv/steps/welcome_step.rb
+++ app/services/idv/steps/welcome_step.rb
@@ -3,16 +3,10 @@ module Idv
     class WelcomeStep < DocAuthBaseStep
       def call
         return no_camera_redirect if params[:no_camera]
-        return skip_to_capture if params[:skip_upload]
         create_document_capture_session(document_capture_session_uuid_key)
       end
 
       private
-
-      def skip_to_capture
-        # See: Idv::DocAuthController#update_if_skipping_upload
-        flow_session[:skip_upload_step] = true
-      end
 
       def no_camera_redirect
         redirect_to idv_doc_auth_errors_no_camera_url
