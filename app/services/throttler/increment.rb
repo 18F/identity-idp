7c29df93e3650108166d3175b025dcf776d6d5fb
Andrew Duthie <andrew.duthie@gsa.gov>
2021-03-23 09:05:30 -0400

    LG-4305: Log new event in case of lockout from proofing (#4803)
    * LG-4305: Log new event in case of lockout from proofing
    
    **Why**: As a login.gov developer, i want to see an event in the event log that indicates that a user was locked out for 6 hours from proofing along with the relevant data points that directly resulted in that lockout, so that I can troubleshoot any issues reported by end users and pinpoint exactly what caused the lock out.
    
    * Consolidate rate limit triggered to single event
    
    **Why**: Easier queryability of events, fewer and more consistent events, compatibility with potential future refactor to move event logging into throttle service or model.
    
    * Handle throttler trigger logging behavior in Throttler service
    
    **Why**: Encapsulate behaviors of throttling within existing service behaviors
    
    * Include analytics in verify_document_action

--- app/services/throttler/increment.rb
+++ app/services/throttler/increment.rb
@@ -1,9 +1,13 @@
 module Throttler
   class Increment
-    def self.call(user_id, throttle_type)
+    def self.call(user_id, throttle_type, analytics: nil)
       throttle = Throttler::FindOrCreate.call(user_id, throttle_type)
       return throttle if throttle.maxed?
-      throttle.update(attempts: throttle.attempts + 1, attempted_at: Time.zone.now)
+      Update.call(
+        throttle: throttle,
+        attributes: { attempts: throttle.attempts + 1, attempted_at: Time.zone.now },
+        analytics: analytics,
+      )
       throttle
     end
   end
