3c7a2aedcbfbb9afc6642e41536a1e2243fa4006
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-03-26 11:27:56 -0400

    remove some tests

--- spec/lib/deploy/activate_spec.rb
+++ spec/lib/deploy/activate_spec.rb
@@ -18,8 +18,6 @@ describe Deploy::Activate do
   let(:set_up_files!) {}
   let(:instance_role) { 'idp' }
 
-  let(:result_yaml_path) { File.join(root, 'config', 'application.yml') }
-  let(:env_yaml_path) { File.join(root, 'config', 'application_s3_env.yml') }
   let(:geolite_path) { File.join(root, 'geo_data', 'GeoLite2-City.mmdb') }
   let(:pwned_passwords_path) { File.join(root, 'pwned_passwords', 'pwned_passwords.txt') }
 
@@ -61,123 +59,13 @@ describe Deploy::Activate do
 
     let(:s3_contents) do
       {
-        'int/idp/v1/application.yml' => application_yml,
         'common/GeoIP2-City.mmdb' => geolite_content,
         'common/pwned-passwords.txt' => pwned_passwords_content,
       }
     end
 
-    let(:application_yml) do
-      <<~YAML
-        production:
-          usps_confirmation_max_days: '5'
-      YAML
-    end
     let(:geolite_content) { 'geolite test' }
     let(:pwned_passwords_content ) { 'pwned passwords test' }
-
-    it 'downloads configs from s3' do
-      subject.run
-
-      expect(s3_client).to have_received(:get_object).with(
-        bucket: 'login-gov.app-secrets.12345-us-west-1',
-        key: 'int/idp/v1/application.yml',
-      )
-
-      expect(File.exist?(result_yaml_path)).to eq(true)
-      expect(File.read(result_yaml_path)).to include("usps_confirmation_max_days: '5'")
-    end
-
-    it 'merges the application.yml from s3 over the application.yml.default' do
-      subject.run
-
-      combined_application_yml = YAML.load_file(result_yaml_path)
-
-      # top-level key from application.yml.default
-      expect(combined_application_yml['recovery_code_length']).to eq('4')
-      # overridden production key from s3
-      expect(combined_application_yml['production']['usps_confirmation_max_days']).to eq('5')
-      # production key from application.yml.example, not overwritten
-      expect(combined_application_yml['production']['lockout_period_in_minutes']).to eq('10')
-    end
-
-    context 'on a web instance' do
-      let(:instance_role) { 'idp' }
-
-      context 'when web.yml exists in s3' do
-        before do
-          s3_contents['int/idp/v1/web.yml'] = <<~YAML
-            web_yaml_value: 'true'
-          YAML
-        end
-
-        it 'merges web.yml into application.yml' do
-          subject.run
-
-          expect(File.exist?("#{root}/config/web.yml")).to eq(true)
-
-          combined_application_yml = YAML.load_file(result_yaml_path)
-          expect(combined_application_yml['web_yaml_value']).to eq('true')
-        end
-      end
-
-      context 'when web.yml does not exist in s3' do
-        it 'warns and leaves application.yml as-is' do
-          expect(logger).to receive(:warn).with(/web.yml/)
-
-          expect { subject.run }.to_not raise_error
-
-          expect(File.exist?("#{root}/config/web.yml")).to eq(false)
-
-          combined_application_yml = YAML.load_file(result_yaml_path)
-          expect(combined_application_yml).to_not have_key('web_yaml_value')
-        end
-      end
-    end
-
-    context 'on a worker instance' do
-      let(:instance_role) { 'worker' }
-
-      context 'when worker.yml exists in s3' do
-        before do
-          s3_contents['int/idp/v1/worker.yml'] = <<~YAML
-            worker_yaml_value: 'true'
-          YAML
-        end
-
-        it 'merges worker.yml into application.yml' do
-          subject.run
-
-          expect(File.exist?("#{root}/config/worker.yml")).to eq(true)
-
-          combined_application_yml = YAML.load_file(result_yaml_path)
-          expect(combined_application_yml['worker_yaml_value']).to eq('true')
-        end
-      end
-
-      context 'when worker.yml does not exist in s3' do
-        it 'warns and leaves application.yml as-is' do
-          expect(logger).to receive(:warn).with(/worker.yml/)
-
-          expect { subject.run }.to_not raise_error
-
-          expect(File.exist?("#{root}/config/worker.yml")).to eq(false)
-
-          combined_application_yml = YAML.load_file(result_yaml_path)
-          expect(combined_application_yml).to_not have_key('worker_yaml_value')
-        end
-      end
-    end
-
-    it 'sets the correct permissions on the YAML files' do
-      subject.run
-
-      application_yml = File.new(result_yaml_path)
-      expect(application_yml.stat.mode.to_s(8)).to eq('100640')
-
-      application_env_yml = File.new(env_yaml_path)
-      expect(application_env_yml.stat.mode.to_s(8)).to eq('100640')
-    end
 
     it 'downloads the pwned passwords and geolite files from s3' do
       subject.run
