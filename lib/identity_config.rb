8e2ffb42ea8db0adbbd20dc0c61293af299bc576
Mitchell Henke <mitchell.henke@gsa.gov>
2021-03-26 14:52:06 -0500

    Configuration class to check types and presence
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    Co-authored-by: Jonathan Hooper <jonathan.hooper@gsa.gov>

---
+++ lib/identity_config.rb
@@ -0,0 +1,49 @@
+class IdentityConfig
+  class << self
+    attr_reader :store
+  end
+
+  CONVERTERS = {
+    uri: proc { |value| URI(value) },
+    string: proc { |value| value.to_s },
+    comma_separated_string_list: proc do |value|
+      value.split(',')
+    end,
+    integer: proc do |value|
+      Integer(value)
+    end,
+    json: proc do |value, options: {}|
+      JSON.parse(value, symbolize_names: options[:symbolize_names])
+    end,
+    boolean: proc do |value|
+      case value
+      when 'true'
+        true
+      when 'false'
+        false
+      else
+        raise 'invalid boolean value'
+      end
+    end,
+  }
+
+  def initialize(read_env)
+    @read_env = read_env
+    @written_env = {}
+  end
+
+  def add(key, type: :string, is_sensitive: false, options: {})
+    value = @read_env[key]
+    raise "#{key} is required but is not present" if value.nil?
+    converted_value = CONVERTERS.fetch(type).call(value, options: options)
+    raise "#{key} is required but is not present" if converted_value.nil?
+
+    @written_env[key] = converted_value
+    @written_env
+  end
+
+  def self.build_store(config_map)
+    config = IdentityConfig.new(config_map)
+    @store = Struct.new('IdentityConfig', *config.keys, keyword_init: true).new(**config)
+  end
+end
