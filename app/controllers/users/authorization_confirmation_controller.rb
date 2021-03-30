e21ecb48c22faeedaa08979dfbfaf4beafd9d8c9
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-03-30 13:38:13 -0500

    Redirect to account page if no sp in session on user_authorization_confirmation page

--- app/controllers/users/authorization_confirmation_controller.rb
+++ app/controllers/users/authorization_confirmation_controller.rb
@@ -2,17 +2,26 @@ module Users
   class AuthorizationConfirmationController < ApplicationController
     include AuthorizationCountConcern
 
+    before_action :ensure_sp_in_session_with_request_url, only: :show
     before_action :bump_auth_count
     before_action :confirm_two_factor_authenticated
 
     def show
       analytics.track_event(Analytics::AUTHENTICATION_CONFIRMATION)
-      @sp = ServiceProvider.find_by(issuer: sp_session[:issuer]) if sp_session
+      @sp = ServiceProvider.find_by(issuer: sp_session[:issuer])
     end
 
     def update
       sign_out :user
       redirect_to new_user_session_url(request_id: sp_session[:request_id])
+    end
+
+    private
+
+    def ensure_sp_in_session_with_request_url
+      return if sp_session&.dig(:request_url)
+
+      redirect_to account_url
     end
   end
 end
