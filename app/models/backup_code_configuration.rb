fe7c92ab37e0693c9012be6ea133f9861242f453
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-04-02 14:16:40 -0700

    Update app/models/backup_code_configuration.rb
    Co-authored-by: Mitchell Henke <mitchell.henke@gsa.gov>

--- app/models/backup_code_configuration.rb
+++ app/models/backup_code_configuration.rb
@@ -43,8 +43,7 @@ class BackupCodeConfiguration < ApplicationRecord
       user_salt_costs = select(:code_salt, :code_cost).
         distinct.
         where(user_id: user_id).
-        where.not(code_salt: nil, code_cost: nil).
-        limit(BackupCodeGenerator::NUMBER_OF_CODES * 2).
+        where.not(code_salt: nil).where.not(code_cost: nil)
         pluck(:code_salt, :code_cost)
 
       salted_fingerprints = user_salt_costs.map do |salt, cost|
