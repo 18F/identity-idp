d905e27a6b61ddd4b9d0725ffd0c5825eb232444
amathews <amathews@fearsol.com>
2021-04-05 18:13:20 -0400

    LG-4327 First pass image metric errors for DPI, Blur, and Glare

--- app/services/doc_auth_router.rb
+++ app/services/doc_auth_router.rb
@@ -1,5 +1,77 @@
 # Helps route between various doc auth backends, provided by the identity-doc-auth gem
 module DocAuthRouter
+
+  ERROR_TRANSLATIONS = {
+    # i18n-tasks-use t('doc_auth.errors.alerts.barcode_content_check')
+    IdentityDocAuth::Errors::BARCODE_CONTENT_CHECK =>
+      'doc_auth.errors.alerts.barcode_content_check',
+    # i18n-tasks-use t('doc_auth.errors.alerts.barcode_read_check')
+    IdentityDocAuth::Errors::BARCODE_READ_CHECK =>
+      'doc_auth.errors.alerts.barcode_read_check',
+    # i18n-tasks-use t('doc_auth.errors.alerts.birth_date_checks')
+    IdentityDocAuth::Errors::BIRTH_DATE_CHECKS =>
+      'doc_auth.errors.alerts.birth_date_checks',
+    # i18n-tasks-use t('doc_auth.errors.alerts.control_number_check')
+    IdentityDocAuth::Errors::CONTROL_NUMBER_CHECK =>
+      'doc_auth.errors.alerts.control_number_check',
+    # i18n-tasks-use t('doc_auth.errors.alerts.doc_crosscheck')
+    IdentityDocAuth::Errors::DOC_CROSSCHECK =>
+      'doc_auth.errors.alerts.doc_crosscheck',
+    # i18n-tasks-use t('doc_auth.errors.alerts.doc_number_checks')
+    IdentityDocAuth::Errors::DOC_NUMBER_CHECKS =>
+      'doc_auth.errors.alerts.doc_number_checks',
+    # i18n-tasks-use t('doc_auth.errors.alerts.expiration_checks')
+    IdentityDocAuth::Errors::EXPIRATION_CHECKS =>
+      'doc_auth.errors.alerts.expiration_checks',
+    # i18n-tasks-use t('doc_auth.errors.alerts.full_name_check')
+    IdentityDocAuth::Errors::FULL_NAME_CHECK =>
+      'doc_auth.errors.alerts.full_name_check',
+    # i18n-tasks-use t('doc_auth.errors.general.liveness')
+    IdentityDocAuth::Errors::GENERAL_ERROR_LIVENESS =>
+      'doc_auth.errors.general.liveness',
+    # i18n-tasks-use t('doc_auth.errors.general.no_liveness')
+    IdentityDocAuth::Errors::GENERAL_ERROR_NO_LIVENESS =>
+      'doc_auth.errors.general.no_liveness',
+    # i18n-tasks-use t('doc_auth.errors.alerts.id_not_recognized')
+    IdentityDocAuth::Errors::ID_NOT_RECOGNIZED =>
+      'doc_auth.errors.alerts.id_not_recognized',
+    # i18n-tasks-use t('doc_auth.errors.alerts.id_not_verified')
+    IdentityDocAuth::Errors::ID_NOT_VERIFIED =>
+      'doc_auth.errors.alerts.id_not_verified',
+    # i18n-tasks-use t('doc_auth.errors.alerts.issue_date_checks')
+    IdentityDocAuth::Errors::ISSUE_DATE_CHECKS =>
+      'doc_auth.errors.alerts.issue_date_checks',
+    # i18n-tasks-use t('doc_auth.errors.general.multiple_back_id_failures')
+    IdentityDocAuth::Errors::MULTIPLE_BACK_ID_FAILURES =>
+      'doc_auth.errors.general.multiple_back_id_failures',
+    # i18n-tasks-use t('doc_auth.errors.general.multiple_front_id_failures')
+    IdentityDocAuth::Errors::MULTIPLE_FRONT_ID_FAILURES =>
+      'doc_auth.errors.general.multiple_front_id_failures',
+    # i18n-tasks-use t('doc_auth.errors.alerts.ref_control_number_check')
+    IdentityDocAuth::Errors::REF_CONTROL_NUMBER_CHECK =>
+      'doc_auth.errors.alerts.ref_control_number_check',
+    # i18n-tasks-use t('doc_auth.errors.alerts.selfie_failure')
+    IdentityDocAuth::Errors::SELFIE_FAILURE => 'doc_auth.errors.alerts.selfie_failure',
+    # i18n-tasks-use t('doc_auth.errors.alerts.sex_check')
+    IdentityDocAuth::Errors::SEX_CHECK => 'doc_auth.errors.alerts.sex_check',
+    # i18n-tasks-use t('doc_auth.errors.alerts.visible_color_check')
+    IdentityDocAuth::Errors::VISIBLE_COLOR_CHECK => 'doc_auth.errors.alerts.visible_color_check',
+    # i18n-tasks-use t('doc_auth.errors.alerts.visible_photo_check')
+    IdentityDocAuth::Errors::VISIBLE_PHOTO_CHECK => 'doc_auth.errors.alerts.visible_photo_check',
+    # i18n-tasks-use t('doc_auth.errors.dpi.top_msg')
+    IdentityDocAuth::Errors::DPI_LOW_ONE_SIDE => 'doc_auth.errors.dpi.top_msg',
+    # i18n-tasks-use t('doc_auth.errors.dpi.top_msg_plural')
+    IdentityDocAuth::Errors::DPI_LOW_BOTH_SIDES => 'doc_auth.errors.dpi.top_msg_plural',
+    # i18n-tasks-use t('doc_auth.errors.sharpness.top_msg')
+    IdentityDocAuth::Errors::SHARP_LOW_ONE_SIDE => 'doc_auth.errors.sharpness.top_msg',
+    # i18n-tasks-use t('doc_auth.errors.sharpness.top_msg_plural')
+    IdentityDocAuth::Errors::SHARP_LOW_BOTH_SIDES => 'doc_auth.errors.sharpness.top_msg_plural',
+    # i18n-tasks-use t('doc_auth.errors.glare.top_msg')
+    IdentityDocAuth::Errors::GLARE_LOW_ONE_SIDE => 'doc_auth.errors.glare.top_msg',
+    # i18n-tasks-use t('doc_auth.errors.glare.top_msg_plural')
+    IdentityDocAuth::Errors::GLARE_LOW_BOTH_SIDES => 'doc_auth.errors.glare.top_msg_plural',
+  }.freeze
+
   # Adds translations to responses from Acuant
   class AcuantErrorTranslatorProxy
     attr_reader :client
@@ -34,11 +106,17 @@ module DocAuthRouter
 
     def translate_friendly_errors!(response)
       response.errors[:results]&.map! do |untranslated_error|
-        friendly_message = FriendlyError::Message.call(untranslated_error, 'doc_auth')
-        if friendly_message == untranslated_error
-          I18n.t('errors.doc_auth.general_error')
+        error_key = ERROR_TRANSLATIONS[untranslated_error]
+
+        if error_key
+          I18n.t(error_key)
         else
-          friendly_message
+          friendly_message = FriendlyError::Message.call(untranslated_error, 'doc_auth')
+          if friendly_message == untranslated_error
+            I18n.t('errors.doc_auth.general_error')
+          else
+            friendly_message
+          end
         end
       end&.uniq!
     end
@@ -87,69 +165,6 @@ module DocAuthRouter
       response
     end
 
-    ERROR_TRANSLATIONS = {
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.barcode_content_check')
-      IdentityDocAuth::LexisNexis::Errors::BARCODE_CONTENT_CHECK =>
-        'doc_auth.errors.lexis_nexis.barcode_content_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.barcode_read_check')
-      IdentityDocAuth::LexisNexis::Errors::BARCODE_READ_CHECK =>
-        'doc_auth.errors.lexis_nexis.barcode_read_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.birth_date_checks')
-      IdentityDocAuth::LexisNexis::Errors::BIRTH_DATE_CHECKS =>
-        'doc_auth.errors.lexis_nexis.birth_date_checks',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.control_number_check')
-      IdentityDocAuth::LexisNexis::Errors::CONTROL_NUMBER_CHECK =>
-        'doc_auth.errors.lexis_nexis.control_number_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.doc_crosscheck')
-      IdentityDocAuth::LexisNexis::Errors::DOC_CROSSCHECK =>
-        'doc_auth.errors.lexis_nexis.doc_crosscheck',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.doc_number_checks')
-      IdentityDocAuth::LexisNexis::Errors::DOC_NUMBER_CHECKS =>
-        'doc_auth.errors.lexis_nexis.doc_number_checks',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.expiration_checks')
-      IdentityDocAuth::LexisNexis::Errors::EXPIRATION_CHECKS =>
-        'doc_auth.errors.lexis_nexis.expiration_checks',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.full_name_check')
-      IdentityDocAuth::LexisNexis::Errors::FULL_NAME_CHECK =>
-        'doc_auth.errors.lexis_nexis.full_name_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.general_error_liveness')
-      IdentityDocAuth::LexisNexis::Errors::GENERAL_ERROR_LIVENESS =>
-        'doc_auth.errors.lexis_nexis.general_error_liveness',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.general_error_no_liveness')
-      IdentityDocAuth::LexisNexis::Errors::GENERAL_ERROR_NO_LIVENESS =>
-        'doc_auth.errors.lexis_nexis.general_error_no_liveness',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.id_not_recognized')
-      IdentityDocAuth::LexisNexis::Errors::ID_NOT_RECOGNIZED =>
-        'doc_auth.errors.lexis_nexis.id_not_recognized',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.id_not_verified')
-      IdentityDocAuth::LexisNexis::Errors::ID_NOT_VERIFIED =>
-        'doc_auth.errors.lexis_nexis.id_not_verified',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.issue_date_checks')
-      IdentityDocAuth::LexisNexis::Errors::ISSUE_DATE_CHECKS =>
-        'doc_auth.errors.lexis_nexis.issue_date_checks',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.multiple_back_id_failures')
-      IdentityDocAuth::LexisNexis::Errors::MULTIPLE_BACK_ID_FAILURES =>
-        'doc_auth.errors.lexis_nexis.multiple_back_id_failures',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.multiple_front_id_failures')
-      IdentityDocAuth::LexisNexis::Errors::MULTIPLE_FRONT_ID_FAILURES =>
-        'doc_auth.errors.lexis_nexis.multiple_front_id_failures',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.ref_control_number_check')
-      IdentityDocAuth::LexisNexis::Errors::REF_CONTROL_NUMBER_CHECK =>
-        'doc_auth.errors.lexis_nexis.ref_control_number_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.selfie_failure')
-      IdentityDocAuth::LexisNexis::Errors::SELFIE_FAILURE =>
-        'doc_auth.errors.lexis_nexis.selfie_failure',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.sex_check')
-      IdentityDocAuth::LexisNexis::Errors::SEX_CHECK =>
-        'doc_auth.errors.lexis_nexis.sex_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.visible_color_check')
-      IdentityDocAuth::LexisNexis::Errors::VISIBLE_COLOR_CHECK =>
-        'doc_auth.errors.lexis_nexis.visible_color_check',
-      # i18n-tasks-use t('doc_auth.errors.lexis_nexis.visible_photo_check')
-      IdentityDocAuth::LexisNexis::Errors::VISIBLE_PHOTO_CHECK =>
-        'doc_auth.errors.lexis_nexis.visible_photo_check',
-    }.freeze
-
     def translate_trueid_errors!(response)
       IdentityDocAuth::LexisNexis::ErrorGenerator::ERROR_KEYS.each do |category|
         response.errors[category]&.map! do |plain_error|
@@ -158,7 +173,8 @@ module DocAuthRouter
             I18n.t(error_key)
           else
             Rails.logger.warn("unknown LexisNexis error=#{plain_error}")
-            I18n.t('doc_auth.errors.lexis_nexis.general_error_no_liveness')
+            # This isn't right, this should depend on the liveness setting
+            I18n.t('doc_auth.errors.general.no_liveness')
           end
         end
       end
@@ -167,7 +183,7 @@ module DocAuthRouter
     # rubocop:disable Style/GuardClause
     def translate_generic_errors!(response)
       if response.errors[:network] == true
-        response.errors[:network] = I18n.t('doc_auth.errors.lexis_nexis.network_error')
+        response.errors[:network] = I18n.t('doc_auth.errors.general.network_error')
       end
     end
     # rubocop:enable Style/GuardClause
@@ -220,6 +236,6 @@ module DocAuthRouter
   end
 
   def self.doc_auth_vendor
-    vendor_from_config = AppConfig.env.doc_auth_vendor
+    AppConfig.env.doc_auth_vendor
   end
 end
