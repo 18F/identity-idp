af67e8bb6f707597082cab5c3b5c88b31f5a50bb
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-04-05 22:53:47 -0500

    Sometimes no user_session

--- app/controllers/concerns/effective_user.rb
+++ app/controllers/concerns/effective_user.rb
@@ -1,13 +1,17 @@
 module EffectiveUser
   def effective_user_id
     [
-      session[:ial2_recovery_user_id],
-      session[:doc_capture_user_id],
+      user_session_or_session[:ial2_recovery_user_id],
+      user_session_or_session[:doc_capture_user_id],
       current_user&.id,
     ].find(&:present?)
   end
 
   def effective_user
     User.find_by(id: effective_user_id) if effective_user_id
+  end
+
+  def user_session_or_session
+    user_session || session
   end
 end
