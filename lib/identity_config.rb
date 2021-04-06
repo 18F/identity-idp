162646fe962ce6331f1561ba0303dbdaeac9be6e
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-06 11:22:59 -0500

    convert job_run_healthchecks_enabled

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -69,6 +69,7 @@ class IdentityConfig
     config.add(:identity_pki_disabled, type: :boolean)
     config.add(:identity_pki_local_dev, type: :boolean)
     config.add(:issuers_with_email_nameid_format, type: :comma_separated_string_list)
+    config.add(:job_run_healthchecks_enabled, type: :boolean)
     config.add(:no_sp_campaigns_whitelist, type: :json)
     config.add(:mailer_domain_name)
     config.add(:nonessential_email_banlist, type: :json)
