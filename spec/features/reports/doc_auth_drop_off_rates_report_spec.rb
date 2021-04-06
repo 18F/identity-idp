d193077da76d9465ebb6e9c427f2677c9b8d9a16
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-04-06 09:03:49 -0500

    Some coverage

--- spec/features/reports/doc_auth_drop_off_rates_report_spec.rb
+++ spec/features/reports/doc_auth_drop_off_rates_report_spec.rb
@@ -4,4 +4,11 @@ feature 'Doc auth drop off rates report' do
   it 'does not throw an error' do
     expect(Reports::DocAuthDropOffRatesReport.new.call).to be_present
   end
+
+  it 'has all the steps in the funnel report in the left most column and justified' do
+    report = Reports::DocAuthDropOffRatesReport.new.call
+    Db::DocAuthLog::DropOffRatesHelper::STEPS.each do |step|
+      expect(report.include?(format('%20s', step))).to be true
+    end
+  end
 end
