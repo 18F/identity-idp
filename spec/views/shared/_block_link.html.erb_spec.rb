ea26ce8608ed1e0debcaa42f8e138a864e924694
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-07 16:24:03 -0400

    Block Link: Throw if no block content given
    Why: https://github.com/18F/identity-idp/pull/4886/files#r608794415

--- spec/views/shared/_block_link.html.erb_spec.rb
+++ spec/views/shared/_block_link.html.erb_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'shared/_block_link.html.erb' do
+  it 'raises an error if no block given' do
+    expect { render 'shared/block_link', url: '/example' }.to raise_error('no block content given')
+  end
+
   it 'renders a link to the given URL with the given text' do
     render('shared/block_link', url: '/example') { 'Link Text' }
 
