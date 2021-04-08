f3f63f8153a488bfd13c2cc1be24ad9a1b2aac3d
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-08 14:37:06 -0400

    Revert multi cert (#4894)
    * Revert "Ensure non-nil fingerprint for SAML logout (#4890)"
    
    This reverts commit 00deec629fd5653459c3929b926ef25b5355e134.
    
    * Revert "Stop referencing ServiceProvider#fingerprint (#4884)"
    
    This reverts commit 7fe7be7ae0bdddb06bc01b38c8c75c01d496b016.
    
    * Revert "Remove fingerprint from seeded local development service provider (#4877)"
    
    This reverts commit a8c948142eede950b8a434fd8aa5a53945e9754d.
    
    * Revert "Allow SPs to have multiple certs (LG-2049) (#4851)"
    
    This reverts commit 0efa17f7cdc48c90740418cb370fef47bd4d605d.
    
    * Restore migration from multi-cert
    
    * Re-add certs column to NullServiceProvider

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -2,7 +2,7 @@ require 'fingerprinter'
 require 'identity_validations'
 
 class ServiceProvider < ApplicationRecord
-  self.ignored_columns = %w[deal_id agency aal fingerprint]
+  self.ignored_columns = %w[deal_id agency aal]
 
   belongs_to :agency
 
@@ -27,18 +27,31 @@ class ServiceProvider < ApplicationRecord
   end
 
   def metadata
-    attributes.symbolize_keys
+    attributes.symbolize_keys.merge(fingerprint: fingerprint)
   end
 
-  # @return [Array<OpenSSL::X509::Certificate>]
-  def ssl_certs
-    @ssl_certs ||= (certs.presence || Array(cert)).map do |cert|
+  def ssl_cert
+    @ssl_cert ||= begin
+      return if cert.blank?
       OpenSSL::X509::Certificate.new(load_cert(cert))
     end
   end
 
+  def fingerprint
+    @_fingerprint ||= super || Fingerprinter.fingerprint_cert(ssl_cert)
+  end
+
   def encrypt_responses?
     block_encryption != 'none'
+  end
+
+  def encryption_opts
+    return nil unless encrypt_responses?
+    {
+      cert: ssl_cert,
+      block_encryption: block_encryption,
+      key_transport: 'rsa-oaep-mgf1p',
+    }
   end
 
   def skip_encryption_allowed
