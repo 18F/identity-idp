82b3f893ab912f3f84f822174a1a0de9015457a6
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-09 12:42:24 -0500

    convert aws_http_timeout

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,8 +4,8 @@ log_formatter = Aws::Log::Formatter.new(pattern)
 
 Aws.config.update(
   region: AppConfig.env.aws_region,
-  http_open_timeout: AppConfig.env.aws_http_timeout.to_f,
-  http_read_timeout: AppConfig.env.aws_http_timeout.to_f,
+  http_open_timeout: IdentityConfig.store.aws_http_timeout.to_f,
+  http_read_timeout: IdentityConfig.store.aws_http_timeout.to_f,
   retry_limit: AppConfig.env.aws_http_retry_limit.to_i,
   retry_max_delay: AppConfig.env.aws_http_retry_max_delay.to_i,
   logger: ActiveSupport::Logger.new(Rails.root.join('log', 'production.log')),
