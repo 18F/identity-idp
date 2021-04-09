72a57aa829b75c6a3ac1f101536666ac0ba92e80
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-09 14:48:55 -0500

    convert min_password_score

--- app/validators/form_password_validator.rb
+++ app/validators/form_password_validator.rb
@@ -36,7 +36,7 @@ module FormPasswordValidator
   end
 
   def min_password_score
-    AppConfig.env.min_password_score.to_i
+    IdentityConfig.store.min_password_score
   end
 
   def i18n_variables
