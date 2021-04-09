123a9de41415c107eae6cb2f8d75254ec5bc3798
Zach Margolis <zachary.margolis@gsa.gov>
2021-04-08 17:29:22 -0700

    the gem saves the matching cert for us

--- app/controllers/concerns/saml_idp_auth_concern.rb
+++ app/controllers/concerns/saml_idp_auth_concern.rb
@@ -161,36 +161,15 @@ module SamlIdpAuthConcern
     )
   end
 
-  def matching_cert
-    return @matching_cert if defined?(@matching_cert)
-
-    @matching_cert = current_service_provider.ssl_certs.last
-
-    # matching_cert = current_service_provider.ssl_certs.find do |ssl_cert|
-    #   fingerprint = Fingerprinter.fingerprint_cert(ssl_cert)
-
-    #   saml_request = SamlIdp::Request.from_deflated_request(
-    #     params[:SAMLRequest],
-    #     get_params: params,
-    #     cert: ssl_cert,
-    #   )
-    #   if saml_request&.service_provider
-    #     # Plumb the fingerprint through to the internal service_provider representation
-    #     saml_request.service_provider.fingerprint = fingerprint
-    #     saml_request.valid_signature?
-    #   end
-    # end
-
-    # @matching_cert = matching_cert || current_service_provider.ssl_certs.first
-  end
-
   def encryption_opts
     query_params = UriService.params(request.original_url)
     if query_params[:skip_encryption].present? && current_service_provider.skip_encryption_allowed
       nil
     elsif current_service_provider.encrypt_responses?
+      cert = saml_request.service_provider.matching_cert || current_service_provider.ssl_certs.first
+
       {
-        cert: matching_cert,
+        cert: cert,
         block_encryption: current_service_provider.block_encryption,
         key_transport: 'rsa-oaep-mgf1p',
       }
