a6bc472a7be5e980a1baa5084839eef078c498fe
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-09 12:57:54 -0500

    convert database_pool_idp

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -65,6 +65,7 @@ class IdentityConfig
     config.add(:aws_kms_multi_region_enabled, type: :boolean)
     config.add(:aws_kms_regions, type: :json)
     config.add(:backup_code_skip_symmetric_encryption, type: :boolean)
+    config.add(:database_pool_idp, type: :integer)
     config.add(:deleted_user_accounts_report_configs, type: :json)
     config.add(:disable_email_sending, type: :boolean)
     config.add(:disallow_all_web_crawlers, type: :boolean)
