fe11e96e5b82078cb6f3bb279f2675a10e6a16f6
Zach Margolis <zachary.margolis@gsa.gov>
2021-04-12 10:30:34 -0700

    Refactor tests to stub less

--- spec/services/request_password_reset_spec.rb
+++ spec/services/request_password_reset_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-describe RequestPasswordReset do
+RSpec.describe RequestPasswordReset do
   describe '#perform' do
+    let(:user) { create(:user) }
+    let(:email) { user.email_addresses.first.email }
+
     context 'when the user is not found' do
       it 'sends the account registration email' do
         email = 'nonexistent@example.com'
@@ -27,50 +30,48 @@ describe RequestPasswordReset do
 
     context 'when the user is found and confirmed' do
       it 'sends password reset instructions' do
-        user = create(:user)
-        email_address = user.email_addresses.first
-        email = email_address.email
+        allow(UserMailer).to receive(:reset_password_instructions).
+          and_wrap_original do |impl, user, email, options|
+            token = options.fetch(:token)
+            expect(token).to be_present
+            expect(Devise.token_generator.digest(User, :reset_password_token, token)).
+              to eq(user.reset_password_token)
 
-        allow(EmailAddress).to receive(:find_with_email).with(email).and_return(email_address)
+            impl.call(user, email, **options)
+          end
 
-        expect(user).to receive(:set_reset_password_token).and_return('asdf1234')
-
-        mail = double
-        expect(mail).to receive(:deliver_now)
-        expect(UserMailer).to receive(:reset_password_instructions).
-          with(user, email, token: 'asdf1234').
-          and_return(mail)
-
-        RequestPasswordReset.new(email: email).perform
+        expect { RequestPasswordReset.new(email: email).perform }.
+          to(change { user.reload.reset_password_token })
       end
     end
 
     context 'when the user is found, not privileged, and not yet confirmed' do
       it 'sends password reset instructions' do
-        user = create(:user, :unconfirmed)
-        email_address = user.email_addresses.first
-        email = email_address.email
+        allow(UserMailer).to receive(:reset_password_instructions).
+          and_wrap_original do |impl, user, email, options|
+            token = options.fetch(:token)
+            expect(token).to be_present
+            expect(Devise.token_generator.digest(User, :reset_password_token, token)).
+              to eq(user.reset_password_token)
 
-        allow(EmailAddress).to receive(:find_with_email).with(email).and_return(email_address)
+            impl.call(user, email, **options)
+          end
 
-        expect(user).to receive(:set_reset_password_token).and_return('asdf1234')
-
-        mail = double
-        expect(mail).to receive(:deliver_now)
-        expect(UserMailer).to receive(:reset_password_instructions).
-          with(user, email, token: 'asdf1234').
-          and_return(mail)
-
-        RequestPasswordReset.new(email: email).perform
+        expect { RequestPasswordReset.new(email: email).perform }.
+          to(change { user.reload.reset_password_token })
       end
     end
 
     context 'when the user is found and confirmed, but the email address is not' do
-      it 'sends the account registration email' do
-        user = create(:user, :with_multiple_emails)
-        unconfirmed_email_address = user.reload.email_addresses.last
-        unconfirmed_email_address.update!(confirmed_at: nil)
+      let(:user) { create(:user, :with_multiple_emails) }
 
+      let(:unconfirmed_email_address) do
+        user.reload.email_addresses.last.tap do |email_address|
+          email_address.update!(confirmed_at: nil)
+        end
+      end
+
+      it 'sends the account registration email' do
         send_sign_up_email_confirmation = instance_double(SendSignUpEmailConfirmation)
         expect(send_sign_up_email_confirmation).to receive(:call).with(
           hash_including(
@@ -112,19 +113,17 @@ describe RequestPasswordReset do
 
     context 'when the user requests password resets above the allowable threshold' do
       it 'throttles the email sending and logs a throttle event' do
-        user = create(:user)
-        email_address = user.email_addresses.first
-        email = email_address.email
         max_attempts = AppConfig.env.reset_password_email_max_attempts.to_i
 
-        allow(EmailAddress).to receive(:find_with_email).with(email).and_return(email_address)
-        expect(user).to receive(:set_reset_password_token).and_return('asdf1234').
-          exactly(max_attempts).times
-
         analytics = FakeAnalytics.new
-        (max_attempts + 1).times do
-          RequestPasswordReset.new(email: email, analytics: analytics).perform
+        max_attempts.times do
+          expect { RequestPasswordReset.new(email: email, analytics: analytics).perform }.
+            to(change { user.reload.reset_password_token })
         end
+
+        # extra time, throttled
+        expect { RequestPasswordReset.new(email: email, analytics: analytics).perform }.
+          to_not(change { user.reload.reset_password_token })
 
         expect(analytics).to have_logged_event(
           Analytics::THROTTLER_RATE_LIMIT_TRIGGERED,
