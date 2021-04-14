284a48c61103491503f6f3b44b3a5e526dbb6f08
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-14 15:08:12 -0400

    Show step indicator on "Come Back Later" screen

--- spec/views/idv/come_back_later/show.html.erb_spec.rb
+++ spec/views/idv/come_back_later/show.html.erb_spec.rb
@@ -50,4 +50,31 @@ describe 'idv/come_back_later/show.html.erb' do
       )
     end
   end
+
+  context 'ial2 step indicator enabled' do
+    it 'shows step indicator with pending status' do
+      render
+
+      expect(view.content_for(:pre_flash_content)).to have_css(
+        '.step-indicator__step--current',
+        text: t('step_indicator.flows.idv.secure_account'),
+      )
+      expect(view.content_for(:pre_flash_content)).to have_css(
+        '.step-indicator__step--pending',
+        text: t('step_indicator.flows.idv.verify_phone_or_address'),
+      )
+    end
+  end
+
+  context 'ial2 step indicator disabled' do
+    before do
+      allow(IdentityConfig.store).to receive(:ial2_step_indicator_enabled).and_return(false)
+    end
+
+    it 'does not show step indicator' do
+      render
+
+      expect(view.content_for(:pre_flash_content)).not_to have_css('.step-indicator')
+    end
+  end
 end
