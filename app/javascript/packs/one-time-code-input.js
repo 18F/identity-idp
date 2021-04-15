75eb85a15a589dbc32f8c2e7ace70dc225449882
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-15 10:09:19 -0400

    Type-cast OTP input element

--- app/javascript/packs/one-time-code-input.js
+++ app/javascript/packs/one-time-code-input.js
@@ -2,5 +2,5 @@ import OneTimeCodeInput from '@18f/identity-one-time-code-input';
 
 const input = document.querySelector('.one-time-code-input');
 if (input) {
-  new OneTimeCodeInput(input).bind();
+  new OneTimeCodeInput(/** @type {HTMLInputElement} */ (input)).bind();
 }
