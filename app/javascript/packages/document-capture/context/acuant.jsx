ebd2803ca671216e2a56847751ef75f3de865a30
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-19 11:20:48 -0400

    LG-4392: Upgrade Acuant SDK from v11.4.3 to v11.4.4
    **Why**: As a user, I expect that login.gov keeps their vendor dependencies up-to-date, so that bug fixes that may improve my likelihood of proofing success have been incorporated.

--- app/javascript/packages/document-capture/context/acuant.jsx
+++ app/javascript/packages/document-capture/context/acuant.jsx
@@ -71,7 +71,7 @@ const AcuantContext = createContext({
  * @param {AcuantContextProviderProps} props Props object.
  */
 function AcuantContextProvider({
-  sdkSrc = '/acuant/11.4.3/AcuantJavascriptWebSdk.min.js',
+  sdkSrc = '/acuant/11.4.4/AcuantJavascriptWebSdk.min.js',
   credentials = null,
   endpoint = null,
   children,
