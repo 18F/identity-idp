0c10c8f8a54fe190c5e0c522056dc4f7c8eeffbc
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-20 16:11:36 -0500

    do not query for user if it is already present

--- spec/controllers/concerns/effective_user_spec.rb
+++ spec/controllers/concerns/effective_user_spec.rb
@@ -7,46 +7,6 @@ RSpec.describe EffectiveUser, type: :controller do
 
   let(:user) { create(:user) }
 
-  describe '#effective_user_id' do
-    subject { controller.effective_user_id }
-
-    context 'logged out' do
-      it 'returns nil' do
-        expect(subject).to be_nil
-      end
-    end
-
-    context 'logged out with ial2 recovery session user id' do
-      before do
-        session[:ial2_recovery_user_id] = user.id
-      end
-
-      it 'returns session user id' do
-        expect(subject).to eq user.id
-      end
-    end
-
-    context 'logged out with doc capture session user id' do
-      before do
-        session[:doc_capture_user_id] = user.id
-      end
-
-      it 'returns session user id' do
-        expect(subject).to eq user.id
-      end
-    end
-
-    context 'logged in' do
-      before do
-        stub_sign_in user
-      end
-
-      it 'returns session user id' do
-        expect(subject).to eq user.id
-      end
-    end
-  end
-
   describe '#effective_user' do
     subject { controller.effective_user }
 
