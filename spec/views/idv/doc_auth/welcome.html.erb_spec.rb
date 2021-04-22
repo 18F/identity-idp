9c4b7bbdf5a672c77aa8c4f299ab4e5d682f10da
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-22 14:36:58 -0400

    LG-4370: Move IAL2 ID requirements troubleshooting to first step (#4946)
    * Add custom focus style for block link
    
    **Why**: Per design
    
    * Support adjacent block list placement within list
    
    **Why**: If multiple block links are rendered, it would often be assumed they're rendered in the context of a list
    
    * Add troubleshooting component
    
    * Move IAL2 ID requirements troubleshooting to first step
    
    * Normalize locale configs

--- spec/views/idv/doc_auth/welcome.html.erb_spec.rb
+++ spec/views/idv/doc_auth/welcome.html.erb_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 describe 'idv/doc_auth/welcome.html.erb' do
   let(:flow_session) { {} }
   let(:user_fully_authenticated) { true }
+  let(:sp_name) { nil }
 
   before do
+    @decorated_session = instance_double(ServiceProviderSessionDecorator)
+    allow(@decorated_session).to receive(:sp_name).and_return(sp_name)
+    allow(view).to receive(:decorated_session).and_return(@decorated_session)
     allow(view).to receive(:flow_session).and_return(flow_session)
     allow(view).to receive(:user_fully_authenticated?).and_return(user_fully_authenticated)
     allow(view).to receive(:url_for).and_return('https://www.example.com/')
@@ -72,6 +76,24 @@ describe 'idv/doc_auth/welcome.html.erb' do
 
       expect(rendered).to have_content('We are currently under maintenance')
       expect(rendered).to_not have_content(t('doc_auth.headings.welcome'))
+    end
+  end
+
+  context 'without service provider' do
+    it 'does not render troubleshooting component' do
+      render template: 'idv/doc_auth/welcome'
+
+      expect(rendered).not_to include(t('idv.troubleshooting.headings.missing_required_items'))
+    end
+  end
+
+  context 'with service provider' do
+    let(:sp_name) { 'Example App' }
+
+    it 'renders troubleshooting component' do
+      render template: 'idv/doc_auth/welcome'
+
+      expect(rendered).to include(t('idv.troubleshooting.headings.missing_required_items'))
     end
   end
 end
