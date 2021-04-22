e7301dd56501951272f84d9d522abbf9dd10bbd8
Andrew Duthie <andrew.duthie@gsa.gov>
2021-04-22 15:31:26 -0400

    LG-4370: Update IAL2 warning screens to use troubleshooting component
    **Why**: As an end user, if I fail one of the IAL2 verification steps, I want to clearly understand what my other options are if I keep failing this step, so that I can get help or try something else if I have already tried to verify my information and failed.

---
+++ spec/views/idv/session_errors/warning.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'idv/session_errors/warning.html.erb' do
+  let(:sp_name) { 'Example SP' }
+  let(:remaining_step_attempts) { 5 }
+
+  before do
+    decorated_session = instance_double(ServiceProviderSessionDecorator)
+    allow(decorated_session).to receive(:sp_name).and_return(sp_name)
+    allow(view).to receive(:decorated_session).and_return(decorated_session)
+
+    assign(:remaining_step_attempts, remaining_step_attempts)
+
+    render
+  end
+
+  it 'shows a primary action' do
+    expect(rendered).to have_link(t('idv.failure.button.warning'), href: idv_doc_auth_path)
+  end
+
+  it 'shows remaining attempts' do
+    expect(rendered).to have_text(t('idv.failure.attempts', count: remaining_step_attempts))
+  end
+
+  it 'renders a list of troubleshooting options' do
+    expect(rendered).to have_link(
+      t('idv.troubleshooting.options.get_help_at_sp', sp_name: sp_name),
+      href: return_to_sp_failure_to_proof_path,
+    )
+  end
+end
---
+++ spec/views/idv/shared/_warning.html.erb_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe 'idv/shared/_warning.html.erb' do
+  let(:sp_name) { nil }
+  let(:extra_options) { nil }
+  let(:heading) { 'Warning' }
+  let(:action) { nil }
+
+  before do
+    decorated_session = instance_double(ServiceProviderSessionDecorator)
+    allow(decorated_session).to receive(:sp_name).and_return(sp_name)
+    allow(view).to receive(:decorated_session).and_return(decorated_session)
+
+    render 'idv/shared/warning', heading: heading, action: action, extra_options: extra_options
+  end
+
+  it 'renders heading' do
+    expect(rendered).to have_css('h1', text: heading)
+  end
+
+  context 'without an SP' do
+    it 'renders a list of troubleshooting options' do
+      expect(rendered).not_to have_link(href: return_to_sp_cancel_path)
+    end
+  end
+
+  context 'with an SP' do
+    let(:sp_name) { 'Example SP' }
+
+    it 'renders a list of troubleshooting options' do
+      expect(rendered).to have_link(
+        t('idv.troubleshooting.options.get_help_at_sp', sp_name: sp_name),
+        href: return_to_sp_failure_to_proof_path,
+      )
+    end
+  end
+
+  describe 'action' do
+    context 'without action' do
+      it 'does not render action button' do
+        expect(rendered).not_to have_css('.usa-button')
+      end
+    end
+
+    context 'with action' do
+      let(:action) { { text: 'Example', url: '#example' } }
+
+      it 'renders action button' do
+        expect(rendered).to have_link('Example', href: '#example')
+      end
+    end
+  end
+
+  describe 'extra_options' do
+    context 'without extra options' do
+      it 'does not render troubleshooting options' do
+        expect(rendered).not_to have_css('.troubleshooting-options')
+      end
+    end
+
+    context 'with extra options' do
+      let(:extra_options) { [{ text: 'Example', url: '#example' }] }
+
+      it 'renders a list of troubleshooting options' do
+        expect(rendered).to have_link('Example', href: '#example')
+      end
+    end
+  end
+end
