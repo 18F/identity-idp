dabf79f48b55085dc83a336b06d92686b667432f
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-27 14:05:20 -0500

    ignore users unlock token column (#4988)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  self.ignored_columns = %w[x509_dn_uuid otp_secret_key totp_timestamp]
+  self.ignored_columns = %w[x509_dn_uuid otp_secret_key totp_timestamp unlock_token]
   include NonNullUuid
 
   include ::NewRelic::Agent::MethodTracer
