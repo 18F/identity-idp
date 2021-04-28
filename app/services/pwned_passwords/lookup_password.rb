3acc84ab8ddcda6701eada1dcbe4aa19b9a3791a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-04-28 15:04:33 -0400

    Convert configs for paths for download files to IdentityConfig
    **Why**: To continue the migration from AppConfig to IdentityConfig

--- app/services/pwned_passwords/lookup_password.rb
+++ app/services/pwned_passwords/lookup_password.rb
@@ -1,6 +1,6 @@
 module PwnedPasswords
   class LookupPassword
-    PWNED_PASSWORD_FILE = Rails.root.join(AppConfig.env.pwned_passwords_file_path).freeze
+    PWNED_PASSWORD_FILE = Rails.root.join(IdentityConfig.store.pwned_passwords_file_path).freeze
 
     def self.call(password)
       BinarySearchSortedHashFile.new(PWNED_PASSWORD_FILE).call(password)
