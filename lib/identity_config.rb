c8cfc61b393abc1a633c8f73d3a3d2764f92f00d
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-04-28 17:38:06 -0400

    convert piv_cac_verify_token_secret

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -190,6 +190,7 @@ class IdentityConfig
     config.add(:pinpoint_sms_configs, type: :json)
     config.add(:pinpoint_voice_configs, type: :json)
     config.add(:piv_cac_service_url)
+    config.add(:piv_cac_verify_token_secret)
     config.add(:piv_cac_verify_token_url)
     config.add(:phone_setups_per_ip_track_only_mode, type: :boolean)
     config.add(:poll_rate_for_verify_in_seconds, type: :integer)
