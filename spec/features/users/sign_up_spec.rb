3944c61649b78a54d010c1fa9c987ff6c4cdc5cc
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-04-28 23:12:50 -0500

    check t('account.terms')

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -39,7 +39,7 @@ feature 'Sign Up' do
 
     it 'allows a user to pick a language when entering email' do
       visit sign_up_email_path
-      find('#user_terms_accepted').set(true)
+      check t('account.terms')
       fill_in t('forms.registration.labels.email'), with: email
       choose 'Español'
       click_button t('forms.buttons.submit.default')
