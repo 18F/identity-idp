b0eb2b228a379d51f5ad5fc670d649c08387334d
Zach Margolis <zachary.margolis@gsa.gov>
2021-04-30 12:08:47 -0700

    forgot to hit save

--- config/application.rb
+++ config/application.rb
@@ -23,23 +23,98 @@ module Upaya
     )
     IdentityConfig.build_store(configuration)
 
+    yaml_config = Identity::Hostdata::ConfigReader.new(app_root: Rails.root).read_configuration(
+      Rails.env, write_copy_to: Rails.root.join('tmp', 'application.yml')
+    )
+    ssm_config = Identity::Hostdata::SsmReader()
+    env_config = ENV.take('my_keys')
+
+    IdentityConfig.new([:yaml, :ssm, :env], {yaml: yaml_config, ssm: ssm_config, env: env_config})
+    keys = [:yaml, :ssm, :env]
+    map_of_configs = {yaml: yaml_config, ssm: ssm_config, env: env_config}
+    keys.reduce do |config, key|
+      config.merge(map_of_configs[key])
+      validate!(config)
+      config
+    end    
+
     Identity::Hostdata.setup do |config|
       # application.yml
       #   load the default frmo source control
       #   load the one from S3
       #   parse em into a DSL
 
-      config.merge_ymls(
-        Rails.root.join('default.yml'),
+      def merge_ymls(**args)
+        
+      end
+
+      combined = (
+        YAML.load(Rails.root.join('default.yml')),
+        { s3_config: '' }
         if in_datacenter?
           config.app_secrets_s3('/%<env>/idp/application.yml')
         else
           Rails.root.join('override.yml')
         end,
         ENV,
-      ).define do |config|
+      ).define(ssm_prefix: '/idp/foo/bar') do |config|
          # set up ***all*** the keys 
         add(:aal_authn_context_enabled, type: :boolean)
+        ## ssm happens inside here
+      end
+
+      define_keys do |config|
+        # set up ***all*** the keys 
+        add(:aal_authn_context_enabled, type: :boolean
+
+        add(:aamva_cert_enabled, type: :boolean, :error_on_default_in_prod)
+        add(:aamva_private_key, type: :string)
+        add(:aamva_public_key, type: :string)
+        add(:aamva_sp_banlist_issuers, type: :json)
+        add(:aamva_verification_request_timeout, type: :integer)
+      end.load_from(
+        EnvSource(ENV), # most important
+        config.ssm_source, # second most important
+        config.yaml_config('/env/ymal'),
+        config.local_yaml_source('application.yml') # least/default
+      )
+
+      class SecretSource
+        def source_name
+        end
+
+        def has_key?()
+        end
+
+        def get
+        end
+      end
+    
+      # use cases we want to guard again
+      ### I put a bad value in application.yml, but it's masked by a good value in ENV
+      ### Optionally error on default value in prod (unsafe default)
+      ### validations on the type?
+      #### ex: JSON but it's not JSON
+      #### make sure they're not nil, flag to ignore that
+      ## collect all the errors, don't blow up on the first one
+
+      def load_from(*sources)
+        @errors = Hash.new { |k, v| k[v] = [] }
+        @errors[:foo] << "some thing is wrongggg"
+
+
+        config_defs.each do |key, type, etc|
+          sources.each do |source|
+            # check for error_on_default_in_prod
+            if source.has_key?(key)
+              set_value(source.load_key(key))
+              save_source
+              break
+            end
+          end
+          errors << error
+          # oh no the key is not defined anywhere, time to load an error
+        end
       end
 
       # download arbirary secrets & expose them as properties
@@ -52,6 +127,28 @@ module Upaya
     Identity::Hostdata.store.aal_authn_context_enabled # its a boolean
     Identity::Hostdata.store.saml_2020_key # its a blarb
 
+    # example DSL
+
+    Identity::Hostdata.store.setup do |store|
+      store.define(:aamva_cert_enabled, type: :boolean, :error_on_default_in_prod)
+      store.define(:aamva_private_key, type: :string)
+      store.define(:saml_2020_key, artifact_path: '/%<env>s/saml2020.key.enc')
+
+      store.load_from(
+        env: Rails.env,
+        sources: [ 
+          store.local_yaml_source(Rails.root.join('config/application.yml.default')),
+          store.s3_yaml_source('/<env>/v1/idp/application.yml')), # no-op if nil
+          store.ssm_source, # no-op in local
+          store.s3_bucket1, # no-op in local
+          store.environment_source(ENV), # last one wins
+        ]
+      )
+    end
+
+    Identity::Hostdata.store.aal_authn_context_enabled # its a boolean
+
+    # end example
 
 
     config.load_defaults '6.1'
