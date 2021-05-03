6369b7e9dc728612f9340ab9763e13b494cf138c
Andrew Duthie <andrew.duthie@gsa.gov>
2021-05-03 08:24:52 -0400

    LG-4370: Update throttled error heading to account for liveness requirement (#5015)
    * LG-4370: Update throttled error heading to account for liveness requirement
    
    **Why**: If the verification does not require liveness, it is confusing to the user if we tell them we cannot verify the "photo of you", since they would not be prompted to capture a photo. We should only show this text if liveness is required.
    
    * normalize yaml

--- spec/views/idv/session_errors/throttled.html.erb_spec.rb
+++ spec/views/idv/session_errors/throttled.html.erb_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 describe 'idv/session_errors/throttled.html.erb' do
   let(:sp_name) { nil }
+  let(:liveness_checking_enabled) { false }
+  let(:sp_session) { {} }
 
   before do
     decorated_session = instance_double(ServiceProviderSessionDecorator, sp_name: sp_name)
     allow(view).to receive(:decorated_session).and_return(decorated_session)
+    allow(FeatureManagement).to receive(:liveness_checking_enabled?).
+      and_return(liveness_checking_enabled)
+    allow(view).to receive(:sp_session).and_return(sp_session)
 
     render
   end
@@ -32,6 +37,34 @@ describe 'idv/session_errors/throttled.html.erb' do
         t('idv.troubleshooting.options.get_help_at_sp', sp_name: sp_name),
         href: return_to_sp_failure_to_proof_path,
       )
+    end
+  end
+
+  context 'with liveness feature disabled' do
+    let(:liveness_checking_enabled) { false }
+
+    it 'renders expected heading' do
+      expect(rendered).to have_text(t('errors.doc_auth.throttled_heading'))
+    end
+  end
+
+  context 'with liveness feature enabled' do
+    let(:liveness_checking_enabled) { true }
+
+    context 'without strict ial2' do
+      let(:sp_session) { {} }
+
+      it 'renders expected heading' do
+        expect(rendered).to have_text(t('errors.doc_auth.throttled_heading'))
+      end
+    end
+
+    context 'with strict ial2' do
+      let(:sp_session) { { ial2_strict: true } }
+
+      it 'renders expected heading' do
+        expect(rendered).to have_text(t('errors.doc_auth.throttled_heading_liveness'))
+      end
     end
   end
 end
