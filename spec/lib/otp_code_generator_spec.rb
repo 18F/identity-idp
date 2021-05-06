145455bf8bf1f6f5b4d73163e7d12f9403924681
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-05-06 09:54:45 -0700

    Cleanup: remove otp_use_alphanumeric feature flag (#5032)

--- spec/lib/otp_code_generator_spec.rb
+++ spec/lib/otp_code_generator_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe OtpCodeGenerator do
   describe '.generate_digits' do
-    before do
-      allow(IdentityConfig.store).to receive(:otp_use_alphanumeric).and_return(otp_use_alphanumeric)
-    end
-    let(:otp_use_alphanumeric) { true }
-
     subject(:generate_digits) do
       OtpCodeGenerator.generate_digits(TwoFactorAuthenticatable::DIRECT_OTP_LENGTH)
     end
@@ -31,20 +26,6 @@ RSpec.describe OtpCodeGenerator do
       expect(SecureRandom).to receive(:random_number).and_return(0)
 
       expect(generate_digits).to eq('0' * TwoFactorAuthenticatable::DIRECT_OTP_LENGTH)
-    end
-
-    context 'wehn otp_use_alphanumeric is disabled' do
-      let(:otp_use_alphanumeric) { false }
-
-
-      it 'is a digits-only code' do
-        int_value = Base32::Crockford.decode('ABC')
-
-        expect(SecureRandom).to receive(:random_number).
-          and_return(int_value)
-
-        expect(generate_digits).to eq("0#{int_value}")
-      end
     end
   end
 end
