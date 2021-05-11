445c4ad6c45bbcc8a270eeacaf0b97a619b5c817
Andrew Duthie <andrew.duthie@gsa.gov>
2021-05-11 10:03:49 -0400

    LG-4398: Support ActiveModel::Errors instance for FormResponse
    **Why**: An ActiveModel::Errors instance can represent both the "message" and "detail" of an error, where the message is the translated, human-readable text shown to the user, and the detail is typically the locale-independent, machine-readable unique identifier of a particular error. By supporting an ActiveModel::Errors instance in FormResponse, we can retain current behaviors of showing and logging a human-readable error message, while also allowing us to identify and group instances of an error across locales in a CloudWatch query.
    
    With few exceptions, this also aligns to how we most often use FormResponse, where it's initialized using errors from an instance of ActiveModel::Errors. The difference is a simplification to pass the errors instance itself, rather than the result of its "messages" method (i.e. `errors: errors` instead of `errors: errors.messages`).
    
    It may be possible to consolidate to avoid overloading and instead supporting _only_ the ActiveModel::Errors instance in this class, but (a) there are a handful of existing cases where we create hashes ad-hoc for logging and (b) in those cases, the ActiveModel::Errors equivalent implementation is relatively clunky.

--- app/services/form_response.rb
+++ app/services/form_response.rb
@@ -1,8 +1,11 @@
 class FormResponse
-  def initialize(success:, errors:, extra: {})
+  def initialize(success:, errors: {}, extra: {})
     @success = success
-    @errors = errors.to_hash
+    @errors = errors.is_a?(ActiveModel::Errors) ? errors.messages.to_hash : errors
     @extra = extra
+    @extra.merge!(
+      error_details: flatten_details(errors.details),
+    ) if errors.is_a?(ActiveModel::Errors) && errors.details.present?
   end
 
   attr_reader :errors, :extra
@@ -18,8 +21,14 @@ class FormResponse
   def merge(other)
     FormResponse.new(
       success: success? && other.success?,
-      errors: errors.merge(other.errors),
-      extra: extra.merge(other.extra),
+      errors: errors.merge(other.errors, &method(:merge_arrays)),
+      extra: extra.merge(other.extra) do |key, first, second|
+        if key == :error_details
+          first.merge(second, &method(:merge_arrays))
+        else
+          second
+        end
+      end,
     )
   end
 
@@ -36,6 +45,14 @@ class FormResponse
   end
 
   private
+
+  def merge_arrays(_key, first, second)
+    Array(first) + Array(second)
+  end
+
+  def flatten_details(details)
+    details.to_hash.transform_values { |errors| errors.pluck(:error) }
+  end
 
   attr_reader :success
 end
