d1f420adfa4945fd18eba0f46a7741e9aa7813af
Oren Kanner <oren.kanner@gsa.gov>
2021-05-11 13:34:33 -0400

    Remove duplicate validations in the ServiceProvider model (#5039)
    Resolves LG-4095
    
    There were also duplicate specs here that felt unnecessary if we're
    relying on the gem for validations so we moved them to the gem.

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -56,21 +56,4 @@ class ServiceProvider < ApplicationRecord
     return OpenSSL::X509::Certificate.new(cert) unless File.exist?(cert_file)
     File.read(cert_file)
   end
-
-  def redirect_uris_are_parsable
-    return if redirect_uris.blank?
-
-    redirect_uris.each do |uri|
-      next if redirect_uri_valid?(uri)
-      errors.add(:redirect_uris, :invalid)
-      break
-    end
-  end
-
-  def redirect_uri_valid?(redirect_uri)
-    parsed_uri = URI.parse(redirect_uri)
-    parsed_uri.scheme.present? && parsed_uri.host.present?
-  rescue URI::BadURIError, URI::InvalidURIError
-    false
-  end
 end
