ad64441c9b131602e326196f2b95b39d7114a212
Andrew Duthie <andrew.duthie@gsa.gov>
2021-05-20 08:32:50 -0400

    LG-4254: Fix SVG scaling in Internet Explorer (#5076)
    * Restore viewBox attribute
    
    * Disable removeViewBox plugin in SVGO optimization
    
    **Why**: IE11 compatibility
    
    * Add test case to ensure viewBox attribute on root element of SVG

--- spec/svg_spec.rb
+++ spec/svg_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'SVG files' do
-  Dir[Rails.root.join('**', '*.svg')].reject { |f| f.include?('node_modules') }.each do |svg_path|
-    relative_path = svg_path.sub(Rails.root.to_s, '')
-
-    next if %w[vendor node_modules].include?(relative_path.split('/')[1])
-
+  files = Dir.glob('{app,public}/**/*.svg', base: Rails.root)
+  files.reject! { |f| f.start_with? 'public/assets/' }
+  files.each do |relative_path|
     describe relative_path do
-      it 'does not contain inline style tags (that render poorly in IE due to CSP)' do
-        doc = Nokogiri::XML(File.read(svg_path))
+      let(:subject) { Nokogiri::XML(File.read(Rails.root.join(relative_path))) }
 
-        expect(doc.css('style')).to be_empty.or(
+      it 'does not contain inline style tags (that render poorly in IE due to CSP)' do
+        expect(subject.css('style')).to be_empty.or(
           have_attributes(text: match(%r{^\s*/\*\!lint-ignore\*/})),
         )
+      end
+
+      it 'defines viewBox attribute on root svg tag' do
+        expect(subject.css(':root[viewBox]')).to be_present
       end
     end
   end
