729e5c41d5fb9f667a170b0f8a0a9b484ab7bf08
Zach Margolis <zachary.margolis@gsa.gov>
2021-05-20 08:06:33 -0700

    Query optimization

--- app/services/db/monthly_sp_auth_count/total_monthly_auth_counts_within_iaa_window.rb
+++ app/services/db/monthly_sp_auth_count/total_monthly_auth_counts_within_iaa_window.rb
@@ -25,84 +25,75 @@ module Db
         # The subqueries create a uniform representation of data:
         # - full months from monthly_sp_auth_counts
         # - partial months by aggregating sp_return_logs
-        # The results are rows with [user_id, ial, auth_count, year_month]
-        subquery = [
-          full_month_subquery(issuer: issuer, full_months: full_months),
-          *partial_month_subqueries(issuer: issuer, partial_months: partial_months),
+        # The results are rows with [ial, auth_count, year_month, issuer, iaa, iaa start, iaa end]
+        union_query = [
+          full_month_subquery(sp: service_provider, full_months: full_months),
+          *partial_month_subqueries(sp: service_provider, partial_months: partial_months),
         ].join(' UNION ALL ')
 
-        params = {
-          iaa: quote(service_provider.iaa),
-          iaa_start_date: quote(iaa_range.begin),
-          iaa_end_date: quote(iaa_range.end),
-          issuer: quote(issuer),
-          subquery: subquery,
-        }
-
-        sql = format(<<~SQL, params)
-          SELECT
-            billing_month_logs.year_month
-          , billing_month_logs.ial
-          , %{issuer} AS issuer
-          , %{iaa} AS iaa
-          , %{iaa_start_date} AS iaa_start_date
-          , %{iaa_end_date} AS iaa_end_date
-          , SUM(billing_month_logs.auth_count)::bigint AS total_auth_count
-          FROM (%{subquery}) billing_month_logs
-          GROUP BY
-            billing_month_logs.year_month
-          , billing_month_logs.ial
-        SQL
-
-        ActiveRecord::Base.connection.execute(sql)
+        ActiveRecord::Base.connection.execute(union_query)
       end
 
 
       # @return [String]
-      def full_month_subquery(issuer:, full_months:)
+      def full_month_subquery(sp:, full_months:)
         params = {
-          issuer: issuer,
+          iaa: sp.iaa,
+          issuer: sp.issuer,
+          iaa_start_date: sp.iaa_start_date,
+          iaa_end_date: sp.iaa_end_date,
           year_months: full_months.map { |r| r.begin.strftime('%Y%m') },
         }.transform_values { |value| quote(value) }
 
         full_month_subquery = format(<<~SQL, params)
           SELECT
-            monthly_sp_auth_counts.user_id
-          , monthly_sp_auth_counts.year_month
-          , monthly_sp_auth_counts.auth_count
+            monthly_sp_auth_counts.year_month
           , monthly_sp_auth_counts.ial
+          , SUM(monthly_sp_auth_counts.auth_count)::bigint AS total_auth_count
+          , %{issuer} AS issuer
+          , %{iaa} AS iaa
+          , %{iaa_start_date} AS iaa_start_date
+          , %{iaa_end_date} AS iaa_end_date
           FROM
             monthly_sp_auth_counts
           WHERE
                 monthly_sp_auth_counts.issuer = %{issuer}
             AND monthly_sp_auth_counts.year_month IN %{year_months}
+          GROUP BY
+            monthly_sp_auth_counts.year_month
+          , monthly_sp_auth_counts.ial
         SQL
       end
 
       # @return [Array<String>]
-      def partial_month_subqueries(issuer:, partial_months:)
+      def partial_month_subqueries(sp:, partial_months:)
         partial_months.map do |month_range|
           params = {
+            iaa: sp.iaa,
+            issuer: sp.issuer,
+            iaa_start_date: sp.iaa_start_date,
+            iaa_end_date: sp.iaa_end_date,
             range_start: month_range.begin,
             range_end: month_range.end,
             year_month: month_range.begin.strftime('%Y%m'),
-            issuer: issuer,
           }.transform_values { |value| quote(value) }
 
           format(<<~SQL, params)
             SELECT
-              sp_return_logs.user_id
-            , %{year_month} AS year_month
-            , COUNT(sp_return_logs.id) AS auth_count
+              %{year_month} AS year_month
             , sp_return_logs.ial
+            , COUNT(sp_return_logs.id) AS auth_count
+            , %{issuer} AS issuer
+            , %{iaa} AS iaa
+            , %{iaa_start_date} AS iaa_start_date
+            , %{iaa_end_date} AS iaa_end_date
             FROM sp_return_logs
             WHERE
                   sp_return_logs.requested_at BETWEEN %{range_start} AND %{range_end}
               AND sp_return_logs.returned_at IS NOT NULL
               AND sp_return_logs.issuer = %{issuer}
             GROUP BY
-              sp_return_logs.user_id
-            , sp_return_logs.ial
+              sp_return_logs.ial
           SQL
         end
       end
