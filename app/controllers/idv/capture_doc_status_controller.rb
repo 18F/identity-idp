bcc5d8554ffaec0e6ead05fd94322d50aab51d35
Andrew Duthie <andrew.duthie@gsa.gov>
2021-05-26 15:20:28 -0400

    Refactor CaptureDocStatusController
    See: https://github.com/18F/identity-idp/pull/5093#discussion_r639792798
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>

--- app/controllers/idv/capture_doc_status_controller.rb
+++ app/controllers/idv/capture_doc_status_controller.rb
@@ -5,29 +5,42 @@ module Idv
     respond_to :json
 
     def show
-      render document_capture_session_poll_render_result
+      render(
+        json: {
+          redirect: status == :too_many_requests ? idv_session_errors_throttled_url : nil,
+        }.compact,
+        status: status,
+      )
     end
 
     private
 
-    def document_capture_session_poll_render_result
-      return { json: nil, status: :unauthorized } if !flow_session || !document_capture_session
-      return { json: nil, status: :gone } if document_capture_session.cancelled_at
-      return {
-        json: { redirect: idv_session_errors_throttled_url },
-        status: :too_many_requests,
-      } if throttled?
-
-      result = document_capture_session.load_result ||
-               document_capture_session.load_doc_auth_async_result
-
-      return { json: nil, status: :accepted } if result.blank?
-      return { json: nil, status: :unauthorized } unless result.success?
-      { json: nil, status: :ok }
+    def status
+      @status ||= begin
+        if !flow_session || !document_capture_session
+          :unauthorized
+        elsif document_capture_session.cancelled_at
+          :gone
+        elsif throttled?
+          :too_many_requests
+        elsif session_result.blank?
+          :accepted
+        elsif !session_result.success?
+          :unauthorized
+        else
+          :ok
+        end
+      end
     end
 
     def flow_session
       user_session['idv/doc_auth']
+    end
+
+    def session_result
+      return @session_result if defined?(@session_result)
+      @session_result = document_capture_session.load_result ||
+                        document_capture_session.load_doc_auth_async_result
     end
 
     def document_capture_session
