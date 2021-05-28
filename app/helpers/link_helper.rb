a8dbc83a0894adbd7a90db3cb97477d5be368c4d
Andrew Duthie <andrew.duthie@gsa.gov>
2021-05-28 10:33:00 -0400

    Add button_or_link_to view helper
    **Why**: So that a reusable view can easily work with both GET and non-GET link/buttons without requiring UJS

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -31,4 +31,13 @@ module LinkHelper
       end
     end
   end
+
+  def button_or_link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options, options, name = options, name, block if block_given?
+    html_options ||= {}
+    method = html_options[:method] || :get
+    helper_method = method == :get ? :link_to : :button_to
+    html_options.delete(:method) if helper_method == :link_to
+    send(helper_method, name, options, html_options, &block)
+  end
 end
