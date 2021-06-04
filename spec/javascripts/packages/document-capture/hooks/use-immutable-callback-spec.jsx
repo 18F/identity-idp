2055051f870b5e5481884e9c263faaec4ff0774f
Andrew Duthie <andrew.duthie@gsa.gov>
2021-06-04 11:05:44 -0400

    Add useImmutableCallback hook
    **Why**: Future use to simplify useFocusTrap behavior and accept all options of createFocusTrap

---
+++ spec/javascripts/packages/document-capture/hooks/use-immutable-callback-spec.jsx
@@ -0,0 +1,31 @@
+import sinon from 'sinon';
+import { renderHook } from '@testing-library/react-hooks';
+import useImmutableCallback from '@18f/identity-document-capture/hooks/use-immutable-callback';
+
+describe('document-capture/hooks/use-immutable-callback', () => {
+  const callback1 = () => {};
+  const callback2 = sinon.stub().callsFake(() => {});
+
+  it('maintains a consistent reference', () => {
+    const { rerender, result } = renderHook(({ fn }) => useImmutableCallback(fn), {
+      initialProps: { fn: callback1 },
+    });
+
+    const { current: renderedCallback1 } = result;
+    rerender({ fn: callback2 });
+    const { current: renderedCallback2 } = result;
+
+    expect(renderedCallback1).to.equal(renderedCallback2);
+  });
+
+  it('invokes the latest reference of callback', () => {
+    const { rerender, result } = renderHook(({ fn }) => useImmutableCallback(fn), {
+      initialProps: { fn: callback1 },
+    });
+
+    rerender({ fn: callback2 });
+    result.current();
+
+    expect(callback2).to.have.been.called();
+  });
+});
