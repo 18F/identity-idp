4cd98b8b7791cf8c03482b27319975799d8a4000
Doug Price <douglas.price@gsa.gov>
2021-06-11 16:47:28 -0400

    LG-4606: re-incorporate lexisnexis gem into idp (#5136)
    * LG-4606: re-incorporate lexisnexis gem into idp
    
    * rubocop
    
    * remove Rails/non-Rails contexts in spec
    
    * remove unnecessary requires

---
+++ app/services/proofing/lexis_nexis/request.rb
@@ -0,0 +1,103 @@
+module Proofing
+  module LexisNexis
+    class RequestError < StandardError; end
+
+    class Request
+      attr_reader :config, :applicant, :url, :headers, :body
+
+      def initialize(config:, applicant:)
+        @config = config
+        @applicant = applicant
+        @body = build_request_body
+        @headers = build_request_headers
+        @url = build_request_url
+      end
+
+      # @see Response#initialize
+      # @param [Hash<Symbol, Object>] response_options a hash of options for the Response class
+      #   * +:dob_year_only+
+      def send(response_options: {})
+        conn = Faraday.new do |f|
+          f.request :instrumentation, name: 'request_metric.faraday'
+          f.options.timeout = timeout
+          f.options.read_timeout = timeout
+          f.options.open_timeout = timeout
+          f.options.write_timeout = timeout
+
+          f.basic_auth config.username, config.password
+        end
+
+        Response.new(
+          conn.post(url, body, headers) do |req|
+            req.options.context = { service_name: metric_name }
+          end,
+          **response_options,
+        )
+      rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
+        # NOTE: This is only for when Faraday is using NET::HTTP if the adapter is changed
+        # this will have to change to handle timeouts
+        if e.message == 'execution expired'
+          raise ::Proofer::TimeoutError,
+                'LexisNexis timed out waiting for verification response'
+        else
+          message = "Lexis Nexis request raised #{e.class} with the message: #{e.message}"
+          raise LexisNexis::RequestError, message
+        end
+      end
+
+      private
+
+      def account_number
+        config.account_id
+      end
+
+      def base_url
+        config.base_url
+      end
+
+      def build_request_headers
+        {
+          'Content-Type' => 'application/json',
+        }
+      end
+
+      def build_request_body
+        raise NotImplementedError, "#{__method__} should be defined by a subclass"
+      end
+
+      def build_request_url
+        URI.join(
+          base_url,
+          url_request_path,
+        ).to_s
+      end
+
+      def mode
+        config.request_mode
+      end
+
+      def url_request_path
+        "/restws/identity/v2/#{account_number}/#{workflow_name}/conversation"
+      end
+
+      def uuid
+        uuid = applicant.fetch(:uuid, SecureRandom.uuid)
+        uuid_prefix = applicant[:uuid_prefix]
+
+        if uuid_prefix.present?
+          "#{uuid_prefix}:#{uuid}"
+        else
+          uuid
+        end
+      end
+
+      def timeout
+        (config.request_timeout || 5).to_i
+      end
+
+      def metric_name
+        raise NotImplementedError
+      end
+    end
+  end
+end
---
+++ spec/services/proofing/lexis_nexis/phone_finder/proofer_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe Proofing::LexisNexis::PhoneFinder::Proofer do
+  let(:applicant) do
+    {
+      uuid_prefix: '0987',
+      uuid: '1234-abcd',
+      first_name: 'Testy',
+      last_name: 'McTesterson',
+      ssn: '123-45-6789',
+      dob: '01/01/1980',
+      phone: '5551231234',
+    }
+  end
+  let(:verification_request) do
+    Proofing::LexisNexis::PhoneFinder::VerificationRequest.new(
+      applicant: applicant,
+      config: LexisNexisFixtures.example_config,
+    )
+  end
+
+  it_behaves_like 'a lexisnexis proofer'
+
+  subject(:instance) do
+    Proofing::LexisNexis::PhoneFinder::Proofer.new(**LexisNexisFixtures.example_config.to_h)
+  end
+
+  describe '#proof' do
+    subject(:result) { instance.proof(applicant) }
+
+    before do
+      stub_request(:post, verification_request.url).
+        to_return(body: response_body, status: 200)
+    end
+
+    context 'when the response is a failure' do
+      let(:response_body) do
+        LexisNexisFixtures.instant_verify_date_of_birth_full_fail_response_json
+      end
+
+      it 'is a failure result' do
+        expect(result.success?).to eq(false)
+        expect(result.errors).to include(
+          base: include(a_kind_of(String)),
+          'Execute Instant Verify': include(a_kind_of(Hash)),
+        )
+      end
+    end
+  end
+end
