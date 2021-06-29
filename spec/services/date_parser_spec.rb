b8dd0f705fa7461d5a750a5b658a9a2ef59c61bd
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-06-29 13:06:04 -0700

    Add more parsing of american-style dates (#5183)
    * Based on errors observed in NewRelic in INT
    
    * Add better test cases that test more date parsing
    
    * Update DateParser.parse_legacy to pass through dates
    **Why**: matches str.to_date behavior

--- spec/services/date_parser_spec.rb
+++ spec/services/date_parser_spec.rb
@@ -2,31 +2,39 @@ require 'rails_helper'
 
 RSpec.describe DateParser do
   describe '.parse_legacy' do
-    subject(:parse) { DateParser.parse_legacy(str) }
+    subject(:parse) { DateParser.parse_legacy(val) }
 
     context 'with an american style date' do
-      let(:str) { '12/31/1970' }
+      let(:val) { '12/31/1970' }
 
       it { is_expected.to eq(Date.new(1970, 12, 31)) }
     end
 
     context 'single digit american date month and year' do
-      let(:str) { '2/1/1970' }
+      let(:val) { '2/1/1970' }
 
       it { is_expected.to eq(Date.new(1970, 2, 1)) }
     end
 
     context 'with an international style date' do
-      let(:str) { '1970-12-31' }
+      let(:val) { '1970-12-31' }
 
       it { is_expected.to eq(Date.new(1970, 12, 31)) }
     end
 
     context 'with something that is not a date' do
-      let(:str) { 'aaa' }
+      let(:val) { 'aaa' }
 
       it 'blows up' do
         expect { parse }.to raise_error
+      end
+    end
+
+    context 'it passes through date objects' do
+      let(:val) { Date.new(2020, 1, 1) }
+
+      it 'is the date' do
+        expect(parse).to eq(val)
       end
     end
   end
