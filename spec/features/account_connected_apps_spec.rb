c37c2133593eb82d20975b5c84fd4c813e6ac63a
Zach Margolis <zachary.margolis@gsa.gov>
2021-07-01 14:54:27 -0700

    rubocop

--- spec/features/account_connected_apps_spec.rb
+++ spec/features/account_connected_apps_spec.rb
@@ -66,7 +66,12 @@ describe 'Account connected applications' do
 
     visit account_connected_accounts_path
     within(find('.profile-info-box')) do
-      within(find('.margin-x-neg-1', text: identity_to_revoke.service_provider_record.friendly_name)) do
+      within(
+        find(
+          '.margin-x-neg-1',
+          text: identity_to_revoke.service_provider_record.friendly_name,
+        ),
+      ) do
         click_link(t('account.revoke_consent.link_title'))
       end
     end
@@ -77,7 +82,10 @@ describe 'Account connected applications' do
     # Accounts page should no longer list this app in the applications section
     within(find('.profile-info-box')) do
       expect(
-        has_selector?('.margin-x-neg-1', text: identity_to_revoke.service_provider_record.friendly_name),
+        has_selector?(
+          '.margin-x-neg-1',
+          text: identity_to_revoke.service_provider_record.friendly_name,
+        ),
       ).to eq(false)
     end
   end
