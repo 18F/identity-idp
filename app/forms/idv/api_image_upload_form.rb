9cc4a55dd1a7b57edbfb3f4a6b82e1e3f9dc2249
Andrew Duthie <andrew.duthie@gsa.gov>
2021-07-19 09:50:34 -0400

    LG-4796: Set Acuant cropping mode by image source
    **Why**: The "automatic" cropping mode currently used is not recommended and may return more "Unknown" results. Instead, set cropping mode to one or the other of "None" or "Always" based on whether the image was captured using Acuant SDK (where cropping is assumed).

--- app/forms/idv/api_image_upload_form.rb
+++ app/forms/idv/api_image_upload_form.rb
@@ -72,6 +72,7 @@ module Idv
         back_image: back.read,
         selfie_image: selfie&.read,
         liveness_checking_enabled: liveness_checking_enabled?,
+        cropping_mode: cropping_mode,
       )
       response.extra.merge!(extra_attributes)
       response.extra[:state] = response.pii_from_doc[:state]
@@ -118,6 +119,14 @@ module Idv
 
     def liveness_checking_enabled?
       @liveness_checking_enabled
+    end
+
+    def cropping_mode
+      if acuant_sdk_capture?
+        IdentityDocAuth::CroppingModes::NONE
+      else
+        IdentityDocAuth::CroppingModes::ALWAYS
+      end
     end
 
     def front
@@ -192,8 +201,13 @@ module Idv
       )
     end
 
+    def acuant_sdk_capture?
+      image_metadata.dig(:front, :source) == 'acuant' &&
+        image_metadata.dig(:back, :source) == 'acuant'
+    end
+
     def image_metadata
-      params.permit(:front_image_metadata, :back_image_metadata).
+      @image_metadata ||= params.permit(:front_image_metadata, :back_image_metadata).
         to_h.
         transform_values do |str|
           JSON.parse(str)
