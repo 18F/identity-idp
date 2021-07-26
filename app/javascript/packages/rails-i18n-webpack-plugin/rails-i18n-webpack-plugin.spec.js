315ccab0f34a089775fc3947f2f90035a928ff81
Andrew Duthie <andrew.duthie@gsa.gov>
2021-07-26 08:37:48 -0400

    Create Webpack plugin to extract translation strings (#5223)
    * Create Webpack plugin to extract translation strings
    
    **Why**:
    
    - 90% reduction in JavaScript string data on root page (10kb to 1kb) by loading only string data relevant for the current page, rather than all string data on all pages.
    - Allows for more cache hits on locale string data for packs with fewer changes.
    - Avoid need to main list of strings for use in JavaScript, instead let them be detected automatically.
    - Reduce dependency on Sprockets.
    
    * Avoid optional chaining syntax
    
    Node v12 compat
    
    * Drop erb suffix from i18n-strings
    
    No ERB in file to process
    
    * Clarify locale suffixing behavior
    
    * RailsI18nWebpackPlugin: Add onMissingString callback for missing key handling
    
    * Use fs for promises import
    
    Node v12 compat
    
    * Throw error on missing key in test environment
    
    * Define devDependencies as peerDependencies
    
    **Why**: Avoid eslint-plugin-import no-extraneous-dependencies errors, while avoiding overhead of keeping per-package devDependencies in sync.
    
    * no nullish coalescing either?! 😩
    
    * ESLint ignore i18n-strings.js
    
    - Issues existed previously, but the file wasn't subject to ESLint because it was .erb file extension.
    - Slated for some future refactoring to remove the file altogether
    
    * Default options using object spread
    
    * Fix onMissingString logic to call before trying default locale
    
    **Why**: Call callback first before trying default locale

---
+++ app/javascript/packages/rails-i18n-webpack-plugin/rails-i18n-webpack-plugin.spec.js
@@ -0,0 +1,160 @@
+const sinon = require('sinon');
+const path = require('path');
+const { promises: fs } = require('fs');
+const webpack = require('webpack');
+const RailsI18nWebpackPlugin = require('./rails-i18n-webpack-plugin.js');
+
+const {
+  dig,
+  fromPairs,
+  uniq,
+  compact,
+  getKeyPath,
+  getKeyDomain,
+  getKeyDomains,
+} = RailsI18nWebpackPlugin;
+
+describe('RailsI18nWebpackPlugin', () => {
+  it('generates expected output', (done) => {
+    const onMissingString = sinon.spy();
+
+    webpack(
+      {
+        entry: {
+          actual: path.resolve(__dirname, 'spec/fixtures/in.js'),
+        },
+        plugins: [
+          new RailsI18nWebpackPlugin({
+            configPath: path.resolve(__dirname, 'spec/fixtures/locales'),
+            onMissingString,
+          }),
+        ],
+        output: {
+          path: path.resolve(__dirname, 'spec/fixtures'),
+        },
+      },
+      async () => {
+        const [script, en, es, fr] = await Promise.all([
+          fs.readFile(path.resolve(__dirname, 'spec/fixtures/actual.js')),
+          ...['.en.js', '.es.js', '.fr.js'].map(async (suffix) => [
+            await fs.readFile(path.resolve(__dirname, `spec/fixtures/expected${suffix}`), 'utf-8'),
+            await fs.readFile(path.resolve(__dirname, `spec/fixtures/actual${suffix}`), 'utf-8'),
+          ]),
+        ]);
+
+        try {
+          expect(script).to.not.be.empty();
+          for (const [expected, actual] of [en, es, fr]) {
+            expect(expected).to.equal(actual);
+          }
+
+          expect(onMissingString).to.have.callCount(7);
+          expect(onMissingString).to.have.been.calledWithExactly('item.1', 'es');
+          expect(onMissingString).to.have.been.calledWithExactly('item.2', 'es');
+          expect(onMissingString).to.have.been.calledWithExactly('item.3', 'es');
+          expect(onMissingString).to.have.been.calledWithExactly('forms.button.submit', 'fr');
+          expect(onMissingString).to.have.been.calledWithExactly('item.2', 'fr');
+          expect(onMissingString).to.have.been.calledWithExactly('item.3', 'fr');
+          expect(onMissingString).to.have.been.calledWithExactly('item.3', 'en');
+
+          done();
+        } catch (error) {
+          done(error);
+        }
+      },
+    );
+  });
+});
+
+describe('dig', () => {
+  it('returns undefined when called on a nullish object', () => {
+    const object = undefined;
+    const result = dig(object, ['a', 'b']);
+
+    expect(result).to.be.undefined();
+  });
+
+  it('returns undefined when path is unreachable', () => {
+    const object = {};
+    const result = dig(object, ['a', 'b']);
+
+    expect(result).to.be.undefined();
+  });
+
+  it('returns value at path', () => {
+    const object = { a: { b: 1 } };
+    const result = dig(object, ['a', 'b']);
+
+    expect(result).to.be.equal(1);
+  });
+});
+
+describe('fromPairs', () => {
+  it('returns pairs of key value in object form', () => {
+    const pairs = [
+      ['a', 1],
+      ['b', 2],
+    ];
+    const result = fromPairs(pairs);
+
+    expect(result).to.deep.equal({ a: 1, b: 2 });
+  });
+});
+
+describe('uniq', () => {
+  it('returns unique values', () => {
+    const values = [1, 2, 2, 3];
+    const result = uniq(values);
+
+    expect(result).to.deep.equal([1, 2, 3]);
+  });
+});
+
+describe('compact', () => {
+  it('returns truthy values', () => {
+    const values = [1, 0, null, false];
+    const result = compact(values);
+
+    expect(result).to.deep.equal([1]);
+  });
+});
+
+describe('getKeyPath', () => {
+  it('returns key path parts', () => {
+    const key = 'a.b.c';
+    const result = getKeyPath(key);
+
+    expect(result).to.deep.equal(['a', 'b', 'c']);
+  });
+});
+
+describe('getKeyDomain', () => {
+  context('key', () => {
+    const key = 'a.b.c';
+
+    it('returns domain', () => {
+      const result = getKeyDomain(key);
+
+      expect(result).to.equal('a');
+    });
+  });
+
+  context('key path', () => {
+    const keyPath = ['a', 'b', 'c'];
+
+    it('returns domain', () => {
+      const result = getKeyDomain(keyPath);
+
+      expect(result).to.equal('a');
+    });
+  });
+});
+
+describe('getKeyDomains', () => {
+  it('returns unique set of domains for keys', () => {
+    const keys = ['a.b.c', 'a.d.e', 'b.f.g'];
+    const domains = getKeyDomains(keys);
+
+    expect(domains).to.deep.equal(['a', 'b']);
+  });
+});
