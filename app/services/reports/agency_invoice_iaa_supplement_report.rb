01d05a3a27c25b2fdcfecad38853b8478eb33d92
Zach Margolis <zachary.margolis@gsa.gov>
2021-07-28 12:02:17 -0700

    Update UniqueMonthlyAuthCountsByIaa to accept iaa data attributes

--- app/services/reports/agency_invoice_iaa_supplement_report.rb
+++ app/services/reports/agency_invoice_iaa_supplement_report.rb
@@ -19,14 +19,7 @@ module Reports
       save_report(REPORT_NAME, results.to_json, extension: 'json')
     end
 
-    # @return [Array<String>]
-    def iaas
-      ServiceProvider.
-        distinct.
-        where.not(iaa: nil).
-        pluck(:iaa)
-    end
-
+    # @return [Array<Hash>]
     def iaas
       Agreements::IaaGtc.
         includes(iaa_orders: { integration_usages: :integration }).
@@ -49,15 +42,15 @@ module Reports
 
     # Turns ial1/ial2 rows into ial1/ial2 columns
     def combine_by_iaa_month(raw_results)
-      raw_results.group_by { |r| [r['iaa'], r['year_month']] }.
+      raw_results.group_by { |r| [r['key'], r['year_month']] }.
         transform_values do |grouped|
-          iaa = grouped.first['iaa']
+          key = grouped.first['key']
           iaa_start_date = grouped.first['iaa_start_date']
           iaa_end_date = grouped.first['iaa_end_date']
           year_month = grouped.first['year_month']
 
           {
-            iaa: iaa,
+            iaa: key,
             iaa_start_date: iaa_start_date,
             iaa_end_date: iaa_end_date,
             year_month: year_month,
