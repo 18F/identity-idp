1bb153a4a789145143a2ad2e3b7b249e3d69f168
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-05 16:13:24 -0700

    missing sublcass

--- app/services/agreements/reports/partner_api_report.rb
+++ app/services/agreements/reports/partner_api_report.rb
@@ -1,6 +1,6 @@
 module Agreements
   module Reports
-    class PartnerApiReport
+    class PartnerApiReport < ApplicationJob
       include GoodJob::ActiveJobExtensions::Concurrency
 
       good_job_control_concurrency_with(
