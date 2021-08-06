1496b143f3b5e4a9ccaf77547ae7311d39bb9217
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-06 13:01:16 -0700

    Add spec coverage for new GpoDailyJob

--- app/jobs/gpo_daily_job.rb
+++ app/jobs/gpo_daily_job.rb
@@ -8,9 +8,9 @@ class GpoDailyJob < ApplicationJob
   )
 
   # Enqueue a test letter every day, but only upload letters on working weekdays
-  def perform(_date)
+  def perform(date)
     GpoDailyTestSender.new.run
 
-    GpoConfirmationUploader.new.run unless CalendarService.weekend_or_holiday?(Time.zone.today)
+    GpoConfirmationUploader.new.run unless CalendarService.weekend_or_holiday?(date)
   end
 end
