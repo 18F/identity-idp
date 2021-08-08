0dc7a1ebfa210b34a70800d83716e5415b60b2d4
Douglas Price <douglas.price@gsa.gov>
2021-08-08 16:44:43 -0400

    removed loa3_saml_settings, loa3_with_bundle_saml_settings

--- spec/support/saml_auth_helper.rb
+++ spec/support/saml_auth_helper.rb
@@ -136,15 +136,6 @@ module SamlAuthHelper
   ##################################################################################################
   ##################################################################################################
 
-  def loa3_saml_settings
-    saml_settings(
-      overrides: {
-        issuer: sp1_issuer,
-        authn_context: Saml::Idp::Constants::LOA3_AUTHN_CONTEXT_CLASSREF,
-      },
-    )
-  end
-
   def ialmax_saml_settings
     saml_settings(
       overrides: {
@@ -156,16 +147,6 @@ module SamlAuthHelper
 
   def ialmax_with_bundle_saml_settings
     settings = ialmax_saml_settings
-    settings.authn_context = [
-      settings.authn_context,
-      "#{Saml::Idp::Constants::REQUESTED_ATTRIBUTES_CLASSREF}first_name:last_name email, ssn",
-      "#{Saml::Idp::Constants::REQUESTED_ATTRIBUTES_CLASSREF}phone",
-    ]
-    settings
-  end
-
-  def loa3_with_bundle_saml_settings
-    settings = loa3_saml_settings
     settings.authn_context = [
       settings.authn_context,
       "#{Saml::Idp::Constants::REQUESTED_ATTRIBUTES_CLASSREF}first_name:last_name email, ssn",
