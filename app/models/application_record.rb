f806ecc9df33a14146632d50470bf2692a16b990
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-09 12:53:55 -0700

    missing connection

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,6 +3,10 @@ class ApplicationRecord < ActiveRecord::Base
 
   connects_to shards: {
     default: { writing: :primary, reading: :primary },
-    read_replica: { reading: :primary_replica },
+    read_replica: {
+      # writing to the read_replica won't work, but AR needs to have something here
+      writing: :primary_replica,
+      reading: :primary_replica,
+    },
   }
 end
