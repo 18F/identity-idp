e5a0500e698cfb15208f8605b1e7cc2fb9c3720c
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-09 17:13:16 -0700

    Set reporting jobs to low priority, declare job queues

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -3,6 +3,7 @@ if IdentityConfig.store.ruby_workers_enabled
     config.good_job.execution_mode = :external
     config.good_job.poll_interval = 5
     config.good_job.enable_cron = true
+    config.good_job.queues = 'default:5,low:1;*'
     # see config/initializers/job_configurations.rb for cron schedule
   end
 end
