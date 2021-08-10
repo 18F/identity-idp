d2dedd1192c61d96525c06b8f4fa4095e0a04554
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-09 17:17:51 -0700

    Add configurable max_threads, set default to GoodJob's default of 5

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -4,6 +4,7 @@ if IdentityConfig.store.ruby_workers_enabled
     config.good_job.poll_interval = 5
     config.good_job.enable_cron = true
     config.good_job.queues = 'default:5,low:1;*'
+    config.good_job.max_threads = IdentityConfig.store.good_job_max_threads
     # see config/initializers/job_configurations.rb for cron schedule
   end
 end
