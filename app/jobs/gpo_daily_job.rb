63e4cae4819d6b2db4d7f5ca7822cfaf1c96df63
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-08-13 11:49:20 -0700

    Discard jobs due to concurrency errors (#5285)
    - When we set concurrency for jobs, our goal is to only run
      these jobs one at a time
    
    * Move discard_on from ApplicationJob to Reports::BaseReport
    
    - And other non-reports jobs that use concurrency
    
    * queue_as :low for non-BaseReport reports

--- app/jobs/gpo_daily_job.rb
+++ app/jobs/gpo_daily_job.rb
@@ -1,4 +1,6 @@
 class GpoDailyJob < ApplicationJob
+  queue_as :low
+
   include GoodJob::ActiveJobExtensions::Concurrency
 
   good_job_control_concurrency_with(
@@ -6,6 +8,8 @@ class GpoDailyJob < ApplicationJob
     perform_limit: 1,
     key: -> { "gpo-daily-job-#{arguments.first}" },
   )
+
+  discard_on GoodJob::ActiveJobExtensions::Concurrency::ConcurrencyExceededError
 
   # Enqueue a test letter every day, but only upload letters on working weekdays
   def perform(date)
