b1314debc4096d2043a58b5278be46e8398f0144
Mitchell Henke <mitchell.henke@gsa.gov>
2021-08-18 16:40:50 -0500

    add specs

--- spec/lib/identity_job_log_subscriber_spec.rb
+++ spec/lib/identity_job_log_subscriber_spec.rb
@@ -29,4 +29,30 @@ RSpec.describe IdentityJobLogSubscriber, type: :job do
       issuer: build(:service_provider).issuer,
     )
   end
+
+  it 'logs errors when exception occurs in job' do
+    expect(Rails.logger).to receive(:error).exactly(2).times do |log|
+      json = JSON.parse(log)
+
+      expect(json['name']).to be_in [
+        'enqueue.active_job', 'perform.active_job'
+      ]
+      expect(json['job_class']).to eq('AddressProofingJob')
+      expect(json.key?('trace_id'))
+      expect(json.key?('duration_ms'))
+      expect(json.key?('job_id'))
+      expect(json.key?('timestamp'))
+      expect(json['exception_message']).to eq 'invalid base64'
+    end
+
+    expect {
+      AddressProofingJob.perform_later(
+        result_id: nil,
+        encrypted_arguments: 'abc',
+        trace_id: nil,
+        user_id: SecureRandom.random_number(1000),
+        issuer: build(:service_provider).issuer,
+      )
+    }.to raise_error(ArgumentError)
+  end
 end
