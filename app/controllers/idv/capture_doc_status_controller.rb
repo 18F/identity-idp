45c436fcb8e15b7b69a4ba4ee2e6ec2f47389313
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-18 09:57:15 -0700

    Remove unneeded guard

--- app/controllers/idv/capture_doc_status_controller.rb
+++ app/controllers/idv/capture_doc_status_controller.rb
@@ -50,7 +50,6 @@ module Idv
     end
 
     def throttled?
-      return unless document_capture_session.user
       Throttle.for(
         user: document_capture_session.user,
         throttle_type: :idv_acuant,
