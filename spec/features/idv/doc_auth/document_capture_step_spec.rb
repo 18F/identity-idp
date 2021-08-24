4186710f564206b1dcf27f4cbbe68326b1a8d48f
Alex Mathews <44584810+amathews-fs@users.noreply.github.com>
2021-08-24 12:49:28 -0400

    Remove Acuant Raw Response Logging (#5325)
    Originally raw alerts and raw regions were used as a backstop in case there were problems with our processing of Acuant responses. This use case was limited and the negative side effect was that CloudWatch logs of these events had consistency problems due to limits of how CloudWatch works. Removing to hopefully allow for more CloudWatch JSON flattening consistency.

--- spec/features/idv/doc_auth/document_capture_step_spec.rb
+++ spec/features/idv/doc_auth/document_capture_step_spec.rb
@@ -152,7 +152,7 @@ feature 'doc auth document capture step' do
         throttle_type: :idv_acuant,
       )
 
-      travel_to(IdentityConfig.store.acuant_attempt_window_in_minutes.minutes.from_now) do
+      travel_to(IdentityConfig.store.acuant_attempt_window_in_minutes.minutes.from_now + 1) do
         sign_in_and_2fa_user(user)
         complete_doc_auth_steps_before_document_capture_step
         attach_and_submit_images
@@ -238,7 +238,7 @@ feature 'doc auth document capture step' do
         throttle_type: :idv_acuant,
       )
 
-      travel_to(IdentityConfig.store.acuant_attempt_window_in_minutes.minutes.from_now) do
+      travel_to(IdentityConfig.store.acuant_attempt_window_in_minutes.minutes.from_now + 1) do
         sign_in_and_2fa_user(user)
         complete_doc_auth_steps_before_document_capture_step
         attach_and_submit_images
