14364e5058ca69a9d9f0b30b6160a11f600d470f
Jessica Dembe <jessica.dembe@gsa.gov>
2021-09-09 11:09:34 -0400

    LG-4920: Add skip link to IdP (#5368)
    * add skip link to base layout
    
    * Add skip link translations
    
    * first pass at refactoring html for a11y
    
    * second pass at refactoring html for a11y
    
    * address PR comments
    
    * Update app/views/layouts/base.html.erb
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Update app/views/layouts/base.html.erb
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Add skipnav component to index.js
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/javascript/app/components/index.js
+++ app/javascript/app/components/index.js
@@ -1,9 +1,9 @@
-import { accordion, banner, navigation } from 'identity-style-guide';
+import { accordion, banner, navigation, skipnav } from 'identity-style-guide';
 import domready from 'domready';
 import Modal from './modal';
 
 window.LoginGov = window.LoginGov || {};
 window.LoginGov.Modal = Modal;
 
-const components = [accordion, banner, navigation];
+const components = [accordion, banner, navigation, skipnav];
 domready(() => components.forEach((component) => component.on()));
