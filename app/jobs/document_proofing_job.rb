c019217666c3f988c7d082af9e07e8b06df2edd3
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-09-15 12:57:05 -0500

    LG-4747 Log unique successful events per session (#5407)

--- app/jobs/document_proofing_job.rb
+++ app/jobs/document_proofing_job.rb
@@ -102,7 +102,7 @@ class DocumentProofingJob < ApplicationJob
       user: document_capture_session.user,
       request: nil,
       sp: document_capture_session.issuer,
-      first_path_visit_this_session: false,
+      session: {},
     )
   end
 
