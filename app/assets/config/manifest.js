ee8cea9a9452bf1e3e00c5c29064f7ae1feb4cc2
Andrew Duthie <andrew.duthie@gsa.gov>
2021-09-20 16:11:51 -0400

    Refactor verification badge to use design system
    **Why**:
    
    - So that we don't have duplicate styles for the same UI element
    - To establish precedent for referencing assets from within the design system, which could be useful for future refactoring
    - To standardize markup of rendered badges to align with design system and support further refactoring as a view component
    - To help toward eliminating references to non-design-system SASS variables (space-1, space-2, white, green, border-radius-xxl)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -10,3 +10,6 @@
 //= link intl-tel-input/build/css/intlTelInput.css
 //= link intl-tel-input/build/img/flags.png
 //= link intl-tel-input/build/img/flags@2x.png
+
+//= link_tree ../../../node_modules/identity-style-guide/dist/assets/img
+//= link_tree ../../../node_modules/identity-style-guide/dist/assets/fonts
