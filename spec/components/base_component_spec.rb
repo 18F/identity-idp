7b997755fbd22c71cff9bf6b5ba93ebec28e4240
Andrew Duthie <andrew.duthie@gsa.gov>
2021-10-06 21:21:43 -0400

    Try to improve test isolation for BaseComponent

--- spec/components/base_component_spec.rb
+++ spec/components/base_component_spec.rb
@@ -7,16 +7,17 @@ RSpec.describe BaseComponent, type: :component do
     end
   end
 
-  after { described_class.rendered_scripts = [] }
+  let!(:initial_rendered_scripts) { described_class.rendered_scripts }
+  after { described_class.rendered_scripts = initial_rendered_scripts }
 
-  it 'initializes rendered scripts as empty array' do
-    expect(described_class.rendered_scripts).to eq([])
+  def rendered_scripts
+    described_class.rendered_scripts - initial_rendered_scripts
   end
 
   it 'does nothing when rendered' do
     render_inline(ExampleComponent.new)
 
-    expect(described_class.rendered_scripts).to eq([])
+    expect(rendered_scripts).to eq([])
   end
 
   context 'declares rendered script' do
@@ -25,7 +26,7 @@ RSpec.describe BaseComponent, type: :component do
     it 'adds script to class variable when rendered' do
       render_inline(ExampleComponentWithScript.new)
 
-      expect(described_class.rendered_scripts).to eq(['example_component_with_script'])
+      expect(rendered_scripts).to eq(['example_component_with_script'])
     end
   end
 
@@ -35,7 +36,7 @@ RSpec.describe BaseComponent, type: :component do
     it 'adds script to class variable when rendered' do
       render_inline(ExampleComponentWithNamedScript.new)
 
-      expect(described_class.rendered_scripts).to eq(['my_script'])
+      expect(rendered_scripts).to eq(['my_script'])
     end
   end
 end
