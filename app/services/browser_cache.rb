85461e84a607ba82f8f1188ab594da8b823679f8
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-10-08 10:40:20 -0700

    Update app/services/browser_cache.rb

--- app/services/browser_cache.rb
+++ app/services/browser_cache.rb
@@ -1,7 +1,7 @@
 class BrowserCache
   @cache = LruRedux::Cache.new(1_000)
 
-  # @param [String] user_agent]
+  # @param [String] user_agent
   # @return [Browser]
   def self.parse(user_agent)
     @cache.getset(user_agent) { Browser.new(user_agent) }
