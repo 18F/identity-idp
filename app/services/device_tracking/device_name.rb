5c571c3b632f34928d229a6f0a2ae2aae5cf2558
Zach Margolis <zachary.margolis@gsa.gov>
2021-10-08 13:48:01 -0700

    Remove separate methods

--- app/services/device_tracking/device_name.rb
+++ app/services/device_tracking/device_name.rb
@@ -1,24 +1,12 @@
 module DeviceTracking
   class DeviceName
     def self.call(device)
-      device_user_agent = device.user_agent
-      parsed_browser = BrowserCache.parse(device_user_agent)
+      browser = BrowserCache.parse(device.user_agent)
       I18n.t(
         'account.index.device',
-        browser: browser(parsed_browser),
-        os: os(parsed_browser),
+        browser: "#{browser.name} #{browser.version}",
+        os: "#{browser.platform.name} #{browser.platform.version.split('.').first}",
       )
     end
-
-    def self.browser(browser)
-      "#{browser.name} #{browser.version}"
-    end
-    private_class_method :browser
-
-    def self.os(browser)
-      major_version = browser.platform.version.split('.').first
-      "#{browser.platform.name} #{major_version}"
-    end
-    private_class_method :os
   end
 end
