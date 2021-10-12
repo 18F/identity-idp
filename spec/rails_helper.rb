14810b660422fea196694313df22fb0ae83b64b8
Andrew Duthie <andrew.duthie@gsa.gov>
2021-10-12 13:57:23 -0400

    Allow skipping expected browser console errors
    e.g. 404 page, or specs asserting against an error resposne

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -107,6 +107,8 @@ RSpec.configure do |config|
   end
 
   config.after(:each, type: :feature, js: true) do |spec|
+    next if spec.metadata[:allow_js_error]
+
     javascript_errors = page.driver.browser.manage.logs.get(:browser).map(&:message)
     # Temporarily allow for document-capture bundle, since it uses React error boundaries to poll.
     javascript_errors.reject! { |e| e.include? 'js/document-capture-' }
