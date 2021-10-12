e1ede13f1bcd9035ba0b549d3e0759fc6ec47955
Andrew Duthie <andrew.duthie@gsa.gov>
2021-10-12 16:25:53 -0400

    Remove redundant visit
    Because (a) it's not necessary and (b) it makes console log catching angry because the page will prompt before navigating.

--- spec/features/idv/doc_auth/link_sent_step_spec.rb
+++ spec/features/idv/doc_auth/link_sent_step_spec.rb
@@ -80,10 +80,6 @@ feature 'doc auth link sent step' do
     metadata[:js] = true
     let(:doc_capture_polling_enabled) { true }
 
-    before do
-      visit current_path
-    end
-
     it 'automatically advances when the mobile flow is complete' do
       expect(page).to_not have_css 'meta[http-equiv="refresh"]', visible: false
       expect(page).to_not have_button(t('forms.buttons.continue'))
@@ -99,10 +95,6 @@ feature 'doc auth link sent step' do
 
   shared_examples 'with doc capture polling disabled' do
     let(:doc_capture_polling_enabled) { false }
-
-    before do
-      visit current_path
-    end
 
     context 'clicks back link' do
       before do
