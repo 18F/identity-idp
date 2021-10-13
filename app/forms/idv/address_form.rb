df4a3789592224a2583a78255108fffbdc44e557
Douglas Price <douglas.price@gsa.gov>
2021-10-13 14:05:22 -0400

    review comments

--- app/forms/idv/address_form.rb
+++ app/forms/idv/address_form.rb
@@ -35,7 +35,7 @@ module Idv
       params.each do |key, value|
         raise_invalid_address_parameter_error(key) unless ATTRIBUTES.include?(key.to_sym)
         send("#{key}=", value)
-        unless send(key) == @pii[key] || (send(key).blank? && @pii[key].blank?)
+        if send(key) != @pii[key] && (send(key).present? || @pii[key].present?)
           @address_edited = true
         end
       end
