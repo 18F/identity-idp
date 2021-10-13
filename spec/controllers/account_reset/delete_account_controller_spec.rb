2b068ef5e8fc68cf93c1684a010162830cba96ee
Zach Margolis <zachary.margolis@gsa.gov>
2021-10-13 16:54:03 -0700

    lints

--- spec/controllers/account_reset/delete_account_controller_spec.rb
+++ spec/controllers/account_reset/delete_account_controller_spec.rb
@@ -70,7 +70,10 @@ describe AccountReset::DeleteAccountController do
       delete :delete
 
       expect(response).to redirect_to(root_url)
-      expect(flash[:error]).to eq t('errors.account_reset.granted_token_missing', app_name: APP_NAME)
+      expect(flash[:error]).to eq t(
+        'errors.account_reset.granted_token_missing',
+        app_name: APP_NAME,
+      )
     end
 
     it 'displays a flash and redirects to root if the token is expired' do
