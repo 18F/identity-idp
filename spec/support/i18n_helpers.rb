a583ff9b5c233756e27b59ce9d40cfff2509da96
Zach Margolis <zachary.margolis@gsa.gov>
2021-10-14 14:43:31 -0700

    lints

--- spec/support/i18n_helpers.rb
+++ spec/support/i18n_helpers.rb
@@ -1,16 +1,22 @@
 module I18nHelpers
   module ClassMethods
     # Wraps I18n.translate to check for unused interpolation arguments
-    def translate_with_interpolation_check(key = nil, throw: false, raise: false, locale: nil, **options)
+    def translate_with_interpolation_check(
+      key = nil,
+      throw: false,
+      raise: false,
+      locale: nil,
+      **options
+    )
       without_interpolation = original_translate(key, throw: throw, raise: raise, locale: locale)
 
       if without_interpolation.is_a?(String)
-        expected_args = without_interpolation
-          .scan(I18n::INTERPOLATION_PATTERN)
-          .map(&:compact)
-          .map(&:first)
-          .map(&:to_sym)
-          .uniq
+        expected_args = without_interpolation.
+          scan(I18n::INTERPOLATION_PATTERN).
+          map(&:compact).
+          map(&:first).
+          map(&:to_sym).
+          uniq
 
         missing_args = expected_args - options.keys
         if missing_args.present?
