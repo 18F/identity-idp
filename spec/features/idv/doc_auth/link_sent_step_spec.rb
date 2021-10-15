e9b57065c5dda5eee8a896c3b2b52b579f7a9c7f
Andrew Duthie <andrew.duthie@gsa.gov>
2021-10-15 14:00:10 -0400

    Increment reload times for JS-disabled doc polling scenario
    It was relying on the page refresh. Better to handle it in the test though, since it matches the test description

--- spec/features/idv/doc_auth/link_sent_step_spec.rb
+++ spec/features/idv/doc_auth/link_sent_step_spec.rb
@@ -109,7 +109,7 @@ feature 'doc auth link sent step' do
     end
 
     it 'refreshes page 4x with meta refresh extending timeout by 40 min and can start over' do
-      3.times do
+      4.times do
         expect(page).to have_css 'meta[http-equiv="refresh"]', visible: false
         visit idv_doc_auth_link_sent_step
       end
