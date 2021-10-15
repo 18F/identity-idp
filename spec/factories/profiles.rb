4d706f2ea5a74c9ac45fa5eedb7d3a7ed0df5bbf
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-10-15 10:22:38 -0700

    Remove pii from FormResponse in VerifyPersonalKeyController (LG-5162) (#5505)
    - Follow-up from #5431

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -14,10 +14,15 @@ FactoryBot.define do
       verified_at { Time.zone.now }
     end
 
+    trait :password_reset do
+      deactivation_reason { :password_reset }
+    end
+
     after(:build) do |profile, evaluator|
       if evaluator.pii
         pii_attrs = Pii::Attributes.new_from_hash(evaluator.pii)
         profile.encrypt_pii(pii_attrs, profile.user.password)
+        profile.encrypt_recovery_pii(pii_attrs)
       end
     end
   end
