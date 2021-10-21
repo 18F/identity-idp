347d2282763450340093687209862fa4aa1e11e9
Malick Diarra <malick.diarra@gsa.gov>
2021-10-21 14:58:25 -0400

    LG-5232: refactor to use validated_form_for

--- app/javascript/packs/one-time-code-input.js
+++ app/javascript/packs/one-time-code-input.js
@@ -1,23 +1,6 @@
 import OneTimeCodeInput from '@18f/identity-one-time-code-input';
 
-function checkInputValidity(event) {
-  const input = /** @type {HTMLInputElement} */ (event.target);
-  input.setAttribute('aria-invalid', 'false');
-  input.classList.remove('usa-input--error');
-  if (
-    event.type === 'invalid' &&
-    !input.validity.valid &&
-    input.parentNode?.querySelector('.display-if-invalid')
-  ) {
-    event.preventDefault();
-    input.setAttribute('aria-invalid', 'true');
-    input.classList.add('usa-input--error');
-  }
-}
-
 const input = document.querySelector('.one-time-code-input');
 if (input) {
   new OneTimeCodeInput(/** @type {HTMLInputElement} */ (input)).bind();
-  input.addEventListener('input', checkInputValidity);
-  input.addEventListener('invalid', checkInputValidity);
 }
