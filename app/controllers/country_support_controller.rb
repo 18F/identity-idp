b190ae599c81393fa4f58dce428350d5a4552f34
Zach Margolis <zachary.margolis@gsa.gov>
2021-10-21 16:27:22 -0700

    Send cache headers in /api/country-support

--- app/controllers/country_support_controller.rb
+++ app/controllers/country_support_controller.rb
@@ -1,5 +1,9 @@
 class CountrySupportController < ApplicationController
+  skip_before_action :disable_caching
+
   def index
+    expires_in 15.minutes, public: true
+
     render json: { countries: PhoneNumberCapabilities::INTERNATIONAL_CODES }
   end
 end
