e62edd571949c44023862b13df5404b389f1107b
amathews <amathews@fearsol.com>
2021-10-22 18:09:26 -0400

    A quick attempt at fixing the failing SubmissionInterstitial test. Thought it would work but it doesn't.

--- app/javascript/packages/document-capture/components/submission-interstitial.jsx
+++ app/javascript/packages/document-capture/components/submission-interstitial.jsx
@@ -31,7 +31,7 @@ function SubmissionInterstitial({ autoFocus = false }) {
         height="116"
         className="margin-bottom-4"
       />
-      <PageHeading>{t('doc_auth.headings.interstitial')}</PageHeading>
+      <PageHeading ref={headingRef}>{t('doc_auth.headings.interstitial')}</PageHeading>
       <p className="margin-top-4">{t('doc_auth.info.interstitial_eta')}</p>
       <p>{t('doc_auth.info.interstitial_thanks')}</p>
     </div>
