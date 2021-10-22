299d118789f4ca5f9efaca139447515ca38746fe
Mitchell Henke <mitchell.henke@gsa.gov>
2021-10-22 14:16:57 -0500

    Fix 500 when SAML authenticating at IAL2 after password reset (#5531)
    * add failing spec
    
    * ask for password if profile is available but not decrypted
    
    * fix flaky device spec
    
    * fix specs
    
    * split decryption check into method

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -19,16 +19,16 @@ describe Device do
   describe '#update_last_used_ip' do
     let(:user) { create(:user) }
     let(:remote_ip) { '1.2.3.4' }
-    let(:user_agent) { 'Chrome/58.0.3029.110 Safari/537.36' }
-    let(:uuid) { 'abc123' }
-    let(:now) { Time.zone.now }
-    let(:old_timestamp) { now - 1.hour }
+    let(:old_timestamp) { 1.hour.ago }
     let(:device) { create(:device, last_used_at: old_timestamp) }
 
     it 'updates the last ip and last_used_at' do
-      expect { device.update_last_used_ip(remote_ip) }.
-        to(change { device.reload.last_used_at.to_i }.from(old_timestamp.to_i).to(now.to_i).
-          and(change { device.reload.last_ip }.to(remote_ip)))
+      freeze_time do
+        now = Time.zone.now
+        device.update_last_used_ip(remote_ip)
+        expect(device.last_ip).to eq(remote_ip)
+        expect(device.last_used_at.to_i).to eq(now.to_i)
+      end
     end
   end
 end
