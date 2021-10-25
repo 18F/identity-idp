fe7e4ef693ea82c00add26db025a04de1b185201
Andrew Duthie <andrew.duthie@gsa.gov>
2021-10-25 09:36:31 -0400

    Restore SubmissionInterstitial autoFocus behavior
    https://github.com/18F/identity-idp/pull/5534/files#r735605388

--- app/javascript/packages/document-capture/components/submission-interstitial.jsx
+++ app/javascript/packages/document-capture/components/submission-interstitial.jsx
@@ -31,7 +31,9 @@ function SubmissionInterstitial({ autoFocus = false }) {
         height="116"
         className="margin-bottom-4"
       />
-      <PageHeading ref={headingRef}>{t('doc_auth.headings.interstitial')}</PageHeading>
+      <PageHeading ref={headingRef} tabIndex={-1}>
+        {t('doc_auth.headings.interstitial')}
+      </PageHeading>
       <p className="margin-top-4">{t('doc_auth.info.interstitial_eta')}</p>
       <p>{t('doc_auth.info.interstitial_thanks')}</p>
     </div>
