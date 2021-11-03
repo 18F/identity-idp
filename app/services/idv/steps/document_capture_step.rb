31951f08a0cd4a957e83267bd90c8303bc060ffc
Andrew Duthie <andrew.duthie@gsa.gov>
2021-11-03 08:25:23 -0400

    Remove unused document capture notice text (#5574)
    **Why**: Because they're never actually presented to the user, who will instead see the message derived from the failing response.

--- app/services/idv/steps/document_capture_step.rb
+++ app/services/idv/steps/document_capture_step.rb
@@ -80,13 +80,8 @@ module Idv
 
       def handle_document_verification_failure(response)
         mark_step_incomplete(:document_capture)
-        notice = if liveness_checking_enabled?
-                   { notice: I18n.t('errors.doc_auth.document_capture_info_with_selfie_html') }
-                 else
-                   { notice: I18n.t('errors.doc_auth.document_capture_info_html') }
-                 end
         log_document_error(response)
-        extra = response.to_h.merge(notice)
+        extra = response.to_h
         failure(response.first_error_message, extra)
       end
 
