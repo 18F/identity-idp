5c21f81dd0580ef15d4f18307b2da81d87e1ed48
Malick Diarra <malick.diarra@gsa.gov>
2021-11-05 14:57:17 -0400

    LG-4924: attempt at fixing

--- app/javascript/packs/one-time-code-input.js
+++ app/javascript/packs/one-time-code-input.js
@@ -1,6 +1,12 @@
 import OneTimeCodeInput from '@18f/identity-one-time-code-input';
 
 const input = document.querySelector('.one-time-code-input');
+const fakeField = document.querySelector('.hidden-input');
+if (input && fakeField) {
+  input.addEventListener('input', () => {
+    fakeField.value = input.value;
+  })
+}
 if (input) {
   new OneTimeCodeInput(/** @type {HTMLInputElement} */ (input)).bind();
 }
