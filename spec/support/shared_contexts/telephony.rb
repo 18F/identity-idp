6157ef9b6846ab8e7c974b01927f5f7ad82cea20
Kevin Dayton <kevin.dayton@gsa.gov>
2021-11-08 09:45:15 -0600

    Move telephony_spec_helper to shared_context.

---
+++ spec/support/shared_contexts/telephony.rb
@@ -0,0 +1,35 @@
+require 'telephony'
+
+def telephony_use_default_config!
+  # Setup some default configs
+  Telephony.instance_variable_set(:@config, nil)
+
+  Telephony.config do |c|
+    c.logger = Logger.new(nil)
+
+    c.voice_pause_time = '0.5s'
+    c.voice_rate = 'slow'
+
+    c.pinpoint.add_sms_config do |sms|
+      sms.region = 'fake-pinpoint-region-sms'
+      sms.access_key_id = 'fake-pnpoint-access-key-id-sms'
+      sms.secret_access_key = 'fake-pinpoint-secret-access-key-sms'
+      sms.application_id = 'fake-pinpoint-application-id-sms'
+      sms.shortcode = '123456'
+    end
+
+    c.pinpoint.add_voice_config do |voice|
+      voice.region = 'fake-pinpoint-region-voice'
+      voice.access_key_id = 'fake-pinpoint-access-key-id-voice'
+      voice.secret_access_key = 'fake-pinpoint-secret-access-key-voice'
+      voice.longcode_pool = ['+12223334444', '+15556667777']
+    end
+  end
+end
+
+RSpec.shared_context 'telephony' do
+  before do
+      Pinpoint::MockClient.reset!
+      telephony_use_default_config!
+  end
+end
