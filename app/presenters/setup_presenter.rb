4e2ef25b2bebefa20d0d89e4e7076ab8765f114f
Andrew Duthie <andrew.duthie@gsa.gov>
2021-11-09 13:53:20 -0500

    Show single vendor outage status on phone setup page

--- app/presenters/setup_presenter.rb
+++ app/presenters/setup_presenter.rb
@@ -1,4 +1,6 @@
 class SetupPresenter
+  include ActionView::Helpers::TranslationHelper
+
   attr_reader :current_user, :user_fully_authenticated, :user_opted_remember_device_cookie
 
   def initialize(current_user:, user_fully_authenticated:, user_opted_remember_device_cookie:,
@@ -12,5 +14,13 @@ class SetupPresenter
   def remember_device_box_checked?
     return @remember_device_default if user_opted_remember_device_cookie.nil?
     ActiveModel::Type::Boolean.new.cast(user_opted_remember_device_cookie)
+  end
+
+  def outage_message
+    if VendorStatus.new.vendor_outage?(:voice)
+      t('vendor_outage.voice.alert')
+    elsif VendorStatus.new.vendor_outage?(:sms)
+      t('vendor_outage.sms.alert')
+    end
   end
 end
