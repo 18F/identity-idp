64fb4ccd889d4851341f3e253e724db1c96b0fab
Malick Diarra <malick.diarra@gsa.gov>
2021-11-12 15:53:19 -0500

    fix lint

--- app/javascript/packs/one-time-code-input.js
+++ app/javascript/packs/one-time-code-input.js
@@ -1,10 +1,8 @@
 import OneTimeCodeInput from '@18f/identity-one-time-code-input';
 
-
 const fakeField = /** @type {HTMLInputElement?} */ (document.querySelector('.one-time-code-input'));
 
 if (fakeField) {
-
   const el = `<input type="hidden" name="code" id="code" class="hidden-input">`;
   fakeField.insertAdjacentHTML('afterend', el);
   fakeField.removeAttribute('name');
