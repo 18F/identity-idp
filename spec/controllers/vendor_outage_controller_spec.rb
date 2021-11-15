b158d7a92664391ad257098f28072e6c097ec1b9
Andrew Duthie <andrew.duthie@gsa.gov>
2021-11-15 15:27:45 -0500

    Test VendorOutageController show_gpo_option assignment

--- spec/controllers/vendor_outage_controller_spec.rb
+++ spec/controllers/vendor_outage_controller_spec.rb
@@ -26,4 +26,24 @@ describe VendorOutageController do
       tracking_data,
     )
   end
+
+  it 'sets show_gpo_option view variable' do
+    get :show
+
+    expect(assigns(:show_gpo_option)).to eq(false)
+  end
+
+  context 'from idv phone' do
+    before { allow(controller).to receive(:from_idv_phone?).and_return(true) }
+
+    context 'gpo letter available' do
+      before { allow(controller).to receive(:gpo_letter_available?).and_return(true) }
+
+      it 'sets show_gpo_option as true' do
+        get :show
+
+        expect(assigns(:show_gpo_option)).to eq(true)
+      end
+    end
+  end
 end
