28b8ea9b98cdc411b0f08f746c7842036de8e729
Kevin Dayton <kevin.dayton@gsa.gov>
2021-11-16 08:11:23 -0600

    Remove additional test.

--- spec/lib/telephony/alert_sender_spec.rb
+++ spec/lib/telephony/alert_sender_spec.rb
@@ -91,35 +91,6 @@ describe Telephony::AlertSender do
     end
   end
 
-  describe 'send_stop_keyword_response' do
-    it 'sends the correct message' do
-      subject.send_stop_keyword_response(to: recipient, country_code: 'US')
-
-      last_message = Telephony::Test::Message.messages.last
-      expect(last_message.to).to eq(recipient)
-      expect(last_message.body).to eq(I18n.t('telephony.stop_keyword_response'))
-    end
-
-    I18n.available_locales.each do |locale|
-      context "in locale #{locale}" do
-        around do |ex|
-          orig_locale = I18n.locale
-          I18n.locale = locale
-          ex.run
-        ensure
-          I18n.locale = orig_locale
-        end
-
-        it 'fits in an SMS messages (160 chars)' do
-          subject.send_stop_keyword_response(to: recipient, country_code: 'US')
-
-          last_message = Telephony::Test::Message.messages.last
-          expect(last_message.body.size).to be <= 160
-        end
-      end
-    end
-  end
-
   describe 'send_help_keyword_response' do
     it 'sends the correct message' do
       subject.send_help_keyword_response(to: recipient, country_code: 'US')
