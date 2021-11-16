266deb154c6a00c51f3be77b833a738c662fc856
Malick Diarra <malick.diarra@gsa.gov>
2021-11-16 15:20:55 -0500

    polyfill custom events

--- app/javascript/packs/one-time-code-input.js
+++ app/javascript/packs/one-time-code-input.js
@@ -1,7 +1,8 @@
 import OneTimeCodeInput from '@18f/identity-one-time-code-input';
+import { loadPolyfills } from '@18f/identity-polyfill';
 
 const fakeField = /** @type {HTMLInputElement?} */ (document.querySelector('.one-time-code-input'));
 
 if (fakeField) {
-  new OneTimeCodeInput(fakeField).bind();
+  loadPolyfills(['custom-event']).then(() => new OneTimeCodeInput(fakeField).bind());
 }
