9dec264355cdc04abbfdd0c53be57dff416082b0
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-11-18 10:48:27 -0800

    Stream query results (LG-5351) (#5618)
    * Stream results in IAA supplement report
    * Stream results in Issuer supplement report
    
    **Why**: The big UNION query takes so long it gets serialization errors
    in prod, so this reworks it to do more logic in Ruby and stream results

--- app/jobs/reports/agency_invoice_iaa_supplement_report.rb
+++ app/jobs/reports/agency_invoice_iaa_supplement_report.rb
@@ -11,13 +11,8 @@ module Reports
 
     def perform(_date)
       raw_results = iaas.flat_map do |iaa|
-        [:sum, :unique, :new_unique].flat_map do |aggregate|
-          transaction_with_timeout do
-            Db::MonthlySpAuthCount::UniqueMonthlyAuthCountsByIaa.call(
-              iaa: iaa,
-              aggregate: aggregate,
-            )
-          end.to_a
+        transaction_with_timeout do
+          Db::MonthlySpAuthCount::UniqueMonthlyAuthCountsByIaa.call(**iaa)
         end
       end
 
@@ -49,30 +44,30 @@ module Reports
 
     # Turns ial1/ial2 rows into ial1/ial2 columns
     def combine_by_iaa_month(raw_results)
-      raw_results.group_by { |r| [r['key'], r['year_month']] }.
+      raw_results.group_by { |r| [r[:key], r[:year_month]] }.
         transform_values do |grouped|
-          key = grouped.first['key']
-          iaa_start_date = grouped.first['iaa_start_date']
-          iaa_end_date = grouped.first['iaa_end_date']
-          year_month = grouped.first['year_month']
+          key = grouped.first[:key]
+          iaa_start_date = grouped.first[:iaa_start_date]
+          iaa_end_date = grouped.first[:iaa_end_date]
+          year_month = grouped.first[:year_month]
 
           {
             iaa: key,
             iaa_start_date: iaa_start_date,
             iaa_end_date: iaa_end_date,
             year_month: year_month,
-            ial1_total_auth_count: extract(grouped, 'total_auth_count', ial: 1),
-            ial2_total_auth_count: extract(grouped, 'total_auth_count', ial: 2),
-            ial1_unique_users: extract(grouped, 'unique_users', ial: 1),
-            ial2_unique_users: extract(grouped, 'unique_users', ial: 2),
-            ial1_new_unique_users: extract(grouped, 'new_unique_users', ial: 1),
-            ial2_new_unique_users: extract(grouped, 'new_unique_users', ial: 2),
+            ial1_total_auth_count: extract(grouped, :total_auth_count, ial: 1),
+            ial2_total_auth_count: extract(grouped, :total_auth_count, ial: 2),
+            ial1_unique_users: extract(grouped, :unique_users, ial: 1),
+            ial2_unique_users: extract(grouped, :unique_users, ial: 2),
+            ial1_new_unique_users: extract(grouped, :new_unique_users, ial: 1),
+            ial2_new_unique_users: extract(grouped, :new_unique_users, ial: 2),
           }
         end.values
     end
 
     def extract(arr, key, ial:)
-      arr.find { |elem| elem['ial'] == ial && elem[key] }&.dig(key) || 0
+      arr.find { |elem| elem[:ial] == ial && elem[key] }&.dig(key) || 0
     end
   end
 end
