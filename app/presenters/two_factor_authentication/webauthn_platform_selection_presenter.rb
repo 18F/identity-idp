25627a270d196e1895925ce8aade8fe4055f914e
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-11-23 14:46:15 -0700

    no more reek

--- app/presenters/two_factor_authentication/webauthn_platform_selection_presenter.rb
+++ app/presenters/two_factor_authentication/webauthn_platform_selection_presenter.rb
@@ -8,7 +8,6 @@ module TwoFactorAuthentication
       ''#'display-none'
     end
 
-    # :reek:UtilityFunction
     def security_level
       I18n.t('two_factor_authentication.two_factor_choice_options.more_secure_label')
     end
