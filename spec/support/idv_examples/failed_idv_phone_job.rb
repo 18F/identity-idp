5c1cdb52f9d16b3cfa041a6060c0789e23bc27de
Douglas Price <douglas.price@gsa.gov>
2021-11-29 17:07:28 -0500

    refactor failed_idv_job to remove unused distinction wrt step

---
+++ spec/support/idv_examples/failed_idv_phone_job.rb
@@ -0,0 +1,69 @@
+shared_examples 'failed idv phone job' do
+  let(:locale) { LinkLocaleResolver.locale }
+
+  before do
+    visit_idp_from_sp_with_ial2(:oidc)
+    complete_idv_steps_before_step(:phone)
+  end
+
+  context 'the proofer raises an error' do
+    before do
+      fill_out_phone_form_error
+      click_idv_continue
+    end
+
+    it 'renders a jobfail failure screen' do
+      expect(page).to have_current_path(phone_jobfail_path)
+      expect(page).to have_content t('idv.failure.phone.heading')
+      expect(page).to have_content t('idv.failure.phone.jobfail')
+    end
+  end
+
+  context 'the proofer times out' do
+    before do
+      fill_out_phone_form_timeout
+      click_idv_continue
+    end
+
+    it 'renders a timeout message on the phone entry screen' do
+      expect(page).to have_current_path(idv_phone_path)
+      expect(page).to have_content(t('idv.failure.timeout'))
+    end
+  end
+
+  def fill_out_idv_form_error
+    fill_out_idv_form_ok
+    fill_in 'profile_first_name', with: 'Fail'
+  end
+
+  def fill_out_phone_form_error
+    fill_in :idv_phone_form_phone, with:
+      Proofing::Mock::AddressMockClient::FAILED_TO_CONTACT_PHONE_NUMBER
+  end
+
+  def fill_out_idv_form_timeout
+    fill_out_idv_form_ok
+    fill_in 'profile_first_name', with: 'Time'
+  end
+
+  def fill_out_phone_form_timeout
+    fill_in :idv_phone_form_phone, with:
+      Proofing::Mock::AddressMockClient::PROOFER_TIMEOUT_PHONE_NUMBER
+  end
+
+  def session_timeout_path
+    idv_session_errors_timeout_path(locale: locale)
+  end
+
+  def session_jobfail_path
+    idv_session_errors_jobfail_path(locale: locale)
+  end
+
+  def phone_timeout_path
+    idv_phone_errors_timeout_path(locale: locale)
+  end
+
+  def phone_jobfail_path
+    idv_phone_errors_jobfail_path(locale: locale)
+  end
+end
