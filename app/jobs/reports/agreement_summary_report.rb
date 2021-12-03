233bb81ff61c636e3b9e4d10456a315047908f3f
Zach Margolis <zachary.margolis@gsa.gov>
2021-12-03 11:43:06 -0800

    Add new report that summarizes IAA information (LG-5433)

---
+++ app/jobs/reports/agreement_summary_report.rb
@@ -0,0 +1,47 @@
+require 'csv'
+
+module Reports
+  class AgreementSummaryReport < BaseReport
+    REPORT_NAME = 'agreement-summary-report'.freeze
+
+    include GoodJob::ActiveJobExtensions::Concurrency
+
+    good_job_control_concurrency_with(
+      total_limit: 1,
+      key: -> { "#{REPORT_NAME}-#{arguments.first}" },
+    )
+
+    def perform(_date)
+      csv = build_report
+
+      save_report(REPORT_NAME, csv, extension: 'csv')
+    end
+
+    # @return [String]
+    def build_report
+      CSV.generate do |csv|
+        csv << %w[
+          gtc_number
+          order_number
+          issuer
+          friendly_name
+          start_date
+          end_date
+        ]
+
+        IaaReportingHelper.iaas.each do |iaa|
+          ServiceProvider.where(issuer: iaa.issuers).each do |service_provider|
+            csv << [
+              iaa.gtc_number,
+              iaa.order_number,
+              service_provider.issuer,
+              service_provider.friendly_name,
+              iaa.start_date,
+              iaa.end_date,
+            ]
+          end
+        end
+      end
+    end
+  end
+end
