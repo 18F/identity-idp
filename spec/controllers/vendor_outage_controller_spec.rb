e1b6f8668b4da75823e34d9652978ab5c7291f06
Andrew Duthie <andrew.duthie@gsa.gov>
2021-12-13 14:25:36 -0500

    Remove specific tracking data from VendorOutageController spec
    **Why**: This is an implementation detail of VendorStatus which should be tested on that class's specs

--- spec/controllers/vendor_outage_controller_spec.rb
+++ spec/controllers/vendor_outage_controller_spec.rb
@@ -3,28 +3,12 @@ require 'rails_helper'
 describe VendorOutageController do
   before do
     stub_analytics
-    allow(@analytics).to receive(:track_event)
-  end
-
-  let(:redirect_from) { nil }
-  let(:tracking_data) do
-    {
-      vendor_status: {
-        acuant: IdentityConfig.store.vendor_status_acuant,
-        lexisnexis_instant_verify: IdentityConfig.store.vendor_status_lexisnexis_instant_verify,
-        lexisnexis_trueid: IdentityConfig.store.vendor_status_lexisnexis_trueid,
-      },
-      redirect_from: redirect_from,
-    }
   end
 
   it 'tracks an analytics event' do
-    get :show
+    expect_any_instance_of(VendorStatus).to receive(:track_event).with(@analytics)
 
-    expect(@analytics).to have_received(:track_event).with(
-      Analytics::VENDOR_OUTAGE,
-      tracking_data,
-    )
+    get :show
   end
 
   it 'sets show_gpo_option view variable' do
