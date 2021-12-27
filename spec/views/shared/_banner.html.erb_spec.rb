954243271c7afc3045bded6b0cb087f581710ea7
Jessica Dembe <jessica.dembe@gsa.gov>
2021-12-27 13:54:36 -0500

    Refactor banner (#5739)
    * refactor base to include navigation
    
    * make test pass
    
    * add yield for the navigation
    
    * add sp name to test
    
    * fix test again
    
    * remove render from banner spec

--- spec/views/shared/_banner.html.erb_spec.rb
+++ spec/views/shared/_banner.html.erb_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 describe 'shared/_banner.html.erb' do
+  before do
+    sp_with_logo = build_stubbed(
+      :service_provider, logo: 'generic.svg', friendly_name: 'Best SP ever'
+    )
+
+    decorated_session = ServiceProviderSessionDecorator.new(
+      sp: sp_with_logo,
+      view_context: '',
+      sp_session: {},
+      service_provider_request: nil,
+    )
+    allow(view).to receive(:decorated_session).and_return(decorated_session)
+  end
+
   it 'properly HTML escapes the secure notification' do
     render
 
