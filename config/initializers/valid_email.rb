4b2caf94b8047938807668496733669822d22f24
Zach Margolis <zachary.margolis@gsa.gov>
2021-12-27 12:05:40 -0800

    Move disposable email configs to application.yml

--- config/initializers/valid_email.rb
+++ config/initializers/valid_email.rb
@@ -1,5 +1,3 @@
-config = Rails.root.join('config', 'valid_email.yml')
-
-BanDisposableEmailValidator.config = YAML.load_file(config)['disposable_email_services']
+BanDisposableEmailValidator.config = IdentityConfig.store.disposable_email_services
 
 MxValidator.config[:timeouts] = [IdentityConfig.store.mx_timeout]
