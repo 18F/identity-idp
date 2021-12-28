ebce7197ed8f11aa13ad944d487434a581acfd15
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-12-28 09:27:33 -0800

    Preserve locale change in identity verification (LG-5329) (#5756)

--- app/controllers/accounts/history_controller.rb
+++ app/controllers/accounts/history_controller.rb
@@ -9,7 +9,7 @@ module Accounts
       @view_model = AccountShow.new(
         decrypted_pii: nil,
         personal_key: flash[:personal_key],
-        sp_session_request_url: sp_session_request_url_without_prompt_login,
+        sp_session_request_url: sp_session_request_url_with_updated_params,
         sp_name: decorated_session.sp_name,
         decorated_user: current_user.decorate,
         locked_for_session: pii_locked_for_session?(current_user),
--- spec/support/features/session_helper.rb
+++ spec/support/features/session_helper.rb
@@ -261,7 +261,7 @@ module Features
 
     def fill_in_code_with_last_phone_otp
       accept_rules_of_use_and_continue_if_displayed
-      fill_in :code, with: last_phone_otp
+      fill_in I18n.t('forms.two_factor.code'), with: last_phone_otp
     end
 
     def accept_rules_of_use_and_continue_if_displayed
