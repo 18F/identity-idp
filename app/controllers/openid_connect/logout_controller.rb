a985b0b90c023ac411db57eeee9df51ee4046a64
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-12-28 13:56:12 -0600

    Only sign out user if OIDC logout request was valid
    **Why**: So that user does not get signed out for invalid requests

--- app/controllers/openid_connect/logout_controller.rb
+++ app/controllers/openid_connect/logout_controller.rb
@@ -12,7 +12,7 @@ module OpenidConnect
       analytics.track_event(Analytics::LOGOUT_INITIATED, result.to_h.except(:redirect_uri))
 
       if (redirect_uri = result.extra[:redirect_uri])
-        sign_out
+        sign_out if result.success?
         redirect_to redirect_uri unless params['prevent_logout_redirect'] == 'true'
       else
         render :error
