f4eb47515bf4198608193d512d1324a27ddff37e
Zach Margolis <zachary.margolis@gsa.gov>
2022-01-03 12:10:23 -0800

    Move around call to #failure so it returns the right thing
    Co-authored-by: Andrew Duthie <aduth@users.noreply.github.com>

--- app/services/idv/steps/send_link_step.rb
+++ app/services/idv/steps/send_link_step.rb
@@ -54,10 +54,6 @@ module Idv
 
       def update_document_capture_session_requested_at(session_uuid)
         document_capture_session = DocumentCaptureSession.find_by(uuid: session_uuid)
-        if document_capture_session.cancelled_at
-          # See note at CancellationsController#cancel_document_capture_session
-          mark_step_incomplete(:link_sent)
-        end
         return unless document_capture_session
         document_capture_session.update!(
           requested_at: Time.zone.now,
