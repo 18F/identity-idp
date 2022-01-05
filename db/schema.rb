2624c449851e16179e44ea9114970088fc1f62a4
Mitchell Henke <mitchell.henke@gsa.gov>
2022-01-05 11:48:39 -0600

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_165320) do
+ActiveRecord::Schema.define(version: 2022_01_05_143347) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
