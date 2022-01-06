cdb973c705a65aecce03f14fbaf3d278c3eaef80
Zach Margolis <zachary.margolis@gsa.gov>
2022-01-06 14:16:11 -0800

    Match the right variable to the right source target

--- scripts/makefile_help.rb
+++ scripts/makefile_help.rb
@@ -3,20 +3,22 @@
 
 expanded_makefile = `make --dry-run --print-data-base --no-builtin-rules`
 
-# Map of lineno => rule name run by printing the Makefile database
+# Map of lineno => [[lineno, rule]] run by printing the Makefile database
 # rubocop:disable Layout/FirstArgumentIndentation
 # rubocop:disable Lint/MixedRegexpCaptureTypes
 expanded_targets = expanded_makefile.scan(
 /\n\n(?<target>[^:]+):( (?<sources>[^\n]+))?
 ((#  [^\n]+)?\n?)*?
 #  commands to execute \(from `Makefile', line (?<lineno>\d+)\):/m,
-).map { |target, *, lineno| [lineno.to_i, target] }.to_h
+).map { |target, *, lineno| [lineno.to_i, target] }.
+  group_by { |lineno, _target| lineno }
 # rubocop:enable Lint/MixedRegexpCaptureTypes
 # rubocop:enable Layout/FirstArgumentIndentation
 
 raw_makefile = File.readlines('Makefile')
 
 # Map of target => [comment, lineno] fom Makefile
+# target might have variables like $(HOST)
 target_comments = raw_makefile.map.with_index.select do |line, _lineno|
   line =~ / ## /
 end.flat_map do |line, lineno|
@@ -30,7 +32,18 @@ end.to_h
 cleaned_up_targets = {}
 
 target_comments.each do |target, (comment, lineno)|
-  cleaned_up_name = (target.include?('$(') && expanded_targets[lineno]) || target
+  cleaned_up_name = if target.include?('$(')
+    # "tmp/$(HOST)-$(PORT).crt" into %r|tmp/.+-.+.crt|
+    rule_regexp = Regexp.new(target.gsub(/(\$\([^\)]+\))/, '.+'))
+
+    _lineno, rule = expanded_targets[lineno].find do |_lineno, rule|
+      rule_regexp.match?(rule)
+    end
+
+    rule
+  end
+
+  cleaned_up_name ||= target
 
   cleaned_up_targets[cleaned_up_name] = comment
 end
