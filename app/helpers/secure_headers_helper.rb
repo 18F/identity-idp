78b6b8e2de2e1f441d9fa89cc05341ac8c30bb42
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-01-07 14:24:06 -0600

    fix secure header invocation in template

--- app/helpers/secure_headers_helper.rb
+++ app/helpers/secure_headers_helper.rb
@@ -6,4 +6,29 @@ module SecureHeadersHelper
       nonced_javascript_tag *args, **opts, &block
     end
   end
+
+  def add_document_capture_image_urls_to_csp(request, urls)
+    cleaned_urls = urls.compact.map do |url|
+      URI(url).tap { |uri| uri.query = nil }.to_s
+    end
+
+    if FeatureManagement.rails_csp_tooling_enabled?
+      add_document_capture_image_urls_to_csp_with_rails_csp_tooling(request, cleaned_urls)
+    else
+      add_document_capture_image_urls_to_csp_with_secure_headers(request, cleaned_urls)
+    end
+  end
+
+  def add_document_capture_image_urls_to_csp_with_secure_headers(request, urls)
+    SecureHeaders.append_content_security_policy_directives(
+      request,
+      connect_src: urls,
+    )
+  end
+
+  def add_document_capture_image_urls_to_csp_with_rails_csp_tooling(request, urls)
+    policy = current_content_security_policy
+    policy.connect_src(*urls)
+    request.content_security_policy = policy
+  end
 end
