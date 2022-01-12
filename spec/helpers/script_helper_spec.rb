d6e518c4c9aba8cf37739939163287308f49684e
Andrew Duthie <andrew.duthie@gsa.gov>
2022-01-12 09:20:30 -0500

    Persist Webpack assets manifest between requests
    **Why**: To avoid touching the filesystem for every request in production, since the manifest will never change during the lifetime of the application.

--- spec/helpers/script_helper_spec.rb
+++ spec/helpers/script_helper_spec.rb
@@ -3,19 +3,6 @@ require 'rails_helper'
 RSpec.describe ScriptHelper do
   include ScriptHelper
 
-  before do
-    allow_any_instance_of(ScriptHelper).to receive(:manifest).and_return(
-      'entrypoints' => {
-        'application' => {
-          'assets' => { 'js' => ['/packs/application.js', '/packs/application.en.js'] },
-        },
-        'document-capture' => {
-          'assets' => { 'js' => ['/packs/document-capture.js', '/packs/document-capture.en.js'] },
-        },
-      },
-    )
-  end
-
   describe '#javascript_include_tag_without_preload' do
     it 'avoids modifying headers' do
       javascript_include_tag_without_preload 'application'
@@ -43,32 +30,31 @@ RSpec.describe ScriptHelper do
       before do
         javascript_packs_tag_once('document-capture', 'document-capture')
         javascript_packs_tag_once('application', prepend: true)
+        allow(AssetSources).to receive(:get_sources).with('application', 'document-capture').
+          and_return(['/application.js', '/document-capture.js'])
       end
 
-      it 'prints all unique packs in order, locale scripts first' do
+      it 'prints script sources' do
         output = render_javascript_pack_once_tags
 
-        selectors = [
-          "script[src^='/packs/application.en.js']",
-          "script[src^='/packs/document-capture.en.js']",
-          "script[src^='/packs/application.js']",
-          "script[src^='/packs/document-capture.js']",
-        ]
-
-        selectors.each_with_index do |selector, i|
-          next_selector = selectors[i + 1]
-          test_selector = selector
-          test_selector += " ~ #{next_selector}" if next_selector
-          expect(output).to have_css(test_selector, count: 1, visible: false)
-        end
+        expect(output).to have_css(
+          "script[src^='/application.js'] ~ script[src^='/document-capture.js']",
+          count: 1,
+          visible: :all,
+        )
       end
     end
 
     context 'with named scripts argument' do
+      before do
+        allow(AssetSources).to receive(:get_sources).with('application').
+          and_return(['/application.js'])
+      end
+
       it 'enqueues those scripts before printing them' do
         output = render_javascript_pack_once_tags('application')
 
-        expect(output).to have_css('script[src="/packs/application.js"]', visible: :all)
+        expect(output).to have_css('script[src="/application.js"]', visible: :all)
       end
     end
 
---
+++ spec/lib/asset_sources_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+require 'asset_sources'
+require 'tempfile'
+
+RSpec.describe AssetSources do
+  include ActionView::Helpers::TranslationHelper
+
+  let(:manifest_file) { Tempfile.new }
+  let(:manifest_content) do
+    <<~STR
+      {
+        "entrypoints": {
+          "application": {
+            "assets": {
+              "js": [
+                "vendor.js",
+                "application.en.js",
+                "application.fr.js",
+                "application.es.js",
+                "application.js"
+              ]
+            }
+          },
+          "input": {
+            "assets": {
+              "js": [
+                "vendor.js",
+                "input.en.js",
+                "input.fr.js",
+                "input.es.js",
+                "input.js"
+              ]
+            }
+          }
+        }
+      }
+    STR
+  end
+
+  before do
+    File.open(manifest_file.path, 'w') { |f| f.puts manifest_content }
+    allow(AssetSources).to receive(:manifest_path).and_return(manifest_file.path)
+    allow(I18n).to receive(:available_locales).and_return([:en, :es, :fr])
+    allow(I18n).to receive(:locale).and_return(:en)
+  end
+
+  after do
+    manifest_file.unlink
+    AssetSources.manifest = nil
+  end
+
+  describe '.get_sources' do
+    it 'returns unique localized assets for existing sources, in order, localized scripts first' do
+      expect(AssetSources.get_sources('application', 'application', 'missing', 'input')).to eq [
+        'application.en.js',
+        'input.en.js',
+        'vendor.js',
+        'application.js',
+        'input.js',
+      ]
+    end
+
+    context 'unset manifest' do
+      let(:manifest_content) { nil }
+
+      it 'returns an empty array' do
+        expect(AssetSources.get_sources('missing')).to eq([])
+      end
+    end
+  end
+
+  describe '.load_manifest' do
+    context 'missing file' do
+      let(:manifest_content) { nil }
+
+      before do
+        manifest_file.unlink
+      end
+
+      it 'gracefully sets nil manifest' do
+        AssetSources.load_manifest
+
+        expect(AssetSources.manifest).to be_nil
+      end
+
+      context 'production environment' do
+        before do
+          allow(Rails.env).to receive(:production?).and_return(true)
+        end
+
+        it 'raises an exception' do
+          expect { AssetSources.load_manifest }.to raise_exception(Errno::ENOENT)
+        end
+      end
+    end
+
+    context 'invalid json' do
+      let(:manifest_content) { '{' }
+
+      it 'gracefully sets nil manifest' do
+        AssetSources.load_manifest
+
+        expect(AssetSources.manifest).to be_nil
+      end
+
+      context 'production environment' do
+        before do
+          allow(Rails.env).to receive(:production?).and_return(true)
+        end
+
+        it 'raises an exception' do
+          expect { AssetSources.load_manifest }.to raise_exception(JSON::ParserError)
+        end
+      end
+    end
+  end
+end
