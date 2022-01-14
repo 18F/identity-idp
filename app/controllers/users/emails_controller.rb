b3e66089ed709904fd3ce793e5958b245f05bc5e
SammySteiner <sammysteiner@gmail.com>
2022-01-14 14:06:04 -0500

    flash error on max email check

--- app/controllers/users/emails_controller.rb
+++ app/controllers/users/emails_controller.rb
@@ -93,6 +93,7 @@ module Users
 
     def check_max_emails_per_account
       return if EmailPolicy.new(current_user).can_add_email?
+      flash[:email_error] = t('users.emails.error_message')
       redirect_to account_url
     end
 
