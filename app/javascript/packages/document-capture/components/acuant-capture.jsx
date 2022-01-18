18F/identity-idp

commit 783ad61621de3cd489e7e836a61443e047beef00

Change useCookie hook to getter, avoid global override (#5778)
* Change useCookie hook to getter, avoid global override

**Why**: As observed in #5747, the current logic can be prone to race conditions when multiple instances of the hook exist (mostly affecting specs). The implementation overriding the global was meant to support a use-case where a component would re-render in response to an external script modifying a cookie (e.g. Acuant SDK). Since our current sole usage is to get the cookie value at the time of a user initiating a capture, this live-updating support isn't necessary and unnecessarily complicates the implementation.

* Add test case for multi-instance synced value

* Implement cookie store as shared context

app/javascript/packages/document-capture/hooks/use-cookie.js
@@ -1,4 +1,13 @@
-import { useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
+
+/** @typedef {import('react').Dispatch<A>} Dispatch @template A */
+/** @typedef {import('react').SetStateAction<S>} SetStateAction @template S */
+
+/**
+ * @typedef {Dispatch<SetStateAction<string|null>>[]} Subscribers
+ */
+
+const CookieSubscriberContext = createContext(/** @type {Map<string, Subscribers>} */ (new Map()));
 
 /**
  * React hook to access and manage a cookie value by name.
@@ -8,13 +17,30 @@ import { useState, useEffect } from 'react';
  * @return {[value: string|null, setValue: (nextValue: string?) => void]}
  */
 function useCookie(name) {
-  const getCookieValue = () =>
+  const getValue = () =>
     document.cookie
       .split(';')
       .map((part) => part.trim().split('='))
       .find(([key]) => key === name)?.[1] ?? null;
 
-  const [value, setStateValue] = useState(getCookieValue);
+  const subscriptions = useContext(CookieSubscriberContext);
+  const [value, setStateValue] = useState(getValue);
+
+  useEffect(() => {
+    if (!subscriptions.has(name)) {
+      subscriptions.set(name, []);
+    }
+
+    const subscribers = /** @type {Subscribers} */ (subscriptions.get(name));
+    subscribers.push(setStateValue);
+
+    return () => {
+      subscribers.splice(subscribers.indexOf(setStateValue), 1);
+      if (!subscribers.length) {
+        subscriptions.delete(name);
+      }
+    };
+  }, [name]);
 
   /**
    * @param {string?} nextValue Value to set, or null to delete the value.
@@ -22,27 +48,9 @@ function useCookie(name) {
   function setValue(nextValue) {
     const cookieValue = nextValue === null ? '; Max-Age=0' : nextValue;
     document.cookie = `${name}=${cookieValue}`;
-    setStateValue(nextValue);
+    const subscribers = /** @type {Subscribers} */ (subscriptions.get(name));
+    subscribers.forEach((setSubscriberValue) => setSubscriberValue(getValue));
   }
-
-  useEffect(() => {
-    const originalCookieDescriptor = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
-    Object.defineProperty(Document.prototype, 'cookie', {
-      ...originalCookieDescriptor,
-      set(nextValue) {
-        originalCookieDescriptor?.set?.call(this, nextValue);
-        setStateValue(getCookieValue);
-      },
-    });
-
-    return () => {
-      Object.defineProperty(
-        Document.prototype,
-        'cookie',
-        /** @type {PropertyDescriptor} */ (originalCookieDescriptor),
-      );
-    };
-  }, []);
 
   return [value, setValue];
 }

spec/javascripts/packages/document-capture/hooks/use-cookie-spec.jsx
@@ -1,3 +1,4 @@
+import sinon from 'sinon';
 import { renderHook } from '@testing-library/react-hooks';
 import useCookie from '@18f/identity-document-capture/hooks/use-cookie';
 
@@ -14,20 +15,15 @@ describe('document-capture/hooks/use-cookie', () => {
     expect(value).to.equal('baz');
   });
 
-  it('does not interfere with default cookie setting behavior', () => {
-    renderHook(() => useCookie('foo'));
-
-    document.cookie = 'foo=bar';
-
-    expect(document.cookie).to.equal('foo=bar');
-  });
-
   it('sets a new cookie value', () => {
-    const { result } = renderHook(() => useCookie('foo'));
+    const render = sinon.stub().callsFake(() => useCookie('foo'));
+    const { result } = renderHook(render);
 
     const [, setValue] = result.current;
 
+    render.resetHistory();
     setValue('bar');
+    expect(render).to.have.been.calledOnce();
 
     const [value] = result.current;
 
@@ -38,15 +34,39 @@ describe('document-capture/hooks/use-cookie', () => {
   it('unsets a cookie value by null', () => {
     document.cookie = 'foo=bar';
 
-    const { result } = renderHook(() => useCookie('foo'));
+    const render = sinon.stub().callsFake(() => useCookie('foo'));
+    const { result } = renderHook(render);
 
     const [, setValue] = result.current;
 
+    render.resetHistory();
     setValue(null);
+    expect(render).to.have.been.calledOnce();
 
     const [value] = result.current;
 
     expect(document.cookie).to.equal('');
     expect(value).to.be.null();
+  });
+
+  it('returns the same updated value between instances', () => {
+    const render1 = sinon.stub().callsFake(() => useCookie('foo'));
+    const render2 = sinon.stub().callsFake(() => useCookie('foo'));
+    const { result: result1 } = renderHook(render1);
+    const { result: result2 } = renderHook(render2);
+
+    const [, setValue] = result1.current;
+
+    render1.resetHistory();
+    render2.resetHistory();
+    setValue('bar');
+    expect(render1).to.have.been.calledOnce();
+    expect(render2).to.have.been.calledOnce();
+
+    const [value1] = result1.current;
+    const [value2] = result2.current;
+
+    expect(value1).to.equal('bar');
+    expect(value2).to.equal('bar');
   });
 });

spec/javascripts/support/console.js
@@ -44,15 +44,17 @@ export function chaiConsoleSpy(chai, utils) {
  * `chaiConsoleSpy` Chai plugin.
  */
 export function useConsoleLogSpy() {
+  let originalConsoleError;
   beforeEach(() => {
     console.unverifiedCalls = [];
-    sinon.stub(console, 'error').callsFake((message, ...args) => {
+    originalConsoleError = console.error;
+    console.error = sinon.stub().callsFake((message, ...args) => {
       console.unverifiedCalls = console.unverifiedCalls.concat(format(message, ...args));
     });
   });
 
   afterEach(() => {
-    console.error.restore();
+    console.error = originalConsoleError;
     expect(console.unverifiedCalls).to.be.empty(
       `Unexpected console logging: ${console.unverifiedCalls.join(', ')}`,
     );

commit b707ed2b8714818f8d54cf7e36eb4da9254b1f8f

Fix Acuant sequence-break error cookie sync issue
**Why**: To fix a bug where if the Acuant camera fails to start due to iOS 15 "sequence-break" error, subsequent attempts to capture would result in the captured image not being set correctly.

This fixes a regression introduced in #5778 where the cookie value tracked by AcuantCapture falls out of date after Acuant handles the error, since we never refresh the value after it is [set internally by Acuant's error handling](https://github.com/Acuant/JavascriptWebSDKV11/blob/9a5387576a33710188501ad6233f986b1b6bb1cb/SimpleHTMLApp/webSdk/dist/AcuantCamera.js#L616). Thus, the next time the user clicks  "Take Photo", the [cookie value logic](https://github.com/18F/identity-idp/blob/eceea0f4df59cf3bb934b561e1d141edf1276174/app/javascript/packages/document-capture/components/acuant-capture.jsx#L400) is out-of-date, and Acuant capture is started instead of the default manual capture handling.

To resolve this, we anticipate that Acuant would set the cookie in their error handling of sequence-break, and refresh the cookie value when we get a chance to handle the error in our own code.

app/javascript/packages/document-capture/components/acuant-capture.jsx
@@ -283,7 +283,9 @@ function AcuantCapture(
   const { isMobile } = useContext(DeviceContext);
   const { t, formatHTML } = useI18n();
   const [attempt, incrementAttempt] = useCounter(1);
-  const [acuantFailureCookie, setAcuantFailureCookie] = useCookie('AcuantCameraHasFailed');
+  const [acuantFailureCookie, setAcuantFailureCookie, refreshAcuantFailureCookie] = useCookie(
+    'AcuantCameraHasFailed',
+  );
   const { onFailedCaptureAttempt, onResetFailedCaptureAttempts } = useContext(
     FailedCaptureAttemptsContext,
   );
@@ -533,6 +535,8 @@ function AcuantCapture(
                   .split(' ')
                   .join(NBSP_UNICODE)}`,
               );
+
+              refreshAcuantFailureCookie();
             } else {
               setOwnErrorMessage(t('doc_auth.errors.camera.failed'));
             }

app/javascript/packages/document-capture/hooks/use-cookie.js
@@ -14,7 +14,7 @@ const CookieSubscriberContext = createContext(/** @type {Map<string, Subscribers
  *
  * @param {string} name Cookie name.
  *
- * @return {[value: string|null, setValue: (nextValue: string?) => void]}
+ * @return {[value: string|null, setValue: (nextValue: string?) => void, refreshValue: () => void]}
  */
 function useCookie(name) {
   const getValue = () =>
@@ -43,16 +43,23 @@ function useCookie(name) {
   }, [name]);
 
   /**
+   * Refresh cookie value for all current subscribers.
+   */
+  function refreshValue() {
+    const subscribers = /** @type {Subscribers} */ (subscriptions.get(name));
+    subscribers.forEach((setSubscriberValue) => setSubscriberValue(getValue));
+  }
+
+  /**
    * @param {string?} nextValue Value to set, or null to delete the value.
    */
   function setValue(nextValue) {
     const cookieValue = nextValue === null ? '; Max-Age=0' : nextValue;
     document.cookie = `${name}=${cookieValue}`;
-    const subscribers = /** @type {Subscribers} */ (subscriptions.get(name));
-    subscribers.forEach((setSubscriberValue) => setSubscriberValue(getValue));
+    refreshValue();
   }
 
-  return [value, setValue];
+  return [value, setValue, refreshValue];
 }
 
 export default useCookie;

spec/javascripts/packages/document-capture/components/acuant-capture-spec.jsx
@@ -260,7 +260,13 @@ describe('document-capture/components/acuant-capture', () => {
       );
 
       initialize({
-        start: sinon.stub().callsArgWithAsync(1, 'iOS 15 sequence break', 'sequence-break-code'),
+        start: sinon.stub().callsFake((_callbacks, onError) => {
+          setTimeout(() => {
+            const code = 'sequence-break-code';
+            document.cookie = `AcuantCameraHasFailed=${code}`;
+            onError('iOS 15 sequence break', code);
+          }, 0);
+        }),
       });
 
       const button = getByLabelText('Image');
@@ -274,6 +280,12 @@ describe('document-capture/components/acuant-capture', () => {
         payload: { field: 'test', error: 'iOS 15 GPU Highwater failure (SEQUENCE_BREAK_CODE)' },
       });
       expect(document.activeElement).to.equal(button);
+
+      const defaultPrevented = !fireEvent.click(button);
+
+      window.AcuantCameraUI.start.resetHistory();
+      expect(defaultPrevented).to.be.false();
+      expect(window.AcuantCameraUI.start.called).to.be.false();
     });
 
     it('calls onCameraAccessDeclined if camera access is declined', async () => {

spec/javascripts/packages/document-capture/hooks/use-cookie-spec.jsx
@@ -69,4 +69,21 @@ describe('document-capture/hooks/use-cookie', () => {
     expect(value1).to.equal('bar');
     expect(value2).to.equal('bar');
   });
+
+  it('refreshes a cookie value', () => {
+    const render = sinon.stub().callsFake(() => useCookie('foo'));
+    const { result } = renderHook(render);
+
+    document.cookie = 'foo=bar';
+
+    const [, , refreshValue] = result.current;
+
+    render.resetHistory();
+    refreshValue();
+    expect(render).to.have.been.calledOnce();
+
+    const [value] = result.current;
+
+    expect(value).to.equal('bar');
+  });
 });