6e0eff0b46803b5ada2b8494fe0e607efc142c4f
Sammy <sammysteiner@gmail.com>
2022-01-20 12:32:34 -0500

    chainable matcher
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- spec/features/multiple_emails/add_email_spec.rb
+++ spec/features/multiple_emails/add_email_spec.rb
@@ -114,7 +114,7 @@ feature 'adding email address' do
     visit add_email_path
     puts current_path
     expect(page).to have_current_path(account_path)
-    expect(page.has_content? t('users.emails.error_message'))
+    expect(page).to have_content t('users.emails.error_message')
   end
 
   it 'stays on form with bad email' do
