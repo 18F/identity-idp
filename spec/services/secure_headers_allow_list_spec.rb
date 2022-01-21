1e1a75ad51c6626859bfa5a2a8d50cd9817373d5
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-01-21 13:07:22 -0500

    expand the unit tests

--- spec/services/secure_headers_allow_list_spec.rb
+++ spec/services/secure_headers_allow_list_spec.rb
@@ -20,5 +20,62 @@ RSpec.describe SecureHeadersAllowList do
         expect(csp_with_sp_redirect_uris(domain, nil)).to eq(full_return[0..1])
       end
     end
+
+    it 'properly reduces web uris' do
+      redirect_uri = 'https://example1.com/auth/result'
+      allowed_redirect_uris = [
+        'https://example1.com/auth/result',
+        'https://example1.com/',
+        'http://example2.com/',
+        'https://example3.com:3000/',
+      ]
+
+      result = csp_with_sp_redirect_uris(redirect_uri, allowed_redirect_uris)
+
+      expect(result).to match_array(
+        ["'self'", 'https://example1.com', 'http://example2.com', 'https://example3.com:3000'],
+      )
+    end
+
+    it 'properly reduces mobile uris' do
+      redirect_uri = 'mymobileapp://result'
+      allowed_redirect_uris = [
+        'mymobileapp://result',
+        'mymobileapp://result2',
+        'myothermobileapp://result',
+        'https://example.com/',
+      ]
+
+      result = csp_with_sp_redirect_uris(redirect_uri, allowed_redirect_uris)
+
+      expect(result).to match_array(
+        ["'self'", 'mymobileapp://', 'myothermobileapp://', 'https://example.com'],
+      )
+    end
+
+    it 'handles nil sp_redirect_uris' do
+      redirect_uri = 'https://example.com/auth/result'
+
+      result = csp_with_sp_redirect_uris(redirect_uri, nil)
+
+      expect(result).to match_array(
+        ["'self'", 'https://example.com'],
+      )
+    end
+
+    it 'handles sp_redirect_uris with nil elements' do
+      redirect_uri = 'https://example1.com/auth/result'
+      allowed_redirect_uris = [
+        'https://example1.com/auth/result',
+        nil,
+        'http://example2.com/',
+      ]
+
+      result = csp_with_sp_redirect_uris(redirect_uri, allowed_redirect_uris)
+
+      expect(result).to match_array(
+        ["'self'", 'https://example1.com', 'http://example2.com'],
+      )
+    end
   end
 end
