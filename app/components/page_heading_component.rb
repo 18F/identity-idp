d8ecc7d37e8cd166a1ac82b42715a1ea49fb9647
Andrew Duthie <andrew.duthie@gsa.gov>
2022-01-24 14:02:51 -0500

    LG-4792: Use component for consistent headings (#5840)
    * LG-4792: Use component for consistent headings
    
    **Why**: To avoid visual inconsistencies, to simplify and streamline developer experience, to create a single source of truth for common heading styles, and to create alignment between Rails and React heading implementations.
    
    * Appease Brakeman
    
    * Fix additional heading spacing
    
    * Resolve more heading + content margin inconsistencies
    
    * Revert PageHeading usage on "pages" views
    
    **Why**: Because these pages don't use the base layout, so accompanying styles don't exist anyways.

---
+++ app/components/page_heading_component.rb
@@ -0,0 +1,11 @@
+class PageHeadingComponent < BaseComponent
+  attr_reader :tag_options
+
+  def initialize(**tag_options)
+    @tag_options = tag_options
+  end
+
+  def call
+    tag.h1 content, **tag_options, class: ['page-heading', *tag_options[:class]]
+  end
+end
