91e695ed4e37f3c121810e949f6372c46dcb04b4
Andrew Duthie <andrew.duthie@gsa.gov>
2022-01-28 13:49:26 -0500

    Re-add parens for safe_join
    See: https://github.com/18F/identity-idp/pull/5869#discussion_r794681077
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>

--- app/components/clipboard_button_component.rb
+++ app/components/clipboard_button_component.rb
@@ -13,9 +13,11 @@ class ClipboardButtonComponent < ButtonComponent
   end
 
   def content
-    safe_join [
-      render(IconComponent.new(icon: :content_copy, class: 'position-absolute')),
-      content_tag(:span, t('links.copy'), class: 'padding-left-3'),
-    ]
+    safe_join(
+      [
+        render(IconComponent.new(icon: :content_copy, class: 'position-absolute')),
+        content_tag(:span, t('links.copy'), class: 'padding-left-3'),
+      ],
+    )
   end
 end
