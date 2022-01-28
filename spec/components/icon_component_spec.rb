acff47184fdadbe4315544655b74ac22e8690590
Andrew Duthie <andrew.duthie@gsa.gov>
2022-01-28 13:47:07 -0500

    Validate icon in IconComponent
    See: https://github.com/18F/identity-idp/pull/5869#discussion_r794684997

--- spec/components/icon_component_spec.rb
+++ spec/components/icon_component_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe IconComponent, type: :component do
     expect(rendered).to have_css('.usa-icon use[href$=".svg#print"]')
   end
 
+  context 'with invalid icon' do
+    it 'raises an error' do
+      expect { render_inline IconComponent.new(icon: :foo) }.to raise_error(ArgumentError)
+    end
+  end
+
   context 'with custom class' do
     it 'renders with class' do
       rendered = render_inline IconComponent.new(icon: :print, class: 'my-custom-class')
