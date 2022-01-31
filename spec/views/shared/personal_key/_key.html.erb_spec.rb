53b1a6aca12bcb97cc3943d184c978637afb336c
Andrew Duthie <andrew.duthie@gsa.gov>
2022-01-31 09:37:44 -0500

    Update key view spec to tolerate whitespace

--- spec/views/shared/personal_key/_key.html.erb_spec.rb
+++ spec/views/shared/personal_key/_key.html.erb_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe 'partials/personal_key/_key.html.erb' do
   let(:code) { 'abcd-efgh-ijkl-mnop' }
 
-  it 'renders the code without whitespace between segments' do
+  it 'renders the code segments' do
     render 'partials/personal_key/key', code: code
 
     doc = Nokogiri::HTML(rendered)
-    expect(doc.text).to include('abcdefghijklmnop')
+    segments = doc.css('[data-personal-key]').map(&:text)
+    expect(segments.join).to eq('abcdefghijklmnop')
   end
 end
