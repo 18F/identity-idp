1439ec73c506bd8089d69432ceb6d72581bebf2b
Alex Bradley <alexander.bradley@gsa.gov>
2022-02-22 17:32:19 -0500

    fixup

--- spec/requests/secure_cookies_spec.rb
+++ spec/requests/secure_cookies_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'secure cookies' do
   it 'flags all cookies sent by the application as secure, httponly, and lax' do
     get root_url
+
     expect(response.headers['Set-Cookie']).to include('; Secure')
     expect(response.headers['Set-Cookie']).to include('; HttpOnly')
     expect(response.headers['Set-Cookie']).to include('; SameSite=Lax')
