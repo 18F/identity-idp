a83206e0ea01c71e9d94a708c55f270a3161b14c
Alex Bradley <alexander.bradley@gsa.gov>
2022-02-23 16:54:51 -0500

    make sure test checks that all cookies are secure

--- spec/requests/secure_cookies_spec.rb
+++ spec/requests/secure_cookies_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
+COOKIE_SEPARATOR = "\n".freeze
 
 RSpec.describe 'secure cookies' do
   it 'flags all cookies sent by the application as secure, httponly, and lax' do
     get root_url
+    cookie_count = response.headers['Set-Cookie'].split(COOKIE_SEPARATOR).count
 
-    expect(response.headers['Set-Cookie']).to include('; Secure')
-    expect(response.headers['Set-Cookie']).to include('; HttpOnly')
-    expect(response.headers['Set-Cookie']).to include('; SameSite=Lax')
+    expect(response.headers['Set-Cookie'].scan('; Secure').size).to eq(cookie_count)
+    expect(response.headers['Set-Cookie'].scan('; HttpOnly').size).to eq(cookie_count)
+    expect(response.headers['Set-Cookie'].scan('; SameSite=Lax').size).to eq(cookie_count)
   end
 end
