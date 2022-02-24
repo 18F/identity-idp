2100b7b25af1773f310f7e7667bf579b892d01b9
Andrew Duthie <andrew.duthie@gsa.gov>
2022-02-24 08:48:42 -0500

    LG-3879: Use design system grid for account header (#5953)
    * LG-3879: Use design system grid for account header
    
    **Why**: To simplify markup and to standardize on design system utilities.
    
    * Center account header
    
    * Configure design system grid with to tablet-lg
    
    * Remove unnecessary footer container
    
    Content is already padded, centered within
    
    * Standardize account grid breakpoint at tablet
    
    * Add BassCSS grid to deprecated classes
    
    * Allow container class
    
    Still used in email stylesheets
    
    * Fix last stragglers for full-width input
    
    * Remove BassCSS grid styles
    
    * Add changelog
    
    changelog: Improvements, Layout, Use design system for layout
    
    * Improve account badges tablet+ relayout

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -27,7 +27,7 @@ SimpleForm.setup do |config|
     b.optional :readonly
     b.use :label, class: 'text-bold'
     b.use :hint,  wrap_with: { tag: 'div', class: 'usa-hint' }
-    b.use :input, class: 'display-block col-12 field', error_class: 'usa-input--error'
+    b.use :input, class: 'display-block width-full field', error_class: 'usa-input--error'
     b.use :error, wrap_with: { tag: 'div', class: 'usa-error-message' }
   end
 
