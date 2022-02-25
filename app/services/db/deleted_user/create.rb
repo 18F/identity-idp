6a7d7783dfccf32158fab11bef7bed24a8f929c5
Mitchell Henke <mitchell.henke@gsa.gov>
2022-02-25 11:14:55 -0600

    rollback transaction if DeletedUser exists

--- app/services/db/deleted_user/create.rb
+++ app/services/db/deleted_user/create.rb
@@ -4,14 +4,19 @@ module Db
       def self.call(user_id)
         user = User.find_by(id: user_id)
         return unless user
-        ::DeletedUser.create!(
-          user_id: user.id,
-          uuid: user.uuid,
-          user_created_at: user.created_at,
-          deleted_at: Time.zone.now,
-        )
-      rescue ActiveRecord::RecordNotUnique
-        ::DeletedUser.where(user_id: user_id)&.first
+
+        ActiveRecord::Base.transaction(requires_new: true) do
+          ::DeletedUser.create!(
+            user_id: user.id,
+            uuid: user.uuid,
+            user_created_at: user.created_at,
+            deleted_at: Time.zone.now,
+          )
+        rescue ActiveRecord::RecordNotUnique
+          raise ActiveRecord::Rollback
+        end
+
+        nil
       end
     end
   end
