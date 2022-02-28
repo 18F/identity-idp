c8ddd974b7914c6f3d5ac92b36a5dea48c035708
Andrew Duthie <andrew.duthie@gsa.gov>
2022-02-28 16:05:31 -0500

    Improve JS string extraction to exclude template literals
    **Why**: Due to how the plugin operates on the post-transpiled code, string template literals may be wrongly considered as candidates for extraction, despite the fact that the complete string includes runtime logic (`String#concat`) which cannot be accounted for at the extraction phase.

--- app/javascript/packages/rails-i18n-webpack-plugin/spec/fixtures/in1.js
+++ app/javascript/packages/rails-i18n-webpack-plugin/spec/fixtures/in1.js
@@ -10,3 +10,6 @@ const message = t('forms.messages', { count: 2 });
  * i18n-tasks-use t('item.3')
  */
 Array.from({ length: 3 }, (_, i) => t(`item.${i + 1}`));
+// Emulate Babel template literal transpilation
+// See: https://babeljs.io/repl#?browsers=ie%2011&code_lz=C4CgBglsCmC2B0ASA3hABAajQRgL5gEog
+Array.from({ length: 3 }, (_, i) => t('item.'.concat(i + 1)));
