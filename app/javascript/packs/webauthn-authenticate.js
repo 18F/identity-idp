4318192435e4e34d70c9850bb1cd4decb09d6b92
Malick Diarra <malick.diarra@gsa.gov>
2022-03-01 08:57:19 -0500

    LG-5515: webauthn authenticaate support safari

--- app/javascript/packs/webauthn-authenticate.js
+++ app/javascript/packs/webauthn-authenticate.js
@@ -1,47 +1,47 @@
 const WebAuthn = require('../app/webauthn');
 
-async function webauthn() {
-  // If webauthn is not supported redirect back to the 2fa options list
+function webauthn() {
   const webauthnInProgressContainer = document.getElementById('webauthn-auth-in-progress');
   const webauthnSuccessContainer = document.getElementById('webauthn-auth-successful');
-
+  
   const webauthnPlatformRequested =
     webauthnInProgressContainer.dataset.platformAuthenticatorRequested === 'true';
   const multipleFactorsEnabled =
     webauthnInProgressContainer.dataset.multipleFactorsEnabled === 'true';
-  const webauthnPlatformEnabled = await window.PublicKeyCredential?.isUserVerifyingPlatformAuthenticatorAvailable();
-  console.log(webauthnPlatformEnabled)
-  if (
-    !WebAuthn.isWebAuthnEnabled() ||
-    (webauthnPlatformRequested && !webauthnPlatformEnabled && !multipleFactorsEnabled)
-  ) {
-    const href = webauthnInProgressContainer.getAttribute('data-webauthn-not-enabled-url');
-    window.location.href = href;
-    return;
-  }
+  const isPlatformAvailable = document.getElementById('webauthn_device').value === 'true';
+
 
   const spinner = document.getElementById('spinner');
   spinner.classList.remove('hidden');
 
-  // if platform auth is not supported on device, we should take user to the error screen if theres no additional methods.
-  WebAuthn.verifyWebauthnDevice({
-    userChallenge: document.getElementById('user_challenge').value,
-    credentialIds: document.getElementById('credential_ids').value,
-  })
-    .then((result) => {
-      document.getElementById('credential_id').value = result.credentialId;
-      document.getElementById('authenticator_data').value = result.authenticatorData;
-      document.getElementById('client_data_json').value = result.clientDataJSON;
-      document.getElementById('signature').value = result.signature;
-      webauthnInProgressContainer.classList.add('hidden');
-      webauthnSuccessContainer.classList.remove('hidden');
+  if (
+    !WebAuthn.isWebAuthnEnabled() ||
+    (webauthnPlatformRequested && !isPlatformAvailable && !multipleFactorsEnabled)
+  ) {
+    const href = webauthnInProgressContainer.getAttribute('data-webauthn-not-enabled-url');
+    window.location.href = href;
+  } else {
+    // if platform auth is not supported on device, we should take user to the error screen if theres no additional methods.
+    // if platform auth is not supported on device, we should take user to the error screen if theres no additional methods.
+    WebAuthn.verifyWebauthnDevice({
+      userChallenge: document.getElementById('user_challenge').value,
+      credentialIds: document.getElementById('credential_ids').value,
     })
-    .catch((error) => {
-      debugger
-      document.getElementById('errors').value = error;
-      document.getElementById('platform').value = webauthnPlatformRequested;
-      document.getElementById('webauthn_form').submit();
-    });
+      .then((result) => {
+        document.getElementById('credential_id').value = result.credentialId;
+        document.getElementById('authenticator_data').value = result.authenticatorData;
+        document.getElementById('client_data_json').value = result.clientDataJSON;
+        document.getElementById('signature').value = result.signature;
+        webauthnInProgressContainer.classList.add('hidden');
+        webauthnSuccessContainer.classList.remove('hidden');
+      })
+      .catch((error) => {
+        debugger;
+        document.getElementById('errors').value = error;
+        document.getElementById('platform').value = webauthnPlatformRequested;
+        document.getElementById('webauthn_form').submit();
+      });
+  } 
 }
 
 function webauthnButton() {
@@ -49,4 +49,12 @@ function webauthnButton() {
   button.addEventListener('click', webauthn);
 }
 
+function isPlatformAuthenticatorAvailable() {
+  debugger
+  return window.PublicKeyCredential?.isUserVerifyingPlatformAuthenticatorAvailable().then((result) => { 
+    document.getElementById('webauthn_device').value = result
+  });
+}
+
 document.addEventListener('DOMContentLoaded', webauthnButton);
+document.addEventListener('DOMContentLoaded', isPlatformAuthenticatorAvailable);
