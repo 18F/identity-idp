d1cf5131759313ccfac4089293f828d2cc210bc5
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-03-03 09:10:14 -0800

    Fix links on SMS opt-in pages (#6029)
    * Remove "get help at SP" link from SMS opt-in page
    * Fix cancel link destination on SMS opt-in pages
    
    changelog: Bug Fixes, Two-Factor Authentication, Update links on SMS opt-in page

--- spec/views/two_factor_authentication/sms_opt_in/error.html.erb_spec.rb
+++ spec/views/two_factor_authentication/sms_opt_in/error.html.erb_spec.rb
@@ -3,16 +3,12 @@ require 'rails_helper'
 RSpec.describe 'two_factor_authentication/sms_opt_in/error.html.erb' do
   let(:phone_configuration) { build(:phone_configuration, phone: '1 888-867-5309') }
   let(:other_mfa_options_url) { nil }
-  let(:cancel_url) { nil }
-
-  let(:sp_name) { nil }
-  let(:decorated_session) { instance_double('SessionDecorator', sp_name: sp_name) }
+  let(:cancel_url) { '/account' }
 
   before do
     assign(:phone_configuration, phone_configuration)
     assign(:other_mfa_options_url, other_mfa_options_url)
     assign(:cancel_url, cancel_url)
-    allow(view).to receive(:decorated_session).and_return(decorated_session)
     allow(view).to receive(:user_signing_up?).and_return(false)
   end
 
@@ -54,34 +50,6 @@ RSpec.describe 'two_factor_authentication/sms_opt_in/error.html.erb' do
           t('two_factor_authentication.login_options_link_text'),
           href: other_mfa_options_url,
         )
-      end
-    end
-
-    context 'with an sp' do
-      let(:sp_name) { 'An Example SP' }
-
-      it 'links to the sp' do
-        render
-
-        expect(rendered).to have_link(
-          t('idv.troubleshooting.options.get_help_at_sp', sp_name: sp_name),
-        )
-      end
-    end
-
-    context 'without an sp' do
-      let(:sp_name) { nil }
-
-      it 'has a contact support but not a "Get Help At" link' do
-        render
-
-        expect(rendered).to have_link(
-          t('links.contact_support', app_name: APP_NAME),
-          href: MarketingSite.contact_url,
-        )
-
-        doc = Nokogiri::HTML(rendered)
-        expect(doc.css('.troubleshooting-options__options li').length).to eq(1)
       end
     end
   end
