8b1ba17c77d6f5489821c01a0f2741057dbf67a6
Steve Urciuoli <steve.urciuoli@gsa.gov>
2022-03-08 15:48:20 -0600

    Remove the FSM prefix from analytics.rb

--- app/controllers/idv/in_person_controller.rb
+++ app/controllers/idv/in_person_controller.rb
@@ -9,7 +9,7 @@ module Idv
       step_url: :idv_in_person_step_url,
       final_url: :account_url,
       flow: Idv::Flows::InPersonFlow,
-      analytics_id: Analytics::IN_PERSON_PROOFING,
+      analytics_id: 'In Person Proofing',
     }.freeze
 
     def render_404_if_disabled
