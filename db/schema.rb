b4d4740e3987d619af84c998d4e2d8bb8e93676e
Justin Grevich <justin@grevi.ch>
2022-03-09 23:35:03 -0800

    Modify autovacuum config for devices and users
    changelog: Internal, Database, Modify autovacuum config for devices and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_224754) do
+ActiveRecord::Schema.define(version: 2022_03_07_214616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
