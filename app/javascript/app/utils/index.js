ef0457de718d315d095b2c15e1efeb0f34554e23
Andrew Duthie <andrew.duthie@gsa.gov>
2022-03-15 15:59:54 -0400

    Reimplement timeout countdown as custom element (#6023)
    * Support .ts files for BaseComponent sidecar script
    
    * Reimplement timeout countdown as custom element
    
    **Why**:
    
    - Reduce size and scope of hot-path application pack
    - Avoid relying on window globals (LoginGov.countdownTimer), since they rely on fragile dependency order
    - Improve testability of element behavior in isolation
    - Allow reuse for common countdown behavior
    
    changelog: Internal, Optimziation, Reduce size of common JavaScript file
    
    * Initialize modal presenter with view_context
    
    Avoid passing from view
    
    See: https://github.com/18F/identity-idp/pull/6023#discussion_r817995755
    
    * Move countdown tag contructor to call
    
    - Simplify tests to avoid accounting for newline
    - Because it's single, simple tag
    
    See: https://github.com/18F/identity-idp/pull/6023#discussion_r817996662
    
    * Render component using view_context.render
    
    **Why**: Avoid errors in runtime
    
    * Convert dataset to getAttribute
    
    Seemingly more compatible with element lifecycle, where dataset can be undefined prior to element being connected. For example, when `define`-ing the custom element, the attributeChangedCallback is triggered, but the element isn't yet mounted.
    
    * Fix lint errors
    
    * Use static variable syntax vs. getter
    
    Example snippets often use getter syntax, since it's better supported in older browsers. But since we transpile, and since reasonably new browsers support static variables, we can update to simplify.
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static
    
    * Guarantee idempotence of CountdownElement#start
    
    * Update countdown text via Text#nodeValue
    
    **Why**: To restore previous behavior of screen reader atomic announcement including time remaining.

--- app/javascript/app/utils/index.js
+++
@@ -1,8 +0,0 @@
-import { countdownTimer } from './countdown-timer';
-import { msFormatter } from './ms-formatter';
-
-window.LoginGov = window.LoginGov || {};
-const { LoginGov } = window;
-
-LoginGov.countdownTimer = countdownTimer;
-LoginGov.msFormatter = msFormatter;
--- spec/presenters/partially_signed_in_modal_presenter_spec.rb
+++ spec/presenters/partially_signed_in_modal_presenter_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 describe PartiallySignedInModalPresenter do
-  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::SanitizeHelper
 
-  let(:time_left_in_session) { 10 }
-  subject(:presenter) { PartiallySignedInModalPresenter.new(time_left_in_session) }
+  let(:expiration) { Time.zone.now + 1.minute + 1.second }
+  let(:lookup_context) { ActionView::LookupContext.new(ActionController::Base.view_paths) }
+  let(:view_context) { ActionView::Base.new(lookup_context, {}, nil) }
+  subject(:presenter) do
+    PartiallySignedInModalPresenter.new(view_context: view_context, expiration: expiration)
+  end
+
+  around do |ex|
+    freeze_time { ex.run }
+  end
 
   describe '#message' do
     it 'returns the partially signed in message' do
-      message = t(
+      expect(strip_tags(presenter.message)).to eq t(
         'notices.timeout_warning.partially_signed_in.message_html',
-        time_left_in_session: content_tag(:span, time_left_in_session, id: 'countdown'),
+        time_left_in_session: '1 minute and 1 second',
       )
+    end
+  end
 
-      expect(presenter.message).to eq message
+  describe '#sr_message' do
+    it 'returns the partially signed in message for screen readers' do
+      expect(strip_tags(presenter.sr_message)).to eq t(
+        'notices.timeout_warning.partially_signed_in.sr_message_html',
+        time_left_in_session: '1 minute and 1 second',
+      )
     end
   end
 
