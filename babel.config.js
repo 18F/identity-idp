f6922fae4ad86c769de03e653c7e67cbab7f811e
Andrew Duthie <andrew.duthie@gsa.gov>
2022-03-16 09:24:51 -0400

    Exclude core-js polyfills from ESM-capable browser bundles (#6068)
    * Exclude core-js polyfills from ESM-capable browser bundles
    
    **Why**: So that the majority of users don't have to pay the performance penalty for polyfills supporting legacy browsers, avoid loading those polyfills except in the legacy browsers.
    
    * changelog: Improvements, Optimization, Skip polyfill load behavior for modern browsers
    
    * Restore regenerator polyfill
    
    ref: https://github.com/babel/babel/blob/dacaab1071a509f334c9c7355c38dd829cda9935/packages/babel-preset-env/src/index.ts#L199-L216

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,9 @@
-module.exports = function (api) {
-  const validEnv = ['development', 'test', 'production'];
-  const currentEnv = api.env();
-  const isDevelopmentEnv = api.env('development');
-  const isProductionEnv = api.env('production');
+module.exports = (api) => {
   const isTestEnv = api.env('test');
-
-  if (!validEnv.includes(currentEnv)) {
-    throw new Error(
-      `${
-        'Please specify a valid `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: '
-      }${JSON.stringify(currentEnv)}.`,
-    );
-  }
 
   return {
     presets: [
+      ['@babel/preset-env', { targets: isTestEnv ? 'current node' : '> 1% or IE 11' }],
       '@babel/typescript',
       [
         '@babel/preset-react',
@@ -24,34 +11,23 @@ module.exports = function (api) {
           runtime: 'automatic',
         },
       ],
-      isTestEnv && [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
-      ],
-      (isProductionEnv || isDevelopmentEnv) && [
-        '@babel/preset-env',
-        {
-          forceAllTransforms: true,
-          useBuiltIns: 'usage',
-          corejs: 3,
-          modules: false,
-          // Exclude polyfills for features known to be provided by @18f/identity-polyfill package.
-          // See: https://github.com/babel/babel-polyfills/blob/main/packages/babel-plugin-polyfill-corejs3/src/built-in-definitions.js
-          exclude: ['web.url', 'web.url-search-params', 'es.promise'],
-        },
-      ],
-    ].filter(Boolean),
-    // For third-party dependencies compiled using Babel, don't assume module source type. Use
-    // "unambiguous" for best-effort attempt to identify source type by patterns.
-    overrides: [
-      {
-        test: /node_modules\/(?!@18f\/identity-)/,
-        sourceType: 'unambiguous',
-      },
     ],
+    plugins: [
+      [
+        'polyfill-corejs3',
+        {
+          method: 'usage-global',
+          targets: isTestEnv ? 'current node' : '> 1% and supports es6-module',
+        },
+      ],
+      [
+        'polyfill-regenerator',
+        {
+          method: 'usage-global',
+          targets: isTestEnv ? 'current node' : '> 1% or IE 11',
+        },
+      ],
+    ],
+    sourceType: 'unambiguous',
   };
 };
