ee4a1ad3a9da6d0284a936c589f99e3cb2256dd9
Andrew Duthie <andrew@andrewduthie.com>
2022-03-16 13:52:43 -0400

    Revert "Exclude core-js polyfills from ESM-capable browser bundles (#6068)"
    This reverts commit f6922fae4ad86c769de03e653c7e67cbab7f811e.

--- app/javascript/packages/polyfill/index.js
+++ app/javascript/packages/polyfill/index.js
@@ -1,7 +1,8 @@
-import 'core-js/actual';
+import 'promise-polyfill/src/polyfill';
 import 'whatwg-fetch';
 import 'classlist-polyfill';
 import 'clipboard-polyfill/overwrite-globals';
 import 'webcrypto-shim';
 import '@webcomponents/custom-elements';
 import 'custom-event-polyfill';
+import 'js-polyfills/url';
