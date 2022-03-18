b75c1da5a8f52e37eb1df6727c49138737dc572e
Parissa <parissa.eggleston@gmail.com>
2022-03-18 13:38:53 -0400

    remove varified_at context

--- spec/features/openid_connect/openid_connect_spec.rb
+++ spec/features/openid_connect/openid_connect_spec.rb
@@ -195,8 +195,6 @@ describe 'OpenID Connect' do
     token_response = sign_in_get_token_response(
       scope: 'openid email profile:verified_at',
       handoff_page_steps: proc do
-        expect(page).to have_content(t('help_text.requested_attributes.verified_at'))
-
         click_agree_and_continue
       end,
     )
