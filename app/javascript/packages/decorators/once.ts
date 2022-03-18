6366386e25c29067424bdceea556cf3e387b8d12
Andrew Duthie <andrew.duthie@gsa.gov>
2022-03-18 10:26:09 -0400

    Add support for once with class function

--- app/javascript/packages/decorators/once.ts
+++ app/javascript/packages/decorators/once.ts
@@ -1,12 +1,18 @@
-export const once = (): MethodDecorator => <T>(_target, _propertyKey, descriptor) => {
-  const get = descriptor.get!;
-  descriptor.get = function () {
-    if (!descriptor.value) {
-      descriptor.value = get.call(this) as T;
-    }
-
-    return descriptor.value;
-  };
+export const once = (): MethodDecorator => <T>(_target, propertyKey, descriptor) => {
+  const { value, get } = descriptor;
+  if (typeof value === 'function') {
+    descriptor.value = function () {
+      const result = value.call(this) as T;
+      Object.defineProperty(this, propertyKey, { value: () => result });
+      return result;
+    };
+  } else if (get) {
+    descriptor.get = function () {
+      const result = get.call(this) as T;
+      Object.defineProperty(this, propertyKey, { value: result });
+      return result;
+    };
+  }
 
   return descriptor;
 };
