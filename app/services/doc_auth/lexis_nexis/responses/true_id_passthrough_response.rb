0cb8331b56eb999ceed46aa0e55cb70aa5635cb4
Douglas Price <douglas.price@gsa.gov>
2022-03-21 17:37:12 -0400

    log the trueid response with values redacted

--- app/services/doc_auth/lexis_nexis/responses/true_id_passthrough_response.rb
+++ app/services/doc_auth/lexis_nexis/responses/true_id_passthrough_response.rb
@@ -18,12 +18,23 @@ module DocAuth
 
         def acuant_response(http_response)
           parsed_response = parsed_trueid_response_body(http_response)
+          Ahoy::Tracker.new.track('Acuant Passthrough API Response', just_keys(parsed_response))
           acuant_body = parsed_response["PassThroughs"][0]["Data"]
           OpenStruct.new(body: acuant_body)
         end
 
         def parsed_trueid_response_body(http_response)
           @parsed_trueid_response_body ||= JSON.parse(http_response.body)
+        end
+
+        def just_keys(hash)
+          hash.to_h do |key, value|
+            if value.is_a?(Hash)
+              [key, just_keys(value)]
+            else
+              [key, 'redacted']
+            end
+          end
         end
 
         def error_messages
