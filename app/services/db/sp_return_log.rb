2b8b49f1ed178a4508048c5a799db893c06874cc
Mitchell Henke <mitchell.henke@gsa.gov>
2022-04-04 11:01:16 -0500

    Ensure service provider return logs is backwards compatible with previous behavior
    changelog: Internal, Optimization, Only create service provider return logs for billable events

--- app/services/db/sp_return_log.rb
+++ app/services/db/sp_return_log.rb
@@ -1,5 +1,6 @@
 module Db
   class SpReturnLog
+    # rubocop:disable Rails/SkipsModelValidations
     def self.create_return(request_id:, user_id:, billable:, ial:, issuer:, requested_at:)
       ::SpReturnLog.create!(
         request_id: request_id,
@@ -11,7 +12,15 @@ module Db
         returned_at: Time.zone.now,
       )
     rescue ActiveRecord::RecordNotUnique
+      # Can be removed after deploy of RC 185
+      ::SpReturnLog.where(request_id: request_id).update_all(
+        user_id: user_id,
+        returned_at: Time.zone.now,
+        billable: billable,
+        ial: ial,
+      )
       nil
     end
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
