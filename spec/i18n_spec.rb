f5c7db38a30019cb4ef26eacb5a9f9df385b81c4
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-06 09:25:34 -0400

    Replace custom color variables with design system tokens (#6086)
    * Replace custom color variables with design system tokens
    
    **Why**: So that we have a single source of truth for colors, to improve consistency and reduce confusion.
    
    changelog: Improvements, Layout, Use design system tooling for page layouts
    
    * Remove color list variations
    
    **Why**: For consistency with design system
    
    * Use StatusPageComponent for IDV forgot password
    
    * Refactor IDV forgot password warning strings to array

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -16,8 +16,9 @@ module I18n
       ALLOWED_UNTRANSLATED_KEYS = [
         { key: 'account.navigation.menu', locales: %i[fr] }, # "Menu" is "Menu" in French
         { key: 'doc_auth.headings.photo', locales: %i[fr] }, # "Photo" is "Photo" in French
-        { key: 'errors.alt.error', locales: %i[es] }, # "Error" is "Error" in Spanish
         { key: 'components.status_page.icons.error', locales: %i[es] }, # "Error" is "Error" in Spanish
+        { key: 'components.status_page.icons.question', locales: %i[fr] }, # "Question" is "Question" in French
+        { key: 'errors.alt.error', locales: %i[es] }, # "Error" is "Error" in Spanish
         { key: /^i18n\.locale\./ }, # Show locale options translated as that language
         { key: /^countries/ }, # Some countries have the same name across languages
         { key: 'links.contact', locales: %i[fr] }, # "Contact" is "Contact" in French
