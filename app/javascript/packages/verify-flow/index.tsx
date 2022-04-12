c62910d55a48f28ff849ee3b9913e9eb2ec5bb68
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-12 10:47:40 -0400

    Render VerifyFlow step indicator

--- app/javascript/packages/verify-flow/index.tsx
+++ app/javascript/packages/verify-flow/index.tsx
@@ -1,4 +1,5 @@
 import { FormSteps } from '@18f/identity-form-steps';
+import { StepIndicator, StepIndicatorStep, StepStatus } from '@18f/identity-step-indicator';
 import { STEPS } from './steps';
 
 export interface VerifyFlowValues {
@@ -10,5 +11,16 @@ interface VerifyFlowProps {
 }
 
 export function VerifyFlow({ initialValues = {} }: VerifyFlowProps) {
-  return <FormSteps steps={STEPS} initialValues={initialValues} promptOnNavigate={false} />;
+  return (
+    <>
+      <StepIndicator className="margin-x-neg-2 margin-top-neg-4 tablet:margin-x-neg-6 tablet:margin-top-neg-4">
+        <StepIndicatorStep title="Getting Started" status={StepStatus.COMPLETE} />
+        <StepIndicatorStep title="Verify your ID" status={StepStatus.COMPLETE} />
+        <StepIndicatorStep title="Verify your personal details" status={StepStatus.COMPLETE} />
+        <StepIndicatorStep title="Verify phone or address" status={StepStatus.COMPLETE} />
+        <StepIndicatorStep title="Secure your account" status={StepStatus.CURRENT} />
+      </StepIndicator>
+      <FormSteps steps={STEPS} initialValues={initialValues} promptOnNavigate={false} />
+    </>
+  );
 }
