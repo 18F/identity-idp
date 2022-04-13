ede90c93cd03c37e9598c37fed1f8e5a72bb29a4
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-13 09:40:06 -0400

    Add basic FormSteps flow for IdV React implementation
    **Why**: So that we can start implementing step screens for the new, React-based identity verification flow.
    
    changelog: Upcoming Features, Identity Verification, Add personal key step screen

---
+++ app/javascript/packages/verify-flow/steps/personal-key/personal-key-step.tsx
@@ -0,0 +1,8 @@
+import { PageHeading } from '@18f/identity-components';
+import { t } from '@18f/identity-i18n';
+
+function PersonalKeyStep() {
+  return <PageHeading>{t('headings.personal_key')}</PageHeading>;
+}
+
+export default PersonalKeyStep;
