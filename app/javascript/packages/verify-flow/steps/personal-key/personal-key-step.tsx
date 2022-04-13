85de52fd9d245e979f4ff68a8e39c01581859bc7
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-13 13:00:47 -0400

    Reorder props
    Bit easier to read with className last

--- app/javascript/packages/verify-flow/steps/personal-key/personal-key-step.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key/personal-key-step.tsx
@@ -8,13 +8,13 @@ function PersonalKeyStep() {
       <PageHeading>{t('headings.personal_key')}</PageHeading>
       <p>{t('instructions.personal_key.info')}</p>
       <div className="full-width-box margin-y-5" />
-      <Button className="margin-right-2 margin-bottom-2 tablet:margin-bottom-0" isOutline>
+      <Button isOutline className="margin-right-2 margin-bottom-2 tablet:margin-bottom-0">
         {t('forms.backup_code.download')}
       </Button>
-      <Button className="margin-right-2 margin-bottom-2 tablet:margin-bottom-0" isOutline>
+      <Button isOutline className="margin-right-2 margin-bottom-2 tablet:margin-bottom-0">
         {t('users.personal_key.print')}
       </Button>
-      <Button className="margin-bottom-2 tablet:margin-bottom-0" isOutline>
+      <Button isOutline className="margin-bottom-2 tablet:margin-bottom-0">
         {t('links.copy')}
       </Button>
       <div className="margin-y-5 clearfix">
