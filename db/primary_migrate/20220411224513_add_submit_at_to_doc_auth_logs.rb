f9683ab763ca2a6bdbb31a6c3ecee4059b3fea14
Steve Urciuoli <steve.urciuoli@gsa.gov>
2022-04-14 14:57:02 -0500

    Remove unnecessary code

--- db/primary_migrate/20220411224513_add_submit_at_to_doc_auth_logs.rb
+++ db/primary_migrate/20220411224513_add_submit_at_to_doc_auth_logs.rb
@@ -1,6 +1,4 @@
 class AddSubmitAtToDocAuthLogs < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
-
   def change
     add_column :doc_auth_logs, :verify_submit_at, :datetime
     add_column :doc_auth_logs, :verify_phone_submit_count, :integer, default: 0
