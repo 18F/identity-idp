90257ba5b609cd74e97ea61ac4f408420665514e
Steve Urciuoli <steve.urciuoli@gsa.gov>
2022-04-14 13:46:20 -0500

    Add index in separate migration

--- db/primary_migrate/20220411224513_add_submit_at_to_doc_auth_logs.rb
+++ db/primary_migrate/20220411224513_add_submit_at_to_doc_auth_logs.rb
@@ -6,7 +6,5 @@ class AddSubmitAtToDocAuthLogs < ActiveRecord::Migration[6.1]
     add_column :doc_auth_logs, :verify_phone_submit_count, :integer, default: 0
     add_column :doc_auth_logs, :verify_phone_submit_at, :datetime
     add_column :doc_auth_logs, :document_capture_submit_at, :datetime
-
-    add_index :doc_auth_logs, :issuer, algorithm: :concurrently
   end
 end
