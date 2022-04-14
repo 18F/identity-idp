6d62f294686f40f635fd16632142304bffb3ea70
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-14 10:09:02 -0400

    LG-6066: Implement print button on FSMv2 Personal Key step
    **Why**:
    
    - So that we can retain feature parity with the existing screen.
    - To reduce size and scope of common application bundle
    - To create a more rigid connection between print JavaScript functionality and server-side render logic
    - To improve test coverage for existing print button behavior
    
    changelog: Upcoming Features, Identity Verification, Add personal key step screen

---
+++ app/javascript/packages/print-button/print-button-element.spec.ts
@@ -0,0 +1,25 @@
+import sinon from 'sinon';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import './print-button-element';
+
+describe('PrintButtonElement', () => {
+  const sandbox = sinon.createSandbox();
+
+  beforeEach(() => {
+    sandbox.stub(window, 'print');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('prints when clicked', () => {
+    document.body.innerHTML = `<lg-print-button><button type="button">Print</button><lg-print-button>`;
+    const button = screen.getByRole('button');
+
+    userEvent.click(button);
+
+    expect(window.print).to.have.been.called();
+  });
+});
