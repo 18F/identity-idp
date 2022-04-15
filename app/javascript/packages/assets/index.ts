aed7e73a297e9685a52879be7bb24b3c835f3165
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-15 14:08:08 -0400

    Add type signature for getAssetPath
    1. Parameter not typed (any)
    2. Inferred return value is "string", when it can be undefined

--- app/javascript/packages/assets/index.ts
+++ app/javascript/packages/assets/index.ts
@@ -1,6 +1,6 @@
 type AssetPaths = Record<string, string>;
 
-export function getAssetPath(path) {
+export function getAssetPath(path: string): string | undefined {
   if (!getAssetPath.cache) {
     try {
       const script = document.querySelector('[data-asset-map]') as HTMLScriptElement;
