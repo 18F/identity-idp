02b6a62e73dd0f1c9a9d8cbbb27e2d1125da16aa
Parissa <parissa.eggleston@gmail.com>
2022-04-15 11:10:46 -0400

    update complete_controller

--- app/controllers/api/verify/complete_controller.rb
+++ app/controllers/api/verify/complete_controller.rb
@@ -1,16 +1,20 @@
 class Api::Verify::CompleteController < ApplicationController
+  include IdvSession
+  before_action :confirm_two_factor_authenticated
+  before_action :confirm_idv_vendor_session_started
+  before_action :confirm_profile_has_been_created
+
 
   def get_personal_key
     #verify_params = params.require(:verify).permit(:password,:details)
 
     # Create profile Maker with Pii using the password
     # Cash temporarly to session
-    #
 
     # Generate Personal Key
     analytics.track_event(Analytics::IDV_PERSONAL_KEY_VISITED)
     add_proofing_component
-    render json: {personal_key: {}, profile_pending: {}}
+    render json: {personal_key: personal_key, profile_pending: {}}
   end
 
   private
@@ -19,9 +23,17 @@ class Api::Verify::CompleteController < ApplicationController
     idv_session.personal_key || generate_personal_key
   end
 
+  def generate_personal_key
+    cacher = Pii::Cacher.new(current_user, user_session)
+    idv_session.profile.encrypt_recovery_pii(cacher.fetch)
+  end
 
   def add_proofing_component
     ProofingComponent.create_or_find_by(user: current_user).update(verified_at: Time.zone.now)
+  end
+
+  def confirm_profile_has_been_created
+    render json: { error: 'User credentials not found'} if idv_session.profile.blank?
   end
 
 end
