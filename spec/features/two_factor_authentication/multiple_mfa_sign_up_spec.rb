218c9fc7efa8092b17866689f23d074409d62c00
Malick Diarra <malick.diarra@gsa.gov>
2022-04-15 09:53:03 -0400

    multipel mfa signup fix

--- spec/features/two_factor_authentication/multiple_mfa_sign_up_spec.rb
+++ spec/features/two_factor_authentication/multiple_mfa_sign_up_spec.rb
@@ -4,14 +4,15 @@ feature 'Multi Two Factor Authentication' do
   before do
     allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
   end
+  
   describe 'When the user has not set up 2FA' do
     scenario 'user can set up 2 MFA methods properly' do
       sign_in_before_2fa
 
       expect(current_path).to eq two_factor_options_path
 
-      select_2fa_option('phone')
-      select_2fa_option('backup_code')
+      click_2fa_option('phone')
+      click_2fa_option('backup_code')
 
       click_continue
 
@@ -37,28 +38,32 @@ feature 'Multi Two Factor Authentication' do
     end
 
     scenario 'user can select 2 MFA methods and complete 1 and skip one' do
-     sign_in_before_2fa
+      sign_in_before_2fa
 
-     expect(current_path).to eq two_factor_options_path
+      expect(current_path).to eq two_factor_options_path
 
-     select_2fa_option('phone')
-     select_2fa_option('backup_code')
+      click_2fa_option('phone')
+      click_2fa_option('backup_code')
 
-     click_continue
+      click_continue
 
-     expect(page).
+      expect(page).
        to have_content t('titles.phone_setup')
 
-     expect(current_path).to eq phone_setup_path
+      expect(current_path).to eq phone_setup_path
 
-     fill_in_code_with_last_phone_otp
-     click_submit_default
+      fill_in_code_with_last_phone_otp
+      click_submit_default
 
-     expect(page).to have_current_path(auth_method_confirmation_path)
+      expect(page).to have_current_path(auth_method_confirmation_path)
 
-     click t('multi_factor_authentication.skip')
+      click t('multi_factor_authentication.skip')
 
-     expect(current_path).to eq account_path
+      expect(current_path).to eq account_path
     end
+  end
+
+  def click_2fa_option(option)
+    find("label[for='two_factor_options_form_selection_#{option}']").click
   end
 end
