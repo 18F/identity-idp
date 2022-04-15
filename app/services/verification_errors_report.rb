101b85a6323aa53fbfee64e6364270c04e5531cf
Steve Urciuoli <steve.urciuoli@gsa.gov>
2022-04-15 15:59:46 -0500

    interpolation

--- app/services/verification_errors_report.rb
+++ app/services/verification_errors_report.rb
@@ -1,20 +1,31 @@
 class VerificationErrorsReport
   def self.call(service_provider, since)
     report_sql = <<~SQL
-      SELECT agency_identities.uuid,welcome_view_at,document_capture_submit_at,ssn_view_at,
-        verify_submit_at,ssn_view_at,verify_phone_submit_at,encrypt_view_at,enter_info_view_at
+      SELECT 
+        agency_identities.uuid,
+        document_capture_submit_at,
+        encrypt_view_at,
+        enter_info_view_at,
+        ssn_view_at,
+        verify_phone_submit_at,
+        verify_submit_at,
+        welcome_view_at
       FROM doc_auth_logs, users, agency_identities, service_providers
       WHERE 
         users.id=doc_auth_logs.user_id AND 
-        service_providers.issuer = '%s' AND 
+        service_providers.issuer = %{issuer} AND 
         agency_identities.agency_id = service_providers.agency_id AND 
         agency_identities.user_id = users.id AND 
-        doc_auth_logs.issuer='%s' AND
+        doc_auth_logs.issuer=%{issuer} AND
         verified_view_at is null AND
-        welcome_view_at > '%s'
+        welcome_view_at > %{since}
       ORDER BY agency_identities.uuid ASC
     SQL
-    sql = format(report_sql, service_provider, service_provider, since)
+    sql = format(
+      report_sql,
+      issuer: ActiveRecord::Base.connection.quote(service_provider),
+      since: ActiveRecord::Base.connection.quote(since),
+    )
     ActiveRecord::Base.connection.execute(sql)
   end
 end
