3f15b44be837ae340ebeedb4cf9ac7dee57463f7
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-18 16:36:23 -0400

    Remove explicit maxLength
    As confirmed via integration specs, Cleave handles this

--- app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.tsx
@@ -2,11 +2,6 @@ import { forwardRef } from 'react';
 import Cleave from 'cleave.js/react';
 import { t } from '@18f/identity-i18n';
 
-/**
- * Maximum length of personal key value, including delimiters.
- */
-const CODE_LENGTH = 19;
-
 function PersonalKeyInput(_props, ref) {
   return (
     <Cleave
@@ -18,7 +13,6 @@ function PersonalKeyInput(_props, ref) {
       aria-label={t('forms.personal_key.confirmation_label')}
       autoComplete="off"
       className="width-full margin-bottom-6 field font-family-mono text-uppercase"
-      maxLength={CODE_LENGTH}
       pattern="[a-zA-Z0-9-]"
       spellCheck={false}
       type="text"
