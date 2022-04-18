9f3755bd35469928b4845bbaa66fcfaba44a9e62
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-18 16:15:27 -0400

    LG-6114: Use Cleave.js to format personal key (IdV app)
    **Why**: So that the value is more readable as the user types, and to ensure feature parity with the existing experience.
    
    changelog: Upcoming Features, Identity Verification, Add personal key step screen

--- app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.tsx
@@ -1,14 +1,24 @@
 import { forwardRef } from 'react';
+import Cleave from 'cleave.js/react';
 import { t } from '@18f/identity-i18n';
+
+/**
+ * Maximum length of personal key value, including delimiters.
+ */
+const CODE_LENGTH = 19;
 
 function PersonalKeyInput(_props, ref) {
   return (
-    <input
+    <Cleave
+      options={{
+        blocks: [4, 4, 4, 4],
+        delimiter: '-',
+      }}
       ref={ref}
       aria-label={t('forms.personal_key.confirmation_label')}
       autoComplete="off"
-      className="width-full margin-bottom-6 border-dashed field font-family-mono personal-key text-uppercase"
-      maxLength={19}
+      className="width-full margin-bottom-6 field font-family-mono text-uppercase"
+      maxLength={CODE_LENGTH}
       pattern="[a-zA-Z0-9-]"
       spellCheck={false}
       type="text"
