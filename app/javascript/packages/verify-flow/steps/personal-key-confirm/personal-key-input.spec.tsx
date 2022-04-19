883e767227a63b43a9a48450105d407054eaf48a
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-19 08:13:02 -0400

    LG-6114: Use Cleave.js to format personal key (IdV app) (#6217)
    * LG-6114: Use Cleave.js to format personal key (IdV app)
    
    **Why**: So that the value is more readable as the user types, and to ensure feature parity with the existing experience.
    
    changelog: Upcoming Features, Identity Verification, Add personal key step screen
    
    * Remove explicit maxLength
    
    As confirmed via integration specs, Cleave handles this
    
    * Rename formatted-fields to ts
    
    Fix type errors
    
    * Fix formatted-fields TypeScript errors

--- app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.spec.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.spec.tsx
@@ -14,13 +14,12 @@ describe('PersonalKeyInput', () => {
   });
 
   it('accepts a value without dashes', () => {
-    const value = '0000000000000000';
     const { getByRole } = render(<PersonalKeyInput />);
 
     const input = getByRole('textbox') as HTMLInputElement;
-    userEvent.type(input, value);
+    userEvent.type(input, '0000000000000000');
 
-    expect(input.value).to.equal(value);
+    expect(input.value).to.equal('0000-0000-0000-0000');
   });
 
   it('does not accept a code longer than one with dashes', () => {
@@ -30,5 +29,29 @@ describe('PersonalKeyInput', () => {
     userEvent.type(input, '0000-0000-0000-00000');
 
     expect(input.value).to.equal('0000-0000-0000-0000');
+  });
+
+  it('formats value as the user types', () => {
+    const { getByRole } = render(<PersonalKeyInput />);
+
+    const input = getByRole('textbox') as HTMLInputElement;
+
+    userEvent.type(input, '1234');
+    expect(input.value).to.equal('1234-');
+
+    userEvent.type(input, '{backspace}');
+    expect(input.value).to.equal('123');
+
+    userEvent.type(input, '4');
+    expect(input.value).to.equal('1234-');
+
+    userEvent.type(input, '-');
+    expect(input.value).to.equal('1234-');
+
+    userEvent.paste(input, '12341234');
+    expect(input.value).to.equal('1234-1234-1234-');
+
+    userEvent.type(input, '12345');
+    expect(input.value).to.equal('1234-1234-1234-1234');
   });
 });
--- app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-input.tsx
@@ -1,14 +1,18 @@
 import { forwardRef } from 'react';
+import Cleave from 'cleave.js/react';
 import { t } from '@18f/identity-i18n';
 
 function PersonalKeyInput(_props, ref) {
   return (
-    <input
+    <Cleave
+      options={{
+        blocks: [4, 4, 4, 4],
+        delimiter: '-',
+      }}
       ref={ref}
       aria-label={t('forms.personal_key.confirmation_label')}
       autoComplete="off"
-      className="width-full margin-bottom-6 border-dashed field font-family-mono personal-key text-uppercase"
-      maxLength={19}
+      className="width-full margin-bottom-6 field font-family-mono text-uppercase"
       pattern="[a-zA-Z0-9-]"
       spellCheck={false}
       type="text"
