a5b03ceb420076572a1abe8ef586543c1deeb1be
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-20 11:03:55 -0400

    Add spec for PersonalKeyConfirmStep validation

--- app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-confirm-step.spec.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-confirm-step.spec.tsx
@@ -1,6 +1,7 @@
 import sinon from 'sinon';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { FormSteps } from '@18f/identity-form-steps';
 import PersonalKeyConfirmStep from './personal-key-confirm-step';
 
 describe('PersonalKeyConfirmStep', () => {
@@ -33,5 +34,31 @@ describe('PersonalKeyConfirmStep', () => {
     await userEvent.type(getByRole('textbox'), '{Escape}');
 
     expect(toPreviousStep).to.have.been.called();
+  });
+
+  it('allows the user to continue only with a correct value', () => {
+    const personalKey = '0000-0000-0000-0000';
+    const onComplete = sinon.spy();
+    const { getByRole, getAllByRole, container } = render(
+      <FormSteps
+        steps={[{ name: 'personal_key_confirm', form: PersonalKeyConfirmStep }]}
+        initialValues={{ personalKey }}
+        onComplete={onComplete}
+      />,
+    );
+
+    const input = getByRole('textbox', { name: 'forms.personal_key.confirmation_label' });
+    const submitButton = getAllByRole('button', { name: 'forms.buttons.submit.default' })[1];
+    userEvent.click(submitButton);
+
+    expect(onComplete).not.to.have.been.called();
+    expect(container.ownerDocument.activeElement).to.equal(input);
+    const errorMessage = document.getElementById(input.getAttribute('aria-describedby')!)!;
+    expect(errorMessage.textContent).to.equal('users.personal_key.confirmation_error');
+
+    userEvent.type(input, personalKey);
+    userEvent.click(submitButton);
+
+    expect(onComplete).to.have.been.called();
   });
 });
