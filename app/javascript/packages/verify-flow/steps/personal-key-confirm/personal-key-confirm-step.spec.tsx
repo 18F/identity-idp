fde87d100eee17a4dfbc63381a7d45fb8af064e7
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-20 08:42:18 -0400

    Add spec for escape-to-close dialog

--- app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-confirm-step.spec.tsx
+++ app/javascript/packages/verify-flow/steps/personal-key-confirm/personal-key-confirm-step.spec.tsx
@@ -13,13 +13,24 @@ describe('PersonalKeyConfirmStep', () => {
     registerField: () => () => {},
   };
 
-  it('allows the user to return to the previous step', () => {
+  it('allows the user to return to the previous step by clicking "Back" button', () => {
     const toPreviousStep = sinon.spy();
     const { getByText } = render(
       <PersonalKeyConfirmStep {...DEFAULT_PROPS} toPreviousStep={toPreviousStep} />,
     );
 
     userEvent.click(getByText('forms.buttons.back'));
+
+    expect(toPreviousStep).to.have.been.called();
+  });
+
+  it('allows the user to return to the previous step by pressing Escape', () => {
+    const toPreviousStep = sinon.spy();
+    const { getByRole } = render(
+      <PersonalKeyConfirmStep {...DEFAULT_PROPS} toPreviousStep={toPreviousStep} />,
+    );
+
+    userEvent.type(getByRole('textbox'), '{esc}');
 
     expect(toPreviousStep).to.have.been.called();
   });
