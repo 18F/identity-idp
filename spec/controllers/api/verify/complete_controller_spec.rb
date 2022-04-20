2b7abf77b97766227fb958874bb9f3c2db5a7640
Parissa <parissa.eggleston@gmail.com>
2022-04-20 16:35:40 -0400

    cleanup test

--- spec/controllers/api/verify/complete_controller_spec.rb
+++ spec/controllers/api/verify/complete_controller_spec.rb
@@ -4,7 +4,6 @@ describe Api::Verify::CompleteController do
   include PersonalKeyValidator
   include SamlAuthHelper
 
-
   def stub_idv_session
     stub_sign_in(user)
   end
@@ -12,25 +11,24 @@ describe Api::Verify::CompleteController do
   let(:password) { 'iambatman' }
   let(:user) { create(:user, :signed_up, password: password) }
   let(:applicant) do
-    {
-        first_name: 'Bruce',
-        last_name: 'Wayne',
-        address1: '123 Mansion St',
-        address2: 'Ste 456',
-        city: 'Gotham City',
-        state: 'NY',
-        zipcode: '10015',
-    }
+    { first_name: 'Bruce',
+      last_name: 'Wayne',
+      address1: '123 Mansion St',
+      address2: 'Ste 456',
+      city: 'Gotham City',
+      state: 'NY',
+      zipcode: '10015' }
   end
 
   let(:pii) do
-    { first_name: 'Bruce', last_name: 'Wayne', ssn: '900-90-1234' }
+    { first_name: 'Bruce',
+      last_name: 'Wayne',
+      ssn: '900-90-1234' }
   end
 
   let(:profile) { subject.idv_session.profile }
   let(:key) { OpenSSL::PKey::RSA.new 2048 }
   let(:pub) { key.public_key }
-
   let(:jwt) { JWT.encode(pii, key, 'RS256', sub: user.uuid) }
 
   before do
@@ -44,14 +42,14 @@ describe Api::Verify::CompleteController do
     it 'includes before_actions from AccountStateChecker' do
       expect(subject).to have_actions(
                              :before,
-                             :confirm_two_factor_authenticated_for_api,
-                             )
+                             :confirm_two_factor_authenticated_for_api
+                         )
     end
   end
 
   describe '#create' do
     context 'when the user is not signed in and submits the password' do
-      it 'does not create a profile and return a key when it does not have a user session' do
+      it 'does not create a profile or return a key' do
         get :create, params: { password: 'iambatman', details: jwt }
         expect(JSON.parse(response.body)["personal_key"]).to be_nil
         expect(JSON.parse(response.body)["status"]).to eq "ERROR"
