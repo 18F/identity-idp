ee5c6de9de8d1861b4b3bb9cae7ebe95eba2571b
Mitchell Henke <mitchell.henke@gsa.gov>
2022-04-22 14:13:47 -0500

    drop unused doc_auths table
    changelog: Internal, Maintenance, Drop unused doc_auths table

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,6 @@ class User < ApplicationRecord
   has_many :webauthn_configurations, dependent: :destroy, inverse_of: :user
   has_many :piv_cac_configurations, dependent: :destroy, inverse_of: :user
   has_many :auth_app_configurations, dependent: :destroy, inverse_of: :user
-  has_one :doc_auth, dependent: :destroy, inverse_of: :user, class_name: 'DocAuthRecord'
   has_many :backup_code_configurations, dependent: :destroy
   has_many :document_capture_sessions, dependent: :destroy
   has_many :throttles, dependent: :destroy
