d340fad39135cbe83fa6399728d4d8bda5275456
Mitchell Henke <mitchell.henke@gsa.gov>
2022-04-28 09:30:26 -0500

    Run fiscal year report on October 1st for SP Active Users report
    changelog: Internal, Reports, Run fiscal year report on October 1st for SP Active Users report

--- spec/jobs/reports/sp_active_users_report_spec.rb
+++ spec/jobs/reports/sp_active_users_report_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Reports::SpActiveUsersReport do
   subject { described_class.new }
 
-  let(:fiscal_start_date) { 1.year.ago.to_s }
   let(:issuer) { 'foo' }
   let(:issuer2) { 'foo2' }
   let(:app_id) { 'app' }
@@ -13,28 +12,70 @@ describe Reports::SpActiveUsersReport do
   end
 
   it 'returns total active user counts per sp broken down by ial1 and ial2' do
-    now = Time.zone.now
+    job_date = Date.new(2022, 1, 1)
+    authenticated_time = job_date.noon
     ServiceProvider.create(issuer: issuer, friendly_name: issuer, app_id: 'app')
     ServiceProviderIdentity.create(
       user_id: 1, service_provider: issuer, uuid: 'foo1',
-      last_ial1_authenticated_at: now, last_ial2_authenticated_at: now
+      last_ial1_authenticated_at: authenticated_time, last_ial2_authenticated_at: authenticated_time
     )
     ServiceProviderIdentity.create(
       user_id: 2, service_provider: issuer, uuid: 'foo2',
-      last_ial1_authenticated_at: now
+      last_ial1_authenticated_at: authenticated_time
     )
     ServiceProviderIdentity.create(
       user_id: 3, service_provider: issuer, uuid: 'foo3',
-      last_ial2_authenticated_at: now
+      last_ial2_authenticated_at: authenticated_time
     )
     ServiceProviderIdentity.create(
       user_id: 4, service_provider: issuer, uuid: 'foo4',
-      last_ial2_authenticated_at: now
+      last_ial2_authenticated_at: authenticated_time
     )
     result = [{ issuer: issuer, app_id: app_id, total_ial1_active: 2,
                 total_ial2_active: 3 }].to_json
 
     expect(subject.perform(Time.zone.today)).to eq(result)
+  end
+
+  it 'when Oct 1, returns total active user counts per sp by ial1 and ial2 for last fiscal year' do
+    # run date is from 2019-10-01 to 2020-09-30
+    job_date = Date.new(2020, 10, 1)
+    beginning_of_last_fiscal_year = job_date.change(year: 2019, month: 10, day: 1).beginning_of_day
+    end_of_last_fiscal_year = job_date.change(year: 2020, month: 9, day: 30).end_of_day
+    middle_of_last_fiscal_year = job_date.change(year: 2020, month: 1, day: 31).end_of_day
+
+    beginning_of_current_fiscal_year = job_date.beginning_of_day
+    current_fiscal_year = job_date.change(hour: 12, minute: 30)
+
+    ServiceProvider.create(issuer: issuer, friendly_name: issuer, app_id: 'app')
+    ServiceProviderIdentity.create(
+      user_id: 1, service_provider: issuer, uuid: 'foo1',
+      last_ial1_authenticated_at: beginning_of_last_fiscal_year,
+      last_ial2_authenticated_at: beginning_of_last_fiscal_year
+    )
+    ServiceProviderIdentity.create(
+      user_id: 2, service_provider: issuer, uuid: 'foo2',
+      last_ial1_authenticated_at: end_of_last_fiscal_year,
+      last_ial2_authenticated_at: end_of_last_fiscal_year
+    )
+    ServiceProviderIdentity.create(
+      user_id: 3, service_provider: issuer, uuid: 'foo3',
+      last_ial2_authenticated_at: middle_of_last_fiscal_year
+    )
+    ServiceProviderIdentity.create(
+      user_id: 4, service_provider: issuer, uuid: 'foo4',
+      last_ial2_authenticated_at: beginning_of_current_fiscal_year
+    )
+
+    ServiceProviderIdentity.create(
+      user_id: 5, service_provider: issuer, uuid: 'foo5',
+      last_ial2_authenticated_at: current_fiscal_year
+    )
+
+    result = [{ issuer: issuer, app_id: app_id, total_ial1_active: 2,
+                total_ial2_active: 3 }].to_json
+
+    expect(subject.perform(job_date)).to eq(result)
   end
 
   describe '#good_job_concurrency_key' do
