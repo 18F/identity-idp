f6c0b4b13ff0e7cd75e730106b9160a2047bba78
Mitchell Henke <mitchell.henke@gsa.gov>
2022-04-28 09:30:26 -0500

    Run fiscal year report on October 1st for SP Active Users report
    changelog: Internal, Reports, Run fiscal year report on October 1st for SP Active Users report

--- app/jobs/reports/sp_active_users_report.rb
+++ app/jobs/reports/sp_active_users_report.rb
@@ -8,14 +8,43 @@ module Reports
 
     good_job_control_concurrency_with(
       total_limit: 1,
-      key: -> { "#{REPORT_NAME}-#{arguments.first}" },
+      key: -> { "#{REPORT_NAME}-#{arguments.first.to_date}" },
     )
 
-    def perform(_date)
+    # This daily job captures the total number of active users per SP from the beginning of the the
+    # current fiscal year until now.
+    #
+    # A fiscal year is from October 1st 12:00:00AM to September 30th 11:59:59PM the following year.
+    #
+    # To provide a report that captures the total number of active users for an entire fiscal year
+    # and avoids reporting partial days, October 1st is treated differently.
+    # The report will run for the entire fiscal year that ended the day before rather than for the
+    # partial day of October 1st in the current fiscal year.
+    def perform(date)
       results = transaction_with_timeout do
-        Db::Identity::SpActiveUserCounts.call(fiscal_start_date)
+        Db::Identity::SpActiveUserCounts.call(start_time(date), finish_time(date))
       end
       save_report(REPORT_NAME, results.to_json, extension: 'json')
+    end
+
+    def start_time(time)
+      if time.month == 10 && time.day == 1
+        fiscal_start_date(time).change(year: fiscal_start_date.year - 2).beginning_of_day
+      else
+        fiscal_start_date(time)
+      end
+    end
+
+    def finish_time(time)
+      if time.month == 10 && time.day == 1
+        fiscal_end_date(time)
+      else
+        time.end_of_day
+      end
+    end
+
+    def fiscal_end_date(time)
+      time.change(year: time.month >= 10 ? time.year : time.year + 1, month: 9, day: 30).end_of_day
     end
   end
 end
