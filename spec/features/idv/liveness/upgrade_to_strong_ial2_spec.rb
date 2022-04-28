389824d934964950bc3e4b937e15fa5a994e17d4
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-04-28 10:17:07 -0400

    Retire the liveness_checking_required flag on service providers (#6263)
    Liveness checking specific functionality is being discarded in favor of a full IAL2 compliant flow which may include additional deltas. That makes this flag confusing and hopefully unnecessary so this commit removes it.
    
    A follow on commit will be required to drop the column.
    
    * changelog: Internal, Service provider management, An unnecessary flag for setting liveness checking requirements on an SP was removed

--- spec/features/idv/liveness/upgrade_to_strong_ial2_spec.rb
+++ spec/features/idv/liveness/upgrade_to_strong_ial2_spec.rb
@@ -6,69 +6,6 @@ describe 'Strong IAL2' do
   include SamlAuthHelper
   include DocAuthHelper
 
-  context 'with an sp that requires livess and a new account' do
-    before do
-      ServiceProvider.find_by(issuer: sp1_issuer).
-        update!(liveness_checking_required: true)
-    end
-
-    it 'starts the proofing process if liveness is enabled' do
-      allow(IdentityConfig.store).to receive(:liveness_checking_enabled).and_return(true)
-
-      visit_idp_from_sp_with_ial2(:saml)
-      sign_up_and_2fa_ial1_user
-
-      click_agree_and_continue_optional
-
-      expect(page.current_path).to eq(idv_doc_auth_welcome_step)
-    end
-  end
-
-  context 'with an sp that requires liveness and a current verified profile with no liveness' do
-    before do
-      ServiceProvider.find_by(issuer: 'urn:gov:gsa:openidconnect:sp:server').update!(
-        liveness_checking_required: true,
-      )
-    end
-
-    it 'upgrades user to IAL2 strict if liveness checking is enabled' do
-      allow(IdentityConfig.store).to receive(:liveness_checking_enabled).and_return(true)
-
-      user ||= create(
-        :profile, :active, :verified,
-        pii: { first_name: 'John', ssn: '111223333' }
-      ).user
-      visit_idp_from_sp_with_ial2(:oidc)
-      sign_in_user(user)
-      fill_in_code_with_last_phone_otp
-      click_submit_default
-      click_agree_and_continue_optional
-
-      expect(page.current_path).to eq(idv_doc_auth_welcome_step)
-
-      complete_all_doc_auth_steps
-      click_continue
-      fill_in 'Password', with: user.password
-      click_continue
-      click_acknowledge_personal_key
-      click_agree_and_continue
-
-      expect(current_url).to start_with('http://localhost:7654/auth/result')
-      expect(user.active_profile.includes_liveness_check?).to be_truthy
-    end
-
-    it 'returns an error if liveness checking is disabled' do
-      allow(IdentityConfig.store).to receive(:liveness_checking_enabled).and_return(false)
-
-      visit_idp_from_sp_with_ial2(:oidc)
-
-      expect(current_url).to start_with(
-        'http://localhost:7654/auth/result?error=invalid_request'\
-        '&error_description=Acr+values+Liveness+checking+is+disabled',
-      )
-    end
-  end
-
   context 'with SP that sends an IAL2 strict request and a verified profile with no liveness' do
     it 'upgrades user to IAL2 strict if liveness checking is enabled' do
       allow(IdentityConfig.store).to receive(:liveness_checking_enabled).and_return(true)
