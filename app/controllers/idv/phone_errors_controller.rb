2fa0983ba55da52dbe98945cf1d3d20729b770f6
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-04-29 15:36:07 -0400

    cleanup

--- app/controllers/idv/phone_errors_controller.rb
+++ app/controllers/idv/phone_errors_controller.rb
@@ -48,13 +48,11 @@ module Idv
     end
 
     def set_gpo_letter_available
-      def gpo_letter_available
-        @gpo_letter_available ||= FeatureManagement.enable_gpo_verification? &&
-                                  !Idv::GpoMail.new(current_user).mail_spammed? &&
-                                  !(sp_session[:ial2_strict] &&
-                                    !IdentityConfig.store.usps_upload_allowed_for_strict_ial2)
+      @gpo_letter_available ||= FeatureManagement.enable_gpo_verification? &&
+                                !Idv::GpoMail.new(current_user).mail_spammed? &&
+                                !(sp_session[:ial2_strict] &&
+                                  !IdentityConfig.store.usps_upload_allowed_for_strict_ial2)
 
-      end
     end
   end
 end
