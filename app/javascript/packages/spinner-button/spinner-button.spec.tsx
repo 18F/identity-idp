6db69b51e21f3d2efe644eba789ae42f7b1b78e9
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-02 16:47:01 -0400

    Add more thorough specs for SpinnerButton

--- app/javascript/packages/spinner-button/spinner-button.spec.tsx
+++ app/javascript/packages/spinner-button/spinner-button.spec.tsx
@@ -17,6 +17,17 @@ describe('SpinnerButton', () => {
     sandbox.restore();
   });
 
+  it('renders a SpinnerButton', async () => {
+    const { getByRole } = render(<SpinnerButton>Spin!</SpinnerButton>);
+
+    const button = getByRole('button', { name: 'Spin!' }) as HTMLButtonElement;
+    const spinner = button.closest('lg-spinner-button')!;
+
+    await userEvent.click(button);
+
+    expect(spinner.classList.contains('spinner-button--spinner-active')).to.be.true();
+  });
+
   it('exposes SpinnerButtonElement instance via forwarded ref', () => {
     const ref = createRef<SpinnerButtonElement>();
     render(<SpinnerButton ref={ref} />);
@@ -43,5 +54,31 @@ describe('SpinnerButton', () => {
 
     expect(status.textContent).not.to.be.empty();
     expect(status.classList.contains('usa-sr-only')).to.be.false();
+  });
+
+  it('forwards options for SpinnerButton', async () => {
+    const { getByRole } = render(
+      <SpinnerButton longWaitDurationMs={1} spinOnClick={false} actionMessage="Loading..." />,
+    );
+
+    const button = getByRole('button') as HTMLButtonElement;
+    const spinner = button.closest('lg-spinner-button')!;
+    const status = getByRole('status')!;
+
+    await userEvent.click(button);
+
+    expect(spinner.classList.contains('spinner-button--spinner-active')).to.be.false();
+
+    spinner.toggleSpinner(true);
+    sandbox.clock.tick(1);
+    expect(status.classList.contains('usa-sr-only')).to.be.false();
+  });
+
+  it('forwards additional props to Button', () => {
+    const { getByRole } = render(<SpinnerButton isOutline />);
+
+    const button = getByRole('button')!;
+
+    expect(button.classList.contains('usa-button--outline')).to.be.true();
   });
 });
