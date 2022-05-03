9854309a96dff0f04e57a0a7e5ca9fdf54b14486
SammySteiner <sammysteiner@gmail.com>
2022-05-03 10:25:10 -0400

    linting

--- spec/components/mfa_selection_component_spec.rb
+++ spec/components/mfa_selection_component_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe MfaSelectionComponent, type: :component do
   let(:lookup_context) { ActionView::LookupContext.new(ActionController::Base.view_paths) }
   let(:view_context) { ActionView::Base.new(lookup_context, {}, controller) }
   let(:form_object) { User.new }
-  let(:presenter) {TwoFactorOptionsPresenter.new(user_agent: nil)}
+  let(:presenter) { TwoFactorOptionsPresenter.new(user_agent: nil) }
   let(:form_builder) do
     SimpleForm::FormBuilder.new(form_object.model_name.param_key, form_object, view_context, {})
   end
 
   let(:options) do
-  {
-    form: form_builder,
-    option: presenter.options[4],
-  }.compact
-end
+    {
+      form: form_builder,
+      option: presenter.options[4],
+    }.compact
+  end
 
   subject(:rendered) do
     render_inline(described_class.new(**options))
@@ -32,5 +32,4 @@ end
       expect(rendered).to_not have_css('.checkbox__alert')
     end
   end
-
 end
