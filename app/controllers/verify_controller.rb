657d61ee521b8b11205cda1c4e1beb8acec5adef
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-03 11:13:40 -0400

    LG-6220: Split IdV app feature flags to per-step
    **Why**: So that we have the option to enable specific polished steps in deployed environments without needing to enable them all.
    
    changelog: Upcoming Features, Identity Verification, Add password reentry step screen

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -1,12 +1,12 @@
 class VerifyController < ApplicationController
+  include RenderConditionConcern
   include IdvSession
+
   before_action :confirm_two_factor_authenticated
   before_action :confirm_idv_vendor_session_started
   before_action :confirm_profile_has_been_created
 
-  include RenderConditionConcern
-
-  check_or_render_not_found -> { IdentityConfig.store.idv_api_enabled }, only: [:show]
+  check_or_render_not_found -> { FeatureManagement.idv_api_enabled? }, only: [:show]
 
   def show
     @app_data = app_data
@@ -20,6 +20,7 @@ class VerifyController < ApplicationController
       app_name: APP_NAME,
       completion_url: completion_url,
       initial_values: { 'personalKey' => personal_key },
+      enabled_step_names: IdentityConfig.store.idv_api_enabled_steps,
     }
   end
 
