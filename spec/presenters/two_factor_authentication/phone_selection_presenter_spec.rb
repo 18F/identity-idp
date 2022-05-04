36d5e696e1f67da71ebe1813772388d9759b3d0e
Alex Bradley <alexander.bradley@gsa.gov>
2022-05-04 15:23:04 -0400

    LG-5700 Fix replicated content for the "Select your authentication method" page (#6261)
    Separate Voice and SMS option text
    
    changelog: Improvements, Content, Separate phone and sms text labels
    
    * add option to not show sms voice if phone option is available
    
    * remove voice and sms options from options_presenter

--- spec/presenters/two_factor_authentication/phone_selection_presenter_spec.rb
+++ spec/presenters/two_factor_authentication/phone_selection_presenter_spec.rb
@@ -4,14 +4,6 @@ RSpec.describe TwoFactorAuthentication::PhoneSelectionPresenter do
   let(:presenter) { described_class.new(phone) }
 
   describe '#info' do
-    context 'when a user has a phone configuration' do
-      let(:phone) { build(:phone_configuration, phone: '+1 888 867-5309') }
-
-      it 'includes the masked the number' do
-        expect(presenter.info).to include('(***) ***-5309')
-      end
-    end
-
     context 'when a user does not have a phone configuration (first time)' do
       let(:phone) { nil }
 
