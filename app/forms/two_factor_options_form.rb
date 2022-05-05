f29a5231a84bc53ad21ef5ec70e3a42787974d35
SammySteiner <sammysteiner@gmail.com>
2022-05-05 11:58:18 -0400

    github comments

--- app/forms/two_factor_options_form.rb
+++ app/forms/two_factor_options_form.rb
@@ -7,7 +7,10 @@ class TwoFactorOptionsForm
   validates :selection, inclusion: { in: %w[phone sms voice auth_app piv_cac
                                             webauthn webauthn_platform
                                             backup_code] }
-  validates :selection, length: { minimum: 2, message: 'phone' }, if: :phone_selected?
+  validates :selection, length: { minimum: 2, message: 'phone' }, if: [
+    :multiple_mfa_options_enabled?,
+    :phone_selected?,
+  ]
 
   def initialize(user)
     self.user = user
@@ -43,8 +46,11 @@ class TwoFactorOptionsForm
     UpdateUser.new(user: user, attributes: user_attributes).call
   end
 
+  def multiple_mfa_options_enabled?
+    IdentityConfig.store.select_multiple_mfa_options
+  end
+
   def phone_selected?
-    return false if !IdentityConfig.store.select_multiple_mfa_options
     selection.include?('phone') || selection.include?('voice') || selection.include?('sms')
   end
 end
