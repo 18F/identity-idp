9e5b29908166c371f2dbe31ecfee0afb6ea50042
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-05 09:51:42 -0400

    Check current path in array

--- spec/features/idv/steps/review_step_spec.rb
+++ spec/features/idv/steps/review_step_spec.rb
@@ -31,7 +31,7 @@ feature 'idv review step' do
     click_idv_continue
 
     expect(page).to have_content(t('headings.personal_key'))
-    expect(page).to have_current_path(idv_personal_key_path)
+    expect(current_path).to be_in([idv_personal_key_path, idv_app_root_path])
   end
 
   context 'choosing to confirm address with phone' do
