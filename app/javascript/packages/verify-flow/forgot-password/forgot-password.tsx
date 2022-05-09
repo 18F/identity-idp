adfef126a4f930dc1ffcfab6a9e43ba62d257f55
Nadya Primak <nadya.primak@gsa.gov>
2022-05-09 15:53:09 -0400

    add image and hide step indicator

--- app/javascript/packages/verify-flow/forgot-password/forgot-password.tsx
+++ app/javascript/packages/verify-flow/forgot-password/forgot-password.tsx
@@ -1,20 +1,37 @@
 import { PageHeading } from '@18f/identity-components';
 import { t } from '@18f/identity-i18n';
 import { Button } from '@18f/identity-components';
+import { getAssetPath } from '@18f/identity-assets';
 
 interface ForgotPasswordProps {
   goBack: () => void;
 }
 
 export function ForgotPassword({ goBack }: ForgotPasswordProps) {
+  const stepIndicator = document.getElementsByTagName('lg-step-indicator')[0];
+  hideStepIndicator();
+
   function goToResetPassword() {
     const resetPasswordUrl = `${window.location.origin}/forgot_password`;
-    console.log(resetPasswordUrl);
     window.location.href = resetPasswordUrl;
+  }
+
+  function hideStepIndicator() {
+    stepIndicator.hidden = true;
+  }
+
+  function showStepIndicator() {
+    stepIndicator.hidden = false;
   }
 
   return (
     <>
+      <img
+        src={getAssetPath('status/info-question.svg')}
+        width="50"
+        height="50"
+        className="margin-bottom-4"
+      ></img>
       <PageHeading>{t('idv.forgot_password.modal_header')}</PageHeading>
       <ul className="usa-list">
         <li>{t('idv.forgot_password.warnings.warning_1')}</li>
@@ -22,9 +39,11 @@ export function ForgotPassword({ goBack }: ForgotPasswordProps) {
       </ul>
       <p>
         <Button
+          className="margin-top-4"
           isBig
           isWide
           onClick={() => {
+            showStepIndicator();
             goBack();
           }}
         >
