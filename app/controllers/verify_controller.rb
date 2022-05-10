8fc061a3591e56280830230e1c766105158246c1
Parissa <parissa.eggleston@gmail.com>
2022-05-10 16:42:29 -0400

    remove debugger

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -2,24 +2,13 @@ class VerifyController < ApplicationController
    include RenderConditionConcern
    include IdvSession
 
-  # before_action :confirm_two_factor_authenticated
-  # before_action :confirm_idv_vendor_session_started
-  # before_action :confirm_profile_has_been_created
+  before_action :confirm_two_factor_authenticated
+  before_action :confirm_idv_vendor_session_started
+  before_action :confirm_profile_has_been_created
 
-  # check_or_render_not_found -> { FeatureManagement.idv_api_enabled? }, only: [:show]
+  check_or_render_not_found -> { FeatureManagement.idv_api_enabled? }, only: [:show]
 
   def show
-    user_session[:idv][:applicant] =
-    { 'firstName' => 'Bruce',
-      'lastName' => 'Wayne',
-      'address1' => '1234 Batcave',
-      'address2' => '',
-      'city' => 'Batcavesville',
-      'state' => 'NY',
-      'zipcode' => '12345',
-      'dob' => '1988-03-30',
-      'ssn' => '900-12-3456',
-      'phone' => '2021234567'}
     @app_data = app_data
   end
 
