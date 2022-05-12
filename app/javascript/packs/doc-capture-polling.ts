f85a639795700a54b0741538e703a6bd23216d78
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-12 15:38:11 -0400

    Fix polling endpoint by attribute value

--- app/javascript/packs/doc-capture-polling.ts
+++ app/javascript/packs/doc-capture-polling.ts
@@ -1,7 +1,9 @@
 import { DocumentCapturePolling } from '@18f/identity-document-capture-polling';
 
 new DocumentCapturePolling({
-  statusEndpoint: document.querySelector('[data-status-endpoint]')?.textContent as string,
+  statusEndpoint: document
+    .querySelector('[data-status-endpoint]')
+    ?.getAttribute('data-status-endpoint') as string,
   elements: {
     backLink: document.querySelector('.link-sent-back-link') as HTMLAnchorElement,
     form: document.querySelector('.link-sent-continue-button-form') as HTMLFormElement,
