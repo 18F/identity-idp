0f3f5112e5b72bb56691157f809b0e695cb4b3c9
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-13 15:44:25 -0400

    Use URL constructor, full URLs for addSearchParams
    Simplify supported use-cases to only those we care about, so that we can simplify the implementation
    
    https://github.com/18F/identity-idp/pull/6350#discussion_r872691167

--- app/javascript/packages/url/index.spec.ts
+++ app/javascript/packages/url/index.spec.ts
@@ -10,36 +10,18 @@ describe('addSearchParams', () => {
     expect(actual).to.equal(expected);
   });
 
-  it('adds search params to a path', () => {
+  it('accepts URL as a path', () => {
     const url = '/example';
-    const expected = '/example?a=1&b=2&c=3';
+    const expected = `${window.location.origin}/example?a=1&b=2&c=3`;
 
     const actual = addSearchParams(url, { a: 1, b: 2, c: 3 });
 
     expect(actual).to.equal(expected);
   });
 
-  it('retains hash fragment unaltered', () => {
-    const url = '?a=1#example';
-    const expected = '?a=1&b=2#example';
-
-    const actual = addSearchParams(url, { b: 2 });
-
-    expect(actual).to.equal(expected);
-  });
-
-  it('adds search params to an existing search fragment', () => {
-    const params = '?a=1&b=1';
-    const expected = '?a=1&b=2&c=3';
-
-    const actual = addSearchParams(params, { b: 2, c: 3 });
-
-    expect(actual).to.equal(expected);
-  });
-
   it('adds search params to an empty URL', () => {
     const params = '';
-    const expected = '?a=1&b=2&c=3';
+    const expected = `${window.location.origin}/?a=1&b=2&c=3`;
 
     const actual = addSearchParams(params, { a: 1, b: 2, c: 3 });
 
--- app/javascript/packages/url/index.ts
+++ app/javascript/packages/url/index.ts
@@ -2,15 +2,13 @@
  * Given a URL or a string fragment of search parameters and an object of parameters, returns a
  * new URL or search parameters with the parameters added.
  *
- * @param url Original URL, path, or search parameters.
+ * @param url Original URL.
  * @param params Search parameters to add.
  *
- * @return Modified URL or search parameters.
+ * @return Modified URL.
  */
 export function addSearchParams(url: string, params: Record<string, any>): string {
-  const [prefix, searchAndFragment = ''] = url.split('?') as [string] | [string, string];
-  const [search, fragment] = searchAndFragment.split('#') as [string] | [string, string];
-  const searchParams = new URLSearchParams(search);
-  Object.entries(params).forEach(([key, value]) => searchParams.set(key, value));
-  return [prefix, [searchParams.toString(), fragment].filter(Boolean).join('#')].join('?');
+  const parsedURL = new URL(url, window.location.href);
+  Object.entries(params).forEach(([key, value]) => parsedURL.searchParams.set(key, value));
+  return parsedURL.toString();
 }
