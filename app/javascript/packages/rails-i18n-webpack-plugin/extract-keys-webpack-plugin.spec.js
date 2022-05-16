23b329ea50ac902e45009c47a5989c88aba6fe52
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-16 16:54:37 -0400

    i18n: Detect array keys for Webpack string extraction
    **Why**: In order for array key support implemented in #6328 to appear as translated, the keys must be extracted, which means we need additional support in the Webpack plugin to detect and extract array keys.
    
    changelog: Internal, Localization, Improve browser localization to support array of messages

--- app/javascript/packages/rails-i18n-webpack-plugin/extract-keys-webpack-plugin.spec.js
+++ app/javascript/packages/rails-i18n-webpack-plugin/extract-keys-webpack-plugin.spec.js
@@ -16,24 +16,30 @@ describe('getAdditionalAssetFilename', () => {
 
 describe('getTranslationKeys', () => {
   const source = `
-    import { t } from '@18f/identity-i18n';
+    const text = t('forms.button.submit');
+    const message = t('forms.messages', { count: 2 });
+    const values = t(['forms.key1', 'forms.key2']);
 
-    const text = t('explicit.call');
-
-    // i18n-tasks-use t('comment.added.1')
-    /* i18n-tasks-use t('comment.added.2') */
+    // i18n-tasks-use t('item.1')
+    /* i18n-tasks-use t('item.2') */
     /**
-     * i18n-tasks-use t('comment.added.3')
+     * i18n-tasks-use t('item.3')
      */
-    Array.from({ length: 3 }, (_, i) => t(\`comment.added.\${i + 1}\`))
+    Array.from({ length: 3 }, (_, i) => t(\`item.$\{i + 1}\`));
+    // Emulate Babel template literal transpilation
+    // See: https://babeljs.io/repl#?browsers=ie%2011&code_lz=C4CgBglsCmC2B0ASA3hABAajQRgL5gEog
+    Array.from({ length: 3 }, (_, i) => t('item.'.concat(i + 1)));
   `;
 
   it('returns keys', () => {
     expect(getTranslationKeys(source)).to.deep.equal([
-      'explicit.call',
-      'comment.added.1',
-      'comment.added.2',
-      'comment.added.3',
+      'forms.button.submit',
+      'forms.messages',
+      'forms.key1',
+      'forms.key2',
+      'item.1',
+      'item.2',
+      'item.3',
     ]);
   });
 });
