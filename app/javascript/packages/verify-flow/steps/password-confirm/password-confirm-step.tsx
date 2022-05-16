248f3dcaebc6ea3ebfd469495ec8d7139498fa1d
Parissa <parissa.eggleston@gmail.com>
2022-05-16 15:06:01 -0400

    update code review feedbacks

--- app/javascript/packages/verify-flow/steps/password-confirm/password-confirm-step.tsx
+++ app/javascript/packages/verify-flow/steps/password-confirm/password-confirm-step.tsx
@@ -1,50 +1,12 @@
 import { PageHeading, Accordion } from '@18f/identity-components';
 import { t } from '@18f/identity-i18n';
 import { FormStepsButton } from '@18f/identity-form-steps';
+import PersonalInfoSummary from './personal-info-summary';
 import type { FormStepComponentProps } from '@18f/identity-form-steps';
-import parsePhoneNumber from 'libphonenumber-js';
 import type { ChangeEvent } from 'react';
 import type { VerifyFlowValues } from '../..';
 
 interface PasswordConfirmStepProps extends FormStepComponentProps<VerifyFlowValues> {}
-
-function getDateFormat(date) {
-  date = new Date(date);
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
-}
-
-function PersonalInfoSummary({ pii }) {
-  const { firstName, lastName, dob, address1, address2, city, state, zipcode, ssn, phone } = pii;
-  const phoneNumber = parsePhoneNumber(`+1${phone}`);
-  return (
-    <div className="padding-x-4">
-      <div className="h6">{t('idv.review.full_name')}</div>
-      <div className="h4 text-bold ico-absolute ico-absolute-success">
-        {firstName} {lastName}
-      </div>
-      <div className="margin-top-4 h6">{t('idv.review.mailing_address')}</div>
-      <div className="h4 text-bold ico-absolute ico-absolute-success">
-        {address1} <br />
-        {address2 || ''}
-        <br />
-        {city && state ? `${city}, ${state} ${zipcode}` : ''}
-      </div>
-      <div className="margin-top-4 h6">{t('idv.review.dob')}</div>
-      <div className="h4 text-bold ico-absolute ico-absolute-success">{getDateFormat(dob)}</div>
-      <div className="margin-top-4 h6">{t('idv.review.ssn')}</div>
-      <div className="h4 text-bold ico-absolute ico-absolute-success">{ssn}</div>
-      {phone && (
-        <>
-          <div className="h6 margin-top-4"> {t('idv.messages.phone.phone_of_record')}</div>
-          <div className="h4 text-bold ico-absolute ico-absolute-success">
-            {phoneNumber?.formatNational()}
-          </div>
-        </>
-      )}
-    </div>
-  );
-}
 
 function PasswordConfirmStep({ registerField, onChange, value }: PasswordConfirmStepProps) {
   return (
