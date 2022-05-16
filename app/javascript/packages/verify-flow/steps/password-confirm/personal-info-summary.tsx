06fd1a0fa937cb1d9380dd651db7a26c4b4e08b8
Parissa <parissa.eggleston@gmail.com>
2022-05-16 15:18:26 -0400

    initial load

---
+++ app/javascript/packages/verify-flow/steps/password-confirm/personal-info-summary.tsx
@@ -0,0 +1,42 @@
+import { parse, format } from 'libphonenumber-js';
+import { t } from '@18f/identity-i18n';
+
+function PersonalInfoSummary({ pii }) {
+  const { firstName, lastName, dob, address1, address2, city, state, zipcode, ssn, phone } = pii;
+  const phoneNumber = parse(`+1${phone}`);
+  const formatted = format(phoneNumber, 'NATIONAL');
+
+  function getDateFormat(date) {
+    date = new Date(date);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  }
+
+  return (
+    <div className="padding-x-4">
+      <div className="h6">{t('idv.review.full_name')}</div>
+      <div className="h4 text-bold ico-absolute ico-absolute-success">
+        {firstName} {lastName}
+      </div>
+      <div className="margin-top-4 h6">{t('idv.review.mailing_address')}</div>
+      <div className="h4 text-bold ico-absolute ico-absolute-success">
+        {address1} <br />
+        {address2 || ''}
+        <br />
+        {city && state ? `${city}, ${state} ${zipcode}` : ''}
+      </div>
+      <div className="margin-top-4 h6">{t('idv.review.dob')}</div>
+      <div className="h4 text-bold ico-absolute ico-absolute-success">{getDateFormat(dob)}</div>
+      <div className="margin-top-4 h6">{t('idv.review.ssn')}</div>
+      <div className="h4 text-bold ico-absolute ico-absolute-success">{ssn}</div>
+      {phone && (
+        <>
+          <div className="h6 margin-top-4"> {t('idv.messages.phone.phone_of_record')}</div>
+          <div className="h4 text-bold ico-absolute ico-absolute-success">{formatted}</div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default PersonalInfoSummary;
