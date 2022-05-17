2bb9dcd63498230df5b754dcc97cb4b29f5975a9
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-17 13:53:43 -0400

    Clarify key ordering requirement
    See: https://github.com/18F/identity-idp/pull/6363#discussion_r875090737
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>

--- app/javascript/packages/react-hooks/use-object-memo.ts
+++ app/javascript/packages/react-hooks/use-object-memo.ts
@@ -7,8 +7,8 @@ import { useMemo } from 'react';
  * This can be useful in situations like object context values, since without memoization an object
  * context would trigger re-renders of all consumers on every update.
  *
- * Note that the keys of the object must remain the same for the lifecycle of the component for the
- * hook to work correctly.
+ * Note that the keys of the object (and their order) must remain the same for the lifecycle of the
+ * component for the hook to work correctly.
  *
  * @param object Object to memoize.
  *
