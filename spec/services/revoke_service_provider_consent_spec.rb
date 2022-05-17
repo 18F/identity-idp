9b3406d998932fe1b73ea0eade36ba432911e05c
Douglas Price <douglas.price@gsa.gov>
2022-05-17 18:20:51 -0400

    Clear the verified attributes when a user disconnects from an app (LG-5981) changelog: Bug Fixes, Account management, Fixed an issue where disconnecting from an IAL2 application doesn't reset consent on IAL2 attributes

--- spec/services/revoke_service_provider_consent_spec.rb
+++ spec/services/revoke_service_provider_consent_spec.rb
@@ -6,12 +6,18 @@ RSpec.describe RevokeServiceProviderConsent do
   subject(:service) { RevokeServiceProviderConsent.new(identity, now: now) }
 
   describe '#call' do
-    let!(:identity) { create(:service_provider_identity, deleted_at: nil) }
+    let!(:identity) { create(:service_provider_identity, deleted_at: nil, verified_attributes: ['email']) }
 
     it 'sets the deleted_at' do
       expect { service.call }.
         to change { identity.reload.deleted_at&.to_i }.
         from(nil).to(now.to_i)
+    end
+
+    it 'clears the verified attributes' do
+      expect { service.call }.
+        to change { identity.reload.verified_attributes }.
+        from(['email']).to(nil)
     end
   end
 end
