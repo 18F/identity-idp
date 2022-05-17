c7d0d6c92bfeed292f38007b07173c3d38d24408
Mitchell Henke <mitchell.henke@gsa.gov>
2022-05-17 16:03:11 -0500

    show configured 2FA on account page
    changelog: Improvements, Account Page, Show configured two-factor methods on main account page

--- spec/policies/webauthn_login_option_policy_spec.rb
+++ spec/policies/webauthn_login_option_policy_spec.rb
@@ -28,4 +28,29 @@ describe TwoFactorAuthentication::WebauthnPolicy do
       end
     end
   end
+
+  describe '#roaming_enabled?' do
+    context 'without a webauthn configured' do
+      let(:user) { build(:user) }
+
+      it { expect(subject.roaming_enabled?).to be_falsey }
+    end
+
+    context 'with a roaming webauthn configured' do
+      let(:user) { create(:user) }
+      before do
+        create(
+          :webauthn_configuration,
+          user: user,
+          credential_id: credential_id,
+          credential_public_key: credential_public_key,
+          platform_authenticator: false,
+        )
+      end
+
+      it 'returns a truthy value' do
+        expect(subject.roaming_enabled?).to be_truthy
+      end
+    end
+  end
 end
