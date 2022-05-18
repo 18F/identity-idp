729bf88285d6acc208bffb5413c6069a0aa1a816
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-18 09:41:49 -0400

    Add link component
    For external, new-tab link behavior required in password confirm step

--- app/javascript/packages/components/index.ts
+++ app/javascript/packages/components/index.ts
@@ -5,6 +5,7 @@ export { default as ButtonTo } from './button-to';
 export { default as BlockLink } from './block-link';
 export { default as Icon } from './icon';
 export { default as FullScreen } from './full-screen';
+export { default as Link } from './link';
 export { default as PageHeading } from './page-heading';
 export { default as SpinnerDots } from './spinner-dots';
 export { default as TextInput } from './text-input';
