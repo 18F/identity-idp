798fefe9be915876c85c2e62d4aa44c18652aca2
Sammy <sammysteiner@gmail.com>
2022-05-18 13:37:07 -0400

    Update spec/presenters/two_factor_authentication/auth_app_selection_presenter_spec.rb
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- spec/presenters/two_factor_authentication/auth_app_selection_presenter_spec.rb
+++ spec/presenters/two_factor_authentication/auth_app_selection_presenter_spec.rb
@@ -4,12 +4,12 @@ describe TwoFactorAuthentication::AuthAppSelectionPresenter do
   let(:configuration) {}
   let(:user_without_mfa) { create(:user) }
   let(:user_with_mfa) { create(:user, :with_authentication_app) }
-  let(:presenter_without_mfa) {
- described_class.new(configuration: configuration, user: user_without_mfa)
-  }
-  let(:presenter_with_mfa) {
- described_class.new(configuration: configuration, user: user_with_mfa)
-  }
+  let(:presenter_without_mfa) do
+    described_class.new(configuration: configuration, user: user_without_mfa)
+  end
+  let(:presenter_with_mfa) do
+    described_class.new(configuration: configuration, user: user_with_mfa)
+  end
 
   describe '#type' do
     it 'returns auth_app' do
