bbf16a395d7015b3b88f403983480784fb37ae81
SammySteiner <sammysteiner@gmail.com>
2022-05-19 14:17:19 -0400

    fix tests

--- spec/views/users/edit_phone/remove_phone.html.erb_spec.rb
+++ spec/views/users/edit_phone/remove_phone.html.erb_spec.rb
@@ -4,25 +4,40 @@ describe 'users/edit_phone/_remove_phone.html.erb' do
   include Devise::Test::ControllerHelpers
 
   let(:user) { create(:user, :with_phone) }
-  let(:phone_configuration) { create(:phone_configuration, user: user, phone: '+1 703-555-1214') }
 
-  context 'with multi mfa disabled' do
+  context 'when there are only 2 phone configurations' do
+    let(:phone_configuration) { create(:phone_configuration, user: user, phone: '+1 703-555-1214') }
     before do
-      allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return false
+      allow(view).to receive(:current_user).and_return(user)
       assign(:phone_configuration, phone_configuration)
     end
 
-    it 'does not render a delete phone button' do
+    it 'renders a delete phone button' do
+      render
+
+      expect(rendered).to have_button('Remove phone')
+    end
+  end
+
+  context 'when there is only 1 phone configuration' do
+    before do
+      allow(view).to receive(:current_user).and_return(user)
+      assign(:phone_configuration, user.phone_configurations.first)
+    end
+
+    it 'renders a delete phone button' do
       render
 
       expect(rendered).to eq('')
     end
   end
 
-  context 'with multi mfa enabled' do
+  context 'when there is 1 phone configuration and 1 other configuration' do
+    let(:user) { create(:user, :with_phone, :with_authentication_app) }
+
     before do
-      allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return true
-      assign(:phone_configuration, phone_configuration)
+      allow(view).to receive(:current_user).and_return(user)
+      assign(:phone_configuration, user.phone_configurations.first)
     end
 
     it 'renders a delete phone button' do
