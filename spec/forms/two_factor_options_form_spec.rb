65c1de063968ddd90c38d672746a5a13adf43448
Jessica Dembe <jessica.dembe@gsa.gov>
2022-05-19 11:58:42 -0400

    uncomment tests

--- spec/forms/two_factor_options_form_spec.rb
+++ spec/forms/two_factor_options_form_spec.rb
@@ -13,72 +13,72 @@ describe TwoFactorOptionsForm do
       end
     end
 
-    # it 'is unsuccessful if the selection is invalid for multi mfa' do
-    #   allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
-    #   %w[phone sms voice !!!!].each do |selection|
-    #     result = subject.submit(selection: selection)
+    it 'is unsuccessful if the selection is invalid for multi mfa' do
+      allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
+      %w[phone sms voice !!!!].each do |selection|
+        result = subject.submit(selection: selection)
 
-    #     expect(result.success?).to eq false
-    #   end
-    # end
+        expect(result.success?).to eq false
+      end
+    end
 
-    # it 'is unsuccessful if the selection is invalid' do
-    #   %w[!!!!].each do |selection|
-    #     result = subject.submit(selection: selection)
+    it 'is unsuccessful if the selection is invalid' do
+      %w[!!!!].each do |selection|
+        result = subject.submit(selection: selection)
 
-    #     expect(result.success?).to eq false
-    #     expect(result.errors).to include :selection
-    #   end
-    # end
+        expect(result.success?).to eq false
+        expect(result.errors).to include :selection
+      end
+    end
 
-    # context "when the selection is different from the user's otp_delivery_preference" do
-    #   it "updates the user's otp_delivery_preference if they have an alternate method selected" do
-    #     user_updater = instance_double(UpdateUser)
-    #     allow(UpdateUser).
-    #       to receive(:new).
-    #       with(
-    #         user: user,
-    #         attributes: { otp_delivery_preference: 'voice' },
-    #       ).
-    #       and_return(user_updater)
-    #     expect(user_updater).to receive(:call)
+    context "when the selection is different from the user's otp_delivery_preference" do
+      it "updates the user's otp_delivery_preference if they have an alternate method selected" do
+        user_updater = instance_double(UpdateUser)
+        allow(UpdateUser).
+          to receive(:new).
+          with(
+            user: user,
+            attributes: { otp_delivery_preference: 'voice' },
+          ).
+          and_return(user_updater)
+        expect(user_updater).to receive(:call)
 
-    #     subject.submit(selection: ['voice', 'backup_code'])
-    #   end
-    # end
+        subject.submit(selection: ['voice', 'backup_code'])
+      end
+    end
 
-    # context "when the selection is the same as the user's otp_delivery_preference" do
-    #   it "does not update the user's otp_delivery_preference" do
-    #     expect(UpdateUser).to_not receive(:new)
+    context "when the selection is the same as the user's otp_delivery_preference" do
+      it "does not update the user's otp_delivery_preference" do
+        expect(UpdateUser).to_not receive(:new)
 
-    #     subject.submit(selection: 'sms')
-    #   end
-    # end
+        subject.submit(selection: 'sms')
+      end
+    end
 
-    # context 'when the selection is not voice or sms' do
-    #   it "does not update the user's otp_delivery_preference" do
-    #     expect(UpdateUser).to_not receive(:new)
+    context 'when the selection is not voice or sms' do
+      it "does not update the user's otp_delivery_preference" do
+        expect(UpdateUser).to_not receive(:new)
 
-    #     subject.submit(selection: 'auth_app')
-    #   end
-    # end
+        subject.submit(selection: 'auth_app')
+      end
+    end
 
-    # context 'when phone is selected as their first authentication method' do
-    #   before do
-    #     allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
-    #   end
+    context 'when phone is selected as their first authentication method' do
+      before do
+        allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
+      end
 
-    #   it 'does not submit the phone when selected as the first single option' do
-    #     %w[phone].each do |selection|
-    #       result = subject.submit(selection: selection)
+      it 'does not submit the phone when selected as the first single option' do
+        %w[phone].each do |selection|
+          result = subject.submit(selection: selection)
 
-    #       expect(result.success?).to eq false
-    #     end
-    #   end
-    # end
+          expect(result.success?).to eq false
+        end
+      end
+    end
 
     context 'when a user wants to select phone as their second authentication method' do
-      let(:user) { create(:user, :with_authentication_app) }   
+      let(:user) { create(:user, :with_authentication_app) }
       before do
         allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
       end
@@ -90,6 +90,6 @@ describe TwoFactorOptionsForm do
           expect(result.success?).to eq true
         end
       end
-    end      
+    end
   end
 end
