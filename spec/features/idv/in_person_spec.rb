e65866941c5d5b48f307924cc27bcda8a6c7aa98
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2022-05-20 15:58:37 -0400

    Begin adding specs

---
+++ spec/features/idv/in_person_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'In Person Proofing' do
+  # create a user
+  # login
+  # starting doc auth
+  # failing doc auth
+  # clicking through continue for each IPP page
+  # visit idv_doc_auth_welcome_step unless current_path == idv_doc_auth_welcome_step
+end
