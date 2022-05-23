234f7b4ade8a1689f6c3fa58321985bf699f32a4
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-05-23 15:43:15 -0400

    Use a depth instead of recursion in the lookup shared device users tooling (#6399)
    We've had issues with this script running into infinite recursion or large groups of related users. This commit prevents that by stopping at a certain depth instead of building a full map of shared devices.
    
    When we wrote this script, we did not have an index on the cookie_uuid column which mean that querying against cookie_uuids was slow and expensive. We now have that index so we can afford to take a more simple approach here.
    
    changelog: Internal, Data requests, The script for looking up users who share devices was changed to avoid infinite recursion.

--- app/services/data_requests/lookup_shared_device_users.rb
+++ app/services/data_requests/lookup_shared_device_users.rb
@@ -9,66 +9,26 @@ module DataRequests
   # network are found.
   #
   class LookupSharedDeviceUsers
-    attr_reader :initial_users
+    attr_reader :initial_users, :depth
 
-    def initialize(initial_users)
+    def initialize(initial_users, depth = 3)
       @initial_users = initial_users
+      @depth = depth
+      @user_ids = initial_users.map(&:id).to_set
+      @device_cookie_uuids = Set.new
     end
 
     def call
-      lookup_users(initial_users)
-      loop do
-        break if user_uuids_to_lookup.empty?
-        lookup_users(users_to_lookup)
+      depth.times do |i|
+        warn "Searching at depth #{i}"
+        self.device_cookie_uuids += Device.where(user_id: user_ids).pluck(:cookie_uuid)
+        self.user_ids += Device.where(cookie_uuid: device_cookie_uuids).pluck(:user_id)
       end
-      user_uuids_by_device
+      User.where(id: user_ids).all
     end
 
     private
 
-    def lookup_device(device)
-      cookie_uuid = device.cookie_uuid
-      return if user_uuids_by_device.key?(cookie_uuid)
-
-      warn "Searching for new devices matching #{cookie_uuid}"
-      user_ids = Device.where(cookie_uuid: cookie_uuid).pluck(:user_id)
-      user_uuids = User.where(id: user_ids).pluck(:uuid)
-      user_uuids_by_device[cookie_uuid] = user_uuids
-    end
-
-    def lookup_users(users)
-      users.each do |user|
-        lookup_user(user)
-      end
-    end
-
-    def lookup_user(user)
-      warn "Looking up devices for user: #{user.uuid}"
-      devices = user.devices
-      devices.each { |device| lookup_device(device) }
-      looked_up_user_uuids.add(user.uuid)
-    end
-
-    def looked_up_user_uuids
-      @looked_up_user_uuids ||= Set.new
-    end
-
-    def users_to_lookup
-      user_uuids_to_lookup.map do |uuid|
-        User.find_by(uuid: uuid)
-      end
-    end
-
-    ##
-    # A hash where the key is a cookie UUID and the values are the UUIDs for the
-    # users associated with that device
-    #
-    def user_uuids_by_device
-      @user_uuids_by_device ||= {}
-    end
-
-    def user_uuids_to_lookup
-      Set.new(user_uuids_by_device.values.flatten) - looked_up_user_uuids
-    end
+    attr_accessor :user_ids, :device_cookie_uuids
   end
 end
