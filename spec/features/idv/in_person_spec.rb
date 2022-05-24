65ede85284122746829616dc982c911a0b0b9448
Tomas Apodaca <thomas.apodaca@gsa.gov>
2022-05-24 13:19:25 -0700

    update feature spec

--- spec/features/idv/in_person_spec.rb
+++ spec/features/idv/in_person_spec.rb
@@ -5,26 +5,70 @@ RSpec.describe 'In Person Proofing' do
   include IdvHelper
 
   it 'works for a happy path', js: true do
-    user = sign_in_and_2fa_user
+    sign_in_and_2fa_user
 
     # welcome step
     visit idv_doc_auth_welcome_step # only thing used from DocAuthHelper
     click_idv_continue
 
     # information step
-    click_on t('doc_auth.buttons.continue')
     find('label', text: t('doc_auth.instructions.consent', app_name: APP_NAME)).click
-
-    click_on t('doc_auth.info.upload_computer_link')
-
-    sleep 100
-
-    # image upload step
-    attach_images_that_fail
     click_idv_continue
 
-    # clicking through continue for each IPP page
+    # upload documents step
+    click_on t('doc_auth.info.upload_computer_link')
+    attach_images_that_fail
+    click_submit_default
+    expect(page).to have_content(t('idv.troubleshooting.options.verify_in_person'), wait: 60)
+
+    # start the IPP flow
+    # TODO: temporarily block the "Leave site?" pop-up from appearing; this can be removed
+    #       when LG-6406 is resolved.
+    execute_script 'window.onbeforeunload = undefined;'
     click_link t('idv.troubleshooting.options.verify_in_person')
+
+    # location page
+    expect(page).to have_content(t('in_person_proofing.headings.location'))
+    click_idv_continue
+
+    # welcome page
+    expect(page).to have_content(t('in_person_proofing.headings.welcome'))
+    click_idv_continue
+
+    # address page
+    expect(page).to have_content(t('in_person_proofing.headings.address'))
+    click_idv_continue
+
+    # state ID page
+    expect(page).to have_content(t('in_person_proofing.headings.state_id'))
+    click_idv_continue
+
+    # ssn page
+    expect(page).to have_content(t('in_person_proofing.headings.ssn'))
+    click_idv_continue
+
+    # verify page
+    expect(page).to have_content(t('in_person_proofing.headings.verify'))
+    click_idv_continue
+
+    # phone page
+    expect(page).to have_content(t('in_person_proofing.headings.phone'))
+    click_idv_continue
+
+    # password confirm page
+    expect(page).to have_content(t('in_person_proofing.headings.password_confirm'))
+    click_idv_continue
+
+    # personal key page
+    expect(page).to have_content(t('in_person_proofing.headings.personal_key'))
+    click_idv_continue
+
+    # barcode page
+    expect(page).to have_content(t('in_person_proofing.headings.barcode'))
+    click_idv_continue
+
+    # returns to account page
+    expect(page).to have_content(t('headings.account.login_info'))
   end
 
   def attach_images_that_fail
