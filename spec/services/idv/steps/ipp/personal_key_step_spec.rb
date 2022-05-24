20b46b13d5adfa919f596d811c3684305846bc6c
Tomas Apodaca <thomas.apodaca@gsa.gov>
2022-05-24 13:18:38 -0700

    add unit tests for all pages

---
+++ spec/services/idv/steps/ipp/personal_key_step_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe Idv::Steps::Ipp::PersonalKeyStep do
+  include Rails.application.routes.url_helpers
+
+  let(:user) { build(:user) }
+  let(:service_provider) { create(:service_provider) }
+  let(:controller) do
+    instance_double(
+      'controller',
+      session: { sp: { issuer: service_provider.issuer } },
+      current_user: user,
+    )
+  end
+
+  let(:flow) do
+    Idv::Flows::InPersonFlow.new(controller, {}, 'idv/in_person')
+  end
+
+  subject(:step) do
+    Idv::Steps::Ipp::PersonalKeyStep.new(flow)
+  end
+
+  describe '#call' do
+    it 'works' do
+      result = step.call
+      expect(result).to be_nil
+    end
+  end
+end
