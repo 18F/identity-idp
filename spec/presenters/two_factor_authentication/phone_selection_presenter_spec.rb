49bf1ad35f250ef2d6b4c9007bbe13f505729903
Jessica Dembe <jessica.dembe@gsa.gov>
2022-05-26 11:50:54 -0400

    add tests

--- spec/presenters/two_factor_authentication/phone_selection_presenter_spec.rb
+++ spec/presenters/two_factor_authentication/phone_selection_presenter_spec.rb
@@ -61,5 +61,15 @@ RSpec.describe TwoFactorAuthentication::PhoneSelectionPresenter do
         ),
       )
     end
+
+    it 'includes a note to select an additional mfa method on first setup' do
+      expect(presenter_without_mfa.info).
+          to include(t('two_factor_authentication.two_factor_choice_options.phone_info'))
+    end
+
+    it 'does not include a note to select an additional mfa on additional setup' do
+      expect(presenter_without_mfa.info).
+          to include(t('two_factor_authentication.two_factor_choice_options.phone_info_html'))
+    end
   end
 end
