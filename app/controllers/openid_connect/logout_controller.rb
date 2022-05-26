3ce784546f8f597b87b84acc5d309344e33dfc89
Alex Bradley <alexander.bradley@gsa.gov>
2022-05-26 13:22:18 -0400

    LG-5936 Document Analytics 17 (#6362)
    * idv GPO verification visited
    
    * invalid authenticity token
    
    * logout initiated
    
    * Multi Factor Auth enter OTP visited
    
    changelog: Internal, Analytics, Update Analytics Events

--- app/controllers/openid_connect/logout_controller.rb
+++ app/controllers/openid_connect/logout_controller.rb
@@ -9,7 +9,7 @@ module OpenidConnect
 
       result = @logout_form.submit
 
-      analytics.track_event(Analytics::LOGOUT_INITIATED, result.to_h.except(:redirect_uri))
+      analytics.logout_initiated(**result.to_h.except(:redirect_uri))
 
       if result.success? && (redirect_uri = result.extra[:redirect_uri])
         sign_out
