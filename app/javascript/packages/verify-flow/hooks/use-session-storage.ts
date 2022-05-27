72f61fc0e03e845cc1bc2422fdbdbafe273c3f8b
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-27 13:04:22 -0400

    Clear session values when flow is complete (IdV app) (#6424)
    **Why**: So that lingering values don't remain beyond the completion of identity verification, even if those values would have been encrypted.
    
    changelog: Upcoming Features, Identity Verification, Clear session values upon completion

--- app/javascript/packages/verify-flow/hooks/use-session-storage.ts
+++ app/javascript/packages/verify-flow/hooks/use-session-storage.ts
@@ -9,10 +9,12 @@ import type { Dispatch } from 'react';
  *
  * @return Tuple of the current value and a setter to assign a value into storage.
  */
-function useSessionStorage(key: string): [string | null, Dispatch<string>] {
+function useSessionStorage(key: string): [string | null, Dispatch<string | null>] {
   const [value, setValue] = useState(() => sessionStorage.getItem(key));
   useDidUpdateEffect(() => {
-    if (value !== null) {
+    if (value === null) {
+      sessionStorage.removeItem(key);
+    } else {
       sessionStorage.setItem(key, value);
     }
   }, [value]);
