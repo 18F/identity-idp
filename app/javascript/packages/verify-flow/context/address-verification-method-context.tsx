193a97b803c44b50e8fb8b54a78ee682a0d206bb
Andrew Duthie <andrew.duthie@gsa.gov>
2022-05-31 15:37:00 -0400

    Document, test AddressVerificationMethodContextProvider

--- app/javascript/packages/verify-flow/context/address-verification-method-context.tsx
+++ app/javascript/packages/verify-flow/context/address-verification-method-context.tsx
@@ -1,22 +1,49 @@
 import { createContext, useState } from 'react';
 import type { ReactNode } from 'react';
 
+/**
+ * Mechanisms by which a user can verify their address.
+ */
 export type AddressVerificationMethod = 'phone' | 'gpo';
 
+/**
+ * Context provider props.
+ */
 interface AddressVerificationMethodContextProviderProps {
+  /**
+   * Optional initial context value.
+   */
   initialMethod?: AddressVerificationMethod;
 
+  /**
+   * Context children.
+   */
   children?: ReactNode;
 }
 
+/**
+ * Context value.
+ */
 type AddressVerificationMethodContextValue = [
+  /**
+   * Current address verification method.
+   */
   addressVerificationMethod: AddressVerificationMethod,
 
+  /**
+   * Setter to update to a new address verification method.
+   */
   setAddressVerificationMethod: (nextMethod: AddressVerificationMethod) => void,
 ];
 
+/**
+ * Default address verification method.
+ */
 const DEFAULT_METHOD: AddressVerificationMethod = 'phone';
 
+/**
+ * Address verification method context container.
+ */
 const AddressVerificationMethodContext = createContext<AddressVerificationMethodContextValue>([
   DEFAULT_METHOD,
   () => {},
