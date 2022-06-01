45fafcaa18384cc4e74bebdbb2be4e9b001e6c05
Andrew Duthie <andrew.duthie@gsa.gov>
2022-06-01 13:59:46 -0400

    Define required_step using class_attribute
    See:
    
    - https://github.com/18F/identity-idp/pull/6435#commitcomment-75106263
    - https://github.com/18F/identity-idp/pull/6435#issuecomment-1143962258

--- app/controllers/api/verify/base_controller.rb
+++ app/controllers/api/verify/base_controller.rb
@@ -3,21 +3,17 @@ module Api
     class BaseController < ApplicationController
       include RenderConditionConcern
 
+      class_attribute :required_step
+
       check_or_render_not_found -> do
+        if self.class.required_step.blank?
+          raise NotImplementedError, 'Controller must define required_step'
+        end
         IdentityConfig.store.idv_api_enabled_steps.include?(self.class.required_step)
       end
       before_action :confirm_two_factor_authenticated_for_api
 
       respond_to :json
-
-      def self.required_step
-        raise NotImplementedError if @step.blank?
-        @step
-      end
-
-      def self.required_step=(step)
-        @step = step
-      end
 
       private
 
