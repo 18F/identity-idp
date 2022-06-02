98489656812a0048cbf894fcd31e6c8d8307e1e3
SammySteiner <sammysteiner@gmail.com>
2022-06-02 10:10:57 -0400

    presenter tests

--- spec/presenters/mfa_confirmation_presenter_spec.rb
+++ spec/presenters/mfa_confirmation_presenter_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe MfaConfirmationPresenter do
   let(:user) { create(:user, :with_phone) }
   let(:presenter) { described_class.new(user) }
+  let(:non_restriced_user) { create(:user, :with_authentication_app) }
+  let(:non_restriced_presenter) { described_class.new(non_restriced_user) }
 
   before do
     allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
@@ -11,6 +13,7 @@ describe MfaConfirmationPresenter do
   describe '#enforce_second_mfa?' do
     it 'checks the multi mfa feature flag and whether the user has a non restriced mfa' do
       expect(presenter.enforce_second_mfa?).to be true
+      expect(non_restriced_presenter.enforce_second_mfa?).to be false
     end
   end
 
@@ -18,20 +21,26 @@ describe MfaConfirmationPresenter do
     it 'supplies a message depending on #enforce_second_mfa?' do
       expect(presenter.heading).
         to eq(t('mfa.non_restricted.heading'))
+      expect(non_restriced_presenter.heading).
+        to eq(t('titles.mfa_setup.suggest_second_mfa'))
     end
   end
 
-  # describe '#info?' do
-  #   it 'supplies a message depending on #enforce_second_mfa?' do
-  #     expect(presenter.confirm_delete_message).
-  #       to eq(t('email_addresses.delete.confirm', email: email_address.email))
-  #   end
-  # end
+  describe '#info?' do
+    it 'supplies a message depending on #enforce_second_mfa?' do
+      expect(presenter.info).
+        to eq(t('mfa.non_restricted.info_html'))
+      expect(non_restriced_presenter.info).
+        to eq(t('mfa.account_info'))
+    end
+  end
 
-  # describe '#button?' do
-  #   it 'supplies a message depending on #enforce_second_mfa?' do
-  #     expect(presenter.confirm_delete_message).
-  #       to eq(t('email_addresses.delete.confirm', email: email_address.email))
-  #   end
-  # end
+  describe '#button?' do
+    it 'supplies a message depending on #enforce_second_mfa?' do
+      expect(presenter.button).
+        to eq(t('mfa.non_restricted.button'))
+      expect(non_restriced_presenter.button).
+        to eq(t('mfa.add'))
+    end
+  end
 end
