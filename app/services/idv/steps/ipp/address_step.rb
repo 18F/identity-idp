7ce0d824d83c53fdbdbbf5ffbd4731b99c1ad10b
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2022-06-09 09:52:28 -0400

    Copy existing address page into IPP flow

--- app/services/idv/steps/ipp/address_step.rb
+++ app/services/idv/steps/ipp/address_step.rb
@@ -3,7 +3,29 @@ module Idv
     module Ipp
       class AddressStep < DocAuthBaseStep
         STEP_INDICATOR_STEP = :verify_info
-        def call; end
+
+        FIELDS = [
+          :address1, :address2, :city, :state,
+          :zipcode
+        ]
+
+        def call
+          flow_session[:pii_from_user].merge!(flow_params.permit(*FIELDS).slice(*FIELDS))
+        end
+
+        def extra_view_variables
+          {
+            pii: flow_session[:pii_from_user],
+          }
+        end
+
+        private
+
+        def form_submit
+          Idv::AddressForm.new(current_user).submit(
+            permit(*FIELDS),
+          )
+        end
       end
     end
   end
