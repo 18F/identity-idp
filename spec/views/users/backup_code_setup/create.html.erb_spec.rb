571b51a5206e48d770ad8dd8ec93ece5e689fe59
SammySteiner <sammysteiner@gmail.com>
2022-06-14 16:11:27 -0400

    tests

---
+++ spec/views/users/backup_code_setup/create.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'data_uri'
+
+describe 'users/backup_code_setup/create.html.erb' do
+  let(:user) { build(:user, :signed_up) }
+
+  module URI
+    def self.decode(value)
+      CGI.unescape(value)
+    end
+  end
+
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+    @codes = BackupCodeGenerator.new(user).create
+  end
+
+  it 'displays download link with plain text content equal to the users backup codes' do
+    render
+
+    doc = Nokogiri::HTML(rendered)
+    download_link = doc.at_css('a[download]')
+    data_uri = URI::Data.new(download_link[:href])
+
+    expect(rendered).to have_content((t('forms.backup_code.download')))
+    expect(data_uri.content_type).to eq('text/plain')
+    expect(data_uri.data).to eq(@codes.join("\n"))
+  end
+end
