3908574b245489f49cf1b384933936474dfc22cb
Andrew Duthie <andrew.duthie@gsa.gov>
2022-06-14 10:33:31 -0400

    Avoid all browser stubbing

--- spec/support/features/doc_capture_helper.rb
+++ spec/support/features/doc_capture_helper.rb
@@ -1,10 +1,8 @@
 module DocCaptureHelper
   def doc_capture_request_uri(user = user_with_2fa)
-    allow_any_instance_of(Browser).to receive(:mobile?).and_return(false)
     sign_in_and_2fa_user(user)
     complete_doc_auth_steps_before_link_sent_step
     url = Telephony::Test::Message.messages.last.body.split(' ').first
-    allow_any_instance_of(Browser).to receive(:mobile?).and_call_original
     URI.parse(url).request_uri
   end
 
