ee987cd1bda9d5e84b11fe11710811d720e7b075
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2022-06-15 14:36:41 -0400

    Extend the existing address validator

--- app/validators/idv/form_in_person_proofing_address_validator.rb
+++ app/validators/idv/form_in_person_proofing_address_validator.rb
@@ -1,13 +1,10 @@
 module Idv
   module FormInPersonProofingAddressValidator
     extend ActiveSupport::Concern
+    include FormAddressValidator
 
     included do
-      validates :address1,
-                :city,
-                :state,
-                :zipcode,
-                :same_address_as_id,
+      validates :same_address_as_id,
                 presence: true
     end
   end
