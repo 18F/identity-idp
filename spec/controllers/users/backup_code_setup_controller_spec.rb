4f5fda8ebdc3b8544254f6875175963e48158a67
SammySteiner <sammysteiner@gmail.com>
2022-06-16 09:32:43 -0400

    add codeclimate comment string

--- spec/controllers/users/backup_code_setup_controller_spec.rb
+++ spec/controllers/users/backup_code_setup_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Users::BackupCodeSetupController do
-  # remove after next deployment
+  # TODO remove after next deployment
   it 'has backup codes available for download' do
     user = build(:user, :signed_up)
     stub_sign_in(user)
