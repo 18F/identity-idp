f3deeadc68728b00e698bca9a770a32db98fefd3
SammySteiner <sammysteiner@gmail.com>
2022-06-16 15:12:44 -0400

    changelog: improvement, remove server side controller for backup code download, LG-6173

--- spec/controllers/users/backup_code_setup_controller_spec.rb
+++ spec/controllers/users/backup_code_setup_controller_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 describe Users::BackupCodeSetupController do
-  # remove after next deployment
-  it 'has backup codes available for download' do
-    user = build(:user, :signed_up)
-    stub_sign_in(user)
-    codes = BackupCodeGenerator.new(user).create
-    controller.user_session[:backup_codes] = codes
-    get :download
-
-    expect(response.body).to eq(codes.join("\r\n") + "\r\n")
-    expect(response.header['Content-Type']).to eq('text/plain')
-  end
-
   it 'creates backup codes' do
     user = build(:user, :signed_up)
     stub_sign_in(user)
