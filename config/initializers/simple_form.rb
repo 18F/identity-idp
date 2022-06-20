9c9fd5bf2843b1af2518881b4755ca7d1680d6c8
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2022-06-20 14:38:17 -0400

    Remove unnecessary plurals

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -46,25 +46,25 @@ SimpleForm.setup do |config|
 
   # Helper proc to define different types of radio button wrappers
   radio_button_builder = proc do |name, bordered|
-    item_label_classes = 'usa-radio__label width-full text-no-wrap' +
-                         (bordered ? '' : ' margin-top-0')
-    legend_classes = 'usa-label' + (bordered ? '' : ' margin-bottom-2')
-    input_classes = 'usa-radio__input' + (bordered ? ' usa-radio__input--bordered' : '')
+    item_label_class = 'usa-radio__label width-full text-no-wrap' +
+                       (bordered ? '' : ' margin-top-0')
+    legend_class = 'usa-label' + (bordered ? '' : ' margin-bottom-2')
+    input_class = 'usa-radio__input' + (bordered ? ' usa-radio__input--bordered' : '')
 
     config.wrappers name,
                     tag: 'fieldset',
                     wrapper_class: 'usa-fieldset margin-bottom-4',
                     item_wrapper_tag: nil,
-                    item_label_class: item_label_classes do |b|
+                    item_label_class: item_label_class do |b|
       b.use :html5
-      b.wrapper :legend, tag: 'legend', class: legend_classes do |ba|
+      b.wrapper :legend, tag: 'legend', class: legend_class do |ba|
         ba.use :label_text
       end
       b.use :hint, wrap_with: { tag: 'div', class: 'usa-hint margin-bottom-05' }
       b.wrapper :grid_row, tag: :div, class: 'grid-row margin-bottom-neg-1' do |gr|
         gr.wrapper :grid_column_radios, tag: :div, class: 'grid-col-fill' do |gc|
           gc.wrapper :column_wrapper, tag: :div, class: 'display-inline-block minw-full' do |cr|
-            cr.use :input, class: input_classes
+            cr.use :input, class: input_class
           end
         end
         gr.wrapper(:grid_column_gap, tag: :div, class: 'grid-col-4 tablet:grid-col-6') {}
