57545ca83d737f446f3a2248a2910bff1e708cd5
Andrew Duthie <andrew.duthie@gsa.gov>
2022-06-22 11:26:03 -0400

    Add support for unstyled button

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,8 +1,9 @@
 class ButtonComponent < BaseComponent
-  attr_reader :action, :icon, :big, :wide, :outline, :tag_options
+  attr_reader :action, :unstyled, :icon, :big, :wide, :outline, :tag_options
 
   def initialize(
     action: ->(**tag_options, &block) { button_tag(**tag_options, &block) },
+    unstyled: false,
     icon: nil,
     big: false,
     wide: false,
@@ -10,6 +11,7 @@ class ButtonComponent < BaseComponent
     **tag_options
   )
     @action = action
+    @unstyled = unstyled
     @icon = icon
     @big = big
     @wide = wide
@@ -22,6 +24,7 @@ class ButtonComponent < BaseComponent
     classes << 'usa-button--big' if big
     classes << 'usa-button--wide' if wide
     classes << 'usa-button--outline' if outline
+    classes << 'usa-button--unstyled' if unstyled
     classes
   end
 
