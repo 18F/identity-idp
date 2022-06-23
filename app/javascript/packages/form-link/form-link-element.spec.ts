6b0cc8e6b2dcf7764aa08cbd7ba6f2d50b79dc07
Andrew Duthie <andrew.duthie@gsa.gov>
2022-06-23 09:44:29 -0400

    Add FormLinkComponent

---
+++ app/javascript/packages/form-link/form-link-element.spec.ts
@@ -0,0 +1,29 @@
+import sinon from 'sinon';
+import { getByRole, fireEvent } from '@testing-library/dom';
+import './form-link-element';
+
+describe('FormLinkElement', () => {
+  function createElement() {
+    document.body.innerHTML = `
+      <lg-form-link>
+        <a href="https://example.com">Submit</a>
+        <form method="post" action="https://example.com" class="display-none"></form>
+      </lg-form-link>
+    `;
+
+    return document.body.querySelector('lg-form-link')!;
+  }
+
+  it('submits form on link click', () => {
+    const element = createElement();
+    const link = getByRole(element, 'link');
+
+    const onSubmit = sinon.stub().callsFake((event) => event.preventDefault());
+    window.addEventListener('submit', onSubmit);
+
+    const didPreventDefault = !fireEvent.click(link);
+
+    expect(onSubmit).to.have.been.called();
+    expect(didPreventDefault).to.be.true();
+  });
+});
