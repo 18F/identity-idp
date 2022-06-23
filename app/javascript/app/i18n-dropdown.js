604e0a18e450bcffbdec804a879811f818626799
Nadya Primak <nadya.primak@gsa.gov>
2022-06-23 14:18:24 -0400

    first pass at tests

--- app/javascript/app/i18n-dropdown.js
+++ app/javascript/app/i18n-dropdown.js
@@ -36,27 +36,35 @@ export function setUp() {
     languagePicker(mobileLink, mobileDropdown);
   }
 
-  function syncLinkURLs() {
+  function syncLanguageLinkURLs() {
     const links = document.querySelectorAll('.i18n-dropdown a[lang]');
+    // const langArray = ['es', 'fr'];
     links.forEach((link) => {
+      //1. url is /foo , lang is es   //2 url is /es/foo, lang is fr
       const lang = link.getAttribute('lang');
       let prefix = '';
       if (lang !== 'en') {
-        prefix = `/${lang}`;
+        prefix = `/${lang}`; //1. es is not en so we make prefix /es/foo   //2 lang is not en so prefix is fr
       }
       const url = new URL(window.location.href);
-      url.pathname = prefix + url.pathname;
-      link.setAttribute('href', url.toString());
+      // langArray.forEach((oldLang) => {
+      //   url.pathname = prefix + url.pathname.replace(oldLang, '');
+      // })
+      link.setAttribute('href', url.toString()); //1. pathname becomes /es/foo    //2. pathname becomes /es/fr/foo because current url has /es
+      if (window.location.pathname.includes(`/${lang}/`)) {
+        //1. current url (/foo) does NOT include es so we skip  //2. current url does NOT have fr so we skip
+        link.setAttribute('href', window.location.href);
+      }
     });
   }
 
   const originalPushState = History.prototype.pushState;
   History.prototype.pushState = function (...args) {
     const result = originalPushState.apply(this, args);
-    syncLinkURLs();
+    syncLanguageLinkURLs();
     return result;
   };
-  window.addEventListener('popstate', syncLinkURLs);
+  window.addEventListener('popstate', syncLanguageLinkURLs);
 
   return () => {
     History.prototype.pushState = originalPushState;
--- spec/javascripts/app/i18n-dropdown-spec.js
+++ spec/javascripts/app/i18n-dropdown-spec.js
@@ -1,15 +1,28 @@
 import { setUp } from '../../../app/javascript/app/i18n-dropdown';
+import sinon from 'sinon';
 
 describe('i18n-dropdown', () => {
   let tearDown;
 
   before(() => {
-    document.body.innerHTML = `
-      <div class="i18n-dropdown">
-        <a href="/" href="en">English</a>
-        <a href="/" lang="fr">Français</a>
-      </div>
-    `; // TODO
+    const sandbox = sinon.createSandbox();
+
+    // const history = ['example.com'];
+
+    // sandbox.stub(window, 'history').value(
+    //   Object.assign(history, {
+    //     pushState(_data, _unused, url) {
+    //       history.push(url);
+    //     },
+    //     replaceState(_data, _unused, url) {
+    //       history[history.length - 1] = url;
+    //     },
+    //     back() {
+    //       history.pop();
+    //       window.dispatchEvent(new CustomEvent('popstate'));
+    //     },
+    //   }),
+    // );
 
     tearDown = setUp();
   });
@@ -18,11 +31,50 @@ describe('i18n-dropdown', () => {
     tearDown();
   });
 
-  it('updates links on History#pushState', () => {
-    // TODO
+  beforeEach(() => {
+    document.body.innerHTML = `
+    <div class="i18n-dropdown">
+      <a href="/" lang="en">English</a>
+      <a href="/" lang="fr">Français</a>
+      <a href="/" lang="es">Español</a>
+    </div>
+  `;
   });
 
-  it('updates links on popstate event', () => {
-    // TODO
+  context('using history.pushState ', () => {
+    it('updates links with default language', () => {
+      window.history.pushState(null, '', '/foo');
+      console.log('pathname', document.querySelectorAll('a[lang="en"]')[0].href);
+      expect(document.querySelectorAll('a[lang="en"]')[0].pathname).to.equal('/foo');
+      expect(document.querySelectorAll('a[lang="es"]')[0].pathname).to.equal('/es/foo');
+      expect(document.querySelectorAll('a[lang="fr"]')[0].pathname).to.equal('/fr/foo');
+    });
+    it('updates links with non default language', () => {
+      window.history.pushState(null, '', '/fr/bar');
+      expect(document.querySelectorAll('a[lang="en"]')[0].pathname).to.equal('/bar');
+      expect(document.querySelectorAll('a[lang="es"]')[0].pathname).to.equal('/es/bar');
+      expect(document.querySelectorAll('a[lang="fr"]')[0].pathname).to.equal('/fr/bar');
+    });
+  });
+
+  context('updates links on popstate event', () => {
+    it('updates links with default language', () => {
+      window.history.pushState(null, '', '/foo');
+      console.log('window url', window.location.href);
+      history.back();
+      console.log('window url', window.location.href);
+      expect(document.querySelectorAll('a[lang="es"]')[0].pathname).to.equal('/es');
+      expect(document.querySelectorAll('a[lang="en"]')[0].pathname).to.equal('/');
+      expect(document.querySelectorAll('a[lang="fr"]')[0].pathname).to.equal('/fr');
+    });
+    it('updates links with non default language', () => {
+      window.history.pushState(null, '', '/es/foo');
+      console.log('window url', window.location.href);
+      history.back();
+      console.log('window url', window.location.href);
+      expect(document.querySelectorAll('a[lang="es"]')[0].pathname).to.equal('/es');
+      expect(document.querySelectorAll('a[lang="en"]')[0].pathname).to.equal('/');
+      expect(document.querySelectorAll('a[lang="fr"]')[0].pathname).to.equal('/fr');
+    });
   });
 });
