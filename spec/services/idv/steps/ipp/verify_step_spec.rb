e8051ecbc03883d8a7f439f06e8219628843ff91
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2022-06-24 10:55:52 -0400

    Add spec for verify step call

--- spec/services/idv/steps/ipp/verify_step_spec.rb
+++ spec/services/idv/steps/ipp/verify_step_spec.rb
@@ -12,8 +12,14 @@ describe Idv::Steps::Ipp::VerifyStep do
     )
   end
 
+  let(:pii) do
+    Idp::Constants::MOCK_IDV_APPLICANT_WITH_SSN.dup
+  end
+
   let(:flow) do
-    Idv::Flows::InPersonFlow.new(controller, {}, 'idv/in_person')
+    Idv::Flows::InPersonFlow.new(controller, {}, 'idv/in_person').tap do |flow|
+      flow.flow_session = { pii_from_user: pii }
+    end
   end
 
   subject(:step) do
@@ -21,8 +27,14 @@ describe Idv::Steps::Ipp::VerifyStep do
   end
 
   describe '#call' do
-    it 'works' do
+    it 'moves PII into IDV session' do
+      expect(flow.idv_session).to_not have_key 'applicant'
+      expect(flow.flow_session).to have_key :pii_from_user
       step.call
+
+      expect(flow.flow_session).to_not have_key :pii_from_user
+      expect(flow.idv_session).to have_key 'applicant'
+      expect(flow.idv_session['applicant']).to include(pii)
     end
   end
 end
