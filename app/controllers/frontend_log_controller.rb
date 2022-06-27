f1166aff8a9b105ac785c4e12b89b523fa638bae
Andrew Duthie <andrew.duthie@gsa.gov>
2022-06-27 09:27:11 -0400

    Use existing IdV events for FSMv2 steps
    **Why**: To avoid conflicts with existing queries and dashboards.
    
    changelog: Upcoming Features, Identity Verification, Reconcile analytics events for new experience

--- app/controllers/frontend_log_controller.rb
+++ app/controllers/frontend_log_controller.rb
@@ -6,8 +6,8 @@ class FrontendLogController < ApplicationController
   before_action :validate_parameter_types
 
   EVENT_MAP = {
-    'IdV: password confirm visited' => :idv_password_confirm_visited,
-    'IdV: password confirm submitted' => :idv_password_confirm_submitted,
+    'IdV: password confirm visited' => :idv_review_info_visited,
+    'IdV: password confirm submitted' => :idv_review_complete,
     'IdV: personal key visited' => :idv_personal_key_visited,
     'IdV: personal key submitted' => :idv_personal_key_submitted,
     'IdV: personal key confirm visited' => :idv_personal_key_confirm_visited,
