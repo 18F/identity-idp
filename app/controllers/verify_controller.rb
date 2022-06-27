33edade729f6b6b66b0fa89d916efb1cd2d7c3fc
Andrew Duthie <andrew.duthie@gsa.gov>
2022-06-27 10:37:52 -0400

    Restore review controller before_action to verify controller

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -1,7 +1,10 @@
 class VerifyController < ApplicationController
+  include IdvStepConcern
   include IdvSession
 
   before_action :validate_step
+  before_action :confirm_idv_steps_complete
+  before_action :confirm_idv_phone_confirmed
   before_action :confirm_two_factor_authenticated
   before_action :confirm_idv_vendor_session_started
 
@@ -10,6 +13,17 @@ class VerifyController < ApplicationController
   end
 
   private
+
+  def confirm_idv_steps_complete
+    return redirect_to(idv_doc_auth_url) unless idv_profile_complete?
+    return redirect_to(idv_phone_url) unless idv_address_complete?
+  end
+
+  def confirm_idv_phone_confirmed
+    return unless idv_session.address_verification_mechanism == 'phone'
+    return if idv_session.phone_confirmed?
+    redirect_to idv_otp_verification_path
+  end
 
   def validate_step
     render_not_found if params[:step].present? && !enabled_steps.include?(params[:step])
@@ -58,5 +72,13 @@ class VerifyController < ApplicationController
       user: current_user,
       idv_session: idv_session,
     ).token
+  end
+
+  def idv_profile_complete?
+    idv_session.profile_confirmation == true
+  end
+
+  def idv_address_complete?
+    idv_session.address_mechanism_chosen?
   end
 end
