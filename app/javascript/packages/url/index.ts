c556ab8688b1c2a31cb70c0997690fe832c651e2
Andrew Duthie <andrew.duthie@gsa.gov>
2022-07-07 14:32:27 -0400

    Add utilities for forced redirection

--- app/javascript/packages/url/index.ts
+++ app/javascript/packages/url/index.ts
@@ -1,1 +1,3 @@
 export { default as addSearchParams } from './add-search-params';
+export { default as forceRedirect } from './force-redirect';
+export { default as removeUnloadProtection } from './remove-unload-protection';
