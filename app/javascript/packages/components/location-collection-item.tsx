bfe3264f7063abe7e343cd3fa9caaf513808a730
Shannon Alexander <shannonalexander@Shannons-MBP.fios-router.home>
2022-07-08 11:44:21 -0400

    align button on right side of container

--- app/javascript/packages/components/location-collection-item.tsx
+++ app/javascript/packages/components/location-collection-item.tsx
@@ -1,8 +1,6 @@
-import type { ReactNode } from 'react';
 import { Button } from '@18f/identity-components';
 import { useI18n } from '@18f/identity-react-i18n';
 
-// TODO: update props to be a locationItemObject
 // TODO: create locationItemObject based on data returned by API, should be separate file, need to look at api to determine fields
 interface LocationCollectionItemProps {
   header: string;
@@ -13,9 +11,6 @@ interface LocationCollectionItemProps {
   hoursSun: string;
 }
 
-// TODO: create strings in i18n for days and import here
-// flex-row doesn't work; flex-column does but the 2 items are now in a col together and stacked on top of each other
-// this means the ui as is does not match the figma file
 function LocationCollectionItem({
   header,
   addressLine1,
@@ -28,13 +23,9 @@ function LocationCollectionItem({
   return (
     <li className="location-collection-item">
       <div className="usa-collection__body">
-        <div className="display-flex flex-column">
-          <div className="flex-align-self-end">
-            <Button>{t('in_person_proofing.body.location.location_button')}</Button>
-          </div>
-          <div className="flex-align-self-start">
-            <h3 className="usa-collection__heading">{header}</h3>
-          </div>
+        <div className="display-flex flex-justify">
+          <h3 className="usa-collection__heading">{header}</h3>
+          <Button>{t('in_person_proofing.body.location.location_button')}</Button>
         </div>
         <div>{addressLine1}</div>
         <div>{addressLine2}</div>
