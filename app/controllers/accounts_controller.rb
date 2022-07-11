24a2dadde8eebc37cd5ce9c5d5fbd06e7c8be5a5
Jessica Dembe <jessica.dembe@gsa.gov>
2022-07-11 08:32:27 -0400

    Address PR comment where AccountResetRequst is filtered

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -15,7 +15,14 @@ class AccountsController < ApplicationController
       sp_name: decorated_session.sp_name,
       decorated_user: current_user.decorate,
       locked_for_session: pii_locked_for_session?(current_user),
-      account_reset_requested: current_user&.account_reset_request.present?,
+      account_reset_requested: account_reset_requested?,
     )
+  end
+
+  def account_reset_requested?
+    AccountResetRequest.where(
+      user: current_user,
+    ).where('cancelled_at > ?',
+            IdentityConfig.store.account_reset_wait_period_days.days.ago).present?
   end
 end
