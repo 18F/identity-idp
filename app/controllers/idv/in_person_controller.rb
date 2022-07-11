d636933298c63bb86a806ee19df9adf07f13a792
Andrew Duthie <andrew.duthie@gsa.gov>
2022-07-11 16:50:07 -0400

    Redirect to phone URL using FSM final_url

--- app/controllers/idv/in_person_controller.rb
+++ app/controllers/idv/in_person_controller.rb
@@ -7,7 +7,7 @@ module Idv
 
     FSM_SETTINGS = {
       step_url: :idv_in_person_step_url,
-      final_url: :account_url,
+      final_url: :idv_phone_url,
       flow: Idv::Flows::InPersonFlow,
       analytics_id: 'In Person Proofing',
     }.freeze
--- app/services/idv/steps/ipp/verify_step.rb
+++ app/services/idv/steps/ipp/verify_step.rb
@@ -7,10 +7,6 @@ module Idv
           # WILLFIX: (LG-6498) make a call to Instant Verify before allowing the user to continue
           save_legacy_state
           delete_pii
-
-          # send the user to the phone page where they'll continue the remainder of
-          # the idv flow
-          redirect_to idv_phone_url
         end
 
         def extra_view_variables
