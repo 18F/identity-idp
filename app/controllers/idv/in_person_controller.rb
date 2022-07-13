31f7d9e22adc91baa7b88921ad65afccd6e1eb3f
Andrew Duthie <andrew.duthie@gsa.gov>
2022-07-13 08:43:51 -0400

    LG-6439: Allow per-SP access to IPP option (#6568)
    * LG-6439: Allow per-SP access to IPP option
    
    **Why**: So that we can have more discrete control over which users will be presented with and allowed to leverage the in-person proofing flow.
    
    changelog: Upcoming Features, In-Person Proofing, Allow per-partner configuration for in-person proofing option
    
    * Remove unnecessary symbolize_names
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update document capture partial spec
    
    * Split in-person proofing enabled to separate configurations
    
    Easier to follow
    
    See: https://github.com/18F/identity-idp/pull/6568#discussion_r917103269
    
    * Enable IPP for IPP feature spec
    
    * Revert method name change render_404_if_disabled
    
    Original name fits just fine, simplify diff
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/controllers/idv/in_person_controller.rb
+++ app/controllers/idv/in_person_controller.rb
@@ -12,8 +12,10 @@ module Idv
       analytics_id: 'In Person Proofing',
     }.freeze
 
+    private
+
     def render_404_if_disabled
-      render_not_found unless IdentityConfig.store.in_person_proofing_enabled
+      render_not_found unless InPersonConfig.enabled_for_issuer?(current_sp&.issuer)
     end
   end
 end
