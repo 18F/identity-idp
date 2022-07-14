801c261ef9033ef61493e193dcc1775a724e28c5
Tomas Apodaca <thomas.apodaca@gsa.gov>
2022-07-13 22:12:32 -0700

    cleanup

--- db/primary_migrate/20220705164321_change_unique_status_constraint_on_in_person_enrollments.rb
+++ db/primary_migrate/20220705164321_change_unique_status_constraint_on_in_person_enrollments.rb
@@ -9,5 +9,5 @@ class ChangeUniqueStatusConstraintOnInPersonEnrollments < ActiveRecord::Migratio
   def down
     remove_index :in_person_enrollments, name: 'index_in_person_enrollments_on_user_id_and_status', algorithm: :concurrently if index_exists?(:in_person_enrollments, [:user_id, :status], name: "index_in_person_enrollments_on_user_id_and_status")
     add_index :in_person_enrollments, [:user_id, :status], unique: true, where: '(status = 0)', algorithm: :concurrently
-  end  
+  end
 end
