44a4518313eb90433c86ac018e9f6ffdce47b3a8
Andrew Duthie <andrew.duthie@gsa.gov>
2022-07-15 15:38:09 -0400

    Sync markup to ready_to_verify controller

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -163,6 +163,27 @@ class UserMailerPreview < ActionMailer::Preview
         profile: unsaveable(Profile.new(user: user)),
         enrollment_code: '2048702198804358',
         created_at: Time.zone.now,
+        current_address_matches_id: true,
+        selected_location_details: {
+          'name' => 'BALTIMORE — Post Office™',
+          'streetAddress' => '900 E FAYETTE ST RM 118',
+          'city' => 'BALTIMORE',
+          'state' => 'MD',
+          'zip5' => '21233',
+          'zip4' => '9715',
+          'phone' => '555-123-6409',
+          'hours' => [
+            {
+              'weekdayHours' => '8:30 AM - 4:30 PM',
+            },
+            {
+              'saturdayHours' => '9:00 AM - 12:00 PM',
+            },
+            {
+              'sundayHours' => 'Closed',
+            },
+          ],
+        },
       ),
     )
   end
