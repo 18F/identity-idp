d50f7ac4821bc399c916fa7e5cc1a2e831b49680
Tomas Apodaca <thomas.apodaca@gsa.gov>
2022-07-25 20:22:21 -0700

    add controller tests

--- spec/controllers/idv/in_person/usps_locations_controller_spec.rb
+++ spec/controllers/idv/in_person/usps_locations_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Idv::InPerson::UspsLocationsController do
+  include IdvHelper
+
   let(:user) { create(:user) }
   let(:in_person_proofing_enabled) { false }
 
@@ -12,19 +14,97 @@ describe Idv::InPerson::UspsLocationsController do
   end
 
   describe '#index' do
+    let(:proofer) { double('Proofer') }
+    let(:locations) {
+      [
+        { name: 'Location 1' },
+        { name: 'Location 2' },
+        { name: 'Location 3' },
+        { name: 'Location 4' },
+      ]
+    }
     subject(:response) { get :index }
 
-    it 'gets successful pilot response' do
-      response = get :index
-      json = response.body
-      facilities = JSON.parse(json)
-      expect(facilities.length).to eq 7
+    before do
+      allow(UspsInPersonProofing::Proofer).to receive(:new).and_return(proofer)
+    end
+
+    context 'with successful fetch' do
+      before do
+        allow(proofer).to receive(:request_pilot_facilities).and_return(locations)
+      end
+
+      it 'gets successful pilot response' do
+        response = get :index
+        json = response.body
+        facilities = JSON.parse(json)
+        expect(facilities.length).to eq 4
+      end
+    end
+
+    context 'with unsuccessful fetch' do
+      before do
+        exception = Faraday::ConnectionFailed.new('error')
+        allow(proofer).to receive(:request_pilot_facilities).and_raise(exception)
+      end
+
+      it 'gets an empty pilot response' do
+        response = get :index
+        json = response.body
+        facilities = JSON.parse(json)
+        expect(facilities.length).to eq 0
+      end
     end
   end
 
-  describe '#update' do
-  end
+  context 'with a session' do
+    let(:idv_session) do
+      Idv::Session.new(
+        user_session: {},
+        current_user: user,
+        service_provider: nil,
+      )
+    end
+    let(:selected_location) do
+      {
+        usps_location: {
+          addressLine2: 'BALTIMORE, MD, 21233-9715',
+          name: 'BALTIMORE',
+          phone: '410-555-1212',
+          saturdayHours: '8:30 AM - 5:00 PM',
+          streetAddress: '123 Fake St.',
+          sundayHours: 'Closed',
+          weekdayHours: '8:30 AM - 7:00 PM',
+        },
+      }
+    end
 
-  describe '#show' do
+    before do
+      allow(subject).to receive(:idv_session).and_return(idv_session)
+    end
+
+    describe '#update' do
+      it 'writes the passed location to session' do
+        put :update, params: selected_location
+
+        selected_location[:usps_location].keys.each do |key|
+          expect(idv_session.applicant[:selected_location_details][key.to_s]).
+            to eq(selected_location[:usps_location][key])
+        end
+      end
+    end
+
+    describe '#show' do
+      it 'loads the saved location from session' do
+        put :update, params: selected_location
+
+        response = get :show
+        body = JSON.parse(response.body)
+        selected_location[:usps_location].keys.each do |key|
+          expect(body[key.to_s]).
+            to eq(selected_location[:usps_location][key])
+        end
+      end
+    end
   end
 end
