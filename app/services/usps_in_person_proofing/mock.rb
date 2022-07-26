00e86988da0adecef2ea59a3a60d78e279f1e91d
Shannon Alexander <shannonalexander@Shannons-MBP.fios-router.home>
2022-07-26 11:17:48 -0400

    update proofer mock file

--- app/services/usps_in_person_proofing/mock.rb
+++ app/services/usps_in_person_proofing/mock.rb
@@ -15,7 +15,7 @@ module UspsInPersonProofing
 
       def request_pilot_facilities
         JSON.load_file(
-          Rails.root.join('spec/fixtures/usps_ipp_responses/request_facilities_response.json'),
+          Rails.root.join('config/ipp_pilot_usps_facilities.json'),
         )
       end
     end
