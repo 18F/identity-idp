c5ce6291390ce378edeaefe9e870a12c5aed7d49
Shanice Cumbee <shanicecumbee@navapbc.com>
2022-07-27 16:12:59 -0400

    added analytics events

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -2312,6 +2312,36 @@ module AnalyticsEvents
     )
   end
 
+  # Tracks exceptions that are raised when running GetUspsProofingResultsJob
+  # @param [String] reason why was the exception raised?
+  # @param [String] enrollment_id
+  # @param [String] exception_class
+  # @param [String] exception_message
+  def idv_in_person_usps_proofing_results_job_exception(
+    reason:, enrollment_id:, exception_class: nil, exception_message: nil, **extra
+  )
+    track_event(
+      'GetUspsProofingResultsJob: Exception raised',
+      reason: reason,
+      enrollment_id: enrollment_id,
+      exception_class: exception_class,
+      exception_message: exception_message,
+      **extra,
+    )
+  end
+
+  # Tracks individual enrollments that fail during GetUspsProofingResultsJob
+  # @param [String] reason why did this enrollment fail?
+  # @param [String] enrollment_id
+  def idv_in_person_usps_proofing_results_job_enrollment_failure(reason:, enrollment_id:, **extra)
+    track_event(
+      'GetUspsProofingResultsJob: Enrollment failed proofing',
+      reason: reason,
+      enrollment_id: enrollment_id,
+      **extra,
+    )
+  end
+
   # Tracks if when a user is enrolled in in-person proofing
   # @param [String] context
   def idv_in_person_usps_request_enroll(context:, **extra)
