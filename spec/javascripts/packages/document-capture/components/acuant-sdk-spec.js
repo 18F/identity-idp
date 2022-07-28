e559ebdebccc9d93f4fb797962be1be171afdc6b
Eric Gade <ecgade@macbook-m1.lan>
2022-07-28 16:42:29 -0400

    Removing tests for 11.5.0 Acuant SDK loading

--- spec/javascripts/packages/document-capture/components/acuant-sdk-spec.js
+++ spec/javascripts/packages/document-capture/components/acuant-sdk-spec.js
@@ -9,7 +9,6 @@ import path from 'path';
 
 const sdkPaths = {
   '11.7.0': '../../../../../public/acuant/11.7.0/AcuantJavascriptWebSdk.min.js',
-  '11.5.0': '../../../../../public/acuant/11.5.0/AcuantJavascriptWebSdk.min.js',
 };
 
 const scriptContent = fs
@@ -35,7 +34,6 @@ describe('Acuant SDK Loading Tests', () => {
     before((done) => {
       let scriptEl = document.createElement('script');
       scriptEl.id = 'test-acuant-sdk-script';
-      //scriptEl.textContent = scriptContent;
       scriptEl.onload = () => {
         done();
       };
@@ -53,41 +51,6 @@ describe('Acuant SDK Loading Tests', () => {
       window.loadAcuantSdk();
       expect(AcuantJavascriptWebSdk).to.exist();
       expect(window.AcuantJavascriptWebSdk).to.not.exist();
-    });
-    after(() => {
-      window = new JSDOM(
-        '<!doctype html><html lang="en"><head><title>JSDOM</title></head></html>',
-        {
-          url: TEST_URL,
-          runScripts: 'dangerously',
-          resources: 'usable',
-        },
-      ).window;
-      document = window.document;
-    });
-  });
-
-  describe('DOM Loading 11.5.0', () => {
-    before((done) => {
-      let scriptEl = document.createElement('script');
-      scriptEl.id = 'test-acuant-sdk-script';
-      //scriptEl.textContent = scriptContent;
-      scriptEl.onload = () => {
-        done();
-      };
-      scriptEl.src = sdkPaths['11.5.0'];
-      document.body.append(scriptEl);
-    });
-    it('There is a script element in the DOM', () => {
-      let found = document.getElementById('test-acuant-sdk-script');
-      expect(found).to.exist();
-    });
-    it('Has a global loadAcuantSdk object on the window', () => {
-      expect(window.loadAcuantSdk).to.exist();
-    });
-    it('Calling loadAcuantSdk gives us AcuantJavascriptWebSdk in the window object', () => {
-      window.loadAcuantSdk();
-      expect(window.AcuantJavascriptWebSdk).to.exist();
     });
   });
 });
