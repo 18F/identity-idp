26bdce350afac5f51a2c32c8fcddffb4b115adfe
Melissa Miller <melissa.miller@gsa.gov>
2022-07-28 11:32:58 -0400

    Spec clean up

--- spec/services/inherited_proofing/va/service_spec.rb
+++ spec/services/inherited_proofing/va/service_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe InheritedProofing::Va::Service do
   let(:payload) { { inherited_proofing_auth: auth_code, exp: 1.day.from_now.to_i } }
   let(:jwt_token) { JWT.encode(payload, private_key, 'RS256') }
   let(:request_uri) {
-    "#{ URI(InheritedProofing::Va::Service::BASE_URI) }/inherited_proofing/user_attributes"
+    "#{InheritedProofing::Va::Service::BASE_URI}/inherited_proofing/user_attributes"
   }
   let(:request_headers) { { Authorization: "Bearer #{jwt_token}" } }
 
