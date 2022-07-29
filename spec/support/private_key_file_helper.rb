9271259cbaa17294c0d61ce12da86a976fb5e989
gangelo <web.gma@gmail.com>
2022-07-29 04:55:00 -0400

    Remove Identity::Hostdata.in_datacenter? guard

--- spec/support/private_key_file_helper.rb
+++ spec/support/private_key_file_helper.rb
@@ -7,11 +7,11 @@ module PrivateKeyFileHelper
 
     puts "WARNING: Private key file '#{file_name}' not found!" if Rails.env.test? && !File.exist?(file_name)
 
-    if Identity::Hostdata.in_datacenter? || !File.exist?(file_name)
+    if File.exist?(file_name)
+      OpenSSL::PKey::RSA.new(File.read(file_name))
+    else
       return AppArtifacts.store.oidc_private_key
     end
-
-    OpenSSL::PKey::RSA.new(File.read(file_name))
   end
 
   # Always ensure we're referencing files in the /tmp/ folder!
