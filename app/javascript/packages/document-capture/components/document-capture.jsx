d46570c25634b977d0587f8cc30e30fd9b1e820a
Andrew Duthie <andrew.duthie@gsa.gov>
2022-07-29 12:26:22 -0400

    Add switch back UI for hybrid IPP opt-in

--- app/javascript/packages/document-capture/components/document-capture.jsx
+++ app/javascript/packages/document-capture/components/document-capture.jsx
@@ -8,6 +8,7 @@ import DocumentsStep from './documents-step';
 import SelfieStep from './selfie-step';
 import InPersonPrepareStep from './in-person-prepare-step';
 import InPersonLocationStep from './in-person-location-step';
+import InPersonSwitchBackStep from './in-person-switch-back-step';
 import ReviewIssuesStep from './review-issues-step';
 import ServiceProviderContext from '../context/service-provider';
 import UploadContext from '../context/upload';
@@ -109,7 +110,11 @@ function DocumentCapture({ isAsyncForm = false, onStepChange }) {
             name: 'prepare',
             form: InPersonPrepareStep,
           },
-        ];
+          flowPath === 'hybrid' && {
+            name: 'switch_back',
+            form: InPersonSwitchBackStep,
+          },
+        ].filter(Boolean);
 
   /** @type {FormStep[]} */
   const steps = submissionError
---
+++ app/javascript/packages/document-capture/components/in-person-switch-back-step.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { PageHeading } from '@18f/identity-components';
+import { getAssetPath } from '@18f/identity-assets';
+import { t } from '@18f/identity-i18n';
+import type { FormStepComponentProps } from '@18f/identity-form-steps';
+
+function InPersonSwitchBackStep({ onChange }: FormStepComponentProps<any>) {
+  useEffect(() => onChange({}, { patch: false }), []);
+
+  return (
+    <>
+      <PageHeading>{t('doc_auth.instructions.switch_back')}</PageHeading>
+      <img src={getAssetPath('idv/switch.png')} width={193} />
+    </>
+  );
+}
+
+export default InPersonSwitchBackStep;
