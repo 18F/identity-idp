3358295cb12eb148e55e9f7c1d457ffba6c4789f
Andrew Duthie <andrew.duthie@gsa.gov>
2022-07-29 12:20:09 -0400

    Use effective user for USPS location
    Because it may be called in the hybrid session

--- app/controllers/idv/in_person/usps_locations_controller.rb
+++ app/controllers/idv/in_person/usps_locations_controller.rb
@@ -4,6 +4,7 @@ module Idv
   module InPerson
     class UspsLocationsController < ApplicationController
       include UspsInPersonProofing
+      include EffectiveUser
 
       # get the list of all pilot Post Office locations
       def index
@@ -28,7 +29,7 @@ module Idv
 
       def enrollment
         UspsInPersonProofing::EnrollmentHelper.
-          establishing_in_person_enrollment_for_user(current_user)
+          establishing_in_person_enrollment_for_user(effective_user)
       end
 
       def permitted_params
