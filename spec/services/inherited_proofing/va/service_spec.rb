c654636117faea5a2b7e537758016d60b8def8c4
Melissa Miller <melissa.miller@gsa.gov>
2022-07-29 08:24:29 -0400

    saving work things are working

--- spec/services/inherited_proofing/va/service_spec.rb
+++ spec/services/inherited_proofing/va/service_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe InheritedProofing::Va::Service do
   end
 
   let(:auth_code) {}
-  let(:key) { OpenSSL::PKey::RSA.new(2048) }
   let(:private_key) { private_key_from_store_or(file_name: 'va_ip.key') }
   let(:payload) { { inherited_proofing_auth: auth_code, exp: 1.day.from_now.to_i } }
   let(:jwt_token) { JWT.encode(payload, private_key, 'RS256') }
@@ -22,8 +21,7 @@ RSpec.describe InheritedProofing::Va::Service do
     "#{InheritedProofing::Va::Service::BASE_URI}/inherited_proofing/user_attributes"
   }
   let(:request_headers) { { Authorization: "Bearer #{jwt_token}" } }
-  let(:user_attributes) { "{ first_name: 'Steven', last_name: 'Universe' }" }
-  let(:encrypted_user_attributes) { JWE.encrypt(user_attributes, private_key, alg: 'RSA-OAEP', enc: 'A128CBC-HS256') }
+  let(:encrypted_user_attributes) { "{\"data\":\"eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkExMjhHQ00ifQ.aE7sR9_LEg3fJsM6OGZovCn1HgNnmOb5j5oY_KCW5Ps1nBtUKIstX5R6jUzoDz9S33SFJOz0co4Ni5P-n5Nz3poy4euG1VkfvBU5tgeOESQlRZAa1MaHltQ5cvCwzhgzxVWF3UhasPwNXTSzDKRyBsFjlpGz2cFtjAQmn_5KN55KveQRHrd8CLWwnyLuUr-OIRV33xsmBFFmKwLz2_pJ0I2qVRiBBfYBJAj8POMvHcb8jcTj3hVPmuSykY1MxficNldhpTycmrbAKS8bmH0GQPW9keeya7_Bbd68W7CaKTdxqLEYq6bnq2ZEr2m2nnNX4sBY_RH01P-pRceIYlP0Ow.ROTfFXEdsyEZ6fkN.ECi341meAV5kjhmp_OZqbskPM140QUAaqKaOUZS7ClV6NZ9BQ9ekukJOKtmyGICudZ6VJrv1WMy2IftSOjwTkjWXxxtBPxvoGqqeDuToennRmVC130ivPH-voAT3NwlwMRqyGuZZmegNo9e4bIjUTnAoz3sglAxtB1YMLSaxD70hzPA9vNMVDu09K_zha9AwMm5VFf4tg-R7SJ_2wYkiMIe38dofCwtZ89WFkdJz2SLp8FSUWRjOwtonxIrp2xHPqFB0yBzAN6ZjxbzwR0ax9W666hb6SiG1ByAh14jCmOM.CI4-YIi-7PDbvXGfsUF4kQ\"}" }
 
   it { respond_to :execute }
 
@@ -38,7 +36,7 @@ RSpec.describe InheritedProofing::Va::Service do
       it 'makes an authenticated request' do
         stub = stub_request(:get, request_uri).
           with(headers: request_headers).
-          to_return(status: 200, body: "", headers: {})
+          to_return(status: 200, body: encrypted_user_attributes, headers: {})
 
         pp service.execute
 
