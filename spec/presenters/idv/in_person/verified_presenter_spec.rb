ed692724616a0290338dc5fa20728b4c8c8f17b2
Tomas Apodaca <thomas.apodaca@gsa.gov>
2022-07-31 22:48:17 -0700

    tests

---
+++ spec/presenters/idv/in_person/verified_presenter_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Idv::InPerson::VerifiedPresenter do
+  let(:location_name) { 'FRIENDSHIP' }
+  let(:status_updated_at) { described_class::USPS_SERVER_TIMEZONE.parse('2022-07-14T00:00:00Z') }
+  let!(:enrollment) do
+    create(
+      :in_person_enrollment,
+      :pending,
+      selected_location_details: { name: location_name },
+    )
+  end
+
+  subject(:presenter) { described_class.new(enrollment: enrollment) }
+
+  describe '#location_name' do
+    it 'returns the enrollment location name' do
+      expect(presenter.location_name).to eq(location_name)
+    end
+  end
+
+  describe '#formatted_verified_date' do
+    around do |example|
+      Time.use_zone('UTC') { example.run }
+    end
+
+    it 'returns a formatted verified date' do
+      enrollment.update(status_updated_at: status_updated_at)
+      expect(presenter.formatted_verified_date).to eq 'July 13, 2022'
+    end
+  end
+end
