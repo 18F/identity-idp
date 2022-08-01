b7966540be69bfb6d3b233b3bd22f099f354e5d1
Andrew Duthie <andrew.duthie@gsa.gov>
2022-08-01 08:47:56 -0400

    Fix hybrid switch back prompt on navigate

--- app/javascript/packages/document-capture/components/in-person-switch-back-step.tsx
+++ app/javascript/packages/document-capture/components/in-person-switch-back-step.tsx
@@ -1,11 +1,15 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { PageHeading } from '@18f/identity-components';
 import { getAssetPath } from '@18f/identity-assets';
 import { t } from '@18f/identity-i18n';
 import type { FormStepComponentProps } from '@18f/identity-form-steps';
 
 function InPersonSwitchBackStep({ onChange }: FormStepComponentProps<any>) {
-  useEffect(() => onChange({}, { patch: false }), []);
+  // Resetting the value prevents the user from being prompted about unsaved changes when closing
+  // the tab. `useLayoutEffect` is used to avoid race conditions where the callback could occur at
+  // the same time as the change handler's `ifStillMounted` wrapping `useEffect`, which would treat
+  // it as unmounted and not update the value.
+  useLayoutEffect(() => onChange({}, { patch: false }), []);
 
   return (
     <>
