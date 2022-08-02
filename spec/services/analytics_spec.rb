dc2432793abec7291802d52e77fd07e433705c92
Kimball Bighorse <kbighorse@yahoo.com>
2022-08-01 17:39:13 -0700

    Rename analytics object
    Changelog: Improvements (LG-6793)
    
    categories:
    - Improvements

--- spec/services/analytics_spec.rb
+++ spec/services/analytics_spec.rb
@@ -229,13 +229,13 @@ describe Analytics do
   end
 
   describe '#idv_gpo_address_letter_requested' do
-    let(:fake_analytics) { FakeAnalytics.new }
+    let(:analytics) { FakeAnalytics.new }
 
     it 'logs letter requested with enqueued at' do
       enqueued_at = Time.zone.now
-      fake_analytics.idv_gpo_address_letter_requested(enqueued_at: enqueued_at)
+      analytics.idv_gpo_address_letter_requested(enqueued_at: enqueued_at)
 
-      expect(fake_analytics).to have_logged_event(
+      expect(analytics).to have_logged_event(
         'IdV: USPS address letter requested',
         enqueued_at: enqueued_at,
       )
