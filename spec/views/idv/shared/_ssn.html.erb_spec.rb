227a018664432680bab286acaf37beea0a9f5ae9
Alex Bradley <alexander.bradley@gsa.gov>
2022-08-04 13:51:03 -0400

    Fix linting errors. Make tests pass

--- spec/views/idv/shared/_ssn.html.erb_spec.rb
+++ spec/views/idv/shared/_ssn.html.erb_spec.rb
@@ -2,51 +2,58 @@ require 'rails_helper'
 
 describe 'idv/shared/_ssn.html.erb' do
   include Devise::Test::ControllerHelpers
-  
-  let(:proofing_device_profiling_collecting_enabled) { nil }
-  let(:lexisnexis_threatmetrix_account_id) { 'test_org_id'}
-  let(:session_id) { "ABCD-1234" }
-  let(:updating_ssn) { false }
-  
-  let (:js_domain) { "h.online-metrix.net"}
 
-  let (:tags_js_url) {
+  let(:proofing_device_profiling_collecting_enabled) { nil }
+  let(:lexisnexis_threatmetrix_account_id) { 'test_org_id' }
+  let(:session_id) { 'ABCD-1234' }
+  let(:updating_ssn) { false }
+  let(:js_domain) { 'h.online-metrix.net' }
+
+  let(:tags_js_url) {
     "https://#{js_domain}/fp/tags.js?org_id=#{lexisnexis_threatmetrix_account_id}&session_id=#{session_id}"
   }
 
-  let (:tags_iframe_url) {
+  let(:tags_iframe_url) {
     "https://#{js_domain}/fp/tags?org_id=#{lexisnexis_threatmetrix_account_id}&session_id=#{session_id}"
   }
 
-  let (:script_tag_rendered) {
-    expect(rendered).to have_css("script[src='#{tags_js_url}']", :visible => false)    
+  let(:script_tag_rendered) {
+    expect(rendered).to have_css("script[src='#{tags_js_url}']", visible: false)
   }
 
-  let (:noscript_tag_rendered) {
-    expect(rendered).to have_css("noscript iframe[src='#{tags_iframe_url}']", :visible => false)
+  let(:noscript_tag_rendered) {
+    expect(rendered).to have_css("noscript iframe[src='#{tags_iframe_url}']", visible: false)
   }
 
-  let (:session_id_input_rendered) {
-    expect(rendered).to have_css("input[type=hidden][name='doc_auth[threatmetrix_session_id]'][value='#{session_id}']", :visible => false)
+  let(:session_id_input_rendered) {
+    expect(rendered).
+      to have_css(
+        "input[type=hidden][name='doc_auth[threatmetrix_session_id]'][value='#{session_id}']",
+        visible: false,
+      )
   }
 
-  let (:script_tag_not_rendered) {
-    expect(rendered).not_to have_css("script[src*='#{js_domain}']", :visible => false)    
+  let(:script_tag_not_rendered) {
+    expect(rendered).not_to have_css("script[src*='#{js_domain}']", visible: false)
   }
 
-  let (:noscript_tag_not_rendered) {
-    expect(rendered).not_to have_css("noscript iframe[src*='#{js_domain}']", :visible => false)
+  let(:noscript_tag_not_rendered) {
+    expect(rendered).not_to have_css("noscript iframe[src*='#{js_domain}']", visible: false)
   }
 
-  let (:session_id_input_not_rendered) {
-    expect(rendered).not_to have_css('input[name="doc_auth[threatmetrix_session_id]"]', :visible => false)
+  let(:session_id_input_not_rendered) {
+    expect(rendered).
+      not_to have_css('input[name="doc_auth[threatmetrix_session_id]"]', visible: false)
   }
 
   before :each do
     allow(view).to receive(:url_for).and_return('https://example.com/')
-    
-    allow(IdentityConfig.store).to receive(:proofing_device_profiling_collecting_enabled).and_return(proofing_device_profiling_collecting_enabled)
-    allow(IdentityConfig.store).to receive(:lexisnexis_threatmetrix_account_id).and_return(lexisnexis_threatmetrix_account_id)
+
+    allow(IdentityConfig.store).
+      to receive(:proofing_device_profiling_collecting_enabled).
+      and_return(proofing_device_profiling_collecting_enabled)
+    allow(IdentityConfig.store).
+      to receive(:lexisnexis_threatmetrix_account_id).and_return(lexisnexis_threatmetrix_account_id)
 
     render partial: 'idv/shared/ssn', locals: {
       flow_session: {},
@@ -107,7 +114,7 @@ describe 'idv/shared/_ssn.html.erb' do
       it 'does not render session_id input' do
         session_id_input_not_rendered
       end
-  end
+    end
   end
 
   context 'threatmetrix collection disabled' do
