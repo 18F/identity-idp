01abbbc54372b7b3d3f084dcf8ec7a491ffcf211
Shannon Alexander <shannonalexander@Shannons-MBP.fios-router.home>
2022-08-04 18:21:59 -0400

    wip on wrapper

--- app/components/memorable_date_component.rb
+++ app/components/memorable_date_component.rb
@@ -1,11 +1,43 @@
 class MemorableDateComponent < BaseComponent
-    attr_reader :month, :day, :year, :hint, :label
+    # include ActiveModel::Validations
+    attr_reader :month, :day, :year, :hint, :label, :form
+    #, :input_type, :tag_options
 
-    def initialize(month:, day:, year:, hint:, label:)
+    alias_method :f, :form
+    # validates :month, numericality: true .... validates checks value b4 you write to the db 
+
+    def initialize(month:, day:, year:, hint:, label:, form:, error_messages: {}, **tag_options)
         @month = month
         @day = day
         @year = year
         @hint = hint
         @label = label
+        @form = form
+         @error_messages = error_messages
+        # @input_type = inferred_input_type
+         @tag_options = []
     end
+
+    # if error then we need to add this css class: usa-input--error
+    def error_messages
+        # { valueMissing: value_missing_error_message }.compact
+    end
+
+#     private
+
+#     # pattern is what is showing the current error now 
+#     # when input type is false, ie missing, then add the error text "required"
+#     def value_missing_error_message
+#         case input_type
+#         when :boolean
+#         t('forms.validation.required_checkbox')
+#         else
+#         t('simple_form.required.text')
+#         end
+#   end
+
+#   def inferred_input_type
+#     # change this so instead it validates field is number
+#     form.send(:default_input_type, month, form.send(:find_attribute_column, month ), tag_options)
+#   end
 end
