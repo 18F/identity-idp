1d51da5f9e8d18a4f3aaa7d2001e096bf71cf7e1
Parissa <parissa.eggleston@gmail.com>
2022-08-05 14:21:53 -0400

    add test for analytics

--- spec/javascripts/packages/document-capture/context/failed-capture-attempts-spec.jsx
+++ spec/javascripts/packages/document-capture/context/failed-capture-attempts-spec.jsx
@@ -1,8 +1,17 @@
 import { useContext } from 'react';
 import { renderHook } from '@testing-library/react-hooks';
+import { DeviceContext, AnalyticsContext } from '@18f/identity-document-capture';
+import AcuantContext, {
+  dirname,
+  Provider as AcuantContextProvider,
+  DEFAULT_ACCEPTABLE_GLARE_SCORE,
+  DEFAULT_ACCEPTABLE_SHARPNESS_SCORE,
+} from '@18f/identity-document-capture/context/acuant';
+
 import FailedCaptureAttemptsContext, {
   Provider,
 } from '@18f/identity-document-capture/context/failed-capture-attempts';
+import sinon from "sinon";
 
 describe('document-capture/context/failed-capture-attempts', () => {
   it('has expected default properties', () => {
@@ -44,6 +53,32 @@ describe('document-capture/context/failed-capture-attempts', () => {
       result.current.onFailedCaptureAttempt(metadata);
 
       expect(result.current.lastAttemptMetadata).to.deep.equal(metadata);
+    });
+    context('failed acuant camera attempts', () => {
+      let result;
+      let addPageAction;
+
+
+        addPageAction = sinon.spy();
+        ({ result } = renderHook(() => useContext(AcuantContext), {
+          wrapper: ({ children }) => (
+            <AnalyticsContext.Provider value={{ addPageAction }}>
+              <DeviceContext.Provider value={{ isMobile: true }}>
+                <AcuantContextProvider sdkSrc="about:blank" cameraSrc="about:blank">
+                  <Provider maxAttemptsBeforeNativeCamera={3}>{children}</Provider>
+                </AcuantContextProvider>
+              </DeviceContext.Provider>
+            </AnalyticsContext.Provider>
+          ),
+        }));
+
+
+      it('calls analytics with native camera message when failed attempts is greater than 2', () => {
+        expect(addPageAction).to.have.been.calledWith(
+          'IdV: Force native camera. Failed attempts: 3',
+          {success: true},
+        );
+      })
     });
   });
 });
