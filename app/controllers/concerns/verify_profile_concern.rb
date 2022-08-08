1d7ba0d52b4879ccd62e2f0ed88bcf55c23d1f39
Jack Cody <jack.cody@gsa.gov>
2022-08-08 17:30:27 -0600

    Add intercept for user needs backup codes reminder

--- app/controllers/concerns/verify_profile_concern.rb
+++ app/controllers/concerns/verify_profile_concern.rb
@@ -3,9 +3,26 @@ module VerifyProfileConcern
 
   def account_or_verify_profile_url
     return reactivate_account_url if user_needs_to_reactivate_account?
-    return account_url unless profile_needs_verification?
-    return idv_gpo_url if gpo_mail_bounced?
-    idv_gpo_verify_url
+    if profile_needs_verification?
+      return idv_gpo_url if gpo_mail_bounced?
+      return idv_gpo_verify_url
+    end
+    return backup_code_reminder_url if user_needs_backup_code_reminder?
+    account_url
+  end
+
+  def user_needs_backup_code_reminder?
+    user_backup_codes_configured? && user_last_signed_in_more_than_5_months_ago?
+  end
+
+  def user_backup_codes_configured?
+    mfa_user = MfaContext.new(current_user)
+    mfa_user.backup_code_configurations.present?
+  end
+
+  def user_last_signed_in_more_than_5_months_ago?
+    user = UserDecorator.new(current_user)
+    user.devices? && user.last_signed_in_at < 5.months.ago
   end
 
   def profile_needs_verification?
