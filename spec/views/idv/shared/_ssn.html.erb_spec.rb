70604392609145ad2751e3aaab23ec8eac94ef96
Alex Bradley <alexander.bradley@gsa.gov>
2022-08-08 16:34:15 -0400

    moved test expectations into methods

--- spec/views/idv/shared/_ssn.html.erb_spec.rb
+++ spec/views/idv/shared/_ssn.html.erb_spec.rb
@@ -4,46 +4,17 @@ describe 'idv/shared/_ssn.html.erb' do
   include Devise::Test::ControllerHelpers
 
   let(:proofing_device_profiling_collecting_enabled) { nil }
-  let(:lexisnexis_threatmetrix_account_id) { 'test_org_id' }
+  let(:lexisnexis_threatmetrix_org_id) { 'test_org_id' }
   let(:session_id) { 'ABCD-1234' }
   let(:updating_ssn) { false }
   let(:js_domain) { 'h.online-metrix.net' }
 
   let(:tags_js_url) {
-    "https://#{js_domain}/fp/tags.js?org_id=#{lexisnexis_threatmetrix_account_id}&session_id=#{session_id}"
+    "https://#{js_domain}/fp/tags.js?org_id=#{lexisnexis_threatmetrix_org_id}&session_id=#{session_id}"
   }
 
   let(:tags_iframe_url) {
-    "https://#{js_domain}/fp/tags?org_id=#{lexisnexis_threatmetrix_account_id}&session_id=#{session_id}"
-  }
-
-  let(:script_tag_rendered) {
-    expect(rendered).to have_css("script[src='#{tags_js_url}']", visible: false)
-  }
-
-  let(:noscript_tag_rendered) {
-    expect(rendered).to have_css("noscript iframe[src='#{tags_iframe_url}']", visible: false)
-  }
-
-  let(:session_id_input_rendered) {
-    expect(rendered).
-      to have_css(
-        "input[type=hidden][name='doc_auth[threatmetrix_session_id]'][value='#{session_id}']",
-        visible: false,
-      )
-  }
-
-  let(:script_tag_not_rendered) {
-    expect(rendered).not_to have_css("script[src*='#{js_domain}']", visible: false)
-  }
-
-  let(:noscript_tag_not_rendered) {
-    expect(rendered).not_to have_css("noscript iframe[src*='#{js_domain}']", visible: false)
-  }
-
-  let(:session_id_input_not_rendered) {
-    expect(rendered).
-      not_to have_css('input[name="doc_auth[threatmetrix_session_id]"]', visible: false)
+    "https://#{js_domain}/fp/tags?org_id=#{lexisnexis_threatmetrix_org_id}&session_id=#{session_id}"
   }
 
   before :each do
@@ -53,7 +24,7 @@ describe 'idv/shared/_ssn.html.erb' do
       to receive(:proofing_device_profiling_collecting_enabled).
       and_return(proofing_device_profiling_collecting_enabled)
     allow(IdentityConfig.store).
-      to receive(:lexisnexis_threatmetrix_account_id).and_return(lexisnexis_threatmetrix_account_id)
+      to receive(:lexisnexis_threatmetrix_org_id).and_return(lexisnexis_threatmetrix_org_id)
 
     render partial: 'idv/shared/ssn', locals: {
       flow_session: {
@@ -71,13 +42,13 @@ describe 'idv/shared/_ssn.html.erb' do
       context 'and entering ssn for the first time' do
         describe '<script> tag' do
           it 'is rendered' do
-            script_tag_rendered
+            expect_script_tag_rendered
           end
         end
 
         describe '<noscript> tag' do
           it 'is rendered' do
-            noscript_tag_rendered
+            expect_noscript_tag_rendered
           end
         end
       end
@@ -86,22 +57,22 @@ describe 'idv/shared/_ssn.html.erb' do
         let(:updating_ssn) { true }
 
         it 'does not render <script> tag' do
-          script_tag_not_rendered
+          expect_script_tag_not_rendered
         end
         it 'does not render <noscript> tag' do
-          noscript_tag_not_rendered
+          expect_noscript_tag_not_rendered
         end
       end
     end
 
     context 'org id not specified' do
-      let(:lexisnexis_threatmetrix_account_id) { '' }
+      let(:lexisnexis_threatmetrix_org_id) { '' }
 
       it 'does not render <script> tag' do
-        script_tag_not_rendered
+        expect_script_tag_not_rendered
       end
       it 'does not render <noscript> tag' do
-        noscript_tag_not_rendered
+        expect_noscript_tag_not_rendered
       end
     end
   end
@@ -110,10 +81,39 @@ describe 'idv/shared/_ssn.html.erb' do
     let(:proofing_device_profiling_collecting_enabled) { false }
 
     it 'does not render <script> tag' do
-      script_tag_not_rendered
+      expect_script_tag_not_rendered
     end
     it 'does not render <noscript> tag' do
-      noscript_tag_not_rendered
+      expect_noscript_tag_not_rendered
     end
+  end
+
+  def expect_script_tag_rendered
+    expect(rendered).to have_css("script[src='#{tags_js_url}']", visible: false)
+  end
+
+  def expect_noscript_tag_rendered
+    expect(rendered).to have_css("noscript iframe[src='#{tags_iframe_url}']", visible: false)
+  end
+
+  def expect_session_id_input_rendered
+    expect(rendered).
+      to have_css(
+        "input[type=hidden][name='doc_auth[threatmetrix_session_id]'][value='#{session_id}']",
+        visible: false,
+      )
+  end
+
+  def expect_script_tag_not_rendered
+    expect(rendered).not_to have_css("script[src*='#{js_domain}']", visible: false)
+  end
+
+  def expect_noscript_tag_not_rendered
+    expect(rendered).not_to have_css("noscript iframe[src*='#{js_domain}']", visible: false)
+  end
+
+  def expect_session_id_input_not_rendered
+    expect(rendered).
+      not_to have_css('input[name="doc_auth[threatmetrix_session_id]"]', visible: false)
   end
 end
