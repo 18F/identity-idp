13b089e9f260f20be98676e39c880d5adb13cb66
Alex Bradley <alexander.bradley@gsa.gov>
2022-08-11 15:26:00 -0400

    tests for ipp ssn_step

--- spec/services/idv/steps/ipp/ssn_step_spec.rb
+++ spec/services/idv/steps/ipp/ssn_step_spec.rb
@@ -6,6 +6,7 @@ describe Idv::Steps::Ipp::SsnStep do
   let(:session) { { sp: { issuer: service_provider.issuer } } }
   let(:user) { build(:user) }
   let(:service_provider) { create(:service_provider) }
+  let(:threatmetrix_session_id) { nil }
   let(:controller) do
     instance_double(
       'controller',
@@ -16,7 +17,12 @@ describe Idv::Steps::Ipp::SsnStep do
   end
 
   let(:flow) do
-    Idv::Flows::InPersonFlow.new(controller, {}, 'idv/in_person')
+    Idv::Flows::InPersonFlow.new(controller, {}, 'idv/in_person').tap do |flow|
+      flow.flow_session = {
+        pii_from_user: {},
+        threatmetrix_session_id: threatmetrix_session_id,
+      }
+    end
   end
 
   subject(:step) do
@@ -37,6 +43,19 @@ describe Idv::Steps::Ipp::SsnStep do
         step.call
 
         expect(session[:idv]['applicant']).to be_blank
+      end
+    end
+
+    context 'with proofing device profiling collecting enabled' do
+      let(:threatmetrix_session_id) { 'ABCD-1234' }
+
+      it 'adds a session id to flow session' do
+        allow(IdentityConfig.store).
+          to receive(:proofing_device_profiling_collecting_enabled).
+          and_return(true)
+        step.call
+
+        expect(flow.flow_session[:threatmetrix_session_id]).to eq(threatmetrix_session_id)
       end
     end
   end
