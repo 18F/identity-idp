afebe47cd97628b647d58ce9246952bc6754cce5
eric-gade <eric.gade@gsa.gov>
2022-08-15 16:39:34 -0400

    Updating FileInput component for better accessibility
    -- What
    The inner text of our FileInput components cannot currently be
    captured by screen readers. For example, when uploading the Front or
    Back image during the idp verification process, only the actual HTML
    labels and the respective side "Front" or "Back" will be captured by
    readers.
    
    To deal with this, we've added to the hidden hint field which is
    referenced by `aria-describedby`. That attribute can take multiple,
    space-separated values to reference multiple possible descriptions of
    the data (and also their order). See
    (https://www.w3.org/WAI/tutorials/forms/instructions/#providing-instructions-within-labels).
    
    changelog: Improvements, Accessibility, updating FileInput accessibility

--- app/javascript/packages/document-capture/components/file-input.jsx
+++ app/javascript/packages/document-capture/components/file-input.jsx
@@ -160,6 +160,7 @@ function FileInput(props, ref) {
   }, [value]);
   const inputId = `file-input-${instanceId}`;
   const hintId = `${inputId}-hint`;
+  const innerHintId = `${hintId}-inner`;
 
   /**
    * In response to a file input change event, confirms that the file is valid before calling
@@ -303,7 +304,7 @@ function FileInput(props, ref) {
             <div className="usa-file-input__instructions" aria-hidden="true">
               {bannerText && <strong className="usa-file-input__banner-text">{bannerText}</strong>}
               {isMobile && bannerText ? null : (
-                <span className="usa-file-input__drag-text">
+                <span className="usa-file-input__drag-text" id={innerHintId}>
                   {formatHTML(t('doc_auth.forms.choose_file_html'), {
                     'lg-underline': ({ children }) => (
                       <span className="usa-file-input__choose">{children}</span>
@@ -328,7 +329,7 @@ function FileInput(props, ref) {
             onClick={onClick}
             onDrop={onDrop}
             accept={accept ? accept.join() : undefined}
-            aria-describedby={hint ? hintId : undefined}
+            aria-describedby={hint ? `${innerHintId} ${hintId}` : undefined}
           />
         </div>
       </div>
