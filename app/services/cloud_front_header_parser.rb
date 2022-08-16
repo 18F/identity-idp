aed1bef6c82665a15866a5a201e664c81c9e5680
Matt Wagner <mattwagner@navapbc.com>
2022-08-16 13:30:27 -0400

    Remove unused methods

--- app/services/cloud_front_header_parser.rb
+++ app/services/cloud_front_header_parser.rb
@@ -20,26 +20,6 @@ class CloudFrontHeaderParser
     @request.get_header 'CloudFront-Viewer-Address'
   end
 
-  # HTTP version used for client connection to CloudFront
-  def http_version
-    @request.get_header 'CloudFront-Viewer-Http-Version'
-  end
-
-  # TLS version and ciphers used for client connection to CloudFront
-  def tls_version
-    @request.get_header 'CloudFront-Viewer-TLS'
-  end
-
-  # ISO country code for IP client used to connect
-  def iso_country
-    @request.get_header 'CloudFront-Viewer-Country'
-  end
-
-  # ISO region subcode for IP client used to connect
-  def iso_region
-    @request.get_header 'CloudFront-Viewer-Country-Region'
-  end
-
   private
 
   def ip_and_port
