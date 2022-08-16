c6a2f0c7fb30168fcdb4e79597acf3ce3276fb21
gangelo <web.gma@gmail.com>
2022-08-16 08:51:25 -0400

    Fix indentation

--- spec/services/idv/inherited_proofing/va/verification_service_spec.rb
+++ spec/services/idv/inherited_proofing/va/verification_service_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Idv::InheritedProofing::Va::VerificationService do
-    include_context 'va_user_context'
+  include_context 'va_user_context'
 
-    subject { described_class.new(config: LexisNexisFixtures.example_config) }
+  subject { described_class.new(config: LexisNexisFixtures.example_config) }
 
-    describe "valid phone" do
-        it "does something" do
-            # get a valid call
-            expect(subject.verify_phone(valid_user)).to eq "foo"
-        end
+  describe "valid phone" do
+    it "does something" do
+      # get a valid call
+      expect(subject.verify_phone(valid_user)).to eq "foo"
     end
+  end
 
-    describe "invalid phone" do
-        it "does something else" do
-            # what error messages?
-        end
+  describe "invalid phone" do
+    it "does something else" do
+      # what error messages?
     end
+  end
 end
