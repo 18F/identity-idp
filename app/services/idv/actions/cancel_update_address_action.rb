bbcb83c1bab0450a24354e8df5dca0d3f2eadf9f
Tim Bradley <timothy.bradley@gsa.gov>
2022-08-16 09:34:11 -0700

    LG-7260: Remove unused action from doc auth flow

--- app/services/idv/actions/cancel_update_address_action.rb
+++
@@ -1,9 +0,0 @@
-module Idv
-  module Actions
-    class CancelUpdateAddressAction < Idv::Steps::DocAuthBaseStep
-      def call
-        mark_step_complete(:address) if flow_session.dig(:pii_from_doc, :address1)
-      end
-    end
-  end
-end
