13b3f3b6415b40f029e4cd76d561edfa4db98d7e
Zach Margolis <zachary.margolis@gsa.gov>
2022-08-16 16:59:46 -0700

    Use strings and #end_with? for readability

--- spec/lib/identity_config_spec.rb
+++ spec/lib/identity_config_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe IdentityConfig do
   describe '.key_types' do
     it 'has all _enabled keys as booleans' do
       aggregate_failures do
-        IdentityConfig.key_types.select { |key, _type| key =~ /_enabled$/ }.
+        IdentityConfig.key_types.select { |key, _type| key.to_s.end_with?('_enabled') }.
           each do |key, type|
             expect(type).to eq(:boolean), "expected #{key} to be a boolean"
           end
@@ -13,7 +13,7 @@ RSpec.describe IdentityConfig do
 
     it 'has all _at keys as timestamps' do
       aggregate_failures do
-        IdentityConfig.key_types.select { |key, _type| key =~ /_at$/ }.
+        IdentityConfig.key_types.select { |key, _type| key.to_s.end_with?('_at') }.
           each do |key, type|
             expect(type).to eq(:timestamp), "expected #{key} to be a timestamp"
           end
