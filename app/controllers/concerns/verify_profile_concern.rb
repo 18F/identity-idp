9070c339702e3560b780a0a59ef006e7b0b0aa2c
jc-gsa <104452882+jc-gsa@users.noreply.github.com>
2022-08-17 14:48:59 -0700

    LG-6885: Remind users about backup codes & regenerate (#6723)
    Add user reminder about backup codes feature
    
    changelog: Internal, Authentication, Add backup code reminder MFA (LG-6885)

--- app/controllers/concerns/verify_profile_concern.rb
+++ app/controllers/concerns/verify_profile_concern.rb
@@ -3,9 +3,27 @@ module VerifyProfileConcern
 
   def account_or_verify_profile_url
     return reactivate_account_url if user_needs_to_reactivate_account?
-    return account_url unless profile_needs_verification?
-    return idv_gpo_url if gpo_mail_bounced?
-    idv_gpo_verify_url
+    if profile_needs_verification?
+      return idv_gpo_url if gpo_mail_bounced?
+      return idv_gpo_verify_url
+    end
+    return backup_code_reminder_url if user_needs_backup_code_reminder?
+    account_url
+  end
+
+  def user_needs_backup_code_reminder?
+    return false unless IdentityConfig.store.backup_code_reminder_redirect
+    user_backup_codes_configured? && user_last_signed_in_more_than_5_months_ago?
+  end
+
+  def user_backup_codes_configured?
+    MfaContext.new(current_user).backup_code_configurations.present?
+  end
+
+  def user_last_signed_in_more_than_5_months_ago?
+    user = UserDecorator.new(current_user)
+    second_last_signed_in_at = user.second_last_signed_in_at
+    second_last_signed_in_at && second_last_signed_in_at < 5.months.ago
   end
 
   def profile_needs_verification?
