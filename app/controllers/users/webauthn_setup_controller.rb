b90f21d6ff2804850190016e254c94bc8adb782c
Luis H. Matos <ThatSpaceGuy@users.noreply.github.com>
2022-08-18 11:00:05 -0500

    Enroll verify failure events (#6772)
    * LG-6986 LG-7240 WebAuthn Enroll Failure logging
    
    changelog: Internal, Attempts API, Track additional events

--- app/controllers/users/webauthn_setup_controller.rb
+++ app/controllers/users/webauthn_setup_controller.rb
@@ -25,6 +25,15 @@ module Users
       analytics.webauthn_setup_visit(**result.to_h)
       save_challenge_in_session
       @exclude_credentials = exclude_credentials
+
+      if !result.success?
+        if @platform_authenticator
+          irs_attempts_api_tracker.mfa_enroll_webauthn_platform(success: false)
+        else
+          irs_attempts_api_tracker.mfa_enroll_webauthn_roaming(success: false)
+        end
+      end
+
       flash_error(result.errors) unless result.success?
     end
 
