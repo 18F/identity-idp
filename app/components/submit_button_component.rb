95427a4b4925d6bb04bf7a2da474ce2ab5064a61
Andrew Duthie <andrew.duthie@gsa.gov>
2022-08-18 09:37:10 -0400

    Set big, wide as SubmitButtonComponent default

--- app/components/submit_button_component.rb
+++ app/components/submit_button_component.rb
@@ -1,4 +1,8 @@
 class SubmitButtonComponent < ButtonComponent
+  def initialize(big: true, wide: true, **tag_options)
+    super(big: big, wide: wide, **tag_options)
+  end
+
   def call
     content_tag(:'lg-submit-button', super)
   end
