66bd6a45f7254c6eecd3eafa1da8480a2ce97962
Andrew Duthie <andrew.duthie@gsa.gov>
2022-08-18 10:29:46 -0400

    Assign content as explicit first argument
    More readable

--- lib/extensions/simple_form/components/submit_component.rb
+++ lib/extensions/simple_form/components/submit_component.rb
@@ -1,7 +1,7 @@
 module SubmitComponent
   def submit(*args, &block)
     options = args.extract_options!
-    content, = args
+    content = args.first
     content = template.capture { yield(content) } if block
     template.render SubmitButtonComponent.new(**options, &block).with_content(content)
   end
