438de09e95f05d63c1d6214f58e1c8a15e25c4aa
Eric Gade <105373963+eric-gade@users.noreply.github.com>
2022-08-19 13:27:54 -0400

    LG-7291 TypeScript refactoring for 6777 (#6769)
    * Updating types to deal with new native camera attempts
    
    -- What
    With the addition of new checks for `maxAttemptsBeforeNativeCamera`,
    which triggers the use of the native camera after a certain number of
    failed Acuant attempts, we needed to update some of the type
    definitions.
    
    In addition to doing so, we have converted several of the affected
    files to full TypeScript (from JSDoc annotations).
    
    * Post-rebase fixes
    
    changelog: Improvements, TypeScript, rebasing typescript changes on
    main and associated fixes
    
    * Fixing linting issues
    
    changelog: Improvements, TypeScript, fixing linting issues
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Fixing incorrect optional props in ImageAnalyticsPayload
    
    Also switching to the global window object, which already is annotated
    with the AcuantJavaScriptWebSdk interface
    
    changelog: Improvements, TypeScript, updating interface property types
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Update app/javascript/packages/document-capture/components/acuant-capture.tsx
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Using interface extension for TS types
    
    Additionally, I have renamed failed-capture-context.jsx to tsx
    explicitly
    
    changelog: Improvements, TypeScript, updating type definitions
    
    * Removing dead type definition
    
    changelog: Improvements, TypeScript, removing old type definition
    
    Co-authored-by: Eric Gade <ecgade@macbook-m1.home>
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

---
+++ app/javascript/packages/document-capture/context/failed-capture-attempts.tsx
@@ -0,0 +1,102 @@
+import { createContext, useState } from 'react';
+import type { ReactNode } from 'react';
+import useCounter from '../hooks/use-counter';
+
+interface CaptureAttemptMetadata {
+  isAssessedAsGlare: boolean;
+  isAssessedAsBlurry: boolean;
+}
+
+interface FailedCaptureAttemptsContextInterface {
+  /**
+   * Current number of failed capture attempts
+   */
+  failedCaptureAttempts: number;
+  /**
+   * Number of failed attempts before showing tips
+   */
+  maxFailedAttemptsBeforeTips: number;
+  /**
+   * The maximum number of failed Acuant capture attempts
+   * before use of the native camera option is triggered
+   */
+  maxAttemptsBeforeNativeCamera: number;
+  /**
+   * Callback triggered on attempt, to increment attempts
+   */
+  onFailedCaptureAttempt: (metadata: CaptureAttemptMetadata) => void;
+  /**
+   * Callback to trigger a reset of attempts
+   */
+  onResetFailedCaptureAttempts: () => void;
+  /**
+   * Metadata about the last attempt
+   */
+  lastAttemptMetadata: CaptureAttemptMetadata;
+  /**
+   * Whether or not the native camera is currently being forced
+   * after maxAttemptsBeforeNativeCamera number of failed attempts
+   */
+  forceNativeCamera: boolean;
+}
+
+const DEFAULT_LAST_ATTEMPT_METADATA: CaptureAttemptMetadata = {
+  isAssessedAsGlare: false,
+  isAssessedAsBlurry: false,
+};
+
+const FailedCaptureAttemptsContext = createContext<FailedCaptureAttemptsContextInterface>({
+  failedCaptureAttempts: 0,
+  onFailedCaptureAttempt: () => {},
+  onResetFailedCaptureAttempts: () => {},
+  maxAttemptsBeforeNativeCamera: Infinity,
+  maxFailedAttemptsBeforeTips: Infinity,
+  lastAttemptMetadata: DEFAULT_LAST_ATTEMPT_METADATA,
+  forceNativeCamera: false,
+});
+
+FailedCaptureAttemptsContext.displayName = 'FailedCaptureAttemptsContext';
+
+interface FailedCaptureAttemptsContextProviderProps {
+  children: ReactNode;
+  maxFailedAttemptsBeforeTips: number;
+  maxAttemptsBeforeNativeCamera: number;
+}
+
+function FailedCaptureAttemptsContextProvider({
+  children,
+  maxFailedAttemptsBeforeTips,
+  maxAttemptsBeforeNativeCamera,
+}: FailedCaptureAttemptsContextProviderProps) {
+  const [lastAttemptMetadata, setLastAttemptMetadata] = useState<CaptureAttemptMetadata>(
+    DEFAULT_LAST_ATTEMPT_METADATA,
+  );
+  const [failedCaptureAttempts, incrementFailedCaptureAttempts, onResetFailedCaptureAttempts] =
+    useCounter();
+
+  function onFailedCaptureAttempt(metadata: CaptureAttemptMetadata) {
+    incrementFailedCaptureAttempts();
+    setLastAttemptMetadata(metadata);
+  }
+
+  const forceNativeCamera = failedCaptureAttempts >= maxAttemptsBeforeNativeCamera;
+
+  return (
+    <FailedCaptureAttemptsContext.Provider
+      value={{
+        failedCaptureAttempts,
+        onFailedCaptureAttempt,
+        onResetFailedCaptureAttempts,
+        maxAttemptsBeforeNativeCamera,
+        maxFailedAttemptsBeforeTips,
+        lastAttemptMetadata,
+        forceNativeCamera,
+      }}
+    >
+      {children}
+    </FailedCaptureAttemptsContext.Provider>
+  );
+}
+
+export default FailedCaptureAttemptsContext;
+export { FailedCaptureAttemptsContextProvider as Provider };
