f857a32aff3b77e6fec83cb7c32c54b898da187e
Mitchell Henke <mitchell.henke@gsa.gov>
2022-08-22 15:52:48 -0500

    Hash Device UUID for IRS Attempts API
    changelog: Improvements, Attempts API, Hash Device UUID for IRS Attempts API

--- spec/services/irs_attempts_api/tracker_spec.rb
+++ spec/services/irs_attempts_api/tracker_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe IrsAttemptsApi::Tracker do
   let(:enabled_for_session) { true }
   let(:request) { instance_double(ActionDispatch::Request) }
   let(:service_provider) { create(:service_provider) }
-  let(:device_fingerprint) { 'device_id' }
+  let(:cookie_device_uuid) { 'device_id' }
   let(:sp_request_uri) { 'https://example.com/auth_page' }
   let(:user) { create(:user) }
 
@@ -26,7 +26,7 @@ RSpec.describe IrsAttemptsApi::Tracker do
       request: request,
       user: user,
       sp: service_provider,
-      device_fingerprint: device_fingerprint,
+      cookie_device_uuid: cookie_device_uuid,
       sp_request_uri: sp_request_uri,
       enabled_for_session: enabled_for_session,
     )
