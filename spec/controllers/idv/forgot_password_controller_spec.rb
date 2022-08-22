1bf6e925da641ba72a96e0b587e5ba0942a12d64
Jack Cody <jack.cody@gsa.gov>
2022-08-22 15:45:54 -0700

    Add cleaner tests

--- spec/controllers/idv/forgot_password_controller_spec.rb
+++ spec/controllers/idv/forgot_password_controller_spec.rb
@@ -8,13 +8,17 @@ describe Idv::ForgotPasswordController do
   end
 
   describe '#new' do
-    it 'tracks the event in analytics when referer is nil' do
+    before do
       stub_sign_in
       stub_analytics
 
-      expect(@analytics).to receive(:track_event).with('IdV: forgot password visited')
+      allow(@analytics).to receive(:track_event)
+    end
 
+    it 'tracks the event in analytics when referer is nil' do
       get :new
+
+      expect(@analytics).to have_received(:track_event).with('IdV: forgot password visited')
     end
   end
 
@@ -25,14 +29,14 @@ describe Idv::ForgotPasswordController do
       stub_sign_in(user)
       stub_analytics
       stub_attempts_tracker
+      allow(@analytics).to receive(:track_event)
       allow(@irs_attempts_api_tracker).to receive(:track_event)
     end
 
     it 'tracks analytics events' do
-      expect(@analytics).to receive(:track_event).with('IdV: forgot password confirmed')
-
       post :update
 
+      expect(@analytics).to have_received(:track_event).with('IdV: forgot password confirmed')
       expect(@irs_attempts_api_tracker).to have_received(:track_event).with(
         :forgot_password_email_sent,
         email: user.email,
