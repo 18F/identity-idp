c44a9fd80887139cb2e53365b799edbc413e47b9
Luis <lmatos@flexion.us>
2022-08-24 15:37:33 -0500

    PR comments

--- app/services/flow/flow_state_machine.rb
+++ app/services/flow/flow_state_machine.rb
@@ -22,9 +22,9 @@ module Flow
       step = current_step
       result = flow.handle(step)
 
-      if params[:controller] == 'idv/doc_auth' && step == 'upload'
-        irs_attempts_api_tracker.document_upload_method_selected(upload_method: params[:type])
-      end
+      # if params[:controller] == 'idv/doc_auth' && step == 'upload'
+      #   irs_attempts_api_tracker.document_upload_method_selected(upload_method: params[:type])
+      # end
 
       if @analytics_id
         increment_step_name_counts
