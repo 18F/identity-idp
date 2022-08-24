3a223214aaac1014e926a4d5b719d5c07493b62a
jc-gsa <104452882+jc-gsa@users.noreply.github.com>
2022-08-24 14:49:55 -0700

    Update app/services/request_password_reset.rb
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/services/request_password_reset.rb
+++ app/services/request_password_reset.rb
@@ -17,10 +17,6 @@ RequestPasswordReset = RedactedStruct.new(
   private
 
   def send_reset_password_instructions
-    irs_params = {
-      email: email,
-    }
-
     if Throttle.new(user: user, throttle_type: :reset_password_email).throttled_else_increment?
       analytics.throttler_rate_limit_triggered(throttle_type: :reset_password_email)
       irs_attempts_api_tracker.forgot_password_email_rate_limited(email: email)
