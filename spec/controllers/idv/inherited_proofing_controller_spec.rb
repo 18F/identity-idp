5b6c456db2705f036f53041354387f6cfba03b94
Jeff Sheffel <jscodefix@sheffel.org>
2022-08-25 08:24:35 -0600

    LG-6970 create inherited proofing basic landing page (#6823)
    * LG-6970 create inherited proofing basic landing page
    
    * fix lint errors for 2 files (get_started.html.erb, en.yml)
    
    * add inherited_proofing_enabled feature flag
    
    changelog: Improvements, inherited proofing, Create basic landing page
    
    * Fix formatting
    
    * Add missing translations to pass specs
    
    * Make placeholders consistent :S
    
    * Fix inconsistent interpolation across fr and es locales
    
    * optimize template button as a submit (w/o js-consent-continue-form)
    
    Co-authored-by: gangelo <web.gma@gmail.com>

---
+++ spec/controllers/idv/inherited_proofing_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe Idv::InheritedProofingController do
+  describe '#index' do
+    it 'redirects to the first step' do
+      get :index
+
+      expect(response).to redirect_to idv_inherited_proofing_step_url(step: :get_started)
+    end
+
+    context 'when the inherited proofing feature flag is disabled' do
+      it 'returns 404 not found' do
+        allow(IdentityConfig.store).to receive(:inherited_proofing_enabled).and_return(false)
+
+        get :index
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe '#show' do
+    it 'renders the correct template' do
+      expect(subject).to receive(:render).with(
+        template: 'layouts/flow_step',
+        locals: hash_including(
+          :flow_session,
+          flow_namespace: 'idv',
+          step_template: 'idv/inherited_proofing/get_started',
+          step_indicator: hash_including(
+            :steps,
+            current_step: :getting_started,
+          ),
+        ),
+      ).and_call_original
+
+      get :show, params: { step: 'get_started' }
+    end
+
+    it 'redirects to the configured next step' do
+      mock_next_step(:some_next_step)
+      get :show, params: { step: 'getting_started' }
+
+      expect(response).to redirect_to idv_inherited_proofing_step_url(:some_next_step)
+    end
+
+    it 'redirects to the first step if a non-existent step is given' do
+      get :show, params: { step: 'non_existent_step' }
+
+      expect(response).to redirect_to idv_inherited_proofing_step_url(step: :get_started)
+    end
+  end
+
+  def mock_next_step(step)
+    allow_any_instance_of(Idv::Flows::InheritedProofingFlow).to receive(:next_step).and_return(step)
+  end
+end
