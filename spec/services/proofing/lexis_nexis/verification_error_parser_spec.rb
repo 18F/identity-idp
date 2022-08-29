73df4f792ab90fab8398b9a5c3c02ac55f3977d1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-08-29 09:50:17 -0400

    clean out verification error parser

--- spec/services/proofing/lexis_nexis/verification_error_parser_spec.rb
+++ spec/services/proofing/lexis_nexis/verification_error_parser_spec.rb
@@ -8,19 +8,6 @@ RSpec.describe Proofing::LexisNexis::VerificationErrorParser do
     let(:response_body) do
       JSON.parse(LexisNexisFixtures.instant_verify_year_of_birth_fail_response_json)
     end
-
-    it 'logs a message formatted as JSON' do
-      expect(Rails.logger).to receive(:info) do |arg|
-        expect(JSON.parse(arg, symbolize_names: true)).to eq(
-          name: 'lexisnexis_partial_dob',
-          original_passed: false,
-          passed_partial_dob: true,
-          partial_dob_override_enabled: false,
-        )
-      end
-
-      expect(error_parser).to be
-    end
   end
 
   describe '#parsed_errors' do
@@ -31,92 +18,6 @@ RSpec.describe Proofing::LexisNexis::VerificationErrorParser do
       expect(errors[:Discovery]).to eq(nil) # This should be absent since it passed
       expect(errors[:SomeOtherProduct]).to eq(response_body['Products'][1])
       expect(errors[:InstantVerify]).to eq(response_body['Products'][2])
-    end
-  end
-
-  describe '#instant_verify_dob_year_only_pass?' do
-    subject(:dob_year_only_pass) do
-      error_parser.send(:instant_verify_dob_year_only_pass?, items)
-    end
-
-    context 'with both DOBYearVerified and DOBFullVerified passing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBYearVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'DOBFullVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'pass' },
-        ]
-      end
-      it { is_expected.to eq(true) }
-    end
-
-    context 'with both DOBYearVerified and DOBFullVerified passing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBYearVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'DOBFullVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'pass' },
-        ]
-      end
-      it { is_expected.to eq(true) }
-    end
-
-    context 'with both DOBYearVerified and DOBFullVerified passing, some other product failing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBYearVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'DOBFullVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'fail' },
-        ]
-      end
-      it { is_expected.to be_falsey }
-    end
-
-    context 'with both DOBYearVerified passed, and DOBFullVerified failing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBYearVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'DOBFullVerified', 'ItemStatus' => 'fail' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'pass' },
-        ]
-      end
-      it { is_expected.to eq(true) }
-    end
-
-    context 'with DOBYearVerified passed and DOBFullVerified failing, some other product failing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBYearVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'DOBFullVerified', 'ItemStatus' => 'fail' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'fail' },
-        ]
-      end
-      it { is_expected.to be_falsey }
-    end
-
-    context 'with DOBYearVerified missing and DOBFullVerified passing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBFullVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'pass' },
-        ]
-      end
-      it { is_expected.to be_falsey }
-    end
-
-    context 'with DOBYearVerified passing and DOBFullVerified missing' do
-      let(:items) do
-        [
-          { 'ItemName' => 'DOBYearVerified', 'ItemStatus' => 'pass' },
-          { 'ItemName' => 'SomeOtherProduct', 'ItemStatus' => 'pass' },
-        ]
-      end
-      it { is_expected.to be_falsey }
-    end
-
-    context 'with a nil Items array' do
-      let(:items) { nil }
-      it { is_expected.to be_falsey }
     end
   end
 end
