e1c806ba560db0a8b6b8a85b8494f993497171d6
Alex Bradley <alexander.bradley@gsa.gov>
2022-08-29 15:04:22 -0400

    add test

---
+++ spec/views/idv/setup_errors/show.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'idv/setup_errors/show.html.erb' do
+  before do
+    render
+  end
+
+  it 'shows step indicator with pending status on secure account' do
+    expect(view.content_for(:pre_flash_content)).to have_css(
+      '.step-indicator__step--current',
+      text: t('step_indicator.flows.idv.secure_account'),
+    )
+  end
+
+  it 'includes a message instructing them to fill out a contact form' do
+    contact = 'http://example.com/contact'
+
+    expect(rendered).to have_text(
+      strip_tags(
+        t('idv.failure.setup.fail_html', contact_form: contact),
+      ),
+    )
+  end
+end
