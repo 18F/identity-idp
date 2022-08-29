ad8f8fd415229c0bae2b7a3e3452b1c699393499
Steve Urciuoli <steve.urciuoli@gsa.gov>
2022-08-29 18:09:36 -0500

    whielisted_response_h typo and rm ParserError

--- app/services/proofing/lexis_nexis/ddp/response_redacter.rb
+++ app/services/proofing/lexis_nexis/ddp/response_redacter.rb
@@ -126,16 +126,12 @@ module Proofing
 
         # @param [Hash] body
         def self.redact(hash)
-          begin
-            whielisted_response_h = hash.slice(*ALLOWED_RESPONSE_FIELDS)
-            unwhitelisted_fields = hash.keys - whielisted_response_h.keys
-            unwhitelisted_fields.each do |key|
-              whielisted_response_h[key] = '[redacted]'
-            end
-            whielisted_response_h
-          rescue JSON::ParserError
-            {}
+          whitelisted_response_h = hash.slice(*ALLOWED_RESPONSE_FIELDS)
+          unwhitelisted_fields = hash.keys - whitelisted_response_h.keys
+          unwhitelisted_fields.each do |key|
+            whitelisted_response_h[key] = '[redacted]'
           end
+          whitelisted_response_h
         end
       end
     end
