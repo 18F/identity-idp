a20d052d0d24b3b0dc9778e231b5b2401c5b4b26
Alex Bradley <alexander.bradley@gsa.gov>
2022-08-30 16:32:00 -0400

    LG-7173 Sorry there was a problem with proofing page (#6866)
    * Add new TMX failure proofing page
    
    * TMX failure page matches design
    
    * added translations
    
    * changed device_profiling_failed? to blocked_by_device_profiling?
    
    * fix linting errors
    
    changelog: Upcoming Features, ThreatMetrix, Add a page for when device
    profiling fails
    
    * add test
    
    * move mock contact url into config
    
    * move contact link text into a config variable
    
    * fixup
    
    * moved support_code to config value

---
+++ spec/views/idv/setup_errors/show.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'idv/setup_errors/show.html.erb' do
+  before do
+    render
+  end
+
+  it 'shows step indicator with pending status on secure account' do
+    expect(view.content_for(:pre_flash_content)).to have_css(
+      '.step-indicator__step--current',
+      text: t('step_indicator.flows.idv.secure_account'),
+    )
+  end
+
+  it 'includes a message instructing them to fill out a contact form' do
+    expect(rendered).to have_text(
+      strip_tags(
+        t(
+          'idv.failure.setup.fail_html',
+          contact_form_link: t('idv.failure.setup.link_text'),
+          support_code: 'ABCD',
+        ),
+      ),
+    )
+  end
+end
