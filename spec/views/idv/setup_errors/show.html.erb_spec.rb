04b02a8f6be20f9afb3df7db3298759238a96f25
Alex Bradley <alexander.bradley@gsa.gov>
2022-08-29 22:19:54 -0400

    move contact link text into a config variable

--- spec/views/idv/setup_errors/show.html.erb_spec.rb
+++ spec/views/idv/setup_errors/show.html.erb_spec.rb
@@ -12,30 +12,11 @@ describe 'idv/setup_errors/show.html.erb' do
     )
   end
 
-  context 'when threatmetrix_mock_contact_url is enabled' do
-    let(:mock_url) { 'https://example.com/contact' }
-    before :each do
-      allow(IdentityConfig.store).
-        to receive(:lexisnexis_threatmetrix_mock_contact_url).
-        and_return(mock_url)
-    end
-
-    it 'includes a message instructing them to fill out a mock contact form' do
-      expect(rendered).to have_text(
-        strip_tags(
-          t('idv.failure.setup.fail_html', contact_form: mock_url),
-        ),
-      )
-    end
-  end
-
-  context 'when threatmetrix_mock_contact_url is not present' do
-    it 'includes a message instructing them to fill out a contact form' do
-      expect(rendered).to have_text(
-        strip_tags(
-          t('idv.failure.setup.fail_html', contact_form: MarketingSite.contact_url),
-        ),
-      )
-    end
+  it 'includes a message instructing them to fill out a contact form' do
+    expect(rendered).to have_text(
+      strip_tags(
+        t('idv.failure.setup.fail_html', contact_form_link: t('idv.failure.setup.link_text')),
+      ),
+    )
   end
 end
