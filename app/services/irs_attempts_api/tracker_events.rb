9f1d431f350c6af2241f54a36307c97a170a0b01
Jack Cody <jack.cody@gsa.gov>
2022-08-29 17:27:44 -0700

    Add tracking event idv_phone_submitted

--- app/services/irs_attempts_api/tracker_events.rb
+++ app/services/irs_attempts_api/tracker_events.rb
@@ -130,6 +130,19 @@ module IrsAttemptsApi
       )
     end
 
+    # Tracks when the user submits their idv phone number
+    # @param [String] phone_number
+    # param [Boolean] success
+    # @param [Hash<Symbol,Array<Symbol>>] failure_reason
+    def idv_phone_submitted(phone_number:, success:, failure_reason: nil)
+      track_event(
+        :idv_phone_submitted,
+        phone_number: phone_number,
+        success: success,
+        failure_reason: failure_reason,
+      )
+    end
+
     # Tracks when a user submits OTP code sent to their phone
     # @param [String] phone_number
     # param [Boolean] success
