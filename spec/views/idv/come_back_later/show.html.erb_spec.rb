e728a1b71425f6144e00fc0b111fd369fb05ec36
Andrew Duthie <andrew.duthie@gsa.gov>
2022-08-31 09:49:09 -0400

    Stub view helper in specs

--- spec/views/idv/come_back_later/show.html.erb_spec.rb
+++ spec/views/idv/come_back_later/show.html.erb_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe 'idv/come_back_later/show.html.erb' do
   let(:sp_name) { '🔒🌐💻' }
+  let(:step_indicator_steps) { Idv::Flows::DocAuthFlow::STEP_INDICATOR_STEPS_GPO }
 
   before do
     @decorated_session = instance_double(ServiceProviderSessionDecorator)
     allow(@decorated_session).to receive(:sp_name).and_return(sp_name)
     allow(view).to receive(:decorated_session).and_return(@decorated_session)
+    allow(view).to receive(:step_indicator_steps).and_return(step_indicator_steps)
   end
 
   context 'with an SP' do
