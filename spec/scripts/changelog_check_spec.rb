de5bdc8310ee108aae3d529e339c420c4abbb3ca
Andrew Duthie <andrew.duthie@gsa.gov>
2022-08-31 14:17:34 -0400

    Improve changelog tool sorting and capitalization (#6880)
    * Use upcase_first to capitalize changelog subcategory
    
    **Why**: Avoids affecting intentional capitalization elsewhere in the changelog (e.g. "API" -> "api")
    
    * Sort changelog by subcategory
    
    **Why**: So that subcategories entries are grouped together in the output.
    
    changelog: Internal, Changelog Tool, Improve changelog tool sorting and capitalization
    
    * Add ActiveSupport#inflector required gem support
    
    * Use shorthand syntax for sort_by block
    
    * Remove upcase formatting from changelog script
    
    See:
    - https://github.com/18F/identity-idp/pull/6880#discussion_r959561620
    - https://github.com/18F/identity-idp/pull/6880#discussion_r959668852

--- spec/scripts/changelog_check_spec.rb
+++ spec/scripts/changelog_check_spec.rb
@@ -86,23 +86,21 @@ RSpec.describe 'scripts/changelog_check' do
         - Security: Upgrade Rails to patch vulnerability ([#6041](https://github.com/18F/identity-idp/pull/6041), [#6042](https://github.com/18F/identity-idp/pull/6042))
       CHANGELOG
     end
-  end
 
-  describe '#generate_changelog' do
-    it 'capitalizes subcategory and capitalizes first letter of change description' do
-      git_log = <<~COMMIT
-        title: Add LOGIN_TASK_LOG_LEVEL env var (#6037)
-        body:- Lets us set log level to minimize STDOUT output
-          from Identity::Hostdata (downloading files from S3, etc)
-
-        * changelog: Improvements, authentication, provide better authentication (LG-4515)
-        DELIMITER
-      COMMIT
-
+    it 'sorts changelog by subcategory' do
+      commits = [
+        git_fixtures['squashed_commit_with_one_commit'],
+        git_fixtures['squashed_commit_2'],
+      ]
+      git_log = commits.pluck('commit_log').join("\n")
       changelogs = generate_changelog(git_log)
+      formatted_changelog = format_changelog(changelogs)
 
-      expect(changelogs.first.subcategory).to eq('Authentication')
-      expect(changelogs.first.change).to start_with('P')
+      expect(formatted_changelog).to eq <<~CHANGELOG.chomp
+        ## Internal
+        - Logging: Update logging flow ([#9999](https://github.com/18F/identity-idp/pull/9999))
+        - Security: Upgrade Rails to patch vulnerability ([#6041](https://github.com/18F/identity-idp/pull/6041))
+      CHANGELOG
     end
   end
 
