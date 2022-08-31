9a3c3893f5929a6258a29a288fb35ea9339acf38
Tim Bradley <timothy.bradley@gsa.gov>
2022-08-31 12:36:47 -0700

    LG-6497: Fix i18n errors and add component documentation

--- app/components/memorable_date_component.rb
+++ app/components/memorable_date_component.rb
@@ -1,17 +1,40 @@
+##
+# Provides a component that accepts a date using the inputs specified
+# by USWDS here: https://designsystem.digital.gov/components/memorable-date/
+#
+# This treats the month, day, and year as nested fields to allow for various
+# uses of the component. Translate the fields in the controller (or equivalent)
+# if you need them to represent a single value.
+#
+# The errors for this component are configurable via +error_messages+, and you may
+# include custom min/max validations via +range_errors+.
 class MemorableDateComponent < BaseComponent
   attr_reader :name, :month, :day, :year, :required, :hint, :label, :form
 
   alias_method :f, :form
 
+  ##
+  # @param [String] name Field name for hash containing month, day, and year
+  # @param [String] hint Additional Hint to show to the user
+  # @param [String] label Label for field
+  # @param [String] form Form that this field belongs to
+  # @param [String] month (nil) Starting value for month
+  # @param [String] day (nil) Starting value for day
+  # @param [String] year (nil) Starting value for year
+  # @param [String] required (false) Whether this field is required
+  # @param [Date,DateTime] min (nil) Minimum allowed date, inclusive
+  # @param [Date,DateTime] max (nil) Maximum allowed date, inclusive
+  # @param [Hash<String,String>] error_messages Array of mappings of error states to messages
+  # @param [Array<Hash>] range_errors Array of custom range errors
+  # @option range_errors [Date,DateTime] min Minimum value for range check
+  # @option range_errors [Date,DateTime] max Maximum value for range check
+  # @option range_errors [String] message Error message to display if range check fails
   def initialize(
-    name:,
+    name:, hint:, label:, form:,
     month: nil,
     day: nil,
     year: nil,
     required: false,
-    hint:,
-    label:,
-    form:,
     min: nil,
     max: nil,
     error_messages: {},
@@ -33,9 +56,12 @@ class MemorableDateComponent < BaseComponent
     @range_errors = range_errors
   end
 
+  # Get error messages to be provided to the component.
+  # Includes both a hash lookup for general error messages
+  # and an array lookup for custom range error messages.
   def error_messages
     {
-      'error_messages' => generate_error_messages(label, min, max, @error_messages),
+      'error_messages' => generate_error_messages(label, @min, @max, @error_messages),
       'range_errors' => @range_errors.map do |err|
         new_err = {
           message: err[:message],
@@ -47,28 +73,32 @@ class MemorableDateComponent < BaseComponent
     }
   end
 
+  # Get min date as a string like 1892-01-23
   def min
     convert_date @min
   end
 
+  # Get max date as a string like 1892-01-23
   def max
     convert_date @max
   end
 
   private
 
-  # Helper function to allow usages to use Date objects directly
+  # Convert a Date or DateTime to a string like 1892-01-23
   def convert_date date
-    if date.instance_of? Date
-      # FIXME: Converted dates not parsed correctly in JS yet
-      I18n.l(date.to_time, format: :event_date)
-    elsif date.blank?
-      nil
-    else
-      date.to_s
+    date.strftime('%Y-%m-%d') if date.instance_of?(Date) || date.instance_of?(DateTime)
+  end
+
+  # Convert a date or DateTime to a long-form localized date string
+  def i18n_long_format date
+    if date.instance_of?(Date) || date.instance_of?(DateTime)
+      I18n.l(date, format: t('date.formats.long'))
     end
   end
 
+  # Configure default generic error messages for component,
+  # then integrate any overrides
   def generate_error_messages(label, min, max, override_error_messages)
     base_error_messages = {
       'missing_month_day_year' => t(
@@ -88,17 +118,28 @@ class MemorableDateComponent < BaseComponent
     }
     if label && min
       base_error_messages['range_underflow'] =
-        t('components.memorable_date.errors.range_underflow', label: label, date: min)
+        t(
+          'components.memorable_date.errors.range_underflow', label: label,
+                                                              date: i18n_long_format(min)
+        )
     end
 
     if label && max
       base_error_messages['range_overflow'] =
-        t('components.memorable_date.errors.range_overflow', label: label, date: max)
+        t(
+          'components.memorable_date.errors.range_overflow', label: label,
+                                                             date: i18n_long_format(max)
+        )
     end
 
     if label && min && max
       base_error_messages['outside_date_range'] =
-        t('components.memorable_date.errors.outside_date_range', label: label, min: min, max: max)
+        t(
+          'components.memorable_date.errors.outside_date_range',
+          label: label,
+          min: i18n_long_format(min),
+          max: i18n_long_format(max),
+        )
     end
 
     if override_error_messages
