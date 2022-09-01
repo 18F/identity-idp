07127c3c35c4d58409cf0d68a578b55fa7e60bf9
John Skiles Skinner <john.skinner@gsa.gov>
2022-09-01 17:10:43 -0500

    Lint

--- spec/services/proofing/lexis_nexis/ddp/response_redacter_spec.rb
+++ spec/services/proofing/lexis_nexis/ddp/response_redacter_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 describe Proofing::LexisNexis::Ddp::ResponseRedacter do
-
-  let(:json) { 
+  let(:json) do
     Proofing::LexisNexis::Ddp::ResponseRedacter.
       redact(sample_hash)
-  }
+  end
 
   describe 'self.redact' do
     context 'hash with unknown keys' do
-      let(:sample_hash) {
+      let(:sample_hash) do
         {
           'unknown_key' => 'dangerous data',
           'first_name' => 'unsafe first name',
           'ssn_hash' => 'unsafe ssn hash',
         }
-      }
+      end
       it 'redacts unknown keys' do
-        expect(json.values).to eq(['[redacted]']*3)
+        expect(json.values).to eq(['[redacted]'] * 3)
       end
       it 'keeps redacted keys' do
         expect(json.keys.length).to eq(3)
@@ -25,45 +24,43 @@ describe Proofing::LexisNexis::Ddp::ResponseRedacter do
     end
 
     context 'preserves known keys' do
-      let(:sample_hash) {
+      let(:sample_hash) do
         {
           'review_status' => 'safe value',
           'summary_risk_score' => 'safe value',
-          'fraudpoint.score' => 'safe value'
+          'fraudpoint.score' => 'safe value',
         }
-      }
+      end
       it 'redacts unknown keys' do
-        expect(json.values).to eq(['safe value']*3)
+        expect(json.values).to eq(['safe value'] * 3)
       end
     end
 
     context 'nil hash argument' do
-      let(:sample_hash) {
+      let(:sample_hash) do
         nil
-      }
+      end
       it 'produces an error about an empty body' do
         expect(json[:error]).to eq('TMx response body was empty')
       end
     end
 
     context 'mismatched data type argument' do
-      let(:sample_hash) {
+      let(:sample_hash) do
         Array.new
-      }
+      end
       it 'produces an error about malformed body' do
         expect(json[:error]).to eq('TMx response body was malformed')
       end
     end
 
     context 'empty hash agrument' do
-      let(:sample_hash) {
+      let(:sample_hash) do
         Hash.new
-      }
+      end
       it 'passes the empty hash onward' do
         expect(json).to eq(Hash.new)
       end
     end
-
   end
-
 end
