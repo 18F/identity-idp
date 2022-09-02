6960f262703dfbb757d6be11670cd98d7beae869
Kimball Bighorse <kbighorse@yahoo.com>
2022-09-02 10:48:14 -0700

    Rename response body parameter

--- spec/services/proofing/lexis_nexis/phone_finder/result_spec.rb
+++ spec/services/proofing/lexis_nexis/phone_finder/result_spec.rb
@@ -4,7 +4,7 @@ describe Proofing::LexisNexis::PhoneFinder::Result do
   let(:response_body) { LexisNexisFixtures.instant_verify_success_response_json }
   let(:response_status) { 200 }
   let(:http_response) do
-    Faraday::Response.new(body: response_body, status: response_status)
+    Faraday::Response.new(response_body: response_body, status: response_status)
   end
   let(:verification_response) do
     Proofing::LexisNexis::Response.new(http_response)
