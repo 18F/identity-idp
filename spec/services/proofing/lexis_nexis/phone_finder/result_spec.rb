9b800c101961122e4cd482bae6fdc2a4ec5b9ced
Kimball Bighorse <kbighorse@yahoo.com>
2022-09-02 10:31:49 -0700

    Test verified phone finder result

---
+++ spec/services/proofing/lexis_nexis/phone_finder/result_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Proofing::LexisNexis::PhoneFinder::Result do
+  let(:response_body) { LexisNexisFixtures.instant_verify_success_response_json }
+  let(:response_status) { 200 }
+  let(:http_response) do
+    Faraday::Response.new(body: response_body, status: response_status)
+  end
+  let(:verification_response) do
+    Proofing::LexisNexis::Response.new(http_response)
+  end
+
+  subject { described_class.new(verification_response) }
+
+  context 'successful response' do
+    it 'returns a successful verified result' do
+      expect(subject.success?).to eq(true)
+    end
+  end
+end
