b1a2e0dd2bcfbe5bc3c1e5e2a5dd1a829dc3592f
Kimball Bighorse <kbighorse@yahoo.com>
2022-09-06 10:12:25 -0700

    Test result elements separately

--- spec/jobs/address_proofing_job_spec.rb
+++ spec/jobs/address_proofing_job_spec.rb
@@ -78,14 +78,11 @@ RSpec.describe AddressProofingJob, type: :job do
 
         result = document_capture_session.load_proofing_result[:result]
 
-        expect(result).to eq(
-          exception: nil,
-          errors: {},
-          success: true,
-          timed_out: false,
-          transaction_id: conversation_id,
-          vendor_name: 'lexisnexis:phone_finder',
-        )
+        expect(result[:exception]).to be_nil
+        expect(result[:errors]).to eq({})
+        expect(result[:success]).to be true
+        expect(result[:timed_out]).to be false
+        expect(result[:vendor_name]).to eq('lexisnexis:phone_finder')
       end
 
       it 'adds cost data' do
