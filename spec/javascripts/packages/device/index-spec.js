7dab8e2c9277a7e52c8c211898876f18f824b72c
eric-gade <eric.gade@gsa.gov>
2022-09-06 17:22:36 -0400

    Fixing broken tests
    -- What
    Mocha evidently has some difficulty changing the writability of
    properties in before() functions when we are using the
    useDefineProperty helper. This fixes that by just declaring the
    properties we are concerned about as being writable at the top of the
    file.
    
    changelog: Improvements, Tablet Compatibility, updating frontend
    platform check tests

--- spec/javascripts/packages/device/index-spec.js
+++ spec/javascripts/packages/device/index-spec.js
@@ -4,161 +4,195 @@ import {
   isCameraCapableMobile,
   isIPad,
 } from '@18f/identity-device';
+
 import { useDefineProperty } from '@18f/identity-test-helpers';
+
+Object.defineProperty(navigator, 'userAgent', {
+  configurable: true,
+  writable: true,
+});
+Object.defineProperty(navigator, 'maxTouchPoints', {
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(navigator, 'mediaDevices', {
+  writable: true,
+  configurable: true,
+});
 
 describe('isIPad', () => {
   const defineProperty = useDefineProperty();
-  Object.defineProperty(navigator, 'userAgent', {
-    configurable: true,
-    writable: true,
-  });
-  Object.defineProperty(navigator, 'maxTouchPoints', {
-    configurable: true,
-    writable: true,
-  });
 
-  it('returns true if ipad is in the user agent string (old format)', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10',
+  context('iPad is in the user agent string (old format)', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10',
+      });
     });
 
-    expect(isIPad()).to.be.true();
+    it('returns true', () => {
+      expect(isIPad()).to.be.true();
+    });
   });
 
-  it('returns false if the user agent is Macintosh but with 0 maxTouchPoints', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+  context('The user agent is Macintosh but with 0 maxTouchPoints', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+      });
     });
 
-    expect(isIPad()).to.be.false();
+    it('returns false', () => {
+      expect(isIPad()).to.be.false();
+    });
   });
 
-  it('returns true if the user agent is Macintosh but with 5 maxTouchPoints', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+  context('The user agent is Macintosh but with 5 maxTouchPoints', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+      });
+      defineProperty(navigator, 'maxTouchPoints', { value: 5 });
     });
-    defineProperty(navigator, 'maxTouchPoints', { value: 5 });
 
-    expect(isIPad()).to.be.true();
+    it('returns true', () => {
+      expect(isIPad()).to.be.true();
+    });
   });
 
-  it('returns false for non-Apple userAgent, even with 5 maxTouchPoints', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.58 Mobile Safari/537.36',
+  context('Non-Apple userAgent, even with 5 maxTouchPoints', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.58 Mobile Safari/537.36',
+      });
+      defineProperty(navigator, 'maxTouchPoints', { value: 5 });
     });
-    defineProperty(navigator, 'maxTouchPoints', { value: 5 });
-
-    expect(isIPad()).to.be.false();
+    it('returns false', () => {
+      expect(isIPad()).to.be.false();
+    });
   });
 });
 
 describe('isLikelyMobile', () => {
   const defineProperty = useDefineProperty();
-  Object.defineProperty(navigator, 'userAgent', {
-    configurable: true,
-    writable: true,
-  });
-  Object.defineProperty(navigator, 'maxTouchPoints', {
-    configurable: true,
-    writable: true,
-  });
 
-  it('returns false if not mobile and has no touchpoints', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+  context('Is not mobile and has no touchpoints', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+        writable: true,
+        configurable: true,
+      });
+      defineProperty(navigator, 'maxTouchPoints', { value: 0 });
     });
-    defineProperty(navigator, 'maxTouchPoints', { value: 0 });
 
-    expect(isLikelyMobile()).to.be.false();
-  });
-
-  it('returns true if there is an Apple user agent and 5 maxTouchPoints', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+    it('returns false', () => {
+      expect(isLikelyMobile()).to.be.false();
     });
-    defineProperty(navigator, 'maxTouchPoints', { value: 5 });
-
-    expect(isLikelyMobile()).to.be.true();
   });
 
-  it('returns true if likely mobile', () => {
+  context('There is an Apple user agent and 5 maxTouchPoints', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+      });
+      defineProperty(navigator, 'maxTouchPoints', { value: 5 });
+    });
+
+    it('returns true', () => {
+      expect(isLikelyMobile()).to.be.true();
+    });
+  });
+
+  context('There is an explicit iPhone user agent', () => {
     defineProperty(navigator, 'userAgent', {
       value:
         'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
     });
 
-    expect(isLikelyMobile()).to.be.true();
+    it('returns true', () => {
+      expect(isLikelyMobile()).to.be.true();
+    });
   });
 });
 
 describe('hasMediaAccess', () => {
+  Object.defineProperty(navigator, 'mediaDevices', {
+    writable: true,
+    configurable: true,
+  });
   const defineProperty = useDefineProperty();
-  beforeEach(() => {
-    Object.defineProperty(navigator, 'mediaDevices', {
-      configurable: true,
-      writable: true,
+
+  context('No media device API access', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'mediaDevices', { value: undefined });
+    });
+
+    it('returns false', () => {
+      expect(hasMediaAccess()).to.be.false();
     });
   });
 
-  it('returns false if no media device API access', () => {
-    defineProperty(navigator, 'mediaDevices', { value: undefined });
+  it('Has media device API access', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'mediaDevices', { value: {} });
+    });
 
-    expect(hasMediaAccess()).to.be.false();
-  });
-
-  it('returns true if media device API access', () => {
-    navigator.mediaDevices = {};
-
-    expect(hasMediaAccess()).to.be.true();
+    it('returns true', () => {
+      expect(hasMediaAccess()).to.be.true();
+    });
   });
 });
 
 describe('isCameraCapableMobile', () => {
   const defineProperty = useDefineProperty();
-  beforeEach(() => {
-    Object.defineProperty(navigator, 'userAgent', {
-      configurable: true,
-      writable: true,
+
+  context('Is not mobile', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+      });
+      defineProperty(navigator, 'mediaDevices', { value: {} });
     });
-    Object.defineProperty(navigator, 'mediaDevices', {
-      configurable: true,
-      writable: true,
+
+    it('returns false', () => {
+      expect(isCameraCapableMobile()).to.be.false();
     });
   });
 
-  it('returns false if not mobile', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+  context('No media device API access', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+      });
+      defineProperty(navigator, 'mediaDevices', { value: undefined });
     });
-    defineProperty(navigator, 'mediaDevices', { value: {} });
 
-    expect(isCameraCapableMobile()).to.be.false();
+    it('returns false', () => {
+      expect(isCameraCapableMobile()).to.be.false();
+    });
   });
 
-  it('returns false if no media device API access', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+  context('Is likely mobile and media device API access', () => {
+    beforeEach(() => {
+      defineProperty(navigator, 'userAgent', {
+        value:
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+      });
+      defineProperty(navigator, 'mediaDevices', {});
     });
-    defineProperty(navigator, 'mediaDevices', { value: undefined });
 
-    expect(isCameraCapableMobile()).to.be.false();
-  });
-
-  it('returns true if likely mobile and media device API access', () => {
-    defineProperty(navigator, 'userAgent', {
-      value:
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+    it('returns true', () => {
+      expect(isCameraCapableMobile()).to.be.true();
     });
-    defineProperty(navigator, 'mediaDevices', {});
-
-    expect(isCameraCapableMobile()).to.be.true();
   });
 });
