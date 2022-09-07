7040ae8456d37adb1448958e082bf293fafade80
Gene M. Angelo, Jr <web.gma@gmail.com>
2022-09-07 11:26:59 -0400

    [LG-7244] Translate the getting started page (#6872)
    * Add Spanish locale file (es)
    
    changelog: Upcoming Features, Accessibility, LG-7244 Translate the getting started page
    
    * Add French locale file (fr)
    
    * Add specs to cover I18n
    
    Specifically, locale-specific rendered view content (:en, :fr, :es).

--- spec/views/idv/inherited_proofing/get_started.html.erb_spec.rb
+++ spec/views/idv/inherited_proofing/get_started.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'idv/inherited_proofing/get_started.html.erb' do
   let(:flow_session) { {} }
   let(:sp_name) { nil }
+  let(:locale) { nil }
 
   before do
     @decorated_session = instance_double(ServiceProviderSessionDecorator)
@@ -16,5 +17,37 @@ describe 'idv/inherited_proofing/get_started.html.erb' do
     render template: 'idv/inherited_proofing/get_started'
 
     expect(rendered).to have_button(t('inherited_proofing.buttons.continue'))
+  end
+
+  describe 'I18n' do
+    before do
+      view.locale = locale
+
+      render template: 'idv/inherited_proofing/get_started'
+    end
+
+    context 'when rendered using the default locale' do
+      let(:locale) { nil }
+
+      it 'renders the correct language' do
+        expect(rendered).to have_content('Get started verifying your identity')
+      end
+    end
+
+    context 'when rendered using the French (:fr) locale' do
+      let(:locale) { :fr }
+
+      it 'renders the correct language' do
+        expect(rendered).to have_content('Commencez à vérifier votre identité')
+      end
+    end
+
+    context 'when rendered using the Spanish (:es) locale' do
+      let(:locale) { :es }
+
+      it 'renders using the correct locale' do
+        expect(rendered).to have_content('Empiece con la verificación de su identidad')
+      end
+    end
   end
 end
