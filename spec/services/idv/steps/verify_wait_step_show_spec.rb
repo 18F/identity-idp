b79be169fdb7dcfb37cec5b47b0080bda771b8bc
Steve Urciuoli <steve.urciuoli@gsa.gov>
2022-09-07 11:34:33 -0500

    LG-7442 Add Threatmetrix Costing (#6917)

--- spec/services/idv/steps/verify_wait_step_show_spec.rb
+++ spec/services/idv/steps/verify_wait_step_show_spec.rb
@@ -6,6 +6,8 @@ describe Idv::Steps::VerifyWaitStepShow do
   let(:user) { build(:user) }
   let(:issuer) { 'test_issuer' }
   let(:service_provider) { build(:service_provider, issuer: issuer) }
+  let(:resolution_transaction_id) { Proofing::Mock::ResolutionMockClient::TRANSACTION_ID }
+  let(:threatmetrix_transaction_id) { Proofing::Mock::DdpMockClient::TRANSACTION_ID }
 
   let(:request) { FakeRequest.new }
 
@@ -25,7 +27,8 @@ describe Idv::Steps::VerifyWaitStepShow do
 
   let(:idv_result) do
     {
-      context: { stages: { resolution: {} } },
+      context: { stages: { resolution: { transaction_id: resolution_transaction_id },
+                           threatmetrix: { transaction_id: threatmetrix_transaction_id } } },
       errors: {},
       exception: nil,
       success: true,
@@ -72,11 +75,21 @@ describe Idv::Steps::VerifyWaitStepShow do
     end
 
     it 'adds costs' do
+      allow(IdentityConfig.store).to receive(:proofing_device_profiling_collecting_enabled).
+        and_return(true)
+      allow(IdentityConfig.store).to receive(:lexisnexis_threatmetrix_enabled).and_return(true)
+
       step.call
 
-      expect(SpCost.where(issuer: issuer).map(&:cost_type)).to contain_exactly(
-        'lexis_nexis_resolution',
-      )
+      sp_costs = SpCost.where(issuer: issuer, cost_type: 'lexis_nexis_resolution')
+      expect(sp_costs[0].transaction_id).to eq(resolution_transaction_id)
+      sp_costs = SpCost.where(issuer: issuer, cost_type: 'threatmetrix')
+      expect(sp_costs[0].transaction_id).to eq(threatmetrix_transaction_id)
+
+      proofing_cost = ProofingCost.last
+      expect(proofing_cost.user_id).to eq(user.id)
+      expect(proofing_cost.threatmetrix_count).to eq(1)
+      expect(proofing_cost.lexis_nexis_resolution_count).to eq(1)
     end
 
     it 'clears pii from session' do
