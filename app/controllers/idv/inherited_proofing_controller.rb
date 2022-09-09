25940ccb6aa410c4271af3df2f52f6a530927e8a
Rakesh <rakesh@Amivero-39.local>
2022-09-09 01:32:13 -0400

    more changes to consent page and controller

--- app/controllers/idv/inherited_proofing_controller.rb
+++ app/controllers/idv/inherited_proofing_controller.rb
@@ -1,6 +1,12 @@
 module Idv
   class InheritedProofingController < ApplicationController
+
+    before_action :confirm_two_factor_authenticated
+    before_action :redirect_if_pending_profile
+
+    include IdvSession
     include Flow::FlowStateMachine
+    include Idv::ThreatMetrixConcern
 
     before_action :render_404_if_disabled
 
@@ -19,6 +25,12 @@ module Idv
 
     def render_404_if_disabled
       render_not_found unless IdentityConfig.store.inherited_proofing_enabled
+    end
+
+    def redirect_if_pending_profile
+      return if sp_session[:ial2_strict] &&
+                !IdentityConfig.store.gpo_allowed_for_strict_ial2
+      redirect_to idv_gpo_verify_url if current_user.decorate.pending_profile_requires_verification?
     end
   end
 end
