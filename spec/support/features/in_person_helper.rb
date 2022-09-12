e94bf230edb98136dcbe96561879e248b5140dfa
Tim Bradley <90272033+NavaTim@users.noreply.github.com>
2022-09-12 10:51:39 -0700

    LG-6497: Revert Memorable Date Changes (#6940)
    * Revert "LG-6497: Minor style/doc fixes related to memorable date change (#6929)"
    
    This reverts commit e0e8ad4c5a042aae4262671920a5ddb02b9c6808.
    
    * Revert "LG-6497: Create and use new Memorable Date component in State ID step of IPP flow (#6713)"
    
    This reverts commit cead2c1d73848323e4371e9dda7df6fa2a94ca22.
    
    * [skip changelog]

--- spec/support/features/in_person_helper.rb
+++ spec/support/features/in_person_helper.rb
@@ -21,9 +21,7 @@ module InPersonHelper
     fill_in t('in_person_proofing.form.state_id.first_name'), with: GOOD_FIRST_NAME
     fill_in t('in_person_proofing.form.state_id.last_name'), with: GOOD_LAST_NAME
     year, month, day = GOOD_DOB.split('-')
-    fill_in t('components.memorable_date.month'), with: month
-    fill_in t('components.memorable_date.day'), with: day
-    fill_in t('components.memorable_date.year'), with: year
+    fill_in t('in_person_proofing.form.state_id.dob'), with: [month, day, year].join('-')
     select GOOD_STATE_ID_JURISDICTION,
            from: t('in_person_proofing.form.state_id.state_id_jurisdiction')
     fill_in t('in_person_proofing.form.state_id.state_id_number'), with: GOOD_STATE_ID_NUMBER
