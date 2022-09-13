29e1e20aa4f1d73d7f48a0097d5ae313ed220784
rakesh nagilla <rakesh.nagilla@gsa.gov>
2022-09-12 22:15:53 -0400

    comment unused methods

--- app/controllers/idv/inherited_proofing_controller.rb
+++ app/controllers/idv/inherited_proofing_controller.rb
@@ -3,9 +3,9 @@ module Idv
     # before_action :confirm_two_factor_authenticated
     # before_action :redirect_if_pending_profile
 
-    include IdvSession
+    # include IdvSession
     include Flow::FlowStateMachine
-    include Idv::ThreatMetrixConcern
+    # include Idv::ThreatMetrixConcern
 
     before_action :render_404_if_disabled
 
@@ -26,10 +26,10 @@ module Idv
       render_not_found unless IdentityConfig.store.inherited_proofing_enabled
     end
 
-    def redirect_if_pending_profile
-      return if sp_session[:ial2_strict] &&
-                !IdentityConfig.store.gpo_allowed_for_strict_ial2
-      redirect_to idv_gpo_verify_url if current_user.decorate.pending_profile_requires_verification?
-    end
+    # def redirect_if_pending_profile
+    #   return if sp_session[:ial2_strict] &&
+    #             !IdentityConfig.store.gpo_allowed_for_strict_ial2
+    #   redirect_to idv_gpo_verify_url if current_user.decorate.pending_profile_requires_verification?
+    # end
   end
 end
