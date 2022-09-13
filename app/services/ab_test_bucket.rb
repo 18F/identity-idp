90a47bb4c3a9040558e073a37fe57bfe15df8f57
Douglas Price <douglas.price@gsa.gov>
2022-09-13 15:32:15 -0400

    flesh out AbTestBucket

--- app/services/ab_test_bucket.rb
+++ app/services/ab_test_bucket.rb
@@ -5,8 +5,10 @@ class AbTestBucket
 
   validate :within_100_percent
 
-  def initialize(buckets: [{ default: 100 }])
+  def initialize(buckets: { default: 100 })
     @buckets = buckets
+    raise 'invalid bucket data structure' unless valid_bucket_data_structure?
+    ensure_numeric_percentages
   end
 
   def bucket(discriminator: nil)
@@ -17,9 +19,9 @@ class AbTestBucket
     user_value = Digest::SHA256.hexdigest(discriminator).to_i(16).to_f / max_sha * 100
 
     min = 0
-    buckets.each do |bucket|
-      max = min + bucket.values.first
-      return bucket.keys.first if user_value > min && user_value <= max
+    buckets.keys.each do |key|
+      max = min + buckets[key]
+      return key if user_value > min && user_value <= max
       min = max
     end
 
@@ -28,17 +30,32 @@ class AbTestBucket
 
   private
 
+  def valid_bucket_data_structure?
+    hash_bucket = buckets.is_a?(Hash)
+    simple_values = true
+    if hash_bucket
+      buckets.values.each do |v|
+        next unless v.is_a?(Hash) || v.is_a?(Array)
+        simple_values = false
+      end
+    end
+
+    hash_bucket && simple_values
+  end
+
+  def ensure_numeric_percentages
+    buckets.keys.each do |key|
+      buckets[key] = buckets[key].to_f if buckets[key].is_a?(String)
+    end
+  end
+
   def within_100_percent
-    return if buckets_percentage_sum <= 100
+    return if valid_bucket_data_structure? && buckets.values.sum <= 100
 
     errors.add(
       :buckets,
       'bucket percentages sum is greater than 100',
       type: :ab_test_configuration,
     )
-  end
-
-  def buckets_percentage_sum
-    buckets.map(&:values).flatten.sum(0)
   end
 end
--- spec/services/ab_test_bucket_spec.rb
+++ spec/services/ab_test_bucket_spec.rb
@@ -7,7 +7,7 @@ describe AbTestBucket do
   let(:default_percent) { 10 }
   let(:acceptable_delta) { 5 }
   let(:subject) do
-    AbTestBucket.new(buckets: [{ foo: foo_percent }, { bar: bar_percent }, { baz: baz_percent }])
+    AbTestBucket.new(buckets: { foo: foo_percent, bar: bar_percent, baz: baz_percent })
   end
 
   context 'configured with buckets adding up to less than 100 percent' do
@@ -18,12 +18,15 @@ describe AbTestBucket do
         results[bucket] = (results[bucket] || 0) + 1
       end
 
-      expect(results[:foo].to_f/10).to be_within(acceptable_delta).of(foo_percent)
-      expect(results[:bar].to_f/10).to be_within(acceptable_delta).of(bar_percent)
-      expect(results[:baz].to_f/10).to be_within(acceptable_delta).of(baz_percent)
-      expect(results[:default].to_f/10).to be_within(acceptable_delta).of(default_percent)
+      expect(results[:foo].to_f / 10).to be_within(acceptable_delta).of(foo_percent)
+      expect(results[:bar].to_f / 10).to be_within(acceptable_delta).of(bar_percent)
+      expect(results[:baz].to_f / 10).to be_within(acceptable_delta).of(baz_percent)
+      expect(results[:default].to_f / 10).to be_within(acceptable_delta).of(default_percent)
     end
 
+    # A slow test that runs the above 10000 times and reports the number of times it fails to pass
+    # due to the random nature of the test. With acceptable_delta == 5, the above spec succeeded
+    # 99.78% of the time.
     xit 'succeeds a LOT' do
       successes = 0
       failures = 0
@@ -34,15 +37,15 @@ describe AbTestBucket do
           bucket = subject.bucket(discriminator: SecureRandom.uuid)
           results[bucket] = (results[bucket] || 0) + 1
         end
-        foo_delta = (results[:foo].to_f/10 - foo_percent).abs
-        bar_delta = (results[:bar].to_f/10 - bar_percent).abs
-        baz_delta = (results[:baz].to_f/10 - baz_percent).abs
-        default_delta = (results[:default].to_f/10 - default_percent).abs
+        foo_delta = (results[:foo].to_f / 10 - foo_percent).abs
+        bar_delta = (results[:bar].to_f / 10 - bar_percent).abs
+        baz_delta = (results[:baz].to_f / 10 - baz_percent).abs
+        default_delta = (results[:default].to_f / 10 - default_percent).abs
 
         if ((foo_delta < acceptable_delta) &&
           (bar_delta < acceptable_delta) &&
           (baz_delta < acceptable_delta) &&
-          (default_delta < acceptable_delta) )
+          (default_delta < acceptable_delta))
           successes += 1
         else
           failures += 1
@@ -54,18 +57,18 @@ describe AbTestBucket do
     end
   end
 
-  context 'configured with buckets adding up to less exactly 100 percent' do
-    let(:baz_percent) { 50 }
-    it 'splits random uuids into the buckets to within an acceptable delta percent' do
+  context 'configured with buckets adding up to exactly 100 percent' do
+    let(:subject) do
+      AbTestBucket.new(buckets: { foo: 20, bar: 30, baz: 50 })
+    end
+
+    it 'divides random uuids into the buckets with no automatic default' do
       results = {}
       1000.times do
         bucket = subject.bucket(discriminator: SecureRandom.uuid)
         results[bucket] = (results[bucket] || 0) + 1
       end
 
-      expect(results[:foo].to_f/10).to be_within(acceptable_delta).of(foo_percent)
-      expect(results[:bar].to_f/10).to be_within(acceptable_delta).of(bar_percent)
-      expect(results[:baz].to_f/10).to be_within(acceptable_delta).of(baz_percent)
       expect(results[:default]).to be_nil
     end
   end
@@ -87,6 +90,34 @@ describe AbTestBucket do
       bucket = subject.bucket(discriminator: SecureRandom.uuid)
 
       expect(bucket).to eq :misconfigured
+    end
+  end
+
+  context 'configured with buckets with string percentages' do
+    let(:subject) { AbTestBucket.new(buckets: { foo: '100' }) }
+
+    it 'converts string percentages to numbers and returns the correct result' do
+      bucket = subject.bucket(discriminator: SecureRandom.uuid)
+
+      expect(bucket).to eq :foo
+    end
+  end
+
+  context 'configured with buckets with random strings' do
+    let(:subject) { AbTestBucket.new(buckets: { foo: 'foo', bar: 'bar' }) }
+
+    it 'converts string to zero percent and returns :default' do
+      bucket = subject.bucket(discriminator: SecureRandom.uuid)
+
+      expect(bucket).to eq :default
+    end
+  end
+
+  context 'misconfigured with buckets in the wrong data structure' do
+    let(:subject) { AbTestBucket.new(buckets: [[:foo, 10], [:bar, 20]]) }
+
+    it 'raises a RuntimeError' do
+      expect { subject }.to raise_error(RuntimeError, 'invalid bucket data structure')
     end
   end
 end
