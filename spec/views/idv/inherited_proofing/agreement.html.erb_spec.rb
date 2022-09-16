3ff65278bd1369cbd066be420fc92c5a472a73ca
rakesh nagilla <rakesh.nagilla@gsa.gov>
2022-09-16 01:47:00 -0400

    more view specs

---
+++ spec/views/idv/inherited_proofing/agreement.html.erb_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+describe 'idv/inherited_proofing/agreement.html.erb' do
+  let(:flow_session) { {} }
+  let(:sp_name) { nil }
+  let(:locale) { nil }
+
+  before do
+    @decorated_session = instance_double(ServiceProviderSessionDecorator)
+    allow(@decorated_session).to receive(:sp_name).and_return(sp_name)
+    allow(view).to receive(:decorated_session).and_return(@decorated_session)
+    allow(view).to receive(:flow_session).and_return(flow_session)
+    allow(view).to receive(:url_for).and_return('https://www.example.com/')
+  end
+
+  it 'renders the Continue button' do
+    render template: 'idv/inherited_proofing/agreement'
+
+    expect(rendered).to have_button(t('inherited_proofing.buttons.continue'))
+  end
+
+  describe 'I18n' do
+    before do
+      view.locale = locale
+
+      render template: 'idv/inherited_proofing/agreement'
+    end
+
+    context 'when rendered using the French (:fr) locale' do
+      let(:locale) { :fr }
+
+      it 'renders the correct language' do
+        expect(rendered).to have_content('to be implemented')
+      end
+    end
+
+    context 'when rendered using the Spanish (:es) locale' do
+      let(:locale) { :es }
+
+      it 'renders using the correct locale' do
+        expect(rendered).to have_content('to be implemented')
+      end
+    end
+
+    context 'without service provider' do
+      it 'renders content' do
+        render template: 'idv/inherited_proofing/agreement'
+
+        expect(rendered).to have_content(t('inherited_proofing.info.lets_go'))
+        expect(rendered).to have_content(
+          t('inherited_proofing.headings.verify_identity'),
+        )
+      end
+    end
+
+    context 'with service provider' do
+      let(:sp_name) { 'Example App' }
+
+      it 'renders content' do
+        render template: 'idv/inherited_proofing/agreement'
+
+        expect(rendered).to have_content(t('inherited_proofing.info.verify_identity'))
+        expect(rendered).to have_content(
+          t('inherited_proofing.headings.secure_account'),
+        )
+        expect(rendered).to have_content(
+          t('inherited_proofing.info.secure_account', sp_name: sp_name),
+        )
+      end
+    end
+  end
+end
