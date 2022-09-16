7aba3eb78287e3a99f8ec823c82c26499e6fd413
eric-gade <eric.gade@gsa.gov>
2022-09-16 14:50:38 -0400

    Updating icon image dimensions in templates
    changelog: Internal, Assets, updating icon images and references

--- app/javascript/packages/components/status-page.tsx
+++ app/javascript/packages/components/status-page.tsx
@@ -86,7 +86,7 @@ function StatusPage({
 
   return (
     <>
-      <img src={src} width={54} height={54} alt={alt} className="display-block margin-bottom-4" />
+      <img src={src} width={88} height={88} alt={alt} className="display-block margin-bottom-4" />
       <PageHeading>{header}</PageHeading>
       {children}
       {actionButtons.length > 0 && (
