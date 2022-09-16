c07014d8288a7610a464a4350515ceeec9006f45
rakesh nagilla <rakesh.nagilla@gsa.gov>
2022-09-16 16:30:40 -0400

    more pr comments on fixing specs

--- spec/views/idv/inherited_proofing/agreement.html.erb_spec.rb
+++ spec/views/idv/inherited_proofing/agreement.html.erb_spec.rb
@@ -17,45 +17,21 @@ describe 'idv/inherited_proofing/agreement.html.erb' do
     expect(rendered).to have_button(t('inherited_proofing.buttons.continue'))
   end
 
-  describe 'I18n' do
-    before do
-      view.locale = locale
-
+  context 'with or without service provider' do
+    it 'renders content' do
       render template: 'idv/inherited_proofing/agreement'
-    end
 
-    context 'when rendered using the French (:fr) locale' do
-      let(:locale) { :fr }
-
-      it 'renders the correct language' do
-        expect(rendered).to have_content('to be implemented')
-      end
-    end
-
-    context 'when rendered using the Spanish (:es) locale' do
-      let(:locale) { :es }
-
-      it 'renders using the correct locale' do
-        expect(rendered).to have_content('to be implemented')
-      end
-    end
-
-    context 'with or without service provider' do
-      it 'renders content' do
-        render template: 'idv/inherited_proofing/agreement'
-
-        expect(rendered).to have_content(t('inherited_proofing.info.lets_go'))
-        expect(rendered).to have_content(
-          t('inherited_proofing.headings.verify_identity'),
-        )
-        expect(rendered).to have_content(t('inherited_proofing.info.verify_identity'))
-        expect(rendered).to have_content(
-          t('inherited_proofing.headings.secure_account'),
-        )
-        expect(rendered).to have_content(
-          t('inherited_proofing.info.secure_account', sp_name: sp_name),
-        )
-      end
+      expect(rendered).to have_content(t('inherited_proofing.info.lets_go'))
+      expect(rendered).to have_content(
+        t('inherited_proofing.headings.verify_identity'),
+      )
+      expect(rendered).to have_content(t('inherited_proofing.info.verify_identity'))
+      expect(rendered).to have_content(
+        t('inherited_proofing.headings.secure_account'),
+      )
+      expect(rendered).to have_content(
+        t('inherited_proofing.info.secure_account', sp_name: sp_name),
+      )
     end
   end
 end
--- spec/views/idv/inherited_proofing/get_started.html.erb_spec.rb
+++ spec/views/idv/inherited_proofing/get_started.html.erb_spec.rb
@@ -26,31 +26,7 @@ describe 'idv/inherited_proofing/get_started.html.erb' do
       render template: 'idv/inherited_proofing/get_started'
     end
 
-    context 'when rendered using the default locale' do
-      let(:locale) { nil }
-
-      it 'renders the correct language' do
-        expect(rendered).to have_content('Get started verifying your identity')
-      end
-    end
-
-    context 'when rendered using the French (:fr) locale' do
-      let(:locale) { :fr }
-
-      it 'renders the correct language' do
-        expect(rendered).to have_content('Commencez à vérifier votre identité')
-      end
-    end
-
-    context 'when rendered using the Spanish (:es) locale' do
-      let(:locale) { :es }
-
-      it 'renders using the correct locale' do
-        expect(rendered).to have_content('Empiece con la verificación de su identidad')
-      end
-    end
-
-    context 'without service provider' do
+    context 'with or without service provider' do
       it 'renders troubleshooting options' do
         render template: 'idv/inherited_proofing/get_started'
 
@@ -59,15 +35,6 @@ describe 'idv/inherited_proofing/get_started.html.erb' do
           t('inherited_proofing.troubleshooting.options.learn_more_phone_or_mail'),
         )
         expect(rendered).not_to have_link(nil, href: idv_inherited_proofing_return_to_sp_path)
-      end
-    end
-
-    context 'with service provider' do
-      let(:sp_name) { 'Example App' }
-
-      it 'renders troubleshooting options' do
-        render template: 'idv/inherited_proofing/get_started'
-
         expect(rendered).to have_link(t('inherited_proofing.troubleshooting.options.get_va_help'))
         expect(rendered).to have_link(
           t('inherited_proofing.troubleshooting.options.learn_more_phone_or_mail'),
