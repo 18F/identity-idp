3deb81542f06a11008d684153ce664145bfffd9e
Andrew Duthie <andrew.duthie@gsa.gov>
2022-09-21 11:33:00 -0400

    Use legacy personal key implementation for account dashboard
    **Why**:
    
    - Since the React implementation is being discontinued and slated for removal (LG-7386)
    - To avoid feature drift
    - To ensure consistency of personal key confirmation feature flag (#6821 / LG-7162)
    
    Effectively reverts #6453
    
    [skip changelog]

--- spec/views/shared/_personal_key.html.erb_spec.rb
+++ spec/views/shared/_personal_key.html.erb_spec.rb
@@ -4,6 +4,8 @@ require 'data_uri'
 RSpec.describe 'shared/_personal_key.html.erb' do
   let(:personal_key) { RandomPhrase.new(num_words: 4).to_s }
 
+  subject(:rendered) { render 'shared/personal_key', code: personal_key, update_path: '/test' }
+
   describe 'download link' do
     around do |ex|
       # data_uri depends on URI.decode which was removed in Ruby 3.0 :sob:
@@ -19,14 +21,37 @@ RSpec.describe 'shared/_personal_key.html.erb' do
     end
 
     it 'has the download attribute and a data: url for the personal key' do
-      render 'shared/personal_key', code: personal_key, update_path: '/test'
-
       doc = Nokogiri::HTML(rendered)
       download_link = doc.at_css('a[download]')
       data_uri = URI::Data.new(download_link[:href])
 
       expect(data_uri.content_type).to eq('text/plain')
       expect(data_uri.data).to eq(personal_key)
+    end
+  end
+
+  describe 'continue button' do
+    let(:idv_personal_key_confirmation_enabled) { nil }
+
+    before do
+      allow(FeatureManagement).to receive(:idv_personal_key_confirmation_enabled?).
+        and_return(idv_personal_key_confirmation_enabled)
+    end
+
+    context 'without idv personal key confirmation' do
+      let(:idv_personal_key_confirmation_enabled) { false }
+
+      it 'renders button with [data-toggle="skip"]' do
+        expect(rendered).to have_css('[data-toggle="skip"]')
+      end
+    end
+
+    context 'with idv personal key confirmation' do
+      let(:idv_personal_key_confirmation_enabled) { true }
+
+      it 'renders button with [data-toggle="modal"]' do
+        expect(rendered).to have_css('[data-toggle="modal"]')
+      end
     end
   end
 end
