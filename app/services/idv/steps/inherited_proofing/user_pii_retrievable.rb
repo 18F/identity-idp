79b5371b31c53b3d2555145abc32ee6a1a1ddef5
gangelo <web.gma@gmail.com>
2022-09-23 04:44:23 -0400

    Add mixin to obtain user pii needed
    In order to proof our Inherited Proofing user:
    
    NOTE: the use of InheritedProofingConcern #inherited_proofing_service,
    members via its inclusion in the InheritedProofingController.
    
    Add delegations needed for conveinient access to controller and idv_session
    from InheritedProofingBaseStep so that we have these in any Flow Step
    we need them.

---
+++ app/services/idv/steps/inherited_proofing/user_pii_retrievable.rb
@@ -0,0 +1,43 @@
+module Idv
+  module Steps
+    module InheritedProofing
+      module UserPiiRetrievable
+        def inherited_proofing_user_pii
+          inherited_proofing_info[0]
+        end
+
+        def inherited_proofing_form_response
+          inherited_proofing_info[1]
+        end
+
+        private
+
+        # This needs error handling.
+        def inherited_proofing_info
+          return @inherited_proofing_info if defined? @inherited_proofing_info
+
+          payload_hash = inherited_proofing_service.execute.dup
+          form = inherited_proofing_form(payload_hash)
+          form_response = form.submit
+
+          user_pii = {}
+          user_pii = form.user_pii if form_response.success?
+
+          @inherited_proofing_info = [user_pii, form_response]
+        end
+
+        def inherited_proofing_service
+          controller.inherited_proofing_service
+        end
+
+        def inherited_proofing_form(payload_hash)
+          controller.inherited_proofing_form payload_hash
+        end
+
+        def inherited_proofing_service_provider_data
+          controller.inherited_proofing_service_provider_data
+        end
+      end
+    end
+  end
+end
