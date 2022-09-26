b4d2f9ae6d3d0fbeb12fa68f57f620855a8bc374
Zach Margolis <zachary.margolis@gsa.gov>
2022-09-26 10:19:47 -0700

    yarn lint

--- app/javascript/packs/mock-device-profiling.tsx
+++ app/javascript/packs/mock-device-profiling.tsx
@@ -69,7 +69,8 @@ function MockDeviceProfilingOptions() {
       const randomChaosOption = CHAOS_OPTIONS[Math.floor(Math.random() * CHAOS_OPTIONS.length)];
       randomChaosOption.apply();
       return randomChaosOption.undo;
-    } else if (selectedValue) {
+    }
+    if (selectedValue) {
       submitMockFraudResult({ result: selectedValue, sessionId: loadSessionId() });
     }
   }, [selectedValue]);
