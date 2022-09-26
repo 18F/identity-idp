f43f000bc5f0fb5df9df9281417bab8f7657fc31
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-09-26 10:55:00 -0700

    Add mock ThreatMetrix Javascript implementation (LG-7174) (#7018)
    * Add feature spec that makes sure mock JS works
    * Enable for all proofing in development by default
    * Use currentScript to avoid scraping DOM
    * Use react effects, hooks
    * Update ddp mock proofer
      - no more magic SSNs, only results from redis
    
    changelog: Internal, Device Profiling, Add ability to select mock device profiling response from UI
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- spec/features/idv/doc_auth/ssn_step_spec.rb
+++ spec/features/idv/doc_auth/ssn_step_spec.rb
@@ -7,6 +7,9 @@ feature 'doc auth ssn step', :js do
 
   context 'desktop' do
     before do
+      allow(IdentityConfig.store).
+        to receive(:no_sp_device_profiling_enabled).and_return(true)
+
       sign_in_and_2fa_user
       complete_doc_auth_steps_before_ssn_step
     end
@@ -20,10 +23,20 @@ feature 'doc auth ssn step', :js do
 
     it 'proceeds to the next page with valid info' do
       fill_out_ssn_form_ok
+
+      match = page.body.match(/session_id=(?<session_id>[^"&]+)/)
+      session_id = match && match[:session_id]
+      expect(session_id).to be_present
+
+      select 'Review', from: 'mock_profiling_result'
+
       expect(page.find_field(t('idv.form.ssn_label_html'))['aria-invalid']).to eq('false')
       click_idv_continue
 
       expect(page).to have_current_path(idv_doc_auth_verify_step)
+
+      profiling_result = Proofing::Mock::DeviceProfilingBackend.new.profiling_result(session_id)
+      expect(profiling_result).to eq('review')
     end
 
     it 'does not proceed to the next page with invalid info' do
--- spec/services/proofing/mock/ddp_mock_client_spec.rb
+++ spec/services/proofing/mock/ddp_mock_client_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Proofing::Mock::DdpMockClient do
+  around do |ex|
+    REDIS_POOL.with { |namespaced| namespaced.redis.flushdb }
+    ex.run
+    REDIS_POOL.with { |namespaced| namespaced.redis.flushdb }
+  end
+
+  let(:threatmetrix_session_id) { SecureRandom.uuid }
+
   let(:applicant) do
     Idp::Constants::MOCK_IDV_APPLICANT_WITH_SSN.merge(
-      threatmetrix_session_id: 'ABCD-1234',
+      threatmetrix_session_id: threatmetrix_session_id,
       request_ip: Faker::Internet.ip_v4_address,
     )
   end
@@ -18,28 +26,37 @@ RSpec.describe Proofing::Mock::DdpMockClient do
   describe '#proof' do
     subject(:result) { instance.proof(applicant) }
 
-    it 'passes by default' do
-      expect(result.review_status).to eq('pass')
+    before do
+      Proofing::Mock::DeviceProfilingBackend.new.record_profiling_result(
+        result: redis_result,
+        session_id: threatmetrix_session_id,
+      )
     end
 
-    context 'with magic "reject" SSN' do
-      let(:applicant) { super().merge(ssn: '666-77-8888') }
-      it 'fails' do
-        expect(result.review_status).to eq('reject')
-      end
-    end
+    context 'with explicit no_result' do
+      let(:redis_result) { 'no_result' }
 
-    context 'with magic "review" SSN' do
-      let(:applicant) { super().merge(ssn: '666-77-9999') }
-      it 'fails' do
-        expect(result.review_status).to eq('review')
-      end
-    end
-
-    context 'with magic "nil" SSN' do
-      let(:applicant) { super().merge(ssn: '666-77-0000') }
-      it 'fails' do
+      it 'has a nil review status' do
         expect(result.review_status).to be_nil
+        expect(result.response_body['review_status']).to be_nil
+      end
+    end
+
+    context 'with reject' do
+      let(:redis_result) { 'reject' }
+
+      it 'has a reject status' do
+        expect(result.review_status).to eq('reject')
+        expect(result.response_body['review_status']).to eq('reject')
+      end
+    end
+
+    context 'with pass' do
+      let(:redis_result) { 'pass' }
+
+      it 'has a pass status' do
+        expect(result.review_status).to eq('pass')
+        expect(result.response_body['review_status']).to eq('pass')
       end
     end
   end
