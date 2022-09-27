77529d5ffc27deab0264ae82b5aa8803d7f45136
Kimball Bighorse <kbighorse@yahoo.com>
2022-09-26 17:51:36 -0700

    Create `Proofing::Aamva::ResultWithException`

---
+++ app/services/proofing/aamva/result_with_exception.rb
@@ -0,0 +1,34 @@
+module Proofing
+  module Aamva
+    class ResultWithException
+      attr_reader :exception, :vendor_name
+
+      def initialize(exception, vendor_name:)
+        @exception = exception
+        @vendor_name = vendor_name
+      end
+
+      def success?
+        false
+      end
+
+      def errors
+        {}
+      end
+
+      def timed_out?
+        exception.is_a?(Proofing::TimeoutError)
+      end
+
+      def to_h
+        {
+          success: success?,
+          errors: errors,
+          exception: exception,
+          timed_out: timed_out?,
+          vendor_name: vendor_name,
+        }
+      end
+    end
+  end
+end
