afc984ac837dec6ebba4e011c61bc29a9562b603
Melissa Miller <melissa.miller@gsa.gov>
2022-09-29 19:43:59 -0400

    Clean up specs

--- spec/features/idv/inherited_proofing/verify_info_step_spec.rb
+++ spec/features/idv/inherited_proofing/verify_info_step_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'inherited proofing verify info' do
   include IdvHelper
   include DocAuthHelper
+  include_context 'va_user_context'
 
   before do
     allow(IdentityConfig.store).to receive(:va_inherited_proofing_mock_enabled).and_return true
@@ -14,57 +15,30 @@ feature 'inherited proofing verify info' do
 
   let(:auth_code) { Idv::InheritedProofing::Va::Mocks::Service::VALID_AUTH_CODE }
 
-  def expect_ip_verify_info_step
-    expect(page).to have_current_path(idv_ip_verify_info_step)
+  before do
+    sign_in_and_2fa_user
+    complete_inherited_proofing_steps_before_verify_step
   end
 
-  # def expect_inherited_proofing_first_step
-  #   expect(page).to have_current_path(idv_inherited_proofing_agreement_step)
-  # end
-
-  context 'when JS is enabled', :js do
-    before do
-      sign_in_and_2fa_user
-      complete_inherited_proofing_steps_before_verify_step
-    end
-
-    it "does something" do
-      # expect_ip_verify_info_step
-      save_and_open_page
-    end
-
-    # it 'shows an inline error if the user clicks continue without giving consent' do
-    #   click_continue
-    #   expect_inherited_proofing_first_step
-    #   expect(page).to have_content(t('forms.validation.required_checkbox'))
-    # end
-
-    # it 'allows the user to continue after checking the checkbox' do
-    #   check t('inherited_proofing.instructions.consent', app_name: APP_NAME)
-    #   click_continue
-
-    #   expect_ip_verify_info_step
-    # end
+  it "displays the user's personal information" do
+    expect(page).to have_text user_attributes[:first_name]
+    expect(page).to have_text user_attributes[:last_name]
+    expect(page).to have_text user_attributes[:birth_date]
   end
 
-  # context 'when JS is disabled' do
-  #   before do
-  #     sign_in_and_2fa_user
-  #     complete_inherited_proofing_steps_before_agreement_step
-  #   end
+  it "displays the user's address" do
+    expect(page).to have_text user_attributes[:address][:street]
+    expect(page).to have_text user_attributes[:address][:city]
+    expect(page).to have_text user_attributes[:address][:state]
+    expect(page).to have_text user_attributes[:address][:zip]
+  end
 
-  #   it 'shows the notice if the user clicks continue without giving consent' do
-  #     click_continue
+  it "obfuscates the user's ssn" do
+    expect(page).to have_text '1**-**-***9'
+  end
 
-  #     expect_inherited_proofing_first_step
-  #     expect(page).to have_content(t('errors.doc_auth.consent_form'))
-  #   end
-
-  #   it 'allows the user to continue after checking the checkbox' do
-  #     check t('inherited_proofing.instructions.consent', app_name: APP_NAME)
-  #     click_continue
-
-  #     expect_ip_verify_info_step
-  #   end
-  # end
+  it "can display the user's ssn when selected" do
+    check 'Show Social Security number'
+    expect(page).to have_text '123-45-6789'
+  end
 end
