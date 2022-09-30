42d11dfb6b94d45e340f761b925f9ec51afc6684
Melissa Miller <melissa.miller@gsa.gov>
2022-09-30 07:28:06 -0400

    Further clean up

--- spec/features/idv/inherited_proofing/verify_info_step_spec.rb
+++ spec/features/idv/inherited_proofing/verify_info_step_spec.rb
@@ -20,25 +20,38 @@ feature 'inherited proofing verify info' do
     complete_inherited_proofing_steps_before_verify_step
   end
 
-  it "displays the user's personal information" do
-    expect(page).to have_text user_attributes[:first_name]
-    expect(page).to have_text user_attributes[:last_name]
-    expect(page).to have_text user_attributes[:birth_date]
+  describe 'page content' do
+    it 'renders the Continue button' do
+      expect(page).to have_button(t('inherited_proofing.buttons.continue'))
+    end
+
+    it 'renders content' do
+      expect(page).to have_content(t('inherited_proofing.headings.verify_information'))
+      expect(page).to have_link(t('inherited_proofing.troubleshooting.options.get_va_help'))
+    end
   end
 
-  it "displays the user's address" do
-    expect(page).to have_text user_attributes[:address][:street]
-    expect(page).to have_text user_attributes[:address][:city]
-    expect(page).to have_text user_attributes[:address][:state]
-    expect(page).to have_text user_attributes[:address][:zip]
-  end
+  describe 'user info' do
+    it "displays the user's personal information" do
+      expect(page).to have_text user_attributes[:first_name]
+      expect(page).to have_text user_attributes[:last_name]
+      expect(page).to have_text user_attributes[:birth_date]
+    end
 
-  it "obfuscates the user's ssn" do
-    expect(page).to have_text '1**-**-***9'
-  end
+    it "displays the user's address" do
+      expect(page).to have_text user_attributes[:address][:street]
+      expect(page).to have_text user_attributes[:address][:city]
+      expect(page).to have_text user_attributes[:address][:state]
+      expect(page).to have_text user_attributes[:address][:zip]
+    end
 
-  it "can display the user's ssn when selected" do
-    check 'Show Social Security number'
-    expect(page).to have_text '123-45-6789'
+    it "obfuscates the user's ssn" do
+      expect(page).to have_text '1**-**-***9'
+    end
+
+    it "can display the user's ssn when selected" do
+      check 'Show Social Security number'
+      expect(page).to have_text '123-45-6789'
+    end
   end
 end
