d0a230838c34c80e49ce3601fcfa572e5125e7f7
Mitchell Henke <mitchell.henke@gsa.gov>
2022-10-03 16:26:32 -0500

    add aal3 spec

--- spec/features/openid_connect/phishing_resistant_required_spec.rb
+++ spec/features/openid_connect/phishing_resistant_required_spec.rb
@@ -8,6 +8,38 @@ describe 'AAL3 authentication required in an OIDC context' do
       it 'sends user to set up AAL3 auth' do
         user = user_with_2fa
 
+        visit_idp_from_ial1_oidc_sp_requesting_aal3(prompt: 'select_account')
+        sign_in_live_with_2fa(user)
+
+        expect(current_url).to eq(authentication_methods_setup_url)
+        expect(page).to have_content(t('two_factor_authentication.two_factor_aal3_choice'))
+        expect(page).to have_xpath("//img[@alt='important alert icon']")
+      end
+    end
+
+    context 'user has aal3 auth configured' do
+      it 'sends user to authenticate with AAL3 auth' do
+        sign_in_before_2fa(user_with_phishing_resistant_2fa)
+
+        visit_idp_from_ial1_oidc_sp_requesting_aal3(prompt: 'select_account')
+        visit login_two_factor_path(otp_delivery_preference: 'sms')
+        expect(current_url).to eq(login_two_factor_webauthn_url)
+      end
+
+      it 'does not allow an already signed in user to bypass AAL3 auth' do
+        sign_in_and_2fa_user(user_with_phishing_resistant_2fa)
+        visit_idp_from_ial1_oidc_sp_requesting_aal3(prompt: 'select_account')
+
+        expect(current_url).to eq(login_two_factor_webauthn_url)
+      end
+    end
+  end
+
+  describe 'OpenID Connect requesting phishing-resistant authentication' do
+    context 'user does not have aal3 auth configured' do
+      it 'sends user to set up AAL3 auth' do
+        user = user_with_2fa
+
         visit_idp_from_ial1_oidc_sp_requesting_phishing_resistant(prompt: 'select_account')
         sign_in_live_with_2fa(user)
 
