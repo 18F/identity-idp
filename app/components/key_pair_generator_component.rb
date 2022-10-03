a3480105a48ab61bb913913f1e81d56d1065f618
Douglas Price <douglas.price@gsa.gov>
2022-10-03 14:28:09 -0400

    use ViewComponent conditional rendering

--- app/components/key_pair_generator_component.rb
+++ app/components/key_pair_generator_component.rb
@@ -6,4 +6,8 @@ class KeyPairGeneratorComponent < BaseComponent
   def initialize(location:)
     @location = location
   end
+
+  def render?
+    AbTests::KEY_PAIR_GENERATION.bucket(SecureRandom.uuid) == :key_pair_group
+  end
 end
