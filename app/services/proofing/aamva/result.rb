b4abd123ee871d5932801869f36db79cdb2ba97f
Kimball Bighorse <kimball.bighorse@gsa.gov>
2022-10-03 10:56:48 -0700

    AAMVA proofer (#7028)
    * Expect address2 to be missing
    
    * Stop inheriting proofing base class
    
    * Simplify described class initialization in test
    
    * Replace `#proof` implementation
    
    * Create `Proofing::Aamva::ResultWithException`
    
    * Use Aamva verification request class
    
    * Pass expected arguments
    
    * VerificationRequest expects an Aamva::Applicant instance
    
    * Scope a level higher
    
    * Indent nested arguments
    
    * Scope locally
    
    * Expose class method vendor_name
    
    * Replace `#proof` with Base implementation
    
    `ProofingSpec#proof` only fails now
    
    * Restore hash argument
    
    * Expose previously assigned instance variable values
    
    * Convert class methods to private
    
    * cleanup the proofer and start on tests
    
    * cleanup aamva proofer spec
    
    * get a result class started
    
    * Put the result to work
    
    * a little more on the result
    
    * Assign aamva result to aamva stage in test
    
    * Expect Aamva proofer to return Aamva result
    
    * Expect a verification response object
    
    * Indent long lines
    
    * Remove unneeded requires
    
    * Add more tests
    
    * More tests
    
    * get the resulotion proofing job green
    
    * cleanup state id mock proofer
    
    * [skip changelog]
    
    * delint and cleanup
    
    * Consolidate to_sym calls
    
    * Test verified attributes
    
    * Consolidate address attributes
    
    * Test verified attributes on success and missing
    
    * Verify attributes, including address, from results
    
    * Format
    
    * do not really need this
    
    * [skip changelog]
    
    * Test for verified attributes in resolution proofing job spec
    
    * Strip out `address2` from verified attributes
    
    * Format array literal
    
    Co-authored-by: Jonathan Hooper <jonathan.hooper@gsa.gov>

---
+++ app/services/proofing/aamva/result.rb
@@ -0,0 +1,89 @@
+module Proofing
+  module Aamva
+    class Result
+      attr_reader :verification_response
+
+      delegate(
+        :success?,
+        to: :verification_response,
+      )
+
+      def initialize(verification_response)
+        raise ArgumentError unless verification_response.instance_of? Response::VerificationResponse
+        @verification_response = verification_response
+      end
+
+      def errors
+        return @errors if defined? @errors
+
+        @errors = {}
+        return @errors if verification_response.success?
+
+        verification_response.verification_results.each do |attribute, v_result|
+          attribute_key = attribute.to_sym
+          next if v_result == true
+          @errors[attribute_key] ||= []
+          @errors[attribute_key].push('UNVERIFIED') if v_result == false
+          @errors[attribute_key].push('MISSING') if v_result.nil?
+        end
+        @errors
+      end
+
+      def transaction_id
+        verification_response.transaction_locator_id
+      end
+
+      def exception
+        nil
+      end
+
+      def timed_out?
+        false
+      end
+
+      def vendor_name
+        'aamva:state_id'
+      end
+
+      def verified_attributes
+        attributes = Set.new
+        results = verification_response.verification_results
+
+        attributes.add :address if address_verified?(results)
+
+        results.delete :address1
+        results.delete :address2
+        results.delete :city
+        results.delete :state
+        results.delete :zipcode
+
+        results.each do |attribute, verified|
+          attributes.add attribute if verified
+        end
+
+        attributes
+      end
+
+      def to_h
+        {
+          exception: exception,
+          errors: errors,
+          success: success?,
+          timed_out: timed_out?,
+          transaction_id: transaction_id,
+          vendor_name: vendor_name,
+          verified_attributes: verified_attributes.to_a,
+        }
+      end
+
+      private
+
+      def address_verified?(results)
+        results[:address1] &&
+          results[:city] &&
+          results[:state] &&
+          results[:zipcode]
+      end
+    end
+  end
+end
--- app/services/proofing/mock/state_id_mock_client.rb
+++ app/services/proofing/mock/state_id_mock_client.rb
@@ -3,20 +3,6 @@
 module Proofing
   module Mock
     class StateIdMockClient < Proofing::Base
-      vendor_name 'StateIdMock'
-
-      required_attributes :uuid,
-                          :first_name,
-                          :last_name,
-                          :dob,
-                          :state_id_number,
-                          :state_id_type,
-                          :state_id_jurisdiction
-
-      optional_attributes :uuid_prefix
-
-      stage :state_id
-
       SUPPORTED_STATE_ID_TYPES = %w[
         drivers_license drivers_permit state_id_card
       ].to_set.freeze
@@ -25,27 +11,67 @@ module Proofing
       TRANSACTION_ID = 'state-id-mock-transaction-id-456'
       TRIGGER_MVA_TIMEOUT = 'mvatimeout'
 
-      proof do |applicant, result|
-        if applicant[:state_id_number].downcase == TRIGGER_MVA_TIMEOUT
-          raise ::Proofing::TimeoutError.new(
-            'ExceptionId: 0047, ExceptionText: MVA did not respond in a timely fashion',
-          )
+      StateIdMockClientResult = Struct.new(:success, :errors, :exception, keyword_init: true) do
+        def success?
+          success
         end
 
+        def timed_out?
+          exception.is_a?(Proofing::TimeoutError)
+        end
+
+        def transaction_id
+          TRANSACTION_ID
+        end
+
+        def to_h
+          {
+            exception: exception,
+            errors: errors,
+            success: success,
+            timed_out: timed_out?,
+            transaction_id: transaction_id,
+            vendor_name: 'StateIdMock',
+          }
+        end
+      end
+
+      def proof(applicant)
+        return mva_timeout_result if applicant[:state_id_number].downcase == TRIGGER_MVA_TIMEOUT
+
+        errors = {}
         if state_not_supported?(applicant[:state_id_jurisdiction])
-          result.add_error(:state_id_jurisdiction, 'The jurisdiction could not be verified')
-
+          errors[:state_id_jurisdiction] = ['The jurisdiction could not be verified']
         elsif invalid_state_id_number?(applicant[:state_id_number])
-          result.add_error(:state_id_number, 'The state ID number could not be verified')
-
+          errors[:state_id_number] = ['The state ID number could not be verified']
         elsif invalid_state_id_type?(applicant[:state_id_type])
-          result.add_error(:state_id_type, 'The state ID type could not be verified')
+          errors[:state_id_type] = ['The state ID type could not be verified']
         end
 
-        result.transaction_id = TRANSACTION_ID
+        return unverifiable_result(errors) if errors.any?
+
+        StateIdMockClientResult.new(success: true, errors: {}, exception: nil)
       end
 
       private
+
+      def mva_timeout_result
+        StateIdMockClientResult.new(
+          success: false,
+          errors: {},
+          exception: Proofing::TimeoutError.new(
+            'ExceptionId: 0047, ExceptionText: MVA did not respond in a timely fashion',
+          ),
+        )
+      end
+
+      def unverifiable_result(errors)
+        StateIdMockClientResult.new(
+          success: false,
+          errors: errors,
+          exception: nil,
+        )
+      end
 
       def state_not_supported?(state_id_jurisdiction)
         !IdentityConfig.store.aamva_supported_jurisdictions.include? state_id_jurisdiction
