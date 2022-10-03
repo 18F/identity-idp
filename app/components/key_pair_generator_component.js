f5fe2511c156a804ae6c014b152f2789da6dc743
Douglas Price <douglas.price@gsa.gov>
2022-10-03 16:04:31 -0400

    utility function for exporting keys

--- app/components/key_pair_generator_component.js
+++ app/components/key_pair_generator_component.js
@@ -1,28 +1,6 @@
 import { trackEvent } from '@18f/identity-analytics';
 
 class KeyPairGeneratorElement extends HTMLElement {
-  async generateKeyPair() {
-    const t0 = performance.now();
-    const keypair = await crypto.subtle.generateKey(
-      {
-        name: 'RSA-OAEP',
-        modulusLength: 4096,
-        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-        hash: 'SHA-256',
-      },
-      true,
-      ['encrypt', 'decrypt'],
-    );
-
-    const encodedPrivate = await crypto.subtle.exportKey('pkcs8', keypair.privateKey);
-    this.privateB64key = btoa(String.fromCharCode.apply(null, new Uint8Array(encodedPrivate)));
-
-    const encodedPublic = await crypto.subtle.exportKey('spki', keypair.publicKey);
-    this.publicB64key = btoa(String.fromCharCode.apply(null, new Uint8Array(encodedPublic)));
-    const t1 = performance.now();
-    this.duration = t1 - t0; // milliseconds
-  }
-
   connectedCallback() {
     this.generateKeyPair().then(() => {
       if (this.duration === undefined) {
@@ -33,6 +11,31 @@ class KeyPairGeneratorElement extends HTMLElement {
         location: this.dataset.location,
       });
     });
+  }
+
+  async generateKeyPair() {
+    const t0 = performance.now();
+
+    const keypair = await crypto.subtle.generateKey(
+      {
+        name: 'RSA-OAEP',
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+        hash: 'SHA-256',
+      },
+      true,
+      ['encrypt', 'decrypt'],
+    );
+    this.privateB64key = await this.exportKey('pkcs8', keypair.privateKey);
+    this.publicB64key = await this.exportKey('spki', keypair.publicKey);
+
+    const t1 = performance.now();
+    this.duration = t1 - t0; // milliseconds
+  }
+
+  async exportKey(format, key) {
+    const encodedKey = await crypto.subtle.exportKey(format, key);
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(encodedKey)));
   }
 }
 
