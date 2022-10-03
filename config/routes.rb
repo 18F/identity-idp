aba39bcf3bda9f13395b8c4ee7b0968ea45c99d0
Mitchell Henke <mitchell.henke@gsa.gov>
2022-10-03 14:41:43 -0500

    remove unused AAL3 controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -244,7 +244,6 @@ Rails.application.routes.draw do
     patch '/second_mfa_setup' => 'users/mfa_selection#update'
     get '/phone_setup' => 'users/phone_setup#index'
     patch '/phone_setup' => 'users/phone_setup#create'
-    get '/aal3_required' => 'users/aal3#show'
     get '/users/two_factor_authentication' => 'users/two_factor_authentication#show',
         as: :user_two_factor_authentication # route name is used by two_factor_authentication gem
     get '/backup_code_refreshed' => 'users/backup_code_setup#refreshed'
