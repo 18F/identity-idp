0686823f9aa10b22c7a79678d1d998c057218ea3
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-10-04 16:49:33 -0400

    Use a shared StateIdResult struct for all State ID proofer calls
    We had issues where we were raising NoMethodErrors because the API between the proofer result objects had drifted. This commit helps to address that problem in the future by collapsing all of the state ID result objects into a single struct and building out the result in the proofer.

--- app/services/proofing/aamva/proofer.rb
+++ app/services/proofing/aamva/proofer.rb
@@ -29,15 +29,71 @@ module Proofing
       def proof(applicant)
         aamva_applicant =
           Aamva::Applicant.from_proofer_applicant(OpenStruct.new(applicant))
-        response = Aamva::VerificationClient.new(
+        verification_response = Aamva::VerificationClient.new(
           config,
         ).send_verification_request(
           applicant: aamva_applicant,
         )
-        Aamva::Result.new(response)
+        build_result_from_response(verification_response)
       rescue => exception
         NewRelic::Agent.notice_error(exception)
-        Proofing::Result.new(exception: exception)
+        Proofing::StateIdResult.new(
+          success: false, errors: {}, exception: exception, vendor_name: 'aamva:state_id',
+          transaction_id: nil, verified_attributes: []
+        )
+      end
+
+      private
+
+      def build_result_from_response(verification_response)
+        Proofing::StateIdResult.new(
+          success: verification_response.success?,
+          errors: parse_verification_errors(verification_response),
+          exception: nil,
+          vendor_name: 'aamva:state_id',
+          transaction_id: verification_response.transaction_locator_id,
+          verified_attributes: verified_attributes(verification_response),
+        )
+      end
+
+      def parse_verification_errors(verification_response)
+        errors = {}
+        return errors if verification_response.success?
+
+        verification_response.verification_results.each do |attribute, v_result|
+          attribute_key = attribute.to_sym
+          next if v_result == true
+          errors[attribute_key] ||= []
+          errors[attribute_key].push('UNVERIFIED') if v_result == false
+          errors[attribute_key].push('MISSING') if v_result.nil?
+        end
+        errors
+      end
+
+      def verified_attributes(verification_response)
+        attributes = Set.new
+        results = verification_response.verification_results
+
+        attributes.add :address if address_verified?(results)
+
+        results.delete :address1
+        results.delete :address2
+        results.delete :city
+        results.delete :state
+        results.delete :zipcode
+
+        results.each do |attribute, verified|
+          attributes.add attribute if verified
+        end
+
+        attributes
+      end
+
+      def address_verified?(results)
+        results[:address1] &&
+          results[:city] &&
+          results[:state] &&
+          results[:zipcode]
       end
     end
   end
