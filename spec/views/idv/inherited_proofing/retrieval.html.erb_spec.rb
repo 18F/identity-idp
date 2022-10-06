f58c6a062a155062d3e7785bdbf436fef7b84379
Melissa Miller <melissa.miller@gsa.gov>
2022-10-06 08:53:00 -0400

    Update spec/views/idv/inherited_proofing/retrieval.html.erb_spec.rb
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- spec/views/idv/inherited_proofing/retrieval.html.erb_spec.rb
+++ spec/views/idv/inherited_proofing/retrieval.html.erb_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 describe 'idv/inherited_proofing/retrieval.html.erb' do
-  let(:flow_session) { {} }
-  let(:sp_name) { nil }
-  let(:locale) { nil }
-
-  before do
-    allow(view).to receive(:decorated_session).and_return(@decorated_session)
-    allow(view).to receive(:flow_session).and_return(flow_session)
-    allow(view).to receive(:url_for).and_return('https://www.example.com/')
-  end
-
   it 'renders' do
     render template: 'idv/inherited_proofing/retrieval'
 
