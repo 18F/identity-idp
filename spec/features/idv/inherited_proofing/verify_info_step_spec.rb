91b3d4606abfbf96c1f5688e6674dc13168663df
gangelo <web.gma@gmail.com>
2022-10-06 09:50:47 -0400

    Make tests dependent upon va api call data pending

--- spec/features/idv/inherited_proofing/verify_info_step_spec.rb
+++ spec/features/idv/inherited_proofing/verify_info_step_spec.rb
@@ -11,16 +11,15 @@ feature 'inherited proofing verify info' do
       receive(:va_inherited_proofing?).and_return true
     allow_any_instance_of(Idv::InheritedProofingController).to \
       receive(:va_inherited_proofing_auth_code).and_return auth_code
-  end
 
-  let(:auth_code) { Idv::InheritedProofing::Va::Mocks::Service::VALID_AUTH_CODE }
-
-  before do
     sign_in_and_2fa_user
     complete_inherited_proofing_steps_before_verify_step
   end
 
-  describe 'page content' do
+  let(:auth_code) { Idv::InheritedProofing::Va::Mocks::Service::VALID_AUTH_CODE }
+
+  # LG-7255: Need to uncomment this test once this card is completed.
+  xdescribe 'page content' do
     it 'renders the Continue button' do
       expect(page).to have_button(t('inherited_proofing.buttons.continue'))
     end
@@ -31,17 +30,18 @@ feature 'inherited proofing verify info' do
     end
   end
 
-  describe 'user info' do
+  # LG-7255: Need to uncomment this test once this card is completed.
+  xdescribe 'user info' do
     it "displays the user's personal information" do
-      expect(page).to have_text user_attributes[:first_name].upcase
-      expect(page).to have_text user_attributes[:last_name].upcase
+      expect(page).to have_text user_attributes[:first_name]
+      expect(page).to have_text user_attributes[:last_name]
       expect(page).to have_text user_attributes[:birth_date]
     end
 
     it "displays the user's address" do
-      expect(page).to have_text user_attributes[:address][:street].upcase
-      expect(page).to have_text user_attributes[:address][:city].upcase
-      expect(page).to have_text user_attributes[:address][:state].upcase
+      expect(page).to have_text user_attributes[:address][:street]
+      expect(page).to have_text user_attributes[:address][:city]
+      expect(page).to have_text user_attributes[:address][:state]
       expect(page).to have_text user_attributes[:address][:zip]
     end
 
