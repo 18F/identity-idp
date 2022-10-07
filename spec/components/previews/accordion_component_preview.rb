63566070a4949e911f963630f7a8b7e41c4eeab7
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-07 14:59:43 -0400

    Add Accordion component preview

---
+++ spec/components/previews/accordion_component_preview.rb
@@ -0,0 +1,27 @@
+class AccordionComponentPreview < ViewComponent::Preview
+  # @!group Kitchen Sink
+  def default
+    render(AccordionComponent.new) do |c|
+      c.header { 'Header' }
+      'Content'
+    end
+  end
+
+  def unbordered
+    render(AccordionComponent.new(bordered: false)) do |c|
+      c.header { 'Header' }
+      'Content'
+    end
+  end
+  # @!endgroup
+
+  # @param header text
+  # @param content text
+  # @param bordered toggle
+  def playground(header: 'Header', content: 'Content', bordered: true)
+    render(AccordionComponent.new(bordered: bordered)) do |c|
+      c.header { header }
+      content
+    end
+  end
+end
