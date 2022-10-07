b0c62c732909e147267bfa07f677682536460fbd
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-07 15:23:21 -0400

    Add Block Link component preview

---
+++ spec/components/previews/block_link_component_preview.rb
@@ -0,0 +1,18 @@
+class BlockLinkComponentPreview < ViewComponent::Preview
+  # @!group Kitchen Sink
+  def default
+    render(BlockLinkComponent.new(url: '', new_tab: false).with_content('Link text'))
+  end
+
+  def new_tab
+    render(BlockLinkComponent.new(url: '', new_tab: true).with_content('Link text'))
+  end
+  # @!endgroup
+
+  # @param content text
+  # @param url text
+  # @param new_tab toggle
+  def playground(content: 'Link text', url: '', new_tab: false)
+    render(BlockLinkComponent.new(url: url, new_tab: new_tab).with_content(content))
+  end
+end
