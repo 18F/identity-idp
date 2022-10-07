847494938618e917012b32e67c90ca00a22616c7
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-07 15:13:10 -0400

    Add Barcode component preview

---
+++ spec/components/previews/barcode_component_preview.rb
@@ -0,0 +1,13 @@
+class BarcodeComponentPreview < ViewComponent::Preview
+  # @!group Kitchen Sink
+  def default
+    render(BarcodeComponent.new(barcode_data: '1234567812345678', label: 'Barcode'))
+  end
+  # @!endgroup
+
+  # @param barcode_data text
+  # @param label text
+  def playground(barcode_data: '1234567812345678', label: 'Barcode')
+    render(BarcodeComponent.new(barcode_data: barcode_data, label: label))
+  end
+end
