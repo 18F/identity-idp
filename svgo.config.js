967b4cc966d5468143cc1007b77af367f194fa0e
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-07 08:35:16 -0400

    Restore/fix ID card keyframes

--- svgo.config.js
+++ svgo.config.js
@@ -1,11 +1,18 @@
 module.exports = /** @type {import('svgo').OptimizeOptions} */ ({
   multipass: true,
-  minifyStyles: false,
   plugins: [
     {
       name: 'preset-default',
       params: {
         overrides: {
+          // `minifyStyles` is disabled since some SVG use `@keyframes` for animations (for example,
+          // `id-card.svg`), which is removed with the `minifyStyles` plugin.
+          // Related: https://github.com/svg/svgo/issues/888
+          minifyStyles: false,
+
+          // `removeViewBox` is disabled since `viewbox` is a meaningful attribute, and notably this
+          // can cause rendering issues in some browsers.
+          // Related: https://github.com/svg/svgo/issues/1128
           removeViewBox: false,
         },
       },
