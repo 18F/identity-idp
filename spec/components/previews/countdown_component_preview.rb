564fedb233d923a77fd03ec0600789b2b6240a1b
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-11 16:54:39 -0400

    Add countdown component preview

---
+++ spec/components/previews/countdown_component_preview.rb
@@ -0,0 +1,24 @@
+class CountdownComponentPreview < ViewComponent::Preview
+  # @!group Kitchen Sink
+  def default
+    render(CountdownComponent.new(expiration: Time.zone.now + 1.5.minutes))
+  end
+  # @!endgroup
+
+  # @param expiration datetime-local
+  # @param update_interval number
+  # @param start_immediately toggle
+  def playground(
+    expiration: Time.zone.now + 1.5.minutes,
+    update_interval: 1,
+    start_immediately: true
+  )
+    render(
+      CountdownComponent.new(
+        expiration: expiration,
+        update_interval: update_interval.seconds,
+        start_immediately: start_immediately,
+      ),
+    )
+  end
+end
