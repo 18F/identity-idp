f013762ac3bae64656440f22b529b346d7f5784a
Zach Margolis <zachary.margolis@gsa.gov>
2022-10-12 12:39:22 -0700

    typo

--- app/services/funnel/registration/add_mfa.rb
+++ app/services/funnel/registration/add_mfa.rb
@@ -2,7 +2,7 @@ module Funnel
   module Registration
     class AddMfa
       def self.call(user_id, mfa_method, analytics)
-        funnel = RegistrationLog.create_or_find_by(user_id: user.id)
+        funnel = RegistrationLog.create_or_find_by(user_id: user_id)
         return if funnel.registered_at.present?
 
         analytics.user_registration_user_fully_registered(mfa_method: mfa_method)
