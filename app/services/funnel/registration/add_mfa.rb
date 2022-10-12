987b863f95ce73c10dbbf2397b3f7aa648cfb28f
Zach Margolis <zachary.margolis@gsa.gov>
2022-10-12 14:03:34 -0700

    Bring back "now" variable, oops

--- app/services/funnel/registration/add_mfa.rb
+++ app/services/funnel/registration/add_mfa.rb
@@ -2,6 +2,7 @@ module Funnel
   module Registration
     class AddMfa
       def self.call(user_id, mfa_method, analytics)
+        now = Time.zone.now
         funnel = RegistrationLog.create_or_find_by(user_id: user_id)
         return if funnel.registered_at.present?
 
