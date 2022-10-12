872eb247af68f89d76b63e8fbb1d220cb052398d
John Maxwell <john.maxwell@gsa.gov>
2022-10-12 18:29:41 -0400

    More dead code removal

--- app/javascript/packs/personal-key-page-controller.js
+++ app/javascript/packs/personal-key-page-controller.js
@@ -1,15 +1,10 @@
-import { encodeInput } from '@18f/identity-personal-key-input';
 import { trackEvent } from '@18f/identity-analytics';
-import { t } from '@18f/identity-i18n';
 
 const modalSelector = '#personal-key-confirm';
-const modal = new window.LoginGov.Modal({ el: modalSelector });
 
 const personalKeyWords = [].slice.call(document.querySelectorAll('[data-personal-key]'));
 const formEl = document.getElementById('confirm-key');
 const input = formEl.querySelector('input[type="text"]');
-const modalTrigger = document.querySelector('[data-toggle="modal"]');
-const modalDismiss = document.querySelector('[data-dismiss="personal-key-confirm"]');
 const downloadLink = document.querySelector('a[download]');
 const acknowledgmentCheckbox = document.getElementById('acknowledgment');
 
@@ -21,13 +16,6 @@ function scrapePersonalKey() {
   });
 
   return keywords.join('-').toUpperCase();
-}
-
-const personalKey = scrapePersonalKey();
-
-function resetForm() {
-  formEl.reset();
-  input.setCustomValidity('');
 }
 
 function downloadForIE(event) {
