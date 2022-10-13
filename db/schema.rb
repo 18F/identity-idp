c745ab8ad0b6869dcef4a47feac18a5d29facc95
Douglas Price <douglas.price@gsa.gov>
2022-10-13 18:46:52 -0400

    restore pgcrypto

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_10_12_172457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "account_reset_requests", force: :cascade do |t|
