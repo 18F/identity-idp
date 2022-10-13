e107404965bd8a9d6334a33ed6a17303c79b82d1
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-10-12 17:23:29 -0700

    Remove query streaming for big reports (#7121)
    **Why**: It may not be helping the reports run faster, and it may
    be making the pg connections more fragile/stateful
    
    * Rescue PG::ConnectionBad too
    
    [skip changelog]

--- app/jobs/reports/query_helpers.rb
+++ app/jobs/reports/query_helpers.rb
@@ -7,20 +7,5 @@ module Reports
         ActiveRecord::Base.connection.quote(value)
       end
     end
-
-    # Wrapper around PG::Result#stream_each, runs a query and yields each row to the block
-    # as it is returned from the DB
-    # @param [String] SQL query
-    # @yieldparam [Hash] row
-    def stream_query(query)
-      ActiveRecord::Base.logger.debug(query) # using send_query skips ActiveRecord logging
-      connection = ActiveRecord::Base.connection.raw_connection
-      connection.send_query(query)
-      connection.set_single_row_mode
-      connection.get_result.stream_each do |row|
-        yield row
-      end
-      connection.get_result
-    end
   end
 end
