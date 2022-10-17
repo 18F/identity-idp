7394c681322117fb32d1783b86b42c7c983a6ac1
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-17 13:04:45 -0400

    Remove unreachable reference to secure_headers gem
    changelog: Internal, Code Quality, Remove unreachable code

---
+++ app/helpers/csp_helper.rb
@@ -0,0 +1,8 @@
+module CspHelper
+  def add_document_capture_image_urls_to_csp(request, urls)
+    cleaned_urls = urls.compact.map { |url| URI(url).tap { |uri| uri.query = nil }.to_s }
+    policy = request.content_security_policy.clone
+    policy.connect_src(*policy.connect_src, *cleaned_urls)
+    request.content_security_policy = policy
+  end
+end
