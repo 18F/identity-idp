08bb16beb3415f990096f15ca2a47305cde557f7
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-19 09:39:22 -0400

    Create DownloadButtonComponent for download button (#7146)
    * Create DownloadButtonComponent for download button
    
    changelog: Bug Fixes, Backup Codes, Fix backup code download for Internet Explorer
    
    * Change "data" argument to "file_data"
    
    Allow use of data-* attributes passed as "data" hash
    
    * Add specs for DownloadButtonComponent
    
    * Simplify to use Uin8Array map argument
    
    See: https://github.com/18F/identity-idp/pull/7146#discussion_r998544674
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>
    
    * Restore customized download label for personal key
    
    Co-authored-by: Zach Margolis <zbmargolis@gmail.com>

---
+++ spec/components/download_button_component_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe DownloadButtonComponent, type: :component do
+  let(:file_data) { 'Downloaded Text' }
+  let(:file_name) { 'file.txt' }
+  let(:tag_options) { {} }
+  let(:instance) do
+    DownloadButtonComponent.new(
+      file_data: file_data,
+      file_name: file_name,
+      **tag_options,
+    )
+  end
+
+  subject(:rendered) { render_inline instance }
+
+  it 'renders link with data and file name' do
+    expect(rendered).to have_css(
+      "lg-download-button a[href*='#{CGI.escape(file_data)}'][download='#{file_name}']",
+      text: t('components.download_button.label'),
+    )
+  end
+
+  context 'with tag options' do
+    let(:tag_options) { { outline: true, data: { foo: 'bar' } } }
+
+    it 'renders button given the tag options' do
+      expect(rendered).to have_css('.usa-button.usa-button--outline[data-foo="bar"]')
+    end
+  end
+
+  context 'with content' do
+    let(:content) { 'Download File' }
+    let(:instance) { super().with_content(content) }
+
+    it 'renders with the given content' do
+      expect(rendered).to have_content(content)
+    end
+  end
+end
