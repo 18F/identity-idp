b877d1f8d82ca154de2214151556e2629eeee238
Douglas Price <douglas.price@gsa.gov>
2022-10-19 16:18:02 -0400

    working on the SAML side

--- spec/features/users/sign_in_irs_spec.rb
+++ spec/features/users/sign_in_irs_spec.rb
@@ -11,6 +11,7 @@ feature 'Sign in to the IRS' do
   end
 
   include IdvHelper
+  include SamlAuthHelper
 
   let(:irs) { create(:service_provider, :irs) }
   let(:other_irs) { create(:service_provider, :irs) }
@@ -26,42 +27,81 @@ feature 'Sign in to the IRS' do
     ).user
   end
 
-  context 'user verified with IRS returns to IRS' do
-    context 'user visits the same IRS SP they verified with' do
-      it "accepts the user's identity as verified" do
+  context 'OIDC' do
+    context 'user verified with IRS returns to IRS' do
+      context 'user visits the same IRS SP they verified with' do
+        it "accepts the user's identity as verified" do
+          visit_idp_from_oidc_sp_with_ial2(client_id: irs.issuer)
+          fill_in_credentials_and_submit(user.email, user.password)
+          fill_in_code_with_last_phone_otp
+          click_submit_default
+
+          expect(current_path).to eq(sign_up_completed_path)
+        end
+      end
+
+      context 'user visits different IRS SP than the one they verified with' do
+        it "accepts the user's identity as verified" do
+          visit_idp_from_oidc_sp_with_ial2(client_id: other_irs.issuer)
+          fill_in_credentials_and_submit(user.email, user.password)
+          fill_in_code_with_last_phone_otp
+          click_submit_default
+
+          expect(current_path).to eq(sign_up_completed_path)
+        end
+      end
+    end
+
+    context 'user verified with other agency signs in to IRS' do
+      let(:initiating_service_provider_issuer) { not_irs.issuer }
+
+      it 'forces the user to re-verify their identity' do
         visit_idp_from_oidc_sp_with_ial2(client_id: irs.issuer)
         fill_in_credentials_and_submit(user.email, user.password)
         fill_in_code_with_last_phone_otp
         click_submit_default
-        click_agree_and_continue
 
-        expect(current_url).to start_with('http://localhost:7654/auth/result')
-      end
-    end
-
-    context 'user visits different IRS SP than the one they verified with' do
-      it "accepts the user's identity as verified" do
-        visit_idp_from_oidc_sp_with_ial2(client_id: other_irs.issuer)
-        fill_in_credentials_and_submit(user.email, user.password)
-        fill_in_code_with_last_phone_otp
-        click_submit_default
-        click_agree_and_continue
-
-        expect(current_url).to start_with('http://localhost:7654/auth/result')
+        expect(current_path).to eq(idv_doc_auth_step_path(step: :welcome))
       end
     end
   end
 
-  context 'user verified with other agency signs in to IRS' do
-    let(:initiating_service_provider_issuer) { not_irs.issuer }
+  context 'SAML', js: true do
+    context 'user verified with IRS returns to IRS' do
+      context 'user visits the same IRS SP they verified with' do
+        it "accepts the user's identity as verified" do
+          visit_idp_from_saml_sp_with_ial2(issuer: irs.issuer)
+          fill_in_credentials_and_submit(user.email, user.password)
+          fill_in_code_with_last_phone_otp
+          click_submit_default
 
-    it 'forces the user to re-verify their identity' do
-      visit_idp_from_oidc_sp_with_ial2(client_id: irs.issuer)
-      fill_in_credentials_and_submit(user.email, user.password)
-      fill_in_code_with_last_phone_otp
-      click_submit_default
+          expect(current_path).to eq(sign_up_completed_path)
+        end
+      end
 
-      expect(current_path).to eq(idv_doc_auth_step_path(step: :welcome))
+      context 'user visits different IRS SP than the one they verified with' do
+        it "accepts the user's identity as verified" do
+          visit_idp_from_saml_sp_with_ial2(issuer: other_irs.issuer)
+          fill_in_credentials_and_submit(user.email, user.password)
+          fill_in_code_with_last_phone_otp
+          click_submit_default
+
+          expect(current_path).to eq(sign_up_completed_path)
+        end
+      end
+    end
+
+    context 'user verified with other agency signs in to IRS' do
+      let(:initiating_service_provider_issuer) { not_irs.issuer }
+
+      it 'forces the user to re-verify their identity' do
+        visit_idp_from_saml_sp_with_ial2(issuer: irs.issuer)
+        fill_in_credentials_and_submit(user.email, user.password)
+        fill_in_code_with_last_phone_otp
+        click_submit_default
+
+        expect(current_path).to eq(idv_doc_auth_step_path(step: :welcome))
+      end
     end
   end
 end
