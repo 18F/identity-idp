d9fd433074521f28a0d38e36bb6497c9efff0b56
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-19 10:33:13 -0400

    Remove FormSteps basePath option handling
    changelog: Internal, Performance, Reduce JavaScript bundle size in critical paths

--- app/javascript/app/i18n-dropdown.js
+++ app/javascript/app/i18n-dropdown.js
@@ -35,35 +35,4 @@ export function setUp() {
   if (mobileLink) {
     languagePicker(mobileLink, mobileDropdown);
   }
-
-  /**
-   * Loops through all of the language links in the dropdown and updates their target url
-   * to reflect the correct route
-   */
-  function syncLanguageLinkURLs() {
-    const links = document.querySelectorAll('.i18n-dropdown a[lang]');
-    links.forEach((link) => {
-      const linkLang = link.getAttribute('lang');
-      const prefix = linkLang === 'en' ? '' : `/${linkLang}`;
-      const url = new URL(window.location.href);
-      const { lang } = document.documentElement;
-      const barePath = url.pathname.replace(new RegExp(`^/${lang}`), '');
-      url.pathname = prefix + barePath;
-      link.setAttribute('href', url.toString());
-    });
-  }
-
-  syncLanguageLinkURLs();
-
-  window.addEventListener('lg:url-change', syncLanguageLinkURLs);
-  return () => {
-    window.removeEventListener('lg:url-change', syncLanguageLinkURLs);
-  };
-}
-
-/**
- *  used to mock this behavior for testing purposes
- */
-if (process.env.NODE_ENV !== 'test') {
-  setUp();
 }
--- app/javascript/packages/form-steps/use-history-param.spec.tsx
+++ app/javascript/packages/form-steps/use-history-param.spec.tsx
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 import userEvent from '@testing-library/user-event';
-import { useDefineProperty, useSandbox } from '@18f/identity-test-helpers';
 import useHistoryParam, { getStepParam } from './use-history-param';
 
 describe('getStepParam', () => {
@@ -11,32 +10,11 @@ describe('getStepParam', () => {
 
     expect(result).to.equal('step');
   });
-
-  context('with subpath', () => {
-    it('returns step', () => {
-      const path = 'step/subpath';
-      const result = getStepParam(path);
-
-      expect(result).to.equal('step');
-    });
-  });
-
-  context('with trailing or leading slashes', () => {
-    it('returns step', () => {
-      const path = '/step/';
-      const result = getStepParam(path);
-
-      expect(result).to.equal('step');
-    });
-  });
 });
 
 describe('useHistoryParam', () => {
-  const sandbox = useSandbox();
-  const defineProperty = useDefineProperty();
-
-  function TestComponent({ initialValue, basePath }: { initialValue?: string; basePath?: string }) {
-    const [count = 0, setCount] = useHistoryParam(initialValue, { basePath });
+  function TestComponent({ initialValue }: { initialValue?: string }) {
+    const [count = 0, setCount] = useHistoryParam(initialValue);
 
     return (
       <>
@@ -53,17 +31,13 @@ describe('useHistoryParam', () => {
   }
 
   let originalHash;
-  let onURLChange;
 
   beforeEach(() => {
     originalHash = window.location.hash;
-    onURLChange = sandbox.stub();
-    window.addEventListener('lg:url-change', onURLChange);
   });
 
   afterEach(() => {
     window.location.hash = originalHash;
-    window.removeEventListener('lg:url-change', onURLChange);
   });
 
   it('returns undefined value if absent from initial URL', () => {
@@ -86,12 +60,10 @@ describe('useHistoryParam', () => {
 
     expect(getByDisplayValue('1')).to.be.ok();
     expect(window.location.hash).to.equal('#1');
-    expect(onURLChange).to.have.been.calledOnce();
     await userEvent.click(getByText('Increment'));
 
     expect(getByDisplayValue('2')).to.be.ok();
     expect(window.location.hash).to.equal('#2');
-    expect(onURLChange).to.have.been.calledTwice();
   });
 
   it('scrolls to top on programmatic history manipulation', async () => {
@@ -117,33 +89,25 @@ describe('useHistoryParam', () => {
   it('syncs by history events', async () => {
     const { getByText, getByDisplayValue, findByDisplayValue } = render(<TestComponent />);
 
-    onURLChange.callsFake(() => expect(window.location.hash).to.equal('#1'));
     await userEvent.click(getByText('Increment'));
-    onURLChange.resetBehavior();
 
     expect(getByDisplayValue('1')).to.be.ok();
     expect(window.location.hash).to.equal('#1');
-    expect(onURLChange).to.have.been.calledOnce();
 
     await userEvent.click(getByText('Increment'));
 
     expect(getByDisplayValue('2')).to.be.ok();
     expect(window.location.hash).to.equal('#2');
-    expect(onURLChange).to.have.been.calledTwice();
 
-    onURLChange.callsFake(() => expect(window.location.hash).to.equal('#1'));
     window.history.back();
-    onURLChange.resetBehavior();
 
     expect(await findByDisplayValue('1')).to.be.ok();
     expect(window.location.hash).to.equal('#1');
-    expect(onURLChange).to.have.been.calledThrice();
 
     window.history.back();
 
     expect(await findByDisplayValue('0')).to.be.ok();
     expect(window.location.hash).to.equal('');
-    expect(onURLChange).to.have.callCount(4);
   });
 
   it('encodes parameter names and values', async () => {
@@ -158,158 +122,11 @@ describe('useHistoryParam', () => {
   it('syncs across instances', () => {
     const inst1 = renderHook(() => useHistoryParam());
     const inst2 = renderHook(() => useHistoryParam());
-    const inst3 = renderHook(() => useHistoryParam(undefined, { basePath: '/base' }));
 
     const [, setPath1] = inst1.result.current;
     setPath1('root');
 
     const [path2] = inst2.result.current;
-    const [path3] = inst3.result.current;
     expect(path2).to.equal('root');
-    expect(path3).to.be.undefined();
-    expect(onURLChange).to.have.been.calledOnce();
-  });
-
-  Object.entries({
-    'with basePath': '/base/',
-    'with basePath, no trailing slash': '/base',
-  }).forEach(([description, basePath]) => {
-    context(description, () => {
-      context('without initial value', () => {
-        beforeEach(() => {
-          const history: string[] = [basePath];
-          defineProperty(window, 'location', {
-            value: {
-              get pathname() {
-                return history[history.length - 1];
-              },
-            },
-          });
-
-          sandbox.stub(window, 'history').value(
-            Object.assign(history, {
-              pushState(_data, _unused, url: string) {
-                history.push(url as string);
-              },
-              replaceState(_data, _unused, url: string) {
-                history[history.length - 1] = url as string;
-              },
-              back() {
-                history.pop();
-                window.dispatchEvent(new CustomEvent('popstate'));
-              },
-            }),
-          );
-        });
-
-        it('returns undefined value', () => {
-          const { getByDisplayValue } = render(<TestComponent basePath={basePath} />);
-
-          expect(getByDisplayValue('0')).to.be.ok();
-        });
-
-        it('syncs by setter', async () => {
-          const { getByText, getByDisplayValue } = render(<TestComponent basePath={basePath} />);
-
-          await userEvent.click(getByText('Increment'));
-
-          expect(getByDisplayValue('1')).to.be.ok();
-          expect(window.location.pathname).to.equal('/base/1');
-          expect(onURLChange).to.have.been.calledOnce();
-
-          await userEvent.click(getByText('Increment'));
-
-          expect(getByDisplayValue('2')).to.be.ok();
-          expect(window.location.pathname).to.equal('/base/2');
-          expect(onURLChange).to.have.been.calledTwice();
-        });
-
-        it('syncs by history events', async () => {
-          const { getByText, getByDisplayValue, findByDisplayValue } = render(
-            <TestComponent basePath="/base/" />,
-          );
-
-          await userEvent.click(getByText('Increment'));
-
-          expect(getByDisplayValue('1')).to.be.ok();
-          expect(window.location.pathname).to.equal('/base/1');
-          expect(onURLChange).to.have.been.calledOnce();
-
-          await userEvent.click(getByText('Increment'));
-
-          expect(getByDisplayValue('2')).to.be.ok();
-          expect(window.location.pathname).to.equal('/base/2');
-          expect(onURLChange).to.have.been.calledTwice();
-
-          window.history.back();
-
-          expect(await findByDisplayValue('1')).to.be.ok();
-          expect(window.location.pathname).to.equal('/base/1');
-          expect(onURLChange).to.have.been.calledThrice();
-
-          window.history.back();
-
-          expect(await findByDisplayValue('0')).to.be.ok();
-          expect(window.location.pathname).to.equal(basePath);
-          expect(onURLChange).to.have.callCount(4);
-        });
-
-        context('with initial provided value', () => {
-          it('returns initial value', () => {
-            const { getByDisplayValue } = render(
-              <TestComponent initialValue="1" basePath={basePath} />,
-            );
-
-            expect(getByDisplayValue('1')).to.be.ok();
-          });
-
-          it('syncs to URL', () => {
-            onURLChange.callsFake(() => expect(window.location.pathname).to.equal('/base/1'));
-            render(<TestComponent initialValue="1" basePath={basePath} />);
-            onURLChange.resetBehavior();
-
-            expect(window.location.pathname).to.equal('/base/1');
-            expect(window.history.length).to.equal(1);
-            expect(onURLChange).to.have.been.calledOnce();
-          });
-        });
-      });
-
-      context('with initial URL value', () => {
-        beforeEach(() => {
-          defineProperty(window, 'location', {
-            value: {
-              get pathname() {
-                return '/base/5/';
-              },
-            },
-          });
-        });
-
-        it('returns initial value', () => {
-          const { getByDisplayValue } = render(<TestComponent basePath={basePath} />);
-
-          expect(getByDisplayValue('5')).to.be.ok();
-        });
-      });
-
-      context('with initial URL value, no trailing slash', () => {
-        beforeEach(() => {
-          defineProperty(window, 'location', {
-            value: {
-              get pathname() {
-                return '/base/5';
-              },
-            },
-          });
-        });
-
-        it('returns initial value', () => {
-          const { getByDisplayValue } = render(<TestComponent basePath={basePath} />);
-
-          expect(getByDisplayValue('5')).to.be.ok();
-        });
-      });
-    });
   });
 });
