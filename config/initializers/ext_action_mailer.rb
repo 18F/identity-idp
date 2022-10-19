b8f658bcb518a8b23a5701a1f5c39f76e29b4863
Mitchell Henke <mitchell.henke@gsa.gov>
2022-10-19 10:35:17 -0500

    use prepend instead of alias_method

--- config/initializers/ext_action_mailer.rb
+++ config/initializers/ext_action_mailer.rb
@@ -1,8 +1,16 @@
 # Monkeypatches the MessageDelivery to add deliver_now_or_later that
 # can route between #deliver_now and #deliver_later
+module DeliverLaterArgumentChecker
+  def deliver_later(...)
+    MailerSensitiveInformationChecker.check_for_sensitive_pii!(@params, @args, @action)
+    super(...)
+  end
+end
+
 module ActionMailer
   class MessageDelivery
-    alias_method :original_deliver_later, :deliver_later
+    prepend DeliverLaterArgumentChecker
+
     def deliver_now_or_later(opts = {})
       MailerSensitiveInformationChecker.check_for_sensitive_pii!(@params, @args, @action)
       # rubocop:disable IdentityIdp/MailLaterLinter
@@ -12,11 +20,6 @@ module ActionMailer
         deliver_now
       end
       # rubocop:enable IdentityIdp/MailLaterLinter
-    end
-
-    def deliver_later(opts = {})
-      MailerSensitiveInformationChecker.check_for_sensitive_pii!(@params, @args, @action)
-      original_deliver_later(opts)
     end
   end
 end
