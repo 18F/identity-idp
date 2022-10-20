fdf8bf029a0710b350c2558e3a1ebf4245effcf5
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-20 13:10:17 -0400

    Add preview for DownloadButton component

---
+++ spec/components/previews/download_button_component_preview.rb
@@ -0,0 +1,13 @@
+class DownloadButtonComponentPreview < ViewComponent::Preview
+  # @!group Kitchen Sink
+  def default
+    render(DownloadButtonComponent.new(file_data: 'File Data', file_name: 'file_name.txt'))
+  end
+  # @!endgroup
+
+  # @param file_data text
+  # @param file_name text
+  def playground(file_data: 'File Data', file_name: 'file_name.txt')
+    render(DownloadButtonComponent.new(file_data: file_data, file_name: file_name))
+  end
+end
