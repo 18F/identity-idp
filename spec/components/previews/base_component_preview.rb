5c805d58b4068ceb8dbfd93ffce0c7e9afbb2fb6
Andrew Duthie <andrew.duthie@gsa.gov>
2022-10-21 12:14:05 -0400

    Add StatusPageComponent component preview

--- spec/components/previews/base_component_preview.rb
+++ spec/components/previews/base_component_preview.rb
@@ -17,4 +17,10 @@ class BaseComponentPreview < ViewComponent::Preview
   def form_instance
     {}
   end
+
+  # rubocop:disable Layout/LineLength
+  def example_long_content
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et tincidunt libero, quis eleifend dui. Quisque dui velit, euismod ac arcu in, vehicula suscipit dui. Vivamus sed justo justo. Nunc a feugiat libero. Nulla dapibus blandit nisl, ac ultrices sapien dapibus ut. Vivamus convallis elementum mi pulvinar elementum. Quisque at aliquet nibh. Donec sed magna ut ipsum auctor dapibus. Proin leo metus, placerat eu finibus sed, consequat eu urna. Nunc tristique purus sollicitudin, luctus nisi eu, commodo tortor. Praesent mattis dictum diam ac sodales.'
+  end
+  # rubocop:enable Layout/LineLength
 end
