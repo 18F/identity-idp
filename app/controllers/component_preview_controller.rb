1663bded8730ebcf1e1b4201f44f650db5d9180b
Zach Margolis <zachary.margolis@gsa.gov>
2022-10-24 10:54:46 -0700

    Make these conditional too

--- app/controllers/component_preview_controller.rb
+++ app/controllers/component_preview_controller.rb
@@ -4,8 +4,8 @@ class ComponentPreviewController < ViewComponentsController
     helper Lookbook::PreviewHelper
     include Lookbook::PreviewController
     include ScriptHelper
-  end
 
-  helper_method :enqueue_component_scripts
-  alias_method :enqueue_component_scripts, :render_javascript_pack_once_tags
+    helper_method :enqueue_component_scripts
+    alias_method :enqueue_component_scripts, :render_javascript_pack_once_tags
+  end
 end
