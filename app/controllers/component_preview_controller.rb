5603a0ad8c51dbecf2993edebb26fcca6e7313ea
Zach Margolis <zachary.margolis@gsa.gov>
2022-10-24 10:47:53 -0700

    Add stub class so Zeitwerk doesn't complain about the file not defining a class

--- app/controllers/component_preview_controller.rb
+++ app/controllers/component_preview_controller.rb
@@ -1,9 +1,15 @@
-class ComponentPreviewController < ViewComponentsController
-  include ActionView::Helpers::AssetTagHelper
-  helper Lookbook::PreviewHelper
-  include Lookbook::PreviewController
-  include ScriptHelper
+if IdentityConfig.store.component_previews_enabled
+  # Define a stub class, in cases where this file is eager loaded but the
+  # dependencies have not been loaded
+  class ComponentPreviewController < ApplicationController; end
+else
+  class ComponentPreviewController < ViewComponentsController
+    include ActionView::Helpers::AssetTagHelper
+    helper Lookbook::PreviewHelper
+    include Lookbook::PreviewController
+    include ScriptHelper
 
-  helper_method :enqueue_component_scripts
-  alias_method :enqueue_component_scripts, :render_javascript_pack_once_tags
-end if IdentityConfig.store.component_previews_enabled # only define the class conditionally
+    helper_method :enqueue_component_scripts
+    alias_method :enqueue_component_scripts, :render_javascript_pack_once_tags
+  end
+end
