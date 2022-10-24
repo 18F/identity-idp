d3c242c27d6bb7fe03e89fc5d75ddda5310d109d
Melissa Miller <melissa.miller@gsa.gov>
2022-10-24 12:36:56 -0400

    LG 7255 - Add API call to "we are retrieving" page (#7149)
    * Add API call to "we are retrieving" page (7255)
    
    Why:
    Inherited proofing users want a logical place in the flow to retrieve their data
    
    changelog: Internal, Inherited Proofing, We are Retrieving API call
    
    async state doesn't work
    
    async works
    
    * Move mark step complete out of module
    
    * Add additional comments for error path
    
    * Refactoring
    
    saving work
    
    saving work
    
    * Service Provider-agnostic Inherited Proofing
    services
    
    ...so that we can obtain Service Provider-agnostic Inherited Proofing
    services in request/response-related code and in other code that
    does not have direct access to Session.
    
    * Job works, refactoring async
    
    saving work
    
    saving work
    
    Bug fix and clean up
    
    * Fixing spec support
    
    * Additional clean up
    
    * Changed perform now to later
    
    * Formatting change
    
    * Remove unused include
    
    * Formatting and removing unused calls
    
    * Better refactoring
    
    * Add existing session check
    
    * Linting
    
    * Transition to new service files
    
    * Remove unneeded files
    
    Co-authored-by: gangelo <web.gma@gmail.com>

--- app/services/idv/steps/inherited_proofing/user_pii_managable.rb
+++ app/services/idv/steps/inherited_proofing/user_pii_managable.rb
@@ -2,20 +2,16 @@ module Idv
   module Steps
     module InheritedProofing
       module UserPiiManagable
-        include UserPiiRetrievable
-
-        def inherited_proofing_save_user_pii_to_session!
-          inherited_proofing_save_session!
+        def inherited_proofing_save_user_pii_to_session!(user_pii)
+          inherited_proofing_save_session!(user_pii)
           inherited_proofing_skip_steps!
         end
 
         private
 
-        def inherited_proofing_save_session!
-          return unless inherited_proofing_form_response.success?
-
+        def inherited_proofing_save_session!(user_pii)
           flow_session[:pii_from_user] =
-            flow_session[:pii_from_user].to_h.merge(inherited_proofing_user_pii)
+            flow_session[:pii_from_user].to_h.merge(user_pii)
           # This is unnecessary, but added for completeness. Any subsequent FLOWS we
           # might splice into will pull from idv_session['applicant'] and merge into
           # flow_session[:pii_from_user] anyhow in their #initialize(r); any subsequent
