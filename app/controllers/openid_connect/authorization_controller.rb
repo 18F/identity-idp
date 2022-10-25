783a3981424042f006c475a9d0e5161c72958962
John Maxwell <john.maxwell@gsa.gov>
2022-10-25 13:44:32 -0400

    Removed more ial2_strict

--- app/controllers/openid_connect/authorization_controller.rb
+++ app/controllers/openid_connect/authorization_controller.rb
@@ -100,8 +100,7 @@ module OpenidConnect
       (@authorize_form.ial2_requested? &&
        (current_user.decorate.identity_not_verified? ||
        decorated_session.requested_more_recent_verification?)) ||
-       current_user.decorate.reproof_for_irs?(service_provider: current_sp) ||
-       identity_needs_strict_ial2_verification?
+       current_user.decorate.reproof_for_irs?(service_provider: current_sp)
     end
 
     def build_authorize_form_from_params
