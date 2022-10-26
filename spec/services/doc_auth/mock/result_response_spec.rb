09068630ed441a03414456c14a25221ddff4c96b
John Skiles Skinner <john.skinner@gsa.gov>
2022-10-26 13:41:01 -0500

    Indentation

--- spec/services/doc_auth/mock/result_response_spec.rb
+++ spec/services/doc_auth/mock/result_response_spec.rb
@@ -259,21 +259,21 @@ RSpec.describe DocAuth::Mock::ResultResponse do
 
     let(:input) do
       <<~YAML
-      document:
-        type: license
-        first_name: Susan
-        last_name: Smith
-        middle_name: Q
-        address1: 1 Microsoft Way
-        address2: Apt 3
-        city: Bayside
-        state: NY
-        zipcode: '11364'
-        dob: 10/06/1938
-        phone: +1 314-555-1212
-        state_id_number: '123456789'
-        state_id_type: drivers_license
-        state_id_jurisdiction: 'NY'
+        document:
+          type: license
+          first_name: Susan
+          last_name: Smith
+          middle_name: Q
+          address1: 1 Microsoft Way
+          address2: Apt 3
+          city: Bayside
+          state: NY
+          zipcode: '11364'
+          dob: 10/06/1938
+          phone: +1 314-555-1212
+          state_id_number: '123456789'
+          state_id_type: drivers_license
+          state_id_jurisdiction: 'NY'
       YAML
     end
 
@@ -304,13 +304,13 @@ RSpec.describe DocAuth::Mock::ResultResponse do
       )
     end
   end
-  
+
   context 'with a yaml file containing a read error' do
     let(:input) do
       <<~YAML
-      image_metrics:
-        back:
-          HorizontalResolution: 100
+        image_metrics:
+          back:
+            HorizontalResolution: 100
       YAML
     end
 
