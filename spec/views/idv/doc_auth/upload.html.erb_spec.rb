e06cfa8a73232b44ea9c1672328e9df822e37c86
John Maxwell <john.maxwell@gsa.gov>
2022-10-28 16:34:20 -0400

    More dead tests

--- spec/views/idv/doc_auth/upload.html.erb_spec.rb
+++ spec/views/idv/doc_auth/upload.html.erb_spec.rb
@@ -11,16 +11,6 @@ describe 'idv/doc_auth/upload.html.erb' do
     allow(view).to receive(:user_signing_up?).and_return(false)
   end
 
-  context 'with liveness enabled' do
-    it 'renders liveness content' do
-      render template: 'idv/doc_auth/upload'
-
-      expect(rendered).to have_content(t('doc_auth.headings.upload_liveness_enabled'))
-      expect(rendered).to have_content(t('doc_auth.info.upload_liveness_enabled'))
-      expect(rendered).to have_content(t('doc_auth.headings.upload_from_phone_liveness_enabled'))
-    end
-  end
-
   context 'with liveness disabled' do
     it 'does not render liveness content' do
       render template: 'idv/doc_auth/upload'
