665f7d9a83d229ad8445dd987547859cc6c7b8cb
Mitchell Henke <mitchell.henke@gsa.gov>
2022-10-31 09:50:50 -0500

    testing

--- app/jobs/heartbeat_job.rb
+++ app/jobs/heartbeat_job.rb
@@ -5,11 +5,14 @@ class HeartbeatJob < ApplicationJob
     IdentityJobLogSubscriber.new.logger.info(
       {
         name: 'queue_metric.good_job',
+        gc_stat: GC.stat,
+        object_space: ObjectSpace.count_objects,
+        pid: Process.pid,
         # borrowed from: https://github.com/bensheldon/good_job/blob/main/engine/app/controllers/good_job/dashboards_controller.rb#L35
-        num_finished: GoodJob::Execution.finished.count,
-        num_unfinished: GoodJob::Execution.unfinished.count,
-        num_running: GoodJob::Execution.running.count,
-        num_errors: GoodJob::Execution.where.not(error: nil).count,
+        # num_finished: GoodJob::Execution.finished.count,
+        # num_unfinished: GoodJob::Execution.unfinished.count,
+        # num_running: GoodJob::Execution.running.count,
+        # num_errors: GoodJob::Execution.where.not(error: nil).count,
       }.to_json,
     )
 
