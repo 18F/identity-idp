f75dd44d774ba7b1f85e4f9b34694da6b9a721bf
Zach Margolis <zachary.margolis@gsa.gov>
2022-11-01 08:02:21 -0700

    Add content-type: application/json headers for stubbed responses

--- spec/support/usps_ipp_helper.rb
+++ spec/support/usps_ipp_helper.rb
@@ -1,38 +1,49 @@
 module UspsIppHelper
   def stub_request_token
     stub_request(:post, %r{/oauth/authenticate}).to_return(
-      status: 200, body: UspsInPersonProofing::Mock::Fixtures.request_token_response,
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_token_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
   def stub_request_facilities
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/getIppFacilityList}).to_return(
-      status: 200, body: UspsInPersonProofing::Mock::Fixtures.request_facilities_response,
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_facilities_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
   def stub_request_enroll
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/optInIPPApplicant}).to_return(
-      status: 200, body: UspsInPersonProofing::Mock::Fixtures.request_enroll_response,
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_enroll_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
   def stub_request_enroll_bad_request_response
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/optInIPPApplicant}).to_return(
-      status: 400, body: UspsInPersonProofing::Mock::Fixtures.request_enroll_bad_request_response,
+      status: 400,
+      body: UspsInPersonProofing::Mock::Fixtures.request_enroll_bad_request_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
   def stub_request_enroll_internal_server_error_response
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/optInIPPApplicant}).to_return(
-      status: 500, body: UspsInPersonProofing::Mock::Fixtures.
-        internal_server_error_response
+      status: 500,
+      body: UspsInPersonProofing::Mock::Fixtures.internal_server_error_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
   def stub_request_enroll_invalid_response
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/optInIPPApplicant}).to_return(
-      status: 200, body: UspsInPersonProofing::Mock::Fixtures.request_enroll_invalid_response,
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_enroll_invalid_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
@@ -44,8 +55,9 @@ module UspsIppHelper
 
   def request_expired_proofing_results_args
     {
-      status: 400, body: UspsInPersonProofing::Mock::Fixtures.
-        request_expired_proofing_results_response
+      status: 400,
+      body: UspsInPersonProofing::Mock::Fixtures.request_expired_proofing_results_response,
+      headers: { 'content-type' => 'application/json' },
     }
   end
 
@@ -56,8 +68,11 @@ module UspsIppHelper
   end
 
   def request_failed_proofing_results_args
-    { status: 200,
-      body: UspsInPersonProofing::Mock::Fixtures.request_failed_proofing_results_response }
+    {
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_failed_proofing_results_response,
+      headers: { 'content-type' => 'application/json' },
+    }
   end
 
   def stub_request_failed_suspected_fraud_proofing_results
@@ -70,7 +85,8 @@ module UspsIppHelper
     {
       status: 200,
       body: UspsInPersonProofing::Mock::
-      Fixtures.request_failed_suspected_fraud_proofing_results_response,
+        Fixtures.request_failed_suspected_fraud_proofing_results_response,
+      headers: { 'content-type' => 'application/json' },
     }
   end
 
@@ -78,7 +94,8 @@ module UspsIppHelper
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/getProofingResults}).to_return(
       status: 200,
       body: UspsInPersonProofing::Mock::
-      Fixtures.request_passed_proofing_unsupported_id_results_response,
+        Fixtures.request_passed_proofing_unsupported_id_results_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
@@ -86,7 +103,8 @@ module UspsIppHelper
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/getProofingResults}).to_return(
       status: 200,
       body: UspsInPersonProofing::Mock::
-      Fixtures.request_passed_proofing_unsupported_status_results_response,
+        Fixtures.request_passed_proofing_unsupported_status_results_response,
+      headers: { 'content-type' => 'application/json' },
     )
   end
 
@@ -98,8 +116,9 @@ module UspsIppHelper
 
   def request_passed_proofing_results_args
     {
-      status: 200, body: UspsInPersonProofing::Mock::Fixtures.
-        request_passed_proofing_results_response
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_passed_proofing_results_response,
+      headers: { 'content-type' => 'application/json' },
     }
   end
 
@@ -111,8 +130,8 @@ module UspsIppHelper
 
   def request_in_progress_proofing_results_args
     {
-      status: 400, body: UspsInPersonProofing::Mock::Fixtures.
-        request_in_progress_proofing_results_response,
+      status: 400,
+      body: UspsInPersonProofing::Mock::Fixtures.request_in_progress_proofing_results_response,
       headers: { 'content-type' => 'application/json' }
     }
   end
@@ -146,13 +165,15 @@ module UspsIppHelper
 
   def stub_request_proofing_results_with_invalid_response
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/getProofingResults}).to_return(
-      status: 200, body: 'invalid',
+      status: 200,
+      body: 'invalid',
     )
   end
 
   def stub_request_enrollment_code
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/requestEnrollmentCode}).to_return(
-      status: 200, body: UspsInPersonProofing::Mock::Fixtures.request_enrollment_code_response,
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_enrollment_code_response,
       headers: { 'content-type' => 'application/json' }
     )
   end
