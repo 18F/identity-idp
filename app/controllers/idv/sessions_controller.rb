a9fefe5385d1d0906bb1374d14e0e6938b68308a
Zach Margolis <zachary.margolis@gsa.gov>
2022-11-01 10:40:06 -0700

    Inline mixins to make the PR diff smaller and easier to understand

--- app/controllers/idv/sessions_controller.rb
+++ app/controllers/idv/sessions_controller.rb
@@ -1,32 +1,27 @@
 module Idv
   class SessionsController < ApplicationController
     include IdvSession
-    include InheritedProofingSession
+    include InheritedProofingConcern
 
     before_action :confirm_two_factor_authenticated
 
     def destroy
-      destroy_idv
-      destroy_inherited_proofing if inherited_proofing?
+      cancel_verification_attempt_if_pending_profile
+      cancel_in_person_enrollment_if_exists
+      cancel_inherited_proofing_if_exists
+      analytics.idv_start_over(
+        step: location_params[:step],
+        location: location_params[:location],
+      )
+      user_session['idv/doc_auth'] = {}
+      user_session['idv/in_person'] = {}
+      user_session['idv/inherited_proofing'] = {}
+      idv_session.clear
+      Pii::Cacher.new(current_user, user_session).delete
       redirect_to idv_url
     end
 
     private
-
-    def location_params
-      params.permit(:step, :location).to_h.symbolize_keys
-    end
-
-    def destroy_idv
-      cancel_processing
-      clear_session
-      log_analytics
-    end
-
-    def cancel_processing
-      cancel_verification_attempt_if_pending_profile
-      cancel_in_person_enrollment_if_exists
-    end
 
     def cancel_verification_attempt_if_pending_profile
       return if current_user.profiles.gpo_verification_pending.blank?
@@ -40,18 +35,17 @@ module Idv
         cancel_stale_establishing_enrollments_for_user(current_user)
     end
 
-    def clear_session
-      user_session['idv/doc_auth'] = {}
-      user_session['idv/in_person'] = {}
-      idv_session.clear
-      Pii::Cacher.new(current_user, user_session).delete
+    def cancel_inherited_proofing_if_exists
+      return if !inherited_proofing?
+      # LG-7128: Implement Inherited Proofing analytics here.
+      # analytics.inherited_proofing_start_over(
+      #   step: location_params[:step],
+      #   location: location_params[:location],
+      # )
     end
 
-    def log_analytics
-      analytics.idv_start_over(
-        step: location_params[:step],
-        location: location_params[:location],
-      )
+    def location_params
+      params.permit(:step, :location).to_h.symbolize_keys
     end
   end
 end
