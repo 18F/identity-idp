50e53f121f05c4337bd07a09d34073dcf6ce3df0
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-11-02 04:29:31 -0700

    Patch to VAIP sessions controller changes (#7269)
    * Inline mixins to make the PR diff smaller and easier to understand
    
    * Inline shared examples that are only used once (not shared)
    
    * Remove unused shared example
    
    * Remove line length override and fix errors
    
    * fix typo

--- spec/features/idv/inherited_proofing/inherited_proofing_cancel_spec.rb
+++ spec/features/idv/inherited_proofing/inherited_proofing_cancel_spec.rb
@@ -1,36 +1,5 @@
 require 'rails_helper'
 
-# rubocop:disable Layout/LineLength
-shared_examples 'steps up to "Get started..." are completed' do
-  it 'should have current path equal to the Getting Started page' do
-    expect(page).to have_current_path(/inherited_proofing[?|\/].*get_started/)
-  end
-end
-
-shared_examples 'steps up to "How verifying..." are completed' do
-  it 'should have current path equal to the How Verifying (agreement step) page' do
-    expect(page).to have_current_path(/inherited_proofing[?|\/].*agreement/)
-  end
-end
-
-shared_examples 'steps up to "We are retrieving..." are completed' do
-  it 'should have current path equal to the We are retrieving (verify_wait step) page' do
-    expect(page).to have_current_path(/inherited_proofing[?|\/].*verify_wait/)
-  end
-end
-
-shared_examples 'steps up to "Verify your information..." are completed' do
-  it 'should have current path equal to the Verify your information (verify_info step) page' do
-    expect(page).to have_current_path(/inherited_proofing[?|\/].*verify_info/)
-  end
-end
-
-shared_examples 'the user is redirected to the Cancellation View' do
-  it 'redirects to the Cancellations view' do
-    expect(page).to have_current_path(idv_inherited_proofing_cancel_path(step: :get_started))
-  end
-end
-
 # Simulates a user (in this case, a VA inherited proofing-authorized user)
 # coming over to login.gov from a service provider, and hitting the
 # OpenidConnect::AuthorizationController#index action.
@@ -60,8 +29,10 @@ end
 
 def complete_idv_steps_up_to_inherited_proofing_we_are_retrieving_step(user,
                                                                        expect_accessible: false)
-  complete_idv_steps_up_to_inherited_proofing_how_verifying_step user,
-                                                                 expect_accessible: expect_accessible
+  complete_idv_steps_up_to_inherited_proofing_how_verifying_step(
+    user,
+    expect_accessible: expect_accessible,
+  )
   unless current_path.match?(/inherited_proofing[?|\/].*verify_wait/)
     check t('inherited_proofing.instructions.consent', app_name: APP_NAME), allow_label_click: true
     click_on t('inherited_proofing.buttons.continue')
@@ -93,7 +64,9 @@ feature 'inherited proofing cancel process', :js do
         complete_idv_steps_up_to_inherited_proofing_get_started_step user
       end
 
-      it_behaves_like 'steps up to "Get started..." are completed'
+      it 'should have current path equal to the Getting Started page' do
+        expect(page).to have_current_path(/inherited_proofing[?|\/].*get_started/)
+      end
 
       context 'when clicking the "Start Over" button from the "Cancel" view' do
         before do
@@ -137,7 +110,9 @@ feature 'inherited proofing cancel process', :js do
         complete_idv_steps_up_to_inherited_proofing_how_verifying_step user
       end
 
-      it_behaves_like 'steps up to "How verifying..." are completed'
+      it 'should have current path equal to the How Verifying (agreement step) page' do
+        expect(page).to have_current_path(/inherited_proofing[?|\/].*agreement/)
+      end
 
       context 'when clicking the "Start Over" button from the "Cancel" view' do
         before do
@@ -180,18 +155,21 @@ feature 'inherited proofing cancel process', :js do
     # includes ActiveJob to move to the next step. Marking the necessary step as completed below,
     # does not seem to do the trick for some reason. This has been manually verified in the
     # meantime.
-    xcontext 'from the "We are retrieving your information..." view, and clicking the "Cancel" link' do
+    xcontext 'from the "We are retrieving your information..." view, and clicking "Cancel"' do
       before do
-        allow_any_instance_of(Idv::Steps::InheritedProofing::AgreementStep).to receive(:enqueue_job).and_wrap_original do |target, *args|
-          target.receiver.mark_step_complete(:agreement)
-          # Omit the original call.
-          # target.call(*args)
-          nil
-        end
+        allow_any_instance_of(Idv::Steps::InheritedProofing::AgreementStep).
+          to receive(:enqueue_job).and_wrap_original do |target, *args|
+            target.receiver.mark_step_complete(:agreement)
+            # Omit the original call.
+            # target.call(*args)
+            nil
+          end
         complete_idv_steps_up_to_inherited_proofing_we_are_retrieving_step user
       end
 
-      it_behaves_like 'steps up to "We are retrieving..." are completed'
+      it 'should have current path equal to the We are retrieving (verify_wait step) page' do
+        expect(page).to have_current_path(/inherited_proofing[?|\/].*verify_wait/)
+      end
 
       context 'when clicking the "Start Over" button from the "Cancel" view' do
         before do
@@ -235,7 +213,9 @@ feature 'inherited proofing cancel process', :js do
         complete_idv_steps_up_to_inherited_proofing_verify_your_info_step user
       end
 
-      it_behaves_like 'steps up to "Verify your information..." are completed'
+      it 'should have current path equal to the Verify your information (verify_info step) page' do
+        expect(page).to have_current_path(/inherited_proofing[?|\/].*verify_info/)
+      end
 
       context 'when clicking the "Start Over" button from the "Cancel" view' do
         before do
@@ -275,4 +255,3 @@ feature 'inherited proofing cancel process', :js do
     end
   end
 end
-# rubocop:enable Layout/LineLength
