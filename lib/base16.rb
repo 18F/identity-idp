ebaeede9c2bb56b865bba358d46b24b6f315befb
Zach Margolis <zachary.margolis@gsa.gov>
2022-11-02 09:23:52 -0700

    even faster with pack/unpack!!

--- lib/base16.rb
+++ lib/base16.rb
@@ -1,18 +1,10 @@
 # See https://www.rfc-editor.org/rfc/rfc4648#section-8
 class Base16
   def self.encode16(str)
-    output = ''
-    str.bytes.each do |char|
-      output << char.to_s(16).upcase.rjust(2, "0")
-    end
-    output
+    str.unpack('H*').first
   end
 
   def self.decode16(str)
-    output = ''
-    str.chars.each_slice(2) do |chars|
-      output << chars.join.to_i(16).chr
-    end
-    output
+    [str].pack('H*')
   end
 end
