746c4b948f485c58198c6b4e864a65e855508573
Rwolfe-Nava <richardwolfe@navapbc.com>
2022-11-03 15:47:17 -0400

    linter fixes

--- app/jobs/irs_attempts_events_batch_job.rb
+++ app/jobs/irs_attempts_events_batch_job.rb
@@ -1,33 +1,32 @@
 class IrsAttemptsEventsBatchJob < ApplicationJob
   queue_as :default
 
-  def perform(subject_timestamp = Time.zone.now - 1.hour, dir_path = "./attempts_api_output")
+  def perform(subject_timestamp = Time.zone.now - 1.hour, dir_path = './attempts_api_output')
     return nil unless IdentityConfig.store.irs_attempt_api_enabled
-    
+
     events = IrsAttemptsApi::RedisClient.new.read_events(timestamp: subject_timestamp)
     event_values = events.values.join("\r\n")
-   
+
     decoded_key_der = Base64.strict_decode64(IdentityConfig.store.irs_attempt_api_public_key)
     pub_key = OpenSSL::PKey::RSA.new(decoded_key_der)
 
     result = IrsAttemptsApi::EnvelopeEncryptor.encrypt(
       data: event_values, timestamp: subject_timestamp, public_key: pub_key,
     )
-    
+
     # write to a file and store on the disk until S3 is setup
     begin
-      Dir.mkdir(dir_path) unless File.exists?(dir_path)
+      Dir.mkdir(dir_path) unless File.exist?(dir_path)
 
       file = File.open("#{dir_path}/#{result.filename}", 'wb')
-      file.write(result.encrypted_data) 
+      file.write(result.encrypted_data)
     rescue IOError => e
-      puts e
+      Rails.logger.debug e
     ensure
-      file.close unless file.nil?
+      file&.close
     end
     return file.path
 
     # Write the file to S3 instead of whatever dir_path winds up being
-
   end
 end
--- spec/jobs/irs_attempts_events_batch_job_spec.rb
+++ spec/jobs/irs_attempts_events_batch_job_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe IrsAttemptsEventsBatchJob, type: :job do
   describe '#perform' do
-
-    events = { "key1": "SomeEncryptedEvent1" }
+    events = { key1: 'SomeEncryptedEvent1' }
     Result = Struct.new(:filename, :iv, :encrypted_key, :encrypted_data, keyword_init: true)
 
     let(:redis_client) { instance_double(IrsAttemptsApi::RedisClient) }
@@ -13,58 +12,55 @@ RSpec.describe IrsAttemptsEventsBatchJob, type: :job do
 
     context 'IRS attempts API is enabled' do
       before do
-
         allow(IdentityConfig.store).to receive(:irs_attempt_api_enabled).and_return(true)
 
-        allow(IrsAttemptsApi::RedisClient)
-        .to receive(:new)
-        .and_return(redis_client)
-  
-        allow(redis_client)
-          .to receive(:read_events)
-          .and_return(events)
-  
-        allow(IrsAttemptsApi::EnvelopeEncryptor)
-        .to receive(:encrypt)
-        .and_return(
-          Result.new(
-            filename: "test_filename",
-            encrypted_data: "EnvelopeEncryptedEvents",
+        allow(IrsAttemptsApi::RedisClient).
+          to receive(:new).
+          and_return(redis_client)
+
+        allow(redis_client).
+          to receive(:read_events).
+          and_return(events)
+
+        allow(IrsAttemptsApi::EnvelopeEncryptor).
+          to receive(:encrypt).
+          and_return(
+            Result.new(
+              filename: 'test_filename',
+              encrypted_data: 'EnvelopeEncryptedEvents',
+            ),
           )
-        )
 
-        allow(File)
-          .to receive(:exists?)
-          .and_return(true)
+        allow(File).
+          to receive(:exists?).
+          and_return(true)
 
-        allow(File)
-          .to receive(:open)
-          .and_return(file_double)
+        allow(File).
+          to receive(:open).
+          and_return(file_double)
 
-        allow(file_double)
-          .to receive(:write)
-        
-        allow(file_double)
-          .to receive(:close)
+        allow(file_double).
+          to receive(:write)
 
-        allow(file_double)
-          .to receive(:path)
-          .and_return("./attempts_api_output/test_filename")
+        allow(file_double).
+          to receive(:close)
 
+        allow(file_double).
+          to receive(:path).
+          and_return('./attempts_api_output/test_filename')
       end
-      
+
       it 'batches and writes attempt events to an encrypted file' do
-        # event hash values are read from redis, 
+        # event hash values are read from redis,
         # then wrote to a file that then is passed through envelope encryptor
 
         # batch job is run hourly by default
         # batches events from the beginning of the previous hour by default
         # file is stored at:  "./attempts_api_output" by default
 
-        file_path = IrsAttemptsEventsBatchJob.perform_now()
+        file_path = IrsAttemptsEventsBatchJob.perform_now
 
-        expect(file_path).to eq("./attempts_api_output/test_filename")
-        
+        expect(file_path).to eq('./attempts_api_output/test_filename')
       end
     end
 
@@ -74,12 +70,9 @@ RSpec.describe IrsAttemptsEventsBatchJob, type: :job do
       end
 
       it 'returns nil if IRS attempts API is not enabled' do
-
-        file_path = IrsAttemptsEventsBatchJob.perform_now()
+        file_path = IrsAttemptsEventsBatchJob.perform_now
         expect(file_path).to eq(nil)
-
       end
     end
-
   end
 end
