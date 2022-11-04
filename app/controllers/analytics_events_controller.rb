6a662006121829c6746b3e1f715966c6dbadb2d4
Zach Margolis <zachary.margolis@gsa.gov>
2022-11-04 16:26:44 -0700

    Revert "introduce a rubocop failure on purpsoe"
    This reverts commit 0a8013c35e59d66402eeaef2758cea77f042e4ce.

--- app/controllers/analytics_events_controller.rb
+++ app/controllers/analytics_events_controller.rb
@@ -7,7 +7,7 @@ class AnalyticsEventsController < ApplicationController
   JSON_FILE = Rails.root.join('public', 'api', '_analytics-events.json')
 
   def index
-    if File.exists?(JSON_FILE)
+    if File.exist?(JSON_FILE)
       expires_in 15.minutes, public: true
 
       send_file JSON_FILE, type: 'application/json', disposition: 'inline'
