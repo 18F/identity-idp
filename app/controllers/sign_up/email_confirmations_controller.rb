c45015baeb709ab07b5130809b5b0f0f58415c59
Luis <lmatos@flexion.us>
2022-11-08 16:48:51 -0600

    LG-8044 De-duplicate email_confirmation event
    changelog: Internal, Attempts API, Standardize events

--- app/controllers/sign_up/email_confirmations_controller.rb
+++ app/controllers/sign_up/email_confirmations_controller.rb
@@ -21,6 +21,10 @@ module SignUp
 
     def process_successful_confirmation
       process_valid_confirmation_token
+      irs_attempts_api_tracker.user_registration_email_confirmation(
+        email: @email_address&.email,
+        success: true,
+      )
       request_id = params.fetch(:_request_id, '')
       redirect_to sign_up_enter_password_url(
         request_id: request_id, confirmation_token: @confirmation_token,
