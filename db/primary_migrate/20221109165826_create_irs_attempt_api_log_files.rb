e38a39b076740e1bf68bcf0286e0b02428f72d40
Rwolfe-Nava <87499456+Rwolfe-Nava@users.noreply.github.com>
2022-11-15 18:17:51 -0500

    Integrating Attempt API batch job with S3 (#7323)
    * WIP initial commit
    
    changelog: Internal, Attempts API, Integrating batch job with s3
    
    * rough batch job S3 integration; Unit test very WIP.
    
    * more wip on spec
    
    * added bucket_name check
    
    * appeasing rubocop
    
    * added index to requested_time on db record
    
    * Mattw/lg 7470 tweaks (#7342)
    
    * LG-7470 | encrypt() now takes key as a string
    
    Outside of its own unit test, every place we were calling
    EnvelopeEncryptor#encrypt had the public key as a string.
    Just accept that and do the RSA juggling ourselves.
    
    * Clean up test a bit
    
    * lintfix
    
    * Now with less dumb!
    
    * db migration for requested_time index
    
    * removed comments per feedback
    
    * Update spec/models/irs_attempt_api_log_file_spec.rb
    
    Co-authored-by: Matt Wagner <mattwagner@navapbc.com>
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

---
+++ db/primary_migrate/20221109165826_create_irs_attempt_api_log_files.rb
@@ -0,0 +1,12 @@
+class CreateIrsAttemptApiLogFiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :irs_attempt_api_log_files do |t|
+      t.string :filename
+      t.string :iv
+      t.text :encrypted_key
+      t.datetime :requested_time, index: true
+
+      t.timestamps
+    end
+  end
+end
