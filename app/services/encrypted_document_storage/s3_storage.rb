4424f81990cffd0ec5e3e88387eed7596fa96378
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-11-16 10:53:47 -0500

    implement s3 storage

--- app/services/encrypted_document_storage/s3_storage.rb
+++ app/services/encrypted_document_storage/s3_storage.rb
@@ -1,7 +1,21 @@
 module EncryptedDocumentStorage
   class S3Storage
     def write_document(encrypted_document:, reference:)
-      # TODO
+      s3_client.put_object(
+        bucket: 'TODO-use-a-real-bucket',
+        body: encrypted_document,
+        key: reference,
+      )
+    end
+
+    private
+
+    def s3_client
+      Aws::S3::Client.new(
+        http_open_timeout: 5,
+        http_read_timeout: 5,
+        compute_checksums: false,
+      )
     end
   end
 end
--- spec/services/encrypted_document_storage/s3_storage_spec.rb
+++ spec/services/encrypted_document_storage/s3_storage_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 RSpec.describe EncryptedDocumentStorage::S3Storage do
   describe '#write_document' do
-    it 'writes the document to S3'
+    it 'writes the document to S3' do
+      encrypted_document = 'hello, i am the encrypted document.'
+      reference = '123abc'
+
+      storage = EncryptedDocumentStorage::S3Storage.new
+
+      stubbed_s3_client = Aws::S3::Client.new(stub_responses: true)
+      allow(storage).to receive(:s3_client).and_return(stubbed_s3_client)
+
+      expect(stubbed_s3_client).to receive(:put_object).and_call_original
+      stubbed_s3_client.stub_responses(
+        :put_object,
+        -> (context) {
+          params = context.params
+          expect(params[:bucket]).to eq('TODO-use-a-real-bucket')
+          expect(params[:key]).to eq(reference)
+          expect(params[:body]).to eq(encrypted_document)
+        },
+      )
+
+      storage.write_document(encrypted_document: encrypted_document, reference: reference)
+    end
   end
 end
