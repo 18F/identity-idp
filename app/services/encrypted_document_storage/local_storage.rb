c79ce45770d25ad34a49a2ea36086c1ae57f1732
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-11-16 11:12:44 -0500

    local storage implementation

--- app/services/encrypted_document_storage/local_storage.rb
+++ app/services/encrypted_document_storage/local_storage.rb
@@ -1,7 +1,13 @@
 module EncryptedDocumentStorage
   class LocalStorage
     def write_document(encrypted_document:, reference:)
-      # TODO
+      FileUtils.mkdir_p(tmp_document_storage_dir)
+      filepath = tmp_document_storage_dir.join(reference)
+      File.write(filepath, encrypted_document)
+    end
+
+    def tmp_document_storage_dir
+      Rails.root.join('tmp/encrypted_doc_storage')
     end
   end
 end
--- spec/services/encrypted_document_storage/local_storage_spec.rb
+++ spec/services/encrypted_document_storage/local_storage_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe EncryptedDocumentStorage::LocalStorage do
   describe '#write_document' do
-    it 'writes the document to the disk'
+    it 'writes the document to the disk' do
+      encrypted_document = "hello, i'm the encrypted document."
+      reference = SecureRandom.uuid
+
+      EncryptedDocumentStorage::LocalStorage.new.write_document(
+        encrypted_document: encrypted_document,
+        reference: reference,
+      )
+
+      result = File.read(
+        Rails.root.join('tmp', 'encrypted_doc_storage', reference),
+      )
+      expect(result).to eq(encrypted_document)
+    end
   end
 end
