4201810c62c6c819dc2448d58528bf7c93913476
Matt Hinz <matt.hinz@gsa.gov>
2022-11-17 13:18:51 -0800

    Add content_type to DataUrlImage

--- app/services/idv/data_url_image.rb
+++ app/services/idv/data_url_image.rb
@@ -2,27 +2,26 @@ module Idv
   class DataUrlImage
     class InvalidUrlFormatError < StandardError; end
 
+    attr_reader :content_type
+
     def initialize(data_url)
       header_and_data = data_url.delete_prefix('data:')
       raise InvalidUrlFormatError.new if header_and_data == data_url
       header, data = header_and_data.split(',', 2)
-      @header = header
+      content_type, encoding = header.split(';', 2)
+
+      @content_type = content_type
+      @base64_encoded = encoding == 'base64'
       @data = data
     end
 
     # @return [String]
     def read
-      if base64_encoded?
+      if @base64_encoded
         Base64.decode64(@data)
       else
         Addressable::URI.unencode(@data)
       end
-    end
-
-    private
-
-    def base64_encoded?
-      @header.end_with?(';base64')
     end
   end
 end
