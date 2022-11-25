018ceb8fdae64f69ec4dcc82e47af843886baff3
Matt Hinz <matt.hinz@gsa.gov>
2022-11-25 10:49:19 -0800

    Add document escrow fields to Attempts API (LG-8057) (#7381)
    * Add content_type to DataUrlImage
    
    * First pass at wiring doc upload into attempts API
    
    changelog: Improvements, IRS Attempts API, Add document escrow fields to IRS attempts API
    
    * Additional tests
    
    * WIP
    
    * Update form test to roundtrip documents
    
    Write documents to encrypted storage and read them back
    
    * Use front_image & back_image lets for tests
    
    * Update ImageUploadsController spec
    
    * Test Attempts API includes images on  validation failure
    
    * Update app/forms/idv/api_image_upload_form.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update app/services/encrypted_document_storage/local_storage.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update *_image -> *_image_uuid
    
    Clarify that these args hold UUIDs identifying the front/back images
    
    * Use let() for regexes in tests
    
    avoid polluting global namespace
    
    * Add doc escrow parameter descriptions
    
    * Minimize unnecessary verbosity
    
    * Lint issues
    
    * Reduce number of document escrow fields
    
    50% off!
    
    * add "filename" to fields
    
    try and make it clearer what these things are
    
    * Remove empty test
    
    whoops
    
    * Update app/services/encrypted_document_storage/document_writer.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update spec from `should` to `is_expected.to`
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/services/encrypted_document_storage/write_document_result.rb
+++ app/services/encrypted_document_storage/write_document_result.rb
@@ -1,9 +1,8 @@
 module EncryptedDocumentStorage
   WriteDocumentResult = Struct.new(
-    :front_uuid,
-    :back_uuid,
-    :front_encryption_key,
-    :back_encryption_key,
+    :front_filename,
+    :back_filename,
+    :encryption_key,
     keyword_init: true,
   )
 end
