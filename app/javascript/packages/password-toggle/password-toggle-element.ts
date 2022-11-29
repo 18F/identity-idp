224e1aaff2707ffe465ca44f37dafb8170cd1316
Andrew Duthie <andrew.duthie@gsa.gov>
2022-11-29 14:44:10 -0500

    Try icon toggle for password visibility

--- app/javascript/packages/password-toggle/password-toggle-element.ts
+++ app/javascript/packages/password-toggle/password-toggle-element.ts
@@ -1,12 +1,22 @@
 import { trackEvent } from '@18f/identity-analytics';
+import { tooltip } from 'identity-style-guide';
 
 interface PasswordToggleLabels {
   show: string;
   hide: string;
 }
 
+function replaceHashFragment(url: string, nextHashFragment: string): string {
+  const parsedURL = new URL(url);
+  parsedURL.hash = nextHashFragment;
+  return parsedURL.toString();
+}
+
 class PasswordToggleElement extends HTMLElement {
+  #isVisible: boolean = false;
+
   connectedCallback() {
+    tooltip.on(this);
     this.toggle.addEventListener('click', () => this.onToggleClick());
     this.setInputType();
   }
@@ -26,14 +36,35 @@ class PasswordToggleElement extends HTMLElement {
     return this.querySelector('.password-toggle__toggle')!;
   }
 
+  get tooltip(): HTMLElement {
+    return this.querySelector('.usa-tooltip__body')!;
+  }
+
+  get icon(): SVGUseElement {
+    return this.querySelector('.usa-icon use')!;
+  }
+
+  get isVisible(): boolean {
+    return this.#isVisible;
+  }
+
+  set isVisible(nextIsVisible: boolean) {
+    this.#isVisible = nextIsVisible;
+    this.setInputType();
+  }
+
   setInputType() {
-    const isCurrentlyHidden = this.toggle.textContent === this.labels.show;
-    this.input.type = isCurrentlyHidden ? 'text' : 'password';
-    this.toggle.textContent = isCurrentlyHidden ? this.labels.hide : this.labels.show;
+    this.input.type = this.isVisible ? 'text' : 'password';
+    this.tooltip.textContent = this.isVisible ? this.labels.hide : this.labels.show;
+    this.icon.href.baseVal = replaceHashFragment(
+      this.icon.href.baseVal,
+      this.isVisible ? 'visibility_off' : 'visibility',
+    );
   }
 
   onToggleClick() {
     this.setInputType();
+    this.isVisible = !this.isVisible;
     trackEvent('Show Password button clicked', { path: window.location.pathname });
   }
 }
