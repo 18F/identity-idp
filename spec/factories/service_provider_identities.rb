f5450aa53e4840fd68d356e172efd73635002935
Matt Wagner <mattwagner@navapbc.com>
2022-11-30 10:37:08 -0500

    LG-8058 | Populate identities consented field (#7374)
    changelog: Internal, Attempts API, Populate last_consented_at field

--- spec/factories/service_provider_identities.rb
+++ spec/factories/service_provider_identities.rb
@@ -7,4 +7,12 @@ FactoryBot.define do
   trait :active do
     last_authenticated_at { Time.zone.now }
   end
+
+  trait :consented do
+    last_consented_at { Time.zone.now - 5.minutes }
+  end
+
+  trait :soft_deleted_5m_ago do
+    deleted_at { Time.zone.now - 5.minutes }
+  end
 end
