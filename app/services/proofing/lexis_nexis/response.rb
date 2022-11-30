0ceb24b06edbdd8b7180a794d581ad6350ccde66
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-11-30 13:04:18 -0500

    Scrub the error message from JSON parse errors of LexisNexis response bodies (#7412)
    The LexisNexis response bodies may contain information that we do not wish to be logged or reported to NewRelic (such as PII).
    
    When a JSON parse error is raised it includes the portion of the JSON that resulted in the error. This commit rescues the JSON parse errors that are raised when a LexisNexis response body is being parsed. These error messages may contain sensitive information. As a result a new JSON parse error is raised in their place with a message that does not include the JSON that caused the error.
    
    [skip changelog]

--- app/services/proofing/lexis_nexis/response.rb
+++ app/services/proofing/lexis_nexis/response.rb
@@ -31,6 +31,14 @@ module Proofing
       # @api private
       def response_body
         @response_body ||= JSON.parse(response.body)
+      rescue JSON::ParserError
+        # IF a JSON parse error occurs the resulting error message will contain the portion of the
+        # response body where the error occured. This portion of the response could potentially
+        # include sensitive informaiton. This commit scrubs the error message by raising a JSON
+        # parse error with a generic message.
+        content_type = response.headers&.[]('Content-Type')
+        error_message = "An error occured parsing the response body JSON, status=#{response.status} content_type=#{content_type}" # rubocop:disable Layout/LineLength
+        raise JSON::ParserError, error_message
       end
 
       private
