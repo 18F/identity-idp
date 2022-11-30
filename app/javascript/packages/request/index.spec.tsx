0d4684e5e7ce584b0369afbbf8665a4aab8ebe14
Matt Gardner <wilburnforce@gmail.com>
2022-11-29 22:48:32 -0500

    Fill out the rest of the tests

--- app/javascript/packages/request/index.spec.tsx
+++ app/javascript/packages/request/index.spec.tsx
@@ -21,11 +21,99 @@ describe('request', () => {
 
     await request('https://example.com', {}, mockGetCSRF);
   });
-  // it('works even if the CSRF token is not found on the page', async () => {});
-  // it('prefers the json prop if both json and body props are provided', async () => {});
-  // it('works with the native body prop', async () => {});
-  // it('includes additional headers supplied in options', async () => {});
-  // it('skips json serialization when json is a boolean', async () => {});
-  // it('converts a POJO to a JSON string with supplied via the json property', async () => {});
-  // it('works when using neither extended json prop nor the csrf flag', async () => {});
+  it('works even if the CSRF token is not found on the page', async () => {
+    sandbox.stub(window, 'fetch').callsFake(() =>
+      Promise.resolve(
+        new Response(new Blob([JSON.stringify({})]), {
+          status: 200,
+        }),
+      ),
+    );
+
+    await request('https://example.com', {}, () => undefined);
+  });
+  it('prefers the json prop if both json and body props are provided', async () => {
+    const preferredData = { prefered: 'data' };
+    sandbox.stub(window, 'fetch').callsFake((url, init = {}) => {
+      expect(init.body).to.equal(JSON.stringify(preferredData));
+
+      return Promise.resolve(
+        new Response(new Blob([JSON.stringify({})]), {
+          status: 200,
+        }),
+      );
+    });
+
+    await request('https://example.com', {
+      json: preferredData,
+      body: JSON.stringify({ bad: 'data' }),
+    });
+  });
+  it('works with the native body prop', async () => {
+    const preferredData = { this: 'works' };
+    sandbox.stub(window, 'fetch').callsFake((url, init = {}) => {
+      expect(init.body).to.equal(JSON.stringify(preferredData));
+
+      return Promise.resolve(
+        new Response(new Blob([JSON.stringify({})]), {
+          status: 200,
+        }),
+      );
+    });
+
+    await request('https://example.com', {
+      body: JSON.stringify(preferredData),
+    });
+  });
+  it('includes additional headers supplied in options', async () => {
+    sandbox.stub(window, 'fetch').callsFake((url, init = {}) => {
+      const headers = init.headers as Headers;
+      expect(headers.get('Some-Fancy')).to.equal('Header');
+
+      return Promise.resolve(
+        new Response(new Blob([JSON.stringify({})]), {
+          status: 200,
+        }),
+      );
+    });
+
+    await request('https://example.com', {
+      headers: {
+        'Some-Fancy': 'Header',
+      },
+    });
+  });
+  it('skips json serialization when json is a boolean', async () => {
+    const preferredData = { this: 'works' };
+    sandbox.stub(window, 'fetch').callsFake((url, init = {}) => {
+      expect(init.body).to.equal(JSON.stringify(preferredData));
+
+      return Promise.resolve(
+        new Response(new Blob([JSON.stringify({})]), {
+          status: 200,
+        }),
+      );
+    });
+
+    await request('https://example.com', {
+      json: true,
+      body: JSON.stringify(preferredData),
+    });
+  });
+  it('converts a POJO to a JSON string with supplied via the json property', async () => {
+    const preferredData = { this: 'works' };
+    sandbox.stub(window, 'fetch').callsFake((url, init = {}) => {
+      expect(init.body).to.equal(JSON.stringify(preferredData));
+
+      return Promise.resolve(
+        new Response(new Blob([JSON.stringify({})]), {
+          status: 200,
+        }),
+      );
+    });
+
+    await request('https://example.com', {
+      json: preferredData,
+    });
+  });
 });
