7be98650214e2274b1e0a45db1f42746ecf39d5e
Andrew Duthie <andrew.duthie@gsa.gov>
2022-12-01 14:09:18 -0500

    build-sass: Improve error recovery in watch mode
    changelog: Internal, Build Tooling, Improve error recovery for local development stylesheet builds

--- app/javascript/packages/build-sass/cli.js
+++ app/javascript/packages/build-sass/cli.js
@@ -46,6 +46,28 @@ function watchOnce(paths, callback) {
 const isSassException = (error) => 'span' in /** @type {SassException} */ (error);
 
 /**
+ * Returns all file paths contained in the given Sass stack trace.
+ *
+ * @example
+ * ```
+ * 'node_modules/identity-style-guide/dist/assets/scss/uswds/core/_functions.scss 35:8     divide()\n' +
+ * 'node_modules/identity-style-guide/dist/assets/scss/uswds/core/mixins/_icon.scss 77:12  add-color-icon()\n' +
+ * 'app/assets/stylesheets/components/_alert.scss 13:5                                     @import\n' +
+ * 'app/assets/stylesheets/components/all.scss 3:9                                         @import\n' +
+ * 'app/assets/stylesheets/application.css.scss 7:9                                        root stylesheet\n',
+ * ```
+ *
+ * @param {string} sassStack Sass stack trace (see example).
+ *
+ * @return {string[]} Array of file paths.
+ */
+const getErrorSassStackPaths = (sassStack) =>
+  sassStack
+    .split(/\.scss \d+:\d+\s+.+?\n/)
+    .filter(Boolean)
+    .map((basename) => `${basename}.scss`);
+
+/**
  * @param {string[]} files
  * @return {Promise<void|void[]>}
  */
@@ -62,9 +84,8 @@ function build(files) {
     /** @param {Error|SassException} error */ (error) => {
       console.error(error);
 
-      if (isWatching && isSassException(error) && error.span.url) {
-        const exceptionPath = fileURLToPath(error.span.url);
-        watchOnce(exceptionPath, () => build(files));
+      if (isWatching && isSassException(error)) {
+        watchOnce(getErrorSassStackPaths(error.sassStack), () => build(files));
       } else {
         throw error;
       }
