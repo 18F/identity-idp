e3998f93ecd05ee588c8b9e13d685237c3086848
Andrew Duthie <andrew.duthie@gsa.gov>
2022-12-01 14:21:58 -0500

    Add more complete example with return value

--- app/javascript/packages/build-sass/cli.js
+++ app/javascript/packages/build-sass/cli.js
@@ -50,11 +50,20 @@ const isSassException = (error) => 'span' in /** @type {SassException} */ (error
  *
  * @example
  * ```
- * 'node_modules/identity-style-guide/dist/assets/scss/uswds/core/_functions.scss 35:8     divide()\n' +
- * 'node_modules/identity-style-guide/dist/assets/scss/uswds/core/mixins/_icon.scss 77:12  add-color-icon()\n' +
- * 'app/assets/stylesheets/components/_alert.scss 13:5                                     @import\n' +
- * 'app/assets/stylesheets/components/all.scss 3:9                                         @import\n' +
- * 'app/assets/stylesheets/application.css.scss 7:9                                        root stylesheet\n',
+ * getErrorStackPaths(
+ *   'node_modules/identity-style-guide/dist/assets/scss/uswds/core/_functions.scss 35:8     divide()\n' +
+ *   'node_modules/identity-style-guide/dist/assets/scss/uswds/core/mixins/_icon.scss 77:12  add-color-icon()\n' +
+ *   'app/assets/stylesheets/components/_alert.scss 13:5                                     @import\n' +
+ *   'app/assets/stylesheets/components/all.scss 3:9                                         @import\n' +
+ *   'app/assets/stylesheets/application.css.scss 7:9                                        root stylesheet\n',
+ * );
+ * // [
+ * //   'node_modules/identity-style-guide/dist/assets/scss/uswds/core/_functions.scss',
+ * //   'node_modules/identity-style-guide/dist/assets/scss/uswds/core/mixins/_icon.scss',
+ * //   'app/assets/stylesheets/components/_alert.scss',
+ * //   'app/assets/stylesheets/components/all.scss',
+ * //   'app/assets/stylesheets/application.css.scss',
+ * // ]
  * ```
  *
  * @param {string} sassStack Sass stack trace (see example).
