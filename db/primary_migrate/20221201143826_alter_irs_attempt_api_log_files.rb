29ff79e0c2350417cdb61dabd6d0ac0cb519b83a
Luis <lmatos@flexion.us>
2022-12-06 15:39:12 -0600

    PR feeback

--- db/primary_migrate/20221201143826_alter_irs_attempt_api_log_files.rb
+++ db/primary_migrate/20221201143826_alter_irs_attempt_api_log_files.rb
@@ -1,6 +1,5 @@
 class AlterIrsAttemptApiLogFiles < ActiveRecord::Migration[7.0]
   def change
     safety_assured { remove_column :irs_attempt_api_log_files, :requested_time, :datetime }
-    add_column :irs_attempt_api_log_files, :requested_time, :string
   end
 end
