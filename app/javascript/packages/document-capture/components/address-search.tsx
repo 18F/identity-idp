4a13e4378686e470d72a630513cca178767b1693
Eileen McFarland <eileenmcfarland@navapbc.com>
2022-12-06 14:33:19 -0500

    rough commit of working inline error

--- app/javascript/packages/document-capture/components/address-search.tsx
+++ app/javascript/packages/document-capture/components/address-search.tsx
@@ -1,6 +1,7 @@
 import { TextInput, Button } from '@18f/identity-components';
 import { request } from '@18f/identity-request';
-import { useState, useCallback, ChangeEvent } from 'react';
+import { useState, useCallback, ChangeEvent, useRef } from 'react';
+import ValidatedField from '@18f/identity-validated-field/validated-field';
 
 interface Location {
   street_address: string;
@@ -12,37 +13,56 @@ interface Location {
 
 interface AddressSearchProps {
   onAddressFound?: (location: Location) => void;
+  registerField: () => {};
 }
 
 export const ADDRESS_SEARCH_URL = '/api/addresses';
 
-function AddressSearch({ onAddressFound = () => {} }: AddressSearchProps) {
+function AddressSearch({ onAddressFound = () => {}, registerField }: AddressSearchProps) {
+  const validatedFieldRef = useRef();
   const [unvalidatedAddressInput, setUnvalidatedAddressInput] = useState('');
   const [addressQuery, setAddressQuery] = useState({} as Location);
-  const handleAddressSearch = useCallback(async () => {
-    const addressCandidates = await request(ADDRESS_SEARCH_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      json: { address: unvalidatedAddressInput },
-    });
 
-    const bestMatchedAddress = addressCandidates[0];
-    setAddressQuery(bestMatchedAddress);
-    onAddressFound(bestMatchedAddress);
-  }, [unvalidatedAddressInput]);
+  const handleAddressSearch = useCallback(
+    async (event) => {
+      event.preventDefault();
+      validatedFieldRef.current.reportValidity();
+      if (unvalidatedAddressInput === '') {
+        return null;
+      }
+      const addressCandidates = await request(ADDRESS_SEARCH_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        json: { address: unvalidatedAddressInput },
+      });
+
+      const bestMatchedAddress = addressCandidates[0];
+      setAddressQuery(bestMatchedAddress);
+      onAddressFound(bestMatchedAddress);
+    },
+    [unvalidatedAddressInput],
+  );
 
   return (
     <>
-      <TextInput
-        value={unvalidatedAddressInput}
-        onChange={(event: ChangeEvent) => {
-          const target = event.target as HTMLInputElement;
-
-          setUnvalidatedAddressInput(target.value);
-        }}
-        label="Search for an address"
-      />
-      <Button onClick={handleAddressSearch}>Search</Button>
+      <ValidatedField
+        ref={validatedFieldRef}
+        messages={{ valueMissing: 'Include a city, state, and ZIP code' }}
+      >
+        <TextInput
+          required
+          ref={registerField('address')}
+          value={unvalidatedAddressInput}
+          onChange={(event: ChangeEvent) => {
+            const target = event.target as HTMLInputElement;
+            setUnvalidatedAddressInput(target.value);
+          }}
+          label="Search for an address"
+        />
+      </ValidatedField>
+      <Button type="submit" onClick={handleAddressSearch}>
+        Search
+      </Button>
       <>{addressQuery.address}</>
     </>
   );
