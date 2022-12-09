97150f281b1db41f9a9bc339e4e18b74cc8db1fa
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-12-09 14:22:12 -0500

    more cleanup

--- spec/support/mailer_helper.rb
+++ spec/support/mailer_helper.rb
@@ -20,7 +20,7 @@ module MailerHelper
       next unless to.present? && sent_mail.to == to
       next unless subject.present? && sent_mail.subject == subject
       if body.present?
-        delivered_body = mail.text_part.decoded.squish
+        delivered_body = sent_mail.text_part.decoded.squish
         body.to_a.each do |expected_body|
           next unless delivered_body.includes?(expected_body)
         end
@@ -36,21 +36,5 @@ module MailerHelper
       Sent mails: #{ActionMailer::Base.deliveries}
     ERROR
     expect(email).to_not be(nil), error_message
-  end
-
-  def expect_delivered_email_at_index(index, hash)
-    mail = ActionMailer::Base.deliveries[index]
-
-    aggregate_failures do
-      expect(mail.to).to eq hash[:to] if hash[:to]
-      expect(mail.subject).to eq hash[:subject] if hash[:subject]
-
-      if hash[:body]
-        delivered_body = mail.text_part.decoded.squish
-        hash[:body].to_a.each do |expected_body|
-          expect(delivered_body).to include(expected_body)
-        end
-      end
-    end
   end
 end
