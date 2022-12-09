ffa4e87617c4de2709f6d4b63ec5e899c4dd04a2
Andrew Duthie <andrew.duthie@gsa.gov>
2022-12-09 10:31:14 -0500

    Fix new_window_link_to link appearance in Safari (#7463)
    * Fix new_window_link_to link appearance in Safari
    
    changelog: Internal, Performance, Improve performance of common layout elements
    
    * Update JavaScript specs for collapsed whitespace

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,13 +1,15 @@
 module LinkHelper
   EXTERNAL_LINK_CLASS = 'usa-link--external'.freeze
 
-  def new_window_link_to(name = nil, options = nil, html_options = {}, &block)
+  def new_window_link_to(name = nil, options = nil, html_options = nil, &block)
     html_options, options, name = options, name, capture(&block) if block
 
+    html_options ||= {}
     html_options[:target] = '_blank'
     html_options[:class] = [*html_options[:class], EXTERNAL_LINK_CLASS]
 
-    name = safe_join([name, content_tag('span', t('links.new_window'), class: 'usa-sr-only')], ' ')
+    name = ERB::Util.unwrapped_html_escape(name).rstrip.html_safe # rubocop:disable Rails/OutputSafety
+    name << content_tag('span', t('links.new_window'), class: 'usa-sr-only')
 
     link_to(name, options, html_options)
   end
