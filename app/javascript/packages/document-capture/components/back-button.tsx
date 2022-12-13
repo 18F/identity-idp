9c2d88fb3a776e1d42011a52630443d9ee7714a5
Eileen McFarland <eileenmcfarland@navapbc.com>
2022-12-13 16:49:55 -0500

    prevent screenreader from reading back 2x on backbutton

--- app/javascript/packages/document-capture/components/back-button.tsx
+++ app/javascript/packages/document-capture/components/back-button.tsx
@@ -10,7 +10,8 @@ interface BackLinkProps extends ComponentProps<typeof Button> {
 function BackButton({ includeBorder = false, ...props }: BackLinkProps) {
   const button = (
     <Button isUnstyled {...props}>
-      &#x2039; {t('forms.buttons.back')}
+      <span aria-hidden="true">&#x2039; </span>
+      <span>{t('forms.buttons.back')}</span>
     </Button>
   );
   if (includeBorder) {
