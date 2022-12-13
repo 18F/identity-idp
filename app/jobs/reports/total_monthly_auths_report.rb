19fb61ca2e1ea1dcf19e537a24c931785164b5dd
Mitchell Henke <mitchell.henke@gsa.gov>
2022-12-13 16:17:11 -0600

    Update GoodJob and remove concurrency controls
    changelog: Internal, Background Jobs, Update GoodJob and remove concurrency controls

--- app/jobs/reports/total_monthly_auths_report.rb
+++ app/jobs/reports/total_monthly_auths_report.rb
@@ -4,13 +4,6 @@ module Reports
   class TotalMonthlyAuthsReport < BaseReport
     REPORT_NAME = 'total-monthly-auths-report'.freeze
 
-    include GoodJob::ActiveJobExtensions::Concurrency
-
-    good_job_control_concurrency_with(
-      total_limit: 1,
-      key: -> { "#{REPORT_NAME}-#{arguments.first}" },
-    )
-
     def perform(_date)
       auth_counts = Db::MonthlySpAuthCount::TotalMonthlyAuthCounts.call
       save_report(REPORT_NAME, auth_counts.to_json, extension: 'json')
