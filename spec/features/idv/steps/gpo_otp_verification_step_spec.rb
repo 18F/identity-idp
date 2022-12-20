d1ae0926fc4efdd38f446afe179daf0d0a5545ea
Matt Hinz <matt.hinz@gsa.gov>
2022-12-20 15:41:42 -0800

    Refactor OTP ThreatMetrix stuff to use shared example

--- spec/features/idv/steps/gpo_otp_verification_step_spec.rb
+++ spec/features/idv/steps/gpo_otp_verification_step_spec.rb
@@ -27,118 +27,39 @@ feature 'idv gpo otp verification step', :js do
   let(:threatmetrix_enabled) { false }
   let(:threatmetrix_review_status) { nil }
 
-  it 'prompts for one-time code at sign in' do
-    sign_in_and_enter_otp
-
-    expect(user.events.account_verified.size).to eq 1
-    expect(page).to_not have_content(t('account.index.verification.reactivate_button'))
+  before do
+    allow(IdentityConfig.store).to receive(:lexisnexis_threatmetrix_enabled).
+      and_return(threatmetrix_enabled)
+    allow(IdentityConfig.store).to receive(:lexisnexis_threatmetrix_required_to_verify).
+      and_return(threatmetrix_enabled)
+    allow(IdentityConfig.store).to receive(:proofing_device_profiling_decisioning_enabled).
+      and_return(threatmetrix_enabled)
   end
 
-  it 'renders an error for an expired GPO OTP' do
-    sign_in_live_with_2fa(user)
-
-    gpo_confirmation_code.update(code_sent_at: 11.days.ago)
-    fill_in t('forms.verify_profile.name'), with: otp
-    click_button t('forms.verify_profile.submit')
-
-    expect(current_path).to eq idv_gpo_verify_path
-    expect(page).to have_content t('errors.messages.gpo_otp_expired')
-
-    user.reload
-
-    expect(user.events.account_verified.size).to eq 0
-    expect(user.active_profile).to be_nil
-  end
-
-  it 'allows a user to resend a letter' do
-    resend_letter
-  end
+  it_behaves_like 'gpo otp verification'
 
   context 'ThreatMetrix enabled' do
-    before do
-      allow(IdentityConfig.store).to receive(:lexisnexis_threatmetrix_enabled).
-        and_return(threatmetrix_enabled)
-      allow(IdentityConfig.store).to receive(:lexisnexis_threatmetrix_required_to_verify).
-        and_return(threatmetrix_enabled)
-      allow(IdentityConfig.store).to receive(:proofing_device_profiling_decisioning_enabled).
-        and_return(threatmetrix_enabled)
-    end
+    let(:threatmetrix_enabled) { true }
 
     context 'ThreatMetrix evaluates as "pass"' do
       let(:threatmetrix_review_status) { 'pass' }
-      it 'redirects to account page after OTP' do
-        sign_in_and_enter_otp
-      end
-      it 'allows requesting a new letter' do
-        resend_letter
-      end
+      it_behaves_like 'gpo otp verification'
     end
 
     context 'ThreatMetrix evaluates as "review"' do
       let(:threatmetrix_review_status) { 'review' }
-      it 'redirects to account page after OTP' do
-        sign_in_and_enter_otp
-      end
-      it 'allows requesting a new letter' do
-        resend_letter
-      end
+      it_behaves_like 'gpo otp verification'
     end
 
     context 'ThreatMetrix evaluates as "reject"' do
       let(:threatmetrix_review_status) { 'reject' }
-      it 'redirects to sad face after OTP' do
-        sign_in_and_enter_otp
-        expect(page).to have_current_path(idv_setup_errors_path)
-      end
-      it 'allows requesting a new letter' do
-        resend_letter
-      end
+      it_behaves_like 'gpo otp verification', redirect_after_verification: :idv_setup_errors_path
     end
 
     context 'ThreatMetrix evaluates as nil' do
       let(:threatmetrix_review_status) { nil }
-      it 'redirects to account page after OTP' do
-        sign_in_and_enter_otp
-      end
-      it 'allows requesting a new letter' do
-        resend_letter
-      end
+      it_behaves_like 'gpo otp verification'
     end
-  end
-
-  def sign_in_and_enter_otp
-    sign_in_live_with_2fa(user)
-
-    expect(current_path).to eq idv_gpo_verify_path
-    expect(page).to have_content t('idv.messages.gpo.resend')
-
-    gpo_confirmation_code
-    fill_in t('forms.verify_profile.name'), with: otp
-    click_button t('forms.verify_profile.submit')
-  end
-
-  def resend_letter
-    allow(Base32::Crockford).to receive(:encode).and_return(otp)
-
-    sign_in_live_with_2fa(user)
-
-    expect(GpoConfirmation.count).to eq(0)
-    expect(GpoConfirmationCode.count).to eq(0)
-    click_on t('idv.messages.gpo.resend')
-
-    expect_step_indicator_current_step(t('step_indicator.flows.idv.get_a_letter'))
-
-    click_on t('idv.buttons.mail.resend')
-
-    expect(GpoConfirmation.count).to eq(1)
-    expect(GpoConfirmationCode.count).to eq(1)
-    expect(current_path).to eq idv_come_back_later_path
-
-    confirmation_code = GpoConfirmationCode.first
-    otp_fingerprint = Pii::Fingerprinter.fingerprint(otp)
-
-    expect(confirmation_code.otp_fingerprint).to eq(otp_fingerprint)
-    expect(confirmation_code.profile).to eq(profile)
   end
 
   context 'with gpo feature disabled' do
---
+++ spec/support/idv_examples/gpo_otp_verification.rb
@@ -0,0 +1,63 @@
+shared_examples 'gpo otp verification' do |options|
+  include IdvStepHelper
+
+  it 'prompts for one-time code at sign in' do
+    sign_in_live_with_2fa(user)
+
+    expect(current_path).to eq idv_gpo_verify_path
+    expect(page).to have_content t('idv.messages.gpo.resend')
+
+    gpo_confirmation_code
+    fill_in t('forms.verify_profile.name'), with: otp
+    click_button t('forms.verify_profile.submit')
+
+    if options && options[:redirect_after_verification]
+      expected_path = send(options[:redirect_after_verification])
+      expect(page).to have_current_path(expected_path)
+      user.reload
+    end
+
+    expect(user.events.account_verified.size).to eq 1
+    expect(page).to_not have_content(t('account.index.verification.reactivate_button'))
+  end
+
+  it 'renders an error for an expired GPO OTP' do
+    sign_in_live_with_2fa(user)
+
+    gpo_confirmation_code.update(code_sent_at: 11.days.ago)
+    fill_in t('forms.verify_profile.name'), with: otp
+    click_button t('forms.verify_profile.submit')
+
+    expect(current_path).to eq idv_gpo_verify_path
+    expect(page).to have_content t('errors.messages.gpo_otp_expired')
+
+    user.reload
+
+    expect(user.events.account_verified.size).to eq 0
+    expect(user.active_profile).to be_nil
+  end
+
+  it 'allows a user to resend a letter' do
+    allow(Base32::Crockford).to receive(:encode).and_return(otp)
+
+    sign_in_live_with_2fa(user)
+
+    expect(GpoConfirmation.count).to eq(0)
+    expect(GpoConfirmationCode.count).to eq(0)
+    click_on t('idv.messages.gpo.resend')
+
+    expect_step_indicator_current_step(t('step_indicator.flows.idv.get_a_letter'))
+
+    click_on t('idv.buttons.mail.resend')
+
+    expect(GpoConfirmation.count).to eq(1)
+    expect(GpoConfirmationCode.count).to eq(1)
+    expect(current_path).to eq idv_come_back_later_path
+
+    confirmation_code = GpoConfirmationCode.first
+    otp_fingerprint = Pii::Fingerprinter.fingerprint(otp)
+
+    expect(confirmation_code.otp_fingerprint).to eq(otp_fingerprint)
+    expect(confirmation_code.profile).to eq(profile)
+  end
+end
