c169cf9cb2e4d0d6fab94a40c0b3d81b2ca40912
John Skiles Skinner <john.skinner@gsa.gov>
2022-12-20 15:13:27 -0800

    Test logic in should_log? method

--- spec/services/proofing/lexis_nexis/verification_error_parser_spec.rb
+++ spec/services/proofing/lexis_nexis/verification_error_parser_spec.rb
@@ -20,6 +20,30 @@ RSpec.describe Proofing::LexisNexis::VerificationErrorParser do
       expect(errors[:Discovery]).to eq(nil) # This should be absent since it passed
       expect(errors[:SomeOtherProduct]).to eq(response_body['Products'][1])
       expect(errors[:InstantVerify]).to eq(response_body['Products'][2])
+      expect(errors[:'Execute Instant Verify']).to be_a Hash # log product error
     end
+
+    it 'should not log a passing response containing no important information' do
+      response_body['Products'].first['ProductType'] = 'Fake Product'
+      response_body['Products'].first['ProductStatus'] = 'pass'
+      response_body['Products'].first['Items'].map{|i| i.delete('ItemReason') }
+
+      expect(errors[:'Execute Instant Verify']).to eq(nil)
+    end
+
+    it 'should log any Instant Verify response, including a pass' do
+      response_body['Products'].first['ProductStatus'] = 'pass'
+      response_body['Products'].first['Items'].map{|i| i.delete('ItemReason') }
+
+      expect(errors[:'Execute Instant Verify']).to be_a Hash
+    end
+
+    it 'should log any response with an ItemReason, including a pass' do
+      response_body['Products'].first['ProductType'] = 'Fake Product'
+      response_body['Products'].first['ProductStatus'] = 'pass'
+
+      expect(errors[:'Execute Instant Verify']).to be_a Hash
+    end
+
   end
 end
