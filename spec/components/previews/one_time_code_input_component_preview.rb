7a991b9e1ebe66ddd07fa1597c944d4321c77992
Andrew Duthie <andrew.duthie@gsa.gov>
2022-12-21 09:08:11 -0500

    Add component preview for One Time Code input
    changelog: Internal, Components, Add One Time Code Input component to local development previews

---
+++ spec/components/previews/one_time_code_input_component_preview.rb
@@ -0,0 +1,18 @@
+class OneTimeCodeInputComponentPreview < BaseComponentPreview
+  # @!group Preview
+  # @display form true
+  def numeric
+    render(OneTimeCodeInputComponent.new(form: form_builder))
+  end
+
+  def alphanumeric
+    render(OneTimeCodeInputComponent.new(form: form_builder, numeric: false))
+  end
+  # @!endgroup
+
+  # @display form true
+  # @param numeric toggle
+  def workbench(numeric: false)
+    render(OneTimeCodeInputComponent.new(form: form_builder, numeric: numeric))
+  end
+end
