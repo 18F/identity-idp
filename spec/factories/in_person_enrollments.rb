3583311e9439f38408b3d7fca6837eced11a0219
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2022-12-27 15:43:25 -0500

    Add additional specs

--- spec/factories/in_person_enrollments.rb
+++ spec/factories/in_person_enrollments.rb
@@ -1,33 +1,34 @@
 FactoryBot.define do
   factory :in_person_enrollment do
-    user { association :user, :signed_up }
-    profile { association :profile, user: user }
     current_address_matches_id { true }
+    profile { association :profile, user: user }
     selected_location_details { { name: 'BALTIMORE' } }
+    unique_id { InPersonEnrollment.generate_unique_id }
+    user { association :user, :signed_up }
 
     trait :establishing do
       status { :establishing }
     end
 
     trait :pending do
-      status { :pending }
       enrollment_code { Faker::Number.number(digits: 16) }
       enrollment_established_at { Time.zone.now }
+      status { :pending }
       status_updated_at { Time.zone.now }
     end
 
     trait :expired do
-      status { :expired }
       enrollment_code { Faker::Number.number(digits: 16) }
       enrollment_established_at { Time.zone.now }
+      status { :expired }
       status_check_attempted_at { Time.zone.now }
       status_updated_at { Time.zone.now }
     end
 
     trait :failed do
-      status { :failed }
       enrollment_code { Faker::Number.number(digits: 16) }
       enrollment_established_at { Time.zone.now }
+      status { :failed }
       status_check_attempted_at { Time.zone.now }
       status_updated_at { Time.zone.now }
     end
--- spec/views/idv/in_person/ready_to_verify/show.html.erb_spec.rb
+++ spec/views/idv/in_person/ready_to_verify/show.html.erb_spec.rb
@@ -5,21 +5,21 @@ describe 'idv/in_person/ready_to_verify/show.html.erb' do
 
   let(:user) { build(:user) }
   let(:profile) { build(:profile, user: user) }
-  let(:enrollment_code) { '2048702198804358' }
   let(:current_address_matches_id) { true }
   let(:selected_location_details) do
     JSON.parse(UspsInPersonProofing::Mock::Fixtures.enrollment_selected_location_details)
   end
   let(:created_at) { Time.zone.parse('2022-07-13') }
+  let(:sp_url) { 'http://service.provider.gov' }
+  let(:service_provider) { build(:service_provider, return_to_sp_url: sp_url) }
   let(:enrollment) do
-    InPersonEnrollment.new(
-      user: user,
-      profile: profile,
-      enrollment_code: enrollment_code,
-      unique_id: InPersonEnrollment.generate_unique_id,
-      created_at: created_at,
+    build(
+      :in_person_enrollment, :pending,
       current_address_matches_id: current_address_matches_id,
+      profile: profile,
       selected_location_details: selected_location_details,
+      service_provider: service_provider,
+      user: user
     )
   end
   let(:presenter) { Idv::InPerson::ReadyToVerifyPresenter.new(enrollment: enrollment) }
@@ -28,6 +28,22 @@ describe 'idv/in_person/ready_to_verify/show.html.erb' do
   before do
     assign(:presenter, presenter)
     allow(view).to receive(:step_indicator_steps).and_return(step_indicator_steps)
+  end
+
+  it 'displays a link back to the service provider' do
+    render
+
+    expect(rendered).to have_content(service_provider.friendly_name)
+  end
+
+  context 'when the user is not coming from a service provider' do
+    let(:service_provider) { nil }
+
+    it 'does not display a link back to a service provider' do
+      render
+
+      expect(rendered).not_to have_content('You may now close this window or')
+    end
   end
 
   context 'with enrollment where current address matches id' do
