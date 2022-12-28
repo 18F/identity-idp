c47248099337accd8d65f3e4f3d4c53506d97615
Matt Gardner <wilburnforce@gmail.com>
2022-12-28 12:56:11 -0500

    LG-8407: Display load state across sequence of requests (#7538)
    * Create new hook uspsSearch
    
    * Expres hook output as keys
    
    * Re-order and group behaviors
    
    * Originate validatedFieldRef within AddressSearch
    
    * Rename variables to more generic
    
    * Troubleshoot duplicate requests
    
    * changelog: Upcoming Features, In-Person Proofing, show loading state across entire sequence of requests
    
    * Update tests
    
    * IPP component enforces async validation (handles no address results)
    
    * Update app/javascript/packages/document-capture/components/in-person-location-post-office-search-step.tsx
    
    Co-authored-by: Tim Bradley <90272033+NavaTim@users.noreply.github.com>
    
    * Move hook to address-search
    
    * Fix ts/lint
    
    * Update tests
    
    * Remove unnecessarily state
    
    * Drop useCallback with no dependencies
    
    Co-authored-by: Tim Bradley <90272033+NavaTim@users.noreply.github.com>

--- app/javascript/packages/document-capture/components/in-person-location-post-office-search-step.tsx
+++ app/javascript/packages/document-capture/components/in-person-location-post-office-search-step.tsx
@@ -2,90 +2,23 @@ import { useState, useEffect, useCallback, useRef, useContext } from 'react';
 import { useI18n } from '@18f/identity-react-i18n';
 import { PageHeading } from '@18f/identity-components';
 import { request } from '@18f/identity-request';
-import useSWR from 'swr';
 import BackButton from './back-button';
 import AnalyticsContext from '../context/analytics';
-import AddressSearch from './address-search';
+import AddressSearch, {
+  transformKeys,
+  snakeCase,
+  LocationQuery,
+  LOCATIONS_URL,
+} from './address-search';
 import InPersonLocations, { FormattedLocation } from './in-person-locations';
-
-interface PostOffice {
-  address: string;
-  city: string;
-  distance: string;
-  name: string;
-  phone: string;
-  saturday_hours: string;
-  state: string;
-  sunday_hours: string;
-  tty: string;
-  weekday_hours: string;
-  zip_code_4: string;
-  zip_code_5: string;
-}
-
-interface LocationQuery {
-  streetAddress: string;
-  city: string;
-  state: string;
-  zipCode: string;
-  address: string;
-}
-
-export const LOCATIONS_URL = '/verify/in_person/usps_locations';
-
-const formatLocation = (postOffices: PostOffice[]) => {
-  const formattedLocations = [] as FormattedLocation[];
-  postOffices.forEach((po: PostOffice, index) => {
-    const location = {
-      formattedCityStateZip: `${po.city}, ${po.state}, ${po.zip_code_5}-${po.zip_code_4}`,
-      id: index,
-      distance: po.distance,
-      name: po.name,
-      phone: po.phone,
-      saturdayHours: po.saturday_hours,
-      streetAddress: po.address,
-      sundayHours: po.sunday_hours,
-      tty: po.tty,
-      weekdayHours: po.weekday_hours,
-    } as FormattedLocation;
-    formattedLocations.push(location);
-  });
-  return formattedLocations;
-};
-
-const snakeCase = (value: string) =>
-  value
-    .split(/(?=[A-Z])/)
-    .join('_')
-    .toLowerCase();
-
-// snake case the keys of the location
-const transformKeys = (location: object, predicate: (key: string) => string) => {
-  const sendObject = {};
-  Object.keys(location).forEach((key) => {
-    sendObject[predicate(key)] = location[key];
-  });
-  return sendObject;
-};
-
-const requestUspsLocations = async (address: LocationQuery): Promise<FormattedLocation[]> => {
-  const response = await request<PostOffice[]>(LOCATIONS_URL, {
-    method: 'post',
-    json: { address: transformKeys(address, snakeCase) },
-  });
-
-  return formatLocation(response);
-};
 
 function InPersonLocationPostOfficeSearchStep({ onChange, toPreviousStep, registerField }) {
   const { t } = useI18n();
-  const [foundAddress, setFoundAddress] = useState<LocationQuery | null>(null);
   const [inProgress, setInProgress] = useState(false);
   const [autoSubmit, setAutoSubmit] = useState(false);
   const { setSubmitEventMetadata } = useContext(AnalyticsContext);
-  const { data: locationResults } = useSWR([LOCATIONS_URL, foundAddress], ([, address]) =>
-    address ? requestUspsLocations(address) : null,
-  );
+  const [locationResults, setLocationResults] = useState<FormattedLocation[] | null>(null);
+  const [foundAddress, setFoundAddress] = useState<LocationQuery | null>(null);
 
   // ref allows us to avoid a memory leak
   const mountedRef = useRef(false);
@@ -140,21 +73,15 @@ function InPersonLocationPostOfficeSearchStep({ onChange, toPreviousStep, regist
     [locationResults, inProgress],
   );
 
-  const handleFoundAddress = useCallback((address) => {
-    setFoundAddress({
-      streetAddress: address.street_address,
-      city: address.city,
-      state: address.state,
-      zipCode: address.zip_code,
-      address: address.address,
-    });
-  }, []);
-
   return (
     <>
       <PageHeading>{t('in_person_proofing.headings.po_search.location')}</PageHeading>
       <p>{t('in_person_proofing.body.location.po_search.po_search_about')}</p>
-      <AddressSearch onAddressFound={handleFoundAddress} registerField={registerField} />
+      <AddressSearch
+        registerField={registerField}
+        onFoundAddress={setFoundAddress}
+        onFoundLocations={setLocationResults}
+      />
       {locationResults && (
         <InPersonLocations
           locations={locationResults}
