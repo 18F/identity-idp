81d4655cb635a31afd91b74946c94eece37e7147
Mitchell Henke <mitchell.henke@gsa.gov>
2022-12-29 13:51:59 -0600

    Update GoodJob and remove concurrency controls (#7491)
    changelog: Internal, Background Jobs, Update GoodJob and remove concurrency controls

--- app/jobs/get_usps_proofing_results_job.rb
+++ app/jobs/get_usps_proofing_results_job.rb
@@ -10,14 +10,6 @@ class GetUspsProofingResultsJob < ApplicationJob
   ]
 
   queue_as :long_running
-  include GoodJob::ActiveJobExtensions::Concurrency
-
-  good_job_control_concurrency_with(
-    total_limit: 1,
-    key: 'get_usps_proofing_results',
-  )
-
-  discard_on GoodJob::ActiveJobExtensions::Concurrency::ConcurrencyExceededError
 
   def email_analytics_attributes(enrollment)
     {
