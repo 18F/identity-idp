74ad74633ac964b95c7f70624bd03e379e23fea5
Mitchell Henke <mitchell.henke@gsa.gov>
2022-12-29 09:14:25 -0600

    Remove account reset health check
    changelog: Internal, Health Checks, Remove account reset health check

--- spec/controllers/health/health_controller_spec.rb
+++ spec/controllers/health/health_controller_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe Health::HealthController do
     context 'when all checked resources are healthy' do
       it 'returns a successful JSON response' do
         allow(DatabaseHealthChecker).to receive(:simple_query).and_return('foo')
-        allow(AccountResetHealthChecker).to receive(:check).
-          and_return(HealthCheckSummary.new(healthy: true, result: 'foo'))
 
         get :index
         json = JSON.parse(response.body, symbolize_names: true)
@@ -14,7 +12,6 @@ RSpec.describe Health::HealthController do
         expect(response.status).to eq(200)
         expect(json[:healthy]).to eq(true)
         expect(json[:statuses][:database][:healthy]).to eq(true)
-        expect(json[:statuses][:account_reset][:healthy]).to eq(true)
       end
     end
 
@@ -22,8 +19,6 @@ RSpec.describe Health::HealthController do
       it 'returns an unsuccessful JSON response' do
         allow(DatabaseHealthChecker).to receive(:simple_query).
           and_raise(RuntimeError.new('canceling statement due to statement timeout'))
-        allow(AccountResetHealthChecker).to receive(:check).
-          and_return(HealthCheckSummary.new(healthy: true, result: 'foo'))
 
         get :index
         json = JSON.parse(response.body, symbolize_names: true)
@@ -31,7 +26,6 @@ RSpec.describe Health::HealthController do
         expect(json[:healthy]).to eq(false)
         expect(json[:statuses][:database][:result]).
           to include('canceling statement due to statement timeout')
-        expect(json[:statuses][:account_reset][:healthy]).to eq(true)
         expect(response.status).to eq(500)
       end
     end
@@ -40,8 +34,6 @@ RSpec.describe Health::HealthController do
       it 'returns an unsuccessful JSON response' do
         allow(DatabaseHealthChecker).to receive(:simple_query).
           and_raise(RuntimeError.new('canceling statement due to statement timeout'))
-        allow(AccountResetHealthChecker).to receive(:check).
-          and_return(HealthCheckSummary.new(healthy: false, result: 'foo'))
 
         get :index
         json = JSON.parse(response.body, symbolize_names: true)
@@ -49,7 +41,6 @@ RSpec.describe Health::HealthController do
         expect(json[:healthy]).to eq(false)
         expect(json[:statuses][:database][:result]).
           to include('canceling statement due to statement timeout')
-        expect(json[:statuses][:account_reset][:healthy]).to eq(false)
         expect(response.status).to eq(500)
       end
     end
