7a83967d973ec4b4c9f906284ca4b8b2b3926dc0
Sonia Connolly <sonia.connolly@gsa.gov>
2022-12-29 16:34:35 -0800

    Show step indicator (and remove puts HELLOOOOO)

--- app/controllers/idv/verify_pii_controller.rb
+++ app/controllers/idv/verify_pii_controller.rb
@@ -1,16 +1,17 @@
 module Idv
   class VerifyPiiController < ApplicationController
-    # TODO Verify needed
     include StepIndicatorConcern
 
     def show
       local_params = {
         pii: pii,
         step_url: method(:idv_doc_auth_step_url),
+        step_indicator: step_indicator_params,
         step_template: "idv/doc_auth/verify",
-        flow_session: flow_session
+        flow_namespace: 'idv',
+        flow_session: flow_session,
       }
-      puts "HELLOO!!!!"
+
       render template: 'layouts/flow_step', locals: local_params
     end
 
@@ -19,10 +20,17 @@ module Idv
       user_session['idv/doc_auth']
     end
 
-
     # copied from verify_step
     def pii
       flow_session[:pii_from_doc]
+    end
+
+    # modified from flow_state_machine
+    def step_indicator_params
+      {
+        steps: Flows::DocAuthFlow::STEP_INDICATOR_STEPS,
+        current_step: :verify_info,
+      }
     end
 
   end
