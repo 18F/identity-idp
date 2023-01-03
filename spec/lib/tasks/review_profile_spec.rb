9330ed99f01ad8a6890f2b8cd5c49000f46de0ca
Matt Hinz <matt.hinz@gsa.gov>
2023-01-03 09:47:50 -0800

    LG-8392: Email user when OK'ing their ThreatMetrix status (#7555)
    * Email user when OK'ing their ThreatMetrix status
    
    When operators manually OK a user's ThreatMetrix status, send the "You verified your identity" email.
    
    changelog: Internal, ThreatMetrix, Email when OK'ing user's ThreatMetrix status
    
    For LG-8392
    
    * s/disavowal_token/build_disavowal_token/
    
    * linty mclinterson

--- spec/lib/tasks/review_profile_spec.rb
+++ spec/lib/tasks/review_profile_spec.rb
@@ -2,30 +2,47 @@ require 'rails_helper'
 require 'rake'
 
 describe 'review_profile' do
+  let(:user) { create(:user, :deactivated_threatmetrix_profile) }
+  let(:task_name) { nil }
+
+  subject(:invoke_task) do
+    Rake::Task[task_name].reenable
+    Rake::Task[task_name].invoke
+  end
+
   before do
     Rake.application.rake_require('lib/tasks/review_profile', [Rails.root.to_s])
     Rake::Task.define_task(:environment)
+    allow(STDIN).to receive(:getpass) { user.uuid }
   end
 
   describe 'users:review:pass' do
+    let(:task_name) { 'users:review:pass' }
+
     it 'sets threatmetrix_review_status to pass' do
-      user = create(:user, :deactivated_threatmetrix_profile)
-
-      allow(STDIN).to receive(:getpass) { user.uuid }
-
-      Rake::Task['users:review:pass'].invoke
+      invoke_task
       expect(user.reload.proofing_component.threatmetrix_review_status).to eq('pass')
       expect(user.reload.profiles.first.active).to eq(true)
+    end
+
+    it 'dispatches account verified alert' do
+      freeze_time do
+        expect(UserAlerts::AlertUserAboutAccountVerified).to receive(:call).with(
+          user: user,
+          date_time: Time.zone.now,
+          disavowal_token: kind_of(String),
+          sp_name: nil,
+        )
+        invoke_task
+      end
     end
   end
 
   describe 'users:review:reject' do
+    let(:task_name) { 'users:review:reject' }
+
     it 'sets threatmetrix_review_status to reject' do
-      user = create(:user, :deactivated_threatmetrix_profile)
-
-      allow(STDIN).to receive(:getpass) { user.uuid }
-
-      Rake::Task['users:review:reject'].invoke
+      invoke_task
       expect(user.reload.proofing_component.threatmetrix_review_status).to eq('reject')
       expect(user.reload.profiles.first.deactivation_reason).to eq('threatmetrix_review_rejected')
     end
