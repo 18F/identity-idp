8175eada7e0e745987d42cc39c56b366c86e88dd
Andrew Duthie <andrew.duthie@gsa.gov>
2023-01-04 10:25:27 -0500

    LG-8489: Improve YAML normalization error tolerance
    changelog: Internal, Build Tooling, Improve YAML normalization error tolerance

--- app/javascript/packages/normalize-yaml/cli.js
+++ app/javascript/packages/normalize-yaml/cli.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+/* eslint-disable no-console */
+
 import { promises as fsPromises } from 'fs';
 import { join } from 'path';
 import prettier from 'prettier';
@@ -24,10 +27,24 @@ const options = {
   ),
 };
 
+/**
+ * @type {Error[]}
+ */
+const errors = [];
+
 Promise.all(
   files.map(async (relativePath) => {
     const absolutePath = join(process.cwd(), relativePath);
     const content = await readFile(absolutePath, 'utf8');
-    await writeFile(absolutePath, normalize(content, options));
+    try {
+      await writeFile(absolutePath, normalize(content, options));
+    } catch (error) {
+      errors.push(new Error(`Error normalizing ${relativePath}: ${error.message}`));
+    }
   }),
-);
+).then(() => {
+  if (errors.length) {
+    errors.forEach((error) => console.error(error.message));
+    process.exit(1);
+  }
+});
