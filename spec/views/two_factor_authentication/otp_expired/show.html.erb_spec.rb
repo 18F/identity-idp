0ab8a4f8fa4876ab49be4578e712a37731dec2e1
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-12 11:06:51 -0500

    create shell for show otp expired shell

--- spec/views/two_factor_authentication/otp_expired/show.html.erb_spec.rb
+++ spec/views/two_factor_authentication/otp_expired/show.html.erb_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 describe 'two_factor_authentication/otp_expired/show.html.erb' do
-  let(:user) { create(:user, :signed_up) }
-
-  before do
-    @presenter = TwoFactorAuthCode::PersonalKeyPresenter.new
-    @personal_key_form = PersonalKeyForm.new(user)
-    allow(view).to receive(:current_user).and_return(user)
+ receive(:current_user).and_return(user)
+ 
   end
 end
