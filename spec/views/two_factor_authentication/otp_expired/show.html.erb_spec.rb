8c527a00df0a205c70dfd66d1ccad679f7c44edc
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-12 12:45:34 -0500

    add basic test

--- spec/views/two_factor_authentication/otp_expired/show.html.erb_spec.rb
+++ spec/views/two_factor_authentication/otp_expired/show.html.erb_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe 'two_factor_authentication/otp_expired/show.html.erb' do
- receive(:current_user).and_return(user)
- 
+  it 'has a localized title' do
+    expect(view).to receive(:title).with(t('titles.otp_expired'))
+
+    render
+  end
+
   end
 end
