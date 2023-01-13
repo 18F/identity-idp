00c667c9422767b04af1328a8a577d236bec0e01
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-13 11:52:24 -0500

    fix decorated session failure

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -2,6 +2,13 @@ module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
     def show
       @otp_delivery_preference = current_user.otp_delivery_preference
+      @sp_name = decorated_session.sp_name
+    end
+
+    private
+
+    def otp_delivery_preference
+      current_user.otp_delivery_preference
     end
   end
 end
