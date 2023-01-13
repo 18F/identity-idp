d5b7ba6a0a6a0bef50608d2632b16ff58004f285
Jonathan Hooper <jonathan.hooper@gsa.gov>
2023-01-13 11:40:45 -0500

    Retire mailcatcher (#7641)
    After we upgraded to Ruby 3.2 we lost support for mailcatcher. Mailcatcher depends on Fixnum was was previously deprecated and is now removed in Rby 3.2
    
    This commit removes mailcatcher and adds 2 new options for previewing emails:
    
    1. Letter opener
    2. File previews
    
    This commit has letter opener as the default method with the ability to switch to the file method by changing configs in your local `application.yml`.
    
    [skip changelog]

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -143,6 +143,7 @@ class IdentityConfig
     config.add(:database_worker_jobs_password, type: :string)
     config.add(:deleted_user_accounts_report_configs, type: :json)
     config.add(:deliver_mail_async, type: :boolean)
+    config.add(:development_mailer_deliver_method, type: :symbol, enum: [:file, :letter_opener])
     config.add(:disable_csp_unsafe_inline, type: :boolean)
     config.add(:disable_email_sending, type: :boolean)
     config.add(:disallow_all_web_crawlers, type: :boolean)
