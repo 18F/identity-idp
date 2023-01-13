3bdc3fe5a7c62078d2e013e126e22b5ada0bcfc5
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-13 10:07:01 -0500

    pass preferred otp delivery preference

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -1,7 +1,7 @@
 module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
     def show
-      @presenter = presenter_for_two_factor_authentication_method
+      @otp_delivery_preference = current_user.otp_delivery_preference
     end
   end
 end
