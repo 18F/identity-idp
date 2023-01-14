db4a3b714d38ff1f9a56f89fe93f6c59561bb3f3
Mitchell Henke <mitchell.henke@gsa.gov>
2023-01-14 10:59:12 -0600

    use authorize form redirect_uri

--- app/controllers/concerns/secure_headers_concern.rb
+++ app/controllers/concerns/secure_headers_concern.rb
@@ -2,12 +2,16 @@ module SecureHeadersConcern
   extend ActiveSupport::Concern
 
   def apply_secure_headers_override(oidc_authorize_form: nil)
-    return if stored_url_for_user.blank?
+    return if oidc_authorize_form.nil? && stored_url_for_user.blank?
 
     authorize_form = oidc_authorize_form || OpenidConnectAuthorizeForm.new(authorize_params)
     return unless authorize_form.valid?
 
-    redirect_uris = csp_uris(redirect_uris: authorize_form.service_provider.redirect_uris)
+    redirect_uri = authorize_form.redirect_uri
+    redirect_uris = csp_uris(
+      redirect_uris: authorize_form.service_provider.redirect_uris,
+      requested_redirect_uri: redirect_uri,
+    )
     override_form_action_csp(redirect_uris)
   end
 
@@ -17,23 +21,24 @@ module SecureHeadersConcern
     request.content_security_policy = policy
   end
 
-  def csp_uris(redirect_uris: nil)
+  def csp_uris(redirect_uris: nil, requested_redirect_uri: nil)
     return ["'self'"] if stored_url_for_user.blank? && redirect_uris.nil?
-    redirect_uris ||= decorated_session.sp_redirect_uris
     # Returns fully formed CSP array w/"'self'" and redirect_uris
     SecureHeadersAllowList.csp_with_sp_redirect_uris(
-      authorize_params[:redirect_uri],
-      redirect_uris,
+      requested_redirect_uri || authorize_params[:redirect_uri],
+      redirect_uris || decorated_session.sp_redirect_uris,
     )
   end
 
   def authorize_params
-    UriService.params(stored_url_for_user)
+    return @authorize_params if defined?(@authorize_params)
+    @authorize_params = UriService.params(stored_url_for_user)
   end
 
   private
 
   def stored_url_for_user
-    sp_session_request_url_with_updated_params
+    return @stored_url_for_user if defined?(@stored_url_for_user)
+    @stored_url_for_user = sp_session_request_url_with_updated_params
   end
 end
