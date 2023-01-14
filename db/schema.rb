0875497177678de7f25e5575a6d45e95f8e2bbc6
Zach Margolis <zachary.margolis@gsa.gov>
2023-01-14 09:19:52 -0800

    redo migration to get more specific index

--- db/schema.rb
+++ db/schema.rb
@@ -559,7 +559,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_202809) do
     t.integer "user_id"
     t.datetime "returned_at", precision: nil
     t.boolean "billable"
-    t.index "((returned_at)::date), issuer", name: "index_sp_return_logs_on_returned_at_date_issuer", where: "(billable = true)"
+    t.index "((returned_at)::date), issuer", name: "index_sp_return_logs_on_returned_at_date_issuer", where: "((billable = true) AND (returned_at IS NOT NULL))"
     t.index ["request_id"], name: "index_sp_return_logs_on_request_id", unique: true
   end
 
