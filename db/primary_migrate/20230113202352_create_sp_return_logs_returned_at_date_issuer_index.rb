1a784f957188aa870fc2be95aaa85b780407cbd1
Zach Margolis <zachmargolis@users.noreply.github.com>
2023-01-14 09:18:13 -0800

    Update db/primary_migrate/20230113202352_create_sp_return_logs_returned_at_date_issuer_index.rb
    Co-authored-by: Mitchell Henke <mitchell.henke@gsa.gov>

--- db/primary_migrate/20230113202352_create_sp_return_logs_returned_at_date_issuer_index.rb
+++ db/primary_migrate/20230113202352_create_sp_return_logs_returned_at_date_issuer_index.rb
@@ -7,7 +7,7 @@ class CreateSpReturnLogsReturnedAtDateIssuerIndex < ActiveRecord::Migration[7.0]
         CREATE INDEX CONCURRENTLY index_sp_return_logs_on_returned_at_date_issuer
         ON public.sp_return_logs
         USING btree ((returned_at::date), issuer)
-        WHERE (billable = true);
+        WHERE (billable = true and returned_at IS NOT NULL);
       SQL
     end
   end
