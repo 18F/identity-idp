0ff586ce8fce0b7719853b650667b024d72a0fc1
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-18 15:25:28 -0500

    fix lint error and test

--- spec/views/two_factor_authentication/otp_expired/show.html.erb_spec.rb
+++ spec/views/two_factor_authentication/otp_expired/show.html.erb_spec.rb
@@ -9,9 +9,28 @@ describe 'two_factor_authentication/otp_expired/show.html.erb' do
 
   context 'when a user selects sms as their otp delivery preference' do
     let(:otp_delivery_preference) { 'sms' }
-    it 'resends the code' do
+    it 'resends the code via sms' do
       render
-      
+
+      resend_path = otp_send_path(
+        otp_delivery_selection_form: {
+          otp_delivery_preference: @otp_delivery_preference,
+          resend: true,
+        },
+      )
+
+      expect(rendered).to have_link(
+        t('links.two_factor_authentication.send_another_code'),
+        href: resend_path,
+      )
+    end
+  end
+
+  context 'when a user selects phone as their otp delivery preference' do
+    let(:otp_delivery_preference) { 'phone' }
+    it 'resends the code via phone' do
+      render
+
       resend_path = otp_send_path(
         otp_delivery_selection_form: {
           otp_delivery_preference: @otp_delivery_preference,
