d2235b6e84ec0ee0b0921fae4605a457115a9454
Malick Diarra <malick.diarra@gsa.gov>
2023-01-18 12:16:35 -0500

    evert "add back migration temporarily"
    This reverts commit 9bd9453b8b9058702f7669d698c10506f9728c2c.

--- db/primary_migrate/20230118163820_add_non_restricted_mfa_required_prompt_skip_date_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddNonRestrictedMfaRequiredPromptSkipDateToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :non_restricted_mfa_required_prompt_skip_date, :date
-  end
-end
