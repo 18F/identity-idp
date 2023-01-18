3ac041edff1e2e2f595f54f146751452afb0e665
Luis <lmatos@flexion.us>
2023-01-18 10:48:09 -0600

    spec fix

--- spec/support/features/irs_attempts_api_tracking_helper.rb
+++ spec/support/features/irs_attempts_api_tracking_helper.rb
@@ -29,7 +29,7 @@ module IrsAttemptsApiTrackingHelper
   def stub_attempts_tracker
     irs_attempts_api_tracker = FakeAttemptsTracker.new
 
-    if !!controller
+    if defined?(controller)
       allow(controller).to receive(:irs_attempts_api_tracker).and_return(irs_attempts_api_tracker)
     else
       allow(self).to receive(:irs_attempts_api_tracker).and_return(irs_attempts_api_tracker)
