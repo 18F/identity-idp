730347864e832b880cd4116ce5470920d61ebb60
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-19 10:13:57 -0500

    remove service provider option and variable

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -2,7 +2,6 @@ module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
     def show
       @otp_delivery_preference = current_user.otp_delivery_preference
-      @sp_name = decorated_session.sp_name
     end
   end
 end
