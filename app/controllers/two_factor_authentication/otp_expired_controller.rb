e05dd1a50e4cdef9c03b4f389baed88101ffef7e
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-20 16:36:26 -0500

    add conditional to show/hide "use another phone number"

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -2,12 +2,17 @@ module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
     def show
       @otp_delivery_preference = otp_delivery_preference
+      @show_try_again_option = show_try_again_option?
     end
 
     private
 
     def otp_delivery_preference
       current_user.otp_delivery_preference
+    end
+
+    def show_try_again_option?
+      !two_factor_enabled?
     end
   end
 end
