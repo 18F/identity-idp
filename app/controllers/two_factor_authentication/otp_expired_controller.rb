0bfe99e53ff55c620004756fba43c9bce9263483
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-20 12:29:31 -0500

    write the rest of the controller test

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -1,7 +1,13 @@
 module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
     def show
-      @otp_delivery_preference = current_user.otp_delivery_preference
+      @otp_delivery_preference = otp_delivery_preference
+    end
+
+    private
+
+    def otp_delivery_preference
+      current_user.otp_delivery_preference
     end
   end
 end
--- spec/controllers/two_factor_authentication/otp_expired_controller_spec.rb
+++ spec/controllers/two_factor_authentication/otp_expired_controller_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 describe TwoFactorAuthentication::OtpExpiredController do
   describe '#show' do
-    it 'renders the page' do
+    it 'renders the page on sign in' do
       sign_in_before_2fa
 
+      get :show
+
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the page when adding a new one time code method' do
+      user = build(:user, otp_delivery_preference: 'voice')
+
+      stub_sign_in_before_2fa(user)
       get :show
 
       expect(response).to render_template(:show)
