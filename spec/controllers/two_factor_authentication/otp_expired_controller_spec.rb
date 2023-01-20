6483d38cf2f3165605eb2cdc07b2216ee49d4f09
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-20 10:34:10 -0500

    add first test

--- spec/controllers/two_factor_authentication/otp_expired_controller_spec.rb
+++ spec/controllers/two_factor_authentication/otp_expired_controller_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe TwoFactorAuthentication::OtpExpiredController do
   describe '#show' do
-    
+    it 'renders the page' do
+      sign_in_before_2fa
+
+      get :show
+
+      expect(response).to render_template(:show)
+    end
   end
 end
