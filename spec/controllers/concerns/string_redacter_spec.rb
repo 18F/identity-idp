ac053c82eed28231f7a6a2bb8708cf925a4b9f26
Sonia Connolly <sonia.connolly@gsa.gov>
2023-01-23 11:13:21 -0800

    Use StringRedacter instead of redact method
    Move test from NewPhoneForm to new StringRedacter spec

---
+++ spec/controllers/concerns/string_redacter_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'StringRedacter' do
+    module Idv
+      class RedactTestController < ApplicationController
+        include StringRedacter
+      end
+    end
+
+  describe '#redact_alphanumeric' do
+    it 'leaves in punctuation and spaces, but removes letters and numbers' do
+      expect(Idv::RedactTestController.new.redact_alphanumeric('+11 (555) DEF-1234')).to eq('+## (###) XXX-####')
+    end
+  end
+end
