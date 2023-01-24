b11769dfd48b02cb172d8097c17fb231cc4a7bb5
Sonia Connolly <sonia.connolly@gsa.gov>
2023-01-24 11:42:52 -0800

    Make redact_alphanumeric a static method per PR feedback

--- spec/controllers/concerns/string_redacter_spec.rb
+++ spec/controllers/concerns/string_redacter_spec.rb
@@ -9,7 +9,7 @@ describe 'StringRedacter' do
 
   describe '#redact_alphanumeric' do
     it 'leaves in punctuation and spaces, but removes letters and numbers' do
-      expect(Idv::RedactTestController.new.redact_alphanumeric('+11 (555) DEF-1234')).
+      expect(StringRedacter.redact_alphanumeric('+11 (555) DEF-1234')).
         to eq('+## (###) XXX-####')
     end
   end
