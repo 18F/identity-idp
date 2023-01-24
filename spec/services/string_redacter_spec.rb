ca27975d06cdd5d8762502819eb8b59542f3f545
Sonia Connolly <sonia.connolly@gsa.gov>
2023-01-24 13:19:05 -0800

    Remove unnecessary controller from spec

--- spec/services/string_redacter_spec.rb
+++ spec/services/string_redacter_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 describe 'StringRedacter' do
-  module Idv
-    class RedactTestController < ApplicationController
-      include StringRedacter
-    end
-  end
-
   describe '#redact_alphanumeric' do
     it 'leaves in punctuation and spaces, but removes letters and numbers' do
       expect(StringRedacter.redact_alphanumeric('+11 (555) DEF-1234')).
