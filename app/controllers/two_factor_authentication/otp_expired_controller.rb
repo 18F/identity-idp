59bb03ccc1d7b87945013c3b8324c2812a2f2a07
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-24 08:43:44 -0500

    remove logic to toggle based on creation vs account verification

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -1,20 +1,13 @@
 module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
-    before_action :phone_method_selected
-
     def show
       @otp_delivery_preference = otp_delivery_preference
-      @show_try_again_option = show_try_again_option?
     end
 
     private
 
     def otp_delivery_preference
       current_user.otp_delivery_preference
-    end
-
-    def show_try_again_option?
-      !user_fully_authenticated?
     end
   end
 end
