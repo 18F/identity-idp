8a77f6ff222d3aa4103274af29bdc946c005c7e4
Andrew Duthie <andrew.duthie@gsa.gov>
2023-01-24 09:11:26 -0500

    Remove JavaScript decorators package
    changelog: Internal, Performance, Reduce size of JavaScript assets

--- app/javascript/packages/form-link/form-link-element.ts
+++ app/javascript/packages/form-link/form-link-element.ts
@@ -1,16 +1,12 @@
-import { once } from '@18f/identity-decorators';
-
 class FormLinkElement extends HTMLElement {
   connectedCallback() {
     this.link.addEventListener('click', this.submit);
   }
 
-  @once()
   get form(): HTMLFormElement {
     return this.querySelector('form')!;
   }
 
-  @once()
   get link(): HTMLAnchorElement {
     return this.querySelector('a')!;
   }
