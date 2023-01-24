56f022c6dde357b4479f8129541da505ebeee27a
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-24 14:15:22 -0500

    add redirect if authentication is complete

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -1,5 +1,7 @@
 module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
+    before_action :validate_otp_expiration
+
     def show
       @otp_delivery_preference = otp_delivery_preference
     end
@@ -8,6 +10,10 @@ module TwoFactorAuthentication
 
     def otp_delivery_preference
       current_user.otp_delivery_preference
+    end
+
+    def validate_otp_expiration
+      redirect_to account_url if user_fully_authenticated?
     end
   end
 end
