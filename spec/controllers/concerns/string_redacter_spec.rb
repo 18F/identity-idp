0f0e226cfd4c72e83a4d7519579b3663e8ddf8c2
Sonia Connolly <sonia.connolly@gsa.gov>
2023-01-24 10:39:20 -0800

    Lint

--- spec/controllers/concerns/string_redacter_spec.rb
+++ spec/controllers/concerns/string_redacter_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 describe 'StringRedacter' do
-    module Idv
-      class RedactTestController < ApplicationController
-        include StringRedacter
-      end
+  module Idv
+    class RedactTestController < ApplicationController
+      include StringRedacter
     end
+  end
 
   describe '#redact_alphanumeric' do
     it 'leaves in punctuation and spaces, but removes letters and numbers' do
-      expect(Idv::RedactTestController.new.redact_alphanumeric('+11 (555) DEF-1234')).to eq('+## (###) XXX-####')
+      expect(Idv::RedactTestController.new.redact_alphanumeric('+11 (555) DEF-1234')).
+        to eq('+## (###) XXX-####')
     end
   end
 end
