a27d2bd6909de413428caf2eb70426ab8d7f156e
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-25 10:48:35 -0500

    Update app/controllers/two_factor_authentication/otp_expired_controller.rb
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -1,5 +1,6 @@
 module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
+    before_action :confirm_two_factor_authenticated, unless: :user_signed_in?
     before_action :validate_otp_expiration
 
     def show
