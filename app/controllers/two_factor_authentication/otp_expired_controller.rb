cb0472242e64f3a70203e903e32d7e970f2f257d
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-26 10:55:34 -0500

    remove undefined `before_action`

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -1,8 +1,7 @@
 module TwoFactorAuthentication
   class OtpExpiredController < ApplicationController
     before_action :confirm_two_factor_authenticated, unless: :user_signed_in?
-    before_action :validate_otp_expiration
-
+    
     def show
       @otp_delivery_preference = otp_delivery_preference
     end
