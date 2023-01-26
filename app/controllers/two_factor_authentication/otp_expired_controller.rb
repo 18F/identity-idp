6c8df7c8e0d0f5a7bd8f9a1cdee7aab32247de04
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-26 10:26:15 -0500

    Update app/controllers/two_factor_authentication/otp_expired_controller.rb
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -13,14 +13,5 @@ module TwoFactorAuthentication
       current_user.otp_delivery_preference
     end
 
-    def validate_otp_expiration
-      redirect_to after_sign_in_path_for(current_user) if user_signed_in? && !otp_expired?
-    end
-
-    def otp_expired?
-      return if current_user.direct_otp_sent_at.blank?
-      (current_user.direct_otp_sent_at +
-        TwoFactorAuthenticatable::DIRECT_OTP_VALID_FOR_SECONDS).past?
-    end
   end
 end
