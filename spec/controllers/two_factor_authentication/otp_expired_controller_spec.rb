4612f71864a77a149941365dcd8360fe9ee33e9f
Jessica Dembe <jessica.dembe@gsa.gov>
2023-01-26 11:02:19 -0500

    clean up test file with to reflect removed validation

--- spec/controllers/two_factor_authentication/otp_expired_controller_spec.rb
+++ spec/controllers/two_factor_authentication/otp_expired_controller_spec.rb
@@ -2,48 +2,35 @@ require 'rails_helper'
 
 describe TwoFactorAuthentication::OtpExpiredController do
   describe '#show' do
-    context 'renders' do
-      it 'receives the expected value' do
-        user = build(:user, otp_delivery_preference: 'voice')
+    it 'receives the expected value' do
+      user = build(:user, otp_delivery_preference: 'voice')
 
-        allow(controller).to receive(:otp_expired?).and_return(true)
+      stub_sign_in_before_2fa(user)
 
-        stub_sign_in_before_2fa(user)
+      get :show
 
-        get :show
-
-        expect(assigns(:otp_delivery_preference)).to eq('voice')
-      end
-
-      it 'once OTP expires' do
-        user = build(:user, otp_delivery_preference: 'voice', direct_otp_sent_at: Time.zone.now)
-
-        allow(controller).to receive(:otp_expired?).and_return(true)
-        stub_sign_in_before_2fa(user)
-
-        get :show
-
-        expect(response).to render_template(:show)
-      end
+      expect(assigns(:otp_delivery_preference)).to eq('voice')
     end
 
-    context 'does not render' do
-      it 'when user is signed out' do
-        allow(controller).to receive(:user_signed_in?).and_return(false)
+    it 'once OTP expires' do
+      user = build(:user, otp_delivery_preference: 'voice', direct_otp_sent_at: Time.zone.now)
 
-        get :show
+      allow(controller).to receive(:otp_expired?).and_return(true)
+      stub_sign_in_before_2fa(user)
 
-        expect(response).to_not render_template(:show)
-      end
+      get :show
 
-      it 'when otp is still valid' do
-        user = build(:user, otp_delivery_preference: 'voice', direct_otp_sent_at: Time.zone.now)
-        stub_sign_in_before_2fa(user)
+      expect(response).to render_template(:show)
+    end
 
-        get :show
 
-        expect(response).to_not render_template(:show)
-      end
+  context 'does not render' do
+    it 'when user is signed out' do
+      allow(controller).to receive(:user_signed_in?).and_return(false)
+
+      get :show
+
+      expect(response).to_not render_template(:show)
     end
   end
 end
