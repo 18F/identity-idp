cda6f9aa6568788e71ba83fff50278f5a60bcc19
Mitchell Henke <mitchell.henke@gsa.gov>
2023-01-30 14:18:12 -0600

    Use Redis for one-time passcode sending rate limits
    changelog: Internal, Rate Limiting, Use Redis for one-time passcode sending rate limits

--- app/services/otp_rate_limiter.rb
+++ app/services/otp_rate_limiter.rb
@@ -15,19 +15,14 @@ class OtpRateLimiter
   end
 
   def max_requests_reached?
-    return throttle.throttled? if IdentityConfig.store.redis_throttle_otp_rate_limiter_read_enabled
-
-    entry_for_current_phone.otp_send_count > otp_maxretry_times
+    return throttle.throttled?
   end
 
   def rate_limit_period_expired?
-    return throttle.expired? if IdentityConfig.store.redis_throttle_otp_rate_limiter_read_enabled
-    otp_last_sent_at.present? && (otp_last_sent_at + otp_findtime) < Time.zone.now
+    return throttle.expired?
   end
 
   def reset_count_and_otp_last_sent_at
-    entry_for_current_phone.update(otp_last_sent_at: Time.zone.now, otp_send_count: 0)
-
     throttle.reset!
   end
 
@@ -36,29 +31,16 @@ class OtpRateLimiter
   end
 
   def increment
-    # DO NOT MEMOIZE
-    @entry = OtpRequestsTracker.atomic_increment(entry_for_current_phone.id)
     throttle.increment!
-    nil
   end
 
   def otp_last_sent_at
-    if IdentityConfig.store.redis_throttle_otp_rate_limiter_read_enabled
-      throttle.attempted_at
-    else
-      entry_for_current_phone.otp_last_sent_at
-    end
+    throttle.attempted_at
   end
 
   private
 
   attr_reader :phone, :user, :phone_confirmed
-
-  # rubocop:disable Naming/MemoizedInstanceVariableName
-  def entry_for_current_phone
-    @entry ||= OtpRequestsTracker.find_or_create_with_phone_and_confirmed(phone, phone_confirmed)
-  end
-  # rubocop:enable Naming/MemoizedInstanceVariableName
 
   def otp_findtime
     IdentityConfig.store.otp_delivery_blocklist_findtime.minutes
