50a15abddfa4a02f5760105f66b9fdc002ecf1de
Matt Gardner <wilburnforce@gmail.com>
2023-01-30 19:51:37 -0500

    Use i18n provider

--- app/javascript/packages/document-capture/components/in-person-location-post-office-search-step.spec.tsx
+++ app/javascript/packages/document-capture/components/in-person-location-post-office-search-step.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { t } from '@18f/identity-i18n';
+import { I18n } from '@18f/identity-i18n';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import type { SetupServerApi } from 'msw/node';
 import { SWRConfig } from 'swr';
+import { I18nContext } from '@18f/identity-react-i18n';
 import { LOCATIONS_URL } from './in-person-location-step';
 import { ADDRESS_SEARCH_URL } from './address-search';
 import InPersonContext from '../context/in-person';
@@ -205,11 +206,24 @@ describe('InPersonLocationStep', () => {
 
     it('displays correct pluralization for a single location result', async () => {
       const { findByLabelText, findByText } = render(
-        <SWRConfig value={{ provider: () => new Map() }}>
-          <InPersonContext.Provider value={{ arcgisSearchEnabled: true }}>
-            <InPersonLocationPostOfficeSearchStep {...DEFAULT_PROPS} />
-          </InPersonContext.Provider>
-        </SWRConfig>,
+        <I18nContext.Provider
+          value={
+            new I18n({
+              strings: {
+                'in_person_proofing.body.location.po_search.results_description': {
+                  one: 'one location result',
+                  other: '%{count} %{address}',
+                },
+              },
+            })
+          }
+        >
+          <SWRConfig value={{ provider: () => new Map() }}>
+            <InPersonContext.Provider value={{ arcgisSearchEnabled: true }}>
+              <InPersonLocationPostOfficeSearchStep {...DEFAULT_PROPS} />
+            </InPersonContext.Provider>
+          </SWRConfig>
+        </I18nContext.Provider>,
       );
       await userEvent.type(
         await findByLabelText('in_person_proofing.body.location.po_search.address_search_label'),
@@ -222,29 +236,35 @@ describe('InPersonLocationStep', () => {
         await findByText('in_person_proofing.body.location.po_search.search_button'),
       );
 
-      const searchResultAlert = findByText(
-        t('in_person_proofing.body.location.po_search.results_description', {
-          address: '222 Merchandise Mart Plaza',
-          count: DEFAULT_RESPONSE.length,
-        }),
-      );
-      expect(searchResultAlert).to.exist();
-    });
-
-    before(() => {
-      server.use(
-        rest.post(ADDRESS_SEARCH_URL, (_req, res, ctx) => res(ctx.json(MULTI_LOCATION_RESPONSE))),
-        rest.post(LOCATIONS_URL, (_req, res, ctx) => res(ctx.json([{ name: 'Baltimore' }]))),
-      );
+      await findByText('one location result');
     });
 
     it('displays correct pluralization for multiple location results', async () => {
+      server.resetHandlers();
+      server.use(
+        rest.post(ADDRESS_SEARCH_URL, (_req, res, ctx) => res(ctx.json(DEFAULT_RESPONSE))),
+        rest.post(LOCATIONS_URL, (_req, res, ctx) => res(ctx.json(MULTI_LOCATION_RESPONSE))),
+      );
+
       const { findByLabelText, findByText } = render(
-        <SWRConfig value={{ provider: () => new Map() }}>
-          <InPersonContext.Provider value={{ arcgisSearchEnabled: true }}>
-            <InPersonLocationPostOfficeSearchStep {...DEFAULT_PROPS} />
-          </InPersonContext.Provider>
-        </SWRConfig>,
+        <I18nContext.Provider
+          value={
+            new I18n({
+              strings: {
+                'in_person_proofing.body.location.po_search.results_description': {
+                  one: 'one location result',
+                  other: 'more than one result: %{count} %{address}',
+                },
+              },
+            })
+          }
+        >
+          <SWRConfig value={{ provider: () => new Map() }}>
+            <InPersonContext.Provider value={{ arcgisSearchEnabled: true }}>
+              <InPersonLocationPostOfficeSearchStep {...DEFAULT_PROPS} />
+            </InPersonContext.Provider>
+          </SWRConfig>
+        </I18nContext.Provider>,
       );
       await userEvent.type(
         await findByLabelText('in_person_proofing.body.location.po_search.address_search_label'),
@@ -256,13 +276,9 @@ describe('InPersonLocationStep', () => {
       await userEvent.click(
         await findByText('in_person_proofing.body.location.po_search.search_button'),
       );
-      const searchResultAlert = findByText(
-        t('in_person_proofing.body.location.po_search.results_description', {
-          address: '222 Merchandise Mart Plaza',
-          count: MULTI_LOCATION_RESPONSE.length,
-        }),
+      await findByText(
+        `more than one result: ${MULTI_LOCATION_RESPONSE.length} ${MULTI_LOCATION_RESPONSE[0].address}`,
       );
-      expect(searchResultAlert).to.exist();
     });
   });
 
