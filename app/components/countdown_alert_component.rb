976eb6e67e34d395af8e081a12d31d2db179980b
Andrew Duthie <andrew.duthie@gsa.gov>
2023-02-01 10:50:45 -0500

    Revert "LG-8336: Implement post timer actions" (#7744)
    * Revert "LG-8336: Implement post timer actions (#7577)"
    
    This reverts commit 2e5a15607eaf9b71ebef754d7b304929572f5fd1.
    
    * changelog: Bug Fixes, Multi-Factor Authentication, Revert OTP expiration screen due to screen being shown early in some cases

--- app/components/countdown_alert_component.rb
+++ app/components/countdown_alert_component.rb
@@ -1,18 +1,11 @@
 class CountdownAlertComponent < BaseComponent
-  attr_reader :show_at_remaining, :alert_options, :countdown_options, :redirect_url, :tag_options
+  attr_reader :show_at_remaining, :alert_options, :countdown_options, :tag_options
 
-  def initialize(
-    show_at_remaining: nil,
-    alert_options: {},
-    countdown_options: {},
-    redirect_url: nil,
-    **tag_options
-  )
+  def initialize(show_at_remaining: nil, alert_options: {}, countdown_options: {}, **tag_options)
     @show_at_remaining = show_at_remaining
     @alert_options = alert_options
     @countdown_options = countdown_options
     @tag_options = tag_options
-    @redirect_url = redirect_url
   end
 
   def call
@@ -22,7 +15,6 @@ class CountdownAlertComponent < BaseComponent
       **tag_options,
       class: css_class,
       'show-at-remaining': show_at_remaining&.in_milliseconds,
-      'redirect-url': redirect_url,
     )
   end
 
