5bd91e310b0aa8da19b3a6376b94028b4d1fce53
Timothy Bradley <timothy.bradley@gsa.gov>
2023-02-01 16:57:58 -0700

    LG-8302: Log information required to detect enrollments that exceed their expiration date

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -3125,6 +3125,7 @@ module AnalyticsEvents
   # @param [String] exception_class
   # @param [String] exception_message
   # @param [String] enrollment_code
+  # @param [Float] minutes_since_established
   # @param [Float] minutes_since_last_status_check
   # @param [Float] minutes_since_last_status_update
   # @param [Float] minutes_to_completion
@@ -3145,7 +3146,7 @@ module AnalyticsEvents
   def idv_in_person_usps_proofing_results_job_exception(
     reason:,
     enrollment_id:,
-    exception_class: nil,
+    minutes_since_established:, exception_class: nil,
     exception_message: nil,
     enrollment_code: nil,
     minutes_since_last_status_check: nil,
@@ -3174,6 +3175,7 @@ module AnalyticsEvents
       exception_class: exception_class,
       exception_message: exception_message,
       enrollment_code: enrollment_code,
+      minutes_since_established: minutes_since_established,
       minutes_since_last_status_check: minutes_since_last_status_check,
       minutes_since_last_status_update: minutes_since_last_status_update,
       minutes_to_completion: minutes_to_completion,
@@ -3249,12 +3251,14 @@ module AnalyticsEvents
   # Tracks individual enrollments that are updated during GetUspsProofingResultsJob
   # @param [String] enrollment_code
   # @param [String] enrollment_id
+  # @param [Float] minutes_since_established
   # @param [Boolean] fraud_suspected
   # @param [Boolean] passed did this enrollment pass or fail?
   # @param [String] reason why did this enrollment pass or fail?
   def idv_in_person_usps_proofing_results_job_enrollment_updated(
     enrollment_code:,
     enrollment_id:,
+    minutes_since_established:,
     fraud_suspected:,
     passed:,
     reason:,
@@ -3264,6 +3268,7 @@ module AnalyticsEvents
       'GetUspsProofingResultsJob: Enrollment status updated',
       enrollment_code: enrollment_code,
       enrollment_id: enrollment_id,
+      minutes_since_established: minutes_since_established,
       fraud_suspected: fraud_suspected,
       passed: passed,
       reason: reason,
@@ -3300,14 +3305,38 @@ module AnalyticsEvents
     )
   end
 
+  # Tracks incomplete enrollments checked via the USPS API
+  # @param [String] enrollment_code
+  # @param [String] enrollment_id
+  # @param [Float] minutes_since_established
+  # @param [String] response_message
+  def idv_in_person_usps_proofing_results_job_enrollment_incomplete(
+    enrollment_code:,
+    enrollment_id:,
+    minutes_since_established:,
+    response_message:,
+    **extra
+  )
+    track_event(
+      'GetUspsProofingResultsJob: Enrollment incomplete',
+      enrollment_code: enrollment_code,
+      enrollment_id: enrollment_id,
+      minutes_since_established: minutes_since_established,
+      response_message: response_message,
+      **extra,
+    )
+  end
+
   # Tracks unexpected responses from the USPS API
   # @param [String] enrollment_code
   # @param [String] enrollment_id
+  # @param [Float] minutes_since_established
   # @param [String] response_message
   # @param [String] reason why was this error unexpected?
   def idv_in_person_usps_proofing_results_job_unexpected_response(
     enrollment_code:,
     enrollment_id:,
+    minutes_since_established:,
     response_message:,
     reason:,
     **extra
@@ -3316,6 +3345,7 @@ module AnalyticsEvents
       'GetUspsProofingResultsJob: Unexpected response received',
       enrollment_code: enrollment_code,
       enrollment_id: enrollment_id,
+      minutes_since_established: minutes_since_established,
       response_message: response_message,
       reason: reason,
       **extra,
