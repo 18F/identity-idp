6e192f39f7635d8c4ac45680953ced17b351e033
svalexander <shannon.alexander-navarro@gsa.gov>
2023-02-02 12:37:09 -0500

    usps_locations_controller location searched analytics event

--- app/controllers/idv/in_person/usps_locations_controller.rb
+++ app/controllers/idv/in_person/usps_locations_controller.rb
@@ -23,6 +23,16 @@ module Idv
               zip_code: search_params['zip_code']
             )
             response = proofer.request_facilities(candidate)
+            if response.length > 0
+              analytics.idv_in_person_location_searched(
+                success: true,
+                result_total: response.length,
+              )
+            else
+              analytics.idv_in_person_location_searched(
+                success: false, errors: 'No USPS locations found',
+              )
+            end
           else
             response = proofer.request_pilot_facilities
           end
