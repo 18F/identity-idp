9c9cef4d8b94ae84c7323ff217dd1fb33af09c16
Mitchell Henke <mitchell.henke@gsa.gov>
2023-02-03 11:12:14 -0600

    Use Redis for one-time passcode sending rate limits (#7725)
    * Use Redis for one-time passcode sending rate limits
    
    changelog: Internal, Rate Limiting, Use Redis for one-time passcode sending rate limits
    
    * Update app/services/otp_rate_limiter.rb
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * Update app/services/otp_rate_limiter.rb
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>
    
    * fix specs
    
    * remove unused methods
    
    * remove OtpRequestsTracker
    
    ---------
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/services/otp_rate_limiter.rb
+++ app/services/otp_rate_limiter.rb
@@ -15,19 +15,14 @@ class OtpRateLimiter
   end
 
   def max_requests_reached?
-    return throttle.throttled? if IdentityConfig.store.redis_throttle_otp_rate_limiter_read_enabled
-
-    entry_for_current_phone.otp_send_count > otp_maxretry_times
+    throttle.throttled?
   end
 
   def rate_limit_period_expired?
-    return throttle.expired? if IdentityConfig.store.redis_throttle_otp_rate_limiter_read_enabled
-    otp_last_sent_at.present? && (otp_last_sent_at + otp_findtime) < Time.zone.now
+    throttle.expired?
   end
 
   def reset_count_and_otp_last_sent_at
-    entry_for_current_phone.update(otp_last_sent_at: Time.zone.now, otp_send_count: 0)
-
     throttle.reset!
   end
 
@@ -36,29 +31,20 @@ class OtpRateLimiter
   end
 
   def increment
-    # DO NOT MEMOIZE
-    @entry = OtpRequestsTracker.atomic_increment(entry_for_current_phone.id)
     throttle.increment!
-    nil
   end
 
   def otp_last_sent_at
-    if IdentityConfig.store.redis_throttle_otp_rate_limiter_read_enabled
-      throttle.attempted_at
-    else
-      entry_for_current_phone.otp_last_sent_at
-    end
+    throttle.attempted_at
+  end
+
+  def throttle
+    @throttle ||= Throttle.new(throttle_type: :phone_otp, target: throttle_key)
   end
 
   private
 
   attr_reader :phone, :user, :phone_confirmed
-
-  # rubocop:disable Naming/MemoizedInstanceVariableName
-  def entry_for_current_phone
-    @entry ||= OtpRequestsTracker.find_or_create_with_phone_and_confirmed(phone, phone_confirmed)
-  end
-  # rubocop:enable Naming/MemoizedInstanceVariableName
 
   def otp_findtime
     IdentityConfig.store.otp_delivery_blocklist_findtime.minutes
@@ -70,10 +56,6 @@ class OtpRateLimiter
 
   def phone_fingerprint
     @phone_fingerprint ||= Pii::Fingerprinter.fingerprint(PhoneFormatter.format(phone))
-  end
-
-  def throttle
-    @throttle ||= Throttle.new(throttle_type: :phone_otp, target: throttle_key)
   end
 
   def throttle_key
