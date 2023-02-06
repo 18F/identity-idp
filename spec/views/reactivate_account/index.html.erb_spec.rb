646101f7956c2c980ef89d8688cb498d55991e01
Jack Cody <jack.cody@gsa.gov>
2023-02-06 16:55:59 -0600

    Add spec

--- spec/views/reactivate_account/index.html.erb_spec.rb
+++ spec/views/reactivate_account/index.html.erb_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 describe 'reactivate_account/index.html.erb' do
-  it 'displays a fallback warning alert when js is off' do
+  subject(:rendered) do
     render
+  end
+
+  let(:presenter_class) { AccountShowPresenter }
+  let(:personal_key_generated_at) { Time.zone.now }
+  let(:presenter) do
+    instance_double(
+      presenter_class,
+      personal_key_generated_at: personal_key_generated_at,
+    )
+  end
+
+  it 'displays a fallback warning alert when js is off' do
+    assign(:presenter, presenter)
 
     expect(rendered).to have_content(t('instructions.account.reactivate.modal.copy'))
+  end
+
+  it 'displays the date the personal key was generated' do
+    assign(:presenter, presenter)
+
+    expect(rendered).to have_content(
+      t(
+        'users.personal_key.generated_on_html',
+        date: I18n.l(Time.zone.today, format: '%B %d, %Y'),
+      ),
+    )
   end
 end
