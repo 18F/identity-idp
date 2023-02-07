72f5f1a50dc5dd906b5882abc52373a697567eb0
Jack Cody <jack.cody@gsa.gov>
2023-02-07 16:27:26 -0600

    Change personal_key_generated_at

--- app/controllers/reactivate_account_controller.rb
+++ app/controllers/reactivate_account_controller.rb
@@ -1,19 +1,11 @@
 class ReactivateAccountController < ApplicationController
   include AccountReactivationConcern
-  include RememberDeviceConcern
 
   before_action :confirm_two_factor_authenticated
   before_action :confirm_password_reset_profile
 
   def index
-    @presenter = AccountShowPresenter.new(
-      decrypted_pii: nil,
-      personal_key: nil,
-      sp_session_request_url: sp_session_request_url_with_updated_params,
-      sp_name: decorated_session.sp_name,
-      decorated_user: current_user.decorate,
-      locked_for_session: pii_locked_for_session?(current_user),
-    )
+    @personal_key_generated_at = current_user.personal_key_generated_at
   end
 
   def update
