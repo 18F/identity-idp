9820a4d876ef28b37221a4e5003912eda887861b
Matt Gardner <wilburnforce@gmail.com>
2023-02-07 19:38:36 -0500

    Working spatial query

--- app/models/usps_ipp_cached_locations.rb
+++ app/models/usps_ipp_cached_locations.rb
@@ -1,10 +1,12 @@
 class UspsIppCachedLocations < PostgisDataApplicationRecord
   self.table_name = "usps_ipp_cached_locations"
 
-  def self.query_by_point(longitude, latitude)
-    query = RGeo::Geos.factory(srid: 4326).point(longitude, latitude)
-    # change to https://postgis.net/docs/ST_Within.html
-    locations = UspsIppCachedLocations.arel_table[:lonlat].st_contains(query)
-    UspsIppCachedLocations.where(UspsIppCachedLocations.arel_table[:lonlat].st_contains(query))
+  FIFTY_MILES_IN_METERS = 50 * 1609
+
+  def self.query_by_point(longitude, latitude, within = FIFTY_MILES_IN_METERS)
+    # https://postgis.net/docs/ST_Point.html
+    # For geodetic coordinates, X is longitude and Y is latitude
+
+    UspsIppCachedLocations.where("ST_DWithin(lonlat::geography,'SRID=4326;POINT(#{longitude} #{latitude})'::geography, #{within})")
   end
 end
