ecdfac008275154958715f05bcd720f3e74e71d3
Matt Gardner <wilburnforce@gmail.com>
2023-02-07 22:05:47 -0500

    Clean up query

--- app/jobs/in_person/ipp_locations_cacher_job.rb
+++ app/jobs/in_person/ipp_locations_cacher_job.rb
@@ -3,18 +3,16 @@ module InPerson
     include ArcgisApi
 
     def perform(post_offices)
-      geocoded_post_offices = geocode_post_offices(post_offices)
-
       UspsIppCachedLocations.upsert_all(
-        geocoded_post_offices.map do |geocoded_post_office|
+        post_offices.map do |post_office|
           {
-            address: geocoded_post_office[:address],
-            city: geocoded_post_office[:city],
-            state: geocoded_post_office[:state],
-            zip: geocoded_post_office[:zip_code_5],
+            address: post_office[:address],
+            city: post_office[:city],
+            state: post_office[:state],
+            zip: post_office[:zip_code_5],
 
-            usps_attributes: geocoded_post_office,
-            lonlat: geocoded_post_office[:lonlat],
+            usps_attributes: post_office,
+            lonlat: geocode(post_office),
           }
         end,
         unique_by: [:address, :city, :state, :zip],
@@ -22,22 +20,17 @@ module InPerson
     end
 
     # TODO: implement batch geocoder
-    def geocode_post_offices(post_offices)
-      post_offices.map do |post_office|
-        geocoded_post_office = Geocoder.new.find_address_candidates(
-          Address: post_office[:address],
-          City: post_office[:city],
-          Region: post_office[:state],
-          Postal: post_office[:zip_code_5],
-        ).first.location
+    def geocode(post_office)
+      geocoded_post_office = Geocoder.new.find_address_candidates(
+        Address: post_office[:address],
+        City: post_office[:city],
+        Region: post_office[:state],
+        Postal: post_office[:zip_code_5],
+      ).first.location
 
-        {
-          **post_office,
-          lonlat: RGeo::Geos.factory(srid: UspsIppCachedLocations::WGS84_SRID).point(
-            geocoded_post_office.longitude, geocoded_post_office.latitude
-          ),
-        }
-      end
+      RGeo::Geos.factory(srid: UspsIppCachedLocations::WGS84_SRID).point(
+        geocoded_post_office.longitude, geocoded_post_office.latitude
+      )
     end
   end
 end
--- app/models/usps_ipp_cached_locations.rb
+++ app/models/usps_ipp_cached_locations.rb
@@ -8,8 +8,12 @@ class UspsIppCachedLocations < PostgisDataApplicationRecord
   def self.query_by_point(longitude, latitude, within = FIFTY_MILES_IN_METERS)
     # https://postgis.net/docs/ST_Point.html
     # For geodetic coordinates, X is longitude and Y is latitude
-    point = "'SRID=#{FIFTY_MILES_IN_METERS};POINT(#{longitude} #{latitude})'::geography"
-    radius_query = "ST_DWithin(lonlat::geography,#{point}, #{within})"
+    radius_query = "ST_DWithin(
+      lonlat::geography,
+      'SRID=#{WGS84_SRID};POINT(#{longitude} #{latitude})'::geography,
+      #{within})
+    ".squish
+
     UspsIppCachedLocations.where(radius_query)
   end
 end
