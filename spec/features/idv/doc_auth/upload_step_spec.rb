c52ee197a4643e3455d2459c235bf33bfecb6c4b
eric-gade <eric.gade@gsa.gov>
2023-02-09 14:01:45 -0500

    Deleting useless hybrid handoff mobile tests
    -- What
    Old code was testing for case where user is at the hybrid handoff
    screen (upload step), but is on mobile. We have not allowed that to
    happen for a long time, so the code is dead.
    
    changelog: User-Facing Improvements, Hybrid Handoff Screen, Combining
    upload and send link steps into one screen

--- spec/features/idv/doc_auth/upload_step_spec.rb
+++ spec/features/idv/doc_auth/upload_step_spec.rb
@@ -16,42 +16,6 @@ feature 'doc auth upload step' do
       and_return(fake_attempts_tracker)
   end
 
-  context 'on a mobile device' do
-    before do
-      allow(BrowserCache).to receive(:parse).and_return(mobile_device)
-    end
-
-    it 'proceeds to send link via email page when user chooses to upload from computer' do
-      expect(fake_attempts_tracker).to receive(
-        :idv_document_upload_method_selected,
-      ).with({ upload_method: 'desktop' })
-
-      expect_step_indicator_current_step(t('step_indicator.flows.idv.verify_id'))
-
-      click_on t('doc_auth.info.upload_computer_link')
-
-      expect(page).to have_current_path(idv_doc_auth_email_sent_step)
-      expect(fake_analytics).to have_logged_event(
-        'IdV: doc auth upload submitted',
-        hash_including(step: 'upload', destination: :email_sent),
-      )
-    end
-
-    it 'proceeds to document capture when user chooses to use phone' do
-      expect(fake_attempts_tracker).to receive(
-        :idv_document_upload_method_selected,
-      ).with({ upload_method: 'mobile' })
-
-      click_on t('doc_auth.buttons.use_phone')
-
-      expect(page).to have_current_path(idv_doc_auth_document_capture_step)
-      expect(fake_analytics).to have_logged_event(
-        'IdV: doc auth upload submitted',
-        hash_including(step: 'upload', destination: :document_capture),
-      )
-    end
-  end
-
   context 'on a desktop device' do
     before do
       allow_any_instance_of(Idv::Steps::UploadStep).to receive(:mobile_device?).and_return(false)
@@ -78,12 +42,13 @@ feature 'doc auth upload step' do
         :idv_document_upload_method_selected,
       ).with({ upload_method: 'mobile' })
 
-      click_on t('doc_auth.buttons.use_phone')
+      #click_on t('doc_auth.buttons.use_phone')
+      click_idv_continue
 
-      expect(page).to have_current_path(idv_doc_auth_send_link_step)
+      expect(page).to have_current_path(idv_doc_auth_link_sent_step)
       expect(fake_analytics).to have_logged_event(
         'IdV: doc auth upload submitted',
-        hash_including(step: 'upload', destination: :send_link),
+        hash_including(step: 'upload', destination: :link_sent),
       )
     end
   end
