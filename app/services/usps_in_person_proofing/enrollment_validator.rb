5c957db5fdaeb3b59a053fd3b19099d90bd347a2
Timothy Bradley <timothy.bradley@gsa.gov>
2023-02-13 16:59:53 -0700

    LG-7832: Improve state ID validation based no testing with API; improve tests

--- app/services/usps_in_person_proofing/enrollment_validator.rb
+++ app/services/usps_in_person_proofing/enrollment_validator.rb
@@ -1,24 +1,20 @@
 module UspsInPersonProofing
   class EnrollmentValidator
-    def initialize(transliterator = Transliterator.new)
-      @additional_unsupported_chars = {
-        first_name: /[^A-Za-z\-' ]/,
-        last_name: /[^A-Za-z\-' ]/,
-        address1: /[^A-Za-z0-9\-' .\/#]/,
-        address2: /[^A-Za-z0-9\-' .\/#]/,
-        city: /[^A-Za-z\-' ]/,
-      }
+    SUPPORTED_FIELDS = [
+      :first_name,
+      :last_name,
+      :address1,
+      :address2,
+      :city,
+    ].freeze
 
-      @unsupported_chars_translation = {
-        first_name: :translate_name_chars_error,
-        last_name: :translate_name_chars_error,
-        address1: :translate_address_chars_error,
-        address2: :translate_address_chars_error,
-        city: :translate_address_chars_error,
-      }
+    def initialize(transliterator = Transliterator.new)
       @transliterator = transliterator
     end
 
+    # Validate that transliteratable fields from the state ID form
+    # can be transliterated to contain acceptable characters.
+    #
     # @param [Hash] fields Name/value pairs of fields to validate
     # @option fields [String,#to_s] :first_name
     # @option fields [String,#to_s] :last_name
@@ -28,13 +24,7 @@ module UspsInPersonProofing
     # @return [Hash,nil] Field names mapped to error messages, or nil if there were no issues
     def validate(fields)
       validation_result = {}
-      fields.slice(
-        :first_name,
-        :last_name,
-        :address1,
-        :address2,
-        :city,
-      ).each do |key, value|
+      fields.slice(*SUPPORTED_FIELDS).each do |key, value|
         # Get transliterated value
         result = @transliterator.transliterate(value)
         transliterated = result.transliterated
@@ -46,7 +36,7 @@ module UspsInPersonProofing
         end
 
         # Scan for unsupported chars for the field
-        additional_chars = transliterated.scan(@additional_unsupported_chars[key])
+        additional_chars = transliterated.scan(ADDITIONAL_UNSUPPORTED_CHARS[key])
 
         # Create sorted list of unique unsupported characters
         unsupported_char_list = (result.unsupported_chars + additional_chars).sort.uniq
@@ -54,7 +44,7 @@ module UspsInPersonProofing
         # Use unsupported character list to generate error message
         unless unsupported_char_list.empty?
           validation_result[key] = send(
-            @unsupported_chars_translation[key],
+            UNSUPPORTED_CHARS_TRANSLATION[key],
             unsupported_char_list,
           )
         end
@@ -64,6 +54,22 @@ module UspsInPersonProofing
     end
 
     private
+
+    ADDITIONAL_UNSUPPORTED_CHARS = ActiveSupport::HashWithIndifferentAccess.new(
+      first_name: /[^A-Za-z\-' ]/,
+      last_name: /[^A-Za-z\-' ]/,
+      address1: /[^A-Za-z0-9\-' .\/#]/,
+      address2: /[^A-Za-z0-9\-' .\/#]/,
+      city: /[^A-Za-z\-' ]/,
+    ).freeze
+
+    UNSUPPORTED_CHARS_TRANSLATION = ActiveSupport::HashWithIndifferentAccess.new(
+      first_name: :translate_name_chars_error,
+      last_name: :translate_name_chars_error,
+      address1: :translate_address_chars_error,
+      address2: :translate_address_chars_error,
+      city: :translate_address_chars_error,
+    ).freeze
 
     def translate_name_chars_error(chars)
       I18n.t(
