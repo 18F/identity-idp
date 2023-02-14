550c09fd054515ee5870c3faf829b630cc3385f1
Sonia Connolly <sonia.connolly@gsa.gov>
2023-02-13 17:15:23 -0800

    Add show tests and make them pass (analytics)

--- app/controllers/idv/in_person/verify_info_controller.rb
+++ app/controllers/idv/in_person/verify_info_controller.rb
@@ -1,7 +1,7 @@
 module Idv
   module InPerson
     class VerifyInfoController < ApplicationController
-#      include IdvSession
+      include IdvSession
 
       before_action :renders_404_if_flag_not_set
       before_action :confirm_two_factor_authenticated
@@ -12,20 +12,86 @@ module Idv
         @in_person_proofing = true
         @which_verify_controller = idv_in_person_verify_info_path
 
+        increment_step_counts
+        analytics.idv_doc_auth_verify_visited(**analytics_arguments)
+        Funnel::DocAuth::RegisterStep.new(current_user.id, sp_session[:issuer]).
+          call('verify', :view, true) # specify in_person?
+
         render 'idv/verify_info/show'
       end
 
       private
 
       def renders_404_if_flag_not_set
-        render_not_found unless IdentityConfig.store.doc_auth_in_person_verify_info_controller_enabled
+        unless IdentityConfig.store.doc_auth_in_person_verify_info_controller_enabled
+          render_not_found
+        end
       end
 
       ##### Move to VerifyInfoConcern
 
+      # copied from address_controller
+      def confirm_ssn_step_complete
+        return if pii.present? && pii[:ssn].present?
+        redirect_to idv_doc_auth_url
+      end
+
       def confirm_profile_not_already_confirmed
         return unless idv_session.profile_confirmation == true
         redirect_to idv_review_url
+      end
+
+      def pii
+        @pii = flow_session[:pii_from_doc] if flow_session
+      end
+
+      def current_flow_step_counts
+        user_session['idv/doc_auth_flow_step_counts'] ||= {}
+        user_session['idv/doc_auth_flow_step_counts'].default = 0
+        user_session['idv/doc_auth_flow_step_counts']
+      end
+
+      def increment_step_counts
+        current_flow_step_counts['verify'] += 1
+      end
+
+      # copied from doc_auth_controller
+      def flow_session
+        user_session['idv/doc_auth']
+      end
+
+      def flow_path
+        flow_session[:flow_path]
+      end
+
+      def irs_reproofing?
+        effective_user&.decorate&.reproof_for_irs?(
+          service_provider: current_sp,
+        ).present?
+      end
+
+      def analytics_arguments
+        {
+          flow_path: flow_path,
+          step: 'verify',
+          step_count: current_flow_step_counts['verify'],
+          analytics_id: 'Doc Auth',
+          irs_reproofing: irs_reproofing?,
+        }.merge(**acuant_sdk_ab_test_analytics_args)
+      end
+
+      # Copied from capture_doc_flow.rb
+      # and from doc_auth_flow.rb
+      def acuant_sdk_ab_test_analytics_args
+        capture_session_uuid = flow_session[:document_capture_session_uuid]
+        if capture_session_uuid
+          {
+            acuant_sdk_upgrade_ab_test_bucket:
+            AbTests::ACUANT_SDK.bucket(capture_session_uuid),
+          }
+        else
+          {}
+        end
       end
     end
   end
--- spec/controllers/idv/in_person/verify_info_controller_spec.rb
+++ spec/controllers/idv/in_person/verify_info_controller_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 describe Idv::InPerson::VerifyInfoController do
   include IdvHelper
 
+  let(:flow_session) do
+    { 'document_capture_session_uuid' => 'fd14e181-6fb1-4cdc-92e0-ef66dad0df4e',
+      :pii_from_doc => Idp::Constants::MOCK_IDV_APPLICANT_WITH_SSN.dup,
+      :flow_path => 'standard' }
+  end
+
   let(:user) { build(:user, :with_phone, with: { phone: '+1 (415) 555-0130' }) }
+
+  before do
+    allow(subject).to receive(:flow_session).and_return(flow_session)
+    stub_sign_in(user)
+  end
 
   describe 'before_actions' do
     it 'includes authentication before_action' do
@@ -34,6 +45,56 @@ describe Idv::InPerson::VerifyInfoController do
       get :show
 
       expect(response).to be_not_found
+    end
+  end
+
+  context 'when doc_auth_ssn_controller_enabled' do
+    before do
+      allow(IdentityConfig.store).to receive(:doc_auth_in_person_verify_info_controller_enabled).
+        and_return(true)
+      stub_analytics
+      stub_attempts_tracker
+      allow(@analytics).to receive(:track_event)
+    end
+
+    describe '#show' do
+      let(:analytics_name) { 'IdV: doc auth verify visited' }
+      let(:analytics_args) do
+        {
+          analytics_id: 'Doc Auth',
+          flow_path: 'standard',
+          irs_reproofing: false,
+          step: 'verify',
+          step_count: 1,
+        }
+      end
+
+      context 'when doc_auth_in_person_verify_info_controller_enabled' do
+        before do
+          allow(IdentityConfig.store).to receive(:doc_auth_ssn_controller_enabled).
+            and_return(true)
+        end
+
+        it 'renders the show template' do
+          get :show
+
+          expect(response).to render_template :show
+        end
+
+        it 'sends analytics_visited event' do
+          get :show
+
+          expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
+        end
+
+        it 'sends correct step count to analytics' do
+          get :show
+          get :show
+          analytics_args[:step_count] = 2
+
+          expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
+        end
+      end
     end
   end
 end
