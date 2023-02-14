6a845e0a40ae37d7a679d6932cf25995c1158301
Timothy Bradley <timothy.bradley@gsa.gov>
2023-02-13 17:01:52 -0700

    LG-7832: Start filling out transliteration validation API route

--- app/controllers/idv/in_person/state_id_verify_controller.rb
+++ app/controllers/idv/in_person/state_id_verify_controller.rb
@@ -3,20 +3,24 @@ module Idv
     class StateIdVerifyController < ApplicationController
       include RenderConditionConcern
       include UspsInPersonProofing
+      include EffectiveUser
+      include UspsInPersonProofing
 
-      before_action :confirm_two_factor_authenticated
+      check_or_render_not_found -> { InPersonConfig.enabled? }
+
+      before_action :confirm_authenticated_for_api
 
       def index
-        params.slice(
-            :first_name,
-            :last_name,
-            :address1,
-            :address2,
-            :city,
-        ).compact.map do |field|
-            result = transliterator.transliterate(field)
+        validator = EnrollmentValidator.new
+        results = validator.validate(
+          params.permit(*EnrollmentValidator::SUPPORTED_FIELDS).to_h,
+        )
 
-        end
+        render json: { success: true, data: results }
+      end
+
+      def confirm_authenticated_for_api
+        render json: { success: false }, status: :unauthorized if !effective_user
       end
     end
   end
