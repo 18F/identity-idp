b8617b1b2ab3f1e97269cf48088633f9c14fb2b3
Jeremy Curcio <jeremy.curcio@gsa.gov>
2023-02-14 21:30:21 -0500

    Shorten the line length for lint

--- app/presenters/openid_connect_user_info_presenter.rb
+++ app/presenters/openid_connect_user_info_presenter.rb
@@ -22,7 +22,7 @@ class OpenidConnectUserInfoPresenter
     info.merge!(x509_attributes) if scoper.x509_scopes_requested?
     info[:verified_at] = verified_at if scoper.verified_at_requested?
     info[:ial] = Saml::Idp::Constants::AUTHN_CONTEXT_IAL_TO_CLASSREF[identity.ial]
-    info[:aal] = Saml::Idp::Constants::AUTHN_CONTEXT_AAL_TO_CLASSREF[identity.aal || ::Idp::Constants::DEFAULT_AAL] 
+    info[:aal] = Saml::Idp::Constants::AUTHN_CONTEXT_AAL_TO_CLASSREF[aal]
 
     scoper.filter(info)
   end
@@ -151,5 +151,9 @@ class OpenidConnectUserInfoPresenter
 
   def out_of_band_session_accessor
     @out_of_band_session_accessor ||= OutOfBandSessionAccessor.new(identity.rails_session_id)
+  end
+
+  def aal
+    identity.aal || ::Idp::Constants::DEFAULT_AAL
   end
 end
