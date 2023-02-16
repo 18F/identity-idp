838e157cf9cf2b9d21a5cc2cd15c8b4ba8ebdb6a
Andrew Duthie <andrew.duthie@gsa.gov>
2023-02-16 10:49:14 -0500

    Revert "Avoid defaulting to U.S. for phone formatter"
    This reverts commit 2666e8bac4418977928bb06502e5394026f23962.

--- app/services/phone_formatter.rb
+++ app/services/phone_formatter.rb
@@ -1,5 +1,7 @@
 module PhoneFormatter
+  DEFAULT_COUNTRY = 'US'.freeze
+
   def self.format(phone, country_code: nil)
-    Phonelib.parse(phone, country_code)&.international
+    Phonelib.parse(phone, country_code || DEFAULT_COUNTRY)&.international
   end
 end
