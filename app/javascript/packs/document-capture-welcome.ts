7720607fdd750f5116ecdc71797a93f56eb35c95
Matt Hinz <matt.hinz@gsa.gov>
2023-02-16 14:25:14 -0800

    Refactor perf measuring slightly
    Pull out to a function that just augments the promise with perf data

--- app/javascript/packs/document-capture-welcome.ts
+++ app/javascript/packs/document-capture-welcome.ts
@@ -8,6 +8,18 @@ function delay(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+async function measure<T>(
+  measureName: string,
+  promise: Promise<T>,
+): Promise<{ result: T; duration: number }> {
+  performance.measure(measureName);
+  const result = await promise;
+  const { duration } = performance.measure(measureName, {
+    end: performance.now(),
+  });
+  return { result, duration };
+}
+
 function addFormInputsForMobileDeviceCapabilities() {
   const form = document.querySelector<HTMLFormElement>('.js-consent-continue-form');
 
@@ -25,33 +37,31 @@ function addFormInputsForMobileDeviceCapabilities() {
 
   // The check for a camera on the device is async -- kick it off here and intercept
   // submit() to ensure that it completes in time.
-  performance.measure(DEVICE_CHECK_EVENT);
-  const cameraCheckPromise = hasCamera().then((cameraPresent: boolean) => {
-    const { duration } = performance.measure(DEVICE_CHECK_EVENT, {
-      end: performance.now(),
-    });
-    trackEvent(DEVICE_CHECK_EVENT, {
-      is_camera_capable_mobile: true,
-      camera_present: cameraPresent,
-      grace_time: GRACE_TIME_FOR_CAMERA_CHECK_MS,
-      duration,
-    });
+  const cameraCheckPromise = measure(DEVICE_CHECK_EVENT, hasCamera()).then(
+    ({ result: cameraPresent, duration }) => {
+      trackEvent(DEVICE_CHECK_EVENT, {
+        is_camera_capable_mobile: true,
+        camera_present: !!cameraPresent,
+        grace_time: GRACE_TIME_FOR_CAMERA_CHECK_MS,
+        duration,
+      });
 
-    if (!cameraPresent) {
-      // Signal to the backend that this is a mobile device, but no camera is present
-      const ncInput = document.createElement('input');
-      ncInput.type = 'hidden';
-      ncInput.name = 'no_camera';
-      form.appendChild(ncInput);
-    }
+      if (!cameraPresent) {
+        // Signal to the backend that this is a mobile device, but no camera is present
+        const ncInput = document.createElement('input');
+        ncInput.type = 'hidden';
+        ncInput.name = 'no_camera';
+        form.appendChild(ncInput);
+      }
 
-    // Signal to the backend that this is a mobile device, and this user should skip the
-    // "hybrid handoff" step.
-    const input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = 'skip_upload';
-    form.appendChild(input);
-  });
+      // Signal to the backend that this is a mobile device, and this user should skip the
+      // "hybrid handoff" step.
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = 'skip_upload';
+      form.appendChild(input);
+    },
+  );
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
