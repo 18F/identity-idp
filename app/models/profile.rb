3932a92db1878d0673f8f459eefb26fd3474cd45
John Maxwell <john.maxwell@gsa.gov>
2023-02-16 11:28:08 -0500

    Fixed incorrect generation of "account rejected" email
    Worked OK in the app itself (where we never call it), but not in the
    async job (where we do call it).

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -43,7 +43,7 @@ class Profile < ApplicationRecord
 
   def deactivate(reason)
     update!(active: false, deactivation_reason: reason)
-    UserMailer.account_rejected.deliver_now_or_later if reason == :threatmetrix_review_rejected
+    UserAlerts::AlertUserAboutAccountRejected.call(user) if reason == :threatmetrix_review_rejected
   end
 
   def decrypt_pii(password)
