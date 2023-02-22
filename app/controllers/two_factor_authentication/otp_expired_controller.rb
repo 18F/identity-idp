4565eb5c17977bf9de1cad8f9ecdcf5f1b941ea6
Jessica Dembe <jessica.dembe@gsa.gov>
2023-02-22 10:51:41 -0500

    add functionality to show/hide based on additional methods available

--- app/controllers/two_factor_authentication/otp_expired_controller.rb
+++ app/controllers/two_factor_authentication/otp_expired_controller.rb
@@ -6,6 +6,7 @@ module TwoFactorAuthentication
 
     def show
       @otp_delivery_preference = otp_delivery_preference
+      @show_choose_another_method_option = show_choose_another_method_option
       analytics.otp_expired_visited(
         otp_sent_at: current_user.direct_otp_sent_at,
         otp_expiration: otp_expiration,
@@ -21,6 +22,10 @@ module TwoFactorAuthentication
 
     def otp_delivery_preference
       current_user.otp_delivery_preference
+    end
+
+    def show_choose_another_method_option
+      mfa_context.enabled_mfa_methods_count > 1
     end
   end
 end
