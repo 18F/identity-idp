d4159688e3e5b94cc4f810fc8db3d43efd82b163
Alex Bradley <alexander.bradley@gsa.gov>
2023-02-23 13:42:55 -0500

    change order of redirect for inperson to superscede fraud_review in doc auth

--- app/controllers/idv/doc_auth_controller.rb
+++ app/controllers/idv/doc_auth_controller.rb
@@ -1,9 +1,9 @@
 module Idv
   class DocAuthController < ApplicationController
     before_action :confirm_two_factor_authenticated
+    before_action :redirect_if_pending_in_person_enrollment
     before_action :handle_pending_fraud_review
     before_action :redirect_if_pending_profile
-    before_action :redirect_if_pending_in_person_enrollment
     before_action :extend_timeout_using_meta_refresh_for_select_paths
 
     include IdvSession
