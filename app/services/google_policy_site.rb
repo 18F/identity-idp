8b6c444b3ef86cc2a10f361eac4854d51c69ed87
Malick Diarra <malick.diarra@gsa.gov>
2023-02-23 09:22:14 -0500

    add locale params and use uri service

--- app/services/google_policy_site.rb
+++ app/services/google_policy_site.rb
@@ -1,22 +1,16 @@
-# frozen_string_literal: true
-
 class GooglePolicySite
   BASE_URL = URI('https://policies.google.com').freeze
 
-  def self.locale_segment
+  def self.locale_params
     active_locale = I18n.locale
-    active_locale == I18n.default_locale ? '' : "?hl=#{active_locale}"
-  end
-
-  def self.base_url
-    URI.join(BASE_URL, locale_segment).to_s
+    active_locale == I18n.default_locale ? {} : { hl: active_locale }
   end
 
   def self.privacy_url
-    URI.join(BASE_URL, "/privacy#{locale_segment}").to_s
+    UriService.add_params(URI.join(BASE_URL, "/privacy"), locale_params)
   end
-
+  
   def self.terms_url
-    URI.join(BASE_URL, "/terms#{locale_segment}").to_s
+    UriService.add_params(URI.join(BASE_URL, "/terms"), locale_params)
   end
 end
