d4fe232e5d0803b2ac51958e496fe91d9899baba
Matt Hinz <matt.hinz@gsa.gov>
2023-02-27 15:05:42 -0800

    LG-8976: Allow using Puerto Rico phone numbers for hybrid handoff (#7874)
    * Add failing tests for LG-8976
    
    * Phone input: Sync up country <select> on init
    
    Looks like we can have disagreement between what the backend thinks the country is and what the frontend does. Here we're letting the frontend win.
    
    * changelog: User-Facing Improvements, Identity verification, Fix issue blocking some Puerto Rico users from using their phones to take pictures of their identity documents.
    
    * Don't force a format of PhoneInput on init
    
    Was leading to input being initialized to "+1"
    
    * Update Idv::PhoneForm to not assume US
    
    Use Phonelib to parse the country out of the user's phone number and intialize the `international_code` dropdown rendered by `PhoneInputComponent`
    
    Update `PhoneFormatter` to use `national` format for the default country (US), `international` for others.
    
    * Don't modify PhoneFormatter
    
    This change probably broke a bunch of tests anyway
    
    * Idv::PhoneForm: initialize @international_code
    
    Init from previous_params if there, otherwise infer from phone number.
    (There are some cases where the international_code value is not submitted, e.g. fetch() calls)
    
    Also took the opportunity to integrate new tests better with existing test infra
    
    * Remove +1 from expected formatted phone
    
    We format PR numbers as just (XXX) XXX-XXXX on the front end
    
    * Remove stray line
    
    * Add test for phone input country initialization
    
    * Only feature test 1 Puerto Rico area code
    
    (These are slow tests)
    
    * Tweak determine_initial_values call
    
    Don't include a spread arg

--- app/forms/idv/phone_form.rb
+++ app/forms/idv/phone_form.rb
@@ -25,7 +25,12 @@ module Idv
       @user = user
       @allowed_countries = allowed_countries
       @delivery_methods = delivery_methods
-      self.phone = initial_phone_value(previous_params[:phone]) unless user_has_multiple_phones?
+
+      @international_code, @phone = determine_initial_values(
+        **previous_params.
+        symbolize_keys.
+        slice(:international_code, :phone),
+      )
     end
 
     def submit(params)
@@ -38,22 +43,29 @@ module Idv
       FormResponse.new(success: success, errors: errors, extra: extra_analytics_attributes)
     end
 
-    def user_has_multiple_phones?
-      @user.phone_configurations.many?
-    end
-
     private
 
     attr_writer :phone, :otp_delivery_preference
 
-    def initial_phone_value(input_phone)
-      initial_phone = input_phone
-      initial_phone ||= begin
-        user_phone = MfaContext.new(user).phone_configurations.take&.phone
-        user_phone if valid_phone?(user_phone, phone_confirmed: true)
+    # @return [Array<string,string>] The international_code and phone values to use.
+    def determine_initial_values(international_code: nil, phone: nil)
+      if phone.nil? && international_code.nil?
+        default_phone = user.default_phone_configuration&.phone
+        if valid_phone?(default_phone, phone_confirmed: true)
+          phone = default_phone
+          international_code = country_code_for(default_phone)
+        end
+      else
+        international_code ||= country_code_for(phone)
       end
 
-      PhoneFormatter.format(initial_phone) if initial_phone
+      phone = PhoneFormatter.format(phone, country_code: international_code)
+
+      [international_code, phone]
+    end
+
+    def country_code_for(phone)
+      Phonelib.parse(phone).country
     end
 
     def validate_valid_phone_for_allowed_countries
