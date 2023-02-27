f4e7ff2f7adbbcd8aa22cc716ee5abdb8f464929
Matt Gardner <wilburnforce@gmail.com>
2023-02-27 17:06:00 -0500

    Export IPP locations markup

--- app/javascript/packages/document-capture/index.ts
+++ app/javascript/packages/document-capture/index.ts
@@ -1,5 +1,6 @@
 /** @typedef {import('./context/upload').FlowPath} FlowPath */
 
 export { default as AddressSearch } from './components/address-search';
+export { default as InPersonLocations } from './components/in-person-locations';
 export { default as DocumentCapture } from './components/document-capture';
 export * from './context';
