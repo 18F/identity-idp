7e46b3d8a6a01c23bd5da114374739e267377b7a
gina-yamada <gina.yamada@gsa.gov>
2023-02-27 15:02:01 -0700

    LG-8861: Create unordered distance mock data

--- spec/support/usps_ipp_helper.rb
+++ spec/support/usps_ipp_helper.rb
@@ -23,6 +23,14 @@ module UspsIppHelper
     )
   end
 
+  def stub_request_facilities_with_unorderd_distance
+    stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/getIppFacilityList}).to_return(
+      status: 200,
+      body: UspsInPersonProofing::Mock::Fixtures.request_facilities_response_with_unordered_distance,
+      headers: { 'content-type' => 'application/json' },
+    )
+  end
+
   def stub_request_facilities_with_duplicates
     stub_request(:post, %r{/ivs-ippaas-api/IPPRest/resources/rest/getIppFacilityList}).to_return(
       status: 200,
