5703dc2cd19a3d103f45199bceb875ed6f3762ae
Jonathan Hooper <jonathan.hooper@gsa.gov>
2023-02-28 14:58:38 -0500

    another small fix

--- app/controllers/concerns/idv/step_utilities_concern.rb
+++ app/controllers/concerns/idv/step_utilities_concern.rb
@@ -13,7 +13,7 @@ module Idv
 
     def confirm_pii_from_doc
       @pii = flow_session&.[]('pii_from_doc') # hash with indifferent access
-      return if @pii.present?
+      return if !@pii.present?
       flow_session.delete('Idv::Steps::DocumentCaptureStep')
       redirect_to idv_doc_auth_url
     end
