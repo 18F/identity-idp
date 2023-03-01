e099c828395238d65e54d932b02314d20b376add
eric-gade <eric.gade@@gsa.gov>
2023-03-01 13:04:33 -0500

    Updating combined upload step feature flag context test
    -- What
    This is relevant to the link_sent step tests

--- spec/features/idv/doc_auth/link_sent_step_spec.rb
+++ spec/features/idv/doc_auth/link_sent_step_spec.rb
@@ -123,19 +123,24 @@ feature 'doc auth link sent step' do
   context 'with combined upload step enabled', js: true do
     let(:user) { sign_in_and_2fa_user }
     let(:doc_capture_polling_enabled) { false }
+    let(:phone_number) { '415-555-0199' }
 
     before do
       allow(IdentityConfig.store).to receive(:doc_auth_combined_hybrid_handoff_enabled).and_return(true)
       allow(FeatureManagement).
         to receive(:doc_capture_polling_enabled?).and_return(doc_capture_polling_enabled)
       user
-      complete_doc_auth_steps_before_link_sent_step
+      #complete_doc_auth_steps_before_link_sent_step
+      complete_doc_auth_steps_before_upload_step
+      binding.pry
+      fill_in :doc_auth_phone, with: ''
+      fill_in :doc_auth_phone, with: phone_number
+      click_send_link
     end
 
     it 'Correctly renders the link sent step page' do
       expect(page).to have_current_path(idv_doc_auth_link_sent_step)
-      expect(false) # Need to check for phone # on page here
-      pending "We need to check the page for the phone number"
+      expect(page).to have_content(phone_number)
     end
   end
 end
