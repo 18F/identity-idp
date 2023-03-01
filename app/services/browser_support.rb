bbce70c1db5d0aa324cc8f0e8e3981f378b43a78
Andrew Duthie <andrew.duthie@gsa.gov>
2023-03-01 15:02:15 -0500

    Cache result and handle load error

--- app/services/browser_support.rb
+++ app/services/browser_support.rb
@@ -1,10 +1,25 @@
 class BrowserSupport
-  def self.supported?(user_agent)
-    matcher = BrowserslistUseragent::Match.new(browser_support_config, user_agent)
-    matcher.browser? && matcher.version?(allow_higher: true)
-  end
+  @cache = LruRedux::Cache.new(1_000)
 
-  def self.browser_support_config
-    @browser_support_config ||= JSON.parse(File.read(Rails.root.join('browsers.json')))
+  class << self
+    attr_reader :cache
+
+    def supported?(user_agent)
+      return false if user_agent.nil?
+      return true if browser_support_config.nil?
+
+      @cache.getset(user_agent) do
+        matcher = BrowserslistUseragent::Match.new(browser_support_config, user_agent)
+        matcher.browser? && matcher.version?(allow_higher: true)
+      end
+    end
+
+    def browser_support_config
+      @browser_support_config = begin
+        JSON.parse(File.read(Rails.root.join('browsers.json')))
+      rescue JSON::ParserError, Errno::ENOENT
+        nil
+      end
+    end
   end
 end
---
+++ spec/services/browser_support_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe BrowserSupport do
+  before { BrowserSupport.cache.clear }
+
+  describe '.supported?' do
+    let(:user_agent) do
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like ' \
+        'Gecko) Chrome/110.0.0.0 Safari/537.36'
+    end
+
+    subject(:supported) { BrowserSupport.supported?(user_agent) }
+
+    context 'with browser support config file missing' do
+      before do
+        expect(File).to receive(:read).with(Rails.root.join('browsers.json')).
+          and_raise(Errno::ENOENT.new)
+      end
+
+      it { expect(supported).to eq(true) }
+    end
+
+    context 'with invalid support config' do
+      before do
+        expect(File).to receive(:read).with(Rails.root.join('browsers.json')).and_return('invalid')
+      end
+
+      it { expect(supported).to eq(true) }
+    end
+
+    context 'with valid browser support config' do
+      before do
+        allow(BrowserSupport).to receive(:browser_support_config).
+          and_return(['chrome 109', 'chrome 108'])
+      end
+
+      context 'with nil user agent' do
+        let(:user_agent) { nil }
+
+        it { expect(supported).to eq(false) }
+      end
+
+      context 'with supported user agent' do
+        let(:user_agent) do
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like ' \
+            'Gecko) Chrome/110.0.0.0 Safari/537.36'
+        end
+
+        it { expect(supported).to eq(true) }
+      end
+
+      context 'with unsupported user agent' do
+        let(:user_agent) do
+          'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/51.0.2704.64 Safari/537.36'
+        end
+
+        it { expect(supported).to eq(false) }
+      end
+    end
+  end
+end
