696db4144aceaa560739712390c2e4944769f045
Mitchell Henke <mitchell.henke@gsa.gov>
2023-02-28 20:01:32 -0600

    Skip redundant delete and save of service provider authorization requests
    changelog: Internal, Service Provider Authorization Requests, Skip redundant delete and save of authorization requests

--- app/services/service_provider_request_handler.rb
+++ app/services/service_provider_request_handler.rb
@@ -7,6 +7,8 @@ class ServiceProviderRequestHandler
   end
 
   def call
+    return if sp_url_stored_in_session == url
+
     pull_request_id_from_current_sp_session_id
 
     delete_sp_request_if_session_has_matching_request_id
@@ -34,8 +36,19 @@ class ServiceProviderRequestHandler
   end
 
   def sp_stored_in_session
+    return if service_provider_request_proxy.blank?
+    service_provider_request_proxy.issuer
+  end
+
+  def sp_url_stored_in_session
+    return if service_provider_request_proxy.blank?
+    service_provider_request_proxy.url
+  end
+
+  def service_provider_request_proxy
     return if sp_request_id.blank?
-    ServiceProviderRequestProxy.from_uuid(sp_request_id).issuer
+    return @service_provider_request_proxy if defined?(@service_provider_request_proxy)
+    @service_provider_request_proxy = ServiceProviderRequestProxy.from_uuid(sp_request_id)
   end
 
   def pull_request_id_from_current_sp_session_id
