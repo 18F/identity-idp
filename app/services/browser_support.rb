f147dab599442a4aa992a33dbe515ec7773262f0
Andrew Duthie <andrew.duthie@gsa.gov>
2023-03-02 09:31:27 -0500

    Use shared matcher instance with ivar manipulation
    Performance optimization
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1123156400

--- app/services/browser_support.rb
+++ app/services/browser_support.rb
@@ -8,15 +8,19 @@ class BrowserSupport
       return false if user_agent.nil?
       return true if browser_support_config.nil?
 
-      cache.getset(user_agent) do
-        BrowserslistUseragent::Match.new(
-          browser_support_config,
-          user_agent,
-        ).version?(allow_higher: true)
-      end
+      cache.getset(user_agent) { matcher_supported_version?(user_agent) }
     end
 
     private
+
+    def matcher_supported_version?(user_agent)
+      matcher.instance_variable_set(:@user_agent_string, user_agent)
+      matcher.version?(allow_higher: true)
+    end
+
+    def matcher
+      @matcher ||= BrowserslistUseragent::Match.new(browser_support_config, nil)
+    end
 
     def browser_support_config
       @browser_support_config = begin
