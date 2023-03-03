6183c2e7d7594b47d57293b833cc748c90d762bf
John Maxwell <john.maxwell@gsa.gov>
2023-03-03 14:48:40 -0500

    Friday test hacking/fix file handles error (#7887)
    Tell WebMock not to delay HTTP connections for this test
    
    Why: spec/features/idv/hybrid_flow_test.rb was failing locally with "too many file handles" error.
    With some experimenting and web searching we found this possible solution and it made the test
    succeed five times in a row. (We hope it's fixed.)
    
    Co-authored-by: John Maxwell <john.maxwell@gsa.gov>
    Co-authored-by: Douglas Price <douglas.price@gsa.gov>
    Co-authored-by: John Skinner <john.skinner@gsa.gov>
    Co-authored-by: Alexander Bradley <alexander.bradley@gsa.gov>
    Co-authored-by: Sonia Connolly <sonia.connolly@gsa.gov>
    
    [skip changelog]

--- spec/features/idv/hybrid_flow_spec.rb
+++ spec/features/idv/hybrid_flow_spec.rb
@@ -9,6 +9,8 @@ describe 'Hybrid Flow' do
     allow(IdentityConfig.store).to receive(:doc_auth_enable_presigned_s3_urls).and_return(true)
     allow(Identity::Hostdata::EC2).to receive(:load).
       and_return(OpenStruct.new(region: 'us-west-2', account_id: '123456789'))
+    # Avoid "Too many open files - socket(2)" error on some local machines
+    WebMock.allow_net_connect!(net_http_connect_on_start: true)
   end
 
   before do
