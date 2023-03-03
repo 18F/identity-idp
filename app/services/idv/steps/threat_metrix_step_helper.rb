a3162ca585d4df6be52a5bca0ffab3241cde4c11
Osman Latif <olatif@flexion.us>
2023-03-03 14:13:15 -0600

    linting fixes

--- app/services/idv/steps/threat_metrix_step_helper.rb
+++ app/services/idv/steps/threat_metrix_step_helper.rb
@@ -51,7 +51,10 @@ module Idv
         return unless FeatureManagement.proofing_device_profiling_collecting_enabled?
         success = result[:review_status] == 'pass'
 
-        if !success && (tmx_summary_reason_code = result.dig(:response_body, :tmx_summary_reason_code))
+        if !success && (tmx_summary_reason_code = result.dig(
+          :response_body,
+          :tmx_summary_reason_code,
+        ))
           failure_reason = {
             tmx_summary_reason_code: tmx_summary_reason_code,
           }
