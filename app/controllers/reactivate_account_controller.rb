433f33184cd06d26895995c7797bbcd2ba87edfd
Jack Cody <jack.cody@gsa.gov>
2023-03-03 18:04:19 -0600

    Add WIP

--- app/controllers/reactivate_account_controller.rb
+++ app/controllers/reactivate_account_controller.rb
@@ -1,10 +1,20 @@
 class ReactivateAccountController < ApplicationController
   include AccountReactivationConcern
+  include RememberDeviceConcern
 
   before_action :confirm_two_factor_authenticated
   before_action :confirm_password_reset_profile
 
-  def index; end
+  def index
+    @presenter = AccountShowPresenter.new(
+      decrypted_pii: nil,
+      personal_key: nil,
+      sp_session_request_url: sp_session_request_url_with_updated_params,
+      sp_name: decorated_session.sp_name,
+      decorated_user: current_user.decorate,
+      locked_for_session: pii_locked_for_session?(current_user),
+    )
+  end
 
   def update
     reactivate_account_session.suspend
