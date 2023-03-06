306b9adc63da97d6292d0fda7a679ce27b7ace8f
Timothy Bradley <timothy.bradley@gsa.gov>
2023-03-06 15:49:44 -0700

    LG-7832: Transliterate name, address, and city for USPS API (w/ FF)
    changelog: Improvements, In-Person Proofing, USPS API Transliteration

---
+++ app/services/usps_in_person_proofing/transliterator.rb
@@ -0,0 +1,46 @@
+module UspsInPersonProofing
+  # Container to hold the results of transliteration
+  TransliterationResult = Struct.new(
+    # Was the value different after transliteration?
+    :changed?,
+    # Original value submtted for transliteration
+    :original,
+    # Transliterated value
+    :transliterated,
+    # Characters from the original that could not be transliterated
+    :unsupported_chars,
+  )
+
+  class Transliterator
+    # This is the default. May not be able to override this in current version.
+    REPLACEMENT = '?'.freeze
+
+    # Transliterate values for usage in the USPS API. This will additionally strip/reduce
+    # excess whitespace and check for special characters that are unsupported by transliteration.
+    # Additional validation may be necessary depending on the specific field being transliterated.
+    #
+    # @param [String,#to_s] value The value to transliterate for USPS
+    # @return [TransliterationResult] The transliterated value
+    def transliterate(value)
+      stripped = value.to_s.gsub(/\s+/, ' ').strip
+      transliterated = I18n.transliterate(stripped, locale: :en)
+
+      unsupported_chars = []
+      unless stripped.count(REPLACEMENT) == transliterated.count(REPLACEMENT)
+        transliterated.chars.each_with_index do |val, index|
+          if val == REPLACEMENT && stripped[index] != REPLACEMENT
+            unsupported_chars.append(stripped[index])
+          end
+        end
+      end
+
+      # Using struct instead of exception here to reduce likelihood of logging PII
+      TransliterationResult.new(
+        changed?: value != transliterated,
+        original: value,
+        transliterated: transliterated,
+        unsupported_chars: unsupported_chars,
+      )
+    end
+  end
+end
---
+++ spec/services/usps_in_person_proofing/transliterator_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe UspsInPersonProofing::Transliterator do
+  describe '#transliterate' do
+    let(:sut) { UspsInPersonProofing::Transliterator.new }
+    context 'baseline functionality' do
+      let(:inputValue) { "\t\n BobИy \t   TЉble?s\r\n" }
+      let(:result) { sut.transliterate(inputValue) }
+      let(:transliteratedResult) { 'Bob?y T?ble?s' }
+
+      let(:inputValue2) { 'Abc Is My Fav Number' }
+      let(:result2) { sut.transliterate(inputValue2) }
+      let(:transliteratedResult2) { inputValue2 }
+
+      it 'strips whitespace from the ends' do
+        expect(result.transliterated).not_to match(/^\s+/)
+        expect(result.transliterated).not_to match(/\s+^/)
+      end
+      it 'replaces consecutive whitespaces with regular spaces' do
+        expect(result.transliterated).not_to match(/\s\s/)
+        expect(result.transliterated).not_to match(/[^\S ]+/)
+      end
+      it 'returns a list of the characters that transliteration does not support' do
+        expect(result.unsupported_chars).to include('И', 'Љ')
+      end
+      it 'transliterates using English locale when default does not match' do
+        expect(I18n).to receive(:transliterate).
+          with(duck_type(:to_s), locale: :en).
+          and_call_original
+        result
+      end
+      it 'does not count question marks as unsupported characters by default' do
+        expect(result.unsupported_chars).not_to include('?')
+        expect(result.transliterated).to include('?')
+      end
+      it 'returns a shorthand for determining when transliteration changed the value' do
+        expect(result.changed?).to be(true)
+        expect(result2.changed?).to be(false)
+      end
+      it 'returns the original value that was requested to be transliterated' do
+        expect(result.original).to eq(inputValue)
+        expect(result2.original).to eq(inputValue2)
+      end
+      it 'returns the expected transliterated value for the examples' do
+        expect(result.transliterated).to eq(transliteratedResult)
+        expect(result2.transliterated).to eq(transliteratedResult2)
+      end
+    end
+
+    context 'for additional values not supported for transliteration by default' do
+      {
+        # Convert okina to apostrophe
+        "ʻ": "'",
+        # Convert quotation marks
+        "’": "'",
+        "‘": "'",
+        "‛": "'",
+        "“": '"',
+        "‟": '"',
+        "”": '"',
+        # Convert hyphens
+        "‐": '-',
+        "‑": '-',
+        "‒": '-',
+        "–": '-',
+        "—": '-',
+        "﹘": '-',
+        # Convert number signs
+        "﹟": '#',
+        "＃": '#',
+      }.each do |key, value|
+        it "converts \"\\u#{key.to_s.ord.to_s(16).rjust(
+          4,
+          '0',
+        )}\" to \"\\u#{value.ord.to_s(16).rjust(
+          4, '0'
+        )}\"" do
+          expect(sut.transliterate(key).transliterated).to eq(value)
+        end
+      end
+    end
+  end
+end
