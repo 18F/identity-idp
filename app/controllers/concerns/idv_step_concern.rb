93fab156ea94480cc6ef6c282b36394538e2aee9
Jonathan Hooper <jonathan.hooper@gsa.gov>
2023-03-06 17:14:36 -0500

    Add `#confirm_address_step_complete` to the Idv Step Concern (#7931)
    The IdV step concern is intended to be used to make certain that previous IdV steps have been completed before allowing a user to complete a step. This commit start the process of implementing these steps by adding an available before action to ensure the address step is complete and using that in the review controller
    
    changelog: Internal, Improvements, A before action to verify that the user has completed the address step before allowing the user to complete steps that depend on the address step was added to IdV step concern so it is available to controllers that implement IdV steps.

--- app/controllers/concerns/idv_step_concern.rb
+++ app/controllers/concerns/idv_step_concern.rb
@@ -7,4 +7,10 @@ module IdvStepConcern
     before_action :confirm_two_factor_authenticated
     before_action :confirm_idv_needed
   end
+
+  def confirm_address_step_complete
+    return if idv_session.address_step_complete?
+
+    redirect_to idv_otp_verification_path
+  end
 end
