e1a1d1958a05964549d3e1e73d2f590e9367e5e5
Andrew Duthie <andrew.duthie@gsa.gov>
2023-03-06 15:50:16 -0500

    Add comments for user agent strings
    For clarity
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1125056006
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>

--- spec/services/browser_support_spec.rb
+++ spec/services/browser_support_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe BrowserSupport do
 
   describe '.supported?' do
     let(:user_agent) do
+      # Chrome v110
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like ' \
         'Gecko) Chrome/110.0.0.0 Safari/537.36'
     end
@@ -42,6 +43,7 @@ RSpec.describe BrowserSupport do
 
       context 'with supported user agent' do
         let(:user_agent) do
+          # Chrome v110
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like ' \
             'Gecko) Chrome/110.0.0.0 Safari/537.36'
         end
@@ -51,6 +53,7 @@ RSpec.describe BrowserSupport do
 
       context 'with unsupported user agent' do
         let(:user_agent) do
+          # Chrome v51
           'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) \
             Chrome/51.0.2704.64 Safari/537.36'
         end
@@ -60,6 +63,7 @@ RSpec.describe BrowserSupport do
 
       context 'with user agent for non-numeric version test' do
         let(:user_agent) do
+          # Opera v12
           'Opera/9.80 (Android; Opera Mini/36.2.2254/119.132; U; id) Presto/2.12.423 Version/12.16)'
         end
 
@@ -69,6 +73,7 @@ RSpec.describe BrowserSupport do
       context 'with user agent for version range test' do
         context 'below version range' do
           let(:user_agent) do
+            # Safari v11.2
             'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, ' \
               'like Gecko) Version/11.2 Mobile/15E148 Safari/604.1'
           end
@@ -78,6 +83,7 @@ RSpec.describe BrowserSupport do
 
         context 'within version range' do
           let(:user_agent) do
+            # Safari v14.6
             'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, ' \
               'like Gecko) Mobile/15E148 Version/14.6 Safari/605.1.15 AlohaBrowser/3.1.5'
           end
@@ -87,6 +93,7 @@ RSpec.describe BrowserSupport do
 
         context 'above version range' do
           let(:user_agent) do
+            # Safari v16.3
             'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML' \
               ', like Gecko) Version/16.3 Mobile/15E148 Safari/604.1'
           end
