c9b09526dcba74f2884ffd4b31c9c109079a004c
Andrew Duthie <andrew.duthie@gsa.gov>
2023-03-07 11:44:20 -0500

    LG-8527: Limit NewRelic browser instrumentation to supported browsers (#7909)
    * LG-8527: Limit NewRelic browser instrumentation to supported browsers
    
    changelog: Internal, Error Reporting, Track frontend errors from supported browsers
    
    * Cache result and handle load error
    
    * Update yarn.lock
    
    * Make BrowserSupport.browser_support_config private
    
    So I don't have to feel compelled to write specs for it
    
    * Leverage attr_reader for internal cache reference
    
    * Simplify stub value
    
    * Generate browsers.json as dependency of make run
    
    https://github.com/18F/identity-idp/pull/7909#discussion_r1122253065
    
    * Fix typo browsers.json
    
    * Define target dependencies for cachebusting
    
    * Remove extra check for browser
    
    Despite this being pulled from their documented example, this appears redundant, since the version check will already include an equivalent (duplicate) check for the browser
    
    See: https://github.com/browserslist/browserslist-useragent-ruby/blob/f17624b1c678a34d67038efcb17394b0175317ae/lib/browserslist_useragent/match.rb#L21
    
    * Use shared matcher instance with ivar manipulation
    
    Performance optimization
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1123156400
    
    * Parse browserslist config and compare against parsed browser
    
    * Add spec for browsers.json supported keys
    
    * Name second part of split version
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1126578381
    
    * Limit `make browsers` to once per spec run
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1125083411
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>
    
    * Define browsers.json as spec subject
    
    * Add comments for user agent strings
    
    For clarity
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1125056006
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>
    
    * Avoid lingering cached class variables between specs
    
    * Test platform for platform-specific browserslist keys
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1125050706
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>
    
    * Remove redundant transform_values
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1127053921
    Co-Authored-By: Zach Margolis <zbmargolis@gmail.com>
    
    * Add cache clearing method for use in tests
    
    See: https://github.com/18F/identity-idp/pull/7909#discussion_r1127053540
    
    * Reorder condition to always run BrowserSupport
    
    ---------
    
    Co-authored-by: Zach Margolis <zbmargolis@gmail.com>

---
+++ spec/services/browser_support_spec.rb
@@ -0,0 +1,127 @@
+require 'rails_helper'
+
+RSpec.describe BrowserSupport do
+  before { BrowserSupport.clear_cache! }
+
+  describe '.supported?' do
+    let(:user_agent) do
+      # Chrome v110
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like ' \
+        'Gecko) Chrome/110.0.0.0 Safari/537.36'
+    end
+
+    subject(:supported) { BrowserSupport.supported?(user_agent) }
+
+    context 'with browser support config file missing' do
+      before do
+        expect(File).to receive(:read).once.with(Rails.root.join('browsers.json')).
+          and_raise(Errno::ENOENT.new)
+      end
+
+      it { expect(supported).to eq(true) }
+
+      it 'memoizes result of parsing browser config' do
+        BrowserSupport.supported?('a')
+        BrowserSupport.supported?('b')
+      end
+    end
+
+    context 'with invalid support config' do
+      before do
+        expect(File).to receive(:read).once.with(Rails.root.join('browsers.json')).
+          and_return('invalid')
+      end
+
+      it { expect(supported).to eq(true) }
+
+      it 'memoizes result of parsing browser config' do
+        BrowserSupport.supported?('a')
+        BrowserSupport.supported?('b')
+      end
+    end
+
+    context 'with valid browser support config' do
+      before do
+        allow(BrowserSupport).to receive(:browser_support_config).
+          and_return(['chrome 109', 'and_chr 108', 'ios_saf 14.5-14.8', 'op_mini all'])
+      end
+
+      context 'with nil user agent' do
+        let(:user_agent) { nil }
+
+        it { expect(supported).to eq(false) }
+      end
+
+      context 'with supported user agent' do
+        let(:user_agent) do
+          # Chrome v110
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like ' \
+            'Gecko) Chrome/110.0.0.0 Safari/537.36'
+        end
+
+        it { expect(supported).to eq(true) }
+      end
+
+      context 'with unsupported user agent' do
+        let(:user_agent) do
+          # Chrome v108
+          'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+            'Chrome/108.0.2704.64 Safari/537.36'
+        end
+
+        it { expect(supported).to eq(false) }
+      end
+
+      context 'with user agent for non-numeric version test' do
+        let(:user_agent) do
+          # Opera v12
+          'Opera/9.80 (Android; Opera Mini/36.2.2254/119.132; U; id) Presto/2.12.423 Version/12.16)'
+        end
+
+        it { expect(supported).to eq(true) }
+      end
+
+      context 'with user agent for version range test' do
+        context 'below version range' do
+          let(:user_agent) do
+            # Safari v11.2
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, ' \
+              'like Gecko) Version/11.2 Mobile/15E148 Safari/604.1'
+          end
+
+          it { expect(supported).to eq(false) }
+        end
+
+        context 'within version range' do
+          let(:user_agent) do
+            # Safari v14.6
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, ' \
+              'like Gecko) Mobile/15E148 Version/14.6 Safari/605.1.15 AlohaBrowser/3.1.5'
+          end
+
+          it { expect(supported).to eq(true) }
+        end
+
+        context 'above version range' do
+          let(:user_agent) do
+            # Safari v16.3
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML' \
+              ', like Gecko) Version/16.3 Mobile/15E148 Safari/604.1'
+          end
+
+          it { expect(supported).to eq(true) }
+        end
+      end
+    end
+
+    context 'with user agent for platform-specific version support' do
+      let(:user_agent) do
+        # Android Chrome v108
+        'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+          'Chrome/108.0.5481.153 Mobile Safari/537.36'
+      end
+
+      it { expect(supported).to eq(true) }
+    end
+  end
+end
