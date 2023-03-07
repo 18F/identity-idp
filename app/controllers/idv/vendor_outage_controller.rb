90d6f8709ecb768c65c22e34cfe1d7348f75c5f4
eric-gade <eric.gade@gsa.gov>
2023-03-07 10:59:45 -0500

    Updating view and localizations

--- app/controllers/idv/vendor_outage_controller.rb
+++ app/controllers/idv/vendor_outage_controller.rb
@@ -1,13 +1,13 @@
 module Idv
   class VendorOutageController < ApplicationController
-    #include IdvSession
-    #include StepIndicatorConcern
+    include IdvSession
+    include StepIndicatorConcern
 
-    # before_action :confirm_two_factor_authenticated
+    before_action :confirm_two_factor_authenticated
 
     def show
       session[:skip_vendor_outage] = true
-      render :show
+      render :show, locals: { current_sp: current_sp }
     end
   end
 end
