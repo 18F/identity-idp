fdb84b412acada0ffd3b10ab86fd34ebaa0e5f0a
Alex Bradley <alexander.bradley@gsa.gov>
2023-03-08 13:58:50 -0500

    fix migration

--- db/primary_migrate/20230307203559_add_index_on_fraud_review_pending_to_profiles.rb
+++ db/primary_migrate/20230307203559_add_index_on_fraud_review_pending_to_profiles.rb
@@ -2,6 +2,6 @@ class AddIndexOnFraudReviewPendingToProfiles < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    add_index :profiles, :fraud_review_pending, algorithm: :concurrently
+    add_index :profiles, [:fraud_review_pending], algorithm: :concurrently
   end
 end
