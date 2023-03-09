a4612d2ccee9c61cbe0baf98e5e8d8a6db722d5b
Matt Gardner <wilburnforce@gmail.com>
2023-03-09 16:33:31 -0500

    Include asset_url

--- spec/components/previews/barcode_component_preview.rb
+++ spec/components/previews/barcode_component_preview.rb
@@ -1,13 +1,13 @@
 class BarcodeComponentPreview < BaseComponentPreview
   # @!group Preview
   def default
-    render(BarcodeComponent.new(barcode_data: '1234567812345678', label: 'Barcode'))
+    render(BarcodeComponent.new(barcode_data: '1234567812345678', label: 'Barcode', logo_image_url: asset_url('logo.svg')))
   end
   # @!endgroup
 
   # @param barcode_data text
   # @param label text
   def workbench(barcode_data: '1234567812345678', label: 'Barcode')
-    render(BarcodeComponent.new(barcode_data:, label:))
+    render(BarcodeComponent.new(barcode_data:, label:, logo_image_url: asset_url('logo.svg')))
   end
 end
