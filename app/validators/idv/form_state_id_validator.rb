58b3934bc8234ef248eaaca6353915532a8e1c0a
Tim Bradley <90272033+NavaTim@users.noreply.github.com>
2023-03-10 10:10:59 -0700

    LG-7832: Validate State ID and Address are transliterable to a valid USPS submission (#7938)
    * LG-7832: Validate State ID and Address are transliterable to a valid USPS submission
    
    changelog: Improvements, In-Person Proofing, Validate State ID and Address are transliterable to a valid USPS submission
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * LG-7832: Make test updates based on PR feedback
    
    * Update spec/services/usps_in_person_proofing/enrollment_helper_spec.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update app/services/usps_in_person_proofing/enrollment_helper.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * LG-7832: Remove incorrect usage of .class
    
    * LG-7832: Feature should only be enabled by default in dev environments
    
    * Update app/services/idv/steps/in_person/address_step.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Update app/services/idv/steps/in_person/state_id_step.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * LG-7832: Use case no longer requires support for HashWithIndifferentAccess
    
    * LG-7832: Clean up translation usage
    
    * LG-7832: Update test
    
    * LG-7832: Update test
    
    * LG-7832: Simplify unsupported character extraction from transliterated string
    
    * LG-7832: Clarify transliteration result documentation
    
    * LG-7832: Refactor helper logic into validator classes
    
    * Switch to ->() lambda syntax to remove extra parens
    
    * Change one more proc syntax to "stabby lambda"
    
    * un-stub the TransliterableValidator spec as much as possible
    
    - Real ActiveModel::Errors instance
    - Model fields all defined in one spot
    - spy on real validator.transliterator instead of stubbing
    
    ---------
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    Co-authored-by: Zach Margolis <zachary.margolis@gsa.gov>

--- app/validators/idv/form_state_id_validator.rb
+++ app/validators/idv/form_state_id_validator.rb
@@ -9,6 +9,16 @@ module Idv
                 :state_id_jurisdiction,
                 :state_id_number,
                 presence: true
+
+      validates_with UspsInPersonProofing::TransliterableValidator,
+                     fields: [:first_name, :last_name],
+                     reject_chars: /[^A-Za-z\-' ]/,
+                     message: ->(invalid_chars) do
+                       I18n.t(
+                         'in_person_proofing.form.state_id.errors.unsupported_chars',
+                         char_list: invalid_chars.join(', '),
+                       )
+                     end
     end
   end
 end
