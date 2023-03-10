17f50808023e396728b08a13d9af594fe76bf31f
Sonia Connolly <sonia.connolly@gsa.gov>
2023-03-10 09:22:16 -0800

    Make hint strings more readable

--- app/presenters/idv/address_presenter.rb
+++ app/presenters/idv/address_presenter.rb
@@ -13,19 +13,19 @@ module Idv
     end
 
     def address_line1_hint
-      I18n.t('forms.example') + ' 150 Calle A Apt 3' if puerto_rico_address?
+      "#{I18n.t('forms.example')} 150 Calle A Apt 3" if puerto_rico_address?
     end
 
     def address_line2_hint
-      I18n.t('forms.example') + ' URB Las Gladiolas' if puerto_rico_address?
+      "#{I18n.t('forms.example')} URB Las Gladiolas" if puerto_rico_address?
     end
 
     def city_hint
-      I18n.t('forms.example') + ' San Juan' if puerto_rico_address?
+      "#{I18n.t('forms.example')} San Juan" if puerto_rico_address?
     end
 
     def zipcode_hint
-      I18n.t('forms.example') + ' 00926' if puerto_rico_address?
+      "#{I18n.t('forms.example')} 00926" if puerto_rico_address?
     end
   end
 end
