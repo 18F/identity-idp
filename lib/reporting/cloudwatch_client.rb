323daa98349559dd6dd9dba4111a501245a871d7
Zach Margolis <zachary.margolis@gsa.gov>
2023-03-14 13:09:01 -0700

    More configuration, add JSON parsing of @message

--- lib/reporting/cloudwatch_client.rb
+++ lib/reporting/cloudwatch_client.rb
@@ -1,7 +1,27 @@
 module Reporting
   class CloudwatchClient
-    NUM_THREADS = 5
-    WAIT_DURATION = 3
+    DEFAULT_NUM_THREADS = 5
+    DEFAULT_WAIT_DURATION = 3
+
+    attr_reader :num_threads, :wait_duration, :slice_interval, :logger
+
+    # @param [Boolean] ensure_complete_logs when true, will detect when queries return exactly
+    #  10,000 rows (Cloudwatch Insights max limit) and then recursively split the query window into
+    #  two queries until we're certain we've queried all rows
+    # @param [ActiveSupport::Duration] slice_interval starting interval to split up and query across
+    def initialize(
+      ensure_complete_logs: true,
+      num_threads: DEFAULT_NUM_THREADS,
+      wait_duration: DEFAULT_WAIT_DURATION,
+      slice_interval: 1.day,
+      logger: Logger.new(STDERR)
+    )
+      @ensure_complete_logs = ensure_complete_logs
+      @num_threads = num_threads
+      @wait_duration = wait_duration
+      @slice_interval = slice_interval
+      @logger = logger
+    end
 
     # @param [Reporting::CloudwatchQuery] query
     # @param [Time] from
@@ -9,16 +29,15 @@ module Reporting
     # @return [Array<Array<Types::ResultField>>]
     def fetch(query:, from:, to:)
       results = Concurrent::Array.new
-      errors = Concurrent::Array.new
       queue = Queue.new
 
-      slice_days(from:, to:).map do |range|
+      slice_time_range(from:, to:).map do |range|
         queue << range
       end
 
-      logger.info("starting query, queue_size=#{queue.length} num_threads=#{NUM_THREADS}")
+      logger.info("starting query, queue_size=#{queue.length} num_threads=#{num_threads}")
 
-      threads = NUM_THREADS.times.map do |thread_idx|
+      threads = num_threads.times.map do |thread_idx|
         Thread.new do
           while (range = queue.pop)
             start_time = range.begin.to_i
@@ -26,12 +45,13 @@ module Reporting
 
             response = fetch_one(query:, start_time:, end_time:)
 
-            if response.results.size == Reporting::CloudwatchQuery::MAX_LIMIT
+            if ensure_complete_logs? && response.results.size == Reporting::CloudwatchQuery::MAX_LIMIT
+              logger.info("exact limit reached, bisecting: start_time=#{start_time} end_time=#{end_time}")
               mid = midpoint(start_time:, end_time:)
               queue << start_time..mid
               queue << mid..end_time
             else
-              results.concat(response.results)
+              results.concat(parse_results(response.results))
             end
           end
 
@@ -42,7 +62,7 @@ module Reporting
       end
 
       until queue.empty?
-        sleep WAIT_DURATION
+        sleep wait_duration
       end
       queue.close
       threads.map(&:value) # wait for all threads
@@ -67,21 +87,31 @@ module Reporting
       wait_for_query_result(query_id)
     end
 
-    def logger
-      @logger = Logger.new(STDERR)
+    def ensure_complete_logs?
+      @ensure_complete_logs
     end
 
     private
 
+    # Pulls out and parses the "@message" field as JSON
+    # @param [Array<Array<Types::ResultField>>] results
+    # @return [Array<Hash>]
+    def parse_results(results)
+      results.map do |result_fields|
+        json_str = result_fields.find { |result_field| result_field.field == '@message' }.value
+        JSON.parse(json_str)
+      end
+    end
+
     # @return [Array<Range<Time>>]
-    def slice_days(from:, to:)
+    def slice_time_range(from:, to:)
       slices = []
       low = from
       high = to
       while low < high
-        slice_end = [low + 1.day, high].min
+        slice_end = [low + slice_interval, high].min
         slices << (low..slice_end)
-        low += 1.day
+        low += slice_interval
       end
       slices
     end
@@ -96,7 +126,7 @@ module Reporting
 
       loop do
         logger.info("waiting on query_id=#{query_id}")
-        sleep WAIT_DURATION
+        sleep wait_duration
         response = cloudwatch_client.get_query_results(query_id: query_id)
         case response.status
         when 'Complete', 'Failed', 'Cancelled'
