dfbf886218e3cb30b106d06abd5800c93be3a508
Alex Bradley <alexander.bradley@gsa.gov>
2023-03-15 15:57:34 -0400

    add redirect for users with rejected profiles

--- app/controllers/concerns/fraud_review_concern.rb
+++ app/controllers/concerns/fraud_review_concern.rb
@@ -9,8 +9,17 @@ module FraudReviewConcern
     redirect_to idv_setup_errors_url
   end
 
+  def redirect_to_fraud_rejection
+    redirect_to idv_setup_errors_url
+  end
+
   def fraud_review_pending?
     return false unless user_fully_authenticated?
     current_user.fraud_review_pending?
+  end
+
+  def fraud_rejection?
+    return false unless user_fully_authenticated?
+    current_user.fraud_rejection?
   end
 end
