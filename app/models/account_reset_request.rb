e5d198d3a7b8d908f00a11b96c929c0cdb40f380
Osman Latif <109746710+olatifflexion@users.noreply.github.com>
2023-03-15 11:33:26 -0500

    LG-9067: Account reset account deleted event fixes (#7982)
    * LG-9067: Account reset account deleted event fixes
    
    changelog: Internal, Attempts API, Update & fixes Account reset account deleted event

--- app/models/account_reset_request.rb
+++ app/models/account_reset_request.rb
@@ -2,6 +2,12 @@ class AccountResetRequest < ApplicationRecord
   self.ignored_columns = %w[reported_fraud_at]
 
   belongs_to :user
+  # rubocop:disable Rails/InverseOf
+  belongs_to :requesting_service_provider,
+             class_name: 'ServiceProvider',
+             foreign_key: 'requesting_issuer',
+             primary_key: 'issuer'
+  # rubocop:enable Rails/InverseOf
 
   def granted_token_valid?
     granted_token.present? && !granted_token_expired?
