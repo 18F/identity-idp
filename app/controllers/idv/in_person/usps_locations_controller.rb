d1aa620f1aee4e5c2d5747b5fd7de8957d3cb1ef
Andrew Duthie <andrew.duthie@gsa.gov>
2023-03-16 11:08:19 -0400

    Remove arcgis_search_enabled feature flag
    Why: This feature is now enabled in production and would not be expected to be reverted to pilot facilities, and its continued existence in the code creates confusion for other developers.

--- app/controllers/idv/in_person/usps_locations_controller.rb
+++ app/controllers/idv/in_person/usps_locations_controller.rb
@@ -20,26 +20,21 @@ module Idv
 
       # retrieve the list of nearby IPP Post Office locations with a POST request
       def index
-        response = []
-        if IdentityConfig.store.arcgis_search_enabled
-          candidate = UspsInPersonProofing::Applicant.new(
-            address: search_params['street_address'],
-            city: search_params['city'], state: search_params['state'],
-            zip_code: search_params['zip_code']
+        candidate = UspsInPersonProofing::Applicant.new(
+          address: search_params['street_address'],
+          city: search_params['city'], state: search_params['state'],
+          zip_code: search_params['zip_code']
+        )
+        response = proofer.request_facilities(candidate)
+        if response.length > 0
+          analytics.idv_in_person_locations_searched(
+            success: true,
+            result_total: response.length,
           )
-          response = proofer.request_facilities(candidate)
-          if response.length > 0
-            analytics.idv_in_person_locations_searched(
-              success: true,
-              result_total: response.length,
-            )
-          else
-            analytics.idv_in_person_locations_searched(
-              success: false, errors: 'No USPS locations found',
-            )
-          end
         else
-          response = proofer.request_pilot_facilities
+          analytics.idv_in_person_locations_searched(
+            success: false, errors: 'No USPS locations found',
+          )
         end
         render json: response.to_json
       end
