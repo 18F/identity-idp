121f4b231b4498328565939200838fbfe234d35e
Matt Gardner <wilburnforce@gmail.com>
2023-03-16 15:20:08 -0400

    Add image assertion

--- spec/features/idv/in_person_spec.rb
+++ spec/features/idv/in_person_spec.rb
@@ -226,6 +226,7 @@ RSpec.describe 'In Person Proofing', js: true do
     enrollment_code = JSON.parse(
       UspsInPersonProofing::Mock::Fixtures.request_enroll_response,
     )['enrollmentCode']
+    expect(page).to have_css("img[alt='#{APP_NAME}']")
     expect(page).to have_content(t('in_person_proofing.headings.barcode'))
     expect(page).to have_content(Idv::InPerson::EnrollmentCodeFormatter.format(enrollment_code))
     expect(page).to have_content(t('in_person_proofing.body.barcode.deadline', deadline: deadline))
