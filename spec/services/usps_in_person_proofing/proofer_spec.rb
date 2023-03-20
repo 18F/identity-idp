4517b294ebb4831642384c43deca5ec06fa27344
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2023-03-20 14:28:48 -0400

    Update specs to not rely on cache implementation

--- spec/services/usps_in_person_proofing/proofer_spec.rb
+++ spec/services/usps_in_person_proofing/proofer_spec.rb
@@ -12,8 +12,11 @@ RSpec.describe UspsInPersonProofing::Proofer do
   end
 
   describe '#retrieve_token!' do
+    let(:auth_token) do
+      '==PZWyMP2ZHGOIeTd17YomIf7XjZUL4G93dboY1pTsuTJN0s9BwMYvOcIS9B3gRvloK2sroi9uFXdXrFuly7=='
+    end
     it 'sets token and expiry' do
-      expect(cache).to receive(:write).with(
+      expect(Rails.cache).to receive(:write).with(
         UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY,
         an_instance_of(String),
         hash_including(expires_at: an_instance_of(ActiveSupport::TimeWithZone)),
@@ -21,9 +24,7 @@ RSpec.describe UspsInPersonProofing::Proofer do
       stub_request_token
       subject.retrieve_token!
 
-      expect(subject.token).to eq(
-        '==PZWyMP2ZHGOIeTd17YomIf7XjZUL4G93dboY1pTsuTJN0s9BwMYvOcIS9B3gRvloK2sroi9uFXdXrFuly7==',
-      )
+      expect(subject.token).to eq("Bearer #{auth_token}")
     end
 
     it 'calls the authenticate endpoint with the expected params' do
@@ -193,84 +194,63 @@ RSpec.describe UspsInPersonProofing::Proofer do
         zip_code: Faker::Address.zip_code,
       )
     end
-    context 'when the token is valid' do
-      before do
-        allow(Rails).to receive(:cache).and_return(
-          ActiveSupport::Cache::RedisCacheStore.new(url: IdentityConfig.store.redis_throttle_url),
-        )
-        stub_request_token
-      end
 
-      it 'returns facilities' do
-        stub_request_facilities
-        facilities = subject.request_facilities(location)
+    before do
+      stub_request_token
+    end
 
-        check_facility(facilities[0])
-      end
+    it 'returns facilities' do
+      stub_request_facilities
+      facilities = subject.request_facilities(location)
 
-      it 'returns facilities sorted by ascending distance' do
-        stub_request_facilities_with_unordered_distance
-        facilities = subject.request_facilities(location)
+      check_facility(facilities[0])
+    end
 
-        expect(facilities.count).to be > 1
-        facilities.each_cons(2) do |facility_a, facility_b|
-          expect(facility_a.distance).to be <= facility_b.distance
-        end
-      end
+    it 'returns facilities sorted by ascending distance' do
+      stub_request_facilities_with_unordered_distance
+      facilities = subject.request_facilities(location)
 
-      it 'does not return duplicates' do
-        stub_request_facilities_with_duplicates
-        facilities = subject.request_facilities(location)
-
-        expect(facilities.length).to eq(9)
-        expect(
-          facilities.count do |post_office|
-            post_office.address == '3775 INDUSTRIAL BLVD'
-          end,
-        ).to eq(1)
+      expect(facilities.count).to be > 1
+      facilities.each_cons(2) do |facility_a, facility_b|
+        expect(facility_a.distance).to be <= facility_b.distance
       end
     end
 
-    context 'when the token is expired' do
-      let(:cache) { double(ActiveSupport::Cache::MemoryStore) }
-      let(:redis) { double(Redis) }
+    it 'does not return duplicates' do
+      stub_request_facilities_with_duplicates
+      facilities = subject.request_facilities(location)
+
+      expect(facilities.length).to eq(9)
+      expect(
+        facilities.count do |post_office|
+          post_office.address == '3775 INDUSTRIAL BLVD'
+        end,
+      ).to eq(1)
+    end
+
+    context 'when the auth token is expired' do
+      let(:expires_in) { 15.minutes }
+
       before do
-        set_up_expired_token(cache, redis)
-        stub_request_token
         stub_request_facilities
       end
 
-      it 'fetches a new token' do
-        check_for_token_refresh_and_method_call(cache, redis)
-
-        facilities = subject.request_facilities(location)
+      it 'refreshes the auth token before making the request' do
+        subject.token
+        expired_time = Time.zone.now + expires_in
+        facilities = nil
+        travel_to(expired_time) do
+          facilities = subject.request_facilities(location)
+        end
 
         expect(WebMock).to have_requested(:post, "#{root_url}/oauth/authenticate").twice
-
         expect(facilities.length).to eq(10)
-        check_facility(facilities[0])
-      end
-    end
-
-    context 'when the token is stored in an in-memory cache' do
-      before do
-        allow(Rails).to receive(:cache).and_return(
-          ActiveSupport::Cache::MemoryStore.new,
-        )
-        stub_request_token
-      end
-      it 'returns facilities' do
-        stub_request_facilities
-        facilities = subject.request_facilities(location)
-
         check_facility(facilities[0])
       end
     end
   end
 
   describe '#request_enroll' do
-    let(:cache) { double(ActiveSupport::Cache::MemoryStore) }
-    let(:redis) { double(Redis) }
     let(:applicant) do
       double(
         'applicant',
@@ -284,62 +264,65 @@ RSpec.describe UspsInPersonProofing::Proofer do
         unique_id: '123456789',
       )
     end
-    context 'when the token is valid' do
-      before do
-        allow(Rails).to receive(:cache).and_return(
-          ActiveSupport::Cache::RedisCacheStore.new(url: IdentityConfig.store.redis_throttle_url),
-        )
-        stub_request_token
-      end
-      it 'returns enrollment information' do
-        stub_request_enroll
 
-        enrollment = subject.request_enroll(applicant)
-        expect(enrollment.enrollment_code).to be_present
-        expect(enrollment.response_message).to be_present
-      end
-
-      it 'returns 400 error' do
-        stub_request_enroll_bad_request_response
-
-        expect { subject.request_enroll(applicant) }.to raise_error(
-          an_instance_of(Faraday::BadRequestError).
-          and(having_attributes(
-            response: include(
-              body: include(
-                'responseMessage' => 'Sponsor for sponsorID 5 not found',
-              ),
-            ),
-          )),
-        )
-      end
-
-      it 'returns 500 error' do
-        stub_request_enroll_internal_server_error_response
-
-        expect { subject.request_enroll(applicant) }.to raise_error(
-          an_instance_of(Faraday::ServerError).
-          and(having_attributes(
-            response: include(
-              body: include(
-                'responseMessage' => 'An internal error occurred processing the request',
-              ),
-            ),
-          )),
-        )
-      end
+    before do
+      stub_request_token
     end
 
-    context 'when the token is expired' do
+    it 'returns enrollment information' do
+      stub_request_enroll
+
+      enrollment = subject.request_enroll(applicant)
+      expect(enrollment.enrollment_code).to be_present
+      expect(enrollment.response_message).to be_present
+    end
+
+    it 'returns 400 error' do
+      stub_request_enroll_bad_request_response
+
+      expect { subject.request_enroll(applicant) }.to raise_error(
+        an_instance_of(Faraday::BadRequestError).
+        and(having_attributes(
+          response: include(
+            body: include(
+              'responseMessage' => 'Sponsor for sponsorID 5 not found',
+            ),
+          ),
+        )),
+      )
+    end
+
+    it 'returns 500 error' do
+      stub_request_enroll_internal_server_error_response
+
+      expect { subject.request_enroll(applicant) }.to raise_error(
+        an_instance_of(Faraday::ServerError).
+        and(having_attributes(
+          response: include(
+            body: include(
+              'responseMessage' => 'An internal error occurred processing the request',
+            ),
+          ),
+        )),
+      )
+    end
+
+    context 'when the auth token is expired' do
+      let(:expires_in) { 15.minutes }
+
       before do
-        set_up_expired_token(cache, redis)
-        stub_request_enroll_expired_token
         stub_request_enroll
       end
 
-      it 'fetches a new token and retries the attempt' do
-        check_for_token_refresh_and_method_call(cache, redis)
-        enrollment = subject.request_enroll(applicant)
+      it 'refreshes the auth token before making the request' do
+        subject.token
+        expired_time = Time.zone.now + expires_in
+        enrollment = nil
+        travel_to(expired_time) do
+          enrollment = subject.request_enroll(applicant)
+        end
+
+        expect(WebMock).to have_requested(:post, "#{root_url}/oauth/authenticate").twice
 
         expect(enrollment.enrollment_code).to be_present
         expect(enrollment.response_message).to be_present
@@ -355,83 +338,72 @@ RSpec.describe UspsInPersonProofing::Proofer do
         enrollment_code: '123456789',
       )
     end
-    context 'when the token is valid' do
+
+    before do
+      stub_request_token
+    end
+
+    it 'returns failed enrollment information' do
+      stub_request_failed_proofing_results
+
+      proofing_results = subject.request_proofing_results(
+        applicant.unique_id,
+        applicant.enrollment_code,
+      )
+      expect(proofing_results['status']).to eq 'In-person failed'
+      expect(proofing_results['fraudSuspected']).to eq false
+    end
+
+    it 'returns passed enrollment information' do
+      stub_request_passed_proofing_results
+
+      proofing_results = subject.request_proofing_results(
+        applicant.unique_id,
+        applicant.enrollment_code,
+      )
+      expect(proofing_results['status']).to eq 'In-person passed'
+      expect(proofing_results['fraudSuspected']).to eq false
+    end
+
+    it 'returns in-progress enrollment information' do
+      stub_request_in_progress_proofing_results
+
+      expect do
+        subject.request_proofing_results(
+          applicant.unique_id,
+          applicant.enrollment_code,
+        )
+      end.to raise_error(
+        an_instance_of(Faraday::BadRequestError).
+        and(having_attributes(
+          response: include(
+            body: include(
+              'responseMessage' => 'Customer has not been to a post office to complete IPP',
+            ),
+          ),
+        )),
+      )
+    end
+
+    context 'when the auth token is expired' do
+      let(:expires_in) { 15.minutes }
+
       before do
-        allow(Rails).to receive(:cache).and_return(
-          ActiveSupport::Cache::RedisCacheStore.new(url: IdentityConfig.store.redis_throttle_url),
-        )
-        stub_request_token
-      end
-      it 'returns failed enrollment information' do
-        stub_request_failed_proofing_results
-
-        proofing_results = subject.request_proofing_results(
-          applicant.unique_id,
-          applicant.enrollment_code,
-        )
-        expect(proofing_results['status']).to eq 'In-person failed'
-        expect(proofing_results['fraudSuspected']).to eq false
-      end
-
-      it 'returns passed enrollment information' do
         stub_request_passed_proofing_results
-
-        proofing_results = subject.request_proofing_results(
-          applicant.unique_id,
-          applicant.enrollment_code,
-        )
-        expect(proofing_results['status']).to eq 'In-person passed'
-        expect(proofing_results['fraudSuspected']).to eq false
       end
 
-      it 'returns in-progress enrollment information' do
-        stub_request_in_progress_proofing_results
-
-        expect do
-          subject.request_proofing_results(
+      it 'refreshes the auth token before making the request' do
+        subject.token
+        expired_time = Time.zone.now + expires_in
+        proofing_results = nil
+        travel_to(expired_time) do
+          proofing_results = subject.request_proofing_results(
             applicant.unique_id,
             applicant.enrollment_code,
           )
-        end.to raise_error(
-          an_instance_of(Faraday::BadRequestError).
-          and(having_attributes(
-            response: include(
-              body: include(
-                'responseMessage' => 'Customer has not been to a post office to complete IPP',
-              ),
-            ),
-          )),
-        )
-      end
-    end
-    context 'when the token is expired' do
-      let(:cache) { double(ActiveSupport::Cache::MemoryStore) }
-      let(:redis) { double(Redis) }
-      before do
-        set_up_expired_token(cache, redis)
-        stub_request_proofing_results_with_forbidden_error
-        stub_request_passed_proofing_results
-      end
+        end
 
-      it 'fetches a new token and retries the attempt' do
-        expect(cache).to receive(:write).with(
-          UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY,
-          an_instance_of(String),
-          hash_including(expires_at: an_instance_of(ActiveSupport::TimeWithZone)),
-        ).twice
-
-        expect(redis).to receive(:expireat).with(
-          UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY,
-          an_instance_of(Integer),
-        ).twice
-
-        expect(cache).to receive(:read).with(UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY)
-
-        proofing_results = subject.request_proofing_results(
-          applicant.unique_id,
-          applicant.enrollment_code,
-        )
-
+        expect(WebMock).to have_requested(:post, "#{root_url}/oauth/authenticate").twice
         expect(proofing_results['status']).to eq 'In-person passed'
         expect(proofing_results['fraudSuspected']).to eq false
       end
@@ -445,52 +417,32 @@ RSpec.describe UspsInPersonProofing::Proofer do
         unique_id: '123456789',
       )
     end
-    context 'when the token is valid' do
-      before do
-        allow(Rails).to receive(:cache).and_return(
-          ActiveSupport::Cache::RedisCacheStore.new(url: IdentityConfig.store.redis_throttle_url),
-        )
-        stub_request_token
-      end
 
-      it 'returns enrollment information' do
-        stub_request_enrollment_code
-
-        enrollment = subject.request_enrollment_code(applicant)
-        expect(enrollment['enrollmentCode']).to be_present
-        expect(enrollment['responseMessage']).to be_present
-      end
+    before do
+      stub_request_token
     end
 
-    context 'when the token is expired' do
-      let(:cache) { double(ActiveSupport::Cache::MemoryStore) }
-      let(:redis) { double(Redis) }
-      before do
-        set_up_expired_token(cache, redis)
-      end
+    it 'returns enrollment information' do
+      stub_request_enrollment_code
 
-      it 'fetches a new token and retries the attempt' do
-        expect(IdentityConfig.store).to receive(:usps_ipp_sponsor_id).
-          and_return(usps_ipp_sponsor_id)
+      enrollment = subject.request_enrollment_code(applicant)
+      expect(enrollment['enrollmentCode']).to be_present
+      expect(enrollment['responseMessage']).to be_present
+    end
 
-        stub_request_enrollment_code_with_forbidden_error
+    context 'when the auth token is expired' do
+      let(:expires_in) { 15.minutes }
+
+      it 'refreshes the auth token before making the request' do
+        subject.token
+        expired_time = Time.zone.now + expires_in
         stub_request_enrollment_code
+        enrollment = nil
+        travel_to(expired_time) do
+          enrollment = subject.request_enrollment_code(applicant)
+        end
 
-        expect(cache).to receive(:write).with(
-          UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY,
-          an_instance_of(String),
-          hash_including(expires_at: an_instance_of(ActiveSupport::TimeWithZone)),
-        ).twice
-
-        expect(redis).to receive(:expireat).with(
-          UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY,
-          an_instance_of(Integer),
-        ).twice
-
-        expect(cache).to receive(:read).with(UspsInPersonProofing::Proofer::AUTH_TOKEN_CACHE_KEY)
-
-        enrollment = subject.request_enrollment_code(applicant)
-
+        expect(WebMock).to have_requested(:post, "#{root_url}/oauth/authenticate").twice
         expect(enrollment['enrollmentCode']).to be_present
         expect(enrollment['responseMessage']).to be_present
       end
