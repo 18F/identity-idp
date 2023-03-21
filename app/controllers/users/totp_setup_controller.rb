5aa97107259b07ad200d0685210d10d0d47cce58
Mitchell Henke <mitchell.henke@gsa.gov>
2023-03-21 09:12:55 -0500

    use 2fa reauthentication in controllers that now require reauthentication if configured

--- app/controllers/users/totp_setup_controller.rb
+++ app/controllers/users/totp_setup_controller.rb
@@ -4,12 +4,16 @@ module Users
     include MfaSetupConcern
     include RememberDeviceConcern
     include SecureHeadersConcern
+    include ReauthenticationRequiredConcern
 
     before_action :authenticate_user!
     before_action :confirm_user_authenticated_for_2fa_setup
     before_action :set_totp_setup_presenter
     before_action :apply_secure_headers_override
     before_action :cap_auth_app_count, only: %i[new confirm]
+    before_action :confirm_recently_authenticated_2fa, if: -> do
+      IdentityConfig.store.reauthentication_for_second_factor_management_enabled
+    end
 
     helper_method :in_multi_mfa_selection_flow?
 
