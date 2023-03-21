84a07cd4fe7f4d9332c5b4cfe1e5cd8d97cc48d2
Sonia Connolly <sonia.connolly@gsa.gov>
2023-03-21 11:50:32 -0700

    Add constant for 14 days to contact fraud review

--- app/controllers/idv/setup_errors_controller.rb
+++ app/controllers/idv/setup_errors_controller.rb
@@ -2,10 +2,12 @@ module Idv
   class SetupErrorsController < ApplicationController
     before_action :confirm_two_factor_authenticated
 
+    FRAUD_REVIEW_CONTACT_WITHIN_DAYS = 14.days
+
     def show
       analytics.idv_setup_errors_visited
 
-      @call_by_date = current_user.profiles.last.verified_at + 14.days
+      @call_by_date = current_user.profiles.last.verified_at + FRAUD_REVIEW_CONTACT_WITHIN_DAYS
     end
   end
 end
