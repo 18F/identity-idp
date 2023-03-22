b3a5ab2ff32c7335bde3d71bd0496a810fa252b2
Kimball Bighorse <kimball.bighorse@gsa.gov>
2023-03-22 12:37:18 -0700

    TAKE 2: Lg 8903 remove hybrid handoff steps (#8051)
    Remove code associated with the SendLink step since it has now been combined with the Upload step
    
    changelog: Internal, Updating Hybrid Handoff, finishing removal of feature-flagged combined hybrid handoff
    
    ---------
    
    Co-authored-by: Jonathan Hooper <jonathan.hooper@gsa.gov>
    Co-authored-by: eric-gade <eric.gade@gsa.gov>
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/services/idv/actions/cancel_link_sent_action.rb
+++ app/services/idv/actions/cancel_link_sent_action.rb
@@ -6,10 +6,7 @@ module Idv
       end
 
       def call
-        mark_step_incomplete(:send_link)
-        if IdentityConfig.store.doc_auth_combined_hybrid_handoff_enabled
-          mark_step_incomplete(:upload)
-        end
+        mark_step_incomplete(:upload)
       end
     end
   end
