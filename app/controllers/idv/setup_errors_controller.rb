8bb3b2372be10c9aec6debc4dce86a6f7c0d8107
Sonia Connolly <sonia.connolly@gsa.gov>
2023-03-22 13:23:24 -0700

    In GPO flow, verified_at is not set yet, so use today
    Co-authored-by: Eric Gade <eric.gade@gsa.gov>

--- app/controllers/idv/setup_errors_controller.rb
+++ app/controllers/idv/setup_errors_controller.rb
@@ -6,8 +6,8 @@ module Idv
 
     def show
       analytics.idv_setup_errors_visited
-
-      @call_by_date = current_user.profiles.last.verified_at + FRAUD_REVIEW_CONTACT_WITHIN_DAYS
+      verified_at = current_user.profiles.last.verified_at || Date.today
+      @call_by_date = verified_at + FRAUD_REVIEW_CONTACT_WITHIN_DAYS
     end
   end
 end
