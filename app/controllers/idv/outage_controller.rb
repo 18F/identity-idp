c2b7f733b6d934c99ce9ce4e8e35d41f4987450e
Eric Gade <105373963+eric-gade@users.noreply.github.com>
2023-03-22 10:21:44 -0400

    Update app/controllers/idv/outage_controller.rb
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/controllers/idv/outage_controller.rb
+++ app/controllers/idv/outage_controller.rb
@@ -7,10 +7,10 @@ module Idv
 
     def show
       session[:skip_vendor_outage] = true
-      render :show, locals: { current_sp: current_sp, exit_url: get_exit_url }
+      render :show, locals: { current_sp:, exit_url: }
     end
 
-    def get_exit_url
+    def exit_url
       current_sp.return_to_sp_url || account_path
     end
   end
