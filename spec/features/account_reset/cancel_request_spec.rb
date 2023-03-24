160e7738a3ce692112c26fa10568571a549a43e6
Matt Hinz <matt.hinz@gsa.gov>
2023-03-24 10:31:44 -0700

    Add test assertion around # of emails that should be sent
    (Chasing a flaky test in CI)

--- spec/features/account_reset/cancel_request_spec.rb
+++ spec/features/account_reset/cancel_request_spec.rb
@@ -19,8 +19,9 @@ describe 'Account Reset Request: Cancellation' do
       reset_email
       click_button t('account_reset.request.yes_continue')
 
-      travel_to(Time.zone.now + 2.days + 1) do
-        AccountReset::GrantRequestsAndSendEmails.new.perform(Time.zone.today)
+      travel_to(Time.zone.now + 2.days + 1.second) do
+        number_of_emails_sent = AccountReset::GrantRequestsAndSendEmails.new.perform(Time.zone.today)
+        expect(number_of_emails_sent).to eql(1)
         open_last_email
         click_email_link_matching(/cancel\?token/)
         click_button t('account_reset.cancel_request.cancel_button')
