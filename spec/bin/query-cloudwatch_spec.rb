df631e32e5f20fa95431297613f6a34e3b8654df
Zach Margolis <zachary.margolis@gsa.gov>
2023-03-24 15:24:17 -0700

    lints

--- spec/bin/query-cloudwatch_spec.rb
+++ spec/bin/query-cloudwatch_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe QueryCloudwatch do
     let(:stdin) { build_stdin_without_query }
     let(:argv) { [] }
     let(:stdout) { StringIO.new }
-    let(:required_parameters) { %W[--from 1d --group some/log --query #{"fields @message"}] }
-    subject(:parse!) { QueryCloudwatch.parse!(argv:, stdin:, stdout:)}
+    let(:required_parameters) { %w[--from 1d --group some/log --query fields\ @message] }
+    subject(:parse!) { QueryCloudwatch.parse!(argv:, stdin:, stdout:) }
 
     before do
       allow(QueryCloudwatch).to receive(:exit)
     end
+
     context 'with no arguments' do
-      
       it 'prints an error messages and exits uncleanly' do
         expect(QueryCloudwatch).to receive(:exit).with(1)
         parse!
-        expect(stdout.string).to include "ERROR: missing"
+        expect(stdout.string).to include 'ERROR: missing'
       end
     end
 
@@ -26,103 +26,103 @@ RSpec.describe QueryCloudwatch do
       it 'prints help and exits cleanly' do
         expect(QueryCloudwatch).to receive(:exit).with(no_args)
         parse!
-        expect(stdout.string).to include "Script to query cloudwatch"
+        expect(stdout.string).to include 'Script to query cloudwatch'
       end
     end
 
-    context "passing in a query" do
+    context 'passing in a query' do
       let(:required_nonquery_args) { %w[--from 1d --group some/log] }
-      context "passing in no query" do
+      context 'passing in no query' do
         let(:argv) { required_nonquery_args }
         let(:stdin) { build_stdin_without_query }
 
-        it "returns an error" do
+        it 'returns an error' do
           parse!
-          expect(stdout.string).to include "ERROR"
+          expect(stdout.string).to include 'ERROR'
         end
       end
-      
-      context "passing in a query in argv" do
+
+      context 'passing in a query in argv' do
         let(:stdin) { build_stdin_without_query }
         let(:argv) { required_parameters }
 
-        it "assigns query to query" do
+        it 'assigns query to query' do
           config = parse!
-          expect(config.query).to include "fields @message"
+          expect(config.query).to include 'fields @message'
         end
       end
 
-      context "passing in a query in stdin" do
-        let(:stdin) { build_stdin_with_query("fields @message") }
+      context 'passing in a query in stdin' do
+        let(:stdin) { build_stdin_with_query('fields @message') }
+        let(:argv) { required_nonquery_args }
 
-        it "assigns query to query"
+        it 'assigns query to query' do
           config = parse!
-          expect(config.query).to include "fields @message"
+          expect(config.query).to include 'fields @message'
         end
       end
     end
-  
-    context "with --no-progress" do
+
+    context 'with --no-progress' do
       let(:argv) { required_parameters + %w[--no-progress] }
 
-      it "assigns progress to false" do
+      it 'assigns progress to false' do
         config = parse!
         expect(config.progress).to be false
       end
     end
 
-    context "with --progress" do
+    context 'with --progress' do
       let(:argv) { required_parameters + %w[--progress] }
 
-      it "assigns progress to true" do
+      it 'assigns progress to true' do
         config = parse!
         expect(config.progress).to be true
       end
     end
-    
-    context "with --no-complete" do
+
+    context 'with --no-complete' do
       let(:argv) { required_parameters + %w[--no-complete] }
 
-      it "assigns complete to false" do
+      it 'assigns complete to false' do
         config = parse!
         expect(config.complete).to be false
       end
     end
 
-    context "with --complete" do
+    context 'with --complete' do
       let(:argv) { required_parameters + %w[--complete] }
 
-      it "assigns complete to true" do
+      it 'assigns complete to true' do
         config = parse!
         expect(config.complete).to be true
       end
     end
-    
 
-    context "with --csv" do
+    context 'with --csv' do
       let(:argv) { required_parameters + %w[--csv] }
 
-      it "assigns format to true" do
+      it 'assigns format to true' do
         config = parse!
         expect(config.format).to eq :csv
       end
     end
 
-    context "with --json" do
+    context 'with --json' do
       let(:argv) { required_parameters + %w[--json] }
 
-      it "assigns format to true" do
+      it 'assigns format to true' do
         config = parse!
         expect(config.format).to eq :json
       end
     end
-    
+
     def build_stdin_without_query
       StringIO.new.tap do |io|
         allow(io).to receive(:tty?).and_return(true)
       end
     end
-  
+
     def build_stdin_with_query(query)
       StringIO.new(query)
     end
@@ -130,19 +130,19 @@ RSpec.describe QueryCloudwatch do
 
   describe '#run' do
     let(:format) { :csv }
-    let(:config) do 
+    let(:config) do
       QueryCloudwatch::Config.new(
-        group: "foobar",
+        group: 'foobar',
         slice: 1.week,
         from: 2.days.ago,
         to: Time.zone.now,
         progress: false,
         wait_duration: 0,
-        query: "fields @timestamp, @message",
+        query: 'fields @timestamp, @message',
         format: format,
       )
     end
-    let(:query_cloudwatch) { QueryCloudwatch.new(config)}
+    let(:query_cloudwatch) { QueryCloudwatch.new(config) }
     let(:stdout) { StringIO.new }
     subject(:run) { query_cloudwatch.run(stdout:) }
 
@@ -171,7 +171,7 @@ RSpec.describe QueryCloudwatch do
       }
     end
 
-    it "outputs a csv format" do
+    it 'outputs a csv format' do
       run
       expect(stdout.string).to eq <<~STR
         timestamp-1,message-1
@@ -179,19 +179,16 @@ RSpec.describe QueryCloudwatch do
       STR
     end
 
-    context "with a json format" do
+    context 'with a json format' do
       let(:format) { :json }
 
-      it "outputs newline deliminated json" do
+      it 'outputs newline deliminated json' do
         run
         expect(stdout.string).to eq <<~STR
           {"@timestamp":"timestamp-1","@message":"message-1"}
           {"@timestamp":"timestamp-2","@message":"message-2"}
         STR
       end
-    
-      
     end
-    
   end
 end
