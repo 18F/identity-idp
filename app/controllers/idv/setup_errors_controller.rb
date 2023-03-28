3c2610517006ad841cb788f8e66a480a88274f3d
Alex Bradley <alexander.bradley@gsa.gov>
2023-03-28 13:21:00 -0400

    check for fraud in errors controllers

--- app/controllers/idv/setup_errors_controller.rb
+++ app/controllers/idv/setup_errors_controller.rb
@@ -1,6 +1,9 @@
 module Idv
   class SetupErrorsController < ApplicationController
+    include FraudReviewConcern
+
     before_action :confirm_two_factor_authenticated
+    before_action :handle_fraud
 
     def show
       analytics.idv_setup_errors_visited
