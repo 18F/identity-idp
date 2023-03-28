0dade67e40d3f16ce6b4bb16878d013e3f3e6411
Mitchell Henke <mitchell.henke@gsa.gov>
2023-03-28 12:56:46 -0500

    Validate SAML action path before rendering form
    changelog: Bug Fixes, SAML Authentication, Validate SAML action path before rendering form

--- app/controllers/saml_completion_controller.rb
+++ app/controllers/saml_completion_controller.rb
@@ -5,23 +5,35 @@
 class SamlCompletionController < ApplicationController
   # Pass the original service provider request to the main SamlIdpController#auth method
   # via a POST with form parameters replacing the url query parameters
-  def index
-    if sp_session.present?
-      request_url = URI(sp_session[:request_url])
-      path_year = request_url.path[-4..-1]
-      path_method = "api_saml_finalauthpost#{path_year}_url"
-      action_url = Rails.application.routes.url_helpers.send(path_method)
+  before_action :verify_sp_session_exists
 
+  def index
+    request_url = URI(sp_session[:request_url])
+    action_path = build_action_path(request_url.path)
+
+    if !action_path
+      render_not_found
+    else
       # Takes the query params which were set internally in the
       # sp_session (so they should always be valid).
       # A bad request that originated outside of the IDP would have
       # already responded with a 400 status before reaching this point.
       form_params = UriService.params(request_url)
 
-      render 'shared/saml_post_form', locals: { action_url: action_url, form_params: form_params },
+      render 'shared/saml_post_form', locals: { action_url: action_path, form_params: form_params },
                                       layout: false
-    else
-      render_not_found
     end
+  end
+
+  def verify_sp_session_exists
+    render_not_found unless sp_session.present? && sp_session[:request_url].present?
+  end
+
+  def build_action_path(path)
+    path_year = path[-4..-1]
+    path = "/api/saml/finalauthpost#{path_year}"
+    recognized_path = Rails.application.routes.recognize_path(path, method: :post)
+
+    path if recognized_path[:controller] == 'saml_idp' && recognized_path[:action] == 'auth'
   end
 end
