3a2cf493f9aedc520531cf88ee9800cdc226e16b
John Maxwell <john.maxwell@gsa.gov>
2023-03-28 11:20:15 -0400

    Push the actual TS code.
    [skip changelog]

---
+++ app/javascript/packs/puerto-rico-guidance.ts
@@ -0,0 +1,18 @@
+function showOrHidePuertoRicoGuidance(forStateCode) {
+  const puertoRicoGuidance = document.getElementById('puerto-rico-extra-text');
+  if (forStateCode === 'PR') {
+    puertoRicoGuidance?.classList?.remove('display-none');
+    puertoRicoGuidance?.classList?.add('display-block');
+  } else {
+    puertoRicoGuidance?.classList?.remove('display-block');
+    puertoRicoGuidance?.classList?.add('display-none');
+  }
+}
+
+function onStateSelectionChange() {
+  const stateSelector = <HTMLInputElement>document.getElementById('idv_form_state');
+  showOrHidePuertoRicoGuidance(stateSelector?.value);
+}
+
+document.getElementById('idv_form_state')?.addEventListener('change', onStateSelectionChange);
+onStateSelectionChange(); // set initial visibility
