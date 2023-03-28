89b25e23fff01ae15874bd87f15e6c40f46ad6b1
Alex Bradley <alexander.bradley@gsa.gov>
2023-03-28 14:04:19 -0400

    remove before actions

--- app/controllers/idv/not_verified_controller.rb
+++ app/controllers/idv/not_verified_controller.rb
@@ -1,9 +1,6 @@
 module Idv
   class NotVerifiedController < ApplicationController
-    include FraudReviewConcern
-
     before_action :confirm_two_factor_authenticated
-    before_action :handle_fraud
 
     def show
       analytics.idv_not_verified_visited
--- app/controllers/idv/setup_errors_controller.rb
+++ app/controllers/idv/setup_errors_controller.rb
@@ -1,9 +1,6 @@
 module Idv
   class SetupErrorsController < ApplicationController
-    include FraudReviewConcern
-
     before_action :confirm_two_factor_authenticated
-    before_action :handle_fraud
 
     def show
       analytics.idv_setup_errors_visited
