d57599197e4bf7c2b7285085b9932a0e3224e4b4
Andrew Duthie <andrew.duthie@gsa.gov>
2023-04-05 08:29:14 -0400

    Add specs for TabNavigationComponent

---
+++ spec/components/tab_navigation_component_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe TabNavigationComponent, type: :component do
+  let(:label) { 'Navigation' }
+  let(:routes) { [{ path: '/first', text: 'First' }, { path: '/second', text: 'Second' }] }
+  let(:tag_options) { { label:, routes: } }
+
+  subject(:rendered) do
+    render_inline TabNavigationComponent.new(**tag_options)
+  end
+
+  it 'renders labelled navigation' do
+    expect(rendered).to have_css('nav[aria-label="Navigation"]')
+    expect(rendered).to have_link('First') { |link| !is_current_link?(link) }
+    expect(rendered).to have_link('Second') { |link| !is_current_link?(link) }
+  end
+
+  context 'with tag options' do
+    let(:tag_options) { super().merge(data: { foo: 'bar' }) }
+
+    it 'renders with tag options forwarded to navigation' do
+      expect(rendered).to have_css('nav[data-foo="bar"]')
+    end
+  end
+
+  context 'with link for current request' do
+    before do
+      allow(request).to receive(:path).and_return('/first')
+    end
+
+    it 'renders current link as highlighted' do
+      expect(rendered).to have_link('First') { |link| is_current_link?(link) }
+      expect(rendered).to have_link('Second') { |link| !is_current_link?(link) }
+    end
+  end
+
+  def is_current_link?(link)
+    !link.matches_css?('.usa-button--outline')
+  end
+end
