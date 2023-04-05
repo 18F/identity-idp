12594e2a02d9cc43a66a0815082bf6c16bb225b8
Andrew Duthie <andrew.duthie@gsa.gov>
2023-04-05 09:48:15 -0400

    Remove request_id parameters from Sign In view links
    They're unnecessary, since the request_id will have already been saved to the session

--- app/controllers/sign_up/registrations_controller.rb
+++ app/controllers/sign_up/registrations_controller.rb
@@ -15,7 +15,7 @@ module SignUp
         attempts_tracker: irs_attempts_api_tracker,
       )
       analytics.user_registration_enter_email_visit
-      render :new, locals: { request_id: nil }, formats: :html
+      render :new, formats: :html
     end
 
     def create
@@ -24,7 +24,7 @@ module SignUp
         attempts_tracker: irs_attempts_api_tracker,
       )
 
-      result = @register_user_email_form.submit(permitted_params)
+      result = @register_user_email_form.submit(permitted_params.merge(request_id:))
 
       analytics.user_registration_email(**result.to_h)
       irs_attempts_api_tracker.user_registration_email_submitted(
@@ -36,7 +36,7 @@ module SignUp
       if result.success?
         process_successful_creation
       else
-        render :new, locals: { request_id: sp_request_id }
+        render :new
       end
     end
 
@@ -50,7 +50,7 @@ module SignUp
     end
 
     def permitted_params
-      params.require(:user).permit(:email, :email_language, :request_id, :terms_accepted)
+      params.require(:user).permit(:email, :email_language, :terms_accepted)
     end
 
     def process_successful_creation
@@ -60,15 +60,11 @@ module SignUp
       resend_confirmation = params[:user][:resend]
       session[:email] = @register_user_email_form.email
 
-      redirect_to sign_up_verify_email_url(
-        resend: resend_confirmation, request_id: permitted_params[:request_id],
-      )
+      redirect_to sign_up_verify_email_url(resend: resend_confirmation)
     end
 
-    def sp_request_id
-      request_id = permitted_params.fetch(:request_id, '')
-
-      ServiceProviderRequestProxy.from_uuid(request_id).uuid
+    def request_id
+      ServiceProviderRequestProxy.from_uuid(sp_session[:request_id]).uuid
     end
 
     def redirect_if_ial2_and_idv_unavailable
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,7 +25,6 @@ module Users
       )
       override_csp_for_google_analytics
 
-      @request_id = request_id_if_valid
       @ial = sp_session_ial
       @browser_is_ie11 = browser_is_ie11?
       super
@@ -121,7 +120,7 @@ module Users
     end
 
     def auth_params
-      params.require(:user).permit(:email, :password, :request_id)
+      params.require(:user).permit(:email, :password)
     end
 
     def process_locked_out_user
@@ -217,14 +216,6 @@ module Users
       AccountReset::FindPendingRequestForUser.new(
         current_user,
       ).call
-    end
-
-    LETTERS_AND_DASHES = /\A[a-z0-9-]+\Z/i
-
-    def request_id_if_valid
-      request_id = (params[:request_id] || sp_session[:request_id]).to_s
-
-      request_id if LETTERS_AND_DASHES.match?(request_id)
     end
 
     def override_csp_for_google_analytics
