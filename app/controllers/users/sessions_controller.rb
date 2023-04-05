77543dd3fe779d9a3464fc646c3290604c758b91
Andrew Duthie <andrew.duthie@gsa.gov>
2023-04-05 15:59:25 -0400

    WIP remove more request_id

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,7 +22,6 @@ module Users
     def new
       override_csp_for_google_analytics
 
-      @request_id = request_id_if_valid
       @ial = sp_session_ial
       @browser_is_ie11 = browser_is_ie11?
       @sign_in_a_b_test_bucket = sign_in_a_b_test_bucket
@@ -124,7 +123,7 @@ module Users
     end
 
     def auth_params
-      params.require(:user).permit(:email, :password, :request_id)
+      params.require(:user).permit(:email, :password)
     end
 
     def process_locked_out_user
@@ -220,14 +219,6 @@ module Users
       AccountReset::FindPendingRequestForUser.new(
         current_user,
       ).call
-    end
-
-    LETTERS_AND_DASHES = /\A[a-z0-9-]+\Z/i
-
-    def request_id_if_valid
-      request_id = (params[:request_id] || sp_session[:request_id]).to_s
-
-      request_id if LETTERS_AND_DASHES.match?(request_id)
     end
 
     def override_csp_for_google_analytics
