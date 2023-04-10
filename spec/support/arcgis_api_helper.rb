30881a61879979009a6b4490c10b4bd7b76c8653
Micah <micah_neveu@hotmail.com>
2023-04-10 18:29:43 -0400

    LG-9103: capture error when ArcGIS service is not returning a token (#8087)
    * LG-9301 capture error and alert when ArcGIS service is not returning a token
    
    If ArcGIS is unable to return a token it was causing an exception in retrieve_token
    and throwing HTTP 500 responses. The UI had no indication of error and the
    responses were causing the on-call engineer to be notified.
    This change catches the missing token, logs the issue, and returns a 400 error which
    allows a retry.
    
    changelog: Internal, PO-Search, Return 400 instead of 500 and logs ArcGIS token issues
    
    * LG-9103 (erroneously noted 9301 previously)
    
    Remove existing token from cache if service is unavailable or creditials are invalid.
    
    changelog: Internal, In-person-proofing address lookup, Prevent http 500 errors from occurring with invalid token
    
    * LG-9103 Capture error and alert when ArcGIS is not returning a token
    Add two tests, one for no service and one for invalid credentials (no token returned)

--- spec/support/arcgis_api_helper.rb
+++ spec/support/arcgis_api_helper.rb
@@ -53,15 +53,16 @@ module ArcgisApiHelper
     )
   end
 
-  def stub_invalid_token_response
-    stub_request(:get, %r{/suggest}).to_return(
-      status: 200, body: {
-        error: {
-          code: 498,
-          message: 'Invalid Token',
-          details: [],
-        },
-      }.to_json,
+  def stub_invalid_token_credentials_response
+    stub_request(:post, %r{/generateToken}).to_return(
+      status: 200, body: ArcgisApi::Mock::Fixtures.invalid_gis_token_credentials_response,
+      headers: { content_type: 'application/json;charset=UTF-8' }
+    )
+  end
+
+  def stub_token_service_unreachable_response
+    stub_request(:post, %r{/generateToken}).to_return(
+      status: 200, body: ArcgisApi::Mock::Fixtures.request_token_service_error,
       headers: { content_type: 'application/json;charset=UTF-8' }
     )
   end
