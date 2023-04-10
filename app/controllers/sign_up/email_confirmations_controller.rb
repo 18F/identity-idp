67ffdb4b7bc4f88a6277c5f9741434f3088cdc67
Andrew Duthie <andrew.duthie@gsa.gov>
2023-04-10 08:45:24 -0400

    Maintain request ID in password reset (#8145)
    * Maintain request ID in password reset
    
    changelog: Bug Fixes, Password Reset, Maintain partner request when user resets password in new session
    
    * Use branded experience assertion consistently
    
    These assertions are prone to false positives because the footer GSA logo includes the sp-logos path
    
    * Use perform_in_browser helper to initiate password reset
    
    * Clarify branded experience assertion
    
    * Move request_id handling from password controller
    
    So that it isn't persisted only in the case of a successful password submission, because otherwise the associated SP is dropped
    
    * Simplify parameter access
    
    * Add specs for SP metadata store

--- app/controllers/sign_up/email_confirmations_controller.rb
+++ app/controllers/sign_up/email_confirmations_controller.rb
@@ -5,6 +5,7 @@ module SignUp
     before_action :find_user_with_confirmation_token
     before_action :confirm_user_needs_sign_up_confirmation
     before_action :stop_if_invalid_token
+    before_action :store_sp_metadata_in_session, only: [:create]
 
     def create
       clear_setup_piv_cac_from_sign_in
@@ -26,10 +27,15 @@ module SignUp
         success: true,
         failure_reason: nil,
       )
-      request_id = params.fetch(:_request_id, '')
-      redirect_to sign_up_enter_password_url(
-        request_id: request_id, confirmation_token: @confirmation_token,
-      )
+      redirect_to sign_up_enter_password_url(confirmation_token: @confirmation_token)
+    end
+
+    def store_sp_metadata_in_session
+      StoreSpMetadataInSession.new(session:, request_id:).call if request_id.present?
+    end
+
+    def request_id
+      params[:_request_id]
     end
   end
 end
