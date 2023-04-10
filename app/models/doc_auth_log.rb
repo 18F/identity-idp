6ec3d8b0b82e1a530124f0a8f19c550a843ec374
Sonia Connolly <sonia.connolly@gsa.gov>
2023-04-10 09:31:24 -0700

    Put ignored columns in single array

--- app/models/doc_auth_log.rb
+++ app/models/doc_auth_log.rb
@@ -7,6 +7,10 @@ class DocAuthLog < ApplicationRecord
              primary_key: 'issuer'
   # rubocop:enable Rails/InverseOf
 
-  self.ignored_columns = [:email_sent_view_at, :email_sent_view_count]
-  self.ignored_columns = [:send_link_view_at, :send_link_view_count]
+  self.ignored_columns = [
+    :email_sent_view_at,
+    :email_sent_view_count,
+    :send_link_view_at,
+    :send_link_view_count,
+  ]
 end
