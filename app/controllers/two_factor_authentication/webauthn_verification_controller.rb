70ff038c06058197c8968a79fb23a3ec25f88624
Mitchell Henke <mitchell.henke@gsa.gov>
2023-04-11 14:40:54 -0500

    Remove implicit two_factor_authentication_method in favor of explicitly passing the authentication method name (#8178)
    * Remove implicit two_factor_authentication_method in favor of explicitly passing the authentication method name
    
    changelog: Internal, Authentication, Remove implicit two_factor_authentication_method in favor of explicitly passing the authentication method name
    
    * use keyword argument in handle_valid_otp_for_context

--- app/controllers/two_factor_authentication/webauthn_verification_controller.rb
+++ app/controllers/two_factor_authentication/webauthn_verification_controller.rb
@@ -3,7 +3,7 @@ module TwoFactorAuthentication
   class WebauthnVerificationController < ApplicationController
     include TwoFactorAuthenticatable
 
-    before_action :check_sp_required_mfa_bypass
+    before_action :check_sp_required_mfa
     before_action :confirm_webauthn_enabled, only: :show
 
     def show
@@ -131,6 +131,10 @@ module TwoFactorAuthentication
         credential_id: params[:credential_id],
         webauthn_error: params[:webauthn_error],
       )
+    end
+
+    def check_sp_required_mfa
+      check_sp_required_mfa_bypass(auth_method: 'webauthn')
     end
   end
 end
