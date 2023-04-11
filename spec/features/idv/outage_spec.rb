2cefabb3cb2e463be4606784548a8cc1f5b8ab4f
Matt Hinz <matt.hinz@gsa.gov>
2023-04-11 14:07:24 -0700

    Unskip my spec

--- spec/features/idv/outage_spec.rb
+++ spec/features/idv/outage_spec.rb
@@ -12,8 +12,7 @@ def sign_in_with_idv_required(user:, sms_or_totp: :sms)
   click_submit_default
 end
 
-# This feature is currently _very_ flaky in CI.
-feature.skip 'IdV Outage Spec' do
+feature 'IdV Outage Spec' do
   include PersonalKeyHelper
   include IdvStepHelper
 
