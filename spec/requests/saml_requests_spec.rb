bf22eca577ced939c0469f37429e7581b8ed7971
Mitchell Henke <mitchell.henke@gsa.gov>
2023-04-11 15:50:03 -0500

    add failing spec

--- spec/requests/saml_requests_spec.rb
+++ spec/requests/saml_requests_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe 'SAML requests', type: :request do
   describe 'POST /api/saml/auth' do
     let(:cookie_regex) { /\A(?<cookie>\w+)=/ }
 
+    it 'renders a form for the SAML year that was requested' do
+      path_year = saml_settings.idp_sso_target_url[-4..-1]
+
+      post saml_settings.idp_sso_target_url
+      page = Capybara.string(response.body)
+      form_action = page.find_css("form[action$=\"#{path_year}\"]").first[:action]
+      expect(form_action).to be_present
+    end
+
     it 'does not set a session cookie' do
       post saml_settings.idp_sso_target_url
       new_cookies = response.header['Set-Cookie'].split("\n").map do |c|
