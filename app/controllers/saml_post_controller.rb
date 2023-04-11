77ffa1c17fd9c92267985bd5ddd2b1d4c3ea8b36
Mitchell Henke <mitchell.henke@gsa.gov>
2023-04-11 14:30:07 -0500

    Revert "Add safer parsing of dynamic SAML urls (LG-8837) (#8166)"
    This reverts commit 111dfd9e6592373488cfe56dabc9f5ccbd2a5ec6.

--- app/controllers/saml_post_controller.rb
+++ app/controllers/saml_post_controller.rb
@@ -3,27 +3,13 @@ class SamlPostController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def auth
-    action_url = build_action_url(request.path)
-    if !action_url
-      render_not_found
-      return
-    end
+    path_year = request.path[-4..-1]
+    path_method = "api_saml_authpost#{path_year}_url"
+    action_url = Rails.application.routes.url_helpers.send(path_method)
 
     form_params = params.permit(:SAMLRequest, :RelayState, :SigAlg, :Signature)
 
     render 'shared/saml_post_form', locals: { action_url: action_url, form_params: form_params },
                                     layout: false
-  end
-
-  private
-
-  def build_action_url(path)
-    path_year = path[-4..-1]
-    path = "/api/saml/authpost#{path_year}"
-    recognized_path = Rails.application.routes.recognize_path(path, method: :post)
-
-    if recognized_path[:controller] == 'saml_idp' && recognized_path[:action] == 'auth'
-      Rails.application.routes.url_for(**recognized_path)
-    end
   end
 end
