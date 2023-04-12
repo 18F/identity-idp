f59995df18f39239cc3dda4502fa9d80438d5c00
Zach Margolis <zachary.margolis@gsa.gov>
2023-04-12 11:12:59 -0700

    rename variable for smaller diff

--- app/controllers/saml_post_controller.rb
+++ app/controllers/saml_post_controller.rb
@@ -3,15 +3,15 @@ class SamlPostController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def auth
-    action_path = api_saml_authpost_path(path_year: params[:path_year])
-    if !valid_path?(action_path)
+    action_url = api_saml_authpost_path(path_year: params[:path_year])
+    if !valid_path?(action_url)
       render_not_found
       return
     end
 
     form_params = params.permit(:SAMLRequest, :RelayState, :SigAlg, :Signature)
 
-    render 'shared/saml_post_form', locals: { action_url: action_path, form_params: form_params },
+    render 'shared/saml_post_form', locals: { action_url: action_url, form_params: form_params },
                                     layout: false
   end
 
