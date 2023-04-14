d69c9dca37784b414b11583e6586cf8007f2136c
Sonia Connolly <sonia.connolly@gsa.gov>
2023-04-14 11:03:48 -0700

    Remove unneeded context wrapping link sent test

--- spec/features/idv/doc_auth/link_sent_step_spec.rb
+++ spec/features/idv/doc_auth/link_sent_step_spec.rb
@@ -5,24 +5,22 @@ feature 'doc auth link sent step' do
   include DocAuthHelper
   include DocCaptureHelper
 
-  context 'with combined upload step enabled' do
-    let(:user) { sign_in_and_2fa_user }
-    let(:doc_capture_polling_enabled) { false }
-    let(:phone_number) { '415-555-0199' }
+  let(:user) { sign_in_and_2fa_user }
+  let(:doc_capture_polling_enabled) { false }
+  let(:phone_number) { '415-555-0199' }
 
-    before do
-      allow(FeatureManagement).
-        to(receive(:doc_capture_polling_enabled?).and_return(doc_capture_polling_enabled))
-      user
-      complete_doc_auth_steps_before_upload_step
-      fill_in :doc_auth_phone, with: ''
-      fill_in :doc_auth_phone, with: phone_number
-      click_send_link
-    end
+  before do
+    allow(FeatureManagement).
+      to(receive(:doc_capture_polling_enabled?).and_return(doc_capture_polling_enabled))
+    user
+    complete_doc_auth_steps_before_upload_step
+    fill_in :doc_auth_phone, with: ''
+    fill_in :doc_auth_phone, with: phone_number
+    click_send_link
+  end
 
-    it 'Correctly renders the link sent step page' do
-      expect(page).to have_current_path(idv_doc_auth_link_sent_step)
-      expect(page).to have_content(phone_number)
-    end
+  it 'Correctly renders the link sent step page' do
+    expect(page).to have_current_path(idv_doc_auth_link_sent_step)
+    expect(page).to have_content(phone_number)
   end
 end
