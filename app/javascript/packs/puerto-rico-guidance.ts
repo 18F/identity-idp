dc8e1ab94c3658a71e18288d0f2dd56539a7c262
John Maxwell <john.maxwell@gsa.gov>
2023-04-17 15:14:07 -0400

    Review comment

--- app/javascript/packs/puerto-rico-guidance.ts
+++ app/javascript/packs/puerto-rico-guidance.ts
@@ -1,10 +1,9 @@
 function showOrHidePuertoRicoExtras(forStateCode) {
   const isPuertoRico = forStateCode === 'PR';
 
-  elements = document.getElementsByClassName('puerto-rico-extras');
-  for (var i = 0; i < elements.length; ++i) {
-    elements.item(i)?.classList?.toggle('display-none', !isPuertoRico);
-  }
+  document.querySelectorAll('.puerto-rico-extras').forEach((element) => {
+    element.classList.toggle('display-none', !isPuertoRico);
+  });
 }
 
 function onStateSelectionChange() {
