665b9f7875b64c346b244a65f8d30f1d9e5f4675
Kimball Bighorse <kbighorse@yahoo.com>
2023-04-17 16:46:21 -0700

    Rename new column name

--- db/primary_migrate/20230417232211_add_new_fraud_timestamps_on_profiles.rb
+++ db/primary_migrate/20230417232211_add_new_fraud_timestamps_on_profiles.rb
@@ -3,9 +3,9 @@ class AddNewFraudTimestampsOnProfiles < ActiveRecord::Migration[7.0]
   
   def change
     add_column :profiles, :fraud_rejected_at, :datetime
-    add_column :profiles, :fraud_reviewed_at, :datetime
+    add_column :profiles, :fraud_reviewing_at, :datetime
 
     add_index :profiles, :fraud_rejected_at, algorithm: :concurrently
-    add_index :profiles, :fraud_reviewed_at, algorithm: :concurrently
+    add_index :profiles, :fraud_reviewing_at, algorithm: :concurrently
   end
 end
