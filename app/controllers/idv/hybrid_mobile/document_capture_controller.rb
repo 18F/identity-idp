260c4bca7591823a2c2c09ace0adaed86bb638aa
Matt Hinz <matt.hinz@gsa.gov>
2023-04-17 11:13:35 -0700

    LG-9488: Entry controller for hybrid mobile document capture flow (#8209)
    * Add placeholder CaptureDocController
    
    * Add controller to handle entry into hybrid flow
    
    Controller looks at `document-capture-session` and does a "light login" to enable the user to start document capture.
    
    changelog: Internal, Flow state machine removal, Add controller for entry into hybrid mobile document capture
    
    * Refactor feature flag check out to HybridMobileConcern
    
    * Add tests around the 'Doc Auth' event
    
    * Send links to new hybrid mobile experience
    
    Going with /verify/documents?document-capture-session=xxxxx as a temporary url for now
    
    * CaptureDocController -> DocumentCaptureController
    
    * Tweak doc capture entry url naming & add comment
    
    * idv_hybrid_mobile_document_capture_entry_url -> idv_hybrid_mobile_entry_url
    
    * Refactor EntryController not to use before_action
    
    * Move HybridMobileConcern into Idv::HybridMobile namespace
    
    * WHOOPS
    
    need to see why this wasn't breaking any tests
    
    * Remove extra redirect
    
    Rails doesn't preserve the querystring when redirecting routes, and this is just an unnecessary extra step anyway
    
    * Add feature spec for mobile hybrid flow entry
    
    * Tweaks to capture complete step spec
    
    Ultimately we'll merge this with a doc capture step, presumably

---
+++ app/controllers/idv/hybrid_mobile/document_capture_controller.rb
@@ -0,0 +1,11 @@
+module Idv
+  module HybridMobile
+    class DocumentCaptureController < ApplicationController
+      include HybridMobileConcern
+
+      def show
+        # TODO
+      end
+    end
+  end
+end
