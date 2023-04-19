3076365fe634d9a2449ebe7d1573d95484a3f03d
Matt Hinz <matt.hinz@gsa.gov>
2023-04-19 09:45:12 -0700

    Use have_logged_event in DocumentCaptureController spec

--- spec/controllers/idv/document_capture_controller_spec.rb
+++ spec/controllers/idv/document_capture_controller_spec.rb
@@ -27,7 +27,6 @@ describe Idv::DocumentCaptureController do
     stub_sign_in(user)
     stub_analytics
     stub_attempts_tracker
-    allow(@analytics).to receive(:track_event)
   end
 
   describe 'before_actions' do
@@ -66,7 +65,7 @@ describe Idv::DocumentCaptureController do
     it 'sends analytics_visited event' do
       get :show
 
-      expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
+      expect(@analytics).to have_logged_event(analytics_name, analytics_args)
     end
 
     it 'updates DocAuthLog document_capture_view_count' do
