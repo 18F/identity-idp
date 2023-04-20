51f29c9aff4ebc268302391b1beffb1d50e06f24
Matt Hinz <matt.hinz@gsa.gov>
2023-04-20 10:01:13 -0700

    Refactor some helper methods to use `defined?` rather than `||=`

--- app/controllers/concerns/idv/hybrid_mobile/hybrid_mobile_concern.rb
+++ app/controllers/concerns/idv/hybrid_mobile/hybrid_mobile_concern.rb
@@ -52,21 +52,18 @@ module Idv
       end
 
       def document_capture_session
-        @document_capture_session ||=
+        return @document_capture_session if defined?(@document_capture_session)
+        @document_capture_session =
           DocumentCaptureSession.find_by(uuid: document_capture_session_uuid)
       end
 
       def document_capture_session_uuid
         session[:document_capture_session_uuid]
-        # TODO: Do we need to fall back to searching in flow_session?
       end
 
       def document_capture_user
-        @document_capture_user ||= begin
-          User.find(session[:doc_capture_user_id])
-        rescue ActiveRecord::RecordNotFound
-          nil
-        end
+        return @document_capture_user if defined?(@document_capture_user)
+        @document_capture_user = User.find_by(id: session[:doc_capture_user_id])
       end
 
       def flow_session
