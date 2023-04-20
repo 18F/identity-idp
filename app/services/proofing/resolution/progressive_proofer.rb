18f4a577b7f5f43469402bfce962cfd1c0e77a20
Eileen McFarland <eileenmcfarland@navapbc.com>
2023-04-20 12:20:22 -0400

    fix lint

--- app/services/proofing/resolution/progressive_proofer.rb
+++ app/services/proofing/resolution/progressive_proofer.rb
@@ -6,9 +6,11 @@ module Proofing
     #   2. The user has only provided one address for their residential and identity document
     #      address or separate residential and identity document addresses
     # @param [Hash] applicant_pii, keys are symbols and values are strings, confidential user info
-    # @param [Boolean] double_address_verification, flag that indicates if user will have both state id address and current residential address verified
+    # @param [Boolean] double_address_verification, flag that indicates if user will have
+    # both state id address and current residential address verified
     # @param [String] request_ip, IP address for request
-    # @param [Boolean] should_proof_state_id, based on state id jurisdiction, indicates if there should be a state id proofing request made to aamva
+    # @param [Boolean] should_proof_state_id, based on state id jurisdiction, indicates if
+    # there should be a state id proofing request made to aamva
     # param [String] threatmetrix_session_id, identifies the threatmetrix session
     # param [JobHelpers::Timer] timer, indicates time elapsed to obtain results
     # param [String] user_email, email address for applicant
