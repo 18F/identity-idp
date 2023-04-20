f747ae0ebe530931fd6befa5d2929f651e27f1d7
John Maxwell <john.maxwell@gsa.gov>
2023-04-20 11:56:23 -0400

    Update app/controllers/idv/please_call_controller.rb
    Co-authored-by: Alex Bradley <alexander.bradley@gsa.gov>

--- app/controllers/idv/please_call_controller.rb
+++ app/controllers/idv/please_call_controller.rb
@@ -6,7 +6,7 @@ module Idv
 
     def show
       analytics.idv_please_call_visited
-      rejected_at = current_user.profiles.last.fraud_rejection_at || Time.zone.today
+      pending_at = current_user.profiles.last.fraud_review_pending_at || Time.zone.today
       @call_by_date = rejected_at + FRAUD_REVIEW_CONTACT_WITHIN_DAYS
     end
   end
