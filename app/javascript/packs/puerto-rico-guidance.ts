ac7882854388720d6ac422fc819d7bc12f98a82b
svalexander <shannon.alexander-navarro@gsa.gov>
2023-04-24 20:11:50 -0400

    show hint when user chooses pr on forms

--- app/javascript/packs/puerto-rico-guidance.ts
+++ app/javascript/packs/puerto-rico-guidance.ts
@@ -13,5 +13,46 @@ function onStateSelectionChange() {
   showOrHidePuertoRicoExtras(stateSelector?.value);
 }
 
+function onIdentityDocStateSelectionChange() {
+  const identityDocStateSelector: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById('state_id_identity_doc_address_state')
+  );
+  showOrHidePuertoRicoExtras(identityDocStateSelector?.value);
+}
+
+function onAddressStateSelectionChange() {
+  const addressStateSelector: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById('in_person_address_state')
+  );
+  showOrHidePuertoRicoExtras(addressStateSelector?.value);
+}
+
+function onUpdateStateId() {
+  const updateIdentityDocStateSelector: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById('state_id_identity_doc_address_state')
+  );
+  showOrHidePuertoRicoExtras(updateIdentityDocStateSelector?.value);
+}
+
+function onUpdateAddress() {
+  const updateAddressStateSelector: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById('in_person_address_state')
+  );
+  showOrHidePuertoRicoExtras(updateAddressStateSelector?.value);
+}
+
 document.getElementById('idv_form_state')?.addEventListener('change', onStateSelectionChange);
-onStateSelectionChange(); // set initial visibility
+document
+  .getElementById('state_id_identity_doc_address_state')
+  ?.addEventListener('change', onIdentityDocStateSelectionChange);
+document
+  .getElementById('in_person_address_state')
+  ?.addEventListener('change', onAddressStateSelectionChange);
+
+// set initial visibility
+onStateSelectionChange();
+onIdentityDocStateSelectionChange();
+onAddressStateSelectionChange();
+
+onUpdateStateId();
+onUpdateAddress();
