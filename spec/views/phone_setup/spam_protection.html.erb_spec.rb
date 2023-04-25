900ce72b12f724ee1f8d2b5d75d3872aa78c360d
Andrew Duthie <andrew.duthie@gsa.gov>
2023-04-25 16:15:42 -0400

    Configure site key for spam protection view spec

--- spec/views/phone_setup/spam_protection.html.erb_spec.rb
+++ spec/views/phone_setup/spam_protection.html.erb_spec.rb
@@ -33,13 +33,6 @@ describe 'users/phone_setup/spam_protection.html.erb' do
     expect(rendered).not_to have_link(t('two_factor_authentication.login_options_link_text'))
   end
 
-  it 'renders recaptcha script' do
-    expect(rendered).to have_css(
-      'script[src="https://www.google.com/recaptcha/api.js"]',
-      visible: :all,
-    )
-  end
-
   context 'with two factor options path' do
     let(:two_factor_options_path) { root_path }
     let(:locals) { { two_factor_options_path: } }
@@ -52,16 +45,29 @@ describe 'users/phone_setup/spam_protection.html.erb' do
     end
   end
 
-  context 'with recaptcha enterprise' do
+  context 'with configured recaptcha site key' do
     before do
-      allow(FeatureManagement).to receive(:recaptcha_enterprise?).and_return(true)
+      allow(IdentityConfig.store).to receive(:recaptcha_site_key_v2).and_return('key')
     end
 
     it 'renders recaptcha script' do
       expect(rendered).to have_css(
-        'script[src="https://www.google.com/recaptcha/enterprise.js"]',
+        'script[src="https://www.google.com/recaptcha/api.js"]',
         visible: :all,
       )
+    end
+
+    context 'with recaptcha enterprise' do
+      before do
+        allow(FeatureManagement).to receive(:recaptcha_enterprise?).and_return(true)
+      end
+
+      it 'renders recaptcha script' do
+        expect(rendered).to have_css(
+          'script[src="https://www.google.com/recaptcha/enterprise.js"]',
+          visible: :all,
+        )
+      end
     end
   end
 end
