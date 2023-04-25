0df6ad72c9993e229bb9189ce444b8118a054415
Mitchell Henke <mitchell.henke@gsa.gov>
2023-04-25 12:43:57 -0500

    update gem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,10 +7,9 @@ APPLICATION_SESSION_COOKIE_KEY = '_identity_idp_session'.freeze
 Rails.application.config.session_store(
   :redis_session_store,
   key: APPLICATION_SESSION_COOKIE_KEY,
+  # cookie expires with browser close
+  expire_after: nil,
   redis: {
-    # cookie expires with browser close
-    expire_after: nil,
-
     read_public_id: IdentityConfig.store.redis_session_read_public_id,
     write_public_id: IdentityConfig.store.redis_session_write_public_id,
     read_private_id: IdentityConfig.store.redis_session_read_private_id,
