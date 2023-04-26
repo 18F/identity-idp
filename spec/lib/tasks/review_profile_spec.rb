868bf03a2c8f65d87958183687c804a406c3ea42
Alex Bradley <alexander.bradley@gsa.gov>
2023-04-26 13:02:06 -0400

    LG-9399 review_profile uses fraud review timestamps (#8276)
    * use timestamps for fraud in user.rb
    
    * update tests for review_profile rake task
    
    * add changelog
    
    changelog: Internal, IdV Fraud, Change user methods to use fraud_review
    timestamp
    
    * use fraud_review_pending trait in tests around fraud review
    
    * remove reududant fraud check in user.rb
    
    * check for nil in fraud_review_eligible

--- spec/lib/tasks/review_profile_spec.rb
+++ spec/lib/tasks/review_profile_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'rake'
 
 describe 'review_profile' do
-  let(:user) { create(:user, :deactivated_threatmetrix_profile) }
+  let(:user) { create(:user, :deactivated_fraud_profile) }
   let(:uuid) { user.uuid }
   let(:task_name) { nil }
 
@@ -96,7 +96,7 @@ describe 'review_profile' do
       end
     end
 
-    context 'when the user profile has a nil verified_at' do
+    context 'when the user profile has a nil fraud_review_pending_at' do
       let(:user) do
         create(
           :user,
