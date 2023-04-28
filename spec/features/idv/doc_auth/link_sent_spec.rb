57b7b9c655661058f4a13e254dda0b3c65cf05a3
Sonia Connolly <sonia.connolly@gsa.gov>
2023-04-28 09:03:29 -0700

    Fix feature spec, add polling spec

--- spec/features/idv/doc_auth/link_sent_spec.rb
+++ spec/features/idv/doc_auth/link_sent_spec.rb
@@ -13,15 +13,28 @@ feature 'doc auth link sent step' do
     before do
       allow(FeatureManagement).
         to(receive(:doc_capture_polling_enabled?).and_return(doc_capture_polling_enabled))
+      allow(IdentityConfig.store).
+        to(receive(:doc_auth_link_sent_controller_enabled).and_return(true))
+
       user
       complete_doc_auth_steps_before_upload_step
       clear_and_fill_in(:doc_auth_phone, phone_number)
       click_send_link
+      visit(idv_link_sent_url)
     end
 
     it 'Correctly renders the link sent step page' do
-      expect(page).to have_current_path(idv_doc_auth_link_sent_step)
+      expect(page).to have_current_path(idv_link_sent_step)
+      expect_step_indicator_current_step(t('step_indicator.flows.idv.verify_id'))
       expect(page).to have_content(phone_number)
+    end
+
+    context 'when link sent polling is enabled' do
+      let(:doc_capture_polling_enabled) { true }
+
+      it 'Does not show continue button' do
+        expect(page).not_to have_content('Continue') #doc_auth.buttons.continue
+      end
     end
   end
 end
