027faef3167dea1c95aca6ae1b09d224b02d04c3
davida marion <davida.marion@gsa.gov>
2023-05-01 11:56:13 -0400

    Add command_line args and move parse_duration method

--- lib/reporting/command_line_options.rb
+++ lib/reporting/command_line_options.rb
@@ -1,5 +1,8 @@
+require 'reporting/cloudwatch_query_time_slice'
+
 module Reporting
   class CommandLineOptions
+    include Reporting::CloudwatchQueryTimeSlice
     # rubocop:disable Rails/Exit
     # @return [Hash]
     def parse!(argv, out: STDOUT)
@@ -8,6 +11,8 @@ module Reporting
       verbose = false
       progress = true
       period = nil
+      slice = 3.hours
+      threads = 5
 
       program_name = Pathname.new($PROGRAM_NAME).relative_path_from(__dir__)
 
@@ -27,10 +32,18 @@ module Reporting
         end
 
         opts.on('--week=DATE', <<~STR.squish) do |date_v|
-          run the report for the week (Sun-Sat) that includes the date in YYYY-MM-DD format'
+          run the report for the week (Sun-Sat) that includes the date in YYYY-MM-DD format
         STR
           date = Date.parse(date_v)
           period = :week
+        end
+
+        opts.on('--month=DATE', <<~STR.squish) do |date_v|
+          run the report for the month that includes the date in YYYY-MM-DD format. recommended
+          to include `--threads 10` and `--slice 1h` to reduce likelihood of timeout
+        STR
+          date = Date.parse(date_v)
+          period = :month
         end
 
         opts.on('--issuer=ISSUER') do |issuer_v|
@@ -43,6 +56,14 @@ module Reporting
 
         opts.on('--[no-]progress', 'shows a progress bar or hides it, defaults on') do |progress_v|
           progress = progress_v
+        end
+
+        opts.on('--slice SLICE', '(optional) query slice size duration, defaults to 1w') do |slice_v|
+          slice = CloudwatchQueryTimeSlice.parse_duration(slice_v)
+        end
+
+        opts.on('--threads THREADS', '(optional) number of threads, defaults to 5') do |threads_v|
+          threads = threads_v.to_i if threads_v.to_i.between?(1,30)
         end
 
         opts.on('-h', '--help') do
@@ -63,6 +84,8 @@ module Reporting
           issuer: issuer,
           verbose: verbose,
           progress: progress,
+          slice: slice,
+          threads: threads
         }
       end
     end
@@ -81,6 +104,8 @@ module Reporting
         utc.all_day
       when :week
         utc.all_week
+      when :month
+        utc.all_month
       else
         raise "unknown period=#{period}"
       end
