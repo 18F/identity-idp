2e71b27b90f0e285db3641f2a5e768e724799fb3
Sonia Connolly <sonia.connolly@gsa.gov>
2023-05-01 15:44:54 -0700

    Remove unneeded combined hybrid handoff context

--- spec/features/idv/doc_auth/link_sent_spec.rb
+++ spec/features/idv/doc_auth/link_sent_spec.rb
@@ -5,38 +5,36 @@ feature 'doc auth link sent step' do
   include DocAuthHelper
   include DocCaptureHelper
 
-  context 'with combined upload step enabled', js: true do
-    let(:user) { sign_in_and_2fa_user }
-    let(:doc_capture_polling_enabled) { false }
-    let(:phone_number) { '415-555-0199' }
+  let(:user) { sign_in_and_2fa_user }
+  let(:doc_capture_polling_enabled) { false }
+  let(:phone_number) { '415-555-0199' }
 
-    before do
-      allow(FeatureManagement).
-        to(receive(:doc_capture_polling_enabled?).and_return(doc_capture_polling_enabled))
-      allow(IdentityConfig.store).
-        to(receive(:doc_auth_link_sent_controller_enabled).and_return(true))
+  before do
+    allow(FeatureManagement).
+      to(receive(:doc_capture_polling_enabled?).and_return(doc_capture_polling_enabled))
+    allow(IdentityConfig.store).
+      to(receive(:doc_auth_link_sent_controller_enabled).and_return(true))
 
-      user
-      complete_doc_auth_steps_before_upload_step
-      clear_and_fill_in(:doc_auth_phone, phone_number)
-      click_send_link
-      visit(idv_link_sent_url)
-    end
+    user
+    complete_doc_auth_steps_before_upload_step
+    clear_and_fill_in(:doc_auth_phone, phone_number)
+    click_send_link
+    visit(idv_link_sent_url)
+  end
 
-    it 'Correctly renders the link sent step page' do
-      expect(page).to have_current_path(idv_link_sent_url)
-      expect_step_indicator_current_step(t('step_indicator.flows.idv.verify_id'))
-      expect(page).to have_content(phone_number)
-    end
+  it 'Correctly renders the link sent step page' do
+    expect(page).to have_current_path(idv_link_sent_url)
+    expect_step_indicator_current_step(t('step_indicator.flows.idv.verify_id'))
+    expect(page).to have_content(phone_number)
+  end
 
-    context 'when link sent polling is enabled' do
-      let(:doc_capture_polling_enabled) { true }
+  context 'when link sent polling is enabled' do
+    let(:doc_capture_polling_enabled) { true }
 
-      # Currently get a javascript error when explicitly visiting the new url
-      # Try this again once upload redirects here.
-      xit 'Does not show continue button' do
-        expect(page).not_to have_content('Continue') # doc_auth.buttons.continue
-      end
+    # Currently get a javascript error when explicitly visiting the new url
+    # Try this again once upload redirects here.
+    xit 'Does not show continue button', :js do
+      expect(page).not_to have_content('Continue') # doc_auth.buttons.continue
     end
   end
 end
