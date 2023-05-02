64dc44b07ff1f501a2607143a2640c1607b4c31b
Kimball Bighorse <kbighorse@yahoo.com>
2023-05-02 13:45:51 -0700

    Freeze time for duration of test

--- spec/jobs/fraud_rejection_daily_job_spec.rb
+++ spec/jobs/fraud_rejection_daily_job_spec.rb
@@ -10,16 +10,18 @@ RSpec.describe FraudRejectionDailyJob do
 
   describe '#perform' do
     it 'rejects profiles which have been review pending for more than 30 days' do
-      create(:profile, fraud_state: 'fraud_reviewing', fraud_reviewing_at: 31.days.ago)
-      create(:profile, fraud_state: 'fraud_reviewing', fraud_reviewing_at: 20.days.ago)
+      freeze_time do
+        create(:profile, fraud_state: 'fraud_reviewing', fraud_reviewing_at: 31.days.ago)
+        create(:profile, fraud_state: 'fraud_reviewing', fraud_reviewing_at: 20.days.ago)
 
-      rejected_profiles = Profile.fraud_rejected
+        rejected_profiles = Profile.fraud_rejected
 
-      expect { job.perform(Time.zone.today) }.to change { rejected_profiles.count }.by(1)
-      expect(job_analytics).to have_logged_event(
-        'Fraud: Automatic Fraud Rejection',
-        fraud_rejection_at: rejected_profiles.first.fraud_rejected_at,
-      )
+        expect { job.perform(Time.zone.today) }.to change { rejected_profiles.count }.by(1)
+        expect(job_analytics).to have_logged_event(
+          'Fraud: Automatic Fraud Rejection',
+          fraud_rejection_at: rejected_profiles.first.fraud_rejected_at,
+        )
+      end
     end
   end
 end
