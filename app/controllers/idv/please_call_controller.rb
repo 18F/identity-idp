2dd871e502cd4bfc5e94f4a78ea2ddf3f7fefd21
John Maxwell <john.maxwell@gsa.gov>
2023-05-04 12:44:51 -0400

    Review comment: Use User#fraud_rejection_profile

--- app/controllers/idv/please_call_controller.rb
+++ app/controllers/idv/please_call_controller.rb
@@ -6,7 +6,7 @@ module Idv
 
     def show
       analytics.idv_please_call_visited
-      pending_at = current_user.profiles.last.fraud_review_pending_at || Time.zone.today
+      pending_at = current_user.fraud_rejection_profile.fraud_review_pending_at || Time.zone.today
       @call_by_date = pending_at + FRAUD_REVIEW_CONTACT_WITHIN_DAYS
     end
   end
