73f0559e9164d71223273891f7fc45ba0b2e888e
Andrew Duthie <andrew.duthie@gsa.gov>
2023-05-08 10:01:02 -0400

    LG-5456: Implement confirmation tooltip for clipboard button (#8307)
    * LG-5456: Implement confirmation tooltip for clipboard button
    
    changelog: User-Facing Improvements, Copy Button, Add confirmation tooltip to Copy buttons
    
    * Allow non-visible idref references
    
    Hidden elements are valid as references for idref
    
    * Workaround to show tooltip full width at small sizes

--- app/javascript/packages/clipboard-button/clipboard-button-element.ts
+++ app/javascript/packages/clipboard-button/clipboard-button-element.ts
@@ -1,17 +1,47 @@
+import { tooltip } from '@18f/identity-design-system';
+
 class ClipboardButtonElement extends HTMLElement {
   button: HTMLButtonElement;
 
   connectedCallback() {
     this.button = this.querySelector('button')!;
 
-    this.button.addEventListener('click', () => this.writeToClipboard());
+    this.setUpTooltip();
+    this.button.addEventListener('click', () => this.handleClick());
   }
 
   /**
    * Returns the text to be copied to the clipboard.
    */
   get clipboardText(): string {
-    return this.dataset.clipboardText || '';
+    return this.getAttribute('clipboard-text')!;
+  }
+
+  /**
+   * Returns the text to be shown in the confirmation tooltip.
+   */
+  get tooltipText(): string {
+    return this.getAttribute('tooltip-text')!;
+  }
+
+  /**
+   * Initializes the tooltip element.
+   */
+  setUpTooltip() {
+    const { tooltipBody } = tooltip.setup(this.button);
+
+    // A default USWDS tooltip will always be visible when the trigger has focus. The clipboard
+    // button only shows the tooltip once activated. To ensure the tooltip content is read when
+    // made visible, change its contents to a live region.
+    tooltipBody.setAttribute('aria-live', 'polite');
+  }
+
+  /**
+   * Handles behaviors associated with clicking the button.
+   */
+  handleClick() {
+    this.writeToClipboard();
+    this.showTooltip();
   }
 
   /**
@@ -19,6 +49,31 @@ class ClipboardButtonElement extends HTMLElement {
    */
   writeToClipboard() {
     navigator.clipboard.writeText(this.clipboardText);
+  }
+
+  /**
+   * Displays confirmation tooltip and binds event to dismiss tooltip on next blur.
+   */
+  showTooltip() {
+    const { trigger, body } = tooltip.getTooltipElements(this.button);
+    body.textContent = this.tooltipText;
+    tooltip.show(body, trigger, 'top');
+
+    function hideTooltip() {
+      body.textContent = '';
+      tooltip.hide(body);
+    }
+
+    // In most browsers, clicking the button will focus it, and the tooltip should remain visible
+    // until the user moves away. In Safari, clicking a button does not give it focus (by design),
+    // so the tooltip sould remain visible as long as the user's cursor remains over the button.
+    //
+    // See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#clicking_and_focus
+    if (document.activeElement === this.button) {
+      this.button.addEventListener('blur', hideTooltip, { once: true });
+    } else {
+      this.button.addEventListener('mouseout', hideTooltip, { once: true });
+    }
   }
 }
 
