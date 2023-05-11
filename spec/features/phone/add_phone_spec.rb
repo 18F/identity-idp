fb9b2fe2344b4f7d445a34c6b0553c94a474f2e8
Jessica Dembe <jessica.dembe@gsa.gov>
2023-05-11 14:48:03 -0400

    change path to different controller

--- spec/features/phone/add_phone_spec.rb
+++ spec/features/phone/add_phone_spec.rb
@@ -294,6 +294,6 @@ describe 'Add a new phone number' do
     click_continue
     click_link t('two_factor_authentication.phone_verification.troubleshooting.change_number')
 
-    expect(page).to have_current_path(phone_setup_path)
+    expect(page).to have_current_path(add_phone_path)
   end
 end
