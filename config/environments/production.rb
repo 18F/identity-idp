2bd11850756406247f2101d7ff776f9dced850f5
Andrew Duthie <andrew.duthie@gsa.gov>
2023-05-12 16:13:03 -0400

    Replace Sprockets with Propshaft
    changelog: Internal, Build Tooling, Update asset pipeline to replace Sprockets with Propshaft

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,6 @@ Rails.application.configure do
     # and request is nil during asset precompilation
     (IdentityConfig.store.asset_host.presence || IdentityConfig.store.domain_name) if request
   end
-  config.assets.compile = false
-  config.assets.digest = true
-  config.assets.gzip = false
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.active_record.dump_schema_after_migration = false
