fc03314f40003d3345a8973510448bef719e81b7
Sonia Connolly <sonia.connolly@gsa.gov>
2023-05-15 15:32:09 -0700

    Add redirect to document_capture in redo_document_capture_action

--- app/services/idv/actions/redo_document_capture_action.rb
+++ app/services/idv/actions/redo_document_capture_action.rb
@@ -8,8 +8,13 @@ module Idv
       def call
         flow_session['redo_document_capture'] = true
         unless flow_session[:skip_upload_step]
-          mark_step_incomplete(:link_sent)
           mark_step_incomplete(:upload)
+
+          if IdentityConfig.store.doc_auth_link_sent_controller_enabled
+            redirect_to idv_document_capture_url
+          else
+            mark_step_incomplete(:link_sent)
+          end
         end
       end
     end
