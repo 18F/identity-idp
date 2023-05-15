631964a60db756f49ee300693c703f3482c67d95
Sonia Connolly <sonia.connolly@gsa.gov>
2023-05-15 12:06:48 -0700

    Fix new link_sent page back link
    Needed to use idv_doc_auth_step_url instead of idv_doc_auth_url
    
    [skip_changelog]
    
    Co-authored-by: Amir Reavis-Bey <amir.reavis-bey@gsa.gov>

--- spec/features/idv/actions/cancel_link_sent_action_spec.rb
+++ spec/features/idv/actions/cancel_link_sent_action_spec.rb
@@ -4,7 +4,11 @@ feature 'doc auth cancel link sent action' do
   include IdvStepHelper
   include DocAuthHelper
 
+  let(:new_controller_enabled) { false }
+
   before do
+    allow(IdentityConfig.store).to receive(:doc_auth_link_sent_controller_enabled).
+      and_return(new_controller_enabled)
     sign_in_and_2fa_user
     complete_doc_auth_steps_before_link_sent_step
   end
@@ -13,5 +17,16 @@ feature 'doc auth cancel link sent action' do
     click_doc_auth_back_link
 
     expect(page).to have_current_path(idv_doc_auth_upload_step)
+  end
+
+  context 'new SendLink controller is enabled' do
+    let(:new_controller_enabled) { true }
+
+    it 'returns to link sent step', :js do
+      expect(page).to have_current_path(idv_link_sent_path)
+      click_doc_auth_back_link
+
+      expect(page).to have_current_path(idv_doc_auth_upload_step)
+    end
   end
 end
