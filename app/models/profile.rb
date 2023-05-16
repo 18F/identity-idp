93ad569bf2f7cf5dbd1ba12e49f32bf3a52e17f7
Alex Bradley <alexander.bradley@gsa.gov>
2023-05-16 16:19:59 -0400

    deprecate gpo_verification_pending deactivation_reason

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -18,7 +18,7 @@ class Profile < ApplicationRecord
   enum deactivation_reason: {
     password_reset: 1,
     encryption_error: 2,
-    # gpo_verification_pending used to be here. Do not map anything to 3 for deactivation_reason
+    gpo_verification_pending_NO_LONGER_USED: 3, # deprecated
     verification_cancelled: 4,
     in_person_verification_pending: 5,
   }
