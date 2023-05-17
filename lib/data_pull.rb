d27b50806c7569f64ec156d680a064d76b469368
Osman Latif <olatif@flexion.us>
2023-05-17 11:27:15 -0500

    LG-9777: Add profile-summary data-pull task

--- lib/data_pull.rb
+++ lib/data_pull.rb
@@ -106,6 +106,7 @@ class DataPull
       'uuid-convert' => UuidConvert,
       'email-lookup' => EmailLookup,
       'ig-request' => InspectorGeneralRequest,
+      'profile-summary' => ProfileSummary,
     }[name]
   end
 
@@ -126,6 +127,8 @@ class DataPull
           * #{basename} email-lookup uuid1 uuid2
 
           * #{basename} ig-request uuid1 uuid2 --requesting-issuer ABC:DEF:GHI
+
+          * #{basename} profile-summary uuid1 uuid2
 
         Options:
       EOS
@@ -263,4 +266,46 @@ class DataPull
       )
     end
   end
+
+  class ProfileSummary
+    def run(args:, config:)
+      uuids = args
+
+      users = User.includes(:profiles).where(uuid: uuids).order(:uuid)
+
+      table = []
+      table << %w[uuid current_status activated_timestamp disabled_reason fraud_review_pending_timestamp fraud_rejection_timestamp]
+
+      users.each do |user|
+        if user.profiles.any?
+          user.profiles.sort_by(&:id).each do |profile|
+            profile_status = profile.active ? 'Active' : 'Inactive'
+            table << [
+              user.uuid,
+              profile_status,
+              profile.activated_at,
+              profile.deactivation_reason,
+              profile.fraud_review_pending_at,
+              profile.fraud_rejection_at,
+            ]
+          end
+        elsif config.include_missing?
+          table << [user.uuid, '[HAS NO PROFILE]', nil, nil, nil, nil]
+        end
+      end
+
+      if config.include_missing?
+        (uuids - users.map(&:uuid)).each do |missing_uuid|
+          table << [missing_uuid, '[UUID NOT FOUND]', nil, nil, nil, nil]
+        end
+      end
+
+      Result.new(
+        subtask: 'profile-summary',
+        uuids: users.map(&:uuid),
+        table:,
+      )
+    end
+  end
+
 end
--- spec/lib/data_pull_spec.rb
+++ spec/lib/data_pull_spec.rb
@@ -238,4 +238,36 @@ RSpec.describe DataPull do
       end
     end
   end
+
+  describe DataPull::ProfileSummary do
+    subject(:subtask) { DataPull::ProfileSummary.new }
+
+    describe '#run' do
+      let(:user) { create(:profile, :active, :verified).user }
+      let(:user_without_profile) { create(:user) }
+
+      let(:args) { [user.uuid, user_without_profile.uuid, 'uuid-does-not-exist'] }
+      let(:include_missing) { true }
+      let(:config) { DataPull::Config.new(include_missing:) }
+      subject(:result) { subtask.run(args:, config:) }
+
+      it 'loads profile summary for the user', aggregate_failures: true do
+        expect(result.table).to match_array(
+          [
+            ['uuid', 'current_status', 'activated_timestamp', 'disabled_reason', 'fraud_review_pending_timestamp', 'fraud_rejection_timestamp'],
+            *user.profiles.sort_by(&:id).map do |p|
+              profile_status = p.active ? 'Active' : 'Inactive'
+              [user.uuid, profile_status, kind_of(Time), p.deactivation_reason, nil, nil]
+            end,
+            [user_without_profile.uuid, '[HAS NO PROFILE]', nil, nil, nil, nil],
+            ['uuid-does-not-exist', '[UUID NOT FOUND]', nil, nil, nil, nil],
+          ],
+        )
+
+        expect(result.subtask).to eq('profile-summary')
+        expect(result.uuids).to match_array([user.uuid, user_without_profile.uuid])
+      end
+    end
+  end
+
 end
