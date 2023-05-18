b9c2334c551a99797de8f92a277b7df491ba2dde
Andrew Duthie <andrew.duthie@gsa.gov>
2023-05-18 15:58:10 -0400

    Add support for per-component stylesheets (#8375)
    * Add support for per-component stylesheets
    
    changelog: Internal, Optimization, Reduce size of stylesheet assets for critical path
    
    * Forward required to use uswds-core conventionally
    
    * Override _uswds-core via internal load path
    
    * Remove unnecessary load path
    
    * Prioritize `--load-path` flag in Sass load path resolution
    
    * Add specs for component stylesheets
    
    * Add specs for StylesheetHelper
    
    * Rename clipboard component stylesheet to simple scss
    
    * Render stylesheet_once_tags everywhere application is rendered
    
    * Fix component preview for per-component stylesheet

--- app/components/base_component.rb
+++ app/components/base_component.rb
@@ -1,21 +1,48 @@
 class BaseComponent < ViewComponent::Base
   def before_render
-    return if @rendered_scripts
-    @rendered_scripts = true
-    if helpers.respond_to?(:enqueue_component_scripts) && self.class.scripts.present?
-      helpers.enqueue_component_scripts(*self.class.scripts)
-    end
+    render_assets unless rendered_assets?
   end
 
   def self.scripts
     @scripts ||= begin
-      scripts = sidecar_files(['js', 'ts']).map { |file| File.basename(file, '.*') }
+      scripts = sidecar_files_basenames(['js', 'ts'])
       scripts.concat superclass.scripts if superclass.respond_to?(:scripts)
       scripts
     end
   end
 
+  def self.stylesheets
+    @stylesheets ||= begin
+      stylesheets = sidecar_files_basenames(['scss'])
+      stylesheets.concat superclass.stylesheets if superclass.respond_to?(:stylesheets)
+      stylesheets
+    end
+  end
+
   def unique_id
     @unique_id ||= SecureRandom.hex(4)
+  end
+
+  private
+
+  attr_accessor :rendered_assets
+  alias_method :rendered_assets?, :rendered_assets
+
+  class << self
+    def sidecar_files_basenames(extensions)
+      sidecar_files(extensions).map { |file| File.basename(file, '.*') }
+    end
+  end
+
+  def render_assets
+    if helpers.respond_to?(:enqueue_component_scripts) && self.class.scripts.present?
+      helpers.enqueue_component_scripts(*self.class.scripts)
+    end
+
+    if helpers.respond_to?(:enqueue_component_stylesheets) && self.class.stylesheets.present?
+      helpers.enqueue_component_stylesheets(*self.class.stylesheets)
+    end
+
+    @rendered_assets = true
   end
 end
