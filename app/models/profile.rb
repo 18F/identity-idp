b5cbd37629bb71c0e92b5e20a8b03ac1848ce909
Kimball Bighorse <kbighorse@yahoo.com>
2023-05-22 17:39:21 -0700

    Restore previous order

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -150,8 +150,8 @@ class Profile < ApplicationRecord
   end
 
   def reject_for_fraud(notify_user:)
-    fraud_reject
     update!(active: false)
+    fraud_reject
     track_fraud_review_adjudication(
       decision: notify_user ? 'manual_reject' : 'automatic_reject',
     )
