3f9ff036ea8c2a20a2e97d5f44558830eabe7d9f
Kimball Bighorse <kbighorse@yahoo.com>
2023-05-23 17:16:14 -0700

    Revert "Restore previous order"
    This reverts commit 6d2079fa3f9843f30cc66dbafb9bbf78e591117a.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -145,8 +145,8 @@ class Profile < ApplicationRecord
   end
 
   def deactivate_for_fraud_review
-    update!(active: false)
     fraud_review
+    update!(active: false)
   end
 
   def reject_for_fraud(notify_user:)
