dce96dbafe267d51752d0cc8364f450906e769f6
Jonathan Hooper <jonathan.hooper@gsa.gov>
2023-05-24 11:15:18 -0400

    Remove the backup code logic from the verify profile concern (#8461)
    The verify profile concern is intened to redirect users with profiles that need additional work to verify or unlock to the appropriate place. The backup code reminder logic got mixed into this which causes a little confusion and indirection. This commit moves that logic into its own concern.
    
    [skip changelog]

--- app/controllers/concerns/verify_profile_concern.rb
+++ app/controllers/concerns/verify_profile_concern.rb
@@ -4,21 +4,7 @@ module VerifyProfileConcern
   def account_or_verify_profile_url
     return reactivate_account_url if user_needs_to_reactivate_account?
     return idv_gpo_verify_url if profile_needs_verification?
-    return backup_code_reminder_url if user_needs_backup_code_reminder?
     account_url
-  end
-
-  def user_needs_backup_code_reminder?
-    user_backup_codes_configured? && user_last_signed_in_more_than_5_months_ago?
-  end
-
-  def user_backup_codes_configured?
-    MfaContext.new(current_user).backup_code_configurations.present?
-  end
-
-  def user_last_signed_in_more_than_5_months_ago?
-    second_last_signed_in_at = current_user.second_last_signed_in_at
-    second_last_signed_in_at && second_last_signed_in_at < 5.months.ago
   end
 
   def profile_needs_verification?
