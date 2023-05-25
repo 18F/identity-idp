8f414d3448e841d36bd4a41b6f8b2130e661c514
Sonia Connolly <sonia.connolly@gsa.gov>
2023-05-25 12:56:56 -0700

    WIP: Add test

--- spec/controllers/concerns/idv_step_concern_spec.rb
+++ spec/controllers/concerns/idv_step_concern_spec.rb
@@ -207,35 +207,55 @@ describe 'IdvStepConcern' do
       before_action :confirm_no_pending_gpo_profile
     end
 
-    before(:each) do
-      sign_in(user)
-      allow(subject).to receive(:current_user).and_return(user)
-      routes.draw do
-        get 'show' => 'idv/step#show'
+    context 'with a current_user' do
+      before(:each) do
+        sign_in(user)
+        allow(subject).to receive(:current_user).and_return(user)
+        routes.draw do
+          get 'show' => 'idv/step#show'
+        end
+      end
+
+      context 'without pending gpo profile' do
+        it 'does not redirect' do
+          get :show
+
+          expect(response.body).to eq 'Hello'
+          expect(response).to_not redirect_to idv_gpo_verify_url
+          expect(response.status).to eq 200
+        end
+      end
+
+      context 'with pending gpo profile' do
+        let(:user) { create(:user, :with_pending_gpo_profile, :fully_registered) }
+
+        it 'redirects to enter your code page' do
+          get :show
+
+          expect(response).to redirect_to idv_gpo_verify_url
+        end
       end
     end
 
-    context 'without pending gpo profile' do
+    context 'without current_user (hybrid_mobile flow)' do
+      let!(:idv_session) do
+        Idv::Session.new(user_session: subject.user_session, current_user: nil, service_provider: nil)
+      end
+
+      before(:each) do
+        sign_in(user)
+#        allow(subject).to receive(:current_user).and_return(nil)
+        routes.draw do
+          get 'show' => 'idv/step#show'
+        end
+      end
+
       it 'does not redirect' do
         get :show
 
         expect(response.body).to eq 'Hello'
-        expect(response).to_not redirect_to idv_in_person_ready_to_verify_url
+        expect(response).to_not redirect_to idv_gpo_verify_url
         expect(response.status).to eq 200
-      end
-    end
-
-    context 'with pending gpo profile' do
-      let(:user) { create(:user, :with_pending_gpo_profile, :fully_registered) }
-
-      before do
-        allow(IdentityConfig.store).to receive(:in_person_proofing_enabled).and_return(true)
-      end
-
-      it 'redirects to enter your code page' do
-        get :show
-
-        expect(response).to redirect_to idv_gpo_verify_url
       end
     end
   end
