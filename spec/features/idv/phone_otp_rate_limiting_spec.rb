031bc05daa930dbacfe5c0dce5e32bb70f381fdd
Andrew Duthie <andrew.duthie@gsa.gov>
2023-05-26 15:47:22 -0400

    Update spec to satisfy code format in wrong code
    Previously, the form would allow this submission, but now that we're more strict about the format, it'd be caught on presubmission form validation

--- spec/features/idv/phone_otp_rate_limiting_spec.rb
+++ spec/features/idv/phone_otp_rate_limiting_spec.rb
@@ -37,7 +37,7 @@ feature 'phone otp rate limiting', :js do
       complete_idv_steps_before_phone_otp_verification_step(user)
 
       max_attempts.times do
-        fill_in('code', with: 'wrong')
+        fill_in('code', with: 'badbad')
         click_button t('forms.buttons.submit.default')
       end
 
