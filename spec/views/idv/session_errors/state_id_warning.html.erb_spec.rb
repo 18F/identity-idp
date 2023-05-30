eb4381e9d471ea5f59b3a0f7680027fdfd88c392
Matt Hinz <matt.hinz@gsa.gov>
2023-05-30 11:36:11 -0700

    Use failure_to_proof_path instead of cancel

--- spec/views/idv/session_errors/state_id_warning.html.erb_spec.rb
+++ spec/views/idv/session_errors/state_id_warning.html.erb_spec.rb
@@ -30,7 +30,10 @@ describe 'idv/session_errors/state_id_warning.html.erb' do
   it 'shows exit login.gov button' do
     expect(rendered).to have_link(
       t('idv.warning.state_id.cancel_button', app_name: APP_NAME),
-      href: idv_cancel_path,
+      href: return_to_sp_failure_to_proof_path(
+        step: 'verify_id',
+        location: 'failure',
+      ),
     )
   end
 end
