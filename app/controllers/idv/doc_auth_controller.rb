fa8b835445ee7b53f26e49e851b5aa211851a062
Sonia Connolly <sonia.connolly@gsa.gov>
2023-05-30 11:12:38 -0700

    Remove UploadStep from DocAuthFlow

--- app/controllers/idv/doc_auth_controller.rb
+++ app/controllers/idv/doc_auth_controller.rb
@@ -20,7 +20,7 @@ module Idv
 
     FLOW_STATE_MACHINE_SETTINGS = {
       step_url: :idv_doc_auth_step_url,
-      final_url: :idv_link_sent_url,
+      final_url: :idv_hybrid_handoff_url,
       flow: Idv::Flows::DocAuthFlow,
       analytics_id: 'Doc Auth',
     }.freeze
