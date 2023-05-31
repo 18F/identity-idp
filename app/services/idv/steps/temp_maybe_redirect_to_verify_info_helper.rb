b6ae081e9113e74286d4a196f0b237d9419191fb
svalexander <shannon.alexander-navarro@gsa.gov>
2023-05-31 09:58:05 -0400

    lint issues

--- app/services/idv/steps/temp_maybe_redirect_to_verify_info_helper.rb
+++ app/services/idv/steps/temp_maybe_redirect_to_verify_info_helper.rb
@@ -3,16 +3,16 @@
 # flag is removed.
 #
 module Idv
-    module Steps
-      module TempMaybeRedirectToVerifyInfoHelper
-        private
-  
-        def maybe_redirect_to_verify_info(skip = false)
-          return unless IdentityConfig.store.in_person_verify_info_controller_enabled
-          return if skip
-          flow_session[:flow_path] = @flow.flow_path
-          redirect_to idv_in_person_verify_info_url
-        end
+  module Steps
+    module TempMaybeRedirectToVerifyInfoHelper
+      private
+
+      def maybe_redirect_to_verify_info(skip = false)
+        return unless IdentityConfig.store.in_person_verify_info_controller_enabled
+        return if skip
+        flow_session[:flow_path] = @flow.flow_path
+        redirect_to idv_in_person_verify_info_url
       end
     end
+  end
   end
