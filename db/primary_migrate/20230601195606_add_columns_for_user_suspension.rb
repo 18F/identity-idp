52135f894d702e8d8caf453c24e24971c4c45d12
Osman Latif <109746710+olatifflexion@users.noreply.github.com>
2023-06-02 10:08:10 -0500

    Update db/primary_migrate/20230601195606_add_columns_for_user_suspension.rb
    Co-authored-by: Mitchell Henke <mitchell.henke@gsa.gov>

--- db/primary_migrate/20230601195606_add_columns_for_user_suspension.rb
+++ db/primary_migrate/20230601195606_add_columns_for_user_suspension.rb
@@ -5,7 +5,7 @@ class AddColumnsForUserSuspension < ActiveRecord::Migration[7.0]
     add_column :users, :suspended_at, :datetime
     add_column :users, :reinstated_at, :datetime
 
-    add_index :users, :suspended_at, algorithm: :concurrently
-    add_index :users, :reinstated_at, algorithm: :concurrently
+    add_index :users, :suspended_at, where: 'suspended_at IS NOT NULL', algorithm: :concurrently
+    add_index :users, :reinstated_at, where: 'reinstated_at IS NOT NULL', algorithm: :concurrently
   end
 end
