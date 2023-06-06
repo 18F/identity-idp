cc8baf7913f17aed7e784a879a4b8cab5c0de898
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-05 17:21:54 -0700

    Initial controller, view, spec

---
+++ app/controllers/idv/agreement_controller.rb
@@ -0,0 +1,74 @@
+module Idv
+  class AgreementController < ApplicationController
+    include IdvSession
+    include IdvStepConcern
+    include StepIndicatorConcern
+    include StepUtilitiesConcern
+
+    before_action :confirm_two_factor_authenticated
+    before_action :render_404_if_agreement_controller_disabled
+    before_action :confirm_welcome_step_complete
+    before_action :confirm_agreement_needed
+
+    def show
+      analytics.idv_doc_auth_agreement_visited(**analytics_arguments)
+
+      Funnel::DocAuth::RegisterStep.new(current_user.id, sp_session[:issuer]).call(
+        'agreement', :view,
+        true
+      )
+
+      render :show
+    end
+
+    def update
+      analytics.idv_doc_auth_agreement_submitted(**analytics_arguments)
+
+      # for the 50/50 state
+      flow_session['Idv::Steps::AgreementStep'] = true
+
+      skip_to_capture if params[:skip_upload]
+
+      Idv::ConsentForm.new.submit(consent_form_params)
+
+      idv_session.agreement_checked = true
+
+      redirect_to idv_hybrid_handoff_url
+    end
+
+    private
+
+    def analytics_arguments
+      {
+        step: 'agreement',
+        analytics_id: 'Doc Auth',
+        irs_reproofing: irs_reproofing?,
+      }
+    end
+
+    def skip_to_capture
+      flow_session[:skip_upload_step] = true
+      flow_session[:flow_path] = 'standard'
+    end
+
+    def consent_form_params
+      params.require(:doc_auth).permit(:ial2_consent_given)
+    end
+
+    def confirm_welcome_step_complete
+      return if flow_session['Idv::Steps::WelcomeStep']
+
+      redirect_to idv_doc_auth_url
+    end
+
+    def confirm_agreement_needed
+      return unless idv_session.agreement_checked
+
+      redirect_to idv_hybrid_handoff_url
+    end
+
+    def render_404_if_agreement_controller_disabled
+      render_not_found unless IdentityConfig.store.doc_auth_agreement_controller_enabled
+    end
+  end
+end
---
+++ spec/controllers/idv/agreement_controller_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+describe Idv::AgreementController do
+  include IdvHelper
+
+  let(:user) { create(:user) }
+
+  let(:feature_flag_enabled) { true }
+
+  before do
+    allow(IdentityConfig.store).to receive(:doc_auth_agreement_controller_enabled).
+      and_return(feature_flag_enabled)
+    stub_sign_in(user)
+    stub_analytics
+    subject.user_session['idv/doc_auth'] = { 'Idv::Steps::WelcomeStep' => true }
+  end
+
+  describe 'before_actions' do
+    it 'includes authentication before_action' do
+      expect(subject).to have_actions(
+        :before,
+        :confirm_two_factor_authenticated,
+      )
+    end
+  end
+
+  describe '#show' do
+    let(:analytics_name) { 'IdV: doc auth agreement visited' }
+    let(:analytics_args) do
+      { step: 'agreement',
+        analytics_id: 'Doc Auth',
+        irs_reproofing: false }
+    end
+
+    it 'renders the show template' do
+      get :show
+
+      expect(response).to render_template :show
+    end
+
+    it 'sends analytics_visited event' do
+      get :show
+
+      expect(@analytics).to have_logged_event(analytics_name, analytics_args)
+    end
+
+    it 'updates DocAuthLog document_capture_view_count' do
+      doc_auth_log = DocAuthLog.create(user_id: user.id)
+
+      expect { get :show }.to(
+        change { doc_auth_log.reload.agreement_view_count }.from(0).to(1),
+      )
+    end
+
+    context 'welcome step is not complete' do
+      it 'redirects to idv_doc_auth_url' do
+        subject.user_session['idv/doc_auth']['Idv::Steps::WelcomeStep'] = nil
+
+        get :show
+
+        expect(response).to redirect_to(idv_doc_auth_url)
+      end
+    end
+
+    context 'agreement already visited' do
+      it 'redirects to hybrid_handoff' do
+        allow(subject.idv_session).to receive(:agreement_checked).and_return(true)
+
+        get :show
+
+        expect(response).to redirect_to(idv_hybrid_handoff_url)
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:analytics_name) { 'IdV: doc auth agreement submitted' }
+
+    let(:analytics_args) do
+      { success: true,
+        errors: {},
+        step: 'agreement',
+        analytics_id: 'Doc Auth',
+        irs_reproofing: false }
+    end
+
+    it 'sends analytics_submitted event' do
+      put :update
+
+      expect(@analytics).to have_logged_event(analytics_name, analytics_args)
+    end
+  end
+
+  context 'when doc_auth_document_capture_controller_enabled is false' do
+    let(:feature_flag_enabled) { false }
+
+    it 'returns 404' do
+      get :show
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+end
