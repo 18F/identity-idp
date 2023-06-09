c020a2c0a5d9c0a15fa3088ee8800619dcdb1513
Zach Margolis <zachary.margolis@gsa.gov>
2023-06-09 09:40:12 -0700

    fix mailer preview spec

--- spec/mailers/previews/report_mailer_preview_spec.rb
+++ spec/mailers/previews/report_mailer_preview_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 require_relative './report_mailer_preview'
 
 RSpec.describe ReportMailerPreview do
+  subject(:mailer_preview) { ReportMailerPreview.new }
+
   describe '#warn_error' do
     it 'generates a warn_error email' do
-      expect { ReportMailerPreview.warn_error }.to_not raise_error
+      expect { mailer_preview.warn_error }.to_not raise_error
     end
   end
 end
