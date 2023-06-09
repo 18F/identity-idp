7aacbf1d3206674662c1dd25684a3ac226b91ee4
Dawei Wang <daweiwang@navapbc.com>
2023-06-09 13:12:49 -0400

    LG-9449: job config and test.

--- spec/jobs/arcgis_token_job_spec.rb
+++ spec/jobs/arcgis_token_job_spec.rb
@@ -1,42 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ArcgisTokenJob, type: :job do
-  let(:subject) { described_class.new }
+  let(:token_keeper) { instance_spy(ArcgisApi::TokenKeeper) }
+  let(:subject) { described_class.new(token_keeper) }
   describe 'arcgis token job' do
-    before(:each) do
-      subject.token_keeper.remove_token!
-    end
-    after(:each) do
-      subject.token_keeper.remove_token!
-    end
     it 'fetches token successfully' do
-      expected = 'ABCDEFG'
-      stub_request(:post, %r{/generateToken}).to_return(
-        {
-          status: 429,
-        },
-        {
-          status: 403,
-        },
-        {
-          status: 200,
-          body: ArcgisApi::Mock::Fixtures.request_token_service_error,
-          headers: { content_type: 'application/json;charset=UTF-8' },
-        },
-        {
-          status: 200,
-          body: ArcgisApi::Mock::Fixtures.invalid_gis_token_credentials_response,
-          headers: { content_type: 'application/json;charset=UTF-8' },
-        },
-        { status: 200,
-          body: {
-            token: expected,
-            expires: (Time.zone.now.to_f + 3600) * 1000,
-            ssl: true,
-          }.to_json,
-          headers: { content_type: 'application/json;charset=UTF-8' } },
-      )
       subject.perform
+      expect(token_keeper).to have_received(:fetch_save_token!).once
     end
   end
 end
