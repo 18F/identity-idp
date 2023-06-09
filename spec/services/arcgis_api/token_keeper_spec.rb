f3e5bf5f956a5a3e23cd35809a04ac9f90d854d5
Dawei Wang <daweiwang@navapbc.com>
2023-06-08 20:20:08 -0400

    LG-9449: WIP

--- spec/services/arcgis_api/token_keeper_spec.rb
+++ spec/services/arcgis_api/token_keeper_spec.rb
@@ -2,12 +2,17 @@
 
 require 'rails_helper'
 
-RSpec.describe ArcgisApi::TokenUtil do
+RSpec.describe ArcgisApi::TokenKeeper do
 
   # Faraday::Connection object that uses the test adapter
-  let(:conn) { ArcgisApi::ConnectionFactory.new.connection }
+  let(:connection_factory) { ArcgisApi::ConnectionFactory.new }
   let(:prefetch_ttl) { 1 }
-  let(:subject) { described_class.new(conn, prefetch_ttl) }
+  let(:analytics) {instance_spy(Analytics)}
+  let(:subject) {
+    obj = described_class.new('test_arcgis_api_token', connection_factory, prefetch_ttl)
+    obj.analytics=(analytics)
+    obj
+  }
 
   let(:expected) { 'ABCDEFG' }
   let(:expected_sec) { 'GFEDCBA' }
@@ -111,13 +116,69 @@ RSpec.describe ArcgisApi::TokenUtil do
 
       end
     end
+
+    context 'value only token in cache' do
+      let(:expected) { 'ABCDEFG' }
+      let(:expected_sec) { 'GFEDCBA' }
+      let(:expires_at) { (Time.zone.now.to_f + 15) * 1000 }
+      before(:each) do
+        stub_request(:post, %r{/generateToken}).to_return(
+          { status: 200,
+            body: {
+              token: expected,
+              expires: expires_at,
+              ssl: true,
+            }.to_json,
+            headers: { content_type: 'application/json;charset=UTF-8' } }
+          )
+        subject.save_token(expected, expires_at)
+      end
+      let(:prefetch_ttl) do
+        5
+      end
+      it 'should use deal with the value only token' do
+        token = subject.token
+        expect(token).to eq(expected)
+      end
+    end
   end
   context 'with in memory store' do
     let(:cache_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
-    include_examples 'acquire token test'
+    context 'sliding expiration enabled' do
+      before(:each) do
+        allow(IdentityConfig.store).to receive(:arcgis_token_sliding_expiration_enabled).and_return(true)
+      end
+      include_examples 'acquire token test'
+    end
   end
   context 'with redis store' do
     let(:cache_store) { ActiveSupport::Cache.lookup_store(:redis_cache_store) }
     include_examples 'acquire token test'
+    context 'retry options' do
+      it 'retry remote request multiple times as needed and emit analytics events' do
+        stub_request(:post, %r{/generateToken}).to_return(
+          {
+            status: 200,
+            body: ArcgisApi::Mock::Fixtures.request_token_service_error,
+            headers: { content_type: 'application/json;charset=UTF-8' },
+          },
+          {
+            status: 200,
+            body: ArcgisApi::Mock::Fixtures.invalid_gis_token_credentials_response,
+            headers: { content_type: 'application/json;charset=UTF-8' },
+          },
+          { status: 200,
+            body: {
+              token: expected,
+              expires: (Time.zone.now.to_f + 3600) * 1000,
+              ssl: true,
+            }.to_json,
+            headers: { content_type: 'application/json;charset=UTF-8' } }
+          )
+        token = subject.fetch_save_token
+        expect(token.fetch(:token)).to eq(expected)
+        expect(analytics).to have_received(:idv_arcgis_request_failure).exactly(2).times
+      end
+    end
   end
 end
