950a38499bbd8399225ab004335f45aa835d0bde
Nadya Primak <nadya.primak@gsa.gov>
2023-06-12 15:11:17 -0400

    refactor and remove json parsing

--- app/jobs/reports/sp_issuer_user_counts_report.rb
+++ app/jobs/reports/sp_issuer_user_counts_report.rb
@@ -5,32 +5,20 @@ module Reports
   class SpIssuerUserCountsReport < BaseReport
     REPORT_NAME = 'sp-issuer-user-counts-report'.freeze
 
-    # [{"issuer"=>"urn:gov:opm:openidconnect.profiles:sp:sso:ServicesOnline:SOL",
-    # "total"=>1238252,
-    # "ial1_total"=>1238252,
-    # "ial2_total"=>0,
-    # "app_id"=>"LGZ317"}]
-
     def perform(_date, issuer)
       user_counts = transaction_with_timeout do
         Db::Identity::SpUserCounts.with_issuer(issuer)
       end
-
-      user_counts_json = JSON.parse(user_counts.to_json)
-      issuer = user_counts_json['issuer']
-      total = user_counts_json['total']
-      ial1_total = user_counts_json['ial1_total']
-      ial2_total = user_counts_json['ial2_total']
 
       emails = IdentityConfig.store.sp_issuer_user_counts.emails
 
       emails.each do |email|
         ReportMailer.system_demand_report(
           email: email,
-          issuer: issuer,
-          total: total,
-          ial1_total: ial1_total,
-          ial2_total: ial2_total,
+          issuer: user_counts['issuer'],
+          total: user_counts['total'],
+          ial1_total: user_counts['ial1_total'],
+          ial2_total: user_counts['ial2_total'],
           name: REPORT_NAME,
         ).deliver_now
       end
