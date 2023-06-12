f5f5d1ce9dc3e0eb0ce605c4c16e36736f7dd49b
Jack Cody <jack.cody@gsa.gov>
2023-06-12 17:22:55 -0500

    Add tests

--- spec/views/users/backup_code_setup/create.html.erb_spec.rb
+++ spec/views/users/backup_code_setup/create.html.erb_spec.rb
@@ -19,4 +19,35 @@ RSpec.describe 'users/backup_code_setup/create.html.erb' do
     expect(data_uri.content_type).to include('text/plain')
     expect(data_uri.read).to eq(@codes.join("\n"))
   end
+
+  it 'displays alert for backup code usage' do
+    render
+
+    expect(rendered).to have_selector(
+      '.usa-alert',
+      text: t('forms.backup_code.caution_codes'),
+    )
+  end
+
+  it 'displays save backup codes checkbox' do
+    render
+
+    expect(rendered).to have_selector('lg-validated-field')
+    expect(rendered).to have_selector('input[type=checkbox]')
+  end
+
+  it 'contains form post to backup_code_continue_path' do
+    render
+
+    expect(rendered).
+      to have_xpath("//form[@action='#{backup_code_continue_path}']")
+    expect(rendered).
+      to have_xpath("//form[@method='post']")
+  end
+
+  it 'has continue button' do
+    render
+
+    expect(rendered).to have_button t('forms.buttons.continue')
+  end
 end
