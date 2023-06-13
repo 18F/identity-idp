efe1ef9e1985eed45281a03926c5195bc5f721ce
Malick Diarra <malick.diarra@gsa.gov>
2023-06-13 14:22:20 -0400

    LG-9972: Implement Usability priority (#8560)
    * changelog: User-Facing Improvements, Authentication, Remove priority a/b testing and do auth app priority first
    
    * clean up more mfa priority test bucket
    
    * rubocop
    
    * fix up two factor presenter tests
    
    * remove unneeded mentions
    
    * add space
    
    * remove whitespace

--- app/controllers/users/backup_code_setup_controller.rb
+++ app/controllers/users/backup_code_setup_controller.rb
@@ -24,10 +24,7 @@ module Users
     def create
       generate_codes
       result = BackupCodeSetupForm.new(current_user).submit
-      analytics_properties = result.to_h.merge(
-        sign_up_mfa_selection_order_bucket:
-                  sign_up_mfa_selection_order_bucket,
-      )
+      analytics_properties = result.to_h
       analytics.backup_code_setup_visit(**analytics_properties)
       irs_attempts_api_tracker.mfa_enroll_backup_code(success: result.success?)
 
@@ -79,7 +76,6 @@ module Users
     def track_backup_codes_confirmation_setup_visit
       analytics.multi_factor_auth_enter_backup_code_confirmation_visit(
         enabled_mfa_methods_count: mfa_user.enabled_mfa_methods_count,
-        sign_up_mfa_selection_order_bucket: sign_up_mfa_selection_order_bucket,
       )
     end
 
@@ -91,11 +87,6 @@ module Users
       revoke_remember_device(current_user)
       @codes = generator.generate
       user_session[:backup_codes] = @codes
-    end
-
-    def sign_up_mfa_selection_order_bucket
-      return unless in_multi_mfa_selection_flow?
-      AbTests::SIGN_UP_MFA_SELECTION.bucket(current_user.uuid)
     end
 
     def set_backup_code_setup_presenter
