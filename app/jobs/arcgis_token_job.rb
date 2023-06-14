b149e7ca83b9c086ced1b2040a03725bec4b6fe3
Dawei Wang <daweiwang@navapbc.com>
2023-06-14 12:01:37 -0400

    LG-9449: job refactor and comment work.

--- app/jobs/arcgis_token_job.rb
+++ app/jobs/arcgis_token_job.rb
@@ -1,12 +1,35 @@
 class ArcgisTokenJob < ApplicationJob
   queue_as :default
 
-  attr_reader :token_keeper
-  def initialize(token_keeper: ArcgisApi::TokenKeeper.new)
-    @token_keeper = token_keeper
+  def perform
+    analytics.idv_arcgis_token_job_started
+    token_keeper.retrieve_token
+    return true
+  rescue StandardError => e
+    analytics.idv_arcgis_token_failure(
+      exception_class: 'ArcGIS',
+      exception_message: e.message,
+      response_body_present: false,
+      response_body: '',
+      response_status_code: '',
+      api_status_code: '',
+    )
+  ensure
+    analytics.idv_arcgis_token_job_completed
   end
 
-  def perform
-    token_keeper.retrieve_token
+  private
+
+  def token_keeper
+    ArcgisApi::TokenKeeper.new
+  end
+
+  def analytics
+    @analytics ||= Analytics.new(
+      user: AnonymousUser.new,
+      request: nil,
+      session: {},
+      sp: nil,
+    )
   end
 end
