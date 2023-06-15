891d1fecbd2361c558befd92aaebf93750a127a1
Mitchell Henke <mitchell.henke@gsa.gov>
2023-06-15 09:42:47 -0500

    Remove deprecated "loa" ServiceProviderRequest attribute
    changelog: Internal, Maintenance, Remove deprecated "loa" ServiceProviderRequest attribute

--- app/models/service_provider_request.rb
+++ app/models/service_provider_request.rb
@@ -5,27 +5,16 @@ class ServiceProviderRequest
   attr_accessor :uuid, :issuer, :url, :ial, :aal, :requested_attributes
 
   def initialize(uuid: nil, issuer: nil, url: nil,
-                 loa: nil, ial: nil, aal: nil, requested_attributes: [])
+                 ial: nil, aal: nil, requested_attributes: [])
     @uuid = uuid
     @issuer = issuer
     @url = url
-    @ial = ial || loa
+    @ial = ial
     @aal = aal
     @requested_attributes = requested_attributes&.map(&:to_s)
-    Rails.logger.info { 'Note: loa used to initialize ServiceProviderRequest' } if loa
   end
 
   def ==(other)
     to_json == other.to_json
-  end
-
-  # To be deleted once we've been deployed for a while and
-  # the cached proxies are all using the new ial parameter
-  def loa
-    @ial
-  end
-
-  def loa=(val)
-    @ial = val
   end
 end
