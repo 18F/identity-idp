6bd793949b47b293c6de58abe90bbf9ed3958fca
JackRyan1989 <jackryan@navapbc.com>
2023-06-15 14:05:34 -0400

    changes to make same_address_as_id not nil for test and adding helper method for creating pii

--- spec/services/idv/steps/in_person/state_id_step_spec.rb
+++ spec/services/idv/steps/in_person/state_id_step_spec.rb
@@ -130,18 +130,7 @@ RSpec.describe Idv::Steps::InPerson::StateIdStep do
             expect(flow.flow_session[:pii_from_user]).to_not have_key attr
           end
 
-          # User picks "Yes, I live at the address on my state-issued ID" on state ID
-          pii_from_user[:same_address_as_id] = 'true' # on form before submission
-          pii_from_user[:identity_doc_address1] = identity_doc_address1
-          pii_from_user[:identity_doc_address2] = identity_doc_address2
-          pii_from_user[:identity_doc_city] = identity_doc_city
-          pii_from_user[:identity_doc_address_state] = identity_doc_address_state
-          pii_from_user[:identity_doc_zipcode] = identity_doc_zipcode
-          pii_from_user[:address1] = address1
-          pii_from_user[:address2] = address2
-          pii_from_user[:city] = city
-          pii_from_user[:state] = state
-          pii_from_user[:zipcode] = zipcode
+          make_pii
 
           # On Verify, user changes response from "Yes,..." to
           # "No, I live at a different address", see submitted_values above
@@ -195,6 +184,9 @@ RSpec.describe Idv::Steps::InPerson::StateIdStep do
           Idv::StateIdForm::ATTRIBUTES.each do |attr|
             expect(flow.flow_session[:pii_from_user]).to_not have_key attr
           end
+
+          make_pii(same_address_as_id: 'false')
+
           # On Verify, user changes response from "No,..." to
           # "Yes, I live at the address on my state-issued ID
           step.call
@@ -231,17 +223,7 @@ RSpec.describe Idv::Steps::InPerson::StateIdStep do
           end
 
           # User picks "No, I live at a different address" on state ID
-          pii_from_user[:same_address_as_id] = 'false' # on form before submission
-          pii_from_user[:identity_doc_address1] = identity_doc_address1
-          pii_from_user[:identity_doc_address2] = identity_doc_address2
-          pii_from_user[:identity_doc_city] = identity_doc_city
-          pii_from_user[:identity_doc_address_state] = identity_doc_address_state
-          pii_from_user[:identity_doc_zipcode] = identity_doc_zipcode
-          pii_from_user[:address1] = address1
-          pii_from_user[:address2] = address2
-          pii_from_user[:city] = city
-          pii_from_user[:state] = state
-          pii_from_user[:zipcode] = zipcode
+          make_pii(same_address_as_id: 'false')
 
           # On Verify, user does not changes response "No,..."
           step.call
