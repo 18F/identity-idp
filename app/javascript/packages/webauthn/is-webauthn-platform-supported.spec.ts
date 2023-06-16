b053288f8966580043500c4ceb38b5e120f55532
Andrew Duthie <andrew.duthie@gsa.gov>
2023-06-16 14:52:30 -0400

    Add coverage for Android Firefox exclusion

--- app/javascript/packages/webauthn/is-webauthn-platform-supported.spec.ts
+++ app/javascript/packages/webauthn/is-webauthn-platform-supported.spec.ts
@@ -10,15 +10,19 @@ const REDUCED_UNSUPPORTED_ANDROID_VERSION_UA =
   'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Mobile Safari/537.36';
 
 // Source: https://www.chromium.org/updates/ua-reduction/#sample-ua-strings-final-reduced-state
-const SUPPORTED_ANDROID_VERSION_UA =
+const SUPPORTED_ANDROID_VERSION_CHROME_UA =
   'Mozilla/5.0 (Linux; Android 9; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.1234.56 Mobile Safari/537.36';
 
+// Source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent/Firefox#mobile_and_tablet_indicators
+const SUPPORTED_ANDROID_VERSION_FIREFOX_UA =
+  'Mozilla/5.0 (Android 9; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0';
+
 // Source: https://chromium.googlesource.com/chromium/src/+/master/docs/ios/user_agent.md
-const UNSUPPORTED_IOS_CHROME_VERSION_UA =
+const UNSUPPORTED_IOS_VERSION_CHROME_UA =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1';
 
 // Source: https://chromium.googlesource.com/chromium/src/+/master/docs/ios/user_agent.md
-const UNSUPPORTED_IOS_SAFARI_VERSION_UA =
+const UNSUPPORTED_IOS_VERSION_SAFARI_UA =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1';
 
 // Source (Adapted): https://chromium.googlesource.com/chromium/src/+/master/docs/ios/user_agent.md
@@ -43,15 +47,16 @@ const DESKTOP_SAFARI_UA =
 
 const SUPPORTED_USER_AGENTS = [
   REDUCED_UNSUPPORTED_ANDROID_VERSION_UA,
-  SUPPORTED_ANDROID_VERSION_UA,
+  SUPPORTED_ANDROID_VERSION_CHROME_UA,
   SUPPORTED_IOS_CHROME_VERSION_UA,
   SUPPORTED_IOS_SAFARI_VERSION_UA,
 ];
 
 const UNSUPPORTED_USER_AGENTS = [
   UNSUPPORTED_ANDROID_VERSION_UA,
-  UNSUPPORTED_IOS_CHROME_VERSION_UA,
-  UNSUPPORTED_IOS_SAFARI_VERSION_UA,
+  SUPPORTED_ANDROID_VERSION_FIREFOX_UA,
+  UNSUPPORTED_IOS_VERSION_CHROME_UA,
+  UNSUPPORTED_IOS_VERSION_SAFARI_UA,
   FIREFOX_UA,
   DESKTOP_CHROME_UA,
   DESKTOP_SAFARI_UA,
@@ -103,7 +108,7 @@ describe('isWebauthnPlatformSupported', () => {
     beforeEach(() => {
       defineProperty(navigator, 'userAgent', {
         configurable: true,
-        value: SUPPORTED_ANDROID_VERSION_UA,
+        value: SUPPORTED_ANDROID_VERSION_CHROME_UA,
       });
     });
 
