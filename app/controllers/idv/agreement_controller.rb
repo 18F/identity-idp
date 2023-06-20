41929c3051ef0dba031ae0cd3c307f45865064f1
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-20 16:01:15 -0700

    LG-10107 welcome controller (#8617)
    * Add doc_auth_welcome_controller_enabled feature flag
    
    * Initial Welcome controller, spec, view, routes
    
    changelog: Internal, Identity Verification refactor, new Welcome Controller (behind feature flag)
    
    * Initial feature spec, go to idv_welcome_url from idv_controller
    
    * Update return to sp failure to proof url in welcome view
    
    * View spec
    
    * Add spec from services welcome_step_spec
    
    * add creating document capture session

--- app/controllers/idv/agreement_controller.rb
+++ app/controllers/idv/agreement_controller.rb
@@ -61,8 +61,13 @@ module Idv
 
     def confirm_welcome_step_complete
       return if flow_session['Idv::Steps::WelcomeStep']
+      return if idv_session.welcome_visited
 
-      redirect_to idv_doc_auth_url
+      if IdentityConfig.store.doc_auth_welcome_controller_enabled
+        redirect_to idv_welcome_url
+      else
+        redirect_to idv_doc_auth_url
+      end
     end
 
     def confirm_agreement_needed
