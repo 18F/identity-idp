8f3be1cd4cc20298b8a54fdb4d1e88961ecccd28
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-22 11:16:04 -0700

    Remove OutageConcern and before action from post-FSM controllers
    since it is now included in IdvStepConcern
    Also remove IdvSession and before_action :confirm_two_factor_authenticated which are in IdvStepConcern

--- app/controllers/idv/in_person/verify_info_controller.rb
+++ app/controllers/idv/in_person/verify_info_controller.rb
@@ -6,11 +6,9 @@ module Idv
       include StepUtilitiesConcern
       include Steps::ThreatMetrixStepHelper
       include VerifyInfoConcern
-      include OutageConcern
 
       before_action :confirm_ssn_step_complete
       before_action :confirm_verify_info_step_needed
-      before_action :check_for_outage, only: :show
 
       def show
         @step_indicator_steps = step_indicator_steps
