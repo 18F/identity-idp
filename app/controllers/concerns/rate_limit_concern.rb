8c0f57b19aef45c9f3ee2209f09d8da21d4050a3
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-06-22 11:16:17 -0400

    revert rate limit concern refactor

--- app/controllers/concerns/rate_limit_concern.rb
+++ app/controllers/concerns/rate_limit_concern.rb
@@ -1,29 +1,17 @@
 module RateLimitConcern
   extend ActiveSupport::Concern
 
-  def confirm_not_rate_limited # confusing returns false if not rate_limited
+  def confirm_not_rate_limited
     rate_limited = false
+    %i[idv_resolution idv_doc_auth proof_address].each do |throttle_type|
+      next if throttle_and_controller_match(throttle_type) && action_name == 'update'
 
-    throttle_types.each do |throttle_type|
       if rate_limit_redirect!(throttle_type)
         rate_limited = true
         break
       end
     end
     rate_limited
-  end
-
-  def throttle_types
-    case self.class
-    when Idv::PhoneController
-      [:proof_address]
-    when Idv::InPerson::VerifyInfoController, Idv::VerifyInfoController
-      [:idv_resolution]#, :proof_ssn]
-    when Idv::DocumentCaptureController
-      [:idv_doc_auth]
-    else
-      [:proof_address, :idv_resolution, :idv_doc_auth] #, :proof_ssn] default or for spec?
-    end
   end
 
   def rate_limit_redirect!(throttle_type)
@@ -49,7 +37,19 @@ module RateLimitConcern
     when :idv_doc_auth
       redirect_to idv_session_errors_throttled_url
     when :proof_address
-      redirect_to idv_phone_errors_failure_url# if self.class != Idv::PhoneController
+      redirect_to idv_phone_errors_failure_url if self.class != Idv::PhoneController
+    end
+  end
+
+  def throttle_and_controller_match(throttle_type)
+    case throttle_type
+    when :idv_resolution
+      self.instance_of?(Idv::VerifyInfoController) ||
+        self.instance_of?(Idv::InPerson::VerifyInfoController)
+    when :idv_doc_auth
+      self.instance_of?(Idv::DocumentCaptureController)
+    when :proof_address
+      self.instance_of?(Idv::PhoneController)
     end
   end
 
