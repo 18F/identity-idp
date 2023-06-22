81ca5b50744d69e167d828af3e063a0c69571406
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-22 16:14:26 -0700

    Update welcome path for new controller, add specs
    changelog: Internal, Outage handling, allow nil flow_session in mail only warning page

--- app/controllers/idv/gpo_only_warning_controller.rb
+++ app/controllers/idv/gpo_only_warning_controller.rb
@@ -7,11 +7,19 @@ module Idv
 
     def show
       user_session.fetch('idv/doc_auth', {})[:skip_vendor_outage] = true
-      render :show, locals: { current_sp:, exit_url: }
+      render :show, locals: { current_sp:, exit_url:, welcome_url: }
     end
 
     def exit_url
       current_sp&.return_to_sp_url || account_path
+    end
+
+    def welcome_url
+      if IdentityConfig.store.doc_auth_welcome_controller_enabled
+        idv_welcome_url
+      else
+        idv_doc_auth_step_path(step: :welcome)
+      end
     end
   end
 end
--- spec/controllers/idv/gpo_only_warning_controller_spec.rb
+++ spec/controllers/idv/gpo_only_warning_controller_spec.rb
@@ -36,4 +36,47 @@ RSpec.describe Idv::GpoOnlyWarningController do
       end
     end
   end
+
+  context 'page links' do
+    render_views
+    context 'exit url' do
+      let(:sp) { create(:service_provider, issuer: 'urn:gov:gsa:openidconnect:sp:test_cookie') }
+
+      context 'with current sp' do
+        it 'links to return_to_sp_url' do
+          allow(controller).to receive(:current_sp).and_return(sp)
+          get :show
+          expect(response.body).to include(sp.return_to_sp_url)
+        end
+      end
+
+      context 'without current sp' do
+        it 'links to account_path' do
+          allow(controller).to receive(:current_sp).and_return(nil)
+          get :show
+          expect(response.body).to include(account_path)
+        end
+      end
+    end
+
+    context 'welcome url' do
+      context 'welcome controller is enabled' do
+        it 'links to idv_welcome_url' do
+          allow(IdentityConfig.store).to receive(:doc_auth_welcome_controller_enabled).
+            and_return(true)
+          get :show
+          expect(response.body).to include(idv_welcome_url)
+        end
+      end
+
+      context 'welcome controller is not enabled' do
+        it 'links to FSM welcome step' do
+          allow(IdentityConfig.store).to receive(:doc_auth_welcome_controller_enabled).
+            and_return(false)
+          get :show
+          expect(response.body).to include(idv_doc_auth_step_path(step: :welcome))
+        end
+      end
+    end
+  end
 end
