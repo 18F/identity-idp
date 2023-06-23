e13b704877565727580df995e5d8e79c8b76de7d
Matt Hinz <matt.hinz@gsa.gov>
2023-06-23 16:30:22 -0700

    Tweak deflakify script to work better with git bisect

--- MH-deflakify.sh
+++ MH-deflakify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Usage: script RUNS COMMAND
+# Usage: script RUNS COMMAND ["string signifying a real failure"]
 # Execute COMMAND RUNS times, track outputs and success rates.
 
 set -euo pipefail
@@ -12,10 +12,12 @@ mkdir -p "$RUN_DIR"
 
 RUNS=$1; shift
 COMMAND=$1; shift
+STRING_THAT_MEANS_FAILURE=${1:-}; shift
 
 ITERATION=0
 SUCCESSES=0
 FAILURES=0
+REAL_FAILURES=0
 
 while (true)
 do
@@ -36,23 +38,33 @@ Started at:  $STARTED_AT" | tee "$OUT_FILE" "$ERR_FILE"
         echo "Result:      ✅ Success"
         SUCCESSES=$(($SUCCESSES + 1))
     else
-        echo "Result:      ❌ Failure"
         FAILURES=$(($FAILURES + 1))
 
-        cat "$ERR_FILE"
+        if grep "$STRING_THAT_MEANS_FAILURE" "$OUT_FILE" "$ERR_FILE" > /dev/null 2>&1; then
+            echo "Result:      ❌ Failure"
+            REAL_FAILURES=$(($REAL_FAILURES + 1))
+        else
+            echo "Result:      🤔 Failure, but ignoring"
+        fi
     fi
 
     echo "Finished at: $(date '+%Y-%m-%d %H:%M:%S')"
 
     SUCCESS_PCT=$((($SUCCESSES * 100) / $ITERATION))
     FAILURE_PCT=$((($FAILURES * 100) / $ITERATION))
+    REAL_FAILURE_PCT=$((($REAL_FAILURES * 100) / $ITERATION))
 
     echo ""
-    echo "Successes:    $SUCCESSES (${SUCCESS_PCT}%)"
-    echo "Failures:     $FAILURES (${FAILURE_PCT}%)"
+    echo "Successes:        $SUCCESSES (${SUCCESS_PCT}%)"
+    echo "Failures (soft):  $FAILURES (${FAILURE_PCT}%)"
+    echo "Failures (hard):  $REAL_FAILURES (${REAL_FAILURE_PCT}%)"
     
     if [[ "$ITERATION" == "$RUNS" ]]; then
-        exit $FAILURES
+        if [[ "$REAL_FAILURES" -gt 0 ]]; then
+            exit 1 # Tell git bisect something broke
+        elif [[ "$FAILURES" -g 0 ]]; then
+            exit 125 # Tell git bisect we couldn't figure it out
+        fi
     fi
 
 done    
