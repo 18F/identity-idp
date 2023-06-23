1dbdde866f73744a8a5aeb4d8072a0616e75fd27
Kimball Bighorse <kbighorse@yahoo.com>
2023-06-23 16:31:42 -0700

    Test around activate after IPP

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -773,9 +773,25 @@ RSpec.describe Profile do
 
       allow(profile).to receive(:update!).and_raise(RuntimeError)
 
+      expect(profile.activated_at).to be_nil
+      expect(profile.active).to eq(false)
+      expect(profile.deactivation_reason).to eq('in_person_verification_pending')
+      expect(profile.fraud_review_pending?).to eq(true)
+      expect(profile.gpo_verification_pending_at).to be_nil
+      expect(profile.initiating_service_provider).to be_nil
+      expect(profile.verified_at).to be_nil
+
       suppress(RuntimeError) do
         profile.activate_after_passing_in_person
       end
+
+      expect(profile.activated_at).to be_nil
+      expect(profile.active).to eq(false)
+      expect(profile.deactivation_reason).to eq('in_person_verification_pending')
+      expect(profile.fraud_review_pending?).to eq(true)
+      expect(profile.gpo_verification_pending_at).to be_nil
+      expect(profile.initiating_service_provider).to be_nil
+      expect(profile.verified_at).to be_nil
 
       expect(profile.deactivation_reason).to eq('in_person_verification_pending')
       expect(profile).to_not be_active
