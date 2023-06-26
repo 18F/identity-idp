bc011a412647b1af241e2403d5eeb5d7727e510c
Osman Latif <olatif@flexion.us>
2023-06-26 10:42:29 -0500

    lint

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -2,7 +2,6 @@ cron_5m = '0/5 * * * *'
 cron_1h = '0 * * * *'
 cron_24h = '0 0 * * *'
 gpo_cron_24h = '0 10 * * *' # 10am UTC is 5am EST/6am EDT
-cron_1w = '0 0 * * 0'
 
 if defined?(Rails::Console)
   Rails.logger.info 'job_configurations: console detected, skipping schedule'
