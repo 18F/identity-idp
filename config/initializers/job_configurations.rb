1a78da841fe3a3f475b9c0669fba7c2046225b0a
Timothy Bradley <timothy.bradley@gsa.gov>
2023-06-28 16:16:20 -0700

    LG-9449: Implement and configure ArcGIS API token refresh job
    changelog: Internal, In-person proofing, Refresh ArcGIS token in job

--- config/initializers/job_configurations.rb
+++ config/initializers/job_configurations.rb
@@ -3,6 +3,7 @@ cron_1h = '0 * * * *'
 cron_24h = '0 0 * * *'
 gpo_cron_24h = '0 10 * * *' # 10am UTC is 5am EST/6am EDT
 cron_1w = '0 0 * * 0'
+cron_55m = '0/55 * * * *'
 
 if defined?(Rails::Console)
   Rails.logger.info 'job_configurations: console detected, skipping schedule'
@@ -169,7 +170,13 @@ else
         cron: cron_24h,
         args: -> { [Time.zone.yesterday] },
       },
-    }
+      arcgis_token: (if IdentityConfig.store.arcgis_api_refresh_token_job_enabled
+                       {
+                         class: 'ArcgisTokenJob',
+                         cron: cron_55m,
+                       }
+                     end),
+    }.compact
   end
   # rubocop:enable Metrics/BlockLength
 
