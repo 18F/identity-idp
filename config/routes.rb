70f65ad122ac96e958361399cdd9971bc913f436
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-30 14:15:59 -0700

    Remove DocAuthController, spec, and routes
    changelog: Internal, Identity verification, remove unsupervised proofing flow state machine controller

--- config/routes.rb
+++ config/routes.rb
@@ -373,10 +373,6 @@ Rails.application.routes.draw do
       delete '/cancel' => 'cancellations#destroy'
       get '/address' => 'address#new'
       post '/address' => 'address#update'
-      get '/doc_auth' => 'doc_auth#index'
-      get '/doc_auth/return_to_sp' => 'doc_auth#return_to_sp'
-      get '/doc_auth/:step' => 'doc_auth#show', as: :doc_auth_step
-      put '/doc_auth/:step' => 'doc_auth#update'
       get '/doc_auth/link_sent/poll' => 'capture_doc_status#show', as: :capture_doc_status
       get '/capture_doc' => 'hybrid_mobile/entry#show'
       get '/capture-doc' => 'hybrid_mobile/entry#show',
