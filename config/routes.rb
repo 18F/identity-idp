a12b3113a606b0c564151ab0d021627bb91af645
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-30 14:34:54 -0700

    Add back deprecated route without alias for 50/50 state

--- config/routes.rb
+++ config/routes.rb
@@ -404,6 +404,7 @@ Rails.application.routes.draw do
       # deprecated routes
       get '/confirmations' => 'personal_key#show'
       post '/confirmations' => 'personal_key#update'
+      get '/doc_auth/link_sent/poll' => 'capture_doc_status#show'
     end
 
     # Old paths to GPO outside of IdV.
