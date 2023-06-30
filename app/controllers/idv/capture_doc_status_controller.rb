556d4b09ad65f4dca3dde30f7b359743d272a9c4
Sonia Connolly <sonia.connolly@gsa.gov>
2023-06-30 15:55:07 -0700

    lint

--- app/controllers/idv/capture_doc_status_controller.rb
+++ app/controllers/idv/capture_doc_status_controller.rb
@@ -64,7 +64,10 @@ module Idv
     end
 
     def throttle
-      @throttle ||= RateLimit.new(user: document_capture_session.user, rate_limit_type: :idv_doc_auth)
+      @throttle ||= RateLimit.new(
+        user: document_capture_session.user,
+        rate_limit_type: :idv_doc_auth,
+      )
     end
 
     def user_has_establishing_in_person_enrollment?
