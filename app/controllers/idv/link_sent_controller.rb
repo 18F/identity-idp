573ea14140fe127bfabcbce0be511269e58c1be1
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-07-03 15:52:40 -0400

    temporarily define flow_session[:flow_path] to accomodate  50/50, remove in post-deploy PR

--- app/controllers/idv/link_sent_controller.rb
+++ app/controllers/idv/link_sent_controller.rb
@@ -71,11 +71,13 @@ module Idv
       save_proofing_components(current_user)
       extract_pii_from_doc(current_user, get_results_response, store_in_session: true)
       idv_session.flow_path = 'hybrid'
+      flow_session[:flow_path] = 'hybrid' # temp added for 50/50, remove in future deploy
     end
 
     def render_document_capture_cancelled
       redirect_to idv_hybrid_handoff_url
       idv_session.flow_path = nil
+      flow_session[:flow_path] = nil # temp added for 50/50, remove in future deploy
       failure(I18n.t('errors.doc_auth.document_capture_cancelled'))
     end
 
