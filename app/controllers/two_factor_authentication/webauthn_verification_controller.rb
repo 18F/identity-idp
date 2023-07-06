227277b3a6e5db8ee07f44faeca91b2c0e2781cc
kevinsmaster5 <kevin.masters@gsa.gov>
2023-07-06 16:52:17 -0400

    revert error method to remove unneeded redirect

--- app/controllers/two_factor_authentication/webauthn_verification_controller.rb
+++ app/controllers/two_factor_authentication/webauthn_verification_controller.rb
@@ -27,16 +27,7 @@ module TwoFactorAuthentication
       handle_webauthn_result(result)
     end
 
-    def error
-      flash[:error] = t(
-        'two_factor_authentication.webauthn_error.multiple_methods',
-        link: view_context.link_to(
-          t('two_factor_authentication.webauthn_error.additional_methods_link'),
-          login_two_factor_options_path,
-        ),
-      )
-      redirect_to login_two_factor_webauthn_url(platform: params[:platform])
-    end
+    def error; end
 
     private
 
