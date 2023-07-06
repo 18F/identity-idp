f0c772b79f24064d72ec59378066e7d354ffa1c5
kevinsmaster5 <kevin.masters@gsa.gov>
2023-07-06 15:23:55 -0400

    fix lint errors

--- app/controllers/two_factor_authentication/webauthn_verification_controller.rb
+++ app/controllers/two_factor_authentication/webauthn_verification_controller.rb
@@ -29,12 +29,12 @@ module TwoFactorAuthentication
 
     def error
       flash[:error] = t(
-          'two_factor_authentication.webauthn_error.multiple_methods',
-          link: view_context.link_to(
-            t('two_factor_authentication.webauthn_error.additional_methods_link'),
-            login_two_factor_options_path,
-          ),
-        )
+        'two_factor_authentication.webauthn_error.multiple_methods',
+        link: view_context.link_to(
+          t('two_factor_authentication.webauthn_error.additional_methods_link'),
+          login_two_factor_options_path,
+        ),
+      )
       redirect_to login_two_factor_webauthn_url(platform: params[:platform])
     end
 
