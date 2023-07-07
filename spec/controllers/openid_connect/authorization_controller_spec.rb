a67b22a063f72c4fc5562e7472520d1683ce0f86
Alex Bradley <alexander.bradley@gsa.gov>
2023-07-07 16:36:57 -0400

    add tests for pending reasons in oidc auth controller

--- spec/controllers/openid_connect/authorization_controller_spec.rb
+++ spec/controllers/openid_connect/authorization_controller_spec.rb
@@ -152,21 +152,73 @@ RSpec.describe OpenidConnect::AuthorizationController do
               expect(controller).to redirect_to(idv_url)
             end
 
-            context 'user is under fraud review' do
-              let(:user) { create(:profile, :fraud_review_pending).user }
+            context 'user has a pending profile' do
+              context 'user has a gpo pending profile' do
+                let(:user) { create(:profile, :verify_by_mail_pending).user }
 
-              it 'redirects to fraud review page if fraud review is pending' do
-                action
-                expect(controller).to redirect_to(idv_please_call_url)
+                it 'redirects to gpo verify page' do
+                  action
+                  expect(controller).to redirect_to(idv_gpo_verify_url)
+                end
               end
-            end
 
-            context 'user is rejected due to fraud' do
-              let(:user) { create(:profile, :fraud_rejection).user }
+              context 'user has an in person pending profile' do
+                let(:user) { create(:profile, :in_person_verification_pending).user }
 
-              it 'redirects to fraud rejection page if user is fraud rejected ' do
-                action
-                expect(controller).to redirect_to(idv_not_verified_url)
+                it 'redirects to gpo verify page' do
+                  action
+                  expect(controller).to redirect_to(idv_in_person_ready_to_verify_url)
+                end
+              end
+
+              context 'user is under fraud review' do
+                let(:user) { create(:profile, :fraud_review_pending).user }
+
+                it 'redirects to fraud review page if fraud review is pending' do
+                  action
+                  expect(controller).to redirect_to(idv_please_call_url)
+                end
+              end
+
+              context 'user is rejected due to fraud' do
+                let(:user) { create(:profile, :fraud_rejection).user }
+
+                it 'redirects to fraud rejection page if user is fraud rejected ' do
+                  action
+                  expect(controller).to redirect_to(idv_not_verified_url)
+                end
+              end
+
+              context 'user has two pending reasons' do
+                context 'user has gpo and fraud review pending' do
+                  let(:user) do
+                    create(
+                      :profile,
+                      :verify_by_mail_pending,
+                      :fraud_review_pending,
+                    ).user
+                  end
+
+                  it 'redirects to gpo verify page if user has gpo and fraud pending' do
+                    action
+                    expect(controller).to redirect_to(idv_gpo_verify_url)
+                  end
+                end
+
+                context 'user has gpo and in person pending' do
+                  let(:user) do
+                    create(
+                      :profile,
+                      :verify_by_mail_pending,
+                      :in_person_verification_pending,
+                    ).user
+                  end
+
+                  it 'redirects to gpo verify page if user has gpo and in person pending' do
+                    action
+                    expect(controller).to redirect_to(idv_gpo_verify_url)
+                  end
+                end
               end
             end
           end
