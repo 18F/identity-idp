c9d0f6d237ce4c01f9cc5671c1760dd7d9b89a5d
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-07-12 12:28:44 -0400

    display alert for failed nubmers

--- app/javascript/packs/idv-phone-alert.ts
+++ app/javascript/packs/idv-phone-alert.ts
@@ -1,9 +1,9 @@
 const alertElement = document.getElementById('phone-already-submitted-alert')!;
 const input = document.getElementById('idv_phone_form_phone') as HTMLInputElement;
-const alreadySubmittedNumbers: string[] = JSON.parse(alertElement.dataset.alreadySubmittedNumbers!);
+const failedPhoneNumbers: string[] = JSON.parse(alertElement.dataset.failedPhoneNumbers!);
 const iti = window.intlTelInputGlobals.getInstance(input);
 
 input.addEventListener('input', () => {
-    const isAlreadySubmittedNumber = alreadySubmittedNumbers.includes(iti.getNumber(intlTelInputUtils.numberFormat.E164));
-    alertElement.hidden = !isAlreadySubmittedNumber;
+    const isFailedPhoneNumber = failedPhoneNumbers.includes(iti.getNumber(intlTelInputUtils.numberFormat.E164));
+    alertElement.hidden = !isFailedPhoneNumber;
 });
