f8ea5d1b033d1ec51ff737a65211ad4efb8bdfab
Andrew Duthie <andrew.duthie@gsa.gov>
2023-07-18 16:52:40 -0400

    Revert "Add temporary redirect for WebAuthn error page (#8773)" (#8776)
    This reverts commit a9c30965a982f9ef372d31878c5b611232fb3624.

--- config/routes.rb
+++ config/routes.rb
@@ -115,7 +115,6 @@ Rails.application.routes.draw do
       get '/login/two_factor/piv_cac' => 'two_factor_authentication/piv_cac_verification#show'
       get '/login/two_factor/piv_cac/present_piv_cac' => 'two_factor_authentication/piv_cac_verification#redirect_to_piv_cac_service'
       get '/login/two_factor/webauthn' => 'two_factor_authentication/webauthn_verification#show'
-      get '/login/two_factor/webauthn_error', to: redirect('/login/two_factor/webauthn')
       patch '/login/two_factor/webauthn' => 'two_factor_authentication/webauthn_verification#confirm'
       get 'login/two_factor/backup_code' => 'two_factor_authentication/backup_code_verification#show'
       post 'login/two_factor/backup_code' => 'two_factor_authentication/backup_code_verification#create'
