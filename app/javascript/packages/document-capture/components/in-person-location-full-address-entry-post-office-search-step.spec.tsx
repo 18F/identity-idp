72ab6d588d44e48c2af6864fc628aae4a9415b3c
Sheldon Bachstein <sheldon.bachstein@gsa.gov>
2023-07-19 13:01:31 -0400

    Remove unnecessary code

--- app/javascript/packages/document-capture/components/in-person-location-full-address-entry-post-office-search-step.spec.tsx
+++ app/javascript/packages/document-capture/components/in-person-location-full-address-entry-post-office-search-step.spec.tsx
@@ -1,6 +1,4 @@
 import { render } from '@testing-library/react';
-import { setupServer } from 'msw/node';
-import type { SetupServer } from 'msw/node';
 import { SWRConfig } from 'swr';
 import { ComponentType } from 'react';
 import InPersonLocationFullAddressEntryPostOfficeSearchStep from './in-person-location-full-address-entry-post-office-search-step';
@@ -16,21 +14,6 @@ describe('InPersonLocationFullAddressEntryPostOfficeSearchStep', () => {
   const wrapper: ComponentType = ({ children }) => (
     <SWRConfig value={{ provider: () => new Map() }}>{children}</SWRConfig>
   );
-
-  let server: SetupServer;
-
-  before(() => {
-    server = setupServer();
-    server.listen();
-  });
-
-  after(() => {
-    server.close();
-  });
-
-  beforeEach(() => {
-    server.resetHandlers();
-  });
 
   it('renders the step', () => {
     const { getByRole } = render(
