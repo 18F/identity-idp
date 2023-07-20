9c09ee7cedd3476adb3a6ade4705e26f6f456cf0
Zach Margolis <zachary.margolis@gsa.gov>
2023-07-20 10:37:23 -0700

    Add EmailNormalizer (LG-10313)
    - Helper class we can use to block associated emails
    
    changelog: Internal, User suspension, Add code to help normalize emails

---
+++ spec/services/email_normalizer_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe EmailNormalizer do
+  subject(:normalizer) { EmailNormalizer.new(email) }
+
+  describe '#normalized_email' do
+    subject(:normalized_email) { normalizer.normalized_email }
+
+    context 'with a non-gmail domain' do
+      let(:email) { 'foobar+123@example.com' }
+
+      it 'is the same email' do
+        expect(normalized_email).to eq(email)
+      end
+    end
+
+    context 'with a gmail domain' do
+      let(:email) { 'foo.bar+123@gmail.com' }
+
+      it 'removes . and anything after the +' do
+        expect(normalized_email).to eq('foobar@gmail.com')
+      end
+    end
+
+    context 'with a Google Apps domain' do
+      let(:email) { 'foo.bar.baz+123@example.com' }
+
+      before do
+        allow(normalizer).to receive(:mx_records).with('example.com').
+          and_return(%w[abcd.mail.google.com])
+      end
+
+      it 'still removes . and anything after the +' do
+        expect(normalized_email).to eq('foobarbaz@example.com')
+      end
+    end
+  end
+end
