82bc9c7ae9c56f37b0a3c280f4d019b33aff96f7
Timothy Bradley <timothy.bradley@gsa.gov>
2023-07-20 11:01:57 -0700

    LG-10405: Fix typo and type issue

--- app/javascript/packages/document-capture/context/in-person.ts
+++ app/javascript/packages/document-capture/context/in-person.ts
@@ -19,18 +19,18 @@ export interface InPersonContextProps {
   /**
    * When true users must enter a full address when searching for a Post Office location
    */
-  inPersonFullAddressEntryEnabled: boolean;
+  inPersonFullAddressEntryEnabled?: boolean;
 
   /**
    * Collection of US states and territories
    * Each item is [Long name, abbreviation], e.g. ['Ohio', 'OH']
    */
-  usStatesTerritiories: Array<[string, string]>;
+  usStatesTerritories: Array<[string, string]>;
 }
 
 const InPersonContext = createContext<InPersonContextProps>({
   inPersonOutageMessageEnabled: false,
-  usStatesTerritiories: [],
+  usStatesTerritories: [],
 });
 
 InPersonContext.displayName = 'InPersonContext';
