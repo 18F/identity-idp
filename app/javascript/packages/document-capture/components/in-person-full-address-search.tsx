b6033ed65aa3c8ffcbca63722b5c1c8feff5e285
Tomas Apodaca <Thomas.Apodaca@gsa.gov>
2023-07-20 19:58:19 -0700

    give options keys

--- app/javascript/packages/document-capture/components/in-person-full-address-search.tsx
+++ app/javascript/packages/document-capture/components/in-person-full-address-search.tsx
@@ -199,11 +199,11 @@ function FullAddressSearch({
           label={t('in_person_proofing.body.location.po_search.state_label')}
           disabled={disabled}
         >
-          <option value="" disabled>
+          <option key="select" value="" disabled>
             {t('in_person_proofing.form.address.state_prompt')}
           </option>
           {usStatesTerritories.map(([name, abbr]) => (
-            <option value={abbr}>{name}</option>
+            <option key={abbr} value={abbr}>{name}</option>
           ))}
         </SelectInput>
       </ValidatedField>
