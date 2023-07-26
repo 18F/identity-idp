9751a1c2c33cd95e11774f7c41c28d77ac0702e7
Sonia Connolly <sonia.connolly@gsa.gov>
2023-07-26 14:15:50 -0700

    Rename telephony ThrottledError -> RateLimitedError

--- lib/telephony/errors.rb
+++ lib/telephony/errors.rb
@@ -68,7 +68,7 @@ module Telephony
     end
   end
 
-  class ThrottledError < TelephonyError
+  class RateLimitedError < TelephonyError
     def friendly_error_message_key
       # i18n-tasks-use t('telephony.error.friendly_message.rate_limited')
       'telephony.error.friendly_message.rate_limited'
