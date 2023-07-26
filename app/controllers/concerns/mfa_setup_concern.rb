ce7ac4fe3b0b4086ae05699de9afa3db39dafb10
kevinsmaster5 <kevin.masters@gsa.gov>
2023-07-26 15:58:39 -0400

    put confirmation step back into workflow

--- app/controllers/concerns/mfa_setup_concern.rb
+++ app/controllers/concerns/mfa_setup_concern.rb
@@ -3,7 +3,8 @@ module MfaSetupConcern
 
   def next_setup_path
     if suggest_second_mfa?
-      second_mfa_setup_url
+      puts('send to conf page')
+      auth_method_confirmation_url
     elsif next_setup_choice
       confirmation_path
     else
