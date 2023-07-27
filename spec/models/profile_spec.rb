c1c39233c54d0d148f8f99170a471392a57bf092
Kimball Bighorse <kbighorse@yahoo.com>
2023-07-27 11:22:12 -0700

    Test `Profile#in_person_verification_pending?`

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -86,6 +86,26 @@ RSpec.describe Profile do
     end
   end
 
+  describe '#in_person_verification_pending?' do
+    it 'returns true if the deactivation_reason is in_person_verification_pending' do
+      profile = create(
+        :profile,
+        :in_person_verification_pending,
+        user: user,
+      )
+
+      allow(profile).to receive(:update!).and_raise(RuntimeError)
+
+      expect(profile.activated_at).to be_nil
+      expect(profile.active).to eq(false)
+      expect(profile.deactivation_reason).to eq('in_person_verification_pending')
+      expect(profile.fraud_review_pending?).to eq(false)
+      expect(profile.gpo_verification_pending_at).to be_nil
+      expect(profile.initiating_service_provider).to be_nil
+      expect(profile.verified_at).to be_nil
+    end
+  end
+
   describe '#encrypt_pii' do
     subject(:encrypt_pii) { profile.encrypt_pii(pii, user.password) }
 
