dd9d4cf79d03c8e9e632319dcbc72e881ba71697
Andrew Duthie <andrew.duthie@gsa.gov>
2023-07-27 08:14:30 -0400

    Render non-passkey-restricted WebAuthn input as visible (#8830)
    * Render non-passkey-restricted WebAuthn input as visible
    
    changelog: User-Facing Enhancements, MFA Methods, Avoid flickering layout for WebAuthn options on authenticator selection
    
    * Fix webauthn_option_hidden spec helper
    
    changelog: User-Facing Improvements, MFA Methods, Avoid flickering layout for WebAuthn options on authenticator selection

--- app/javascript/packages/webauthn/webauthn-input-element.spec.ts
+++ app/javascript/packages/webauthn/webauthn-input-element.spec.ts
@@ -17,8 +17,38 @@ describe('WebauthnInputElement', () => {
     quibble.reset();
   });
 
-  context('input for non-platform authenticator', () => {
+  context('device does not support passkey', () => {
+    context('unsupported passkey not shown', () => {
+      beforeEach(() => {
+        isWebauthnPasskeySupported.returns(false);
+        document.body.innerHTML = `<lg-webauthn-input hidden></lg-webauthn-input>`;
+      });
+
+      it('stays hidden', () => {
+        const element = document.querySelector('lg-webauthn-input')!;
+
+        expect(element.hidden).to.be.true();
+      });
+    });
+
+    context('unsupported passkey shown', () => {
+      beforeEach(() => {
+        isWebauthnPasskeySupported.returns(false);
+        document.body.innerHTML = `<lg-webauthn-input show-unsupported-passkey hidden></lg-webauthn-input>`;
+      });
+
+      it('becomes visible, with modifier class', () => {
+        const element = document.querySelector('lg-webauthn-input')!;
+
+        expect(element.hidden).to.be.false();
+        expect(element.classList.contains('webauthn-input--unsupported-passkey')).to.be.true();
+      });
+    });
+  });
+
+  context('device supports passkey', () => {
     beforeEach(() => {
+      isWebauthnPasskeySupported.returns(true);
       document.body.innerHTML = `<lg-webauthn-input hidden></lg-webauthn-input>`;
     });
 
@@ -26,64 +56,6 @@ describe('WebauthnInputElement', () => {
       const element = document.querySelector('lg-webauthn-input')!;
 
       expect(element.hidden).to.be.false();
-    });
-  });
-
-  context('input for platform authenticator', () => {
-    context('no passkey only restriction', () => {
-      beforeEach(() => {
-        document.body.innerHTML = `<lg-webauthn-input platform hidden></lg-webauthn-input>`;
-      });
-
-      it('becomes visible', () => {
-        const element = document.querySelector('lg-webauthn-input')!;
-
-        expect(element.hidden).to.be.false();
-      });
-    });
-
-    context('passkey supported only', () => {
-      context('device does not support passkey', () => {
-        context('unsupported passkey not shown', () => {
-          beforeEach(() => {
-            isWebauthnPasskeySupported.returns(false);
-            document.body.innerHTML = `<lg-webauthn-input platform passkey-supported-only hidden></lg-webauthn-input>`;
-          });
-
-          it('stays hidden', () => {
-            const element = document.querySelector('lg-webauthn-input')!;
-
-            expect(element.hidden).to.be.true();
-          });
-        });
-
-        context('unsupported passkey shown', () => {
-          beforeEach(() => {
-            isWebauthnPasskeySupported.returns(false);
-            document.body.innerHTML = `<lg-webauthn-input platform passkey-supported-only show-unsupported-passkey hidden></lg-webauthn-input>`;
-          });
-
-          it('becomes visible, with modifier class', () => {
-            const element = document.querySelector('lg-webauthn-input')!;
-
-            expect(element.hidden).to.be.false();
-            expect(element.classList.contains('webauthn-input--unsupported-passkey')).to.be.true();
-          });
-        });
-      });
-
-      context('device supports passkey', () => {
-        beforeEach(() => {
-          isWebauthnPasskeySupported.returns(true);
-          document.body.innerHTML = `<lg-webauthn-input platform passkey-supported-only hidden></lg-webauthn-input>`;
-        });
-
-        it('becomes visible', () => {
-          const element = document.querySelector('lg-webauthn-input')!;
-
-          expect(element.hidden).to.be.false();
-        });
-      });
     });
   });
 });
