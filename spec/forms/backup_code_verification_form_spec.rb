28e055155b6bf84ec014290ca654d68a6eee135a
Jack Cody <jack.cody@gsa.gov>
2023-07-27 18:04:52 -0500

    Add additional tests

--- spec/forms/backup_code_verification_form_spec.rb
+++ spec/forms/backup_code_verification_form_spec.rb
@@ -11,14 +11,26 @@ RSpec.describe BackupCodeVerificationForm do
       backup_code: code,
     }
   end
-  let(:result) { form.submit(params) }
+  subject(:result) { form.submit(params) }
 
   describe '#submit' do
     context 'with a valid backup code' do
       let(:code) { backup_codes.first }
+      let(:backup_code_config) do
+        BackupCodeConfiguration.find_with_code(code: code, user_id: user.id)
+      end
 
       it 'returns succcess' do
         expect(result.success?).to eq(true)
+        expect(result.extra[:multi_factor_auth_method]).to eq('backup_code')
+        expect(result.extra[:multi_factor_auth_method_created_at]).
+          to eq(backup_code_config.created_at)
+      end
+
+      it 'marks code as used' do
+        subject
+
+        expect(backup_code_config.reload.used_at).not_to eq(nil)
       end
     end
 
