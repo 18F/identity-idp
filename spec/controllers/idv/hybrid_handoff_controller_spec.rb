5c5e3d26ade198a8ab6c7771d724f8d5b9718bc7
Matt Hinz <matt.hinz@gsa.gov>
2023-07-31 14:34:28 -0700

    Add test for use of doc cap session uuid to HybridHandoffController

--- spec/controllers/idv/hybrid_handoff_controller_spec.rb
+++ spec/controllers/idv/hybrid_handoff_controller_spec.rb
@@ -199,6 +199,24 @@ RSpec.describe Idv::HybridHandoffController do
         expect(subject.idv_session.phone_for_mobile_flow).to eq('+1 202-555-5555')
         expect(@analytics).to have_logged_event(analytics_name, analytics_args)
       end
+
+      context 'document_capture_session_uuid in flow_session' do
+        let(:document_capture_session_uuid) { '09228b6d-dd39-4925-bf82-b69104095517' }
+
+        before do
+          subject.flow_session[:document_capture_session_uuid] = document_capture_session_uuid
+        end
+
+        it 'sends a doc auth link' do
+          expect(Telephony).to receive(:send_doc_auth_link).with(
+            hash_including(
+              link: a_string_including(document_capture_session_uuid),
+            ),
+          ).and_call_original
+
+          put :update, params: params
+        end
+      end
     end
 
     context 'desktop flow' do
