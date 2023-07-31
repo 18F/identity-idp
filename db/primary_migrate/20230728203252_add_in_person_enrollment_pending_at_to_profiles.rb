5f99b14f07fc36744491baf4a9682c6f92cccf05
Alex Bradley <alexander.bradley@gsa.gov>
2023-07-31 11:22:51 -0400

    remove in_person_enrollment_pending_at
    removed in_person_enrollment_pending_at as it is not needed. We can use
    user.establishing_in_person_enrollment to check for enrollments

--- db/primary_migrate/20230728203252_add_in_person_enrollment_pending_at_to_profiles.rb
+++
@@ -1,8 +0,0 @@
-class AddInPersonEnrollmentPendingAtToProfiles < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction!
-
-  def change
-    add_column :profiles, :in_person_enrollment_pending_at, :datetime
-    add_index :profiles, :in_person_enrollment_pending_at, algorithm: :concurrently
-  end
-end
