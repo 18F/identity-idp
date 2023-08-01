08a735431794c98c86c0fc3d73d1b19aa72d1aba
charleyf <charleyferguson@navapbc.com>
2023-08-01 09:54:49 -0400

    Add missing matchers

--- spec/javascript/packages/document-capture/components/acuant-capture-spec.jsx
+++ spec/javascript/packages/document-capture/components/acuant-capture-spec.jsx
@@ -635,6 +635,7 @@ describe('document-capture/components/acuant-capture', () => {
         width: 1748,
         attempt: sinon.match.number,
         size: sinon.match.number,
+        acuantCaptureType: sinon.match.string,
       });
 
       expect(error).to.be.ok();
@@ -690,6 +691,7 @@ describe('document-capture/components/acuant-capture', () => {
         width: 1748,
         attempt: sinon.match.number,
         size: sinon.match.number,
+        acuantCaptureType: sinon.match.string,
       });
 
       expect(error).to.be.ok();
@@ -798,6 +800,7 @@ describe('document-capture/components/acuant-capture', () => {
         width: 1748,
         attempt: sinon.match.number,
         size: sinon.match.number,
+        acuantCaptureType: sinon.match.string,
       });
     });
 
