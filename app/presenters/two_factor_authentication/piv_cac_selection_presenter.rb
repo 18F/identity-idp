5b22e0a26c62c1f3356252ec6c511761930a67ff
kevinsmaster5 <kevin.masters@gsa.gov>
2023-08-04 11:29:51 -0400

    revert presenter

--- app/presenters/two_factor_authentication/piv_cac_selection_presenter.rb
+++ app/presenters/two_factor_authentication/piv_cac_selection_presenter.rb
@@ -8,8 +8,19 @@ module TwoFactorAuthentication
       user&.piv_cac_configurations&.any?
     end
 
+    def mfa_configuration_description
+      return '' if !disabled?
+      t(
+        'two_factor_authentication.two_factor_choice_options.no_count_configuration_added',
+      )
+    end
+
+    def mfa_added_label
+      ''
+    end
+
     def mfa_configuration_count
-      user&.piv_cac_configurations&.count?
+      user.piv_cac_configurations.count
     end
   end
 end
