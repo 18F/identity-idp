35950fe6f93c8d976f5d160caf56abc295c3f5c3
Jack Cody <jack.cody@gsa.gov>
2023-08-08 16:22:30 -0500

    Update WebAuthnHelper

--- spec/support/features/webauthn_helper.rb
+++ spec/support/features/webauthn_helper.rb
@@ -47,6 +47,11 @@ module WebAuthnHelper
   end
 
   def mock_cancelled_webauthn_authentication
+    webauthn_general_error = t(
+      'two_factor_authentication.webauthn_error.connect_html',
+      link: t('two_factor_authentication.webauthn_error.additional_methods_link'),
+    )
+
     if javascript_enabled?
       page.evaluate_script(<<~JS)
         navigator.credentials.get = () => Promise.reject(new DOMException('', 'NotAllowedError'));
@@ -68,7 +73,7 @@ module WebAuthnHelper
           wait: 5,
         )
       else
-        expect(page).to have_content(t('errors.general'), wait: 5)
+        expect(page).to have_content(webauthn_general_error, wait: 5)
       end
     else
       yield
