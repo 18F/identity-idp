b72eada85a80983d9ed2ee9d976ce119333287d6
John Maxwell <john.maxwell@gsa.gov>
2023-08-10 16:40:47 -0400

    Specs for basic cases.

--- app/services/gpo_reminder_sender.rb
+++ app/services/gpo_reminder_sender.rb
@@ -1,5 +1,7 @@
 class GpoReminderSender
   def send_emails
-
+    Profile.where.not(gpo_verification_pending_at: nil).each do |profile|
+      profile.user.send_email_to_all_addresses(:gpo_reminder)
+    end
   end
 end
--- spec/services/gpo_reminder_sender_spec.rb
+++ spec/services/gpo_reminder_sender_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe GpoReminderSender do
   describe '#send_emails' do
     let(:user) { create(:user, :with_pending_gpo_profile) }
 
+    def set_gpo_verification_pending_at(to_time)
+      user.gpo_verification_pending_profile.update(gpo_verification_pending_at: to_time)
+    end
+
+    def set_reminder_sent_at(to_time)
+      gpo_confirmation_code = user.gpo_verification_pending_profile.gpo_confirmation_codes.first
+      gpo_confirmation_code.reminder_sent_at = Time.zone.now - 1.day
+      gpo_confirmation_code.save
+    end
+
     context 'when no users need a reminder' do
-      before do
-        user.gpo_verification_pending_profile.gpo_verification_pending_at = Time.zone.now - 13.days
-        user.save
-      end
+      before { set_gpo_verification_pending_at(Time.zone.now - 13.days) }
 
       it 'sends no emails' do
         expect{subject.send_emails}.to change{ActionMailer::Base.deliveries.size}.by(0)
@@ -16,19 +23,14 @@ RSpec.describe GpoReminderSender do
     end
 
     context 'when a user is due for a reminder' do
-      before do
-        user.gpo_verification_pending_profile.gpo_verification_pending_at = Time.zone.now - 14.days
-        user.save
-      end
+      before { set_gpo_verification_pending_at(Time.zone.now - 14.days) }
 
       it 'sends that user an email' do
         expect{subject.send_emails}.to change{ActionMailer::Base.deliveries.size}.by(1)
       end
 
       context 'but a reminder has already been sent' do
-        before do
-          user.gpo_verification_pending_profile.gpo_reminder_sent_at = Time.zone.now - 1.day
-        end
+        before { set_reminder_sent_at(Time.zone.now - 1.day) }
 
         it 'does not send that user an email' do
           expect{subject.send_emails}.to change{ActionMailer::Base.deliveries.size}.by(0)
