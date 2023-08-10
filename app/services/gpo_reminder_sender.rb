db0d9938b15455f99ef90453227973bb85d1dba0
John Maxwell <john.maxwell@gsa.gov>
2023-08-10 17:21:20 -0400

    Add time range to profile select.

--- app/services/gpo_reminder_sender.rb
+++ app/services/gpo_reminder_sender.rb
@@ -1,6 +1,10 @@
 class GpoReminderSender
   def send_emails
-    Profile.where.not(gpo_verification_pending_at: nil).each do |profile|
+    profiles = Profile# joins(:usps_confirmation_codes)
+                 .where(gpo_verification_pending_at: ..(Time.zone.now - 14.days))
+                 # .where(reminder_sent_at: nil)
+
+    profiles.each do |profile|
       profile.user.send_email_to_all_addresses(:gpo_reminder)
     end
   end
