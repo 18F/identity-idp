58b0d61d7b242ded697a70b35a3d4002e2f73ac4
Tim Bradley <90272033+NavaTim@users.noreply.github.com>
2023-08-16 13:42:23 -0700

    LG-8886: Split percent logging for USPS proofing networking errors (#8993)
    * LG-8886: Split percent logging for USPS proofing networking errors
    
    * LG-8886: Include connection error; update logged field name
    
    * changelog: Internal, In-Person Proofing, Log network error stats separately for USPS proofing results job

--- app/jobs/get_usps_proofing_results_job.rb
+++ app/jobs/get_usps_proofing_results_job.rb
@@ -20,6 +20,7 @@ class GetUspsProofingResultsJob < ApplicationJob
     @enrollment_outcomes = {
       enrollments_checked: 0,
       enrollments_errored: 0,
+      enrollments_network_error: 0,
       enrollments_expired: 0,
       enrollments_failed: 0,
       enrollments_in_progress: 0,
@@ -45,7 +46,8 @@ class GetUspsProofingResultsJob < ApplicationJob
     analytics.idv_in_person_usps_proofing_results_job_completed(
       **enrollment_outcomes,
       duration_seconds: (Time.zone.now - started_at).seconds.round(2),
-      percent_enrollments_errored: percent_errored,
+      percent_enrollments_errored: summary_percent(:enrollments_errored),
+      percent_enrollments_network_error: summary_percent(:enrollments_network_error),
       job_name: self.class.name,
     )
 
@@ -112,11 +114,11 @@ class GetUspsProofingResultsJob < ApplicationJob
     Analytics.new(user: user, request: nil, session: {}, sp: nil)
   end
 
-  def percent_errored
+  def summary_percent(outcomes_key)
     error_rate = 0
     if enrollment_outcomes[:enrollments_checked] > 0
       error_rate =
-        (enrollment_outcomes[:enrollments_errored].fdiv(
+        (enrollment_outcomes[outcomes_key].fdiv(
           enrollment_outcomes[:enrollments_checked],
         ) * 100).round(2)
     end
@@ -159,7 +161,12 @@ class GetUspsProofingResultsJob < ApplicationJob
       response_status_code: err.response_status,
       job_name: self.class.name,
     )
-    enrollment_outcomes[:enrollments_errored] += 1
+
+    if err.is_a?(Faraday::TimeoutError) || err.is_a?(Faraday::ConnectionFailed)
+      enrollment_outcomes[:enrollments_network_error] += 1
+    else
+      enrollment_outcomes[:enrollments_errored] += 1
+    end
   end
 
   def handle_standard_error(err, enrollment)
