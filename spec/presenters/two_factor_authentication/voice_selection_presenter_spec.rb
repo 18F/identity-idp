a12a321f2a4f3597efd9085a529ce87e17d0eb45
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2023-08-17 10:17:22 -0400

    Kmas lg 9961 more mfa same type (#8863)
    * changelog: User-Facing Improvements, Authentication Methods, Implement ability to select more than one MFA of the same type
    
    * add list of selected mfa options to Authentication Method setup followup view
    
    * fix lint errors
    
    * revise tests involving redirect to two-factor-options page
    
    * remove added scss, moved styles to utility classes
    
    * put confirmation step back into workflow
    
    * fix lint error
    
    * fix spec 3/22
    
    * fix build errors
    
    * fix build errors
    
    * fix build errors
    
    * fix bug: adding new mfa from account page ended up on add second mfa page
    
    * fix lint
    
    * reset test
    
    * reset test
    
    * reset test
    
    * reset test
    
    * fix spec test to account for redirecting to add second mfa screen
    
    * review comment changes
    
    * review comment changes
    
    * review comment changes
    
    * move mfa_added logic to presenter and out of view files
    
    * pr review comments
    
    * revert account setup to original workflow
    
    * address review comments. fix html bug, refactor presenter logic
    
    * fix build error
    
    * add nil-safe operator
    
    * fix build error
    
    * fix build error
    
    * fix lint
    
    * fix build
    
    * fix lint
    
    * fix build
    
    * move null operator to method
    
    * fix build error
    
    * refactor backup code presenter
    
    * adds test cases
    
    * correct double equals operator
    
    * fix indentation
    
    * make test for disabled more robust
    
    * fix disabled test case
    
    * correct spec test logic
    
    * removes stubbed in nil state
    
    * face or touch unlock mfa only 1x available at signup
    
    * fix build error
    
    * fix build error
    
    * fix build
    
    * fix build
    
    * De-indent
    
    * Apply default disabled logic only to setup
    
    * Always pass user to selection presenter
    
    Avoid having to deal with nil user
    
    * Make user required argument
    
    * fix build error
    
    * fix build
    
    * fix lint
    
    * fix build errors
    
    * refactor for correct :user and :configuration variable
    
    * fix lint
    
    * streamline spec use of user keyword, add selection_presenter test
    
    ---------
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- spec/presenters/two_factor_authentication/voice_selection_presenter_spec.rb
+++ spec/presenters/two_factor_authentication/voice_selection_presenter_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TwoFactorAuthentication::VoiceSelectionPresenter do
-  let(:subject) { described_class.new(configuration: phone) }
-
+  let(:subject) { described_class.new(configuration: phone, user: user) }
+  let(:user) { build(:user) }
   describe '#type' do
     context 'when a user has only one phone configuration' do
       let(:user) { create(:user, :with_phone) }
@@ -30,7 +30,6 @@ RSpec.describe TwoFactorAuthentication::VoiceSelectionPresenter do
   describe '#info' do
     context 'when a user has a phone configuration' do
       let(:phone) { build(:phone_configuration, phone: '+1 888 867-5309') }
-
       it 'includes the masked the number' do
         expect(subject.info).to include('(***) ***-5309')
       end
@@ -39,7 +38,6 @@ RSpec.describe TwoFactorAuthentication::VoiceSelectionPresenter do
 
   describe '#disabled?' do
     let(:phone) { build(:phone_configuration, phone: '+1 888 867-5309') }
-
     it { expect(subject.disabled?).to eq(false) }
 
     context 'voice vendor outage' do
