f9b6979446a3a1446962a89def55af52d37450d0
Sonia Connolly <sonia.connolly@gsa.gov>
2023-08-18 11:07:25 -0700

    Allow idv_session to be undefined in ab_test_analytics_concern (hybrid flow)

--- spec/controllers/concerns/idv/ab_test_analytics_concern_spec.rb
+++ spec/controllers/concerns/idv/ab_test_analytics_concern_spec.rb
@@ -20,9 +20,6 @@ RSpec.describe 'AbTestAnalyticsConcern' do
     let(:getting_started_args) { { gs_bucket: :gs_value } }
 
     before do
-      sign_in(user)
-      expect(subject).to receive(:idv_session).and_return(idv_session)
-
       allow(subject).to receive(:current_user).and_return(user)
       expect(subject).to receive(:acuant_sdk_ab_test_analytics_args).
         and_return(acuant_sdk_args)
@@ -30,17 +27,29 @@ RSpec.describe 'AbTestAnalyticsConcern' do
         and_return(getting_started_args)
     end
 
-    it 'includes acuant_sdk_ab_test_analytics_args' do
-      expect(controller.ab_test_analytics_buckets).to include(acuant_sdk_args)
+    context 'idv_session is available' do
+      before do
+        sign_in(user)
+        expect(subject).to receive(:idv_session).and_return(idv_session)
+      end
+      it 'includes acuant_sdk_ab_test_analytics_args' do
+        expect(controller.ab_test_analytics_buckets).to include(acuant_sdk_args)
+      end
+
+      it 'includes getting_started_ab_test_analytics_bucket' do
+        expect(controller.ab_test_analytics_buckets).to include(getting_started_args)
+      end
+
+      it 'includes skip_hybrid_handoff' do
+        idv_session.skip_hybrid_handoff = :shh_value
+        expect(controller.ab_test_analytics_buckets).to include({ skip_hybrid_handoff: :shh_value })
+      end
     end
 
-    it 'includes getting_started_ab_test_analytics_bucket' do
-      expect(controller.ab_test_analytics_buckets).to include(getting_started_args)
-    end
-
-    it 'includes skip_hybrid_handoff' do
-      idv_session.skip_hybrid_handoff = :shh_value
-      expect(controller.ab_test_analytics_buckets).to include({ skip_hybrid_handoff: :shh_value })
+    context 'idv_session is not available' do
+      it 'still works' do
+        expect(controller.ab_test_analytics_buckets).to include(acuant_sdk_args)
+      end
     end
   end
 end
