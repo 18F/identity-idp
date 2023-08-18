8b533eb380e97c0ca4361bf479de42c1ed903410
Sonia Connolly <sonia.connolly@gsa.gov>
2023-08-18 10:26:18 -0700

    LG-10733 Third option for GettingStarted A/B test (#9033)
    * Add second template for Welcome controller for A/B test
    
    We got unexpected results for our A/B test for the GettingStarted page that replaces both the Welcome and
    Agreement steps. Add a third option that just replaces the Welcome template with one that has the new content
    for the GettingStarted page (except the Agreement content) and then sends the user to the Agreement step as
    usual.
    
    changelog: User-facing Improvements, Identity Verification Getting Started Experience, Additional Welcome template for three-way A/B test
    
    Co-authored-by: Jonathan Hooper <jonathan.hooper@gsa.gov>
    
    * Erb spec for Welcome template A/B test
    
    Co-authored-by John Maxwell <john.maxwell@gsa.gov>
    
    * Add controller spec for rendering the right partial
    
    * Update analytics_spec A/B test bucket :welcome -> :welcome_default
    
    Co-authored-by Amir Reavis-Bey <amir.reavis-bey@gsa.gov>

--- app/controllers/idv/welcome_controller.rb
+++ app/controllers/idv/welcome_controller.rb
@@ -13,6 +13,11 @@ module Idv
       Funnel::DocAuth::RegisterStep.new(current_user.id, sp_session[:issuer]).
         call('welcome', :view, true)
 
+      @sp_name = decorated_session.sp_name || APP_NAME
+      @title = t('doc_auth.headings.getting_started', sp_name: @sp_name)
+
+      @ab_test_bucket = getting_started_ab_test_bucket
+
       render :show
     end
 
