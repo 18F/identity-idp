c63b517fb9e62177c0f21e2728338a7ea0da1a6d
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-21 10:01:13 -0400

    Add specs for BIMI image validity

--- spec/requests/bimi_logo_spec.rb
+++ spec/requests/bimi_logo_spec.rb
@@ -10,4 +10,60 @@ RSpec.describe 'BIMI logo' do
 
     expect(response.status).to eq(200)
   end
+
+  describe 'validity' do
+    # Test cases in this block reference best practices documentation from BIMI group:
+    # See: https://bimigroup.org/creating-bimi-svg-logo-files/
+
+    let(:path) { Rails.public_path.join('images/login-icon-bimi.svg') }
+    subject(:image) { Nokogiri::XML(File.read(path)) }
+
+    it 'is no larger than 32kb' do
+      # "The SVG document should be as small as possible and should not exceed 32 kilobytes."
+      size_in_kilobytes = File.size(path).to_f / 1024
+
+      expect(size_in_kilobytes).to be <= 32
+    end
+
+    it 'has expected root attributes' do
+      # "When building your SVG there are a number of required elements in the structure of the
+      # file:"
+
+      # "The “baseProfile” attribute set to “tiny-ps”"
+      expect(image.css('svg[baseProfile="tiny-ps"]')).to be_present
+
+      # "The “version” attribute set to “1.2”"
+      expect(image.css('svg[version="1.2"]')).to be_present
+
+      # "A <title> element must be included that reflects the company name, though there are no
+      # strict requirements for the content of the element."
+      expect(image.css('title').text).to be_present
+    end
+
+    it 'does not include forbidden elements' do
+      # "The SVG document must not include any of the following in order to be valid under the
+      # tiny-ps designation:"
+
+      # "Any external links or references (other than to the specified XML namespaces)"
+      # expect(image.css('[xlink\\:href]')).to be_blank
+      expect(image.xpath('//*[@href]')).to be_blank
+
+      # "Any scripts, animation, or other interactive elements"
+      expect(image.css('animate')).to be_blank
+      expect(image.css('script')).to be_blank
+
+      # "“x=” or “y=” attributes within the <svg> root element"
+      expect(image.css('[x]')).to be_blank
+      expect(image.css('[y]')).to be_blank
+    end
+
+    it 'is square' do
+      # "The image should be a square aspect ratio"
+      root = image.css('svg')
+      width = root.attr('width').value
+      height = root.attr('height').value
+
+      expect(width).to eq(height)
+    end
+  end
 end
