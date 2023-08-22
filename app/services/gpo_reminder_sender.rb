2e7d1a18590b725171b92eeeaa6cb1a90e00b79d
John Maxwell <john.maxwell@gsa.gov>
2023-08-22 10:34:33 -0400

    Improved query for GPO reminder letters.
    co-authored by: Jonathan Hooper <jonathan.hooper@gsa.gov>
    Changelog: Bug Fixes,Verify by Mail,Fixed database query for reminder letters

--- app/services/gpo_reminder_sender.rb
+++ app/services/gpo_reminder_sender.rb
@@ -1,12 +1,16 @@
 class GpoReminderSender
   def send_emails(for_letters_sent_before)
+    letter_eligible_range =
+      IdentityConfig.store.usps_confirmation_max_days.days.ago..for_letters_sent_before
+
     profiles_due_for_reminder = Profile.joins(:gpo_confirmation_codes).
-      where(
-        gpo_verification_pending_at: ..for_letters_sent_before,
-        gpo_confirmation_codes: { reminder_sent_at: nil },
-      )
+                                  where(
+                                    gpo_verification_pending_at: letter_eligible_range,
+                                    gpo_confirmation_codes: { reminder_sent_at: nil },
+                                  )
 
     profiles_due_for_reminder.each do |profile|
+      next unless user.pending_profile == profile
       profile.user.send_email_to_all_addresses(:gpo_reminder)
       profile.gpo_confirmation_codes.first.update(reminder_sent_at: Time.zone.now)
       analytics.idv_gpo_reminder_email_sent(user_id: profile.user.uuid)
