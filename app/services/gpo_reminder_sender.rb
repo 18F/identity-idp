fd2ac5041abcf4239c5e1399bdc1f06aaea7f4de
John Maxwell <john.maxwell@gsa.gov>
2023-08-22 11:04:29 -0400

    Fixed to pass specs.

--- app/services/gpo_reminder_sender.rb
+++ app/services/gpo_reminder_sender.rb
@@ -4,13 +4,13 @@ class GpoReminderSender
       IdentityConfig.store.usps_confirmation_max_days.days.ago..for_letters_sent_before
 
     profiles_due_for_reminder = Profile.joins(:gpo_confirmation_codes).
-                                  where(
-                                    gpo_verification_pending_at: letter_eligible_range,
-                                    gpo_confirmation_codes: { reminder_sent_at: nil },
-                                  )
+      where(
+        gpo_verification_pending_at: letter_eligible_range,
+        gpo_confirmation_codes: { reminder_sent_at: nil },
+      )
 
     profiles_due_for_reminder.each do |profile|
-      next unless user.pending_profile == profile
+      next unless profile.user.pending_profile == profile
       profile.user.send_email_to_all_addresses(:gpo_reminder)
       profile.gpo_confirmation_codes.first.update(reminder_sent_at: Time.zone.now)
       analytics.idv_gpo_reminder_email_sent(user_id: profile.user.uuid)
