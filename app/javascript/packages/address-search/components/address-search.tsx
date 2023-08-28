d7c2b7654d9a2e62a3c4c7e167066a1f1407982f
Matt Gardner <wilburnforce@gmail.com>
2023-08-28 15:28:15 -0400

    Make prop optional

--- app/javascript/packages/address-search/components/address-search.tsx
+++ app/javascript/packages/address-search/components/address-search.tsx
@@ -35,7 +35,7 @@ function AddressSearch({
         onFoundAddress={setFoundAddress}
         onFoundLocations={(locations) => {
           setLocationResults(locations);
-          onFoundLocations(locations);
+          onFoundLocations && onFoundLocations(locations);
         }}
         onLoadingLocations={setLoadingLocations}
         onError={setApiError}
