0628821ceeceda062432d5349f1fa9a6cc11ccdc
Matt Gardner <wilburnforce@gmail.com>
2023-08-28 13:06:46 -0400

    changelog: Internal, In-Person Proofing, Move full single line AddressSearch into package

--- app/javascript/packages/address-search/index.tsx
+++ app/javascript/packages/address-search/index.tsx
@@ -1,7 +1,8 @@
 import { snakeCase, formatLocations, transformKeys } from './utils';
 import InPersonLocations from './components/in-person-locations';
 import AddressInput from './components/address-input';
+import AddressSearch from './components/address-search';
 
-export { snakeCase, formatLocations, transformKeys, InPersonLocations };
+export { snakeCase, formatLocations, transformKeys, InPersonLocations, AddressInput };
 
-export default AddressInput;
+export default AddressSearch;
