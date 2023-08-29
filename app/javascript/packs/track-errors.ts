914adefd16f40bbf75e945ddfd365c6015c46b32
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-29 15:22:07 -0400

    Filter events to same-host script errors

--- app/javascript/packs/track-errors.ts
+++ app/javascript/packs/track-errors.ts
@@ -1,4 +1,4 @@
-import { trackError } from '@18f/identity-analytics';
+import { trackError, isTrackableErrorEvent } from '@18f/identity-analytics';
 
 export interface WindowWithInitialErrors extends Window {
   _e: ErrorEvent[];
@@ -8,6 +8,7 @@ declare let window: WindowWithInitialErrors;
 
 const { _e: initialErrors } = window;
 
-const handleErrorEvent = (event: ErrorEvent) => trackError(event.error);
+const handleErrorEvent = (event: ErrorEvent) =>
+  isTrackableErrorEvent(event) && trackError(event.error);
 initialErrors.forEach(handleErrorEvent);
 window.addEventListener('error', handleErrorEvent);
