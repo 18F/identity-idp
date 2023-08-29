6470b0b46edcaf08a87bd6b8e72d67ce979263df
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-29 15:43:01 -0400

    Log Webpack script errors in development environment

--- app/javascript/packages/analytics/is-trackable-error-event.ts
+++ app/javascript/packages/analytics/is-trackable-error-event.ts
@@ -1,6 +1,10 @@
 function isTrackableErrorEvent(event: ErrorEvent): boolean {
   try {
-    return new URL(event.filename).host === window.location.host;
+    const { host, protocol } = new URL(event.filename);
+    return (
+      host === window.location.host ||
+      (process.env.NODE_ENV === 'development' && protocol === 'webpack-internal:')
+    );
   } catch {
     return false;
   }
