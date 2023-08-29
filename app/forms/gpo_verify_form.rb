3f24605b3d477d62c911b8554f34690abb6ea8b9
Kimball Bighorse <kbighorse@yahoo.com>
2023-08-28 15:04:10 -1000

    Use `User#has_in_person_enrollment?` in `GpoVerifyForm#submit`

--- app/forms/gpo_verify_form.rb
+++ app/forms/gpo_verify_form.rb
@@ -22,7 +22,7 @@ class GpoVerifyForm
     if result
       pending_profile&.remove_gpo_deactivation_reason
 
-      if pending_profile&.in_person_enrollment&.establishing?
+      if user.has_in_person_enrollment?
         schedule_in_person_enrollment_and_deactivate_profile
       elsif fraud_check_failed && threatmetrix_enabled?
         pending_profile&.deactivate_for_fraud_review
