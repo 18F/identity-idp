164cb8a1c63296843eaa67a912c23a4f7e150ae1
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-29 15:52:49 -0400

    Revert "Log Webpack script errors in development environment"
    This reverts commit bafdea8b1e5bdfcc1ef130fad73a5a4d11e546f9.

--- app/javascript/packages/analytics/is-trackable-error-event.ts
+++ app/javascript/packages/analytics/is-trackable-error-event.ts
@@ -1,10 +1,6 @@
 function isTrackableErrorEvent(event: ErrorEvent): boolean {
   try {
-    const { host, protocol } = new URL(event.filename);
-    return (
-      host === window.location.host ||
-      (process.env.NODE_ENV === 'development' && protocol === 'webpack-internal:')
-    );
+    return new URL(event.filename).host === window.location.host;
   } catch {
     return false;
   }
