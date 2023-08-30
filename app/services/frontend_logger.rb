ff8a00f830f91bb2bc6012dc61de5258d4b2684b
Zach Margolis <zachary.margolis@gsa.gov>
2023-08-30 15:26:39 -0700

    Slight simplification, support #call-able objects

--- app/services/frontend_logger.rb
+++ app/services/frontend_logger.rb
@@ -1,24 +1,26 @@
 class FrontendLogger
   attr_reader :analytics, :event_map
 
-  # @param [Analytics]
-  # @param [Hash{String=>UnboundMethod,Proc}]
+  # @param [Analytics] analytics
+  # @param [Hash{String=>Symbol,#call}] event_map map of string event names to method names
+  #   on Analytics, or a custom implementation that's callable (like a Proc or Method)
   def initialize(analytics:, event_map:)
     @analytics = analytics
     @event_map = event_map
   end
 
+  # Logs an event and converts the payload to the correct keyword args
   # @param [String] name
   # @param [Hash] attributes
   def track_event(name, attributes)
     analytics_method = event_map[name]
-    case analytics_method
-    when Symbol
+
+    if analytics_method.kind_of?(Symbol)
       analytics.send(
         analytics_method,
         **hash_from_method_kwargs(attributes, analytics.method(analytics_method)),
       )
-    when Method
+    elsif analytics_method.respond_to?(:call)
       analytics_method.call(**hash_from_method_kwargs(attributes, analytics_method))
     else
       analytics.track_event("Frontend: #{name}", attributes)
