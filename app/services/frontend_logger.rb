f2f1da4a5f7722b53d58f066ccfe58440d177129
Zach Margolis <zachary.margolis@gsa.gov>
2023-08-30 13:17:35 -0700

    WIP: experimenting with less special-casing of Analytics class

--- app/services/frontend_logger.rb
+++ app/services/frontend_logger.rb
@@ -1,25 +1,32 @@
 class FrontendLogger
-  attr_reader :analytics, :event_map
+  attr_reader :analytics, :error_logger, :event_map
 
-  # @param [Analytics]
-  # @param [Hash{String=>UnboundMethod,Proc}]
-  def initialize(analytics:, event_map:)
+  # @param [Analytics] analytics
+  # @param [FrontendErrorLogger] error_logger
+  # @param [Hash{String=>Symbol,Method}] event_map
+  def initialize(analytics:, error_logger:, event_map:)
     @analytics = analytics
+    @error_logger = error_logger
     @event_map = event_map
   end
 
   # @param [String] name
   # @param [Hash] attributes
   def track_event(name, attributes)
-    analytics_method = event_map[name]
-    case analytics_method
-    when Symbol
-      analytics.send(
+    target_class, analytics_method = event_map[name]
+
+    # case/when doesn't work because === on classes is an instance check, not identity
+    target = if target_class == Analytics
+      analytics
+    elsif target_class == FrontendErrorLogger
+      error_logger
+    end
+
+    if analytics_method
+      target.send(
         analytics_method,
-        **hash_from_method_kwargs(attributes, analytics.method(analytics_method)),
+        **hash_from_method_kwargs(attributes, target.method(analytics_method)),
       )
-    when Method
-      analytics_method.call(**hash_from_method_kwargs(attributes, analytics_method))
     else
       analytics.track_event("Frontend: #{name}", attributes)
     end
