c45092f99b40509578dae827177e962601a72932
Zach Margolis <zachary.margolis@gsa.gov>
2023-08-30 15:17:04 -0700

    Revert "WIP: experimenting with less special-casing of Analytics class"
    This reverts commit ae086b4a4792e5ee1b704db43599cebbdcddf845.

--- app/controllers/frontend_log_controller.rb
+++ app/controllers/frontend_log_controller.rb
@@ -11,26 +11,26 @@ class FrontendLogController < ApplicationController
   # Please try to keep this list alphabetical as well!
   # rubocop:disable Layout/LineLength
   EVENT_MAP = {
-    'Frontend Error' => [FrontendErrorLogger, :track_error],
-    'IdV: consent checkbox toggled' => [Analytics, :idv_consent_checkbox_toggled],
-    'IdV: download personal key' => [Analytics, :idv_personal_key_downloaded],
-    'IdV: location submitted' => [Analytics, :idv_in_person_location_submitted],
-    'IdV: location visited' => [Analytics, :idv_in_person_location_visited],
-    'IdV: Mobile device and camera check' => [Analytics, :idv_mobile_device_and_camera_check],
-    'IdV: Native camera forced after failed attempts' => [Analytics, :idv_native_camera_forced],
-    'IdV: personal key acknowledgment toggled' => [Analytics, :idv_personal_key_acknowledgment_toggled],
-    'IdV: prepare submitted' => [Analytics, :idv_in_person_prepare_submitted],
-    'IdV: prepare visited' => [Analytics, :idv_in_person_prepare_visited],
-    'IdV: switch_back submitted' => [Analytics, :idv_in_person_switch_back_submitted],
-    'IdV: switch_back visited' => [Analytics, :idv_in_person_switch_back_visited],
-    'IdV: user clicked sp link on ready to verify page' => [Analytics, :idv_in_person_ready_to_verify_sp_link_clicked],
-    'IdV: user clicked what to bring link on ready to verify page' => [Analytics, :idv_in_person_ready_to_verify_what_to_bring_link_clicked],
-    'IdV: verify in person troubleshooting option clicked' => [Analytics, :idv_verify_in_person_troubleshooting_option_clicked],
-    'Multi-Factor Authentication: download backup code' => [Analytics, :multi_factor_auth_backup_code_download],
-    'Show Password button clicked' => [Analytics, :show_password_button_clicked],
-    'Sign In: IdV requirements accordion clicked' => [Analytics, :sign_in_idv_requirements_accordion_clicked],
-    'User prompted before navigation' => [Analytics, :user_prompted_before_navigation],
-    'User prompted before navigation and still on page' => [Analytics, :user_prompted_before_navigation_and_still_on_page],
+    'Frontend Error' => FrontendErrorLogger.method(:track_error),
+    'IdV: consent checkbox toggled' => :idv_consent_checkbox_toggled,
+    'IdV: download personal key' => :idv_personal_key_downloaded,
+    'IdV: location submitted' => :idv_in_person_location_submitted,
+    'IdV: location visited' => :idv_in_person_location_visited,
+    'IdV: Mobile device and camera check' => :idv_mobile_device_and_camera_check,
+    'IdV: Native camera forced after failed attempts' => :idv_native_camera_forced,
+    'IdV: personal key acknowledgment toggled' => :idv_personal_key_acknowledgment_toggled,
+    'IdV: prepare submitted' => :idv_in_person_prepare_submitted,
+    'IdV: prepare visited' => :idv_in_person_prepare_visited,
+    'IdV: switch_back submitted' => :idv_in_person_switch_back_submitted,
+    'IdV: switch_back visited' => :idv_in_person_switch_back_visited,
+    'IdV: user clicked sp link on ready to verify page' => :idv_in_person_ready_to_verify_sp_link_clicked,
+    'IdV: user clicked what to bring link on ready to verify page' => :idv_in_person_ready_to_verify_what_to_bring_link_clicked,
+    'IdV: verify in person troubleshooting option clicked' => :idv_verify_in_person_troubleshooting_option_clicked,
+    'Multi-Factor Authentication: download backup code' => :multi_factor_auth_backup_code_download,
+    'Show Password button clicked' => :show_password_button_clicked,
+    'Sign In: IdV requirements accordion clicked' => :sign_in_idv_requirements_accordion_clicked,
+    'User prompted before navigation' => :user_prompted_before_navigation,
+    'User prompted before navigation and still on page' => :user_prompted_before_navigation_and_still_on_page,
   }.freeze
   # rubocop:enable Layout/LineLength
 
@@ -43,7 +43,7 @@ class FrontendLogController < ApplicationController
   private
 
   def frontend_logger
-    FrontendLogger.new(analytics: analytics, error_logger: FrontendErrorLogger.new, event_map: EVENT_MAP)
+    FrontendLogger.new(analytics: analytics, event_map: EVENT_MAP)
   end
 
   def log_params
