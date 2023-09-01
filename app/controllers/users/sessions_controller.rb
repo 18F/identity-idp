330eb33b8802366b8c9fcfacf08f1b8b01fc48a8
Andrew Duthie <andrew.duthie@gsa.gov>
2023-09-01 11:19:54 -0400

    Try again to redirect to account path from sessions controller
    If the user intentionally visits the root page while already signed in, we should probably default to the account page and not worry about all the other pre-checks

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -93,7 +93,7 @@ module Users
 
     def check_user_needs_redirect
       if user_fully_authenticated?
-        redirect_to after_sign_in_path_for(current_user)
+        redirect_to account_path
       elsif current_user
         redirect_to user_two_factor_authentication_url
       end
