9a7da922558af804d16415231cb73172e6ef3bba
Luis <lmatos@flexion.us>
2023-09-04 10:30:29 -0500

    LG-10912 Reformat Account Reuse Report
    changelog: Internal, formatting, un-embed the CSV from within JSON

--- app/jobs/reports/monthly_account_reuse_report.rb
+++ app/jobs/reports/monthly_account_reuse_report.rb
@@ -10,12 +10,11 @@ module Reports
       @report_date = report_date
 
       _latest, path = generate_s3_paths(REPORT_NAME, 'json', now: report_date)
-      body = report_body
 
       if bucket_name.present?
         upload_file_to_s3_bucket(
           path: path,
-          body: body,
+          body: report_csv,
           content_type: 'text/csv',
           bucket: bucket_name,
         )
