9ee2004c6c047c310b6534c04d6c9692d35ee08c
Kimball Bighorse <kbighorse@yahoo.com>
2023-09-05 10:19:04 -1000

    Stop setting `deactivation_reason` in profile spec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1059,7 +1059,7 @@ RSpec.describe Profile do
 
       expect(profile.activated_at).to be_nil
       expect(profile.active).to eq(false)
-      expect(profile.deactivation_reason).to eq('in_person_verification_pending') # to change
+      expect(profile.deactivation_reason).to be_nil
       expect(profile.fraud_review_pending?).to eq(false)
       expect(profile.gpo_verification_pending_at).to be_nil
       expect(profile.in_person_verification_pending_at).to be_present # to change
