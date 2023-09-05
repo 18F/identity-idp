598a86245902d97737f09037241e95a59ac1b766
gina-yamada <gina.yamada@gsa.gov>
2023-09-05 13:53:32 -0600

    Export new component and version change

--- app/javascript/packages/address-search/index.tsx
+++ app/javascript/packages/address-search/index.tsx
@@ -2,7 +2,15 @@ import { snakeCase, formatLocations, transformKeys } from './utils';
 import InPersonLocations from './components/in-person-locations';
 import AddressInput from './components/address-input';
 import AddressSearch from './components/address-search';
+import PostOfficeNoResults from './components/post-office-no-results';
 
-export { snakeCase, formatLocations, transformKeys, InPersonLocations, AddressInput };
+export {
+  snakeCase,
+  formatLocations,
+  transformKeys,
+  InPersonLocations,
+  AddressInput,
+  PostOfficeNoResults,
+};
 
 export default AddressSearch;
