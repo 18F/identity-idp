8f94903f85f7e5e94a65d4dd4167ea5e5284524e
gina-yamada <gina.yamada@gsa.gov>
2023-09-06 08:40:44 -0600

    fix linter errors

--- app/javascript/packages/address-search/components/no-in-person-locations-display.tsx
+++ app/javascript/packages/address-search/components/no-in-person-locations-display.tsx
@@ -3,12 +3,12 @@ import { t } from '@18f/identity-i18n';
 
 function NoInPersonLocationsDisplay({ address }) {
   return (
-    <div className="grid-row grid-gap grid-gap-2" >
+    <div className="grid-row grid-gap grid-gap-2">
       <img
         className="grid-col-2"
         style={{ marginTop: '20px' }}
         alt="exclamation mark inside of map pin"
-        width={65} 
+        width={65}
         height={65}
         src={getAssetPath('info-pin-map.svg')}
       />
