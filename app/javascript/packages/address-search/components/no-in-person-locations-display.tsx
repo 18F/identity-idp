a6ac4dc903d9ed0485f36e1c6e19be28005def99
gina-yamada <gina.yamada@gsa.gov>
2023-09-06 09:33:14 -0600

    more style changes

--- app/javascript/packages/address-search/components/no-in-person-locations-display.tsx
+++ app/javascript/packages/address-search/components/no-in-person-locations-display.tsx
@@ -6,16 +6,16 @@ function NoInPersonLocationsDisplay({ address }) {
     <div className="grid-row grid-gap grid-gap-2">
       <img
         className="grid-col-2"
-        style={{ marginTop: '20px' }}
+        style={{ marginTop: '30px' }}
         alt="exclamation mark inside of map pin"
         width={65}
         height={65}
         src={getAssetPath('info-pin-map.svg')}
       />
       <div className="inline-block grid-col-10">
-        <h3 role="status">
+        <h2 role="status">
           {t('in_person_proofing.body.location.po_search.none_found', { address })}
-        </h3>
+        </h2>
         <p>{t('in_person_proofing.body.location.po_search.none_found_tip')}</p>
       </div>
     </div>
