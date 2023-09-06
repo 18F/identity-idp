f7c7c2401a3d40bd812f75e176c04360b076c828
gina-yamada <gina.yamada@gsa.gov>
2023-09-06 08:34:56 -0600

    Modify style

--- app/javascript/packages/address-search/components/post-office-no-results.tsx
+++ app/javascript/packages/address-search/components/post-office-no-results.tsx
@@ -3,20 +3,22 @@ import { t } from '@18f/identity-i18n';
 
 function PostOfficeNoResults({ address }) {
   return (
-    <>
+    <div className="grid-row grid-gap grid-gap-2" >
       <img
-        className="grid-col-2 inline-block vertical-align-top margin-top-22"
-        style={{ display: 'inline-block', marginTop: '20px', verticalAlign: 'top' }}
+        className="grid-col-2"
+        style={{ marginTop: '20px' }}
         alt="exclamation mark inside of map pin"
+        width={65} 
+        height={65}
         src={getAssetPath('info-pin-map.svg')}
       />
-      <div className="grid-offset-1 grid-col-9 inline-block" style={{ display: 'inline-block' }}>
+      <div className="inline-block grid-col-10">
         <h3 role="status">
           {t('in_person_proofing.body.location.po_search.none_found', { address })}
         </h3>
         <p>{t('in_person_proofing.body.location.po_search.none_found_tip')}</p>
       </div>
-    </>
+    </div>
   );
 }
 
