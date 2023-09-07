f4a32ae8d6669652e055e5a2823f9fa30043a47d
gina-yamada <125507397+gina-yamada@users.noreply.github.com>
2023-09-07 09:04:25 -0600

    LG-10330 No PO Results Component (#9145)
    * PostOfficeNoResult component and tests
    
    * Add new svg
    
    * changelog: Upcoming Features, In-person proofing, created a new component to display when no po search results are found
    
    * Added extra line at end of file
    
    * lint fix
    
    * Export new component and version change
    
    * optimize assest
    
    * fix spelling
    
    * Modify style
    
    * rename component
    
    * fix linter errors
    
    * more style changes
    
    * Add alt text translations for info-pin-map
    
    * updated test to be dynamic for translations
    
    * Use NoInPersonLocationsDisplay component in in-person-locations
    
    * modify style of no in person location display
    
    * modified grip gap

--- app/javascript/packages/address-search/index.tsx
+++ app/javascript/packages/address-search/index.tsx
@@ -2,7 +2,15 @@ import { snakeCase, formatLocations, transformKeys } from './utils';
 import InPersonLocations from './components/in-person-locations';
 import AddressInput from './components/address-input';
 import AddressSearch from './components/address-search';
+import NoInPersonLocationsDisplay from './components/no-in-person-locations-display';
 
-export { snakeCase, formatLocations, transformKeys, InPersonLocations, AddressInput };
+export {
+  snakeCase,
+  formatLocations,
+  transformKeys,
+  InPersonLocations,
+  AddressInput,
+  NoInPersonLocationsDisplay,
+};
 
 export default AddressSearch;
