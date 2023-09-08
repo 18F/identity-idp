43074d9a38207baf7a968074f053ba2754bd90de
Matt Gardner <wilburnforce@gmail.com>
2023-09-08 11:41:08 -0400

    changelog: Internal, In-Person Proofing, Separate components

--- app/javascript/packages/document-capture/components/in-person-full-address-search.tsx
+++ app/javascript/packages/document-capture/components/in-person-full-address-search.tsx
@@ -1,8 +1,9 @@
-import { TextInput, SelectInput } from '@18f/identity-components';
+import { TextInput, SelectInput, Alert, PageHeading } from '@18f/identity-components';
 import { useState, useRef, useEffect, useCallback, useContext } from 'react';
 import { t } from '@18f/identity-i18n';
 import ValidatedField from '@18f/identity-validated-field/validated-field';
 import SpinnerButton, { SpinnerButtonRefHandle } from '@18f/identity-spinner-button/spinner-button';
+import { InPersonLocations } from '@18f/identity-address-search';
 import type { RegisterFieldCallback } from '@18f/identity-form-steps';
 import { useDidUpdateEffect } from '@18f/identity-react-hooks';
 import type { LocationQuery, FormattedLocation } from '@18f/identity-address-search/types';
@@ -21,7 +22,7 @@ interface FullAddressSearchProps {
   locationsURL: string;
 }
 
-function FullAddressSearch({
+function FullAddressSearchInput({
   registerField = () => undefined,
   onFoundLocations = () => undefined,
   onLoadingLocations = () => undefined,
@@ -172,6 +173,55 @@ function FullAddressSearch({
           {t('in_person_proofing.body.location.po_search.search_button')}
         </SpinnerButton>
       </div>
+    </>
+  );
+}
+
+function FullAddressSearch({
+  registerField,
+  locationsURL,
+  handleLocationSelect,
+  disabled,
+  onFoundLocations,
+}) {
+  const [apiError, setApiError] = useState<Error | null>(null);
+  const [foundAddress, setFoundAddress] = useState<LocationQuery | null>(null);
+  const [locationResults, setLocationResults] = useState<FormattedLocation[] | null | undefined>(
+    null,
+  );
+  const [isLoadingLocations, setLoadingLocations] = useState<boolean>(false);
+
+  return (
+    <>
+      {apiError && (
+        <Alert type="error" className="margin-bottom-4">
+          {t('idv.failure.exceptions.post_office_search_error')}
+        </Alert>
+      )}
+      <PageHeading>{t('in_person_proofing.headings.po_search.location')}</PageHeading>
+      <p>{t('in_person_proofing.body.location.po_search.po_search_about')}</p>
+      <FullAddressSearchInput
+        registerField={registerField}
+        onFoundLocations={(
+          address: LocationQuery | null,
+          locations: FormattedLocation[] | null | undefined,
+        ) => {
+          setFoundAddress(address);
+          setLocationResults(locations);
+          onFoundLocations(locations);
+        }}
+        onLoadingLocations={setLoadingLocations}
+        onError={setApiError}
+        disabled={disabled}
+        locationsURL={locationsURL}
+      />
+      {locationResults && foundAddress && !isLoadingLocations && (
+        <InPersonLocations
+          locations={locationResults}
+          onSelect={handleLocationSelect}
+          address={foundAddress.address || ''}
+        />
+      )}
     </>
   );
 }
