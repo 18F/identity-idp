11dd9abc9fb9cc6c5805d5dd989ff3a822c8a961
Douglas Price <douglas.price@gsa.gov>
2023-09-11 17:51:15 -0400

    Added a bunch of procs that may be useful...

--- app/services/idv/allowed_step.rb
+++ app/services/idv/allowed_step.rb
@@ -19,36 +19,80 @@ module Idv
 
 
     STEP_PRECONDITIONS = {
-      welcome: [],
-      getting_started: [],
-      agreement: [:welcome_visited],
-      hybrid_handoff: [:welcome_visited, :idv_consent_given],
-      document_capture: [:welcome_visited, :idv_consent_given, :flow_path],
-      link_sent: [:welcome_visited, :idv_consent_given, :flow_path, :pii_from_doc],
-      ssn: [:welcome_visited, :idv_consent_given, :flow_path, :pii_from_doc, ],
-      (proc)
-
+      welcome: [nil, welcome_proc],
+      getting_started: [nil, welcome_proc],
+      agreement: [nil, agreement_proc],
+      hybrid_handoff: [nil, hybrid_handoff_proc],
+      document_capture_standard: [nil, document_capture_standard_proc],
+      document_capture_hybrid: [nil, document_capture_hybrid_proc],
+      ssn: [ssn_predecessor_proc, ssn_proc]
     }
 
 
-    STEP_PRECONDITIONS = {
-      welcome: [],
-      getting_started: [],
-      agreement: [:welcome, :welcome_visited],
-      hybrid_handoff: [:agreement/:getting_started, :idv_consent_given],
-      document_capture: [:hybrid_handoff, :flow_path],
-      link_sent: [:welcome_visited, :idv_consent_given, :flow_path, :pii_from_doc],
-      ssn: [:welcome_visited, :idv_consent_given, :flow_path, :pii_from_doc, ],
-      (proc_predecessor, proc_requirements
-
-    }
+    # STEP_PRECONDITIONS = {
+    #   welcome: [],
+    #   getting_started: [],
+    #   agreement: [:welcome, :welcome_visited],
+    #   hybrid_handoff: [:agreement/:getting_started, :idv_consent_given],
+    #   document_capture: [:hybrid_handoff, :flow_path],
+    #   link_sent: [:welcome_visited, :idv_consent_given, :flow_path, :pii_from_doc],
+    #   ssn: [:welcome_visited, :idv_consent_given, :flow_path, :pii_from_doc, ],
+    # }
 
     def allowed_step(idv_session:, step:)
-      gather_required_properties(step).all |prop| idv_session.send(prop)
+      STEP_PRECONDITIONS[step].first.call(idv_session)
+      # gather_required_properties(step).all |prop| idv_session.send(prop)
     end
 
     def gather_step_preconditions(step)
       # chain through dependencies
+    end
+
+    def welcome_proc
+      ->(idv_session) { true }
+    end
+
+    def agreement_proc
+      ->(idv_session) do
+        idv_session.welcome_visited
+      end
+    end
+
+    def hybrid_handoff_proc
+      ->(idv_session) do
+        idv_session.idv_consent_given
+      end
+    end
+
+    def document_capture_standard_proc
+      ->(idv_session) do
+        idv_session.flow_path == 'standard'
+      end
+    end
+
+    def document_capture_hybrid_proc
+      ->(idv_session) do
+        idv_session.flow_path == 'hybrid'
+      end
+    end
+
+    def ssn_predecessor_proc
+      ->(idv_session) do
+        case idv_session.flow_path
+        when 'standard'
+          :document_capture_standard
+        when 'hybrid'
+          :document_capture_hybrid
+        else
+          nil
+        end
+      end
+    end
+
+    def ssn_proc
+      ->(idv_session) do
+        idv_session.pii_from_doc
+      end
     end
   end
 end
