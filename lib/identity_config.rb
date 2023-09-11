995250d7c1fa8e7dbceb61dd108617f98a3a153e
Jonathan Hooper <jonathan.hooper@gsa.gov>
2023-09-11 13:23:01 -0400

    Remove the `aws_kms_multi_region_read_enabled` flag (#9187)
    The `aws_kms_multi_region_read_enabled` was in place to toggle the changes introduced in #9047. Those changes have been deployed, enabled, and have been running without issue for some time.
    
    This commit removes the tooling to toggle the feature since we will be using it going forward.
    
    [skip changelog]

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -138,7 +138,6 @@ class IdentityConfig
     config.add(:aws_kms_client_multi_pool_size, type: :integer)
     config.add(:aws_kms_key_id, type: :string)
     config.add(:aws_kms_multi_region_key_id, type: :string)
-    config.add(:aws_kms_multi_region_read_enabled, type: :boolean)
     config.add(:aws_kms_session_key_id, type: :string)
     config.add(:aws_logo_bucket, type: :string)
     config.add(:aws_region, type: :string)
