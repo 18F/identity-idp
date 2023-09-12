185c684d9def73618df22d789baccf7d31dd371c
Luis <lmatos@flexion.us>
2023-09-12 17:36:06 -0500

    Add looping over intervals

--- lib/reporting/proofing_rate_report.rb
+++ lib/reporting/proofing_rate_report.rb
@@ -13,36 +13,36 @@ module Reporting
     attr_accessor :ivr
 
     def report_for(start_date:)
-      @ivr = Reporting::IdentityVerificationReport.new(
-        issuers: nil, # all issuers
-        # FIXME: We actually want to loop over DATE_INVERVALS but it's
-        # easier to start with one and then add that in.
-        time_range: (start_date - 30.days)..start_date,
-      )
-
       csv = []
+      DATE_INTERVALS.each do |interval|
+        @ivr = Reporting::IdentityVerificationReport.new(
+          issuers: nil, # all issuers
+          time_range: (start_date - interval.days)..start_date,
+        )
 
-      csv << ['Blanket Proofing Rate', blanket_proofing_rate]
-      csv << ['Intent Proofing Rate', intent_proofing_rate]
-      csv << ['Actual Proofing Rate', actual_proofing_rate]
-      csv << ['Industry Proofing Rate', 'FIXME']
-      csv << []
-      # Two additional queries:
-      # 1. How many users started IDV and reached IPP / GPO / fraud review?
-      # 2. How many users started IDV and were rejected from moving forward, due to “fraud”?
-      # marked_as_fraudulent should be #2, also used in Industry Proofing Rate
+        csv << [interval, '-DAY RANGE PROOFING RATES']
+        csv << ['Blanket Proofing Rate ', blanket_proofing_rate]
+        csv << ['Intent Proofing Rate ', intent_proofing_rate]
+        csv << ['Actual Proofing Rate ', actual_proofing_rate]
+        # csv << ['Industry Proofing Rate', 'FIXME']
+        csv << []
+        # Two additional queries:
+        # 1. How many users started IDV and reached IPP / GPO / fraud review?
+        # 2. How many users started IDV and were rejected from moving forward, due to “fraud”?
+        # marked_as_fraudulent should be #2, also used in Industry Proofing Rate
+      end
     end
 
     def blanket_proofing_rate
-      ivr.idv_started / ivr.successfully_verified_users
+      ivr.idv_started.to_f / ivr.successfully_verified_users
     end
 
     def intent_proofing_rate
-      ivr.idv_doc_auth_welcome_submitted / ivr.successfully_verified_users
+      ivr.idv_doc_auth_welcome_submitted.to_f / ivr.successfully_verified_users
     end
 
     def actual_proofing_rate
-      ivr.idv_doc_auth_image_vendor_submitted / ivr.successfully_verified_users
+      ivr.idv_doc_auth_image_vendor_submitted.to_f / ivr.successfully_verified_users
     end
 
     def marked_as_fraudulent
@@ -56,7 +56,7 @@ module Reporting
 
     def abandon_the_process
       # not marked_as_fraudulent (just subtract that)
-      #  is not blocked by us (e.g. is stopped in funnel w/ 
+      #  is not blocked by us (e.g. is stopped in funnel w/
       #  rejection codes by Acuant/InstantVerify/PhoneFinder/USPS)
       # I think we need to return (total - sum of all those "not..." cases)
     end
