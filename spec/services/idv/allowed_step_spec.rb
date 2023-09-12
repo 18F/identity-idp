96e82393646965cbf68f7655235f18ae47918f25
Sonia Connolly <sonia.connolly@gsa.gov>
2023-09-12 13:15:05 -0700

    Add latest_step

--- spec/services/idv/allowed_step_spec.rb
+++ spec/services/idv/allowed_step_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Idv::AllowedStep' do
-  subject { Idv::AllowedStep.new(idv_session: nil)}
+  let(:user) { create(:user) }
 
-  context 'when condition' do
+  let(:idv_session) do
+    Idv::Session.new(
+      user_session: {},
+      current_user: user,
+      service_provider: nil,
+    )
+  end
+
+  subject { Idv::AllowedStep.new(idv_session: idv_session) }
+
+  context '#step_allowed?' do
     it 'allows the welcome step' do
       expect(subject.step_allowed?(step: :welcome)).to be true
+    end
+  end
+
+  context '#latest_step' do
+    it 'returns welcome for empty session' do
+      expect(subject.latest_step).to eq(:welcome)
     end
   end
 end
