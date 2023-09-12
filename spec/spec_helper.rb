b5ec350b4f908f8a95bd180c416ffa05469258fd
Kimball Bighorse <kbighorse@yahoo.com>
2023-09-11 20:54:11 -1000

    Rename spec directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
   config.profile_examples = RSPEC_RUNNING_IN_PARALLEL ? 10 : 0
 
   config.include Sequent::Test::CommandHandlerHelpers
-  config.include DomainTests, file_path: /spec\/command_handlers/
+  config.include DomainTests, file_path: /spec\/cqrs/
 
   # Domain tests run with a clean sequent configuration and the in memory FakeEventStore
   config.around :each, :domain_tests do |example|
