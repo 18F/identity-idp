03a56f79da9b4f0cb75adafad971087295af578c
Zach Margolis <zachmargolis@users.noreply.github.com>
2023-09-13 12:02:52 -0700

    Tried reorganizing proofing rate report again (#9199)
    Co-authored-by: Luis H. Matos <ThatSpaceGuy@users.noreply.github.com>

--- lib/reporting/proofing_rate_report.rb
+++ lib/reporting/proofing_rate_report.rb
@@ -5,66 +5,64 @@ require 'reporting/identity_verification_report'
 
 module Reporting
   class ProofingRateReport
-    DATE_INTERVALS = ['30', '60', '90']
+    DATE_INTERVALS = [30, 60, 90].freeze
 
-    # The basic goal here is to call IdentityVerificationReport for 30, 60, and 90 days
-    # And then return some of the stuff.
+    attr_reader :start_date
 
-    attr_accessor :ivr
+    def initialize(start_date:)
+      @start_date = start_date
+    end
 
-    def report_for(start_date:)
+    # rubocop:disable Layout/LineLength
+    def as_csv
       csv = []
-      @ivr = {}
-      DATE_INTERVALS.each do |interval|
-        @ivr[interval.to_sym] = Reporting::IdentityVerificationReport.new(
+
+      csv << ['Metric', 'Trailing 30d', 'Trailing 60d', 'Trailing 90d']
+
+      csv << ['IDV Started', *reports.map(&:idv_started)]
+      csv << ['Welcome Submitted', *reports.map(&:idv_doc_auth_welcome_submitted)]
+      csv << ['Image Submitted', *reports.map(&:idv_doc_auth_image_vendor_submitted)]
+      csv << ['Successfully Verified', *reports.map(&:successfully_verified_users)]
+
+      csv << ['Blanket Proofing Rate (IDV Started to Successfully Verified)', *blanket_proofing_rates(reports)]
+      csv << ['Intent Proofing Rate (Welcome Submitted to Successfully Verified)', *intent_proofing_rates(reports)]
+      csv << ['Actual Proofing Rate (Image Submitted to Successfully Verified)', *actual_proofing_rates(reports)]
+
+      csv
+    end
+    # rubocop:enable Layout/LineLength
+
+    def reports
+      @reports ||= DATE_INTERVALS.map do |interval|
+        Reporting::IdentityVerificationReport.new(
           issuers: nil, # all issuers
           time_range: (start_date - interval.days)..start_date,
         )
       end
-
-      csv << ['PROOFING RATES', 'Trailing 30d', 'Trailing 60d', 'Trailing 90d']
-      csv << ['Blanket Proofing Rate ', *blanket_proofing_rates]
-      csv << ['Intent Proofing Rate ', *intent_proofing_rates]
-      csv << ['Actual Proofing Rate ', *actual_proofing_rates]
-      # csv << ['Industry Proofing Rate', 'FIXME']
-      csv << []
-      # Two additional queries:
-      # 1. How many users started IDV and reached IPP / GPO / fraud review?
-      # 2. How many users started IDV and were rejected from moving forward, due to “fraud”?
-      # marked_as_fraudulent should be #2, also used in Industry Proofing Rate
     end
 
-    def blanket_proofing_rates
-      blanket_rates = []
-      DATE_INTERVALS.each do |interval|
-        user_stats = ivr[interval.to_sym]
-        blanket_rates << "#{
-          user_stats.idv_started.to_f / user_stats.successfully_verified_users
-        }%"
+    # @param [Array<Reporting::IdentityVerificationReport>] reports
+    # @return [Array<Float>]
+    def blanket_proofing_rates(reports)
+      reports.map do |report|
+        user_stats.idv_started.to_f / user_stats.successfully_verified_users
       end
-      blanket_rates
     end
 
-    def intent_proofing_rates
-      intent_rates = []
-      DATE_INTERVALS.each do |interval|
-        user_stats = ivr[interval.to_sym]
-        intent_rates << "#{
-          user_stats.idv_doc_auth_welcome_submitted.to_f / user_stats.successfully_verified_users
-        }%"
+    # @param [Array<Reporting::IdentityVerificationReport>] reports
+    # @return [Array<Float>]
+    def intent_proofing_rates(reports)
+      reports.map do |report|
+        user_stats.idv_doc_auth_welcome_submitted.to_f / user_stats.successfully_verified_users
       end
-      intent_rates
     end
 
-    def actual_proofing_rates
-      actual_rates = []
-      DATE_INTERVALS.each do |interval|
-        user_stats = ivr[interval.to_sym]
-        actual_rates << "#{
-          user_stats.idv_doc_auth_image_vendor_submitted.to_f / user_stats.successfully_verified_users
-        }%"
+    # @param [Array<Reporting::IdentityVerificationReport>] reports
+    # @return [Array<Float>]
+    def actual_proofing_rates(reports)
+      reports.map do |report|
+        user_stats.idv_doc_auth_image_vendor_submitted.to_f / user_stats.successfully_verified_users
       end
-      actual_rates
     end
   end
 end
