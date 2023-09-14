c9d605c7ee01419810ae11741d57889baff9feba
Andrew Duthie <andrew.duthie@gsa.gov>
2023-09-14 10:29:52 -0400

    Log errors as expected

--- app/services/frontend_error_logger.rb
+++ app/services/frontend_error_logger.rb
@@ -2,6 +2,10 @@ class FrontendErrorLogger
   class FrontendError < StandardError; end
 
   def self.track_error(name:, message:, stack:)
-    NewRelic::Agent.notice_error(FrontendError.new, custom_params: { name:, message:, stack: })
+    NewRelic::Agent.notice_error(
+      FrontendError.new,
+      expected: true,
+      custom_params: { name:, message:, stack: },
+    )
   end
 end
