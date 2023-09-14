11490e7d8985ff833edff294c0f695bba8f60f9b
Malick Diarra <malick.diarra@gsa.gov>
2023-09-14 14:04:03 -0400

    auth app selection

---
+++ app/presenters/two_factor_authentication/auth_app_selection_presentor.rb
@@ -0,0 +1,12 @@
+module TwoFactorAuthentication
+    class AuthAppSelectionPresenter < SelectionPresenter
+      def method
+        :auth_app
+      end
+  
+      def mfa_configuration_count
+        user.auth_app_configurations.count
+      end
+    end
+    end
+  
