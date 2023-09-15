55b3db4ebd7a76a6a1e44ac1563add5436e48227
Zach Margolis <zachary.margolis@gsa.gov>
2023-09-15 11:32:27 -0700

    Update type comments

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -36,9 +36,9 @@ class ReportMailer < ActionMailer::Base
   # @param [String] email
   # @param [String] subject
   # @param [String] env name of current deploy environment
-  # @param [Array<Array<Array<String>>>] tables
+  # @param [Array<Array<Hash,Array<String>>>] tables
   #   an array of tables (which are arrays of rows (arrays of strings))
-  #   each table can have an first hash with options
+  #   each table can have a first "row" that is a hash with options
   # @option opts [Boolean] :float_as_percent whether or not to render floats as percents
   # @option opts [Boolean] :title title of the table
   def tables_report(email:, subject:, tables:, env: Identity::Hostdata.env)
