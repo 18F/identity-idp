b4406a7ed842ee18916063b350e552aa4773ec20
Sonia Connolly <sonia.connolly@gsa.gov>
2023-09-18 12:50:44 -0700

    Remove strong tag from around phone warning message about by mail length of time
    The strong tag was unexpectedly in the view template, not the message itself.

--- spec/views/idv/phone_errors/warning.html.erb_spec.rb
+++ spec/views/idv/phone_errors/warning.html.erb_spec.rb
@@ -98,9 +98,8 @@ RSpec.describe 'idv/phone_errors/warning.html.erb' do
     end
 
     it 'says how long gpo takes' do
-      expect(rendered).to have_css(
-        'strong',
-        text: strip_tags(t('idv.failure.phone.warning.gpo.how_long_it_takes_html')),
+      expect(rendered).to have_text(
+        strip_tags(t('idv.failure.phone.warning.gpo.how_long_it_takes_html')),
       )
     end
 
