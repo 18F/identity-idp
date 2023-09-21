1bee97a21a7bec54038ff992e6ffe603679b9f92
kevinsmaster5 <kevin.masters@gsa.gov>
2023-09-21 16:54:40 -0400

    adds test case for setup and phone access code validation redirect on cancel

--- spec/features/multi_factor_authentication/mfa_cta_spec.rb
+++ spec/features/multi_factor_authentication/mfa_cta_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'mfa cta banner' do
   include DocAuthHelper
   include SamlAuthHelper
+  include WebAuthnHelper
 
   describe 'multiple MFA handling' do
     it 'displays a banner after configuring a single MFA method' do
@@ -69,6 +70,32 @@ RSpec.feature 'mfa cta banner' do
       acknowledge_backup_code_confirmation
       click_link(t('mfa.second_method_warning.link'))
       expect(page).to have_current_path(authentication_methods_setup_path)
+    end
+
+    it 'At setup, phone as second MFA show a cancel link that returns to mfa setup' do
+      allow(IdentityConfig.store).to receive(:platform_auth_set_up_enabled).and_return(true)
+      allow(IdentityConfig.store).
+        to receive(:show_unsupported_passkey_platform_authentication_setup).
+        and_return(true)
+      mock_webauthn_setup_challenge
+      user = sign_up_and_set_password
+      user.password = Features::SessionHelper::VALID_PASSWORD
+      select_2fa_option('webauthn_platform', visible: :all)
+
+      click_continue
+      fill_in_nickname_and_click_continue
+      mock_press_button_on_hardware_key_on_setup
+
+      select_2fa_option('phone')
+      click_continue
+
+      fill_in :new_phone_form_phone, with: '3015551212'
+      click_send_one_time_code
+
+      expect(page).to have_link(
+        t('links.cancel'),
+        href: authentication_methods_setup_path,
+      )
     end
   end
 end
