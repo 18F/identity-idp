1e550eb88ac14548570de337f84dd4c63e159467
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-09-25 12:27:00 -0400

    resave document capture session

--- app/models/document_capture_session.rb
+++ app/models/document_capture_session.rb
@@ -11,13 +11,9 @@ class DocumentCaptureSession < ApplicationRecord
   def session_result
     return @session_result if defined?(@session_result)
 
-    @session_result = load_result
-    if store_session_result?# !@session_result || redo_document_capture?
-      @session_result = DocumentCaptureSessionResult.new(
-        id: generate_result_id,
-        created_at: Time.zone.now,
-      )
-    end
+    @session_result = load_result || DocumentCaptureSessionResult.new(
+      id: generate_result_id,
+    )
     @session_result
   end
 
@@ -25,6 +21,7 @@ class DocumentCaptureSession < ApplicationRecord
     session_result.success = doc_auth_response.success?
     session_result.pii = doc_auth_response.pii_from_doc
     session_result.attention_with_barcode = doc_auth_response.attention_with_barcode?
+    session_result.created_at = Time.zone.now
     EncryptedRedisStructStorage.store(
       session_result,
       expires_in: IdentityConfig.store.doc_capture_request_valid_for_minutes.minutes.seconds.to_i,
@@ -35,6 +32,7 @@ class DocumentCaptureSession < ApplicationRecord
 
   def store_failed_auth_image_fingerprint(front_image_fingerprint, back_image_fingerprint)
     session_result.success = false
+    session_result.created_at = Time.zone.now
     session_result.add_failed_front_image!(front_image_fingerprint) if front_image_fingerprint
     session_result.add_failed_back_image!(back_image_fingerprint) if back_image_fingerprint
     EncryptedRedisStructStorage.store(
@@ -110,9 +108,5 @@ class DocumentCaptureSession < ApplicationRecord
 
   def generate_result_id
     self.result_id = SecureRandom.uuid
-  end
-
-  def store_session_result? # rename
-    !@session_result || self.requested_at > @session_result.created_at
   end
 end
