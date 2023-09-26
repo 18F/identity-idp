f038671f2af5f8ab2213a46a49988c4cae9a4f96
Kimball Bighorse <kbighorse@yahoo.com>
2023-09-26 13:17:37 -1000

    Remove `metadata` from `create` event

--- app/models/events/profile/created.rb
+++ app/models/events/profile/created.rb
@@ -3,11 +3,10 @@
 module Events
   module Profile
     class Created < Events::Profile::BaseEvent
-      data_attributes :user_id, :metadata
+      data_attributes :user_id
 
       def apply(profile)
         profile.user_id = user_id
-        profile.metadata = metadata
 
         profile
       end
