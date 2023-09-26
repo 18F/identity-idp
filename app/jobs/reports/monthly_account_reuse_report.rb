5f759ee79411d6bb29e44b230a404283def4f461
Zach Margolis <zachmargolis@users.noreply.github.com>
2023-09-26 16:51:33 -0700

    Un-scale percents in Monthly Account Reuse Report (#9268)
    * Cleanup: remove trailing spaces
    * Un-scale percents in monthly account reuse report
    
    **Why**: it's more conventional for percents to be between 0.0 and 1.0,
    and the tables report mailer expects them in this format
    
    changelog: Internal, Reporting, Fix percentage scaling in monthly account reuse report

--- app/jobs/reports/monthly_account_reuse_report.rb
+++ app/jobs/reports/monthly_account_reuse_report.rb
@@ -44,23 +44,23 @@ module Reports
               SELECT
                   COUNT(DISTINCT agencies.id) AS num_agencies
                   , identities.user_id
-              FROM 
+              FROM
                   identities
-              JOIN 
+              JOIN
                   service_providers sp ON identities.service_provider = sp.issuer
-              JOIN 
+              JOIN
                   agencies ON sp.agency_id = agencies.id
               WHERE
                   identities.last_ial2_authenticated_at IS NOT NULL
               AND
                   identities.verified_at < %{query_date}
-              GROUP BY 
+              GROUP BY
                   identities.user_id
           ) agencies_per_user
-          GROUP BY 
+          GROUP BY
               agencies_per_user.num_agencies
           HAVING agencies_per_user.num_agencies > 1
-          ORDER BY 
+          ORDER BY
               num_agencies ASC
       SQL
 
@@ -109,8 +109,7 @@ module Reports
 
         if total_proofed > 0
           reuse_stats.each_with_index do |result_entry, index|
-            reuse_stats[index]['percentage'] =
-              result_entry['num_users'] / total_proofed.to_f * 100
+            reuse_stats[index]['percentage'] = result_entry['num_users'] / total_proofed.to_f
 
             reuse_total_percentage += reuse_stats[index]['percentage']
           end
