1c4a5caac429c564a2319d3516c242054e7ebabd
gina-yamada <gina.yamada@gsa.gov>
2023-09-27 16:58:14 -0600

    change data type

--- app/javascript/packages/address-search/types.d.ts
+++ app/javascript/packages/address-search/types.d.ts
@@ -99,5 +99,5 @@ interface FullAddressSearchProps {
   onFoundLocations: Dispatch<SetStateAction<FormattedLocation[] | null | undefined>>;
   registerField: RegisterFieldCallback;
   resultsHeaderComponent?: ComponentType;
-  usStatesTerritories: string[][];
+  usStatesTerritories: Array<Array<string>>;
 }
