67cc58894fd07108a9cd02047d2c35a43395c0d2
Micah <micah_neveu@hotmail.com>
2023-09-27 11:15:16 -0400

    lg-10082 usps enrollment code email format (#9259)
    * changelog:
    internal, in-person proofing usps enrollments, ensure parsing body covers html and text parts
    
    * changelog:
    internal, in-person proofing usps enrollments, correct a failing rebase
    
    * changelog:
    internal, ipp in-person proofing usps enrollments, correct lint and magical trailing spaces
    
    * changelog:
    internal, in-person proofing usps enrollments, add tests to cover all branches
    
    * changelog:
    internal, in-person proofing usps enrollments, lint fix because I always forget to check

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -1611,6 +1611,24 @@ module AnalyticsEvents
     track_event('IdV: in person proofing switch_back visited', flow_path: flow_path, **extra)
   end
 
+  # An email from USPS with an enrollment code has been received, indicating
+  # the enrollment is approved or failed. A check is required to get the status
+  # it is not included in the email.
+  # @param [boolean] multi_part If the email is marked as multi_part
+  # @param [string] part_found Records if the enrollment code was found in text_part or html_part
+  def idv_in_person_usps_proofing_enrollment_code_email_received(
+    multi_part: nil,
+    part_found: nil,
+    **extra
+  )
+    track_event(
+      'IdV: in person usps proofing enrollment code email received',
+      multi_part: multi_part,
+      part_found: part_found,
+      **extra,
+    )
+  end
+
   # GetUspsProofingResultsJob has completed. Includes counts of various outcomes encountered
   # @param [Float] duration_seconds number of minutes the job was running
   # @param [Integer] enrollments_checked number of enrollments eligible for status check
