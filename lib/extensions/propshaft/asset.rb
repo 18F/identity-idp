f789d4f56ee3edb8305a649179758d27477c81b0
Andrew Duthie <andrew.duthie@gsa.gov>
2023-09-27 14:33:03 -0400

    Add explaining comment for monkeypatch

--- lib/extensions/propshaft/asset.rb
+++ lib/extensions/propshaft/asset.rb
@@ -1,3 +1,7 @@
+# Monkey-patch Propshaft::Asset to produce a shorter digest as an optimization to built output size.
+#
+# See: https://github.com/rails/propshaft/blob/main/lib/propshaft/asset.rb
+
 module Extensions
   Propshaft::Asset.class_eval do
     alias_method :original_digest, :digest
