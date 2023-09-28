8af4ac32f32ca07fd7c9ec3cc102125cf9cce5ed
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-09-28 17:15:01 -0400

    init page if user has a phone to take pictures for A/B testing

---
+++ app/controllers/idv/smart_phone_controller.rb
@@ -0,0 +1,37 @@
+module Idv
+  class SmartPhoneController < ApplicationController
+    include ActionView::Helpers::DateHelper
+    include IdvStepConcern
+    include StepIndicatorConcern
+
+    before_action :confirm_verify_info_step_needed
+    before_action :confirm_agreement_step_complete
+    before_action :confirm_hybrid_handoff_needed, only: :show
+
+    def show
+      # analytics.idv_doc_auth_smart_phone_visited(**analytics_arguments)
+
+      @title = t('doc_auth.headings.smart_phone')
+    end
+
+    def confirm_agreement_step_complete
+      return if idv_session.idv_consent_given
+
+      redirect_to idv_agreement_url
+    end
+
+    def confirm_hybrid_handoff_needed
+      if idv_session.skip_hybrid_handoff?
+        redirect_to idv_hybrid_handoff_url
+      end
+
+      if !FeatureManagement.idv_allow_hybrid_flow?
+        redirect_to idv_hybrid_handoff_url
+      end
+
+      if idv_session.flow_path.present?
+        redirect_to idv_hybrid_handoff_url
+      end
+    end
+  end
+end
---
+++ spec/controllers/idv/smart_phone_controller_spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+
+RSpec.describe Idv::SmartPhoneController do
+  include IdvHelper
+
+  let(:user) { create(:user) }
+
+  before do
+    stub_sign_in(user)
+    stub_analytics
+    stub_attempts_tracker
+    subject.user_session['idv/doc_auth'] = {}
+    subject.idv_session.idv_consent_given = true
+  end
+
+  describe 'before_actions' do
+    it 'includes authentication before_action' do
+      expect(subject).to have_actions(
+        :before,
+        :confirm_two_factor_authenticated,
+      )
+    end
+
+    it 'includes outage before_action' do
+      expect(subject).to have_actions(
+        :before,
+        :check_for_mail_only_outage,
+      )
+    end
+
+    it 'checks that agreement step is complete' do
+      expect(subject).to have_actions(
+        :before,
+        :confirm_agreement_step_complete,
+      )
+    end
+
+    it 'checks that hybrid_handoff is needed' do
+      expect(subject).to have_actions(
+        :before,
+        :confirm_hybrid_handoff_needed,
+      )
+    end
+  end
+
+  describe '#show' do
+    let(:analytics_name) { 'IdV: doc auth smart phone visited' }
+    let(:analytics_args) do
+      {
+        step: 'smart_phone',
+        analytics_id: 'Doc Auth',
+        skip_hybrid_handoff: nil,
+        irs_reproofing: false,
+      }.merge(ab_test_args)
+    end
+
+    it 'renders the show template' do
+      get :show
+
+      expect(response).to render_template :show
+    end
+
+    xit 'sends analytics_visited event' do
+      get :show
+
+      expect(@analytics).to have_logged_event(analytics_name, analytics_args)
+    end
+
+    context 'agreement step is not complete' do
+      before do
+        subject.idv_session.idv_consent_given = nil
+      end
+
+      it 'redirects to idv_agreement_url' do
+        get :show
+
+        expect(response).to redirect_to(idv_agreement_url)
+      end
+    end
+
+    context 'hybrid_handoff already visited' do
+      it 'redirects to document_capture in standard flow' do
+        subject.idv_session.flow_path = 'standard'
+
+        get :show
+
+        expect(response).to redirect_to(idv_hybrid_handoff_url)
+      end
+
+      it 'redirects to link_sent in hybrid flow' do
+        subject.idv_session.flow_path = 'hybrid'
+
+        get :show
+
+        expect(response).to redirect_to(idv_hybrid_handoff_url)
+      end
+    end
+
+    context 'hybrid flow is not available' do
+      before do
+        allow(FeatureManagement).to receive(:idv_allow_hybrid_flow?).and_return(false)
+      end
+
+      it 'redirects the user straight to document capture' do
+        get :show
+        expect(response).to redirect_to(idv_hybrid_handoff_url)
+      end
+    end
+  end
+end
