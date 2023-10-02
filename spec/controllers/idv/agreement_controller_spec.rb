c5ee3f85480d834b8eb56acd9d34fa376b93b04b
Matt Hinz <matt.hinz@gsa.gov>
2023-10-02 12:27:51 -0700

    Update AgreementController spec

--- spec/controllers/idv/agreement_controller_spec.rb
+++ spec/controllers/idv/agreement_controller_spec.rb
@@ -74,12 +74,12 @@ RSpec.describe Idv::AgreementController do
     end
 
     context 'agreement already visited' do
-      it 'redirects to hybrid_handoff' do
+      it 'does not redirect to hybrid_handoff' do
         allow(subject.idv_session).to receive(:idv_consent_given).and_return(true)
 
         get :show
 
-        expect(response).to redirect_to(idv_hybrid_handoff_url)
+        expect(response).to render_template('idv/agreement/show')
       end
     end
   end
