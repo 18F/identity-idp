789bc398675364c0ca68af124ebb9d8387859ab8
Osman Latif <109746710+olatifflexion@users.noreply.github.com>
2023-10-03 15:09:49 -0500

    specs

--- spec/services/reporting/account_deletion_rate_report_spec.rb
+++ spec/services/reporting/account_deletion_rate_report_spec.rb
@@ -10,11 +10,29 @@ RSpec.describe Reporting::AccountDeletionRateReport do
   end
 
   describe '#account_deletion_report' do
+    before do
+      travel_to(report_date - 2.months) do
+        create_and_delete_accounts
+      end
+      travel_to(report_date - 1.month) do
+        create_and_delete_accounts
+      end
+      travel_to(report_date - 1.week) do
+        create_and_delete_accounts
+      end
+    end
+
     it 'returns a report with the total account deleted (last 30 days)' do
       account_deletion_table = report.account_deletion_report
-      expected_account_deletion_table = [['Total account deleted (last 30 days)'], [0]]
+      expected_account_deletion_table = [['Total account deleted (last 30 days)'], [2]]
 
       expect(account_deletion_table).to eq(expected_account_deletion_table)
     end
+  end
+
+  def create_and_delete_accounts
+    user = create(:user)
+    DeletedUser.create_from_user(user)
+    user.destroy!
   end
 end
