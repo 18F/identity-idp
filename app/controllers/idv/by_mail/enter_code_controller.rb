03be446c92d6b2f7dcb54eb6831a2abd297a77f1
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-10-03 12:49:40 -0400

    display warning banner on gpo welcome back page if gpo letter requests are spammed
    changelog: User-Facing Improvements, Identity Verification, display warning banner if user has sent max letter requests within a time window

--- app/controllers/idv/by_mail/enter_code_controller.rb
+++ app/controllers/idv/by_mail/enter_code_controller.rb
@@ -24,12 +24,14 @@ module Idv
         end
 
         gpo_mail = Idv::GpoMail.new(current_user)
+        @gpo_mail_spammed = gpo_mail.mail_spammed?
+        @last_date_letter_was_sent = current_user.gpo_verification_pending_profile&.gpo_confirmation_codes.maximum(:code_sent_at)
         @gpo_verify_form = GpoVerifyForm.new(user: current_user, pii: pii)
         @code = session[:last_gpo_confirmation_code] if FeatureManagement.reveal_gpo_code?
 
         @should_prompt_user_to_request_another_letter =
           FeatureManagement.gpo_verification_enabled? &&
-          !gpo_mail.mail_spammed? &&
+          !@gpo_mail_spammed &&
           !gpo_mail.profile_too_old?
 
         if pii_locked?
