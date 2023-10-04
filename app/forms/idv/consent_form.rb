6cd44b828209395c056ae164d078ba18e70e8e15
Matt Hinz <matt.hinz@gsa.gov>
2023-10-04 12:28:28 -0700

    Clean up ial2_consent_given vs idv_consent_given

--- app/forms/idv/consent_form.rb
+++ app/forms/idv/consent_form.rb
@@ -2,7 +2,9 @@ module Idv
   class ConsentForm
     include ActiveModel::Model
 
-    validates :ial2_consent_given,
+    attr_reader :idv_consent_given
+
+    validates :idv_consent_given,
               acceptance: { message: proc { I18n.t('errors.doc_auth.consent_form') } }
 
     def initialize(idv_consent_given: false)
@@ -13,14 +15,6 @@ module Idv
       @idv_consent_given = params[:idv_consent_given] == '1'
 
       FormResponse.new(success: valid?, errors: errors)
-    end
-
-    def ial2_consent_given
-      idv_consent_given?
-    end
-
-    def idv_consent_given?
-      @idv_consent_given
     end
   end
 end
