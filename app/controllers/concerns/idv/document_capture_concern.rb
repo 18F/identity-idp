d971cba2a9b64f0c2a864892f80fc9bd82947c27
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-10-10 12:39:31 -0400

    LG-11012 follow up PR to remove load doc auth async result (#9327)
    * remove load_doc_auth_async result unsed method
    
    * fix tests not to rely on load stored result async
    
    * changelog: Internal, Code Quality, remove unused async doc auth loading
    
    * remove DocumentCaptureSessionAsyncResult from codebase
    
    * resolve merge conflict
    
    * remove no longer needed begin block
    
    * remove lingering end

--- app/controllers/concerns/idv/document_capture_concern.rb
+++ app/controllers/concerns/idv/document_capture_concern.rb
@@ -30,7 +30,6 @@ module Idv
     end
 
     # @param [DocAuth::Response,
-    #   DocumentCaptureSessionAsyncResult,
     #   DocumentCaptureSessionResult] response
     def extract_pii_from_doc(user, response, store_in_session: false)
       pii_from_doc = response.pii_from_doc.merge(
