28294b9b201888e4c1796c4f5deb861b3cefbc88
Mitchell Henke <mitchell.henke@gsa.gov>
2023-10-10 14:29:27 -0500

    update to rack 3
    changelog: Internal, Dependencies, Upgrade to Rack 3

--- lib/secure_cookies.rb
+++ lib/secure_cookies.rb
@@ -2,7 +2,6 @@
 
 # Reimplements SecureHeaders secure cookie functionality to make sure all cookies are secure
 class SecureCookies
-  COOKIE_SEPARATOR = "\n"
   SECURE_REGEX = /; Secure/i
   HTTP_ONLY_REGEX = /; HttpOnly/i
   SAME_SITE_REGEX = /; SameSite/i
@@ -15,17 +14,14 @@ class SecureCookies
     status, headers, body = @app.call(env)
 
     if (cookie_header = headers['Set-Cookie']).present?
-      cookies = cookie_header.split(COOKIE_SEPARATOR)
 
-      cookies.each do |cookie|
+      Array(cookie_header).each do |cookie|
         next if cookie.blank?
 
         cookie << '; Secure' if env['HTTPS'] == 'on' && !cookie.match?(SECURE_REGEX)
         cookie << '; HttpOnly' if !cookie.match?(HTTP_ONLY_REGEX)
         cookie << '; SameSite=Lax' if !cookie.match?(SAME_SITE_REGEX)
       end
-
-      headers['Set-Cookie'] = cookies.join(COOKIE_SEPARATOR)
     end
 
     [status, headers, body]
