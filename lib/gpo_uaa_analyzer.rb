75d1e5f0dc09d7d085a29b4f928ea83fbe273649
Douglas Price <douglas.price@gsa.gov>
2023-10-12 19:11:55 -0400

    LG-10293: Collect data on GPO UAAs
    UAA: Undeliverable As Addressed
    
    First cut at code to collect requested data on UAAs.
    
    Co-authored-by: Sonia Connolly <sonia.connolly@gsa.gov>

---
+++ lib/gpo_uaa_analyzer.rb
@@ -0,0 +1,55 @@
+class GpoUaaAnalyzer
+  def doit
+    data = []
+    get_codes.each do |code|
+      gpo_confirmation_code = GpoConfirmationCode.first_with_otp(code)
+      profile = gpo_confirmation_code.profile
+      data << [
+        code,
+        gpo_confirmation_code.created_at,
+        profile.verified_at,
+        which_letter(profile, gpo_confirmation_code),
+        profile.gpo_confirmation_codes.length,
+        profile.user.uuid,
+      ].join(',')
+    end
+    data
+  end
+
+  def header
+    %w[
+      OTP
+      created_at
+      verified_at
+      which_letter
+      total_letters
+      uuid
+    ].join(',')
+  end
+
+  def writeit
+    result = doit
+    File.open('/tmp/foo.csv', 'w') do |f|
+      f.write header + "\n"
+      result.each do |line|
+        f.write line + "\n"
+      end
+    end
+  end
+
+  def putsit
+    puts header
+    doit.each do |line|
+      puts line
+    end
+  end
+
+  def which_letter(profile, gpo_confirmation_code)
+    profile.gpo_confirmation_codes.sort_by(&:code_sent_at).
+      index(gpo_confirmation_code) + 1
+  end
+
+  def get_codes
+    []
+  end
+end
