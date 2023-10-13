c330a57644993e2feb5a380c2e80c003c25b0a3e
Alex Bradley <alexander.bradley@gsa.gov>
2023-10-13 14:26:16 -0400

    current step indicator for user not in gpo flow yet

--- app/controllers/idv/confirm_start_over_controller.rb
+++ app/controllers/idv/confirm_start_over_controller.rb
@@ -8,7 +8,14 @@ module Idv
     before_action :confirm_idv_needed
 
     def index
+      @current_step = requested_letter_before? ? :get_a_letter : :verify_phone_or_address
       analytics.idv_gpo_confirm_start_over_visited
+    end
+
+    private
+
+    def requested_letter_before?
+      current_user&.gpo_verification_pending_profile?
     end
   end
 end
