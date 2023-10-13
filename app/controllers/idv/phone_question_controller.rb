8a261422f8ba5fd270cbc54953bfaf50b5f56b46
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2023-10-13 13:15:23 -0400

    LG-11091: phone question links routing (#9315)
    * create destination for phone_question links
    
    * changelog: User-Facing Improvements, Document Authentication, Identity verificcation phone question page buttons routing
    
    * phone question page no phone link go to document capture with no_phone param
    
    * happy linting
    
    * change phone question page  param to camera phone
    
    * happy linting
    
    * refactor tests
    
    * add routes to answer phone_question page
    
    * update phone question controller specs to test phone answers
    
    * happy linting
    
    * setup a/b testing routing and logging for phone question page
    
    * analytics spec to include phone_question logging
    
    Co-authored-by: Sonia Connolly <sonia.connolly@gsa.gov>
    
    * add phone question ab testing logging into api image uploads
    
    * add phone_question_ab_test_bucket to image uploads controller spec
    
    * updater phone question controller spec to use phone_with_camera
    
    * happy linting
    
    * remove unused phone_question_visited from idv session
    
    * add a/b phone question to doc auth warning log
    
    * test hybrid handoffs usage of maybe_redirect_for_phone_question_ab_test
    
    * happy linting
    
    * add phone question ab test bucket to api image upload form spec
    
    * test phone quesiton ab test concern
    
    * verify phone_with_camera is logged on phone quesiton page
    
    * update analytics events naming for phone question
    
    * add param doc for phone_question_ab_test_bucket
    
    * fix typo
    
    * alphabetize new analytics events
    
    * Update spec/controllers/idv/phone_question_controller_spec.rb
    
    Co-authored-by: Sonia Connolly <sonia.connolly@gsa.gov>
    
    * verify that link sent returns to hybrid handoff after phone_question page has been visited
    
    * happy linting
    
    * add missing phone_question_ab_test_bucket: :bypass_phone_question in analytics events spec
    
    ---------
    
    Co-authored-by: Sonia Connolly <sonia.connolly@gsa.gov>

--- app/controllers/idv/phone_question_controller.rb
+++ app/controllers/idv/phone_question_controller.rb
@@ -10,13 +10,45 @@ module Idv
     before_action :confirm_hybrid_handoff_needed, only: :show
 
     def show
+      analytics.idv_doc_auth_phone_question_visited(**analytics_arguments)
+
       @title = t('doc_auth.headings.phone_question')
     end
+
+    def phone_with_camera
+      analytics.idv_doc_auth_phone_question_submitted(
+        **analytics_arguments.
+                merge(phone_with_camera: true),
+      )
+
+      redirect_to idv_hybrid_handoff_url
+    end
+
+    def phone_without_camera
+      idv_session.flow_path = 'standard'
+      analytics.idv_doc_auth_phone_question_submitted(
+        **analytics_arguments.
+                merge(phone_with_camera: false),
+      )
+
+      redirect_to idv_hybrid_handoff_url
+    end
+
+    private
 
     def confirm_agreement_step_complete
       return if idv_session.idv_consent_given
 
       redirect_to idv_agreement_url
+    end
+
+    def analytics_arguments
+      {
+        step: 'phone_question',
+        analytics_id: 'Doc Auth',
+        skip_hybrid_handoff: idv_session.skip_hybrid_handoff,
+        irs_reproofing: irs_reproofing?,
+      }.merge(ab_test_analytics_buckets)
     end
   end
 end
