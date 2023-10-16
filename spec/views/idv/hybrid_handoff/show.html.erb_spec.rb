d9bbd1cbb35676f6cac7f840c4f2836530da2165
Brittany Greaner <brittany.greaner@gsa.gov>
2023-10-16 16:04:44 -0700

    Update to use controller var + tests
    While updating the tests, I found there was some additional setup in the
    tests that was not needed, so I removed that as well.

--- spec/views/idv/hybrid_handoff/show.html.erb_spec.rb
+++ spec/views/idv/hybrid_handoff/show.html.erb_spec.rb
@@ -1,14 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
-  let(:show_phone_question) { 0 }
-  let(:step_indicator_steps) { Idv::StepIndicatorConcern::STEP_INDICATOR_STEPS }
-
   before do
-    allow(view).to receive(:step_indicator_steps).and_return(step_indicator_steps)
     allow(view).to receive(:current_user).and_return(@user)
-    allow(IdentityConfig.store).to receive(:idv_phone_question_a_b_testing).
-      and_return(show_phone_question:)
     @idv_form = Idv::PhoneForm.new(user: build_stubbed(:user), previous_params: nil)
   end
 
@@ -19,7 +13,9 @@ RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
   end
 
   context 'with shown phone question' do
-    let(:show_phone_question) { 1 }
+    before do
+      @phone_question_ab_test_bucket = :show_phone_question
+    end
 
     it 'displays the expected headings from the "b" case' do
       expect(rendered).to have_selector('h1', text: t('doc_auth.headings.upload_from_phone'))
@@ -30,7 +26,9 @@ RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
   end
 
   context 'without shown phone question' do
-    let(:show_phone_question) { 0 }
+    before do
+      @phone_question_ab_test_bucket = :bypass_phone_question
+    end
 
     it 'displays the expected headings from the "a" case' do
       expect(rendered).to have_selector('h1', text: t('doc_auth.headings.hybrid_handoff'))
