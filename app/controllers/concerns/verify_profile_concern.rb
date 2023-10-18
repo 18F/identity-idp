9b739d9d326db299b158e5d0b50e3af51166f904
Malick Diarra <malick.diarra@gsa.gov>
2023-10-18 10:23:59 -0400

    changelog: User-Facing Improvements, Authentication, In person proofing waiting should allow access to account page

--- app/controllers/concerns/verify_profile_concern.rb
+++ app/controllers/concerns/verify_profile_concern.rb
@@ -3,9 +3,16 @@ module VerifyProfileConcern
 
   def url_for_pending_profile_reason
     return idv_verify_by_mail_enter_code_url if current_user.gpo_verification_pending_profile?
-    return idv_in_person_ready_to_verify_url if current_user.in_person_pending_profile?
+    if current_user.in_person_pending_profile? 
+      if ial2_requested?
+        return idv_in_person_ready_to_verify_url
+      else
+        return account_path
+      end
+    end
     return idv_please_call_url if current_user.fraud_review_pending?
-    idv_not_verified_url if current_user.fraud_rejection?
+    return idv_not_verified_url if current_user.fraud_rejection?
+    account_path
   end
 
   def user_has_pending_profile?
