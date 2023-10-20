5bee10c4f5e6374899267b6a068411aa61ba3149
John Maxwell <john.maxwell@gsa.gov>
2023-10-20 16:57:07 -0400

    Updated template.

--- app/controllers/idv/by_mail/enter_code_controller.rb
+++ app/controllers/idv/by_mail/enter_code_controller.rb
@@ -23,16 +23,18 @@ module Idv
           return
         end
 
-        gpo_mail = Idv::GpoMail.new(current_user)
-        @gpo_mail_spammed = gpo_mail.mail_spammed?
         @last_date_letter_was_sent = last_date_letter_was_sent
         @gpo_verify_form = GpoVerifyForm.new(user: current_user, pii: pii)
         @code = session[:last_gpo_confirmation_code] if FeatureManagement.reveal_gpo_code?
 
-        @should_prompt_user_to_request_another_letter =
+        gpo_mail = Idv::GpoMail.new(current_user)
+        @can_request_another_letter =
           FeatureManagement.gpo_verification_enabled? &&
-          !@gpo_mail_spammed &&
+          !gpo_mail.mail_spammed? &&
           !gpo_mail.profile_too_old?
+
+        puts "gpo_mail.mail_spammed?: #{gpo_mail.mail_spammed?}"
+        puts "gpo_mail.profile_too_old?: #{gpo_mail.profile_too_old?}"
 
         if pii_locked?
           redirect_to capture_password_url
@@ -156,8 +158,11 @@ module Idv
       end
 
       def last_date_letter_was_sent
-        current_user.gpo_verification_pending_profile&.gpo_confirmation_codes&.
-          pluck(:updated_at)&.max
+        current_user.
+          gpo_verification_pending_profile&.
+          gpo_confirmation_codes&.
+          pluck(:updated_at)&.
+          max
       end
     end
   end
