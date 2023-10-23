2295570fa6c672dfc174fcd9c0b4af7cd1deb832
charleyf <charleyferguson@navapbc.com>
2023-10-23 14:53:41 -0400

    Extract step order code

--- app/javascript/packages/document-capture/components/document-capture.tsx
+++ app/javascript/packages/document-capture/components/document-capture.tsx
@@ -79,58 +79,7 @@ function DocumentCapture({ onStepChange = () => {} }: DocumentCaptureProps) {
     initialValues = formValues;
   }
 
-  const inPersonLocationPostOfficeSearchForm = inPersonFullAddressEntryEnabled
-    ? InPersonLocationFullAddressEntryPostOfficeSearchStep
-    : InPersonLocationPostOfficeSearchStep;
-
-  const inPersonSteps: FormStep[] =
-    inPersonURL === undefined
-      ? []
-      : ([
-          {
-            name: 'prepare',
-            form: InPersonPrepareStep,
-            title: t('in_person_proofing.headings.prepare'),
-          },
-          {
-            name: 'location',
-            form: inPersonLocationPostOfficeSearchForm,
-            title: t('in_person_proofing.headings.po_search.location'),
-          },
-          flowPath === 'hybrid' && {
-            name: 'switch_back',
-            form: InPersonSwitchBackStep,
-            title: t('in_person_proofing.headings.switch_back'),
-          },
-        ].filter(Boolean) as FormStep[]);
-
-  const steps: FormStep[] = submissionError
-    ? (
-        [
-          {
-            name: 'review',
-            form:
-              submissionError instanceof UploadFormEntriesError
-                ? withProps({
-                    remainingAttempts: submissionError.remainingAttempts,
-                    isFailedResult: submissionError.isFailedResult,
-                    isFailedDocType: submissionError.isFailedDocType,
-                    captureHints: submissionError.hints,
-                    pii: submissionError.pii,
-                    failedImageFingerprints: submissionError.failed_image_fingerprints,
-                  })(ReviewIssuesStep)
-                : ReviewIssuesStep,
-            title: t('errors.doc_auth.rate_limited_heading'),
-          },
-        ] as FormStep[]
-      ).concat(inPersonSteps)
-    : ([
-        {
-          name: 'documents',
-          form: DocumentsStep,
-          title: t('doc_auth.headings.document_capture'),
-        },
-      ].filter(Boolean) as FormStep[]);
+  const steps = getSteps(submissionError, inPersonURL, inPersonFullAddressEntryEnabled, flowPath);
 
   const stepIndicatorPath =
     stepName && ['location', 'prepare', 'switch_back'].includes(stepName)
@@ -178,5 +127,74 @@ function DocumentCapture({ onStepChange = () => {} }: DocumentCaptureProps) {
     </>
   );
 }
+
+const getReviewStep = (submissionError) => {
+  const { t } = useI18n();
+
+  return {
+    name: 'review',
+    form:
+      submissionError instanceof UploadFormEntriesError
+        ? withProps({
+            remainingAttempts: submissionError.remainingAttempts,
+            isFailedResult: submissionError.isFailedResult,
+            isFailedDocType: submissionError.isFailedDocType,
+            captureHints: submissionError.hints,
+            pii: submissionError.pii,
+            failedImageFingerprints: submissionError.failed_image_fingerprints,
+          })(ReviewIssuesStep)
+        : ReviewIssuesStep,
+    title: t('errors.doc_auth.rate_limited_heading'),
+  };
+};
+
+const getSteps = (submissionError, inPersonURL, inPersonFullAddressEntryEnabled, flowPath) => {
+  const { t } = useI18n();
+  const reviewStep: FormStep = getReviewStep(submissionError);
+
+  if (!submissionError) {
+    return [
+      {
+        name: 'documents',
+        form: DocumentsStep,
+        title: t('doc_auth.headings.document_capture'),
+      },
+    ];
+  }
+
+  const locationStep: FormStep = inPersonFullAddressEntryEnabled
+    ? {
+        name: 'location',
+        form: InPersonLocationFullAddressEntryPostOfficeSearchStep,
+        title: t('in_person_proofing.headings.po_search.location'),
+      }
+    : {
+        name: 'location',
+        form: InPersonLocationPostOfficeSearchStep,
+        title: t('in_person_proofing.headings.po_search.location'),
+      };
+
+  const inPersonProofingSteps: FormStep[] = !inPersonURL
+    ? []
+    : [
+        {
+          name: 'prepare',
+          form: InPersonPrepareStep,
+          title: t('in_person_proofing.headings.prepare'),
+        },
+        locationStep,
+      ];
+
+  const steps: FormStep[] = [reviewStep].concat(inPersonProofingSteps);
+
+  if (flowPath === 'hybrid') {
+    return steps.concat({
+      name: 'switch_back',
+      form: InPersonSwitchBackStep,
+      title: t('in_person_proofing.headings.switch_back'),
+    });
+  }
+  return steps;
+};
 
 export default DocumentCapture;
