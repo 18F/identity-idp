0e629f35d9b628a03b56d381225511c6f156bc19
JackRyan1989 <jack.ryan@gsa.gov>
2023-10-26 12:13:01 -0400

    Removing unneeded redirects for in_person_proofing/ssn url

--- config/routes.rb
+++ config/routes.rb
@@ -377,15 +377,6 @@ Rails.application.routes.draw do
           # sometimes underscores get messed up when linked to via SMS
           as: :capture_doc_dashes
 
-      # DEPRECATION NOTICE
-      # Usage of the /in_person_proofing/ssn routes is deprecated.
-      # Use the /in_person/ssn routes instead.
-      #
-      # These have been left in temporarily to prevent any impact to users
-      # during the deprecation process.
-      get '/in_person_proofing/ssn' => redirect('/verify/in_person/ssn', status: 307)
-      put '/in_person_proofing/ssn' => redirect('/verify/in_person/ssn', status: 307)
-
       get '/in_person' => 'in_person#index'
       get '/in_person/ready_to_verify' => 'in_person/ready_to_verify#show',
           as: :in_person_ready_to_verify
