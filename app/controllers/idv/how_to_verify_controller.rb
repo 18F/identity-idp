05b488604e1e4676e65b9d4204c9632f7c959b47
Matt Gardner <wilburnforce@gmail.com>
2023-10-26 17:18:38 -0400

    404 the page if disabled

--- app/controllers/idv/how_to_verify_controller.rb
+++ app/controllers/idv/how_to_verify_controller.rb
@@ -1,5 +1,9 @@
 module Idv
   class HowToVerifyController < ApplicationController
+    include RenderConditionConcern
+
+    check_or_render_not_found -> { enabled? }
+
     def show
       @idv_how_to_verify_form = Idv::HowToVerifyForm.new
     end
@@ -16,6 +20,10 @@ module Idv
 
     def how_to_verify_form_params
       params.require(:idv_how_to_verify_form).permit(:selection)
+    end
+
+    def enabled?
+      IdentityConfig.store.in_person_proofing_opt_in_option
     end
   end
 end
