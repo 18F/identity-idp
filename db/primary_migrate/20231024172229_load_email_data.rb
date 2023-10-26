ad3074106f2695c85450a2891a010c7077fd7d78
Malick Diarra <malick.diarra@gsa.gov>
2023-10-26 11:04:17 -0400

    delete load

--- db/primary_migrate/20231024172229_load_email_data.rb
+++
@@ -1,9 +0,0 @@
-class LoadEmailData < ActiveRecord::Migration[7.0]
-  def change
-    ActiveRecord::Base.connection.execute <<~PG
-      COPY disposable_domains (name)
-      FROM '#{EmailData.data_dir.join('disposable_domains')}.txt'
-      (FORMAT CSV)
-    PG
-  end
-end
