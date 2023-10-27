0847cca6b654fa4429d6029fbece03f2f0c220d4
kevinsmaster5 <kevin.masters@gsa.gov>
2023-10-27 09:04:03 -0400

    changelog: Internal, tech debt, Break up MFA selection presenter classes for Webauthn

---
+++ app/presenters/two_factor_authentication/sign_in_webauthn_platform_selection_presenter.rb
@@ -0,0 +1,28 @@
+module TwoFactorAuthentication
+  class SignInWebauthnPlatformSelectionPresenter < SelectionPresenter
+    def method
+      :webauthn_platform
+    end
+
+    def render_in(view_context, &block)
+      view_context.render(
+        WebauthnInputComponent.new(
+          platform: true,
+          passkey_supported_only: configuration.blank?,
+          show_unsupported_passkey:
+            configuration.blank? &&
+            IdentityConfig.store.show_unsupported_passkey_platform_authentication_setup,
+        ),
+        &block
+      )
+    end
+
+    def label
+      t('two_factor_authentication.login_options.webauthn_platform')
+    end
+
+    def info
+      t('two_factor_authentication.login_options.webauthn_platform_info', app_name: APP_NAME)
+    end
+  end
+end
