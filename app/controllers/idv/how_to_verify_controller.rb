78d1d5837a6ea705e17d5eec88158992c0f71dda
Matt Gardner <wilburnforce@gmail.com>
2023-10-30 12:18:32 -0400

    Prefer strings

--- app/controllers/idv/how_to_verify_controller.rb
+++ app/controllers/idv/how_to_verify_controller.rb
@@ -4,17 +4,14 @@ module Idv
 
     check_or_render_not_found -> { enabled? }
 
-    VERIFICATION_OPTIONS = %w[ipp remote].freeze
-
     def show
       analytics.idv_doc_auth_how_to_verify_visited(**analytics_arguments)
       @idv_how_to_verify_form = Idv::HowToVerifyForm.new
-      @verification_options = VERIFICATION_OPTIONS
     end
 
     def update
       analytics.idv_doc_auth_how_to_verify_submitted(**analytics_arguments)
-      if how_to_verify_form_params['selection'] == VERIFICATION_OPTIONS[:ipp]
+      if VERIFICATION_OPTIONS.include?(how_to_verify_form_params['selection'])
         redirect_to idv_document_capture_url
       else
         redirect_to idv_hybrid_handoff_url
