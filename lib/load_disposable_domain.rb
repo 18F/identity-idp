04eaf69326d721721a92d8b3152fd295b247932d
Malick Diarra <malick.diarra@gsa.gov>
2023-10-31 09:44:26 -0400

    rubocop

--- lib/load_disposable_domain.rb
+++ lib/load_disposable_domain.rb
@@ -2,6 +2,6 @@ class LoadDisposableDomain
   def self.load_disposable_domains(url)
     Faraday.get(url).body.each_line do |line|
       DisposableDomain.find_or_create_by(name: line)
-    end    
+    end
   end
 end
