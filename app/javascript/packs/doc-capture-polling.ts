ca4e6c0d30d1c6eab1c5847cbede474da1286b8c
charleyf <charleyferguson@navapbc.com>
2023-10-31 11:10:17 -0400

    Add missing closing brackets on query selector

--- app/javascript/packs/doc-capture-polling.ts
+++ app/javascript/packs/doc-capture-polling.ts
@@ -5,10 +5,10 @@ new DocumentCapturePolling({
     .querySelector('[data-status-endpoint]')
     ?.getAttribute('data-status-endpoint') as string,
   phoneQuestionAbTestBucket: document
-    .querySelector('[data-phone-question-ab-test-bucket')
+    .querySelector('[data-phone-question-ab-test-bucket]')
     ?.getAttribute('data-phone-question-ab-test-bucket') as string,
   phoneWithCamera: document
-    .querySelector('[data-phone-with-camera')
+    .querySelector('[data-phone-with-camera]')
     ?.getAttribute('data-phone-with-camera') as string,
   elements: {
     backLink: document.querySelector('.link-sent-back-link') as HTMLAnchorElement,
