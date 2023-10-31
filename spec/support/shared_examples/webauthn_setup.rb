58616d65ef60b4acaad79531d5c1a40f6613df70
Jessica Dembe <jessica.dembe@gsa.gov>
2023-10-31 10:33:17 -0400

    remove from support spec

--- spec/support/shared_examples/webauthn_setup.rb
+++ spec/support/shared_examples/webauthn_setup.rb
@@ -1,12 +1,4 @@
 RSpec.shared_examples 'webauthn setup' do
-  let(:fake_analytics) { FakeAnalytics.new }
-  let(:submitted_error_hash) do
-    { name: [I18n.t(
-      'errors.webauthn_platform_setup.attestation_error',
-      link: MarketingSite.contact_url,
-    )] }
-  end
-
   it 'allows a user to setup webauthn' do
     mock_webauthn_setup_challenge
     visit_webauthn_setup
@@ -34,20 +26,6 @@ RSpec.shared_examples 'webauthn setup' do
     mock_press_button_on_hardware_key_on_setup
 
     expect_webauthn_setup_error
-  end
-
-  it 'logs an error when submit fails' do
-    visit_webauthn_setup
-
-    fill_in_nickname_and_click_continue
-    mock_press_button_on_hardware_key_on_setup
-
-    expect_webauthn_setup_error
-
-    expect(fake_analytics).to have_logged_event(
-      :webauthn_setup_submitted,
-      :submitted_error_hash,
-    )
   end
 
   it 'renders an error if the hardware key button has not been pressed' do
