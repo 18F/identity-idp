bb7425207df2556d5ab709c197b12eb8363b9923
kevinsmaster5 <kevin.masters@gsa.gov>
2023-11-01 12:45:06 -0400

    add back in missing test coverage

--- spec/presenters/two_factor_authentication/set_up_webauthn_platform_selection_presenter_spec.rb
+++ spec/presenters/two_factor_authentication/set_up_webauthn_platform_selection_presenter_spec.rb
@@ -17,6 +17,32 @@ RSpec.describe TwoFactorAuthentication::SetUpWebauthnPlatformSelectionPresenter 
     end
   end
 
+  describe '#render_in' do
+    it 'renders a WebauthnInputComponent' do
+      view_context = instance_double(ActionView::Base)
+      expect(view_context).to receive(:render) do |component, &block|
+        expect(component).to be_instance_of(WebauthnInputComponent)
+        expect(component.passkey_supported_only?).to be(true)
+        expect(block.call).to eq('content')
+      end
+
+      presenter.render_in(view_context) { 'content' }
+    end
+
+    context 'with configured authenticator' do
+      let(:user) { create(:user, :with_webauthn_platform) }
+
+      it 'renders a WebauthnInputComponent with passkey_supported_only false' do
+        view_context = instance_double(ActionView::Base)
+        expect(view_context).to receive(:render) do |component, &block|
+          expect(component.passkey_supported_only?).to be(false)
+        end
+
+        presenter.render_in(view_context)
+      end
+    end
+  end
+
   describe '#mfa_configuration' do
     it 'returns an empty string when user has not configured this authenticator' do
       expect(presenter_without_mfa.mfa_configuration_description).to eq('')
