a7402413240edc934845ee36cd2ea37c13c58237
John Maxwell <john.maxwell@gsa.gov>
2023-11-01 16:37:24 -0400

    Cleaned up matcher.

--- spec/support/matchers/aria_view_matchers.rb
+++ spec/support/matchers/aria_view_matchers.rb
@@ -1,9 +1,27 @@
+# Very minimal start on accessibility matchers for view specs.
+# Defines one matcher, specific to buttons generated with Rails'
+# `button_to` helper
+#
+# Matcher: `have_button_to_with_accessibility`
+# Use:
+#
+# expect(rendered).to have_button_to_with_accessibility('expected button text',
+#                                                       'expected button action')
+# or
+#
+# expect(rendered).to have_button_to_with_accessibility('expected button text',
+#                                                       'expected button action',
+#                                                       'expected aria label')
+#
+# In the first case, the expected aria-label attribute value will be
+# the expected button text.
+
 class HaveButtonToWithAccessibilityMatcher
   attr_reader :expected_button_text, :expected_action, :expected_aria_label, :actual
 
   def initialize(expected_button_text,
                  expected_action,
-                 expected_aria_label = expected_button_text)
+                 expected_aria_label)
     @expected_button_text = expected_button_text
     @expected_aria_label = expected_aria_label
     @expected_action = expected_action
@@ -19,10 +37,10 @@ class HaveButtonToWithAccessibilityMatcher
     return "expected to find button with text '#{expected_button_text}'" unless button
     return 'expected button to be in a (Rails) "button_to" form tag' unless enclosing_form
 
-    message = ''
-    message += aria_label_failure_message unless aria_label_ok?
-    message += action_failure_message unless action_ok?
-    message
+    messages = []
+    messages += aria_label_failure_message unless aria_label_ok?
+    messages += action_failure_message unless action_ok?
+    messages.join("\n")
   end
 
   private
@@ -56,8 +74,16 @@ class HaveButtonToWithAccessibilityMatcher
   end
 end
 
-RSpec::Matchers.define :have_button_to_with_accessibility do |expected_button_text, expected_action|
-  matcher = HaveButtonToWithAccessibilityMatcher.new(expected_button_text, expected_action)
+RSpec::Matchers.define :have_button_to_with_accessibility do |expected_button_text,
+                                                              expected_action,
+                                                              expected_aria_label =
+                                                                expected_button_text|
+  matcher = HaveButtonToWithAccessibilityMatcher.new(
+    expected_button_text,
+    expected_action,
+    expected_aria_label,
+  )
+
   match { |actual_text| matcher.match(actual_text) }
   failure_message { |actual_text| matcher.failure_message(actual_text) }
 end
