2cd0bbfdfcf739671622d613658b753c67f3ed6b
Andrew Duthie <andrew.duthie@gsa.gov>
2023-11-01 10:19:24 -0400

    Add feature spec coverage for SMS opt-in (#9497)
    changelog: Internal, Automated Testing, Improve test coverage for SMS opt-in

--- spec/controllers/two_factor_authentication/sms_opt_in_controller_spec.rb
+++ spec/controllers/two_factor_authentication/sms_opt_in_controller_spec.rb
@@ -53,8 +53,6 @@ RSpec.describe TwoFactorAuthentication::SmsOptInController do
     end
 
     context 'when loaded while adding a new phone' do
-      render_views
-
       let(:user) { create(:user) }
       let(:phone) { Faker::PhoneNumber.cell_phone_in_e164 }
       let(:opt_out_uuid) { PhoneNumberOptOut.create_or_find_with_phone(phone).uuid }
