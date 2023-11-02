8b0acc5e5693e909933a6d9955ad566206354122
Zach Margolis <zachmargolis@users.noreply.github.com>
2023-11-02 07:20:49 -0700

    Adds APG (Agency Priority Goals) user counts (LG-11156) (#9519)
    - The way we report on APGs will double-count unique users
    
    changelog: Internal, Reporting, Update monthly key metrics report

--- app/services/reporting/active_users_count_report.rb
+++ app/services/reporting/active_users_count_report.rb
@@ -14,6 +14,14 @@ module Reporting
       )
     end
 
+    def active_users_count_apg_emailable_report
+      EmailableReport.new(
+        title: 'Active Users (APG)',
+        table: generate_apg_report,
+        filename: 'active_users_count_apg',
+      )
+    end
+
     class ReportRow
       # hash comes from Db::Identity::SpActiveUserCounts.overall
       def self.from_hash_time_range(hash:, time_range:)
@@ -30,17 +38,6 @@ module Reporting
         @ial1 = ial1
         @idv = idv
         @time_range = time_range
-      end
-
-      def merge(other)
-        min_range = [time_range.begin, other.time_range.begin].min
-        max_range = [time_range.end, other.time_range.end].max
-
-        self.class.new(
-          ial1: ial1 + other.ial1,
-          idv: idv + other.idv,
-          time_range: min_range..max_range,
-        )
       end
 
       def as_csv(title:)
@@ -70,6 +67,18 @@ module Reporting
       ]
     end
 
+    def generate_apg_report
+      q1, q2, q3, q4 = apg_fiscal_year_active_users_per_quarter_cumulative
+
+      [
+        ['Active Users (APG)', 'IAL1', 'IDV', 'Total', 'Range start', 'Range end'],
+        q1.as_csv(title: 'Fiscal year Q1'),
+        q2.as_csv(title: 'Fiscal year Q2 cumulative'),
+        q3.as_csv(title: 'Fiscal year Q3 cumulative'),
+        q4.as_csv(title: 'Fiscal year Q4 cumulative'),
+      ]
+    end
+
     # @return [ReportRow]
     def monthly_active_users
       @monthly_active_users ||= Reports::BaseReport.transaction_with_timeout do
@@ -85,35 +94,49 @@ module Reporting
 
     # @return [Array<ReportRow>]
     def fiscal_year_active_users_per_quarter_cumulative
-      @fiscal_year_active_users_per_quarter_cumulative ||= [
-        CalendarService.fiscal_start_date(report_date),
-        CalendarService.fiscal_q2_start(report_date),
-        CalendarService.fiscal_q3_start(report_date),
-        CalendarService.fiscal_q4_start(report_date),
-        CalendarService.fiscal_end_date(report_date).next_day(1),
-      ].each_cons(2).map do |quarter_start, next_start|
-        quarter_start.beginning_of_day..next_start.prev_day(1).end_of_day
-      end.map do |quarter_range|
-        Reports::BaseReport.transaction_with_timeout do
-          ReportRow.from_hash_time_range(
-            time_range: quarter_range,
-            hash: Db::Identity::SpActiveUserCounts.overall(
-              quarter_range.begin,
-              quarter_range.end,
-            ).first,
-          )
+      @fiscal_year_active_users_per_quarter_cumulative ||= cumulative_quarter_ranges.
+        map do |quarter_range|
+          Reports::BaseReport.transaction_with_timeout do
+            ReportRow.from_hash_time_range(
+              time_range: quarter_range,
+              hash: Db::Identity::SpActiveUserCounts.overall(
+                quarter_range.begin,
+                quarter_range.end,
+              ).first,
+            )
+          end
         end
-      end.yield_self do |quarters_array|
-        q1, q2, q3, q4 = quarters_array
+    end
 
-        [
-          q1,
-          q1.merge(q2),
-          q1.merge(q2).merge(q3),
-          q1.merge(q2).merge(q3).merge(q4),
-        ]
+    # @return [Array<ReportRow>]
+    def apg_fiscal_year_active_users_per_quarter_cumulative
+      @apg_fiscal_year_active_users_per_quarter_cumulative ||= cumulative_quarter_ranges.
+        map do |quarter_range|
+          Reports::BaseReport.transaction_with_timeout do
+            ReportRow.from_hash_time_range(
+              time_range: quarter_range,
+              hash: Db::Identity::SpActiveUserCounts.overall_apg(
+                quarter_range.begin,
+                quarter_range.end,
+              ).first,
+            )
+          end
+        end
+    end
+
+    # rubocop:disable Layout/LineLength
+    # @return [Array<Range<Time>>]
+    def cumulative_quarter_ranges
+      [
+        CalendarService.fiscal_start_date(report_date)..CalendarService.fiscal_q2_start(report_date),
+        CalendarService.fiscal_start_date(report_date)..CalendarService.fiscal_q3_start(report_date),
+        CalendarService.fiscal_start_date(report_date)..CalendarService.fiscal_q4_start(report_date),
+        CalendarService.fiscal_start_date(report_date)..CalendarService.fiscal_end_date(report_date).next_day(1),
+      ].map do |range|
+        range.begin.beginning_of_day..range.end.prev_day(1).end_of_day
       end
     end
+    # rubocop:enable Layout/LineLength
 
     def monthly_range
       report_date.all_month
