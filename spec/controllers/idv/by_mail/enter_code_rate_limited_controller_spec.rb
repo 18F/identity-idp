32b5cec29f24497010f894dcc1ca20c766374047
Alex Bradley <alexander.bradley@gsa.gov>
2023-11-02 15:54:37 -0400

    use an actual gpo pending profile

--- spec/controllers/idv/by_mail/enter_code_rate_limited_controller_spec.rb
+++ spec/controllers/idv/by_mail/enter_code_rate_limited_controller_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Idv::ByMail::EnterCodeRateLimitedController do
-  let(:user) { build_stubbed(:user, :fully_registered) }
-  let(:gpo_verification_pending_profile) { true }
+  # let(:user) { build_stubbed(:user, :fully_registered) }
+  let(:user) { build_stubbed(:user, :with_pending_gpo_profile) }
+  let(:pending_profile) do
+    if user
+      create(:profile, :verify_by_mail_pending, user: user)
+    end
+  end
+  let(:has_pending_profile) { true }
 
   before do
-    allow(user).to receive(:gpo_verification_pending_profile?).
-      and_return(gpo_verification_pending_profile)
     stub_sign_in(user)
+    stub_user_with_pending_profile(user)
     stub_analytics
     stub_attempts_tracker
     RateLimiter.new(rate_limit_type: :verify_gpo_key, user: user).increment_to_limited!
