19dceee88324a6183aa318c3d31c5cea3757a488
Andrew Duthie <andrew.duthie@gsa.gov>
2023-11-03 09:00:42 -0400

    Add specs for title helpers
    changelog: Internal, Accessibility, Hide missing title errors from user

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper do
+  describe '#title' do
+    let(:assigned_title) { nil }
+    let(:raise_on_missing_title) { nil }
+    subject(:title) { helper.title }
+
+    before do
+      helper.title = assigned_title if assigned_title
+    end
+
+    context 'with a title assigned' do
+      let(:assigned_title) { 'example' }
+
+      it 'returns the assigned title' do
+        expect(title).to eq(assigned_title)
+      end
+    end
+
+    context 'without a title assigned' do
+      before do
+        allow(IdentityConfig.store).to receive(:raise_on_missing_title).
+          and_return(raise_on_missing_title)
+      end
+
+      context 'configured not to raise on missing title' do
+        let(:raise_on_missing_title) { false }
+
+        it 'returns an empty string' do
+          expect(title).to eq('')
+        end
+
+        it 'notices an error' do
+          expect(NewRelic::Agent).to receive(:notice_error).with(
+            instance_of(ApplicationHelper::MissingTitleError),
+          )
+
+          title
+        end
+      end
+
+      context 'configured not to raise on missing title' do
+        let(:raise_on_missing_title) { true }
+
+        it 'raises an error' do
+          expect { title }.to raise_error(ApplicationHelper::MissingTitleError)
+        end
+      end
+    end
+  end
+
+  describe '#title=' do
+    let(:title) { 'example' }
+
+    it 'assigns the title region content' do
+      helper.title = title
+
+      expect(helper.view_flow.get(:title)).to eq(title)
+    end
+  end
+
   describe '#session_with_trust?' do
     context 'no user present' do
       before do
