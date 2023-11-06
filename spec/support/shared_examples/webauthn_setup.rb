52cfbc7266aca180da62e49cb486ce333dec2e3f
Jessica Dembe <jessica.dembe@gsa.gov>
2023-11-06 13:06:52 -0500

    test theory on setting up user

--- spec/support/shared_examples/webauthn_setup.rb
+++ spec/support/shared_examples/webauthn_setup.rb
@@ -41,6 +41,7 @@ RSpec.shared_examples 'webauthn setup' do
 
   context 'platform authenticator logging' do
     let(:fake_analytics) { FakeAnalytics.new }
+    let(:user) { sign_up_and_set_password }
 
     before do
       allow(IdentityConfig.store).to receive(:platform_auth_set_up_enabled).and_return(true)
