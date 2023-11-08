1088056bfb70f2a154c789161d1d63b1425c4954
charleyf <charleyferguson@navapbc.com>
2023-11-08 16:18:20 -0500

    Log various callbacks

--- app/javascript/packages/document-capture/components/acuant-camera.tsx
+++ app/javascript/packages/document-capture/components/acuant-camera.tsx
@@ -336,32 +336,41 @@ function AcuantCamera({
   );
   const faceCaptureCallback = {
     onDetectorInitialized: () => {
+      console.log('onDetectorInitialized');
       // This callback is triggered when the face detector is ready.
       // Until then, no actions are executed and the user sees only the camera stream.
       // You can opt to display an alert before the callback is triggered.
     },
     onDetection: (text) => {
+      console.log('onDetection', text);
       // Triggered when the face does not pass the scan. The UI element
       // should be updated here to provide guidence to the user
     },
     onOpened: () => {
       // Camera has opened
+      console.log('onOpened');
     },
     onClosed: () => {
       // Camera has closed
+      console.log('onClosed');
     },
     onError: (error) => {
       // Error occurred. Camera permission not granted will
       // manifest here with 1 as error code. Unexpected errors will have 2 as error code.
+      console.log('onError', error);
     },
     onPhotoTaken: () => {
       // The photo has been taken and it's showing a preview with a button to accept or retake the image.
+      console.log('onPhotoTaken');
     },
     onPhotoRetake: () => {
       // Triggered when retake button is tapped
+      console.log('onPhotoRetake');
     },
     onCaptured: (base64Image) => {
       // Triggered when accept button is tapped
+      console.log('onCaptured');
+      //onImageCaptureSuccess({image: base64Image});
     },
   };
 
@@ -415,7 +424,7 @@ function AcuantCamera({
     const startSelfieCamera = () => {
       window.AcuantPassiveLiveness = getActualAcuantPassiveLiveness();
       // This opens the native camera, but TODO callbacks
-      // window.AcuantPassiveLiveness.startManualCapture();
+      //window.AcuantPassiveLiveness.startManualCapture((image) => console.log('image', image));
       window.AcuantPassiveLiveness.start(faceCaptureCallback, faceDetectionStates);
       setIsActive(true);
     };
