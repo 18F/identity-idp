84f87e9ed07a22c539d91ce5084334e6df6f4b72
Douglas Price <douglas.price@gsa.gov>
2023-11-09 12:41:50 -0500

    LG-11343: removes the alert banner telling users the one-time code is now called verification code. Enough time has passed that any letters sent with the "one-time code" language have expired.
    changelog: User-Facing Improvements, Verify by Mail, Removed an alert banner no longer needed.

--- spec/features/idv/steps/enter_code_step_spec.rb
+++ spec/features/idv/steps/enter_code_step_spec.rb
@@ -154,7 +154,12 @@ RSpec.feature 'idv enter letter code step' do
 
       expect(current_path).to eq idv_verify_by_mail_enter_code_path
       expect(page).to have_content t('idv.gpo.alert_info')
-      expect(page).to have_content strip_tags(t('idv.gpo.change_to_verification_code_html'))
+      # The following test ensures that removed content is actually removed. The text
+      # is hardcoded since the i18n entries have been removed. Once we're satisfied,
+      # this can be removed.
+      expect(page).not_to have_content(
+        'The one-time code from your letter is now referred to as verification code',
+      )
       expect(page).to have_content t('idv.gpo.wrong_address')
       expect(page).to have_content Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE[:address1]
       verify_no_rate_limit_banner
