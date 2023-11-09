ff744b9a0b177f88c0faeea71bc5615791f2d083
Mitchell Henke <mitchell.henke@gsa.gov>
2023-11-09 11:21:49 -0600

    use string keys

--- scripts/review_app_service_providers.rb
+++ scripts/review_app_service_providers.rb
@@ -5,19 +5,19 @@ require 'yaml'
 dashboard_url = "https://#{ENV.fetch('CI_ENVIRONMENT_SLUG')}-review-app-dashboard.review-app.identitysandbox.gov"
 
 hash = {
-  production: {
+  'production' => {
     'urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:dashboard' => {
-      friendly_name: 'Dashboard',
-      agency: 'GSA',
-      agency_id: 2,
-      logo: '18f.svg',
-      certs: ['identity_dashboard_cert'],
-      return_to_sp_url: dashboard_url,
-      redirect_uris: [
+      'friendly_name' => 'Dashboard',
+      'agency' => 'GSA',
+      'agency_id' => 2,
+      'logo' => '18f.svg',
+      'certs' => ['identity_dashboard_cert'],
+      'return_to_sp_url' => dashboard_url,
+      'redirect_uris' => [
         "#{dashboard_url}/auth/logindotgov/callback",
         dashboard_url,
       ],
-      push_notification_url: "#{dashboard_url}/api/security_events",
+      'push_notification_url' => "#{dashboard_url}/api/security_events",
     }
   }
 }
