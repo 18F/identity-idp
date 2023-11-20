e0208790c6feb6e7e111e8da87da956b0536d8d1
Charley Ferguson <charleyferguson@navapbc.com>
2023-11-20 10:19:09 -0500

    Remove unnessecary `React` import
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- app/javascript/packages/document-capture/components/acuant-selfie-camera.tsx
+++ app/javascript/packages/document-capture/components/acuant-selfie-camera.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import type { ReactNode } from 'react';
 import AcuantContext from '../context/acuant';
 
