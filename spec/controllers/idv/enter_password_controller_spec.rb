88b5bf07065160069ab3d9219b7dccb4438f0933
Douglas Price <douglas.price@gsa.gov>
2023-11-24 14:12:18 -0500

    move the stubbing of sign_in and attempts_tracker to global before

--- spec/controllers/idv/enter_password_controller_spec.rb
+++ spec/controllers/idv/enter_password_controller_spec.rb
@@ -31,6 +31,9 @@ RSpec.describe Idv::EnterPasswordController do
 
   before do
     stub_analytics
+    stub_sign_in(user)
+    stub_attempts_tracker
+    allow(@irs_attempts_api_tracker).to receive(:track_event)
     allow(IdentityConfig.store).to receive(:usps_mock_fallback).and_return(false)
     allow(subject).to receive(:ab_test_analytics_buckets).and_return(ab_test_args)
   end
@@ -65,9 +68,7 @@ RSpec.describe Idv::EnterPasswordController do
       end
     end
 
-    before(:each) do
-      stub_sign_in(user)
-      stub_attempts_tracker
+    before do
       routes.draw do
         post 'show' => 'idv/enter_password#show'
       end
@@ -112,10 +113,6 @@ RSpec.describe Idv::EnterPasswordController do
   end
 
   describe '#new' do
-    before do
-      stub_sign_in(user)
-    end
-
     context 'user has completed all steps' do
       before do
         idv_session
@@ -203,11 +200,6 @@ RSpec.describe Idv::EnterPasswordController do
   end
 
   describe '#create' do
-    before do
-      stub_sign_in(user)
-      allow(subject).to receive(:confirm_idv_applicant_created).and_return(true)
-    end
-
     context 'user fails to supply correct password' do
       before do
         idv_session
@@ -235,8 +227,6 @@ RSpec.describe Idv::EnterPasswordController do
     context 'user has completed all steps' do
       before do
         idv_session
-        stub_attempts_tracker
-        allow(@irs_attempts_api_tracker).to receive(:track_event)
       end
 
       it 'redirects to personal key path' do
@@ -564,9 +554,6 @@ RSpec.describe Idv::EnterPasswordController do
                     allow(IdentityConfig.store).to receive(:proofing_device_profiling).
                       and_return(proofing_device_profiling_state)
                     idv_session.threatmetrix_review_status = review_status
-                  end
-
-                  before(:each) do
                     stub_request_token
                   end
 
