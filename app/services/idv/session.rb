5044b0f9413b8477c6c1b1321cf747a663313bb7
Douglas Price <douglas.price@gsa.gov>
2023-11-24 13:38:48 -0500

    rename address_step_complete? to phone_or_address_step_complete?

--- app/services/idv/session.rb
+++ app/services/idv/session.rb
@@ -163,7 +163,7 @@ module Idv
       resolution_successful
     end
 
-    def address_step_complete?
+    def phone_or_address_step_complete?
       if address_verification_mechanism == 'gpo'
         true
       else
