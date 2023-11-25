af89d7865e7b089cfad69ced1a405be74c122de6
Sonia Connolly <sonia.connolly@gsa.gov>
2023-11-24 16:30:59 -0800

    EnterPasswordController and associated cleanup (#9656)
    * rename address_step_complete? to phone_or_address_step_complete?
    
    * remove spec for #confirm_idv_steps_complete because it no longer exists
    
    * rm unnecessary let(:applicant)s because it doesn't have the phone_confirmed_at attribute
    
    * remove mocks of confirm_idv_applicant_created because that does not exist
    
    * move the stubbing of sign_in and attempts_tracker to global before
    
    * clean up befores
    
    * Clean up PhoneConfirmation concern
    
    The method was only used in PhoneSetupController, so move method & private method there and stop
    including this where it is not used.
    
    * FraudReviewConcern is already included via IdvStepConcern
    
    * Use idv_session.address_verification_mechanism rather than digging in user_session
    
    idv_session should always be available in EnterPasswordController and PersonalKeyController. This indirection
    is probably left over from FSM days.
    
    [skip changelog]
    
    * move the mock of usps_mock_fallback into the IPP context
    
    * add Idv::Session#verify_by_mail?
    
    * inline gpo_user_flow? method
    
    * fix failing specs
    
    ---------
    
    Co-authored-by: Douglas Price <douglas.price@gsa.gov>
    Co-authored-by: Matt Hinz <matt.hinz@gsa.gov>

--- app/controllers/users/phone_setup_controller.rb
+++ app/controllers/users/phone_setup_controller.rb
@@ -2,7 +2,6 @@ module Users
   class PhoneSetupController < ApplicationController
     include TwoFactorAuthenticatableMethods
     include UserAuthenticator
-    include PhoneConfirmation
     include MfaSetupConcern
     include RecaptchaConcern
     include ReauthenticationRequiredConcern
@@ -84,6 +83,28 @@ module Users
         flash[:error] = t('errors.messages.phone_duplicate')
         redirect_to phone_setup_url
       end
+    end
+
+    def prompt_to_confirm_phone(id:, phone:, selected_delivery_method: nil,
+                                selected_default_number: nil, phone_type: nil)
+
+      user_session[:unconfirmed_phone] = phone
+      user_session[:context] = 'confirmation'
+      user_session[:phone_type] = phone_type.to_s
+
+      redirect_to otp_send_url(
+        otp_delivery_selection_form: {
+          otp_delivery_preference: otp_delivery_method(id, phone, selected_delivery_method),
+          otp_make_default_number: selected_default_number,
+        },
+      )
+    end
+
+    def otp_delivery_method(_id, phone, selected_delivery_method)
+      capabilities = PhoneNumberCapabilities.new(phone, phone_confirmed: false)
+      return :sms if capabilities.sms_only?
+      return selected_delivery_method if selected_delivery_method.present?
+      current_user.otp_delivery_preference
     end
 
     def check_max_phone_numbers_per_account
