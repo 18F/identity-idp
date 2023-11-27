4668eaa4c8605414d529da4e0a0794db0ade6aeb
Luis <lmatos@flexion.us>
2023-11-27 12:07:55 -0600

    split SQL queries

--- app/services/reporting/account_reuse_report.rb
+++ app/services/reporting/account_reuse_report.rb
@@ -17,44 +17,39 @@ module Reporting
         '% of accounts',
       ]
 
-      total_reuse_report[:sp_reuse_stats].each do |result_entry|
-        account_reuse_table << [
-          "#{result_entry['num_sps']} apps",
-          result_entry['num_all_users'],
-          result_entry['all_percent'],
-          result_entry['num_idv_users'],
-          result_entry['idv_percent'],
-        ]
+      total_reuse_report.each do |report_key, report_value|
+        if account_reuse_table.length > 1
+          # Add a separator between the report sections
+          account_reuse_table <<
+            ['-----------', '--------------', '--------------', '---------------', '--------------']
+        end
+
+        report_results = report_value[:results]
+        total_metric = ''
+        if report_key == :sp_reuse_stats
+          report_results.each do |result|
+            account_reuse_table <<
+              ["#{result[:num_sps]} apps", result[:num_all_users], result[:all_percent],
+               result[:num_idv_users], result[:idv_percent]]
+          end
+
+          total_metric = '2+ apps'
+        end
+
+        if report_key == :agency_reuse_stats
+          report_results.each do |result|
+            account_reuse_table <<
+              ["#{result[:num_agencies]} agencies", result[:num_all_users], result[:all_percent],
+               result[:num_idv_users], result[:idv_percent]]
+          end
+
+          total_metric = '2+ agencies'
+        end
+
+        account_reuse_table <<
+          [total_metric, report_value[:total_all_users], report_value[:total_all_percent],
+           report_value[:total_idv_users], report_value[:total_idv_percent]]
       end
-
-      account_reuse_table << [
-        '2+ apps',
-        total_reuse_report[:sp_reuse_stats][:total_all_users],
-        total_reuse_report[:sp_reuse_stats][:total_all_percent],
-        total_reuse_report[:sp_reuse_stats][:total_idv_users],
-        total_reuse_report[:sp_reuse_stats][:total_idv_percent],
-      ]
-
-      # Blank line to separate multiple app use from multiple agency use
-      account_reuse_table << ['', '', '', '', '']
-
-      total_reuse_report[:agency_reuse_stats].each do |result_entry|
-        account_reuse_table << [
-          "#{result_entry['num_agencies']} agencies",
-          result_entry['num_all_users'],
-          result_entry['all_percent'],
-          result_entry['num_idv_users'],
-          result_entry['idv_percent'],
-        ]
-      end
-
-      account_reuse_table << [
-        '2+ agencies',
-        total_reuse_report[:agency_reuse_stats][:total_all_users],
-        total_reuse_report[:agency_reuse_stats][:total_all_percent],
-        total_reuse_report[:agency_reuse_stats][:total_idv_users],
-        total_reuse_report[:agency_reuse_stats][:total_idv_percent],
-      ]
 
       account_reuse_table
     end
@@ -70,22 +65,82 @@ module Reporting
     end
 
     def stats_month
-      report_date.strftime('%b-%Y')
+      report_date.prev_month(1).strftime('%b-%Y')
     end
 
     private
 
     def total_reuse_report
       return @total_reuse_report if defined?(@total_reuse_report)
+
+      reuse_results = {
+        sp: [{
+          num_sps: 0,
+          num_idv_users: 0,
+          num_all_users: 0,
+        }],
+        agency: [{
+          num_agencies: 0,
+          num_idv_users: 0,
+          num_all_users: 0,
+        }],
+      }
+
+      sp_reuse_results_idv.each do |result|
+        reuse_results[:sp][result[:num_sps]] = {
+          num_sps: result[:num_sps],
+          num_idv_users: result[:num_idv_users],
+          num_all_users: 0, # Fill it in with 'all' results later
+        }
+      end
+      sp_reuse_results_all.each do |result|
+        if reuse_results[:sp][result[:num_sps]].is_a?(Hash)
+          # Hash exists, so replace the zero placeholder value
+          reuse_results[:sp][result[:num_sps]][:num_all_users] = result[:num_all_users]
+        else
+          reuse_results[:sp][result[:num_sps]] = {
+            num_sps: result[:num_sps],
+            num_idv_users: 0, # Since it didn't exist, fill with zero 'idv' results
+            num_all_users: result[:num_all_users],
+          }
+        end
+      end
+      agency_reuse_results_idv.each do |result|
+        reuse_results[:agency][result[:num_agencies]] = {
+          num_agencies: result[:num_agencies],
+          num_idv_users: result[:num_idv_users],
+          num_all_users: 0, # Fill it in with 'all' results later
+        }
+      end
+      agency_reuse_results_all.each do |result|
+        if reuse_results[:agency][result[:num_agencies]].is_a?(Hash)
+          # Hash exists, so replace the zero placeholder value
+          reuse_results[:agency][result[:num_agencies]][:num_all_users] = result[:num_all_users]
+        else
+          reuse_results[:agency][result[:num_agencies]] = {
+            num_agencies: result[:num_agencies],
+            num_idv_users: 0, # Since it didn't exist, fill with zero 'idv' results
+            num_all_users: result[:num_all_users],
+          }
+        end
+      end
+
+      reuse_results.each do |results|
+        if results.length > 1
+          # If there are results, then remove the zero placeholder
+          results[0] = nil
+        end
+      end
+
       sp_reuse_stats = {
-        results: sp_reuse_results,
+        results: reuse_results[:sp].compact,
         total_all_users: 0,
         total_all_percent: 0,
         total_idv_users: 0,
         total_idv_percent: 0,
       }
       agency_reuse_stats = {
-        results: agency_reuse_results,
+        results: reuse_results[:agency].compact,
         total_all_users: 0,
         total_all_percent: 0,
         total_idv_users: 0,
@@ -95,23 +150,25 @@ module Reporting
       total_proofed = num_active_profiles
 
       [sp_reuse_stats, agency_reuse_stats].each do |stats|
-        if !stats['results'].empty?
+
+        if !stats[:results].nil? && !stats[:results].empty?
+
           # Count how many total users have multiples for both sps and agencies
-          stats['results'].each do |result_entry|
-            stats['total_all_users'] += result_entry['num_all_users']
-            stats['total_idv_users'] += result_entry['num_idv_users']
+          stats[:results].each do |result_entry|
+            stats[:total_all_users] += result_entry[:num_all_users]
+            stats[:total_idv_users] += result_entry[:num_idv_users]
           end
 
           if total_proofed > 0
             # Calculate percentages for breakdowns with both sps and angencies
-            stats['results'].each_with_index do |result_entry, index|
-              stats['results'][index]['all_percent'] =
-                result_entry['num_all_users'] / total_proofed.to_f
-              stats['results'][index]['idv_percent'] =
-                result_entry['num_idv_users'] / total_proofed.to_f
+            stats[:results].each_with_index do |result_entry, index|
+              stats[:results][index][:all_percent] =
+                result_entry[:num_all_users] / total_proofed.to_f
+              stats[:results][index][:idv_percent] =
+                result_entry[:num_idv_users] / total_proofed.to_f
 
-              stats['total_all_percent'] += agency_reuse_stats[index]['all_percent']
-              stats['total_idv_percent'] += agency_reuse_stats[index]['idv_percent']
+              stats[:total_all_percent] += stats[:results][index][:all_percent]
+              stats[:total_idv_percent] += stats[:results][index][:idv_percent]
             end
           end
         end
@@ -121,90 +178,143 @@ module Reporting
       @total_reuse_report = {
         sp_reuse_stats: sp_reuse_stats,
         agency_reuse_stats: agency_reuse_stats,
-        total_proofed: total_proofed,
       }
     end
 
-    def sp_reuse_results
-      # TODO: Test this query so that it returns correct data
-      sp_sql = format(<<-SQL, params)
-          SELECT
-              num_all_users
-              , all_query.sps_per_all_users.num_sps
-              , COUNT(*) AS num_idv_users
-          FROM (
-            SELECT
-                COUNT(*) AS num_all_users
-                , sps_per_all_users.num_sps
-                , identities
-            FROM (
-                SELECT
-                    COUNT(*) AS num_sps
-                    , identities.user_id
-                FROM
-                    identities
-                AND
-                    identities.verified_at < %{query_date}
-                GROUP BY
-                    identities.user_id
-              ) sps_per_all_users
-            GROUP BY
-              sps_per_all_users.num_sps
-            HAVING sps_per_all_users.num_sps > 1
-            ) all_query
-          WHERE
-            identities.last_ial2_authenticated_at IS NOT NULL
-          ORDER BY
-              num_sps ASC
-      SQL
-
-      sp_results = Reports::BaseReport.transaction_with_timeout do
-        ActiveRecord::Base.connection.execute(sp_sql)
-      end
-
-      sp_results.as_json
-    end
-
-    def agency_reuse_results
-      agency_sql = format(<<-SQL, params)
+    def sp_reuse_results_all
+      sp_all_sql = format(<<-SQL, params)
         SELECT
-          num_all_users
-          , all_query.agencies_per_all_users.num_agencies
-          , COUNT(*) AS num_idv_users
+            COUNT(*) AS num_all_users
+            , sps_per_all_users.num_sps
         FROM (
             SELECT
-                COUNT(*) AS num_all_users
-                , agencies_per_all_users.num_agencies
-            FROM (
-                SELECT
-                    COUNT(DISTINCT agencies.id) AS num_agencies
-                    , identities.user_id
-                FROM
-                    identities
-                JOIN
-                    service_providers sp ON identities.service_provider = sp.issuer
-                JOIN
-                    agencies ON sp.agency_id = agencies.id
-                AND
-                    identities.verified_at < %{query_date}
-                GROUP BY
-                    identities.user_id
-            ) agencies_per_all_users
-            GROUP BY
-                agencies_per_all_users.num_agencies
-            HAVING agencies_per_all_users.num_agencies > 1
-            ) all_query
+                COUNT(*) AS num_sps
+                , identities.user_id
+            FROM
+                identities
             WHERE
-              identities.last_ial2_authenticated_at IS NOT NULL
-          ORDER BY
+                identities.verified_at < %{query_date}
+            GROUP BY
+                identities.user_id
+        ) sps_per_all_users
+        GROUP BY
+            sps_per_all_users.num_sps
+        HAVING 
+            sps_per_all_users.num_sps > 1
+        ORDER BY
+            num_sps ASC
+      SQL
+
+      sp_all_results = Reports::BaseReport.transaction_with_timeout do
+        ActiveRecord::Base.connection.execute(sp_all_sql)
+      end
+
+      sp_all_results.as_json
+    end
+
+    def sp_reuse_results_idv
+      sp_idv_sql = format(<<-SQL, params)
+        SELECT
+            COUNT(*) AS num_idv_users
+            , sps_per_idv_users.num_sps
+        FROM (
+            SELECT
+                COUNT(*) AS num_sps
+                , identities.user_id
+            FROM
+                identities
+            WHERE
+                identities.last_ial2_authenticated_at IS NOT NULL
+            AND
+                identities.verified_at < %{query_date}
+            GROUP BY
+                identities.user_id
+        ) sps_per_idv_users
+        GROUP BY
+            sps_per_idv_users.num_sps
+        HAVING 
+            sps_per_idv_users.num_sps > 1
+        ORDER BY
+            num_sps ASC
+      SQL
+
+      sp_idv_results = Reports::BaseReport.transaction_with_timeout do
+        ActiveRecord::Base.connection.execute(sp_idv_sql)
+      end
+
+      sp_idv_results.as_json
+    end
+
+    def agency_reuse_results_all
+      agency_all_sql = format(<<-SQL, params)
+      SELECT
+          COUNT(*) AS num_all_users
+          , agencies_per_user.num_agencies
+      FROM (
+          SELECT
+              COUNT(DISTINCT agencies.id) AS num_agencies
+              , identities.user_id
+          FROM
+              identities
+          JOIN
+              service_providers sp ON identities.service_provider = sp.issuer
+          JOIN
+              agencies ON sp.agency_id = agencies.id
+          WHERE
+              identities.verified_at < %{query_date}
+          GROUP BY
+              identities.user_id
+      ) agencies_per_user
+      GROUP BY
+          agencies_per_user.num_agencies
+      HAVING 
+          agencies_per_user.num_agencies > 1
+      ORDER BY
+          num_agencies ASC
+      SQL
+
+      agency_all_results = Reports::BaseReport.transaction_with_timeout do
+        ActiveRecord::Base.connection.execute(agency_all_sql)
+      end
+
+      agency_all_results.as_json
+    end
+
+    def agency_reuse_results_idv
+      agency_idv_sql = format(<<-SQL, params)
+        SELECT
+            COUNT(*) AS num_idv_users
+            , agencies_per_user.num_agencies
+        FROM (
+            SELECT
+                COUNT(DISTINCT agencies.id) AS num_agencies
+                , identities.user_id
+            FROM
+                identities
+            JOIN
+                service_providers sp ON identities.service_provider = sp.issuer
+            JOIN
+                agencies ON sp.agency_id = agencies.id
+            WHERE
+                identities.last_ial2_authenticated_at IS NOT NULL
+            AND
+                identities.verified_at < %{query_date}
+            GROUP BY
+                identities.user_id
+        ) agencies_per_user
+        GROUP BY
+            agencies_per_user.num_agencies
+        HAVING 
+            agencies_per_user.num_agencies > 1
+        ORDER BY
             num_agencies ASC
       SQL
 
-      agency_results = Reports::BaseReport.transaction_with_timeout do
-        ActiveRecord::Base.connection.execute(agency_sql)
+      agency_idv_results = Reports::BaseReport.transaction_with_timeout do
+        ActiveRecord::Base.connection.execute(agency_idv_sql)
       end
 
-      agency_results.as_json
+      agency_idv_results.as_json
     end
 
     def num_active_profiles
@@ -228,8 +338,12 @@ module Reporting
 
     def params
       {
-        query_date: report_date.end_of_day,
+        query_date: first_day_of_report_month,
       }.transform_values { |v| ActiveRecord::Base.connection.quote(v) }
+    end
+
+    def first_day_of_report_month
+      report_date.beginning_of_month.strftime('%Y-%m-%d')
     end
   end
 end
