9c98e6ce1560f5fffbb51eeedafd3582a308fae1
Matt Hinz <matt.hinz@gsa.gov>
2023-11-28 10:32:49 -0800

    Refactor PersonalKeyController spec (#9666)
    * Allow passing applicant into some stub_idv_steps* methods
    
    * Refactor PersonalKeyController spec
    
    - Favor using factory_bot factories where possible
    - Differentiate between the profile tied to the idv_session and other profiles
    
    * [skip changelog]

--- spec/support/controller_helper.rb
+++ spec/support/controller_helper.rb
@@ -34,39 +34,33 @@ module ControllerHelper
     controller.user_session[TwoFactorAuthenticatable::NEED_AUTHENTICATION] = true
   end
 
-  def stub_idv_steps_before_verify_step(user)
+  def stub_idv_steps_before_verify_step(
+    user,
+    applicant: Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE
+  )
     user_session = {}
     stub_sign_in(user)
     idv_session = Idv::Session.new(
       user_session: user_session, current_user: user,
       service_provider: nil
     )
-    idv_session.applicant = {
-      first_name: 'Some',
-      last_name: 'One',
-      uuid: SecureRandom.uuid,
-      dob: 50.years.ago.to_date.to_s,
-      ssn: '666-12-1234',
-    }.with_indifferent_access
+    idv_session.applicant = applicant
     allow(subject).to receive(:confirm_idv_applicant_created).and_return(true)
     allow(subject).to receive(:idv_session).and_return(idv_session)
     allow(subject).to receive(:user_session).and_return(user_session)
   end
 
-  def stub_verify_steps_one_and_two(user)
+  def stub_verify_steps_one_and_two(
+    user,
+    applicant: Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE
+  )
     user_session = ActiveSupport::HashWithIndifferentAccess.new
     stub_sign_in(user)
     idv_session = Idv::Session.new(
       user_session: user_session, current_user: user,
       service_provider: nil
     )
-    idv_session.applicant = {
-      first_name: 'Some',
-      last_name: 'One',
-      uuid: SecureRandom.uuid,
-      dob: 50.years.ago.to_date.to_s,
-      ssn: '666-12-1234',
-    }.with_indifferent_access
+    idv_session.applicant = applicant
     idv_session.resolution_successful = true
     allow(subject).to receive(:idv_session).and_return(idv_session)
     allow(subject).to receive(:user_session).and_return(user_session)
