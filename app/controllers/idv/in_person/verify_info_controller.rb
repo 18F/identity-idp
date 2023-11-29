104b624dae51abb07e0f8109e5b7c6b3530e7255
Sonia Connolly <sonia.connolly@gsa.gov>
2023-11-29 13:26:53 -0800

    Review comments - restore tests and remove unneeded undo from in_person verify_info_controller

--- app/controllers/idv/in_person/verify_info_controller.rb
+++ app/controllers/idv/in_person/verify_info_controller.rb
@@ -42,7 +42,6 @@ module Idv
           end,
           undo_step: ->(idv_session:, user:) do
             idv_session.resolution_successful = nil
-            idv_session.address_edited = nil
             idv_session.verify_info_step_document_capture_session_uuid = nil
             idv_session.threatmetrix_review_status = nil
             idv_session.applicant = nil
