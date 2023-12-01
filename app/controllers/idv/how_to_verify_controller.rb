5f61f74526cbf55665332e51210a5db81a05a5e4
Jack Ryan <jackryan@navapbc.com>
2023-12-01 11:32:54 -0500

    LG-11601 How to Verify Analytics updates (#9679)
    * Grabbing param value on success and merging in ab test analytics
    
    * changelog: Internal, In-Person Proofing, Update how to verify analytics to capture selected value, ab test buckets and other standard values
    
    * Update app/controllers/idv/how_to_verify_controller.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Add specs for how to verify selection analytics
    
    * Editing spec to take advantage of existing code
    
    ---------
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/controllers/idv/how_to_verify_controller.rb
+++ app/controllers/idv/how_to_verify_controller.rb
@@ -22,9 +22,14 @@ module Idv
     def update
       clear_future_steps!
       result = Idv::HowToVerifyForm.new.submit(how_to_verify_form_params)
+      if how_to_verify_form_params[:selection] == []
+        sendable_form_params = {}
+      else
+        sendable_form_params = how_to_verify_form_params
+      end
 
       analytics.idv_doc_auth_how_to_verify_submitted(
-        **analytics_arguments.merge(result.to_h),
+        **analytics_arguments.merge(sendable_form_params).merge(result.to_h),
       )
 
       if result.success?
@@ -65,7 +70,9 @@ module Idv
       {
         step: 'how_to_verify',
         analytics_id: 'Doc Auth',
-      }
+        skip_hybrid_handoff: idv_session.skip_hybrid_handoff,
+        irs_reproofing: irs_reproofing?,
+      }.merge(ab_test_analytics_buckets)
     end
 
     def how_to_verify_form_params
--- spec/controllers/idv/how_to_verify_controller_spec.rb
+++ spec/controllers/idv/how_to_verify_controller_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe Idv::HowToVerifyController do
   let(:user) { create(:user) }
   let(:enabled) { true }
+  let(:ab_test_args) do
+    { sample_bucket1: :sample_value1, sample_bucket2: :sample_value2 }
+  end
 
   before do
     allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { enabled }
     stub_sign_in(user)
     stub_analytics
+    allow(@analytics).to receive(:track_event)
+    allow(subject).to receive(:ab_test_analytics_buckets).and_return(ab_test_args)
     subject.idv_session.welcome_visited = true
     subject.idv_session.idv_consent_given = true
   end
@@ -28,11 +33,26 @@ RSpec.describe Idv::HowToVerifyController do
   end
 
   describe '#show' do
+    let(:analytics_name) { :idv_doc_auth_how_to_verify_visited }
+    let(:analytics_args) do
+      {
+        step: 'how_to_verify',
+        analytics_id: 'Doc Auth',
+        skip_hybrid_handoff: nil,
+        irs_reproofing: false,
+      }.merge(ab_test_args)
+    end
     it 'renders the show template' do
       get :show
 
       expect(subject.idv_session.skip_doc_auth).to be_nil
       expect(response).to render_template :show
+    end
+
+    it 'sends analytics_visited event' do
+      get :show
+
+      expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
     end
 
     context 'agreement step not completed' do
@@ -54,31 +74,84 @@ RSpec.describe Idv::HowToVerifyController do
         idv_how_to_verify_form: { selection: selection },
       }
     end
-    let(:selection) { 'remote' }
+    let(:analytics_name) { :idv_doc_auth_how_to_verify_submitted }
+    context 'no selection made' do
+      let(:analytics_args) do
+        {
+          step: 'how_to_verify',
+          analytics_id: 'Doc Auth',
+          skip_hybrid_handoff: nil,
+          irs_reproofing: false,
+          error_details: { selection: { blank: true } },
+          errors: { selection: ['Select a way to verify your identity.'] },
+          success: false,
+        }.merge(ab_test_args)
+      end
 
-    it 'invalidates future steps' do
-      expect(subject).to receive(:clear_future_steps!)
+      it 'invalidates future steps' do
+        expect(subject).to receive(:clear_future_steps!)
 
-      put :update
+        put :update
+      end
+
+      it 'sends analytics_submitted event when nothing is selected' do
+        put :update
+
+        expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
+      end
     end
 
     context 'remote' do
+      let(:selection) { 'remote' }
+      let(:analytics_args) do
+        {
+          analytics_id: 'Doc Auth',
+          skip_hybrid_handoff: nil,
+          step: 'how_to_verify',
+          irs_reproofing: false,
+          errors: {},
+          success: true,
+          'selection' => selection,
+        }.merge(ab_test_args)
+      end
       it 'sets skip doc auth on idv session to false and redirects to hybrid handoff' do
         put :update, params: params
 
         expect(subject.idv_session.skip_doc_auth).to be false
         expect(response).to redirect_to(idv_hybrid_handoff_url)
       end
+
+      it 'sends analytics_submitted event when remote proofing is selected' do
+        put :update, params: params
+
+        expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
+      end
     end
 
     context 'ipp' do
       let(:selection) { 'ipp' }
-
+      let(:analytics_args) do
+        {
+          analytics_id: 'Doc Auth',
+          skip_hybrid_handoff: nil,
+          step: 'how_to_verify',
+          irs_reproofing: false,
+          errors: {},
+          success: true,
+          'selection' => selection,
+        }.merge(ab_test_args)
+      end
       it 'sets skip doc auth on idv session to true and redirects to document capture' do
         put :update, params: params
 
         expect(subject.idv_session.skip_doc_auth).to be true
         expect(response).to redirect_to(idv_document_capture_url)
+      end
+
+      it 'sends analytics_submitted event when remote proofing is selected' do
+        put :update, params: params
+
+        expect(@analytics).to have_received(:track_event).with(analytics_name, analytics_args)
       end
     end
 
