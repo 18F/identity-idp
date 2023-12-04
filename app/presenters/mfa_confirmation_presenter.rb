df1c16ab94b8e5750bd3ff173f020aae7f0b72f4
Jessica Dembe <jessica.dembe@gsa.gov>
2023-12-04 10:03:01 -0500

    create logic for webauthn platform setup success
    changelog: User-Facing Improvements, F/T unlock, change messaging for F/T success on sign up

--- app/presenters/mfa_confirmation_presenter.rb
+++ app/presenters/mfa_confirmation_presenter.rb
@@ -1,14 +1,23 @@
 class MfaConfirmationPresenter
-  def initialize(show_skip_additional_mfa_link: true)
+  def initialize(show_skip_additional_mfa_link: true, webauthn_platform_set_up: false)
     @show_skip_additional_mfa_link = show_skip_additional_mfa_link
+    @webauthn_platform_set_up = webauthn_platform_set_up
   end
 
   def heading
-    I18n.t('titles.mfa_setup.suggest_second_mfa')
+    if @webauthn_platform_set_up
+      I18n.t('titles.mfa_setup.face_touch_unlock_confirmation')
+    else
+      I18n.t('titles.mfa_setup.suggest_second_mfa')
+    end
   end
 
   def info
-    I18n.t('mfa.account_info')
+    if @webauthn_platform_set_up
+      I18n.t('mfa.webauthn_platform_info')
+    else
+      I18n.t('mfa.account_info')
+    end
   end
 
   def button
