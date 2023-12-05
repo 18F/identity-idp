ed5188db3354b3e4e3ac81febb2584de5d88bc85
gina-yamada <gina.yamada@gsa.gov>
2023-12-05 09:45:52 -0700

    Allow back button from verify info to ssn

--- spec/controllers/idv/in_person/ssn_controller_spec.rb
+++ spec/controllers/idv/in_person/ssn_controller_spec.rb
@@ -107,27 +107,14 @@ RSpec.describe Idv::InPerson::SsnController do
     end
 
     context 'with an ssn in idv_session' do
-      let(:referer) { idv_in_person_step_url(step: :address) }
       before do
         subject.idv_session.ssn = ssn
-        request.env['HTTP_REFERER'] = referer
       end
 
-      context 'referer is not verify_info' do
-        it 'redirects to verify_info' do
-          get :show
+      it 'allows the back button' do
+        get :show
 
-          expect(response).to redirect_to(idv_in_person_verify_info_url)
-        end
-      end
-
-      context 'referer is verify_info' do
-        let(:referer) { idv_in_person_verify_info_url }
-        it 'does not redirect' do
-          get :show
-
-          expect(response).to render_template 'idv/shared/ssn'
-        end
+        expect(response).to render_template 'idv/shared/ssn'
       end
     end
   end
