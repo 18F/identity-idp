7834cc3ecb03611f7bb7df18e4d52a5579918ff2
John Maxwell <john.maxwell@gsa.gov>
2023-12-04 19:11:49 -0500

    Spec cleanup

--- spec/services/pii/re_encryptor_spec.rb
+++ spec/services/pii/re_encryptor_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Pii::ReEncryptor do
   describe '#perform' do
-    it 're-encrypts PII using new code' do
-      profile = create(:profile, :active, :verified, pii: { ssn: '1234' })
-      user = profile.user
-      user_session = {
-        decrypted_pii: { ssn: '1234' }.to_json,
-      }
+    let(:active_profile) { create(:profile, :active, :verified, pii: pii) }
+    let(:pii) { { ssn: ssn } }
+    let(:ssn) { ' 1234' }
+    let(:user) { active_profile.user }
+    let(:user_session) { { decrypted_pii: pii.to_json } }
 
+    let(:expected_pii_attributes) do
       pii_attributes = Pii::Attributes.new
-      pii_attributes[:ssn] = '1234'
+      pii_attributes[:ssn] = ssn
+      pii_attributes
+    end
 
-      expect(user.active_profile).to receive(:encrypt_recovery_pii).
-        with(pii_attributes).and_call_original
-      expect(user.active_profile).to receive(:save!).and_call_original
+    before do
+      allow(user.active_profile).to receive(:encrypt_recovery_pii).and_call_original
+      allow(user.active_profile).to receive(:save!).and_call_original
 
       Pii::ReEncryptor.new(user: user, user_session: user_session).perform
+    end
+
+    it 're-encrypts PII using new code' do
+      expect(user.active_profile).to have_received(:encrypt_recovery_pii).
+        with(expected_pii_attributes)
+      expect(user.active_profile).to have_received(:save!)
     end
   end
 end
