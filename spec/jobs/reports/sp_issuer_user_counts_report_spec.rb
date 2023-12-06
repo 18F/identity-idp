8f5d9a518b1f0020dd41fb7c99a00479f90290f4
davida marion <davida.marion@gsa.gov>
2023-12-06 14:42:17 -0500

    Update report to use tables_report

--- spec/jobs/reports/sp_issuer_user_counts_report_spec.rb
+++ spec/jobs/reports/sp_issuer_user_counts_report_spec.rb
@@ -1,30 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Reports::SpIssuerUserCountsReport do
-  let(:name) { 'sp-issuer-user-counts-report' }
   let(:issuer) { 'urn:gov:gsa:openidconnect:sp:sinatra' }
   let(:email) { 'foo@bar.com' }
-  let(:user) { create(:user) }
-  let(:uuid) { 'foo' }
-  let(:last_authenticated_at) { '2020-01-02 12:03:04 UTC' }
+  let(:date) { Date.today.to_s }
+  let(:user_counts) do
+    {
+      "total" => 56,
+      "ial1_total" => 40,
+      "ial2_total" => 16
+    }
+  end
 
   subject { described_class.new }
 
-  it 'sends out a report to the email listed with one total user' do
-    create(
-      :service_provider_identity,
-      service_provider: issuer,
-      user: user,
-      uuid: uuid,
-      last_authenticated_at: last_authenticated_at,
-    )
+  let(:reports) do
+    [
+      Reporting::EmailableReport.new(
+        title: 'Overview',
+        table: [
+          ['Report Generated', date],
+          ['Issuer', issuer],
+        ],
+      ),
+      Reporting::EmailableReport.new(
+        title: 'User counts',
+        table: [
+          ['Metric', 'Number of users'],
+          ['Total Users', 56],
+          ['IAL1 Users', 40],
+          ['Identity Verified Users', 16]
+        ],
+      ),
+    ]
+  end
 
-    allow(IdentityConfig.store).to receive(:sp_issuer_user_counts_report_configs).and_return(
-      [{ 'issuer' => issuer, 'emails' => [email] }],
+  before do
+    expect(Db::Identity::SpUserCounts).to receive(:with_issuer).with(issuer).
+      and_return(user_counts)
+
+    allow(IdentityConfig.store).to receive(:sp_issuer_user_counts_report_configs).
+      and_return([{ 'issuer' => issuer, 'emails' => [email] }])
+
+    allow(ReportMailer).to receive(:tables_report).and_call_original
+  end
+
+
+  it 'emails the csv' do
+    expect(ReportMailer).to receive(:tables_report).with(
+      email:,
+      subject: "Service provider user count report",
+      message: "Report: #{Reports::SpIssuerUserCountsReport::REPORT_NAME}",
+      reports:,
+      attachment_format: :csv,
     )
-    expect(ReportMailer).to receive(:sp_issuer_user_counts_report).with(
-      name: name, email: email, ial1_total: 1, ial2_total: 0, issuer: issuer, total: 1,
-    ).and_call_original
 
     subject.perform(Time.zone.today)
   end
