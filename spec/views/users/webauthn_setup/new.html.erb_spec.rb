dce75fe7c96304f4d45282b457c0affd31237ac0
kevinsmaster5 <kevin.masters@gsa.gov>
2023-12-06 15:43:10 -0500

    reorder spec pretext values and add context for non-platform webauthn

--- spec/views/users/webauthn_setup/new.html.erb_spec.rb
+++ spec/views/users/webauthn_setup/new.html.erb_spec.rb
@@ -3,30 +3,31 @@ require 'rails_helper'
 RSpec.describe 'users/webauthn_setup/new.html.erb' do
   let(:user) { create(:user, :fully_registered) }
 
+  let(:user_session) do
+    { webauthn_challenge: 'fake_challenge' }
+  end
+  let(:presenter) do
+    WebauthnSetupPresenter.new(
+      current_user: user,
+      user_fully_authenticated: true,
+      user_opted_remember_device_cookie: true,
+      remember_device_default: true,
+      platform_authenticator: platform_authenticator,
+      url_options: {},
+    )
+  end
+
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:user_session).and_return(user_session)
+    allow(view).to receive(:in_multi_mfa_selection_flow?).and_return(false)
+    assign(:platform_authenticator, platform_authenticator)
+    assign(:user_session, user_session)
+    assign(:presenter, presenter)
+  end
+
   context 'webauthn platform' do
     let(:platform_authenticator) { true }
-    let(:user_session) do
-      { webauthn_challenge: 'fake_challenge' }
-    end
-    let(:presenter) do
-      WebauthnSetupPresenter.new(
-        current_user: user,
-        user_fully_authenticated: true,
-        user_opted_remember_device_cookie: true,
-        remember_device_default: true,
-        platform_authenticator: platform_authenticator,
-        url_options: {},
-      )
-    end
-
-    before do
-      allow(view).to receive(:current_user).and_return(user)
-      allow(view).to receive(:user_session).and_return(user_session)
-      allow(view).to receive(:in_multi_mfa_selection_flow?).and_return(false)
-      assign(:platform_authenticator, platform_authenticator)
-      assign(:user_session, user_session)
-      assign(:presenter, presenter)
-    end
 
     it 'has a localized title' do
       expect(view).to receive(:title=).with(presenter.page_title)
@@ -75,6 +76,16 @@ RSpec.describe 'users/webauthn_setup/new.html.erb' do
 
         expect(rendered).to_not have_content(I18n.t('forms.webauthn_platform_setup.info_text'))
       end
+    end
+  end
+
+  context 'non-platform webauthn' do
+    let(:platform_authenticator) { false }
+    it 'displays the form' do
+      render
+      expect(rendered).to have_content(
+        t('two_factor_authentication.two_factor_choice_options.webauthn'),
+      )
     end
   end
 end
