ac29692f585a8aa8ea72605c201ffc90c9f23d1f
John Maxwell <john.maxwell@gsa.gov>
2023-12-11 16:21:27 -0500

    Spec cleanup.

--- spec/services/pii/re_encryptor_spec.rb
+++ spec/services/pii/re_encryptor_spec.rb
@@ -2,45 +2,60 @@ require 'rails_helper'
 
 RSpec.describe Pii::ReEncryptor do
   describe '#perform' do
-    it 're-encrypts PII onto the active profile using new code' do
-      profile = create(:profile, :active, :verified, pii: { ssn: '1234' })
+    let(:active_ssn) { '1234' }
+    let(:active_pii) { { ssn: active_ssn } }
 
-      user = profile.user
+    let(:pending_ssn) { '5678' }
+    let(:pending_pii) { { ssn: pending_ssn } }
 
-      # make sure it doesn't use this
-      create(:profile, :verify_by_mail_pending, user: user, pii: { ssn: '5678' })
+    let(:user) { create(:user) }
+    let(:user_session) { {}.with_indifferent_access }
+    let(:pii_cacher) { Pii::Cacher.new(user, user_session) }
+    let(:re_encryptor) { Pii::ReEncryptor.new(user: user, user_session: user_session) }
 
-      user_session = {}.with_indifferent_access
-      cacher = Pii::Cacher.new(user, user_session)
-      cacher.save_decrypted_pii({ ssn: '1234' }, profile.id)
+    context 'when the user has an active profile and a pending profile' do
+      let(:expected_pii) do
+        expected_pii = Pii::Attributes.new
+        expected_pii[:ssn] = active_ssn
+        expected_pii
+      end
 
-      Pii::ReEncryptor.new(user: user, user_session: user_session).perform
-      personal_key = user.active_profile.personal_key
-      dr_attributes = Profile.find(profile.id).recover_pii(personal_key.gsub(/-/, ' '))
+      it 're-encrypts PII onto the active profile using new code' do
+        active_profile = create(:profile, :active, :verified, pii: active_pii, user: user)
+        # creating a pending profile to make sure pending profiles are ignored when an active one is present
+        create(:profile, :verify_by_mail_pending, pii: pending_pii, user: user)
 
-      pii_attributes = Pii::Attributes.new
-      pii_attributes[:ssn] = '1234'
+        pii_cacher.save_decrypted_pii(active_pii, active_profile.id)
 
-      expect(dr_attributes).to eq(pii_attributes)
+        re_encryptor.perform
+        personal_key = user.active_profile.personal_key
+        recovered_pii = Profile.find(user.active_profile.id).recover_pii(
+          PersonalKeyGenerator.new(user).normalize(personal_key),
+        )
+
+        expect(recovered_pii).to eq(expected_pii)
+      end
     end
 
-    it 're-encrypts PII onto the pending profile using new code' do
-      profile = create(:profile, :verify_by_mail_pending, pii: { ssn: '5678' })
+    context 'when the user has a pending profile, but no active profile' do
+      let(:expected_pii) do
+        expected_pii = Pii::Attributes.new
+        expected_pii[:ssn] = pending_ssn
+        expected_pii
+      end
 
-      user = profile.user
+      it 're-encrypts PII onto the pending profile using new code' do
+        pending_profile = create(:profile, :verify_by_mail_pending, pii: pending_pii, user: user)
+        pii_cacher.save_decrypted_pii(pending_pii, pending_profile.id)
 
-      user_session = {}.with_indifferent_access
-      cacher = Pii::Cacher.new(user, user_session)
-      cacher.save_decrypted_pii({ ssn: '5678' }, profile.id)
+        re_encryptor.perform
+        personal_key = user.pending_profile.personal_key
+o        recovered_pii = Profile.find(pending_profile.id).recover_pii(
+          PersonalKeyGenerator.new(user).normalize(personal_key),
+        )
 
-      Pii::ReEncryptor.new(user: user, user_session: user_session).perform
-      personal_key = user.pending_profile.personal_key
-      dr_attributes = Profile.find(profile.id).recover_pii(personal_key.gsub(/-/, ' '))
-
-      pii_attributes = Pii::Attributes.new
-      pii_attributes[:ssn] = '5678'
-
-      expect(dr_attributes).to eq(pii_attributes)
+        expect(recovered_pii).to eq(expected_pii)
+      end
     end
   end
 end
