73c16a06dd51ebca065b46bd6024c8ccfb598c43
Douglas Price <douglas.price@gsa.gov>
2023-12-11 18:00:24 -0500

    spec for Idv::LexisnexisInstantVerify

--- spec/services/idv/lexisnexis_instant_verify_spec.rb
+++ spec/services/idv/lexisnexis_instant_verify_spec.rb
@@ -1,31 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe Idv::LexisnexisInstantVerify, :controller do
-  controller ApplicationController do
-    include Idv::LexisnexisInstantVerify
-
-    def index; end
-  end
-
+RSpec.describe Idv::LexisnexisInstantVerify do
   let(:session_uuid) { SecureRandom.uuid }
+  let(:default_workflow) { 'legacy_workflow' }
+  let(:alternate_workflow) { 'equitable_workflow' }
+  let(:percent) { 5 }
+  let(:ab_testing_enabled) { false }
+  let(:variables) { subject.workflow_ab_testing_variables }
 
-  subject(:variables) { controller.workflow_ab_testing_variables }
+  subject { Idv::LexisnexisInstantVerify.new(session_uuid) }
 
   before do
-    allow(controller).to receive(:document_capture_session_uuid).
-      and_return(session_uuid)
+    allow(IdentityConfig.store).
+      to receive(:lexisnexis_instant_verify_workflow_ab_testing_enabled).
+      and_return(ab_testing_enabled)
+    allow(IdentityConfig.store).
+      to receive(:lexisnexis_instant_verify_workflow_ab_testing_percent).
+      and_return(percent)
+    allow(IdentityConfig.store).
+      to receive(:lexisnexis_instant_verify_workflow).
+      and_return(default_workflow)
+    allow(IdentityConfig.store).
+      to receive(:lexisnexis_instant_verify_workflow_alternate).
+      and_return(alternate_workflow)
   end
 
   context 'with lexisnexis instant verify workflow A/B testing disabled' do
-    let(:session_uuid) { SecureRandom.uuid }
+    let(:ab_testing_enabled) { false }
 
-    before do
-      allow(IdentityConfig.store).
-        to receive(:lexisnexis_instant_verify_workflow_ab_testing_enabled).
-        and_return(false)
+    it 'passes correct variables' do
+      expect(variables[:ab_testing_enabled]).to eq(false)
+      expect(variables[:use_alternate_workflow]).to eq(false)
+      expect(variables[:instant_verify_workflow]).to eq(default_workflow)
+    end
+  end
+
+  context 'with lexisnexis instant verify workflow A/B testing enabled' do
+    let(:ab_testing_enabled) { true }
+
+    context 'and A/B test specifies the alternate workflow' do
+      before do
+        stub_const(
+          'AbTests::LEXISNEXIS_INSTANT_VERIFY_WORKFLOW',
+          FakeAbTestBucket.new.tap { |ab| ab.assign(session_uuid => :use_alternate_workflow) },
+        )
+      end
+
+      it 'passes correct variables' do
+        expect(variables[:ab_testing_enabled]).to eq(true)
+        expect(variables[:use_alternate_workflow]).to eq(true)
+        expect(variables[:instant_verify_workflow]).to eq(alternate_workflow)
+      end
     end
 
-    context 'and A/B test specifies the older acuant version' do
+    context 'and A/B test specifies the default workflow' do
       before do
         stub_const(
           'AbTests::LEXISNEXIS_INSTANT_VERIFY_WORKFLOW',
@@ -33,60 +61,11 @@ RSpec.describe Idv::LexisnexisInstantVerify, :controller do
         )
       end
 
-      it 'passes correct variables and acuant version when older is specified' do
-        expect(variables[:acuant_sdk_upgrade_a_b_testing_enabled]).to eq(false)
-        expect(variables[:use_alternate_sdk]).to eq(false)
-        expect(variables[:acuant_version]).to eq(default_sdk_version)
+      it 'passes correct variables' do
+        expect(variables[:ab_testing_enabled]).to eq(true)
+        expect(variables[:use_alternate_workflow]).to eq(false)
+        expect(variables[:instant_verify_workflow]).to eq(default_workflow)
       end
-    end
-  end
-
-  context 'with acuant sdk upgrade A/B testing enabled' do
-    before do
-      allow(IdentityConfig.store).
-        to receive(:idv_acuant_sdk_upgrade_a_b_testing_enabled).
-        and_return(true)
-    end
-
-    context 'and A/B test specifies the newer acuant version' do
-      before do
-        stub_const(
-          'AbTests::ACUANT_SDK',
-          FakeAbTestBucket.new.tap { |ab| ab.assign(session_uuid => :use_alternate_sdk) },
-        )
-      end
-
-      it 'passes correct variables and acuant version when newer is specified' do
-        expect(variables[:acuant_sdk_upgrade_a_b_testing_enabled]).to eq(true)
-        expect(variables[:use_alternate_sdk]).to eq(true)
-        expect(variables[:acuant_version]).to eq(alternate_sdk_version)
-      end
-    end
-
-    context 'and A/B test specifies the older acuant version' do
-      before do
-        stub_const(
-          'AbTests::ACUANT_SDK',
-          FakeAbTestBucket.new.tap { |ab| ab.assign(session_uuid => 0) },
-        )
-      end
-
-      it 'passes correct variables and acuant version when older is specified' do
-        expect(variables[:acuant_sdk_upgrade_a_b_testing_enabled]).to eq(true)
-        expect(variables[:use_alternate_sdk]).to eq(false)
-        expect(variables[:acuant_version]).to eq(default_sdk_version)
-      end
-    end
-  end
-
-  describe '#override_csp_to_allow_acuant' do
-    it 'sets the headers for the document capture step' do
-      get :index, params: { step: 'document_capture' }
-
-      csp = response.request.content_security_policy
-      expect(csp.script_src).to include("'unsafe-eval'")
-      expect(csp.style_src).to include("'unsafe-inline'")
-      expect(csp.img_src).to include('blob:')
     end
   end
 end
