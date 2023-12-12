101897908d59c883d9a4dc5ce67665e36770d959
Jack Cody <jack.cody@gsa.gov>
2023-12-12 12:20:52 -0600

    Add is_disposable? helper

--- app/models/disposable_domain.rb
+++ app/models/disposable_domain.rb
@@ -1,2 +1,9 @@
 class DisposableDomain < ApplicationRecord
+  class << self
+    def is_disposable?(domain)
+      return false if !domain.is_a?(String) || domain.empty?
+
+      exists?(name: domain)
+    end
+  end
 end
