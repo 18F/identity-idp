1d2604d4100821e34b5b6339c5bcdd7a8dd708ad
Mitchell Henke <mitchell.henke@gsa.gov>
2023-12-13 16:24:40 -0600

    oops

--- spec/jobs/application_job_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationJob, type: :job do
-  describe '.warning_error_classes' do
-    it 'is empty by default' do
-      expect(described_class.warning_error_classes).to be_empty
-    end
-  end
-end
