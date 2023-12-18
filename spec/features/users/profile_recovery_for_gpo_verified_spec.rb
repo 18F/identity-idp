d16e7f3a25e1e2b3202e24da79cc9475612bbd77
John Maxwell <john.maxwell@gsa.gov>
2023-12-18 16:02:12 -0500

    LG-11743 - Personal key doesn't work for GPO users
    Spec to check profile reactivation for GPO-verified users. This is a
    bug reported by and believed fixed by Matt Hinz (see LG-11549), but we
    wanted a feature spec to test this exact scenario.
    
    changelog: Internal,Feature Specs,Added a feature spec for an issue in LG-11549
    
    Co-authored by: Matt Hinz <matt.hinz@gsa.gov>

---
+++ spec/features/users/profile_recovery_for_gpo_verified_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.feature 'Password recovery via personal key for a GPO-verified user' do
+  include IdvStepHelper
+
+  let(:user) { create(:user, :fully_registered) }
+  let(:new_password) { 'some really awesome new password' }
+
+  before do
+    allow(FeatureManagement).to receive(:reveal_gpo_code?).and_return(true)
+  end
+
+  scenario 'lets them reactivate their profile with their personal key', email: true, js: true do
+    complete_idv_steps_with_gpo_before_confirmation_step(user)
+    click_on 'Continue'
+
+    click_on t('account.index.verification.reactivate_button')
+    click_on t('idv.gpo.form.submit')
+
+    personal_key = scrape_personal_key
+    check t('forms.personal_key.required_checkbox')
+    click_continue
+
+    click_on 'Sign out'
+
+    trigger_reset_password_and_click_email_link(user.email)
+    reset_password_and_sign_back_in(user, new_password)
+    fill_in_code_with_last_phone_otp
+    click_submit_default
+
+    click_on t('links.account.reactivate.with_key')
+
+    expect(current_path).to eq verify_personal_key_path
+    fill_in 'personal_key', with: personal_key
+    click_continue
+
+    expect(current_path).to eq verify_password_path
+    fill_in 'Password', with: new_password
+    click_continue
+
+    expect(page).to have_content t('idv.messages.personal_key')
+    expect(page).to have_content t('headings.account.verified_account')
+  end
+end
