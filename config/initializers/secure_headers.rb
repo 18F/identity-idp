db5d9c2eb075c41f5154d911414d6cacf73c6501
Nadya Primak <nadya.primak@gsa.gov>
2023-12-18 15:17:43 -0500

    move logic into secure_headers_concern

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -10,18 +10,8 @@ Rails.application.configure do
                      IdentityConfig.store.component_previews_enabled
 
   config.action_dispatch.default_headers.merge!(
+    'X-Frame-Options' => previews_enabled ? 'SAMEORIGIN' : 'DENY',
     'X-XSS-Protection' => '1; mode=block',
     'X-Download-Options' => 'noopen',
   )
-
-  if Identity::Hostdata.env == 'dev'
-    # so we can embed a special lookbook component into the dev docs
-    config.action_dispatch.default_headers.merge!(
-      'Content-Security-Policy' => "frame-ancestors 'self' https://developers.login.gov/",
-    )
-  else
-    config.action_dispatch.default_headers.merge!(
-      'X-Frame-Options' => previews_enabled ? 'SAMEORIGIN' : 'DENY',
-    )
-  end
 end
