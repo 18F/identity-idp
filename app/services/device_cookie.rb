f05856f55fc9686f6200802cd178415d0f623ebf
kevinsmaster5 <kevin.masters@gsa.gov>
2023-12-18 13:04:06 -0500

    remove unneeded changes

--- app/services/device_cookie.rb
+++ app/services/device_cookie.rb
@@ -1,7 +1,8 @@
 class DeviceCookie
-  def self.check_for_new_device(cookies, current_user)
+  def self.check_for_new_device(cookies, user)
+    return unless user&.id
     cookies[:device] && Device.find_by(
-      user_id: current_user.id,
+      user_id: user.id,
       cookie_uuid: cookies[:device],
     )
   end
