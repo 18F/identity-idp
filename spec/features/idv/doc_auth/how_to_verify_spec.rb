3601197bcf3d76fc024f001ffe6320e0d8eb97b8
JackRyan1989 <jack.ryan@gsa.gov>
2023-12-19 11:32:38 -0500

    Reformatting it blocks and editing test descriptions

--- spec/features/idv/doc_auth/how_to_verify_spec.rb
+++ spec/features/idv/doc_auth/how_to_verify_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature 'how to verify step', js: true do
       complete_agreement_step
     end
 
-    it 'skips when disabled and redirects to hybird handoff)' do
+    it 'skips when disabled and redirects to hybird handoff' do
       expect(page).to have_current_path(idv_hybrid_handoff_url)
     end
   end
@@ -74,7 +74,7 @@ RSpec.feature 'how to verify step', js: true do
   end
 
   describe 'navigating to How To Verify from Agreement page in 50/50 state' do
-    context "opt in false at start but true during navigation" do
+    context 'opt in false at start but true during navigation' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled) { true }
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { false }
@@ -82,18 +82,21 @@ RSpec.feature 'how to verify step', js: true do
         sign_in_and_2fa_user
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
+      end
+
+      it 'should be on the hybrid handoff page' do
         expect(page).to have_current_path(idv_hybrid_handoff_url)
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { true }
         page.refresh
       end
-      
+
       it 'should be not be bounced back from hybrid handoff to how to verify' do
         expect(page).not_to have_current_path(idv_how_to_verify_url)
         expect(page).to have_current_path(idv_hybrid_handoff_url)
       end
     end
 
-    context "opt in true at start but false during navigation" do
+    context 'opt in true at start but false during navigation' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled) { true }
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { true }
@@ -101,19 +104,22 @@ RSpec.feature 'how to verify step', js: true do
         sign_in_and_2fa_user
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
+      end
+
+      it 'should be on the how to verify page' do
         expect(page).to have_current_path(idv_how_to_verify_url)
+      end
+
+      it 'should be on the hybrid handoff page when opt in is false' do
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { false }
         page.refresh
-      end
-      
-      it 'should be on the hybrid handoff page' do
         expect(page).to have_current_path(idv_hybrid_handoff_url)
       end
     end
   end
 
   describe 'navigating backwards from How to Verify page in 50/50 state' do
-    context 'Going back from Hybrid Handoff with opt in disabled in transit' do
+    context 'Going back from Hybrid Handoff with opt in disabled midstream' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled) { true }
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { true }
@@ -122,16 +128,20 @@ RSpec.feature 'how to verify step', js: true do
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
         complete_how_to_verify_step
+      end
+
+      it 'should be on the how to verify page' do
         expect(page).to have_current_path(idv_hybrid_handoff_url)
+      end
+
+      it 'should be not be bounced back to how to verify with opt in disabled midstream' do
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { false }
         page.go_back
-      end
-      
-      it 'should be not be bounced back to how to verify' do
         expect(page).to have_current_path(idv_hybrid_handoff_url)
       end
     end
-    context "Going back from Hybrid Handoff with opt in enabled in transit" do
+    
+    context 'Going back from Hybrid Handoff with opt in enabled midstream' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled) { true }
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { false }
@@ -139,16 +149,20 @@ RSpec.feature 'how to verify step', js: true do
         sign_in_and_2fa_user
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
+      end
+
+      it 'should be on the hybrid handoff url' do
         expect(page).to have_current_path(idv_hybrid_handoff_url)
+      end
+
+      it 'should go back to the agreement step from hybrid handoff with opt in toggled midstream' do
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { true }
         page.go_back
-      end
-      
-      it 'should be not be bounced back to how to verify' do
         expect(page).to have_current_path(idv_agreement_url)
       end
     end
-    context "Going back from Hybrid Handoff with opt in enabled the whole time" do
+    
+    context 'Going back from Hybrid Handoff with opt in enabled the whole time' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled) { true }
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { true }
@@ -157,15 +171,19 @@ RSpec.feature 'how to verify step', js: true do
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
         complete_how_to_verify_step
-        expect(page).to have_current_path(idv_hybrid_handoff_url)
-        page.go_back
       end
-      
+
+      it 'should be on the hybrid handoff page' do
+        expect(page).to have_current_path(idv_hybrid_handoff_url)
+      end
+
       it 'should be bounced back to how to verify' do
+        page.go_back
         expect(page).to have_current_path(idv_how_to_verify_url)
       end
     end
-    context "Going back from Hybrid Handoff with opt in disabled the whole time" do
+    
+    context 'Going back from Hybrid Handoff with opt in disabled the whole time' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled) { true }
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled) { false }
@@ -173,11 +191,14 @@ RSpec.feature 'how to verify step', js: true do
         sign_in_and_2fa_user
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
-        expect(page).to have_current_path(idv_hybrid_handoff_url)
-        page.go_back
       end
-      
+
+      it 'should be on the hybrid handoff page' do
+        expect(page).to have_current_path(idv_hybrid_handoff_url)
+      end
+
       it 'should be not be bounced back to how to verify' do
+        page.go_back
         expect(page).to have_current_path(idv_agreement_url)
       end
     end
