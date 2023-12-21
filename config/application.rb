e7af79515afec02bb43fb4d235963a6da1a54402
Nadya Primak <nadya.primak@gsa.gov>
2023-12-21 11:09:16 -0500

    Update config/application.rb
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- config/application.rb
+++ config/application.rb
@@ -153,7 +153,7 @@ module Identity
       config.lookbook.auto_refresh = false
       config.lookbook.project_name = "#{APP_NAME} Component Previews"
       config.lookbook.ui_theme = 'blue'
-      if Identity::Hostdata.env == 'dev'
+      if IdentityConfig.store.component_previews_embed_frame_ancestors.present?
         # so we can embed a lookbook component into the dev docs
         config.lookbook.preview_embeds.policy = "ALLOWALL"
       end
