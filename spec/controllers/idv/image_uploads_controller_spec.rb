d5b82f6f7d462280399defcf1e2b46e79c6e0558
charleyf <charleyferguson@navapbc.com>
2023-12-21 17:20:31 -0500

    Add test for missing state_id_number

--- spec/controllers/idv/image_uploads_controller_spec.rb
+++ spec/controllers/idv/image_uploads_controller_spec.rb
@@ -705,6 +705,92 @@ RSpec.describe Idv::ImageUploadsController do
           end
         end
 
+        context 'but doc_pii validation fails due to missing state_id_number' do
+          let(:state_id_number) { nil }
+
+          it 'tracks state_id_number validation errors in analytics' do
+            stub_analytics
+            stub_attempts_tracker
+
+            expect(@analytics).to receive(:track_event).with(
+              'IdV: doc auth image upload form submitted',
+              success: true,
+              errors: {},
+              user_id: user.uuid,
+              attempts: 1,
+              remaining_attempts: IdentityConfig.store.doc_auth_max_attempts - 1,
+              pii_like_keypaths: pii_like_keypaths,
+              flow_path: 'standard',
+              front_image_fingerprint: an_instance_of(String),
+              back_image_fingerprint: an_instance_of(String),
+            )
+
+            expect(@analytics).to receive(:track_event).with(
+              'IdV: doc auth image upload vendor submitted',
+              success: true,
+              errors: {},
+              attention_with_barcode: false,
+              async: false,
+              billed: true,
+              exception: nil,
+              doc_auth_result: 'Passed',
+              state: 'ND',
+              state_id_type: 'drivers_license',
+              user_id: user.uuid,
+              attempts: 1,
+              remaining_attempts: IdentityConfig.store.doc_auth_max_attempts - 1,
+              client_image_metrics: {
+                front: { glare: 99.99 },
+                back: { glare: 99.99 },
+              },
+              pii_like_keypaths: pii_like_keypaths,
+              flow_path: 'standard',
+              vendor_request_time_in_ms: a_kind_of(Float),
+              front_image_fingerprint: an_instance_of(String),
+              back_image_fingerprint: an_instance_of(String),
+              doc_type_supported: boolean,
+            )
+
+            expect(@analytics).to receive(:track_event).with(
+              'IdV: doc auth image upload vendor pii validation',
+              success: false,
+              errors: {
+                state_id_number: [I18n.t('doc_auth.errors.general.no_liveness')],
+              },
+              error_details: {
+                state_id_number: { blank: true },
+              },
+              attention_with_barcode: false,
+              user_id: user.uuid,
+              attempts: 1,
+              remaining_attempts: IdentityConfig.store.doc_auth_max_attempts - 1,
+              pii_like_keypaths: pii_like_keypaths,
+              flow_path: 'standard',
+              front_image_fingerprint: an_instance_of(String),
+              back_image_fingerprint: an_instance_of(String),
+              classification_info: hash_including(:Front, :Back),
+            )
+
+            expect(@irs_attempts_api_tracker).to receive(:track_event).with(
+              :idv_document_upload_submitted,
+              success: false,
+              document_back_image_filename: nil,
+              document_front_image_filename: nil,
+              document_image_encryption_key: nil,
+              document_state: 'ND',
+              document_number: state_id_number,
+              document_issued: nil,
+              document_expiration: nil,
+              first_name: 'FAKEY',
+              last_name: 'MCFAKERSON',
+              date_of_birth: '10/06/1938',
+              address: address1,
+            )
+
+            action
+          end
+        end
+
         context 'but doc_pii validation fails due to invalid DOB' do
           let(:dob) { nil }
 
