388445aaf41dd740adf4d741c55388ff772b2f8f
Matt Hinz <matt.hinz@gsa.gov>
2023-12-21 14:38:47 -0800

    Add validation to GpoConfirmation model
    Validate that:
    
    - entry is present
    - entry contains all required fields
    - entry contains a valid zipcode

--- app/models/gpo_confirmation.rb
+++ app/models/gpo_confirmation.rb
@@ -1,6 +1,13 @@
 class GpoConfirmation < ApplicationRecord
   self.table_name = 'usps_confirmations'
 
+  validates :entry, presence: true
+  validate :entry_has_all_required_fields
+  validate :entry_has_valid_zipcode
+
+  ZIP_REGEX = /\A\d{5}\s*-?\s*(\d{4})?\Z/
+  REQUIRED_ENTRY_FIELDS = %i[otp address1 city state zipcode ]
+
   # Store the pii as encrypted json
   def entry=(entry_hash)
     self[:entry] = encryptor.encrypt(entry_hash.to_json)
@@ -15,5 +22,19 @@ class GpoConfirmation < ApplicationRecord
 
   def encryptor
     Encryption::Encryptors::BackgroundProofingArgEncryptor.new
+  end
+
+  def entry_has_all_required_fields
+    REQUIRED_ENTRY_FIELDS.each do |field|
+      if entry[field].blank?
+        errors.add(:entry, :invalid)
+    end
+    end
+  end
+
+  def entry_has_valid_zipcode
+    if !ZIP_REGEX.match(entry[:zipcode]&.strip)
+      errors.add(:entry, :invalid)
+    end
   end
 end
--- spec/services/gpo_confirmation_spec.rb
+++ spec/services/gpo_confirmation_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe GpoConfirmation do
-  let(:attributes) do
+  let(:valid_attributes) do
     {
-      first_name: 'Homer',
-      last_name: 'Simpson',
-      ssn: '123-456-7890',
+      address1: '1234 Imaginary Ave',
+      address2: '',
+      city: 'Anywhere',
+      otp: 'ABCD1234',
+      first_name: 'Pat',
+      last_name: 'Person',
+      state: 'OH',
+      zipcode: '56789',
+      issuer: nil,
     }
   end
+
+  let(:attributes) { valid_attributes }
+
   let(:encryptor) { Encryption::Encryptors::BackgroundProofingArgEncryptor.new }
 
   subject { GpoConfirmation.create!(entry: attributes) }
@@ -24,6 +33,52 @@ RSpec.describe GpoConfirmation do
 
     it 'retrieves the entry as an unencrypted hash with symbolized keys' do
       expect(subject.entry).to eq(attributes)
+    end
+
+    describe 'validation' do
+      it 'passes when valid' do
+        expect { subject }.not_to raise_error
+      end
+
+      %i[otp address1 city state zipcode].each do |prop|
+        context "#{prop} not present" do
+          let(:attributes) { valid_attributes.tap { |a| a.delete(prop) } }
+          it 'fails validation' do
+            expect { subject }.to raise_error
+          end
+        end
+
+        context "#{prop} all whitespace" do
+          let(:attributes) { valid_attributes.tap { |a| a[prop] = "\n\t\t " } }
+          it 'fails validation' do
+            expect { subject }.to raise_error
+          end
+        end
+      end
+
+      [
+        ['0184', false],
+        ['982251', false],
+        [' 98225 ', true],
+        [' 98225 - 3938 ', true],
+        [' 98225 - 393 ', false],
+        ['98225-3938', true],
+        ['982253938', true],
+        ['1234', false],
+      ].each do |input, should_pass|
+        context "zipcode = #{input.inspect}" do
+          let(:attributes) { valid_attributes.tap { |a| a[:zipcode] = input } }
+          if should_pass
+            it 'validates' do
+              expect { subject }.not_to raise_error
+            end
+          else
+            it 'does not validate' do
+              expect { subject }.to raise_error
+            end
+          end
+        end
+      end
     end
   end
 
