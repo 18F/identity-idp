3692e6ea35441288480376e88ed9f53da1a7ec3f
Nadya Primak <nadya.primak@gsa.gov>
2024-01-10 10:51:02 -0500

    inherit from base refactor

--- app/components/login_button_component.rb
+++ app/components/login_button_component.rb
@@ -1,14 +1,16 @@
 class LoginButtonComponent < BaseComponent
   VALID_COLORS = ['primary', 'primary-darker', 'primary-lighter'].freeze
 
-  attr_reader :color, :tag_options
+  attr_reader :color, :big, :tag_options
 
-  def initialize(color: 'primary', **tag_options)
+  def initialize(color: 'primary', big: false, **tag_options)
     if !VALID_COLORS.include?(color)
       raise ArgumentError, "`color` #{color}} is invalid, expected one of #{VALID_COLORS}"
     end
 
+    @big = big
     @color = color
+    @tag_options = tag_options
   end
 
   def css_class
