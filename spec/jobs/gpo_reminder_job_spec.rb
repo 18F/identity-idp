ef2af65356ce7b9c186e7c9675086fe2657dc055
Doug Price <douglas.price@gsa.gov>
2024-01-10 15:30:03 -0500

    LG-12015: stop spamming users who requested letters (#9892)
    * LG-12015: stop spamming users who requested letters
    
    If a user has an old GPO code that has received a reminder and a new
    code that is not yet eligible for a reminder, we were sending them an
    email every day. /o\ This was due to the join where a gpo code had a nil
    reminder_sent_at and a gpo code created in the reminder window.
    
    Now we only send an email if we find a gpo code that meets all
    requirements.
    
    changelog: Bug Fixes, Verify By Mail, No longer send email reminders to some users every day
    
    * Set gpo_confirmation_code created_at dates in reminder job feature spec
    
    * Update spec not to set code created_at date when updating reminder date
    
    * Use profiles factory code_sent_at instead of setting date afterwards
    
    ---------
    
    Co-authored-by: Sonia Connolly <sonia.connolly@gsa.gov>

--- spec/jobs/gpo_reminder_job_spec.rb
+++ spec/jobs/gpo_reminder_job_spec.rb
@@ -9,10 +9,36 @@ RSpec.describe GpoReminderJob do
 
     let(:job) { GpoReminderJob.new }
 
-    let(:gpo_expired_user) { create(:user, :with_pending_gpo_profile) }
-    let(:due_for_reminder_user) { create(:user, :with_pending_gpo_profile) }
-    let(:not_yet_due_for_reminder_user) { create(:user, :with_pending_gpo_profile) }
-    let(:user_with_invalid_profile) { create(:user, :with_pending_gpo_profile) }
+    let!(:gpo_expired_user) do
+      create(
+        :user, :with_pending_gpo_profile,
+        code_sent_at: (max_days_ago_to_send_letter + 1).days.ago
+      )
+    end
+    let!(:due_for_reminder_user) do
+      create(
+        :user, :with_pending_gpo_profile,
+        code_sent_at: days_before_sending_reminder.days.ago
+      )
+    end
+    let!(:not_yet_due_for_reminder_user) do
+      create(
+        :user, :with_pending_gpo_profile,
+        code_sent_at: (days_before_sending_reminder - 1).days.ago
+      )
+    end
+    let!(:user_with_invalid_profile) do
+      create(
+        :user, :with_pending_gpo_profile,
+        code_sent_at: days_before_sending_reminder.days.ago
+      )
+    end
+    let!(:user_with_new_gpo_code) do
+      create(
+        :user, :with_pending_gpo_profile,
+        code_sent_at: (max_days_ago_to_send_letter + 1).days.ago
+      )
+    end
 
     let(:job_analytics) { FakeAnalytics.new }
 
@@ -21,22 +47,11 @@ RSpec.describe GpoReminderJob do
         and_return(max_days_ago_to_send_letter)
       allow(Analytics).to receive(:new).and_return(job_analytics)
 
-      gpo_expired_user.gpo_verification_pending_profile.update(
-        gpo_verification_pending_at: (max_days_ago_to_send_letter + 1).days.ago,
-      )
-
-      due_for_reminder_user.gpo_verification_pending_profile.update(
-        gpo_verification_pending_at: days_before_sending_reminder.days.ago,
-      )
-
-      not_yet_due_for_reminder_user.gpo_verification_pending_profile.update(
-        gpo_verification_pending_at: (days_before_sending_reminder - 1).days.ago,
-      )
-
-      user_with_invalid_profile.gpo_verification_pending_profile.update(
-        gpo_verification_pending_at: days_before_sending_reminder.days.ago,
-      )
       user_with_invalid_profile.gpo_verification_pending_profile.deactivate(:password_reset)
+
+      new_confirmation_code = create(:gpo_confirmation_code, created_at: 1.day.ago)
+      user_with_new_gpo_code.gpo_verification_pending_profile.gpo_confirmation_codes <<
+        new_confirmation_code
     end
 
     it 'sends only one reminder email, to the correct user' do
