e898be810dbb9de838a30bfe54554a258acec7a2
Andrew Duthie <andrew.duthie@gsa.gov>
2024-01-11 09:26:43 -0500

    LG-11899: Remove unreliable operating system version from Chromium device names
    changelog: User-Facing Improvements, Device Name, Avoid misleading operating system version number in Chromium-based device names

--- app/services/device_name.rb
+++ app/services/device_name.rb
@@ -1,14 +1,28 @@
 class DeviceName
-  def self.from_user_agent(user_agent)
-    browser = BrowserCache.parse(user_agent)
-    os = browser.platform.name
-    os_version = browser.platform.version&.split('.')&.first
-    os = "#{os} #{os_version}" if os_version
+  class << self
+    def from_user_agent(user_agent)
+      browser = BrowserCache.parse(user_agent)
+      os = browser.platform.name
+      os_version = browser.platform.version&.split('.')&.first if reliable_os_version?(browser)
+      os = "#{os} #{os_version}" if os_version
 
-    I18n.t(
-      'account.index.device',
-      browser: "#{browser.name} #{browser.version}",
-      os: os,
-    )
+      I18n.t(
+        'account.index.device',
+        browser: "#{browser.name} #{browser.version}",
+        os: os,
+      )
+    end
+
+    private
+
+    def reliable_os_version?(browser)
+      # Chromium's user agent reduction initiative will produce a unified platform string which may
+      # include an inaccurate operation system version.
+      # See: https://www.chromium.org/updates/ua-reduction/#unified-format
+      #
+      # Consider using Browser#chromium_based? if/when it becomes available
+      # See: https://github.com/fnando/browser/issues/541
+      !browser.chrome? && !browser.edge?
+    end
   end
 end
--- spec/services/device_name_spec.rb
+++ spec/services/device_name_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe DeviceName do
   describe '.device_name' do
-    let(:device) { create(:device) }
-    it 'gives a shortened os and browser name' do
-      name = DeviceName.from_user_agent(device.user_agent)
-      expect(name).to eq('Chrome 58 on Windows 10')
+    let(:user_agent) {}
+    let(:device) { create(:device, user_agent:) }
+    subject(:name) { DeviceName.from_user_agent(device.user_agent) }
+
+    context 'with a user agent producing a reliable OS version' do
+      let(:user_agent) do
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_2_1) AppleWebKit/605.1.15 (KHTML, like Gecko) ' \
+          'Version/17.2 Safari/605.1.15'
+      end
+
+      it 'gives a shortened browser name with operating system version' do
+        expect(name).to eq('Safari 17 on macOS 14')
+      end
+    end
+
+    context 'with a user agent not producing a reliable OS version' do
+      let(:user_agent) do
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+          'Chrome/120.0.0.0 Safari/537.36 Edg/120.0.2210.121'
+      end
+
+      it 'gives a shortened browser name with unversioned operating system' do
+        expect(name).to eq('Microsoft Edge 120 on Windows')
+      end
     end
   end
 end
