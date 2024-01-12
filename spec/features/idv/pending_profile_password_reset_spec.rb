34d732d264952041dfe176e7f0780fcd8c2084ab
gina-yamada <gina.yamada@gsa.gov>
2024-01-12 16:43:03 -0700

    add spec to test opt in ipp set to true

--- spec/features/idv/pending_profile_password_reset_spec.rb
+++ spec/features/idv/pending_profile_password_reset_spec.rb
@@ -49,6 +49,37 @@ RSpec.describe 'Resetting password with a pending profile' do
     expect(user.reload.active_or_pending_profile).to be_nil
   end
 
+  context 'with in_person_proofing_opt_in_enabled flag set to true' do
+    before do
+      allow(IdentityConfig.store).to receive(:in_person_proofing_enabled).and_return(true)
+      allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled).and_return(true)
+    end
+
+    scenario 'while in-person pending requires the user to reproof' do
+      user = create(:user, :with_phone, :with_pending_in_person_enrollment)
+
+      visit_idp_from_ial2_oidc_sp
+      fill_forgot_password_form(user)
+      click_reset_password_link_from_email
+
+      new_password = '$alty pickles'
+      fill_in t('forms.passwords.edit.labels.password'), with: new_password
+      fill_in t('components.password_confirmation.confirm_label'),
+              with: new_password
+      fill_in t('components.password_confirmation.confirm_label'),
+              with: new_password
+      click_button t('forms.passwords.edit.buttons.submit')
+
+      user.password = new_password
+      sign_in_live_with_2fa(user)
+
+      expect(page).to have_content(t('doc_auth.headings.welcome'))
+      expect(current_path).to eq(idv_welcome_path)
+
+      expect(user.reload.active_or_pending_profile).to be_nil
+    end
+  end
+
   scenario 'while fraud pending' do
     user = create(:user, :with_phone, :fraud_review_pending)
 
