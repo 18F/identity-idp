846072aa32e9a819cfd0dd6b1c005d0d49aaf616
svalexander <shannon.alexander-navarro@gsa.gov>
2024-01-12 13:49:16 -0500

    remove some unnecessary comments

--- spec/features/idv/steps/in_person/state_id_step_spec.rb
+++ spec/features/idv/steps/in_person/state_id_step_spec.rb
@@ -128,15 +128,15 @@ RSpec.describe 'doc auth IPP state ID step', js: true do
       complete_steps_before_state_id_step
 
       fill_out_state_id_form_ok(same_address_as_id: true)
-      # blank out the zip code field
       fill_in t('in_person_proofing.form.state_id.zipcode'), with: ''
-      # try to enter invalid input into the zip code field
       fill_in t('in_person_proofing.form.state_id.zipcode'), with: 'invalid input'
       expect(page).to have_field(t('in_person_proofing.form.state_id.zipcode'), with: '')
+
       # enter valid characters, but invalid length
       fill_in t('in_person_proofing.form.state_id.zipcode'), with: '123'
       click_idv_continue
       expect(page).to have_css('.usa-error-message', text: t('idv.errors.pattern_mismatch.zipcode'))
+
       # enter a valid zip and make sure we can continue
       fill_in t('in_person_proofing.form.state_id.zipcode'), with: '123456789'
       expect(page).to have_field(t('in_person_proofing.form.state_id.zipcode'), with: '12345-6789')
@@ -144,7 +144,7 @@ RSpec.describe 'doc auth IPP state ID step', js: true do
       expect(page).to have_current_path(idv_in_person_ssn_url)
     end
 
-    # validate date?
+    # validate date? -> what we really need to check is that error is shown
   end
 
   context 'State selection' do
