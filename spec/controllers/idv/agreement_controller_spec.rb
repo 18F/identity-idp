de5bb41fc548c00a04a87aa122f6c8901f3a4e56
Sonia Connolly <sonia.connolly@gsa.gov>
2024-01-12 15:27:28 -0800

    Fix agreement controller spec expectation

--- spec/controllers/idv/agreement_controller_spec.rb
+++ spec/controllers/idv/agreement_controller_spec.rb
@@ -160,10 +160,10 @@ RSpec.describe Idv::AgreementController do
           }.from(nil).to(true)
         end
 
-        it 'redirects to hybrid handoff' do
+        it 'redirects to how to verify' do
           put :update, params: params
 
-          expect(response).to redirect_to(idv_hybrid_handoff_url)
+          expect(response).to redirect_to(idv_how_to_verify_url)
         end
       end
 
