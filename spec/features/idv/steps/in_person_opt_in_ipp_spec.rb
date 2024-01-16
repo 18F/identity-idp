7affe945c695b3c8c14f55dac909191cae59498a
Sonia Connolly <sonia.connolly@gsa.gov>
2024-01-16 12:19:16 -0800

    Update Address is now a link, not a button. Update spec.

--- spec/features/idv/steps/in_person_opt_in_ipp_spec.rb
+++ spec/features/idv/steps/in_person_opt_in_ipp_spec.rb
@@ -179,7 +179,7 @@ RSpec.describe 'In Person Proofing - Opt-in IPP ', js: true do
       expect(page).to have_current_path(idv_in_person_verify_info_path)
 
       # click update address button
-      click_button t('idv.buttons.change_address_label')
+      click_link t('idv.buttons.change_address_label')
       expect(page).to have_content(t('in_person_proofing.headings.update_address'))
       click_button t('forms.buttons.submit.update')
       expect(page).to have_content(t('headings.verify'))
