2c437fd280dba806e6d75b554d4730fdd92b4ed8
Eileen McFarland <eileenmcfarland@navapbc.com>
2024-01-18 15:19:36 -0500

    refactor document_capture_session & document_capture_session_result to use selfie_status instead of selfie_success

--- app/services/document_capture_session_result.rb
+++ app/services/document_capture_session_result.rb
@@ -10,11 +10,11 @@ DocumentCaptureSessionResult = RedactedStruct.new(
   :failed_back_image_fingerprints,
   :captured_at,
   :selfie_check_performed,
-  :doc_auth_success, :selfie_success,
+  :doc_auth_success, :selfie_status,
   keyword_init: true,
   allowed_members: [:id, :success, :attention_with_barcode, :failed_front_image_fingerprints,
                     :failed_back_image_fingerprints, :captured_at, :selfie_check_performed,
-                    :doc_auth_success, :selfie_success]
+                    :doc_auth_success, :selfie_status]
 ) do
   def self.redis_key_prefix
     'dcs:result'
