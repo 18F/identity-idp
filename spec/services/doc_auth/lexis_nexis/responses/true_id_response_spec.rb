4c17ca2a050e23d5f366305e68d3794177156e83
Brittany Greaner <brittany.greaner@gsa.gov>
2024-01-19 09:34:29 -0800

    LG-12038: Update all_passed? to consider selfie success (#9937)
    * changelog: Internal, Doc Auth, Update all_passed? to consider selfie success
    
    Technical notes:
    
    - Since `all_passed?` is a private method, I created tests for the
    method that calls it (`successful_result?`).
        - Tests did not yet exist for this method, but rather than backfill
        them I focused on selfie, assuming all other checks passed
    - I found the values in `true_id_request_spec.rb` for
    `PORTRAIT_MATCH_RESULT` were off - (according to our documentation)[https://drive.google.com/file/d/1MmXMYkFqPsn2VMbsMVYAUyC-amLRHVCp/view],
    (pg 228) FaceMatchResult will either return `Pass` or `Fail`.
    - Because `selfie_success` can return `nil` if selfie is not performed,
    and because we don't want it to affect the outcome even if it somehow
    did get a response of `false` when selfie capture was not enabled, we
    simply return `true` if `liveness_checking_enabled` is `false`.

--- spec/services/doc_auth/lexis_nexis/responses/true_id_response_spec.rb
+++ spec/services/doc_auth/lexis_nexis/responses/true_id_response_spec.rb
@@ -2,40 +2,27 @@ require 'rails_helper'
 
 RSpec.describe DocAuth::LexisNexis::Responses::TrueIdResponse do
   let(:success_response_body) { LexisNexisFixtures.true_id_response_success_3 }
-  let(:success_with_liveness_response_body) do
-    LexisNexisFixtures.true_id_response_success_with_liveness
-  end
   let(:success_response) do
     instance_double(Faraday::Response, status: 200, body: success_response_body)
   end
-  let(:success_with_liveness_response) do
-    instance_double(Faraday::Response, status: 200, body: success_with_liveness_response_body)
-  end
-  let(:failure_body_no_liveness) { LexisNexisFixtures.true_id_response_failure_no_liveness }
-  let(:failure_body_with_liveness) { LexisNexisFixtures.true_id_response_failure_with_liveness }
-  let(:failure_body_with_all_failures) do
-    LexisNexisFixtures.true_id_response_failure_with_all_failures
-  end
-  let(:failure_body_no_liveness_low_dpi) do
-    LexisNexisFixtures.true_id_response_failure_no_liveness_low_dpi
-  end
-
-  let(:failure_body_tampering) do
-    LexisNexisFixtures.true_id_response_failure_tampering
-  end
-
   # rubocop:disable Layout/LineLength
+  let(:success_with_liveness_response) do
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_success_with_liveness)
+  end
+  let(:failure_response_face_match_fail) do
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_with_face_match_fail)
+  end
   let(:failure_response_no_liveness) do
-    instance_double(Faraday::Response, status: 200, body: failure_body_no_liveness)
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_failure_no_liveness)
   end
   let(:failure_response_with_liveness) do
-    instance_double(Faraday::Response, status: 200, body: failure_body_with_liveness)
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_failure_with_liveness)
   end
   let(:failure_response_tampering) do
-    instance_double(Faraday::Response, status: 200, body: failure_body_tampering)
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_failure_tampering)
   end
   let(:failure_response_with_all_failures) do
-    instance_double(Faraday::Response, status: 200, body: failure_body_with_all_failures)
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_failure_with_all_failures)
   end
   let(:communications_error_response) do
     instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.communications_error)
@@ -53,7 +40,7 @@ RSpec.describe DocAuth::LexisNexis::Responses::TrueIdResponse do
     instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_barcode_read_attention)
   end
   let(:failure_response_no_liveness_low_dpi) do
-    instance_double(Faraday::Response, status: 200, body: failure_body_no_liveness_low_dpi)
+    instance_double(Faraday::Response, status: 200, body: LexisNexisFixtures.true_id_response_failure_no_liveness_low_dpi)
   end
   # rubocop:enable Layout/LineLength
 
@@ -688,6 +675,42 @@ RSpec.describe DocAuth::LexisNexis::Responses::TrueIdResponse do
         let(:response) { described_class.new(failure_response_with_liveness, config, true) }
         it 'returns false' do
           expect(response.selfie_success).to eq(false)
+        end
+      end
+    end
+  end
+
+  describe '#successful_result?' do
+    context 'when all checks other than selfie pass' do
+      context 'and selfie check is enabled' do
+        liveness_checking_enabled = true
+
+        it 'returns true with a passing selfie' do
+          response = described_class.new(
+            success_with_liveness_response, config, liveness_checking_enabled
+          )
+
+          expect(response.successful_result?).to eq(true)
+        end
+
+        it 'returns false with a failing selfie' do
+          response = described_class.new(
+            failure_response_face_match_fail, config, liveness_checking_enabled
+          )
+
+          expect(response.successful_result?).to eq(false)
+        end
+      end
+
+      context 'and selfie check is disabled' do
+        liveness_checking_enabled = false
+
+        it 'returns true no matter what the value of selfie is' do
+          response = described_class.new(
+            failure_response_face_match_fail, config, liveness_checking_enabled
+          )
+
+          expect(response.successful_result?).to eq(true)
         end
       end
     end
