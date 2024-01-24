0ca7d1957f15128a1df36cf5ae0c9bfbe8e90241
Charley Ferguson <charleyferguson@navapbc.com>
2024-01-24 16:13:31 -0500

    Send Selfie Errors from BE to FE (#9941)
    changelog: User Facing Improvements, In-Person Proofing, add error messages behind feature flag for new liveness checks

--- app/services/doc_auth/response.rb
+++ app/services/doc_auth/response.rb
@@ -1,6 +1,7 @@
 module DocAuth
   class Response
-    attr_reader :errors, :exception, :extra, :pii_from_doc, :doc_type_supported
+    attr_reader :errors, :exception, :extra, :pii_from_doc, :doc_type_supported, :selfie_live,
+                :selfie_quality_good
 
     ID_TYPE_SLUGS = {
       'Identification Card' => 'state_id_card',
@@ -15,7 +16,9 @@ module DocAuth
       pii_from_doc: {},
       attention_with_barcode: false,
       doc_type_supported: true,
-      selfie_check_performed: false
+      selfie_check_performed: false,
+      selfie_live: true,
+      selfie_quality_good: true
     )
       @success = success
       @errors = errors.to_h
@@ -25,6 +28,8 @@ module DocAuth
       @attention_with_barcode = attention_with_barcode
       @doc_type_supported = doc_type_supported
       @selfie_check_performed = selfie_check_performed
+      @selfie_live = selfie_live
+      @selfie_quality_good = selfie_quality_good
     end
 
     def merge(other)
@@ -36,6 +41,8 @@ module DocAuth
         pii_from_doc: pii_from_doc.merge(other.pii_from_doc),
         attention_with_barcode: attention_with_barcode? || other.attention_with_barcode?,
         doc_type_supported: doc_type_supported? || other.doc_type_supported?,
+        selfie_live: selfie_live?,
+        selfie_quality_good: selfie_quality_good?,
       )
     end
 
@@ -47,6 +54,14 @@ module DocAuth
       @doc_type_supported
     end
 
+    def selfie_live?
+      @selfie_live
+    end
+
+    def selfie_quality_good?
+      @selfie_quality_good
+    end
+
     # We use `#to_h` to serialize this for logging. Make certain not to include
     # the `#pii` value here.
     def to_h
@@ -56,6 +71,8 @@ module DocAuth
         exception: exception,
         attention_with_barcode: attention_with_barcode?,
         doc_type_supported: doc_type_supported?,
+        selfie_live: selfie_live?,
+        selfie_quality_good: selfie_quality_good?,
         doc_auth_success: doc_auth_success?,
         selfie_success: selfie_success,
       }.merge(extra)
