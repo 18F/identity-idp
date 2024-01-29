4f165702c7d52970a5aab961b21d81187554ccfc
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2024-01-29 11:30:53 -0500

    normalize-yaml: Enhance to collapse spacing (#9980)
    * normalize-yaml: Enhance to collapse spacing between sentences
    
    changelog: User-Facing Improvements, Content, Improve consistency of spacing between content sentences
    
    * Normalize YAML
    
    * Fix renamed file for package.json
    
    * General spacing collapse
    
    See: https://github.com/18F/identity-idp/pull/9980#discussion_r1467926309
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/javascript/packages/normalize-yaml/visitors/index.js
+++ app/javascript/packages/normalize-yaml/visitors/index.js
@@ -1,18 +1,32 @@
 import smartPunctuation from './smart-punctuation.js';
 import sortKeys from './sort-keys.js';
+import collapseSpacing from './collapse-spacing.js';
 
 /** @typedef {import('yaml').visitor} Visitor */
 /** @typedef {import('../').Formatter} Formatter */
 
 /** @type {Record<Formatter, Visitor>} */
-const DEFAULT_VISITORS = { smartPunctuation, sortKeys };
+const DEFAULT_VISITORS = { smartPunctuation, sortKeys, collapseSpacing };
+
+/** @type {(...callbacks: Array<(...args: any[]) => any>) => (...args: any[]) => void} */
+const over =
+  (...callbacks) =>
+  (...args) =>
+    callbacks.forEach((callback) => callback(...args));
 
 /**
  * @param {{ exclude?: Formatter[] }} exclude
  *
  * @return {Visitor}
  */
-export const getVisitors = ({ exclude = [] }) =>
+export const getUnifiedVisitor = ({ exclude = [] }) =>
   Object.entries(DEFAULT_VISITORS)
     .filter(([formatter]) => !exclude.includes(/** @type {Formatter} */ (formatter)))
-    .reduce((result, [, visitor]) => Object.assign(result, visitor), /* @type {Visitor} */ {});
+    .map(([_formatter, visitor]) => visitor)
+    .reduce((result, visitor) => {
+      Object.entries(visitor).forEach(([key, callback]) => {
+        result[key] = result[key] ? over(result[key], callback) : callback;
+      });
+
+      return result;
+    }, /** @type {Visitor} */ ({}));
