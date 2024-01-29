19e47acc6f22559d296b0e2cc1243e26506600f9
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-01-29 13:40:12 -0500

    remove comma typo

--- app/models/document_capture_session.rb
+++ app/models/document_capture_session.rb
@@ -19,7 +19,7 @@ class DocumentCaptureSession < ApplicationRecord
     session_result.attention_with_barcode = doc_auth_response.attention_with_barcode?
     session_result.selfie_check_performed = doc_auth_response.selfie_check_performed?
     session_result.doc_auth_success = doc_auth_response.doc_auth_success?
-    session_result.selfie_status = doc_auth_response.selfie_status,
+    session_result.selfie_status = doc_auth_response.selfie_status
     EncryptedRedisStructStorage.store(
       session_result,
       expires_in: IdentityConfig.store.doc_capture_request_valid_for_minutes.minutes.seconds.to_i,
