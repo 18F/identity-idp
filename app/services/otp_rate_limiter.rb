3d020b94aef7865688c6dc00736859f62ca6f946
John Maxwell <john.maxwell@gsa.gov>
2024-01-29 15:04:51 -0500

    Debugging in progress. Pushed to change branches.

--- app/services/otp_rate_limiter.rb
+++ app/services/otp_rate_limiter.rb
@@ -6,40 +6,73 @@ class OtpRateLimiter
   end
 
   def exceeded_otp_send_limit?
+    puts "def exceeded_otp_send_limit?"
+
     if rate_limit_period_expired?
       reset_count_and_otp_last_sent_at
+      puts "  rate limit period expired; exceeded_otp_send_limit?: returning false"
       return false
     end
 
-    max_requests_reached?
+    rv = max_requests_reached?
+    puts "  exceeded_otp_send_limit?: returning #{rv.inspect}"
+    rv
   end
 
   def max_requests_reached?
-    rate_limiter.limited?
+    puts "def max_requests_reached?"
+
+    rv = rate_limiter.limited?
+    puts "  max_requests_reached?: returning #{rv.inspect}"
+    rv
   end
 
   def rate_limit_period_expired?
-    rate_limiter.expired?
+    puts "def rate_limit_period_expired?"
+
+    rv = rate_limiter.expired?
+    puts "  rate_limit_period_expired?: returning #{rv.inspect}"
+    rv
   end
 
   def reset_count_and_otp_last_sent_at
-    rate_limiter.reset!
+    puts "def reset_count_and_otp_last_sent_at"
+
+    rv = rate_limiter.reset!
+    puts "  reset_count_and_otp_last_sent_at: returning #{rv.inspect}"
+    rv
   end
 
   def lock_out_user
-    UpdateUser.new(user: user, attributes: { second_factor_locked_at: Time.zone.now }).call
+    puts "def lock_out_user"
+
+    rv = UpdateUser.new(user: user, attributes: { second_factor_locked_at: Time.zone.now }).call
+    puts "  lock_out_user: returning #{rv.inspect}"
+    rv
   end
 
   def increment
-    rate_limiter.increment!
+    puts "def increment"
+
+    rv = rate_limiter.increment!
+    puts "  increment: returning #{rv.inspect}"
+    rv
   end
 
   def otp_last_sent_at
-    rate_limiter.attempted_at
+    puts "def otp_last_sent_at"
+
+    rv = rate_limiter.attempted_at
+    puts "  otp_last_sent_at: returning: #{rv.inspect}"
+    rv
   end
 
   def rate_limiter
-    @rate_limiter ||= RateLimiter.new(rate_limit_type: :phone_otp, target: rate_limit_key)
+    puts "def rate_limiter"
+
+    rv = (@rate_limiter ||= RateLimiter.new(rate_limit_type: :phone_otp, target: rate_limit_key))
+    puts "  rate_limiter: returning #{rv.inspect}"
+    rv
   end
 
   private
@@ -47,18 +80,34 @@ class OtpRateLimiter
   attr_reader :phone, :user, :phone_confirmed
 
   def otp_findtime
-    IdentityConfig.store.otp_delivery_blocklist_findtime.minutes
+    puts "def otp_findtime"
+
+    rv = IdentityConfig.store.otp_delivery_blocklist_findtime.minutes
+    puts "  otp_findtime: returning #{rv.inspect}"
+    rv
   end
 
   def otp_maxretry_times
-    IdentityConfig.store.otp_delivery_blocklist_maxretry
+    puts "def otp_maxretry_times"
+
+    rv = IdentityConfig.store.otp_delivery_blocklist_maxretry
+    puts "  otp_maxretry_times: returning #{rv.inspect}"
+    rv
   end
 
   def phone_fingerprint
-    @phone_fingerprint ||= Pii::Fingerprinter.fingerprint(PhoneFormatter.format(phone))
+    puts "def phone_fingerprint"
+
+    rv = @phone_fingerprint ||= Pii::Fingerprinter.fingerprint(PhoneFormatter.format(phone))
+    puts "  phone_fingerprint: returning #{rv.inspect}"
+    rv
   end
 
   def rate_limit_key
-    "#{phone_fingerprint}:#{phone_confirmed}"
+    puts "def rate_limit_key"
+
+    rv = "#{phone_fingerprint}:#{phone_confirmed}"
+    puts "  rate_limit_key: returning #{rv.inspect}"
+    rv
   end
 end
