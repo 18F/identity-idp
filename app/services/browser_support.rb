cbfc2739835c0b7dab5484479933c3113f5a329f
Andrew Duthie <andrew.duthie@gsa.gov>
2024-01-30 12:22:27 -0500

    Browser support: Check iOS Safari engine for all iOS browsers
    changelog: Internal, Browser Support, Improve browser detection for iOS browsers

--- app/services/browser_support.rb
+++ app/services/browser_support.rb
@@ -1,22 +1,20 @@
 class BrowserSupport
   @cache = LruRedux::Cache.new(1_000)
 
-  # rubocop:disable Layout/LineLength
   BROWSERSLIST_TO_BROWSER_MAP = {
-    and_chr: ->(browser, version) { browser.android? && !browser.platform.android_webview? && browser.chrome?(version) },
+    and_chr: ->(browser, version) { browser.android? && browser.chrome?(version) },
     and_uc: ->(browser, version) { browser.android? && browser.uc_browser?(version) },
-    android: ->(browser, version) { browser.platform.android_webview? && browser.send(:detect_version?, browser.version, version) },
-    chrome: ->(browser, version) { !browser.platform.android_webview? && browser.chrome?(version) },
+    android: ->(browser, version) { browser.send(:detect_version?, browser.version, version) },
+    chrome: ->(browser, version) { browser.chrome?(version) },
     edge: ->(browser, version) { browser.edge?(version) },
     firefox: ->(browser, version) { browser.firefox?(version) },
-    ios_saf: ->(browser, version) { browser.ios? && browser.safari?(version) },
+    ios_saf: ->(browser, version) { browser.ios?(version) },
     op_mini: ->(browser, version) { browser.opera_mini?(version) },
     op_mob: ->(browser, version) { browser.platform.android? && browser.opera?(version) },
     opera: ->(browser, version) { browser.opera?(version) },
     safari: ->(browser, version) { browser.safari?(version) },
     samsung: ->(browser, version) { browser.samsung_browser?(version) },
-  }.with_indifferent_access.freeze
-  # rubocop:enable Layout/LineLength
+  }.freeze
 
   class << self
     def supported?(user_agent)
@@ -24,7 +22,8 @@ class BrowserSupport
       return true if browser_support_config.nil?
 
       cache.getset(user_agent) do
-        matchers.any? { |matcher| matcher.call(BrowserCache.parse(user_agent)) }
+        browser = BrowserCache.parse(user_agent)
+        matchers(browserslist_to_browser_map(browser)).any? { |matcher| matcher.call(browser) }
       end
     end
 
@@ -38,10 +37,20 @@ class BrowserSupport
 
     attr_reader :cache
 
-    def matchers
+    def browserslist_to_browser_map(browser)
+      if browser.ios?
+        BROWSERSLIST_TO_BROWSER_MAP.slice(:ios_saf)
+      elsif browser.platform.android_webview?
+        BROWSERSLIST_TO_BROWSER_MAP.slice(:android)
+      else
+        BROWSERSLIST_TO_BROWSER_MAP
+      end
+    end
+
+    def matchers(mapping)
       @matchers ||= browser_support_config.flat_map do |config_entry|
         key, version = config_entry.split(' ', 2)
-        browser_matcher = BROWSERSLIST_TO_BROWSER_MAP[key]
+        browser_matcher = mapping[key.to_sym]
         next [] if !browser_matcher
 
         low_version, _high_version = version.split('-', 2)
