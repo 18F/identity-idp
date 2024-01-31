34740242b24cca04207c11a5600644643858892b
Malick Diarra <malick.diarra@gsa.gov>
2024-01-31 15:57:58 -0500

    change to sms

--- app/controllers/users/delete_controller.rb
+++ app/controllers/users/delete_controller.rb
@@ -14,7 +14,7 @@ module Users
       irs_attempts_api_tracker.logged_in_account_purged(success: true)
       send_push_notifications
       notify_user_via_email_of_deletion
-      notify_user_via_text_of_deletion
+      notify_user_via_sms_of_deletion
       delete_user
       sign_out
       flash[:success] = t('devise.registrations.destroyed')
@@ -60,7 +60,7 @@ module Users
       end
     end
 
-    def notify_user_via_text_of_deletion
+    def notify_user_via_sms_of_deletion
       phone_configurations = current_user.confirmed_phone_configurations
       phone_configurations.each do |configuration|
         next unless configuration.capabilities.supports_sms?
