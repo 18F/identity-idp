0c92db2051b29d639c36a6e02b1bbce0818845f7
Matt Hinz <matt.hinz@gsa.gov>
2024-01-30 16:43:35 -0800

    Actually run through all of remote unsupervised auth
    (Need to clean up the document capture step)

--- spec/features/idv/step_up_spec.rb
+++ spec/features/idv/step_up_spec.rb
@@ -15,21 +15,29 @@ RSpec.describe 'IdV step up flow', allowed_extra_analytics: [:*] do
     allow(IdentityConfig.store).to receive(:doc_auth_selfie_capture_enabled).and_return(true)
   end
 
-  scenario 'User with active profile can redo idv when selfie required' do
+  scenario 'User with active profile can redo idv when selfie required', js: true do
     visit_idp_from_sp_with_ial2(sp, biometric_comparison_required: true)
     sign_in_live_with_2fa(user)
 
     expect(page).to have_current_path(idv_welcome_path)
 
-    click_continue
+    complete_doc_auth_steps_before_document_capture_step
 
-    expect(page).to have_current_path(idv_agreement_path)
+    # TODO: Refactor this
+    attach_images
+    attach_selfie
+    submit_images
+
+    complete_ssn_step
+    complete_verify_step
+    complete_phone_step(user)
+    complete_enter_password_step(user)
+    acknowledge_and_confirm_personal_key
   end
 
   scenario 'User with active profile cannot redo idv when selfie not required' do
     visit_idp_from_sp_with_ial2(sp)
     sign_in_live_with_2fa(user)
-
     expect(page).to have_current_path(sign_up_completed_path)
   end
 end
