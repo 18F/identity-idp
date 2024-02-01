625436175cb9c4eafad9acd520172c0c63073d78
kevinsmaster5 <kevin.masters@gsa.gov>
2024-02-01 14:35:50 -0500

    fix mfa feature spec

--- spec/support/features/session_helper.rb
+++ spec/support/features/session_helper.rb
@@ -547,7 +547,6 @@ module Features
 
     def set_up_mfa_with_backup_codes
       click_on t('forms.buttons.continue')
-      click_on t('forms.buttons.continue')
     end
 
     def register_user(email = 'test@test.com')
@@ -593,7 +592,7 @@ module Features
 
     def set_up_2fa_with_backup_codes
       select_2fa_option('backup_code')
-      
+
       expect(page).to have_current_path auth_method_confirmation_path
     end
 
