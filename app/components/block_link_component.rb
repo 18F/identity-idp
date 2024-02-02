ba60b4b467f062be70de5671447842256bbfe6be
Andrew Duthie <andrew.duthie@gsa.gov>
2024-02-02 09:28:19 -0500

    wip

--- app/components/block_link_component.rb
+++ app/components/block_link_component.rb
@@ -3,9 +3,9 @@ class BlockLinkComponent < BaseComponent
 
   alias_method :new_tab?, :new_tab
 
-  def initialize(url: '#', render_as_link: true, new_tab: false, **tag_options)
+  def initialize(url: nil, action: tag.method(:a), new_tab: false, **tag_options)
+    @action = action
     @url = url
-    @render_as_link = render_as_link
     @new_tab = new_tab
     @tag_options = tag_options
   end
@@ -21,11 +21,6 @@ class BlockLinkComponent < BaseComponent
   end
 
   def wrapper(&block)
-    if render_as_link
-      action = tag.method(:a)
-      action.call(**tag_options, href: url, class: css_class, target:, &block)
-    else
-      content_tag(:div, capture(&block), class: 'usa-link block-link')
-    end
+    action.call(**tag_options, href: url, class: css_class, target:, &block)
   end
 end
