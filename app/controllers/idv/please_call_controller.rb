a1feb3b0a4b8e8d54a53ed221ea00a772560208b
JackRyan1989 <jack.ryan@gsa.gov>
2024-02-05 13:02:50 -0500

    edits to please call controller

--- app/controllers/idv/please_call_controller.rb
+++ app/controllers/idv/please_call_controller.rb
@@ -25,8 +25,8 @@ module Idv
     end
 
     def in_person_proofing?
-      enrollment_exists = current_user&.pending_profile&.in_person_enrollment
-      IdentityConfig.store.in_person_proofing_enabled && enrollment_exists
+      IdentityConfig.store.in_person_proofing_enabled && 
+        current_user&.pending_profile&.in_person_enrollment.status == 'passed'
     end
   end
 end
