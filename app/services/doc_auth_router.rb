7ff6c51e8585b91136bc7cb30e62f2c965a6227f
charleyf <charleyferguson@navapbc.com>
2024-02-06 14:08:47 -0500

    Remove unnessecary error message and routing

--- app/services/doc_auth_router.rb
+++ app/services/doc_auth_router.rb
@@ -54,8 +54,6 @@ module DocAuthRouter
     # i18n-tasks-use t('doc_auth.errors.alerts.ref_control_number_check')
     DocAuth::Errors::REF_CONTROL_NUMBER_CHECK =>
       'doc_auth.errors.alerts.ref_control_number_check',
-    # i18n-tasks-use t('doc_auth.errors.alerts.selfie_failure')
-    DocAuth::Errors::SELFIE_FAILURE => 'doc_auth.errors.alerts.selfie_fail',
     # i18n-tasks-use t('doc_auth.errors.alerts.selfie_not_live')
     DocAuth::Errors::SELFIE_NOT_LIVE => 'doc_auth.errors.alerts.selfie_not_live',
     # i18n-tasks-use t('doc_auth.errors.alerts.selfie_poor_quality')
