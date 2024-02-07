c1872571688b436bc6e6cac49ad2f72bffe9af1b
Andrew Duthie <andrew.duthie@gsa.gov>
2024-02-07 15:26:43 -0500

    Revert to export from index
    Avoid dealing with dependency stubbing issue

--- app/javascript/packages/analytics/index.spec.ts
+++ app/javascript/packages/analytics/index.spec.ts
@@ -1,4 +1,4 @@
-import { trackEvent } from '@18f/identity-analytics';
+import { trackEvent, trackError } from '@18f/identity-analytics';
 import { useSandbox } from '@18f/identity-test-helpers';
 import type { SinonStub } from 'sinon';
 
@@ -79,6 +79,55 @@ describe('trackEvent', () => {
         const result = trackEvent('name');
         expect(result).to.be.undefined();
       });
+    });
+  });
+});
+
+describe('trackError', () => {
+  const sandbox = useSandbox();
+  const endpoint = '/log';
+
+  beforeEach(() => {
+    sandbox.stub(global.navigator, 'sendBeacon').returns(true);
+    document.body.innerHTML = `<script type="application/json" data-config>{"analyticsEndpoint":"${endpoint}"}</script>`;
+  });
+
+  it('tracks event', async () => {
+    trackError(new Error('Oops!'));
+
+    expect(global.navigator.sendBeacon).to.have.been.calledOnce();
+
+    const [actualEndpoint, data] = (global.navigator.sendBeacon as SinonStub).firstCall.args;
+    expect(actualEndpoint).to.eql(endpoint);
+
+    const { event, payload } = JSON.parse(await data.text());
+    const { name, message, stack } = payload;
+
+    expect(event).to.equal('Frontend Error');
+    expect(name).to.equal('Error');
+    expect(message).to.equal('Oops!');
+    expect(stack).to.be.a('string');
+  });
+
+  context('with event parameter', () => {
+    it('tracks event', async () => {
+      const error = new Error('Oops!');
+      const errorEvent = new ErrorEvent('error', { error, filename: 'file.js' });
+      trackError(error, errorEvent);
+
+      expect(global.navigator.sendBeacon).to.have.been.calledOnce();
+
+      const [actualEndpoint, data] = (global.navigator.sendBeacon as SinonStub).firstCall.args;
+      expect(actualEndpoint).to.eql(endpoint);
+
+      const { event, payload } = JSON.parse(await data.text());
+      const { name, message, stack, filename } = payload;
+
+      expect(event).to.equal('Frontend Error');
+      expect(name).to.equal('Error');
+      expect(message).to.equal('Oops!');
+      expect(stack).to.be.a('string');
+      expect(filename).to.equal('file.js');
     });
   });
 });
--- app/javascript/packages/analytics/index.ts
+++ app/javascript/packages/analytics/index.ts
@@ -1,3 +1,29 @@
+import { getConfigValue } from '@18f/identity-config';
+
 export { default as isTrackableErrorEvent } from './is-trackable-error-event';
-export { default as trackError } from './track-error';
-export { default as trackEvent } from './track-event';
+
+/**
+ * Logs an event.
+ *
+ * @param event Event name.
+ * @param payload Payload object.
+ */
+export function trackEvent(event: string, payload?: object) {
+  const endpoint = getConfigValue('analyticsEndpoint');
+
+  // Make analytics requests using sendBeacon(), which can be prioritized appropriately by the
+  // browser and have a better chance of succeeding during page unload than fetch().
+  if (endpoint && navigator.sendBeacon) {
+    const eventJson = JSON.stringify({ event, payload });
+    const blob = new Blob([eventJson], { type: 'application/json' });
+    navigator.sendBeacon(endpoint, blob);
+  }
+}
+
+/**
+ * Logs an error.
+ *
+ * @param error Error object.
+ */
+export const trackError = ({ name, message, stack }: Error, event?: ErrorEvent) =>
+  trackEvent('Frontend Error', { name, message, stack, filename: event?.filename });
