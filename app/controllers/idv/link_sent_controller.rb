e0f8ee69664b48b405da29957644ad13a7a23201
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-02-09 19:27:23 -0500

    remove unnecessary document_capture_session_result method

--- app/controllers/idv/link_sent_controller.rb
+++ app/controllers/idv/link_sent_controller.rb
@@ -80,12 +80,7 @@ module Idv
     end
 
     def take_photo_with_phone_successful?
-      document_capture_session_result.present? && document_capture_session_result.success? &&
-        selfie_requirement_met?
-    end
-
-    def document_capture_session_result
-      @document_capture_session_result ||= document_capture_session&.load_result
+      stored_result&.success? && selfie_requirement_met?
     end
   end
 end
