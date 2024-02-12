94f8f7f070394d3d5c38a726707025e1d4bc6fb0
kevinsmaster5 <kevin.masters@gsa.gov>
2024-02-12 13:44:41 -0500

    fix spec

--- spec/controllers/users/backup_code_setup_controller_spec.rb
+++ spec/controllers/users/backup_code_setup_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Users::BackupCodeSetupController, allowed_extra_analytics: [:*] d
         :authenticate_user!,
         :confirm_user_authenticated_for_2fa_setup,
         :apply_secure_headers_override,
-        [:confirm_recently_authenticated_2fa, except: ['reminder']],
       )
     end
   end
@@ -22,8 +21,26 @@ RSpec.describe Users::BackupCodeSetupController, allowed_extra_analytics: [:*] d
     Funnel::Registration::AddMfa.call(user.id, 'phone', analytics)
     expect(PushNotification::HttpPush).to receive(:deliver).
       with(PushNotification::RecoveryInformationChangedEvent.new(user: user))
+
     expect(@analytics).to receive(:track_event).
-      with('User marked authenticated', { authentication_type: :valid_2fa_confirmation })
+      with('Multi-Factor Authentication Setup', {
+        enabled_mfa_methods_count: 2,
+        errors: nil,
+        in_account_creation_flow: false,
+        multi_factor_auth_method: 'backup_codes',
+        success: true
+      })
+    expect(@analytics).to receive(:track_event).
+      with('Backup Code Created', {
+        enabled_mfa_methods_count: 2,
+        in_account_creation_flow: false,
+      })
+    expect(@irs_attempts_api_tracker).to receive(:track_event).
+      with(:mfa_enroll_backup_code, success: true)
+
+    expect(@analytics).to receive(:track_event).
+      with('User marked authenticated', {:authentication_type=>:valid_2fa_confirmation})
+
     expect(@analytics).to receive(:track_event).
       with('Backup Code Setup Visited', {
         success: true,
@@ -34,16 +51,8 @@ RSpec.describe Users::BackupCodeSetupController, allowed_extra_analytics: [:*] d
         enabled_mfa_methods_count: 1,
         in_account_creation_flow: false,
       })
-    expect(@analytics).to receive(:track_event).
-      with('Backup Code Created', {
-        enabled_mfa_methods_count: 2,
-        in_account_creation_flow: false,
-      })
-    expect(@irs_attempts_api_tracker).to receive(:track_event).
-      with(:mfa_enroll_backup_code, success: true)
-
     post :create
-
+    
     expect(response).to render_template('create')
     expect(user.backup_code_configurations.length).to eq BackupCodeGenerator::NUMBER_OF_CODES
   end
