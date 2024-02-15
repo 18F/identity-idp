8ded9b0e898f3a2b741f28dc539a4bcab280a098
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2024-02-15 12:52:17 -0500

    Remove fallback logging for unknown frontend event (#10086)
    changelog: Internal, Analytics, Remove fallback logging for unknown frontend event

--- app/services/frontend_logger.rb
+++ app/services/frontend_logger.rb
@@ -25,8 +25,6 @@ class FrontendLogger
       analytics_method.call(**hash_from_kwargs(attributes, analytics_method))
       true
     else
-      # 2023-10-31 - Temporary
-      analytics.track_event("Frontend (warning): #{name}", attributes)
       false
     end
   end
