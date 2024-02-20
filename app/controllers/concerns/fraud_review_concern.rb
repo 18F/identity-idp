98ae90b78d8ba84ce58e75ca59f71704b436eab2
JackRyan1989 <jack.ryan@gsa.gov>
2024-02-20 16:44:32 -0500

    refer to new methods and simplify code to handle redirection only when ipp passed

--- app/controllers/concerns/fraud_review_concern.rb
+++ app/controllers/concerns/fraud_review_concern.rb
@@ -4,11 +4,9 @@ module FraudReviewConcern
   delegate :fraud_check_failed?,
            :fraud_review_pending?,
            :fraud_rejection?,
-           :ipp_fraud_review_pending?,
            to: :fraud_review_checker
 
   def handle_fraud
-    in_person_handle_pending_fraud_review
     handle_pending_fraud_review
     handle_fraud_rejection
   end
@@ -16,25 +14,19 @@ module FraudReviewConcern
   def handle_pending_fraud_review
     # If the user has not passed IPP at a post office, allow them to
     # complete another enrollment by not redirecting to please call
-    return if in_person_can_perform_fraud_review?
+    # or rejection screen
+    return if in_person_prevent_fraud_redirection?
     redirect_to_fraud_review if fraud_review_pending?
   end
 
   def handle_fraud_rejection
+    return if in_person_prevent_fraud_redirection?
     redirect_to_fraud_rejection if fraud_rejection?
   end
 
-  def in_person_handle_pending_fraud_review
-    return unless in_person_can_perform_fraud_review?
-    if fraud_review_pending? && current_user.in_person_enrollment_status == 'passed'
-      redirect_to_fraud_review
-    end
-  end
-
-  def in_person_can_perform_fraud_review?
+  def in_person_prevent_fraud_redirection?
     IdentityConfig.store.in_person_proofing_enforce_tmx &&
-      current_user.in_person_enrollment_status != 'canceled' &&
-      !current_user.in_person_enrollment_status.nil?
+      current_user.in_person_enrollment_status != 'passed'
   end
 
   def redirect_to_fraud_review
