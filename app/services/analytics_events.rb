3d94f395e08219fb1787945c6637102b40128e55
charleyf <charleyferguson@navapbc.com>
2024-02-21 08:58:20 -0500

    Rename to `request_id` and fix tests

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -1013,7 +1013,7 @@ module AnalyticsEvents
   # @param [String] selfie_status
   # @param [String] vendor
   # @param [String] conversation_id
-  # @param [String] reference_id ReferenceID from TrueID
+  # @param [String] request_id RequestId from TrueID
   # @param [String] reference
   # @param [String] transaction_status
   # @param [String] transaction_reason_code
@@ -1062,7 +1062,7 @@ module AnalyticsEvents
     selfie_status: nil,
     vendor: nil,
     conversation_id: nil,
-    reference_id: nil,
+    request_id: nil,
     reference: nil,
     transaction_status: nil,
     transaction_reason_code: nil,
@@ -1099,7 +1099,7 @@ module AnalyticsEvents
       selfie_status:,
       vendor:,
       conversation_id:,
-      reference_id:,
+      request_id:,
       reference:,
       transaction_status:,
       transaction_reason_code:,
