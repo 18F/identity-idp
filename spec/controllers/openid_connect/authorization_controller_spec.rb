bc56dd19819aa26ab836d1da2012a2d7ef9bca14
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-02-21 15:09:43 -0500

    Stop writing to `sp_session[:ialmax]`
    A previous change (ref: https://github.com/18F/identity-idp/pull/10127) stopped reading from `sp_session[:ialmax]` and started using the result from the `AuthnContextResolver` instead.
    
    This change follows up by removing writes to the `ialmax` property in the SP session. This should not be merged until the change to stop reading is fully merged and deployed.
    
    [skip changelog]

--- spec/controllers/openid_connect/authorization_controller_spec.rb
+++ spec/controllers/openid_connect/authorization_controller_spec.rb
@@ -1083,7 +1083,6 @@ RSpec.describe OpenidConnect::AuthorizationController, allowed_extra_analytics: 
           piv_cac_requested: false,
           phishing_resistant_requested: false,
           ial2: false,
-          ialmax: false,
           issuer: 'urn:gov:gsa:openidconnect:test',
           request_id: sp_request_id,
           request_url: request.original_url,
