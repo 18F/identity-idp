5644f353381f03f1462738c8699833460b529452
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-02-23 16:50:25 -0500

    make width, height and fingerprint optional

--- app/javascript/packages/document-capture/components/acuant-capture.tsx
+++ app/javascript/packages/document-capture/components/acuant-capture.tsx
@@ -35,11 +35,11 @@ interface ImageAnalyticsPayload {
   /**
    * Image width, or null if unknown
    */
-  width: number | null;
+  width?: number | null;
   /**
    * Image height, or null if unknown
    */
-  height: number | null;
+  height?: number | null;
   /**
    * Mime type, or null if unknown
    */
@@ -65,7 +65,7 @@ interface ImageAnalyticsPayload {
   /**
    * Fingerprint of the image, base64 encoded SHA-256 digest
    */
-  fingerprint: string | null;
+  fingerprint?: string | null;
 
   /**
    *
@@ -513,9 +513,6 @@ function AcuantCapture(
 
   function onSelfieCaptureSuccess({ image }: { image: string }) {
     const analyticsPayload: ImageAnalyticsPayload = getAddAttemptAnalyticsPayload({
-      width: null,
-      height: null,
-      fingerprint: null,
       mimeType: 'image/jpeg', // Acuant Web SDK currently encodes all images as JPEG
       source: 'acuant',
       size: getDecodedBase64ByteSize(image),
