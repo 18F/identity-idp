052d6efc3f862081a844fec2aba042ddabdeb003
John Maxwell <john.maxwell@gsa.gov>
2024-02-27 18:52:24 -0500

    ApplicationController VoT spec for resolved_authn_context_result

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -459,60 +459,54 @@ RSpec.describe ApplicationController do
   end
 
   describe '#resolved_authn_context_result' do
+    let(:sp) { build(:service_provider, ial: 2) }
+
+    let(:sp_session) { { vtr: vtr, acr_values: acr_values } }
+
+    let(:result) { subject.resolved_authn_context_result }
+
+    before do
+      allow(controller).to receive(:sp_from_sp_session).and_return(sp)
+      allow(controller).to receive(:sp_session).and_return(sp_session)
+    end
+
     context 'when using acr values' do
-      it 'returns a resolved authn context result' do
-        sp = build(:service_provider, ial: 2)
-        acr_values = [
+      let(:vtr) { nil }
+      let(:acr_values) do
+        [
           'http://idmanagement.gov/ns/assurance/aal/1',
         ].join(' ')
-        sp_session = { vtr: nil, acr_values: acr_values }
-        allow(controller).to receive(:sp_from_sp_session).and_return(sp)
-        allow(controller).to receive(:sp_session).and_return(sp_session)
+      end
 
-        result = subject.resolved_authn_context_result
-
+      it 'returns a resolved authn context result' do
         expect(result.aal2?).to eq(true)
         expect(result.identity_proofing?).to eq(true)
       end
 
       context 'without an SP' do
+        let(:sp) { nil }
+        let(:sp_session) { nil }
+
         it 'returns a no-SP result' do
-          sp = nil
-          sp_session = {}
-          allow(controller).to receive(:sp_from_sp_session).and_return(sp)
-          allow(controller).to receive(:sp_session).and_return(sp_session)
-
-          result = subject.resolved_authn_context_result
-
           expect(result).to eq(Vot::Parser::Result.no_sp_result)
         end
       end
     end
 
     context 'when using vot values' do
+      let(:acr_values) { nil }
+      let(:vtr) { ['P1'] }
+
       it 'returns a resolved authn context result' do
-        sp = build(:service_provider, ial: 2)
-        acr_values = nil
-        vtr = ['C2.P1']
-        sp_session = { vtr: vtr, acr_values: acr_values }
-        allow(controller).to receive(:sp_from_sp_session).and_return(sp)
-        allow(controller).to receive(:sp_session).and_return(sp_session)
-
-        result = subject.resolved_authn_context_result
-
         expect(result.aal2?).to eq(true)
         expect(result.identity_proofing?).to eq(true)
       end
 
       context 'without an SP' do
+        let(:sp) { nil }
+        let(:sp_session) { nil }
+
         it 'returns a no-SP result' do
-          sp = nil
-          sp_session = {}
-          allow(controller).to receive(:sp_from_sp_session).and_return(sp)
-          allow(controller).to receive(:sp_session).and_return(sp_session)
-
-          result = subject.resolved_authn_context_result
-
           expect(result).to eq(Vot::Parser::Result.no_sp_result)
         end
       end
