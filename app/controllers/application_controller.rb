3f453096fa4ef9f83d6156d98ffe202b291ce30a
John Maxwell <john.maxwell@gsa.gov>
2024-02-27 14:45:55 -0500

    Merge fix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -405,8 +405,8 @@ class ApplicationController < ActionController::Base
       service_provider: sp_from_sp_session,
       auth_methods_session:,
       aal_level_requested: resolved_authn_context_result.aal_level_requested,
-      piv_cac_requested: sp_session[:piv_cac_requested],
-      phishing_resistant_requested: sp_session[:phishing_resistant_requested],
+      piv_cac_requested: resolved_authn_context_result.hspd12?,
+      phishing_resistant_requested: resolved_authn_context_result.phishing_resistant_required?,
     )
   end
   delegate :user_needs_sp_auth_method_setup?, to: :service_provider_mfa_policy
