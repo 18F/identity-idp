e5940cfde991dcbf23a2962d6cd4d2a5302218ef
Dawei Wang <daweiwang@navapbc.com>
2024-02-27 10:37:19 -0500

    LG-12306: clean up.

--- spec/features/idv/doc_auth/hybrid_handoff_spec.rb
+++ spec/features/idv/doc_auth/hybrid_handoff_spec.rb
@@ -12,12 +12,15 @@ RSpec.feature 'hybrid_handoff step send link and errors', allowed_extra_analytic
     IdentityConfig.store.idv_send_link_attempt_window_in_minutes
   end
 
+  before do
+    sign_in_and_2fa_user
+    allow_any_instance_of(ApplicationController).to receive(:analytics).and_return(fake_analytics)
+    allow_any_instance_of(ApplicationController).to receive(:irs_attempts_api_tracker).
+      and_return(fake_attempts_tracker)
+  end
+
   context 'on a desktop device send link' do
     before do
-      sign_in_and_2fa_user
-      allow_any_instance_of(ApplicationController).to receive(:analytics).and_return(fake_analytics)
-      allow_any_instance_of(ApplicationController).to receive(:irs_attempts_api_tracker).
-        and_return(fake_attempts_tracker)
       complete_doc_auth_steps_before_hybrid_handoff_step
     end
 
@@ -204,37 +207,6 @@ RSpec.feature 'hybrid_handoff step send link and errors', allowed_extra_analytic
       document_capture_session = DocumentCaptureSession.find_by(uuid: document_capture_session_uuid)
       expect(document_capture_session).to be
       expect(document_capture_session).to have_attributes(requested_at: a_kind_of(Time))
-    end
-  end
-
-  context 'on a desktop device when selfie required', js: true do
-    let(:user) { user_with_2fa }
-    let(:desktop_selfie_enabled) { false }
-    before do
-      allow(IdentityConfig.store).to receive(:doc_auth_selfie_desktop_test_mode).
-        and_return(desktop_selfie_enabled)
-      expect(FeatureManagement).to receive(:idv_allow_selfie_check?).at_least(:once).
-        and_return(true)
-      sign_in_and_2fa_user(user)
-      visit_idp_from_sp_with_ial2(:oidc, biometric_comparison_required: true)
-      allow_any_instance_of(ApplicationController).to receive(:analytics).and_return(fake_analytics)
-      allow_any_instance_of(ApplicationController).to receive(:irs_attempts_api_tracker).
-        and_return(fake_attempts_tracker)
-      complete_doc_auth_steps_before_document_capture_step
-    end
-    describe 'with desktop selfie disabled' do
-      let(:desktop_selfie_enabled) { false }
-      it 'it prevents from proceeding to document capture' do
-        expect(page).to have_current_path(idv_hybrid_handoff_path)
-        click_on t('forms.buttons.upload_photos')
-        expect(page).to have_current_path(idv_hybrid_handoff_path)
-      end
-    end
-    describe 'with desktop selfie enabled' do
-      let(:desktop_selfie_enabled) { true }
-      it 'it proceeds to document capture' do
-        expect(page).to have_current_path(idv_document_capture_path)
-      end
     end
   end
 end
