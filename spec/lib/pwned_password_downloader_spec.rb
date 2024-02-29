ab84cf730156a43ba39842601c6457b0730fada8
Andrew Duthie <andrew.duthie@gsa.gov>
2024-02-29 16:39:16 -0500

    Assert downloaded contents

--- spec/lib/pwned_password_downloader_spec.rb
+++ spec/lib/pwned_password_downloader_spec.rb
@@ -4,7 +4,11 @@ require 'tempfile'
 
 RSpec.describe PwnedPasswordDownloader do
   subject(:downloader) do
-    PwnedPasswordDownloader.new(destination: @destination, output_progress: false)
+    PwnedPasswordDownloader.new(
+      destination: @destination,
+      output_progress: false,
+      keep_threshold: 5,
+    )
   end
 
   around do |example|
@@ -19,10 +23,11 @@ RSpec.describe PwnedPasswordDownloader do
       body: <<~BODY,
         0005AD76BD555C1D6D771DE417A4B87E4B4:10
         000A8DAE4228F821FB418F59826079BF368:4
+        03643C928B2BCD37475C574E6F31B4650AD:22
       BODY
     )
     stub_request(:get, URI.join(PwnedPasswordDownloader::RANGE_API_ROOT, '00001').to_s).
-      to_return(body: '0005DE2A9668A41F6A508AFB6A6FC4A5610:1')
+      to_return(body: '00C271B56ABE9E5C137217BF2DE657C7B2F:5')
     allow(Thread).to receive(:new).and_yield(rand.to_s)
     allow(downloader).to receive(:queue).and_return([])
     allow(downloader).to receive(:wait_for_progress)
@@ -37,6 +42,13 @@ RSpec.describe PwnedPasswordDownloader do
       run
 
       expect(Dir.entries(@destination)).to eq(['.', '..', '00000', '00001'])
+      expect(File.readlines(File.join(@destination, '00000'))).to eq [
+        "000000005AD76BD555C1D6D771DE417A4B87E4B4:10\n",
+        "0000003643C928B2BCD37475C574E6F31B4650AD:22\n",
+      ]
+      expect(File.readlines(File.join(@destination, '00001'))).to eq [
+        "0000100C271B56ABE9E5C137217BF2DE657C7B2F:5\n",
+      ]
     end
   end
 end
