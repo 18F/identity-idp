c23f56ade645f0063468973190d9d695fde6c676
Keith WIlliams <keithwilliams@navapbc.com>
2024-02-29 16:59:47 -0500

    fix: another linting fix

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -2843,90 +2843,90 @@ def idv_request_letter_visited(
   )
 end
 
-  # @param [Integer] captureAttempts number of attempts to capture / upload an image
-  #                  (previously called "attempt")
-  # User captured and approved of their selfie
-  # rubocop:disable Naming/VariableName,Naming/MethodParameterName
-  def idv_sdk_selfie_image_added(captureAttempts: nil, **extra)
-    track_event(:idv_sdk_selfie_image_added, captureAttempts: captureAttempts, **extra)
-  end
+# @param [Integer] captureAttempts number of attempts to capture / upload an image
+#                  (previously called "attempt")
+# User captured and approved of their selfie
+# rubocop:disable Naming/VariableName,Naming/MethodParameterName
+def idv_sdk_selfie_image_added(captureAttempts: nil, **extra)
+  track_event(:idv_sdk_selfie_image_added, captureAttempts: captureAttempts, **extra)
+end
 
-  # @param [Integer] captureAttempts number of attempts to capture / upload an image
-  #                  (previously called "attempt")
-  # User closed the SDK for taking a selfie without submitting a photo
-  def idv_sdk_selfie_image_capture_closed_without_photo(captureAttempts: nil, **extra)
-    track_event(
-      :idv_sdk_selfie_image_capture_closed_without_photo,
-      captureAttempts: captureAttempts,
-      **extra,
-    )
-  end
-
-  # @param [Integer] sdk_error_code SDK code for the error encountered
-  # @param [String] sdk_error_message SDK message for the error encountered
-  # @param [Integer] captureAttempts number of attempts to capture / upload an image
-  #                  (previously called "attempt")
-  # User encountered an error with the SDK selfie process
-  # Error code 1: camera permission not granted
-  # Error code 2: unexpected errors
-  def idv_sdk_selfie_image_capture_failed(
-    sdk_error_code:,
-    sdk_error_message:,
-    captureAttempts: nil,
-    **extra
+# @param [Integer] captureAttempts number of attempts to capture / upload an image
+#                  (previously called "attempt")
+# User closed the SDK for taking a selfie without submitting a photo
+def idv_sdk_selfie_image_capture_closed_without_photo(captureAttempts: nil, **extra)
+  track_event(
+    :idv_sdk_selfie_image_capture_closed_without_photo,
+    captureAttempts: captureAttempts,
+    **extra,
   )
-    track_event(
-      :idv_sdk_selfie_image_capture_failed,
-      sdk_error_code: sdk_error_code,
-      sdk_error_message: sdk_error_message,
-      captureAttempts: captureAttempts,
-      **extra,
-    )
-  end
+end
 
-  # @param [Integer] captureAttempts number of attempts to capture / upload an image
-  # User opened the SDK to take a selfie
-  def idv_sdk_selfie_image_capture_opened(captureAttempts: nil, **extra)
-    track_event(:idv_sdk_selfie_image_capture_opened, captureAttempts: captureAttempts, **extra)
-  end
-
-  # @param [Integer] captureAttempts number of attempts to capture / upload an image
-  #                  (previously called "attempt")
-  # @param [Integer] failedImageResubmission
-  # @param [String] fingerprint fingerprint of the image added
-  # @param [String] flow_path whether the user is in the hybrid or standard flow
-  # @param [Integer] height height of image added in pixels
-  # @param [String] mimeType MIME type of image added
-  # @param [Integer] size size of image added in bytes
-  # @param [String] source
-  # @param [Integer] width width of image added in pixels
-  # User uploaded a selfie using the file picker
-  def idv_selfie_image_file_uploaded(
-    captureAttempts:,
-    failedImageResubmission:,
-    fingerprint:,
-    flow_path:,
-    height:,
-    mimeType:,
-    size:,
-    source:,
-    width:,
-    **_extra
+# @param [Integer] sdk_error_code SDK code for the error encountered
+# @param [String] sdk_error_message SDK message for the error encountered
+# @param [Integer] captureAttempts number of attempts to capture / upload an image
+#                  (previously called "attempt")
+# User encountered an error with the SDK selfie process
+# Error code 1: camera permission not granted
+# Error code 2: unexpected errors
+def idv_sdk_selfie_image_capture_failed(
+  sdk_error_code:,
+  sdk_error_message:,
+  captureAttempts: nil,
+  **extra
+)
+  track_event(
+    :idv_sdk_selfie_image_capture_failed,
+    sdk_error_code: sdk_error_code,
+    sdk_error_message: sdk_error_message,
+    captureAttempts: captureAttempts,
+    **extra,
   )
-    track_event(
-      :idv_selfie_image_file_uploaded,
-      captureAttempts: captureAttempts,
-      failedImageResubmission: failedImageResubmission,
-      fingerprint: fingerprint,
-      flow_path: flow_path,
-      height: height,
-      mimeType: mimeType,
-      size: size,
-      source: source,
-      width: width,
-    )
-  end
-  # rubocop:enable Naming/VariableName,Naming/MethodParameterName
+end
+
+# @param [Integer] captureAttempts number of attempts to capture / upload an image
+# User opened the SDK to take a selfie
+def idv_sdk_selfie_image_capture_opened(captureAttempts: nil, **extra)
+  track_event(:idv_sdk_selfie_image_capture_opened, captureAttempts: captureAttempts, **extra)
+end
+
+# @param [Integer] captureAttempts number of attempts to capture / upload an image
+#                  (previously called "attempt")
+# @param [Integer] failedImageResubmission
+# @param [String] fingerprint fingerprint of the image added
+# @param [String] flow_path whether the user is in the hybrid or standard flow
+# @param [Integer] height height of image added in pixels
+# @param [String] mimeType MIME type of image added
+# @param [Integer] size size of image added in bytes
+# @param [String] source
+# @param [Integer] width width of image added in pixels
+# User uploaded a selfie using the file picker
+def idv_selfie_image_file_uploaded(
+  captureAttempts:,
+  failedImageResubmission:,
+  fingerprint:,
+  flow_path:,
+  height:,
+  mimeType:,
+  size:,
+  source:,
+  width:,
+  **_extra
+)
+  track_event(
+    :idv_selfie_image_file_uploaded,
+    captureAttempts: captureAttempts,
+    failedImageResubmission: failedImageResubmission,
+    fingerprint: fingerprint,
+    flow_path: flow_path,
+    height: height,
+    mimeType: mimeType,
+    size: size,
+    source: source,
+    width: width,
+  )
+end
+# rubocop:enable Naming/VariableName,Naming/MethodParameterName
 
 # Tracks when the user visits one of the the session error pages.
 # @param [String] type
