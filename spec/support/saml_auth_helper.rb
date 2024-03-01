5a03ba151805bd68d4fcb0e59448b0863ce52145
John Maxwell <john.maxwell@gsa.gov>
2024-03-01 16:00:19 -0500

    Turned on `use_vot_in_sp_requests` in test
    Also reverted changes to `visit_idp_from_oidc_sp_with_ial1`

--- spec/support/saml_auth_helper.rb
+++ spec/support/saml_auth_helper.rb
@@ -244,8 +244,7 @@ module SamlAuthHelper
       @state = SecureRandom.hex
       @client_id = OidcAuthHelper::OIDC_IAL1_ISSUER
       @nonce = SecureRandom.hex
-      visit_idp_from_oidc_sp_with_vtr(
-        vtr: 'C1',
+      visit_idp_from_oidc_sp_with_ial1(
         state: @state,
         client_id: @client_id,
         nonce: @nonce,
