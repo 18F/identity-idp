7bc1dc9d3ed8265522bc6e0f5a08c46ee5dbc35a
Eileen McFarland <eileenmcfarland@navapbc.com>
2024-03-04 13:18:08 -0500

    move ResultCodes and IssuerTypes into LexisNexis module

--- spec/services/doc_auth/lexis_nexis/result_codes_spec.rb
+++ spec/services/doc_auth/lexis_nexis/result_codes_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe DocAuth::LexisNexis::ResultCodes do
   describe '.from_int' do
     it 'is a result code for the int' do
-      result_code = DocAuth::ResultCodes.from_int(1)
-      expect(result_code).to be_a(DocAuth::ResultCodes::ResultCode)
+      result_code = DocAuth::LexisNexis::ResultCodes.from_int(1)
+      expect(result_code).to be_a(DocAuth::LexisNexis::ResultCodes::ResultCode)
       expect(result_code.billed?).to eq(true)
     end
 
     it 'is nil when there is no matching code' do
-      result_code = DocAuth::ResultCodes.from_int(999)
+      result_code = DocAuth::LexisNexis::ResultCodes.from_int(999)
       expect(result_code).to be_nil
     end
   end
