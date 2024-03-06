f6ffed1c08c0b2231ff8b4555d33e361aa7a5380
JackRyan1989 <jack.ryan@gsa.gov>
2024-03-06 12:33:19 -0500

    initial specs complete

--- spec/features/idv/in_person_threatmetrix_spec.rb
+++ spec/features/idv/in_person_threatmetrix_spec.rb
@@ -18,12 +18,15 @@ RSpec.describe 'In Person Proofing Threatmetrix', js: true, allowed_extra_analyt
         update(in_person_proofing_enabled: true)
   end
 
+  before(:each) do
+    complete_entire_ipp_flow(user, tmx_status)
+  end
+
   context 'ThreatMetrix determination of Review' do
     let(:tmx_status) { 'Review' }
 
-    context 'User passes IPP and TMX Review' do
+    context 'User passes IPP and passes TMX Review' do
       it 'initially shows the user the barcode page', allow_browser_log: true do
-        complete_entire_ipp_flow(user, tmx_status)
         expect_in_person_step_indicator_current_step(
             t('step_indicator.flows.idv.go_to_the_post_office'),
             )
@@ -33,8 +36,6 @@ RSpec.describe 'In Person Proofing Threatmetrix', js: true, allowed_extra_analyt
       end
 
       it 'shows the user the Please Call screen after passing IPP', allow_browser_log: true do
-        complete_entire_ipp_flow(user, tmx_status)
-
         Capybara.reset_session!
         sign_in_live_with_2fa(user)
         expect(page).to have_current_path(account_path)
@@ -56,29 +57,143 @@ RSpec.describe 'In Person Proofing Threatmetrix', js: true, allowed_extra_analyt
         expect(page).to have_current_path(idv_please_call_path)
       end
 
-      it 'does not allow the user to restart the IPP flow' do
+      it 'does not allow the user to restart the IPP flow', allow_browser_log: true do
+        enrollment = InPersonEnrollment.last
+        profile = enrollment.profile
+        
+        # set fraud review to pending: 
+        profile.deactivate_for_fraud_review
+        # pass the enrollment:
+        enrollment.update(status: 'passed')
+        
+        profile.reload
+        enrollment.reload
+
+        visit_idp_from_sp_with_ial2(sp)
+        expect(page).to have_current_path(idv_please_call_path)
+        page.visit('/verify/welcome')
+        expect(page).to have_current_path(idv_please_call_path)
+        page.visit('/verify/in_person/document_capture')
+        expect(page).to have_current_path(idv_please_call_path)
       end
 
-      it 'notifies the user after passing TMX review' do
-      end
+      it 'shows the user the successful verification screen after passing TMX review', allow_browser_log: true do
+        enrollment = InPersonEnrollment.last
+        profile = enrollment.profile
+        
+        # set fraud review to pending: 
+        profile.deactivate_for_fraud_review
+        # pass the enrollment:
+        enrollment.update(status: 'passed')
+        
+        profile.reload
+        enrollment.reload
 
-      it 'shows the user the successful verification screen after passing TMX review' do
+        visit_idp_from_sp_with_ial2(sp)
+        expect(page).to have_current_path(idv_please_call_path)
+
+        mark_in_person_enrollment_passed(user)
+        page.visit('/verify/welcome')
+        expect(page).to have_current_path(idv_activated_path)
       end
     end
 
-    context 'User fails IPP and passes TMX review' do
-    end
+    context 'User fails IPP and deactivates for TMX review' do
+      it 'initially shows the user the barcode page', allow_browser_log: true do
+        expect_in_person_step_indicator_current_step(
+            t('step_indicator.flows.idv.go_to_the_post_office'),
+            )
+        expect(page).to have_content(t('in_person_proofing.headings.barcode').tr(' ', ' '))
+        expect(page).to have_content(Idv::InPerson::EnrollmentCodeFormatter.format(enrollment_code))
+        expect(page).to have_current_path(idv_in_person_ready_to_verify_path)
+      end 
 
-    context 'User fails IPP and fails TMX review' do
+      it 'allows the user to restart the flow', allow_browser_log: true do
+        enrollment = InPersonEnrollment.last
+        profile = enrollment.profile
+        # set fraud review to pending: 
+        profile.deactivate_for_fraud_review
+        # fail at post office
+        enrollment.update(
+          status: :failed,
+          proofed_at: Time.zone.now,
+          status_check_completed_at: Time.zone.now
+        )
 
+        profile.reload
+        enrollment.reload
+
+        visit_idp_from_sp_with_ial2(sp)
+        expect(page).not_to have_current_path(idv_please_call_path)
+        # redo the flow:
+        complete_entire_ipp_flow(user, tmx_status)
+        expect(page).to have_current_path(idv_in_person_ready_to_verify_path)
+      end 
     end
   end
 
   context 'ThreatMetrix determination of Reject' do
     let(:tmx_status) { 'Reject' }
 
-    context 'User passes IPP and fails TMX review' do
+    context 'User passes IPP and rejects for TMX review' do
+      it 'initially shows the user the barcode page', allow_browser_log: true do
+        expect_in_person_step_indicator_current_step(
+            t('step_indicator.flows.idv.go_to_the_post_office'),
+            )
+        expect(page).to have_content(t('in_person_proofing.headings.barcode').tr(' ', ' '))
+        expect(page).to have_content(Idv::InPerson::EnrollmentCodeFormatter.format(enrollment_code))
+        expect(page).to have_current_path(idv_in_person_ready_to_verify_path)
+      end
 
+      it 'shows the user the Please Call screen after passing IPP', allow_browser_log: true do
+        Capybara.reset_session!
+        sign_in_live_with_2fa(user)
+        expect(page).to have_current_path(account_path)
+        visit_idp_from_sp_with_ial2(sp)
+        expect(page).to have_current_path(idv_in_person_ready_to_verify_path)
+
+        enrollment = InPersonEnrollment.last
+        profile = enrollment.profile
+        
+        # set fraud review to pending: 
+        profile.deactivate_for_fraud_review
+        # pass the enrollment:
+        enrollment.update(status: 'passed')
+        
+        profile.reload
+        enrollment.reload
+        expect(page).to have_current_path(idv_in_person_ready_to_verify_path)
+        page.refresh
+        expect(page).to have_current_path(idv_please_call_path)
+
+        # reject the user
+        profile.reject_for_fraud(notify_user: false)
+        page.refresh
+        expect(page).to have_current_path(idv_not_verified_path)
+      end
+
+      it 'does not allow the user to restart the IPP flow', allow_browser_log: true do
+        enrollment = InPersonEnrollment.last
+        profile = enrollment.profile
+        
+        # set fraud review to pending: 
+        profile.deactivate_for_fraud_review
+        # pass the enrollment:
+        enrollment.update(status: 'passed')
+        
+        profile.reload
+        enrollment.reload
+
+         # reject the user
+         profile.reject_for_fraud(notify_user: false)
+
+        visit_idp_from_sp_with_ial2(sp)
+        expect(page).to have_current_path(idv_not_verified_path)
+        page.visit('/verify/welcome')
+        expect(page).to have_current_path(idv_not_verified_path)
+        page.visit('/verify/in_person/document_capture')
+        expect(page).to have_current_path(idv_not_verified_path)
+      end
     end
   end
 end
