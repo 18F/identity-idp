00480e54e877e4bc19ee3f9bcdfac4dea2b1fa4d
Malick Diarra <malick.diarra@gsa.gov>
2024-03-06 10:25:50 -0500

    use the reset concern in pending presenter

--- app/presenters/account_reset/pending_presenter.rb
+++ app/presenters/account_reset/pending_presenter.rb
@@ -1,5 +1,6 @@
 module AccountReset
   class PendingPresenter
+    include AccountResetConcern
     include ActionView::Helpers::DateHelper
 
     attr_reader :account_reset_request
@@ -9,7 +10,7 @@ module AccountReset
     end
 
     def time_remaining_until_granted(now: Time.zone.now)
-      wait_time = account_reset_wait_period_days
+      wait_time = account_reset_wait_period_days(user)
 
       distance_of_time_in_words(
         now,
@@ -24,36 +25,14 @@ module AccountReset
 
       distance_of_time_in_words(
         current_time,
-        current_time + account_reset_wait_period_days,
+        current_time + account_reset_wait_period_days(user),
         true,
-        accumulate_on: reset_accumulation_type,
+        accumulate_on: reset_accumulation_type(user),
       )
     end
 
-    def account_reset_wait_period_days
-      if supports_fraud_account_reset?
-        IdentityConfig.store.account_reset_fraud_user_wait_period_days.days
-      else
-        IdentityConfig.store.account_reset_wait_period_days.days
-      end
-    end
-
-    def supports_fraud_account_reset?
-      (account_reset_request_user.fraud_review_pending? ||
-        account_reset_request_user.fraud_rejection?) &&
-        IdentityConfig.store.account_reset_fraud_user_wait_period_days.present?
-    end
-
-    def account_reset_request_user
+    def user
       account_reset_request.user
-    end
-
-    def reset_accumulation_type
-      if account_reset_wait_period_days > 3
-        :days
-      else
-        :hours
-      end
     end
   end
 end
