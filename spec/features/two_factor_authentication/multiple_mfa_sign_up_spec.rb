0c12d4249f27ef2eb6d6e52bff0a1d15a4f3a79a
kevinsmaster5 <kevin.masters@gsa.gov>
2024-03-06 15:35:21 -0500

    fix spec

--- spec/features/two_factor_authentication/multiple_mfa_sign_up_spec.rb
+++ spec/features/two_factor_authentication/multiple_mfa_sign_up_spec.rb
@@ -109,7 +109,7 @@ RSpec.feature 'Multi Two Factor Authentication', allowed_extra_analytics: [:*] d
       expect(current_path).to eq backup_code_setup_path
       travel_to((IdentityConfig.store.reauthn_window + 5).seconds.from_now) do
         click_continue
-        expect(current_path).to eq login_two_factor_options_path  
+        expect(current_path).to eq login_two_factor_options_path
 
         find("label[for='two_factor_options_form_selection_auth_app']").click
         click_on t('forms.buttons.continue')
