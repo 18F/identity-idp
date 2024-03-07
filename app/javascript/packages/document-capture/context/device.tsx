18a68fd5484d03dcc6d96d60846c4df5f0c4887c
charleyf <charleyferguson@navapbc.com>
2024-03-07 11:31:49 -0500

    Sketch in resolution detection

--- app/javascript/packages/document-capture/context/device.tsx
+++ app/javascript/packages/document-capture/context/device.tsx
@@ -13,9 +13,70 @@ const DeviceContext = createContext({
 
 DeviceContext.displayName = 'DeviceContext';
 
+async function videoTracksAvailable() {
+  // Check that there are tracks in the device, also stop them?
+  try {
+    const first = await navigator.mediaDevices.getUserMedia({ video: true });
+    first.getTracks().forEach((track) => track.stop());
+    return true;
+  } catch (err) {
+    // Probably log that the camera resolution check failed
+  }
+}
+
+function getConstraints(deviceId) {
+  return {
+    video: {
+      width: {
+        ideal: 999999,
+      },
+      height: {
+        ideal: 999999,
+      },
+      deviceId: {
+        exact: deviceId,
+      },
+    },
+  };
+}
+
+function getCameraInfo(videoTrack) {
+  const cameraInfo = {
+    label: videoTrack.label,
+    frameRate: videoTrack.getSettings().frameRate,
+    height: videoTrack.getSettings().height,
+    width: videoTrack.getSettings().width,
+  };
+  videoTrack.stop();
+  return cameraInfo;
+}
+
+async function updateConstraintsAndGetInfo(videoDevice) {
+  const updatedConstraints = getConstraints(videoDevice.deviceId);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(updatedConstraints);
+    const videoTracks = stream.getVideoTracks();
+    const cameras = videoTracks.map((videoTrack) => getCameraInfo(videoTrack));
+    // I get an object here, but unresolved promises everywhere else
+    return cameras;
+  } catch (err) {
+    // Log an error
+  }
+}
+
+async function getDeviceInfo() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter((device) => device.kind === 'videoinput');
+  const info = videoDevices.map((videoDevice) => updateConstraintsAndGetInfo(videoDevice));
+  return info;
+}
+
 function DeviceContextProvider({ isMobile, children }: DeviceContextValue) {
-  const detectCameraResolution = () => {
-    console.log('camera resolution detected');
+  const detectCameraResolution = async () => {
+    if (navigator?.mediaDevices?.getUserMedia) {
+      const available = await videoTracksAvailable();
+      const info = await getDeviceInfo();
+    }
   };
 
   const value = useObjectMemo({
