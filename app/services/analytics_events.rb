64f0031203c9b7539b00cdffee8772eeb3c76243
charleyf <charleyferguson@navapbc.com>
2024-03-11 11:43:11 -0400

    Add info to log on BE

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -787,9 +787,9 @@ module AnalyticsEvents
     )
   end
 
-  def idv_camera_resolution_logged(facingMode:, cameraInfo:, **_extra)
+  def idv_camera_resolution_logged(flow_path:, facingMode:, cameraInfo:, **_extra)
     track_event(
-      'Frontend: IdV: camera resolution logged',
+      'Frontend: IdV: camera resolution logged', facingMode: facingMode, cameraInfo: cameraInfo,
       #'Frontend: IdV: camera resolution logged', facingMode, cameraInfo
     )
   end
