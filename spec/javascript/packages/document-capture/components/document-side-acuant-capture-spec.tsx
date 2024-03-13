d81b08ce347544dfca96fd4e070f2dc7258a9d12
Brittany Greaner <brittany.greaner@gsa.gov>
2024-03-13 15:39:07 -0700

    Add all test cases

--- spec/javascript/packages/document-capture/components/document-side-acuant-capture-spec.tsx
+++ spec/javascript/packages/document-capture/components/document-side-acuant-capture-spec.tsx
@@ -13,44 +13,163 @@ describe('DocumentSideAcuantCapture', () => {
   };
 
   context('when selfie is _not_ enabled', () => {
-    it('_does_ display a photo upload button', () => {
-      const { queryAllByText } = render(
-        <DeviceContext.Provider value={{ isMobile: true }}>
-          <SelfieCaptureContext.Provider
-            value={{ isSelfieCaptureEnabled: false, isSelfieDesktopMode: false }}
-          >
-            <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
-            <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
-          </SelfieCaptureContext.Provider>
-        </DeviceContext.Provider>,
-      );
+    context('and using mobile', () => {
+      context('and doc_auth_selfie_desktop_test_mode is false', () => {
+        it('_does_ display a photo upload button', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: true }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: false, isSelfieDesktopTestMode: false }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
 
-      const takeOrUploadPictureText = queryAllByText(
-        'doc_auth.buttons.take_or_upload_picture_html',
-      );
-      expect(takeOrUploadPictureText).to.have.lengthOf(2);
+          const takeOrUploadPictureText = queryAllByText(
+            'doc_auth.buttons.take_or_upload_picture_html',
+          );
+          expect(takeOrUploadPictureText).to.have.lengthOf(2);
+        });
+      });
+
+      context('and doc_auth_selfie_desktop_test_mode is true', () => {
+        it('_does_ display a photo upload button', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: true }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: false, isSelfieDesktopTestMode: true }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
+
+          const takeOrUploadPictureText = queryAllByText(
+            'doc_auth.buttons.take_or_upload_picture_html',
+          );
+          expect(takeOrUploadPictureText).to.have.lengthOf(2);
+        });
+      });
+    });
+
+    context('and using desktop', () => {
+      context('and doc_auth_selfie_desktop_test_mode is false', () => {
+        it('shows a file pick area for each field', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: false }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: false, isSelfieDesktopTestMode: false }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
+
+          const uploadPictureText = queryAllByText('doc_auth.forms.choose_file_html');
+          expect(uploadPictureText).to.have.lengthOf(2);
+        });
+      });
+
+      context('and doc_auth_selfie_desktop_test_mode is true', () => {
+        it('shows a file pick area for each field', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: false }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: false, isSelfieDesktopTestMode: true }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
+
+          const uploadPictureText = queryAllByText('doc_auth.forms.choose_file_html');
+          expect(uploadPictureText).to.have.lengthOf(2);
+        });
+      });
     });
   });
 
   context('when selfie _is_ enabled', () => {
-    it('does _not_ display a photo upload button', () => {
-      const { queryAllByText } = render(
-        <DeviceContext.Provider value={{ isMobile: true }}>
-          <SelfieCaptureContext.Provider
-            value={{ isSelfieCaptureEnabled: true, isSelfieDesktopMode: false }}
-          >
-            <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
-            <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
-            <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="selfie" />
-          </SelfieCaptureContext.Provider>
-        </DeviceContext.Provider>,
-      );
+    context('and using mobile', () => {
+      context('and doc_auth_selfie_desktop_test_mode is false', () => {
+        it('does _not_ display a photo upload button', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: true }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: true, isSelfieDesktopTestMode: false }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="selfie" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
 
-      const takePictureText = queryAllByText('doc_auth.buttons.take_picture');
-      expect(takePictureText).to.have.lengthOf(3);
+          const takePictureText = queryAllByText('doc_auth.buttons.take_picture');
+          expect(takePictureText).to.have.lengthOf(3);
 
-      const notExpectedText = 'doc_auth.buttons.take_or_upload_picture_html';
-      expect(queryAllByText(notExpectedText)).to.be.an('array').that.is.empty;
+          const takeOrUploadPictureText = queryAllByText(
+            'doc_auth.buttons.take_or_upload_picture_html',
+          );
+          expect(takeOrUploadPictureText).to.have.lengthOf(0);
+        });
+      });
+
+      context('and doc_auth_selfie_desktop_test_mode is true', () => {
+        it('does _not_ display a photo upload button', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: true }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: true, isSelfieDesktopTestMode: true }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="selfie" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
+
+          const takePictureText = queryAllByText('doc_auth.buttons.take_picture');
+          expect(takePictureText).to.have.lengthOf(3);
+
+          const takeOrUploadPictureText = queryAllByText(
+            'doc_auth.buttons.take_or_upload_picture_html',
+          );
+          expect(takeOrUploadPictureText).to.have.lengthOf(3);
+        });
+      });
+    });
+
+    context('and using desktop', () => {
+      context('and doc_auth_selfie_desktop_test_mode is false', () => {
+        it('never loads these components', () => {
+          // noop
+        });
+      });
+
+      context('and doc_auth_selfie_desktop_test_mode is true', () => {
+        it('shows a file pick area for each field', () => {
+          const { queryAllByText } = render(
+            <DeviceContext.Provider value={{ isMobile: false }}>
+              <SelfieCaptureContext.Provider
+                value={{ isSelfieCaptureEnabled: true, isSelfieDesktopTestMode: true }}
+              >
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="front" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="back" />
+                <DocumentSideAcuantCapture {...DEFAULT_PROPS} side="selfie" />
+              </SelfieCaptureContext.Provider>
+            </DeviceContext.Provider>,
+          );
+
+          const uploadPictureText = queryAllByText('doc_auth.forms.choose_file_html');
+          expect(uploadPictureText).to.have.lengthOf(3);
+        });
+      });
     });
   });
 });
