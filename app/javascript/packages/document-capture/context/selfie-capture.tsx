e9ee7b3d76604cd39693a7b9a5c941cf7b5f7ce6
Brittany Greaner <brittany.greaner@gsa.gov>
2024-03-14 11:02:33 -0700

    LG-12307: don't allow upload when selfie capture is enabled (#10232)
    * changelog: User-Facing Improvements, Doc Auth, When selfie capture is enabled, only allow images through SDK
    
    * Prevent upload if selfie capture is enabled
    * Prevent native camera event if selfie enabled
    
    The "IdV: Native camera forced after failed attempts" event was firing
    even though the previous commit disallows file upload and use of the
    native camera.
    
    This commit prevents this event from firing.
    
    * Mv SelfieCaptureContext above FailedCaptureAttempts
    
    [See this Slack thread for details](https://gsa-tts.slack.com/archives/C05HSH9RQ57/p1709940634853369?thread_ts=1709857278.599209&cid=C05HSH9RQ57).
    
    * Add isSelfieDesktopMode to selfieCaptureContext
    
    isSelfieDesktopMode is needed in the next commit to allow upload when
    isSelfieDesktopMode is true.
    
    * Allow upload when desktopMode enabled
    
    Without looking at this value, we won't be able to upload yml files if
    desktop test mode and selfie are both enabled.

--- app/javascript/packages/document-capture/context/selfie-capture.tsx
+++ app/javascript/packages/document-capture/context/selfie-capture.tsx
@@ -5,10 +5,15 @@ interface SelfieCaptureProps {
    * Specify whether to show the selfie capture on the doc capture screen.
    */
   isSelfieCaptureEnabled: boolean;
+  /**
+   * Specify whether to allow uploads for selfie when in test mode.
+   */
+  isSelfieDesktopTestMode: boolean;
 }
 
 const SelfieCaptureContext = createContext<SelfieCaptureProps>({
   isSelfieCaptureEnabled: false,
+  isSelfieDesktopTestMode: false,
 });
 
 SelfieCaptureContext.displayName = 'SelfieCaptureContext';
