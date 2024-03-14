52b78f56803745c1778a1f8c4dfeb5b989eb10ca
Andrew Duthie <andrew.duthie@gsa.gov>
2024-03-14 16:51:19 -0400

    Fix SetUpWebauthnPlatformSelectionPresenter specs

--- spec/presenters/two_factor_authentication/set_up_webauthn_platform_selection_presenter_spec.rb
+++ spec/presenters/two_factor_authentication/set_up_webauthn_platform_selection_presenter_spec.rb
@@ -1,73 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe TwoFactorAuthentication::SetUpWebauthnPlatformSelectionPresenter do
-  let(:user_without_mfa) { create(:user) }
-  let(:user_with_mfa) { create(:user) }
-  let(:configuration) {}
-  let(:presenter_without_mfa) do
-    described_class.new(user: user_without_mfa)
-  end
-  let(:presenter_with_mfa) do
-    described_class.new(user: user_with_mfa)
-  end
+  let(:user) { create(:user) }
   subject(:presenter) do
-    TwoFactorAuthentication::SetUpWebauthnPlatformSelectionPresenter.new(
-      user:,
-      configuration: user.webauthn_configurations.platform_authenticators.first,
-    )
+    TwoFactorAuthentication::SetUpWebauthnPlatformSelectionPresenter.new(user:)
   end
 
   describe '#type' do
     it 'returns webauthn_platform' do
-      expect(presenter_without_mfa.type).to eq :webauthn_platform
+      expect(presenter.type).to eq(:webauthn_platform)
     end
   end
 
   describe '#render_in' do
-    let(:user) { create(:user) }
+    let(:view_context) { instance_double(ActionView::Base) }
+    subject(:rendered) { presenter.render_in(view_context) { 'content' } }
+
     it 'renders a WebauthnInputComponent' do
-      view_context = instance_double(ActionView::Base)
       expect(view_context).to receive(:render) do |component, &block|
         expect(component).to be_instance_of(WebauthnInputComponent)
         expect(component.passkey_supported_only?).to be(true)
         expect(block.call).to eq('content')
       end
 
-      presenter.render_in(view_context) { 'content' }
+      rendered
     end
 
     context 'with configured authenticator' do
       let(:user) { create(:user, :with_webauthn_platform) }
 
       it 'renders a WebauthnInputComponent with passkey_supported_only false' do
-        view_context = instance_double(ActionView::Base)
         expect(view_context).to receive(:render) do |component, &block|
           expect(component.passkey_supported_only?).to be(true)
         end
 
-        presenter.render_in(view_context)
+        rendered
       end
     end
   end
 
-  describe '#mfa_configuration' do
-    it 'returns an empty string when user has not configured this authenticator' do
-      expect(presenter_without_mfa.mfa_configuration_description).to eq('')
+  describe '#mfa_configuration_description' do
+    subject(:mfa_configuration_description) { presenter.mfa_configuration_description }
+
+    context 'when user has not configured this authenticator' do
+      let(:user) { create(:user) }
+
+      it 'returns an empty string' do
+        expect(mfa_configuration_description).to eq('')
+      end
     end
 
-    it 'returns an # added when user has configured this authenticator' do
-      create(:webauthn_configuration, platform_authenticator: true, user: user_with_mfa)
-      expect(presenter_with_mfa.mfa_configuration_description).to eq(
-        t(
-          'two_factor_authentication.two_factor_choice_options.no_count_configuration_added',
-          count: 1,
-        ),
-      )
+    context 'when user has configured this authenticator' do
+      let(:user) { create(:user, :with_webauthn_platform) }
+
+      it 'returns number of added authenticators' do
+        expect(mfa_configuration_description).to eq(
+          t(
+            'two_factor_authentication.two_factor_choice_options.no_count_configuration_added',
+            count: 1,
+          ),
+        )
+      end
     end
   end
 
   describe '#phishing_resistant?' do
-    subject(:phishing_resistant) { presenter_without_mfa.phishing_resistant? }
+    subject(:phishing_resistant) { presenter.phishing_resistant? }
 
     it { expect(phishing_resistant).to eq(true) }
   end
