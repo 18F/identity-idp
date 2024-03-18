53dc2c66079b573368bc23ec3d7c7bca7238c6bd
Malick Diarra <malick.diarra@gsa.gov>
2024-03-18 08:58:02 -0400

    commenting presenter to use proper language

--- app/presenters/account_reset/pending_presenter.rb
+++ app/presenters/account_reset/pending_presenter.rb
@@ -21,14 +21,7 @@ module AccountReset
     end
 
     def account_reset_deletion_period
-      current_time = Time.zone.now
-
-      distance_of_time_in_words(
-        current_time,
-        current_time + account_reset_wait_period_days(user),
-        true,
-        accumulate_on: reset_accumulation_type(user),
-      )
+      account_reset_deletion_period_interval(user)
     end
 
     def user
