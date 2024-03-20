0fc7795c0b2b634da453d98b1dc226c4beb0244a
Matt Hinz <matt.hinz@gsa.gov>
2024-03-20 10:00:13 -0700

    Make Idv::AnalyticsEventsEnhancer opt-out (#10263)
    * compact common_analytics_attributes
    
    Don't include nil values here
    
    * Refactor proofing_components into its own describe block
    
    * Make AnalyticsEventsEnhancer opt out
    
    Update AnalyticsEventsEnhancer so that it augments any event where the _method name_ starts with `^idv_` UNLESS it has been told not to.
    
    changelog: Internal, Analytics, Add additional data to IdV analytics events by default.

--- spec/services/idv/analytics_events_enhancer_spec.rb
+++ spec/services/idv/analytics_events_enhancer_spec.rb
@@ -5,63 +5,89 @@ RSpec.describe Idv::AnalyticsEventsEnhancer do
   let(:analytics_class) do
     Class.new(FakeAnalytics) do
       include AnalyticsEvents
+      include(
+        Module.new do
+          def idv_test_method(**extra)
+            track_event(:idv_test_method, **extra)
+          end
+        end,
+      )
       prepend Idv::AnalyticsEventsEnhancer
 
-      def idv_final(**kwargs)
-        @called_kwargs = kwargs
-      end
-
-      attr_reader :user, :called_kwargs
+      attr_reader :called_kwargs
 
       def initialize(user:)
         @user = user
+      end
+
+      def track_event(_event, **kwargs)
+        @called_kwargs = kwargs
       end
     end
   end
   let(:analytics) { analytics_class.new(user: user) }
 
-  it 'includes decorated methods' do
-    expect(analytics.methods).to include(*described_class::DECORATED_METHODS)
-    expect(
-      analytics.methods.
-        intersection(described_class::DECORATED_METHODS).
-        map { |method| analytics.method(method).source_location.first }.
-        uniq,
-    ).to eq([Idv::AnalyticsEventsEnhancer.const_source_location(:DECORATED_METHODS).first])
-  end
+  it 'enhances idv_ methods by default, but ignores those in IGNORED_METHODS' do
+    enhancer_source_file = described_class.const_source_location(:IGNORED_METHODS).first
 
-  it 'calls analytics method with original and decorated attributes' do
-    analytics.idv_final(extra: true)
+    idv_methods = analytics_class.instance_methods.filter { |method| /^idv_/.match?(method) }
 
-    expect(analytics.called_kwargs).to eq(extra: true, proofing_components: nil)
+    idv_methods.each do |method_name|
+      method = analytics_class.instance_method(method_name)
+      method_source_file = method.source_location.first
+
+      should_be_ignored = described_class.const_get(:IGNORED_METHODS).include?(method_name)
+      if should_be_ignored
+        expect(method_source_file).not_to eql(enhancer_source_file),
+                                          "#{method_name} should not be enhanced"
+      else
+        expect(
+          method_source_file,
+        ).to eql(enhancer_source_file), "#{method_name} should be enhanced"
+       end
+    end
   end
 
   context 'with anonymous analytics user' do
     let(:user) { AnonymousUser.new }
 
-    it 'calls analytics method with original and decorated attributes' do
-      analytics.idv_final(extra: true)
+    it 'calls analytics method with original attributes' do
+      analytics.idv_test_method(extra: true)
 
-      expect(analytics.called_kwargs).to eq(extra: true, proofing_components: nil)
+      expect(analytics.called_kwargs).to eq(extra: true)
     end
   end
 
-  context 'with proofing component' do
-    let(:proofing_components) do
-      ProofingComponent.new(source_check: Idp::Constants::Vendors::AAMVA)
-    end
+  describe 'proofing_components' do
+    let(:proofing_components) { nil }
 
     before do
       user.proofing_component = proofing_components
     end
 
-    it 'calls analytics method with original and decorated attributes' do
-      analytics.idv_final(extra: true)
+    context 'without proofing component' do
+      it 'calls analytics method with original attributes' do
+        analytics.idv_test_method(extra: true)
 
-      expect(analytics.called_kwargs).to match(
-        extra: true,
-        proofing_components: kind_of(Idv::ProofingComponentsLogging),
-      )
+        expect(analytics.called_kwargs).to match(
+          extra: true,
+        )
+      end
+    end
+
+    context 'with proofing component' do
+      let(:proofing_components) do
+        ProofingComponent.new(source_check: Idp::Constants::Vendors::AAMVA)
+      end
+
+      it 'calls analytics method with original attributes and proofing_components' do
+        analytics.idv_test_method(extra: true)
+
+        expect(analytics.called_kwargs).to match(
+          extra: true,
+          proofing_components: kind_of(Idv::ProofingComponentsLogging),
+        )
+      end
     end
   end
 end
