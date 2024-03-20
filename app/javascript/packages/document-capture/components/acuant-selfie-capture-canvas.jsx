46c25a3b3ab6d05cdb4d6c5d213cf6c2527ba976
charleyf <charleyferguson@navapbc.com>
2024-03-20 11:00:23 -0400

    Add Acuant SDK aria-labeling code

--- app/javascript/packages/document-capture/components/acuant-selfie-capture-canvas.jsx
+++ app/javascript/packages/document-capture/components/acuant-selfie-capture-canvas.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { getAssetPath } from '@18f/identity-assets';
 import { useI18n } from '@18f/identity-react-i18n';
 import AcuantContext from '../context/acuant';
@@ -21,6 +21,35 @@ function AcuantSelfieCaptureCanvas({ imageCaptureText, onSelfieCaptureClosed }) 
   // The Acuant SDK script AcuantPassiveLiveness attaches to whatever element has
   // this id. It then uses that element as the root for the full screen selfie capture
   const acuantCaptureContainerId = 'acuant-face-capture-container';
+
+  // The Acuant SDK doesn't include appropriate aria-labels. This is a hack to monitor the page
+  // until the Acuant SDK adds specific elements, then attach an aria-label to those elements
+  useEffect(() => {
+    const findElementAndAddAriaLabel = (shadowRoot, selector, label) => {
+      const closeTextButton = shadowRoot.querySelector(selector);
+      if (closeTextButton) {
+        closeTextButton.setAttribute('aria-label', label);
+      }
+    };
+    const intervalId = setInterval(() => {
+      // Find the div that Acuant attaches the capture pane to, then retrieve the capture pane (shadowRoot)
+      // https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_shadow_DOM
+      const shadowRoot = document.querySelector('#acuant-face-capture-camera')?.shadowRoot;
+
+      if (shadowRoot) {
+        // Find and label the top right corner white "close" text buton that closes the capture
+        findElementAndAddAriaLabel(shadowRoot, 'div.close', 'close-text-button-aria-label');
+        // Find and label the red "capture" icon button that shows the preview, but does not close the window
+        findElementAndAddAriaLabel(shadowRoot, 'button.shoot', 'capture-button-aria-label');
+        // Find and label the green checkmark captuure icon button that finalizes the selfie and closes the window
+        findElementAndAddAriaLabel(shadowRoot, 'img.shoot', 'finish-capture-button-aria-label');
+      }
+      // This is how often to try finding the buttons in ms
+    }, 500);
+    // Stop looking for the buttons if the component gets unmounted while still looking
+    return () => clearInterval(intervalId);
+  });
+
   return (
     <>
       {!isReady && <LoadingSpinner />}
