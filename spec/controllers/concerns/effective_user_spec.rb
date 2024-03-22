3e8d28f5002ca1fa6191294025baf05e289f9a63
Matt Hinz <matt.hinz@gsa.gov>
2024-03-22 11:21:47 -0700

    Don't use stale document capture user ids for effective_user
    If the user deletes their account, it is possible that the user id stored in the session for document capture will not be valid. This results in analytics events being improperly attributed to `anonymous-uuid`.
    
    changelog: Bug Fixes, Identity verification, Fix log attribute issue associated with hybrid handoff.
    
    Co-authored-by: Doug Price <douglas.price@gsa.gov>

--- spec/controllers/concerns/effective_user_spec.rb
+++ spec/controllers/concerns/effective_user_spec.rb
@@ -14,21 +14,36 @@ RSpec.describe EffectiveUser, type: :controller do
       it 'returns nil' do
         expect(subject).to be_nil
       end
+
+      context 'with valid doc capture session user id' do
+        before do
+          session[:doc_capture_user_id] = user.id
+        end
+
+        it 'returns session user id' do
+          expect(subject).to eq user
+        end
+      end
+
+      context 'with invalid doc capture session user id' do
+        before do
+          session[:doc_capture_user_id] = -1
+        end
+
+        it 'returns session user id' do
+          expect(subject).to be_nil
+        end
+
+        it 'deletes the session key' do
+          subject
+          expect(session).not_to include(:doc_capture_user_id)
+        end
+      end
     end
 
     context 'non-existent user' do
       it 'returns session user id' do
         expect(subject).to be_nil
-      end
-    end
-
-    context 'logged out with doc capture session user id' do
-      before do
-        session[:doc_capture_user_id] = user.id
-      end
-
-      it 'returns session user id' do
-        expect(subject).to eq user
       end
     end
 
@@ -39,6 +54,33 @@ RSpec.describe EffectiveUser, type: :controller do
 
       it 'returns session user id' do
         expect(subject).to eq user
+      end
+
+      context 'with valid doc capture session user id that is not the logged-in user' do
+        let(:doc_capture_user) { create(:user) }
+
+        before do
+          session[:doc_capture_user_id] = doc_capture_user.id
+        end
+
+        it 'returns doc capture user id' do
+          expect(subject).to eq(doc_capture_user)
+        end
+      end
+
+      context 'with invalid doc capture session user id' do
+        before do
+          session[:doc_capture_user_id] = -1
+        end
+
+        it 'returns logged in user' do
+          expect(subject).to eql(user)
+        end
+
+        it 'deletes the session key' do
+          subject
+          expect(session).not_to include(:doc_capture_user_id)
+        end
       end
     end
   end
