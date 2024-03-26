97c9b1709b246d338b80955467cdb98598d275b2
kevinsmaster5 <kevin.masters@gsa.gov>
2024-03-26 16:07:28 -0400

    apply Feature logic to disavowal spec. slight change to new test

--- spec/services/create_new_device_alert_spec.rb
+++ spec/services/create_new_device_alert_spec.rb
@@ -1,28 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CreateNewDeviceAlert do
-  let(:user) { create(:user) }
-  describe '#perform' do
-    let(:now) { Time.zone.now }
-    context 'after waiting the full wait period' do
-      it 'deletes user sign_in_new_device value' do
-        before_waiting_the_full_wait_period(now) do
-          user.sign_in_new_device = now - IdentityConfig.store.
-            new_device_alert_delay_in_minutes.minutes
-        end
-
-        travel_to(now + IdentityConfig.store.new_device_alert_delay_in_minutes.minutes)
-        CreateNewDeviceAlert.new.perform
-
-        expect(user.sign_in_new_device).to eq(nil)
-      end
-    end
+  let(:user) do
+    create(
+      :user, sign_in_new_device: IdentityConfig.store.
+      new_device_alert_delay_in_minutes.minutes.ago
+    )
   end
-
-  def before_waiting_the_full_wait_period(now)
-    minutes = IdentityConfig.store.new_device_alert_delay_in_minutes.minutes
-    travel_to(now - minutes) do
-      yield
+  describe '#perform' do
+    it 'deletes user sign_in_new_device value' do
+      CreateNewDeviceAlert.new.perform
+      expect(user.sign_in_new_device).to eq(nil)
     end
   end
 end
