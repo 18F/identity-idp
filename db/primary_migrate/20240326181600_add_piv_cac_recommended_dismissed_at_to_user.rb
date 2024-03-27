9e84dec66885c2fb51c495057e2d04b0c9cdf6c8
Malick Diarra <malick.diarra@gsa.gov>
2024-03-27 08:39:13 -0400

    rename method to to instead of on

--- db/primary_migrate/20240326181600_add_piv_cac_recommended_dismissed_at_to_user.rb
+++ db/primary_migrate/20240326181600_add_piv_cac_recommended_dismissed_at_to_user.rb
@@ -1,4 +1,4 @@
-class AddPivCacRecommendedDismissedAtOnUser < ActiveRecord::Migration[7.1]
+class AddPivCacRecommendedDismissedAtToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :piv_cac_recommended_dismissed_at, :datetime, default: nil
   end
