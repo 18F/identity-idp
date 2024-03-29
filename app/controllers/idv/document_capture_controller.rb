2f130b0148605afd82059c89a97db50554e590a8
Dawei Wang <daweiwang@navapbc.com>
2024-03-29 08:50:25 -0400

    LG-12631: minor change.

--- app/controllers/idv/document_capture_controller.rb
+++ app/controllers/idv/document_capture_controller.rb
@@ -113,8 +113,8 @@ module Idv
     end
 
     def allow_direct_ipp?
-      return false unless idv_session.welcome_visited == true &&
-                          idv_session.idv_consent_given == true
+      return false unless idv_session.welcome_visited &&
+                          idv_session.idv_consent_given
       # not allowed when no step param and action:show(get request)
       return false if params[:step].blank? || params[:action].to_s != 'show' ||
                       idv_session.flow_path == 'hybrid'
