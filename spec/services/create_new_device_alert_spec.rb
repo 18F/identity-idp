b7513209b505d26c239e4a9f4d4bc5a0549cb6a6
kevinsmaster5 <kevin.masters@gsa.gov>
2024-03-29 11:21:09 -0400

    change to new device spec

--- spec/services/create_new_device_alert_spec.rb
+++ spec/services/create_new_device_alert_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CreateNewDeviceAlert do
-  let(:user) { create(:user) }
+  let(:user) do
+    create(
+      :user,
+      :fully_registered,
+      sign_in_new_device_at: Time.zone.now - IdentityConfig.store.new_device_alert_delay_in_minutes.
+                                              minutes,
+    )
+  end
   describe '#perform' do
-    before do
-      allow(user).to receive(:sign_in_new_device_at).and_return(
-        Time.zone.now,
-      )
-    end
     it 'deletes user sign_in_new_device_at value' do
-      travel_to(IdentityConfig.store.new_device_alert_delay_in_minutes.minutes.from_now) do
-        CreateNewDeviceAlert.new.perform
-        expect(user.sign_in_new_device_at).to eq(nil)
-      end
+      CreateNewDeviceAlert.new.perform
+
+      expect(user.sign_in_new_device_at).to eq(nil)
     end
   end
 end
