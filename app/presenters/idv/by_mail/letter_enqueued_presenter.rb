a60cc7ea1e6461662f74e12db683d88d2cd8c938
John Maxwell <john.maxwell@gsa.gov>
2024-04-01 15:02:16 -0400

    Supply default (empty) PII for letter

--- app/presenters/idv/by_mail/letter_enqueued_presenter.rb
+++ app/presenters/idv/by_mail/letter_enqueued_presenter.rb
@@ -5,7 +5,7 @@ module Idv
       include Rails.application.routes.url_helpers
 
       def initialize(idv_session)
-        @pii = idv_session.pii_from_doc
+        @pii = idv_session.pii_from_doc || {}
         @sp = idv_session.service_provider
       end
 
