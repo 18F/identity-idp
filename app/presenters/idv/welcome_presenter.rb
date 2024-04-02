fad88f2738cab306fde638428a6316906e6e9c10
John Maxwell <john.maxwell@gsa.gov>
2024-04-02 16:15:02 -0400

    Minor cleanup

--- app/presenters/idv/welcome_presenter.rb
+++ app/presenters/idv/welcome_presenter.rb
@@ -26,13 +26,12 @@ module Idv
       if first_time_idv?
         t(
           'doc_auth.info.getting_started_html',
-          sp_name: sp_name,
+          sp_name:,
           link_html: help_link,
         )
       else
         t(
           'doc_auth.info.stepping_up_html',
-          sp_name:,
           link_html: help_link,
         )
       end
