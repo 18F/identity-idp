030714ae0321a1387a6644fea3d68eeedffcb79d
Malick Diarra <malick.diarra@gsa.gov>
2024-04-03 12:09:31 -0400

    add query for user

--- app/jobs/notify_password_compromised_job.rb
+++ app/jobs/notify_password_compromised_job.rb
@@ -3,16 +3,11 @@ class NotifyPasswordCompromisedJob < ApplicationJob
 
 	def perform(now)
 		notifications_sent = 0
-		Email.where(
+		User.joins(:emails).where(
 			sql_query_for_users_eligible_to_verify_compromised,
 			sign_in_date:,
 		).each do |arr|
 			
 		end
-	end
-
-	private
-
-	def sql_query_for_recently_signed_in_users_eligible_to_verify_compromised
 	end
 end
