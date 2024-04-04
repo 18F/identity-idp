462ae07d28c7946f43a00d7b82314099a3f06a09
Andrew Duthie <andrew.duthie@gsa.gov>
2024-04-04 11:17:18 -0400

    Check WCAG 2.2 AA
    The intent of the previous code was "latest 2.x", meaning 2.2 currently. In reality, "wcag2aa" means "2.0" specifically.
    
    Ref: https://github.com/dequelabs/axe-core/blob/master/doc/API.md#axe-core-tags

--- spec/support/matchers/accessibility.rb
+++ spec/support/matchers/accessibility.rb
@@ -327,7 +327,7 @@ class AccessibleName
 end
 
 def expect_page_to_have_no_accessibility_violations(page, validate_markup: true)
-  expect(page).to be_axe_clean.according_to(:wcag2aa, :"best-practice").
+  expect(page).to be_axe_clean.according_to(:wcag22aa, :"best-practice").
     # Axe flags redundant img role on img elements, but is necessary for a Safari bug
     # See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#identifying_svg_as_an_image
     excluding('img[alt=""][src$=".svg" i]')
