d4878cef27e5a63a15ab8007c41a503031e12cfe
Nadya Primak <nadya.primak@gsa.gov>
2024-04-05 10:47:34 -0400

    add inline styles to the component

--- app/components/login_button_component.rb
+++ app/components/login_button_component.rb
@@ -3,16 +3,27 @@
 class LoginButtonComponent < BaseComponent
   VALID_COLORS = ['primary', 'primary-darker', 'primary-lighter'].freeze
 
-  attr_reader :color, :big, :tag_options
+  css_file_path = Rails.root.join(
+    'app',
+    'assets',
+    'builds',
+    'login_button_component.css',
+  )
+
+  CSS = File.read(css_file_path)
+
+  attr_reader :color, :big, :css, :tag_options
 
   def initialize(color: 'primary', big: false, **tag_options)
     if !VALID_COLORS.include?(color)
       raise ArgumentError, "`color` #{color}} is invalid, expected one of #{VALID_COLORS}"
     end
 
+    @css = CSS
     @big = big
     @color = color
     @tag_options = tag_options
+
   end
 
   def css_class
