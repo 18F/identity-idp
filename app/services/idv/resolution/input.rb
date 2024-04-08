fa73c7c6c316188e541a2e9b94ae406dbb768d36
Matt Hinz <matt.hinz@gsa.gov>
2024-04-08 13:00:44 -0700

    Add basic inputs w/ mapping from idv_session

--- app/services/idv/resolution/input.rb
+++ app/services/idv/resolution/input.rb
@@ -2,6 +2,107 @@
 
 module Idv
   module Resolution
-    Input = Data.new.freeze
+    Address = Data.define(
+      :address1,
+      :address2,
+      :city,
+      :state,
+      :zipcode,
+    ) do
+      def initialize(
+        address2: nil, **rest
+      )
+        super(address2:, **rest)
+      end
+    end.freeze
+
+    # Captures PII attributes contained in a state-issued identity document
+    # (that is, a drivers license or state ID).
+    StateId = Data.define(
+      :type,
+      :number,
+      :issuing_jurisdiction,
+      :first_name,
+      :middle_name,
+      :last_name,
+      :address,
+      :dob,
+    ) do
+      def self.from_pii_from_doc(pii_from_doc)
+        StateId.new(
+          type: pii_from_doc[:state_id_type],
+          number: pii_from_doc[:state_id_number],
+          issuing_jurisdiction: pii_from_doc[:state_id_jurisdiction],
+          first_name: pii_from_doc[:first_name],
+          middle_name: pii_from_doc[:middle_name],
+          last_name: pii_from_doc[:last_name],
+          dob: pii_from_doc[:dob],
+          address: Address.new(
+            **pii_from_doc.slice(
+              :address1,
+              :address2,
+              :city,
+              :state,
+              :zipcode,
+            ),
+          ),
+        )
+      end
+    end.freeze
+
+    OtherAttributes = Data.define(
+      :ssn,
+    ).freeze
+
+    Input = Data.define(
+      :state_id,
+      :address_of_residence,
+      :other,
+    ) do
+      def initialize(
+        state_id: nil,
+        address_of_residence: nil,
+        other: nil
+      )
+        super(state_id:, address_of_residence:, other:)
+      end
+
+      # Convert data from an idv_session into an Input.
+      def self.from_idv_session(
+        pii_from_doc: nil,
+        pii_from_user: nil,
+        ssn: nil,
+        **_kwargs
+      )
+
+        state_id = pii_from_doc ? StateId.from_pii_from_doc(**pii_from_doc) : nil
+
+        address_of_residence = nil
+        if pii_from_user
+          address_of_residence = Address.new(
+            **pii_from_user.slice(
+              :address1,
+              :address2,
+              :city,
+              :state,
+              :zipcode,
+            ),
+          )
+        end
+
+        other = nil
+        if ssn
+          other = OtherAttributes.new(
+            ssn:,
+          )
+        end
+
+        Input.new(
+          state_id:,
+          address_of_residence:,
+          other:,
+        )
+      end
+    end.freeze
   end
 end
---
+++ spec/services/idv/resolution/input_spec.rb
@@ -0,0 +1,101 @@
+require 'rails_helper'
+
+RSpec.describe Idv::Resolution::Input do
+  describe '#from_idv_session' do
+    let(:idv_session) do
+      {
+        pii_from_doc:,
+        pii_from_user:,
+        ssn:,
+      }
+    end
+
+    let(:pii_from_doc) { nil }
+    let(:pii_from_user) { nil }
+    let(:ssn) { nil }
+
+    subject { described_class.from_idv_session(**idv_session) }
+
+    context 'with drivers license' do
+      let(:pii_from_doc) { Idp::Constants::MOCK_IDV_APPLICANT }
+
+      it 'maps to state_id' do
+        expect(subject.state_id).to eql(
+          Idv::Resolution::StateId.new(
+            first_name: 'FAKEY',
+            middle_name: nil,
+            last_name: 'MCFAKERSON',
+            dob: '1938-10-06',
+            address: Idv::Resolution::Address.new(
+              address1: '1 FAKE RD',
+              address2: nil,
+              city: 'GREAT FALLS',
+              state: 'MT',
+              zipcode: '59010',
+            ),
+            number: '1111111111111',
+            issuing_jurisdiction: 'ND',
+            type: 'drivers_license',
+          ),
+        )
+      end
+    end
+
+    context 'with residential address' do
+      let(:pii_from_user) { Idp::Constants::MOCK_IDV_APPLICANT_SAME_ADDRESS_AS_ID.dup }
+
+      it 'maps to address_of_residence' do
+        expect(subject.address_of_residence).to eql(
+          Idv::Resolution::Address.new(
+            address1: '1 FAKE RD',
+            address2: nil,
+            city: 'GREAT FALLS',
+            state: 'MT',
+            zipcode: '59010',
+          ),
+        )
+      end
+    end
+
+    context 'with an ssn' do
+      let(:ssn) { '666-12-3456' }
+      it 'maps to other.ssn' do
+        expect(subject.other).to eql(
+          Idv::Resolution::OtherAttributes.new(
+            ssn:,
+          ),
+        )
+      end
+    end
+  end
+
+  describe Idv::Resolution::StateId do
+    describe '#from_pii_from_doc' do
+      let(:pii_from_doc) do
+        Idp::Constants::MOCK_IDV_APPLICANT
+      end
+
+      it 'works' do
+        actual = described_class.from_pii_from_doc(pii_from_doc)
+        expect(actual).to eql(
+          described_class.new(
+            first_name: 'FAKEY',
+            middle_name: nil,
+            last_name: 'MCFAKERSON',
+            dob: '1938-10-06',
+            address: Idv::Resolution::Address.new(
+              address1: '1 FAKE RD',
+              address2: nil,
+              city: 'GREAT FALLS',
+              state: 'MT',
+              zipcode: '59010',
+            ),
+            number: '1111111111111',
+            issuing_jurisdiction: 'ND',
+            type: 'drivers_license',
+          ),
+        )
+      end
+    end
+  end
+end
