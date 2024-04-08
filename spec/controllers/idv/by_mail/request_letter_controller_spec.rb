641eb3b776114fa7df0117bc83b7cb07dc3d257c
Douglas Price <douglas.price@gsa.gov>
2024-04-08 11:44:54 -0400

    remove obsolete specs

--- spec/controllers/idv/by_mail/request_letter_controller_spec.rb
+++ spec/controllers/idv/by_mail/request_letter_controller_spec.rb
@@ -76,12 +76,6 @@ RSpec.describe Idv::ByMail::RequestLetterController,
       expect(response).to be_ok
     end
 
-    it 'assigns the current step indicator step as "verify phone or address"' do
-      get :index
-
-      expect(assigns(:step_indicator_current_step)).to eq(:verify_address)
-    end
-
     context 'with letter already sent' do
       before do
         allow_any_instance_of(Idv::ByMail::RequestLetterPresenter).
@@ -95,18 +89,6 @@ RSpec.describe Idv::ByMail::RequestLetterController,
           'IdV: request letter visited',
           letter_already_sent: true,
         )
-      end
-    end
-
-    context 'resending a letter' do
-      before do
-        allow(controller).to receive(:resend_requested?).and_return(true)
-      end
-
-      it 'assigns the current step indicator step as "get a letter"' do
-        get :index
-
-        expect(assigns(:step_indicator_current_step)).to eq(:verify_address)
       end
     end
 
