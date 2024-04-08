43fa4cfe99f32457839b8fb4d45604de0e400c29
Andrew Duthie <andrew.duthie@gsa.gov>
2024-04-08 14:36:03 -0400

    Update AlertUserAboutNewDevice specs to pass

--- spec/services/user_alerts/alert_user_about_new_device_spec.rb
+++ spec/services/user_alerts/alert_user_about_new_device_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe UserAlerts::AlertUserAboutNewDevice do
   describe '#call' do
     let(:user) { create(:user, :fully_registered) }
+    let(:event) { create(:event, user:) }
     let(:disavowal_token) { 'the_disavowal_token' }
     let(:device) { create(:device, user: user) }
 
@@ -12,24 +13,27 @@ RSpec.describe UserAlerts::AlertUserAboutNewDevice do
           :feature_new_device_alert_aggregation_enabled,
         ).and_return(true)
       end
-      it 'sets the user sign_in_new_device_at value to current datetime' do
-        described_class.call(user, device, disavowal_token)
-        expect(user.sign_in_new_device_at).not_to be_nil
+
+      it 'sets the user sign_in_new_device_at value to time of the given event' do
+        described_class.call(event:, device:, disavowal_token:)
+
+        expect(user.sign_in_new_device_at).to be_present.and eq(event.created_at)
       end
     end
 
-    context 'aggregated new device alerts disenabled' do
+    context 'aggregated new device alerts disabled' do
       before do
         allow(IdentityConfig.store).to receive(
           :feature_new_device_alert_aggregation_enabled,
         ).and_return(false)
       end
+
       it 'sends an email to all confirmed email addresses' do
         user.email_addresses.destroy_all
         confirmed_email_addresses = create_list(:email_address, 2, user: user)
         create(:email_address, user: user, confirmed_at: nil)
 
-        described_class.call(user, device, disavowal_token)
+        described_class.call(event:, device:, disavowal_token:)
 
         expect_delivered_email_count(2)
         expect_delivered_email(
