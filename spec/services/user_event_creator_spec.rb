cdaf48d404e21fea422146b1f03056b09d44611b
Andrew Duthie <andrew.duthie@gsa.gov>
2024-04-08 14:24:03 -0400

    Update specs for UserEventCreator calls to AlertUserAboutNewDevice

--- spec/services/user_event_creator_spec.rb
+++ spec/services/user_event_creator_spec.rb
@@ -65,7 +65,11 @@ RSpec.describe UserEventCreator do
         event, _disavowal_token = subject.create_user_event(event_type, user)
 
         expect(event).to be_a(Event)
-        expect(UserAlerts::AlertUserAboutNewDevice).to have_received(:call).with(event:)
+        expect(UserAlerts::AlertUserAboutNewDevice).to have_received(:call).with(
+          event: user.events.first,
+          device: user.events.first.device,
+          disavowal_token: instance_of(String),
+        )
       end
     end
 
@@ -100,7 +104,11 @@ RSpec.describe UserEventCreator do
         event, _disavowal_token = subject.create_user_event(event_type, user)
 
         expect(event).to be_a(Event)
-        expect(UserAlerts::AlertUserAboutNewDevice).to have_received(:call).with(event:)
+        expect(UserAlerts::AlertUserAboutNewDevice).to have_received(:call).with(
+          event: user.events.first,
+          device: user.events.first.device,
+          disavowal_token: instance_of(String),
+        )
       end
     end
   end
