38dc46cf5a5a8dd70c72753e873703b31d3444cf
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-04-09 10:50:02 -0400

    Start reading from `idv_session.updated_user_address_data`
    In #10385 we started writing the address the user provides in the `Idv::AddressController` to `idv_session.updated_user_address_data`. This commit starts reading the address from there so that we can eventually stop overwriting the address in `pii_from_doc`.
    
    [skip changelog]

--- spec/forms/idv/address_form_spec.rb
+++ spec/forms/idv/address_form_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Idv::AddressForm do
-  let(:pii) do
-    {
-      first_name: 'Test',
-      last_name: 'McTesterson',
+  let(:initial_address) do
+    Pii::Address.new(
       address1: '123 Main St',
-      address2: nil,
+      address2: '',
       city: 'Testertown',
       state: 'TX',
       zipcode: '11111',
-    }
+    )
   end
 
   let(:params) do
@@ -24,9 +22,9 @@ RSpec.describe Idv::AddressForm do
   end
 
   it 'is initialized with values from the hash in the initializer' do
-    address_form = Idv::AddressForm.new(pii)
+    address_form = Idv::AddressForm.new(initial_address)
     expect(address_form.address1).to eq('123 Main St')
-    expect(address_form.address2).to eq(nil)
+    expect(address_form.address2).to eq('')
     expect(address_form.city).to eq('Testertown')
     expect(address_form.state).to eq('TX')
     expect(address_form.zipcode).to eq('11111')
@@ -35,10 +33,9 @@ RSpec.describe Idv::AddressForm do
   describe '#submit' do
     context 'with valid params' do
       it 'returns a successful result' do
-        result = Idv::AddressForm.new(pii).submit(params)
+        result = Idv::AddressForm.new(initial_address).submit(params)
 
         expect(result.success?).to eq(true)
-        expect(result.extra[:address_edited]).to eq(true)
       end
     end
 
@@ -46,7 +43,7 @@ RSpec.describe Idv::AddressForm do
       it 'returns an error result' do
         params[:zipcode] = 'this is not a valid zipcde'
 
-        result = Idv::AddressForm.new(pii).submit(params)
+        result = Idv::AddressForm.new(initial_address).submit(params)
 
         expect(result.success?).to eq(false)
         expect(result.errors[:zipcode]).to be_present
@@ -57,21 +54,10 @@ RSpec.describe Idv::AddressForm do
       it 'returns an error result' do
         params.delete(:zipcode)
 
-        result = Idv::AddressForm.new(pii).submit(params)
+        result = Idv::AddressForm.new(initial_address).submit(params)
 
         expect(result.success?).to eq(false)
         expect(result.errors[:zipcode]).to be_present
-      end
-    end
-
-    context 'the user submits the same address that is in the pii' do
-      it 'does not set `address_edited` to true' do
-        params = pii.slice(:address1, :address2, :city, :state, :zipcode)
-
-        result = Idv::AddressForm.new(pii).submit(params)
-
-        expect(result.success?).to eq(true)
-        expect(result.extra[:address_edited]).to eq(false)
       end
     end
   end
