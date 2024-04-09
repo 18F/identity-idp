26861188eb8fdd9cb3d14993833e5583b7cc15bb
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-04-09 10:25:40 -0400

    Add `Pii::Address` and use it store address in `Idv::AddressController`
    Currently `Idv::AddressController` collects an updated address from the user and merges it into `pii_from_doc` in `idv_session`. This results in the address that was read from the doc being overwritten. This approach leads to a few issues:
    
    1. It necessitates a `address_edited` property on `Idv::Session` and custom code to maintain the value of that column. This code also has a tricky bug where it does not track address edits if the same data is submitted twice.
    2. It differs from the in-person workflow where the document address and the user address are maintained seperatly for use in double address verification
    
    This commit introduces a new pattern where the PII collected from the user is stored in an immutable object in the `Idv::Session`. This lets us be confident that the PII we have in the session is in fact what the user entered and operate it on such.
    
    This commit makes changes to the session so it will require a follow-up change to read this data once this has been deployed. All of that will need to be followed by a change to stop writing the data using the old approach.
    
    [skip changelog]

--- spec/controllers/idv/address_controller_spec.rb
+++ spec/controllers/idv/address_controller_spec.rb
@@ -81,6 +81,22 @@ RSpec.describe Idv::AddressController do
       )
     end
 
+    it 'adds updated_user_data to idv_session' do
+      expect do
+        put :update, params: params
+      end.to change { subject.idv_session.updated_user_address }
+
+      expect(subject.idv_session.updated_user_address).to eql(
+        Pii::Address.new(
+          address1: '1234 Main St',
+          address2: 'Apt B',
+          city: 'Beverly Hills',
+          state: 'CA',
+          zipcode: '90210',
+        ),
+      )
+    end
+
     it 'invalidates future steps' do
       expect(subject).to receive(:clear_future_steps!)
 
@@ -109,6 +125,13 @@ RSpec.describe Idv::AddressController do
         expect(response).to render_template(:new)
         expect(response.body).to include(t('idv.errors.pattern_mismatch.zipcode'))
       end
+    end
+
+    xit 'has the correct `address_edited` value when submitted twice with the same data' do
+      put :update, params: params
+      expect(subject.idv_session.address_edited).to eq(true)
+      put :update, params: params
+      expect(subject.idv_session.address_edited).to eq(true)
     end
   end
 end
