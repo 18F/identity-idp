3ab92af9ae4811f1784ac5439fb3e118e31ba68d
Gina <125507397+gina-yamada@users.noreply.github.com>
2024-04-09 08:09:25 -0600

    LG-12979: Create new idv_level value of 'in_person' on Profile (#10371)
    * Add new idv_level profile attribute for tmx
    
    * Move test setup to specific test
    
    * changelog: Upcoming Features, In-person-proofing, Add new value for idv_level on profile
    
    * Apply suggestions from code review
    
    added multiple newline to separate cases, updated context for consistent communication around tmx
    
    Co-authored-by: Matt Wagner <mattwagner@navapbc.com>
    
    * Updated if/else logic to set idv_level
    
    * Update spec to return true for enforce tmx flag
    
    ---------
    
    Co-authored-by: Matt Wagner <mattwagner@navapbc.com>

--- app/services/idv/profile_maker.rb
+++ app/services/idv/profile_maker.rb
@@ -48,7 +48,11 @@ module Idv
 
     def set_idv_level(in_person_verification_needed:, selfie_check_performed:)
       if in_person_verification_needed
-        :legacy_in_person
+        if IdentityConfig.store.in_person_proofing_enforce_tmx
+          :in_person
+        else
+          :legacy_in_person
+        end
       elsif FeatureManagement.idv_allow_selfie_check? && selfie_check_performed
         :unsupervised_with_selfie
       else
