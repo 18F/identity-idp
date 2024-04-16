c49b984e8ac9d1e78c195a9b942b24d888d9dd11
Malick Diarra <malick.diarra@gsa.gov>
2024-04-16 12:15:48 -0400

    remove question mark

--- app/controllers/users/password_compromised_controller.rb
+++ app/controllers/users/password_compromised_controller.rb
@@ -3,7 +3,7 @@
 module Users
   class PasswordCompromisedController < ApplicationController
     before_action :confirm_two_factor_authenticated
-    before_action :verify_feature_toggle_on?
+    before_action :verify_feature_toggle_on
 
     def show
       session.delete(:redirect_to_password_compromised)
@@ -11,7 +11,7 @@ module Users
       analytics.user_password_compromised_visited
     end
 
-    def verify_feature_toggle_on?
+    def verify_feature_toggle_on
       redirect_to after_sign_in_path_for(current_user) unless
         FeatureManagement.check_password_enabled?
     end
