2260b5b025f42a86ad3945a8220616df83fc0795
Charley Ferguson <charleyferguson@navapbc.com>
2024-04-16 10:19:39 -0400

    Use role="status" rather than aria-live="polite"
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- app/javascript/packages/document-capture/components/status-message.tsx
+++ app/javascript/packages/document-capture/components/status-message.tsx
@@ -24,7 +24,7 @@ function StatusMessage({ status, className, children }: StatusMessageProps) {
   const role = status === Status.ERROR ? 'alert' : 'status';
 
   return (
-    <span role={role} className={classes} aria-live="polite">
+    <span role="status" className={classes}>
       {children}
     </span>
   );
