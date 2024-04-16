c90f72276e220ebc6c5eba0da0ec410f1b1f73f4
John Maxwell <john.maxwell@gsa.gov>
2024-04-16 10:26:39 -0400

    LG-12624 additional content update for getting started screen (#10352)
    changelog: User-Facing Improvements, Biometrics, Improved language on getting started page.
    
    String updates per the new LQA process and UX; also a little bit of internal cleanup on the template.

--- spec/views/idv/welcome/show.html.erb_spec.rb
+++ spec/views/idv/welcome/show.html.erb_spec.rb
@@ -3,41 +3,43 @@ require 'rails_helper'
 RSpec.describe 'idv/welcome/show.html.erb' do
   include Devise::Test::ControllerHelpers
 
-  let(:selfie_required) { false }
-  let(:selfie_capture_enabled) { false }
   let(:user) { create(:user) }
   let(:sp_session) { {} }
+  let(:view_context) { ActionController::Base.new.view_context }
+  let(:sp) { build(:service_provider) }
 
   before do
-    sp = build(:service_provider)
+    allow(view_context).to receive(:current_user).and_return(user)
+
     decorated_sp_session = ServiceProviderSession.new(
       sp: sp,
-      view_context: nil,
+      view_context: view_context,
       sp_session: sp_session,
       service_provider_request: nil,
     )
     presenter = Idv::WelcomePresenter.new(decorated_sp_session)
     assign(:presenter, presenter)
-
-    allow(IdentityConfig.store).to receive(:doc_auth_selfie_capture_enabled).
-      and_return(selfie_capture_enabled)
+    assign(:current_user, user)
+    render
   end
 
   context 'in doc auth with an authenticated user' do
-    before do
-      assign(:current_user, user)
-      render
-    end
-
     it 'renders a link to return to the SP' do
       expect(rendered).to have_link(t('links.cancel'))
     end
 
     it 'renders the welcome template' do
       expect(rendered).to have_content(
-        t('doc_auth.headings.welcome', sp_name: 'Test Service Provider'),
+        t('doc_auth.headings.welcome', sp_name: sp.friendly_name),
       )
-      expect(rendered).to have_content(t('doc_auth.instructions.getting_started'))
+
+      expect(rendered).to have_content(
+        t(
+          'doc_auth.info.getting_started_html',
+          sp_name: sp.friendly_name,
+          link_html: '',
+        ),
+      )
       expect(rendered).to have_content(t('doc_auth.instructions.bullet1'))
       expect(rendered).to have_link(
         t('doc_auth.info.getting_started_learn_more'),
@@ -50,17 +52,23 @@ RSpec.describe 'idv/welcome/show.html.erb' do
         ),
       )
     end
+  end
 
-    context 'when the SP requests IAL2 verification' do
-      let(:sp_session) do
-        { biometric_comparison_required: true }
-      end
+  context 'in doc auth with a step-up user' do
+    let(:user) { create(:user, :proofed) }
 
-      let(:selfie_capture_enabled) { true }
-
-      it 'renders a modified welcome template' do
-        expect(rendered).to have_content(t('doc_auth.instructions.bullet1_with_selfie'))
-      end
+    it 'renders a modified welcome template' do
+      expect(rendered).to have_content(t('doc_auth.info.stepping_up_html', link_html: ''))
+      expect(rendered).to have_link(
+        t('doc_auth.info.getting_started_learn_more'),
+        href: help_center_redirect_path(
+          category: 'verify-your-identity',
+          article: 'how-to-verify-your-identity',
+          flow: :idv,
+          step: :welcome,
+          location: 'intro_paragraph',
+        ),
+      )
     end
   end
 end
