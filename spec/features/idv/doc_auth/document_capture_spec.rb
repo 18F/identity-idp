a5005290cefa5b016ff60b110ae909983f0449d4
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-04-19 10:26:25 -0400

    update selfie not live and poor quality i18n keys in specs

--- spec/features/idv/doc_auth/document_capture_spec.rb
+++ spec/features/idv/doc_auth/document_capture_spec.rb
@@ -317,7 +317,7 @@ RSpec.feature 'document capture step', :js, allowed_extra_analytics: [:*] do
             submit_images
             message = strip_tags(t('errors.doc_auth.selfie_not_live_or_poor_quality_heading'))
             expect(page).to have_content(message)
-            detail_message = strip_tags(t('doc_auth.errors.alerts.selfie_not_live'))
+            detail_message = strip_tags(t('doc_auth.errors.alerts.selfie_not_live_or_poor_quality'))
             security_message = strip_tags(
               t(
                 'idv.warning.attempts_html',
@@ -354,7 +354,7 @@ RSpec.feature 'document capture step', :js, allowed_extra_analytics: [:*] do
             submit_images
             message = strip_tags(t('errors.doc_auth.selfie_not_live_or_poor_quality_heading'))
             expect(page).to have_content(message)
-            detail_message = strip_tags(t('doc_auth.errors.alerts.selfie_poor_quality'))
+            detail_message = strip_tags(t('doc_auth.errors.alerts.selfie_not_live_or_poor_quality'))
             security_message = strip_tags(
               t(
                 'idv.warning.attempts_html',
@@ -431,7 +431,7 @@ RSpec.feature 'document capture step', :js, allowed_extra_analytics: [:*] do
             submit_images
             message = strip_tags(t('errors.doc_auth.selfie_not_live_or_poor_quality_heading'))
             expect(page).to have_content(message)
-            detail_message = strip_tags(t('doc_auth.errors.alerts.selfie_not_live'))
+            detail_message = strip_tags(t('doc_auth.errors.alerts.selfie_not_live_or_poor_quality'))
             security_message = strip_tags(
               t(
                 'idv.warning.attempts_html',
