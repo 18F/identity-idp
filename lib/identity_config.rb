72a44783e43eca53bc1502a724cf5c5dfe1e33ab
Zach Margolis <zachary.margolis@gsa.gov>
2024-04-23 08:37:26 -0700

    Move IdentityConfig logic into Identity::Hostdata
    - Allows for better sharing across codebases
    - Keeps "IdentityConfig.store" around to forward, which allows
      keeping old syntax for now
    
    changelog: Internal, Source code, Use shared logic for accessing config data

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -1,93 +1,20 @@
 # frozen_string_literal: true
 
-require 'csv'
-
-class IdentityConfig
+module IdentityConfig
   GIT_SHA = `git rev-parse --short=8 HEAD`.chomp.freeze
   GIT_TAG = `git tag --points-at HEAD`.chomp.split("\n").first.freeze
   GIT_BRANCH = `git rev-parse --abbrev-ref HEAD`.chomp.freeze
 
   VENDOR_STATUS_OPTIONS = %i[operational partial_outage full_outage].freeze
 
-  class << self
-    attr_reader :store, :key_types, :unused_keys
+  # Shorthand to allow using old syntax to access configs, minimizes merge conflicts
+  # while migrating to newer syntax
+  def self.store
+    Identity::Hostdata.config
   end
 
-  CONVERTERS = {
-    # Allows loading a string configuration from a system environment variable
-    # ex: To read DATABASE_HOST from system environment for the database_host key
-    # database_host: ['env', 'DATABASE_HOST']
-    # To use a string value directly, you can specify a string explicitly:
-    # database_host: 'localhost'
-    string: proc do |value|
-      if value.is_a?(Array) && value.length == 2 && value.first == 'env'
-        ENV.fetch(value[1])
-      elsif value.is_a?(String)
-        value
-      else
-        raise 'invalid system environment configuration value'
-      end
-    end,
-    symbol: proc { |value| value.to_sym },
-    comma_separated_string_list: proc do |value|
-      CSV.parse_line(value).to_a
-    end,
-    integer: proc do |value|
-      Integer(value)
-    end,
-    float: proc do |value|
-      Float(value)
-    end,
-    json: proc do |value, options: {}|
-      JSON.parse(value, symbolize_names: options[:symbolize_names])
-    end,
-    boolean: proc do |value|
-      case value
-      when 'true', true
-        true
-      when 'false', false
-        false
-      else
-        raise 'invalid boolean value'
-      end
-    end,
-    date: proc { |value| Date.parse(value) if value },
-    timestamp: proc do |value|
-      # When the store is built `Time.zone` is not set resulting in a NoMethodError
-      # if Time.zone.parse is called
-      #
-      # rubocop:disable Rails/TimeZone
-      Time.parse(value)
-      # rubocop:enable Rails/TimeZone
-    end,
-  }.freeze
-
-  attr_reader :key_types
-
-  def initialize(read_env)
-    @read_env = read_env
-    @written_env = {}
-    @key_types = {}
-  end
-
-  def add(key, type: :string, allow_nil: false, enum: nil, options: {})
-    value = @read_env[key]
-
-    @key_types[key] = type
-
-    converted_value = CONVERTERS.fetch(type).call(value, options: options) if !value.nil?
-    raise "#{key} is required but is not present" if converted_value.nil? && !allow_nil
-    if enum && !(enum.include?(converted_value) || (converted_value.nil? && allow_nil))
-      raise "unexpected #{key}: #{value}, expected one of #{enum}"
-    end
-
-    @written_env[key] = converted_value.freeze
-    @written_env
-  end
-
-  attr_reader :written_env
-
-  def self.build_store(config_map)
+  # rubocop:disable Metrics/BlockLength
+  BUILDER = proc do |config|
     #  ______________________________________
     # / Adding something new in here? Please \
     # \ keep methods sorted alphabetically.  /
@@ -100,8 +27,6 @@ class IdentityConfig
     #    ./  / /\ \   | \ \  \ \
     #       / /  \ \  | |\ \  \7
     #        "     "    "  "
-
-    config = IdentityConfig.new(config_map)
     config.add(:aamva_auth_request_timeout, type: :float)
     config.add(:aamva_auth_url, type: :string)
     config.add(:aamva_cert_enabled, type: :boolean)
@@ -515,11 +440,6 @@ class IdentityConfig
     config.add(:vtm_url)
     config.add(:weekly_auth_funnel_report_config, type: :json)
     config.add(:x509_presented_hash_attribute_requested_issuers, type: :json)
-
-    @key_types = config.key_types.freeze
-    @unused_keys = (config_map.keys - config.written_env.keys).freeze
-    config.written_env.freeze
-    @store = RedactedStruct.new('IdentityConfig', *config.written_env.keys, keyword_init: true).
-      new(**config.written_env)
-  end
+  end.freeze
+  # rubocop:enable Metrics/BlockLength
 end
