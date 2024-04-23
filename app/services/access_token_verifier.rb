93edf8f1d9bb2edf4de94f23e333be4d23a03fd3
Andrew Duthie <andrew.duthie@gsa.gov>
2024-04-23 10:30:27 -0400

    Return identity as tuple pair with submission response
    See: https://github.com/18F/identity-idp/pull/10481#discussion_r1576334514

--- app/services/access_token_verifier.rb
+++ app/services/access_token_verifier.rb
@@ -12,26 +12,21 @@ class AccessTokenVerifier
   end
 
   def submit
-    FormResponse.new(
-      success: valid?, errors: errors, extra: {
-        client_id: identity&.service_provider,
-        ial: identity&.ial,
-      }
+    response = FormResponse.new(
+      success: valid?,
+      errors: errors,
+      extra: {
+        client_id: @identity&.service_provider,
+        ial: @identity&.ial,
+      },
     )
-  end
 
-  def identity
-    valid? ? @identity : nil
+    [response, @identity]
   end
 
   private
 
   attr_reader :http_authorization_header
-
-  def valid?
-    return @valid if defined?(@valid)
-    @valid = super
-  end
 
   def validate_access_token
     access_token = extract_access_token(http_authorization_header)
