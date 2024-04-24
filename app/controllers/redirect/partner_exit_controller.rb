64def3027e1dd050d9a768f669aac4d027631838
Jessica Dembe <jessica.dembe@gsa.gov>
2024-04-24 13:17:46 -0400

    edit controller, write test, and fix failing spec

--- app/controllers/redirect/partner_exit_controller.rb
+++ app/controllers/redirect/partner_exit_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Redirect
-  class PartnerExitController < Redirect::ReturnToSpController
+  class PartnerExitController < ApplicationController
     before_action :validate_sp_exists
 
     def show
--- spec/controllers/redirect/partner_exit_controller_spec.rb
+++ spec/controllers/redirect/partner_exit_controller_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Redirect::PartnerExitController do
-  before do
-    stub_analytics
-  end
-
   describe '#show' do
+    let(:user) { create(:user, :fully_registered) }
+
+    before do
+      stub_sign_in_before_2fa(user)
+      stub_analytics
+    end
+
+    it 'redirects with no service provider attached' do
+      get 'show'
+
+      expect(response).to redirect_to(account_path)
+    end
   end
 end
