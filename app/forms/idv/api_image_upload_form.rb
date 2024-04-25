1c42c2156cf83734b09aefdf8ee57938cc050142
Dawei Wang <daweiwang@navapbc.com>
2024-04-25 17:56:09 -0400

    LG-13047: log birth year when doc auth vendor submitted.
    changelog: Internal, Doc Auth, Analytics log for birth year

--- app/forms/idv/api_image_upload_form.rb
+++ app/forms/idv/api_image_upload_form.rb
@@ -342,8 +342,10 @@ module Idv
     def update_analytics(client_response:, vendor_request_time_in_ms:)
       add_costs(client_response)
       update_funnel(client_response)
+      birth_year = birth_year(client_response)
       analytics.idv_doc_auth_submitted_image_upload_vendor(
         **client_response.to_h.merge(
+          birth_year: birth_year,
           client_image_metrics: image_metadata,
           async: false,
           flow_path: params[:flow_path],
@@ -351,6 +353,19 @@ module Idv
         ).except(:classification_info).
         merge(acuant_sdk_upgrade_ab_test_data),
       )
+    end
+
+    def birth_year(client_response)
+      dob = client_response.pii_from_doc&.dob
+      if dob.respond_to?(:year)
+        dob.year
+      else
+        begin
+          dob&.to_date&.year
+        rescue
+          nil
+        end
+      end
     end
 
     def store_encrypted_images_if_required
