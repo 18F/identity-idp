67be029763d735997308f327bf419ff34acdca9a
John Maxwell <john.maxwell@gsa.gov>
2024-04-25 09:34:17 -0400

    One last spec cleanup.

--- spec/controllers/concerns/remember_device_concern_spec.rb
+++ spec/controllers/concerns/remember_device_concern_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe RememberDeviceConcern do
-  let(:sp) { build(:service_provider) }
+  let(:sp) { nil }
   let(:raw_session) { {} }
-  let(:request) do
-    double(
-      'request',
+
+  subject(:test_controller) do
+    test_controller_class =
+      Class.new(ApplicationController) do
+        include(RememberDeviceConcern)
+
+        attr_reader :sp, :raw_session, :request
+        alias :sp_from_sp_session :sp
+        alias :sp_session :raw_session
+
+        def initialize(sp, raw_session, request)
+          @sp = sp
+          @raw_session = raw_session
+          @request = request
+        end
+      end
+
+    test_request = double(
+      'test_request',
       session: raw_session,
       parameters: {},
       filtered_parameters: {},
     )
+
+    test_controller_class.new(sp, raw_session, test_request)
   end
-
-  let(:test_class) do
-    Class.new(ApplicationController) do
-      include(RememberDeviceConcern)
-
-      attr_reader :sp, :raw_session, :request
-      alias :sp_from_sp_session :sp
-      alias :sp_session :raw_session
-
-      def initialize(sp, raw_session, request)
-        @sp = sp
-        @raw_session = raw_session
-        @request = request
-      end
-    end
-  end
-
-  subject(:test_instance) { test_class.new(sp, raw_session, request) }
 
   describe '#mfa_expiration_interval' do
     let(:expected_aal_1_expiration) { 720.hours }
@@ -37,19 +37,19 @@ RSpec.describe RememberDeviceConcern do
     context 'with no sp' do
       let(:sp) { nil }
 
-      it { expect(test_instance.mfa_expiration_interval).to eq(expected_aal_1_expiration) }
+      it { expect(test_controller.mfa_expiration_interval).to eq(expected_aal_1_expiration) }
     end
 
     context 'with an AAL2 sp' do
       let(:sp) { build(:service_provider, default_aal: 2) }
 
-      it { expect(test_instance.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
+      it { expect(test_controller.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
     end
 
     context 'with an IAL2 sp' do
       let(:sp) { build(:service_provider, ial: 2) }
 
-      it { expect(test_instance.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
+      it { expect(test_controller.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
     end
 
     context 'with an sp that is not AAL2 or IAL2' do
@@ -59,13 +59,13 @@ RSpec.describe RememberDeviceConcern do
         context 'with vtr' do
           let(:raw_session) { { vtr: ['C1'] } }
 
-          it { expect(test_instance.mfa_expiration_interval).to eq(30.days) }
+          it { expect(test_controller.mfa_expiration_interval).to eq(30.days) }
         end
 
         context 'with legacy acr' do
           let(:raw_session) { { acr_values: Saml::Idp::Constants::AAL1_AUTHN_CONTEXT_CLASSREF } }
 
-          it { expect(test_instance.mfa_expiration_interval).to eq(30.days) }
+          it { expect(test_controller.mfa_expiration_interval).to eq(30.days) }
         end
       end
 
@@ -73,13 +73,13 @@ RSpec.describe RememberDeviceConcern do
         context 'with vtr' do
           let(:raw_session) { { vtr: ['C2'] } }
 
-          it { expect(test_instance.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
+          it { expect(test_controller.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
         end
 
         context 'with legacy acr' do
           let(:raw_session) { { acr_values: Saml::Idp::Constants::AAL2_AUTHN_CONTEXT_CLASSREF } }
 
-          it { expect(test_instance.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
+          it { expect(test_controller.mfa_expiration_interval).to eq(expected_aal_2_expiration) }
         end
       end
     end
