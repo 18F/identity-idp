551b2d5f28d2d341e5e2b573cc6b92aa6528863a
Jessica Dembe <jessica.dembe@gsa.gov>
2024-04-26 09:56:08 -0400

    add controller test

--- spec/controllers/completions_cancellation_controller_spec.rb
+++ spec/controllers/completions_cancellation_controller_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CompletionsCancellationController do
-  describe '#show' do
-    subject(:action) { get :index }
+  let(:user) { build_stubbed(:user, :fully_registered) }
+
+  before do
+    stub_sign_in(user)
+  end
+
+  it 'renders the show template' do
+    stub_analytics
+
+    expect(@analytics).to receive(:track_event).with(
+      :exit_to_sp_confirmation_page_visited,
+    )
+
+    get :show
+
+    expect(response).to render_template :show
   end
 end
