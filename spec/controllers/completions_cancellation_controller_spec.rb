9d1cdb8fa3feed8662cc462804b37d88e389ddf7
Jessica Dembe <jessica.dembe@gsa.gov>
2024-04-30 10:46:28 -0400

    add html, lintfix

--- spec/controllers/completions_cancellation_controller_spec.rb
+++ spec/controllers/completions_cancellation_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe CompletionsCancellationController do
     stub_analytics
 
     get :show
-    
+
     expect(@analytics).to have_logged_event(:completions_cancellation_visited)
 
     expect(response).to render_template :show
