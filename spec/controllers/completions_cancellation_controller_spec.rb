18d1ebaf7abd178f9cf01e87fb0372c4228ac3e3
Jessica Dembe <jessica.dembe@gsa.gov>
2024-04-30 10:31:29 -0400

    Update spec/controllers/completions_cancellation_controller_spec.rb
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- spec/controllers/completions_cancellation_controller_spec.rb
+++ spec/controllers/completions_cancellation_controller_spec.rb
@@ -10,11 +10,9 @@ RSpec.describe CompletionsCancellationController do
   it 'renders the show template' do
     stub_analytics
 
-    expect(@analytics).to receive(:track_event).with(
-      :exit_to_sp_confirmation_page_visited,
-    )
-
     get :show
+    
+    expect(@analytics).to have_logged_event(:completions_cancellation_visited)
 
     expect(response).to render_template :show
   end
