7fe060996d0efb7805a62caa65eb5c14cd968b2a
Colter <59977618+colter-nattrass@users.noreply.github.com>
2024-04-30 14:10:59 -0600

    lg-13013-fixed branch naming

--- app/services/db/monthly_sp_auth_count/unique_monthly_auth_counts_by_partner.rb
+++ app/services/db/monthly_sp_auth_count/unique_monthly_auth_counts_by_partner.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 module Db
-    module MonthlySpAuthCount
-        module UniqueMonthlyAuthCountsByPartner
-            extend Reports::QueryHelpers
+  module MonthlySpAuthCount
+  module UniqueMonthlyAuthCountsByPartner
+    extend Reports::QueryHelpers
 
-            module_function
+    module_function
 
-            # @param [String] partner label for billing
-            # @param [Array<String>] issuers issuers for the partner
-            # @param [Date] start_date partner start date
-            # @param [Date] end_date partner end date
-            # @return [PG::Result, Array]
+    # @param [String] partner label for billing
+    # @param [Array<String>] issuers issuers for the partner
+    # @param [Date] start_date partner start date
+    # @param [Date] end_date partner end date
+    # @return [PG::Result, Array]
+    def call(partner:, issuers:, start_date:, end_date:)
+        date_range = start_date...end_date
+
+        return [] if !date_range || issuers.blank?
+
+        months = Reports::MonthHelper.months(date_range)
+        queries = build_queries(issuers: issuers, months: months)
+
+        
