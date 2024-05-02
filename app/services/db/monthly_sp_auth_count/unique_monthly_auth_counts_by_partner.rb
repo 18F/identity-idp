f86bd035936c1187227bc90b07cc4eabf5e0b139
samathad2023 <sdondeti@flexion.us>
2024-05-02 13:53:00 -0700

    lg-13013 updated specs and unqui_counts_by_partner helper
    changelog: Internal, Reporting,update specs and unique partner helper

--- app/services/db/monthly_sp_auth_count/unique_monthly_auth_counts_by_partner.rb
+++ app/services/db/monthly_sp_auth_count/unique_monthly_auth_counts_by_partner.rb
@@ -7,31 +7,28 @@ module Db
 
       module_function
 
-      # @param [String] partner key label for billing
-      # @param [Array<String>] issuers issuers for the partner
-      # @param [Date] start_date partner start date
-      # @param [Date] end_date partner end date
+      # @param [String] key label for billing (Partner requesting agency)
+      # @param [Array<String>] issuers issuers for the iaa
+      # @param [Date] start_date iaa start date
+      # @param [Date] end_date iaa end date
       # @return [PG::Result, Array]
       def call(key:, issuers:, start_date:, end_date:)
         date_range = start_date...end_date
 
         return [] if !date_range || issuers.blank?
 
+        # Query a month at a time, to keep query time/result size fairly reasonable
+        # The results are rows with [user_id, ial, auth_count, year_month]
         months = Reports::MonthHelper.months(date_range)
         queries = build_queries(issuers: issuers, months: months)
-        puts("queries: #{queries}")
-        # ial_to_year_month_to_users = Hash.new do |ial_h, ial_k|
-        #   ial_h[ial_k] = Hash.new { |ym_h, ym_k| ym_h[ym_k] = Multiset.new }
-        # end
+
         ial_to_year_month_to_users = Hash.new do |ial_h, ial_k|
-          ial_h[ial_k] = Hash.new do |year_h, year_k|
-            year_h[year_k] = Hash.new { |month_h, month_k| month_h[month_k] = Multiset.new }
-          end
+          ial_h[ial_k] = Hash.new { |ym_h, ym_k| ym_h[ym_k] = Multiset.new }
         end
-        puts("ial_to_year_month_to_users: #{ial_to_year_month_to_users.inspect}")
+
         queries.each do |query|
           temp_copy = ial_to_year_month_to_users.deep_dup
-          puts("temp_copy: #{temp_copy.inspect}")  
+
           with_retries(
             max_tries: 3,
             rescue: [
@@ -48,79 +45,45 @@ module Db
             Reports::BaseReport.transaction_with_timeout do
               ActiveRecord::Base.connection.execute(query).each do |row|
                 user_id = row['user_id']
-                verified_year = row['verified_year']
-                verified_month = row['verified_month']
-                # year_month = row['year_month']
+                year_month = row['year_month']
                 auth_count = row['auth_count']
                 ial = row['ial']
 
-                ial_to_year_month_to_users[ial][verified_year][verified_month].add(user_id, auth_count)
+                ial_to_year_month_to_users[ial][year_month].add(user_id, auth_count)
               end
             end
           end
         end
-        puts("ial_to_year_month_to_users end: #{ial_to_year_month_to_users}") 
+
         rows = []
-        puts("year_month_to_users after: #{ial_to_year_month_to_users}")
-        ial_to_year_month_to_users.each do |ial, verified_years, verified_months|
+
+        ial_to_year_month_to_users.each do |ial, year_month_to_users|
           prev_seen_users = Set.new
-          puts("prev_seen_users: #{prev_seen_users}")
-          # year_months = year_month_to_users.keys.sort
-          # puts("year_months: #{year_months}")
-          # year_months.each do |year_month|
-          #   year_month_users = year_month_to_users[year_month]
 
-          #   auth_count = year_month_users.count
-          #   unique_users = year_month_users.uniq.to_set
+          year_months = year_month_to_users.keys.sort
 
-          #   new_unique_users = unique_users - prev_seen_users
-          #   prev_seen_users |= unique_users
+          year_months.each do |year_month|
+            year_month_users = year_month_to_users[year_month]
 
-          #   rows << {
-          #     key: key,
-          #     ial: ial,
-          #     year: verified_year,
-          #     month: verified_month,
-          #     year_month: year_month,
-          #     iaa_start_date: date_range.begin.to_s,
-          #     iaa_end_date: date_range.end.to_s,
-          #     total_auth_count: auth_count,
-          #     unique_users: unique_users.count,
-          #     new_unique_users: new_unique_users.count,
-          #   }
-          # end
-          years = verified_years.keys.sort
-          puts("years: #{years}")
-          years.each do |year|
-            # month_to_users = verified_years[year]
-            months = verified_months.keys.sort
-            puts("months: #{months}")
-            puts("month_to_users: #{month_to_users}")
-            months.each do |month|
-              month_users = month_to_users[month]
+            auth_count = year_month_users.count
+            unique_users = year_month_users.uniq.to_set
 
-              auth_count = month_users.count
-              unique_users = month_users.uniq.to_set
+            new_unique_users = unique_users - prev_seen_users
+            prev_seen_users |= unique_users
 
-              new_unique_users = unique_users - prev_seen_users
-              prev_seen_users |= unique_users
-
-              rows << {
-                key: key,
-                ial: ial,
-                year: year,
-                month: month,
-                # year_month: "#{year}-#{month}",
-                iaa_start_date: date_range.begin.to_s,
-                iaa_end_date: date_range.end.to_s,
-                total_auth_count: auth_count,
-                unique_users: unique_users.count,
-                new_unique_users: new_unique_users.count,
-              }
-            end
+            rows << {
+              key: key,
+              ial: ial,
+              year_month: year_month,
+              iaa_start_date: date_range.begin.to_s,
+              iaa_end_date: date_range.end.to_s,
+              total_auth_count: auth_count,
+              unique_users: unique_users.count,
+              new_unique_users: new_unique_users.count,
+            }
           end
         end
-        puts("rows: #{rows}")
+
         rows
       end
 
@@ -133,20 +96,18 @@ module Db
           params = {
             range_start: month_range.begin,
             range_end: month_range.end,
-            verified_year: month_range.begin.year,
-            verified_month: month_range.begin.month,
-            # year_month: month_range.begin.strftime('%Y%m'),
+            year_month: month_range.begin.strftime('%Y%m'),
             issuers: issuers,
           }.transform_values { |value| quote(value) }
 
           format(<<~SQL, params)
             SELECT
               sp_return_logs.user_id
-            , %{verified_year} AS verified_year
-            , %{verified_month} AS verified_month
+            , %{year_month} AS year_month
             , COUNT(sp_return_logs.id) AS auth_count
             , sp_return_logs.ial
             FROM sp_return_logs
+            join  service_providers on sp_return_logs.issuer = service_providers.issuer
             WHERE
                   sp_return_logs.profile_verified_at::date BETWEEN %{range_start} AND %{range_end}
               AND sp_return_logs.issuer IN %{issuers}
