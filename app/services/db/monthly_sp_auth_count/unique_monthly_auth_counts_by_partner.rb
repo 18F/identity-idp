fdd7b60b66e21464f107884fc06f239f92c5bf87
Colter <59977618+colter-nattrass@users.noreply.github.com>
2024-05-03 15:58:41 -0600

    lg-13013-modifying sql statements to bring in verification by year sums

--- app/services/db/monthly_sp_auth_count/unique_monthly_auth_counts_by_partner.rb
+++ app/services/db/monthly_sp_auth_count/unique_monthly_auth_counts_by_partner.rb
@@ -131,12 +131,20 @@ module Db
       # @return [Array<String>]
       def build_queries(issuers:, months:)
         months.map do |month_range|
+          today = Date.today
           params = {
             range_start: month_range.begin,
             range_end: month_range.end,
             year_month: month_range.begin.strftime('%Y%m'),
             issuers: issuers,
+            one_years_ago: today - 365,
+            two_years_ago: today - 2 * 365,
+            three_years_ago: today - 3 * 365,
+            four_years_ago: today - 4 * 365,
+            five_years_ago: today - 5 * 365,
           }.transform_values { |value| quote(value) }
+
+
 
           format(<<~SQL, params)
             SELECT
