c3a21d5ce6837e802efdebbc21835a1bf0f59935
Zach Margolis <zachary.margolis@gsa.gov>
2024-05-08 14:49:01 -0700

    Use YAML array syntax, remove array conversion code

--- lib/i18n_flat_yml_backend.rb
+++ lib/i18n_flat_yml_backend.rb
@@ -52,25 +52,6 @@ class I18nFlatYmlBackend < I18n::Backend::Simple
       to_insert[last] = value
     end
 
-    convert_arrays(result)
-  end
-
-  # If all keys of a hash are numeric, converts the hash to an array
-  def self.convert_arrays(outer)
-    outer.transform_values do |inner|
-      if inner.is_a?(Hash)
-        if inner.keys.all? { |key| numeric_key?(key) }
-          inner.to_a.sort_by { |k, _v| k.to_i }.map { |_k, v| v }
-        else
-          convert_arrays(inner)
-        end
-      else
-        inner
-      end
-    end
-  end
-
-  def self.numeric_key?(str)
-    /\A\d+\Z/.match?(str)
+    result
   end
 end
