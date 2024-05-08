71b3ca98b566307d549ec4d65ff96a529a873cd9
Andrew Duthie <andrew.duthie@gsa.gov>
2024-05-08 10:19:08 -0400

    Temporary: Demonstrate failures

--- spec/controllers/users/totp_setup_controller_spec.rb
+++ spec/controllers/users/totp_setup_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Users::TotpSetupController, devise: true do
+RSpec.describe Users::TotpSetupController, devise: true, allowed_extra_analytics: [:*] do
   describe 'before_actions' do
     it 'includes appropriate before_actions' do
       expect(subject).to have_actions(
