309751f23665cdc23bc45ad8d49634bc4678e7e2
Andrew Duthie <andrew.duthie@gsa.gov>
2024-05-10 09:43:42 -0400

    LG-12706: Include user_id in piv_cac_login event
    changelog: Internal, Analytics, Include user_id in piv_cac_login event

--- spec/support/analytics_helper.rb
+++ spec/support/analytics_helper.rb
@@ -1,7 +1,14 @@
 module AnalyticsHelper
-  def stub_analytics
-    controller.analytics = FakeAnalytics.new
-    @analytics = controller.analytics
+  def stub_analytics(user: nil)
+    @analytics = FakeAnalytics.new
+
+    allow(controller).to receive(:analytics).and_wrap_original do |_original|
+      expect(controller.analytics_user).to eq(user) if user
+      allow(controller).to receive(:analytics).and_call_original
+      controller.analytics = @analytics
+    end
+
+    @analytics
   end
 
   def unstub_analytics
