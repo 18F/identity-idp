8d6da7b885be2dc28eca295f303f9f99ddb2c5ba
kevinsmaster5 <kevin.masters@gsa.gov>
2024-05-10 10:05:33 -0400

    improve concern spec with time freeze and clarify test context

--- spec/controllers/concerns/sign_in_duration_concern_spec.rb
+++ spec/controllers/concerns/sign_in_duration_concern_spec.rb
@@ -16,17 +16,27 @@ RSpec.describe SignInDurationConcern, type: :controller do
   let(:instance) { test_class.new }
 
   describe '#sign_in_duration_seconds' do
+    let(:sign_in_page_visited_at) {}
+    around do |example|
+      freeze_time { example.run }
+    end
+
     before do
-      instance.session[:sign_in_page_visited_at] = 6.seconds.ago.to_s
+      instance.session[:sign_in_page_visited_at] = sign_in_page_visited_at
     end
 
-    it 'returns 6 seconds' do
-      expect(instance.sign_in_duration_seconds).to eq(6)
+    context 'when session value is assigned' do
+      let(:sign_in_page_visited_at) { 6.seconds.ago.to_s }
+      it 'returns seconds since value' do
+        expect(instance.sign_in_duration_seconds).to eq(6)
+      end
     end
 
-    it 'returns nil' do
-      instance.session[:sign_in_page_visited_at] = nil
-      expect(instance.sign_in_duration_seconds).to eq(nil)
+    context 'when session value is not assigned' do
+      let(:sign_in_page_visited_at) { nil }
+      it 'returns nil' do
+        expect(instance.sign_in_duration_seconds).to be_nil
+      end
     end
   end
 end
