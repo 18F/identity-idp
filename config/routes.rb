b32c6eb6cca97c146247005899555b4218a98963
Matt Wagner <matt.wagner@gsa.gov>
2024-05-13 16:31:56 -0400

    LG-12086 | Renames CaptureDocStatusController
    changelog: Internal, Tech Debt, Renames CaptureDocStatusController to LinkSentPollController

--- config/routes.rb
+++ config/routes.rb
@@ -351,7 +351,7 @@ Rails.application.routes.draw do
       put '/hybrid_handoff' => 'hybrid_handoff#update'
       get '/link_sent' => 'link_sent#show'
       put '/link_sent' => 'link_sent#update'
-      get '/link_sent/poll' => 'capture_doc_status#show', as: :capture_doc_status
+      get '/link_sent/poll' => 'link_sent_poll#show'
       get '/ssn' => 'ssn#show'
       put '/ssn' => 'ssn#update'
       get '/verify_info' => 'verify_info#show'
