b910bfa45dcba468e09dc1d0246c1cfc234e77b0
Mitchell Henke <mitchell.henke@gsa.gov>
2024-05-14 09:06:10 -0500

    consolidate tests

--- spec/features/accessibility/static_pages_spec.rb
+++ spec/features/accessibility/static_pages_spec.rb
@@ -8,33 +8,21 @@ RSpec.feature 'Accessibility on static pages', :js do
     expect_page_to_have_no_accessibility_violations(page)
   end
 
-  scenario '401 page' do
-    visit '/401'
+  scenario 'other static error pages' do
+    static_pages = [
+      '/401',
+      '/406',
+      '/422',
+      '/429',
+      '/500',
+    ]
 
-    expect_page_to_have_no_accessibility_violations(page)
-  end
+    aggregate_failures do
+      static_pages.each do |path|
+        visit path
 
-  scenario '406 page' do
-    visit '/406'
-
-    expect_page_to_have_no_accessibility_violations(page)
-  end
-
-  scenario '422 page' do
-    visit '/422'
-
-    expect_page_to_have_no_accessibility_violations(page)
-  end
-
-  scenario '429 page' do
-    visit '/429'
-
-    expect_page_to_have_no_accessibility_violations(page)
-  end
-
-  scenario '500 page' do
-    visit '/500'
-
-    expect_page_to_have_no_accessibility_violations(page)
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
   end
 end
