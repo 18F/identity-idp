fd7b29065e98384c44a672510addde32803ab7cb
svalexander <shannon.alexander-navarro@gsa.gov>
2024-05-14 16:46:57 -0400

    delete state id step files
    changelog: Internal, In-person proofing, delete state id FSM step files

--- app/services/idv/analytics_events_enhancer.rb
+++ app/services/idv/analytics_events_enhancer.rb
@@ -54,12 +54,10 @@ module Idv
       idv_in_person_prepare_submitted
       idv_in_person_prepare_visited
       idv_in_person_proofing_address_visited
-      idv_in_person_proofing_cancel_update_state_id
       idv_in_person_proofing_enrollments_ready_for_status_check_job_completed
       idv_in_person_proofing_enrollments_ready_for_status_check_job_ingestion_error
       idv_in_person_proofing_enrollments_ready_for_status_check_job_started
       idv_in_person_proofing_nontransliterable_characters_submitted
-      idv_in_person_proofing_redo_state_id_submitted
       idv_in_person_proofing_residential_address_submitted
       idv_in_person_proofing_state_id_submitted
       idv_in_person_proofing_state_id_visited
