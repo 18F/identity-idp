e345ebd09faa95024c4d85542969a94324fc0828
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-05-14 16:08:11 -0400

    Stop creating `ServiceProviderRequest` records with unused fields
    Previous commits retired the following fields on the `ServiceProviderRequest` model:
    
    - ial
    - aal
    - biometric_comparison_required
    
    These fields were left behind because removing them leads to issues during deploys when old and new versions of the code are running.
    
    The `ServiceProviderRequest` model has default values for these fields. This means that we can stop creating records with these fields. However, we use the hash that is written to redis to create `ServiceProviderRequest`s. This means that if we remove the keyword arguments for these fields we will encounter a `ArgumentError` when we try to create a record on a new host with a hash written by an old host.
    
    [skip changelog]

--- app/models/service_provider_request.rb
+++ app/models/service_provider_request.rb
@@ -4,27 +4,29 @@ class ServiceProviderRequest
   # WARNING - Modification of these params requires particular care
   # since these objects are serialized to/from Redis and may be present
   # upon deployment
-  attr_accessor :uuid, :issuer, :url, :ial, :aal, :requested_attributes,
-                :biometric_comparison_required, :acr_values, :vtr
+  attr_accessor :uuid, :issuer, :url, :requested_attributes, :acr_values, :vtr
+
+  # Deprecated attributes to remove
+  attr_accessor :ial, :aal, :biometric_comparison_required
 
   def initialize(
     uuid: nil,
     issuer: nil,
     url: nil,
+    requested_attributes: [],
+    acr_values: nil,
+    vtr: nil,
+    # Deprecated attributes to remove
+    # rubocop:disable Lint/UnusedMethodArgument
     ial: nil,
     aal: nil,
-    requested_attributes: [],
-    biometric_comparison_required: false,
-    acr_values: nil,
-    vtr: nil
+    biometric_comparison_required: false
+    # rubocop:enable Lint/UnusedMethodArgument
   )
     @uuid = uuid
     @issuer = issuer
     @url = url
-    @ial = ial
-    @aal = aal
     @requested_attributes = requested_attributes&.map(&:to_s)
-    @biometric_comparison_required = biometric_comparison_required
     @acr_values = acr_values
     @vtr = vtr
   end
