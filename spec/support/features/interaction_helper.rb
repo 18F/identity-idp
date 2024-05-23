d8d45bf42fc3245ea49f5ab6ea5053aef534f220
Mitchell Henke <mitchell.henke@gsa.gov>
2024-05-23 12:08:43 -0500

    implement wait helper

--- spec/support/features/interaction_helper.rb
+++ spec/support/features/interaction_helper.rb
@@ -1,8 +1,14 @@
 module InteractionHelper
   def click_spinner_button_and_wait(...)
     click_on(...)
-    begin
+    wait_for_content_to_disappear do
       expect(page).to have_no_css('lg-spinner-button.spinner-button--spinner-active', wait: 10)
+    end
+  end
+
+  def wait_for_content_to_disappear do
+    begin
+      yield
     rescue Selenium::WebDriver::Error::StaleElementReferenceError
       # A StaleElementReferenceError means that the context the element
       # was in has disappeared, which means the element is gone too.
