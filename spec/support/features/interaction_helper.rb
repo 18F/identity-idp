d43e786617f526c9e93ba62dd904eef4f6d601d2
Mitchell Henke <mitchell.henke@gsa.gov>
2024-05-23 12:22:23 -0500

    Update spec/support/features/interaction_helper.rb
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- spec/support/features/interaction_helper.rb
+++ spec/support/features/interaction_helper.rb
@@ -7,16 +7,14 @@ module InteractionHelper
   end
 
   def wait_for_content_to_disappear do
-    begin
-      yield
-    rescue Selenium::WebDriver::Error::StaleElementReferenceError
-      # A StaleElementReferenceError means that the context the element
-      # was in has disappeared, which means the element is gone too.
-    rescue Selenium::WebDriver::Error::UnknownError => e
-      # We sometimes see "UnknownError" with an error message that is similar to a
-      # StaleElementReferenceError, but have not been able to resolve it and are ignoring it
-      # for now.
-      raise e if !e.message.include?('Node with given id does not belong to the document')
-    end
+    yield
+  rescue Selenium::WebDriver::Error::StaleElementReferenceError
+    # A StaleElementReferenceError means that the context the element
+    # was in has disappeared, which means the element is gone too.
+  rescue Selenium::WebDriver::Error::UnknownError => e
+    # We sometimes see "UnknownError" with an error message that is similar to a
+    # StaleElementReferenceError, but have not been able to resolve it and are ignoring it
+    # for now.
+    raise e if !e.message.include?('Node with given id does not belong to the document')
   end
 end
