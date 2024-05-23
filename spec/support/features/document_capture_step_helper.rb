005f1bd8c575415b20da12cf95a821864d8dd1c8
Mitchell Henke <mitchell.henke@gsa.gov>
2024-05-23 12:08:43 -0500

    implement wait helper

--- spec/support/features/document_capture_step_helper.rb
+++ spec/support/features/document_capture_step_helper.rb
@@ -3,17 +3,8 @@ module DocumentCaptureStepHelper
     click_on 'Submit'
 
     # Wait for the the loading interstitial to disappear before continuing
-
-    begin
+    wait_for_content_to_disappear do
       expect(page).not_to have_content(t('doc_auth.headings.interstitial'), wait: 10)
-    rescue Selenium::WebDriver::Error::StaleElementReferenceError
-      # A StaleElementReferenceError means that the context the element
-      # was in has disappeared, which means the element is gone too.
-    rescue Selenium::WebDriver::Error::UnknownError => e
-      # We sometimes see "UnknownError" with an error message that is similar to a
-      # StaleElementReferenceError, but have not been able to resolve it and are ignoring it
-      # for now.
-      raise e if !e.message.include?('Node with given id does not belong to the document')
     end
   end
 
--- spec/support/features/in_person_helper.rb
+++ spec/support/features/in_person_helper.rb
@@ -108,16 +108,8 @@ module InPersonHelper
     end
 
     # pause for the location list to disappear
-    begin
+    wait_for_content_to_disappear do
       expect(page).to have_no_css('.location-collection-item')
-    rescue Selenium::WebDriver::Error::StaleElementReferenceError
-      # A StaleElementReferenceError means that the context the element
-      # was in has disappeared, which means the element is gone too.
-    rescue Selenium::WebDriver::Error::UnknownError => e
-      # We sometimes see "UnknownError" with an error message that is similar to a
-      # StaleElementReferenceError, but have not been able to resolve it and are ignoring it
-      # for now.
-      raise e if !e.message.include?('Node with given id does not belong to the document')
     end
   end
 
