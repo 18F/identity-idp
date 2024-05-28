c3c9be0ea5a119976894fa0780ea8b5c49f16e45
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-05-28 15:33:07 -0400

    overviewify everything

--- app/services/marketing_site.rb
+++ app/services/marketing_site.rb
@@ -16,7 +16,6 @@ class MarketingSite
     verify-your-identity/verify-your-identity-in-person
     verify-your-identity/phone-number
     verify-your-identity/verify-your-address-by-mail
-    verify-your-identity/how-to-verify-your-identity
     verify-your-identity/overview
   ].to_set.freeze
 
