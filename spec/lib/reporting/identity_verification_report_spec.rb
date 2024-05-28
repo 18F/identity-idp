bac8bfa9970fbaecf080c8a2edb43095a02f14fb
Osman Latif <olatif@flexion.us>
2024-05-28 16:21:56 -0500

    LG-13359: MKMR Proofing rate verified users count fixes
    changelog: Internal, Reporting,key metrics count

--- spec/lib/reporting/identity_verification_report_spec.rb
+++ spec/lib/reporting/identity_verification_report_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Reporting::IdentityVerificationReport do
         { 'user_id' => 'user3', 'name' => 'Fraud: Profile review passed', 'success' => '1' },
 
         # Success through address confirmation user
-        { 'user_id' => 'user4', 'name' => 'IdV: GPO verification submitted' },
+        { 'user_id' => 'user3', 'name' => 'IdV: GPO verification submitted' },
 
         # Success through in-person verification, failed doc auth (rejected)
         { 'user_id' => 'user5', 'name' => 'IdV: doc auth welcome visited' },
@@ -78,15 +78,15 @@ RSpec.describe Reporting::IdentityVerificationReport do
         ['Workflow completed - In-Person Pending', 1],
         ['Workflow completed - Fraud Review Pending', 1],
         [],
-        ['Successfully Verified', 4],
+        ['Successfully Verified', 3],
         ['Successfully Verified - With phone number', 1],
         ['Successfully Verified - With mailed code', 1],
         ['Successfully Verified - In Person', 1],
         ['Successfully Verified - Passed fraud review', 1],
-        ['Blanket Proofing Rate (IDV Started to Successfully Verified)', 0.8],
-        ['Intent Proofing Rate (Welcome Submitted to Successfully Verified)', 0.8],
-        ['Actual Proofing Rate (Image Submitted to Successfully Verified)', 0.8],
-        ['Industry Proofing Rate (Verified minus IDV Rejected)', 0.8],
+        ['Blanket Proofing Rate (IDV Started to Successfully Verified)', 0.6],
+        ['Intent Proofing Rate (Welcome Submitted to Successfully Verified)', 0.6],
+        ['Actual Proofing Rate (Image Submitted to Successfully Verified)', 0.6],
+        ['Industry Proofing Rate (Verified minus IDV Rejected)', 0.75],
       ]
       # rubocop:enable Layout/LineLength
 
@@ -120,15 +120,15 @@ RSpec.describe Reporting::IdentityVerificationReport do
         ['Workflow completed - In-Person Pending', '1'],
         ['Workflow completed - Fraud Review Pending', '1'],
         [],
-        ['Successfully Verified', '4'],
+        ['Successfully Verified', '3'],
         ['Successfully Verified - With phone number', '1'],
         ['Successfully Verified - With mailed code', '1'],
         ['Successfully Verified - In Person', '1'],
         ['Successfully Verified - Passed fraud review', '1'],
-        ['Blanket Proofing Rate (IDV Started to Successfully Verified)', '0.8'],
-        ['Intent Proofing Rate (Welcome Submitted to Successfully Verified)', '0.8'],
-        ['Actual Proofing Rate (Image Submitted to Successfully Verified)', '0.8'],
-        ['Industry Proofing Rate (Verified minus IDV Rejected)', '0.8'],
+        ['Blanket Proofing Rate (IDV Started to Successfully Verified)', '0.6'],
+        ['Intent Proofing Rate (Welcome Submitted to Successfully Verified)', '0.6'],
+        ['Actual Proofing Rate (Image Submitted to Successfully Verified)', '0.6'],
+        ['Industry Proofing Rate (Verified minus IDV Rejected)', '0.75'],
       ]
 
       aggregate_failures do
