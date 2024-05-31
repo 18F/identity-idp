353227a32db664e36a84eb50464aea5b9a634a31
Zach Margolis <zachary.margolis@gsa.gov>
2024-05-31 13:13:53 -0700

    Remove AccountReset::NotifyUserOfRequestCancellation
    - Move logic into AccountResetRequest#cancel!

--- app/models/account_reset_request.rb
+++ app/models/account_reset_request.rb
@@ -25,7 +25,8 @@ class AccountResetRequest < ApplicationRecord
 
   def cancel!(now: Time.zone.now)
     update(cancelled_at: now)
-    AccountReset::NotifyUserOfRequestCancellation.new(user).call
+    notify_user_via_email_of_account_reset_cancellation
+    notify_user_via_phone_of_account_reset_cancellation
   end
 
   def granted_token_valid?
@@ -36,5 +37,24 @@ class AccountResetRequest < ApplicationRecord
     granted_at.present? &&
       ((Time.zone.now - granted_at) >
        IdentityConfig.store.account_reset_token_valid_for_days.days)
+  end
+
+  private
+
+  def notify_user_via_email_of_account_reset_cancellation
+    user.confirmed_email_addresses.each do |email_address|
+      UserMailer.with(user: user, email_address: email_address).account_reset_cancel.
+        deliver_now_or_later
+    end
+  end
+
+  def notify_user_via_phone_of_account_reset_cancellation
+    MfaContext.new(user).phone_configurations.each do |phone_configuration|
+      phone = phone_configuration.phone
+      Telephony.send_account_reset_cancellation_notice(
+        to: phone,
+        country_code: Phonelib.parse(phone).country,
+      )
+    end
   end
 end
--- spec/models/account_reset_request_spec.rb
+++ spec/models/account_reset_request_spec.rb
@@ -135,14 +135,29 @@ RSpec.describe AccountResetRequest do
       expect(other_request.reload.cancelled_at).to be_nil
     end
 
-    it "sends an email to the user's confirmed email addresses" do
-      notify_user_of_cancellation = instance_double(AccountReset::NotifyUserOfRequestCancellation)
-      expect(AccountReset::NotifyUserOfRequestCancellation).to receive(:new).
-        with(user).
-        and_return(notify_user_of_cancellation)
-      expect(notify_user_of_cancellation).to receive(:call)
+    it "sends an email to the user's confirmed email addresses and phone numbers" do
+      email_address1 = user.email_addresses.first
+      email_address2 = create(:email_address, user: user)
+
+      phone_config1 = create(:phone_configuration, user: user)
+      phone_config2 = create(:phone_configuration, user: user)
+
+      expect(Telephony).to receive(:send_account_reset_cancellation_notice).
+        with(to: phone_config1.phone, country_code: 'US')
+      expect(Telephony).to receive(:send_account_reset_cancellation_notice).
+        with(to: phone_config2.phone, country_code: 'US')
 
       cancel!
+
+      expect_delivered_email_count(2)
+      expect_delivered_email(
+        to: [email_address1.email],
+        subject: t('user_mailer.account_reset_cancel.subject'),
+      )
+      expect_delivered_email(
+        to: [email_address2.email],
+        subject: t('user_mailer.account_reset_cancel.subject'),
+      )
     end
   end
 end
