5b996577b2c85908d21510e51fdb582d74c1b4aa
gina-yamada <gina.yamada@gsa.gov>
2024-06-04 17:05:48 -0600

    update logic to eval if eipp for ready to verify

--- app/controllers/idv/in_person/ready_to_verify_controller.rb
+++ app/controllers/idv/in_person/ready_to_verify_controller.rb
@@ -17,7 +17,7 @@ module Idv
       before_action :confirm_in_person_session
 
       def show
-        @is_eipp = sp_session.key?('vtr') && sp_session['vtr'].first&.include?('Pe')
+        @is_eipp = @resolved_authn_context_result.enhanced_ipp?
         analytics.idv_in_person_ready_to_verify_visit(**opt_in_analytics_properties)
         @presenter = ReadyToVerifyPresenter.new(enrollment: enrollment, is_eipp: @is_eipp)
       end
