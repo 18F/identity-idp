696f4acc7d10b7617faa8b716fea7fa3599494e3
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-06-07 13:48:28 -0400

    add socure webhook key

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -374,6 +374,7 @@ module IdentityConfig
     config.add(:skip_encryption_allowed_list, type: :json)
     config.add(:socure_document_endpoint, type: :string)
     config.add(:socure_id_secret_key, type: :string)
+    config.add(:socure_webhook_secret_key, type: :string)
     config.add(:sp_handoff_bounce_max_seconds, type: :integer)
     config.add(:sp_issuer_user_counts_report_configs, type: :json)
     config.add(:state_tracking_enabled, type: :boolean)
