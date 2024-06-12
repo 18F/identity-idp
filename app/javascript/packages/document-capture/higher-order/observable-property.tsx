fa1eda1e2aae7fdd71d5d2006f46b1688df04d1e
Zach Margolis <zachary.margolis@gsa.gov>
2024-06-12 15:36:10 -0700

    Typescript convert so we dont have to add any exceptions

--- app/javascript/packages/document-capture/higher-order/observable-property.tsx
+++ app/javascript/packages/document-capture/higher-order/observable-property.tsx
@@ -2,12 +2,12 @@
  * Defines a property on the given object, calling the change callback when that property is set to
  * a new value.
  *
- * @param {any} object Object on which to define property.
- * @param {string} property Property name to observe.
- * @param {(nextValue: any) => void} onChangeCallback Callback to trigger on change.
+ * @param object Object on which to define property.
+ * @param property Property name to observe.
+ * @param onChangeCallback Callback to trigger on change.
  */
-export function defineObservableProperty(object, property, onChangeCallback) {
-  let currentValue;
+export function defineObservableProperty(object: any, property: string, onChangeCallback: (nextValue: any) => void) {
+  let currentValue: any;
 
   Object.defineProperty(object, property, {
     get() {
@@ -25,10 +25,10 @@ export function defineObservableProperty(object, property, onChangeCallback) {
  * Removes an observable property by removing the defined getter/setter methods
  * and replaces the value with the most recent value.
  *
- * @param {any} object Object on which to remove defined property.
- * @param {string} property Property name to remove observer for
+ * @param object Object on which to remove defined property.
+ * @param property Property name to remove observer for
  */
-export function stopObservingProperty(object, property) {
+export function stopObservingProperty(object: any, property: string) {
   const currentValue = object[property];
 
   Object.defineProperty(object, property, { value: currentValue, writable: true });
