5cd0c8e35312794983d2b9aaa0f61215462f4281
Mitchell Henke <mitchell.henke@gsa.gov>
2024-06-13 11:09:51 -0500

    oscal

---
+++ spec/compliance.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'rspec_oscal_formatter'
+
+RSpec.configure do |config|
+  config.add_formatter RSpec::RSpecOscalFormatter::Formatter
+end
+
+RSpec.describe '800-63B' do
+  it 'confirms passwords are set to the appropriate minimum length',
+     control_id: 'ms-01', statement_id: 'ms-01_smt',
+     assessment_plan_uuid: 'da1ce957-e50e-42a0-936e-1a44f9d8a96c' do |assessment|
+    expect(Devise.password_length.min).to be >= 8
+  end
+end
