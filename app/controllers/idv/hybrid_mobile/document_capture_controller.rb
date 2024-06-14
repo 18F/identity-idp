02bd99ea037d7107c433bc0414cceed447f38073
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-06-14 15:15:01 -0400

    redirect to socure capture app

--- app/controllers/idv/hybrid_mobile/document_capture_controller.rb
+++ app/controllers/idv/hybrid_mobile/document_capture_controller.rb
@@ -42,6 +42,8 @@ module Idv
         @msg = doc_resp['msg']
         @reference_id = doc_resp.dig('referenceId')
         @qr_code = nil
+
+        redirect_to @url, allow_other_host: true if @url
       end
 
       def update
