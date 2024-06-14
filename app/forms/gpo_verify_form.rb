42a5a5f1348d31474b5618d66c7d17495fc5f700
gina-yamada <gina.yamada@gsa.gov>
2024-06-14 11:25:07 -0600

    Restore file, modify comment

--- app/forms/gpo_verify_form.rb
+++ app/forms/gpo_verify_form.rb
@@ -64,9 +64,7 @@ class GpoVerifyForm
   end
 
   def schedule_in_person_enrollment_and_deactivate_profile
-    UspsInPersonProofing::EnrollmentHelper.schedule_in_person_enrollment(
-      user, pii
-    )
+    UspsInPersonProofing::EnrollmentHelper.schedule_in_person_enrollment(user, pii)
     pending_profile&.deactivate_for_in_person_verification
   end
 
