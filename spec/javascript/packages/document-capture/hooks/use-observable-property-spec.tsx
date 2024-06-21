d5ee33cc50ced33bad3bb13258917db79a7c8c83
Zach Margolis <zachmargolis@users.noreply.github.com>
2024-06-21 09:11:55 -0700

    Convert observable property functions to a React Hook (#10838)
    * Force side effects to happen via act() in specs
    
    changelog: Internal, Source code, Update code to match React conventions

--- spec/javascript/packages/document-capture/hooks/use-observable-property-spec.tsx
+++ spec/javascript/packages/document-capture/hooks/use-observable-property-spec.tsx
@@ -1,14 +1,14 @@
 import sinon from 'sinon';
-import {
-  defineObservableProperty,
-  stopObservingProperty,
-} from '@18f/identity-document-capture/higher-order/observable-property';
+import { useObservableProperty } from '@18f/identity-document-capture/hooks/use-observable-property';
+import { renderHook } from '@testing-library/react-hooks';
 
-describe('document-capture/higher-order/observable-property', () => {
-  describe('defineObservableProperty', () => {
+describe('document-capture/hooks/use-observable-property', () => {
+  describe('useObservableProperty', () => {
     it('behaves like an object', () => {
       const object = {} as { key?: string };
-      defineObservableProperty(object, 'key', () => {});
+
+      renderHook(() => useObservableProperty(object, 'key', () => {}));
+
       object.key = 'value';
 
       expect(object.key).to.equal('value');
@@ -17,22 +17,21 @@ describe('document-capture/higher-order/observable-property', () => {
     it('calls the callback on changes, with the changed value', () => {
       const callback = sinon.spy();
       const object = {} as { key?: string };
-      defineObservableProperty(object, 'key', callback);
+
+      renderHook(() => useObservableProperty(object, 'key', callback));
       object.key = 'value';
 
       expect(callback).to.have.been.calledOnceWithExactly('value');
     });
-  });
 
-  describe('stopObservingProperty', () => {
-    it('removes the defined property and set the last value as a plain value', () => {
+    it('returns a cleanup function that removes the observer', () => {
       const object = {} as { key?: string };
       const callback = sinon.spy();
-      defineObservableProperty(object, 'key', callback);
+      const { unmount } = renderHook(() => useObservableProperty(object, 'key', callback));
 
       object.key = 'value';
 
-      stopObservingProperty(object, 'key');
+      unmount();
       expect(object.key).to.equal('value');
 
       object.key = 'second_value';
