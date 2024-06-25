6c0463a2107417a7b37f56a802ee63b5c70bac69
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-06-25 14:23:31 -0400

    Remove the resend functionality from the `RequestLetterController`
    In #10864 a new controller was introduced to handle resends. In #10865 the controller started being used. Once both of those changes are fully deployed this change can be merged to remove the resend funcitonality from the `RequestLetterController`.
    
    This change will make the `RequestLetterController` behave like the rest of the IdV-step controllers. It will no longer be accessible outside of the IdV workflow.
    
    [skip changelog]

--- app/controllers/idv/by_mail/request_letter_controller.rb
+++ app/controllers/idv/by_mail/request_letter_controller.rb
@@ -5,39 +5,25 @@ module Idv
     class RequestLetterController < ApplicationController
       include Idv::AvailabilityConcern
       include IdvStepConcern
-      skip_before_action :confirm_no_pending_gpo_profile
       include Idv::StepIndicatorConcern
       include VerifyByMailConcern
 
       before_action :confirm_mail_not_rate_limited
       before_action :confirm_step_allowed
-      before_action :confirm_profile_not_too_old
 
       def index
         @applicant = idv_session.applicant
-        @presenter = RequestLetterPresenter.new(current_user, url_options)
 
         Funnel::DocAuth::RegisterStep.new(current_user.id, current_sp&.issuer).
           call(:usps_address, :view, true)
-        analytics.idv_request_letter_visited(
-          letter_already_sent: @presenter.resend_requested?,
-        )
+        analytics.idv_request_letter_visited
       end
 
       def create
         clear_future_steps!
         update_tracking
         idv_session.address_verification_mechanism = :gpo
-
-        if resend_requested? && pii_locked?
-          redirect_to capture_password_url
-        elsif resend_requested?
-          resend_letter
-          flash[:success] = t('idv.messages.gpo.another_letter_on_the_way')
-          redirect_to idv_letter_enqueued_url
-        else
-          redirect_to idv_enter_password_url
-        end
+        redirect_to idv_enter_password_url
       end
 
       def self.step_info
@@ -55,57 +41,18 @@ module Idv
 
       private
 
-      def confirm_profile_not_too_old
-        redirect_to idv_path if gpo_verify_by_mail_policy.profile_too_old?
-      end
-
       def update_tracking
         Funnel::DocAuth::RegisterStep.new(current_user.id, current_sp&.issuer).
           call(:usps_letter_sent, :update, true)
 
-        log_letter_requested_analytics(resend: resend_requested?)
+        log_letter_requested_analytics(resend: false)
         create_user_event(:gpo_mail_sent, current_user)
 
         ProofingComponent.find_or_create_by(user: current_user).update(address_check: 'gpo_letter')
       end
 
-      def resend_requested?
-        current_user.gpo_verification_pending_profile?
-      end
-
       def confirm_mail_not_rate_limited
         redirect_to idv_enter_password_url if gpo_verify_by_mail_policy.rate_limited?
-      end
-
-      def resend_letter
-        log_letter_enqueued_analytics(resend: true)
-        confirmation_maker = confirmation_maker_perform
-        send_reminder
-        return unless FeatureManagement.reveal_gpo_code?
-        session[:last_gpo_confirmation_code] = confirmation_maker.otp
-      end
-
-      def confirmation_maker_perform
-        confirmation_maker = GpoConfirmationMaker.new(
-          pii: pii,
-          service_provider: current_sp,
-          profile: current_user.pending_profile,
-        )
-        confirmation_maker.perform
-        confirmation_maker
-      end
-
-      def pii
-        Pii::Cacher.new(current_user, user_session).
-          fetch(current_user.gpo_verification_pending_profile.id)
-      end
-
-      def send_reminder
-        current_user.send_email_to_all_addresses(:verify_by_mail_letter_requested)
-      end
-
-      def pii_locked?
-        !Pii::Cacher.new(current_user, user_session).exists_in_session?
       end
 
       def step_indicator_steps
