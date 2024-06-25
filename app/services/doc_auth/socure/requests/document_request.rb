ad2f60ae46388f488da208d41e174e518d5975bb
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-06-25 14:15:00 -0400

    rebase main - merge conflicts resolved

---
+++ app/services/doc_auth/socure/requests/document_request.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module DocAuth
+  module Socure
+    module Requests
+      class DocumentRequest < DocAuth::Socure::Request
+        attr_reader :verification_level, :document_type
+
+        def initialize(verification_level: '2', document_type: 'license')
+          @verification_level = verification_level
+          @document_type = document_type
+        end
+
+        private
+
+        def body
+          {
+            config: {
+              documentType: document_type,
+            },
+            verificationLevel: verification_level,
+          }.to_json
+        end
+
+        def handle_http_response(http_response)
+          byebug
+        end
+
+        def method
+          :post
+        end
+
+        def endpoint
+          IdentityConfig.store.socure_document_endpoint
+        end
+
+        def metric_name
+          'socure_doc_auth_docv'
+        end
+      end
+    end
+  end
+end
