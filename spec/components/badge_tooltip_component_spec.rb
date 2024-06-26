60c178fc9bef0c05002cc6bd74190cad98c28dcf
Andrew Duthie <andrew.duthie@gsa.gov>
2024-06-26 09:15:49 -0400

    Render tag options on lg-badge-tooltip wrapper

--- spec/components/badge_tooltip_component_spec.rb
+++ spec/components/badge_tooltip_component_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe BadgeTooltipComponent, type: :component do
   let(:tooltip_text) { 'Your identity has been verified.' }
   let(:content) { 'Verified' }
-  let(:tag_options) { { icon: :check_circle } }
+  let(:icon) { :check_circle }
+  let(:options) { { icon: :check_circle } }
 
   subject(:rendered) do
-    render_inline BadgeTooltipComponent.new(tooltip_text:, **tag_options).with_content(content)
+    render_inline BadgeTooltipComponent.new(tooltip_text:, icon:, **options).with_content(content)
   end
 
   it 'renders with tooltip text as an attribute' do
@@ -15,5 +16,13 @@ RSpec.describe BadgeTooltipComponent, type: :component do
 
   it 'renders badge with content as tooltip' do
     expect(rendered).to have_css('.lg-verification-badge.usa-tooltip', text: content)
+  end
+
+  context 'with additional tag options' do
+    let(:options) { super().merge(data: { foo: 'bar' }) }
+
+    it 'renders tag options on root wrapper element' do
+      expect(rendered).to have_css('lg-badge-tooltip[data-foo="bar"]')
+    end
   end
 end
