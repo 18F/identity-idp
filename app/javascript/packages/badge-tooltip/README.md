c5b54ec5089af368ffa37791e29c14e3e41ef478
Andrew Duthie <andrew.duthie@gsa.gov>
2024-06-26 09:13:18 -0400

    Assign tooltip classes internal to BadgeTooltipComponent

--- app/javascript/packages/badge-tooltip/README.md
+++ app/javascript/packages/badge-tooltip/README.md
@@ -14,10 +14,8 @@ import '@18f/identity-badge-tooltip/badge-tooltip-element';
 
 ```html
 <lg-badge-tooltip tooltip-text="Your identity has been verified.">
-  <span class="usa-tooltip">
-    <div class="lg-verification-badge usa-tooltip__trigger" data_position="top" type="badge" data-position="top" aria-describedby="tooltip-712979" tabindex="0">
-      <span class="icon usa-icon text-success" id="icon-b48becfc"></span>
-    </div>
-  </span>
+  <div class="lg-verification-badge usa-tooltip">
+    <span class="icon usa-icon text-success" id="icon-b48becfc"></span>
+  </div>
 </lg-badge-tooltip>
 ```
--- spec/components/badge_tooltip_component_spec.rb
+++ spec/components/badge_tooltip_component_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe BadgeTooltipComponent, type: :component do
   let(:tooltip_text) { 'Your identity has been verified.' }
+  let(:content) { 'Verified' }
   let(:tag_options) { { icon: :check_circle } }
 
   subject(:rendered) do
-    render_inline BadgeTooltipComponent.new(tooltip_text:, **tag_options).with_content(tooltip_text)
+    render_inline BadgeTooltipComponent.new(tooltip_text:, **tag_options).with_content(content)
   end
 
   it 'renders with tooltip text as an attribute' do
     expect(rendered).to have_css("lg-badge-tooltip[tooltip-text='#{tooltip_text}']")
+  end
+
+  it 'renders badge with content as tooltip' do
+    expect(rendered).to have_css('.lg-verification-badge.usa-tooltip', text: content)
   end
 end
