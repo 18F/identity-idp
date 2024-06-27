768d30b73b55f27d8ff33c32c0f8d0c2c406f826
Jack Cody <jack.cody@gsa.gov>
2024-06-27 18:50:01 -0500

    Add sp data as query params to redirect controller

--- app/controllers/redirect/redirect_controller.rb
+++ app/controllers/redirect/redirect_controller.rb
@@ -10,6 +10,25 @@ module Redirect
       params.permit(*PERMITTED_LOCATION_PARAMS).to_h.symbolize_keys
     end
 
+    def partner_query_params
+      @partner_query_params ||= begin
+        {
+          partner: current_sp&.agency&.name,
+          partner_div: current_sp&.integration&.name,
+        }.compact
+      end
+    end
+
+    def add_partner_query_params(url)
+      uri = Addressable::URI.parse(url)
+
+      if partner_query_params.any?
+        uri.query_values = (uri.query_values || {}).merge(partner_query_params)
+      end
+
+      uri.to_s
+    end
+
     def redirect_to_and_log(url, event: nil, tracker_method: analytics.method(:external_redirect))
       if event
         # Once all events have been moved to tracker methods, we can remove the event: param
@@ -17,7 +36,9 @@ module Redirect
       else
         tracker_method.call(redirect_url: url, **location_params)
       end
-      redirect_to(url, allow_other_host: true)
+
+      redirect_url = add_partner_query_params(url)
+      redirect_to(redirect_url, allow_other_host: true)
     end
   end
 end
