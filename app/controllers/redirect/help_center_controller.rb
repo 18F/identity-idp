8c667da225102d2e1db8e2efd16e5838e6445948
Jack Cody <jack.cody@gsa.gov>
2024-06-27 18:49:19 -0500

    Improve help center redirect

--- app/controllers/redirect/help_center_controller.rb
+++ app/controllers/redirect/help_center_controller.rb
@@ -5,26 +5,19 @@ module Redirect
     before_action :validate_help_center_article_params
 
     def show
-      redirect_to_and_log MarketingSite.help_center_article_url(**article_params_with_provider)
+      redirect_to_and_log MarketingSite.help_center_article_url(**article_params)
     end
 
     private
 
     def validate_help_center_article_params
       return if MarketingSite.valid_help_center_article?(**article_params)
-      redirect_to root_url
+      redirect_to_and_log MarketingSite.help_url
     end
 
     def article_params
       category, article, article_anchor = params.values_at(:category, :article, :article_anchor)
       { category:, article:, article_anchor: }
-    end
-
-    def article_params_with_provider
-      params = article_params
-      params[:partner] = current_sp&.agency&.name
-      params[:partner_division] = current_sp&.integration&.name
-      params
     end
   end
 end
