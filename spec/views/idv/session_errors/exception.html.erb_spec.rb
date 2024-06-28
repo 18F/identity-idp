6b0ade9bc12a2c63eca1447a58658d95d6f028b2
Jack Cody <jack.cody@gsa.gov>
2024-06-28 17:31:21 -0500

    Tests contact_redirect_url

--- spec/views/idv/session_errors/exception.html.erb_spec.rb
+++ spec/views/idv/session_errors/exception.html.erb_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe 'idv/session_errors/exception.html.erb' do
   it 'renders a list of troubleshooting options' do
     expect(rendered).to have_link(
       t('idv.troubleshooting.options.contact_support', app_name: APP_NAME),
-      href: MarketingSite.contact_url,
+      href: contact_redirect_url,
     )
   end
 
@@ -36,7 +36,7 @@ RSpec.describe 'idv/session_errors/exception.html.erb' do
     it 'renders a list of troubleshooting options' do
       expect(rendered).to have_link(
         t('idv.troubleshooting.options.contact_support', app_name: APP_NAME),
-        href: MarketingSite.contact_url,
+        href: contact_redirect_url,
       )
     end
   end
