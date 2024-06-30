63d3615a8228ef1b85623fce2220a6b92f18e16c
Will Birdsall <wbirdsall@fearless.tech>
2024-06-30 18:48:05 -0400

    Fix presenter err

--- app/presenters/idv/in_person/usps_states_territories_presenter.rb
+++ app/presenters/idv/in_person/usps_states_territories_presenter.rb
@@ -1,6 +1,8 @@
+include FormHelper
+
 class Idv::InPerson::UspsStatesTerritoriesPresenter
   # Filtered list to remove territories that cause errors in the USPS API
   def usps_states_territories
-    us_states_territories.reject { |_name, abbrev| %w[AA AE AP UM].include?(abbrev) }
+    FormHelper.us_states_territories.reject { |_name, abbrev| %w[AA AE AP UM].include?(abbrev) }
   end
 end
