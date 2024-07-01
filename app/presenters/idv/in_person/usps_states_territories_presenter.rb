63670e44532e9579fc2ce74694fa9f86c03f0a84
Will Birdsall <birdsallwill@protonmail.com>
2024-07-01 11:09:48 -0400

    Update app/presenters/idv/in_person/usps_states_territories_presenter.rb
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- app/presenters/idv/in_person/usps_states_territories_presenter.rb
+++ app/presenters/idv/in_person/usps_states_territories_presenter.rb
@@ -1,8 +1,8 @@
-include FormHelper
-
 class Idv::InPerson::UspsStatesTerritoriesPresenter
+  include FormHelper
+  
   # Filtered list to remove territories that cause errors in the USPS API
   def usps_states_territories
-    FormHelper.us_states_territories.reject { |_name, abbrev| %w[AA AE AP UM].include?(abbrev) }
+    us_states_territories.reject { |_name, abbrev| %w[AA AE AP UM].include?(abbrev) }
   end
 end
