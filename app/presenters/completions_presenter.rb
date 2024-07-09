df2d9c3cadf5a905773486130897b302ebeec70e
kevinsmaster5 <kevin.masters@gsa.gov>
2024-07-09 11:45:00 -0400

    changelog: Upcoming Features, Partner account, Select email to share with partner

--- app/presenters/completions_presenter.rb
+++ app/presenters/completions_presenter.rb
@@ -38,6 +38,10 @@ class CompletionsPresenter
     @requested_attributes = requested_attributes
     @ial2_requested = ial2_requested
     @completion_context = completion_context
+
+    current_user.email_addresses.each do |e|
+      puts e.email
+    end
   end
 
   def ial2_requested?
@@ -118,6 +122,10 @@ class CompletionsPresenter
     displayable_attribute_keys.index_with do |attribute_name|
       displayable_pii[attribute_name]
     end
+  end
+
+  def multiple_emails?
+    current_user.email_addresses.count > 1
   end
 
   private
