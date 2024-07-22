c62afd464153f7180e7bf295d752c7eeb48e2935
Malick Diarra <malick.diarra@gsa.gov>
2024-07-22 10:01:56 -0400

    change spec name

--- spec/controllers/sign_in_security_check_failed_controller_spec.rb
+++ spec/controllers/sign_in_security_check_failed_controller_spec.rb
@@ -7,13 +7,11 @@ RSpec.describe SignInSecurityCheckFailedController do
     stub_sign_in(user)
   end
 
-  it 'renders the show template' do
-    stub_analytics
-
-    get :show
-
-    expect(@analytics).to have_logged_event(:sign_in_security_check_failed_visited)
-
-    expect(response).to render_template :show
+  describe '#show' do
+    it 'logs an event' do
+      stub_analytics
+      get :show
+      expect(@analytics).to have_logged_event(:sign_in_security_check_failed_visited)
+    end
   end
 end
