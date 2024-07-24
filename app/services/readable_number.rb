2a1cad83efbb4cc38946a93f7540e0087202ce18
Andrew Duthie <andrew.duthie@gsa.gov>
2024-07-24 14:17:32 -0400

    Use condition for control flow
    See: https://github.com/18F/identity-idp/pull/10970#discussion_r1690212806
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/services/readable_number.rb
+++ app/services/readable_number.rb
@@ -9,13 +9,12 @@ class ReadableNumber
   # @param number [Integer] Number to transform to readable form
   # @return [String] Number in readable form
   def self.of(number)
-    readable_number = catch(:use_number) do
-      throw :use_number if FORCED_NUMERIC_LOCALES.include?(I18n.locale)
-      throw :use_number if number > MAX_NUMBER_TO_SPELL_OUT
-      throw :use_number if !NumbersAndWords::I18n.languages.include?(I18n.locale)
+    if FORCED_NUMERIC_LOCALES.include?(I18n.locale) ||
+       number > MAX_NUMBER_TO_SPELL_OUT ||
+       !NumbersAndWords::I18n.languages.include?(I18n.locale)
+      number.to_s
+    else
       number.to_words
     end
-
-    readable_number || number.to_s
   end
 end
