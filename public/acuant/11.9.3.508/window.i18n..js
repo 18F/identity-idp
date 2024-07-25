470a3317e2756c7f7656e6cd5d7df712267e163c
John Maxwell <john.maxwell@gsa.gov>
2024-07-25 13:14:44 -0400

    Saved copy of 508 directory

---
+++ public/acuant/11.9.3.508/window.i18n..js
@@ -0,0 +1,43 @@
+window.acuant_passive_liveness_language = "en";
+window.i18n = {
+    "en": {
+        FACE_NOT_FOUND: "FACE NOT FOUND",
+        TOO_MANY_FACES: "TOO MANY FACES",
+        FACE_ANGLE_TOO_LARGE: "FACE ANGLE TOO LARGE",
+        PROBABILITY_TOO_SMALL: "PROBABILITY TOO SMALL",
+        FACE_TOO_SMALL: "FACE TOO SMALL",
+        FACE_CLOSE_TO_BORDER: "TOO CLOSE TO THE FRAME",
+        CLOSE_TEXT: "close",
+        RETAKE_TEXT: "retake",
+        INTRO_TEXT: "",
+        SUBMIT_ALT: "",
+        CAPTURE_ALT: ""
+    }, 
+    "fr": {
+        FACE_NOT_FOUND : "VISAGE INTROUVABLE",
+        TOO_MANY_FACES : "TROP DE VISAGES",
+        FACE_ANGLE_TOO_LARGE : "ANGLE DU VISAGE TROP GRAND",
+        PROBABILITY_TOO_SMALL : "PROBABILITÉ TROP PETITE",
+        FACE_TOO_SMALL : "VISAGE TROP PETIT",
+        FACE_CLOSE_TO_BORDER : "TROP PRÈS DU CADRE",
+        CLOSE_TEXT : "fermer",
+        RETAKE_TEXT : "reprendre",
+        INTRO_TEXT: "",
+        SUBMIT_ALT: "",
+        CAPTURE_ALT: ""
+    }
+    , 
+    "es": {
+        FACE_NOT_FOUND: "CARA NO ENCONTRADA",
+        TOO_MANY_FACES: "DEMASIADAS CARAS",
+        FACE_ANGLE_TOO_LARGE: "ÁNGULO DE LA CARA DEMASIADO GRANDE",
+        PROBABILITY_TOO_SMALL: "PROBABILIDAD DEMASIADO PEQUEÑA",
+        FACE_TOO_SMALL: "CARA DEMASIADO PEQUEÑA",
+        FACE_CLOSE_TO_BORDER: "DEMASIADO CERCA DEL MARCO",
+        CLOSE_TEXT: "cerrar",
+        RETAKE_TEXT: "retomar",
+        INTRO_TEXT: "",
+        SUBMIT_ALT: "",
+        CAPTURE_ALT: ""
+    }
+}
