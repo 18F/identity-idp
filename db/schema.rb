738308dab98853c493ad9dcb638644b5269e0ce9
Malick Diarra <malick.diarra@gsa.gov>
2024-08-01 09:07:19 -0400

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_30_185830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "account_reset_requests", force: :cascade do |t|
