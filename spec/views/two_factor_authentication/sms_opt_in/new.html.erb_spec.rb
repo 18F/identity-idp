4a8d64d9e2a064d6dc106bad7acfea4fea81e200
Jack Cody <jack.cody@gsa.gov>
2024-08-07 11:00:57 -0500

    Update specs
    Changelog: User-Facing Improvements, Authentication, Update authentication method links

--- spec/views/two_factor_authentication/sms_opt_in/new.html.erb_spec.rb
+++ spec/views/two_factor_authentication/sms_opt_in/new.html.erb_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'two_factor_authentication/sms_opt_in/new.html.erb' do
       t('two_factor_authentication.learn_more'),
       href: help_center_redirect_path(
         category: 'get-started',
-        article: 'authentication-options',
+        article: 'authentication-methods',
         flow: :two_factor_authentication,
         step: :sms_opt_in,
       ),
