12e65ebfa0df25be124d0959f39d8bf3230be11f
Andrew Duthie <andrew.duthie@gsa.gov>
2024-08-07 09:48:35 -0400

    Create reusable node_id method with inline documentation

--- spec/support/features/browser_protocol_helper.rb
+++ spec/support/features/browser_protocol_helper.rb
@@ -8,15 +8,26 @@ module BrowserProtocolHelper
   end
 
   def accessibility_tree(element)
-    # See: https://github.com/SeleniumHQ/selenium/blob/trunk/rb/lib/selenium/webdriver/common/element.rb
-    node_id = element.native.ref[1].split('.').last.to_i
     send_bridge_command('Accessibility.enable')
-    ax_tree_response = send_bridge_command(
+    send_bridge_command(
       'Accessibility.getPartialAXTree',
-      backendNodeId: node_id,
+      backendNodeId: node_id(element),
       fetchRelatives: false,
-    )
-    ax_tree_response['value']['nodes'].first
+    )['value']['nodes'].first
+  end
+
+  def node_id(element)
+    # Selenium internally tracks the ID for an element, and exposes it on a tuple `ref` method as
+    # the second member. The Selenium ID includes the Chromium node ID as the last portion of a
+    # dot-delimited string.
+    #
+    # Example `ref`:
+    # [:element, "f.EEC554C38DC5E6172B08F9C59A572EEA.d.6CDD556B3BF6E9A7FDAEA1F6CBD9EDF2.e.77"]
+    #
+    # In the example above, the Chromium node ID is `77`.
+    #
+    # See: https://github.com/SeleniumHQ/selenium/blob/trunk/rb/lib/selenium/webdriver/common/element.rb
+    element.native.ref[1].split('.').last.to_i
   end
 
   def send_bridge_command(command, params = {})
