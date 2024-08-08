b969e02353fd1f4dcc75b41b174543f28c9a22c4
Andrew Duthie <andrew.duthie@gsa.gov>
2024-08-08 14:39:31 -0400

    Revert "Remove aamva ignored column"
    This reverts commit 5e58c6fdf400b6b00ac52229ab5238c5bc307cfa.

--- app/models/doc_auth_log.rb
+++ app/models/doc_auth_log.rb
@@ -8,4 +8,8 @@ class DocAuthLog < ApplicationRecord
              foreign_key: 'issuer',
              primary_key: 'issuer'
   # rubocop:enable Rails/InverseOf
+
+  self.ignored_columns = [
+    :aamva,
+  ]
 end
