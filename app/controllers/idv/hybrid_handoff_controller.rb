6d873723072016d1cc96ebbeb38594452b646ead
Keith WIlliams <keithwilliams@navapbc.com>
2024-08-08 15:56:08 -0400

    feat: remove skip_doc_auth from hybrid handoff

--- app/controllers/idv/hybrid_handoff_controller.rb
+++ app/controllers/idv/hybrid_handoff_controller.rb
@@ -43,16 +43,7 @@ module Idv
       end
     end
 
-    def self.selected_remote(idv_session:)
-      if IdentityConfig.store.in_person_proofing_opt_in_enabled &&
-         IdentityConfig.store.in_person_proofing_enabled &&
-         idv_session.service_provider&.in_person_proofing_enabled
-        idv_session.skip_doc_auth == false
-      else
-        idv_session.skip_doc_auth.nil? ||
-          idv_session.skip_doc_auth == false
-      end
-    end
+    
 
     def self.step_info
       Idv::StepInfo.new(
@@ -61,7 +52,6 @@ module Idv
         next_steps: [:link_sent, :document_capture, :socure_document_capture],
         preconditions: ->(idv_session:, user:) {
                          idv_session.idv_consent_given? &&
-                           (self.selected_remote(idv_session: idv_session) || # from opt-in screen
                              # back from ipp doc capture screen
                              idv_session.skip_doc_auth_from_handoff)
                        },
