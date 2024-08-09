29fb27372534889de7ef4b998c7c4dae577dc576
Malick Diarra <malick.diarra@gsa.gov>
2024-08-09 11:29:55 -0400

    update schema

--- db/primary_migrate/20240809152808_create_federal_email_domain.rb
+++ db/primary_migrate/20240809152808_create_federal_email_domain.rb
@@ -1,4 +1,4 @@
-class CreateFederalEmailDomainsTable < ActiveRecord::Migration[7.1]
+class CreateFederalEmailDomain < ActiveRecord::Migration[7.1]
   def change
     create_table :federal_email_domains do |t|
       t.citext :name, null: false
