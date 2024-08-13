ae3165977794b5a9137e05f2b289ca377b8db99f
Mitchell Henke <mitchell.henke@gsa.gov>
2024-08-13 17:15:12 -0500

    add test to check for redundant YAML configurations
    changelog: Internal, Configuration, Add redundancy checks to YAML configuration file

--- spec/lib/identity_config_spec.rb
+++ spec/lib/identity_config_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe IdentityConfig do
+  let(:default_yaml_config) do
+    YAML.safe_load_file(Rails.root.join('config', 'application.yml.default')).freeze
+  end
+
   describe '.key_types' do
     subject(:key_types) { Identity::Hostdata.config_builder.key_types }
 
@@ -71,6 +75,55 @@ RSpec.describe IdentityConfig do
   describe '.unused_keys' do
     it 'does not have any unused keys' do
       expect(Identity::Hostdata.config_builder.unused_keys).to be_empty
+    end
+  end
+
+  describe 'redundant configuration' do
+    it 'does not use the default value in development' do
+      aggregate_failures do
+        default_yaml_config['development'].each do |key, value|
+          next unless default_yaml_config.key?(key)
+          expect(value).to_not eq(default_yaml_config[key]),
+                               "#{key} in development uses the default value"
+        end
+      end
+    end
+
+    it 'does not use the default value in production' do
+      aggregate_failures do
+        default_yaml_config['production'].each do |key, value|
+          next unless default_yaml_config.key?(key)
+          expect(value).to_not eq(default_yaml_config[key]),
+                               "#{key} in production uses the default value"
+        end
+      end
+    end
+
+    it 'does not use the default value in test' do
+      aggregate_failures do
+        default_yaml_config['test'].each do |key, value|
+          next unless default_yaml_config.key?(key)
+          expect(value).to_not eq(default_yaml_config[key]), "#{key} in test uses the default value"
+        end
+      end
+    end
+
+    it 'does not define an identical value in development, production, and test' do
+      keys = Identity::Hostdata.config_builder.key_types.map { |key, _type| key.to_s }
+      aggregate_failures do
+        keys.each do |key|
+          expect(
+            !default_yaml_config.key?(key) && (
+              default_yaml_config['production'].key?(key) &&
+              default_yaml_config['production'][key] == default_yaml_config['test'][key] &&
+              default_yaml_config['test'][key] == default_yaml_config['development'][key]
+            ),
+          ).to(
+            eq(false),
+            "#{key} uses the same value in development, production and test instead of a default",
+          )
+        end
+      end
     end
   end
 end
