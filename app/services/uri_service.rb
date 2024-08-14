e755a614e6dc793d0f03f2e59757ec70d9600cd7
jc-gsa <104452882+jc-gsa@users.noreply.github.com>
2024-08-14 11:38:46 -0500

    LG-13750: Share referrer info with identity-site (#10889)
    * Share referrer info (agency, integration) with identity-site 
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Remove function
    
    * Handle nil params to add in UriService
    
    * Lint style
    
    * changelog: Internal, Marketing Site, Add metadata to redirect URLs
    
    * empty commit
    
    * Add spec for empty params
    
    * Update help_center_controller_spec.rb
    
    * empty commit
    
    * Update param description
    
    ---------
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- app/services/uri_service.rb
+++ app/services/uri_service.rb
@@ -8,14 +8,14 @@ module UriService
   end
 
   # @param [#to_s] original_uri
-  # @param [Hash] params_to_add
+  # @param [Hash, nil] params_to_add
   # @return [URI, nil]
   def self.add_params(original_uri, params_to_add)
     return if original_uri.blank?
 
     URI(original_uri).tap do |uri|
-      query = params(uri)
-      uri.query = query.merge(params_to_add).to_query
+      query = params(uri).merge(params_to_add || {})
+      uri.query = query.empty? ? nil : query.to_query
     end.to_s
   rescue URI::BadURIError, URI::InvalidURIError
     nil
