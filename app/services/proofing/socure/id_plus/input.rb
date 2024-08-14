f5619636514fbba0ea24de77134321e4a6b3f4c9
Matt Hinz <matt.hinz@gsa.gov>
2024-08-14 15:53:58 -0700

    Socure KYC Proofer
    - Basic Proofer implementation for Socure KYC.
    - Request / Response classes, error handling, etc.
    
    changelog: Upcoming Features, Identity verification, Implement proofer for Socure KYC

---
+++ app/services/proofing/socure/id_plus/input.rb
@@ -0,0 +1,21 @@
+module Proofing
+  module Socure
+    module IdPlus
+      Input = RedactedStruct.new(
+        :address1,
+        :address2,
+        :city,
+        :dob,
+        :first_name,
+        :last_name,
+        :middle_name,
+        :state,
+        :zipcode,
+        :phone,
+        :email,
+        :ssn,
+        keyword_init: true,
+      ).freeze
+    end
+  end
+end
---
+++ spec/services/proofing/socure/id_plus/request_spec.rb
@@ -0,0 +1,243 @@
+require 'rails_helper'
+
+RSpec.describe Proofing::Socure::IdPlus::Request do
+  let(:config) do
+    Proofing::Socure::IdPlus::Config.new(
+      api_key:,
+      base_url:,
+      timeout:,
+    )
+  end
+
+  let(:api_key) { 'super-$ecret' }
+
+  let(:base_url) { 'https://example.org/' }
+
+  let(:timeout) { 5 }
+
+  let(:user) { build(:user) }
+
+  let(:input) do
+    Proofing::Socure::IdPlus::Input.new(
+      email: user.email,
+      **Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE.slice(
+        *Proofing::Socure::IdPlus::Input.members,
+      ),
+    )
+  end
+
+  subject do
+    described_class.new(config:, input:)
+  end
+
+  describe '#body' do
+    it 'looks right' do
+      freeze_time do
+        expect(JSON.parse(subject.body, symbolize_names: true)).to eql(
+          {
+            modules: [
+              'kyc',
+            ],
+            firstName: 'FAKEY',
+            surName: 'MCFAKERSON',
+            dob: '1938-10-06',
+            physicalAddress: '1 FAKE RD',
+            physicalAddress2: nil,
+            city: 'GREAT FALLS',
+            state: 'MT',
+            zip: '59010-1234',
+            country: 'US',
+            nationalId: Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE[:ssn],
+            countryOfOrigin: 'US',
+
+            email: user.email,
+            mobileNumber: Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE[:phone],
+
+            userConsent: true,
+
+            # XXX: This should be set to the time the user submitted agreement
+            consentTimestamp: Time.zone.now.to_date.to_s,
+          },
+        )
+      end
+    end
+  end
+
+  describe '#headers' do
+    it 'look right' do
+      expect(subject.headers).to eql(
+        'Content-Type' => 'application/json',
+        'Authorization' => "SocureApiKey #{api_key}",
+      )
+    end
+  end
+
+  describe '#send_request' do
+    before do
+      stub_request(:post, 'https://example.org/api/3.0/EmailAuthScore').
+        to_return(
+          headers: {
+            'Content-Type' => 'application/json',
+          },
+          body: JSON.generate(
+            {
+              referenceId: 'a-big-unique-reference-id',
+              kyc: {
+                reasonCodes: [
+                  'I100',
+                  'R200',
+                ],
+                fieldValidations: {
+                  firstName: 0.99,
+                  surName: 0.99,
+                  streetAddress: 0.99,
+                  city: 0.99,
+                  state: 0.99,
+                  zip: 0.99,
+                  mobileNumber: 0.99,
+                  dob: 0.99,
+                  ssn: 0.99,
+                },
+              },
+            },
+          ),
+        )
+    end
+
+    it 'includes API key' do
+      subject.send_request
+
+      expect(WebMock).to have_requested(
+        :post, 'https://example.org/api/3.0/EmailAuthScore'
+      ).with(headers: { 'Authorization' => "SocureApiKey #{api_key}" })
+    end
+
+    it 'includes JSON serialized body' do
+      subject.send_request
+
+      expect(WebMock).to have_requested(
+        :post, 'https://example.org/api/3.0/EmailAuthScore'
+      ).with(body: subject.body)
+    end
+
+    context 'when service returns HTTP 200 response' do
+      it 'method returns a Proofing::Socure::IdPlus::Response' do
+        res = subject.send_request
+        expect(res).to be_a(Proofing::Socure::IdPlus::Response)
+      end
+
+      it 'response has kyc data' do
+        res = subject.send_request
+        expect(res.kyc_field_validations).to be
+        expect(res.kyc_reason_codes).to be
+      end
+    end
+
+    context 'when service returns an HTTP 400 response' do
+      before do
+        stub_request(:post, 'https://example.org/api/3.0/EmailAuthScore').
+          to_return(
+            status: 400,
+            headers: {
+              'Content-Type' => 'application/json',
+            },
+            body: JSON.generate(
+              {
+                status: 'Error',
+                referenceId: 'a-big-unique-reference-id',
+                data: {
+                  parameters: ['firstName'],
+                },
+                msg: 'Request-specific error message goes here',
+              },
+            ),
+          )
+      end
+
+      it 'raises RequestError' do
+        expect do
+          subject.send_request
+        end.to raise_error(
+          Proofing::Socure::IdPlus::RequestError,
+          'Request-specific error message goes here (400)',
+        )
+      end
+
+      it 'includes reference_id on RequestError' do
+        expect do
+          subject.send_request
+        end.to raise_error(
+          Proofing::Socure::IdPlus::RequestError,
+        ) do |err|
+          expect(err.reference_id).to eql('a-big-unique-reference-id')
+        end
+      end
+    end
+
+    context 'when service returns an HTTP 401 reponse' do
+      before do
+        stub_request(:post, 'https://example.org/api/3.0/EmailAuthScore').
+          to_return(
+            status: 401,
+            headers: {
+              'Content-Type' => 'application/json',
+            },
+            body: JSON.generate(
+              {
+                status: 'Error',
+                referenceId: 'a-big-unique-reference-id',
+                msg: 'Request-specific error message goes here',
+              },
+            ),
+          )
+      end
+
+      it 'raises RequestError' do
+        expect do
+          subject.send_request
+        end.to raise_error(
+          Proofing::Socure::IdPlus::RequestError,
+          'Request-specific error message goes here (401)',
+        )
+      end
+    end
+
+    context 'when service returns weird HTTP 500 response' do
+      before do
+        stub_request(:post, 'https://example.org/api/3.0/EmailAuthScore').
+          to_return(
+            status: 500,
+            body: 'It works!',
+          )
+      end
+
+      it 'raises RequestError' do
+        expect do
+          subject.send_request
+        end.to raise_error(Proofing::Socure::IdPlus::RequestError)
+      end
+    end
+
+    context 'when request times out' do
+      before do
+        stub_request(:post, 'https://example.org/api/3.0/EmailAuthScore').
+          to_timeout
+      end
+
+      it 'raises a ProofingTimeoutError' do
+        expect { subject.send_request }.to raise_error Proofing::TimeoutError
+      end
+    end
+
+    context 'when connection is reset' do
+      before do
+        stub_request(:post, 'https://example.org/api/3.0/EmailAuthScore').
+          to_raise(Errno::ECONNRESET)
+      end
+
+      it 'raises a RequestError' do
+        expect { subject.send_request }.to raise_error Proofing::Socure::IdPlus::RequestError
+      end
+    end
+  end
+end
