f7309101edf027027da84ed900084c60f223fbe0
Mitchell Henke <mitchell.henke@gsa.gov>
2024-08-14 16:41:12 +0000

    Log unused_identity_config_keys event as JSON (#11080)
    changelog: Internal, Logging, Log unused_identity_config_keys event as JSON

--- config/initializers/unused_identity_config_keys.rb
+++ config/initializers/unused_identity_config_keys.rb
@@ -3,6 +3,6 @@
 if Identity::Hostdata.config_builder.unused_keys.present?
   Rails.logger.warn(
     { name: 'unused_identity_config_keys',
-      keys: Identity::Hostdata.config_builder.unused_keys },
+      keys: Identity::Hostdata.config_builder.unused_keys }.to_json,
   )
 end
