e8e327b5595f8e5cf440008170271f1abb876cfb
ashukla <abir.shukla@gsa.gov>
2024-08-15 12:47:14 -0500

    Added back Unknown error for selnium webdriver

--- spec/support/features/interaction_helper.rb
+++ spec/support/features/interaction_helper.rb
@@ -11,8 +11,8 @@ module InteractionHelper
   rescue Selenium::WebDriver::Error::StaleElementReferenceError
     # A StaleElementReferenceError means that the context the element
     # was in has disappeared, which means the element is gone too.
-  rescue Selenium::WebDriver::Error::GeneralError => e
-    # We sometimes see "GeneralError" with an error message that is similar to a
+  rescue Selenium::WebDriver::Error::UnknownError => e
+    # We sometimes see "UnknownError" with an error message that is similar to a
     # StaleElementReferenceError, but have not been able to resolve it and are ignoring it
     # for now.
     raise e if !e.message.include?('Node with given id does not belong to the document')
