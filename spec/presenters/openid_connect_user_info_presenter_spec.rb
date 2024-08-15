3a324eacad3127b392687deb9988e043c03acc09
kevinsmaster5 <kevin.masters@gsa.gov>
2024-08-15 10:57:33 -0400

    add test for deleted email

--- spec/presenters/openid_connect_user_info_presenter_spec.rb
+++ spec/presenters/openid_connect_user_info_presenter_spec.rb
@@ -368,5 +368,28 @@ RSpec.describe OpenidConnectUserInfoPresenter do
         end
       end
     end
+
+    context 'with a deleted email' do
+      let(:identity) do
+        build(
+          :service_provider_identity,
+          rails_session_id: rails_session_id,
+          user: create(:user, :fully_registered, :with_multiple_emails),
+          scope: scope,
+        )
+      end
+
+      before do
+        identity.email_address_id = identity.user.email_addresses.first.id
+        identity.user.email_addresses.first.delete
+      end
+
+      it 'defers to user alternate email' do
+        expect(identity.user.reload.email_addresses.first.id).
+          to_not eq(identity.email_address_id)
+        expect(identity.user.reload.email_addresses.count).to be 1
+        expect(user_info[:email]).to eq(identity.user.email_addresses.last.email)
+      end
+    end
   end
 end
