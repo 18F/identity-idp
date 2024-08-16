d4f442e19bb4fa2c8b4ccab2d67eacef81c69883
Andrew Duthie <andrew.duthie@gsa.gov>
2024-08-16 16:28:13 -0400

    Remove monkeypatch file reference

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,6 @@
 
 # Be sure to restart your server when you modify this file.
 
-require 'extensions/propshaft/asset'
-
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
