272f96945b1511701bd80c3de31156fd8a36dc42
Andrew Duthie <andrew.duthie@gsa.gov>
2024-08-16 15:09:35 -0400

    Use conventional capitalization
    See: https://github.com/18F/identity-idp/blob/main/docs/frontend.md#naming-conventions

--- app/javascript/packages/analytics/digital-analytics-program.spec.ts
+++ app/javascript/packages/analytics/digital-analytics-program.spec.ts
@@ -12,11 +12,11 @@ describe('digital analytics program', () => {
     });
 
     // Reference: https://github.com/nodejs/node/issues/30682
-    const toDataUrl = (source: string) =>
+    const toDataURL = (source: string) =>
       new URL(`data:text/javascript,${encodeURIComponent(source)}`);
     const url = pathToFileURL(join(__dirname, './digital-analytics-program.js'));
     const code = `await import(${JSON.stringify(url)});`;
-    new Worker(toDataUrl(code)).on('error', (error) => {
+    new Worker(toDataURL(code)).on('error', (error) => {
       expect(error).not.to.be.instanceOf(SyntaxError);
       resolve();
     });
