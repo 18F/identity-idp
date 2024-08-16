40f1a3372928ca34e24948738dbdaf438a1c6811
John Maxwell <john.maxwell@gsa.gov>
2024-08-16 17:06:50 -0400

    Fixed controller spec.

--- spec/requests/socure_webhook_spec.rb
+++ spec/requests/socure_webhook_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe SocureWebhookController do
-  describe 'POST /api/webhooks/socure/event' do
+  describe 'POST #create' do
     it 'returns OK' do
+      binding.pry
       post :create
       expect(response).to have_http_status(:ok)
     end
