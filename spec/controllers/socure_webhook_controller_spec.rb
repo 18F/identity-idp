7fc550ff388facd18e523146d8fcaa04f7138976
John Maxwell <john.maxwell@gsa.gov>
2024-08-19 10:49:54 -0400

    Deleted request spec.
    We're covered by a controller spec here; the request spec is redundant.

--- spec/controllers/socure_webhook_controller_spec.rb
+++ spec/controllers/socure_webhook_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe SocureWebhookController do
-  describe 'POST #create' do
+  describe 'POST /api/webhooks/socure/event' do
     it 'returns OK' do
       post :create
       expect(response).to have_http_status(:ok)
