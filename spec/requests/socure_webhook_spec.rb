5aaf77edf631dd41d6eaaa818fa3821ca62082b8
John Maxwell <john.maxwell@gsa.gov>
2024-08-19 10:34:50 -0400

    Update spec/requests/socure_webhook_spec.rb
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- spec/requests/socure_webhook_spec.rb
+++ spec/requests/socure_webhook_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe SocureWebhookController do
   describe 'POST #create' do
     it 'returns OK' do
-      binding.pry
       post :create
       expect(response).to have_http_status(:ok)
     end
