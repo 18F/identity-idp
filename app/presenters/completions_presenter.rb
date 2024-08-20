97aba1faaf484bae031611e9caa442c9e3ce5244
kevinsmaster5 <kevin.masters@gsa.gov>
2024-08-20 10:51:14 -0400

    revise conditional to detect multiple confirmed email addresses

--- app/presenters/completions_presenter.rb
+++ app/presenters/completions_presenter.rb
@@ -106,7 +106,7 @@ class CompletionsPresenter
   end
 
   def multiple_emails?
-    current_user.confirmed_email_addresses.any?
+    current_user.confirmed_email_addresses.count > 1
   end
 
   private
