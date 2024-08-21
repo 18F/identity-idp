6a8077b301cb02742e0ebf655dd8a0ea9e64e312
Andrew Duthie <andrew.duthie@gsa.gov>
2024-08-21 10:53:45 -0400

    Avoid redundant enrollment creation with pending IPP profile

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -215,8 +215,7 @@ FactoryBot.define do
 
     trait :with_pending_in_person_enrollment do
       after :build do |user|
-        profile = create(:profile, :with_pii, :in_person_verification_pending, user: user)
-        create(:in_person_enrollment, :pending, user: user, profile: profile)
+        create(:profile, :with_pii, :in_person_verification_pending, user:)
       end
     end
 
