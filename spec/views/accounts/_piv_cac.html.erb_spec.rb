0b0872caf11d0bbb3035450e66a7c859b88b44db
Andrew Duthie <andrew.duthie@gsa.gov>
2024-09-03 15:24:04 -0400

    Use valid listitem role

--- spec/views/accounts/_piv_cac.html.erb_spec.rb
+++ spec/views/accounts/_piv_cac.html.erb_spec.rb
@@ -24,6 +24,6 @@ RSpec.describe 'accounts/_piv_cac.html.erb' do
   end
 
   it 'renders a list of piv cac configurations' do
-    expect(rendered).to have_selector('[role="list"] [role="list-item"]', count: 2)
+    expect(rendered).to have_selector('[role="list"] [role="listitem"]', count: 2)
   end
 end
