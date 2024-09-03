2c06ba255447a2686682a236a3b4317980d88b38
Andrew Duthie <andrew.duthie@gsa.gov>
2024-09-03 07:44:02 -0400

    Run Axe scan against all WCAG 2.0-2.2 A & AA
    changelog: Internal, Accessibility Tests, Run Axe scan against all WCAG 2.0-2.2 A & AA

--- spec/support/matchers/accessibility.rb
+++ spec/support/matchers/accessibility.rb
@@ -337,7 +337,15 @@ class AccessibleName
 end
 
 def expect_page_to_have_no_accessibility_violations(page, validate_markup: true)
-  expect(page).to be_axe_clean.according_to(:wcag22aa, :"best-practice")
+  expect(page).to be_axe_clean.according_to(
+    :wcag2a,
+    :wcag2aa,
+    :wcag21a,
+    :wcag21aa,
+    :wcag22a,
+    :wcag22aa,
+    :"best-practice",
+  )
   expect(page).to have_unique_ids
   expect(page).to have_valid_idrefs
   expect(page).to label_required_fields
