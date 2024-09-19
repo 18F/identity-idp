b28b574e839f3be0dd83a2461dab8a2520304ec0
ashukla <abir.shukla@gsa.gov>
2024-09-19 08:55:27 -0500

    Resolving PR comments

--- app/javascript/packages/document-capture/components/selfie-step.tsx
+++ app/javascript/packages/document-capture/components/selfie-step.tsx
@@ -29,7 +29,6 @@ export function SelfieCaptureStep({
   const { t } = useI18n();
   return (
     <>
-      <hr className="margin-y-5" />
       <h1>{t('doc_auth.headings.document_capture_subheader_selfie')}</h1>
       <p>{t('doc_auth.info.selfie_capture_content')}</p>
       <TipList
@@ -60,10 +59,8 @@ export default function SelfieStep({
   onError = () => {},
   registerField = () => undefined,
 }: FormStepComponentProps<DocumentsAndSelfieStepValue>) {
-  const { t } = useI18n();
   const { isLastStep } = useContext(FormStepsContext);
   const { flowPath } = useContext(UploadContext);
-  const pageHeaderText = t('doc_auth.headings.document_capture_with_selfie');
 
   const defaultSideProps: DefaultSideProps = {
     registerField,
@@ -74,12 +71,7 @@ export default function SelfieStep({
   return (
     <>
       {flowPath === 'hybrid' && <HybridDocCaptureWarning className="margin-bottom-4" />}
-      <PageHeading>{pageHeaderText}</PageHeading>
-      <SelfieCaptureStep
-        defaultSideProps={defaultSideProps}
-        selfieValue={value.selfie}
-        isReviewStep={false}
-      />
+      <SelfieCaptureStep defaultSideProps={defaultSideProps} selfieValue={value.selfie} isReviewStep={false}/>
       {isLastStep ? <FormStepsButton.Submit /> : <FormStepsButton.Continue />}
       <Cancel />
     </>
