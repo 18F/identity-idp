5f0d3b453ce39bf1cdeefefe0971bb0983272df8
Aaron Nagucki <aaron.nagucki@gmail.com>
2024-09-23 12:56:23 -0500

    update for friendly name

--- app/jobs/reports/idv_legacy_conversion_supplement_report.rb
+++ app/jobs/reports/idv_legacy_conversion_supplement_report.rb
@@ -13,24 +13,27 @@ module Reports
 
     # @return [String] CSV report
     def build_csv
-      results = Agreements::IaaOrder.joins(:integrations, :iaa_gtc).joins(
-        <<-SQL,
-          INNER JOIN (
-            SELECT DISTINCT ON (user_id) *
-            FROM sp_upgraded_biometric_profiles
-          ) sp ON sp.issuer = integrations.issuer
-        SQL
-      ).select(
-        <<-SQL,
-          iaa_orders.*,
-          iaa_gtcs.gtc_number AS gtc_number,
-          sp.issuer AS issuer,
-          DATE_TRUNC('month', sp.upgraded_at) AS year_month,
-          count(distinct sp.user_id) AS user_count
-        SQL
-      ).where(
-        'sp.upgraded_at BETWEEN iaa_orders.start_date AND iaa_orders.end_date',
-      ).group('iaa_orders.id, sp.issuer, year_month, iaa_gtcs.gtc_number').
+      results = Agreements::IaaOrder.joins(integrations: :service_provider).joins(:iaa_gtc).
+        joins(
+          <<-SQL,
+            INNER JOIN (
+              SELECT DISTINCT ON (user_id) *
+              FROM sp_upgraded_biometric_profiles
+            ) sp ON sp.issuer = integrations.issuer
+          SQL
+        ).select(
+          <<-SQL,
+            iaa_orders.*,
+            iaa_gtcs.gtc_number AS gtc_number,
+            sp.issuer AS issuer,
+            service_providers.friendly_name AS friendly_name,
+            DATE_TRUNC('month', sp.upgraded_at) AS year_month,
+            count(distinct sp.user_id) AS user_count
+          SQL
+        ).where(
+          'sp.upgraded_at BETWEEN iaa_orders.start_date AND iaa_orders.end_date',
+        ).group('iaa_orders.id, sp.issuer, year_month, iaa_gtcs.gtc_number,
+          service_providers.friendly_name').
         order('iaa_orders.id, year_month')
 
       CSV.generate do |csv|
@@ -39,6 +42,7 @@ module Reports
           'iaa_start_date',
           'iaa_end_date',
           'issuer',
+          'friendly_name',
           'year_month',
           'year_month_readable',
           'user_count',
@@ -50,6 +54,7 @@ module Reports
             iaa.start_date,
             iaa.end_date,
             iaa.issuer,
+            iaa.friendly_name,
             iaa.year_month.strftime('%Y%m'),
             iaa.year_month.strftime('%B %Y'),
             iaa.user_count,
