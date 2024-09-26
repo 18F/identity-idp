afb95c5970780531120616e7ef0ce19801002981
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2024-09-26 16:38:59 -0400

    update piv_cac_setup specs

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -5041,10 +5041,12 @@ module AnalyticsEvents
   # @param [Integer] enabled_mfa_methods_count Number of enabled MFA methods on the account
   # @param [Boolean] in_account_creation_flow whether user is going through creation flow
   # @param ['piv_cac'] method_name Authentication method added
+  # @param [Integer] mfa_attempts number of MFA setup attempts
   def multi_factor_auth_added_piv_cac(
     enabled_mfa_methods_count:,
     in_account_creation_flow:,
     method_name: :piv_cac,
+    mfa_attempts: nil,
     **extra
   )
     track_event(
@@ -5052,6 +5054,7 @@ module AnalyticsEvents
       method_name:,
       enabled_mfa_methods_count:,
       in_account_creation_flow:,
+      mfa_attempts:,
       **extra,
     )
   end
@@ -5993,11 +5996,18 @@ module AnalyticsEvents
   # Tracks when user's piv cac setup
   # @param [Boolean] in_account_creation_flow Whether user is going through account creation
   # @param [Integer] enabled_mfa_methods_count Number of enabled MFA methods on the account
-  def piv_cac_setup_visited(in_account_creation_flow:, enabled_mfa_methods_count: nil, **extra)
+  # @param [Integer] mfa_attempts number of MFA setup attempts
+  def piv_cac_setup_visited(
+      in_account_creation_flow:,
+      enabled_mfa_methods_count: nil,
+      mfa_attempts: nil,
+      **extra
+    )
     track_event(
       :piv_cac_setup_visited,
       in_account_creation_flow:,
       enabled_mfa_methods_count:,
+      mfa_attempts:,
       **extra,
     )
   end
