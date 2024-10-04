73ecdc86149e4c3d3193f128b723deaaad2640b3
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2024-10-04 15:26:48 -0400

    move mfa attempts to 2fa methods concern, change to user_session

--- app/controllers/users/webauthn_setup_controller.rb
+++ app/controllers/users/webauthn_setup_controller.rb
@@ -143,7 +143,7 @@ module Users
     def analytics_properties
       {
         in_account_creation_flow: user_session[:in_account_creation_flow] || false,
-        mfa_attempts: session[:mfa_attempts] || nil,
+        mfa_attempts: user_session[:mfa_attempts] || nil,
       }
     end
 
