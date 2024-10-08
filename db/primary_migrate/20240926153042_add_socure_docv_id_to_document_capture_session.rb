ef4db514f2071bf4a2cec673a11b8ad4bf70388e
John Maxwell <john.maxwell@gsa.gov>
2024-10-08 13:05:53 -0400

    Merge conflict

--- db/primary_migrate/20240926153042_add_socure_docv_id_to_document_capture_session.rb
+++ db/primary_migrate/20240926153042_add_socure_docv_id_to_document_capture_session.rb
@@ -1,5 +1,5 @@
 class AddSocureDocvIdToDocumentCaptureSession < ActiveRecord::Migration[7.1]
   def change
-    add_column :document_capture_sessions, :socure_docv_token, :string
+    add_column :document_capture_sessions, :socure_docv_token, :string, comment: 'sensitive=false'
   end
 end
