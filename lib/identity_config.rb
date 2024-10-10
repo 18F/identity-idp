a2e33f7ac6ea389e40de4883434391fe6c8ebac4
Vraj Mohan <vraj.mohan@gsa.gov>
2024-10-10 06:43:47 -0700

    LG-14397: Remove use_fed_domain_class (#11326)
    * LG-14397 Remove use_fed_domain_class config
    
    changelog: Internal, Configuration, Remove configuration not needed any longer
    
    * Fix incorrect example description
    * Add test for "email addresses" wihout domains

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -425,7 +425,6 @@ module IdentityConfig
     config.add(:usps_auth_token_refresh_job_enabled, type: :boolean)
     config.add(:usps_confirmation_max_days, type: :integer)
     config.add(:usps_eipp_sponsor_id, type: :string)
-    config.add(:use_fed_domain_class, type: :boolean)
     config.add(:usps_ipp_client_id, type: :string)
     config.add(:usps_ipp_password, type: :string)
     config.add(:usps_ipp_request_timeout, type: :integer)
