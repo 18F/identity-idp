a70739bf287d91d7b1ccd2cb87000dea7da5c995
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-10-17 11:31:49 -0400

    switch to rename_column instead of add/remove column

--- db/primary_migrate/20241017153042_add_socure_docv_transaction_token_to_document_capture_session.rb
+++
@@ -1,5 +0,0 @@
-class AddSocureDocvTransactionTokenToDocumentCaptureSession < ActiveRecord::Migration[7.1]
-  def change
-    add_column :document_capture_sessions, :socure_docv_transaction_token, :string, comment: 'sensitive=false'
-  end
-end
