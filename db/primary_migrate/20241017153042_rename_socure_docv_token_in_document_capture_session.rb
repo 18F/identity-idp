24685b3392bd3d8b45abe41a10af0343577a8e60
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2024-10-17 11:54:55 -0400

    this is a safe change since this column is not used in our deployed envs

--- db/primary_migrate/20241017153042_rename_socure_docv_token_in_document_capture_session.rb
+++ db/primary_migrate/20241017153042_rename_socure_docv_token_in_document_capture_session.rb
@@ -1,5 +1,5 @@
 class RenameSocureDocvTokenInDocumentCaptureSession < ActiveRecord::Migration[7.1]
   def change
-    rename_column :document_capture_sessions, :socure_doc_token, :socure_docv_transaction_token
+    safety_assured { rename_column :document_capture_sessions, :socure_docv_token, :socure_docv_transaction_token }
   end
 end
