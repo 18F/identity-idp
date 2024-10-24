f8773158d584221c4276d86ff49fd338576aacb1
Matt Hinz <matt.hinz@gsa.gov>
2024-10-24 08:51:53 -0700

    Log Socure reason code descriptions
    Save us a step when analyzing data and include descriptions for known reason codes when logging.
    
    [skip changelog]

--- app/services/proofing/socure/id_plus/proofer.rb
+++ app/services/proofing/socure/id_plus/proofer.rb
@@ -77,9 +77,10 @@ module Proofing
         # @param [Proofing::Socure::IdPlus::Response] response
         # @return [Hash]
         def reason_codes_as_errors(response)
-          {
-            reason_codes: response.kyc_reason_codes.sort,
-          }
+          known_codes = SocureReasonCode.where(
+            code: response.kyc_reason_codes,
+          ).pluck(:code, :description).to_h
+          response.kyc_reason_codes.index_with { |code| known_codes[code] || '' }
         end
 
         # @param [Proofing::Socure::IdPlus::Response] response
