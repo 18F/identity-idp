28e022b98e743f3cd56fccca26003489c24891b6
Aaron Nagucki <aaron.nagucki@gmail.com>
2024-10-28 09:55:20 -0500

    Refactor job

--- app/jobs/data_warehouse/daily_sensitive_column_job.rb
+++ app/jobs/data_warehouse/daily_sensitive_column_job.rb
@@ -12,42 +12,37 @@ module DataWarehouse
     def fetch_columns
       tables = ActiveRecord::Base.connection.tables - %w[schema_migrations ar_internal_metadata]
 
+      sensitive_hash = []
+      insensitive_hash = []
+
       tables.each do |table|
         true_sensitives, false_sensitives = ActiveRecord::Base.connection.columns(table).
           reject { |col| col.name == 'id' }.partition do |column|
           column.comment&.match?(/sensitive=true/i)
         end
-        insensitive_hash << false_sensitives.flat_map do |column|
-          {
-            object_locator: {
-              column_name: column.name,
-              table_name: table,
-            },
-          }
-        end
-        sensitive_hash << true_sensitives.flat_map do |column|
-          {
-            object_locator: {
-              column_name: column.name,
-              table_name: table,
-            },
-          }
-        end
+        insensitive_hash.concat(generate_column_data(false_sensitives, table))
+        sensitive_hash.concat(generate_column_data(true_sensitives, table))
       end
-      { sensitive: sensitive_hash, insensitive: insensitive_hash }.to_json
+      {
+        sensitive: dasherize_keys(sensitive_hash),
+        insensitive: dasherize_keys(insensitive_hash),
+      }.to_json
     end
 
-    def insensitive_hash
-      @insensitive_hash ||= []
-      @insensitive_hash = @insensitive_hash.flatten.each do |column|
-        column.deep_transform_keys! { |key| key.to_s.tr('_', '-') }
+    def generate_column_data(columns, table)
+      columns.map do |column|
+        {
+          object_locator: {
+            column_name: column.name,
+            table_name: table,
+          },
+        }
       end
     end
 
-    def sensitive_hash
-      @sensitive_hash ||= []
-      @sensitive_hash = @sensitive_hash.flatten.each do |column|
-        column.deep_transform_keys! { |key| key.to_s.tr('_', '-') }
+    def dasherize_keys(columns)
+      columns.each do |column|
+        column.deep_transform_keys! { |key| key.to_s.dasherize }
       end
     end
 
