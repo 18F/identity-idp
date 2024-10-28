0af89981ca065beb86c62e9d42eccbf83af0b6c7
Jenny Verdeyen <jennifer.verdeyen@gsa.gov>
2024-10-28 11:14:15 -0400

    lint fix

--- config/routes.rb
+++ config/routes.rb
@@ -401,9 +401,8 @@ Rails.application.routes.draw do
           # sometimes underscores get messed up when linked to via SMS
           as: :capture_doc_dashes
 
-      #Deprecated route - temporary redirect while state id changes are rolled out
+      # Deprecated route - temporary redirect while state id changes are rolled out
       get '/in_person_proofing/state_id' => redirect('verify/in_person/state_id', status: 307)
-      # put '/in_person_proofing/state_id' => redirect('verify/in_person/state_id', status: 307)
       put '/in_person_proofing/state_id' => 'in_person/state_id#update'
 
       get '/in_person' => 'in_person#index'
