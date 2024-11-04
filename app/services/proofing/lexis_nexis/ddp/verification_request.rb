0c7fb63c92266befbcc2e2f293db163b3af1d017
Malick Diarra <malick.diarra@gsa.gov>
2024-11-04 16:13:22 -0500

    leverage same verification request

--- app/services/proofing/lexis_nexis/ddp/verification_request.rb
+++ app/services/proofing/lexis_nexis/ddp/verification_request.rb
@@ -7,47 +7,33 @@ module Proofing
         private
 
         def build_request_body
-          if config.ddp_policy == IdentityConfig.store.lexisnexis_authentication_threatmetrix_policy
-            default_body.to_json
-          else
-            default_body.merge(proofing_request_body).to_json
-          end
-        end
-
-        def default_body
           {
             api_key: config.api_key,
             org_id: config.org_id,
+            account_address_street1: applicant[:address1] || '',
+            account_address_street2: applicant[:address2] || '',
+            account_address_city: applicant[:city] || '',
+            account_address_state: applicant[:state] || '',
+            account_address_country: applicant[:state] ? 'US' : '',
+            account_address_zip: applicant[:zipcode] || '',
+            account_date_of_birth: applicant[:dob] ?
+              Date.parse(applicant[:dob]).strftime('%Y%m%d') : '',
             account_email: applicant[:email],
+            account_first_name: applicant[:first_name] || '',
+            account_last_name: applicant[:last_name] || '',
+            account_telephone: '', # applicant[:phone], decision was made not to send phone
+            account_drivers_license_number: applicant[:state_id_number]&.gsub(/\W/, ''),
+            account_drivers_license_type: applicant[:state_id_number] ? 'us_dl' : '',
+            account_drivers_license_issuer: applicant[:state_id_jurisdiction].to_s.strip || '',
             event_type: 'ACCOUNT_CREATION',
             policy: config.ddp_policy,
             service_type: 'all',
             session_id: applicant[:threatmetrix_session_id],
+            national_id_number: applicant[:ssn].gsub(/\D/, ''),
+            national_id_type: applicant[:ssn] ? 'US_SSN' : '',
             input_ip_address: applicant[:request_ip],
-          }
-        end
-
-        def proofing_request_body
-          {
-            account_address_street1: applicant[:address1],
-            account_address_street2: applicant[:address2] || '',
-            account_address_city: applicant[:city],
-            account_address_state: applicant[:state],
-            account_address_country: 'US',
-            account_address_zip: applicant[:zipcode],
-            account_date_of_birth: applicant[:dob] ?
-              Date.parse(applicant[:dob]).strftime('%Y%m%d') : '',
-            account_email: applicant[:email],
-            account_first_name: applicant[:first_name],
-            account_last_name: applicant[:last_name],
-            account_telephone: '', # applicant[:phone], decision was made not to send phone
-            account_drivers_license_number: applicant[:state_id_number]&.gsub(/\W/, ''),
-            account_drivers_license_type: 'us_dl',
-            account_drivers_license_issuer: applicant[:state_id_jurisdiction]&.to_s&.strip,
-            national_id_number: applicant[:ssn]&.gsub(/\D/, ''),
-            national_id_type: 'US_SSN',
-            local_attrib_1: applicant[:uuid_prefix],
-          }
+            local_attrib_1: applicant[:uuid_prefix] || '',
+          }.to_json
         end
 
         def metric_name
