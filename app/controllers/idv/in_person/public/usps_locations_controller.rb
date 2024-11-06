a053ada1ae7d2a176b5036f02705745252187a93
Jenny Verdeyen <jennifer.verdeyen@gsa.gov>
2024-11-06 14:38:06 -0500

    Adding error handling and tests to public usps locations controller
    changelog: Internal, In-person Proofing, Adding graceful error handling and analytics in public usps locations controller

--- app/controllers/idv/in_person/public/usps_locations_controller.rb
+++ app/controllers/idv/in_person/public/usps_locations_controller.rb
@@ -3,8 +3,23 @@
 module Idv
   module InPerson
     module Public
+      class UspsLocationsError < StandardError
+        def initialize
+          super('Unsupported characters in address field.')
+        end
+      end
+
       class UspsLocationsController < ApplicationController
         skip_forgery_protection
+
+        include IppHelper
+
+        rescue_from ActionController::InvalidAuthenticityToken,
+                    Faraday::Error,
+                    StandardError,
+                    UspsLocationsError,
+                    Faraday::BadRequestError,
+                    with: :handle_error
 
         def index
           candidate = UspsInPersonProofing::Applicant.new(
@@ -12,6 +27,11 @@ module Idv
             city: search_params['city'], state: search_params['state'],
             zip_code: search_params['zip_code']
           )
+
+          unless candidate.has_valid_address?
+            raise UspsLocationsError.new
+          end
+
           locations = proofer.request_facilities(candidate, false)
 
           render json: localized_locations(locations).to_json
@@ -32,6 +52,27 @@ module Idv
           locations.map do |location|
             UspsInPersonProofing::EnrollmentHelper.localized_location(location)
           end
+        end
+
+        def handle_error(err)
+          remapped_error = case err
+                           when ActionController::InvalidAuthenticityToken,
+                                Faraday::Error,
+                                UspsLocationsError
+                             :unprocessable_entity
+                           else
+                             :internal_server_error
+                           end
+
+          analytics.idv_in_person_locations_request_failure(
+            api_status_code: Rack::Utils.status_code(remapped_error),
+            exception_class: err.class,
+            exception_message: scrub_message(err.message),
+            response_body_present: err.respond_to?(:response_body) && err.response_body.present?,
+            response_body: err.respond_to?(:response_body) && scrub_body(err.response_body),
+            response_status_code: err.respond_to?(:response_status) && err.response_status,
+          )
+          render json: {}, status: remapped_error
         end
 
         def search_params
