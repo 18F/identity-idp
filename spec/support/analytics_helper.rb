5b649f3d6d9d7633ff936d521653d1158eca4ea2
Andrew Duthie <andrew.duthie@gsa.gov>
2024-11-07 07:40:08 -0500

    Try consolidating analytics stubbing logic

--- spec/support/analytics_helper.rb
+++ spec/support/analytics_helper.rb
@@ -2,13 +2,9 @@ module AnalyticsHelper
   def stub_analytics(user: nil)
     analytics = FakeAnalytics.new
 
-    if user
-      allow(controller).to receive(:analytics).and_wrap_original do |original|
-        expect(original.call.user).to eq(user)
-        analytics
-      end
-    else
-      controller.analytics = analytics
+    allow(controller).to receive(:analytics).and_wrap_original do |original|
+      expect(original.call.user).to eq(user)
+      analytics
     end
 
     @analytics = analytics
