4298aa19d259e321c6fade3dabeb1ebffa145e20
Lauren George <lauren.george@gsa.gov>
2024-11-08 16:57:38 -0500

    PR feedback: add spec example that tests the resolution failure w/o exception scenario

--- spec/controllers/idv/verify_info_controller_spec.rb
+++ spec/controllers/idv/verify_info_controller_spec.rb
@@ -492,6 +492,65 @@ RSpec.describe Idv::VerifyInfoController do
       end
     end
 
+    context 'when the resolution proofing job fails and there is no exception' do
+      before do
+        allow(controller).to receive(:load_async_state).and_return(async_state)
+      end
+
+      let(:document_capture_session) do
+        DocumentCaptureSession.create(user:)
+      end
+
+      let(:async_state) do
+        # Here we're trying to match the store to redis -> read from redis flow this data travels
+        adjudicated_result = Proofing::Resolution::ResultAdjudicator.new(
+          state_id_result: Proofing::StateIdResult.new(
+            success: true,
+            errors: {},
+            exception: nil,
+            vendor_name: :aamva,
+            transaction_id: 'abc123',
+            verified_attributes: [],
+          ),
+          device_profiling_result: Proofing::DdpResult.new(success: true),
+          ipp_enrollment_in_progress: true,
+          residential_resolution_result: Proofing::Resolution::Result.new(success: true),
+          resolution_result: Proofing::Resolution::Result.new(
+            success: false,
+            errors: {
+              base: [
+                "Verification failed with code: 'priority.scoring.model.verification.fail'",
+              ],
+            },
+          ),
+          same_address_as_id: true,
+          should_proof_state_id: true,
+          applicant_pii: Idp::Constants::MOCK_IDV_APPLICANT_WITH_SSN,
+        ).adjudicated_result.to_h
+
+        document_capture_session.create_proofing_session
+
+        document_capture_session.store_proofing_result(adjudicated_result)
+
+        document_capture_session.load_proofing_result
+      end
+
+      it 'renders the warning page' do
+        get :show
+        expect(response).to redirect_to(idv_session_errors_warning_url)
+      end
+
+      it 'logs an event' do
+        get :show
+
+        expect(@analytics).to have_logged_event(
+          'IdV: doc auth warning visited',
+          step_name: 'verify_info',
+          remaining_submit_attempts: kind_of(Numeric),
+        )
+      end
+    end
+
     context 'when the resolution proofing job has not completed' do
       let(:async_state) do
         ProofingSessionAsyncResult.new(status: ProofingSessionAsyncResult::IN_PROGRESS)
