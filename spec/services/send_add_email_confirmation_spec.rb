484596456f54728357426ace31458eec464ecbba
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2024-11-08 08:31:16 -0500

    improve spec use of subject

--- spec/services/send_add_email_confirmation_spec.rb
+++ spec/services/send_add_email_confirmation_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SendAddEmailConfirmation do
+  subject(:instance) { described_class.new(user) }
+
   describe '#call' do
+    subject(:result) { instance.call(email_address:, request_id:, in_select_email_flow:) }
+
     let(:user) { create(:user, confirmed_at: nil) }
     let(:email_address) { user.email_addresses.take }
     let(:request_id) { '1234-abcd' }
+    let(:in_select_email_flow) { nil }
     let(:confirmation_token) { 'confirm-me' }
 
     before do
@@ -15,12 +20,11 @@ RSpec.describe SendAddEmailConfirmation do
         confirmation_sent_at: nil,
       )
     end
-
-    subject { described_class.new(user) }
     it 'sends the user an email with a confirmation link and the request id' do
       email_address.update!(confirmed_at: Time.zone.now)
 
-      subject.call(email_address:, request_id:, in_select_email_flow: nil)
+      result
+
       expect_delivered_email_count(1)
       expect_delivered_email(
         to: [user.email_addresses.first.email],
