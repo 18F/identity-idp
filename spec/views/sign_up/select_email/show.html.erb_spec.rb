2ce0dda49a8513fdd827fc4bbe86858d0493bba4
Andrew Duthie <andrew.duthie@gsa.gov>
2024-11-12 07:33:02 -0500

    Fix HTML escaping for partner email sharing
    changelog: Upcoming Features, Partner Email Selection, Fix HTML escaping for partner email sharing

--- spec/views/sign_up/select_email/show.html.erb_spec.rb
+++ spec/views/sign_up/select_email/show.html.erb_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'sign_up/select_email/show.html.erb' do
+  subject(:rendered) { render }
   let(:email) { 'michael.motorist@email.com' }
   let(:email2) { 'michael.motorist2@email.com' }
   let(:user) { create(:user) }
@@ -10,11 +11,16 @@ RSpec.describe 'sign_up/select_email/show.html.erb' do
     create(:email_address, email: email2, user:)
     @user_emails = user.confirmed_email_addresses
     @select_email_form = SelectEmailForm.new(user:)
+    @sp_name = 'Test Service Provider'
+  end
+
+  it 'renders introduction text' do
+    expect(rendered).to have_content(
+      strip_tags(t('help_text.select_preferred_email_html', sp: @sp_name)),
+    )
   end
 
   it 'shows all of the user\'s emails' do
-    render
-
     expect(rendered).to include('michael.motorist@email.com')
     expect(rendered).to include('michael.motorist2@email.com')
   end
