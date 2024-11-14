db083113dc69f21ad8e333cd0cb90a64946c8011
Andrew Duthie <andrew.duthie@gsa.gov>
2024-11-14 07:08:59 -0500

    Remove allowed_extra_analytics
    changelog: Internal, Analytics, Document analytics event parameters

--- spec/features/idv/end_to_end_idv_spec.rb
+++ spec/features/idv/end_to_end_idv_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Identity verification', :js, allowed_extra_analytics: [:*] do
+RSpec.describe 'Identity verification', :js do
   include IdvStepHelper
   include InPersonHelper
 
--- spec/features/idv/steps/enter_code_step_spec.rb
+++ spec/features/idv/steps/enter_code_step_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'idv enter letter code step', allowed_extra_analytics: [:*] do
+RSpec.feature 'idv enter letter code step' do
   include IdvStepHelper
 
   let(:otp) { 'ABC123' }
