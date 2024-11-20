0b53d6a6a3e6676f138d01d63a4496abb7cffd96
Alex Bradley <alexander.bradley@gsa.gov>
2024-11-20 16:00:45 -0500

    add check for mock vendor

--- app/controllers/concerns/idv/doc_auth_vendor_concern.rb
+++ app/controllers/concerns/idv/doc_auth_vendor_concern.rb
@@ -6,12 +6,16 @@ module Idv
 
     # @returns[String] String identifying the vendor to use for doc auth.
     def doc_auth_vendor
-      if resolved_authn_context_result.facial_match?
+      if resolved_authn_context_result.facial_match? && default_vendor_is_not_mock?
         bucket = :lexis_nexis
       else
         bucket = ab_test_bucket(:DOC_AUTH_VENDOR)
       end
       DocAuthRouter.doc_auth_vendor_for_bucket(bucket)
+    end
+
+    def default_vendor_is_not_mock?
+      IdentityConfig.store.doc_auth_vendor_default != Idp::Constants::Vendors::MOCK
     end
   end
 end
