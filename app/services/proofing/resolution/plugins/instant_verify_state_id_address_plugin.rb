c0eaa1d8b4c595165105647eae5b4c8d92bda9e4
Matt Hinz <matt.hinz@gsa.gov>
2024-11-21 16:48:10 -0800

    Remove proofer_mock_fallback, erm, fallback (#11516)
    * Remove proofer_mock_fallback fallback
    
    Remove support for the proofer_mock_fallback setting when creating resolution proofers.
    
    [skip changelog]
    
    * Update ResolutionProofingJob spec
    
    Tell it to use the Instant Verify proofer for its tests.
    
    * tonight my autoformatter let me down

--- app/services/proofing/resolution/plugins/instant_verify_state_id_address_plugin.rb
+++ app/services/proofing/resolution/plugins/instant_verify_state_id_address_plugin.rb
@@ -44,50 +44,22 @@ module Proofing
         end
 
         def proofer
-          @proofer ||= begin
-            # Historically, proofer_mock_fallback has controlled whether we
-            # use mock implementations of the Resolution and Address proofers
-            # (true = use mock, false = don't use mock).
-            # We are transitioning to a place where we will have separate
-            # configs for both. For the time being, we want to keep support for
-            # proofer_mock_fallback here. This can be removed after this code
-            # has been deployed and configs have been updated in all relevant
-            # environments.
-
-            old_config_says_mock = IdentityConfig.store.proofer_mock_fallback
-            old_config_says_iv = !old_config_says_mock
-            new_config_says_mock =
-              IdentityConfig.store.idv_resolution_default_vendor == :mock
-            new_config_says_iv =
-              IdentityConfig.store.idv_resolution_default_vendor == :instant_verify
-
-            proofer_type =
-              if new_config_says_mock && old_config_says_iv
-                # This will be the case immediately after deployment, when
-                # environment configs have not been updated. We need to
-                # fall back to the old config here.
-                :instant_verify
-              elsif new_config_says_iv
-                :instant_verify
-              else
-                :mock
-              end
-
-            if proofer_type == :mock
-              Proofing::Mock::ResolutionMockClient.new
-            else
-              Proofing::LexisNexis::InstantVerify::Proofer.new(
-                instant_verify_workflow: IdentityConfig.store.lexisnexis_instant_verify_workflow,
-                account_id: IdentityConfig.store.lexisnexis_account_id,
-                base_url: IdentityConfig.store.lexisnexis_base_url,
-                username: IdentityConfig.store.lexisnexis_username,
-                password: IdentityConfig.store.lexisnexis_password,
-                hmac_key_id: IdentityConfig.store.lexisnexis_hmac_key_id,
-                hmac_secret_key: IdentityConfig.store.lexisnexis_hmac_secret_key,
-                request_mode: IdentityConfig.store.lexisnexis_request_mode,
-              )
+          @proofer ||=
+            case IdentityConfig.store.idv_resolution_default_vendor
+              when :instant_verify
+                Proofing::LexisNexis::InstantVerify::Proofer.new(
+                  instant_verify_workflow: IdentityConfig.store.lexisnexis_instant_verify_workflow,
+                  account_id: IdentityConfig.store.lexisnexis_account_id,
+                  base_url: IdentityConfig.store.lexisnexis_base_url,
+                  username: IdentityConfig.store.lexisnexis_username,
+                  password: IdentityConfig.store.lexisnexis_password,
+                  hmac_key_id: IdentityConfig.store.lexisnexis_hmac_key_id,
+                  hmac_secret_key: IdentityConfig.store.lexisnexis_hmac_secret_key,
+                  request_mode: IdentityConfig.store.lexisnexis_request_mode,
+                )
+              when :mock
+                Proofing::Mock::ResolutionMockClient.new
             end
-          end
         end
 
         def resolution_cannot_pass
