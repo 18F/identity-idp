36e6c74a26a1ed7574982ebc5a326f1dd0f3ca7e
Jessica Dembe <jessica.dembe@gsa.gov>
2024-11-25 14:19:37 -0500

    add test that should not have been deleted

--- spec/components/webauthn_input_component_spec.rb
+++ spec/components/webauthn_input_component_spec.rb
@@ -74,6 +74,17 @@ RSpec.describe WebauthnInputComponent, type: :component do
             )
           end
 
+          context 'with show_unsupported_passkey option true' do
+            let(:options) { super().merge(show_unsupported_passkey: true) }
+
+            it 'renders with show-unsupported-passkey attribute' do
+              expect(rendered).to have_css(
+                'lg-webauthn-input[hidden][show-unsupported-passkey]',
+                visible: false,
+              )
+            end
+          end
+
           context 'with show_unsupported_passkey option' do
             context 'with show_unsupported_passkey option false' do
               let(:options) { super().merge(show_unsupported_passkey: false) }
