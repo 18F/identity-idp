42c1f17b075a0b95329a454b12517f1e45b3326a
Matt Hinz <matt.hinz@gsa.gov>
2024-11-26 09:07:03 -0800

    Remove ProofingComponent model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe User do
     it { is_expected.to have_one(:account_reset_request) }
     it { is_expected.to have_many(:phone_configurations) }
     it { is_expected.to have_many(:webauthn_configurations) }
-    it { is_expected.to have_one(:proofing_component) }
     it { is_expected.to have_many(:in_person_enrollments).dependent(:destroy) }
     it {
       is_expected.to have_one(:pending_in_person_enrollment).
