196ca28567d6ef89e0a508ba27546fa5d6feb915
Jessica Dembe <jessica.dembe@gsa.gov>
2024-11-26 16:50:17 -0500

    add supporting test

---
+++ spec/controllers/accounts/connected_accounts_controller_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Accounts::ConnectedAccountsController do
+  describe '#show' do
+    let(:user) { create(:user, :fully_registered) }
+
+    before do
+      stub_sign_in(user) if user
+    end
+
+    context 'on page load' do
+      it 'shows and logs a visit' do
+        stub_analytics
+
+        get :show
+
+        expect(@analytics).to have_logged_event(:connected_accounts_page_visited)
+      end
+    end
+  end
+end
