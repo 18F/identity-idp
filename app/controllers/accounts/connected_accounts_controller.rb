6720b37ba3d75d55019b5533a0af451726fd205e
Jessica Dembe <jessica.dembe@gsa.gov>
2024-12-02 08:54:21 -0500

    Update app/controllers/accounts/connected_accounts_controller.rb
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- app/controllers/accounts/connected_accounts_controller.rb
+++ app/controllers/accounts/connected_accounts_controller.rb
@@ -8,7 +8,7 @@ module Accounts
     layout 'account_side_nav'
 
     def show
-      analytics.track_event(:connected_accounts_page_visited)
+      analytics.connected_accounts_page_visited
       @presenter = AccountShowPresenter.new(
         decrypted_pii: nil,
         sp_session_request_url: sp_session_request_url_with_updated_params,
