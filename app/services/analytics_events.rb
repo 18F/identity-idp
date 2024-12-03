87e77ff3892dbe4d501ee0d596ceb546d9af7c33
Douglas Price <douglas.price@gsa.gov>
2024-12-03 10:44:54 -0500

    log an event on choosing In Person

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -1698,6 +1698,25 @@ module AnalyticsEvents
     )
   end
 
+  # User chooses to try In Person from the Socure timeout error page
+  # @param [Integer] remaining_submit_attempts The number of remaining attempts to submit
+  # @param [Boolean] skip_hybrid_handoff Whether the user skipped the hybrid handoff A/B test
+  # @param [Boolean] opted_in_to_in_person_proofing Whether the user opted into in-person proofing
+  def idv_doc_auth_socure_choose_in_person(
+    remaining_submit_attempts:,
+    skip_hybrid_handoff: nil,
+    opted_in_to_in_person_proofing: nil,
+    **extra
+  )
+    track_event(
+      :idv_doc_auth_socure_choose_in_person,
+      remaining_submit_attempts:,
+      skip_hybrid_handoff:,
+      opted_in_to_in_person_proofing:,
+      **extra,
+    )
+  end
+
   # User is shown the Socure timeout error page
   # @param [String] type The type of error that occurred
   # @param [Integer] remaining_submit_attempts The number of remaining attempts to submit
