f64e73015d5fbf5c39ec9a568291bc3cd12b9fa2
Alex Bradley <alexander.bradley@gsa.gov>
2024-12-03 13:15:10 -0500

    add mobile_required to how to verify

--- app/presenters/idv/how_to_verify_presenter.rb
+++ app/presenters/idv/how_to_verify_presenter.rb
@@ -4,14 +4,15 @@ class Idv::HowToVerifyPresenter
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::TranslationHelper
 
-  attr_reader :selfie_required
+  attr_reader :mobile_required, :selfie_required
 
-  def initialize(selfie_check_required:)
+  def initialize(mobile_required:, selfie_check_required:)
+    @mobile_required = mobile_required
     @selfie_required = selfie_check_required
   end
 
   def how_to_verify_info
-    if selfie_required
+    if mobile_required
       t('doc_auth.info.how_to_verify_selfie')
     else
       t('doc_auth.info.how_to_verify')
@@ -19,7 +20,7 @@ class Idv::HowToVerifyPresenter
   end
 
   def asset_url
-    if selfie_required
+    if mobile_required
       'idv/mobile-phone-icon.svg'
     else
       'idv/remote.svg'
@@ -27,7 +28,7 @@ class Idv::HowToVerifyPresenter
   end
 
   def alt_text
-    if selfie_required
+    if mobile_required
       t('image_description.phone_icon')
     else
       t('image_description.laptop_and_phone')
@@ -35,7 +36,7 @@ class Idv::HowToVerifyPresenter
   end
 
   def verify_online_text
-    if selfie_required
+    if mobile_required
       t('doc_auth.headings.verify_online_selfie')
     else
       t('doc_auth.headings.verify_online')
@@ -43,15 +44,14 @@ class Idv::HowToVerifyPresenter
   end
 
   def verify_online_instruction
-    if selfie_required
-      t('doc_auth.info.verify_online_instruction_selfie')
-    else
-      t('doc_auth.info.verify_online_instruction')
-    end
+    return t('doc_auth.info.verify_online_instruction_selfie') if selfie_required
+    return t('doc_auth.info.verify_online_instruction_mobile_no_selfie') if mobile_required
+
+    t('doc_auth.info.verify_online_instruction')
   end
 
   def verify_online_description
-    if selfie_required
+    if mobile_required
       t('doc_auth.info.verify_online_description_selfie')
     else
       t('doc_auth.info.verify_online_description')
@@ -59,7 +59,7 @@ class Idv::HowToVerifyPresenter
   end
 
   def submit
-    if selfie_required
+    if mobile_required
       t('forms.buttons.continue_remote_selfie')
     else
       t('forms.buttons.continue_remote')
@@ -75,7 +75,7 @@ class Idv::HowToVerifyPresenter
   end
 
   def post_office_description
-    if selfie_required
+    if mobile_required
       t('doc_auth.info.verify_at_post_office_description_selfie')
     else
       t('doc_auth.info.verify_at_post_office_description')
