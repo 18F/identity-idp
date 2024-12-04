fbf2291a2fe8f3b9853ee5137e93c52ed98d7a03
John Maxwell <john.maxwell@gsa.gov>
2024-12-04 14:52:34 -0500

    Check in to change branches.

--- config/routes.rb
+++ config/routes.rb
@@ -361,7 +361,7 @@ Rails.application.routes.draw do
       put '/document_capture' => 'document_capture#update'
       get '/socure/document_capture' => 'socure/document_capture#show'
       get '/socure/document_capture_update' => 'socure/document_capture#update', as: :socure_document_capture_update
-      get '/socure/document_capture_errors' => 'socure/document_capture#errors', as: :socure_document_capture_errors
+      get '/socure/document_capture_errors' => 'socure/document_capture_errors#show'
       get '/socure/document_capture_goto_in_person' => 'socure/document_capture#goto_in_person', as: :socure_document_capture_goto_in_person
       # This route is included in SMS messages sent to users who start the IdV hybrid flow. It
       # should be kept short, and should not include underscores ("_").
@@ -371,7 +371,7 @@ Rails.application.routes.draw do
       get '/hybrid_mobile/capture_complete' => 'hybrid_mobile/capture_complete#show'
       get '/hybrid_mobile/socure/document_capture' => 'hybrid_mobile/socure/document_capture#show'
       get '/hybrid_mobile/socure/document_capture_update' => 'hybrid_mobile/socure/document_capture#update', as: :hybrid_mobile_socure_document_capture_update
-      get '/hybrid_mobile/socure/document_capture_errors' => 'hybrid_mobile/socure/document_capture#errors', as: :hybrid_mobile_socure_document_capture_errors
+      get '/hybrid_mobile/socure/document_capture_errors' => 'hybrid_mobile/socure/document_capture_errors#show'
       get '/hybrid_mobile/socure/document_capture_goto_in_person' => 'hybrid_mobile/socure/document_capture#goto_in_person', as: :hybrid_mobile_socure_document_capture_goto_in_person
       get '/hybrid_handoff' => 'hybrid_handoff#show'
       put '/hybrid_handoff' => 'hybrid_handoff#update'
