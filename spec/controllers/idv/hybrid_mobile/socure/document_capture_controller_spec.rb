de8643ee1df89c88d863e90dbd138831559f80a0
ashukla <abir.shukla@gsa.gov>
2024-12-05 14:34:58 -0600

    Resolving PR comments

--- spec/controllers/idv/hybrid_mobile/socure/document_capture_controller_spec.rb
+++ spec/controllers/idv/hybrid_mobile/socure/document_capture_controller_spec.rb
@@ -323,12 +323,15 @@ RSpec.describe Idv::HybridMobile::Socure::DocumentCaptureController do
       before do
         allow(request_class).to receive(:new).and_call_original
         allow(I18n).to receive(:locale).and_return(expected_language)
-        allow(DocumentCaptureSession).to receive(:find_by).and_return(document_capture_session)
       end
 
       it 'does not create a DocumentRequest when valid capture app exists' do
-        document_capture_session.socure_docv_capture_app_url = fake_capture_app_url
-        document_capture_session.save
+        dcs = create(
+          :document_capture_session,
+          uuid: user.id,
+          socure_docv_capture_app_url: fake_capture_app_url,
+        )
+        allow(DocumentCaptureSession).to receive(:find_by).and_return(dcs)
         get(:show)
         expect(request_class).not_to have_received(:new)
         expect(document_capture_session.socure_docv_capture_app_url).to eq(fake_capture_app_url)
