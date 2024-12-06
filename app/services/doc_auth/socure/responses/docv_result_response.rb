6d905ff91c61d7c0047f90221df8af60975c1a3d
John Maxwell <john.maxwell@gsa.gov>
2024-12-06 15:29:05 -0500

    Added PII check to DocV error checks.

--- app/services/doc_auth/socure/responses/docv_result_response.rb
+++ app/services/doc_auth/socure/responses/docv_result_response.rb
@@ -51,7 +51,7 @@ module DocAuth
           NewRelic::Agent.notice_error(e)
           super(
             success: false,
-            errors: { network: true },
+            errors: { network: true, details: e.to_s },
             exception: e,
             extra: {
               backtrace: e.backtrace,
@@ -59,9 +59,7 @@ module DocAuth
           )
         end
 
-        def doc_auth_success?
-          success?
-        end
+        alias_method :doc_auth_success?, :success?
 
         def selfie_status
           :not_processed
@@ -80,7 +78,7 @@ module DocAuth
             flow_path: nil,
             liveness_checking_required: @biometric_comparison_required,
             issue_year: state_id_issued&.year,
-            doc_auth_success: successful_result?,
+            doc_auth_success: doc_auth_success?,
             vendor: 'Socure',
             address_line2_present: address2.present?,
             zip_code: zipcode,
@@ -91,7 +89,22 @@ module DocAuth
 
         private
 
+        def pii_valid?
+          return @pii_valid if defined?(@pii_valid)
+
+          response = Idv::DocPiiForm.new(
+            pii: pii_from_doc.to_h,
+            attention_with_barcode: attention_with_barcode?,
+          ).submit
+
+          @pii_valid = response.success?
+        end
+
         def successful_result?
+          socure_succeeded? && pii_valid?
+        end
+
+        def socure_succeeded?
           get_data(DATA_PATHS[:decision_value]) == 'accept'
         end
 
@@ -99,6 +112,7 @@ module DocAuth
           return {} if successful_result?
 
           {
+            pii_invalid: !pii_valid?,
             socure: { reason_codes: get_data(DATA_PATHS[:reason_codes]) },
           }
         end
@@ -140,6 +154,7 @@ module DocAuth
           rescue JSON::JSONError
             {}
           end
+          @parsed_response_body
         end
 
         def state
---
+++ spec/services/doc_auth/socure/responses/docv_result_response_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe DocAuth::Socure::Responses::DocvResultResponse do
+  subject(:response) do
+    described_class.new(http_response:, biometric_comparison_required:)
+  end
+
+  let(:http_response) { double('fake faraday response') }
+  let(:biometric_comparison_required) { false }
+  let(:response_json) { SocureDocvFixtures.pass_json }
+
+  before do
+    allow(http_response).to receive(:body).and_return(response_json)
+  end
+
+  context 'happy path' do
+    it 'success? is true' do
+      expect(subject.success?).to eq(true)
+    end
+
+    it 'doc_type is supported' do
+      expect(subject.doc_type_supported?).to eq(true)
+    end
+
+    it 'doc_auth_success? is true' do
+      expect(subject.doc_auth_success?).to eq(true)
+    end
+  end
+
+  context 'with a Socure error' do
+    let(:response_json) { SocureDocvFixtures.fail_json(['I808']) }
+
+    it 'fails' do
+      expect(subject.success?).to eq(false)
+    end
+
+    it 'adds the socure error to the errors' do
+      expect(subject.errors.dig(:socure, :reason_codes)).to eq(['I808'])
+    end
+  end
+
+  context 'when the PII is not satisfactory' do
+    before do
+      allow_any_instance_of(Idv::DocAuthFormResponse).to receive(:success?).and_return(false)
+    end
+
+    it 'fails' do
+      expect(subject.success?).to eq(false)
+    end
+
+    it 'adds the pii error to the errors' do
+      expect(subject.errors).to have_key(:pii_invalid)
+    end
+  end
+end
