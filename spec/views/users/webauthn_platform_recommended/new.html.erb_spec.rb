078470c5b83f7f001fe7b9d4e86a8a9e1e510011
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2024-12-06 13:36:06 -0500

    Fix Face/Touch recommendation duplicate submission (#11608)
    * Fall back to after_mfa_setup_path for nil next_setup_path
    
    changelog: Bug Fixes, Face/Touch Recommendation, Fix edge case for duplicate submission in recommendation
    
    * Use SubmitButtonComponent for Face/Touch recommendation

--- spec/views/users/webauthn_platform_recommended/new.html.erb_spec.rb
+++ spec/views/users/webauthn_platform_recommended/new.html.erb_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe 'users/webauthn_platform_recommended/new.html.erb' do
   subject(:rendered) { render }
 
+  it 'renders separate forms with submission for options to add' do
+    expect(rendered).to have_css('form:has(input[name=add_method]):has([type=submit])')
+    expect(rendered).to have_css('form:not(:has(input[name=add_method])):has([type=submit])')
+  end
+
   it 'renders a help link for phishing-resistant including flow path' do
     @sign_in_flow = :example
 
