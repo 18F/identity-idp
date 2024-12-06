e5700245ece63fe4d4eb5da2bdba9dcb2b4738bf
Lauren George <lauren.george@gsa.gov>
2024-12-06 15:25:06 -0500

    PR feedback: reduce opted out events, array literal syntax

--- app/services/idv/analytics_events_enhancer.rb
+++ app/services/idv/analytics_events_enhancer.rb
@@ -7,85 +7,72 @@ module Idv
   # - +active_profile_idv_level+: ID verification level of user's active profile.
   # - +pending_profile_idv_level+: ID verification level of user's pending profile.
   # Generally, analytics events that are called in contexts where there is no expectation
-  # of an {Idv::Session} being present (e.g., jobs, client-generated events, action scripts)
-  # are opted-out.
+  # of an {Idv::Session} being present or may be excessively noisy are opted-out.
+  # (e.g., jobs, client-generated events, action scripts).
   #
   # Additionally, +profile_history+, the list of a User's profiles
-  # (sorted by creaton date, oldest to newest), may be added to events, but this is opt-in only.
-  # See {AnalyticsEventsEnhancer::METHODS_WITH_PROFILE_HISTORY} for the list of included events.
+  # (sorted by creation date, oldest to newest), may be added to events, but this is opt-in only.
+  # See #METHODS_WITH_PROFILE_HISTORY for the list of included events.
   module AnalyticsEventsEnhancer
-    EXCLUDED_FRONTEND_EVENT_METHODS = [
-      :idv_acuant_sdk_loaded,
-      :idv_barcode_warning_continue_clicked,
-      :idv_barcode_warning_retake_photos_clicked,
-      :idv_camera_info_error,
-      :idv_camera_info_logged,
-      :idv_capture_troubleshooting_dismissed,
-      :idv_consent_checkbox_toggled,
-      :idv_image_capture_failed,
-      :idv_in_person_location_submitted,
-      :idv_in_person_ready_to_verify_sp_link_clicked,
-      :idv_in_person_ready_to_verify_what_to_bring_link_clicked,
-      :idv_link_sent_capture_doc_polling_complete,
-      :idv_link_sent_capture_doc_polling_started,
-      :idv_native_camera_forced,
-      :idv_sdk_error_before_init,
-      :idv_sdk_selfie_image_capture_closed_without_photo,
-      :idv_sdk_selfie_image_capture_failed,
-      :idv_sdk_selfie_image_capture_initialized,
-      :idv_sdk_selfie_image_capture_opened,
-      :idv_sdk_selfie_image_re_taken,
-      :idv_sdk_selfie_image_taken,
-      :idv_selfie_image_added,
-      :idv_verify_in_person_troubleshooting_option_clicked,
-      :idv_warning_action_triggered,
-      :idv_warning_shown,
+    EXCLUDED_FRONTEND_EVENT_METHODS = %i[
+      idv_acuant_sdk_loaded
+      idv_barcode_warning_continue_clicked
+      idv_barcode_warning_retake_photos_clicked
+      idv_capture_troubleshooting_dismissed
+      idv_consent_checkbox_toggled
+      idv_image_capture_failed
+      idv_in_person_location_submitted
+      idv_in_person_ready_to_verify_sp_link_clicked
+      idv_in_person_ready_to_verify_what_to_bring_link_clicked
+      idv_sdk_error_before_init
+      idv_sdk_selfie_image_capture_closed_without_photo
+      idv_sdk_selfie_image_capture_failed
+      idv_sdk_selfie_image_capture_initialized
+      idv_sdk_selfie_image_capture_opened
+      idv_sdk_selfie_image_re_taken
+      idv_sdk_selfie_image_taken
+      idv_selfie_image_added
+      idv_verify_in_person_troubleshooting_option_clicked
     ].freeze
 
-    EXCLUDED_JOB_EVENT_METHODS = [
-      :idv_gpo_expired,
-      :idv_gpo_reminder_email_sent,
-      :idv_in_person_email_reminder_job_email_initiated,
-      :idv_in_person_email_reminder_job_exception,
-      :idv_in_person_proofing_enrollments_ready_for_status_check_job_completed,
-      :idv_in_person_proofing_enrollments_ready_for_status_check_job_ingestion_error,
-      :idv_in_person_proofing_enrollments_ready_for_status_check_job_started,
-      :idv_in_person_send_proofing_notification_attempted,
-      :idv_in_person_send_proofing_notification_job_completed,
-      :idv_in_person_send_proofing_notification_job_exception,
-      :idv_in_person_send_proofing_notification_job_skipped,
-      :idv_in_person_send_proofing_notification_job_started,
-      :idv_in_person_usps_proofing_enrollment_code_email_received,
-      :idv_in_person_usps_proofing_results_job_completed,
-      :idv_in_person_usps_proofing_results_job_deadline_passed_email_exception,
-      :idv_in_person_usps_proofing_results_job_deadline_passed_email_initiated,
-      :idv_in_person_usps_proofing_results_job_email_initiated,
-      :idv_in_person_usps_proofing_results_job_enrollment_incomplete,
-      :idv_in_person_usps_proofing_results_job_enrollment_updated,
-      :idv_in_person_usps_proofing_results_job_exception,
-      :idv_in_person_usps_proofing_results_job_please_call_email_initiated,
-      :idv_in_person_usps_proofing_results_job_started,
-      :idv_in_person_usps_proofing_results_job_unexpected_response,
-      :idv_in_person_usps_proofing_results_job_user_sent_to_fraud_review,
-      :idv_ipp_deactivated_for_never_visiting_post_office,
-      :idv_socure_document_request_submitted,
-      :idv_socure_reason_code_download,
-      :idv_socure_shadow_mode_proofing_result,
-      :idv_socure_shadow_mode_proofing_result_missing,
-      :idv_socure_verification_data_requested,
-      :idv_usps_auth_token_refresh_job_completed,
-      :idv_usps_auth_token_refresh_job_network_error,
-      :idv_usps_auth_token_refresh_job_started,
-    ].freeze
-
-    EXCLUDED_MISC_EVENT_METHODS = [
-      :idv_in_person_proofing_nontransliterable_characters_submitted,
+    EXCLUDED_JOB_EVENT_METHODS = %i[
+      idv_gpo_expired
+      idv_gpo_reminder_email_sent
+      idv_in_person_email_reminder_job_email_initiated
+      idv_in_person_email_reminder_job_exception
+      idv_in_person_proofing_enrollments_ready_for_status_check_job_completed
+      idv_in_person_proofing_enrollments_ready_for_status_check_job_ingestion_error
+      idv_in_person_proofing_enrollments_ready_for_status_check_job_started
+      idv_in_person_send_proofing_notification_attempted
+      idv_in_person_send_proofing_notification_job_completed
+      idv_in_person_send_proofing_notification_job_exception
+      idv_in_person_send_proofing_notification_job_skipped
+      idv_in_person_send_proofing_notification_job_started
+      idv_in_person_usps_proofing_enrollment_code_email_received
+      idv_in_person_usps_proofing_results_job_completed
+      idv_in_person_usps_proofing_results_job_deadline_passed_email_exception
+      idv_in_person_usps_proofing_results_job_deadline_passed_email_initiated
+      idv_in_person_usps_proofing_results_job_email_initiated
+      idv_in_person_usps_proofing_results_job_enrollment_incomplete
+      idv_in_person_usps_proofing_results_job_enrollment_updated
+      idv_in_person_usps_proofing_results_job_exception
+      idv_in_person_usps_proofing_results_job_please_call_email_initiated
+      idv_in_person_usps_proofing_results_job_started
+      idv_in_person_usps_proofing_results_job_unexpected_response
+      idv_in_person_usps_proofing_results_job_user_sent_to_fraud_review
+      idv_ipp_deactivated_for_never_visiting_post_office
+      idv_socure_reason_code_download
+      idv_socure_shadow_mode_proofing_result
+      idv_socure_shadow_mode_proofing_result_missing
+      idv_socure_verification_data_requested
+      idv_usps_auth_token_refresh_job_completed
+      idv_usps_auth_token_refresh_job_network_error
+      idv_usps_auth_token_refresh_job_started
     ].freeze
 
     IGNORED_METHODS = [
       *EXCLUDED_FRONTEND_EVENT_METHODS,
       *EXCLUDED_JOB_EVENT_METHODS,
-      *EXCLUDED_MISC_EVENT_METHODS,
     ].uniq.freeze
 
     STANDARD_ARGUMENTS = %i[
@@ -100,7 +87,7 @@ module Idv
       idv_final
       idv_please_call_visited
       idv_start_over
-    ].uniq.freeze
+    ].freeze
 
     def self.included(_mod)
       raise 'this mixin is intended to be prepended, not included'
