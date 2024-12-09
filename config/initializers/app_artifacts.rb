89dfd053c219f9aeeec7c50f2b2854a37eeeaaff
Mitchell Henke <mitchell.henke@gsa.gov>
2024-12-09 16:10:29 -0600

    Validate identity provider public/private keys
    changelog: Internal, OpenID Connect, Validate identity provider public/private keys
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- config/initializers/app_artifacts.rb
+++ config/initializers/app_artifacts.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'app_artifacts'
+require 'openid_connect_key_validation'
 
 AppArtifacts.setup do |store|
   # When adding or removing certs, make sure to update the 'saml_endpoint_configs' config
@@ -12,3 +13,9 @@ AppArtifacts.setup do |store|
   store.add_artifact(:oidc_private_key, '/%<env>s/oidc.key') { |k| OpenSSL::PKey::RSA.new(k) }
   store.add_artifact(:oidc_public_key, '/%<env>s/oidc.pub') { |k| OpenSSL::PKey::RSA.new(k) }
 end
+
+valid = OpenidConnectKeyValidation.valid?(
+  public_key: AppArtifacts.store.oidc_public_key,
+  private_key: AppArtifacts.store.oidc_private_key,
+)
+raise 'OIDC Public/Private Keys do not match' if !valid
