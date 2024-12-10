1a835a1eba5a15e1075092e21ac763d392c00e09
Jenny Verdeyen <jennifer.verdeyen@gsa.gov>
2024-12-10 15:01:22 -0500

    Mock domain name

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,6 +4,13 @@ ENV['RAILS_ENV'] = 'test'
 
 require 'capybara/cucumber'
 require_relative 'helpers'
+require 'cucumber/rspec/doubles'
 
 Capybara.default_driver = :selenium_chrome
-Capybara.server_port = '63629'
+
+server = Capybara.current_session.server
+server_domain = "#{server.host}:#{server.port}"
+
+Before do
+  allow(IdentityConfig.store).to receive(:domain_name).and_return(server_domain)
+end
