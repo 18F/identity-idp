75e888bff41656a48139e624dd6d5d728a5e4eb8
Shane Chesnutt <shane.chesnutt@gsa.gov>
2024-12-10 15:06:23 -0500

    Actually use spec support code

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+require_relative '../../spec/support/features/idv_helper'
+require_relative '../../spec/support/features/personal_key_helper'
+require_relative '../../spec/support/features/session_helper'
+require_relative '../../spec/support/otp_helper'
+
+World(
+  ActionView::Helpers::TranslationHelper,
+  Features::SessionHelper,
+  IdvHelper,
+  OtpHelper,
+)
