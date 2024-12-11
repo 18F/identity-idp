1023da34bd40500bb4ce8cdde6bccc7d9b61fd9a
Matt Hinz <matt.hinz@gsa.gov>
2024-12-11 10:32:21 -0800

    Add ExampleMatcher
    Add an example matcher that just counts events and outputs how many it saw.
    
    [skip changelog]

---
+++ lib/event_summarizer/example_matcher.rb
@@ -0,0 +1,26 @@
+module EventSummarizer
+  class ExampleMatcher
+    attr_reader :event_count
+
+    def initialize
+      @event_count = 0
+    end
+
+    def handle_cloudwatch_event(_event)
+      @event_count += 1
+    end
+
+    def finish
+      [
+        {
+          title: 'Processed some events',
+          attributes: [
+            { type: :event_count, description: "Processed #{event_count} event(s)" },
+          ],
+        }.tap do
+          @event_count = 0
+        end,
+      ]
+    end
+  end
+end
