a97df5b1c8cf0ca1c10d3c9d69d1e84d3e5c57a5
Shane Chesnutt <shane.chesnutt@gsa.gov>
2024-12-12 10:40:51 -0500

    Add state id form submission test

--- features/id_ipp_state_id_page.feature
+++ features/id_ipp_state_id_page.feature
@@ -7,3 +7,23 @@ Feature: ID-IPP flow: State ID page
     When the user reaches the state id page
     Then the step "verify_info" is active on the step nav
     And the state id form is present
+
+  @ui
+  Scenario: User submits the state id form having the same address as ID
+    Given a user is logged in
+    And the user reaches the state id page
+    When the user fills out the state id form with:
+    | first_name      | Jimmy      |
+    | last_name       | Testington |
+    | birth_month     | 6          |
+    | birth_day       | 24         |
+    | birth_year      | 2000       |
+    | state_id_number | 123456     |
+    | state           | Alaska     |
+    | address_1       | qwerty     |
+    | address_2       |            |
+    | city            | qwertyton  |
+    | zipcode         | 12345      |
+    | same_as_address | true       |
+    And the user submits the form
+    Then the user is navigated to the ssn page
--- features/step_definitions/id_ipp/id_ipp_state_id_steps.rb
+++ features/step_definitions/id_ipp/id_ipp_state_id_steps.rb
@@ -4,6 +4,36 @@ When('the user reaches the state id page') do
   complete_ipp_steps_before_state_id_controller(@user)
 end
 
+When('the user fills out the state id form with:') do |table|
+  form_data = table.rows_hash
+  fill_in(t('in_person_proofing.form.state_id.first_name'), with: form_data['first_name'])
+  fill_in(t('in_person_proofing.form.state_id.last_name'), with: form_data['last_name'])
+  fill_in(t('components.memorable_date.month'), with: form_data['birth_month'])
+  fill_in(t('components.memorable_date.day'), with: form_data['birth_day'])
+  fill_in(t('components.memorable_date.year'), with: form_data['birth_year'])
+  select(form_data['state'], from: t('in_person_proofing.form.state_id.state_id_jurisdiction'))
+  fill_in(t('in_person_proofing.form.state_id.state_id_number'), with: form_data['state_id_number'])
+  select(form_data['state'], from: t('in_person_proofing.form.state_id.identity_doc_address_state'))
+  fill_in(t('in_person_proofing.form.state_id.address1'), with: form_data['address_1'])
+  fill_in(t('in_person_proofing.form.state_id.address2'), with: form_data['address_2'])
+  fill_in(t('in_person_proofing.form.state_id.city'), with: form_data['city'])
+  fill_in(t('in_person_proofing.form.state_id.zipcode'), with: form_data['zipcode'])
+
+  if form_data['same_as_address'] == 'true'
+    choose(t('in_person_proofing.form.state_id.same_address_as_id_yes'), allow_label_click: true)
+  else
+    choose(t('in_person_proofing.form.state_id.same_address_as_id_no'), allow_label_click: true)
+  end
+end
+
+When('the user submits the form') do
+  click_idv_continue
+end
+
+Then('the user is navigated to the ssn page') do
+  expect(page).to have_current_path(idv_in_person_ssn_path)
+end
+
 Then('the state id form is present') do
   expect(page).to have_current_path(idv_in_person_state_id_path, wait: 10)
   expect(page).to have_content(strip_nbsp(t('in_person_proofing.headings.state_id_milestone_2')))
