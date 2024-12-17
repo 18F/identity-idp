7a10f4e72899cd8547708221d06d2c8b520afc4e
Malick Diarra <malick.diarra@gsa.gov>
2024-12-17 15:19:11 -0500

    remove unneeded check

--- app/controllers/users/two_factor_authentication_setup_controller.rb
+++ app/controllers/users/two_factor_authentication_setup_controller.rb
@@ -23,9 +23,6 @@ module Users
         enabled_mfa_methods_count:,
         gov_or_mil_email: fed_or_mil_email?,
       )
-      @tmx_view_needed =
-        FeatureManagement.account_creation_device_profiling_collecting_enabled? &&
-        mfa_context.enabled_mfa_methods_count == 0
       render :index, locals: threatmetrix_variables
     end
 
