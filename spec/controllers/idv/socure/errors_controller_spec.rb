17dff48d899731f2c74ca05c4c4114d3d669a506
Douglas Price <douglas.price@gsa.gov>
2024-12-17 12:10:49 -0500

    remove outdated spec

--- spec/controllers/idv/socure/errors_controller_spec.rb
+++ spec/controllers/idv/socure/errors_controller_spec.rb
@@ -21,21 +21,4 @@ RSpec.describe Idv::Socure::ErrorsController do
       expect(@analytics).to have_logged_event(:idv_doc_auth_socure_error_visited)
     end
   end
-
-  describe '#go_in_person' do
-    it 'redirects to the start of the in person path' do
-      get(:go_in_person)
-
-      expect(subject.idv_session.opted_in_to_in_person_proofing).to eq(true)
-      expect(subject.idv_session.flow_path).to eq('standard')
-      expect(subject.idv_session.skip_doc_auth_from_how_to_verify).to eq(true)
-      expect(response).to redirect_to(idv_document_capture_url(step: :idv_doc_auth))
-    end
-
-    it 'logs an event' do
-      get(:go_in_person)
-
-      expect(@analytics).to have_logged_event(:idv_doc_auth_socure_choose_in_person)
-    end
-  end
 end
