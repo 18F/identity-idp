4616d9fa714e9ddfdb9e1e1e0dd1803aa14e5b33
Matt Hinz <matt.hinz@gsa.gov>
2024-12-17 09:53:16 -0800

    Add expect_email_not_delivered helper

--- spec/support/mailer_helper.rb
+++ spec/support/mailer_helper.rb
@@ -33,6 +33,34 @@ module MailerHelper
     expect(email).to_not be(nil), error_message
   end
 
+  # @param [String,nil] to If provided, the email address the message must've been sent to
+  # @param [String,nil] subject If provided, the subject the email must've had
+  # @param [String[],nil] Array of substrings that must appear in body.
+  def expect_email_not_delivered(to: nil, subject: nil, body: nil)
+    email = ActionMailer::Base.deliveries.find do |sent_mail|
+      to_matches = to.nil? || sent_mail.to.include?(to)
+      subject_matches = subject.nil? || sent_mail.subject == subject
+      body_matches = body.nil? || begin
+        delivered_body = sent_mail.text_part.decoded.squish
+        body.to_a.all? do |expected_substring|
+          delivered_body.include?(expected_substring)
+        end
+      end
+
+      to_matches && subject_matches && body_matches
+    end
+
+    error_message = <<~ERROR
+      Found an email matching the below (but shouldn't have):
+        to: #{to}
+        subject: #{subject}
+        body: #{body}
+      Sent mails: #{ActionMailer::Base.deliveries}
+    ERROR
+
+    expect(email).to be(nil), error_message
+  end
+
   private
 
   def body_matches(email:, body:)
@@ -92,7 +120,7 @@ module MailerHelper
 
   def summarize_all_deliveries(query)
     ActionMailer::Base.deliveries.map do |email|
-      summary = summarize_delivery(email:, **query)
+      summary = summarize_delivery(delivery:, **query)
       [
         "- #{summary.lines.first.strip}",
         *summary.lines.drop(1).map { |l| l.strip.indent(2) },
