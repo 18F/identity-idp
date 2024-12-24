3c6f1115ba601fc01d11443f0f7aa514f91e2aec
Doug Price <douglas.price@gsa.gov>
2024-12-23 20:50:59 -0500

    LG-15059: Socure timeout page (#11572)
    If a user is returned from Socure and is waiting too long for their data
    to be fetched, they are redirected to the Timeout page where they can
    either Try again, Try in person (if available), or Cancel.
    
    changelog: Upcoming Features, Identity Verification, Socure timeout provides users with options.
    
    * updates to copy for timeout page, specs
    * fix the timeout template, check for in_person_enabled?
    * gymnastics to enter the IPP flow
    * renamed warning spec to timeout spec
    * log an event on choosing In Person
    * spec for socure_errors_controller
    * Move the direct_to_in_person route to the DocumentCaptureController.
    Rename Idv::Socure::SocureErrorsController to Idv::Socure::ErrorsController
    * feature spec: time out takes you to the Try Again page
    * remove outdated spec
    * reorg document_capture_controller_spec.rb to include `it`s before `context`s
    * spec for document_capture_controller#direct_in_person
    * remove `go_in_person` from socure errors concern
    * refactoring two socure error controllers into one
    * hybrid path can't use errors_controller - no idv_session
    
    ---------
    
    Co-authored-by: John Maxwell <john.maxwell@gsa.gov>
    Co-authored-by: Amir Reavis-Bey <amir.reavis-bey@gsa.gov>

--- app/controllers/idv/document_capture_controller.rb
+++ app/controllers/idv/document_capture_controller.rb
@@ -8,12 +8,12 @@ module Idv
     include IdvStepConcern
     include StepIndicatorConcern
 
-    before_action :confirm_not_rate_limited, except: [:update]
+    before_action :confirm_not_rate_limited, except: [:update, :direct_in_person]
     before_action :confirm_step_allowed, unless: -> { allow_direct_ipp? }
     before_action :override_csp_to_allow_acuant
     before_action :set_usps_form_presenter
     before_action -> { redirect_to_correct_vendor(Idp::Constants::Vendors::LEXIS_NEXIS, false) },
-                  only: :show
+                  only: [:show], unless: -> { allow_direct_ipp? }
 
     def show
       analytics.idv_doc_auth_document_capture_visited(**analytics_arguments)
@@ -43,6 +43,44 @@ module Idv
       end
     end
 
+    # Given that the start of the IPP flow is in the TrueID doc_auth React app,
+    # we need a generic, direct way to start the IPP flow
+    def direct_in_person
+      attributes = {
+        remaining_submit_attempts: rate_limiter.remaining_count,
+      }.merge(ab_test_analytics_buckets)
+      analytics.idv_in_person_direct_start(**attributes)
+
+      redirect_to idv_document_capture_url(step: :idv_doc_auth)
+    end
+
+    def self.step_info
+      Idv::StepInfo.new(
+        key: :document_capture,
+        controller: self,
+        next_steps: [:ssn, :ipp_ssn], # :ipp_state_id
+        preconditions: ->(idv_session:, user:) {
+          idv_session.flow_path == 'standard' && (
+            # mobile
+            idv_session.skip_doc_auth_from_handoff ||
+              idv_session.skip_hybrid_handoff ||
+              idv_session.skip_doc_auth_from_how_to_verify ||
+              !idv_session.selfie_check_required || # desktop but selfie not required
+              idv_session.desktop_selfie_test_mode_enabled?
+          )
+        },
+        undo_step: ->(idv_session:, user:) do
+          idv_session.pii_from_doc = nil
+          idv_session.invalidate_in_person_pii_from_user!
+          idv_session.had_barcode_attention_error = nil
+          idv_session.had_barcode_read_failure = nil
+          idv_session.selfie_check_performed = nil
+        end,
+      )
+    end
+
+    private
+
     def extra_view_variables
       {
         document_capture_session_uuid: document_capture_session_uuid,
@@ -58,33 +96,6 @@ module Idv
         acuant_sdk_upgrade_a_b_testing_variables,
       )
     end
-
-    def self.step_info
-      Idv::StepInfo.new(
-        key: :document_capture,
-        controller: self,
-        next_steps: [:ssn, :ipp_ssn], # :ipp_state_id
-        preconditions: ->(idv_session:, user:) {
-                         idv_session.flow_path == 'standard' && (
-                           # mobile
-                           idv_session.skip_doc_auth_from_handoff ||
-                           idv_session.skip_hybrid_handoff ||
-                            idv_session.skip_doc_auth_from_how_to_verify ||
-                            !idv_session.selfie_check_required || # desktop but selfie not required
-                             idv_session.desktop_selfie_test_mode_enabled?
-                         )
-                       },
-        undo_step: ->(idv_session:, user:) do
-          idv_session.pii_from_doc = nil
-          idv_session.invalidate_in_person_pii_from_user!
-          idv_session.had_barcode_attention_error = nil
-          idv_session.had_barcode_read_failure = nil
-          idv_session.selfie_check_performed = nil
-        end,
-      )
-    end
-
-    private
 
     def analytics_arguments
       {
@@ -102,8 +113,9 @@ module Idv
       return false unless idv_session.welcome_visited &&
                           idv_session.idv_consent_given?
       # not allowed when no step param and action:show(get request)
-      return false if params[:step].blank? || params[:action].to_s != 'show' ||
-                      idv_session.flow_path == 'hybrid'
+      return false if params[:step].blank?
+      return false if params[:action].to_s != 'show' && params[:action] != 'direct_in_person'
+      return false if idv_session.flow_path == 'hybrid'
       # Only allow direct access to document capture if IPP available
       return false unless IdentityConfig.store.in_person_doc_auth_button_enabled &&
                           Idv::InPersonConfig.enabled_for_issuer?(decorated_sp_session.sp_issuer)
@@ -117,6 +129,10 @@ module Idv
 
     def set_usps_form_presenter
       @presenter = Idv::InPerson::UspsFormPresenter.new
+    end
+
+    def rate_limiter
+      RateLimiter.new(user: idv_session.current_user, rate_limit_type: :idv_doc_auth)
     end
   end
 end
