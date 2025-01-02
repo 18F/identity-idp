f8066ca9c9a4a4e1e9550e2c818b153f94c9a3df
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-01-02 17:48:13 -0500

    undo spec preconditions change

--- app/controllers/idv/socure/document_capture_controller.rb
+++ app/controllers/idv/socure/document_capture_controller.rb
@@ -98,7 +98,8 @@ module Idv
               idv_session.skip_doc_auth_from_handoff ||
               idv_session.skip_hybrid_handoff ||
               idv_session.skip_doc_auth_from_how_to_verify ||
-              !idv_session.selfie_check_required)
+              !idv_session.selfie_check_required ||
+              idv_session.desktop_selfie_test_mode_enabled?)
           },
           undo_step: ->(idv_session:, user:) do
             idv_session.pii_from_doc = nil
