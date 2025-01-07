0b5ee9b00372c828eebb775eeca8286c5ca3846a
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-01-07 10:33:31 -0500

    fix yaml ordering and reset bad_password spec

--- spec/features/visitors/bad_password_spec.rb
+++ spec/features/visitors/bad_password_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Visitor signs in with bad passwords and gets locked out' do
   include ActionView::Helpers::DateHelper
   let(:user) { create(:user, :fully_registered) }
-  let(:sign_in_failure) { 'badpassword' }
+  let(:bad_password) { 'badpassword' }
   let(:window) { IdentityConfig.store.max_sign_in_failures_window_in_seconds.seconds }
 
   scenario 'visitor tries too many bad passwords gets locked out then waits window seconds' do
@@ -13,7 +13,7 @@ RSpec.feature 'Visitor signs in with bad passwords and gets locked out' do
       link_html: t('devise.failure.invalid_link_text'),
     )
     IdentityConfig.store.max_sign_in_failures.times do
-      fill_in_credentials_and_submit(user.email, sign_in_failure)
+      fill_in_credentials_and_submit(user.email, bad_password)
       expect(page).to have_content(error_message)
       expect(page).to have_current_path(new_user_session_path)
     end
@@ -22,7 +22,7 @@ RSpec.feature 'Visitor signs in with bad passwords and gets locked out' do
     visit new_user_session_path
     2.times do
       freeze_time do
-        fill_in_credentials_and_submit(user.email, sign_in_failure)
+        fill_in_credentials_and_submit(user.email, bad_password)
 
         expect(page).to have_current_path(new_user_session_path)
         new_time = Time.zone.at(locked_at) + window
@@ -50,7 +50,7 @@ RSpec.feature 'Visitor signs in with bad passwords and gets locked out' do
     end
 
     travel_to(IdentityConfig.store.max_sign_in_failures_window_in_seconds.seconds.from_now) do
-      fill_in_credentials_and_submit(user.email, sign_in_failure)
+      fill_in_credentials_and_submit(user.email, bad_password)
       expect(page).to have_content(error_message)
       fill_in_credentials_and_submit(user.email, user.password)
       expect(page).to have_current_path(login_two_factor_path(otp_delivery_preference: 'sms'))
