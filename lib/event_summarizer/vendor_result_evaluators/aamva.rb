ae56c014664ea8a53bded4b191f202f03fa5e70e
Matt Hinz <matt.hinz@gsa.gov>
2025-01-08 11:47:33 -0800

    Add script to build narratives from event logs (#11691)
    * Initial cloudwatch query script to summarize events
    
    * Query cloudwatch and get user events
    
    * add timestamp remove limit
    
    * [Hackathon] Allow sourcing events from stdin (#11619)
    
    * Allow sourcing events from stdin
    
    It may be useful sometimes to take a local cache of cloudwatch events and pipe them into this command.
    
    [skip changelog]
    
    * Add 'limit: 10000' to CW query
    
    This is required for `complete` to work
    
    * [Hackathon] Add ExampleMatcher (#11622)
    
    * Add ExampleMatcher
    
    Add an example matcher that just counts events and outputs how many it saw.
    
    [skip changelog]
    
    * Remove excess whitespace
    
    * Add frozen_string_literal: true
    
    * use optparse to allow command options/defaults (#11627)
    
    * [HACKATHON] Initial crack at an IdV matcher (#11624)
    
    * Initial crack at an IdV matcher
    
    Matcher is a state machine that collects IDV "attempts" as they happen and tries to suss out interesting things about them.
    
    [skip changelog]
    
    * removed unused method
    
    ---------
    
    Co-authored-by: Douglas Price <douglas.price@gsa.gov>
    
    * [HACKATHON] Output formatting tweaks (#11635)
    
    * Normalize @timestamp to UTC for each event
    
    Pre-parse it in the script so that matchers don't have to worry about it
    
    * Slightly improve output
    
    - Include timestamps where possible
    
    [skip changelog]
    
    * [HACKATHON] Minor tweaks (#11637)
    
    * Don't crash if no events found
    
    * Tweak handling of --end-date
    
    - Use a dash rather than underscore
    - Make sure we respect it if it's passed in
    
    * Sort events on stdin before processing
    
    Events from Cloudwatch queries will be sorted, but stdin is not guaranteed.
    
    Processing unsorted events can lead to weird, weird, outcomes
    
    * report on TrueID success/failure (#11638)
    
    * Try to identify IDV abandonment (#11639)
    
    If the user:
    
    - Has not completed the initial workflow and
    - Does not have an idv-related event new that 1 hour
    
    Call their attempt abandoned
    
    * Login hackathon 2024 user narrative account deletion (#11629)
    
    * include timestamp
    
    * add account deletion narrative matcher
    
    * remove unneeded matcher requirement
    
    * add deletion matcher
    
    * lint
    
    * rename account deletion
    
    * read events from file without changing stdin
    
    * remove ipp from gpo code submission event
    
    * update example documentation in script
    
    * Update lib/event_summarizer/vendor_result_evaluators/aamva.rb
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Start writing a spec
    
    * Tidy up logic in IV result evaluator
    
    * Set event['name'] if not already set
    
    * Fix typo
    
    * Use Eastern time zone by default
    
    * Update pluralization code + add spec
    
    * Start on spec for summarize-user-events command
    
    * Protect rubocop's delicate sensibilities
    
    * Add more specs
    
    Add some specs around option parsing, time parsing, and actually running the program
    
    * Look at banner michael
    
    ---------
    
    Co-authored-by: Malick Diarra <malick.diarra@gsa.gov>
    Co-authored-by: Doug Price <douglas.price@gsa.gov>
    Co-authored-by: Eileen <eileenmcfarland@navapbc.com>
    Co-authored-by: eileen-nava <80347702+eileen-nava@users.noreply.github.com>
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

---
+++ lib/event_summarizer/vendor_result_evaluators/aamva.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+
+module EventSummarizer
+  module VendorResultEvaluators
+    module Aamva
+      ID_TYPES = {
+        'state_id_card' => 'non-driving ID card',
+        'drivers_license' => "drivers' license",
+      }.freeze
+
+      # TODO: Load these from the AAMVA proofer or put them somewhere common
+
+      REQUIRED_VERIFICATION_ATTRIBUTES = %i[
+        state_id_number
+        dob
+        last_name
+        first_name
+      ].freeze
+
+      REQUIRED_IF_PRESENT_ATTRIBUTES = [:state_id_expiration].freeze
+
+      # @param result {Hash} The result structure logged to Cloudwatch
+      # @return [Hash] A Hash with a type, timestamp, and description key.
+      def self.evaluate_result(result)
+        if result['success']
+          return {
+            type: :aamva_success,
+            description: 'AAMVA call succeeded',
+          }
+        end
+
+        if result['timed_out']
+          return {
+            type: :aamva_timed_out,
+            description: 'AAMVA request timed out.',
+          }
+        end
+
+        if result['mva_exception']
+          state = result['state_id_jurisdiction']
+          return {
+            type: :aamva_mva_exception,
+            # rubocop:disable Layout/LineLength
+            description: "AAMVA request failed because the MVA in #{state} failed to return a response.",
+            # rubocop:enable Layout/LineLength
+          }
+        end
+
+        if result['exception']
+
+          description = 'AAMVA request resulted in an exception'
+
+          m = /ExceptionText: (.+?),/.match(result['exception'])
+          if m.present?
+            description = "#{description} (#{m[1]})"
+          end
+
+          return {
+            type: :aamva_exception,
+            description:,
+          }
+        end
+
+        # The API call failed because of actual errors in the user's data.
+        # Try to come up with an explanation
+
+        explanation = explain_errors(result) || 'Check logs for more info.'
+
+        return {
+          type: :aamva_error,
+          description: "AAMVA request failed. #{explanation}",
+        }
+      end
+
+      def self.explain_errors(result)
+        # The values in the errors object are arrays
+        attributes = {}
+        result['errors'].each do |key, values|
+          attributes[key] = values.first
+        end
+
+        id_type = ID_TYPES[result['state_id_type']] || 'id card'
+        state = result['state_id_jurisdiction']
+
+        if mva_says_invalid_id_number?(attributes)
+          # rubocop:disable Layout/LineLength
+          return "The ID # from the user's #{id_type} was invalid according to the state of #{state}"
+          # rubocop:enable Layout/LineLength
+        end
+
+        failed_attributes = relevant_failed_attributes(attributes)
+
+        if !failed_attributes.empty?
+          plural = failed_attributes.length == 1 ? '' : 's'
+
+          # rubocop:disable Layout/LineLength
+          "#{failed_attributes.length} attribute#{plural} failed to validate: #{failed_attributes.join(', ')}"
+          # rubocop:enable Layout/LineLength
+        end
+      end
+
+      def self.mva_says_invalid_id_number?(attributes)
+        # When all attributes are marked "MISSING", except ID number,
+        # which is marked "UNVERIFIED", that indicates the MVA could not
+        # find the ID number to compare PII
+
+        missing_count = attributes.count do |_attr, status|
+          status == 'MISSING'
+        end
+
+        attributes['state_id_number'] == 'UNVERIFIED' && missing_count == attributes.count - 1
+      end
+
+      def self.relevant_failed_attributes(attributes)
+        failed_attributes = Set.new
+
+        REQUIRED_VERIFICATION_ATTRIBUTES.each do |attr|
+          failed_attributes << attr if attributes[attr] != 'VERIFIED'
+        end
+
+        REQUIRED_IF_PRESENT_ATTRIBUTES.each do |attr|
+          failed_attributes << attr if attributes[attr].present? && attributes[attr] != 'VERIFIED'
+        end
+
+        failed_attributes
+      end
+    end
+  end
+end
