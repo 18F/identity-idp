d2362294cab4b75f9c4e50063378ff0c6836698c
Jean Lucas <jean@4ray.co>
2025-01-12 00:05:05 -0500

    Update flake dependencies

--- flake.nix
+++ flake.nix
@@ -23,11 +23,14 @@
 
           mkShell {
             buildInputs = [
+              ruby
+              yarn
               openssl.dev
               postgresql.dev
               libyaml.dev
               zlib.dev
-            ];
+              goreman # Use goreman since nginx launch will fail gracefully and launch Puma, as opposed to when using foreman
+             ];
 
             shellHook = ''
               export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.postgresql.dev}/lib/pkgconfig:${pkgs.libyaml.dev}/lib/pkgconfig:${pkgs.zlib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH";
