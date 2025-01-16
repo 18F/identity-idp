8c772ddfe6a6f94606c893bc9d5ec40e7b58def5
Malick DiarrA <malick.diarra@gsa.gov>
2025-01-16 12:05:30 -0500

    remove proofing component schema

--- db/schema.rb
+++ db/schema.rb
@@ -470,26 +470,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_06_232958) do
     t.index ["user_id", "active"], name: "index_profiles_on_user_id_and_active", unique: true, where: "(active = true)"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
-
-  create_table "proofing_components", force: :cascade do |t|
-    t.integer "user_id", null: false, comment: "sensitive=false"
-    t.string "document_check", comment: "sensitive=false"
-    t.string "document_type", comment: "sensitive=false"
-    t.string "source_check", comment: "sensitive=false"
-    t.string "resolution_check", comment: "sensitive=false"
-    t.string "address_check", comment: "sensitive=false"
-    t.datetime "verified_at", precision: nil, comment: "sensitive=false"
-    t.datetime "created_at", precision: nil, null: false, comment: "sensitive=false"
-    t.datetime "updated_at", precision: nil, null: false, comment: "sensitive=false"
-    t.string "liveness_check", comment: "sensitive=false"
-    t.string "device_fingerprinting_vendor", comment: "sensitive=false"
-    t.boolean "threatmetrix", comment: "sensitive=false"
-    t.string "threatmetrix_review_status", comment: "sensitive=false"
-    t.string "threatmetrix_risk_rating", comment: "sensitive=false"
-    t.string "threatmetrix_policy_score", comment: "sensitive=false"
-    t.index ["user_id"], name: "index_proofing_components_on_user_id", unique: true
-    t.index ["verified_at"], name: "index_proofing_components_on_verified_at"
-  end
   
   create_table "registration_logs", force: :cascade do |t|
     t.integer "user_id", null: false, comment: "sensitive=false"
