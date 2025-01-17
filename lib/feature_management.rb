5631633e815e5031e120a0fdccabaefaaa36c933
Tim Spencer <timothy.spencer@gsa.gov>
2025-01-17 09:59:02 -0800

    ruby syntax is funny

--- lib/feature_management.rb
+++ lib/feature_management.rb
@@ -34,9 +34,9 @@ class FeatureManagement
   end
 
   def self.prefill_otp_codes_allowed_in_sandbox?
-    (Identity::Hostdata.domain == ('identitysandbox.gov')
-      || Identity::Hostdata.domain.end_with? ('.identitysandbox.gov'))
-      && telephony_test_adapter?
+    (Identity::Hostdata.domain == 'identitysandbox.gov' ||
+      Identity::Hostdata.domain.end_with?('.identitysandbox.gov')) &&
+      telephony_test_adapter?
   end
 
   def self.enable_load_testing_mode?
@@ -65,8 +65,8 @@ class FeatureManagement
   end
 
   def self.current_env_allowed_to_see_gpo_code?
-    (Identity::Hostdata.domain == ('identitysandbox.gov')
-      || Identity::Hostdata.domain.end_with? ('.identitysandbox.gov'))
+    (Identity::Hostdata.domain == ('identitysandbox.gov') ||
+      Identity::Hostdata.domain.end_with? ('.identitysandbox.gov'))
   end
 
   def self.show_demo_banner?
