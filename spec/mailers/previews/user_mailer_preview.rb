e8999172f1e1e3d70beb25b8c4f11d96a6be0ec4
Zach Margolis <zachmargolis@users.noreply.github.com>
2025-01-23 09:42:41 -0800

    Remove Faker use in UserMailerPreview (#11791)
    - Mailer previews occasionally load in production environments (sandbox)
      but in #11757, we moved the Faker gem to the test group (which breaks
      these previews in sandbox)
    - Removing the usage of Faker in the previews helps keep the previews
      working, and keeps the production dependencies smaller
    
    changelog: Internal, Source code, Remove gem dependency from mailer previews

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -295,8 +295,8 @@ class UserMailerPreview < ActionMailer::Preview
         devices: [
           unsaveable(
             Device.new(
-              user_agent: Faker::Internet.user_agent,
-              last_ip: Faker::Internet.ip_v4_address,
+              user_agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', # rubocop:disable Layout/LineLength
+              last_ip: '8.8.8.8',
             ),
           ),
         ],
