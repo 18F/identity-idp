3987266d6a2c8c3a5612f6dfc9e63ed151f0a246
A Shukla <abir.shukla@gsa.gov>
2025-01-23 11:29:52 -0600

    Lg 15159 A user who has reached the capture complete page in hybrid flow should not be able to recapture (#11782)
    * Bug Fixes, socure, user who reached capture complete page should not be able to recapture
    
    * changelog: Bug Fixes, socure, user who reached capture complete page should not be able to recapture
    
    * Addressing pr comments for improved code styling

--- app/controllers/idv/hybrid_mobile/socure/document_capture_controller.rb
+++ app/controllers/idv/hybrid_mobile/socure/document_capture_controller.rb
@@ -11,6 +11,7 @@ module Idv
         include SocureErrorsConcern
 
         check_or_render_not_found -> { IdentityConfig.store.socure_docv_enabled }
+        before_action :validate_step_not_completed, only: [:show]
         before_action :check_valid_document_capture_session, except: [:update]
         before_action -> do
           redirect_to_correct_vendor(Idp::Constants::Vendors::SOCURE, in_hybrid_mobile: true)
@@ -83,6 +84,11 @@ module Idv
         end
 
         private
+
+        def validate_step_not_completed
+          return if stored_result.blank? || !stored_result.success?
+          redirect_to idv_hybrid_mobile_capture_complete_url
+        end
 
         def socure_errors_presenter(result)
           SocureErrorPresenter.new(
