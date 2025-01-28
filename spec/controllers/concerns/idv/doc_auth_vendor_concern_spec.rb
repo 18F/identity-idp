a37f0ff126e3b50dc4451115bd874f5190261d26
Alex Bradley <alexander.bradley@gsa.gov>
2025-01-28 16:36:23 -0500

    add test for every method in this concern

--- spec/controllers/concerns/idv/doc_auth_vendor_concern_spec.rb
+++ spec/controllers/concerns/idv/doc_auth_vendor_concern_spec.rb
@@ -23,15 +23,89 @@ RSpec.describe Idv::DocAuthVendorConcern, :controller do
         .and_return(bucket)
     end
 
-    context 'bucket is socure' do
+    context 'bucket is LexisNexis' do
+      let(:bucket) { :lexis_nexis }
+
+      it 'returns lexis nexis as the vendor' do
+        expect(controller.doc_auth_vendor).to eq(Idp::Constants::Vendors::LEXIS_NEXIS)
+      end
+    end
+
+    context 'bucket is Mock' do
+      let(:bucket) { :mock }
+
+      it 'returns mock as the vendor' do
+        expect(controller.doc_auth_vendor).to eq(Idp::Constants::Vendors::MOCK)
+      end
+    end
+
+    context 'bucket is Socure' do
       let(:bucket) { :socure }
 
-      it 'should return socure as the vendor' do
+      it 'returns socure as the vendor' do
         expect(controller.doc_auth_vendor).to eq(Idp::Constants::Vendors::SOCURE)
       end
 
       it 'adds a user to the socure redis set' do
         expect { controller.doc_auth_vendor }.to change { socure_user_set.count }.by(1)
+      end
+    end
+
+    context 'facial match is required' do
+      let(:bucket) { :socure }
+      let(:acr_values) do
+        [
+          Saml::Idp::Constants::IAL2_BIO_REQUIRED_AUTHN_CONTEXT_CLASSREF,
+          Saml::Idp::Constants::DEFAULT_AAL_AUTHN_CONTEXT_CLASSREF,
+        ].join(' ')
+      end
+
+      before do
+        allow(IdentityConfig.store)
+          .to receive(:doc_auth_vendor_switching_enabled).and_return(true)
+        allow(IdentityConfig.store)
+          .to receive(:doc_auth_vendor_lexis_nexis_percent).and_return(50)
+        allow(controller).to receive(:resolved_authn_context_result).and_return(true)
+        resolved_authn_context = AuthnContextResolver.new(
+          user: user,
+          service_provider: nil,
+          vtr: nil,
+          acr_values: acr_values,
+        ).result
+        allow(controller).to receive(:resolved_authn_context_result)
+          .and_return(resolved_authn_context)
+      end
+
+      it 'returns Lexis Nexis as the vendor' do
+        expect(controller.doc_auth_vendor).to eq(Idp::Constants::Vendors::LEXIS_NEXIS)
+      end
+    end
+  end
+
+  describe '#doc_auth_vendor_enabled?' do
+    let(:vendor) { Idp::Constants::Vendors::LEXIS_NEXIS }
+
+    context 'doc_auth_vendor_switching is false' do
+      before do
+        allow(IdentityConfig.store)
+          .to receive(:doc_auth_vendor_switching_enabled).and_return(false)
+      end
+
+      it 'returns false' do
+        expect(controller.doc_auth_vendor_enabled?(vendor)).to eq false
+      end
+    end
+
+    context 'Lexis Nexis is disabled' do
+      before do
+        allow(IdentityConfig.store)
+          .to receive(:doc_auth_vendor_switching_enabled).and_return(true)
+        allow(IdentityConfig.store)
+          .to receive(:doc_auth_vendor_lexis_nexis_percent).and_return(0)
+      end
+
+      it 'returns false' do
+        expect(controller.doc_auth_vendor_enabled?(vendor)).to eq false
       end
     end
   end
