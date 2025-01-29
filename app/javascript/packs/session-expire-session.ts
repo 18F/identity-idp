7a4692997c52705bf2991a411aa52b230e73615c
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-01-29 17:14:36 -0500

    move login page to its own ts file. reduce constant hammering at session status

--- app/javascript/packs/session-expire-session.ts
+++ app/javascript/packs/session-expire-session.ts
@@ -1,67 +1,39 @@
-import { forceRedirect } from '@18f/identity-url';
 import { requestSessionStatus, extendSession } from '@18f/identity-session';
-import type { SessionStatus } from '@18f/identity-session';
 import type { CountdownElement } from '@18f/identity-countdown/countdown-element';
 import type { ModalElement } from '@18f/identity-modal';
 
-const warningEl = document.getElementById('session-timeout-cntnr');
+const warningEl = document.getElementById('session-timeout-cntnr')!;
 
-const defaultTime = '60';
-
-const frequency = parseInt(warningEl?.dataset.frequency || defaultTime, 10) * 1000;
-const warning = parseInt(warningEl?.dataset.warning || defaultTime, 10) * 1000;
-const start = parseInt(warningEl?.dataset.start || defaultTime, 10) * 1000;
-const timeoutURL = warningEl?.dataset.timeoutUrl!;
-const sessionsURL = warningEl?.dataset.sessionsUrl!;
-const sessionTimeout = parseInt(warningEl?.dataset.sessionTimeoutIn || defaultTime, 10) * 1000;
-
+const warning = Number(warningEl.dataset.warning!) * 1000;
+const sessionsURL = warningEl.dataset.sessionsUrl!;
+const sessionTimeout = Number(warningEl.dataset.sessionTimeoutIn!) * 1000;
 const modal = document.querySelector<ModalElement>('lg-modal.session-timeout-modal')!;
 const keepaliveEl = document.getElementById('session-keepalive-btn');
 const countdownEls: NodeListOf<CountdownElement> = modal.querySelectorAll('lg-countdown');
 
-let now = new Date();
-
-let sessionTime = new Date(now.getTime() + sessionTimeout);
-
-function success({ isLive, timeout }: SessionStatus) {
-  if (!isLive && timeoutURL) {
-    if (timeoutURL) {
-      forceRedirect(timeoutURL);
-    }
-    return;
-  }
-
-  const timeRemaining = timeout
-    ? timeout.valueOf() - Date.now()
-    : sessionTime.valueOf() - Date.now();
-  const showWarning = timeRemaining < warning;
-  if (showWarning) {
-    modal.show();
-    countdownEls.forEach((countdownEl) => {
-      countdownEl.expiration = timeout || sessionTime;
-      countdownEl.start();
-    });
-  }
-
-  const nextPingTimeout =
-    timeRemaining > 0 && timeRemaining < frequency ? timeRemaining : frequency;
-
-  // Disable reason: circular dependency between ping and success
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  setTimeout(ping, nextPingTimeout);
-}
+let sessionExpiration = new Date(Date.now() + sessionTimeout);
 
 const ping = () => {
-  requestSessionStatus(sessionsURL).then(success);
+  requestSessionStatus(sessionsURL);
 };
+
+function showModal() {
+  modal.show();
+  setInterval(ping, warning / 2);
+  countdownEls.forEach((countdownEl) => {
+    countdownEl.expiration = sessionExpiration;
+    countdownEl.start();
+  });
+}
 
 function keepalive() {
   modal.hide();
-  now = new Date();
-  sessionTime = new Date(now.getTime() + sessionTimeout);
+  requestSessionStatus(sessionsURL);
+  sessionExpiration = new Date(Date.now() + sessionTimeout);
+  setTimeout(showModal, sessionTimeout - warning);
   countdownEls.forEach((countdownEl) => countdownEl.stop());
   extendSession(sessionsURL);
 }
 
 keepaliveEl?.addEventListener('click', keepalive, false);
-setTimeout(ping, start);
+setTimeout(showModal, sessionTimeout - warning);
---
+++ app/javascript/packs/session-timeout-ping.ts
@@ -0,0 +1,56 @@
+import { forceRedirect } from '@18f/identity-url';
+import { requestSessionStatus, extendSession } from '@18f/identity-session';
+import type { SessionStatus } from '@18f/identity-session';
+import type { CountdownElement } from '@18f/identity-countdown/countdown-element';
+import type { ModalElement } from '@18f/identity-modal';
+
+const warningEl = document.getElementById('session-timeout-cntnr');
+
+const defaultTime = '60';
+
+const frequency = parseInt(warningEl?.dataset.frequency || defaultTime, 10) * 1000;
+const warning = parseInt(warningEl?.dataset.warning || defaultTime, 10) * 1000;
+const start = parseInt(warningEl?.dataset.start || defaultTime, 10) * 1000;
+const timeoutURL = warningEl?.dataset.timeoutUrl!;
+const sessionsURL = warningEl?.dataset.sessionsUrl!;
+
+const modal = document.querySelector<ModalElement>('lg-modal.session-timeout-modal')!;
+const keepaliveEl = document.getElementById('session-keepalive-btn');
+const countdownEls: NodeListOf<CountdownElement> = modal.querySelectorAll('lg-countdown');
+
+function success({ isLive, timeout }: SessionStatus) {
+  if (!isLive) {
+    if (timeoutURL) {
+      forceRedirect(timeoutURL);
+    }
+    return;
+  }
+
+  const timeRemaining = timeout.valueOf() - Date.now();
+  const showWarning = timeRemaining < warning;
+  if (showWarning) {
+    modal.show();
+    countdownEls.forEach((countdownEl) => {
+      countdownEl.expiration = timeout;
+      countdownEl.start();
+    });
+  }
+
+  const nextPingTimeout =
+    timeRemaining > 0 && timeRemaining < frequency ? timeRemaining : frequency;
+
+  // Disable reason: circular dependency between ping and success
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
+  setTimeout(ping, nextPingTimeout);
+}
+
+const ping = () => requestSessionStatus(sessionsURL).then(success);
+
+function keepalive() {
+  modal.hide();
+  countdownEls.forEach((countdownEl) => countdownEl.stop());
+  extendSession(sessionsURL);
+}
+
+keepaliveEl?.addEventListener('click', keepalive, false);
+setTimeout(ping, start);
