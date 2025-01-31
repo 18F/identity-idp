1901b3631b76d91f883d76ac17ff1b12c1df25b2
Malick DiarrA <malick.diarra@gsa.gov>
2025-01-31 11:22:08 -0500

    changelog: Internal, Sign in Analyticsg, Tab tracking analytics event added

---
+++ app/javascript/packs/tab-navigation-tracking-tracking.ts
@@ -0,0 +1,12 @@
+import { trackEvent} from '@18f/identity-analytics';
+
+const signInButton = document.getElementById('sign-in');
+
+const signUpButton = document.getElementById('sign-up');
+signInButton?.addEventListener('click', () => {
+
+})
+
+signUpButton?.addEventListener('click', () => {
+    
+})
