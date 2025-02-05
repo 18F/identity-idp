454cc73fc8751092f1c729ca7fb8b2d24312b513
Mitchell Henke <mitchell.henke@gsa.gov>
2025-02-05 09:22:17 -0600

    Reduce object allocations in identity verification
    changelog: Internal, Performance, Reduce object allocations in identity verification

--- app/policies/idv/flow_policy.rb
+++ app/policies/idv/flow_policy.rb
@@ -5,6 +5,36 @@ module Idv
     attr_reader :idv_session, :user
 
     FINAL = :final
+    STEPS =
+      {
+        root: Idv::StepInfo.new(
+          key: :root,
+          controller: AccountsController,
+          next_steps: [:welcome, :request_letter],
+          preconditions: ->(idv_session:, user:) { true },
+          undo_step: ->(idv_session:, user:) { true },
+        ),
+        welcome: Idv::WelcomeController.step_info,
+        agreement: Idv::AgreementController.step_info,
+        how_to_verify: Idv::HowToVerifyController.step_info,
+        hybrid_handoff: Idv::HybridHandoffController.step_info,
+        link_sent: Idv::LinkSentController.step_info,
+        document_capture: Idv::DocumentCaptureController.step_info,
+        socure_document_capture: Idv::Socure::DocumentCaptureController.step_info,
+        socure_errors: Idv::Socure::ErrorsController.step_info,
+        ipp_address: Idv::InPerson::AddressController.step_info,
+        ssn: Idv::SsnController.step_info,
+        ipp_ssn: Idv::InPerson::SsnController.step_info,
+        verify_info: Idv::VerifyInfoController.step_info,
+        ipp_verify_info: Idv::InPerson::VerifyInfoController.step_info,
+        address: Idv::AddressController.step_info,
+        phone: Idv::PhoneController.step_info,
+        phone_errors: Idv::PhoneErrorsController.step_info,
+        otp_verification: Idv::OtpVerificationController.step_info,
+        request_letter: Idv::ByMail::RequestLetterController.step_info,
+        enter_password: Idv::EnterPasswordController.step_info,
+        personal_key: Idv::PersonalKeyController.step_info,
+      }.freeze
 
     def initialize(idv_session:, user:)
       @idv_session = idv_session
@@ -44,35 +74,7 @@ module Idv
     end
 
     def steps
-      {
-        root: Idv::StepInfo.new(
-          key: :root,
-          controller: AccountsController,
-          next_steps: [:welcome, :request_letter],
-          preconditions: ->(idv_session:, user:) { true },
-          undo_step: ->(idv_session:, user:) { true },
-        ),
-        welcome: Idv::WelcomeController.step_info,
-        agreement: Idv::AgreementController.step_info,
-        how_to_verify: Idv::HowToVerifyController.step_info,
-        hybrid_handoff: Idv::HybridHandoffController.step_info,
-        link_sent: Idv::LinkSentController.step_info,
-        document_capture: Idv::DocumentCaptureController.step_info,
-        socure_document_capture: Idv::Socure::DocumentCaptureController.step_info,
-        socure_errors: Idv::Socure::ErrorsController.step_info,
-        ipp_address: Idv::InPerson::AddressController.step_info,
-        ssn: Idv::SsnController.step_info,
-        ipp_ssn: Idv::InPerson::SsnController.step_info,
-        verify_info: Idv::VerifyInfoController.step_info,
-        ipp_verify_info: Idv::InPerson::VerifyInfoController.step_info,
-        address: Idv::AddressController.step_info,
-        phone: Idv::PhoneController.step_info,
-        phone_errors: Idv::PhoneErrorsController.step_info,
-        otp_verification: Idv::OtpVerificationController.step_info,
-        request_letter: Idv::ByMail::RequestLetterController.step_info,
-        enter_password: Idv::EnterPasswordController.step_info,
-        personal_key: Idv::PersonalKeyController.step_info,
-      }
+      STEPS
     end
 
     def step_allowed?(key:)
