696a7d91343df9199aeb785f426cfb8abbb78e3b
Malick DiarrA <malick.diarra@gsa.gov>
2025-02-06 10:35:35 -0500

    Update observer component

--- app/components/click_observer_component.rb
+++ app/components/click_observer_component.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 class ClickObserverComponent < BaseComponent
-  attr_reader :event_name, :tag_options
+  attr_reader :event_name, :tag_options, :payload
 
-  def initialize(event_name:, **tag_options)
+  def initialize(event_name:, payload: {}, **tag_options)
     @event_name = event_name
+    @payload = payload
     @tag_options = tag_options
   end
 
   def call
-    content_tag(:'lg-click-observer', content, 'event-name': @event_name, **tag_options)
+    content_tag(
+      :'lg-click-observer', content, 'event-name': @event_name,
+                                     data: { payload: payload }, **tag_options
+    )
   end
 end
