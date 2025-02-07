1109872927db5a178358aff74ce87de582eb4c83
Malick DiarrA <malick.diarra@gsa.gov>
2025-02-07 08:31:38 -0500

    update spacing

--- app/components/click_observer_component.rb
+++ app/components/click_observer_component.rb
@@ -3,20 +3,19 @@
 class ClickObserverComponent < BaseComponent
   attr_reader :event_name, :tag_options, :payload
 
-  def initialize(event_name:, payload: {}, **tag_options)
+  def initialize(event_name:, payload: nil, **tag_options)
     @event_name = event_name
-    if payload.is_a?(Hash)
-      @payload = payload
-    else
-      @payload = {}
-    end
+    @payload = payload
     @tag_options = tag_options
   end
 
   def call
     content_tag(
-      :'lg-click-observer', content, 'event-name': @event_name,
-                                     data: { payload: payload }, **tag_options
+      :'lg-click-observer',
+      content,
+      'event-name': @event_name,
+      data: { payload: payload },
+      **tag_options,
     )
   end
 end
