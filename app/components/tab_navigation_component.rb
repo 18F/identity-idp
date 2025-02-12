46d68beaf852a957802c802603475df756d3f378
Andrew Duthie <andrew.duthie@gsa.gov>
2025-02-12 08:51:27 -0500

    Update design of Sign In/Create Account tabs
    changelog: User-Facing Improvements, Sign In, Improve design clarity of "Sign In" / "Create an Account" tabs

--- app/components/tab_navigation_component.rb
+++ app/components/tab_navigation_component.rb
@@ -10,10 +10,35 @@ class TabNavigationComponent < BaseComponent
   end
 
   def current_path?(path)
-    recognized_path = Rails.application.routes.recognize_path(path, method: request.method)
-    request.params[:controller] == recognized_path[:controller] &&
-      request.params[:action] == recognized_path[:action]
-  rescue ActionController::RoutingError
-    false
+    @current_path ||= {}
+    if !@current_path.key?(path)
+      @current_path[path] = begin
+        recognized_path = Rails.application.routes.recognize_path(path, method: request.method)
+        request.params[:controller] == recognized_path[:controller] &&
+          request.params[:action] == recognized_path[:action]
+      rescue ActionController::RoutingError
+        false
+      end
+    end
+
+    @current_path[path]
+  end
+
+  private
+
+  def nav_list_item(route, &block)
+    if current_path?(route[:path])
+      render(
+        ClickObserverComponent.new(
+          event_name: 'tab_navigation_current_page_clicked',
+          payload: { path: route[:path] },
+          role: 'listitem',
+          class: 'usa-button-group__item display-list-item',
+        ),
+        &block
+      )
+    else
+      tag.li(class: 'usa-button-group__item', &block)
+    end
   end
 end
