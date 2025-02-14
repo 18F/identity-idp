e428f313ce45842f32de300bd612a9038c96e534
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-02-14 08:36:00 -0500

    changelog: Internal, reCaptcha optimization, Load reCAPTCHA script earlier in page

--- app/components/captcha_submit_button_component.rb
+++ app/components/captcha_submit_button_component.rb
@@ -17,19 +17,6 @@ class CaptchaSubmitButtonComponent < BaseComponent
     IdentityConfig.store.recaptcha_mock_validator
   end
 
-  def recaptcha_script_src
-    return @recaptcha_script_src if defined?(@recaptcha_script_src)
-    @recaptcha_script_src =
-      if IdentityConfig.store.recaptcha_site_key.present?
-        UriService.add_params(
-          recaptcha_enterprise? ?
-            'https://www.google.com/recaptcha/enterprise.js' :
-            'https://www.google.com/recaptcha/api.js',
-          render: IdentityConfig.store.recaptcha_site_key,
-        )
-      end
-  end
-
   def recaptcha_enterprise?
     FeatureManagement.recaptcha_enterprise?
   end
--- spec/components/captcha_submit_button_component_spec.rb
+++ spec/components/captcha_submit_button_component_spec.rb
@@ -45,22 +45,6 @@ RSpec.describe CaptchaSubmitButtonComponent, type: :component do
         "lg-captcha-submit-button[recaptcha-site-key='#{recaptcha_site_key}']",
       )
     end
-
-    it 'renders script tag for recaptcha' do
-      src = "https://www.google.com/recaptcha/api.js?render=#{recaptcha_site_key}"
-      expect(rendered).to have_css("script[src='#{src}']", visible: :all)
-    end
-
-    context 'with recaptcha enterprise' do
-      before do
-        allow(FeatureManagement).to receive(:recaptcha_enterprise?).and_return(true)
-      end
-
-      it 'renders script tag for recaptcha' do
-        src = "https://www.google.com/recaptcha/enterprise.js?render=#{recaptcha_site_key}"
-        expect(rendered).to have_css("script[src='#{src}']", visible: :all)
-      end
-    end
   end
 
   context 'with additional tag options' do
