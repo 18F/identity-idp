c8d5df608bc040f6ba6d5aa91ff2a9ee11889d60
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-02-14 13:29:35 -0500

    move recaptcha script to a yield preceeding the stylesheets

--- spec/views/devise/sessions/new.html.erb_spec.rb
+++ spec/views/devise/sessions/new.html.erb_spec.rb
@@ -273,29 +273,6 @@ RSpec.describe 'devise/sessions/new.html.erb' do
           ),
         )
       end
-
-      context 'with configured recaptcha site key' do
-        let(:recaptcha_site_key) { 'site_key' }
-        before do
-          allow(IdentityConfig.store).to receive(:recaptcha_site_key).and_return(recaptcha_site_key)
-        end
-
-        it 'renders script tag for recaptcha' do
-          src = "https://www.google.com/recaptcha/api.js?render=#{recaptcha_site_key}"
-          expect(rendered).to have_css("script[src='#{src}']", visible: :all)
-        end
-
-        context 'with recaptcha enterprise' do
-          before do
-            allow(FeatureManagement).to receive(:recaptcha_enterprise?).and_return(true)
-          end
-
-          it 'renders script tag for recaptcha' do
-            src = "https://www.google.com/recaptcha/enterprise.js?render=#{recaptcha_site_key}"
-            expect(rendered).to have_css("script[src='#{src}']", visible: :all)
-          end
-        end
-      end
     end
   end
 end
