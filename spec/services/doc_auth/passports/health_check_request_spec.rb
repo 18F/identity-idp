cac5465c1d240adf4a52dc6c937806ac51c83042
John Maxwell <john.maxwell@gsa.gov>
2025-02-14 11:00:39 -0500

    Fix success?

--- spec/services/doc_auth/passports/health_check_request_spec.rb
+++ spec/services/doc_auth/passports/health_check_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DocAuth::Passports::HealthCheckRequest do
-  subject(:health_check) { described_class.new(analytics:) }
+  subject(:health_check_request) { described_class.new(analytics:) }
 
   let(:analytics) { FakeAnalytics.new }
 
@@ -10,14 +10,14 @@ RSpec.describe DocAuth::Passports::HealthCheckRequest do
   end
 
   describe '#fetch' do
-    let(:result) { subject.fetch }
+    let(:result) { health_check_request.fetch }
 
-    context "happy path" do
+    context 'happy path' do
       before do
         stub_request(:get, health_check_endpoint)
         result
       end
-      
+
       it 'hits the endpoint' do
         expect(WebMock).to have_requested(:get, health_check_endpoint)
       end
@@ -26,9 +26,51 @@ RSpec.describe DocAuth::Passports::HealthCheckRequest do
         expect(analytics).to have_logged_event(:passport_api_health_check)
       end
 
-      describe "the #fetch result" do
-        it "succeeds" do
+      describe 'the #fetch result' do
+        it 'succeeds' do
           expect(result).to be_success
+        end
+      end
+    end
+
+    context 'when Faraday raises an error' do
+      before do
+        stub_request(:get, health_check_endpoint).to_raise(Faraday::Error)
+        result
+      end
+
+      it 'hits the endpoint' do
+        expect(WebMock).to have_requested(:get, health_check_endpoint)
+      end
+
+      it 'logs the request' do
+        expect(analytics).to have_logged_event(:passport_api_health_check)
+      end
+
+      describe 'the #fetch result' do
+        it 'does not succeed' do
+          expect(result).not_to be_success
+        end
+      end
+    end
+
+    context 'when Faraday returns an HTTP error' do
+      before do
+        stub_request(:get, health_check_endpoint).to_return(status: 500)
+        result
+      end
+
+      it 'hits the endpoint' do
+        expect(WebMock).to have_requested(:get, health_check_endpoint)
+      end
+
+      it 'logs the request' do
+        expect(analytics).to have_logged_event(:passport_api_health_check)
+      end
+
+      describe 'the #fetch result' do
+        it 'does not succeed' do
+          expect(result).not_to be_success
         end
       end
     end
