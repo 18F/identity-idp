63ca218947e5c86469f005432127a4543efb4594
Andrew Duthie <andrew.duthie@gsa.gov>
2025-02-18 13:28:50 -0500

    Delegate annotate job logic to existing RecaptchaAnnotator

--- app/jobs/recaptcha_annotate_job.rb
+++ app/jobs/recaptcha_annotate_job.rb
@@ -3,8 +3,6 @@
 class RecaptchaAnnotateJob < ApplicationJob
   include GoodJob::ActiveJobExtensions::Concurrency
 
-  BASE_ENDPOINT = 'https://recaptchaenterprise.googleapis.com/v1'
-
   queue_as :low
 
   good_job_control_concurrency_with(
@@ -13,39 +11,6 @@ class RecaptchaAnnotateJob < ApplicationJob
   )
 
   def perform(assessment_id:, reason: nil, annotation: nil)
-    submit_annotation(assessment_id:, reason:, annotation:)
-  end
-
-  private
-
-  def submit_annotation(assessment_id:, reason:, annotation:)
-    request_body = { annotation:, reasons: reason && [reason] }.compact
-    faraday.post(annotation_url(assessment_id:), request_body) do |request|
-      request.options.context = { service_name: 'recaptcha_annotate' }
-    end
-  rescue Faraday::Error => error
-    NewRelic::Agent.notice_error(error)
-  end
-
-  def faraday
-    Faraday.new do |conn|
-      conn.options.timeout = IdentityConfig.store.recaptcha_request_timeout_in_seconds
-
-      conn.request :instrumentation, name: 'request_log.faraday'
-      conn.request :json
-      conn.response :json
-    end
-  end
-
-  def annotation_url(assessment_id:)
-    UriService.add_params(
-      format(
-        '%{base_endpoint}/%{assessment_id}:annotate',
-        base_endpoint: BASE_ENDPOINT,
-        project_id: IdentityConfig.store.recaptcha_enterprise_project_id,
-        assessment_id:,
-      ),
-      key: IdentityConfig.store.recaptcha_enterprise_api_key,
-    )
+    RecaptchaAnnotator.annotate(assessment_id:, reason:, annotation:)
   end
 end
