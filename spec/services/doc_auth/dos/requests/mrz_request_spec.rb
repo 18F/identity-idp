95f118fc826fcded9e4f7007e821ee26bc45fd87
Douglas Price <douglas.price@gsa.gov>
2025-02-20 17:38:28 -0500

    fixed spec

--- spec/services/doc_auth/dos/requests/mrz_request_spec.rb
+++ spec/services/doc_auth/dos/requests/mrz_request_spec.rb
@@ -6,6 +6,8 @@ MrzResponse = Struct.new(:body, :status, :success?)
 
 RSpec.describe DocAuth::Dos::Requests::MrzRequest do
   let(:mrz_endpoint) { 'https://dos.example.test/mrz' }
+  let(:client_id) { 'client_id' }
+  let(:client_secret) { 'client_secret' }
   let(:mrz) { '1234567890' }
   let(:request_id) { '1234567890' }
   let(:subject) do
@@ -17,14 +19,16 @@ RSpec.describe DocAuth::Dos::Requests::MrzRequest do
 
   before do
     allow(IdentityConfig.store).to receive(:dos_passport_mrz_endpoint).and_return(mrz_endpoint)
+    allow(IdentityConfig.store).to receive(:dos_passport_client_id).and_return(client_id)
+    allow(IdentityConfig.store).to receive(:dos_passport_client_secret).and_return(client_secret)
     stub_request(:post, mrz_endpoint)
       .with(
         body: "{\"mrz\":\"#{mrz}\",\"request_id\":\"#{request_id}\",\"category\":\"book\"}",
         headers: {
           'Accept' => '*/*',
           'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Client-Id' => 'client_id',
-          'Client-Secret' => 'client_secret',
+          'Client-Id' => client_id,
+          'Client-Secret' => client_secret,
           'Content-Type' => 'application/json',
           'User-Agent' => 'Faraday v2.12.2',
         },
