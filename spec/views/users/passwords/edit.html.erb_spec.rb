4aa47ddb1ac6c63c1742de68fb4564c1e19cd25e
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-02-20 13:41:27 -0500

    add id for password strength block and include in password field described-by attribute

--- spec/views/users/passwords/edit.html.erb_spec.rb
+++ spec/views/users/passwords/edit.html.erb_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe 'users/passwords/edit.html.erb' do
   it 'has aria described by' do
     render
 
-    expect(rendered).to have_selector('[aria-describedby="password-description"]')
+    expect(rendered).to have_selector('[aria-describedby="password-strength password-description"]')
   end
 
   context 'required password change' do
@@ -71,7 +71,9 @@ RSpec.describe 'users/passwords/edit.html.erb' do
     it 'aria described by is blank' do
       render
 
-      expect(rendered).to_not have_selector('[aria-describedby="password-description"]')
+      expect(rendered).to_not have_selector(
+        '[aria-describedby="password-strength password-description"]',
+      )
     end
   end
 end
