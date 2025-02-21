47fd564b2f9fd1a856c9e054b7321d46bdd528db
Malick DiarrA <malick.diarra@gsa.gov>
2025-02-21 11:22:45 -0500

    update sp check

--- app/controllers/concerns/fully_authenticatable.rb
+++ app/controllers/concerns/fully_authenticatable.rb
@@ -3,7 +3,11 @@
 module FullyAuthenticatable
   def delete_branded_experience(logout: false)
     ServiceProviderRequestProxy.delete(request_id)
-    session[:sp][:successful_handoff] = true
+    if session[:sp]
+      session[:sp][:successful_handoff] = true
+
+    end
+
     session[:sp] = {} if logout
     nil
   end
