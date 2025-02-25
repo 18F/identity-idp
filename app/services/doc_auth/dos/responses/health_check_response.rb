db33eec051092f70637cf144eef4c73ad85a718a
John Maxwell <john.maxwell@gsa.gov>
2025-02-25 12:02:54 -0500

    New Response class

---
+++ app/services/doc_auth/dos/responses/health_check_response.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module DocAuth
+  module Dos
+    module Responses
+      class HealthCheckResponse < DocAuth::Response
+        attr_accessor :faraday_response
+
+        def initialize(faraday_response:)
+          @faraday_response = faraday_response
+
+          super(
+            success: success_value,
+            extra: extra_value,
+            errors: errors_value,
+            exception: exception_value,
+          )
+        end
+
+        def success_value
+          return false if faraday_response.kind_of?(Faraday::Error)
+          parsed_body = JSON.parse(body)
+          parsed_body[:status].to_s.downcase == 'up'
+        end
+
+        def extra_value
+          {
+            body: body,
+          }
+        end
+
+        def body
+          faraday_response&.respond_to?(:body) && faraday_response.body
+        end
+
+        def errors_value
+          if faraday_response.kind_of?(Faraday::Error)
+            {
+              network: faraday_response.response_status,
+            }
+          else
+            {}
+          end
+        end
+
+        def exception_value
+          if faraday_response.is_a?(Faraday::Error)
+            faraday_response.inspect
+          end
+        end
+      end
+    end
+  end
+end
