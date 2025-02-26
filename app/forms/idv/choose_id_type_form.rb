0ea95dec4a48978a7dd67223917677ad782818f4
Alex Bradley <alexander.bradley@gsa.gov>
2025-02-26 13:42:27 -0500

    use idv_session variables for passports

--- app/forms/idv/choose_id_type_form.rb
+++ app/forms/idv/choose_id_type_form.rb
@@ -4,6 +4,10 @@ module Idv
   class ChooseIdTypeForm
     include ActiveModel::Model
 
+    attr_reader :chosen_id_type
+
+    validates :chosen_id_type, inclusion: { in: %w[drivers_license passport]}
+
     def initialize(chosen_id_type = :drivers_license)
       @chosen_id_type = chosen_id_type
     end
@@ -12,10 +16,6 @@ module Idv
       @chosen_id_type = params[:choose_id_type_preference]
 
       FormResponse.new(success: valid?, errors: errors)
-    end
-
-    def chosen_id_type
-      @chosen_id_type
     end
   end
 end
