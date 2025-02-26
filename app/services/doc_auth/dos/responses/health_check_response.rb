b46f5cfd83ed33aa6fd7ca6483cb7369dcd491b7
John Maxwell <john.maxwell@gsa.gov>
2025-02-26 09:13:05 -0500

    LG-15676 Passport API Health Check Infrastructure
    Created request and response classes for the DoS API health check endpoints.
    
    [skip changelog] (because it's a task)
    
    Co-authored-by: Douglas Price <douglas.price@gsa.gov>

---
+++ app/services/doc_auth/dos/responses/health_check_response.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module DocAuth
+  module Dos
+    module Responses
+      class HealthCheckResponse < DocAuth::Response
+        attr_accessor :faraday_response
+
+        def initialize(faraday_response:)
+          @faraday_response = faraday_response
+
+          super(
+            success:,
+            extra:,
+            errors:,
+            exception:,
+          )
+        end
+
+        def success
+          return false if faraday_error?
+          return false if !parsed_body
+          parsed_body[:status].to_s.downcase == 'up'
+        end
+
+        def extra
+          {
+            body: body,
+          }
+        end
+
+        def body
+          return faraday_response.response_body if faraday_response.respond_to?(:response_body)
+          return faraday_response.env.response_body if faraday_response.respond_to?(:env)
+        end
+
+        def parsed_body
+          body && JSON.parse(body, symbolize_names: true)
+        end
+
+        def errors
+          if faraday_error?
+            if faraday_response&.response_status
+              {
+                network: faraday_response.response_status,
+              }
+            else
+              { network: 'faraday exception' }
+            end
+          else
+            {}
+          end
+        end
+
+        def exception
+          if faraday_error?
+            faraday_response.inspect
+          end
+        end
+
+        def faraday_error?
+          faraday_response.is_a?(Faraday::Error)
+        end
+      end
+    end
+  end
+end
