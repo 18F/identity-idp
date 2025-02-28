04071d990a6f9348aa839343545c4dcb426abbef
John Maxwell <john.maxwell@gsa.gov>
2025-02-28 08:57:28 -0500

    Comments from Amir
    Switched to using doc_auth_vendor setting to enable /
    disable. Extracted _session_controls partial. Made Mock::Socure a real
    object instead of a pseudo-module.

--- lib/idp/constants.rb
+++ lib/idp/constants.rb
@@ -14,6 +14,7 @@ module Idp
       LEXIS_NEXIS = 'lexis_nexis'
       SOCURE = 'socure'
       MOCK = 'mock'
+      SOCURE_MOCK = 'mock_socure'
       USPS = 'usps'
       AAMVA = 'aamva'
       AAMVA_UNSUPPORTED_JURISDICTION = 'UnsupportedJurisdiction'
