af3bedf156bc14ff144f52adf89a84ae846ea8cd
Malick DiarrA <malick.diarra@gsa.gov>
2025-03-03 11:08:30 -0500

    change to user_uuid

--- app/jobs/account_creation_threat_metrix_job.rb
+++ app/jobs/account_creation_threat_metrix_job.rb
@@ -7,7 +7,7 @@ class AccountCreationThreatMetrixJob < ApplicationJob
     request_ip: nil,
     email: nil,
     uuid_prefix: nil,
-    uuid: nil
+    user_uuid: nil
   )
     device_profiling_result = AccountCreation::DeviceProfiling.new.proof(
       request_ip: request_ip,
