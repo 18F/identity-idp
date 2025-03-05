d2efe2e1b40872cc720fde7e6ddadb87c8167b89
ashukla <abir.shukla@gsa.gov>
2025-03-05 15:25:37 -0600

    started work on ticket

---
+++ app/services/pii/passport.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/MutableConstant
+module Pii
+    Passport = RedactedData.define(
+      :first_name,
+      :last_name,
+      :dob,
+      :birth_place,
+      :passport_expiration,
+      :issuing_country_code,
+      :mrz,
+      :passport_issued,
+      :nationality_code,
+      :personal_number,
+    )
+  end
+# rubocop:enable Style/MutableConstant
