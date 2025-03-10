029ddf58845cd6049c89d9024bfec896cc834cc3
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-03-10 12:27:37 -0400

    move passports_allowed to doc_auth_vendor_concern

--- app/controllers/idv/welcome_controller.rb
+++ app/controllers/idv/welcome_controller.rb
@@ -71,13 +71,5 @@ module Idv
         current_user,
       )
     end
-
-    def passport_allowed?
-      return if doc_auth_vendor == Idp::Constants::Vendors::SOCURE
-
-      if dos_passport_api_healthy?(analytics:)
-        idv_session.passport_allowed ||= (ab_test_bucket(:DOC_AUTH_PASSPORT) == :passport_enabled)
-      end
-    end
   end
 end
