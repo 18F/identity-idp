86dae92214c7f980ba8e4f10efcfbbe0bfe5e264
Jonathan Hooper <jonathan.hooper@gsa.gov>
2025-03-10 13:10:58 -0400

    LG-15573 Make the `--depth` argument required for IG data pull script (#11960)
    We often get requests for data that involve searching for accounts that share devices. These kinds of requests occur more often than not. Currently we default to not searching for shared devices. This has led to mistakes where we did not provide shared account data.
    
    This commit attempts to address the issue by making the `--depth` argument required. This will require investigators to explicitly opt-in or out of shared device searches.
    
    The `exit 1` in `lib/script_base.rb` was causing some rather opaque failures in CI as it sets the status code to 1 and caused some tests to end early rather without reporting whether they failed. The unhelpful message at the end of a CI run was Stopped processing SimpleCov as a previous error not related to SimpleCov has been detected. This only became an issue because the PR added a case where we raise an exception and it resulted in a non-zero status code.
    
    [skip changelog]
    
    Co-authored-by: Mitchell Henke <mitchell.henke@gsa.gov>

--- spec/lib/data_pull_spec.rb
+++ spec/lib/data_pull_spec.rb
@@ -110,7 +110,8 @@ RSpec.describe DataPull do
       let(:identity) { IdentityLinker.new(user, service_provider).link_identity }
 
       let(:argv) do
-        ['ig-request', identity.uuid, '--requesting-issuer', service_provider.issuer]
+        ['ig-request', identity.uuid, '--requesting-issuer', service_provider.issuer,
+         '--depth', '1']
       end
 
       it 'runs the data requests report and prints it as JSON' do
@@ -130,7 +131,7 @@ RSpec.describe DataPull do
       context 'with a UUID that is not found' do
         let(:uuid) { 'abcdef' }
         let(:argv) do
-          ['ig-request', uuid, '--requesting-issuer', service_provider.issuer]
+          ['ig-request', uuid, '--requesting-issuer', service_provider.issuer, '--depth', '1']
         end
 
         it 'returns an empty hash for that user' do
@@ -327,7 +328,9 @@ RSpec.describe DataPull do
       let(:service_provider) { create(:service_provider) }
       let(:identity) { IdentityLinker.new(user, service_provider).link_identity }
       let(:args) { [user.uuid] }
-      let(:config) { ScriptBase::Config.new(requesting_issuers: [service_provider.issuer]) }
+      let(:config) do
+        ScriptBase::Config.new(requesting_issuers: [service_provider.issuer], depth: 0)
+      end
 
       subject(:result) { subtask.run(args:, config:) }
 
@@ -348,7 +351,7 @@ RSpec.describe DataPull do
 
       context 'with SP UUID argument and no requesting issuer' do
         let(:args) { [identity.uuid] }
-        let(:config) { ScriptBase::Config.new }
+        let(:config) { ScriptBase::Config.new(depth: 0) }
 
         it 'runs the report with computed requesting issuer', aggregate_failures: true do
           expect(result.table).to be_nil
@@ -363,6 +366,14 @@ RSpec.describe DataPull do
 
           expect(result.subtask).to eq('ig-request')
           expect(result.uuids).to eq([user.uuid])
+        end
+      end
+
+      context 'without a depth argument' do
+        let(:config) { ScriptBase::Config.new }
+
+        it 'raises an error about the argument being required' do
+          expect { result }.to raise_error('Required argument --depth is missing')
         end
       end
     end
