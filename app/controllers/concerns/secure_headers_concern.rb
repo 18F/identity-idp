6356810e7ac99e18fb8c7be1e2d493fc87028cb2
Mitchell Henke <mitchell.henke@gsa.gov>
2025-03-11 08:55:09 -0500

    Remove OpenID Connect override configurations
    changelog: Internal, Maintenance, Remove OpenID Connect override configurations

--- app/controllers/concerns/secure_headers_concern.rb
+++ app/controllers/concerns/secure_headers_concern.rb
@@ -10,10 +10,7 @@ module SecureHeadersConcern
     authorize_form = OpenidConnectAuthorizeForm.new(authorize_params)
     return unless authorize_form.valid?
 
-    return if form_action_csp_disabled_and_not_server_side_redirect?(
-      issuer: authorize_form.service_provider.issuer,
-      user_uuid: current_user&.uuid,
-    )
+    return if form_action_csp_disabled_and_not_server_side_redirect?
 
     override_form_action_csp(csp_uris)
   end
