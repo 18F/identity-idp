50852e4d4b9b1f77a4754c7dd36581362ba4c1d7
ashukla <abir.shukla@gsa.gov>
2025-03-20 10:51:37 -0500

    Fixing spec

--- spec/features/idv/hybrid_mobile/hybrid_choose_id_type_spec.rb
+++ spec/features/idv/hybrid_mobile/hybrid_choose_id_type_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'mobile hybrid flow choose id type', :js, :allow_net_connect_on_start do
+RSpec.feature 'mobile hybrid flow choose id type', :js do
   include IdvHelper
   include IdvStepHelper
   include DocAuthHelper
@@ -11,6 +11,7 @@ RSpec.feature 'mobile hybrid flow choose id type', :js, :allow_net_connect_on_st
   before do
     allow(IdentityConfig.store).to receive(:doc_auth_passports_enabled).and_return(true)
     allow(IdentityConfig.store).to receive(:doc_auth_passports_percent).and_return(100)
+    allow(IdentityConfig.store).to receive(:doc_auth_vendor_default).and_return('mock')
     stub_request(:get, IdentityConfig.store.dos_passport_composite_healthcheck_endpoint)
       .to_return({ status: 200, body: { status: 'UP' }.to_json })
     allow(Telephony).to receive(:send_doc_auth_link).and_wrap_original do |impl, config|
@@ -18,25 +19,19 @@ RSpec.feature 'mobile hybrid flow choose id type', :js, :allow_net_connect_on_st
       impl.call(**config)
     end.at_least(1).times
     reload_ab_tests
-    sign_in_and_2fa_user
   end
 
   after do
     reload_ab_tests
   end
 
-  it 'proofs and hands off to mobile', js: true do
+  it 'choose id type screen before doc capture in hybrid flow and proceeds after passport select',
+     js: true do
     perform_in_browser(:desktop) do
+      sign_in_and_2fa_user
       complete_doc_auth_steps_before_hybrid_handoff_step
       clear_and_fill_in(:doc_auth_phone, phone_number)
       click_send_link
-
-      expect(page).to have_content(t('doc_auth.headings.text_message'))
-      expect(page).to have_content(t('doc_auth.info.you_entered'))
-      expect(page).to have_content('+1 415-555-0199')
-
-      # Confirm that Continue button is not shown when polling is enabled
-      expect(page).not_to have_content(t('doc_auth.buttons.continue'))
     end
 
     expect(@sms_link).to be_present
@@ -44,6 +39,29 @@ RSpec.feature 'mobile hybrid flow choose id type', :js, :allow_net_connect_on_st
     perform_in_browser(:mobile) do
       visit @sms_link
       expect(page).to have_current_path(idv_hybrid_mobile_choose_id_type_url)
+      choose(t('doc_auth.forms.id_type_preference.passport'))
+      click_on t('forms.buttons.continue')
+      expect(page).to have_current_path(idv_hybrid_mobile_document_capture_url)
+    end
+  end
+
+  it 'choose id type screen before doc capture in hybrid flow and proceeds after state id select',
+     js: true do
+    perform_in_browser(:desktop) do
+      sign_in_and_2fa_user
+      complete_doc_auth_steps_before_hybrid_handoff_step
+      clear_and_fill_in(:doc_auth_phone, phone_number)
+      click_send_link
+    end
+
+    expect(@sms_link).to be_present
+
+    perform_in_browser(:mobile) do
+      visit @sms_link
+      expect(page).to have_current_path(idv_hybrid_mobile_choose_id_type_url)
+      choose(t('doc_auth.forms.id_type_preference.drivers_license'))
+      click_on t('forms.buttons.continue')
+      expect(page).to have_current_path(idv_hybrid_mobile_document_capture_url)
     end
   end
 end
