af01b2208e4f1e3fa245b2a3439d838a126da303
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-03-24 10:12:54 -0400

    updated schema

--- db/primary_migrate/20250321182247_remove_phone_confirmed_at_from_users.rb
+++ db/primary_migrate/20250321182247_remove_phone_confirmed_at_from_users.rb
@@ -1,5 +1,5 @@
 class RemovePhoneConfirmedAtFromUsers < ActiveRecord::Migration[8.0]
   def change
-    remove_column :users, :phone_confirmed_at, :datetime
+    safety_assured { remove_column :users, :phone_confirmed_at, :datetime }
   end
 end
