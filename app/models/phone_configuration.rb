0ece6729a0f04161a88bd84eade643177e99be48
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-03-24 16:11:54 -0400

    back out from ignored_columns

--- app/models/phone_configuration.rb
+++ app/models/phone_configuration.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class PhoneConfiguration < ApplicationRecord
-  self.ignored_columns += ['confirmation_sent_at']
-  self.ignored_columns += ['confirmed_at']
-
   include EncryptableAttribute
 
   belongs_to :user, inverse_of: :phone_configurations
