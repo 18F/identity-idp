d0c314ad87fcfb1f368eaee6b00929bbf8f14896
Alex Kritikos <alex.kritikos@gsa.gov>
2025-04-10 16:04:55 -0500

    Remove unnecessary dependencies

--- devenv.nix
+++ devenv.nix
@@ -15,7 +15,6 @@ in
     ssm-session-manager-plugin
     aws-iam-authenticator
     yubikey-manager
-    terragrunt
     git
     jq
     postgresql
@@ -25,21 +24,8 @@ in
     pkgs-unstable.chromedriver
   ];
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
   cachix.enable = false;
   
-  # https://devenv.sh/languages/
-  languages.nix.enable = true;
-
-  # temporary workaround
-  # https://github.com/cachix/devenv/issues/792
-  # NIXPKGS_ALLOW_INSECURE=1 nix run --impure github:bobvanderlinden/nixpkgs-ruby\#'"ruby-3.0.5"'
-  # may also require this
-  # NIXPKGS_ALLOW_INSECURE=1 nix run --impure "nixpkgs#openssl_1_1"
-  # nix run "nixpkgs#clang_16"
-
   languages.ruby = {
     enable = true;
     bundler.enable = true;
@@ -52,12 +38,6 @@ in
     yarn.enable = true;
   };
 
-  languages.terraform = {
-    enable = true;
-    # version = "1.10.5";
-    version = "1.11.0";
-  };
-
   services.postgres = {
     enable = true;
     listen_addresses = "127.0.0.1";
@@ -66,8 +46,4 @@ in
   services.redis = {
     enable = true;
   };
-  
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
 }
