35f137ec1d6695be0880c25e7bf6330ea898eb6f
Davida Marion <davida.marion@gsa.gov>
2025-04-10 10:51:20 -0400

    Abc order tracker events

--- app/services/attempts_api/tracker_events.rb
+++ app/services/attempts_api/tracker_events.rb
@@ -162,6 +162,17 @@ module AttemptsApi
       )
     end
 
+    # Tracks when user submits registration password
+    # @param [String<'backup_code', 'otp', 'piv_cac', 'totp'>] mfa_device_type
+    # The user has exceeded the rate limit during enrollment
+    # and account has been locked
+    def mfa_enroll_code_rate_limited(mfa_device_type:)
+      track_event(
+        'mfa-enroll-code-rate-limited',
+        mfa_device_type:,
+      )
+    end
+
     # @param [Boolean] success
     # A user has attempted to enroll the TOTP MFA method to their account
     def mfa_enroll_totp(success:)
@@ -171,14 +182,21 @@ module AttemptsApi
       )
     end
 
-    # Tracks when user submits registration password
-    # @param [String<'backup_code', 'otp', 'piv_cac', 'totp'>] mfa_device_type
-    # The user has exceeded the rate limit during enrollment
-    # and account has been locked
-    def mfa_enroll_code_rate_limited(mfa_device_type:)
+    # @param [Boolean] success
+    # Tracks when the user has attempted to enroll the WebAuthn-Platform MFA method to their account
+    def mfa_enroll_webauthn_platform(success:)
       track_event(
-        'mfa-enroll-code-rate-limited',
-        mfa_device_type:,
+        'mfa-enroll-webauthn-platform',
+        success:,
+      )
+    end
+
+    # @param [Boolean] success
+    # Tracks when the user has attempted to enroll the WebAuthn MFA method to their account
+    def mfa_enroll_webauthn_roaming(success:)
+      track_event(
+        'mfa-enroll-webauthn-roaming',
+        success:,
       )
     end
 
