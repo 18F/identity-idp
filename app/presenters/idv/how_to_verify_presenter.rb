808405cf14a1bc4e87c222d9d9bc8fbe3e880b31
gina-yamada <gina.yamada@gsa.gov>
2025-04-14 09:21:33 -0600

    fix linter error

--- app/presenters/idv/how_to_verify_presenter.rb
+++ app/presenters/idv/how_to_verify_presenter.rb
@@ -86,7 +86,7 @@ class Idv::HowToVerifyPresenter
   def post_office_description
     if passport_allowed && IdentityConfig.store.in_person_passports_enabled
       t('doc_auth.info.verify_online_description_passport')
-    elsif  passport_allowed && !IdentityConfig.store.in_person_passports_enabled
+    elsif passport_allowed && !IdentityConfig.store.in_person_passports_enabled
       t('doc_auth.info.verify_at_post_office_description_passport_html')
     else
       ''
