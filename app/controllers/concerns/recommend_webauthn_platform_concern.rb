1404e6b02e6a8be3587347428b895d5e36448fa0
Jessica Dembe <jessica.dembe@gsa.gov>
2025-04-15 15:59:40 -0400

    add logic for path

--- app/controllers/concerns/recommend_webauthn_platform_concern.rb
+++ app/controllers/concerns/recommend_webauthn_platform_concern.rb
@@ -1,19 +1,8 @@
 # frozen_string_literal: true
 
 module RecommendWebauthnPlatformConcern
-  def recommend_webauthn_platform_for_sms_user?(bucket)
-    # Only consider for A/B test if:
-    # 1. Option would be offered for setup
-    # 2. User is viewing content in English
-    # 3. Other recommendations have not already been offered (e.g. PIV/CAC for federal emails)
-    # 4. User selected to setup phone or authenticated with phone
-    # 5. User has not already set up a platform authenticator
-    return false if !device_supports_platform_authenticator_setup?
-    return false if I18n.locale != :en
-    return false if current_user.webauthn_platform_recommended_dismissed_at?
-    return false if !user_set_up_or_authenticated_with_phone?
-    return false if current_user.webauthn_configurations.platform_authenticators.present?
-    ab_test_bucket(:RECOMMEND_WEBAUTHN_PLATFORM_FOR_SMS_USER) == bucket
+  def recommend_webauthn_platform_for_sms_user?
+    device_supports_platform_authenticator_setup? && user_set_up_with_phone? && user_set_up_with_phone?
   end
 
   private
@@ -26,15 +15,7 @@ module RecommendWebauthnPlatformConcern
     user_session[:in_account_creation_flow] == true
   end
 
-  def user_set_up_or_authenticated_with_phone?
-    if in_account_creation_flow?
-      current_user.phone_configurations.any? do |phone_configuration|
-        phone_configuration.mfa_enabled? && phone_configuration.delivery_preference == 'sms'
-      end
-    else
-      auth_methods_session.auth_events
-        .pluck(:auth_method)
-        .include?(TwoFactorAuthenticatable::AuthMethod::SMS)
-    end
+  def user_set_up_with_phone?
+    user_session[:in_account_creation_flow] == true && MfaContext.new(current_user).enabled_mfa_methods_count == 1
   end
 end
