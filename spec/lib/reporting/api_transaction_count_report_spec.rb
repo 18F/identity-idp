4e6424ff70e1c23b90ea2b92c2ab453520786b82
KolawoleHOseni <kolawolehoseni@fcoh2j-gqn94y2t.gsa.gov>
2025-04-15 21:04:02 -0500

    Spec Update

--- spec/lib/reporting/api_transaction_count_report_spec.rb
+++ spec/lib/reporting/api_transaction_count_report_spec.rb
@@ -1,36 +1,45 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 require 'reporting/api_transaction_count_report'
 
 RSpec.describe Reporting::ApiTransactionCountReport do
-  let(:mock_time_range) do
-    Time.zone.parse('2024-04-01')..Time.zone.parse('2024-04-07')
+  let(:time_range) { Date.new(2022, 1, 1).in_time_zone('UTC').all_month }
+  let(:mock_results) do
+    [
+      { 'uuid' => '123', 'id' => '1', 'sp' => 'SP1' },
+      { 'uuid' => '456', 'id' => '2', 'sp' => 'SP2' },
+      { 'uuid' => '789', 'id' => '3', 'sp' => 'SP3' },
+    ]
   end
 
-  let(:mock_results) { Array.new(5) { { id: SecureRandom.uuid } } }
+  let(:expected_api_transaction_count_table) do
+    [
+      ['Week', 'True ID', 'Instant verify', 'Phone Finder', 'Acuant', 'Socure',
+       'Fraud Score and Attribute', 'Instant Verify', 'Threat Metrix'],
+      ["#{time_range.begin.to_date} - #{time_range.end.to_date}", 10, 15, 20, 25, 30, 35, 40, 45],
+    ]
+  end
 
-  subject(:report) { described_class.new(time_range: mock_time_range) }
+  subject(:report) { Reporting::ApiTransactionCountReport.new(time_range:) }
 
   before do
     allow_any_instance_of(Reporting::CloudwatchClient).to receive(:fetch).and_return(mock_results)
   end
 
-  # describe '#api_transaction_count' do
-  #   it 'returns an array with correct headers and values' do
-  #     table = report.api_transaction_count
+  describe '#api_transaction_count' do
+    it 'returns an array with correct headers and values' do
+      table = report.api_transaction_count
 
-  #     expect(table).to be_an(Array)
-  #     expect(table.size).to eq(2)
+      expect(table).to be_an(Array)
+      expect(table.size).to eq(2)
 
-  #     header_row = table.first
-  #     data_row = table.last
+      header_row = table.first
+      data_row = table.last
 
-  #     expect(header_row).to include('Week', 'True ID', 'Instant verify', 'Phone Finder', 'Acuant')
-  #     expect(data_row.first).to eq('2024-04-01 - 2024-04-07')
-  #     expect(data_row[1..].all? { |val| val.is_a?(Integer) || val.is_a?(Array) }).to be true
-  #   end
-  # end
+      expect(header_row).to include('Week', 'True ID', 'Instant verify', 'Phone Finder', 'Acuant')
+      expect(data_row.first).to eq("#{time_range.begin.to_date} - #{time_range.end.to_date}")
+      expect(data_row[1..].all? { |val| val.is_a?(Integer) || val.is_a?(Array) }).to be true
+    end
+  end
 
   describe '#to_csvs' do
     it 'generates valid CSV output' do
@@ -40,8 +49,8 @@ RSpec.describe Reporting::ApiTransactionCountReport do
       expect(csvs.size).to eq(1)
 
       csv = csvs.first
-      expect(csv).to include('Week', 'True ID', 'Instant verify')
-      expect(csv).to include('2024-04-01 - 2024-04-07')
+      expect(csv).to include('Week,True ID,Instant verify')
+      expect(csv).to include("#{time_range.begin.to_date} - #{time_range.end.to_date}")
     end
   end
 
