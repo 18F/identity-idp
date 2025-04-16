5e777945a0d73016bc31d2f280d632f190c19f4e
Davida Marion <davida.marion@gsa.gov>
2025-04-16 14:56:43 -0400

    Fix more tests

--- spec/controllers/idv/in_person/verify_info_controller_spec.rb
+++ spec/controllers/idv/in_person/verify_info_controller_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Idv::InPerson::VerifyInfoController do
 
   before do
     stub_analytics
+    stub_attempts_tracker
     stub_sign_in(user)
     subject.idv_session.flow_path = 'standard'
     subject.idv_session.ssn = Idp::Constants::MOCK_IDV_APPLICANT_SAME_ADDRESS_AS_ID[:ssn]
