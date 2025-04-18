7d45fa8b68f9b65712a6f4310d0878a2767879ea
Jessica Dembe <jessica.dembe@gsa.gov>
2025-04-18 11:18:24 -0400

    change how user session is added

--- app/controllers/users/webauthn_platform_recommended_controller.rb
+++ app/controllers/users/webauthn_platform_recommended_controller.rb
@@ -15,21 +15,12 @@ module Users
 
     def create
       analytics.webauthn_platform_recommended_submitted(opted_to_add: opted_to_add?)
-      store_webauthn_platform_recommended_in_session if opted_to_add?
+      user_session[:webauthn_platform_recommended] = true if opted_to_add?
       current_user.update(webauthn_platform_recommended_dismissed_at: Time.zone.now)
       redirect_to dismiss_redirect_path
     end
 
     private
-
-    def store_webauthn_platform_recommended_in_session
-      user_session[:webauthn_platform_recommended] =
-        if in_account_creation_flow?
-          :account_creation
-        else
-          :authentication
-        end
-    end
 
     def opted_to_add?
       params[:add_method].present?
--- spec/controllers/users/webauthn_platform_recommended_controller_spec.rb
+++ spec/controllers/users/webauthn_platform_recommended_controller_spec.rb
@@ -117,19 +117,14 @@ RSpec.describe Users::WebauthnPlatformRecommendedController do
         expect(response).to redirect_to(webauthn_setup_path(platform: true))
       end
 
-      it 'assigns recommended session value to recommendation flow' do
-        expect { response }.to change { controller.user_session[:webauthn_platform_recommended] }
-          .from(nil).to(:authentication)
-      end
-
       context 'user is creating account' do
         before do
           allow(controller).to receive(:in_account_creation_flow?).and_return(true)
         end
 
-        it 'assigns recommended session value to recommendation flow' do
+        it 'adds recommended session value during recommendation flow' do
           expect { response }.to change { controller.user_session[:webauthn_platform_recommended] }
-            .from(nil).to(:account_creation)
+            .from(nil).to(true)
         end
       end
     end
