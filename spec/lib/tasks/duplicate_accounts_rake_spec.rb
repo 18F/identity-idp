e877d493465190e9d10555c945dd957d60ce799e
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-04-21 10:49:28 -0400

    add test for a rake with results

--- spec/lib/tasks/duplicate_accounts_rake_spec.rb
+++ spec/lib/tasks/duplicate_accounts_rake_spec.rb
@@ -8,12 +8,30 @@ RSpec.describe 'duplicate accounts rake task' do
   end
 
   describe 'duplicate_accounts:report' do
-    let(:task) { 'duplicate_accounts:report' }
-
     context 'with an empty report array' do
+      let(:task) { 'duplicate_accounts:report' }
       it 'displays a no results found message' do
         expect { Rake::Task[task].invoke }.to \
           output("no results found\n").to_stdout
+      end
+    end
+
+    context 'with a duplicate account shown in report array' do
+      let(:task) { 'duplicate_accounts:report' }
+      let(:results) do
+        [
+          {
+            uuid: 'abc123def456',
+            service_provider: 'AAA:Test:SP:localhost',
+            friendly_name: 'AAA Test SP',
+            latest_activity: Date.yesterday.middle_of_day,
+            activated_at: Date.yesterday.beginning_of_day,
+          },
+        ]
+      end
+      it 'displays a result' do
+        expect { Rake::Task[task].invoke }.to \
+          output("result to csv\n").to_stdout
       end
     end
   end
