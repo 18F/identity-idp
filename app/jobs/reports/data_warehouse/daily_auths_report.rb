6724d5a18e7fdc4f23155d3615638aaffa5b3a95
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-04-21 12:10:14 -0500

    files
    update config
    
    update connector
    
    update identity config
    
    LG-15974: Add document_type to in_person_enrollments table (#12096)
    
    Used to pass the type of ID to downstream IPP vendor
    
    Sub-task: LG-16126
    
    [skip changelog]
    
    files
    
    Add table to record user confirmation of duplicate profiles (#12081)
    
    changelog: Upcoming Features, One Account, Add table to record user confirmation of duplicate profiles
    
    LG-15974: Add document_type to in_person_enrollments table (#12096)
    
    Used to pass the type of ID to downstream IPP vendor
    
    Sub-task: LG-16126
    
    [skip changelog]
    
    files
    
    LG-15974: Add document_type to in_person_enrollments table (#12096)
    
    Used to pass the type of ID to downstream IPP vendor
    
    Sub-task: LG-16126
    
    [skip changelog]
    
    config
    
    turn off config
    
    turn off config
    
    update
    
    turn off health check
    
    update config
    
    stop data-warehouse
    
    undo gem
    
    blank model
    
    turn on the WAR HOUSE
    
    gemfile
    
    lock
    
    update typo
    
    turn off warehouse
    
    turn off
    
    remove dw ref
    
    undo adapter
    
    redshift again
    
    update the WARHOUSE
    
    use store
    
    update config
    
    redshift maybe
    
    report house
    
    make instance
    
    update prefix
    
    update configuration hash
    
    ssl mode
    
    root cert, search paths
    
    rm ssl
    
    rm unused controller

---
+++ app/jobs/reports/data_warehouse/daily_auths_report.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+module Reports
+  module DataWarehouse
+    class DailyAuthsReport < BaseReport
+      REPORT_NAME = 'dw-daily-auths-report'
+
+      attr_reader :report_date
+
+      def perform(report_date)
+        @report_date = report_date
+
+        _latest, path = generate_s3_paths(REPORT_NAME, 'json', now: report_date)
+        body = report_body.to_json
+
+        [
+          bucket_name, # default reporting bucket
+          IdentityConfig.store.s3_public_reports_enabled && public_bucket_name,
+        ].select(&:present?)
+          .each do |bucket_name|
+          upload_file_to_s3_bucket(
+            path: path,
+            body: body,
+            content_type: 'application/json',
+            bucket: bucket_name,
+          )
+        end
+      end
+
+      def start
+        report_date.beginning_of_day
+      end
+
+      def finish
+        report_date.end_of_day
+      end
+
+      def report_body
+        params = {
+          start: start,
+          finish: finish,
+        }.transform_values { |v| ActiveRecord::Base.connection.quote(v) }
+
+        sql = format(<<-SQL, params)
+          SELECT
+            COUNT(*)
+          , sp_return_logs.ial
+          , sp_return_logs.issuer
+          , service_providers.iaa
+          , MAX(service_providers.friendly_name) AS friendly_name
+          , MAX(agencies.name) AS agency
+          FROM
+            idp.sp_return_logs
+          LEFT JOIN
+            idp.service_providers ON service_providers.issuer = sp_return_logs.issuer
+          LEFT JOIN
+            idp.agencies ON service_providers.agency_id = agencies.id
+          WHERE
+            sp_return_logs.returned_at::date BETWEEN %{start} AND %{finish}
+            AND sp_return_logs.billable = true
+          GROUP BY
+            sp_return_logs.ial
+          , sp_return_logs.issuer
+          , service_providers.iaa
+        SQL
+
+        results = data_warehouse_transaction_with_timeout do
+          ActiveRecord::Base.connection.execute(sql)
+        end
+
+        {
+          start: start,
+          finish: finish,
+          results: results.as_json,
+        }
+      end
+    end
+  end
+end
