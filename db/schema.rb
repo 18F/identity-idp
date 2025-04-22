c2ad4b0ec6e13142e73a0f7a8f4544f3cb373326
Lauren George <lauren.george@gsa.gov>
2025-04-22 14:24:14 -0400

    LG-15974: Add document_type to in_person_enrollments table
    Used to pass the type of ID to downstream IPP vendor
    
    Sub-task: LG-16126
    
    [skip changelog]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_16_181519) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_22_182109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -347,6 +347,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_16_181519) do
     t.datetime "last_batch_claimed_at", comment: "sensitive=false"
     t.string "sponsor_id", null: false, comment: "sensitive=false"
     t.string "doc_auth_result", comment: "sensitive=false"
+    t.integer "document_type", comment: "sensitive=false"
     t.index ["profile_id"], name: "index_in_person_enrollments_on_profile_id"
     t.index ["ready_for_status_check"], name: "index_in_person_enrollments_on_ready_for_status_check", where: "(ready_for_status_check = true)"
     t.index ["status_check_attempted_at"], name: "index_in_person_enrollments_on_status_check_attempted_at", where: "(status = 1)"
