8ae3d9644e3935ca522f688bfbfc00e71468511a
lmgeorge <lmgeorge@users.noreply.github.com>
2025-04-22 11:48:10 -0700

    LG-15974: Add document_type to in_person_enrollments table (#12096)
    Used to pass the type of ID to downstream IPP vendor
    
    Sub-task: LG-16126
    
    [skip changelog]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_27_195800) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_22_182109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
