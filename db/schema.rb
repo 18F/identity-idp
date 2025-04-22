3ea06973a7a58c3828f40c37e1ce7ce345ccf372
Vraj Mohan <vraj.mohan@gsa.gov>
2025-04-22 10:15:07 -0700

    Add table to record user confirmation of duplicate profiles (#12081)
    changelog: Upcoming Features, One Account, Add table to record user confirmation of duplicate profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_164321) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_16_181519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -206,6 +206,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_164321) do
     t.index ["socure_docv_transaction_token"], name: "index_socure_docv_transaction_token", unique: true
     t.index ["user_id"], name: "index_document_capture_sessions_on_user_id"
     t.index ["uuid"], name: "index_document_capture_sessions_on_uuid"
+  end
+
+  create_table "duplicate_profile_confirmations", force: :cascade do |t|
+    t.bigint "profile_id", null: false, comment: "sensitive=false"
+    t.datetime "confirmed_at", precision: nil, null: false, comment: "sensitive=false"
+    t.bigint "duplicate_profile_ids", null: false, comment: "sensitive=false", array: true
+    t.boolean "confirmed_all", comment: "sensitive=false"
+    t.datetime "created_at", null: false, comment: "sensitive=false"
+    t.datetime "updated_at", null: false, comment: "sensitive=false"
+    t.index ["profile_id"], name: "index_duplicate_profile_confirmations_on_profile_id"
   end
 
   create_table "email_addresses", force: :cascade do |t|
@@ -680,6 +690,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_164321) do
   end
 
   add_foreign_key "document_capture_sessions", "users"
+  add_foreign_key "duplicate_profile_confirmations", "profiles"
   add_foreign_key "iaa_gtcs", "partner_accounts"
   add_foreign_key "iaa_orders", "iaa_gtcs"
   add_foreign_key "in_person_enrollments", "profiles"
