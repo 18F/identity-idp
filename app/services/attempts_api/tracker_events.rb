23872c5ee580b9e75b3d1466547ed5f4bc9180f3
Davida Marion <davida.marion@gsa.gov>
2025-04-22 12:07:22 -0400

    Updates language as per code review

--- app/services/attempts_api/tracker_events.rb
+++ app/services/attempts_api/tracker_events.rb
@@ -11,7 +11,7 @@ module AttemptsApi
       )
     end
 
-    # A user is transferred to the in-person proofing workflow
+    # A user becomes able to visit the post office for in-person proofing
     def idv_ipp_ready_to_verify_visit
       track_event('idv-ipp-ready-to-verify-visit')
     end
