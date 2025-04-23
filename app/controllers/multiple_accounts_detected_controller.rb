85dca2dd132c56ca0b58d21245243ff628debe68
Malick DiarrA <malick.diarra@gsa.gov>
2025-04-23 10:55:27 -0400

    add show

--- app/controllers/multiple_accounts_detected_controller.rb
+++ app/controllers/multiple_accounts_detected_controller.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class MultipleAccountsDetectedController < ApplicationController
-    before_action :confirm_two_factor_authenticated
-  
-    def show
-    end
-  
-    def skip
-    end
+  before_action :confirm_two_factor_authenticated
 
-    def own_all_accounts
-
-    end
+  def show
+      
   end
+
+  def skip
+  end
+
+  def own_all_accounts
+
+  end
+end
   
