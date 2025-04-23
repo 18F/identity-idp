e22d55906c8f630b1fb44b207291f0122389ff85
John Maxwell <john.maxwell@gsa.gov>
2025-04-23 09:27:59 -0400

    More work on the Mock client to support passports

---
+++ app/services/doc_auth/mock/dos_passport_api_client.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module DocAuth
+  module Mock
+    class DosPassportApiClient
+      def initialize(lexis_nexis_response)
+        @lexis_nexis_response = lexis_nexis_response
+      end
+
+      def fetch
+        if lexis_nexis_response&.passport_check_result&.dig(:PassportCheckResult) == 'Fail'
+          DocAuth::Response.new(
+            success: false,
+            errors: { mrz: 'bad' },
+          )
+        else
+          DocAuth::Response.new(success: true)
+        end
+      end
+
+      private
+
+      attr_accessor :lexis_nexis_response
+    end
+  end
+end
