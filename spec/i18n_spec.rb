645d90f0d0d656c54e424cb34cc92c6b1ee25592
Jessica Dembe <jessica.dembe@gsa.gov>
2025-04-23 13:18:24 -0400

    LG-15837: Turn-on F/T unlock to SMS users - using security messaging variant (#12086)
    * make changes to text, language, and view
    
    changelog: Upcoming Features, face/touch unlock security, promote F/T unlock through security upsell
    
    * add logic for path
    
    * remove webauthn recommended concern, moved logic and tests
    
    * change how user session is added
    
    * change how mfa redirect happens after dismissing f/t unlock
    
    * fix first test
    
    * update french translation
    
    * restore tests
    
    * move test to make it a feature spec?
    
    * update test for mfa setup concern
    
    * make it so that feature is only on those setting up via sms
    
    * lintfix
    
    * make change on determining who gets recommended
    
    * re-name test suite
    
    * code cleanup

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -84,10 +84,6 @@ module I18n
         { key: 'time.formats.event_timestamp', locales: %i[zh] },
         { key: 'time.formats.full_date', locales: %i[es] }, # format is the same in Spanish and English
         { key: 'time.formats.sms_date' }, # for us date format
-        { key: 'webauthn_platform_recommended.cta' }, # English-only A/B test
-        { key: 'webauthn_platform_recommended.description_save_time' }, # English-only A/B test
-        { key: 'webauthn_platform_recommended.heading' }, # English-only A/B test
-        { key: 'webauthn_platform_recommended.skip' }, # English-only A/B test
       ].freeze
       # rubocop:enable Layout/LineLength
 
