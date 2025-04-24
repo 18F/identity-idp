6313a6d93b1f6844e9015c0b8baeb7d5ba049624
Davi (she/they) <davida.marion@gsa.gov>
2025-04-24 16:22:24 -0400

    Add idv-reproof event (#12103)
    * changelog: Upcoming Features, Attempts API, Add idv-reproof event

--- app/controllers/idv/enter_password_controller.rb
+++ app/controllers/idv/enter_password_controller.rb
@@ -163,6 +163,10 @@ module Idv
     end
 
     def confirm_no_profile_yet
+      if current_user.has_proofed_before?
+        attempts_api_tracker.idv_reproof
+      end
+
       # When no profile has been minted yet, keep them on this page.
       return if !idv_session.profile.present?
 
