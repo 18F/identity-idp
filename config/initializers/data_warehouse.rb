d861be123b01613542d7a09713fa7f41d54b6528
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-04-25 11:34:28 -0500

    stop data-warehouse

--- config/initializers/data_warehouse.rb
+++ config/initializers/data_warehouse.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 if IdentityConfig.store.data_warehouse_enabled
-  ActiveRecord::Base.establish_connection(:data_warehouse)
+  # ActiveRecord::Base.establish_connection(:data_warehouse)
 end
