ec3f5fc4b48ad2e72c16569642ade4b09657049b
gina-yamada <gina.yamada@gsa.gov>
2025-04-30 12:26:16 -0600

    update ipp passport controller specs

--- spec/controllers/idv/in_person/passport_controller_spec.rb
+++ spec/controllers/idv/in_person/passport_controller_spec.rb
@@ -10,93 +10,66 @@ RSpec.describe Idv::InPerson::PassportController do
   before do
     stub_sign_in(user)
     stub_up_to(:hybrid_handoff, idv_session: subject.idv_session)
+    allow(user).to receive(:establishing_in_person_enrollment).and_return(enrollment)
+    subject.user_session['idv/in_person'] = { pii_from_user: {} }
     stub_analytics
   end
 
   describe 'before_actions' do
     before do
-      allow(IdentityConfig.store).to receive(:doc_auth_passports_enable).and_return(true)
-      allow(IdentityConfig.store).to receive(:in_person_passports_enabled).and_return(true)
+      allow(idv_session).to receive(:in_person_passports_allowed?).and_return(true)
     end
 
     it 'includes before_action' do
-      expect(subject).to have_actions(:before, :render_404_if_controller_not_enabled)
+      expect(subject).to have_actions(:before, :confirm_step_allowed)
       expect(subject).to have_actions(:before, :initialize_pii_from_user)
     end
   end
 
   describe '#show' do
-    context 'when passports are not allowed' do
+    context 'when in person passports are not allowed' do
       before do
-        allow(IdentityConfig.store).to receive(:doc_auth_passports_enable).and_return(false)
+        allow(idv_session).to receive(:in_person_passports_allowed?).and_return(false)
       end
 
-      context 'when in person passports are not allowed' do
-        it 'does not log the idv_in_person_proofing_passport_visited event' do
-          expect(@analytics).to_not have_logged_event(
-            :idv_in_person_proofing_passport_visited,
-          )
-        end
+      it 'does not render the passport form' do
+        expect(response).to_not render_template 'idv/in_person/passport/show'
       end
 
-      context 'when in person passports are allowed' do
-        before do
-          allow(IdentityConfig.store).to receive(:in_person_passports_enabled).and_return(true)
-        end
-
-        it 'does not log the idv_in_person_proofing_passport_visited event' do
-          expect(@analytics).to_not have_logged_event(
-            :idv_in_person_proofing_passport_visited,
-          )
-        end
+      it 'does not log the idv_in_person_proofing_passport_visited event' do
+        expect(@analytics).to_not have_logged_event(:idv_in_person_proofing_passport_visited)
       end
     end
 
-    context 'when passports are allowed' do
+    context 'when in person passports are allowed' do
       before do
-        allow(IdentityConfig.store).to receive(:doc_auth_passports_enabled).and_return(true)
+        allow(idv_session).to receive(:in_person_passports_allowed?).and_return(true)
       end
 
-      context 'when in person passports are not allowed' do
-        before do
-          allow(IdentityConfig.store).to receive(:in_person_passports_enabled).and_return(false)
-        end
-
-        it 'does not log the idv_in_person_proofing_passport_visited event' do
-          expect(@analytics).to_not have_logged_event(:idv_in_person_proofing_passport_visited)
-        end
+      let(:analytics_arguments) do
+        {
+          step: 'passport',
+          analytics_id: 'In Person Proofing',
+          skip_hybrid_handoff: false,
+        }
       end
 
-      context 'when in person passports are allowed' do
-        before do
-          allow(IdentityConfig.store).to receive(:in_person_passports_enabled).and_return(true)
-        end
+      before do
+        subject.idv_session.opted_in_to_in_person_proofing =
+          analytics_arguments[:opted_in_to_in_person_proofing]
+        subject.idv_session.skip_hybrid_handoff = analytics_arguments[:skip_hybrid_handoff]
+        get :show
+      end
 
-        let(:analytics_arguments) do
-          {
-            step: 'passport',
-            analytics_id: 'In Person Proofing',
-            skip_hybrid_handoff: false,
-          }
-        end
+      it 'renders the passport form' do
+        expect(response).to render_template 'idv/in_person/passport/show'
+      end
 
-        before do
-          subject.idv_session.opted_in_to_in_person_proofing =
-            analytics_arguments[:opted_in_to_in_person_proofing]
-          subject.idv_session.skip_hybrid_handoff = analytics_arguments[:skip_hybrid_handoff]
-          get :show
-        end
-
-        it 'renders the passport form' do
-          expect(response).to render_template 'idv/in_person/passport/show'
-        end
-
-        it 'logs the idv_in_person_proofing_passport_visited event' do
-          expect(@analytics).to have_logged_event(
-            :idv_in_person_proofing_passport_visited,
-            analytics_arguments,
-          )
-        end
+      it 'logs the idv_in_person_proofing_passport_visited event' do
+        expect(@analytics).to have_logged_event(
+          :idv_in_person_proofing_passport_visited,
+          analytics_arguments,
+        )
       end
     end
   end
