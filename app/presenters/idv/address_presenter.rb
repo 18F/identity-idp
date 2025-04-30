f446021607741c562024aa5c023805c84b8300fc
A Shukla <abir.shukla@gsa.gov>
2025-04-30 15:20:37 -0500

    Lg-15447 support re enter mail address for verify by mail (#12116)
    * changelog: User-Facing Improvements, Doc Auth, support re enter mail address from verify by mail
    
    * Updating keys from title to heading
    
    * Added feature test
    
    * updating to gpo_letter_requested instead of requested_letter
    
    * Updating view spec

--- app/presenters/idv/address_presenter.rb
+++ app/presenters/idv/address_presenter.rb
@@ -2,6 +2,36 @@
 
 module Idv
   class AddressPresenter
+    def initialize(gpo_letter_requested:)
+      @gpo_letter_requested = gpo_letter_requested
+    end
+
+    attr_reader :gpo_letter_requested
+
+    def address_heading
+      if gpo_letter_requested
+        I18n.t('doc_auth.headings.mailing_address')
+      else
+        I18n.t('doc_auth.headings.address')
+      end
+    end
+
+    def address_info
+      if gpo_letter_requested
+        I18n.t('doc_auth.info.mailing_address')
+      else
+        I18n.t('doc_auth.info.address')
+      end
+    end
+
+    def form_button_text
+      if gpo_letter_requested
+        I18n.t('forms.buttons.continue')
+      else
+        I18n.t('forms.buttons.submit.update')
+      end
+    end
+
     def address_line1_hint
       "#{I18n.t('forms.example')} 150 Calle A Apt 3"
     end
