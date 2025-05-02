6557d66da1c17b8e02510a3f98f43039553d8965
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-05-02 10:05:12 -0500

    turn off

--- app/models/data_warehouse_record.rb
+++ app/models/data_warehouse_record.rb
@@ -2,6 +2,6 @@ class DataWarehouseRecord < ActiveRecord::Base
   self.abstract_class = true
 
   if Rails.env.production?
-    connects_to database: { reading: :data_warehouse }
+    # connects_to database: { reading: :data_warehouse }
   end
 end
