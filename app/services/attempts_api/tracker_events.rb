ef94994bdc6bd1b48e1a0119fd112aea2eeecbc0
Davi (she/they) <davida.marion@gsa.gov>
2025-05-07 11:42:43 -0400

    Adds forgot-password-email-sent event (#12150)
    * changelog: Upcoming Features, Attempts API, Adds forgot-password-email-sent event

--- app/services/attempts_api/tracker_events.rb
+++ app/services/attempts_api/tracker_events.rb
@@ -65,6 +65,15 @@ module AttemptsApi
       )
     end
 
+    # @param [String] email The user's email address
+    #  A user has requested a password reset.
+    def forgot_password_email_sent(email:)
+      track_event(
+        :forgot_password_email_sent,
+        email:,
+      )
+    end
+
     # @param [Boolean] success True if new password was successfully submitted
     # @param [Hash<Symbol,Array<Symbol>>] failure_reason
     # A user submits a new password have requesting a password reset
