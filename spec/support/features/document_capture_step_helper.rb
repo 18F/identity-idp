a8b80ede7560f646da2ee845ae9d9b61762c5249
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-05-10 07:56:16 -0400

    happy linting

--- spec/support/features/document_capture_step_helper.rb
+++ spec/support/features/document_capture_step_helper.rb
@@ -112,11 +112,17 @@ module DocumentCaptureStepHelper
   end
 
   def stub_docv_verification_data_pass(docv_transaction_token:, reason_codes: nil)
-    stub_docv_verification_data(body: SocureDocvFixtures.pass_json(reason_codes:), docv_transaction_token:)
+    stub_docv_verification_data(
+      body: SocureDocvFixtures.pass_json(reason_codes:),
+      docv_transaction_token:,
+    )
   end
 
   def stub_docv_verification_data_fail_with(docv_transaction_token:, reason_codes:)
-    stub_docv_verification_data(body: SocureDocvFixtures.fail_json(reason_codes:), docv_transaction_token:)
+    stub_docv_verification_data(
+      body: SocureDocvFixtures.fail_json(reason_codes:),
+      docv_transaction_token:,
+    )
   end
 
   def stub_docv_verification_data(docv_transaction_token:, body:)
