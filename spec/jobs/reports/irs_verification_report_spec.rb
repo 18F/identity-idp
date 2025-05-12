6a4b5f55585637c5339cdb7908187eb79f7ff4f8
KolawoleHOseni <kolawolehoseni@fcoh2j-gqn94y2t.gsa.gov>
2025-05-12 16:32:04 -0400

    Lint fix

--- spec/jobs/reports/irs_verification_report_spec.rb
+++ spec/jobs/reports/irs_verification_report_spec.rb
@@ -1,28 +1,41 @@
 require 'rails_helper'
+require 'active_support/testing/time_helpers'
 
 RSpec.describe Reports::IrsVerificationReport do
+  include ActiveSupport::Testing::TimeHelpers
+
   let(:report_date) { Time.zone.today.end_of_day }
   let(:report) { described_class.new(report_date) }
   let(:dummy_report_data) { [['Header1', 'Header2'], ['Value1', 'Value2']] }
+
   let(:mock_report_object) do
     instance_double(
-      Reporting::IrsVerificationReport, as_emailable_reports: [
+      Reporting::IrsVerificationReport,
+      as_emailable_reports: [
         Struct.new(:table, :filename).new(dummy_report_data, 'dummy.csv'),
-      ]
+      ],
     )
   end
 
   before do
+    travel_to Time.zone.parse('2025-05-13 10:00:00')
+
     allow(IdentityConfig.store).to receive(:irs_verification_report_config)
       .and_return(['test@example.com'])
+
     allow(IdentityConfig.store).to receive(:irs_verification_report_issuers)
       .and_return(['issuer1'])
+
     allow(IdentityConfig.store).to receive(:team_all_login_emails)
       .and_return(['team@example.com'])
+
     allow(Reporting::IrsVerificationReport).to receive(:new).and_return(mock_report_object)
     allow(report).to receive(:upload_file_to_s3_bucket).and_return(true)
     allow(report).to receive(:bucket_name).and_return('my-test-bucket')
     allow(ReportMailer).to receive_message_chain(:tables_report, :deliver_now)
+  end
+
+  after do
   end
 
   describe '#perform' do
