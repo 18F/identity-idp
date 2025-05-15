5b0ddf6d3e06590bf92fb98532c2e62e825a1405
KolawoleHOseni <kolawolehoseni@fcoh2j-gqn94y2t.gsa.gov>
2025-05-15 18:43:29 -0400

    fix

--- lib/reporting/irs_verification_report.rb
+++ lib/reporting/irs_verification_report.rb
@@ -18,8 +18,8 @@ module Reporting
 
     VERIFICATION_DEMAND = 'IdV: doc auth welcome visited'
     DOCUMENT_AUTHENTICATION_SUCCESS = 'IdV: doc auth ssn visited'
-    INFORMATION_VALIDATION_SUCCESS = 'IdV: doc auth verify submitted'
-    PHONE_VERIFICATION_SUCCESS = 'IdV: enter password visited'
+    INFORMATION_VALIDATION_SUCCESS = 'IdV: phone of record visited'
+    PHONE_VERIFICATION_SUCCESS = 'idv_enter_password_submitted'
     TOTAL_VERIFIED = 'User registration: complete'
 
     # @param [Array<String>] issuers
