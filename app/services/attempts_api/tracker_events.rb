ff627b470bd53b3460b7685a1f47e16e97773873
Davi (she/they) <davida.marion@gsa.gov>
2025-05-16 09:48:47 -0400

    user registration email events (#12175)
    * changelog: Upcoming Features, Attempts API, User registration email events

--- app/services/attempts_api/tracker_events.rb
+++ app/services/attempts_api/tracker_events.rb
@@ -344,12 +344,49 @@ module AttemptsApi
     end
 
     # @param [Boolean] success
+    # @param [String] email
+    # @param [Hash<Symbol,Array<Symbol>>] failure_reason
+    # Tracks when user clicks on the confirmation link sent to their provided email address
+    def user_registration_email_confirmed(success:, email:, failure_reason: nil)
+      track_event(
+        'user-registration-email-confirmed',
+        success:,
+        email:,
+        failure_reason:,
+      )
+    end
+
+    # @param [Boolean] success
+    # @param [String] email
+    # @param [Hash<Symbol,Array<Symbol>>] failure_reason
+    # Tracks when user submits email address to create a new account
+    def user_registration_email_submitted(success:, email: nil, failure_reason: nil)
+      track_event(
+        'user-registration-email-submitted',
+        success:,
+        email:,
+        failure_reason:,
+      )
+    end
+
+    # Tracks when user is rate limited for submitting registration email
+    # @param [String] email
+    # @param [Boolean] email_already_registered
+    def user_registration_email_submission_rate_limited(
+      email:,
+      email_already_registered:
+    )
+      track_event(
+        'user-registration-email-submission-rate-limited',
+        email:,
+        email_already_registered:,
+      )
+    end
+
+    # @param [Boolean] success
     # @param [Hash<Symbol,Array<Symbol>>] failure_reason
     # Tracks when user submits registration password
-    def user_registration_password_submitted(
-      success:,
-      failure_reason: nil
-    )
+    def user_registration_password_submitted(success:, failure_reason: nil)
       track_event(
         'user-registration-password-submitted',
         success:,
--- spec/views/sign_up/registrations/new.html.erb_spec.rb
+++ spec/views/sign_up/registrations/new.html.erb_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe 'sign_up/registrations/new.html.erb' do
     allow(view).to receive(:current_user).and_return(nil)
     @register_user_email_form = RegisterUserEmailForm.new(
       analytics: FakeAnalytics.new,
+      attempts_api_tracker: AttemptsApiTrackingHelper::FakeAttemptsTracker.new,
     )
     view_context = ActionController::Base.new.view_context
     allow(view_context).to receive(:new_user_session_url)
