ee54017ec5673984e4afbc80fb49b7d812644d2c
Malick DiarrA <malick.diarra@gsa.gov>
2025-05-27 10:20:31 -0400

    add document type

--- db/schema.rb
+++ db/schema.rb
@@ -361,6 +361,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_19_152453) do
     t.datetime "last_batch_claimed_at", comment: "sensitive=false"
     t.string "sponsor_id", null: false, comment: "sensitive=false"
     t.string "doc_auth_result", comment: "sensitive=false"
+    t.integer "document_type", comment: "sensitive=false"
     t.index ["profile_id"], name: "index_in_person_enrollments_on_profile_id"
     t.index ["ready_for_status_check"], name: "index_in_person_enrollments_on_ready_for_status_check", where: "(ready_for_status_check = true)"
     t.index ["status_check_attempted_at"], name: "index_in_person_enrollments_on_status_check_attempted_at", where: "(status = 1)"
