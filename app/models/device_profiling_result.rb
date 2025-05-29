b79028e5b4208b9084d8dd4893f1559cb48584d6
Malick DiarrA <malick.diarra@gsa.gov>
2025-05-29 12:18:16 -0400

    changelog: Upcoming Features, Threatmetrix, add migration for threat metrix processing

--- app/models/device_profiling_result.rb
+++ app/models/device_profiling_result.rb
@@ -6,27 +6,4 @@ class DeviceProfilingResult < ApplicationRecord
   PROFILING_TYPES= {
     :account_creation => 'ACCOUNT_CREATION'
   }
-  
-  # Check if a user has passed device profiling
-  def self.passed?(user_id:, type:)
-    result = find_by(user_id: user_id)
-    result && result.success?
-  end
-  
-  # Check if a user has failed device profiling
-  def self.failed?(user_id:, type:)
-    result = find_by(user_id: user_id)
-    result && !result.success?
-  end
-  
-  # Check if a user has been automatically rejected
-  def self.auto_rejected?(user_id:, type:)
-    result = find_by(user_id: user_id)
-    result && result.review_status == 'reject'
-  end
-  
-  # Get the result for a user, or nil if no result exists
-  def self.for_user(user_id)
-    where(user_id: user_id)
-  end
 end
