637f57ddfcea4dd08f29489de09edb0644db5592
Malick DiarrA <malick.diarra@gsa.gov>
2025-05-29 14:16:09 -0400

    update result

--- app/models/device_profiling_result.rb
+++ app/models/device_profiling_result.rb
@@ -3,7 +3,7 @@
 class DeviceProfilingResult < ApplicationRecord
   belongs_to :user
 
-  PROFILING_TYPES= {
-    :account_creation => 'ACCOUNT_CREATION'
-  }
+  PROFILING_TYPES = {
+    account_creation: 'ACCOUNT_CREATION',
+  }.freeze
 end
