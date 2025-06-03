b03b514deb50f8f4d84aa6b3b8d6e9acf1f3acd7
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-06-03 12:31:37 -0400

    rename table and method

--- db/primary_migrate/20250603152947_add_sms_ft_upsell_dismissed_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSmsFtUpsellDismissedAtToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :sms_ft_upsell_dismissed_at, :datetime, comment: 'sensitive=false'
-  end
-end
