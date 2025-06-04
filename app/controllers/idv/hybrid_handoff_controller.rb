b87a67c206c5408ece8171a323792012a928a071
ashukla <abir.shukla@gsa.gov>
2025-06-04 15:12:50 -0500

    Fixing mobile routing bug

--- app/controllers/idv/hybrid_handoff_controller.rb
+++ app/controllers/idv/hybrid_handoff_controller.rb
@@ -70,7 +70,10 @@ module Idv
         controller: self,
         next_steps: [:choose_id_type, :link_sent, :document_capture, :socure_document_capture],
         preconditions: ->(idv_session:, user:) {
-                         idv_session.idv_consent_given?
+                         idv_session.idv_consent_given? &&
+                         (self.selected_remote(idv_session: idv_session) || # from opt-in screen
+                             # back from ipp doc capture screen
+                             idv_session.skip_doc_auth_from_handoff)
                        },
         undo_step: ->(idv_session:, user:) do
           idv_session.flow_path = nil
