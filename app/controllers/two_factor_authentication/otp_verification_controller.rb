803b19da8503fe76b66bb0cfdf9f6d0f1644fabb
Kevin Masters <135744319+kevinsmaster5@users.noreply.github.com>
2025-06-04 09:41:28 -0400

    remove unneeded schema changes. add check for platform auth available and redirect

--- app/controllers/two_factor_authentication/otp_verification_controller.rb
+++ app/controllers/two_factor_authentication/otp_verification_controller.rb
@@ -45,6 +45,9 @@ module TwoFactorAuthentication
 
         if UserSessionContext.confirmation_context?(context)
           handle_valid_confirmation_otp
+        elsif user_session[:platform_authenticator_available] &&
+              !current_user.webauthn_platform_recommended_dismissed_at?
+          redirect_to webauthn_platform_recommended_path
         else
           redirect_to after_sign_in_path_for(current_user)
         end
