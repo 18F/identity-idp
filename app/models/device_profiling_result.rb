2f1a3ff66a63657e5bda56bf02b3a314803c5f5b
Malick DiarrA <malick.diarra@gsa.gov>
2025-06-10 08:56:28 -0400

    fix device profiling result

--- app/models/device_profiling_result.rb
+++ app/models/device_profiling_result.rb
@@ -16,27 +16,11 @@ class DeviceProfilingResult < ApplicationRecord
     )
   end
 
-  def self.passed?(user_id:, type:)
-    result = find_by(user_id:, profiling_type: type)
-    result&.success?
-  end
-
-  def self.failed?(user_id:, type:)
-    result = find_by(user_id:, profiling_type: type)
-    result && (result.review_status != 'pass')
-  end
-
-  def self.auto_rejected?(user_id:, type:)
-    result = find_by(user_id:, profiling_type: type)
-    result && result.review_status == 'reject'
-  end
-
-  # Get the result for a user, or nil if no result exists
   def self.for_user(user_id:, type:)
     where(user_id:, profiling_type: type)
   end
 
   def rejected?
-    review_status != 'pass'
+    review_status == 'reject'
   end
 end
