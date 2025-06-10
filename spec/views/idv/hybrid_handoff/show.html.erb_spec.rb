967aa135f23255f769653d7326019b162017a1dc
A Shukla <abir.shukla@gsa.gov>
2025-06-10 10:03:06 -0500

    Lg 16188 update how to verify page (#12221)
    * changelog: User-Facing Improvements, Doc Auth, updating hybrid how to verify page
    
    * Fixing feature and view specs
    
    * removing unused key and fixing hybrid handoff spec
    
    * Removing redudant code
    
    * Resolved Pr comments for UX
    
    * Fixing selfie conditional text
    
    * Resolving pr comments
    
    * Updating specs
    
    * Updating to show post office content conditionally and updating specs to use new button
    
    * Fixing specs
    
    * Updating logic in controller and spec
    
    * Fixing no ipp spec in hybrid handoff
    
    * reverting logic
    
    * Updating hybrid spec
    
    * updating precondition for hybrid handoff to support new flow
    
    * Updating specs
    
    * Fixing passport spec
    
    * Updating specs again
    
    * Updating feature specs
    
    * Updating aria label
    
    * Adding id for aria label
    
    * Removing aria label
    
    * Fixing mobile routing bug
    
    * Fixing controller specs
    
    * moving abandon_any_ipp_progress

--- spec/views/idv/hybrid_handoff/show.html.erb_spec.rb
+++ spec/views/idv/hybrid_handoff/show.html.erb_spec.rb
@@ -4,11 +4,21 @@ RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
   before do
     allow(view).to receive(:current_user).and_return(@user)
     @idv_form = Idv::PhoneForm.new(user: build_stubbed(:user), previous_params: nil)
+    @idv_how_to_verify_form = Idv::HowToVerifyForm.new
+    @presenter = Idv::HowToVerifyPresenter.new(
+      mobile_required: true,
+      selfie_check_required: true,
+      passport_allowed: true,
+    )
   end
 
   subject(:rendered) do
     render template: 'idv/hybrid_handoff/show', locals: {
       idv_phone_form: @idv_form,
+      idv_how_to_verify_form: @idv_how_to_verify_form,
+      post_office_enabled: @post_office_enabled,
+      selfie_required: @selfie_required,
+      presenter: @presenter,
     }
   end
 
@@ -31,21 +41,18 @@ RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
     end
 
     it 'displays the expected headings from the "a" case' do
-      expect(rendered).to have_selector('h1', text: t('doc_auth.headings.hybrid_handoff'))
+      expect(rendered).to have_selector('h1', text: t('doc_auth.headings.how_to_verify'))
       expect(rendered).to have_selector('h2', text: t('doc_auth.headings.upload_from_phone'))
     end
 
     it 'does not display IPP related content' do
-      expect(rendered).to_not have_content(strip_tags(t('doc_auth.info.hybrid_handoff_ipp_html')))
-      expect(rendered).to_not have_link(
-        t('in_person_proofing.headings.prepare'),
-        href: idv_document_capture_path(step: :hybrid_handoff),
-      )
+      expect(rendered).to_not have_content(strip_tags(t('doc_auth.headings.verify_at_post_office')))
     end
   end
   context 'when selfie is required' do
     before do
       @selfie_required = true
+      @post_office_enabled = true
     end
     it 'has a form for starting mobile doc auth with an aria label tag' do
       expect(rendered).to have_selector(
@@ -54,7 +61,7 @@ RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
       )
     end
     it 'displays the expected headings from the "a" case' do
-      expect(rendered).to have_selector('h1', text: t('doc_auth.headings.hybrid_handoff_selfie'))
+      expect(rendered).to have_selector('h1', text: t('doc_auth.headings.how_to_verify'))
     end
 
     describe 'when ipp is enabled' do
@@ -62,20 +69,17 @@ RSpec.describe 'idv/hybrid_handoff/show.html.erb' do
         @direct_ipp_with_selfie_enabled = true
       end
       it 'displays content and link for choose ipp' do
-        expect(rendered).to have_content(strip_tags(t('doc_auth.info.hybrid_handoff_ipp_html')))
-        expect(rendered).to have_link(
-          t('in_person_proofing.headings.prepare'),
-          href: idv_document_capture_path(step: :hybrid_handoff),
-        )
+        expect(rendered).to have_content(t('doc_auth.headings.verify_at_post_office'))
       end
     end
 
     describe 'when ipp is not enabled' do
       before do
         @direct_ipp_with_selfie_enabled = false
+        @post_office_enabled = false
       end
       it 'displays content and link for choose ipp' do
-        expect(rendered).to_not have_content(strip_tags(t('doc_auth.info.hybrid_handoff_ipp_html')))
+        expect(rendered).to_not have_content(t('doc_auth.headings.verify_at_post_office'))
         expect(rendered).to_not have_link(
           t('in_person_proofing.headings.prepare'),
           href: idv_document_capture_path(step: :hybrid_handoff),
