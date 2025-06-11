0d40a6c38ef743d5f271ded3606c2011035131a2
Mawar2 <malik.warren@gsa.gov>
2025-06-11 14:47:07 -0400

    Add implementation for detecting and displaying selfie verification failures in Socure document capture flow.

--- app/controllers/idv/socure/errors_controller.rb
+++ app/controllers/idv/socure/errors_controller.rb
@@ -8,6 +8,7 @@ module Idv
       include IdvStepConcern
       include StepIndicatorConcern
       include AbTestAnalyticsConcern
+      include SocureErrorsConcern
 
       before_action :confirm_step_allowed
 
@@ -68,21 +69,6 @@ module Idv
         )
       end
 
-      def error_code_for(result)
-        if result.errors[:unaccepted_id_type]
-          :unaccepted_id_type
-        elsif result.errors[:socure]
-          result.errors.dig(:socure, :reason_codes).first
-        elsif result.errors[:network]
-          :network
-        elsif result.errors[:pii_validation]
-          :pii_validation
-        else
-          # No error information available (shouldn't happen). Default
-          # to :network if it does.
-          :network
-        end
-      end
     end
   end
 end
