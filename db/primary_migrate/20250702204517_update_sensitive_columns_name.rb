de8be2eacd2938157b4a5bcfcf12508c8eba49ef
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-07-02 16:13:02 -0500

    choose correct table

--- db/primary_migrate/20250702204517_update_sensitive_columns_name.rb
+++ db/primary_migrate/20250702204517_update_sensitive_columns_name.rb
@@ -2,6 +2,6 @@ class UpdateSensitiveColumnsName < ActiveRecord::Migration[8.0]
   def change
     change_column_comment :webauthn_configurations, :name, from: "sensitive=false", to: "sensitive=true"
     change_column_comment :auth_app_configurations, :name, from: "sensitive=false", to: "sensitive=true"
-    change_column_comment :auth_app_configurations, :name, from: "sensitive=false", to: "sensitive=true"
+    change_column_comment :piv_cac_configurations, :name, from: "sensitive=false", to: "sensitive=true"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -459,7 +459,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_204517) do
   create_table "piv_cac_configurations", force: :cascade do |t|
     t.integer "user_id", null: false, comment: "sensitive=false"
     t.string "x509_dn_uuid", null: false, comment: "sensitive=false"
-    t.string "name", null: false, comment: "sensitive=false"
+    t.string "name", null: false, comment: "sensitive=true"
     t.datetime "created_at", precision: nil, null: false, comment: "sensitive=false"
     t.datetime "updated_at", precision: nil, null: false, comment: "sensitive=false"
     t.string "x509_issuer", comment: "sensitive=false"
