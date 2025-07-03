b15daada0efe7499c96f8101cff3f60eb2f93bd3
ashukla <abir.shukla@gsa.gov>
2025-07-03 15:14:22 -0500

    Switching to .dig

--- app/services/idv/session.rb
+++ app/services/idv/session.rb
@@ -300,9 +300,7 @@ module Idv
     end
 
     def remote_submitted_doc_type
-      return nil if session[:pii_from_doc].blank?
-
-      session[:pii_from_doc][:id_doc_type]
+      session.dig(:pii_from_doc, :id_doc_type)
     end
 
     def ipp_document_capture_complete?
