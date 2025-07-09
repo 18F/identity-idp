35667e3a05805b8d65953175a0e5a1823cc71884
Malick DiarrA <malick.diarra@gsa.gov>
2025-07-09 09:32:08 -0400

    update profile spec

--- spec/presenters/duplicate_profiles_detected_presenter_spec.rb
+++ spec/presenters/duplicate_profiles_detected_presenter_spec.rb
@@ -22,59 +22,8 @@ RSpec.describe DuplicateProfilesDetectedPresenter do
       before do
         user_session[:duplicate_profile_ids] = [profile2.id]
       end
-      it 'should return singular element' do
-        expect(presenter.duplicate_profiles.count).to eq(1)
-      end
-    end
-  end
-
-  describe '#recognize_all_profiles' do
-    context 'when multiple duplicate profiles were found for user' do
-      let(:profile3) { create(:profile, :facial_match_proof) }
-
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id, profile3.id]
-      end
-
-      it 'should return plural text' do
-        expect(presenter.recognize_all_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.yes_many'))
-      end
-    end
-
-    context 'when a single duplicate profiles were found for user' do
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id]
-      end
-      it 'should return singular text' do
-        expect(presenter.recognize_all_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.yes_single'))
-      end
-    end
-  end
-
-  describe '#dont_recognize_some_profiles' do
-    context 'when multiple duplicate profiles were found for user' do
-      let(:profile3) { create(:profile, :facial_match_proof) }
-
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id, profile3.id]
-      end
-
-      it 'should return multiple text' do
-        expect(presenter.dont_recognize_some_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.no_recognize_many'))
-      end
-    end
-
-    context 'when a single duplicate profiles were found for user' do
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id]
-      end
-
-      it 'should return singular text' do
-        expect(presenter.dont_recognize_some_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.no_recognize_single'))
+      it 'should return user element and other profile' do
+        expect(presenter.associated_profiles.count).to eq(2)
       end
     end
   end
