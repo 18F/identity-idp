194f96f64a6131d8377aac5173e0efeb8bc97ef8
Malick Diarra <malick.diarra@gsa.gov>
2025-07-09 15:32:29 -0400

    changelog: Upcoming Features, One Account, initial designs for users … (#12309)
    * changelog: Upcoming Features, One Account, initial designs for users with duplicate profiles
    
    * udpated presenter
    
    * update show html
    
    * fix linting
    
    * update yaml
    
    * update linting
    
    * update spanish translations
    
    * update yaml files
    
    * update presenter
    
    * fix rspec
    
    * update profile spec

--- spec/presenters/duplicate_profiles_detected_presenter_spec.rb
+++ spec/presenters/duplicate_profiles_detected_presenter_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe DuplicateProfilesDetectedPresenter do
   let(:presenter) { described_class.new(user: user, user_session: user_session) }
   let(:profile2) { create(:profile, :facial_match_proof) }
 
-  describe '#duplicate_profiles' do
+  describe '#associated_profiles' do
     context 'when multiple duplicate profiles were found for user' do
       let(:profile3) { create(:profile, :facial_match_proof) }
       before do
         user_session[:duplicate_profile_ids] = [profile2.id, profile3.id]
       end
 
-      it 'should return multiple elements' do
-        expect(presenter.duplicate_profiles.count).to eq(2)
+      it 'should return multiple elements and user element' do
+        expect(presenter.associated_profiles.count).to eq(3)
       end
     end
 
@@ -22,59 +22,8 @@ RSpec.describe DuplicateProfilesDetectedPresenter do
       before do
         user_session[:duplicate_profile_ids] = [profile2.id]
       end
-      it 'should return singular element' do
-        expect(presenter.duplicate_profiles.count).to eq(1)
-      end
-    end
-  end
-
-  describe '#recognize_all_profiles' do
-    context 'when multiple duplicate profiles were found for user' do
-      let(:profile3) { create(:profile, :facial_match_proof) }
-
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id, profile3.id]
-      end
-
-      it 'should return plural text' do
-        expect(presenter.recognize_all_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.yes_many'))
-      end
-    end
-
-    context 'when a single duplicate profiles were found for user' do
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id]
-      end
-      it 'should return singular text' do
-        expect(presenter.recognize_all_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.yes_single'))
-      end
-    end
-  end
-
-  describe '#dont_recognize_some_profiles' do
-    context 'when multiple duplicate profiles were found for user' do
-      let(:profile3) { create(:profile, :facial_match_proof) }
-
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id, profile3.id]
-      end
-
-      it 'should return multiple text' do
-        expect(presenter.dont_recognize_some_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.no_recognize_many'))
-      end
-    end
-
-    context 'when a single duplicate profiles were found for user' do
-      before do
-        user_session[:duplicate_profile_ids] = [profile2.id]
-      end
-
-      it 'should return singular text' do
-        expect(presenter.dont_recognize_some_profiles)
-          .to eq(I18n.t('duplicate_profiles_detected.no_recognize_single'))
+      it 'should return user element and other profile' do
+        expect(presenter.associated_profiles.count).to eq(2)
       end
     end
   end
