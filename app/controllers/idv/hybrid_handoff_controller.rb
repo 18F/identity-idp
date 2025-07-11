d05988e22cfc821455d093f61d77603d61badcd1
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-07-11 17:05:22 -0400

    only IPP for SP as configured
    [skip changelog]

--- app/controllers/idv/hybrid_handoff_controller.rb
+++ app/controllers/idv/hybrid_handoff_controller.rb
@@ -21,7 +21,10 @@ module Idv
                                         )
       @post_office_enabled = IdentityConfig.store.in_person_proofing_enabled &&
                              IdentityConfig.store.in_person_proofing_opt_in_enabled &&
-                             IdentityConfig.store.in_person_doc_auth_button_enabled
+                             IdentityConfig.store.in_person_doc_auth_button_enabled &&
+                             Idv::InPersonConfig.enabled_for_issuer?(
+                               decorated_sp_session.sp_issuer,
+                             )
       @selfie_required = idv_session.selfie_check_required
       @idv_how_to_verify_form = Idv::HowToVerifyForm.new
       set_how_to_verify_presenter
