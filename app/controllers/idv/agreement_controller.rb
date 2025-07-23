d61c88c6e3a63dabcfc7e5dcbb63e3848b1ce0dc
ashukla <abir.shukla@gsa.gov>
2025-07-23 17:17:55 -0500

    Added work for mobile flow how to verify page

--- app/controllers/idv/agreement_controller.rb
+++ app/controllers/idv/agreement_controller.rb
@@ -42,16 +42,11 @@ module Idv
       if result.success?
         idv_session.idv_consent_given_at = Time.zone.now
 
+        puts "idv_consent given: #{idv_session.idv_consent_given?}"
+        puts "idv_sp_ipp enabled: #{idv_session.service_provider&.in_person_proofing_enabled}"
+
         if params[:skip_hybrid_handoff]
-          if in_person_proofing_route_enabled?
-            redirect_to idv_how_to_verify_url
-          elsif idv_session.passport_allowed
-            idv_session.opted_in_to_in_person_proofing = false
-            idv_session.skip_doc_auth_from_how_to_verify = false
-            redirect_to idv_choose_id_type_url
-          else
-            redirect_to idv_how_to_verify_url
-          end
+          redirect_to idv_how_to_verify_url
         else
           idv_session.opted_in_to_in_person_proofing = false
           idv_session.skip_doc_auth_from_how_to_verify = false
