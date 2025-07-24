0b4195c81a61e4dbe0b7f31b9536bfc33310b3c3
ashukla <abir.shukla@gsa.gov>
2025-07-24 11:09:16 -0500

    Fixing specs

--- spec/features/idv/doc_auth/choose_id_type_spec.rb
+++ spec/features/idv/doc_auth/choose_id_type_spec.rb
@@ -3,14 +3,24 @@ require 'rails_helper'
 RSpec.feature 'choose id type step error checking' do
   include DocAuthHelper
   include AbTestsHelper
+  include IdvStepHelper
 
   context 'happy path' do
+    let(:ipp_service_provider) do
+      create(:service_provider, :active, :in_person_proofing_enabled)
+    end
     before do
       allow(IdentityConfig.store).to receive(:doc_auth_passports_enabled).and_return(true)
       allow(IdentityConfig.store).to receive(:doc_auth_passports_percent).and_return(100)
+      allow_any_instance_of(ServiceProviderSession).to receive(:sp_name)
+        .and_return(ipp_service_provider)
       stub_request(:get, IdentityConfig.store.dos_passport_composite_healthcheck_endpoint)
         .to_return({ status: 200, body: { status: 'UP' }.to_json })
       reload_ab_tests
+      visit_idp_from_sp_with_ial2(
+        :oidc,
+        **{ client_id: ipp_service_provider.issuer },
+      )
       sign_in_and_2fa_user
     end
 
@@ -42,11 +52,15 @@ RSpec.feature 'choose id type step error checking' do
       before do
         allow(IdentityConfig.store).to receive(:in_person_proofing_enabled).and_return(true)
         allow(IdentityConfig.store).to receive(:in_person_proofing_opt_in_enabled).and_return(true)
+        allow_any_instance_of(ServiceProvider).to receive(
+          :in_person_proofing_enabled,
+        ).and_return(true)
         complete_doc_auth_steps_before_agreement_step
         complete_agreement_step
       end
 
       it 'shows choose id type screen and continues after drivers license option' do
+        click_button t('forms.buttons.continue_online')
         expect(page).to have_current_path(idv_choose_id_type_url)
         choose(t('doc_auth.forms.id_type_preference.drivers_license'))
         click_on t('forms.buttons.continue')
