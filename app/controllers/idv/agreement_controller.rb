4519ce3418dbc1a2db452035557089b14d674c4e
ashukla <abir.shukla@gsa.gov>
2025-07-25 16:04:32 -0500

    Updating ipp logic in agreement controller

--- app/controllers/idv/agreement_controller.rb
+++ app/controllers/idv/agreement_controller.rb
@@ -86,8 +86,12 @@ module Idv
     end
 
     def in_person_proofing_route_enabled?
-      IdentityConfig.store.in_person_proofing_opt_in_enabled &&
-        IdentityConfig.store.in_person_proofing_enabled
+      IdentityConfig.store.in_person_proofing_enabled &&
+        IdentityConfig.store.in_person_proofing_opt_in_enabled &&
+        IdentityConfig.store.in_person_doc_auth_button_enabled &&
+        Idv::InPersonConfig.enabled_for_issuer?(
+          decorated_sp_session.sp_issuer,
+        )
     end
 
     def skip_to_capture
