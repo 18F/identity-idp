646e8e9ba7be8f881a6d1e1a04be2d60116b151c
Malick DiarrA <malick.diarra@gsa.gov>
2025-07-29 16:25:32 -0400

    update spec

--- spec/jobs/alert_user_duplicate_profile_discovered_job_spec.rb
+++ spec/jobs/alert_user_duplicate_profile_discovered_job_spec.rb
@@ -5,18 +5,17 @@ RSpec.describe AlertUserDuplicateProfileDiscoveredJob do
   let(:agency) { 'Test Agency' }
   let(:email1) { user.email_addresses.first }
   let(:job_analytics) { FakeAnalytics.new }
-  let(:user_mailer) { double(UserMailer) }
 
   before do
     allow(user).to receive(:confirmed_email_addresses).and_return([email1])
-    allow(UserMailer).to receive(:with).and_return(user_mailer)
   end
 
   describe '#perform' do
     context 'when type is :account_created' do
       it 'sends dupe_profile_created email for each confirmed email address' do
-        expect(UserMailer).to receive(:with).with(user: user, email_address: email1)
-        expect(user_mailer).to receive(:dupe_profile_created).with(agency_name: agency)
+        expect_any_instance_of(UserMailer).to receive(:dupe_profile_created)
+          .with(agency_name: agency)
+          .and_call_original
 
         subject.perform(user: user, agency: agency, type: :account_created)
       end
@@ -24,8 +23,9 @@ RSpec.describe AlertUserDuplicateProfileDiscoveredJob do
 
     context 'when type is :sign_in_attempted' do
       it 'sends dupe_profile_sign_in_attempted email for each confirmed email address' do
-        expect(UserMailer).to receive(:with).with(user: user, email_address: email1)
-        expect(user_mailer).to receive(:dupe_profile_sign_in_attempted).with(agency_name: agency)
+        expect_any_instance_of(UserMailer).to receive(:dupe_profile_sign_in_attempted)
+          .with(agency_name: agency)
+          .and_call_original
 
         subject.perform(user: user, agency: agency, type: :sign_in_attempted)
       end
@@ -41,24 +41,6 @@ RSpec.describe AlertUserDuplicateProfileDiscoveredJob do
           .with(type: invalid_type)
 
         subject.perform(user: user, agency: agency, type: invalid_type)
-      end
-    end
-
-    context 'with multiple confirmed email addresses' do
-      let(:email2) { create(:email_address, user: user) }
-      let(:email_addresses) { [email1, email2] }
-
-      before do
-        allow(user).to receive(:confirmed_email_addresses).and_return(email_addresses)
-      end
-
-      it 'sends emails to all confirmed email addresses' do
-        email_addresses.each do |email|
-          expect(UserMailer).to receive(:with).with(user: user, email_address: email)
-        end
-        expect(user_mailer).to receive(:dupe_profile_created).twice.with(agency_name: agency)
-
-        subject.perform(user: user, agency: agency, type: :account_created)
       end
     end
   end
