7e7867f02faacfa749bf5f9d99ea19ef51754fd9
Aaron Nagucki <aaron.nagucki@gmail.com>
2025-07-29 10:09:12 -0500

    extend transaction block

--- app/jobs/reports/base_report.rb
+++ app/jobs/reports/base_report.rb
@@ -6,38 +6,18 @@ module Reports
   class BaseReport < ApplicationJob
     queue_as :long_running
 
-    def self.transaction_with_timeout(rails_env = Rails.env)
+    def self.transaction_with_timeout(rails_env = Rails.env, shard: :read_replica)
       # rspec-rails's use_transactional_tests does not seem to act as expected when switching
       # connections mid-test, so we just skip for now :[
       return yield if rails_env.test?
 
-      ActiveRecord::Base.connected_to(role: :reading, shard: :read_replica) do
+      ActiveRecord::Base.connected_to(role: :reading, shard:) do
         ActiveRecord::Base.transaction do
           quoted_timeout = ActiveRecord::Base.connection.quote(IdentityConfig.store.report_timeout)
           ActiveRecord::Base.connection.execute("SET LOCAL statement_timeout = #{quoted_timeout}")
           yield
         end
       end
-    end
-
-    # def self.data_warehouse_transaction_with_timeout
-    #   original_config = ActiveRecord::Base.connection_pool.db_config.configuration_hash
-    #   ActiveRecord::Base.establish_connection(:data_warehouse)
-    #   ActiveRecord::Base.transaction do
-    #     quoted_timeout = ActiveRecord::Base.connection.quote(IdentityConfig.store.report_timeout)
-    #     ActiveRecord::Base.connection.execute("SET LOCAL statement_timeout = #{quoted_timeout}")
-    #     yield
-    #   end
-    # ensure
-    #   ActiveRecord::Base.establish_connection(original_config)
-    # end
-
-    def self.data_warehouse_transaction_with_timeout
-      ActiveRecord::Base.connected_to(role: :reading, shard: :data_warehouse) do
-        ActiveRecord::Base.transaction do
-          quoted_timeout = ActiveRecord::Base.connection.quote(IdentityConfig.store.report_timeout)
-          ActiveRecord::Base.connection.execute("SET LOCAL statement_timeout = #{quoted_timeout}")
-          yield
     end
 
     private
@@ -58,10 +38,6 @@ module Reports
 
     def transaction_with_timeout(...)
       self.class.transaction_with_timeout(...)
-    end
-
-    def data_warehouse_transaction_with_timeout(...)
-      self.class.data_warehouse_transaction_with_timeout(...)
     end
 
     def save_report(report_name, body, extension:)
