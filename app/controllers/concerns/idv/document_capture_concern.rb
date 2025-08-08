11b63b7e29a840b3b76dd390e9f69adc74e35c6f
Shane Chesnutt <shane.chesnutt@gsa.gov>
2025-08-08 10:24:42 -0400

    Convert state_id to state_id_card in remote flow

--- app/controllers/concerns/idv/document_capture_concern.rb
+++ app/controllers/concerns/idv/document_capture_concern.rb
@@ -176,7 +176,7 @@ module Idv
 
     def id_type_requested
       document_capture_session.passport_requested? ? Idp::Constants::DocumentTypes::PASSPORT :
-        Idp::Constants::DocumentTypes::STATE_ID
+        Idp::Constants::DocumentTypes::STATE_ID_CARD
     end
 
     def track_document_issuing_state(user, state)
