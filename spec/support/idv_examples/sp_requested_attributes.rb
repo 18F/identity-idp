41c52705902c5b69ed0f93e1bf7277bd96170593
Mitchell Henke <mitchell.henke@gsa.gov>
2025-08-14 10:53:27 -0500

    Reset cookie when validating HTML markup in accessibility test to improve reliability
    changelog: Internal, Testing, Reset cookie when validating HTML markup in accessibility test to improve reliability
    
    Co-authored-by: Davi (she/they) <davida.marion@gsa.gov>

--- spec/support/idv_examples/sp_requested_attributes.rb
+++ spec/support/idv_examples/sp_requested_attributes.rb
@@ -9,9 +9,7 @@ RSpec.shared_examples 'sp requesting attributes' do |sp|
   context 'visiting an SP for the first time' do
     it 'requires the user to verify the attributes submitted to the SP', js: true do
       visit_idp_from_sp_with_ial2(sp)
-      sign_in_user(user)
-      fill_in_code_with_last_phone_otp
-      click_submit_default
+      sign_in_live_with_2fa(user)
 
       expect(page).to have_current_path idv_welcome_path
 
