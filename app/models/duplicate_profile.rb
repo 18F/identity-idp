97417952ab933a2110c8160eaa0ab37e62618f2c
Malick DiarrA <malick.diarra@gsa.gov>
2025-08-19 10:19:44 -0400

    update confirmation

--- app/models/duplicate_profile.rb
+++ app/models/duplicate_profile.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class DuplicateProfile < ApplicationRecord
+  def self.involving_profile(profile_id:, service_provider:)
+    where(service_provider: service_provider)
+      .where('? = ANY(profile_ids)', profile_id)
+  end
+
+  def self.get_profile_info(profile_id:, service_provider:)
+    involving_profile(profile_id: profile_id, service_provider: service_provider).first
+  end
 end
