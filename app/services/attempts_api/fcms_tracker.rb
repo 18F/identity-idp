e1e99c93b061115756a76fce351fd899215f4c45
Davida Marion <davida.marion@gsa.gov>
2025-08-19 10:10:08 -0400

    Small refactor of fcms code

--- app/services/attempts_api/fcms_tracker.rb
+++ app/services/attempts_api/fcms_tracker.rb
@@ -7,35 +7,16 @@ module AttemptsApi
 
     private
 
-    def build_event_metadata(event_type, metadata)
-      extra_metadata =
-        if metadata.has_key?(:failure_reason) &&
-           (metadata[:failure_reason].blank? || metadata[:success].present?)
-          metadata.except(:failure_reason)
-        else
-          metadata
-        end
-
-      # Custom Metadata for FCMS
-      event_metadata = {
+    def extra_attributes(event_type:)
+      {
+        agency_uuid: agency_uuid(event_type:),
         user_uuid: user&.uuid,
         user_id: user&.id,
         unique_session_id: user&.unique_session_id,
-        agency_uuid: agency_uuid(event_type: event_type),
-        device_id: cookie_device_uuid,
-        user_ip_address: request&.remote_ip,
-        user_agent: request&.user_agent,
-        application_url: sp_request_uri,
-        language: user&.email_language || I18n.locale.to_s,
-        client_port: CloudFrontHeaderParser.new(request).client_port,
-        aws_region: IdentityConfig.store.aws_region,
-        google_analytics_cookies: google_analytics_cookies(request),
       }
-
-      event_metadata.merge!(extra_metadata)
     end
 
-    def build_jwe(event)
+    def jwe(event)
       event.payload_json(issuer: sp.issuer)
     end
 
---
+++ spec/services/attempts_api/fcms_tracker_spec.rb
@@ -0,0 +1,275 @@
+require 'rails_helper'
+
+RSpec.describe AttemptsApi::FcmsTracker do
+  before do
+    allow(FeatureManagement).to receive(:fcms_enabled?)
+      .and_return(fcms_enabled)
+    allow(request).to receive(:user_agent).and_return('example/1.0')
+    allow(request).to receive(:remote_ip).and_return('192.0.2.1')
+    allow(request).to receive(:cookies).and_return(nil)
+    allow(request).to receive(:headers).and_return(
+      { 'CloudFront-Viewer-Address' => '192.0.2.1:1234' },
+    )
+  end
+
+  let(:fcms_enabled) { true }
+  let(:session_id) { 'test-session-id' }
+  let(:enabled_for_session) { true }
+  let(:request) { instance_double(ActionDispatch::Request) }
+  let(:service_provider) { create(:service_provider) }
+  let(:cookie_device_uuid) { 'device_id' }
+  let(:sp_request_uri) { 'https://example.com/auth_page' }
+  let(:user) { create(:user) }
+
+  subject do
+    described_class.new(
+      session_id:,
+      request:,
+      user:,
+      sp: service_provider,
+      cookie_device_uuid:,
+      sp_request_uri:,
+      enabled_for_session:,
+    )
+  end
+
+  describe '#track_event' do
+    it 'omit failure reason when success is true' do
+      freeze_time do
+        event = subject.track_event(:test_event, foo: :bar, success: true, failure_reason: nil)
+        expect(event.event_metadata).to_not have_key(:failure_reason)
+      end
+    end
+
+    it 'omit failure reason when failure_reason is blank' do
+      freeze_time do
+        event = subject.track_event(:test_event, foo: :bar, failure_reason: nil)
+        expect(event.event_metadata).to_not have_key(:failure_reason)
+      end
+    end
+
+    it 'includes GA cookies' do
+      allow(request).to receive(:cookies).and_return(
+        {
+          '_ga' => 'GA1.ABC',
+          '_ga_ABC' => 'GS2.ABC',
+          'other_cookie' => 'abc',
+        },
+      )
+
+      event = subject.track_event(:test_event)
+      expect(event.event_metadata).to include(
+        {
+          google_analytics_cookies: {
+            '_ga' => 'GA1.ABC',
+            '_ga_ABC' => 'GS2.ABC',
+          },
+        },
+      )
+    end
+
+    it 'should not omit failure reason when success is false and failure_reason is not blank' do
+      freeze_time do
+        event = subject.track_event(
+          :test_event, foo: :bar, success: false,
+                       failure_reason: { foo: [:bar] }
+        )
+        expect(event.event_metadata).to have_key(:failure_reason)
+        expect(event.event_metadata).to have_key(:success)
+      end
+    end
+
+    it 'records the event in redis' do
+      freeze_time do
+        subject.track_event(:test_event, foo: :bar)
+
+        events = AttemptsApi::FcmsRedisClient.new.read_events(
+          issuer: service_provider.issuer,
+        )
+
+        expect(events.values.length).to eq(1)
+      end
+    end
+
+    it 'does not store events in plaintext in redis' do
+      freeze_time do
+        subject.track_event(:event, first_name: Idp::Constants::MOCK_IDV_APPLICANT[:first_name])
+
+        events = AttemptsApi::FcmsRedisClient.new.read_events(
+          issuer: service_provider.issuer,
+        )
+
+        expect(events.keys.first).to_not include('first_name')
+        expect(events.values.first).to_not include(Idp::Constants::MOCK_IDV_APPLICANT[:first_name])
+      end
+    end
+
+    context 'with nil user' do
+      let(:user) { nil }
+
+      it 'logs nil user_uuid' do
+        event = subject.track_event(:test_event)
+        expect(event.event_metadata[:user]).to be_nil
+      end
+
+      it 'returns the default locale as the language attribute' do
+        event = subject.track_event(:test_event)
+        expect(event.event_metadata[:language]).to eq('en')
+      end
+    end
+
+    context 'with AnonymousUser user' do
+      let(:user) { AnonymousUser.new }
+
+      it 'logs nil user_uuid' do
+        event = subject.track_event(:test_event)
+        expect(event.event_metadata[:user]).to be_nil
+      end
+
+      it 'returns default locale as the language attribute' do
+        event = subject.track_event(:test_event)
+        expect(event.event_metadata[:language]).to eq('en')
+      end
+    end
+
+    context 'user that has a locale selected' do
+      before { user.update(email_language: :es) }
+      it 'returns that locale as a language attribute in event' do
+        event = subject.track_event(:test_event)
+
+        expect(event.event_metadata[:language]).to eq('es')
+      end
+    end
+
+    context 'user has existing Agency UUID' do
+      context 'for event that skips UUID creation' do
+        it 'returns Agency UUID in event' do
+          identity = create(
+            :agency_identity,
+            user_id: user.id,
+            agency_id: service_provider.agency.id,
+          )
+
+          event = subject.track_event(
+            AttemptsApi::Tracker::SKIP_AGENCY_UUID_CREATION_EVENT_TYPES.sample,
+          )
+
+          expect(event.event_metadata[:agency_uuid]).to eq(identity.uuid)
+          expect(event.event_metadata[:user_uuid]).to eq(user.uuid)
+          expect(event.event_metadata[:user_id]).to eq(user.id)
+        end
+      end
+
+      context 'for event that does not skip UUID creation' do
+        it 'returns Agency UUID in event' do
+          identity = create(
+            :agency_identity,
+            user_id: user.id,
+            agency_id: service_provider.agency.id,
+          )
+
+          event = subject.track_event(
+            'my-fake-event',
+          )
+
+          expect(event.event_metadata[:agency_uuid]).to eq(identity.uuid)
+          expect(event.event_metadata[:user_uuid]).to eq(user.uuid)
+          expect(event.event_metadata[:user_id]).to eq(user.id)
+        end
+      end
+    end
+
+    context 'user does not have existing Agency UUID' do
+      context 'for event that skips UUID creation' do
+        it 'returns nil agency UUID in event and tracks primary uuid' do
+          event = subject.track_event(
+            AttemptsApi::Tracker::SKIP_AGENCY_UUID_CREATION_EVENT_TYPES.sample,
+          )
+
+          identity = AgencyIdentity.find_by(
+            user_id: user.id,
+            agency_id: service_provider.agency.id,
+          )
+          expect(event.event_metadata[:agency_uuid]).to eq(nil)
+          expect(event.event_metadata[:user_uuid]).to eq(user.uuid)
+          expect(event.event_metadata[:user_id]).to eq(user.id)
+          expect(identity).to eq(nil)
+        end
+      end
+
+      context 'for event that does not skip UUID creation' do
+        it 'creates new Agency UUID and returns Agency UUID in event' do
+          event = subject.track_event(
+            'my-fake-event',
+          )
+          identity = AgencyIdentity.find_by(
+            user_id: user.id,
+            agency_id: service_provider.agency.id,
+          )
+          expect(identity).to_not be_nil
+           expect(event.event_metadata[:agency_uuid]).to eq(identity.uuid)
+          expect(event.event_metadata[:user_uuid]).to eq(user.uuid)
+          expect(event.event_metadata[:user_id]).to eq(user.id)
+        end
+      end
+    end
+
+    context 'the current session is not an attempts API session' do
+      let(:enabled_for_session) { false }
+
+      it 'does not record any events in redis' do
+        freeze_time do
+          subject.track_event(:test_event, foo: :bar)
+
+          events = AttemptsApi::RedisClient.new.read_events(
+            issuer: service_provider.issuer,
+          )
+
+          expect(events.values.length).to eq(0)
+        end
+      end
+    end
+
+    context 'the attempts API is not enabled' do
+      let(:fcms_enabled) { false }
+
+      it 'does not record any events in redis' do
+        freeze_time do
+          subject.track_event(:test_event, foo: :bar)
+
+          events = AttemptsApi::RedisClient.new.read_events(
+            issuer: service_provider.issuer,
+          )
+
+          expect(events.values.length).to eq(0)
+        end
+      end
+    end
+  end
+
+  describe '#parse_failure_reason' do
+    let(:mock_error_message) { 'failure_reason_from_error' }
+    let(:mock_error_details) { { mock_error: { failure_reason_from_error_details: true } } }
+
+    it 'parses failure_reason from error_details' do
+      test_failure_reason = subject.parse_failure_reason(
+        { errors: mock_error_message,
+          error_details: mock_error_details },
+      )
+
+      expect(test_failure_reason).to eq({ mock_error: [:failure_reason_from_error_details] })
+    end
+
+    it 'parses failure_reason from errors when no error_details present' do
+      mock_failure_reason = double(
+        'MockFailureReason',
+        errors: mock_error_message,
+        to_h: {},
+      )
+
+      test_failure_reason = subject.parse_failure_reason(mock_failure_reason)
+
+      expect(test_failure_reason).to eq(mock_error_message)
+    end
+  end
+end
