9a2d19d2eec8b268f3f24510c48aa566acf5873e
Vraj Mohan <vraj.mohan@gsa.gov>
2025-08-29 06:32:53 -0700

    Check only against "open" DuplicateProfile (#12463)
    changelog: Upcoming Features, One Account, Check only against "open" DuplicateProfile

--- app/models/duplicate_profile.rb
+++ app/models/duplicate_profile.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DuplicateProfile < ApplicationRecord
+  scope :open, -> { where(closed_at: nil) }
+
   def self.involving_profile(profile_id:, service_provider:)
     where(service_provider: service_provider)
       .where('? = ANY(profile_ids)', profile_id).first
