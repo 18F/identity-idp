7c6801c015a0bbf6822688e379085d44a4c087c9
Alex Bradley <alexander.bradley@gsa.gov>
2025-09-04 11:48:01 -0400

    set socure docv attributes to nil in choose_id_type_concern

--- app/controllers/concerns/idv/choose_id_type_concern.rb
+++ app/controllers/concerns/idv/choose_id_type_concern.rb
@@ -12,17 +12,9 @@ module Idv
 
     def set_passport_requested
       if passport_chosen?
-        unless document_capture_session.passport_requested?
-          document_capture_session.update!(
-            passport_status: 'requested',
-            doc_auth_vendor: nil,
-          )
-        end
+        set_passport_as_requested
       else
-        document_capture_session.update!(
-          passport_status: 'not_requested',
-          doc_auth_vendor: nil,
-        )
+        set_passport_as_not_requested
       end
     end
 
@@ -58,6 +50,40 @@ module Idv
         form_submit_url:,
         disable_passports:,
         auto_check_value: disable_passports ? :drivers_license : selected_id_type,
+      }
+    end
+
+    private
+
+    def set_passport_as_requested
+      return if document_capture_session.passport_requested?
+
+      document_capture_session.update!(
+        passport_status: 'requested',
+        doc_auth_vendor: nil,
+        socure_docv_capture_app_url: nil,
+        socure_docv_transaction_token: nil,
+      )
+    end
+
+    def set_passport_as_not_requested
+      attributes = passport_not_requested_attributes
+      attributes.merge!(clear_socure_attributes) if document_capture_session.passport_requested?
+
+      document_capture_session.update!(attributes)
+    end
+
+    def passport_not_requested_attributes
+      {
+        passport_status: 'not_requested',
+        doc_auth_vendor: nil,
+      }
+    end
+
+    def clear_socure_attributes
+      {
+        socure_docv_capture_app_url: nil,
+        socure_docv_transaction_token: nil,
       }
     end
   end
