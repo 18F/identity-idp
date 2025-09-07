e534dba00d2d066c8cb47ade9d389c8a6fab9c15
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-09-06 21:43:09 -0400

    fix specs

--- spec/features/idv/doc_auth/document_capture_spec.rb
+++ spec/features/idv/doc_auth/document_capture_spec.rb
@@ -698,6 +698,7 @@ RSpec.feature 'document capture step', :js do
               click_on t('forms.buttons.back')
               expect(page).to have_current_path(idv_how_to_verify_url)
               click_on t('forms.buttons.continue_online')
+              complete_choose_id_type_step
               expect(page).to have_current_path(idv_document_capture_url)
             end
           end
@@ -938,6 +939,7 @@ RSpec.feature 'document capture step', :js do
               expect(page).to have_content(t('doc_auth.headings.how_to_verify'))
               expect(page).to have_content(t('doc_auth.info.upload_from_computer'))
               click_on t('forms.buttons.upload_photos')
+              complete_choose_id_type_step
               expect(page).to have_current_path(idv_document_capture_url)
               expect_step_indicator_current_step(t('step_indicator.flows.idv.verify_id'))
               expect(page).to have_text(t('doc_auth.headings.document_capture'))
--- spec/support/features/doc_auth_helper.rb
+++ spec/support/features/doc_auth_helper.rb
@@ -104,12 +104,14 @@ module DocAuthHelper
     unless page.mode == :headless_chrome_mobile
       if IdentityConfig.store.in_person_proofing_opt_in_enabled
         click_on t('forms.buttons.continue_online')
-      else
-        complete_hybrid_handoff_step
-        expect_page_to_have_no_accessibility_violations(page) if expect_accessible
-        complete_choose_id_type_step(expect_accessible:, choose_id_type:)
       end
+
+      expect_page_to_have_no_accessibility_violations(page) if expect_accessible
+      complete_hybrid_handoff_step
     end
+
+    complete_choose_id_type_step(expect_accessible:, choose_id_type:)
+    expect_page_to_have_no_accessibility_violations(page) if expect_accessible
   end
 
   def complete_up_to_how_to_verify_step_for_opt_in_ipp(remote: true)
