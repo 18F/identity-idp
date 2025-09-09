235a96ff5abca47acf392e297a030c10a40b63a9
Alex Bradley <alexander.bradley@gsa.gov>
2025-09-09 17:16:11 -0400

    make sure mail_only_shown does not get cleared in welcome step

--- app/controllers/idv/welcome_controller.rb
+++ app/controllers/idv/welcome_controller.rb
@@ -24,7 +24,7 @@ module Idv
 
     def update
       clear_future_steps!
-      idv_session.clear
+      clear_idv_session
       idv_session.proofing_started_at ||= Time.zone.now.iso8601
       create_document_capture_session
       analytics.idv_doc_auth_welcome_submitted(**analytics_arguments)
@@ -47,6 +47,12 @@ module Idv
     end
 
     private
+
+    def clear_idv_session
+      mail_only_warning_shown = idv_session.mail_only_warning_shown
+      idv_session.clear
+      idv_session.mail_only_warning_shown = mail_only_warning_shown
+    end
 
     def analytics_arguments
       {
