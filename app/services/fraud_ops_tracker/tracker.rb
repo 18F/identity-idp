12b6b94aa919010f9ab6f70426ecfb7df4bd0907
gerardo.cruz-ortiz <59618057+astrogeco@users.noreply.github.com>
2025-09-10 18:22:00 -0400

    Clean up private methods
    Change order to match source Class
    
    Delete unused methods

--- app/services/fraud_ops_tracker/tracker.rb
+++ app/services/fraud_ops_tracker/tracker.rb
@@ -6,20 +6,6 @@ module FraudOpsTracker
 
     private
 
-    def extra_attributes(event_type:)
-      {
-        agency_uuid: agency_uuid(event_type:),
-        user_uuid: user&.uuid,
-        user_id: user&.id,
-        unique_session_id: user&.unique_session_id,
-      }
-    end
-
-    # TODO: Come up with something for when issuer is nil
-    def issuer
-      sp&.issuer
-    end
-
     def jwe(event)
       if fraudops_key_exists?
         super
@@ -29,12 +15,26 @@ module FraudOpsTracker
       end
     end
 
+    def extra_attributes(event_type:)
+      {
+        agency_uuid: agency_uuid(event_type:),
+        user_uuid: user&.uuid,
+        user_id: user&.id,
+        unique_session_id: user&.unique_session_id,
+      }
+    end
+
     def enabled?
       FeatureManagement.fraudops_enabled?
     end
 
     def redis_client
       @redis_client ||= RedisClient.new
+    end
+
+    # TODO: Come up with something for when issuer is nil
+    def issuer
+      sp&.issuer
     end
 
     def public_key
@@ -47,15 +47,6 @@ module FraudOpsTracker
 
     def fraudops_config
       IdentityConfig.store.fraudops_config
-    end
-
-    def key(timestamp, issuer)
-      formatted_time = timestamp.in_time_zone('UTC').change(min: 0, sec: 0).iso8601
-      "fraudops-events:#{sanitize(issuer)}:#{sanitize(formatted_time)}"
-    end
-
-    def sanitize(key_string)
-      key_string.tr(':', '-')
     end
   end
 end
