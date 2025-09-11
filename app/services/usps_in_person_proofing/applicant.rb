677265ed63ccf20bd5e271003dea90a8f95b5551
Shane Chesnutt <shane.chesnutt@gsa.gov>
2025-09-11 17:15:08 -0400

    LG-16718 Add document attributes to USPS enrollment request
    changelog: Internal, Upcoming Feature, Add document attributes to USPS enrollment request. Attributes are behind a feature flag to be enabled in the future.

--- app/services/usps_in_person_proofing/applicant.rb
+++ app/services/usps_in_person_proofing/applicant.rb
@@ -1,12 +1,40 @@
 # frozen_string_literal: true
 
 module UspsInPersonProofing
-  Applicant = Struct.new(
+  Applicant = RedactedStruct.new(
     :unique_id, :first_name, :last_name, :address, :city, :state, :zip_code,
-    :email, keyword_init: true
+    :email, :document_type, :document_number, :document_expiration_date, keyword_init: true
   ) do
+    def self.from_usps_applicant_and_enrollment(applicant, enrollment)
+      self.new(
+        unique_id: enrollment.unique_id,
+        first_name: transliterate(applicant.first_name),
+        last_name: transliterate(applicant.last_name),
+        address: transliterate(applicant.address1),
+        city: transliterate(applicant.city),
+        state: applicant.state,
+        zip_code: applicant.zipcode,
+        email: IdentityConfig.store.usps_ipp_enrollment_status_update_email_address.presence,
+        document_number: applicant.id_number,
+        document_expiration_date: applicant.id_expiration,
+        document_type: enrollment.document_type,
+      )
+    end
+
     def has_valid_address?
       (address =~ /[^A-Za-z0-9\-' .\/#]/).nil?
     end
-  end
+
+    private
+
+    def self.transliterate(value)
+      transliterator = Transliterator.new
+      result = transliterator.transliterate(value)
+      if result.unsupported_chars.present?
+        result.original
+      else
+        result.transliterated
+      end
+    end
+  end.freeze
 end
