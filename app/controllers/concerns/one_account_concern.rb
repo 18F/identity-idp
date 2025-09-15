bcb83277b9f3456922a571163d38306eac17f261
Malick DiarrA <malick.diarra@gsa.gov>
2025-09-15 09:49:32 -0400

    update rspec

--- app/controllers/concerns/one_account_concern.rb
+++ app/controllers/concerns/one_account_concern.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module OneAccountConcern
-  def handle_duplicate_profile_user
+  def handle_duplicate_profile_user(source: :sign_in)
     return unless user_duplicate_profiles_detected?
-    redirect_to duplicate_profiles_detected_url(source: :sign_in)
+    redirect_to duplicate_profiles_detected_url(source: source)
   end
 
   def user_duplicate_profiles_detected?
@@ -18,15 +18,9 @@ module OneAccountConcern
   end
 
   def user_eligible_for_one_account?
-    return false unless sp_eligible_for_one_account?
-    return false unless current_user&.active_profile
-    return false unless user_in_one_account_verification_bucket?
-    true
-  end
-
-  def user_in_one_account_verification_bucket?
-    return true if user_has_duplicate_account_profiles?
-    ab_test_bucket(:ONE_ACCOUNT_USER_VERIFICATION_ENABLED) == :one_account_user_verification_enabled
+    sp_eligible_for_one_account? &&
+      current_user&.active_profile &&
+      user_in_one_account_verification_bucket?
   end
 
   def sp_eligible_for_one_account?
@@ -36,6 +30,15 @@ module OneAccountConcern
   def profile_creation_duplicate_profile_detected?
     return false unless IdentityConfig.store.one_account_profile_creation_check_enabled
     user_has_duplicate_account_profiles?
+  end
+
+  def user_in_one_account_verification_bucket?
+    return true if user_has_duplicate_account_profiles?
+    ab_test_bucket(:ONE_ACCOUNT_USER_VERIFICATION_ENABLED) == :one_account_user_verification_enabled
+  end
+
+  def handle_duplicate_profile_account_creation_user
+    handle_duplicate_profile_account_creation_user(source: :account_verified)
   end
 
   def user_has_duplicate_account_profiles?
