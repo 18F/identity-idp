b346fbe4fb691f44a06cd476214929d7ed49df15
Malick DiarrA <malick.diarra@gsa.gov>
2025-09-18 13:56:43 -0400

    changelog: Internal, One Account, Basic Feature tests for flow

--- spec/features/one_account/sign_in_spec.rb
+++ spec/features/one_account/sign_in_spec.rb
@@ -12,7 +12,9 @@ RSpec.feature 'One Account Sign In' do
   include AbTestsHelper
 
   let(:user) { create(:user, :fully_registered) }
-  let(:service_provider) { create(:service_provider, :active, issuer: 'urn:gov:gsa:openidconnect:sp:server') }
+  let(:service_provider) do
+    create(:service_provider, :active, issuer: 'urn:gov:gsa:openidconnect:sp:server')
+  end
   let(:pii_attrs) do
     {
       first_name: 'John',
@@ -22,7 +24,7 @@ RSpec.feature 'One Account Sign In' do
       address1: '123 Main St',
       city: 'Anytown',
       state: 'NY',
-      zipcode: '12345'
+      zipcode: '12345',
     }
   end
 
@@ -31,15 +33,15 @@ RSpec.feature 'One Account Sign In' do
     let(:user2) { create(:user, :fully_registered) }
     let(:issuer) { OidcAuthHelper::OIDC_ISSUER }
     let(:current_sp) { ServiceProvider.find_by(issuer: issuer) }
-    let(:ssn_fingerprint) { 'aaa' }  
+    let(:ssn_fingerprint) { 'aaa' }
     let!(:profile1) do
       create(
-        :profile, 
-        :active, 
-        :facial_match_proof, 
-        :with_pii, 
+        :profile,
+        :active,
+        :facial_match_proof,
+        :with_pii,
         ssn_signature: ssn_fingerprint,
-        user: user
+        user: user,
       )
     end
 
@@ -48,7 +50,7 @@ RSpec.feature 'One Account Sign In' do
         .and_return([issuer])
     end
 
-    context 'User has profile with matching SSN signature' do
+    context 'User2 has profile with matching SSN signature' do
       let(:user2) { create(:user, :fully_registered) }
       let!(:profile2) do
         create(
@@ -57,17 +59,16 @@ RSpec.feature 'One Account Sign In' do
           :facial_match_proof,
           :with_pii,
           ssn_signature: ssn_fingerprint,
-          user: user2
+          user: user2,
         )
       end
-      context 'with Matching User with profile linked to SP' do
-      
+      context 'with User2 with profile linked to SP' do
         before do
           link_identity(user, current_sp, 2)
           link_identity(user2, current_sp, 2)
         end
 
-        scenario 'User sign in shows duplicate profile detected' do
+        scenario 'User1 sign in shows duplicate profile detected' do
           visit_idp_from_ial2_oidc_sp(facial_match_required: true)
           sign_in_user(user)
           fill_in_code_with_last_phone_otp
@@ -76,11 +77,14 @@ RSpec.feature 'One Account Sign In' do
         end
       end
 
-      context 'with Matching User with profile but not linked to SP yet' do
+      context 'with User2 with profile but not linked to SP yet' do
         before do
           link_identity(user, current_sp, 2)
           identity = user.identities.find_by(service_provider: current_sp.issuer)
-          identity.update(verified_attributes: ["email", "given_name", "family_name", "social_security_number"])
+          identity.update(
+            verified_attributes: ['email', 'given_name', 'family_name',
+                                  'social_security_number'],
+          )
         end
 
         scenario 'User sign in gets to  SP' do
@@ -93,48 +97,56 @@ RSpec.feature 'One Account Sign In' do
         end
       end
 
-      context 'Matching User with profile but linked to different sp' do
-        let(:different_sp) { create(:service_provider, :active, issuer: 'urn:gov:gsa:openidconnect:sp:server2') }
-
-        before do
-          link_identity(user, current_sp, 2)
-          link_identity(user2, different_sp, 2)
+      context 'User2 with profile but linked to different sp' do
+        let(:different_sp) do
+          create(:service_provider, :active, issuer: 'urn:gov:gsa:openidconnect:sp:server2')
         end
 
-        scenario 'User sign in shows duplicate profile detected' do
+        before do
+          identity = link_identity(user, current_sp, 2)
+          link_identity(user2, different_sp, 2)
+          identity.update(
+            verified_attributes: ['email', 'given_name', 'family_name',
+                                  'social_security_number'],
+          )
+        end
+
+        scenario 'User is redirected to SP without issue' do
           visit_idp_from_ial2_oidc_sp(facial_match_required: true)
           sign_in_user(user)
           fill_in_code_with_last_phone_otp
           click_submit_default
-          expect(page).to have_current_path(duplicate_profiles_detected_path(source: :sign_in))
+
+          expect(oidc_redirect_url).to match('http://localhost:7654/auth/result')
         end
       end
     end
 
-    # context 'with Matching User with profile but not linked to SP yet' do
-    #   let!(:profile) { create(:profile, :active, :facial_match_proof, :with_pii, user: user) }
-    #   before do
-    #     allow(IdentityConfig.store).to receive(:eligible_one_account_providers)
-    #       .and_return([issuer])
-    #   end
+    context 'User2 has profile with non-matching SSN signature' do
+      let(:user2) { create(:user, :fully_registered) }
+      let!(:profile2) do
+        create(
+          :profile,
+          :active,
+          :facial_match_proof,
+          :with_pii,
+          ssn_signature: 'bbb',
+          user: user2,
+        )
+      end
 
-    #   scenario 'User sign in links identity and goes to SP' do
-    #     visit_idp_from_ial2_oidc_sp(facial_match_required: true)
-    #     sign_in_user(user)
-    #     fill_in_code_with_last_phone_otp
-    #     click_submit_default
+      before do
+        link_identity(user, current_sp, 2)
+        link_identity(user2, current_sp, 2)
+      end
 
-    #     expect(page).to have_current_path(root_path)
-
-    #     identity = user.identities.find_by(service_provider: current_sp.issuer)
-    #     expect(identity).not_to be_nil
-    #     expect(identity.ial).to eq(2)
-    #     expect(identity.last_consented_at).not_to be_nil
-    #   end
-    # end
-  end
-
-  context 'with One account disabled for SP' do
-
+      scenario 'User1 sign in does not show duplicate profile detected' do
+        visit_idp_from_ial2_oidc_sp(facial_match_required: true)
+        sign_in_user(user)
+        fill_in_code_with_last_phone_otp
+        click_submit_default
+        expect(oidc_redirect_url).to match('http://localhost:7654/auth/result')
+      end
+    end
   end
 end
