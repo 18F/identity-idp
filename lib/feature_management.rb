1bdf39351180a2f56e6b355a0915843d68782ad9
Moncef Belyamani <moncef.belyamani@gsa.gov>
2025-09-25 13:35:28 -0400

    Remove unused config setting for approving IPP enrollments (#12477)
    * Remove unused config setting for approving IPP enrollments
    
    I had originally submitted PR #12014 to allow immediate approval of
    IPP enrollments in `int`, to make it easier for partners to test IPP in
    the sandbox environment. At the time, I thought this was the only way to
    do it, after seeing PR #11653 that introduced the ability to manually
    approve IPP enrollments in local development only.
    
    However, it turns out the route that displays the IPP enrollments is
    behind another config setting that requires enabling test routes. We
    didn't think it was a good idea to enable test routes in `int`, and we
    then realized that solving our problem was as easy as changing these two
    settings that were causing the 1.5 hour delay between completing IPP and
    receiving the confirmation email:
    
    ```
    get_usps_proofing_results_job_cron: '0/1 * * * *'
    in_person_results_delay_in_hours: 0
    ```
    
    whereas the default values in our lower environments were:
    
    ```
    get_usps_proofing_results_job_cron: '0/30 * * * *'
    in_person_results_delay_in_hours: 1
    ```
    which resulted in having to wait 1.5 hours to complete IPP testing.
    
    changelog: Internal, IPP, Remove unused config setting

--- lib/feature_management.rb
+++ lib/feature_management.rb
@@ -175,13 +175,6 @@ class FeatureManagement
       outage_status.phone_finder_outage?
   end
 
-  # This feature allows pending IPP enrollments to be approved immediately, as
-  # opposed to having to wait close to 2 hours, which is not ideal when testing.
-  # See test/ipp_controller.rb
-  def self.allow_ipp_enrollment_approval?
-    IdentityConfig.store.in_person_enrollments_immediate_approval_enabled
-  end
-
   def self.doc_escrow_enabled?(service_provider)
     IdentityConfig.store.doc_escrow_enabled && service_provider&.attempts_api_enabled?
   end
