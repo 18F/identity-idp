af5a2fef42e2d09c6eae0f5f73474d93f2d62aaa
Shane Chesnutt <shane.chesnutt@gsa.gov>
2025-09-26 10:22:43 -0400

    LG-16724 Add methods to pii passport and state_id structs
    Added methods to pii passport and state_id structs for determining if
    residential address is needed and constructing pii address from internal
    object data.
    
    changelog: Internal, Remote IdV, Add methods to pii passport and state_id structs

---
+++ spec/services/pii/passport_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Pii::Passport do
+  let(:passport) do
+    {
+      first_name: Faker::Name.first_name,
+      middle_name: Faker::Name.middle_name,
+      last_name: Faker::Name.last_name,
+      dob: Faker::Date.between(from: 90.years.ago, to: 13.years.ago).strftime('%Y-%m-%d'),
+      sex: Faker::Gender.short_binary_type,
+      birth_place: Faker::Address.city,
+      passport_expiration: Faker::Date.between(from: 1.day.after, to: 2.years.after)
+        .strftime('%Y-%m-%d'),
+      issuing_country_code: 'USA',
+      mrz: Idp::Constants::MOCK_IDV_APPLICANT_WITH_PASSPORT[:mrz],
+      passport_issued: Faker::Date.between(from: 3.years.ago, to: 1.year.ago).strftime('%Y-%m-%d'),
+      nationality_code: 'USA',
+      document_number: Faker::Number.number(digits: 9),
+      document_type_received: 'passport',
+    }
+  end
+
+  subject { described_class.new(**passport) }
+
+  describe '#id_doc_type' do
+    it 'returns the value of document_type_received' do
+      expect(subject.id_doc_type).to eq(passport[:document_type_received])
+    end
+  end
+
+  describe '#residential_address_required?' do
+    it 'returns true' do
+      expect(subject.residential_address_required?).to be(true)
+    end
+  end
+
+  describe '#to_pii_address' do
+    it 'returns an empty Pii::Address' do
+      expect(subject.to_pii_address).to have_attributes(
+        address1: nil,
+        address2: nil,
+        city: nil,
+        state: nil,
+        zipcode: nil,
+      )
+    end
+  end
+end
