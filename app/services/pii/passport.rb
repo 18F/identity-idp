b3a6662f30d34be5f135b5be731c9540d2661f76
Shane Chesnutt <shane.chesnutt@gsa.gov>
2025-09-30 11:24:50 -0400

    LG-16724 Add methods to pii passport and state_id structs (#12533)
    Added methods to pii passport and state_id structs for determining if
    residential address is needed and constructing pii address from internal
    object data.
    
    changelog: Internal, Remote IdV, Add methods to pii passport and state_id structs

--- app/services/pii/passport.rb
+++ app/services/pii/passport.rb
@@ -20,6 +20,22 @@ module Pii
     def id_doc_type
       document_type_received
     end
+
+    # @returns [Boolean] Whether the document requires a residential address.
+    def residential_address_required?
+      true
+    end
+
+    # @returns [Pii::Address] The address created from the document.
+    def to_pii_address
+      Pii::Address.new(
+        address1: nil,
+        address2: nil,
+        city: nil,
+        state: nil,
+        zipcode: nil,
+      )
+    end
   end
 end
 # rubocop:enable Style/MutableConstant
