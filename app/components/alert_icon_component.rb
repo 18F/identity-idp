da3dbeafba0054e932a16e8e323b7626d7097446
ashukla <abir.shukla@gsa.gov>
2025-10-02 09:38:55 -0500

    using .present inside render method

--- app/components/alert_icon_component.rb
+++ app/components/alert_icon_component.rb
@@ -23,7 +23,7 @@ class AlertIconComponent < BaseComponent
   end
 
   def render?
-    ICON_SOURCE.key?(icon_name) if ICON_SOURCE[icon_name]
+    ICON_SOURCE[icon_name].present?
   end
 
   def source
