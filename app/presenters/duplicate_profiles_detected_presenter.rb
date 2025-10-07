fc2a3b9ffdb7b4dc69988d21e7a19a8521a6bb53
Joshua Alley <joshua.alley@gsa.gov>
2025-10-07 10:32:33 -0700

    [LG-16741] Fix last logged in on duplicate profile page (#12555)

--- app/presenters/duplicate_profiles_detected_presenter.rb
+++ app/presenters/duplicate_profiles_detected_presenter.rb
@@ -18,11 +18,15 @@ class DuplicateProfilesDetectedPresenter
     profiles = Profile.where(id: duplicate_profile_set.profile_ids)
     profiles.map do |profile|
       dupe_user = profile.user
+      sp_identity = ServiceProviderIdentity.find_by(
+        user_id: dupe_user.id,
+        service_provider: duplicate_profile_set.service_provider,
+      )
       email = dupe_user.last_sign_in_email_address.email
       {
         email: email,
         masked_email: EmailMasker.mask(email),
-        last_sign_in: dupe_user.last_sign_in_email_address.last_sign_in_at,
+        last_sign_in: sp_identity&.last_authenticated_at,
         created_at: dupe_user.created_at,
         connected_accts: user.connected_apps.count,
         current_account: dupe_user.id == user.id,
