c5c7721452496019eef1fd90b452774926420af8
Jessica Dembe <jessica.dembe@gsa.gov>
2025-10-09 10:31:25 -0400

    WIP

--- app/controllers/concerns/two_factor_authenticatable_methods.rb
+++ app/controllers/concerns/two_factor_authenticatable_methods.rb
@@ -116,14 +116,12 @@ module TwoFactorAuthenticatableMethods
   end
 
   def handle_max_attempts(type)
-    disavowal_token = create_user_event_with_disavowal(:sign_in_unsuccessful_2fa)
-
     presenter = TwoFactorAuthCode::MaxAttemptsReachedPresenter.new(
       type,
       current_user,
     )
 
-    UserAlerts::AlertUserAboutMaxAttempts.call(user: current_user, disavowal_token:)
+    UserAlerts::AlertUserAboutMaxAttempts.call(user: current_user)
 
     sign_out
     render_full_width('two_factor_authentication/_locked', locals: { presenter: presenter })
--- app/services/user_alerts/alert_user_about_max_attempts.rb
+++ app/services/user_alerts/alert_user_about_max_attempts.rb
@@ -2,11 +2,11 @@
 
 module UserAlerts
   class AlertUserAboutMaxAttempts
-    def self.call(user:, disavowal_token:)
+    def self.call(user:)
       user.confirmed_email_addresses.each do |email_address|
         mailer = UserMailer.with(user:, email_address:)
         mailer.with(user: user, email_address: email_address)
-          .new_device_sign_in_before_2fa(events:, disavowal_token:)
+          .new_device_sign_in_before_2fa(events:)
       end
     end
   end
