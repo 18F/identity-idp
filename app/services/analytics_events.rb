4f6664cbf387651d789f07b4e5506baf32add987
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-10-10 15:29:14 -0400

    happy linting

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -5597,6 +5597,25 @@ module AnalyticsEvents
     )
   end
 
+  # Logs a Socure Phone Risk result alongside a resolution result for later comparison.
+  # @param [Hash] socure_result Result from Socure KYC API call
+  # @param [Hash] phone_result Result from resolution proofing
+  # @param [String,nil] phone_source Whether the phone number is from MFA or hybrid handoff
+  def idv_socure_shadow_mode_phonerisk_result(
+    socure_result:,
+    phone_result:,
+    phone_source:,
+    **extra
+  )
+    track_event(
+      :idv_socure_shadow_mode_phonerisk_result,
+      phone_result: phone_result.to_h,
+      phone_source:,
+      socure_result: socure_result.to_h,
+      **extra,
+    )
+  end
+
   # Logs a Socure KYC result alongside a resolution result for later comparison.
   # @param [Hash] socure_result Result from Socure KYC API call
   # @param [Hash] resolution_result Result from resolution proofing
@@ -5620,25 +5639,6 @@ module AnalyticsEvents
   # attempted to look for one.
   def idv_socure_shadow_mode_proofing_result_missing(**extra)
     track_event(:idv_socure_shadow_mode_proofing_result_missing, **extra)
-  end
-
-  # Logs a Socure Phone Risk result alongside a resolution result for later comparison.
-  # @param [Hash] socure_result Result from Socure KYC API call
-  # @param [Hash] phone_result Result from resolution proofing
-  # @param [String,nil] phone_source Whether the phone number is from MFA or hybrid handoff
-  def idv_socure_shadow_mode_phonerisk_result(
-    socure_result:,
-    phone_result:,
-    phone_source:,
-    **extra
-  )
-    track_event(
-      :idv_socure_shadow_mode_phonerisk_result,
-      phone_result: phone_result.to_h,
-      phone_source:,
-      socure_result: socure_result.to_h,
-      **extra,
-    )
   end
 
   # Indicates that no proofing result was found when SocureShadowModeProofingJob
