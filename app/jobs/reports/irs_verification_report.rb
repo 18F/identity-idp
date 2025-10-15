e7b300ffdf3edb70801b4e83db848afc9d4e9f50
shilen <shilen.patel@gsa.gov>
2025-10-15 17:04:40 -0500

    [GL-Data-Reporting-234] Add Job_configuration helper and cleanup Job configuration (#12595)
    * Add Job_configuration helper and change Job Schedulers and refactor reports & specs
    
    * changelog: Internal, Reporting, Cleanup IRS Job configs
    
    see https://gitlab.login.gov/lg-teams/Team-Data/reporting/-/issues/234

--- app/jobs/reports/irs_verification_report.rb
+++ app/jobs/reports/irs_verification_report.rb
@@ -7,10 +7,11 @@ module Reports
   class IrsVerificationReport < BaseReport
     REPORT_NAME = 'irs-verification-report'
 
-    attr_reader :report_date
+    attr_reader :report_date, :report_receiver
 
-    def initialize(report_date = nil, *args, **rest)
+    def initialize(report_date = nil, report_receiver = :internal, *args, **rest)
       @report_date = report_date
+      @report_receiver = report_receiver.to_sym
       super(*args, **rest)
     end
 
@@ -60,11 +61,7 @@ module Reports
     end
 
     def previous_week_range
-      today = Time.zone.today
-      last_sunday = today.beginning_of_week(:sunday) - 7.days
-      last_saturday = last_sunday + 6.days
-
-      last_sunday.beginning_of_day..last_saturday.end_of_day
+      @report_date.beginning_of_week(:sunday).prev_occurring(:sunday).all_week(:sunday)
     end
 
     def irs_verification_report
@@ -75,11 +72,13 @@ module Reports
     end
 
     def emails
-      emails = [*IdentityConfig.store.irs_verification_report_config]
-      if report_date.next_day.day == 1
-        emails += IdentityConfig.store.team_all_login_emails
+      internal_emails = [*IdentityConfig.store.irs_verification_report_config]
+      irs_emails = [] # Need to add IRS email config
+
+      case report_receiver
+      when :internal then internal_emails
+      when :both then (internal_emails + irs_emails)
       end
-      emails
     end
 
     def upload_to_s3(report_body, report_name: nil)
