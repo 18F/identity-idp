fbc51aeeccacff9a58fe963239ddacc5d20a312e
Malick Diarra <malick.diarra@gsa.gov>
2025-10-22 13:21:34 -0400

    Lg 16478: LG-77 block from accessing account (#12599)
    * update application controller
    
    * changelog: User-Facing Improvements, Authentication, Prevent users that are suspended from performing any action
    
    * update sign in logic for suspended user
    
    * use existing rspec method instead of creating new one
    
    * open
    
    * update application controller and move to two factor authentication
    
    * Empty commit
    
    * fix application controller

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -33,7 +33,7 @@ RSpec.feature 'Sign in' do
       .to have_link t('devise.failure.not_found_in_database_link_text', href: link_url)
   end
 
-  scenario 'user is suspended, gets show please call page after 2fa' do
+  scenario 'user is suspended before 2fa' do
     user = create(:user, :fully_registered, :suspended)
     service_provider = ServiceProvider.find_by(issuer: OidcAuthHelper::OIDC_IAL1_ISSUER)
     IdentityLinker.new(user, service_provider).link_identity(
@@ -42,8 +42,6 @@ RSpec.feature 'Sign in' do
 
     visit_idp_from_sp_with_ial1(:oidc)
     fill_in_credentials_and_submit(user.email, user.password)
-    fill_in_code_with_last_phone_otp
-    click_submit_default
 
     expect(page).to have_current_path(user_please_call_path)
   end
