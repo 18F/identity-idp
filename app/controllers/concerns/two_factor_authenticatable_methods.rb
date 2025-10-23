beb099bb51348c3bca67336a5cfdf19c2c4f6783
Jessica Dembe <jessica.dembe@gsa.gov>
2025-10-23 10:16:21 -0400

    merge all changes from 401cb85

--- app/controllers/concerns/two_factor_authenticatable_methods.rb
+++ app/controllers/concerns/two_factor_authenticatable_methods.rb
@@ -116,10 +116,17 @@ module TwoFactorAuthenticatableMethods
   end
 
   def handle_max_attempts(type)
+    disavowal_token = create_user_event_with_disavowal(:sign_in_before_2fa)
     presenter = TwoFactorAuthCode::MaxAttemptsReachedPresenter.new(
       type,
       current_user,
     )
+
+    UserAlerts::AlertUserAboutMaxAttempts.max_attempts_alert(
+      user: current_user,
+      disavowal_token:,
+    )
+
     sign_out
     render_full_width('two_factor_authentication/_locked', locals: { presenter: presenter })
   end
