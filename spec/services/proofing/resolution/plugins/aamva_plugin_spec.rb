568057a54175f1da9499715d12abfb39a5096b52
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-10-23 15:39:42 -0400

    move passport_applicant? specs to aamva_plugin spec

--- spec/services/proofing/resolution/plugins/aamva_plugin_spec.rb
+++ spec/services/proofing/resolution/plugins/aamva_plugin_spec.rb
@@ -384,4 +384,90 @@ RSpec.describe Proofing::Resolution::Plugins::AamvaPlugin do
       end
     end
   end
+
+  describe '#passport_applicant?' do
+    context 'with new field name (document_type_received)' do
+      let(:applicant_pii) do
+        {
+          document_type_received: 'passport',
+          first_name: 'Test',
+          last_name: 'User',
+        }
+      end
+
+      it 'correctly identifies passport applicant' do
+        expect(described_class.new.send(:passport_applicant?, applicant_pii)).to be true
+      end
+    end
+
+    context 'with old field name (id_doc_type)' do
+      let(:applicant_pii) do
+        {
+          id_doc_type: 'passport',
+          first_name: 'Test',
+          last_name: 'User',
+        }
+      end
+
+      it 'correctly identifies passport applicant using old field name' do
+        expect(described_class.new.send(:passport_applicant?, applicant_pii)).to be true
+      end
+    end
+
+    context 'with both field names present (new takes precedence)' do
+      let(:applicant_pii) do
+        {
+          document_type_received: 'passport',
+          id_doc_type: 'drivers_license',
+          first_name: 'Test',
+          last_name: 'User',
+        }
+      end
+
+      it 'uses new field name when both are present' do
+        expect(described_class.new.send(:passport_applicant?, applicant_pii)).to be true
+      end
+    end
+
+    context 'with non-passport document using new field' do
+      let(:applicant_pii) do
+        {
+          document_type_received: 'drivers_license',
+          first_name: 'Test',
+          last_name: 'User',
+        }
+      end
+
+      it 'correctly identifies non-passport applicant' do
+        expect(described_class.new.send(:passport_applicant?, applicant_pii)).to be false
+      end
+    end
+
+    context 'with non-passport document using old field' do
+      let(:applicant_pii) do
+        {
+          id_doc_type: 'drivers_license',
+          first_name: 'Test',
+          last_name: 'User',
+        }
+      end
+
+      it 'correctly identifies non-passport applicant using old field' do
+        expect(described_class.new.send(:passport_applicant?, applicant_pii)).to be false
+      end
+    end
+
+    context 'with neither field present' do
+      let(:applicant_pii) do
+        {
+          first_name: 'Test',
+          last_name: 'User',
+        }
+      end
+
+      it 'returns false when document type is not specified' do
+        expect(described_class.new.send(:passport_applicant?, applicant_pii)).to be false
+      end
+    end
+  end
 end
