1fb3a7c85d85c86c8c35a96f628553c8b94f2109
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-10-24 17:55:41 -0400

    init draft of phone precheck

--- app/controllers/idv/verify_info_controller.rb
+++ app/controllers/idv/verify_info_controller.rb
@@ -59,6 +59,8 @@ module Idv
           idv_session.verify_info_step_document_capture_session_uuid = nil
           idv_session.threatmetrix_review_status = nil
           idv_session.applicant = nil
+          idv_session.phone_precheck_successful = nil
+          idv_session.invalidate_phone_step!
         end,
       )
     end
