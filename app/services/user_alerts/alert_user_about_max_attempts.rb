c7a303b8262e087be0a2cdd13a3678f9183ff42d
Jessica Dembe <jessica.dembe@gsa.gov>
2025-10-27 14:25:09 -0400

    only show unsuccessful attempts in email
    changelog: Bug Fixes, email aggregation, show only events leading to account lockout

--- app/services/user_alerts/alert_user_about_max_attempts.rb
+++ app/services/user_alerts/alert_user_about_max_attempts.rb
@@ -6,10 +6,9 @@ module UserAlerts
       events = user.events.where(
         created_at: user.sign_in_new_device_at..,
         event_type: [
-          'sign_in_before_2fa',
           'sign_in_unsuccessful_2fa',
         ],
-      ).order(:created_at).includes(:device).to_a
+      ).order(created_at: :desc).includes(:device).limit(IdentityConfig.store.login_otp_confirmation_max_attempts).to_a
 
       user.confirmed_email_addresses.each do |email_address|
         UserMailer.with(user:, email_address:)
