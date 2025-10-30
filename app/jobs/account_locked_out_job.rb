b88c298250db6409c62d54bf3486eb9a4d8732a7
Jessica Dembe <jessica.dembe@gsa.gov>
2025-10-30 13:02:50 -0400

    add logic to send email after 5 minutes

---
+++ app/jobs/account_locked_out_job.rb
@@ -0,0 +1,38 @@
+class CreateNewDeviceAlertJob < ApplicationJob
+  queue_as :default
+
+  def perform(now)
+    emails_sent = 0
+    User.where(
+      sql_query_for_users_with_new_device,
+      tvalue: now - IdentityConfig.store.new_device_alert_delay_in_minutes.minutes,
+    ).limit(1_000).find_each(batch_size: 100) do |user|
+      emails_sent += 1 if expire_sign_in_notification_timeframe_and_send_alert(user)
+    end
+
+    emails_sent
+  end
+
+  private
+
+  def analytics
+    @analytics ||= Analytics.new(user: AnonymousUser.new, request: nil, sp: nil, session: {})
+  end
+
+  def sql_query_for_users_with_new_device
+    <<~SQL
+      sign_in_new_device_at IS NOT NULL AND
+      sign_in_new_device_at < :tvalue
+    SQL
+  end
+
+  def expire_sign_in_notification_timeframe_and_send_alert(user)
+    disavowal_event, disavowal_token = UserEventCreator.new(current_user: user)
+      .create_out_of_band_user_event_with_disavowal(:sign_in_notification_timeframe_expired)
+
+    if !user.second_factor_locked_at.present? &&
+        user.second_factor_attempts_count < !IdentityConfig.store.login_otp_confirmation_max_attempts
+      UserAlerts::AlertUserAboutNewDevice.send_alert(user:, disavowal_event:, disavowal_token:)
+    end
+  end
+end
