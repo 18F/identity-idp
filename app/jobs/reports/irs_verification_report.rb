d433f2c8ca763006df4fbeb8b8244e554ba4d8b2
shilen.patel <shilen.patel@gsa.gov>
2025-11-14 16:21:06 -0600

    Add irs verification report file

---
+++ app/jobs/reports/irs_verification_report.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'reporting/irs_verification_report'
+
+module Reports
+  class IrsVerificationReport < BaseReport
+    REPORT_NAME = 'irs-verification-report'
+
+    attr_reader :report_date, :report_receiver
+
+    def initialize(init_date = nil, init_receiver = :internal, *args, **rest)
+      @report_date = init_date
+      @report_receiver = init_receiver.to_sym
+      super(init_date, init_receiver, *args, **rest)
+    end
+
+    def perform(perform_date = Time.zone.yesterday.end_of_day, perform_receiver = :internal)
+      @report_date = perform_date
+      @report_receiver = perform_receiver.to_sym
+
+      email_addresses = emails.select(&:present?)
+      if email_addresses.empty?
+        Rails.logger.warn 'No email addresses received - IRS Verification Report NOT SENT'
+        return false
+      end
+
+      reports.each do |report|
+        upload_to_s3(report.table, report_name: report.filename)
+      end
+
+      ReportMailer.tables_report(
+        email: email_addresses,
+        subject: "IRS Verification Report - #{report_date.to_date}",
+        reports: reports,
+        message: preamble,
+        attachment_format: :csv,
+      ).deliver_now
+    end
+
+    # Explanatory text to go before the report in the email
+    # @return [String]
+    def preamble(env: Identity::Hostdata.env || 'local')
+      ERB.new(<<~ERB).result(binding).html_safe # rubocop:disable Rails/OutputSafety
+        <% if env != 'prod' %>
+          <div class="usa-alert usa-alert--info usa-alert--email">
+            <div class="usa-alert__body">
+              <h2 class="usa-alert__heading">
+                Non-Production Report
+              </h2>
+              <p class="usa-alert__text">
+                This was generated in the <strong><%= env %></strong> environment.
+              </p>
+            </div>
+          </div>
+        <% end %>
+      ERB
+    end
+
+    def reports
+      @reports ||= irs_verification_report.as_emailable_reports
+    end
+
+    def previous_week_range
+      @report_date.beginning_of_week(:sunday).prev_occurring(:sunday).all_week(:sunday)
+    end
+
+    def irs_verification_report
+      @irs_verification_report ||= Reporting::IrsVerificationReport.new(
+        time_range: previous_week_range,
+        issuers: IdentityConfig.store.irs_verification_report_issuers || [],
+      )
+    end
+
+    def emails
+      internal_emails = [*IdentityConfig.store.team_daily_reports_emails]
+      irs_emails = [*IdentityConfig.store.irs_verification_report_config]
+
+      case report_receiver
+      when :internal then internal_emails
+      when :both then (internal_emails + irs_emails)
+      end
+    end
+
+    def upload_to_s3(report_body, report_name: nil)
+      _latest, path = generate_s3_paths(REPORT_NAME, 'csv', subname: report_name, now: report_date)
+
+      if bucket_name.present?
+        upload_file_to_s3_bucket(
+          path: path,
+          body: csv_file(report_body),
+          content_type: 'text/csv',
+          bucket: bucket_name,
+        )
+      end
+    end
+
+    def csv_file(report_array)
+      CSV.generate do |csv|
+        report_array.each do |row|
+          csv << row
+        end
+      end
+    end
+  end
+end
