b682e50d74d78812a0924d0fe9327623de70ff5c
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-11-14 21:07:14 -0500

    update name of phone_finder_precheck_passed

--- spec/features/idv/analytics_spec.rb
+++ spec/features/idv/analytics_spec.rb
@@ -105,7 +105,7 @@ RSpec.feature 'Analytics Regression', :js do
       ssn_is_unique: true,
       timed_out: false,
       threatmetrix_review_status: 'pass',
-      phone_finder_precheck_passed: false,
+      phone_precheck_passed: false,
       context: {
         device_profiling_adjudication_reason: 'device_profiling_result_pass',
         resolution_adjudication_reason: 'pass_resolution_and_state_id',
@@ -166,7 +166,7 @@ RSpec.feature 'Analytics Regression', :js do
       ssn_is_unique: true,
       timed_out: false,
       threatmetrix_review_status: 'pass',
-      phone_finder_precheck_passed: false,
+      phone_precheck_passed: false,
       context: {
         device_profiling_adjudication_reason: 'device_profiling_result_pass',
         resolution_adjudication_reason: 'pass_resolution_and_state_id',
