f5a07c851d8aa758e6ff2f3ab9665c5ad4f6b6f8
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2025-11-19 15:32:28 -0500

    pass result success

--- app/jobs/address_proofing_job.rb
+++ app/jobs/address_proofing_job.rb
@@ -36,7 +36,7 @@ class AddressProofingJob < ApplicationJob
       {
         name: 'ProofAddress',
         trace_id: trace_id,
-        success: proofer_result,
+        success: proofer_result[:success],
         timing: timer.results,
       }.to_json,
     )
