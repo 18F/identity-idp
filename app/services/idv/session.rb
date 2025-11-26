8c3fbb3770e0efffff539d91113cb692d826ae05
Malik Warren <33402370+Mawar2@users.noreply.github.com>
2025-11-26 15:19:51 -0500

    LG-16831 Add AAMVA verification to IPP flow before SSN (#12652)
    * Add AAMVA verification to IPP flow before SSN
    
    changelog: Upcoming Features, In-person proofing, Add AAMVA verification before SSN step
    
    * Move AAMVA to state ID controller and make it async
    
    * Add feature test for AAMVA bypass in Socure flow
    
    * Address code review feedback
    
    * fix aamva polling issue and updated analytic event
    
    * Fix IPP AAMVA rate limiter increment and add exception analytics
    
    * Refactor IPP AAMVA integration to standardize rate limiting and add E2E tests
    
    * Add rate limiter increment for IPP AAMVA verification attempts
    
    * Fix rate limiter to allow successful final attempts
    
    * code cleanup and rebase

--- app/services/idv/session.rb
+++ app/services/idv/session.rb
@@ -16,6 +16,9 @@ module Idv
   # @attr idv_consent_given [Boolean, nil]
   # @attr idv_consent_given_at [String, nil]
   # @attr idv_phone_step_document_capture_session_uuid [String, nil]
+  # @attr ipp_aamva_document_capture_session_uuid [String, nil]
+  # @attr ipp_aamva_redirect_url [String, nil]
+  # @attr ipp_aamva_result [Hash, nil]
   # @attr mail_only_warning_shown [Boolean, nil]
   # @attr opted_in_to_in_person_proofing [Boolean, nil]
   # @attr passport_requested [Boolean, nil]
@@ -64,6 +67,9 @@ module Idv
       idv_consent_given
       idv_consent_given_at
       idv_phone_step_document_capture_session_uuid
+      ipp_aamva_document_capture_session_uuid
+      ipp_aamva_redirect_url
+      ipp_aamva_result
       mail_only_warning_shown
       opted_in_to_in_person_proofing
       personal_key
