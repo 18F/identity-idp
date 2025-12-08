9fd61e9260497b4ec017f72ec7ba2c0e877e5edd
Davida Marion <davida.marion@gsa.gov>
2025-12-08 16:31:49 -0500

    Updates ApplicationController to not sent vector_of_trust arg

--- app/services/vot/component_value.rb
+++ app/services/vot/component_value.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Vot
-  ComponentValue = Data.define(:name, :description, :implied_component_values, :requirements).freeze
+  ComponentValue = Data.define(:name, :description, :requirements).freeze
 end
