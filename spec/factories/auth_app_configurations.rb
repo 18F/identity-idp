5f604e3319e87df7feea504f81d2e8325b2fdada
Joshua Alley <joshua.alley@gsa.gov>
2025-12-29 09:02:14 -0800

    adjust factories for new character limits on user supplied names

--- spec/factories/auth_app_configurations.rb
+++ spec/factories/auth_app_configurations.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   Faker::Config.locale = :en
 
   factory :auth_app_configuration do
-    name { Faker::Lorem.unique.words.join(' ') }
+    name { Faker::Lorem.characters(number: 10)}
     otp_secret_key { SecureRandom.hex(16) }
     user
   end
