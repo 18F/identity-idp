c2bbe0fe3c4148c689a1ea2fb26ca1dfa9c7e953
Joshua Alley <joshua.alley@gsa.gov>
2026-01-06 15:50:48 -0800

    Adjusting stubs for configuration names

--- spec/factories/piv_cac_configurations.rb
+++ spec/factories/piv_cac_configurations.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   Faker::Config.locale = :en
 
   factory :piv_cac_configuration do
-    name { Faker::Lorem.unique.words.join(' ') }
+    name { Faker::Lorem.unique.words.join(' ')[0, 19] }
     x509_dn_uuid { Random.uuid }
     user
   end
