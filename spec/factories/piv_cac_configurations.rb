6448fc7115416a2d383884763707e51473b6f78d
Joshua Alley <joshua.alley@gsa.gov>
2026-01-08 10:46:42 -0800

    Changelog: Internal, Flakey Test Improvement, Remove potential for trailing spaces in factory names

--- spec/factories/piv_cac_configurations.rb
+++ spec/factories/piv_cac_configurations.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   Faker::Config.locale = :en
 
   factory :piv_cac_configuration do
-    name { Faker::Lorem.unique.words.join(' ')[0, UserSuppliedNameAttributes::MAX_NAME_LENGTH] }
+    name do
+      Faker::Lorem.unique.words.join(' ')[0, UserSuppliedNameAttributes::MAX_NAME_LENGTH].strip
+    end
     x509_dn_uuid { Random.uuid }
     user
   end
