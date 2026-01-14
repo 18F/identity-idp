bd3b6583445fe99097a568d0edbc31ec31195640
Jessica Dembe <jessica.dembe@gsa.gov>
2026-01-14 12:55:27 -0500

    update test

--- spec/mailers/sms_text_mailer_spec.rb
+++ spec/mailers/sms_text_mailer_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe SmsTextMailer, type: :mailer do
   describe 'daily_voice_limit_reached' do
     let(:mail) { SmsTextMailer.daily_voice_limit_reached }
 
-    it 'renders the headers' do
+    it 'renders the text message' do
       expect(mail.subject).to eq('Daily voice limit reached')
-      expect(mail.to).to eq(['NO EMAIL'])
+      expect(mail.to).to eq('NO EMAIL')
       expect(mail.from).to eq(['no-reply@login.gov'])
     end
   end
