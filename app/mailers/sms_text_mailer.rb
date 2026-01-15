71ef8f0b240747069251420217ca347e9a3a666d
Jessica Dembe <jessica.dembe@gsa.gov>
2026-01-15 15:34:49 -0500

    refactor sms text mailer, add test

--- app/mailers/sms_text_mailer.rb
+++ app/mailers/sms_text_mailer.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class SmsTextMailer < ActionMailer::Base
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.sms_text_mailer.daily_voice_limit_reached.subject
-  #
   def daily_voice_limit_reached
     @message = t('telephony.error.friendly_message.daily_voice_limit_reached')
 
-    mail to: 'NO EMAIL'
+    mail to: email
+  end
+
+  private
+
+  def email
+    'NO EMAIL'
   end
 end
