54ab565aedcbf5a83a43eda7311ddfaba31cf01c
Jessica Dembe <jessica.dembe@gsa.gov>
2026-01-20 15:27:34 -0500

    fix test

--- app/controllers/sms_preview_controller.rb
+++ app/controllers/sms_preview_controller.rb
@@ -2,12 +2,6 @@
 
 class SmsPreviewController < ApplicationController
   def show
-    show_sms_preview
-  end
-
-  private
-
-  def show_sms_preview
     redirect_to '/rails/mailers/sms_text_mailer'
   end
 end
--- spec/controllers/sms_preview_controller_spec.rb
+++ spec/controllers/sms_preview_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe SmsPreviewController do
   describe '#show' do
     it 'redirects to SMS preview page in actionmailer' do
+      allow(IdentityConfig.store).to receive(:rails_mailer_previews_enabled) { true }
       get :show
 
       expect(response).to redirect_to '/rails/mailers/sms_text_mailer'
