34877650165f10b8d8e95feae03d30e0c286d08c
Jessica Dembe <jessica.dembe@gsa.gov>
2026-01-22 10:57:24 -0500

    WIP

--- app/controllers/test/sms_preview_controller.rb
+++ app/controllers/test/sms_preview_controller.rb
@@ -2,8 +2,17 @@
 
 module Test
   class SmsPreviewController < ApplicationController
+    before_action :render_not_found_in_production
+
     def show
       redirect_to '/rails/mailers/sms_text_mailer'
+    end
+
+    private
+
+    def render_not_found_in_production
+      return unless Rails.env.production?
+      render_not_found
     end
   end
 end
