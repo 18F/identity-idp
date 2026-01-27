ea6fa1225ef33699b606ab4bb7e9837588da5005
Jessica Dembe <jessica.dembe@gsa.gov>
2026-01-27 14:26:22 -0500

    refactor templates and mailer

--- app/mailers/sms_text_mailer.rb
+++ app/mailers/sms_text_mailer.rb
@@ -2,24 +2,16 @@
 
 class SmsTextMailer < ActionMailer::Base
   def account_deleted_notice
-    @message = t('telephony.account_deleted_notice', app_name: APP_NAME)
-
     mail_to
   end
 
   def daily_voice_limit_reached
-    @message = t('telephony.error.friendly_message.daily_voice_limit_reached')
-
     mail_to
   end
 
   private
 
   def mail_to
-    mail to: email
-  end
-
-  def email
-    'NO EMAIL'
+    mail to: 'NO EMAIL'
   end
 end
