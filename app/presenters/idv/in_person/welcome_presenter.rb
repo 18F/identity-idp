9540863c73223be0d6b8a89220dc6bd093320c47
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-01-30 16:12:41 -0500

    removing cruft from ipp welcome presenter

--- app/presenters/idv/in_person/welcome_presenter.rb
+++ app/presenters/idv/in_person/welcome_presenter.rb
@@ -14,7 +14,6 @@ module Idv
       include ActionView::Helpers::UrlHelper
 
       attr_accessor :url_options
-      attr_reader :show_sp_reproof_banner
 
       def initialize(decorated_sp_session:)
         @decorated_sp_session = decorated_sp_session
@@ -23,29 +22,6 @@ module Idv
 
       def sp_name
         decorated_sp_session.sp_name || APP_NAME
-      end
-
-      def title
-        if show_sp_reproof_banner
-          t('doc_auth.headings.welcome_sp_reproof', sp_name: sp_name)
-        else
-          t('doc_auth.headings.welcome', sp_name: sp_name)
-        end
-      end
-
-      def explanation_text(help_link)
-        if first_time_idv? || show_sp_reproof_banner
-          t(
-            'doc_auth.info.getting_started_html',
-            sp_name:,
-            link_html: help_link,
-          )
-        else
-          t(
-            'doc_auth.info.stepping_up_html',
-            link_html: help_link,
-          )
-        end
       end
 
       def bullet_points
@@ -94,16 +70,8 @@ module Idv
         decorated_sp_session&.current_user
       end
 
-      def id_type_copy
-        t('doc_auth.instructions.bullet1')
-      end
-
       def bullet_point(text)
         BulletPoint.new(bullet: nil, text:)
-      end
-
-      def first_time_idv?
-        !decorated_sp_session&.current_user&.active_profile?
       end
     end
   end
