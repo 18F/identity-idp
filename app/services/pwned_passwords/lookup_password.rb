84da5fdb3bda0f4985f1a025b1ce4496ac96a312
Joshua Alley <joshua.alley@gsa.gov>
2026-02-04 14:39:51 -0800

    remove development notes

--- app/services/pwned_passwords/lookup_password.rb
+++ app/services/pwned_passwords/lookup_password.rb
@@ -5,11 +5,7 @@ module PwnedPasswords
     PWNED_PASSWORD_FILE = Rails.root.join(IdentityConfig.store.pwned_passwords_file_path).freeze
 
     def self.call(password)
-      response = BinarySearchSortedHashFile.new(PWNED_PASSWORD_FILE).call(password)
-      # JRA: we should not add event here -- it neesd to be only on login
-      puts 'Looooooooooooook'
-      puts response
-      response
+      BinarySearchSortedHashFile.new(PWNED_PASSWORD_FILE).call(password)
     end
   end
 end
