a3bdb1fb425c071d1d4ac86d1e8d4e1503fb3f3b
Shane Chesnutt <shane.chesnutt@gsa.gov>
2026-02-06 16:25:19 -0500

    Add search route
    Still need to complete search view

--- app/controllers/idv/in_person/post_office_controller.rb
+++ app/controllers/idv/in_person/post_office_controller.rb
@@ -10,6 +10,22 @@ module Idv
       def show
         @presenter = Idv::InPerson::PostOfficePresenter.new
       end
+
+      def search
+        @presenter = Idv::InPerson::PostOfficePresenter.new
+        @post_offices = [{
+          address: 'Happy Lane',
+          city: 'Qwertyton',
+          distance: 1.28,
+          name: 'Happy Post Office',
+          saturday_hours: '1:00 - 2:00',
+          state: 'PA',
+          sunday_hours: '1:00 - 2:00',
+          weekday_hours: '1:00 - 2:00',
+          zip_code_4: '4633',
+          zip_code_5: '12345',
+        }]
+      end
     end
   end
 end
