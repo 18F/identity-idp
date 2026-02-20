d5dbfab5ce0a434f6e5049fb014fd1e6f7f9df9c
shilen <shilen.patel@gsa.gov>
2026-02-20 10:52:15 -0600

    genercize monthly cred metric (#12832)
    * changelog: Internal, Reporting, genercize monthly cred metric
    
    see https://gitlab.login.gov/lg-teams/Team-Data/reporting/-/issues/372

---
+++ app/jobs/reports/sp_cred_metrics_report_orchestrator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Reports
+  class SpCredMetricsReportOrchestrator < BaseReport
+    def perform(perform_date = Time.zone.yesterday.end_of_day, perform_receiver = :internal)
+      GoodJob::Batch.enqueue do
+        IdentityConfig.store.sp_monthly_cred_metric_report_configs.each do |report_config|
+          Reports::IrsMonthlyCredMetricsReport.perform_later(
+            perform_date,
+            perform_receiver,
+            report_config,
+          )
+        end
+      end
+    end
+  end
+end
