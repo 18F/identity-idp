fe26d2014139b2b7e64685957ead5b57f658eaa9
kalea <kalea.sebesta@gsa.gov>
2026-02-21 07:39:41 -0600

    Updates to API Transaction Count Report
    * changelog: Internal, Reporting, update querys (soucre phone risk prod, ln phone finder, instantverify, fraud score and attribute) in api_transaction_count report
    
    see https://gitlab.login.gov/lg-teams/Team-Data/reporting/-/issues/486

--- spec/lib/reporting/api_transaction_count_report_spec.rb
+++ spec/lib/reporting/api_transaction_count_report_spec.rb
@@ -21,13 +21,14 @@ RSpec.describe Reporting::ApiTransactionCountReport do
     allow(report).to receive(:socure_table).and_return([25, mock_results])
     allow(report).to receive(:socure_docv_selfie_table).and_return([26, mock_results])
     allow(report).to receive(:socure_kyc_table).and_return([30, mock_results])
-    allow(report).to receive(:fraud_score_and_attribute_table).and_return([40, mock_results])
     allow(report).to receive(:threat_metrix_idv_table).and_return([45, mock_results])
     allow(report).to receive(:threat_metrix_auth_only_table).and_return([50, mock_results])
     allow(report).to receive(:ln_emailage_table).and_return([60, mock_results])
     allow(report).to receive(:gpo_table).and_return([70, mock_results])
     allow(report).to receive(:aamva_table).and_return([80, mock_results])
     allow(report).to receive(:socure_phonerisk_table).and_return([90, mock_results])
+    allow(report).to receive(:socure_phonerisk_prod_table).and_return([10, mock_results])
+    allow(report).to receive(:ln_phonefinder_table).and_return([90, mock_results])
   end
 
   describe '#api_transaction_count' do
@@ -50,18 +51,19 @@ RSpec.describe Reporting::ApiTransactionCountReport do
           'Socure (DocV)',
           'Socure (DocV - Selfie)',
           'Socure (KYC)',
-          'Fraud Score and Attribute',
           'Threat Metrix (IDV)',
           'Threat Metrix (Auth Only)',
           'LN Emailage',
           'GPO',
           'AAMVA',
           'Socure PhoneRisk (Shadow)',
+          'Socure PhoneRisk (Prod)',
+          'LN Phone Finder',
         ],
       )
 
       expect(data_row.first).to eq("#{time_range.begin.to_date} - #{time_range.end.to_date}")
-      expect(data_row[1..]).to eq([10, 11, 15, 20, 25, 26, 30, 40, 45, 50, 60, 70, 80, 90])
+      expect(data_row[1..]).to eq([10, 11, 15, 20, 25, 26, 30, 45, 50, 60, 70, 80, 90, 10, 90])
     end
   end
 
@@ -84,18 +86,19 @@ RSpec.describe Reporting::ApiTransactionCountReport do
         'Socure (DocV)',
         'Socure (DocV - Selfie)',
         'Socure (KYC)',
-        'Fraud Score and Attribute',
         'Threat Metrix (IDV)',
         'Threat Metrix (Auth Only)',
         'LN Emailage',
         'GPO',
         'AAMVA',
         'Socure PhoneRisk (Shadow)',
+        'Socure PhoneRisk (Prod)',
+        'LN Phone Finder',
       ].join(',')
 
       expect(csv).to include(expected_headers)
       expect(csv).to include("#{time_range.begin.to_date} - #{time_range.end.to_date}")
-      expect(csv).to include('10,11,15,20,25,26,30,40,45,50,60,70,80,90')
+      expect(csv).to include('10,11,15,20,25,26,30,45,50,60,70,80,90,10,90')
     end
   end
 
