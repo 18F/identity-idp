7f04f0a8284f7a787cdf62935dd13efae6986458
ashukla <abir.shukla@gsa.gov>
2026-02-23 09:35:47 -0600

    changelog: Upcoming Features, Doc Auth, add liveness check to ddp

--- app/services/doc_auth/lexis_nexis/responses/ddp/true_id_response.rb
+++ app/services/doc_auth/lexis_nexis/responses/ddp/true_id_response.rb
@@ -46,7 +46,8 @@ module DocAuth
           def successful_result?
             return false if passport_card_detected?
 
-            doc_auth_success?
+            doc_auth_success? &&
+              (@liveness_checking_enabled ? selfie_passed? : true)
           end
 
           def doc_auth_success?
@@ -93,6 +94,21 @@ module DocAuth
             end
 
             basic_logging_info.merge(attrs)
+          end
+
+          # @return [:success, :fail, :not_processed]
+          # When selfie result is missing or not requested:
+          #   return :not_processed
+          # Otherwise:
+          #   return :success if selfie check result == 'Pass'
+          #   return :fail
+          def selfie_status
+            return :not_processed if selfie_result.nil? || !@liveness_checking_enabled
+            selfie_result == 'Pass' ? :success : :fail
+          end
+
+          def selfie_passed?
+            selfie_status == :success
           end
 
           private
@@ -213,6 +229,10 @@ module DocAuth
               vendor: 'TrueID DDP',
               billed: billed?,
             }
+          end
+
+          def selfie_result
+            portrait_match_results&.dig(:FaceMatchResult)
           end
         end
       end
