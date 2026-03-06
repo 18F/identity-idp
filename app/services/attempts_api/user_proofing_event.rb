09ff1b11e801d80775adc30946f11fa18b192a8b
AJ Farkas <aj.farkas@gsa.gov>
2026-03-06 10:16:54 -0500

    Protocols #86 test storing UserProofingEvent

---
+++ app/services/attempts_api/user_proofing_event.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module AttemptsApi
+  class UserProofingEvent
+    # def initialize(
+    #   encrypted_events: '',
+    #   profile_id,
+    #   service_providers_sent: [],
+    #   cost: '',
+    #   salt: ''
+    # )
+    # end
+  end
+end
+
+# UserProofingEvents(
+#   id: string,
+#   encrypted_events: string,
+#   profile_id: integer,
+#   service_providers_sent: jsonb,
+#   cost: string,
+#   salt: string,
+#   created_at: datetime,
+#   updated_at: datetime
+# )
