ea4bced8f1dc847a04cb3c8ac6d5553b10c8b6d1
Vraj Mohan <vraj.mohan@gsa.gov>
2026-03-09 11:21:10 -0700

    Add SMS previews (#12926)
    Fixes https://cm-jira.usa.gov/browse/LG-17185
    
    changelog: Internal, SMS, Add SMS previews
    
    Also:
    - remove preview that was not an SMS
    - remove test that was of limited utility

--- app/mailers/user_sms_text_mailer.rb
+++ app/mailers/user_sms_text_mailer.rb
@@ -1,17 +1,75 @@
 # frozen_string_literal: true
 
 class UserSmsTextMailer < ActionMailer::Base
-  def account_deleted_notice
+  include ActionView::Helpers::DateHelper
+  include Rails.application.routes.url_helpers
+
+  def mail_to
+    mail to: 'NO EMAIL'
+  end
+
+  alias_method :account_deletion_cancelled, :mail_to
+  alias_method :account_deleted_notice, :mail_to
+  alias_method :duplicate_profile_created, :mail_to
+  alias_method :duplicate_profile_sign_in_attempted, :mail_to
+  alias_method :personal_key_regeneration_notice, :mail_to
+  alias_method :personal_key_sign_in_notice, :mail_to
+
+  def account_deletion_started
+    @interval = account_reset_wait_period
     mail_to
   end
 
-  def daily_voice_limit_reached
+  def confirmation_ipp_enrollment_result
+    @proof_date = I18n.l(Time.current, format: :sms_date)
+    @contact_number = IdentityConfig.store.idv_contact_phone_number
+    @reference_string = '765261560'
     mail_to
+  end
+
+  def doc_auth_link
+    @link = url_for(idv_hybrid_mobile_entry_url) + "?document-capture-session=#{SecureRandom.uuid}"
+    @sp_or_app_name = 'Sample SAML Sinatra SP'
+    mail_to
+  end
+
+  def authentication_otp
+    @code = OtpCodeGenerator.generate_digits(TwoFactorAuthenticatable::DIRECT_OTP_LENGTH)
+    @expiration = otp_expiration
+    @domain = domain
+    mail_to
+  end
+
+  def confirmation_otp
+    @code = OtpCodeGenerator.generate_alphanumeric_digits(
+      TwoFactorAuthenticatable::DIRECT_OTP_LENGTH,
+    )
+    @expiration = otp_expiration
+    @domain = domain
+    mail_to
+  end
+
+  def url_options
+    { host: domain, protocol: 'https' }
   end
 
   private
 
-  def mail_to
-    mail to: 'NO EMAIL'
+  def account_reset_wait_period
+    current_time = Time.zone.now
+    distance_of_time_in_words(
+      current_time,
+      current_time + IdentityConfig.store.account_reset_wait_period_days.days,
+      true,
+      accumulate_on: :hours,
+    )
+  end
+
+  def domain
+    IdentityConfig.store.domain_name
+  end
+
+  def otp_expiration
+    TwoFactorAuthenticatable::DIRECT_OTP_VALID_FOR_MINUTES
   end
 end
--- spec/mailers/previews/user_sms_text_mailer_preview_spec.rb
+++ spec/mailers/previews/user_sms_text_mailer_preview_spec.rb
@@ -4,9 +4,19 @@ require_relative './user_sms_text_mailer_preview'
 RSpec.describe UserSmsTextMailerPreview do
   let(:mailer_class) { described_class.class_name.gsub(/Preview$/, '').constantize }
 
-  it 'has a preview method for each text' do
+  it 'has a mailer method for each preview' do
     mailer_methods = mailer_class.instance_methods(false)
     preview_methods = described_class.instance_methods(false)
-    expect(mailer_methods - preview_methods).to eql([])
+    expect(preview_methods - mailer_methods).to be_empty
+  end
+
+  it 'has a preview method for every SMS message' do
+    i18n_key_values = I18n::Tasks::BaseTask.new.data[:en].key_values
+    sms_messages = i18n_key_values.select do |msg|
+      /^telephony/.match(msg[0]) && # Telephony translations
+        !/^telephony.error|telephony.format|telephony.*voice/.match(msg[0]) # Not SMS message
+    end
+    preview_methods = described_class.instance_methods(false)
+    expect(sms_messages.count).to eq(preview_methods.count)
   end
 end
