07d247b290c5845c51d694e8c0f0b48441fcecd4
gerardo.cruz-ortiz <59618057+astrogeco@users.noreply.github.com>
2026-03-10 16:52:40 -0400

    Fix old parameter name

--- app/jobs/reports/sp_cred_metrics_report_orchestrator.rb
+++ app/jobs/reports/sp_cred_metrics_report_orchestrator.rb
@@ -4,7 +4,7 @@ module Reports
   class SpCredMetricsReportOrchestrator < BaseReport
     def perform(perform_date = Time.zone.yesterday.end_of_day, perform_receiver = :internal)
       GoodJob::Batch.enqueue do
-        IdentityConfig.store.sp_monthly_cred_metric_report_configs.each do |report_config|
+        IdentityConfig.store.sp_cred_metric_report_configs.each do |report_config|
           Reports::SpCredMetricsReport.perform_later(
             perform_date,
             perform_receiver,
