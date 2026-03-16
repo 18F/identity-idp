4112cfbd1200524cc3838d208b62b880c76b0ab5
Shane Chesnutt <shane.chesnutt@gsa.gov>
2026-03-16 16:42:30 -0400

    Add Socure KYC evaluator to event summarizer (#12949)
    https://gitlab.login.gov/lg-teams/joan/-/issues/17
    
    changelog: Internal, Event Summarizer, Add Socure KYC evaluator to event summarizer

---
+++ spec/lib/event_summarizer/vendor_result_evaluators/socure_kyc_spec.rb
@@ -0,0 +1,82 @@
+require 'event_summarizer/vendor_result_evaluators/socure_kyc'
+
+RSpec.describe EventSummarizer::VendorResultEvaluators::SocureKyc do
+  describe '.evaluate_result' do
+    subject { described_class }
+
+    let(:description_bullet) { "#{' ' * 17}- " }
+    let(:reason_codes) { nil }
+    let(:timeout) { false }
+    let(:result) do
+      {
+        'success' => success,
+        'timeout' => timeout,
+        'reason_codes' => reason_codes,
+      }
+    end
+
+    context 'when the result is successful' do
+      let(:success) { true }
+
+      it 'returns a success hash' do
+        expect(subject.evaluate_result(result)).to eq(
+          {
+            type: :socure_kyc_success,
+            description: 'Socure KYC call succeeded',
+          },
+        )
+      end
+    end
+
+    context 'when the result is not successful' do
+      let(:success) { false }
+
+      context 'when the result is a timeout' do
+        let(:timeout) { true }
+
+        it 'returns a timeout response' do
+          expect(subject.evaluate_result(result)).to eq(
+            {
+              type: :socure_kyc_timeout,
+              description: 'Socure KYC call timed out',
+            },
+          )
+        end
+      end
+
+      context 'when the result is not a timeout' do
+        let(:timeout) { false }
+
+        context 'when reason codes are present' do
+          let(:reason_codes) do
+            {
+              'I600' => 'Some numbers match',
+              'R800' => 'I am error',
+              'R801' => 'Identity not associated',
+            }
+          end
+
+          it 'returns a failure hash' do
+            expect(subject.evaluate_result(result)).to eq(
+              type: :socure_kyc_failures,
+              description: "Socure KYC request failed:" \
+              "\n#{description_bullet}R800: I am error" \
+              "\n#{description_bullet}R801: Identity not associated",
+            )
+          end
+        end
+      end
+
+      context 'when reason codes are not present' do
+        let(:reason_codes) { nil }
+
+        it 'returns a failure hash' do
+          expect(subject.evaluate_result(result)).to eq(
+            type: :socure_kyc_failures,
+            description: 'Socure KYC request failed: Without reason codes',
+          )
+        end
+      end
+    end
+  end
+end
