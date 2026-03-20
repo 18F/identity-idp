f7945cdfca86451347a246f0b64f1bdbe76bd3b9
Douglas Price <douglas.price@gsa.gov>
2026-03-20 17:37:03 -0400

    LG-17292: Validate the attributes received from the proofing agent.
    Uses `ActionController::Parameters#expect` to ensure only expected
    attributes are used. Then, reformats the data into the pii format needed
    for validation via the `DocPiiForm`.
    
    changelog: Upcoming Features, Proofing Agent, Validate proofing data

--- app/controllers/api/proofing_agent/proofing_agent_controller.rb
+++ app/controllers/api/proofing_agent/proofing_agent_controller.rb
@@ -16,7 +16,80 @@ module Api
       end
 
       def proof_user
+        pii_validation = Idv::DocPiiForm.new(pii: pii_from_agent).submit
+        render_bad_request and return if !pii_validation.success?
+
         render json: { request_id: SecureRandom.uuid }
+      rescue ActionController::ParameterMissing
+        render_bad_request and return
+      end
+
+      # private
+
+      def proof_params
+        return @proof_params if defined?(@proof_params)
+
+        result = {}
+
+        required_keys = %i[suspected_fraud email first_name last_name dob phone ssn id_type]
+        required_keys.each do |key|
+          result[key] = params.expect(key)
+        end
+
+        optional_keys = %i[residential_address state_id passport]
+        optional_parameters = {
+          residential_address: %i[address1 address2 city state zip_code],
+          state_id: %i[document_number jurisdiction expiration_date issue_date
+                       address1 address2 city state zip_code],
+          passport: %i[expiration_date issue_date mrz issuing_country_code],
+        }
+        optional_keys.each do |key|
+          if params[key].present?
+            result[key] =
+              params.expect(key => optional_parameters[key]).to_h.with_indifferent_access
+          end
+        end
+
+        @proof_params = result.to_h.with_indifferent_access
+      end
+
+      def pii_from_agent
+        return @pii_from_agent if defined?(@pii_from_agent)
+
+        result = proof_params.deep_dup
+        result[:document_type_received] = result.delete(:id_type)
+
+        case result[:document_type_received]
+        when *Idp::Constants::DocumentTypes::SUPPORTED_STATE_ID_TYPES
+          state_id = result.delete(:state_id)
+          raise ActionController::ParameterMissing.new(state_id: {}) if state_id.blank?
+
+          result[:address1] = state_id[:address1]
+          result[:address2] = state_id[:address2]
+          result[:city] = state_id[:city]
+          result[:state] = state_id[:state]
+          result[:zipcode] = state_id[:zip_code]
+          result[:state_id_jurisdiction] = state_id[:jurisdiction]
+          result[:state_id_number] = state_id[:document_number]
+          result[:state_id_expiration] = state_id[:expiration_date]
+
+        when *Idp::Constants::DocumentTypes::SUPPORTED_PASSPORT_TYPES
+          passport = result.delete(:passport)
+          address = result.delete(:residential_address)
+          raise ActionController::ParameterMissing.new(passport: {}) if passport.blank?
+          raise ActionController::ParameterMissing.new(residential_address: {}) if address.blank?
+
+          result[:passport_expiration] = passport[:expiration_date]
+          result[:issuing_country_code] = passport[:issuing_country_code]
+          result[:mrz] = passport[:mrz]
+          result[:address1] = address[:address1]
+          result[:address2] = address[:address2]
+          result[:city] = address[:city]
+          result[:state] = address[:state]
+          result[:zipcode] = address[:zip_code]
+        end
+
+        @pii_from_agent = result
       end
     end
   end
--- spec/controllers/api/proofing_agent/proofing_agent_controller_spec.rb
+++ spec/controllers/api/proofing_agent/proofing_agent_controller_spec.rb
@@ -5,8 +5,72 @@ require 'rails_helper'
 RSpec.describe Api::ProofingAgent::ProofingAgentController do
   let(:enabled) { false }
 
+  let(:drivers_license_type) { Idp::Constants::DocumentTypes::DRIVERS_LICENSE }
+  let(:passport_type) { Idp::Constants::DocumentTypes::PASSPORT }
+  let(:first_name) { 'FirstName' }
+  let(:last_name) { 'LastName' }
+  let(:dob) { (Time.zone.today - 14.years).strftime('%Y-%m-%d') }
+  let(:document_number) { '123' }
+  let(:jurisdiction) { 'MD' }
+  let(:address1) { '123 Main' }
+  let(:zip_code) { '12345-6789' }
+  let(:expiration_date) { (Time.zone.today + 1.day).strftime('%Y-%m-%d') }
+  let(:issuing_country_code) { 'USA' }
+  let(:mrz) { 'P<USATRAVELER<<HAPPY<<<<<<<<<<<<<<<<<<<1234567890USA8501019M2412317<<<<<<<<<<<4' }
+  let(:valid_residential_address) do
+    {
+      address1: '456 Side St',
+      address2: 'Apt 123',
+      city: 'City',
+      state: 'MD',
+      zip_code: '12354',
+    }
+  end
+  let(:valid_state_id) do
+    {
+      document_number:,
+      jurisdiction:,
+      expiration_date:,
+      issue_date: '2025-01-01',
+      address1:,
+      address2: nil,
+      city: 'City',
+      state: jurisdiction,
+      zip_code:,
+    }
+  end
+  let(:valid_passport) do
+    {
+      expiration_date:,
+      issue_date: '2025-01-01',
+      issuing_country_code:,
+      mrz:,
+    }
+  end
+
+  let(:id_type) { 'library_card' }
+  let(:residential_address) { nil }
+  let(:state_id) { nil }
+  let(:passport) { nil }
+  let(:agent_params) do
+    ActionController::Parameters.new(
+      suspected_fraud: false,
+      email: 'foo@bar.com',
+      first_name:,
+      last_name:,
+      dob:,
+      phone: '555-555-5555',
+      ssn: '111223333',
+      id_type:,
+      residential_address:,
+      state_id:,
+      passport:,
+    )
+  end
+
   before do
     allow(FeatureManagement).to receive(:idv_proofing_agent_enabled?).and_return(enabled)
+    allow(controller).to receive(:params).and_return(agent_params)
   end
 
   describe '#search_user' do
@@ -45,14 +109,167 @@ RSpec.describe Api::ProofingAgent::ProofingAgentController do
     context 'when proofing agent is enabled' do
       let(:enabled) { true }
 
-      it 'returns 200' do
-        expect(action.status).to eq(200)
+      context 'when the id_type is drivers_licence' do
+        let(:id_type) { drivers_license_type }
+        let(:state_id) { valid_state_id }
+
+        context 'when valid state id data is received' do
+          it 'returns 200' do
+            expect(action.status).to eq(200)
+          end
+
+          it 'includes request_id in the response' do
+            action
+            body = JSON.parse(response.body)
+            expect(body['request_id']).to be_present
+          end
+        end
+
+        context 'when the first_name is missing' do
+          let(:first_name) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the last_name is missing' do
+          let(:last_name) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the dob is missing' do
+          let(:dob) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the dob does not meet our minimum age requirements' do
+          let(:dob) { (Time.zone.today - 10.years).strftime('%Y-%m-%d') }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the address1 is missing' do
+          let(:address1) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the zip_code is invalid' do
+          let(:zip_code) { '123456' }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the jurisdiction is missing' do
+          let(:jurisdiction) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the document_number is missing' do
+          let(:document_number) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the state_id is expired' do
+          let(:expiration_date) { '2026-01-01' }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
       end
 
-      it 'includes request_id in the response' do
-        action
-        body = JSON.parse(response.body)
-        expect(body['request_id']).to be_present
+      context 'when the id_type is passport' do
+        let(:id_type) { passport_type }
+        let(:passport) { valid_passport }
+        let(:residential_address) { valid_residential_address }
+
+        context 'when valid passport data is received' do
+          it 'returns 200' do
+            expect(action.status).to eq(200)
+          end
+
+          it 'includes request_id in the response' do
+            action
+            body = JSON.parse(response.body)
+            expect(body['request_id']).to be_present
+          end
+        end
+
+        context 'when the mrz is missing' do
+          let(:mrz) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the passport is expired' do
+          let(:expiration_date) { '2026-01-01' }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the first_name is missing' do
+          let(:first_name) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the last_name is missing' do
+          let(:last_name) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the dob is missing' do
+          let(:dob) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the dob does not meet our minimum age requirements' do
+          let(:dob) { (Time.zone.today - 10.years).strftime('%Y-%m-%d') }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
+
+        context 'when the residential address is missing' do
+          let(:residential_address) { nil }
+
+          it 'returns 400' do
+            expect(action.status).to eq(400)
+          end
+        end
       end
     end
   end
