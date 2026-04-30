dbd7d23f856527d72f65e0f265747aa922e57699
Davida Marion <davida.marion@gsa.gov>
2026-04-30 17:09:36 -0400

    changelog: Upcoming Features, Historical Attempts Data, Update tables for updated data storage design

--- spec/controllers/concerns/idv/historical_attempts_concern_spec.rb
+++ spec/controllers/concerns/idv/historical_attempts_concern_spec.rb
@@ -83,26 +83,7 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       end
     end
 
-    context 'service_provider is not an allowed_attempts_providers' do
-      # TODO: This spec is not correct.
-      # We are recording on all IAL2 requests.
-      # We are only sending the data to SPs that are allowed_attempts_providers and have not already
-      # received the data.
-      before do
-        allow(IdentityConfig.store).to receive(
-          :allowed_attempts_providers,
-        ).and_return([])
-      end
-
-      it 'does not modify or create a UserProofingEvent' do
-        record_user_proofing_events
-
-        expect(UserProofingEvent).to_not have_received(:new)
-        expect(UserProofingEvent).to_not have_received(:save)
-      end
-    end
-
-    context 'when historical events need to be sent' do
+    context 'historical_attempts_api_enabled feature flag is true' do
       before do
         allow(Encryption::Encryptors::PiiEncryptor).to receive(:new).and_return(encryptor_mock)
         allow(encryptor_mock).to receive(:encrypt).and_return(encrypted_events.to_json)
@@ -119,6 +100,25 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
         expect(user_proofing_event.salt).to eq(encrypted_events['salt'])
         expect(user_proofing_event.profile).to eq(profile)
       end
+
+      context 'service_provider is not an allowed_attempts_providers' do
+        # TODO: This spec is not correct.
+        # We are recording on all IAL2 requests.
+        # We are only sending the data to SPs that are allowed_attempts_providers and have not already
+        # received the data.
+        before do
+          allow(IdentityConfig.store).to receive(
+            :allowed_attempts_providers,
+          ).and_return([])
+        end
+
+        it 'does not modify or create a UserProofingEvent' do
+          record_user_proofing_events
+
+          expect(UserProofingEvent).to_not have_received(:new)
+          expect(UserProofingEvent).to_not have_received(:save)
+        end
+      end
     end
   end
 
@@ -134,18 +134,41 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       )
     end
 
-    context 'historical events are disallowed' do
-      context 'historical_attempts_api_enabled feature flag is false' do
-        before do
-          allow(IdentityConfig.store).to receive(:historical_attempts_api_enabled).and_return(false)
-        end
+    context 'historical_attempts_api_enabled feature flag is false' do
+      before do
+        allow(IdentityConfig.store).to receive(:historical_attempts_api_enabled).and_return(false)
+      end
 
-        it 'does not decrypt or encrypt events' do
-          expect(pii_encryptor).to_not receive(:decrypt)
-          expect(pii_encryptor).to_not receive(:encrypt)
+      it 'does not decrypt or encrypt events' do
+        expect(pii_encryptor).to_not receive(:decrypt)
+        expect(pii_encryptor).to_not receive(:encrypt)
 
-          cache_user_proofing_events
-        end
+        cache_user_proofing_events
+      end
+    end
+
+    context 'historical_attempts_api_enabled feature flag is true' do
+      let(:mock_session_encryptor) { double }
+      let(:kms_encrypted_events) { 'kms_encrypted_events' }
+
+      before do
+        allow(SessionEncryptor).to receive(:new).and_return(mock_session_encryptor)
+        allow(mock_session_encryptor).to receive(:kms_encrypt).and_return(kms_encrypted_events)
+        cache_user_proofing_events
+      end
+
+      it 'decrypts the appropriate UserProofingEvent' do
+        expect(pii_encryptor).to have_received(:decrypt).once
+      end
+
+      it 'encrypts with the kms session key' do
+        expect(mock_session_encryptor).to have_received(:kms_encrypt).once.with(
+          idv_attempts.to_json,
+        )
+      end
+
+      it 'updates the session with the UserProofingEvent' do
+        expect(controller.user_session[:encrypted_proofing_events]).to eq(kms_encrypted_events)
       end
 
       context 'service_provider is not an allowed_attempts_providers' do
@@ -176,31 +199,6 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
 
           cache_user_proofing_events
         end
-      end
-    end
-
-    context 'historical events are permitted' do
-      let(:mock_session_encryptor) { double }
-      let(:kms_encrypted_events) { 'kms_encrypted_events' }
-
-      before do
-        allow(SessionEncryptor).to receive(:new).and_return(mock_session_encryptor)
-        allow(mock_session_encryptor).to receive(:kms_encrypt).and_return(kms_encrypted_events)
-        cache_user_proofing_events
-      end
-
-      it 'decrypts the appropriate UserProofingEvent' do
-        expect(pii_encryptor).to have_received(:decrypt).once
-      end
-
-      it 'encrypts with the kms session key' do
-        expect(mock_session_encryptor).to have_received(:kms_encrypt).once.with(
-          idv_attempts.to_json,
-        )
-      end
-
-      it 'updates the session with the UserProofingEvent' do
-        expect(controller.user_session[:encrypted_proofing_events]).to eq(kms_encrypted_events)
       end
     end
   end
