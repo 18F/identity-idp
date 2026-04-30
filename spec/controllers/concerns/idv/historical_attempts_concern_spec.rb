ebf1ac874266a55442517353edcef4b2b77561a9
Davida Marion <davida.marion@gsa.gov>
2026-04-30 08:07:07 -0400

    Update UserProofingEvent

--- spec/controllers/concerns/idv/historical_attempts_concern_spec.rb
+++ spec/controllers/concerns/idv/historical_attempts_concern_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
   let(:sp) { create(:service_provider, ial: 2, issuer: issuer) }
   let(:profile) { create(:profile, :active, :verified) }
   let(:applicant) { Idp::Constants::MOCK_IDV_APPLICANT_WITH_PHONE }
-  let(:user_proofing_event_new) { build(:user_proofing_event) }
   let(:encryptor_mock) { double }
   let(:encrypted_events) do
     {
@@ -23,18 +22,14 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       'cost' => '000$0$0$',
     }
   end
-  let(:test_data) { { test_data: 'data' } }
-  let(:old_data) { { old_data: 'data' } }
   let(:idv_attempts) do
     [
-      { 'idv-ssn-submitted' => test_data },
-      { 'idv-enrollment-complete' => test_data },
+      { 'idv-ssn-submitted' => { 'user_uuid' => registered_user.uuid } },
     ]
   end
   let(:pii_encryptor) { Encryption::Encryptors::PiiEncryptor.new(registered_user.password) }
-  let(:existing_events) { [{ 'old_attempt' => old_data }] }
   let(:encrypted_existing_events) do
-    pii_encryptor.encrypt(existing_events.to_json, user_uuid: registered_user.uuid)
+    pii_encryptor.encrypt(idv_attempts.to_json, user_uuid: registered_user.uuid)
   end
 
   controller ApplicationController do
@@ -61,9 +56,8 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       historical_attempts_api_enabled: true,
     )
 
-    allow(UserProofingEvent).to receive(:new).and_return(user_proofing_event_new)
-    allow(user_proofing_event_new).to receive(:save).and_return(true)
-    allow(UserProofingEvent).to receive(:save).and_return(true)
+    allow(UserProofingEvent).to receive(:new).and_call_original
+    allow(UserProofingEvent).to receive(:save).and_call_original
     allow(Encryption::Encryptors::PiiEncryptor).to receive(:new).and_return(pii_encryptor)
     allow(pii_encryptor).to receive(:decrypt).and_call_original
     allow(pii_encryptor).to receive(:encrypt).and_call_original
@@ -74,7 +68,7 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       controller.record_user_proofing_events(password)
     end
 
-    context 'historical_attempts_api_enabled is false at the secrets level' do
+    context 'historical_attempts_api_enabled feature flag is false' do
       before do
         allow(IdentityConfig.store).to receive(
           :historical_attempts_api_enabled,
@@ -90,6 +84,10 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
     end
 
     context 'service_provider is not an allowed_attempts_providers' do
+      # TODO: This spec is not correct.
+      # We are recording on all IAL2 requests.
+      # We are only sending the data to SPs that are allowed_attempts_providers and have not already
+      # received the data.
       before do
         allow(IdentityConfig.store).to receive(
           :allowed_attempts_providers,
@@ -113,17 +111,13 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
 
       it 'creates and saves a UserProofingEvent' do
         expect(UserProofingEvent).to have_received(:new)
-        expect(user_proofing_event_new).to have_received(:save)
       end
 
-      it 'includes the encrypted_events' do
-        expect(UserProofingEvent).to have_received(:new).with(
-          encrypted_events: encrypted_events.to_json,
-          profile_id: registered_user.active_profile.id,
-          service_providers_sent: [],
-          cost: encrypted_events['cost'],
-          salt: encrypted_events['salt'],
-        )
+      it 'includes the encrypted event metadata' do
+        user_proofing_event = UserProofingEvent.last
+        expect(user_proofing_event.cost).to eq(encrypted_events['cost'])
+        expect(user_proofing_event.salt).to eq(encrypted_events['salt'])
+        expect(user_proofing_event.profile).to eq(profile)
       end
     end
   end
@@ -133,28 +127,17 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       controller.cache_user_proofing_events(password)
     end
 
-    let(:user_proofing_event) do
-      create(
-        :user_proofing_event,
-        encrypted_events: encrypted_existing_events,
-        service_providers_sent: [],
+    let!(:user_proofing_event) do
+      registered_user.active_profile.build_user_proofing_event(
         cost: JSON.parse(encrypted_existing_events)['cost'],
         salt: JSON.parse(encrypted_existing_events)['salt'],
-        profile_id: registered_user.active_profile.id,
       )
     end
 
-    before do
-      allow(UserProofingEvent).to receive(:find_by).and_return(user_proofing_event)
-      allow(user_proofing_event).to receive(:update_encrypted_events).and_return(true)
-    end
-
     context 'historical events are disallowed' do
-      context 'historical_attempts_api_enabled is false at the secrets level' do
+      context 'historical_attempts_api_enabled feature flag is false' do
         before do
-          allow(IdentityConfig.store).to receive(
-            :historical_attempts_api_enabled,
-          ).and_return(false)
+          allow(IdentityConfig.store).to receive(:historical_attempts_api_enabled).and_return(false)
         end
 
         it 'does not decrypt or encrypt events' do
@@ -167,9 +150,7 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
 
       context 'service_provider is not an allowed_attempts_providers' do
         before do
-          allow(IdentityConfig.store).to receive(
-            :allowed_attempts_providers,
-          ).and_return([])
+          allow(IdentityConfig.store).to receive(:allowed_attempts_providers).and_return([])
         end
 
         it 'does not decrypt or encrypt events' do
@@ -182,18 +163,11 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
 
       context 'events already sent to SP' do
         let(:user_proofing_event) do
-          create(
-            :user_proofing_event,
-            encrypted_events: encrypted_existing_events,
-            service_providers_sent: [sp.issuer],
+          registered_user.active_profile.build_user_proofing_event(
             cost: JSON.parse(encrypted_existing_events)['cost'],
             salt: JSON.parse(encrypted_existing_events)['salt'],
-            profile_id: registered_user.active_profile.id,
+            service_provider_ids_sent: [sp.id],
           )
-        end
-
-        before do
-          allow(UserProofingEvent).to receive(:find_by).and_return(user_proofing_event)
         end
 
         it 'does not decrypt or encrypt events' do
@@ -216,15 +190,12 @@ RSpec.describe Idv::HistoricalAttemptsConcern, type: :controller do
       end
 
       it 'decrypts the appropriate UserProofingEvent' do
-        expect(pii_encryptor).to have_received(:decrypt).once.with(
-          user_proofing_event.encrypted_events,
-          user_uuid: registered_user.uuid,
-        )
+        expect(pii_encryptor).to have_received(:decrypt).once
       end
 
       it 'encrypts with the kms session key' do
         expect(mock_session_encryptor).to have_received(:kms_encrypt).once.with(
-          existing_events.to_json,
+          idv_attempts.to_json,
         )
       end
 
--- spec/models/user_proofing_event_spec.rb
+++ spec/models/user_proofing_event_spec.rb
@@ -3,31 +3,30 @@ require 'rails_helper'
 RSpec.describe UserProofingEvent, type: :model do
   let(:user_proofing_event) do
     UserProofingEvent.new(
-      encrypted_events: 'these_events_are_encrypted',
       profile_id: 1,
-      service_providers_sent: [],
+      service_provider_ids_sent: [],
       cost: '0$cost$',
       salt: 'salt0',
     )
   end
 
   describe '#add_sp_sent' do
-    let(:issuer) { 'a:test:issuer' }
+    let(:sp) { create(:service_provider) }
 
     before do
-      user_proofing_event.add_sp_sent(issuer)
+      user_proofing_event.add_sp_sent(sp.id)
     end
 
-    it 'should add a given issuer to the list of service_providers_sent' do
-      expect(user_proofing_event.service_providers_sent).to eq([issuer])
+    it 'should add a given id to the list of service_provider_ids_sent' do
+      expect(user_proofing_event.service_provider_ids_sent).to eq([sp.id])
     end
 
     it 'should be idempotent' do
-      expect(user_proofing_event.service_providers_sent.length).to eq(1)
+      expect(user_proofing_event.service_provider_ids_sent.length).to eq(1)
 
-      user_proofing_event.add_sp_sent(issuer)
+      user_proofing_event.add_sp_sent(sp.id)
 
-      expect(user_proofing_event.service_providers_sent).to eq([issuer])
+      expect(user_proofing_event.service_provider_ids_sent).to eq([sp.id])
     end
   end
 end
