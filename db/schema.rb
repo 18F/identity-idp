fee1fa8bcc8700e80bb41adb992328a23c4d8330
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-05-05 16:56:14 -0400

    rename migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_30_204118) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_05_164618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -217,6 +217,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_30_204118) do
     t.string "passport_status", comment: "sensitive=false"
     t.string "hybrid_mobile_threatmetrix_session_id", comment: "sensitive=false"
     t.string "hybrid_mobile_request_ip", comment: "sensitive=false"
+    t.integer "document_type", comment: "sensitive=false"
     t.index ["result_id"], name: "index_document_capture_sessions_on_result_id"
     t.index ["socure_docv_transaction_token"], name: "index_socure_docv_transaction_token", unique: true
     t.index ["user_id"], name: "index_document_capture_sessions_on_user_id"
