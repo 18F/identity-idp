96e767fc896a10c590864da8772069167ed8ee99
Malick DiarrA <malick.diarra@gsa.gov>
2026-05-06 11:34:49 -0400

    fix redirect for after autopasskey prompt

--- app/controllers/users/webauthn_setup_controller.rb
+++ app/controllers/users/webauthn_setup_controller.rb
@@ -227,6 +227,10 @@ module Users
       (Time.zone.now.to_f - started_at.to_f)
     end
 
+    def next_setup_path
+      super || (authentication_methods_setup_path if @platform_authenticator && user_session[:auto_passkey_prompted])
+    end
+
     def need_to_set_up_additional_mfa?
       return false unless @platform_authenticator
       in_multi_mfa_selection_flow? && mfa_selection_count < 2
