03a9ad39e8700b5b72f294c099da932d86818032
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-05-06 18:01:04 -0400

    update specs to ensure no other attrs are updated

--- spec/lib/tasks/document_capture_sessions_rake_spec.rb
+++ spec/lib/tasks/document_capture_sessions_rake_spec.rb
@@ -44,5 +44,59 @@ RSpec.describe 'document_capture_sessions rake tasks', type: :task do
         Idp::Constants::DocumentTypes::PASSPORT,
       )
     end
+
+    context 'ensure only document_type_requested is updated' do
+      context 'state_id_card_session' do
+        before do
+          @state_id_attrs = state_id_card_session.attributes.except(
+            'id', 'document_type_requested', 'created_at', 'updated_at'
+          )
+        end
+
+        it 'does not change other attributes' do
+          task.execute
+          expect(state_id_card_session.reload.attributes.except(
+            'id', 'document_type_requested', 'created_at', 'updated_at'
+          )).to eq(@state_id_attrs)
+        end
+      end
+
+      context 'passport_session' do
+        before do
+          @passport_attrs = passport_session.attributes.except(
+            'document_type_requested', 'updated_at'
+          )
+        end
+
+        it 'does not change other attributes' do
+          task.execute
+          expect(passport_session.reload.attributes.except(
+            'document_type_requested', 'updated_at'
+          )).to eq(@passport_attrs)
+        end
+      end
+
+      context 'no_passport_status_session' do
+        before do
+          @no_passport_attrs = no_passport_status_session.attributes
+        end
+
+        it 'does not change any attributes' do
+          task.execute
+          expect(no_passport_status_session.reload.attributes).to eq(@no_passport_attrs)
+        end
+      end
+
+      context 'already_backfilled_session' do
+        before do
+          @already_backfilled_attrs = already_backfilled_session.attributes
+        end
+
+        it 'does not change any attributes' do
+          task.execute
+          expect(already_backfilled_session.reload.attributes).to eq(@already_backfilled_attrs)
+        end
+      end
+    end
   end
 end
