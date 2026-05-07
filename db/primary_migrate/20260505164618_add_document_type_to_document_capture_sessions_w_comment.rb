8661803d9b8fc03fd11704f064a70e5055b18be1
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-05-07 13:22:35 -0400

    unintended in rebase

--- db/primary_migrate/20260505164618_add_document_type_to_document_capture_sessions_w_comment.rb
+++
@@ -1,9 +0,0 @@
-class AddDocumentTypeToDocumentCaptureSessionsWComment < ActiveRecord::Migration[8.0]
-  def up
-    add_column :document_capture_sessions, :document_type_requested, :integer, comment: 'sensitive=false'
-  end
-
-  def down
-    remove_column :document_capture_sessions, :document_type_requested
-  end
-end
