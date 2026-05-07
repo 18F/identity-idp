b4465b72cce5ae6ddb162eff9066bee9510366b6
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-05-07 15:49:08 -0400

    rename migration file name and index name too long

--- db/primary_migrate/20260507152408_add_index_passport_status_to_document_capture_sessions.rb
+++ db/primary_migrate/20260507152408_add_index_passport_status_to_document_capture_sessions.rb
@@ -1,7 +1,7 @@
 class AddIndexPassportStatusToDocumentCaptureSessions < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
-  INDEX_NAME = "index_document_capture_sessions_on_passport_status_null_document_type_requested"
+  INDEX_NAME = "idx_document_capture_sessions_on_passport_status_null_doc_type"
 
   def up
     add_index(
