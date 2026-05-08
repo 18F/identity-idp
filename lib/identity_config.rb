3f14d0a2a230785184eecd8fbcc96ed0cc41fdac
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-05-08 17:38:51 -0400

    show mdl on choose id type

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -239,6 +239,7 @@ module IdentityConfig
     config.add(:idv_attempt_window_in_hours, type: :integer)
     config.add(:idv_available, type: :boolean)
     config.add(:idv_contact_phone_number, type: :string)
+    config.add(:idv_doc_auth_mdl_enabled_percent, type: :integer)
     config.add(:idv_gpo_verification_enabled, type: :boolean)
     config.add(:idv_max_attempts, type: :integer)
     config.add(:idv_min_age_years, type: :integer)
