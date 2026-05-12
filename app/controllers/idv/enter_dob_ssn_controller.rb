1e62dccd357334d4a7c89ec0c2e388965e473c78
Alex Bradley <alexander.bradley@gsa.gov>
2026-05-12 15:10:56 -0400

    initial creation of dob ssn form

---
+++ app/controllers/idv/enter_dob_ssn_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Idv
+  class EnterDobSsnController < ApplicationController
+    include Idv::AvailabilityConcern
+    include IdvStepConcern
+    include Idv::StepIndicatorConcern
+
+    def new
+      @ssn_presenter = Idv::SsnPresenter.new(
+        sp_name: decorated_sp_session.sp_name,
+        ssn_form: Idv::SsnFormatForm.new(idv_session.ssn),
+        step_indicator_steps: step_indicator_steps,
+      )
+    end
+
+    def update
+    end
+  end
+end
