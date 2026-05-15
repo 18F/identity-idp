bceb4ab03cfbfbc5066eed2711ccd86496534623
ColtonGLapp <colton.lapp@gsa.gov>
2026-05-15 12:50:12 -0500

    refactor job emailing to mirror reporting-rails and be simpler date logic

--- app/jobs/job_helpers/delayed_report_configuration_helper.rb
+++ app/jobs/job_helpers/delayed_report_configuration_helper.rb
@@ -2,62 +2,32 @@
 
 module JobHelpers
   module DelayedReportConfigurationHelper
+    DATA_LAG_DAYS = 2 # 2 day lag to account for data sync delay into DW
+
     module_function
 
-    # This helper is meant to return the end date for a recent quarter, month, week or day
-    # It also has logic to determine if a report should be internal or external given that
-    # ending date. It was built to enable sending monthly reports externally only
-    # if it is the end of a quarter
-    def determine_end_date_for_time_period_and_receiver(
-      starting_date: Time.zone.today,
-      go_backwards_to_most_recent: 'month',
+    def determine_job_args_for_demographics(
+      run_date: Time.zone.now,
+      lookback_days: 5,
       external_rule: 'external_if_quarter_end'
     )
-      raise ArgumentError, 'starting_date cannot be nil' unless starting_date
+      # Figure out what reporting period we're covering
+      report_period_date = run_date - lookback_days.days
+      quarter_end = report_period_date.all_quarter.end
 
-      end_date = calculate_end_date_for_previous_period(
-        starting_date,
-        go_backwards_to_most_recent,
-      )
+      # Determine receiver based on rule
+      receiver = determine_receiver_for_period(quarter_end, external_rule)
 
-      receiver = determine_receiver(end_date, external_rule)
-
-      [end_date, receiver]
+      [run_date, lookback_days, receiver]
     end
 
     private
 
-    def calculate_end_date_for_previous_period(starting_date, period_type)
-      case period_type.to_s.downcase
-      when 'month'
-        # Go back to end of most recent complete month
-        starting_date.beginning_of_month.prev_day.end_of_day
-      when 'quarter'
-        # Go back to end of most recent complete quarter
-        starting_date.beginning_of_quarter.prev_day.end_of_day
-      when 'week'
-        # Go back to end of most recent complete week (Sunday)
-        starting_date.beginning_of_week.prev_day.end_of_day
-      when 'day'
-        # Go back to end of previous day
-        starting_date.prev_day.end_of_day
-      else
-        raise ArgumentError, "Unsupported period type: #{period_type}"
-      end
-    end
-
-    def determine_receiver(end_date, external_rule)
+    def determine_receiver_for_period(quarter_end_date, external_rule)
       case external_rule.to_s.downcase
       when 'external_if_quarter_end'
-        # Send external if this date is also the end of a quarter
-        if end_date.end_of_quarter.to_date == end_date.to_date
-          :both
-        else
-          :internal
-        end
-      when 'external_if_year_end'
-        # Send external if this date is also the end of a year
-        if end_date.end_of_year.to_date == end_date.to_date
+        # Send external if we're past the quarter end + reasonable lag
+        if quarter_end_date.to_date <= Date.current - DATA_LAG_DAYS.days
           :both
         else
           :internal
