82b6ecc221f9472e95f047fd391ad619484bf45e
Davida Marion <davida.marion@gsa.gov>
2026-05-19 14:50:02 -0400

    changelog: Upcoming Feature, Historical Attempts Data, Transform data so as to not leak provider data across SPs

---
+++ spec/services/attempts_api/historical_attempt_event_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe AttemptsApi::HistoricalAttemptEvent do
+  let(:attempts_api_private_key) { OpenSSL::PKey::RSA.new(2048) }
+  let(:attempts_api_public_key) { attempts_api_private_key.public_key }
+
+  let(:signing_key) { OpenSSL::PKey::EC.generate('prime256v1') }
+  let(:singing_private_key) { signing_key.private_to_pem }
+  let(:signing_public_key) { OpenSSL::PKey::EC.new(signing_key.public_to_pem) }
+
+  let(:jti) { 'test-unique-id' }
+  let(:iat) { Time.zone.now.to_i }
+  let(:event_type) { 'idv-test-event' }
+  let(:session_id) { 'test-session-id' }
+  let(:occurred_at) { Time.zone.now.round }
+  let(:service_provider) { create(:service_provider, :idv) }
+  let(:user) { create(:user) }
+  let(:proofing_identity) { AgencyIdentityLinker.for(user:, service_provider:, skip_create: false) }
+  let(:aaca_sp) { create(:service_provider, :idv) }
+  let!(:aaca_identity) do
+    AgencyIdentityLinker.for(user:, service_provider: aaca_sp, skip_create: false)
+  end
+  let(:event_data) do
+    {
+      'jti' => jti,
+      'iat' => iat,
+      'occurred_at' => occurred_at.to_f,
+      'event_type' => event_type,
+      'session_id' => session_id,
+      'event_metadata' => {
+        'user_uuid' => proofing_identity.uuid,
+        'application_url' => 'https://example.com/app',
+        'client_port' => '8080',
+      },
+    }
+  end
+
+  subject do
+    described_class.new(event_data:, sp: aaca_sp)
+  end
+
+  describe '#initialize' do
+    it 'initializes the event with the correct event data' do
+      expect(subject.jti).to eq(jti)
+      expect(subject.iat).to eq(iat)
+      expect(subject.event_type).to eq(event_type)
+      expect(subject.session_id).to eq(session_id)
+      expect(subject.occurred_at).to eq(occurred_at)
+      expect(subject.event_metadata).to eq(
+        {
+          'user_uuid' => aaca_identity.uuid,
+          'application_url' => nil,
+          'client_port' => nil,
+        },
+      )
+    end
+  end
+end
