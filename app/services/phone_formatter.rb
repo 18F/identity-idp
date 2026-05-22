912bc27a5cfe62bfc7c82ae680856b540b1890b2
Malick Diarra <malick.diarra@gsa.gov>
2026-05-22 15:28:52 -0400

    Update phonelib (#13119)
    * changelog: Internal, Phonelib, bump phone lib
    
    * update phone formatter
    
    * take into account for asterisk as well
    
    * Refactor formatter

--- app/services/phone_formatter.rb
+++ app/services/phone_formatter.rb
@@ -11,7 +11,24 @@ module PhoneFormatter
   def self.mask(phone)
     return '' if phone.blank?
 
-    formatted = Phonelib.parse(phone).national
-    formatted[0..-5].gsub(/\d/, '*') + formatted[-4..-1]
+    parsed_phone = Phonelib.parse(phone)
+    formatted = parsed_phone.national.to_s
+    return '' if formatted.blank?
+
+    digits = parsed_phone.raw_national.to_s.presence || formatted.gsub(/\D/, '')
+    if formatted.count('0-9') > digits.length
+      formatted = parsed_phone.international.to_s.delete_prefix(
+        "+#{parsed_phone.country_code}",
+      ).strip
+    end
+
+    digits_to_mask = [digits.length - 4, 0].max
+
+    formatted.gsub(/\d/) do |digit|
+      next digit if digits_to_mask.zero?
+
+      digits_to_mask -= 1
+      '*'
+    end
   end
 end
