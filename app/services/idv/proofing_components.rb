2f0e9034a47f7ac134e29903767ec7f5df91d9e8
Anthony <acampbell@luckyrabbit.tech>
2026-05-26 15:28:35 -0400

    LG-16932: Remove id_doc_type reference in app
    changelog: Internal, Identity Verification, Remove id_doc_type backwards-compatibility code

--- app/services/idv/proofing_components.rb
+++ app/services/idv/proofing_components.rb
@@ -11,7 +11,7 @@ module Idv
     end
 
     def document_type_received
-      idv_session.pii_from_doc&.document_type_received || idv_session.pii_from_doc&.id_doc_type
+      idv_session.pii_from_doc&.document_type_received
     end
 
     def source_check
