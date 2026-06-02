b1c787b351b7b2770c09cc0ab6a2ca7daa57ec46
Shane Chesnutt <shane.chesnutt@gsa.gov>
2026-06-02 12:38:38 -0400

    Fix irreversible migration error (#13170)
    The previous version of the
    20260529170050_update_email_addresses_encrypted_email_comment migration
    was throwing an ActiveRecord::IrreversibleMigration error when rolling
    back.
    
    changelog: Bug Fixes, Database Migration, Fix IrreversibleMigration issue with 20260529170050_update_email_addresses_encrypted_email_comment migration.

--- db/primary_migrate/20260529170050_update_email_addresses_encrypted_email_comment.rb
+++ db/primary_migrate/20260529170050_update_email_addresses_encrypted_email_comment.rb
@@ -1,5 +1,6 @@
 class UpdateEmailAddressesEncryptedEmailComment < ActiveRecord::Migration[8.0]
   def change
-    change_column_comment :email_addresses, :encrypted_email, "sensitive=false"
+    change_column_comment :email_addresses, :encrypted_email, to: 'sensitive=false',
+                                                              from: 'sensitive=true'
   end
 end
