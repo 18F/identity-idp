37105401b9833be70985a3ef7e50297aa72195ee
Davida Marion <davida.marion@gsa.gov>
2026-06-03 09:43:10 -0400

    Update specs to include last_X_authenticated_at

--- spec/services/identity_linker_spec.rb
+++ spec/services/identity_linker_spec.rb
@@ -156,61 +156,84 @@ RSpec.describe IdentityLinker do
       IdentityLinker.new(user, service_provider2).link_identity(rails_session_id: rails_session_id)
     end
 
-    context 'identity.last_ial2_authenticated_at' do
-      context 'the request includes identity proofing' do
-        it 'sets the timestamp' do
-          IdentityLinker.new(user, service_provider).link_identity(ial: 2)
+    context '#identity.last_ial2_authenticated_at' do
+      let(:ial) { 2 }
+      before do
+        IdentityLinker.new(user, service_provider).link_identity(ial:)
+      end
 
+      context 'when the request includes identity proofing' do
+        it 'sets the timestamp' do
           expect(
             user.last_identity.last_ial2_authenticated_at,
           ).to be_within(1.second).of(Time.zone.now)
         end
       end
 
-      context 'the request does not include identity proofing' do
+      context 'when the request does not include identity proofing' do
+        let(:ial) { 1 }
         it 'does not set the timestamp' do
-          IdentityLinker.new(user, service_provider).link_identity(ial: 1)
-
           expect(user.last_identity.last_ial2_authenticated_at).to be_nil
         end
       end
 
-      context 'the request is IALMax and verified_at is null' do
-        it 'does not set the timestamp' do
-          IdentityLinker.new(user, service_provider).link_identity(ial: 0)
-
-          expect(user.last_identity.last_ial2_authenticated_at).to be_nil
+      context 'when the request is IALMax' do
+        let(:ial) { 0 }
+        context 'and the user is not verified' do
+          it 'does not set the timestamp' do
+            expect(user.last_identity.last_ial2_authenticated_at).to be_nil
+          end
         end
-      end
 
-      context 'the request is IALMax and verified_at is not null' do
-        it 'sets the timestamp' do
-          IdentityLinker.new(user, service_provider).link_identity(ial: 2)
-          IdentityLinker.new(user, service_provider).link_identity(ial: 0)
-
-          expect(
-            user.last_identity.last_ial2_authenticated_at,
-          ).to be_within(1.second).of(Time.zone.now)
+        context 'the user is verified' do
+          let(:user) { create(:user, :proofed) }
+          it 'sets the timestamp' do
+            expect(user.last_identity.last_ial2_authenticated_at).to be_within(
+              1.second,
+            ).of(Time.zone.now)
+          end
         end
       end
     end
 
     context 'identity.last_ial1_authenticated_at' do
+      let(:ial) { 2 }
+      before do
+        IdentityLinker.new(user, service_provider).link_identity(ial:)
+      end
+
       context 'the request includes identity proofing' do
         it 'does not set the timestamp' do
-          IdentityLinker.new(user, service_provider).link_identity(ial: 2)
-
           expect(user.last_identity.last_ial1_authenticated_at).to be_nil
         end
       end
 
       context 'the request does not include identity proofing' do
+        let(:ial) { 1 }
         it 'sets the timestamp' do
           IdentityLinker.new(user, service_provider).link_identity(ial: 1)
 
           expect(
             user.last_identity.last_ial1_authenticated_at,
           ).to be_within(1.second).of(Time.zone.now)
+        end
+      end
+
+      context 'when the request is IALMax' do
+        let(:ial) { 0 }
+        context 'and the user is not verified' do
+          it 'sets the timestamp' do
+            expect(user.last_identity.last_ial1_authenticated_at).to be_within(
+              1.second,
+            ).of(Time.zone.now)
+          end
+        end
+
+        context 'the user is verified' do
+          let(:user) { create(:user, :proofed) }
+          it 'does not set the timestamp' do
+            expect(user.last_identity.last_ial1_authenticated_at).to be_nil
+          end
         end
       end
     end
