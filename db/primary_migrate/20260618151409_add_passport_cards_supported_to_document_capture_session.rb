904ef8a5f9154253f26401782fc304685ada6c80
ashukla <abir.shukla@gsa.gov>
2026-06-18 12:33:37 -0500

    Fixing specs... hopefully

--- db/primary_migrate/20260618151409_add_passport_cards_supported_to_document_capture_session.rb
+++ db/primary_migrate/20260618151409_add_passport_cards_supported_to_document_capture_session.rb
@@ -1,6 +1,7 @@
 class AddPassportCardsSupportedToDocumentCaptureSession < ActiveRecord::Migration[8.0]
   def change
     add_column :document_capture_sessions, :passport_cards_supported, :boolean, default: false,
-                                                                                null: false
+                                                                                null: false,
+                                                                                comment: 'sensitive=false'
   end
 end
