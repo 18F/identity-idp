06533f74b2f26c6b34bcb4f46c4372e894aaa2a2
Joshua Alley <joshua.alley@gsa.gov>
2026-06-22 15:34:34 -0700

    add sensitive false to reset password key

--- db/primary_migrate/20260616161534_add_reset_password_email_to_users.rb
+++ db/primary_migrate/20260616161534_add_reset_password_email_to_users.rb
@@ -1,5 +1,5 @@
 class AddResetPasswordEmailToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :reset_password_email, :string, limit: 255
+    add_column :users, :reset_password_email, :string, limit: 255, comment: 'sensitive=false'
   end
 end
