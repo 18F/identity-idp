7848d4a815d4366b4eac2ea00f311547e0873a9c
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-07-10 15:00:09 -0400

    update migration

--- db/primary_migrate/20260710150518_add_mdl_enabled_to_document_capture_sessions_w_comment.rb
+++ db/primary_migrate/20260710150518_add_mdl_enabled_to_document_capture_sessions_w_comment.rb
@@ -1,9 +1,5 @@
 class AddMdlEnabledToDocumentCaptureSessionsWComment < ActiveRecord::Migration[8.0]
-  def up
+  def change
     add_column :document_capture_sessions, :mdl_enabled, :boolean, comment: 'sensitive=false'
-  end
-
-  def down
-    remove_column :document_capture_sessions, :mdl_enabled
   end
 end
