e8b064e7a1648600371b67668d7ec7d05f17174b
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-07-29 10:29:09 -0400

    temp rename spec

--- spec/services/doc_auth/lexis_nexis/responses/ddp/true_id_response_spec.rb
+++ spec/services/doc_auth/lexis_nexis/responses/ddp/true_id_response_spec.rb
@@ -365,7 +365,7 @@ RSpec.describe DocAuth::LexisNexis::Responses::Ddp::TrueIdResponse do
       let(:passport_cards_supported) { true }
       let(:ddp_response_body) { LexisNexisFixtures.ddp_true_id_passport_card_response_success }
 
-      it 'is a successful result' do
+      it 'is an usuccessful result' do
         expect(response.success?).to eq(false)
       end
     end
