d7488479a368712499c068859bebd6c7cb619661
Davida Marion <davida.marion@gsa.gov>
2026-08-04 08:58:11 -0400

    Adding failure cases

--- app/controllers/concerns/idv/historical_attempts_concern.rb
+++ app/controllers/concerns/idv/historical_attempts_concern.rb
@@ -9,15 +9,17 @@ module Idv
       # we always need to cache events if the feature is enabled
       # in case we have to re-encrypt them
       return unless IdentityConfig.store.historical_attempts_api_enabled
-      return unless current_user.active_profile.present?
+      return unless profile.present?
 
       AttemptsApi::Cacher.new(current_user, user_session).save(password:)
     end
 
     def send_historic_events?
       return false, :idv_not_requested unless idv_requested?
+      return false, :no_active_profile if profile.blank?
       return false, :no_user_proofing_event if existing_user_proofing_event.blank?
       return false, :already_sent if existing_user_proofing_event.already_sent_to_sp?(current_sp.id)
+      return false, :no_encryped_file_reference if profile.encrypted_attempts_file_reference.blank?
 
       return true, nil
     end
@@ -32,8 +34,12 @@ module Idv
       IdentityConfig.store.historical_attempts_api_enabled
     end
 
+    def profile
+      @profile ||= current_user.active_profile
+    end
+
     def existing_user_proofing_event
-      @existing_user_proofing_event ||= current_user.active_profile.user_proofing_event
+      @existing_user_proofing_event ||= profile.user_proofing_event
     end
   end
 end
