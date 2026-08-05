aff5e90029deca36be4e0ca60f348e6b1016d467
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-08-05 11:55:56 -0400

    fix typo

--- app/services/proofing/clear/requests/session_request.rb
+++ app/services/proofing/clear/requests/session_request.rb
@@ -20,7 +20,7 @@ module Proofing
           FormResponse.new(
             success: true,
             extra: response_body.slice(
-              :id, :object_name, :projet_id, :redirect_url, :expires_at, :created_at, :status, :token, 
+              :id, :object_name, :project_id, :redirect_url, :expires_at, :created_at, :status, :token,
             ),
           )
         rescue => exception
