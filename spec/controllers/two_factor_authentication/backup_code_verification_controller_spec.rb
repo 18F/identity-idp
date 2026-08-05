5a4a6a794d817bab7f7361ae1c4c4de878adf1cd
Malick DiarrA <malick.diarra@gsa.gov>
2026-08-05 13:15:05 -0400

    fix rspec

--- spec/controllers/two_factor_authentication/backup_code_verification_controller_spec.rb
+++ spec/controllers/two_factor_authentication/backup_code_verification_controller_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe TwoFactorAuthentication::BackupCodeVerificationController do
         controller.session[:sign_in_recaptcha_assessment_id] = assessment_id
 
         expect(RecaptchaAnnotator).to receive(:annotate)
-          .with(**recaptcha_annotation)
+          .with(**recaptcha_annotation, analytics: kind_of(FakeAnalytics))
           .and_return(recaptcha_annotation)
 
         stub_sign_in_before_2fa(user)
