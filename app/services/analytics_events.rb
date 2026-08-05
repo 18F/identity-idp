54ab0b71b13ef406318a7d3a94a931f6f194f83c
Davi (she/they) <davida.marion@gsa.gov>
2026-08-05 08:49:56 -0400

    Add analytic events for historic attempts (#13289)
    * changelog: Internal, Historic Attempts Analytics, Add analytic events for Historic Attempt data capture and release

--- app/services/analytics_events.rb
+++ app/services/analytics_events.rb
@@ -854,6 +854,34 @@ module AnalyticsEvents
     )
   end
 
+  # Historic Attempt data was destroyed when a user deleted their account
+  # It deletes ALL historic data, not just the most recent active profile
+  def historic_event_data_destroyed
+    track_event(:historic_event_data_destroyed)
+  end
+
+  # Historic data was potentially sent when a user accessed an Attempts Api Consumer App
+  # @param [Boolean] success Whether the historic attempt data was released
+  # rubocop:disable Layout/LineLength
+  # @param [:idv_not_requested, :no_user_proofing_event, :already_sent, :no_encrypted_file_reference nil] exception
+  # rubocop:enable Layout/LineLength
+  # @param [Integer,nil] profile_id ID of the active profile associated with attempts data
+  def historic_event_data_released(success:, exception: nil, profile_id: nil, **extra)
+    track_event(
+      :historic_event_data_released,
+      success:,
+      exception:,
+      profile_id:,
+      **extra,
+    )
+  end
+
+  # @param [Integer] profile_id ID of the active profile associated with attempts data
+  # Historic Attempt data was saved when a user completed the IdV process
+  def historic_event_data_saved(profile_id:, **extra)
+    track_event(:historic_event_data_saved, profile_id:, **extra)
+  end
+
   # User visited sign-in URL from the "You've been successfully verified email" CTA button
   # @param issuer [String] the ServiceProvider.issuer
   # @param campaign_id [String] the email campaign ID
