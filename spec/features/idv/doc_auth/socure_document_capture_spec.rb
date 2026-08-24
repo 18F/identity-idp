7ecb87439d74e4a3e0566bc1b12d68e81f0dcbda
Mawar2 <mawar2@icloud.com>
2026-08-24 15:52:14 -0400

    LG-17876 Log an event when users continue to the Socure DocV capture app
    changelog: Internal, Identity Verification, Log an event when users continue to the Socure DocV capture app

--- spec/features/idv/doc_auth/socure_document_capture_spec.rb
+++ spec/features/idv/doc_auth/socure_document_capture_spec.rb
@@ -498,6 +498,10 @@ RSpec.feature 'document capture step', :js, driver: :headless_chrome_mobile do
 
           expect(DocAuthLog.find_by(user_id: user.id).state).to eq('MD')
           expect(fake_analytics).to have_logged_event(
+            :idv_socure_docv_redirect_requested,
+            document_type_requested: Idp::Constants::DocumentTypes::STATE_ID_CARD,
+          )
+          expect(fake_analytics).to have_logged_event(
             :idv_socure_document_request_submitted,
           )
           expect(fake_analytics).to have_logged_event(
@@ -605,6 +609,10 @@ RSpec.feature 'document capture step', :js, driver: :headless_chrome_mobile do
 
             expect(page).to have_current_path(idv_ssn_url)
 
+            expect(fake_analytics).to have_logged_event(
+              :idv_socure_docv_redirect_requested,
+              document_type_requested: Idp::Constants::DocumentTypes::MDL,
+            )
             expect(fake_analytics).to have_logged_event(
               :idv_socure_document_request_submitted,
             )
