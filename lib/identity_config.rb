ba90a1480108412961f981ad69272f8b61205828
Chris Ameter <29186822+ameter@users.noreply.github.com>
2026-09-01 11:54:49 -0600

    Joan-117: Add a per-vendor feature flag to control AAMVA get-to-yes and fix DDP Instant Verify missing additional verification attributes (#13443)
    changelog: Internal, Identity Verification, Add a per-vendor feature flag to control AAMVA get-to-yes and fix DDP Instant Verify missing additional verification attributes
    
    Co-authored-by: Shane Chesnutt <shane.chesnutt@gsa.gov>

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -291,6 +291,7 @@ module IdentityConfig
     config.add(:idv_socure_reason_codes_docv_selfie_pass, type: :json)
     config.add(:idv_sp_required, type: :boolean)
     config.add(:idv_aamva_bypass_exception_ids, type: :json)
+    config.add(:idv_aamva_get_to_yes_enabled_vendors, type: :json)
     config.add(:idv_aamva_split_last_name_states, type: :json)
     config.add(:in_person_completion_survey_delivery_enabled, type: :boolean)
     config.add(:in_person_completion_survey_url, type: :string)
