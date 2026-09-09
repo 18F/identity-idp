211e1f67cf2a8c9819cb386ea52dbfe9f05ccf5d
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-09-09 16:09:21 -0400

    profiles authed with mdl yield enhanced profile
    changelog: Bug Fixes, Identity Verification, MDL authed profiles result in enhanced profiles

--- app/services/idv/profile_maker.rb
+++ app/services/idv/profile_maker.rb
@@ -35,6 +35,7 @@ module Idv
       profile.idv_level = set_idv_level(
         in_person_verification_needed: in_person_verification_needed,
         selfie_check_performed: selfie_check_performed,
+        digital_id: IdP::Constants::DIGITAL_ID_TYPES.include?(pii_attributes[:document_type]),
         proofing_agent_requested: proofing_agent_requested,
       )
 
@@ -52,6 +53,7 @@ module Idv
     def set_idv_level(
       in_person_verification_needed:,
       selfie_check_performed:,
+      digital_id:,
       proofing_agent_requested:
     )
       if in_person_verification_needed
@@ -61,6 +63,8 @@ module Idv
         else
           :legacy_in_person
         end
+      elsif digital_id
+        :unsupervised_with_digital_id
       elsif selfie_check_performed
         :unsupervised_with_selfie
       elsif proofing_agent_requested
