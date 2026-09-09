e2f161ddc818dbdbaf72c0d4549579871e20cdd7
Davida Marion <davida.marion@gsa.gov>
2026-09-09 15:47:31 -0400

    Update cacher to pass profile directly in for save

--- spec/services/attempts_api/cacher_spec.rb
+++ spec/services/attempts_api/cacher_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe AttemptsApi::Cacher do
     end
 
     it 'encrypts and saves proofing events in the session' do
-      subject.save(password:)
+      subject.save(password:, profile:)
 
       expect(user_session[:encrypted_proofing_events]).to be_present
 
@@ -43,7 +43,7 @@ RSpec.describe AttemptsApi::Cacher do
       context 'no decrypted events are returned' do
         let(:returned_events) { nil }
         it 'does not attempt to encrypt events' do
-          expect { subject.save(password:) }.to_not raise_error
+          expect { subject.save(password:, profile:) }.to_not raise_error
 
           expect(user_session[:encrypted_proofing_events]).to be_blank
         end
