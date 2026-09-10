0d3b4fd3bf7bdd2a0b64a62073cebc99c7477fd0
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2026-09-10 14:34:40 -0400

    LG-17924: profiles authed with mdl yield enhanced profile (#13534)
    * profiles authed with mdl yield enhanced profile
    
    changelog: Bug Fixes, Identity Verification, MDL authed profiles result in enhanced profiles
    
    * pass document type received instead
    
    * add specs to ensure idv_level for mdl
    
    * happy linting
    
    * fix typo

--- spec/controllers/idv/enter_password_controller_spec.rb
+++ spec/controllers/idv/enter_password_controller_spec.rb
@@ -317,6 +317,12 @@ RSpec.describe Idv::EnterPasswordController do
       expect(response).to redirect_to idv_personal_key_path
     end
 
+    it 'creates the profile with idv_level of legacy_unsupervised' do
+      put :create, params: { user: { password: ControllerHelper::VALID_PASSWORD } }
+
+      expect(user.profiles.last.idv_level).to eq('legacy_unsupervised')
+    end
+
     it 'redirects to confirmation path after user presses the back button' do
       put :create, params: { user: { password: ControllerHelper::VALID_PASSWORD } }
       allow_any_instance_of(User).to receive(:active_profile).and_return(true)
@@ -552,6 +558,12 @@ RSpec.describe Idv::EnterPasswordController do
           subject.idv_session.applicant =
             Idp::Constants::MOCK_IDV_APPLICANT_SAME_ADDRESS_AS_ID_WITH_PHONE
           allow(IdentityConfig.store).to receive(:in_person_proofing_enabled).and_return(true)
+        end
+
+        it 'creates the profile with idv_level of legacy_in_person' do
+          put :create, params: { user: { password: ControllerHelper::VALID_PASSWORD } }
+
+          expect(user.profiles.last.idv_level).to eq('legacy_in_person')
         end
 
         it 'redirects to personal key path' do
@@ -1173,6 +1185,48 @@ RSpec.describe Idv::EnterPasswordController do
               subject: t('user_mailer.idv_please_call.subject'),
             )
           end
+        end
+      end
+    end
+
+    context 'user submited a mobile drivers license' do
+      before do
+        subject.idv_session.pii_from_doc = Pii::StateId.new(
+          **Idp::Constants::MOCK_IDV_APPLICANT
+            .merge(document_type_received: Idp::Constants::DocumentTypes::MDL),
+        )
+      end
+
+      it 'creates the profile with idv_level of unsupervised_with_digital_id' do
+        put :create, params: { user: { password: ControllerHelper::VALID_PASSWORD } }
+
+        expect(user.profiles.last.idv_level).to eq('unsupervised_with_digital_id')
+      end
+    end
+
+    context 'selfie check was performed' do
+      before do
+        subject.idv_session.selfie_check_performed = true
+      end
+
+      it 'creates the profile with idv_level of unsupervised_with_selfie' do
+        put :create, params: { user: { password: ControllerHelper::VALID_PASSWORD } }
+
+        expect(user.profiles.last.idv_level).to eq('unsupervised_with_selfie')
+      end
+
+      context 'user submited a mobile drivers license' do
+        before do
+          subject.idv_session.pii_from_doc = Pii::StateId.new(
+            **Idp::Constants::MOCK_IDV_APPLICANT
+              .merge(document_type_received: Idp::Constants::DocumentTypes::MDL),
+          )
+        end
+
+        it 'creates the profile with idv_level of unsupervised_with_digital_id' do
+          put :create, params: { user: { password: ControllerHelper::VALID_PASSWORD } }
+
+          expect(user.profiles.last.idv_level).to eq('unsupervised_with_digital_id')
         end
       end
     end
