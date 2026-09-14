154f257919a0a9e5ca5f9d95659122505bd1d644
Davida Marion <davida.marion@gsa.gov>
2026-09-14 15:02:24 -0400

    Updating active user count specs

--- spec/services/db/identity/sp_user_counts_spec.rb
+++ spec/services/db/identity/sp_user_counts_spec.rb
@@ -20,16 +20,17 @@ RSpec.describe Db::Identity::SpUserCounts do
     end
 
     it 'returns the total user counts per sp broken down by ial1 and ial2' do
-      ServiceProviderIdentity.create(user_id: user1.id, service_provider: issuer, uuid: 'foo1')
-      ServiceProviderIdentity.create(user_id: user2.id, service_provider: issuer, uuid: 'foo2')
+      ServiceProviderIdentity.create(user_id: user1.id, service_provider: issuer, ial: 1)
+      ServiceProviderIdentity.create(user_id: user2.id, service_provider: issuer, ial: 1)
       ServiceProviderIdentity.create(
-        user_id: user3.id, service_provider: issuer, uuid: 'foo3',
+        user_id: user3.id, service_provider: issuer, ial: 2,
         verified_at: Time.zone.now
       )
       ServiceProviderIdentity.create(
-        user_id: proofed_user.id, service_provider: issuer2, uuid: 'foo4',
+        user_id: proofed_user.id, service_provider: issuer2, ial: 2,
         verified_at: Time.zone.now
       )
+
       result = { issuer:, total: 3, ial1_total: 2, ial2_total: 1, app_id: }.to_json
       result2 = { issuer: issuer2, total: 1, ial1_total: 0, ial2_total: 1, app_id: app_id2 }.to_json
 
