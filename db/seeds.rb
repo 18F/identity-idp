04a669370c56ef61e5b0e0fe4bca733d9ce4cb43
Jeremy Curcio <jeremy.curcio@gsa.gov>
2026-09-14 11:12:43 -0400

    Add logging to seeders (#13543)
    * Add logging to seeders
    
    changelog: Internal, Database Seeding, Log seeder start/success/failure and continue on failure so db:seed reports every seeder's outcome

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+seed_runner = SeedRunner.new
+
 # add config/service_providers.yml
 if ENV['KUBERNETES_REVIEW_APP'] == 'true' && ENV['DASHBOARD_URL'].present?
   dashboard_url = ENV['DASHBOARD_URL']
@@ -7,23 +9,28 @@ if ENV['KUBERNETES_REVIEW_APP'] == 'true' && ENV['DASHBOARD_URL'].present?
   # This should never be invoked in production.
   # If we change how production is deployed, we should revisit the above conditionals to ensure
   # production never runs this.
-  ServiceProviderSeeder.new.run_review_app(dashboard_url: dashboard_url)
-  ReviewAppUserSeeder.new.run
+  seed_runner.run(
+    ServiceProviderSeeder.new,
+    name: 'ServiceProviderSeeder#run_review_app',
+  ) { |seeder| seeder.run_review_app(dashboard_url: dashboard_url) }
+  seed_runner.run(ReviewAppUserSeeder.new, &:run)
 else
-  ServiceProviderSeeder.new.run
+  seed_runner.run(ServiceProviderSeeder.new, &:run)
 end
 
 # add config/agencies.yml
-AgencySeeder.new.run
+seed_runner.run(AgencySeeder.new, &:run)
 
 # add partnerships / agreements data, note that the order matters!
 if IdentityConfig.store.seed_agreements_data
   Rails.logger.info('=== Seeding agreements data ===')
 
-  Agreements::PartnerAccountStatusSeeder.new.run
-  Agreements::PartnerAccountSeeder.new.run
-  Agreements::IaaGtcSeeder.new.run
-  Agreements::IntegrationStatusSeeder.new.run
-  Agreements::IntegrationSeeder.new.run
-  Agreements::IaaOrderSeeder.new.run
+  seed_runner.run(Agreements::PartnerAccountStatusSeeder.new, &:run)
+  seed_runner.run(Agreements::PartnerAccountSeeder.new, &:run)
+  seed_runner.run(Agreements::IaaGtcSeeder.new, &:run)
+  seed_runner.run(Agreements::IntegrationStatusSeeder.new, &:run)
+  seed_runner.run(Agreements::IntegrationSeeder.new, &:run)
+  seed_runner.run(Agreements::IaaOrderSeeder.new, &:run)
 end
+
+seed_runner.finish!
