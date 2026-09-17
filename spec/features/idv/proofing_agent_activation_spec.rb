4d8c0096aaa240b3d2742b68e2fe33504fbd3df9
Alex Bradley <alexander.bradley@gsa.gov>
2026-09-17 12:16:13 -0400

    add feature spec

--- spec/features/idv/proofing_agent_activation_spec.rb
+++ spec/features/idv/proofing_agent_activation_spec.rb
@@ -80,6 +80,17 @@ RSpec.describe 'Proofing agent activation', :js do
     end
   end
 
+  context 'user tries going to enter password page without binding' do
+    scenario 'user is redirected to binding page' do
+      visit_idp_from_ial2_oidc_sp
+      sign_in_live_with_2fa(user)
+      expect(page).to have_current_path idv_enter_dob_ssn_path
+      binding.pry
+      visit idv_enter_password_path
+      expect(page).to have_current_path idv_enter_dob_ssn_path
+    end
+  end
+
   scenario 'user activates their profile and connects it to the service provider' do
     sign_in_live_with_2fa(user)
     expect(page).to have_current_path idv_enter_dob_ssn_path
