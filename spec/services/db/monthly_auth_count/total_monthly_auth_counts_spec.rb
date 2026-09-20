f4ed5aebadd62b4c8583e492479bb36a4d5704c3
Davi (she/they) <davida.marion@gsa.gov>
2026-09-19 22:59:08 -0400

    Fix flaky specs (#13568)
    * changelog: Internal, Fix Flaky Spec, Create users rather than relying on hard-coded user ids
    
    * Updating active user count specs
    
    * Update sp_active_users_report_spec
    
    * Update the rest of the specs user issuer 'foo'
    
    * cleanup%

--- spec/services/db/monthly_auth_count/total_monthly_auth_counts_spec.rb
+++ spec/services/db/monthly_auth_count/total_monthly_auth_counts_spec.rb
@@ -3,21 +3,25 @@ require 'rails_helper'
 RSpec.describe Db::MonthlySpAuthCount::TotalMonthlyAuthCounts do
   subject { described_class }
 
-  let(:issuer) { 'foo' }
   let(:app_id) { 'app_id' }
+  let(:sp) { create(:service_provider, app_id:) }
+  let(:issuer) { sp.issuer }
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:user3) { create(:user) }
+  let(:year_month) { '201901' }
 
   it 'is empty' do
     expect(subject.call.length).to eq(0)
   end
 
   it 'returns the total auth counts' do
-    ServiceProvider.create(issuer: issuer, friendly_name: issuer, app_id: app_id)
     7.times do
       create(
         :sp_return_log,
-        issuer: issuer,
+        issuer:,
         ial: 1,
-        user_id: 2,
+        user_id: user1.id,
         returned_at: Date.new(2019, 1, 15),
         billable: true,
       )
@@ -25,9 +29,9 @@ RSpec.describe Db::MonthlySpAuthCount::TotalMonthlyAuthCounts do
     3.times do
       create(
         :sp_return_log,
-        issuer: issuer,
+        issuer:,
         ial: 1,
-        user_id: 3,
+        user_id: user2.id,
         returned_at: Date.new(2019, 1, 15),
         billable: true,
       )
@@ -36,20 +40,20 @@ RSpec.describe Db::MonthlySpAuthCount::TotalMonthlyAuthCounts do
     2.times do
       create(
         :sp_return_log,
-        issuer: issuer,
+        issuer:,
         ial: 1,
-        user_id: 3,
+        user_id: user3.id,
         returned_at: Date.new(2019, 2, 10),
         billable: true,
       )
     end
 
     first_month_result = {
-      issuer: issuer,
+      issuer:,
       ial: 1,
-      year_month: '201901',
+      year_month:,
       total: 10,
-      app_id: app_id,
+      app_id:,
     }.stringify_keys
 
     second_month_result = {
@@ -57,7 +61,7 @@ RSpec.describe Db::MonthlySpAuthCount::TotalMonthlyAuthCounts do
       ial: 1,
       year_month: '201902',
       total: 2,
-      app_id: app_id,
+      app_id:,
     }.stringify_keys
 
     result = subject.call
